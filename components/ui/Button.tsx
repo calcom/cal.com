6023a8a3144a7a6b883b9a102b48c40fa4764416
Bailey Pumfleet <bpumfleet@gmail.com>
2021-05-27 21:34:02 +0100

    Add loading spinner to book button

---
+++ components/ui/Button.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+export default function Button(props) {
+    const [loading, setLoading] = useState(false);
+
+    return(
+        <button type="submit" className="btn btn-primary" onClick={setLoading}>
+            {!loading && props.children}
+            {loading && 
+                <svg className="animate-spin mx-4 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+            }
+        </button>
+    );
+}
