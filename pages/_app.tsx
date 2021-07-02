dcf274456674c7e6ab7efd14c50e1a7a9b17e0b6
Mihai C <34626017+mihaic195@users.noreply.github.com>
2021-07-02 13:32:45 +0300

    Update pages/_app.tsx
    Co-authored-by: Bailey Pumfleet <pumfleet@hey.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import {createTelemetryClient, TelemetryProvider} from '../lib/telemetry';
 import { Provider } from 'next-auth/client';
-import type { AppProps } from "next/app"
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
