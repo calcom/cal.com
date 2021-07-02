2af9ed55a7a45acb92ca4e872169ab52289091ce
Mihai C <34626017+mihaic195@users.noreply.github.com>
2021-07-02 13:32:11 +0300

    Update pages/settings/teams.tsx
    Co-authored-by: Bailey Pumfleet <pumfleet@hey.com>

--- pages/settings/teams.tsx
+++ pages/settings/teams.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next"
+import { GetServerSideProps } from "next";
 import Head from 'next/head';
 import Shell from '../../components/Shell';
 import SettingsShell from '../../components/Settings';
