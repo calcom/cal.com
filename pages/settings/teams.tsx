8500a8dd64e1ab5daa6ff7413ad7451a8e91d4a9
Mihai C <34626017+mihaic195@users.noreply.github.com>
2021-07-02 13:31:59 +0300

    Update pages/settings/teams.tsx
    Co-authored-by: Bailey Pumfleet <pumfleet@hey.com>

--- pages/settings/teams.tsx
+++ pages/settings/teams.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import Shell from '../../components/Shell';
 import SettingsShell from '../../components/Settings';
 import { useEffect, useState } from 'react';
-import type { Session } from "next-auth"
+import type { Session } from "next-auth";
 import { useSession, getSession } from 'next-auth/client';
 import {
   UsersIcon,
