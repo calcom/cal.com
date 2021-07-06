47d7634638ef0f5c5798b7d5aff510bbfa6657dc
Alex van Andel <me@alexvanandel.com>
2021-07-06 16:10:22 +0000

    Adds prisma helper function whereAndSelect

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,9 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 let prisma: PrismaClient;
-const globalAny:any = global;
+const globalAny: any = global;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
   if (!globalAny.prisma) {
@@ -11,5 +11,19 @@ if (process.env.NODE_ENV === 'production') {
   }
   prisma = globalAny.prisma;
 }
+
+const whereAndSelect = (modelQuery, criteria: Record<string, unknown>, pluckedAttributes: string[]) =>
+  modelQuery({
+    where: criteria,
+    select: pluckedAttributes.reduce(
+      (select: { [string]: boolean }, attr: string) => ({
+        ...select,
+        [attr]: true,
+      }),
+      {}
+    ),
+  });
+
+export { whereAndSelect };
 
 export default prisma;
