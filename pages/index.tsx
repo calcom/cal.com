7c5c40024d9dda87416bae6dd1e690faf239d85c
femyeda <femi@individual.company>
2021-07-27 15:40:59 -0500

    if using team domain display team screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,50 @@
 import Loader from "@components/Loader";
 import { useRouter } from "next/router";
+import { getTeamFromContext } from "../lib/getTeam";
+import Head from "next/head";
+import Team from "@components/team/screens/Team";
 
-function RedirectPage() {
+function RedirectPage(props) {
   const router = useRouter();
+  if (props.team) {
+    return (
+      <div>
+        <Head>
+          <title>{props.team.name} | Calendso</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main className="max-w-2xl mx-auto my-24">
+          <Team team={props.team} />
+        </main>
+      </div>
+    );
+  }
+
   if (typeof window !== "undefined") {
     router.push("/event-types");
     return;
   }
-  return (
-    <Loader/>
-  );
+
+  return <Loader />;
 }
 
-RedirectPage.getInitialProps = (ctx) => {
-  if (ctx.res) {
-    ctx.res.writeHead(302, { Location: "/event-types" });
-    ctx.res.end();
+export async function getServerSideProps(context) {
+  const team = await getTeamFromContext(context);
+
+  if (team) {
+    return {
+      props: {
+        team,
+      },
+    };
+  }
+
+  if (context.res) {
+    context.res.writeHead(302, { Location: "/event-types" });
+    context.res.end();
   }
   return {};
-};
+}
 
 export default RedirectPage;
