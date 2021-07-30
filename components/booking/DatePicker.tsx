9f8b1f372e4a619652f08e4524342c628473a113
Marc Seitz <marc@mfts.io>
2021-07-30 15:48:51 +0300

    add dark and light variants chevron color

--- components/booking/DatePicker.tsx
+++ components/booking/DatePicker.tsx
@@ -171,10 +171,13 @@ const DatePicker = ({
         <span className="w-1/2 text-gray-600 dark:text-white">
           {dayjs().month(selectedMonth).format("MMMM YYYY")}
         </span>
-        <div className="w-1/2 text-right text-gray-400">
+        <div className="w-1/2 text-right text-gray-600 dark:text-gray-400">
           <button
             onClick={decrementMonth}
-            className={"mr-4 " + (selectedMonth <= dayjs().tz(inviteeTimeZone).month() && "text-gray-600")}
+            className={
+              "mr-4 " +
+              (selectedMonth <= dayjs().tz(inviteeTimeZone).month() && "text-gray-400 dark:text-gray-600")
+            }
             disabled={selectedMonth <= dayjs().tz(inviteeTimeZone).month()}>
             <ChevronLeftIcon className="w-5 h-5" />
           </button>
