5515db7e9df73402a0c15f460b2469c433e1217d
Lola-Ojabowale <lola.ojabowale@gmail.com>
2021-08-15 18:42:52 -0400

    modifying how daily emails send

--- lib/emails/VideoEventAttendeeMail.ts
+++ lib/emails/VideoEventAttendeeMail.ts
@@ -1,6 +1,8 @@
 import EventAttendeeMail from "./EventAttendeeMail";
 import { getFormattedMeetingId, getIntegrationName } from "./helpers";
 
+
+
 export default class VideoEventAttendeeMail extends EventAttendeeMail {
   /**
    * Adds the video call information to the mail body.
--- lib/emails/VideoEventOrganizerMail.ts
+++ lib/emails/VideoEventOrganizerMail.ts
@@ -38,4 +38,12 @@ export default class VideoEventOrganizerMail extends EventOrganizerMail {
     }</a><br />
     `;
   }
+  if (isDaily){
+    return `
+<strong>Video call provider:</strong> ${getIntegrationName(this.videoCallData)}<br />
+<strong>Meeting URL:</strong> <a href="${this.videoCallData.url}">${this.videoCallData.url}</a><br />
+    `;
+  }
+}
+
 }
