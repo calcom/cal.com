351712f06ff7e8d57f5e672bdde318199b36341d
Lola-Ojabowale <lola.ojabowale@gmail.com>
2021-08-15 18:42:52 -0400

    modifying how daily emails send

--- lib/emails/VideoEventAttendeeMail.ts
+++ lib/emails/VideoEventAttendeeMail.ts
@@ -5,6 +5,8 @@ import { VideoCallData } from "../videoClient";
 import EventAttendeeMail from "./EventAttendeeMail";
 import { getFormattedMeetingId, getIntegrationName } from "./helpers";
 
+
+
 export default class VideoEventAttendeeMail extends EventAttendeeMail {
   videoCallData: VideoCallData;
 
@@ -25,11 +27,20 @@ export default class VideoEventAttendeeMail extends EventAttendeeMail {
    * @protected
    */
   protected getAdditionalBody(): string {
+    const isDaily = this.videoCallData.type === "Daily Video Chat & Conferencing"
+    if (!isDaily){
     return `
       <strong>Video call provider:</strong> ${getIntegrationName(this.videoCallData)}<br />
       <strong>Meeting ID:</strong> ${getFormattedMeetingId(this.videoCallData)}<br />
       <strong>Meeting Password:</strong> ${this.videoCallData.password}<br />
       <strong>Meeting URL:</strong> <a href="${this.videoCallData.url}">${this.videoCallData.url}</a><br />
     `;
+    }
+    if (isDaily){
+      return `
+      <strong>Video call provider:</strong> ${getIntegrationName(this.videoCallData)}<br />
+      <strong>Meeting URL:</strong> <a href="${this.videoCallData.url}">${this.videoCallData.url}</a><br />
+    `;
+    }
   }
 }
