3387159d8420f9186931c332f5c86fcf2b1ef0d1
tung mac <tungmm@zigvy.com>
2021-08-16 10:07:47 +0700

    update code

--- pages/api/amili/integration/zoomvideo/add.ts
+++ pages/api/amili/integration/zoomvideo/add.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const client_id = process.env.ZOOM_CLIENT_ID;
+const publishare_client_id = process.env.ZOOM_PUBLISHABLE_CLIENT_ID;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const addIntegration = async (req: NextApiRequest, res: NextApiResponse): Promise<any> => {
@@ -14,10 +15,9 @@ const addIntegration = async (req: NextApiRequest, res: NextApiResponse): Promis
   const redirectUri = encodeURI(
     `${process.env.BASE_URL}/api/amili/integration/zoomvideo/callback?info=${assUserId}*${coachId}*${isCoachUser}`
   );
-  const authUrl = `https://zoom.us/oauth/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirectUri}`;
 
-  //Use publishable URL for testing
-  // const authUrl = `https://zoom.us/oauth/authorize?response_type=code&client_id=a6cVrnc3RGi0A9BdR9msQg&redirect_uri=${redirectUri}`;
+  const mainClientId = process.env.NODE_ENV !== "production" ? publishare_client_id : client_id;
+  const authUrl = `https://zoom.us/oauth/authorize?response_type=code&client_id=${mainClientId}&redirect_uri=${redirectUri}`;
 
   return res.status(200).json({ url: authUrl });
 };
