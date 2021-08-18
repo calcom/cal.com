36bfdbb046d245b83e4582290efb14b379f723f5
KATT <alexander@n1s.se>
2021-08-18 10:39:56 +0200

    fix types for auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,11 @@
-import { hash, compare } from 'bcryptjs';
+import { hash, compare } from "bcryptjs";
 
-export async function hashPassword(password) {
-    const hashedPassword = await hash(password, 12);
-    return hashedPassword;
+export async function hashPassword(password: string) {
+  const hashedPassword = await hash(password, 12);
+  return hashedPassword;
 }
 
-export async function verifyPassword(password, hashedPassword) {
-    const isValid = await compare(password, hashedPassword);
-    return isValid;
+export async function verifyPassword(password: string, hashedPassword: string) {
+  const isValid = await compare(password, hashedPassword);
+  return isValid;
 }
