87e9f3905bdeeecfeec4881f5a44c8a5bb0223ec
KATT <alexander@n1s.se>
2021-08-18 10:58:38 +0200

    implement safer to use `getSession`

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,6 @@
-import { hash, compare } from "bcryptjs";
+import { compare, hash } from "bcryptjs";
+import { DefaultSession } from "next-auth";
+import { getSession as getSessionInner, GetSessionOptions } from "next-auth/client";
 
 export async function hashPassword(password: string) {
   const hashedPassword = await hash(password, 12);
@@ -8,4 +10,20 @@ export async function hashPassword(password: string) {
 export async function verifyPassword(password: string, hashedPassword: string) {
   const isValid = await compare(password, hashedPassword);
   return isValid;
+}
+
+type DefaultSessionUser = NonNullable<DefaultSession["user"]>;
+type CalendsoSessionUser = DefaultSessionUser & {
+  id: number;
+  username: string;
+};
+export interface CalendsoSession extends DefaultSession {
+  user?: CalendsoSessionUser;
+}
+
+export async function getSession(options: GetSessionOptions): Promise<CalendsoSession | null> {
+  const session = await getSessionInner(options);
+
+  // that these are equal are unsured in `[...nextauth]`'s callback
+  return session as CalendsoSession;
 }
--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,59 +1,73 @@
-import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers';
-import prisma from '../../../lib/prisma';
-import {verifyPassword} from "../../../lib/auth";
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import prisma from "../../../lib/prisma";
+import { CalendsoSession, verifyPassword } from "../../../lib/auth";
 
 export default NextAuth({
-    session: {
-        jwt: true
+  session: {
+    jwt: true,
+  },
+  pages: {
+    signIn: "/auth/login",
+    signOut: "/auth/logout",
+    error: "/auth/error", // Error code passed in query string as ?error=
+  },
+  providers: [
+    Providers.Credentials({
+      name: "Calendso",
+      credentials: {
+        email: { label: "Email Address", type: "email", placeholder: "john.doe@example.com" },
+        password: { label: "Password", type: "password", placeholder: "Your super secure password" },
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user) {
+          throw new Error("No user found");
+        }
+        if (!user.password) {
+          throw new Error("Incorrect password");
+        }
+
+        const isValid = await verifyPassword(credentials.password, user.password);
+
+        if (!isValid) {
+          throw new Error("Incorrect password");
+        }
+
+        return {
+          id: user.id,
+          username: user.username,
+          email: user.email,
+          name: user.name,
+          image: user.avatar,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt(token, user) {
+      // Add username to the token right after signin
+      if (user?.username) {
+        token.id = user.id;
+        token.username = user.username;
+      }
+      return token;
     },
-    pages: {
-        signIn: '/auth/login',
-        signOut: '/auth/logout',
-        error: '/auth/error', // Error code passed in query string as ?error=
-    },
-    providers: [
-        Providers.Credentials({
-            name: 'Calendso',
-            credentials: {
-                email: { label: "Email Address", type: "email", placeholder: "john.doe@example.com" },
-                password: { label: "Password", type: "password", placeholder: "Your super secure password" }
-            },
-            async authorize(credentials) {
-                const user = await prisma.user.findFirst({
-                    where: {
-                        email: credentials.email
-                    }
-                });
-
-                if (!user) {
-                    throw new Error('No user found');
-                }
-
-                const isValid = await verifyPassword(credentials.password, user.password);
-
-                if (!isValid) {
-                    throw new Error('Incorrect password');
-                }
-
-                return {id: user.id, username: user.username, email: user.email, name: user.name, image: user.avatar};
-            }
-        })
-    ],
-    callbacks: {
-        async jwt(token, user, account, profile, isNewUser) {
-            // Add username to the token right after signin
-            if (user?.username) {
-                token.id = user.id;
-                token.username = user.username;
-            }
-            return token;
+    async session(session, token) {
+      const calendsoSession: CalendsoSession = {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id as number,
+          username: token.username as string,
         },
-        async session(session, token) {
-            session.user = session.user || {}
-            session.user.id = token.id;
-            session.user.username = token.username;
-            return session;
-        },
+      };
+      return calendsoSession;
     },
+  },
 });
