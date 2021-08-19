e93fbbc922ab5d70a60f3049b780a0b084a5e582
Alex van Andel <me@alexvanandel.com>
2021-08-19 10:52:17 +0000

    Explicitly select which fields to expose in /api/me

--- pages/api/me.ts
+++ pages/api/me.ts
@@ -13,9 +13,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     where: {
       id: session.user.id,
     },
+    select: {
+      id: true,
+      username: true,
+      name: true,
+      email: true,
+      bio: true,
+      timeZone: true,
+      weekStart: true,
+      startTime: true,
+      endTime: true,
+      bufferTime: true,
+      theme: true,
+      createdDate: true,
+      hideBranding: true,
+      avatar: true,
+    },
   });
-
-  delete user.password;
 
   res.status(200).json(user);
 }
