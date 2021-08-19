507d4cc1ab76b26def39a5dc24081024aea82ad9
KATT <alexander@n1s.se>
2021-08-19 16:22:22 +0200

    fix

--- pages/api/integrations/googlecalendar/callback.ts
+++ pages/api/integrations/googlecalendar/callback.ts
@@ -27,7 +27,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   await prisma.credential.create({
     data: {
       type: "google_calendar",
-      key: key,
+      key,
       userId: session.user.id,
     },
   });
