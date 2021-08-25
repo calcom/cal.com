7f86096f2c7497a2543cc5f8f35f1f838106ee58
femyeda <femi@individual.company>
2021-08-24 23:19:28 -0500

    remove photo upload

--- pages/getting_started/index.tsx
+++ pages/getting_started/index.tsx
@@ -475,35 +475,6 @@ export default function Page(props: Props) {
                 A few sentences about yourself. This will appear on your personal url page.
               </Text>
             </fieldset>
-
-            <fieldset>
-              <label htmlFor="file-upload" className="block text-sm font-medium text-gray-700">
-                Profile Photo
-              </label>
-              <div className="flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                <div className="space-y-1 text-center">
-                  <svg
-                    className="mx-auto h-12 w-12 text-gray-400"
-                    stroke="currentColor"
-                    fill="none"
-                    viewBox="0 0 48 48"
-                    aria-hidden="true">
-                    <path
-                      d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                  <div className="flex text-sm text-gray-600">
-                    <span>Upload a file</span>
-                    <input id="file-upload" name="file-upload" type="file" className="sr-only" />
-                    <p className="pl-1">or drag and drop</p>
-                  </div>
-                  <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-                </div>
-              </div>
-            </fieldset>
           </section>
         </form>
       ),
