427ed49db4cc6258b2a07bee1a347ddbd7bba94a
femyeda <femi@individual.company>
2021-08-25 11:38:37 -0500

    remove unused code

--- lib/availability/mutations/updateUserAvailability.ts
+++
@@ -1,9 +0,0 @@
-import request from "@lib/request";
-
-export async function updateUserAvailability({ start, end, buffer }) {
-  request.update("/api/availability/day", {
-    start: start,
-    end: end,
-    buffer: buffer,
-  });
-}
