calcom/cal.com

commit 9844cf92a87949360b4f596e06eaa539404eccaa

Temporarily disable checking video busy times

pages/api/availability/[user].ts
@@ -31,12 +31,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     req.query.dateTo,
     selectedCalendars
   );
-  const videoBusyTimes = await getBusyVideoTimes(
-    currentUser.credentials,
-    req.query.dateFrom,
-    req.query.dateTo
-  );
-  calendarBusyTimes.push(...videoBusyTimes);
+  // const videoBusyTimes = await getBusyVideoTimes(
+  //   currentUser.credentials,
+  //   req.query.dateFrom,
+  //   req.query.dateTo
+  // );
+  // calendarBusyTimes.push(...videoBusyTimes);
 
   const bufferedBusyTimes = calendarBusyTimes.map((a) => ({
     start: dayjs(a.start).subtract(currentUser.bufferTime, "minute").toString(),

commit 9f32ae77a3ff02d5affe7890b3d1dda2e8734454

fix linting issue introduced in  9844cf92a87949360b4f596e06eaa539404eccaa

pages/api/availability/[user].ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@lib/prisma";
 import { getBusyCalendarTimes } from "@lib/calendarClient";
-import { getBusyVideoTimes } from "@lib/videoClient";
+// import { getBusyVideoTimes } from "@lib/videoClient";
 import dayjs from "dayjs";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {