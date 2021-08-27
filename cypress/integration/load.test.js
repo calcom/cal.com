a88d3c7073c8b4bf7eb7bff3727f8e5b1791128d
KATT <alexander@n1s.se>
2021-08-27 17:47:25 +0200

    fix test

--- cypress/integration/load.test.js
+++ cypress/integration/load.test.js
@@ -1,6 +1,8 @@
 /// <reference types="cypress" />
 
-test("load /", () => {
-  cy.visit("/");
-  cy.contains("Sign in to your account");
+describe("silly test", () => {
+  it("loads /", () => {
+    cy.visit("/");
+    cy.contains("Sign in to your account");
+  });
 });
