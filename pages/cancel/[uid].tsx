2c19032e91bfd45375da4c4451342b623185479f
Omar López <omar.lopez@housecallpro.com>
2021-08-27 13:34:56 -0600

    Fixes type inference in cancel page
    # Conflicts:
    #	pages/cancel/[uid].tsx

--- pages/cancel/[uid].tsx
+++ pages/cancel/[uid].tsx
@@ -7,7 +7,7 @@ import isBetween from "dayjs/plugin/isBetween";
 import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
 import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
@@ -50,7 +50,7 @@ export default function Type(props: InferGetServerSidePropsType<typeof getServer
       const successURL = "/cancel/success";
       const urlParams = [];
       if (props.user?.username) urlParams.push("user=" + props.user.username);
-      if (props.user?.title) urlParams.push("title=" + props.user.title);
+      if (props.booking?.title) urlParams.push("title=" + props.booking.title);
       if (from) urlParams.push("from=" + from);
       router.push(`${successURL}?${urlParams.join("&")}`);
     } else {
@@ -62,9 +62,9 @@ export default function Type(props: InferGetServerSidePropsType<typeof getServer
   return (
     <div>
       <HeadSeo
-        title={`Cancel ${props.booking && props.booking.title} | ${props.user.name || props.user.username}`}
+        title={`Cancel ${props.booking && props.booking.title} | ${props.user?.name || props.user?.username}`}
         description={`Cancel ${props.booking && props.booking.title} | ${
-          props.user.name || props.user.username
+          props.user?.name || props.user?.username
         }`}
       />
       <main className="max-w-3xl mx-auto my-24">
@@ -144,7 +144,7 @@ export default function Type(props: InferGetServerSidePropsType<typeof getServer
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
   const booking = await prisma.booking.findFirst({
     where: {
       uid: context.query.uid as string,
