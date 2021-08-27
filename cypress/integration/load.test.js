bf1ae408ce7aa00f8798f1c085a534ec1d0a0224
KATT <alexander@n1s.se>
2021-08-27 17:20:30 +0200

    clean

---
+++ cypress/integration/load.test.js
@@ -0,0 +1,3 @@
+/// <reference types="cypress" />
+
+describe("load /", () => { });
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -19,4 +20,4 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+};
