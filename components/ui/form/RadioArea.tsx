93958c14184bcb73654a9eacb6d2ffb77ec8c37b
Alex van Andel <me@alexvanandel.com>
2021-08-30 22:51:10 +0000

    Cleanup in seperate commit so I can cherrypick later

--- components/ui/form/RadioArea.tsx
+++
@@ -1,5 +0,0 @@
-import RadioAreaSelect from "@components/ui/form/radioarea/RadioAreaSelect";
-import RadioAreaInput from "@components/ui/form/radioarea/RadioAreaInput";
-import RadioAreaInputGroup from "@components/ui/form/radioarea/RadioAreaInputGroup";
-
-export { RadioAreaSelect, RadioAreaInput, RadioAreaInputGroup };
