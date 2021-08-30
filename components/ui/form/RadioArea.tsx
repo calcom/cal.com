f86fc35c089651bd6d489b7991964070e5084418
Alex van Andel <me@alexvanandel.com>
2021-08-30 18:27:13 +0000

    Make backup of current version of radio area pending refactor

---
+++ components/ui/form/RadioArea.tsx
@@ -0,0 +1,5 @@
+import RadioAreaSelect from "@components/ui/form/radioarea/RadioAreaSelect";
+import RadioAreaInput from "@components/ui/form/radioarea/RadioAreaInput";
+import RadioAreaInputGroup from "@components/ui/form/radioarea/RadioAreaInputGroup";
+
+export { RadioAreaSelect, RadioAreaInput, RadioAreaInputGroup };
---
+++ components/ui/form/radioarea/RadioAreaSelect.tsx
@@ -0,0 +1,64 @@
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@radix-ui/react-collapsible";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+import React, { useState } from "react";
+import { RadioAreaInput, RadioAreaInputGroup } from "@components/ui/form/RadioArea";
+import classNames from "@lib/classNames";
+
+export type Option = {
+  value?: string | readonly string[] | number;
+  label: string;
+  description?: string;
+};
+
+type RadioAreaSelectProps = React.SelectHTMLAttributes<HTMLSelectElement> & {
+  options: Option[];
+};
+
+export const RadioAreaSelect = function RadioAreaSelect(props: RadioAreaSelectProps) {
+  const options: Option[] = props.options || [];
+
+  const [option, setOption] = useState<Option | null>(
+    props.defaultValue
+      ? {
+          value: props.defaultValue,
+          label: props.options.find((opt) => opt.value === props.defaultValue).label,
+        }
+      : null
+  );
+
+  const changeHandler = (e) => {
+    if (props.onChange) {
+      props.onChange(e);
+    }
+    setOption(e);
+  };
+
+  return (
+    <Collapsible className={classNames("w-full", props.className)}>
+      <CollapsibleTrigger
+        as="button"
+        className={classNames(
+          "mb-1 cursor-pointer focus:outline-none focus:ring focus:ring-neutral-500 text-left border border-1 bg-white p-2 shadow-sm block w-full sm:text-sm border-gray-300 rounded-sm",
+          (!options.length || props.disabled) && "focus:ring-0 cursor-default bg-gray-200 "
+        )}>
+        {option?.label || props.placeholder || "Select..."}
+        <ChevronDownIcon className="float-right h-5 w-5 text-neutral-500" />
+      </CollapsibleTrigger>
+      <CollapsibleContent>
+        <RadioAreaInputGroup className="space-y-2 text-sm" name={props.name} onChange={changeHandler}>
+          {options.map((option) => (
+            <RadioAreaInput
+              {...option}
+              key={option.value}
+              defaultChecked={props.defaultValue === option.value}>
+              <strong className="block mb-1">{option.label}</strong>
+              <p>{option.description}</p>
+            </RadioAreaInput>
+          ))}
+        </RadioAreaInputGroup>
+      </CollapsibleContent>
+    </Collapsible>
+  );
+};
+
+export default RadioAreaSelect;
