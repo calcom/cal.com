fb7586f925d0c763d06111b300850012f379e972
Omar López <omar.lopez@housecallpro.com>
2021-08-31 08:52:07 -0600

    Moves stripe helpers and add client ones

---
+++ lib/stripe/client.ts
@@ -0,0 +1,17 @@
+/**
+ * This is a singleton to ensure we only instantiate Stripe once.
+ */
+import { Stripe, loadStripe } from "@stripe/stripe-js";
+
+let stripePromise: Promise<Stripe | null>;
+
+const getStripe = () => {
+  if (!stripePromise) {
+    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!, {
+      locale: "es-419",
+    });
+  }
+  return stripePromise;
+};
+
+export default getStripe;
