aec28966042c631f15fef8baf74df8b5c69abb79
KATT <alexander@n1s.se>
2021-09-02 17:56:51 +0200

    tweak

--- lib/types/inferSSRProps.ts
+++ lib/types/inferSSRProps.ts
@@ -4,7 +4,7 @@ type GetSSRResult<TProps> =
   //
   { props: TProps } | { redirect: any } | { notFound: true };
 
-type GetSSRFn<TProps extends any> = (args: any) => Promise<GetSSRResult<TProps>>;
+type GetSSRFn<TProps> = (...args: any[]) => Promise<GetSSRResult<TProps>>;
 
 export type inferSSRProps<TFn extends GetSSRFn<any>> = TFn extends GetSSRFn<infer TProps>
   ? NonNullable<TProps>
