22c21e2168c2c09736188d488c698926712d43af
Omar López <omar.lopez@housecallpro.com>
2021-09-03 16:17:31 -0600

    Fixes for dark mode

--- components/booking/CustomCardElement.tsx
+++ components/booking/CustomCardElement.tsx
@@ -1,3 +1,4 @@
+import useDarkMode from "@lib/core/browser/useDarkMode";
 import { CardElement, CardElementProps } from "@stripe/react-stripe-js";
 import React, { FC } from "react";
 
@@ -6,9 +7,27 @@ const CARD_OPTIONS = {
   classes: {
     base: "block p-2 w-full border-solid border-2 border-gray-300 rounded-md shadow-sm dark:bg-black dark:text-white dark:border-gray-900 focus-within:ring-black focus-within:border-black sm:text-sm",
   },
+  style: {
+    base: {
+      fontSmoothing: "antialiased",
+      color: "#000",
+      iconColor: "#000",
+      "::placeholder": {
+        color: "#888888",
+      },
+    },
+  },
 };
 
 const CustomCardElement: FC<CardElementProps> = (props) => {
+  const { isDarkMode } = useDarkMode();
+
+  if (isDarkMode) {
+    CARD_OPTIONS.style.base.color = "#fff";
+    CARD_OPTIONS.style.base.iconColor = "#fff";
+    CARD_OPTIONS.style.base["::placeholder"].color = "#fff";
+  }
+
   return <CardElement options={CARD_OPTIONS} {...props} />;
 };
 
