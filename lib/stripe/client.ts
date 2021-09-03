a8e553f24e59fd9b068f0924055a3c3e1aec5458
Omar López <omar.lopez@housecallpro.com>
2021-09-03 13:18:21 -0600

    Adds helpers for creating payment intents

--- lib/stripe/client.ts
+++ lib/stripe/client.ts
@@ -14,4 +14,20 @@ const getStripe = () => {
   return stripePromise;
 };
 
+export const createPaymentIntent = async (data) => {
+  const response = await fetch("/api/integrations/stripe/payment-intent", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  return response.json();
+};
+
 export default getStripe;
--- lib/stripe/server.ts
+++ lib/stripe/server.ts
@@ -1,7 +1,29 @@
+import { formatAmountForStripe } from "@lib/formatCurrency";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2020-08-27",
 });
+
+export async function createPaymentIntent(amount: number, connectedAccountId?: string) {
+  const formattedAmount = formatAmountForStripe(amount);
+  const params: Stripe.PaymentIntentCreateParams = {
+    payment_method_types: ["card"],
+    amount: formattedAmount,
+    currency: process.env.NEXT_PUBLIC_CURRENCY_CODE!,
+    application_fee_amount: formattedAmount * 0.1,
+  };
+  if (connectedAccountId) {
+    params.on_behalf_of = connectedAccountId;
+    params.transfer_data = {
+      destination: connectedAccountId,
+    };
+  }
+  const payment_intent: Stripe.PaymentIntent = await stripe.paymentIntents.create(params, {
+    stripeAccount: connectedAccountId,
+  });
+
+  return payment_intent;
+}
 
 export default stripe;
