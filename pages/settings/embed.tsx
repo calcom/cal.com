86404b8e104f7d7fe32c90c142ed6efdc125ed7e
Omar López <omar.lopez@housecallpro.com>
2021-09-08 09:29:09 -0600

    Rebranding

--- pages/settings/embed.tsx
+++ pages/settings/embed.tsx
@@ -18,8 +18,8 @@ export default function Embed(props) {
       <SettingsShell>
         <div className="py-6 lg:pb-8 lg:col-span-9">
           <div className="mb-6">
-            <h2 className="text-lg leading-6 font-medium text-gray-900">iframe Embed</h2>
-            <p className="mt-1 text-sm text-gray-500">The easiest way to embed Calendso on your website.</p>
+            <h2 className="text-lg font-medium leading-6 text-gray-900">iframe Embed</h2>
+            <p className="mt-1 text-sm text-gray-500">The easiest way to embed Genda on your website.</p>
           </div>
           <div className="grid grid-cols-2 space-x-4">
             <div>
@@ -29,7 +29,7 @@ export default function Embed(props) {
               <div className="mt-1">
                 <textarea
                   id="iframe"
-                  className="h-32 shadow-sm focus:ring-black focus:border-black block w-full sm:text-sm border-gray-300 rounded-sm"
+                  className="block w-full h-32 border-gray-300 rounded-sm shadow-sm focus:ring-black focus:border-black sm:text-sm"
                   placeholder="Loading..."
                   defaultValue={
                     '<iframe src="' +
@@ -49,7 +49,7 @@ export default function Embed(props) {
               <div className="mt-1">
                 <textarea
                   id="fullscreen"
-                  className="h-32 shadow-sm focus:ring-black focus:border-black block w-full sm:text-sm border-gray-300 rounded-sm"
+                  className="block w-full h-32 border-gray-300 rounded-sm shadow-sm focus:ring-black focus:border-black sm:text-sm"
                   placeholder="Loading..."
                   defaultValue={
                     '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Schedule a meeting</title><style>body {margin: 0;}iframe {height: calc(100vh - 4px);width: calc(100vw - 4px);box-sizing: border-box;}</style></head><body><iframe src="' +
@@ -64,7 +64,7 @@ export default function Embed(props) {
             </div>
           </div>
           <div className="my-6">
-            <h2 className="text-lg leading-6 font-medium text-gray-900">Calendso API</h2>
+            <h2 className="text-lg font-medium leading-6 text-gray-900">Calendso API</h2>
             <p className="mt-1 text-sm text-gray-500">
               Leverage our API for full control and customizability.
             </p>
