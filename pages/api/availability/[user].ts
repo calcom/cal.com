944bc4bf77f090a2b058ba422e990790067d8cc5
Omar López <zomars@me.com>
2021-09-23 11:53:43 -0600

    Type fixes

--- pages/api/availability/[user].ts
+++ pages/api/availability/[user].ts
@@ -65,10 +65,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }));
 
   const timeZone = eventType?.timeZone || currentUser.timeZone;
+  const defaultAvailability = {
+    startTime: currentUser.startTime,
+    endTime: currentUser.endTime,
+    days: [0, 1, 2, 3, 4, 5, 6],
+  };
   const workingHours = eventType?.availability.length
     ? eventType.availability
     : // currentUser.availability /* note(zomars) There's no UI nor default for this as of today */
-      [0, 1, 2, 3, 4, 5, 6]; /* note(zomars) For now, make every day available as fallback */
+      [defaultAvailability]; /* note(zomars) For now, make every day available as fallback */
 
   res.status(200).json({
     busy: bufferedBusyTimes,
