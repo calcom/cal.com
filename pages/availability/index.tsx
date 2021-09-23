476d83a104fd59a8e9f137a3b5bf5d79c9ced2c3
Shashwat Bagaria <shashwatbagaria-c24@work.local>
2021-09-23 16:55:19 +0530

    [#690] Hide Troubleshoot card and handle availabilit/troubleshoot path

--- pages/availability/index.tsx
+++ pages/availability/index.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+// import Link from "next/link";
 import prisma from "@lib/prisma";
 import Modal from "@components/Modal";
 import Shell from "@components/Shell";
@@ -119,10 +119,10 @@ export default function Availability(props) {
         <div className="flex">
           <div className="w-1/2 mr-2 bg-white border border-gray-200 rounded-sm">
             <div className="px-4 py-5 sm:p-6">
-              <h3 className="text-lg leading-6 font-medium text-gray-900">
+              <h3 className="text-lg font-medium leading-6 text-gray-900">
                 Change the start and end times of your day
               </h3>
-              <div className="mt-2 max-w-xl text-sm text-gray-500">
+              <div className="max-w-xl mt-2 text-sm text-gray-500">
                 <p>
                   Currently, your day is set to start at {convertMinsToHrsMins(props.user.startTime)} and end
                   at {convertMinsToHrsMins(props.user.endTime)}.
@@ -136,12 +136,12 @@ export default function Availability(props) {
             </div>
           </div>
 
-          <div className="w-1/2 ml-2 border border-gray-200 rounded-sm">
+          {/* <div className="w-1/2 ml-2 border border-gray-200 rounded-sm">
             <div className="px-4 py-5 sm:p-6">
-              <h3 className="text-lg leading-6 font-medium text-gray-900">
+              <h3 className="text-lg font-medium leading-6 text-gray-900">
                 Something doesn&apos;t look right?
               </h3>
-              <div className="mt-2 max-w-xl text-sm text-gray-500">
+              <div className="max-w-xl mt-2 text-sm text-gray-500">
                 <p>Troubleshoot your availability to explore why your times are showing as they are.</p>
               </div>
               <div className="mt-5">
@@ -150,30 +150,30 @@ export default function Availability(props) {
                 </Link>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
         {showChangeTimesModal && (
           <div
-            className="fixed z-50 inset-0 overflow-y-auto"
+            className="fixed inset-0 z-50 overflow-y-auto"
             aria-labelledby="modal-title"
             role="dialog"
             aria-modal="true">
-            <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div className="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
               <div
-                className="fixed inset-0 bg-gray-500 z-0 bg-opacity-75 transition-opacity"
+                className="fixed inset-0 z-0 transition-opacity bg-gray-500 bg-opacity-75"
                 aria-hidden="true"></div>
 
               <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
                 &#8203;
               </span>
 
-              <div className="inline-block align-bottom bg-white rounded-sm px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
-                <div className="sm:flex sm:items-start mb-4">
-                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-neutral-100 sm:mx-0 sm:h-10 sm:w-10">
-                    <ClockIcon className="h-6 w-6 text-neutral-600" />
+              <div className="inline-block px-4 pt-5 pb-4 overflow-hidden text-left align-bottom transition-all transform bg-white rounded-sm shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+                <div className="mb-4 sm:flex sm:items-start">
+                  <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto rounded-full bg-neutral-100 sm:mx-0 sm:h-10 sm:w-10">
+                    <ClockIcon className="w-6 h-6 text-neutral-600" />
                   </div>
                   <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+                    <h3 className="text-lg font-medium leading-6 text-gray-900" id="modal-title">
                       Change your available times
                     </h3>
                     <div>
@@ -185,7 +185,7 @@ export default function Availability(props) {
                 </div>
                 <form onSubmit={updateStartEndTimesHandler}>
                   <div className="flex mb-4">
-                    <label className="w-1/4 pt-2 block text-sm font-medium text-gray-700">Start time</label>
+                    <label className="block w-1/4 pt-2 text-sm font-medium text-gray-700">Start time</label>
                     <div>
                       <label htmlFor="hours" className="sr-only">
                         Hours
@@ -195,12 +195,12 @@ export default function Availability(props) {
                         type="number"
                         name="hours"
                         id="hours"
-                        className="shadow-sm focus:ring-neutral-500 focus:border-neutral-500 block w-full sm:text-sm border-gray-300 rounded-sm"
+                        className="block w-full border-gray-300 rounded-sm shadow-sm focus:ring-neutral-500 focus:border-neutral-500 sm:text-sm"
                         placeholder="9"
                         defaultValue={convertMinsToHrsMins(props.user.startTime).split(":")[0]}
                       />
                     </div>
-                    <span className="mx-2 pt-1">:</span>
+                    <span className="pt-1 mx-2">:</span>
                     <div>
                       <label htmlFor="minutes" className="sr-only">
                         Minutes
@@ -210,14 +210,14 @@ export default function Availability(props) {
                         type="number"
                         name="minutes"
                         id="minutes"
-                        className="shadow-sm focus:ring-neutral-500 focus:border-neutral-500 block w-full sm:text-sm border-gray-300 rounded-sm"
+                        className="block w-full border-gray-300 rounded-sm shadow-sm focus:ring-neutral-500 focus:border-neutral-500 sm:text-sm"
                         placeholder="30"
                         defaultValue={convertMinsToHrsMins(props.user.startTime).split(":")[1]}
                       />
                     </div>
                   </div>
                   <div className="flex mb-4">
-                    <label className="w-1/4 pt-2 block text-sm font-medium text-gray-700">End time</label>
+                    <label className="block w-1/4 pt-2 text-sm font-medium text-gray-700">End time</label>
                     <div>
                       <label htmlFor="hours" className="sr-only">
                         Hours
@@ -227,12 +227,12 @@ export default function Availability(props) {
                         type="number"
                         name="hours"
                         id="hours"
-                        className="shadow-sm focus:ring-neutral-500 focus:border-neutral-500 block w-full sm:text-sm border-gray-300 rounded-sm"
+                        className="block w-full border-gray-300 rounded-sm shadow-sm focus:ring-neutral-500 focus:border-neutral-500 sm:text-sm"
                         placeholder="17"
                         defaultValue={convertMinsToHrsMins(props.user.endTime).split(":")[0]}
                       />
                     </div>
-                    <span className="mx-2 pt-1">:</span>
+                    <span className="pt-1 mx-2">:</span>
                     <div>
                       <label htmlFor="minutes" className="sr-only">
                         Minutes
@@ -242,14 +242,14 @@ export default function Availability(props) {
                         type="number"
                         name="minutes"
                         id="minutes"
-                        className="shadow-sm focus:ring-neutral-500 focus:border-neutral-500 block w-full sm:text-sm border-gray-300 rounded-sm"
+                        className="block w-full border-gray-300 rounded-sm shadow-sm focus:ring-neutral-500 focus:border-neutral-500 sm:text-sm"
                         placeholder="30"
                         defaultValue={convertMinsToHrsMins(props.user.endTime).split(":")[1]}
                       />
                     </div>
                   </div>
                   <div className="flex mb-4">
-                    <label className="w-1/4 pt-2 block text-sm font-medium text-gray-700">Buffer</label>
+                    <label className="block w-1/4 pt-2 text-sm font-medium text-gray-700">Buffer</label>
                     <div>
                       <label htmlFor="hours" className="sr-only">
                         Hours
@@ -259,12 +259,12 @@ export default function Availability(props) {
                         type="number"
                         name="hours"
                         id="hours"
-                        className="shadow-sm focus:ring-neutral-500 focus:border-neutral-500 block w-full sm:text-sm border-gray-300 rounded-sm"
+                        className="block w-full border-gray-300 rounded-sm shadow-sm focus:ring-neutral-500 focus:border-neutral-500 sm:text-sm"
                         placeholder="0"
                         defaultValue={convertMinsToHrsMins(props.user.bufferTime).split(":")[0]}
                       />
                     </div>
-                    <span className="mx-2 pt-1">:</span>
+                    <span className="pt-1 mx-2">:</span>
                     <div>
                       <label htmlFor="minutes" className="sr-only">
                         Minutes
@@ -274,7 +274,7 @@ export default function Availability(props) {
                         type="number"
                         name="minutes"
                         id="minutes"
-                        className="shadow-sm focus:ring-neutral-500 focus:border-neutral-500 block w-full sm:text-sm border-gray-300 rounded-sm"
+                        className="block w-full border-gray-300 rounded-sm shadow-sm focus:ring-neutral-500 focus:border-neutral-500 sm:text-sm"
                         placeholder="10"
                         defaultValue={convertMinsToHrsMins(props.user.bufferTime).split(":")[1]}
                       />
@@ -284,7 +284,7 @@ export default function Availability(props) {
                     <button type="submit" className="btn btn-primary">
                       Update
                     </button>
-                    <button onClick={toggleChangeTimesModal} type="button" className="btn btn-white mr-2">
+                    <button onClick={toggleChangeTimesModal} type="button" className="mr-2 btn btn-white">
                       Cancel
                     </button>
                   </div>
--- pages/availability/troubleshoot.tsx
+++ pages/availability/troubleshoot.tsx
@@ -1,74 +1,75 @@
-import Loader from "@components/Loader";
+// import Loader from "@components/Loader";
 import prisma from "@lib/prisma";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import { GetServerSideProps } from "next";
 import { getSession } from "@lib/auth";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import Shell from "@components/Shell";
 
 dayjs.extend(utc);
 
-export default function Troubleshoot({ user }) {
+export default function Troubleshoot() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [loading, setLoading] = useState(true);
-  const [availability, setAvailability] = useState([]);
+  // const [loading, setLoading] = useState(true);
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [selectedDate, setSelectedDate] = useState(dayjs());
+  // const [selectedDate, setSelectedDate] = useState(dayjs());
 
-  function convertMinsToHrsMins(mins) {
-    let h = Math.floor(mins / 60);
-    let m = mins % 60;
-    h = h < 10 ? "0" + h : h;
-    m = m < 10 ? "0" + m : m;
-    return `${h}:${m}`;
-  }
+  // const [availability, setAvailability] = useState([]);
+  // function convertMinsToHrsMins(mins) {
+  //   let h = Math.floor(mins / 60);
+  //   let m = mins % 60;
+  //   h = h < 10 ? "0" + h : h;
+  //   m = m < 10 ? "0" + m : m;
+  //   return `${h}:${m}`;
+  // }
 
-  const fetchAvailability = (date) => {
-    const dateFrom = date.startOf("day").utc().format();
-    const dateTo = date.endOf("day").utc().format();
+  // const fetchAvailability = (date) => {
+  //   const dateFrom = date.startOf("day").utc().format();
+  //   const dateTo = date.endOf("day").utc().format();
 
-    fetch(`/api/availability/${user.username}?dateFrom=${dateFrom}&dateTo=${dateTo}`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((availableIntervals) => {
-        setAvailability(availableIntervals);
-        setLoading(false);
-      })
-      .catch((e) => {
-        console.error(e);
-      });
-  };
+  //   fetch(`/api/availability/${user.username}?dateFrom=${dateFrom}&dateTo=${dateTo}`)
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     .then((availableIntervals) => {
+  //       setAvailability(availableIntervals);
+  //       setLoading(false);
+  //     })
+  //     .catch((e) => {
+  //       console.error(e);
+  //     });
+  // };
 
-  useEffect(() => {
-    fetchAvailability(selectedDate);
-  }, [selectedDate]);
+  // useEffect(() => {
+  //   fetchAvailability(selectedDate);
+  // }, [selectedDate]);
 
-  if (loading) {
-    return <Loader />;
-  }
+  // if (loading) {
+  //   return <Loader />;
+  // }
 
   return (
     <div>
       <Shell
         heading="Troubleshoot"
         subtitle="Understand why certain times are available and others are blocked.">
-        <div className="bg-white max-w-md overflow-hidden shadow rounded-sm">
+        <div className="px-4 py-5 sm:p-6">Development In Progress 🚧</div>
+        {/* <div className="max-w-md overflow-hidden bg-white rounded-sm shadow">
           <div className="px-4 py-5 sm:p-6">
             Here is an overview of your day on {selectedDate.format("D MMMM YYYY")}:
             <small className="block text-neutral-400">
               Tip: Hover over the bold times for a full timestamp
             </small>
             <div className="mt-4 space-y-4">
-              <div className="bg-black overflow-hidden rounded-sm">
-                <div className="px-4 sm:px-6 py-2 text-white">
+              <div className="overflow-hidden bg-black rounded-sm">
+                <div className="px-4 py-2 text-white sm:px-6">
                   Your day starts at {convertMinsToHrsMins(user.startTime)}
                 </div>
               </div>
               {availability.map((slot) => (
-                <div key={slot.start} className="bg-neutral-100 overflow-hidden rounded-sm">
-                  <div className="px-4 py-5 sm:p-6 text-black">
+                <div key={slot.start} className="overflow-hidden rounded-sm bg-neutral-100">
+                  <div className="px-4 py-5 text-black sm:p-6">
                     Your calendar shows you as busy between{" "}
                     <span className="font-medium text-neutral-800" title={slot.start}>
                       {dayjs(slot.start).format("HH:mm")}
@@ -82,14 +83,14 @@ export default function Troubleshoot({ user }) {
                 </div>
               ))}
               {availability.length === 0 && <Loader />}
-              <div className="bg-black overflow-hidden rounded-sm">
-                <div className="px-4 sm:px-6 py-2 text-white">
+              <div className="overflow-hidden bg-black rounded-sm">
+                <div className="px-4 py-2 text-white sm:px-6">
                   Your day ends at {convertMinsToHrsMins(user.endTime)}
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </Shell>
     </div>
   );
@@ -102,9 +103,9 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   }
 
   const user = await prisma.user.findFirst({
-    where: {
-      id: session.user.id,
-    },
+    // where: {
+    //   id: session.user.id,
+    // },
     select: {
       startTime: true,
       endTime: true,
