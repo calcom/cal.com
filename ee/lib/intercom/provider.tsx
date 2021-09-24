9e7cb2c0b83e29bd30c37ad32b9f6788f408d613
Omar López <zomars@me.com>
2021-09-24 14:02:03 -0600

    CAL-469 Adds intercom dynamically (#762)
    * Adds react-use-intercom
    
    * Adds intercom env var
    
    * Loads intercom dynamically if env is set
    
    * CAL-473 Fixes client-side routing for authed pages
    
    * Moves intercom code to ee
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ ee/lib/intercom/provider.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { IntercomProvider } from "react-use-intercom";
+
+const Provider: FC = ({ children }) => (
+  <IntercomProvider appId={process.env.NEXT_PUBLIC_INTERCOM_APP_ID!}>{children}</IntercomProvider>
+);
+
+export default Provider;
