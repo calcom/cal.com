420daec147dd3be7e661cfe7f1b0f4109601ea99
Omar López <zomars@me.com>
2021-09-24 04:16:46 -0600

    CAL-473 Fixes client-side routing for authed pages (#763)

--- lib/app-providers.tsx
+++ lib/app-providers.tsx
@@ -1,18 +1,26 @@
 import { Provider } from "next-auth/client";
 import React from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { HydrateProps, QueryClient, QueryClientProvider } from "react-query";
 import { Hydrate } from "react-query/hydration";
 
+import { Session } from "@lib/auth";
 import { createTelemetryClient, TelemetryProvider } from "@lib/telemetry";
 
 export const queryClient = new QueryClient();
 
-const AppProviders: React.FC = (props, pageProps) => {
+type AppProviderProps = {
+  pageProps: {
+    session?: Session;
+    dehydratedState?: HydrateProps;
+  };
+};
+
+const AppProviders: React.FC<AppProviderProps> = ({ pageProps, children }) => {
   return (
     <TelemetryProvider value={createTelemetryClient()}>
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
-          <Provider session={pageProps.session}>{props.children}</Provider>
+          <Provider session={pageProps.session}>{children}</Provider>
         </Hydrate>
       </QueryClientProvider>
     </TelemetryProvider>
