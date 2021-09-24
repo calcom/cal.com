bb1da8150f19aaac3cf7c9017ee6a25153d38a09
Peer Richelsen <peeroke@gmail.com>
2021-09-24 12:28:57 +0100

    fixed event-type buttons on mobile, added more font-cal classes (#764)

--- components/Shell.tsx
+++ components/Shell.tsx
@@ -160,7 +160,7 @@ export default function Shell(props) {
             <div className="py-8">
               <div className="block sm:flex justify-between px-4 sm:px-6 md:px-8">
                 <div className="mb-8">
-                  <h1 className="text-xl font-bold text-gray-900">{props.heading}</h1>
+                  <h1 className="font-cal text-xl font-bold text-gray-900">{props.heading}</h1>
                   <p className="text-sm text-neutral-500 mr-4">{props.subtitle}</p>
                 </div>
                 <div className="mb-4 flex-shrink-0">{props.CTA}</div>
--- components/ui/Text/Headline/Headline.tsx
+++ components/ui/Text/Headline/Headline.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { TextProps } from "../Text";
 
 const Headline: React.FunctionComponent<TextProps> = (props: TextProps) => {
-  const classes = classnames("text-xl font-bold text-gray-900 dark:text-white", props?.className);
+  const classes = classnames("font-cal text-xl font-bold text-gray-900 dark:text-white", props?.className);
 
   return <p className={classes}>{props?.text || props.children}</p>;
 };
