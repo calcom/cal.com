33cb49b4818ec6fc4877581c265a12c3ac2b0abe
Heaust-ops <heaust.azure@gmail.com>
2021-09-27 09:54:26 +0530

    resolved typescript problems for success.tsx, useTheme.tsx and event.ts

--- lib/hooks/useTheme.tsx
+++ lib/hooks/useTheme.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 
 // makes sure the ui doesn't flash
-export default function useTheme(theme?: string) {
+export default function useTheme(theme: string | null) {
   const [isReady, setIsReady] = useState(false);
   useEffect(() => {
     if (!theme && window.matchMedia("(prefers-color-scheme: dark)").matches) {
       document.documentElement.classList.add("dark");
+    } else if (!theme) {
+      /** Uncovered case */
     } else {
       document.documentElement.classList.add(theme);
     }
