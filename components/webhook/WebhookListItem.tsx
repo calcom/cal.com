1460ae79f7c34f20b4dbb243cfe43ee678cdbad1
Syed Ali Shahbaz <alishahbaz7@gmail.com>
2021-09-27 22:33:41 +0530

    added more add-webhook flow items--WIP

--- components/webhook/WebhookListItem.tsx
+++ components/webhook/WebhookListItem.tsx
@@ -1,5 +1,4 @@
-import { TrashIcon, DotsHorizontalIcon, PencilAltIcon } from "@heroicons/react/outline";
-import Dropdown, { DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/Dropdown";
+import { TrashIcon, PencilAltIcon } from "@heroicons/react/outline";
 import { Dialog, DialogClose, DialogContent, DialogHeader, DialogTrigger } from "@components/Dialog";
 import ConfirmationDialogContent from "@components/dialog/ConfirmationDialogContent";
 import Button from "@components/ui/Button";
@@ -19,6 +18,7 @@ export default function WebhookListItem(props: { onChange: () => void; key: numb
     "BOOKING_CANCELLED",
   ]);
   const [webhookEventTypes, setWebhookEventTypes] = useState([]);
+  const [webhookEnabled, setWebhookEnabled] = useState(true);
   const subUrlRef = useRef<HTMLInputElement>() as React.MutableRefObject<HTMLInputElement>;
 
   const handleErrors = async (resp: Response) => {
@@ -38,6 +38,9 @@ export default function WebhookListItem(props: { onChange: () => void; key: numb
         break;
       case "booking-cancelled":
         setBookingCancelled(!bookingCancelled);
+        break;
+      case "webhook-enabled":
+        setWebhookEnabled(!webhookEnabled);
         break;
     }
     const arr = [];
@@ -60,8 +63,17 @@ export default function WebhookListItem(props: { onChange: () => void; key: numb
           </span>
         </div>
         <div className="flex w-full">
-          <div className="inline-block ml-3 space-y-1">
+          <div className="self-center inline-block ml-3 space-y-1">
             <span className="flex text-sm text-neutral-700">{props.webhook.subscriberUrl}</span>
+
+            <div className="inline-block space-y-1">
+              {props.webhook.webhookEvents.map((webhookEvent, ind: number) => (
+                <span key={ind} className="px-2 py-1 text-xs text-gray-700 rounded-md w-max bg-gray-50">
+                  {"/"}
+                  {webhookEvent.slug}
+                </span>
+              ))}
+            </div>
           </div>
         </div>
         <div className="flex">
@@ -75,126 +87,7 @@ export default function WebhookListItem(props: { onChange: () => void; key: numb
               Enabled
             </span>
           )}
-          {/* <Dropdown>
-            <DropdownMenuTrigger className="self-center">
-              <DotsHorizontalIcon className="w-5 h-5" />
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuItem>
-                <Dialog>
-                  <DialogTrigger asChild>
-                    <Button
-                      onClick={(e) => {
-                        e.stopPropagation();
-                      }}
-                      color="minimal"
-                      StartIcon={PencilAltIcon}
-                      className="w-full">
-                      Edit Webhook
-                    </Button>
-                  </DialogTrigger>
-                  <DialogContent>
-                    <DialogHeader title="Edit Webhook" subtitle="Edit your webhook details" />
-                    <div className="my-4">
-                      <div className="mb-4">
-                        <label htmlFor="subUrl" className="block text-sm font-medium text-gray-700">
-                          Subscriber Url
-                        </label>
-                        <input
-                          ref={subUrlRef}
-                          type="text"
-                          name="subUrl"
-                          id="subUrl"
-                          value={props.webhook.subscriberUrl || ""}
-                          placeholder="https://example.com/sub"
-                          required
-                          className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-sm shadow-sm focus:outline-none focus:ring-neutral-500 focus:border-neutral-500 sm:text-sm"
-                        />
-                        <legend className="block mt-4 mb-2 text-sm font-medium text-gray-700">
-                          {" "}
-                          Select Event Triggers{" "}
-                        </legend>
-                        <div className="flex py-4">
-                          <div className="w-10/12">
-                            <h2 className="font-medium text-gray-800">Booking Created</h2>
-                          </div>
-                          <div className="w-2/12 text-right">
-                            <Switch
-                              defaultChecked={true}
-                              cid="booking-created"
-                              value={bookingCreated}
-                              onCheckedChange={onCheckedChange}
-                            />
-                          </div>
-                        </div>
-                        <div className="flex py-1">
-                          <div className="w-10/12">
-                            <h2 className="font-medium text-gray-800">Booking Rescheduled</h2>
-                          </div>
-                          <div className="w-2/12 text-right">
-                            <Switch
-                              defaultChecked={true}
-                              cid="booking-rescheduled"
-                              value={bookingRescheduled}
-                              onCheckedChange={onCheckedChange}
-                            />
-                          </div>
-                        </div>
-                        <div className="flex py-4">
-                          <div className="w-10/12">
-                            <h2 className="font-medium text-gray-800">Booking Cancelled</h2>
-                          </div>
-                          <div className="w-2/12 text-right">
-                            <Switch
-                              defaultChecked={true}
-                              cid="booking-cancelled"
-                              value={bookingCancelled}
-                              onCheckedChange={onCheckedChange}
-                            />
-                          </div>
-                        </div>
-                      </div>
-                      <div className="gap-2 mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-                        <Button type="button" color="primary" className="ml-2">
-                          Save
-                        </Button>
-                        <DialogClose asChild>
-                          <Button color="secondary">Cancel</Button>
-                        </DialogClose>
-                      </div>
-                    </div>
-                  </DialogContent>
-                </Dialog>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Dialog>
-                  <DialogTrigger asChild>
-                    <Button
-                      onClick={(e) => {
-                        e.stopPropagation();
-                      }}
-                      color="warn"
-                      StartIcon={TrashIcon}
-                      className="w-full">
-                      Delete Webhook
-                    </Button>
-                  </DialogTrigger>
-                  <ConfirmationDialogContent
-                    variety="danger"
-                    title="Delete Webhook"
-                    confirmBtnText="Yes, delete webhook"
-                    cancelBtnText="Cancel"
-                    onConfirm={() => {
-                      console.log("confirm");
-                      //delete webhook
-                    }}>
-                    Are you sure you want to delete this webhook? You will no longer receive Cal.com meeting
-                    data at a specified URL, in real-time, when an event is scheduled or canceled .
-                  </ConfirmationDialogContent>
-                </Dialog>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </Dropdown> */}
+
           <Dialog>
             <DialogTrigger asChild>
               <Button
@@ -267,6 +160,46 @@ export default function WebhookListItem(props: { onChange: () => void; key: numb
                       </div>
                     </div>
                   </div>
+                  {/* <legend className="block pt-4 mb-2 text-sm font-medium text-gray-700"> Event Types </legend>
+                  <div className="p-2 border border-gray-300 rounded-sm">
+                    {props.webhook.webhookEvents.map((webhookEvent, ind: number) => (
+                      <div key={webhookEvent.slug} className="flex">
+                        <div className="w-10/12">
+                          <h2 className="font-medium text-gray-800">
+                            {"/"}
+                            {webhookEvent.slug}
+                          </h2>
+                        </div>
+                        <div className="flex items-center justify-center w-2/12 text-right">
+                          <Switch
+                            defaultChecked={true}
+                            cid={webhookEvent.slug}
+                            value={webhookEvent.slug}
+                            onCheckedChange={onCheckedChange}
+                          />
+                        </div>
+                      </div>
+                    ))}
+                  </div> */}
+                  <legend className="block pt-4 mb-2 text-sm font-medium text-gray-700">
+                    {" "}
+                    Webhook Status{" "}
+                  </legend>
+                  <div className="p-2 border border-gray-300 rounded-sm">
+                    <div className="flex">
+                      <div className="w-10/12">
+                        <h2 className="font-medium text-gray-800">Webhook Enabled</h2>
+                      </div>
+                      <div className="flex items-center justify-center w-2/12 text-right">
+                        <Switch
+                          defaultChecked={true}
+                          cid="webhook-enabled"
+                          value={webhookEnabled}
+                          onCheckedChange={onCheckedChange}
+                        />
+                      </div>
+                    </div>
+                  </div>
                 </div>
                 <div className="gap-2 mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
                   <Button type="button" color="primary" className="ml-2">
@@ -297,6 +230,7 @@ export default function WebhookListItem(props: { onChange: () => void; key: numb
               onConfirm={() => {
                 console.log("confirm");
                 //delete webhook
+                deleteWebhook();
               }}>
               Are you sure you want to delete this webhook? You will no longer receive Cal.com meeting data at
               a specified URL, in real-time, when an event is scheduled or canceled .
--- pages/settings/embed.tsx
+++ pages/settings/embed.tsx
@@ -35,6 +35,7 @@ import { Dialog, DialogClose, DialogContent, DialogHeader, DialogTrigger } from 
 export default function Embed(props: { err: string | undefined; BASE_URL: string; user: Member }) {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [session, loading] = useSession();
+  const [isLoading, setLoading] = useState(false);
   const [bookingCreated, setBookingCreated] = useState(true);
   const [bookingRescheduled, setBookingRescheduled] = useState(true);
   const [bookingCancelled, setBookingCancelled] = useState(true);
@@ -45,6 +46,7 @@ export default function Embed(props: { err: string | undefined; BASE_URL: string
     "BOOKING_CANCELLED",
   ]);
   const [webhookEventTypes, setWebhookEventTypes] = useState([]);
+  const [selectedWebhookEventTypes, setSelectedWebhookEventTypes] = useState([]);
 
   const subUrlRef = useRef<HTMLInputElement>() as React.MutableRefObject<HTMLInputElement>;
 
@@ -62,24 +64,13 @@ export default function Embed(props: { err: string | undefined; BASE_URL: string
     return resp.json();
   };
 
-  const onCheckedChange = (val: boolean | string, ref: string) => {
-    switch (ref) {
-      case "booking-created":
-        setBookingCreated(!bookingCreated);
-        break;
-      case "booking-rescheduled":
-        setBookingRescheduled(!bookingRescheduled);
-        break;
-      case "booking-cancelled":
-        setBookingCancelled(!bookingCancelled);
-        break;
-    }
-    const arr = [];
-    bookingCreated && arr.push("BOOKING_CREATED");
-    bookingRescheduled && arr.push("BOOKING_RESCHEDULED");
-    bookingCancelled && arr.push("BOOKING_CANCELLED");
-    setWebhookEventTriggers(arr);
-  };
+  // const onCheckedChange = () => {
+  //   const arr = [];
+  //   bookingCreated && arr.push("BOOKING_CREATED");
+  //   bookingRescheduled && arr.push("BOOKING_RESCHEDULED");
+  //   bookingCancelled && arr.push("BOOKING_CANCELLED");
+  //   setWebhookEventTriggers(arr);
+  // };
 
   const getWebhooks = () => {
     fetch("/api/webhook")
@@ -95,6 +86,7 @@ export default function Embed(props: { err: string | undefined; BASE_URL: string
         );
       })
       .catch(console.log);
+    setLoading(false);
   };
 
   const getEventTypes = () => {
@@ -106,6 +98,15 @@ export default function Embed(props: { err: string | undefined; BASE_URL: string
   };
 
   useEffect(() => {
+    const arr = [];
+    bookingCreated && arr.push("BOOKING_CREATED");
+    bookingRescheduled && arr.push("BOOKING_RESCHEDULED");
+    bookingCancelled && arr.push("BOOKING_CANCELLED");
+    setWebhookEventTriggers(arr);
+    console.log(selectedWebhookEventTypes);
+  }, [bookingCreated, bookingRescheduled, bookingCancelled, selectedWebhookEventTypes]);
+
+  useEffect(() => {
     getWebhooks();
     getEventTypes();
   }, []);
@@ -114,7 +115,30 @@ export default function Embed(props: { err: string | undefined; BASE_URL: string
     return <Loader />;
   }
 
+  function eventTypeSelectionHandler(eventType) {
+    return (selected) => {
+      const i = webhookEventTypes.findIndex((c) => c.slug === eventType.slug);
+      webhookEventTypes[i].selected = selected;
+      if (selected) {
+        // push
+        if (!selectedWebhookEventTypes.includes(eventType.slug)) {
+          setSelectedWebhookEventTypes([...selectedWebhookEventTypes, eventType.slug]);
+        }
+      } else {
+        //pop
+        const index = selectedWebhookEventTypes.indexOf(eventType.slug);
+        if (index > -1) {
+          const arr = selectedWebhookEventTypes;
+          arr.splice(index, 1);
+          console.log(arr);
+          setSelectedWebhookEventTypes(arr);
+        }
+      }
+    };
+  }
+
   const createWebhook = () => {
+    setLoading(true);
     fetch("/api/webhook", {
       method: "POST",
       body: JSON.stringify({
@@ -171,13 +195,133 @@ export default function Embed(props: { err: string | undefined; BASE_URL: string
             </div>
           </div>
           <hr className="mt-8" />
-          <div className="my-6">
-            <h2 className="text-lg font-medium leading-6 text-gray-900">Webhooks</h2>
-            <p className="mt-1 text-sm text-gray-500">
-              Receive Cal.com meeting data at a specified URL, in real-time, when an event is scheduled or
-              cancelled.{" "}
-            </p>
+          <div className="flex justify-between my-6">
+            <div>
+              <h2 className="text-lg font-medium leading-6 text-gray-900">Webhooks</h2>
+              <p className="mt-1 text-sm text-gray-500">
+                Receive Cal meeting data at a specified URL, in real-time, when an event is scheduled or
+                cancelled.{" "}
+              </p>
+            </div>
+            <div>
+              <Dialog>
+                <DialogTrigger className="px-4 py-2 my-6 text-sm font-medium text-white border border-transparent rounded-sm shadow-sm bg-neutral-900 hover:bg-neutral-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-900">
+                  <PlusIcon className="inline w-5 h-5 mr-1" />
+                  New Webhook
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader
+                    title="Create a new webhook"
+                    subtitle="Create a new webhook to your account"
+                  />
+                  <div className="my-4">
+                    <div className="mb-4">
+                      <label htmlFor="subUrl" className="block text-sm font-medium text-gray-700">
+                        Subscriber Url
+                      </label>
+                      <input
+                        ref={subUrlRef}
+                        type="text"
+                        name="subUrl"
+                        id="subUrl"
+                        placeholder="https://example.com/sub"
+                        required
+                        className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-sm shadow-sm focus:outline-none focus:ring-neutral-500 focus:border-neutral-500 sm:text-sm"
+                      />
+                      <legend className="block pt-4 mb-2 text-sm font-medium text-gray-700">
+                        {" "}
+                        Event Types{" "}
+                      </legend>
+                      <div className="p-2 border border-gray-300 rounded-sm">
+                        <ul className="overflow-y-auto divide-y divide-gray-200 max-h-96">
+                          {webhookEventTypes.map((eventType) => (
+                            <li key={eventType.slug} className="flex py-2">
+                              <div className="w-10/12">
+                                <h2 className="font-medium text-gray-800 align-middle">{eventType.slug}</h2>
+                              </div>
+                              <div className="flex items-center justify-center w-2/12 text-right">
+                                <Switch
+                                  defaultChecked={true}
+                                  onCheckedChange={eventTypeSelectionHandler(eventType)}
+                                />
+                              </div>
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                      <legend className="block pt-4 mb-2 text-sm font-medium text-gray-700">
+                        {" "}
+                        Event Triggers{" "}
+                      </legend>
+                      <div className="p-2 border border-gray-300 rounded-sm">
+                        <div className="flex pb-4">
+                          <div className="w-10/12">
+                            <h2 className="font-medium text-gray-800">Booking Created</h2>
+                          </div>
+                          <div className="flex items-center justify-center w-2/12 text-right">
+                            <Switch
+                              defaultChecked={true}
+                              id="booking-created"
+                              value={bookingCreated}
+                              onCheckedChange={() => {
+                                setBookingCreated(!bookingCreated);
+                              }}
+                            />
+                          </div>
+                        </div>
+                        <div className="flex py-1">
+                          <div className="w-10/12">
+                            <h2 className="font-medium text-gray-800">Booking Rescheduled</h2>
+                          </div>
+                          <div className="flex items-center justify-center w-2/12 text-right">
+                            <Switch
+                              defaultChecked={true}
+                              id="booking-rescheduled"
+                              value={bookingRescheduled}
+                              onCheckedChange={() => {
+                                setBookingRescheduled(!bookingRescheduled);
+                              }}
+                            />
+                          </div>
+                        </div>
+                        <div className="flex pt-4">
+                          <div className="w-10/12">
+                            <h2 className="font-medium text-gray-800">Booking Cancelled</h2>
+                          </div>
+                          <div className="flex items-center justify-center w-2/12 text-right">
+                            <Switch
+                              defaultChecked={true}
+                              id="booking-cancelled"
+                              value={bookingCancelled}
+                              onCheckedChange={() => {
+                                setBookingCancelled(!bookingCancelled);
+                              }}
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="gap-2 mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                      <DialogClose asChild>
+                        <Button
+                          type="button"
+                          loading={isLoading}
+                          onClick={createWebhook}
+                          color="primary"
+                          className="ml-2">
+                          Create Webhook
+                        </Button>
+                      </DialogClose>
+                      <DialogClose asChild>
+                        <Button color="secondary">Cancel</Button>
+                      </DialogClose>
+                    </div>
+                  </div>
+                </DialogContent>
+              </Dialog>
+            </div>
           </div>
+
           <div className="divide-y divide-gray-200 lg:col-span-9">
             <div className="py-6 lg:pb-8">
               <div className="flex flex-col justify-between md:flex-row">
@@ -198,86 +342,6 @@ export default function Embed(props: { err: string | undefined; BASE_URL: string
                     </div>
                   )} */}
                 </div>
-
-                <Dialog>
-                  <DialogTrigger className="px-4 py-2 my-6 text-sm font-medium text-white border border-transparent rounded-sm shadow-sm bg-neutral-900 hover:bg-neutral-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-900">
-                    <PlusIcon className="inline w-5 h-5 mr-1" />
-                    New Webhook
-                  </DialogTrigger>
-                  <DialogContent>
-                    <DialogHeader
-                      title="Create a new webhook"
-                      subtitle="Create a new webhook to your account"
-                    />
-                    <div className="my-4">
-                      <div className="mb-4">
-                        <label htmlFor="subUrl" className="block text-sm font-medium text-gray-700">
-                          Subscriber Url
-                        </label>
-                        <input
-                          ref={subUrlRef}
-                          type="text"
-                          name="subUrl"
-                          id="subUrl"
-                          placeholder="https://example.com/sub"
-                          required
-                          className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-sm shadow-sm focus:outline-none focus:ring-neutral-500 focus:border-neutral-500 sm:text-sm"
-                        />
-                        <legend className="block mt-4 mb-2 text-sm font-medium text-gray-700">
-                          {" "}
-                          Select Event Triggers{" "}
-                        </legend>
-                        <div className="flex py-4">
-                          <div className="w-10/12">
-                            <h2 className="font-medium text-gray-800">Booking Created</h2>
-                          </div>
-                          <div className="w-2/12 text-right">
-                            <Switch
-                              defaultChecked={true}
-                              cid="booking-created"
-                              value={bookingCreated}
-                              onCheckedChange={onCheckedChange}
-                            />
-                          </div>
-                        </div>
-                        <div className="flex py-1">
-                          <div className="w-10/12">
-                            <h2 className="font-medium text-gray-800">Booking Rescheduled</h2>
-                          </div>
-                          <div className="w-2/12 text-right">
-                            <Switch
-                              defaultChecked={true}
-                              cid="booking-rescheduled"
-                              value={bookingRescheduled}
-                              onCheckedChange={onCheckedChange}
-                            />
-                          </div>
-                        </div>
-                        <div className="flex py-4">
-                          <div className="w-10/12">
-                            <h2 className="font-medium text-gray-800">Booking Cancelled</h2>
-                          </div>
-                          <div className="w-2/12 text-right">
-                            <Switch
-                              defaultChecked={true}
-                              cid="booking-cancelled"
-                              value={bookingCancelled}
-                              onCheckedChange={onCheckedChange}
-                            />
-                          </div>
-                        </div>
-                      </div>
-                      <div className="gap-2 mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-                        <Button type="button" onClick={createWebhook} color="primary" className="ml-2">
-                          Create Webhook
-                        </Button>
-                        <DialogClose asChild>
-                          <Button color="secondary">Cancel</Button>
-                        </DialogClose>
-                      </div>
-                    </div>
-                  </DialogContent>
-                </Dialog>
               </div>
               <div>
                 {!!webhooks.length && <WebhookList webhooks={webhooks} onChange={getWebhooks}></WebhookList>}
