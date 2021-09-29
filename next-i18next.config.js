776b2afdc174496d2d4130e60aaf1870493b591e
KATT <alexander@n1s.se>
2021-09-29 15:15:41 +0100

    rm localeDetection

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 module.exports = {
   i18n: {
+    localeDetection: false,
     defaultLocale: "en",
     locales: ["en", "fr", "it", "ru", "es", "de", "pt", "ro"],
   },
