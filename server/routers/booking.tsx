a30381f22999b4fe7e2524cd2a35016fd129ceab
Alex van Andel <me@alexvanandel.com>
2021-09-29 15:58:25 +0100

    Lowercase username try #3 (#821)
    3 times is a charm.. can't believe this took 3 tries.

--- server/routers/booking.tsx
+++ server/routers/booking.tsx
@@ -12,7 +12,7 @@ export const bookingRouter = createRouter().query("userEventTypes", {
 
     const user = await prisma.user.findUnique({
       where: {
-        username,
+        username: username.toLowerCase(),
       },
       select: {
         id: true,
