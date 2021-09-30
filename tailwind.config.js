70d1421e7e0a500373df83730505b0270242e8ec
Peer Richelsen <peeroke@richelsen.net>
2021-09-30 23:22:07 +0100

    added /ee/ to tailwind purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./ee/components/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: "class",
   theme: {
     extend: {
