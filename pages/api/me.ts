af95098835e140775e931aeda822e8b89e71e7f1
Anton Podviaznikov <anton@podviaznikov.com>
2021-10-06 10:38:57 -0700

    use api key auth for api/me route

--- pages/api/me.ts
+++ pages/api/me.ts
@@ -1,19 +1,28 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getToken } from "next-auth/jwt";
 
 import { getSession } from "@lib/auth";
 import prisma from "@lib/prisma";
 import { defaultAvatarSrc } from "@lib/profile";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const session = await getSession({ req: req });
-  if (!session) {
-    res.status(401).json({ message: "Not authenticated" });
-    return;
+  let userId;
+  // Temporary auth using either token or cookie.
+  const token = await getToken({ req, secret: process.env.JWT_SECRET });
+  if (token && token.id) {
+    userId = token.id;
+  } else {
+    const session = await getSession({ req: req });
+    if (!session || !session.user) {
+      res.status(401).json({ message: "Not authenticated" });
+      return;
+    }
+    userId = session.user?.id;
   }
 
   const user: User = await prisma.user.findUnique({
     where: {
-      id: session.user.id,
+      id: userId,
     },
     select: {
       id: true,
