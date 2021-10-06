a6aed88cfc6962e5a30470ce851879c48b0447e5
Nicole Reed <nicolereed@Nicoles-MacBook-Air.local>
2021-10-06 18:26:51 +0300

    removed img, replaced with svg

--- components/ui/PoweredByCal.tsx
+++ components/ui/PoweredByCal.tsx
@@ -5,11 +5,6 @@ const PoweredByCal = () => (
     <Link href={`cal.com?utm_source=embed&utm_medium=powered-by-button`}>
       <a target="_blank" className="text-gray-500 opacity-50 hover:opacity-100">
         powered by{" "}
-        {/* <img
-          className="w-auto inline h-[10px] relative -mt-px"
-          src="https://cal.com/logo.svg"
-          alt="Cal.com Logo"
-        /> */}
         <svg
           className="calLogo"
           width="45"
