1dced4c797d3017673b037a8864eb7c5e4eec1f1
KATT <alexander@n1s.se>
2021-10-07 10:32:28 +0200

    dont test dayjs internals

--- test/lib/parseZone.test.ts
+++ test/lib/parseZone.test.ts
@@ -10,5 +10,4 @@ const EXPECTED_DATE_STRING = "2021-06-20T11:59:59+02:00";
 it("has the right utcOffset regardless of the local timeZone", async () => {
   expect(parseZone(EXPECTED_DATE_STRING)?.utcOffset()).toEqual(120);
   expect(parseZone(EXPECTED_DATE_STRING)?.format()).toEqual(EXPECTED_DATE_STRING);
-  // expect(dayjs(EXPECTED_DATE_STRING).format("YYYY-mm-dd")).not.toEqual(EXPECTED_DATE_STRING);
 });
