ec6d4da6424b2430b24bad5e910027c29009eb17
mihaic195 <colceriumi@gmail.com>
2021-10-08 16:32:28 +0300

    fix: move cancelled upcoming booking to cancelled tab

--- server/routers/viewer.tsx
+++ server/routers/viewer.tsx
@@ -28,7 +28,7 @@ export const viewerRouter = createProtectedRouter()
       const { prisma, user } = ctx;
       const bookingListingByStatus = input.status;
       const bookingListingFilters: Record<typeof bookingListingByStatus, Prisma.BookingWhereInput[]> = {
-        upcoming: [{ endTime: { gte: new Date() } }],
+        upcoming: [{ endTime: { gte: new Date() }, NOT: { status: { equals: BookingStatus.CANCELLED }} }],
         past: [{ endTime: { lte: new Date() } }],
         cancelled: [{ status: { equals: BookingStatus.CANCELLED } }],
       };
