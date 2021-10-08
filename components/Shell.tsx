b77a4874e5b6c2096d102ad9a8eeb3adf99be85a
KATT <alexander@n1s.se>
2021-10-08 14:47:14 +0200

    wip

--- components/Shell.tsx
+++ components/Shell.tsx
@@ -56,13 +56,9 @@ function useRedirectToLoginIfUnauthenticated() {
   }, [loading, session, router]);
 }
 
-function ShellHeading(props: { title: ReactNode; subtitle: ReactNode; CTA?: ReactNode; className?: string }) {
+function ShellHeading(props: { title: ReactNode; subtitle: ReactNode; CTA?: ReactNode }) {
   return (
-    <div
-      className={classNames(
-        "block sm:flex justify-between px-4 sm:px-6 md:px-8 min-h-[80px]",
-        props.className
-      )}>
+    <div className="block sm:flex justify-between px-4 sm:px-6 md:px-8 min-h-[80px]">
       <div className="mb-8">
         <h1 className="font-cal text-xl font-bold text-gray-900">{props.title}</h1>
         <p className="text-sm text-neutral-500 mr-4">{props.subtitle}</p>
