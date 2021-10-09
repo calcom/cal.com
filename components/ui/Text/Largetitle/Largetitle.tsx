f03a2c2a1a032c98bf4117618404f808c1f2fd6d
Ciarán Hanrahan <justciaranhanrahan@gmail.com>
2021-10-09 14:34:13 +0100

    onboarding changes (#889)

--- components/ui/Text/Largetitle/Largetitle.tsx
+++ components/ui/Text/Largetitle/Largetitle.tsx
@@ -4,7 +4,10 @@ import React from "react";
 import { TextProps } from "../Text";
 
 const Largetitle: React.FunctionComponent<TextProps> = (props: TextProps) => {
-  const classes = classnames("text-3xl font-extrabold text-gray-900 dark:text-white", props?.className);
+  const classes = classnames(
+    "font-cal tracking-wider text-3xl text-gray-900 dark:text-white mb-2",
+    props?.className
+  );
 
   return <p className={classes}>{props?.text || props.children}</p>;
 };
