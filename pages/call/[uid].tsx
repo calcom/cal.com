9614848387a815ae8fe8b7f75876ead457747086
Peer Richelsen <peeroke@richelsen.net>
2021-10-09 13:30:43 +0100

    wip

--- pages/call/[uid].tsx
+++ pages/call/[uid].tsx
@@ -4,8 +4,6 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 
-import Logo from "@components/Logo";
-
 import prisma from "../../lib/prisma";
 
 export default function JoinCall(props, session) {
@@ -54,7 +52,15 @@ export default function JoinCall(props, session) {
   return (
     <div style={{ zIndex: 2, position: "relative" }}>
       <Link href="/">
-        <Logo />
+        <img
+          className="hidden sm:inline-block z-10 fixed w-auto h-5"
+          src="https://cal.com/logo-white.svg"
+          alt="Cal.com Logo"
+          style={{
+            top: 46,
+            left: 24,
+          }}
+        />
       </Link>
       {JoinCall}
     </div>
