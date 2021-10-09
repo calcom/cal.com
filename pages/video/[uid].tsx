15fd674515c90dd256401e6db736ff8d8ce26f31
Peer Richelsen <peeroke@richelsen.net>
2021-10-09 13:14:10 +0100

    added cal logo to daily call

--- pages/video/[uid].tsx
+++ pages/video/[uid].tsx
@@ -1,8 +1,7 @@
 import DailyIframe from "@daily-co/daily-js";
 import { getSession } from "next-auth/client";
-import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 
 import prisma from "../../lib/prisma";
 
@@ -13,7 +12,7 @@ export default function JoinCall(props, session) {
   const emptyBooking = props.booking === null;
   useEffect(() => {
     if (emptyBooking) {
-      router.push("/call/no-meeting-found");
+      router.push("/video/no-meeting-found");
     }
   });
 
@@ -49,22 +48,7 @@ export default function JoinCall(props, session) {
     }
   }, []);
 
-  return (
-    <div style={{ zIndex: 2, position: "relative" }}>
-      <Link href="/">
-        <img
-          className="hidden sm:inline-block z-10 fixed w-auto h-5"
-          src="/logo-white.svg"
-          alt="Cal.com Logo"
-          style={{
-            top: 46,
-            left: 24,
-          }}
-        />
-      </Link>
-      {JoinCall}
-    </div>
-  );
+  return JoinCall;
 }
 
 export async function getServerSideProps(context) {
