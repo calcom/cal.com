6db2b90e462574f42da38dc2be9bdfe172455141
KATT <alexander@n1s.se>
2021-10-14 00:50:16 +0200

    simplify i18n handler and remove redundant(?) fn check

--- components/I18nLanguageHandler.tsx
+++ components/I18nLanguageHandler.tsx
@@ -3,13 +3,16 @@ import { useEffect } from "react";
 
 import { trpc } from "@lib/trpc";
 
+/**
+ * Auto-switches locale client-side to the logged in user's preference
+ */
 const I18nLanguageHandler = (): null => {
   const { i18n } = useTranslation("common");
   const locale = trpc.useQuery(["viewer.me"]).data?.locale;
 
   useEffect(() => {
     if (locale && i18n.language && i18n.language !== locale) {
-      if (typeof i18n.changeLanguage === "function") i18n.changeLanguage(locale);
+      i18n.changeLanguage(locale);
     }
   }, [i18n, locale, i18n.language]);
 
