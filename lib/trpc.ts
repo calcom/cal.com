c2c37b701ee7eded3a3d90c6c59260997636698d
Omar López <zomars@me.com>
2021-10-14 13:22:01 -0600

    Zomars/fixes trpc typo (#957)

--- lib/trpc.ts
+++ lib/trpc.ts
@@ -1,10 +1,10 @@
 // ℹ️ Type-only import:
 // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-8.html#type-only-imports-and-export
-import { createReactQueryHooks } from "@trpc/react";
-import type { inferProcedureOutput, inferProcedureInput } from "@trpc/server";
 import superjson from "superjson";
 
 import type { AppRouter } from "@server/routers/_app";
+import { createReactQueryHooks } from "@trpc/react";
+import type { inferProcedureOutput, inferProcedureInput } from "@trpc/server";
 
 /**
  * A set of strongly-typed React hooks from your `AppRouter` type signature with `createReactQueryHooks`.
