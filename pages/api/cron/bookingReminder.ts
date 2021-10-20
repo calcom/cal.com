7912d7196ad7262499f03c6dc17885bc143a27e4
KATT <alexander@n1s.se>
2021-10-20 16:04:23 +0200

    tweak

--- pages/api/cron/bookingReminder.ts
+++ pages/api/cron/bookingReminder.ts
@@ -17,64 +17,62 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return;
   }
 
-  if (req.method == "POST") {
-    const reminderIntervalMinutes = [48 * 60, 24 * 60, 3 * 60];
-    let notificationsSent = 0;
-    for (const interval of reminderIntervalMinutes) {
-      const bookings = await prisma.booking.findMany({
-        where: {
-          confirmed: false,
-          rejected: false,
-          createdAt: {
-            lte: dayjs().add(-interval, "minutes").toDate(),
-          },
+  const reminderIntervalMinutes = [48 * 60, 24 * 60, 3 * 60];
+  let notificationsSent = 0;
+  for (const interval of reminderIntervalMinutes) {
+    const bookings = await prisma.booking.findMany({
+      where: {
+        confirmed: false,
+        rejected: false,
+        createdAt: {
+          lte: dayjs().add(-interval, "minutes").toDate(),
         },
-        select: {
-          title: true,
-          description: true,
-          startTime: true,
-          endTime: true,
-          attendees: true,
-          user: true,
-          id: true,
-          uid: true,
-        },
-      });
+      },
+      select: {
+        title: true,
+        description: true,
+        startTime: true,
+        endTime: true,
+        attendees: true,
+        user: true,
+        id: true,
+        uid: true,
+      },
+    });
 
-      const reminders = await prisma.reminderMail.findMany({
-        where: {
+    const reminders = await prisma.reminderMail.findMany({
+      where: {
+        reminderType: ReminderType.PENDING_BOOKING_CONFIRMATION,
+        referenceId: {
+          in: bookings.map((b) => b.id),
+        },
+        elapsedMinutes: {
+          gte: interval,
+        },
+      },
+    });
+
+    for (const booking of bookings.filter((b) => !reminders.some((r) => r.referenceId == b.id))) {
+      const evt: CalendarEvent = {
+        type: booking.title,
+        title: booking.title,
+        description: booking.description,
+        startTime: booking.startTime.toISOString(),
+        endTime: booking.endTime.toISOString(),
+        organizer: { email: booking.user.email, name: booking.user.name, timeZone: booking.user.timeZone },
+        attendees: booking.attendees,
+      };
+
+      await new EventOrganizerRequestReminderMail(evt, booking.uid).sendEmail();
+      await prisma.reminderMail.create({
+        data: {
+          referenceId: booking.id,
           reminderType: ReminderType.PENDING_BOOKING_CONFIRMATION,
-          referenceId: {
-            in: bookings.map((b) => b.id),
-          },
-          elapsedMinutes: {
-            gte: interval,
-          },
+          elapsedMinutes: interval,
         },
       });
-
-      for (const booking of bookings.filter((b) => !reminders.some((r) => r.referenceId == b.id))) {
-        const evt: CalendarEvent = {
-          type: booking.title,
-          title: booking.title,
-          description: booking.description,
-          startTime: booking.startTime.toISOString(),
-          endTime: booking.endTime.toISOString(),
-          organizer: { email: booking.user.email, name: booking.user.name, timeZone: booking.user.timeZone },
-          attendees: booking.attendees,
-        };
-
-        await new EventOrganizerRequestReminderMail(evt, booking.uid).sendEmail();
-        await prisma.reminderMail.create({
-          data: {
-            referenceId: booking.id,
-            reminderType: ReminderType.PENDING_BOOKING_CONFIRMATION,
-            elapsedMinutes: interval,
-          },
-        });
-        notificationsSent++;
-      }
+      notificationsSent++;
     }
-    res.status(200).json({ notificationsSent });
   }
+  res.status(200).json({ notificationsSent });
 }
