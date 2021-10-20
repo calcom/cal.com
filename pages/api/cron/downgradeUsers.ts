a78f7b90f1caef25d95c9424307c5c3979654941
KATT <alexander@n1s.se>
2021-10-20 15:31:30 +0200

    auth

--- pages/api/cron/downgradeUsers.ts
+++ pages/api/cron/downgradeUsers.ts
@@ -6,7 +6,7 @@ import prisma from "@lib/prisma";
 const TRIAL_LIMIT_DAYS = 14;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const apiKey = req.query.apiKey;
+  const apiKey = req.headers.authorization || req.query.apiKey;
   if (process.env.CRON_API_KEY !== apiKey) {
     return res.status(401).json({ message: "Not authenticated" });
   }
