879d9b24a80f084667bd59c1b4a842037165d6df
Mihai C <34626017+mihaic195@users.noreply.github.com>
2021-10-28 17:29:43 +0300

    Revert "bookings page infinite scroll PR comments (#1060)"
    This reverts commit 81f887da1931bb5b9cf5c84f0b3352a6a74a979c.

--- pages/bookings/[status].tsx
+++ pages/bookings/[status].tsx
@@ -1,18 +1,14 @@
 import { CalendarIcon } from "@heroicons/react/outline";
 import { useRouter } from "next/router";
-import { Fragment } from "react";
 
-import { useInViewObserver } from "@lib/hooks/useInViewObserver";
+import { QueryCell } from "@lib/QueryCell";
 import { useLocale } from "@lib/hooks/useLocale";
 import { inferQueryInput, trpc } from "@lib/trpc";
 
 import BookingsShell from "@components/BookingsShell";
 import EmptyScreen from "@components/EmptyScreen";
-import Loader from "@components/Loader";
 import Shell from "@components/Shell";
 import BookingListItem from "@components/booking/BookingListItem";
-import { Alert } from "@components/ui/Alert";
-import Button from "@components/ui/Button";
 
 type BookingListingStatus = inferQueryInput<"viewer.bookings">["status"];
 
@@ -34,56 +30,46 @@ export default function Bookings() {
     getNextPageParam: (lastPage) => lastPage.nextCursor,
   });
 
-  const buttonInView = useInViewObserver(() => {
-    if (!query.isFetching && query.hasNextPage && query.status === "success") {
-      query.fetchNextPage();
-    }
-  });
+  const { isFetchingNextPage, hasNextPage, fetchNextPage } = query;
 
   return (
-    <Shell heading={t("bookings")} subtitle={t("bookings_description")}>
+    <Shell
+      heading={t("bookings")}
+      subtitle={t("bookings_description")}
+      isFetchingNextPage={isFetchingNextPage}
+      hasNextPage={hasNextPage}
+      fetchNextPage={fetchNextPage}>
       <BookingsShell>
         <div className="flex flex-col -mx-4 sm:mx-auto">
           <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-              {query.status === "error" && (
-                <Alert severity="error" title={t("something_went_wrong")} message={query.error.message} />
-              )}
-              {query.status === "loading" || (query.status === "idle" && <Loader />)}
-              {query.status === "success" && query.data.pages[0].bookings.length > 0 ? (
-                <>
-                  <div className="my-6 overflow-hidden border border-b border-gray-200 rounded-sm">
-                    <table className="min-w-full divide-y divide-gray-200">
-                      <tbody className="bg-white divide-y divide-gray-200" data-testid="bookings">
-                        {query.data.pages.map((page, index) => (
-                          <Fragment key={index}>
-                            {page.bookings.map((booking) => (
-                              <BookingListItem key={booking.id} {...booking} />
-                            ))}
-                          </Fragment>
-                        ))}
-                      </tbody>
-                    </table>
-                  </div>
-                  <div className="text-center p-2" ref={buttonInView.ref}>
-                    <Button
-                      loading={query.isFetchingNextPage}
-                      disabled={!query.hasNextPage}
-                      onClick={() => query.fetchNextPage()}>
-                      {query.hasNextPage ? t("load_more_results") : t("no_more_results")}
-                    </Button>
-                  </div>
-                </>
-              ) : (
-                <EmptyScreen
-                  Icon={CalendarIcon}
-                  headline={t("no_status_bookings_yet", { status: status })}
-                  description={t("no_status_bookings_yet_description", {
-                    status: status,
-                    description: descriptionByStatus[status],
-                  })}
-                />
-              )}
+              <QueryCell
+                query={query}
+                success={({ data }) => {
+                  console.log("DATA", data);
+                  return (
+                    <div className="my-6 overflow-hidden border border-b border-gray-200 rounded-sm">
+                      <table className="min-w-full divide-y divide-gray-200">
+                        <tbody className="bg-white divide-y divide-gray-200" data-testid="bookings">
+                          {data?.pages.map((page) =>
+                            page?.bookings.map((booking) => <BookingListItem key={booking.id} {...booking} />)
+                          )}
+                        </tbody>
+                      </table>
+                    </div>
+                  );
+                }}
+                empty={() => (
+                  <EmptyScreen
+                    Icon={CalendarIcon}
+                    headline={t("no_status_bookings_yet", { status: status })}
+                    description={t("no_status_bookings_yet_description", {
+                      status: status,
+                      description: descriptionByStatus[status],
+                    })}
+                  />
+                )}
+              />
             </div>
           </div>
         </div>
