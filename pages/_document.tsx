32f05b54b46a00ae9541d077ffff79d286fdb6c2
bregtemundo <vanbilsen.bregt@gmail.com>
2021-10-29 09:35:10 +0200

    Added favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,8 +16,8 @@ class MyDocument extends Document<Props> {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
-          <meta name="msapplication-TileColor" content="#ff0000" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#60c9a1" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
           <meta name="theme-color" content="#ffffff" />
         </Head>
         <body className="dark:bg-black bg-gray-100">
