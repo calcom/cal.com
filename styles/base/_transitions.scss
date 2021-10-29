45481ec31b940721e0724f14d8549aa7310a3ccc
bregtemundo <vanbilsen.bregt@gmail.com>
2021-10-29 09:51:07 +0200

    Added styles lib and @types tsconfig

---
+++ styles/base/_transitions.scss
@@ -0,0 +1,1 @@
+// Silence is golden.
---
+++ styles/lib/tools/mixins/config.rb
@@ -0,0 +1,8 @@
+http_path = "/"
+css_dir = "."
+sass_dir = "."
+images_dir = "img"
+javascripts_dir = "js"
+output_style = :compressed
+relative_assets=true
+line_comments = false
