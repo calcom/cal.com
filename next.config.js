16a04c1b925410e26a8a04fbd18349c741ca9adb
bregtemundo <vanbilsen.bregt@gmail.com>
2021-10-30 08:39:14 +0200

    Netlify setting needed?

--- next.config.js
+++ next.config.js
@@ -55,6 +55,8 @@ plugins.push(withTM);
 
 // prettier-ignore
 module.exports = () => plugins.reduce((acc, next) => next(acc), {
+  // netlify setting
+  target: "serverless",
   i18n,
   eslint: {
     // This allows production builds to successfully complete even if the project has ESLint errors.
