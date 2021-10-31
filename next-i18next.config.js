ad46fc121d34dd6cb3b37f1a5d826213a000a120
Peer Richelsen <peeroke@gmail.com>
2021-10-31 21:31:21 +0000

    added jp (japanese) (#1085)

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "fr", "it", "ru", "es", "de", "pt", "ro", "nl", "pt-BR", "es-419", "ko"],
+    locales: ["en", "fr", "it", "ru", "es", "de", "pt", "ro", "nl", "pt-BR", "es-419", "ko", "jp"],
   },
   localePath: path.resolve("./public/static/locales"),
 };
