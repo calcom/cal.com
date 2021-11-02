a38d86aed279d81309624d7d05081bb09421bd0f
KATT <alexander@n1s.se>
2021-11-02 13:36:59 +0000

    maybe?

--- playwright/login.test.ts
+++ playwright/login.test.ts
@@ -3,7 +3,7 @@ jest.setTimeout(60e3);
 test("login with pro@example.com", async () => {
   const context = await browser.newContext();
   const page = await context.newPage();
-  await page.goto("http://localhost:3000/event-types");
+  await page.goto("http://localhost:3000/auth/login");
   // Click input[name="email"]
   await page.click('input[name="email"]');
   // Fill input[name="email"]
