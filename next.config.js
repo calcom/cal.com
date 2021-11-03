5f5875828175731e583edc35c74485ede86cc899
KATT <alexander@n1s.se>
2021-11-03 13:50:14 +0000

    Revert "disable i18n test"
    This reverts commit a3fd5d70fc88bc078d23b2fecfbdedc501b085c2.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withTM = require("@vercel/edge-functions-ui/transpile")(["react-timezone-select"]);
-// const { i18n } = require("./next-i18next.config");
+const { i18n } = require("./next-i18next.config");
 
 // So we can test deploy previews preview
 if (process.env.VERCEL_URL && !process.env.BASE_URL) {
@@ -55,7 +55,7 @@ plugins.push(withTM);
 
 // prettier-ignore
 module.exports = () => plugins.reduce((acc, next) => next(acc), {
-  // i18n,
+  i18n,
   eslint: {
     // This allows production builds to successfully complete even if the project has ESLint errors.
     ignoreDuringBuilds: true,
