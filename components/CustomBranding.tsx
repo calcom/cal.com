b75647f784ce5eb6a87c7aab39a06d42804a3e61
Syed Ali Shahbaz <alishahbaz7@gmail.com>
2021-11-11 19:37:34 +0530

    lint fix

--- components/CustomBranding.tsx
+++ components/CustomBranding.tsx
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
 
-const BrandColor = ({ val="#292929" }: { val: string | undefined }) => {
+const BrandColor = ({ val = "#292929" }: { val: string | undefined }) => {
   useEffect(() => {
     document.documentElement.style.setProperty("--brand-color", val);
-  },[val]);
+  }, [val]);
   return null;
 };
 
