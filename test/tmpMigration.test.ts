ec252f2183cdbf1b9f6120f37d6cdf8a7d2232e0
KATT <alexander@n1s.se>
2021-11-15 19:24:19 +0100

    seems to work

--- test/tmpMigration.test.ts
+++ test/tmpMigration.test.ts
@@ -1,9 +1,39 @@
 import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
 
 import prisma from "../lib/prisma";
-import { tmpMigration } from "../pages/api/availability/__tmp__migration";
 import { randomString } from "../playwright/lib/testUtils";
 
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+async function tmpMigration() {
+  await prisma.$queryRaw(`
+    insert into "Availability" ("userId", "startTime", "endTime", "days")
+    select
+      id as "userId", 
+      CAST(CONCAT(CAST(("startTime" / 60) AS text), ':00') AS time) AT TIME ZONE "timeZone" AT TIME ZONE 'UTC',
+      CAST(CONCAT(CAST(("endTime" / 60) AS text), ':00') AS time) AT TIME ZONE "timeZone" AT TIME ZONE 'UTC',
+      ARRAY [0,1,2,3,4,5,6]
+    from 
+      (
+        select 
+          users.id, 
+          users."startTime", 
+          users."endTime", 
+          users."timeZone",
+          count("Availability".id) as availability_count
+        from users 
+        left join "Availability" on "Availability"."userId" = users.id
+        where users."timeZone" != ''
+        group by users.id
+      ) usersWithAvailabilityNumber
+    where availability_count < 1
+  `);
+
+  return NaN;
+}
 test("tmpMigration", async () => {
   // const unknownTimezoneUser = await prisma.user.create({
   //   data: {
