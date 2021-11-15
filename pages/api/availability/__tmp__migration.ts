707f4505174bfed488d67cd9ab493b9323828598
KATT <alexander@n1s.se>
2021-11-15 12:41:44 +0100

    tmp mig

---
+++ pages/api/availability/__tmp__migration.ts
@@ -0,0 +1,53 @@
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import prisma from "@lib/prisma";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      startTime: true,
+      endTime: true,
+      timeZone: true,
+      availability: {
+        select: {
+          id: true,
+        },
+      },
+    },
+  });
+
+  const usersWithNoAvailability = users.filter((user) => user.availability.length === 0);
+
+  for (const user of usersWithNoAvailability) {
+    // convert startTime/endTime to timezone using `user.timezone`
+    const baseDate = dayjs()
+      .tz(user.timeZone ?? "Europe/London")
+      .set("hour", 0)
+      .set("minute", 0)
+      .set("second", 0)
+      .set("millisecond", 0);
+
+    const startTime = baseDate.add(user.startTime, "minute").toDate();
+    const endTime = baseDate.add(user.endTime, "minute").toDate();
+    // create availabiltiy for every day of the week
+    await prisma.availability.create({
+      data: {
+        userId: user.id,
+        days: [0, 1, 2, 3, 4, 5, 6],
+        startTime,
+        endTime,
+      },
+    });
+  }
+
+  res.send({
+    message: `${usersWithNoAvailability.length} users updated`,
+  });
+}
