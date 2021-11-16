681cc45643c143c51dd0b06aa5357d82d0b4fd07
Syed Ali Shahbaz <alishahbaz7@gmail.com>
2021-11-16 13:17:48 +0530

    conflict resolution

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,6 +1,14 @@
+import { trpc } from "@lib/trpc";
+
+import CustomBranding from "@components/CustomBranding";
+
 export default function Loader() {
+  const query = trpc.useQuery(["viewer.me"]);
+  const user = query.data;
+
   return (
     <div className="loader border-brand dark:border-white">
+      <CustomBranding val={user?.brandColor || "#292929"} />
       <span className="loader-inner bg-brand dark:bg-white"></span>
     </div>
   );
