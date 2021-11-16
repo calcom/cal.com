f4cac4cf5dabbea162d63e2dfda466c367f92e63
Alex van Andel <me@alexvanandel.com>
2021-11-16 13:03:18 +0100

    Added UTC overflow to days

--- lib/availability.ts
+++ lib/availability.ts
@@ -4,7 +4,7 @@ import customParseFormat from "dayjs/plugin/customParseFormat";
 import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
 
-import { Schedule, TimeRange } from "./types/schedule";
+import { Schedule, TimeRange, WorkingHours } from "./types/schedule";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
@@ -72,17 +72,39 @@ export function getWorkingHours(
     ];
   }
 
-  const workingHours = availability.map((schedule) => ({
-    days: schedule.days,
-    startTime: dayjs
+  const workingHours = availability.reduce((workingHours: WorkingHours[], schedule) => {
+    const startTime = dayjs
       .utc(dayjs.utc(schedule.startTime).format("HH:mm:ss"), "HH:mm:ss")
       .tz(timeZone, true)
-      .diff(dayjs.utc().startOf("day"), "minutes"),
-    endTime: dayjs
+      .diff(dayjs.utc().startOf("day"), "minutes");
+    const endTime = dayjs
       .utc(dayjs.utc(schedule.endTime).format("HH:mm:ss"), "HH:mm:ss")
       .tz(timeZone, true)
-      .diff(dayjs.utc().startOf("day"), "minutes"),
-  }));
+      .diff(dayjs.utc().startOf("day"), "minutes");
+    // add to working hours, keeping startTime and endTimes between bounds (0-1439)
+    workingHours.push({
+      days: schedule.days,
+      startTime: Math.max(0, Math.min(1439, startTime)),
+      endTime: Math.max(0, Math.min(1439, endTime)),
+    });
+    // check for overflow to the previous day
+    if (startTime < 0 || endTime < 0) {
+      workingHours.push({
+        days: schedule.days.map((day) => day - 1),
+        startTime: startTime + 1440,
+        endTime: Math.min(endTime + 1440, 1439),
+      });
+    }
+    // else, check for overflow in the next day
+    else if (startTime > 1439 || endTime > 1439) {
+      workingHours.push({
+        days: schedule.days.map((day) => day + 1),
+        startTime: Math.max(startTime - 1439, 0),
+        endTime: endTime - 1439,
+      });
+    }
+    return workingHours;
+  }, []);
 
   workingHours.sort((a, b) => a.startTime - b.startTime);
 
