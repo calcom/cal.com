a80394dd57f7ab4f47a08f6a946fe213cec37dec
Bailey Pumfleet <pumfleet@hey.com>
2021-11-26 16:28:01 +0000

    Fix sidebar

--- components/Shell.tsx
+++ components/Shell.tsx
@@ -212,14 +212,14 @@ export default function Shell(props: {
                         className={classNames(
                           item.current
                             ? "bg-neutral-100 text-neutral-900 dark:bg-gray-700 dark:text-white"
-                            : "text-neutral-500 hover:bg-gray-50 hover:text-neutral-900 dark:text-gray-400",
+                            : "text-neutral-500 hover:bg-gray-50 hover:text-neutral-900 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white",
                           "group flex items-center px-2 py-2 text-sm font-medium rounded-sm"
                         )}>
                         <item.icon
                           className={classNames(
                             item.current
                               ? "text-neutral-500 dark:text-gray-100"
-                              : "text-neutral-400 group-hover:text-neutral-500",
+                              : "text-neutral-400 group-hover:text-neutral-500 dark:group-hover:text-gray-100",
                             "mr-3 flex-shrink-0 h-5 w-5"
                           )}
                           aria-hidden="true"
