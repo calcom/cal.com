00037e978f49dce2a3c056e1ab8a31fef54b1691
Deepak Prabhakara <deepak@boxyhq.com>
2021-12-13 18:34:01 +0000

    cleaned up cors, not needed by the oauth flow

--- pages/api/auth/saml/token.ts
+++ pages/api/auth/saml/token.ts
@@ -1,31 +1,9 @@
-import Cors from "cors";
 import { NextApiRequest, NextApiResponse } from "next";
 
 import jackson from "@lib/jackson";
 
-// Initializing the cors middleware
-const cors = Cors({
-  methods: ["GET", "HEAD"],
-});
-
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-
-      return resolve(result);
-    });
-  });
-}
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    await runMiddleware(req, res, cors);
-
     const { oauthController } = await jackson();
     const result = await oauthController.token(req.body);
 
