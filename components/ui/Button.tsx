8bbfc0c7f0b225dcfd93edebb96a2ee610b41659
Syed Ali Shahbaz <52925846+alishaz-polymath@users.noreply.github.com>
2021-12-14 16:09:32 +0530

    Adds complementing text color for various brand colors that the user might choose (#1289)
    * added contrast evaluator
    
    * added brandtext --WIP
    
    * further changes and fixes
    
    * fixed type err
    
    * fixed datepicker bug
    
    * changed brandtext to brandcontrast
    
    * further dark mode changes
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -64,7 +64,7 @@ export const Button = forwardRef<HTMLAnchorElement | HTMLButtonElement, ButtonPr
         color === "primary" &&
           (disabled
             ? "border border-transparent bg-gray-400 text-white"
-            : "border border-transparent dark:text-black text-white bg-brand dark:bg-white hover:bg-opacity-90 hover:shadow-md focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-neutral-900"),
+            : "border border-transparent dark:text-brandcontrast text-brandcontrast bg-brand dark:bg-brand hover:bg-opacity-90 hover:shadow-md focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-neutral-900"),
         color === "secondary" &&
           (disabled
             ? "border border-gray-200 text-gray-400 bg-white"
