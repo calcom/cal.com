d39068b79d04babb0788cdc056da1d8fca27bce4
Syed Ali Shahbaz <alishahbaz7@gmail.com>
2021-12-14 16:15:47 +0530

    added brand color to dark mode timepicker

--- components/booking/AvailableTimes.tsx
+++ components/booking/AvailableTimes.tsx
@@ -84,7 +84,7 @@ const AvailableTimes: FC<AvailableTimesProps> = ({
               <div key={slot.time.format()}>
                 <Link href={bookingUrl}>
                   <a
-                    className="block py-4 mb-2 font-medium bg-white border rounded-sm dark:bg-gray-600 text-primary-500 dark:text-neutral-200 border-brand dark:border-transparent hover:text-white hover:bg-brand hover:text-brandcontrast dark:hover:border-black dark:hover:bg-black"
+                    className="block py-4 mb-2 font-medium bg-white border rounded-sm dark:bg-gray-600 text-primary-500 dark:text-neutral-200 border-brand dark:border-transparent hover:text-white hover:bg-brand hover:text-brandcontrast dark:hover:border-black dark:hover:bg-brand dark:hover:text-brandcontrast"
                     data-testid="time">
                     {slot.time.format(timeFormat)}
                   </a>
