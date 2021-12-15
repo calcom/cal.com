6ce8264a7af4238d5a62e31e37bf552862184be4
Alex van Andel <me@alexvanandel.com>
2021-12-15 23:54:41 +0100

    startOf blocked out the AvailableTimes but not the DatePicker

--- lib/slots.ts
+++ lib/slots.ts
@@ -19,7 +19,7 @@ export type GetSlots = {
 
 const getMinuteOffset = (date: Dayjs, step: number) => {
   // Diffs the current time with the given date and iff same day; (handled by 1440) - return difference; otherwise 0
-  const minuteOffset = Math.min(date.diff(date.hour(0).minute(0).second(0), "minute"), 1440) % 1440;
+  const minuteOffset = Math.min(date.diff(dayjs.utc().hour(0).minute(0).second(0), "minute"), 1440) % 1440;
   // round down to nearest step
   return Math.ceil(minuteOffset / step) * step;
 };
@@ -53,8 +53,8 @@ const getSlots = ({ inviteeDate, frequency, minimumBookingNotice, workingHours }
     if (
       localWorkingHours.some((hours) =>
         slot.isBetween(
-          inviteeDate.startOf("day").add(hours.startTime, "minute"),
-          inviteeDate.startOf("day").add(hours.endTime, "minute"),
+          inviteeDate.hour(0).minute(0).second(0).add(hours.startTime, "minute"),
+          inviteeDate.hour(0).minute(0).second(0).add(hours.endTime, "minute"),
           null,
           "[)"
         )
