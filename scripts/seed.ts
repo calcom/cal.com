1c97cf45c49fa5fd17300b57553c01ff82b5d9d8
Nathaniel <nat.kitzke@gmail.com>
2021-12-14 21:32:40 -0600

    fix linting

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,7 +1,12 @@
-import { MembershipRole, Prisma, PrismaClient, UserPlan } from "@prisma/client";
-import dayjs from "dayjs";
-import { uuid } from "short-uuid";
+import {
+  // MembershipRole,
+  Prisma,
+  PrismaClient,
+  UserPlan,
+} from "@prisma/client";
 
+// import dayjs from "dayjs";
+// import { uuid } from "short-uuid";
 import { hashPassword } from "../lib/auth";
 import { DEFAULT_SCHEDULE, getAvailabilityFromSchedule } from "../lib/availability";
 
@@ -111,46 +116,46 @@ async function createUserAndEventType(opts: {
   return user;
 }
 
-async function createTeamAndAddUsers(
-  teamInput: Prisma.TeamCreateInput,
-  users: { id: number; username: string; role?: MembershipRole }[]
-) {
-  const createTeam = async (team: Prisma.TeamCreateInput) => {
-    try {
-      return await prisma.team.create({
-        data: {
-          ...team,
-        },
-      });
-    } catch (_err) {
-      if (_err instanceof Error && _err.message.indexOf("Unique constraint failed on the fields") !== -1) {
-        console.log(`Team '${team.name}' already exists, skipping.`);
-        return;
-      }
-      throw _err;
-    }
-  };
+// async function createTeamAndAddUsers(
+//   teamInput: Prisma.TeamCreateInput,
+//   users: { id: number; username: string; role?: MembershipRole }[]
+// ) {
+//   const createTeam = async (team: Prisma.TeamCreateInput) => {
+//     try {
+//       return await prisma.team.create({
+//         data: {
+//           ...team,
+//         },
+//       });
+//     } catch (_err) {
+//       if (_err instanceof Error && _err.message.indexOf("Unique constraint failed on the fields") !== -1) {
+//         console.log(`Team '${team.name}' already exists, skipping.`);
+//         return;
+//       }
+//       throw _err;
+//     }
+//   };
 
-  const team = await createTeam(teamInput);
-  if (!team) {
-    return;
-  }
+//   const team = await createTeam(teamInput);
+//   if (!team) {
+//     return;
+//   }
 
-  console.log(`🏢 Created team '${teamInput.name}' - ${process.env.BASE_URL}/team/${team.slug}`);
+//   console.log(`🏢 Created team '${teamInput.name}' - ${process.env.BASE_URL}/team/${team.slug}`);
 
-  for (const user of users) {
-    const { role = MembershipRole.OWNER, id, username } = user;
-    await prisma.membership.create({
-      data: {
-        teamId: team.id,
-        userId: id,
-        role: role,
-        accepted: true,
-      },
-    });
-    console.log(`\t👤 Added '${teamInput.name}' membership for '${username}' with role '${role}'`);
-  }
-}
+//   for (const user of users) {
+//     const { role = MembershipRole.OWNER, id, username } = user;
+//     await prisma.membership.create({
+//       data: {
+//         teamId: team.id,
+//         userId: id,
+//         role: role,
+//         accepted: true,
+//       },
+//     });
+//     console.log(`\t👤 Added '${teamInput.name}' membership for '${username}' with role '${role}'`);
+//   }
+// }
 
 async function main() {
   await createUserAndEventType({
@@ -287,16 +292,16 @@ async function main() {
     ],
   });
 
-  const freeUserTeam = await createUserAndEventType({
-    user: {
-      email: "teamfree@example.com",
-      password: "teamfree",
-      username: "teamfree",
-      name: "Team Free Example",
-      plan: "FREE",
-    },
-    eventTypes: [],
-  });
+  // const freeUserTeam = await createUserAndEventType({
+  //   user: {
+  //     email: "teamfree@example.com",
+  //     password: "teamfree",
+  //     username: "teamfree",
+  //     name: "Team Free Example",
+  //     plan: "FREE",
+  //   },
+  //   eventTypes: [],
+  // });
 
   // const proUserTeam = await createUserAndEventType({
   //   user: {
