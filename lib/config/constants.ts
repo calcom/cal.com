db1c7adb2daf63ef3bec05a4bfbeb649d016f94e
Nathaniel <nat.kitzke@gmail.com>
2021-12-16 13:53:01 -0600

    type in baseurl

--- lib/config/constants.ts
+++ lib/config/constants.ts
@@ -1,6 +1,6 @@
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
 export const BASE_URL = process.env.IS_SANDBOX
-  ? `https://https://calendso-sandbox.vercel.app`
+  ? `https://calendso-sandbox.vercel.app`
   : IS_PRODUCTION
   ? `https://bookings.bullbitcoin.com`
   : process.env.BASE_URL || `https://${process.env.VERCEL_URL}`;
