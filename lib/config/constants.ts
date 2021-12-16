27a84107484b74e7f91d223281e4ec33c06f9228
Nathaniel <nat.kitzke@gmail.com>
2021-12-16 13:24:28 -0600

    fix baseurl for sandbox

--- lib/config/constants.ts
+++ lib/config/constants.ts
@@ -1,4 +1,6 @@
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
-export const BASE_URL = IS_PRODUCTION
+export const BASE_URL = process.env.IS_SANDBOX
+  ? `https://https://calendso-sandbox.vercel.app`
+  : IS_PRODUCTION
   ? `https://bookings.bullbitcoin.com`
   : process.env.BASE_URL || `https://${process.env.VERCEL_URL}`;
