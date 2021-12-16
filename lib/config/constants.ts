525cb430a4c114bcb0683ea8dbfab8875f5bd2cd
Alejandro Aguilar Higuera <aaguilar@grupoadhoc.mx>
2021-12-16 11:12:00 -0700

    add NEXT_PUBLIC_SITE_NAME alias SITE_NAME .env.example

--- lib/config/constants.ts
+++ lib/config/constants.ts
@@ -1,2 +1,3 @@
 export const BASE_URL = process.env.BASE_URL || `https://${process.env.VERCEL_URL}`;
+export const SITE_NAME = process.env.NEXT_PUBLIC_SITE_NAME || "Cal.com";
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
