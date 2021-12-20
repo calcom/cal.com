953da4766998a7e03125c266bf33fed56455c874
zomars <zomars@me.com>
2021-12-20 14:35:39 -0700

    Migration fixes

--- prisma/migrations/20211220192703_email_to_lowercase/migration.sql
+++ prisma/migrations/20211220192703_email_to_lowercase/migration.sql
@@ -1,2 +1,6 @@
+-- DropIndex
+DROP INDEX "users.email_unique";
 -- UpdateTable
-UPDATE users SET email=lower(email);
+UPDATE users SET email=LOWER(email);
+-- CreateIndex
+CREATE UNIQUE INDEX "users.email_unique" ON "users"("email");
