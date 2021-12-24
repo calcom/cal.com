87101d3eae69ba4b2dc8f8cc10850f2313460436
André Ricardo <andrermr.dev@gmail.com>
2021-12-24 11:13:45 -0400

    :truck: pac-parque-10 > pac-p10

--- common/mock/sites.ts
+++ common/mock/sites.ts
@@ -4,15 +4,10 @@ const availableSites = [
   "pac-alvorada",
   "pac-sao-jose",
   "pac-galeria-dos-remedios",
-  "pac-parque-10",
+  "pac-p10",
   "pac-educandos",
   "pac-compensa",
   "pac-leste",
-  "pac-iranduba",
-  "pac-parintins",
-  "pac-itacoatiara",
-  "pac-manacapuru",
-  "paac-posto-avancado-atendimento-cidadania",
 ] as const;
 
 export type TSite = typeof availableSites[number];
@@ -23,13 +18,8 @@ export const sitesTranslation: Record<TSite, string> = {
   "pac-alvorada": "PAC Alvorada",
   "pac-sao-jose": "PAC São José",
   "pac-galeria-dos-remedios": "PAC Galeria dos Remédios",
-  "pac-parque-10": "PAC Parque 10",
+  "pac-p10": "PAC Parque 10",
   "pac-educandos": "PAC Educandos",
   "pac-compensa": "PAC Compensa",
   "pac-leste": "PAC Leste",
-  "pac-iranduba": "PAC Iranduba",
-  "pac-parintins": "PAC Parintins",
-  "pac-itacoatiara": "PAC Itacoatiara",
-  "pac-manacapuru": "PAC Manacapuru",
-  "paac-posto-avancado-atendimento-cidadania": "PAAC Posto Avançado Atendimento Cidadania",
 };
