e509c0a39732d4d6d1e7a4d7751c7c794d0228ea
zomars <zomars@me.com>
2022-01-04 12:03:57 -0700

    Allow cross-domain cookies

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,12 +1,29 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 import { authenticator } from "otplib";
+import { URL } from "url";
 
 import { ErrorCode, Session, verifyPassword } from "@lib/auth";
 import { symmetricDecrypt } from "@lib/crypto";
 import prisma from "@lib/prisma";
 
+const useSecureCookies = process.env.NEXTAUTH_URL!.startsWith("https://");
+const cookiePrefix = useSecureCookies ? "__Secure-" : "";
+const hostName = new URL(process.env.NEXTAUTH_URL!).hostname;
+
 export default NextAuth({
+  cookies: {
+    sessionToken: {
+      name: `${cookiePrefix}next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: useSecureCookies,
+        domain: `${hostName === "localhost" ? "" : "."}${hostName}`, // add a . in front so that subdomains are included
+      },
+    },
+  },
   session: {
     jwt: true,
   },
