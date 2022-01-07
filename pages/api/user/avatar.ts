f4863cf5a8fada77a88446cfffa1d0c1bc567c33
Syed Ali Shahbaz <alishahbaz7@gmail.com>
2022-01-07 14:57:28 +0530

    adding gravatar fallback support --wip

--- pages/api/user/avatar.ts
+++ pages/api/user/avatar.ts
@@ -1,4 +1,3 @@
-// import fs from "fs";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 import { getSession } from "@lib/auth";
@@ -10,11 +9,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     res.status(401).json({ message: "Not authenticated" });
     return;
   }
-
-  const decoded = req.body.avatar.toString().replace("data:image/png;base64,", "");
-  const imageResp = new Buffer(decoded, "base64");
-  console.log("imageResp=>", imageResp);
-  //   const imageBuffer = fs.readFileSync(req.body.avatar);
+  // Add support for empty req.body.avatar (gravatar)
+  let decoded = req.body.avatar.toString().replace("data:image/png;base64,", "");
+  decoded = req.body.avatar.toString().replace("data:image/jpeg;base64,", "");
+  const imageResp = !req.body.gravatar ? Buffer.from(decoded, "base64") : Buffer.from(decoded, "utf-8");
+  console.log("decoded=>", decoded, "++imageResp=>", imageResp);
   res.setHeader("Content-Type", "image/png");
   res.send(imageResp);
 }
