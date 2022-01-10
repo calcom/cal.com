ae66664023d0e1abc873c10f55bb7c53642acc86
Syed Ali Shahbaz <alishahbaz7@gmail.com>
2022-01-10 21:13:32 +0530

    --wip

--- components/seo/head-seo.tsx
+++ components/seo/head-seo.tsx
@@ -71,7 +71,7 @@ const constructImage = (name: string, description: string, username: string): st
   return (
     encodeURIComponent("Meet **" + name + "** <br>" + description).replace(/'/g, "%27") +
     ".png?md=1&images=https%3A%2F%2Fcal.com%2Flogo-white.svg&images=" +
-    process.env.BASE_URL +
+    process.env.NEXT_PUBLIC_APP_URL +
     "/" +
     username +
     "/avatar.png"
