55b8db6cd3a950943dd197963b77bb32dc0041c5
Jamie <ijamespine@me.com>
2022-01-10 23:13:51 -0800

    working modal

--- components/eventtype/CreateEventType.tsx
+++ components/eventtype/CreateEventType.tsx
@@ -1,7 +1,8 @@
 import { ChevronDownIcon, PlusIcon } from "@heroicons/react/solid";
 import { SchedulingType } from "@prisma/client";
 import { useRouter } from "next/router";
-import React, { useRef } from "react";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
 import { useMutation } from "react-query";
 
 import { HttpError } from "@lib/core/http/error";
@@ -12,7 +13,7 @@ import showToast from "@lib/notification";
 import { CreateEventType } from "@lib/types/event-type";
 
 import { Dialog, DialogClose, DialogContent } from "@components/Dialog";
-import { TextField, InputLeading, TextAreaField } from "@components/form/fields";
+import { TextField, InputLeading, TextAreaField, Form } from "@components/form/fields";
 import Avatar from "@components/ui/Avatar";
 import { Button } from "@components/ui/Button";
 import Dropdown, {
@@ -45,7 +46,21 @@ export default function CreateEventTypeButton(props: Props) {
   const { t } = useLocale();
   const router = useRouter();
   const modalOpen = useToggleQuery("new");
-  const slugRef = useRef<HTMLInputElement>(null);
+
+  const form = useForm<CreateEventType>({
+    defaultValues: { length: 15 },
+  });
+  const { setValue, watch, register } = form;
+
+  useEffect(() => {
+    const subscription = watch((value, { name, type }) => {
+      if (name === "title" && type === "change") {
+        if (value.title) setValue("slug", value.title.replace(/\s+/g, "-").toLowerCase());
+        else setValue("slug", "");
+      }
+    });
+    return () => subscription.unsubscribe();
+  }, [watch, setValue]);
 
   // URL encoded params
   const teamId: number | null = Number(router.query.teamId) || null;
@@ -66,6 +81,8 @@ export default function CreateEventTypeButton(props: Props) {
 
   // inject selection data into url for correct router history
   const openModal = (option: EventTypeParent) => {
+    console.log("HIHIHI");
+
     router.push({
       pathname: router.pathname,
       query: {
@@ -81,9 +98,12 @@ export default function CreateEventTypeButton(props: Props) {
     });
   };
 
-  // remove url params after close modal to
+  // remove url params after close modal to reset state
   const closeModal = () => {
-    console.log({ router });
+    router.replace({
+      pathname: router.pathname,
+      query: { id: router.query.id },
+    });
   };
 
   return (
@@ -127,62 +147,40 @@ export default function CreateEventTypeButton(props: Props) {
             <p className="text-sm text-gray-500">{t("new_event_type_to_book_description")}</p>
           </div>
         </div>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-
-            const target = e.target as unknown as Record<
-              "title" | "slug" | "description" | "length" | "schedulingType",
-              { value: string }
-            >;
-
+        <Form
+          form={form}
+          handleSubmit={(values) => {
             const payload: CreateEventType = {
-              title: target.title.value,
-              slug: target.slug.value,
-              description: target.description.value,
-              length: parseInt(target.length.value),
+              title: values.title,
+              slug: values.slug,
+              description: values.description,
+              length: values.length,
             };
-
             if (router.query.teamId) {
               payload.teamId = parseInt(`${router.query.teamId}`, 10);
-              payload.schedulingType = target.schedulingType.value as SchedulingType;
+              payload.schedulingType = values.schedulingType as SchedulingType;
             }
 
             createMutation.mutate(payload);
           }}>
-          <div className="inline-block mt-3 space-y-4">
-            <TextField
-              label={t("title")}
-              name="title"
-              placeholder={t("quick_chat")}
-              onChange={(e) => {
-                if (!slugRef.current) {
-                  return;
-                }
-                slugRef.current.value = e.target.value.replace(/\s+/g, "-").toLowerCase();
-              }}
-            />
+          <div className="mt-3 space-y-4">
+            <TextField label={t("title")} placeholder={t("quick_chat")} {...register("title")} />
 
             <TextField
               label={t("url")}
-              ref={slugRef}
-              name="url"
               required
               addOnLeading={
                 <InputLeading>
                   {process.env.NEXT_PUBLIC_APP_URL}/{pageSlug}/
                 </InputLeading>
               }
-              onChange={(e) => {
-                if (!slugRef.current) return;
-                slugRef.current.value = e.target.value.replace(/\s+/g, "-").toLowerCase();
-              }}
+              {...register("slug")}
             />
 
             <TextAreaField
               label={t("description")}
-              name="description"
               placeholder={t("quick_video_meeting")}
+              {...register("description")}
             />
 
             <div className="relative">
@@ -193,7 +191,7 @@ export default function CreateEventTypeButton(props: Props) {
                 defaultValue={15}
                 label={t("length")}
                 className="pr-20"
-                name="length"
+                {...register("length")}
               />
               <div className="absolute inset-y-0 right-0 flex items-center pt-4 mt-1.5 pr-3 text-sm text-gray-400">
                 {t("minutes")}
@@ -206,7 +204,8 @@ export default function CreateEventTypeButton(props: Props) {
                   {t("scheduling_type")}
                 </label>
                 <RadioArea.Group
-                  name="schedulingType"
+                  {...register("schedulingType")}
+                  onChange={(val) => form.setValue("schedulingType", val as SchedulingType)}
                   className="relative flex mt-1 space-x-6 rounded-sm shadow-sm">
                   <RadioArea.Item value={SchedulingType.COLLECTIVE} className="w-1/2 text-sm">
                     <strong className="block mb-1">{t("collective")}</strong>
@@ -228,7 +227,7 @@ export default function CreateEventTypeButton(props: Props) {
               <Button color="secondary">{t("cancel")}</Button>
             </DialogClose>
           </div>
-        </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
