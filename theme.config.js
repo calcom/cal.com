edc9f1c46503f170a3c57f9f9b521e1298ec3612
Bailey Pumfleet <pumfleet@hey.com>
2022-01-17 14:17:14 +0000

    Update config

--- theme.config.js
+++ theme.config.js
@@ -1,30 +1,29 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
-  logo: (
-    <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
-      <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
-      </span>
-    </>
-  ),
+  github: 'https://github.com/calendso/calendso',
+  docsRepositoryBase: 'https://github.com/calendso/docs/blob/master',
+  titleSuffix: ' | Cal.com',
+  logo: <img className="hidden md:inline" src="https://cal.com/logo.svg" />,
   head: (
     <>
       <meta name="msapplication-TileColor" content="#ffffff" />
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta
+        name="description"
+        content="Explore advice and explanations for all of our features, and discover new tips and tricks to get the most out of your subscription."
+      />
+      <meta
+        name="og:description"
+        content="Explore advice and explanations for all of our features, and discover new tips and tricks to get the most out of your subscription."
+      />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="twitter:image" content="https://cal.com/og-image.png" />
+      <meta name="twitter:site:domain" content="docs.cal.com" />
+      <meta name="twitter:url" content="https://docs.cal.com" />
+      <meta name="og:title" content="Cal.com Documentation" />
+      <meta name="og:image" content="https://cal.com/og-image.png" />
+      <meta name="apple-mobile-web-app-title" content="Cal.com Docs" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -62,6 +61,8 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: (
+    <>© {new Date().getFullYear()} Cal.com, Inc. All rights reserved.</>
+  ),
   unstable_faviconGlyph: '👋',
 }
