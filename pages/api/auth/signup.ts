77f481b62b0cee6de20c4e067c31e24b1ed591fc
Miguel Nieto A <anietom@uninorte.edu.co>
2022-01-21 10:19:34 -0500

    Remove emailVerified not null from query

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -41,13 +41,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           email: userEmail,
         },
       ],
-      AND: [
-        {
-          emailVerified: {
-            not: null,
-          },
-        },
-      ],
     },
   });
 
