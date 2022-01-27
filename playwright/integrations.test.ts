f9f394814413b44195e1760049b7c5f9784febc2
Syed Ali Shahbaz <alishahbaz7@gmail.com>
2022-01-27 20:08:30 +0530

    test changes revert

--- playwright/integrations.test.ts
+++ playwright/integrations.test.ts
@@ -63,12 +63,12 @@ test.describe("integrations", () => {
     body.payload.location = dynamic;
     for (const attendee of body.payload.attendees) {
       attendee.timeZone = dynamic;
-      attendee.language.locale = dynamic;
+      attendee.language = dynamic;
     }
     body.payload.organizer.timeZone = dynamic;
-    body.payload.organizer.language.locale = dynamic;
+    body.payload.organizer.language = dynamic;
     body.payload.uid = dynamic;
-    // body.payload.additionInformation = dynamic;
+    body.payload.additionInformation = dynamic;
 
     // if we change the shape of our webhooks, we can simply update this by clicking `u`
     // console.log("BODY", body);
