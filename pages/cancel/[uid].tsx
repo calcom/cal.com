07b75dadbd6f171339b94ff7cbb43d16fe9e5b8b
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2022-01-28 18:40:29 +0100

    feat: add option to provide cancellation reason for email (#1587)
    * feat: add option to provide cancellation reason for email
    
    * chore: move pos of getCancellationReason method in classes
    
    * fix: only show cancellation reason if given

--- pages/cancel/[uid].tsx
+++ pages/cancel/[uid].tsx
@@ -12,6 +12,7 @@ import { collectPageParameters, telemetryEventTypes, useTelemetry } from "@lib/t
 import { inferSSRProps } from "@lib/types/inferSSRProps";
 
 import CustomBranding from "@components/CustomBranding";
+import { TextField } from "@components/form/fields";
 import { HeadSeo } from "@components/seo/head-seo";
 import { Button } from "@components/ui/Button";
 
@@ -25,6 +26,7 @@ export default function Type(props: inferSSRProps<typeof getServerSideProps>) {
   const [is24h] = useState(false);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(props.booking ? null : t("booking_already_cancelled"));
+  const [cancellationReason, setCancellationReason] = useState<string>("");
   const telemetry = useTelemetry();
 
   return (
@@ -89,50 +91,60 @@ export default function Type(props: inferSSRProps<typeof getServerSideProps>) {
                       </div>
                     </div>
                     {props.cancellationAllowed && (
-                      <div className="mt-5 space-x-2 text-center sm:mt-6">
-                        <Button
-                          color="secondary"
-                          data-testid="cancel"
-                          onClick={async () => {
-                            setLoading(true);
+                      <div className="mt-5 sm:mt-6">
+                        <TextField
+                          name={t("cancellation_reason")}
+                          placeholder={t("cancellation_reason_placeholder")}
+                          value={cancellationReason}
+                          onChange={(e) => setCancellationReason(e.target.value)}
+                          className="mb-5 sm:mb-6"
+                        />
+                        <div className="text-center space-x-2">
+                          <Button
+                            color="secondary"
+                            data-testid="cancel"
+                            onClick={async () => {
+                              setLoading(true);
 
-                            const payload = {
-                              uid: uid,
-                            };
+                              const payload = {
+                                uid: uid,
+                                reason: cancellationReason,
+                              };
 
-                            telemetry.withJitsu((jitsu) =>
-                              jitsu.track(telemetryEventTypes.bookingCancelled, collectPageParameters())
-                            );
-
-                            const res = await fetch("/api/cancel", {
-                              body: JSON.stringify(payload),
-                              headers: {
-                                "Content-Type": "application/json",
-                              },
-                              method: "DELETE",
-                            });
-
-                            if (res.status >= 200 && res.status < 300) {
-                              await router.push(
-                                `/cancel/success?name=${props.profile.name}&title=${
-                                  props.booking.title
-                                }&eventPage=${props.profile.slug}&team=${
-                                  props.booking.eventType?.team ? 1 : 0
-                                }`
+                              telemetry.withJitsu((jitsu) =>
+                                jitsu.track(telemetryEventTypes.bookingCancelled, collectPageParameters())
                               );
-                            } else {
-                              setLoading(false);
-                              setError(
-                                `${t("error_with_status_code_occured", { status: res.status })} ${t(
-                                  "please_try_again"
-                                )}`
-                              );
-                            }
-                          }}
-                          loading={loading}>
-                          {t("cancel")}
-                        </Button>
-                        <Button onClick={() => router.push("/reschedule/" + uid)}>{t("reschedule")}</Button>
+
+                              const res = await fetch("/api/cancel", {
+                                body: JSON.stringify(payload),
+                                headers: {
+                                  "Content-Type": "application/json",
+                                },
+                                method: "DELETE",
+                              });
+
+                              if (res.status >= 200 && res.status < 300) {
+                                await router.push(
+                                  `/cancel/success?name=${props.profile.name}&title=${
+                                    props.booking.title
+                                  }&eventPage=${props.profile.slug}&team=${
+                                    props.booking.eventType?.team ? 1 : 0
+                                  }`
+                                );
+                              } else {
+                                setLoading(false);
+                                setError(
+                                  `${t("error_with_status_code_occured", { status: res.status })} ${t(
+                                    "please_try_again"
+                                  )}`
+                                );
+                              }
+                            }}
+                            loading={loading}>
+                            {t("cancel")}
+                          </Button>
+                          <Button onClick={() => router.push("/reschedule/" + uid)}>{t("reschedule")}</Button>
+                        </div>
                       </div>
                     )}
                   </>
