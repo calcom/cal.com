fab6c8bed3c488d1e690a2dc8f5953f328b181f2
Peer Richelsen <peeroke@richelsen.net>
2022-01-31 12:09:26 +0000

    updated favicon, added cal sans

--- theme.config.js
+++ theme.config.js
@@ -1,8 +1,12 @@
 export default {
-  github: 'https://github.com/calendso/calendso',
+  github: 'https://github.com/calendso/docs',
   docsRepositoryBase: 'https://github.com/calendso/docs/blob/master',
   titleSuffix: ' | Cal.com',
-  logo: <img className="hidden md:inline" src="https://cal.com/logo.svg" />,
+  logo: (
+    <h4 className="m-0">
+      Cal.com
+    </h4>
+  ),
   head: (
     <>
       <meta name="msapplication-TileColor" content="#ffffff" />
@@ -24,16 +28,11 @@ export default {
       <meta name="og:title" content="Cal.com Documentation" />
       <meta name="og:image" content="https://cal.com/og-image.png" />
       <meta name="apple-mobile-web-app-title" content="Cal.com Docs" />
+
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/apple-icon-180x180.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href="/android-icon-192x192.png"
+        href="/apple-touch-icon.png"
       />
       <link
         rel="icon"
@@ -44,16 +43,13 @@ export default {
       <link
         rel="icon"
         type="image/png"
-        sizes="96x96"
-        href="/favicon-96x96.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
         sizes="16x16"
         href="/favicon-16x16.png"
       />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+      <meta name="msapplication-TileColor" content="#ff0000" />
+      <meta name="theme-color" content="#ffffff" />
     </>
   ),
   search: true,
@@ -64,5 +60,4 @@ export default {
   footerText: (
     <>© {new Date().getFullYear()} Cal.com, Inc. All rights reserved.</>
   ),
-  unstable_faviconGlyph: '👋',
 }
