0fea543f1130f1eccc24681f2751852dd6d07180
Peer Richelsen <peeroke@richelsen.net>
2022-01-31 11:33:00 +0000

    wip

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ class MyDocument extends Document<Props> {
 
   render() {
     const { locale } = this.props.__NEXT_DATA__;
-    const dir = "rtl"; // locale === "ar" || locale === "he" ? "rtl" : "ltr";
+    const dir = locale === "ar" || locale === "he" ? "rtl" : "ltr";
 
     return (
       <Html lang={locale} dir={dir}>
