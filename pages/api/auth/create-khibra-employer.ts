e57b285614cca5af1bf500e4a3e7b65c21966bb6
Vyacheslav Mikhailenko <prostoslavan@gmail.com>
2022-02-01 10:23:45 +0200

    change pro plan for khibra employer, create default event type

--- pages/api/auth/create-khibra-employer.ts
+++ pages/api/auth/create-khibra-employer.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import { hashPassword } from "@lib/auth";
 import prisma from "@lib/prisma";
+import slugify from "@lib/slugify";
 
 import { IdentityProvider } from ".prisma/client";
 
@@ -13,29 +14,42 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return;
   }
 
-  const { employerId, employerName: username } = req.body;
+  const { employerId, employerName } = req.body;
 
+  const username = slugify(employerName);
   const email = getKhibraEmployerEmail(employerId);
   const hashedPassword = await hashPassword(khibraPassword);
 
-  const result = await prisma.user.upsert({
-    where: { email },
-    update: {
-      username,
-      password: hashedPassword,
-      emailVerified: new Date(Date.now()),
-      identityProvider: IdentityProvider.CAL,
-    },
-    create: {
+  const existingUser = await prisma.user.findFirst({ where: { email } });
+
+  if (existingUser) {
+    return res.status(422).json({ message: "khibra_employer_reg_duplicate" });
+  }
+
+  const user = await prisma.user.create({
+    data: {
       username,
       email,
       password: hashedPassword,
       emailVerified: new Date(Date.now()),
       identityProvider: IdentityProvider.CAL,
+      completedOnboarding: true,
+      locale: "en",
+      plan: "PRO",
+    },
+  });
+
+  await prisma.eventType.create({
+    data: {
+      userId: user.id,
+      users: { connect: { id: user.id } },
+      title: "Default title",
+      slug: "default-book",
+      length: 60,
     },
   });
 
   res.status(201).json({
-    calUserId: result.id,
+    calUserId: user.id,
   });
 }
