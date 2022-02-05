2f3969fedca60576712245b2f1b62efadabfd6e5
Philipp Dormann <philipp@philippdormann.de>
2022-02-05 02:23:45 +0100

    full JitsiVideoApiAdapter implementation
    ref #1445

--- lib/integrations/Jitsi/JitsiVideoApiAdapter.ts
+++ lib/integrations/Jitsi/JitsiVideoApiAdapter.ts
@@ -1,16 +1,31 @@
 import { v4 as uuidv4 } from "uuid";
 
+import { PartialReference } from "@lib/events/EventManager";
 import { VideoApiAdapter, VideoCallData } from "@lib/videoClient";
 
 const JitsiVideoApiAdapter = (): VideoApiAdapter => {
   return {
+    getAvailability: () => {
+      return Promise.resolve([]);
+    },
     createMeeting: async (): Promise<VideoCallData> => {
       const meetingID = uuidv4();
       return Promise.resolve({
         type: "jitsi_video",
         id: meetingID,
         password: "",
-        url: "https://meet.jit.si/cal/LwX3btM3zFyrrYshsy44fyG8bGQLiVPejGSDBMJLFSlAx7wzRP",
+        url: "https://meet.jit.si/cal/" + meetingID,
+      });
+    },
+    deleteMeeting: async (): Promise<void> => {
+      Promise.resolve();
+    },
+    updateMeeting: (bookingRef: PartialReference): Promise<VideoCallData> => {
+      return Promise.resolve({
+        type: "jitsi_video",
+        id: bookingRef.meetingId as string,
+        password: bookingRef.meetingPassword as string,
+        url: bookingRef.meetingUrl as string,
       });
     },
   };
