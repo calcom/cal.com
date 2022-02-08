48f8c9451b755722e77a1180641d97809f784800
zomars <zomars@me.com>
2022-02-08 15:24:07 -0700

    Type fixes

--- components/ui/form/CheckedSelect.tsx
+++ components/ui/form/CheckedSelect.tsx
@@ -1,15 +1,15 @@
-import { CheckIcon, XIcon } from "@heroicons/react/outline";
-import React, { ForwardedRef, useEffect, useState } from "react";
-
-import { useLocale } from "@lib/hooks/useLocale";
-
 import Avatar from "@components/ui/Avatar";
 import Select from "@components/ui/form/Select";
+import { CheckIcon, XIcon } from "@heroicons/react/outline";
+import { useLocale } from "@lib/hooks/useLocale";
+import React, { useEffect, useState } from "react";
+import { MultiValue } from "react-select";
 
 type CheckedSelectValue = {
   avatar: string;
   label: string;
   value: string;
+  disabled?: boolean;
 }[];
 
 export type CheckedSelectProps = {
@@ -21,25 +21,13 @@ export type CheckedSelectProps = {
   disabled: boolean;
 };
 
-export const CheckedSelect = React.forwardRef((props: CheckedSelectProps, ref: ForwardedRef<unknown>) => {
+export const CheckedSelect = (props: CheckedSelectProps) => {
   const [selectedOptions, setSelectedOptions] = useState<CheckedSelectValue>(props.defaultValue || []);
   const { t } = useLocale();
 
   useEffect(() => {
     props.onChange(selectedOptions);
   }, [selectedOptions]);
-
-  const formatOptionLabel = ({ label, avatar, disabled }) => (
-    <div className="flex">
-      <Avatar className="mr-3 h-6 w-6 rounded-full" displayName={label} imageSrc={avatar} />
-      {label}
-      {disabled && (
-        <div className="flex-grow">
-          <CheckIcon className="float-right h-6 w-6 text-neutral-500" />
-        </div>
-      )}
-    </div>
-  );
 
   const options = props.options.map((option) => ({
     ...option,
@@ -49,7 +37,7 @@ export const CheckedSelect = React.forwardRef((props: CheckedSelectProps, ref: F
   const removeOption = (value: string) =>
     setSelectedOptions(selectedOptions.filter((option) => option.value !== value));
 
-  const changeHandler = (selections) =>
+  const changeHandler = (selections: MultiValue<CheckedSelectValue[number]>) =>
     selections.forEach((selected) => {
       if (selectedOptions.find((option) => option.value === selected.value)) {
         removeOption(selected.value);
@@ -60,8 +48,7 @@ export const CheckedSelect = React.forwardRef((props: CheckedSelectProps, ref: F
 
   return (
     <>
-      <Select
-        ref={ref}
+      <Select<CheckedSelectValue[number], true>
         styles={{
           option: (styles, { isDisabled }) => ({
             ...styles,
@@ -71,10 +58,20 @@ export const CheckedSelect = React.forwardRef((props: CheckedSelectProps, ref: F
         name={props.name}
         placeholder={props.placeholder || t("select")}
         isSearchable={false}
-        formatOptionLabel={formatOptionLabel}
+        formatOptionLabel={({ label, avatar, disabled }) => (
+          <div className="flex">
+            <Avatar className="mr-3 h-6 w-6 rounded-full" alt={label} imageSrc={avatar} />
+            {label}
+            {disabled && (
+              <div className="flex-grow">
+                <CheckIcon className="float-right h-6 w-6 text-neutral-500" />
+              </div>
+            )}
+          </div>
+        )}
         options={options}
         isMulti
-        value={props.placeholder || t("select")}
+        // value={props.placeholder || t("select")}
         onChange={changeHandler}
       />
       {selectedOptions.map((option) => (
@@ -82,7 +79,7 @@ export const CheckedSelect = React.forwardRef((props: CheckedSelectProps, ref: F
           <Avatar
             className="inline h-6 w-6 rounded-full ltr:mr-2 rtl:ml-2"
             imageSrc={option.avatar}
-            displayName={option.label}
+            alt={option.label}
           />
           {option.label}
           <XIcon
@@ -93,7 +90,7 @@ export const CheckedSelect = React.forwardRef((props: CheckedSelectProps, ref: F
       ))}
     </>
   );
-});
+};
 
 CheckedSelect.displayName = "CheckedSelect";
 
