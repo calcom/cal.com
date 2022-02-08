5e9423ac5776682466ddcd6241f558f323dbc01c
Syed Ali Shahbaz <alishahbaz7@gmail.com>
2022-02-08 12:26:56 +0530

    added swatch component and integrated with brand color picker

--- components/ui/colorpicker.tsx
+++ components/ui/colorpicker.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { HexColorInput, HexColorPicker } from "react-colorful";
 
+import Swatch from "@components/Swatch";
+
 export type ColorPickerProps = {
   defaultValue: string;
   onChange: (text: string) => void;
@@ -8,19 +10,24 @@ export type ColorPickerProps = {
 
 const ColorPicker = (props: ColorPickerProps) => {
   const [color, setColor] = useState(props.defaultValue);
+  const [isOpen, toggle] = useState(false);
 
   return (
-    <>
-      <HexColorPicker
-        className="!w-32 !h-32"
-        color={color}
-        onChange={(val) => {
-          setColor(val);
-          props.onChange(val);
-        }}
-      />
+    <div className="relative flex items-center justify-center mt-1">
+      <Swatch size="sm" backgroundColor={color} onClick={() => toggle(!isOpen)} />
+
+      {isOpen && (
+        <HexColorPicker
+          className="!w-32 !h-32 !absolute !top-10 !left-0 !z-10"
+          color={color}
+          onChange={(val) => {
+            setColor(val);
+            props.onChange(val);
+          }}
+        />
+      )}
       <HexColorInput
-        className="block w-full px-3 py-2 mt-1 border border-gray-300 rounded-sm shadow-sm focus:outline-none focus:ring-neutral-800 focus:border-neutral-800 sm:text-sm"
+        className="block w-full px-3 py-2 ml-1 border border-gray-300 rounded-sm shadow-sm focus:outline-none focus:ring-neutral-800 focus:border-neutral-800 sm:text-sm"
         color={color}
         onChange={(val) => {
           setColor(val);
@@ -28,7 +35,7 @@ const ColorPicker = (props: ColorPickerProps) => {
         }}
         type="text"
       />
-    </>
+    </div>
   );
 };
 
