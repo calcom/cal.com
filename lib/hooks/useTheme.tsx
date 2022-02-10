83988a34c502b5bb3f9538782e4040c4de48a54c
Hariom Balhara <hariombalhara@gmail.com>
2022-02-10 13:12:14 +0530

    Uglify Theme Applier code

--- lib/hooks/useTheme.tsx
+++ lib/hooks/useTheme.tsx
@@ -1,12 +1,23 @@
 import Head from "next/head";
 import { useEffect, useState } from "react";
+import { minify as minifyType } from "uglify-js";
 
 import { Maybe } from "@trpc/server";
 
+const isServer = typeof window === "undefined";
+
+let minify: typeof minifyType;
+
+if (isServer) {
+  // HACK: To import uglify-js only on server use `require`
+  // Dynamic Imports are dynamic and thus can't be used in the component on server.
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  minify = require("uglify-js").minify;
+}
+
 // This method is stringified and executed only on client. So,
 // - Pass all the params explicitly to this method. Don't use closure
-// - Keep the code to minimum as it isn't minified.
-function themeFn(theme: string) {
+function applyTheme(theme: string) {
   const mediaQueryList = window.matchMedia("(prefers-color-scheme: dark)");
   const applyTheme = function (theme: string, darkMatch: boolean) {
     if (!theme) {
@@ -28,14 +39,22 @@ function themeFn(theme: string) {
 // makes sure the ui doesn't flash
 export default function useTheme(theme?: Maybe<string>) {
   const [isReady, setIsReady] = useState(false);
+
   useEffect(() => {
+    // TODO: isReady doesn't seem required now. This is also impacting PSI Score.
     setIsReady(true);
   }, []);
+
   function Theme() {
+    const isServer = typeof window === "undefined";
+    if (!isServer) {
+      return null;
+    }
+    const code = minify(applyTheme.toString()).code;
     const themeStr = theme ? `"${theme}"` : null;
     return (
       <Head>
-        <script dangerouslySetInnerHTML={{ __html: `(${themeFn.toString()})(${themeStr})` }}></script>
+        <script dangerouslySetInnerHTML={{ __html: `(${code})(${themeStr})` }}></script>
       </Head>
     );
   }
