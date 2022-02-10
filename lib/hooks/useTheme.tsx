2722d44b5610496e5409770993317769c77cdc14
Hariom Balhara <hariombalhara@gmail.com>
2022-02-10 12:45:06 +0530

    Listen to changes in pref-color-scheme and act

--- lib/hooks/useTheme.tsx
+++ lib/hooks/useTheme.tsx
@@ -3,6 +3,28 @@ import { useEffect, useState } from "react";
 
 import { Maybe } from "@trpc/server";
 
+// This method is stringified and executed only on client. So,
+// - Pass all the params explicitly to this method. Don't use closure
+// - Keep the code to minimum as it isn't minified.
+function themeFn(theme: string) {
+  const mediaQueryList = window.matchMedia("(prefers-color-scheme: dark)");
+  const applyTheme = function (theme: string, darkMatch: boolean) {
+    if (!theme) {
+      if (darkMatch) {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+      }
+    } else {
+      document.documentElement.classList.add(theme);
+    }
+  };
+  mediaQueryList.onchange = function (e) {
+    applyTheme(theme, e.matches);
+  };
+  applyTheme(theme, mediaQueryList.matches);
+}
+
 // makes sure the ui doesn't flash
 export default function useTheme(theme?: Maybe<string>) {
   const [isReady, setIsReady] = useState(false);
@@ -10,21 +32,10 @@ export default function useTheme(theme?: Maybe<string>) {
     setIsReady(true);
   }, []);
   function Theme() {
-    const themeString = theme ? `"${theme}"` : null;
+    const themeStr = theme ? `"${theme}"` : null;
     return (
       <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function (theme) {
-            if (!theme && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-              document.documentElement.classList.add("dark");
-            } else if (!theme) {
-              /** Uncovered case */
-            } else {
-              document.documentElement.classList.add(theme);
-            }
-          })(${themeString})`,
-          }}></script>
+        <script dangerouslySetInnerHTML={{ __html: `(${themeFn.toString()})(${themeStr})` }}></script>
       </Head>
     );
   }
