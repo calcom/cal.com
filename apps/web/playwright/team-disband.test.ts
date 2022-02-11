85c5d7e1dab2bc248df36bfae6261baf50299f8e
Camilo Valencia <camilo.valencia@kommit.co>
2022-02-11 17:32:12 -0500

    change disband-team test location

--- apps/web/playwright/team-disband.test.ts
+++ apps/web/playwright/team-disband.test.ts
@@ -3,31 +3,30 @@ import { randomString } from "../lib/random";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("/settings/teams");
-  await page.waitForSelector('[data-testid=teams]');
+  await page.waitForSelector("[data-testid=teams]");
 });
 
-test.describe('owner user', () => {
-
+test.describe("owner user", () => {
   test.use({ storageState: "playwright/artifacts/trialStorageState.json" });
 
-  test('can disband a team', async ({ page }) => {
+  test("can disband a team", async ({ page }) => {
     // Creata a new team
     const nonce = randomString(5);
     const teamName = `Team ${nonce}`;
     await page.locator("[data-testid=new-team]").click();
     await page.fill("[name=name]", teamName);
-    await page.click('[type=submit]');
+    await page.click("[type=submit]");
     await expect(page.locator(`text='${teamName}'`)).toBeVisible();
 
     // select created team
-    const $teams = await page.$$('[data-testid=teams] > *');
+    const $teams = await page.$$("[data-testid=teams] > *");
     expect($teams.length).toBeGreaterThanOrEqual(0);
-    const [ , $second] = $teams;
+    const [, $second] = $teams;
     $second.click();
 
     await page.click("[data-testid=disband-team]");
     await page.click("[data-testid=confirmbt-disband-team]");
 
-    await expect(page).toHaveURL("/settings/teams", {timeout: 2000});
+    await expect(page).toHaveURL("/settings/teams", { timeout: 2000 });
   });
 });
