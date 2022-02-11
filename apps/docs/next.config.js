28d668ff389b8ae6f72338d99c13e4b64dcbe497
Omar López <zomars@me.com>
2022-02-11 13:05:12 -0700

    Eslint fix to test deploy (#1818)
    * Eslint fix to test deploy
    
    * E2E fixes

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,6 +1,7 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.js',
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.js",
   unstable_staticImage: true,
-})
-module.exports = withNextra()
+});
+module.exports = withNextra();
