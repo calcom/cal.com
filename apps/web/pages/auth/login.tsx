8b79409fb981de5818950f451533f6ed2567d5d7
Agusti Fernandez <agusti@colony.io>
2022-02-15 00:02:31 +0100

    fix: remove use of window.location in redirect of callbackUrl in Login Page

--- apps/web/pages/auth/login.tsx
+++ apps/web/pages/auth/login.tsx
@@ -98,7 +98,7 @@ export default function Login({
               .then((res) => {
                 if (!res) setErrorMessage(errorMessages[ErrorCode.InternalServerError]);
                 // we're logged in! let's do a hard refresh to the desired url
-                else if (!res.error) window.location.replace(callbackUrl);
+                else if (!res.error) router.push(callbackUrl)
                 // reveal two factor input if required
                 else if (res.error === ErrorCode.SecondFactorRequired) setTwoFactorRequired(true);
                 // fallback if error not found
