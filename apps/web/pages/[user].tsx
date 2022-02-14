22e39af2d769a606ae4cb90e17e3228e6edd79d5
Agusti Fernandez <agusti@colony.io>
2022-02-14 16:17:39 +0100

    feat: replacement for web3 implementation with wagmi

--- apps/web/pages/[user].tsx
+++ apps/web/pages/[user].tsx
@@ -5,7 +5,6 @@ import { GetServerSidePropsContext } from "next";
 import dynamic from "next/dynamic";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useState } from "react";
 import { Toaster } from "react-hot-toast";
 import { JSONObject } from "superjson/dist/types";
 
@@ -21,6 +20,7 @@ import { ssrInit } from "@server/lib/ssr";
 const EventTypeDescription = dynamic(() => import("@components/eventtype/EventTypeDescription"));
 const HeadSeo = dynamic(() => import("@components/seo/head-seo").then((mod) => mod.HeadSeo));
 const CryptoSection = dynamic(() => import("../ee/components/web3/CryptoSection"));
+const Web3Provider = dynamic(() => import("../ee/components/web3/Web3Provider"));
 
 interface EvtsToVerify {
   [evtId: string]: boolean;
@@ -35,7 +35,6 @@ export default function User(props: inferSSRProps<typeof getServerSideProps>) {
   delete query.user; // So it doesn't display in the Link (and make tests fail)
 
   const nameOrUsername = user.name || user.username || "";
-  const [evtsToVerify, setEvtsToVerify] = useState<EvtsToVerify>({});
   return (
     <>
       <Theme />
@@ -61,45 +60,44 @@ export default function User(props: inferSSRProps<typeof getServerSideProps>) {
           <div className="space-y-6" data-testid="event-types">
             {!user.away &&
               eventTypes.map((type) => (
-                <div
-                  key={type.id}
-                  style={{ display: "flex" }}
-                  className="group hover:border-brand relative rounded-sm border border-neutral-200 bg-white hover:bg-gray-50 dark:border-0 dark:bg-neutral-900 dark:hover:border-neutral-600">
-                  <ArrowRightIcon className="absolute right-3 top-3 h-4 w-4 text-black opacity-0 transition-opacity group-hover:opacity-100 dark:text-white" />
-                  <Link
-                    href={{
-                      pathname: `/${user.username}/${type.slug}`,
-                      query,
-                    }}>
-                    <a
-                      onClick={async (e) => {
-                        // If a token is required for this event type, add a click listener that checks whether the user verified their wallet or not
-                        if (type.metadata.smartContractAddress && !evtsToVerify[type.id]) {
-                          const showToast = (await import("@lib/notification")).default;
-                          e.preventDefault();
-                          showToast(
-                            "You must verify a wallet with a token belonging to the specified smart contract first",
-                            "error"
-                          );
-                        }
-                      }}
-                      className="block w-full px-6 py-4"
-                      data-testid="event-type-link">
-                      <h2 className="grow font-semibold text-neutral-900 dark:text-white">{type.title}</h2>
-                      <EventTypeDescription eventType={type} />
-                    </a>
-                  </Link>
-                  {type.isWeb3Active && type.metadata.smartContractAddress && (
-                    <CryptoSection
-                      id={type.id}
-                      pathname={`/${user.username}/${type.slug}`}
-                      smartContractAddress={type.metadata.smartContractAddress as string}
-                      verified={evtsToVerify[type.id]}
-                      setEvtsToVerify={setEvtsToVerify}
-                      oneStep
-                    />
-                  )}
-                </div>
+                <Web3Provider>
+                  <div
+                    key={type.id}
+                    style={{ display: "flex" }}
+                    className="group hover:border-brand relative rounded-sm border border-neutral-200 bg-white hover:bg-gray-50 dark:border-0 dark:bg-neutral-900 dark:hover:border-neutral-600">
+                    <ArrowRightIcon className="absolute right-3 top-3 h-4 w-4 text-black opacity-0 transition-opacity group-hover:opacity-100 dark:text-white" />
+                    <Link
+                      href={{
+                        pathname: `/${user.username}/${type.slug}`,
+                        query,
+                      }}>
+                      <a
+                        onClick={async (e) => {
+                          // If a token is required for this event type, add a click listener that checks whether the user verified their wallet or not
+                          if (type.metadata.smartContractAddress) {
+                            const showToast = (await import("@lib/notification")).default;
+                            e.preventDefault();
+                            showToast(
+                              "You must verify a wallet with a token belonging to the specified smart contract first",
+                              "error"
+                            );
+                          }
+                        }}
+                        className="min-w-1/2 block px-6 py-4"
+                        data-testid="event-type-link">
+                        <h2 className="grow font-semibold text-neutral-900 dark:text-white">{type.title}</h2>
+                        <EventTypeDescription eventType={type} />
+                      </a>
+                    </Link>
+                    {type.isWeb3Active && type.metadata.smartContractAddress && (
+                      <CryptoSection
+                        id={type.id}
+                        pathname={`/${user.username}/${type.slug}`}
+                        smartContractAddress={type.metadata.smartContractAddress as string}
+                      />
+                    )}
+                  </div>
+                </Web3Provider>
               ))}
           </div>
           {eventTypes.length === 0 && (
