d309a03976bf33e25daa8cec720a43ab460d67c3
Hariom Balhara <hariombalhara@gmail.com>
2022-02-14 12:32:58 +0530

    remove from success and cancel pages as well

--- apps/web/lib/hooks/usePublicPage.ts
+++ apps/web/lib/hooks/usePublicPage.ts
@@ -2,6 +2,6 @@ import { useRouter } from "next/router";
 
 export default function usePublicPage() {
   const router = useRouter();
-  const isPublicPage = ["/[user]"].find((route) => router.pathname.startsWith(route));
+  const isPublicPage = ["/[user]", "/success", "/cancel"].find((route) => router.pathname.startsWith(route));
   return isPublicPage;
 }
