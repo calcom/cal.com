01066079fe87ecc72c416f8ab70e12c56f6b307c
zomars <zomars@me.com>
2022-02-15 11:14:28 -0700

    Ensures port 3000 for CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ const config: PlaywrightTestConfig = {
   globalSetup: require.resolve("./apps/web/playwright/lib/globalSetup"),
   outputDir: "./playwright/results",
   webServer: {
-    command: "yarn workspace @calcom/web start",
+    command: "yarn workspace @calcom/web start -p 3000",
     port: 3000,
     timeout: 60_000,
     reuseExistingServer: !process.env.CI,
