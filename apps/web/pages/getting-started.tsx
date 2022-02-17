9c36d62c82a5c86a8077df5efca7fd8a5f49c86b
Deepak Prabhakara <deepak@boxyhq.com>
2022-02-17 22:19:06 +0000

    type fix

--- apps/web/pages/getting-started.tsx
+++ apps/web/pages/getting-started.tsx
@@ -365,7 +365,7 @@ export default function Onboarding(props: inferSSRProps<typeof getServerSideProp
                     id="username"
                     data-testid="username"
                     placeholder={t("username")}
-                    defaultValue={props.usernameParam ? props.usernameParam : props.user?.username}
+                    defaultValue={props.usernameParam ? props.usernameParam : props.user?.username ?? ""}
                     required
                     className="mt-1 block w-full rounded-sm border border-gray-300 px-3 py-2 shadow-sm focus:border-neutral-500 focus:outline-none focus:ring-neutral-500 sm:text-sm"
                   />
