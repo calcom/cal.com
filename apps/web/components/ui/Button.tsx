bdab8e76d6bea3ea8d824b9c7199b8568e614534
zomars <zomars@me.com>
2022-02-18 18:20:34 -0700

    Add fallback

---
+++ apps/web/components/ui/Button.tsx
@@ -0,0 +1,2 @@
+// TODO: Remove this file once every Button is imported from `@calcom/ui`
+export * from "@calcom/ui/Button";
---
+++ apps/web/lib/classNames.ts
@@ -0,0 +1,2 @@
+// TODO: Remove this file once every `classNames` is imported from `@calcom/lib`
+export * from "@calcom/lib/classNames";
