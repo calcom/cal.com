3ae0266f93d876f0920e6f724bfa65d1ce3b6afd
Hariom Balhara <hariombalhara@gmail.com>
2022-02-21 12:16:02 +0530

    Revert Test Changes

--- apps/web/lib/profile.ts
+++ apps/web/lib/profile.ts
@@ -1,5 +1,11 @@
 import crypto from "crypto";
 
 export const defaultAvatarSrc = function ({ email, md5 }: { md5?: string; email?: string }) {
-  return `https://www.gravatar.com/avatar/?s=160&d=identicon&r=PG`;
+  if (!email && !md5) return "";
+
+  if (email && !md5) {
+    md5 = crypto.createHash("md5").update(email).digest("hex");
+  }
+
+  return `https://www.gravatar.com/avatar/${md5}?s=160&d=identicon&r=PG`;
 };
