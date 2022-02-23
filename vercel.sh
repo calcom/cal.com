38bff5c94536a776bbb4c4a2bae715127c47a88a
zomars <zomars@me.com>
2022-02-23 13:13:10 -0700

    Cleanup git logs in Vercel

--- vercel.sh
+++ vercel.sh
@@ -26,6 +26,7 @@ cd tmp # go into the tmp folder
 
 # checkout the current submodule commit
 git config --global init.defaultBranch main
+git config --global advice.detachedHead false
 git init # initialise empty repo
 git remote add origin https://$GITHUB_ACCESS_TOKEN@$SUBMODULE_GITHUB # add origin of the submodule
 git fetch --depth=1 origin $COMMIT # fetch only the required version
