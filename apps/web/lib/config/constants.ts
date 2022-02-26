f7032967507ea601d449d7fd600dd477bac00455
Agusti Fernandez Pardo <git@agusti.me>
2022-02-26 20:18:19 +0100

    fixes adding NEXT_PUBLIC_BASE_URL to consts and using that

--- apps/web/lib/config/constants.ts
+++ apps/web/lib/config/constants.ts
@@ -3,3 +3,4 @@ export const WEBSITE_URL = process.env.NEXT_PUBLIC_APP_URL || "https://cal.com";
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
 export const TRIAL_LIMIT_DAYS = 14;
 export const HOSTED_CAL_FEATURES = process.env.HOSTED_CAL_FEATURES || BASE_URL === "https://app.cal.com";
+export const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || `https://${process.env.VERCEL_URL}`;
