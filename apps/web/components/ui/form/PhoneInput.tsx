0fb44887e3daca180fb8f959cee4c337a809617c
Syed Ali Shahbaz <52925846+alishaz-polymath@users.noreply.github.com>
2022-03-03 15:27:59 +0530

    Fix missing phone data from location after booking (#1968)

--- apps/web/components/ui/form/PhoneInput.tsx
+++ apps/web/components/ui/form/PhoneInput.tsx
@@ -1,18 +1,24 @@
 import React from "react";
-import BasePhoneInput, { Props as PhoneInputProps } from "react-phone-number-input";
+import { Control } from "react-hook-form";
+import BasePhoneInput, { Props } from "react-phone-number-input/react-hook-form";
 import "react-phone-number-input/style.css";
 
 import classNames from "@lib/classNames";
-import { Optional } from "@lib/types/utils";
 
-export const PhoneInput = (
-  props: Optional<PhoneInputProps<React.InputHTMLAttributes<HTMLInputElement>>, "onChange">
-) => (
+type PhoneInputProps = {
+  value: string;
+  id: string;
+  placeholder: string;
+  required: boolean;
+};
+
+export const PhoneInput = ({ control, name, ...rest }: Props<PhoneInputProps>) => (
   <BasePhoneInput
-    {...props}
+    {...rest}
+    name={name}
+    control={control}
     className={classNames(
-      "border-1 focus-within:border-brand block w-full rounded-sm border border-gray-300 py-px px-3 shadow-sm ring-black focus-within:ring-1 dark:border-black dark:bg-black dark:text-white",
-      props.className
+      "border-1 focus-within:border-brand block w-full rounded-sm border border-gray-300 py-px px-3 shadow-sm ring-black focus-within:ring-1 dark:border-black dark:bg-black dark:text-white"
     )}
     onChange={() => {
       /* DO NOT REMOVE: Callback required by PhoneInput, comment added to satisfy eslint:no-empty-function */
