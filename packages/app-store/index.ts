6a09872d9432aaa9672a3de142c1e47d246a3d16
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-03-03 18:34:16 -0500

    Create teamsvideo package

--- packages/app-store/index.ts
+++ packages/app-store/index.ts
@@ -1,11 +1,13 @@
 import * as example from "./_example";
 import * as dailyvideo from "./dailyvideo";
+import * as teamsvideo from "./teamsvideo";
 import * as zoomvideo from "./zoomvideo";
 
 const appStore = {
   example,
   dailyvideo,
   zoomvideo,
+  teamsvideo,
 };
 
 export default appStore;
---
+++ packages/app-store/teamsvideo/index.ts
@@ -0,0 +1,26 @@
+import type { App } from "@calcom/types/App";
+
+import _package from "./package.json";
+
+export const metadata = {
+  name: "MS Teams",
+  description: _package.description,
+  installed: !!(process.env.MS_GRAPH_CLIENT_ID && process.env.MS_GRAPH_CLIENT_SECRET),
+  type: "zoom_video",
+  imageSrc: "apps/zoom.svg",
+  variant: "conferencing",
+  logo: "/apps/zoom.svg",
+  publisher: "Cal.com",
+  url: "https://www.microsoft.com/en-ca/microsoft-teams/group-chat-software",
+  verified: true,
+  rating: 4.3, // TODO: placeholder for now, pull this from TrustPilot or G2
+  reviews: 69, // TODO: placeholder for now, pull this from TrustPilot or G2
+  category: "video",
+  label: "MS Teams",
+  slug: "msteams",
+  title: "MS Teams",
+  trending: true,
+} as App;
+
+export * as api from "./api";
+export * as lib from "./lib";
