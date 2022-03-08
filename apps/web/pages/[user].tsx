a0d4f1e9897de67c50fbf688bb2a4d3d1cad1f52
Peer Richelsen <peer@hey.com>
2022-03-08 23:55:58 +0000

    eplaced shadow with border

--- apps/web/pages/[user].tsx
+++ apps/web/pages/[user].tsx
@@ -59,9 +59,9 @@ export default function User(props: inferSSRProps<typeof getServerSideProps>) {
           </div>
           <div className="space-y-6" data-testid="event-types">
             {user.away ? (
-              <div className="overflow-hidden rounded-sm shadow">
+              <div className="overflow-hidden rounded-sm border dark:border-gray-900">
                 <div className="p-8 text-center text-gray-400 dark:text-white">
-                  <h2 className="font-cal text-3xl font-semibold text-gray-600 dark:text-white">
+                  <h2 className="font-cal mb-2 text-3xl font-semibold text-gray-600 dark:text-white">
                     {t("user_away")}
                   </h2>
                   <p className="mx-auto max-w-md">{t("user_away_description")}</p>
@@ -114,9 +114,9 @@ export default function User(props: inferSSRProps<typeof getServerSideProps>) {
             )}
           </div>
           {eventTypes.length === 0 && (
-            <div className="overflow-hidden rounded-sm shadow">
+            <div className="overflow-hidden rounded-sm border dark:border-gray-900">
               <div className="p-8 text-center text-gray-400 dark:text-white">
-                <h2 className="font-cal text-3xl font-semibold text-gray-600 dark:text-white">
+                <h2 className="font-cal mb-2 text-3xl font-semibold text-gray-600 dark:text-white">
                   {t("uh_oh")}
                 </h2>
                 <p className="mx-auto max-w-md">{t("no_event_types_have_been_setup")}</p>
