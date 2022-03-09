e53648d218320ec52f7b57a7bc22dc9dce11bc97
Omar López <zomars@me.com>
2022-03-09 10:52:48 -0700

    Linting (#2083)
    * Runs yarn format
    
    * Update website
    
    * Web lint is failing
    
    * Formatting
    
    * Adds cache for dependency install
    
    * Adds linting reports for GH
    
    * Fixes artifact upload
    
    * Caching tests
    
    * Merge reports on GH actions
    
    * Linting
    
    * Fix prettier plugin conflicts
    
    * Dep fixes
    
    * Moves tailwind to monorepo package (#2091)
    
    * New Crowdin translations by Github Action (#2077)
    
    Co-authored-by: Crowdin Bot <support+bot@crowdin.com>
    
    * fix redirection to 404 page on login (#2086)
    
    Co-authored-by: Peer Richelsen <peeroke@gmail.com>
    
    * fix: update contributing (#2084)
    
    Co-authored-by: Bailey Pumfleet <pumfleet@hey.com>
    
    * Formatting
    
    * Update website
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Crowdin Bot <support+bot@crowdin.com>
    Co-authored-by: Hariom Balhara <hariombalhara@gmail.com>
    Co-authored-by: Peer Richelsen <peeroke@gmail.com>
    Co-authored-by: Louis <48682663+louis-27@users.noreply.github.com>
    Co-authored-by: Bailey Pumfleet <pumfleet@hey.com>

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,1 @@
-module.exports = {
-  bracketSpacing: true,
-  bracketSameLine: true,
-  singleQuote: false,
-  jsxSingleQuote: false,
-  trailingComma: "es5",
-  semi: true,
-  printWidth: 110,
-  arrowParens: "always",
-  importOrder: ["^@(calcom|ee)/(.*)$", "^@lib/(.*)$", "^@components/(.*)$", "^@(server|trpc)/(.*)$", "^[./]"],
-  importOrderSeparation: true,
-  plugins: [require("./merged-prettier-plugin")],
-};
+module.exports = require("./packages/config/prettier-preset");
