455a3f0c3c6df00aa5ced71601478b0e4628331b
sean-brydon <seanbrydon.me@gmail.com>
2022-03-10 23:08:24 +0000

    Adding connect button if there is on user

--- packages/app-store/slackapp/lib/showCreateEventMessage.ts
+++ packages/app-store/slackapp/lib/showCreateEventMessage.ts
@@ -1,4 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Blocks, Elements, Message } from "slack-block-builder";
+
+import { BASE_URL } from "@calcom/lib/constants";
 
 import { WhereCredsEqualsId } from "./WhereCredsEqualsID";
 import { CreateEventModal } from "./views";
@@ -23,7 +26,22 @@ export default async function showCreateEventMessage(req: NextApiRequest, res: N
     },
   });
 
-  if (!data) res.status(200).json({ message: "No user found" });
+  if (!data)
+    res.status(200).json(
+      Message()
+        .blocks(
+          Blocks.Section({ text: "This slack account is not linked with a cal.com account" }),
+          Blocks.Actions().elements(
+            Elements.Button({ text: "Cancel", actionId: "cancel" }).danger(),
+            Elements.Button({
+              text: "Connect",
+              actionId: "open.connect.link",
+              url: `${BASE_URL}/apps/installed`,
+            }).primary()
+          )
+        )
+        .buildToJSON()
+    );
 
   res.status(200).json(CreateEventModal(data));
 }
