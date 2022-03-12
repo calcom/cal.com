520ad77b3608a495dada09a569b0855ea2e2b921
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-03-12 14:12:47 -0500

    Send and write reminder to db if booking is <60 mins

--- apps/web/lib/reminderManager.tsx
+++ apps/web/lib/reminderManager.tsx
@@ -1,14 +1,17 @@
 import { EventTypeAttendeeReminder } from "@prisma/client/";
 import dayjs from "dayjs";
+import isBetween from "dayjs/plugin/isBetween";
+import twilio from "twilio";
 
-// import twilio from "twilio";
 import prisma from "@lib/prisma";
 
-const accountSid = process.env.TWILIO_SID;
-const token = process.env.TWILIO_TOKEN;
-const senderNumber = process.env.TWILIO_PHONE_NUMBER;
+dayjs.extend(isBetween);
 
-// const client = twilio(accountSid, token);
+const TWILIO_SID = process.env.TWILIO_SID;
+const TWILIO_TOKEN = process.env.TWILIO_TOKEN;
+const TWILIO_MESSAGING_SID = process.env.TWILIO_MESSAGING_SID;
+
+const client = twilio(TWILIO_SID, TWILIO_TOKEN);
 
 export const scheduleSMSAttendeeReminders = async (
   uid: string,
@@ -16,33 +19,31 @@ export const scheduleSMSAttendeeReminders = async (
   startTime: string,
   attendeeReminder: EventTypeAttendeeReminder
 ) => {
-  // console.log("🚀 ~ file: reminder-manager.ts ~ line 8 ~ startTime", startTime);
-  console.log(
-    "🚀 ~ file: reminder-manager.ts ~ line 8 ~ scheduleAttendeeReminders ~ eventType",
-    attendeeReminder
-  );
-  // console.log(
-  //   "🚀 ~ file: reminder-manager.ts ~ line 8 ~ scheduleAttendeeReminders ~ bookings",
-  //   reminderPhone
-  // );
+  const currentDate = dayjs();
+  const startTimeObject = dayjs(startTime);
+  const scheduledDate = dayjs(startTime).subtract(attendeeReminder.time, attendeeReminder.unitTime);
 
-  // await client.messages.create({
-  //   body: "This is a test",
-  //   from: senderNumber,
-  //   to: reminderPhone,
-  // });
+  // Check the scheduled date and right now
+  // Can only schedule at least 60 minutes in advance
+  if (currentDate.isBetween(startTimeObject.subtract(1, "hour"), startTimeObject)) {
+    const response = await client.messages.create({
+      body: "This is a test",
+      messagingServiceSid: TWILIO_MESSAGING_SID,
+      to: reminderPhone,
+    });
 
-  await prisma.attendeeReminder.create({
-    data: {
-      booking: {
-        connect: {
-          uid: uid,
+    await prisma.attendeeReminder.create({
+      data: {
+        booking: {
+          connect: {
+            uid: uid,
+          },
         },
+        method: "SMS",
+        referenceId: response.sid,
+        scheduledDate: dayjs().toISOString(),
+        scheduled: true,
       },
-      method: "SMS",
-      referenceId: "123",
-      scheduledDate: dayjs().toISOString(),
-      scheduled: true,
-    },
-  });
+    });
+  }
 };
