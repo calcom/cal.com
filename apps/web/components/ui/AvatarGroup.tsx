ba1fbd151f682530eb1db7a0fa03e36d3d936a7e
Mayowa-Ojo <mayowaojo.e@gmail.com>
2022-03-13 14:47:15 -0600

    truncate to 2 avatars on mobile with remaining indicator

--- apps/web/components/ui/AvatarGroup.tsx
+++ apps/web/components/ui/AvatarGroup.tsx
@@ -18,23 +18,50 @@ export type AvatarGroupProps = {
 
 export const AvatarGroup = function AvatarGroup(props: AvatarGroupProps) {
   return (
-    <ul className={classNames(props.className)}>
-      {props.items.slice(0, props.truncateAfter).map((item, idx) => {
-        if (item.image != null) {
-          return (
-            <li key={idx} className="-mr-2 inline-block">
-              <Avatar
-                className={props.border}
-                imageSrc={item.image}
-                title={item.title}
-                alt={item.alt || ""}
-                size={props.size}
-              />
-            </li>
-          );
-        }
-      })}
-    </ul>
+    <>
+      {/* mobile-view */}
+      <ul className={classNames("flex md:hidden", props.className)}>
+        {props.items.slice(0, 2).map((item, idx) => {
+          if (item.image != null) {
+            return (
+              <li key={idx} className="-mr-2 inline-block">
+                <Avatar
+                  className={props.border}
+                  imageSrc={item.image}
+                  title={item.title}
+                  alt={item.alt || ""}
+                  size={props.size}
+                />
+              </li>
+            );
+          }
+        })}
+        <li className="-mr-2 inline-block">
+          <button className="cursor-default">
+            <span className="flex h-9 w-9 items-center justify-center rounded-full border-2 border-white bg-gray-100 dark:border-gray-900">
+              <p className="text-gray-800">+{props.items.length - 2}</p>
+            </span>
+          </button>
+        </li>
+      </ul>
+      <ul className={classNames("hidden sm:block", props.className)}>
+        {props.items.slice(0, props.truncateAfter).map((item, idx) => {
+          if (item.image != null) {
+            return (
+              <li key={idx} className="-mr-2 inline-block">
+                <Avatar
+                  className={props.border}
+                  imageSrc={item.image}
+                  title={item.title}
+                  alt={item.alt || ""}
+                  size={props.size}
+                />
+              </li>
+            );
+          }
+        })}
+      </ul>
+    </>
   );
 };
 
