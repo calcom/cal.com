92adb3a2efb255b96913c7c3868bbd6ecbbeb4dc
Alex van Andel <me@alexvanandel.com>
2022-03-15 21:45:47 +0100

    Fixed type error coming from main

--- apps/web/pages/settings/profile.tsx
+++ apps/web/pages/settings/profile.tsx
@@ -136,9 +136,7 @@ function SettingsView(props: ComponentProps<typeof Settings> & { localeProp: str
   const localeOptions = useMemo(() => {
     return (router.locales || []).map((locale) => ({
       value: locale,
-      // FIXME
-      // @ts-ignore
-      label: new Intl.DisplayNames(props.localeProp, { type: "language" }).of(locale),
+      label: new Intl.DisplayNames(props.localeProp, { type: "language" }).of(locale) || "",
     }));
   }, [props.localeProp, router.locales]);
 
