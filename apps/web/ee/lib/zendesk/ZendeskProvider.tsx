608bf944c4b0e3ee9305200cb576c62371ca8031
Peer Richelsen <peer@cal.com>
2022-03-15 14:09:00 +0000

    MVP zendesk provider

---
+++ apps/web/ee/lib/zendesk/ZendeskProvider.tsx
@@ -0,0 +1,23 @@
+import Zendesk from "react-zendesk";
+
+const ZENDESK_KEY = process.env.NEXT_PUBLIC_ZENDESK_KEY;
+
+const setting = {
+  color: {
+    theme: "#292929",
+  },
+  launcher: {
+    chatLabel: {
+      "en-US": "Need Help",
+    },
+  },
+  contactForm: {
+    fields: [{ id: "description", prefill: { "*": "My pre-filled description" } }],
+  },
+};
+
+export default function ZendeskProvider() {
+  if (!ZENDESK_KEY) return null;
+  else
+    return <Zendesk defer zendeskKey={ZENDESK_KEY} {...setting} onLoaded={() => console.log("is loaded")} />;
+}
