2180f6ba1ace6de9e341fb5477a1011c5a347e21
Demian Caldelas <denik.works@protonmail.com>
2022-03-17 05:20:15 -0300

    Fix E2E bugs causing CI failutes

--- apps/web/playwright/login.test.ts
+++ apps/web/playwright/login.test.ts
@@ -1,11 +1,13 @@
 import { test } from "@playwright/test";
 
-// Using logged in state from globalSteup
-test.use({ storageState: "playwright/artifacts/proStorageState.json" });
+test.describe("Login tests", () => {
+  // Using logged in state from globalSteup
+  test.use({ storageState: "playwright/artifacts/proStorageState.json" });
 
-test("login with pro@example.com", async ({ page }) => {
-  // Try to go homepage
-  await page.goto("/");
-  // It should redirect you to the event-types page
-  await page.waitForSelector("[data-testid=event-types]");
+  test("login with pro@example.com", async ({ page }) => {
+    // Try to go homepage
+    await page.goto("/");
+    // It should redirect you to the event-types page
+    await page.waitForSelector("[data-testid=event-types]");
+  });
 });
