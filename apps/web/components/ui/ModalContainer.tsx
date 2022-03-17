5be92576571b4aceadd8a65678214001d7e79008
Syed Ali Shahbaz <alishahbaz7@gmail.com>
2022-03-17 12:02:51 +0530

    lint fix

--- apps/web/components/ui/ModalContainer.tsx
+++ apps/web/components/ui/ModalContainer.tsx
@@ -1,8 +1,6 @@
 import classNames from "classnames";
 import React from "react";
 
-import { Dialog, DialogContent, DialogFooter } from "@calcom/ui/Dialog";
-
 interface Props extends React.PropsWithChildren<any> {
   wide?: boolean;
   scroll?: boolean;
@@ -38,34 +36,6 @@ export default function ModalContainer(props: Props) {
           </div>
         </div>
       </div>
-      <Dialog defaultOpen={true}>
-        <DialogContent
-          onInteractOutside={(e) => {
-            e.preventDefault();
-          }}>
-          <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-            <CheckIcon className="h-6 w-6 text-green-600" />
-          </div>
-          <div className="mt-5 flex justify-center">
-            <DialogHeader title={t("cancellation_successful")} />
-          </div>
-          {!loading && !session?.user && (
-            <div className="-mt-6 flex justify-center">
-              <p className="text-center text-sm text-gray-500">{t("free_to_pick_another_event_type")}</p>
-            </div>
-          )}
-          <div className="flex justify-center">
-            <DialogFooter>
-              {!loading && !session?.user && <Button href={eventPage as string}>Pick another</Button>}
-              {!loading && session?.user && (
-                <Button data-testid="back-to-bookings" href="/bookings" EndIcon={ArrowRightIcon}>
-                  {t("back_to_bookings")}
-                </Button>
-              )}
-            </DialogFooter>
-          </div>
-        </DialogContent>
-      </Dialog>
     </>
   );
 }
