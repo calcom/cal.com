d9418a337b2417b11d28276a469d96d6047ada3b
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-03-17 17:27:07 -0400

    Add Exchange package

---
+++ packages/app-store/exchangecalendar/index.ts
@@ -0,0 +1,29 @@
+import { validJson } from "@calcom/lib/jsonUtils";
+import type { App } from "@calcom/types/App";
+
+import _package from "./package.json";
+
+export const metadata = {
+  name: "Microsoft Exchange Calendar",
+  description: _package.description,
+  installed: true,
+  type: "exchange_calendar",
+  title: "Microsoft Exchange Calendar",
+  // Add exchange icon to calendar
+  imageSrc: "apps/caldav.svg",
+  variant: "calendar",
+  category: "calendar",
+  label: "Exchange Calendar",
+  logo: "/apps/caldav.svg",
+  publisher: "Cal.com",
+  rating: 5,
+  reviews: 69,
+  slug: "exchange-calendar",
+  trending: false,
+  url: "https://cal.com/",
+  verified: true,
+  email: "help@cal.com",
+} as App;
+
+export * as components from "./components";
+export * as lib from "./lib";
