5556f0e4bbc49d4e6c6ed259abdc9e6e299c4e18
Peer Richelsen <peer@cal.com>
2022-03-18 18:10:29 +0000

    improved a11y for user dropdown

--- apps/web/ee/lib/helpscout/HelpscoutMenuItem.tsx
+++ apps/web/ee/lib/helpscout/HelpscoutMenuItem.tsx
@@ -29,7 +29,7 @@ export default function HelpscoutMenuItem() {
             <ChatAltIcon
               className={classNames(
                 "text-neutral-400 group-hover:text-neutral-500",
-                "h-5 w-5 flex-shrink-0 ltr:mr-2"
+                "h-5 w-5 flex-shrink-0 ltr:mr-3"
               )}
               aria-hidden="true"
             />
