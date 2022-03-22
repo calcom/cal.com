a731d9653302659ceac56f674a57a7f3760b9269
Hariom Balhara <hariombalhara@gmail.com>
2022-03-22 10:45:32 +0530

    Add queuing logic and api

--- apps/web/lib/embed-iframe.ts
+++ apps/web/lib/embed-iframe.ts
@@ -1,4 +1,4 @@
-import { addEventListener } from "@lib/sdk-event";
+import { addEventListener, fireEvent } from "@lib/sdk-event";
 
 if (typeof window !== "undefined") {
   addEventListener("*", (e) => {
@@ -6,4 +6,28 @@ if (typeof window !== "undefined") {
     //console.log(detail.fullType, detail.type, detail.data);
     parent.postMessage(detail, "*");
   });
+
+  (function keepParentInformedAboutDimensionChanges() {
+    let knownHiddenHeight: Number | null = null;
+    let numDimensionChanges = 0;
+    requestAnimationFrame(function informAboutScroll() {
+      // Because of scroll="no", this much is hidden from the user.
+      const hiddenHeight = document.documentElement.scrollHeight - window.innerHeight;
+      if (knownHiddenHeight !== hiddenHeight) {
+        knownHiddenHeight = hiddenHeight;
+        numDimensionChanges++;
+        fireEvent("dimension-changed", {
+          hiddenHeight,
+        });
+      }
+      // Parent Counterpart would change the dimension of iframe and thus page's dimension would be impacted which is recursive.
+      // It should stop ideally by reaching a hiddenHeight value of 0.
+      // FIXME: If 0 can't be reached we need to just abandon our quest for perfect iframe and let scroll be there. Such case can be logged in the wild and fixed later on.
+      if (numDimensionChanges > 50) {
+        console.warn("Too many dimension changes detected.");
+        return;
+      }
+      requestAnimationFrame(informAboutScroll);
+    });
+  })();
 }
--- apps/web/public/embed.js
+++ apps/web/public/embed.js
@@ -1,7 +1,46 @@
-import { fireEvent } from "http://localhost:3000/sdk-event.js";
+import { fireEvent, addEventListener } from "http://localhost:3000/sdk-event.js";
+let iframe = null;
 Cal.__config = {
 	origin: "http://localhost:3000"
 };
+
+//TODO: Move it to a CSS file and import
+const css = `
+	.cal-embed {
+		border: 0px;
+	}
+`;
+document.head.appendChild(document.createElement('style')).innerHTML = css;
+
+function log(...args) {
+	console.log(...args)
+}
+
+function processInstruction(instruction) {
+	const [method, argument] = instruction;
+	if (!Cal[method]) {
+		// Instead of throwing error, log and move forward in the queue
+		log(`Instruction ${method} not found`);
+	}
+	try {
+		Cal[method](argument);
+	} catch (e) {
+		// Instead of throwing error, log and move forward in the queue
+		log(`Instruction couldn't be executed`, e);
+	}
+}
+
+function processQueue(queue) {
+	queue.forEach((instruction) => {
+		processInstruction(instruction)
+	})
+
+	queue.splice(0);
+
+	queue.push = (instruction) => {
+		processInstruction(instruction)
+	}
+}
 
 Cal.configure = ({ origin }) => {
 	Cal.__config.origin = origin;
@@ -11,17 +50,50 @@ function getConfig() {
 	return Cal.__config;
 }
 
-Cal.init = ({ calendarLink }) => {
+// Cal.init = ({ calendarLink }) => {
+
+// }
+
+function createIframe({ calendarLink }) {
 	const iframe = document.createElement('iframe');
+	iframe.scrolling = "no";
 	const config = getConfig();
-	iframe.className = "cal-embed";
 	iframe.src = `${config.origin}/${calendarLink}`;
-	iframe.style.height = "100%";
-	iframe.style.width = "100%";
-	document.body.appendChild(iframe);
+	return iframe;
 }
 
 window.addEventListener("message", (e) => {
 	const detail = e.data;
+	if (!e.data.fullType?.startsWith("Cal.")) {
+		return;
+	}
 	fireEvent(detail.type, detail.data);
-})
+});
+
+(function () {
+	function setAppropriateHeight(proposedHeightByIframeWebsite) {
+		iframe.style.height = proposedHeightByIframeWebsite;
+	}
+	// 1. Initial iframe width and height would be according to 100% value of the parent element
+	// 2. Once webpage inside iframe renders, it would tell how much iframe height should be increased so that my entire content is visible without iframe scroll
+	// 3. Parent window would check what iframe height can be set according to parent Element
+	addEventListener("dimension-changed", (e) => {
+		const { data } = e.detail
+		let proposedHeightByIframeWebsite = parseFloat(getComputedStyle(iframe).height) + data.hiddenHeight
+		setAppropriateHeight(proposedHeightByIframeWebsite);
+	});
+})();
+
+Cal.inline = ({ calendarLink, elementOrSelector }) => {
+	iframe = createIframe({ calendarLink })
+	iframe.className = "cal-embed";
+	iframe.style.height = "100%";
+	iframe.style.width = "100%";
+	let element = elementOrSelector instanceof HTMLElement ? elementOrSelector : document.querySelector(elementOrSelector)
+	if (!element) {
+		throw new Error("Element not found");
+	}
+	element.appendChild(iframe);
+}
+
+processQueue(Cal.q)
