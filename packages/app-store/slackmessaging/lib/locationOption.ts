da00e66d667eb9a85227e67ad0a5668afe937a73
zomars <zomars@me.com>
2022-03-28 14:37:03 -0700

    Delete locationOption.ts

--- packages/app-store/slackmessaging/lib/locationOption.ts
+++
@@ -1,9 +0,0 @@
-import { LocationType } from "@calcom/lib/location";
-
-const locationOption = {
-  value: LocationType.Slack,
-  label: "Slack App",
-  disabled: false,
-};
-
-export default locationOption;
