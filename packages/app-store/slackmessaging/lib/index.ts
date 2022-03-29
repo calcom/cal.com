c3c1b1183c43950e98d733666daef0bd47a025eb
zomars <zomars@me.com>
2022-03-29 10:54:46 -0700

    Build fixes

--- packages/app-store/slackmessaging/lib/index.ts
+++ packages/app-store/slackmessaging/lib/index.ts
@@ -1,4 +1,3 @@
-export { default as locationOption } from "./locationOption";
 export { default as showCreateEventMessage } from "./showCreateEventMessage";
 export { default as showTodayMessage } from "./showTodayMessage";
-export { default as utils } from "./utils";
+export * as utils from "./utils";
