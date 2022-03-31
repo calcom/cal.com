92b64d100070674a693de986120282151a5f55f3
zomars <zomars@me.com>
2022-03-30 17:00:32 -0700

    WIP

--- packages/app-store/_example/index.ts
+++ packages/app-store/_example/index.ts
@@ -8,8 +8,8 @@ export const metadata = {
   installed: true,
   category: "video",
   // If using static next public folder, can then be referenced from the base URL (/).
-  imageSrc: "/cal-com-icon.svg",
-  logo: "/cal-com-icon.svg",
+  imageSrc: "/api/app-store/_example/icon.svg",
+  logo: "/api/app-store/_example/icon.svg",
   label: "Example App",
   publisher: "Cal.com",
   rating: 5,
