3694506eb12970b4086af4f76be2377d1c7124a9
zomars <zomars@me.com>
2022-04-01 13:18:36 -0700

    Adds missing confirm email after payment

--- apps/web/ee/pages/api/integrations/stripepayment/webhook.ts
+++ apps/web/ee/pages/api/integrations/stripepayment/webhook.ts
@@ -10,6 +10,7 @@ import { CalendarEvent } from "@calcom/types/Calendar";
 
 import { IS_PRODUCTION } from "@lib/config/constants";
 import { HttpError as HttpCode } from "@lib/core/http/error";
+import { sendScheduledEmails } from "@lib/emails/email-manager";
 
 import { getTranslation } from "@server/lib/i18n";
 
@@ -125,6 +126,8 @@ async function handlePaymentSuccess(event: Stripe.Event) {
       },
     });
   }
+
+  await sendScheduledEmails({ ...evt });
 
   throw new HttpCode({
     statusCode: 200,
