2b4a745f121e2660757fb39a80eaa9488dc5cd79
Agusti Fernandez Pardo <git@agusti.me>
2022-04-01 22:04:42 +0200

    feat: improve validations of users/attendee/availabilty to support required fields

--- lib/validations/availability.ts
+++ lib/validations/availability.ts
@@ -1,10 +1,20 @@
 import { withValidation } from "next-validations";
+import { z } from "zod";
 
 import { _AvailabilityModel as Availability } from "@calcom/prisma/zod";
 
-export const schemaAvailabilityBodyParams = Availability.omit({ id: true });
+export const schemaAvailabilityBaseBodyParams = Availability.omit({ id: true }).partial();
 
 export const schemaAvailabilityPublic = Availability.omit({});
+
+const schemaAvailabilityRequiredParams = z.object({
+  startTime: z.string(),
+  endTime: z.string(),
+});
+
+export const schemaAvailabilityBodyParams = schemaAvailabilityBaseBodyParams.merge(
+  schemaAvailabilityRequiredParams
+);
 
 export const withValidAvailability = withValidation({
   schema: schemaAvailabilityBodyParams,
--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -1,14 +1,21 @@
 import { withValidation } from "next-validations";
+import { z } from "zod";
 
 import { _UserModel as User } from "@calcom/prisma/zod";
 
-export const schemaUserBodyParams = User.omit({
+export const schemaUserBaseBodyParams = User.omit({
   id: true,
   createdAt: true,
   password: true,
   twoFactorEnabled: true,
   twoFactorSecret: true,
 }).partial();
+
+const schemaUserRequiredParams = z.object({
+  email: z.string().email(),
+});
+
+export const schemaUserBodyParams = schemaUserBaseBodyParams.merge(schemaUserRequiredParams);
 
 export const schemaUserPublic = User.omit({
   identityProvider: true,
