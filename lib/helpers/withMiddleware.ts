5233dad5f9f9ba3813f17a10092d99c3aef7ed92
Agusti Fernandez Pardo <git@agusti.me>
2022-04-04 00:49:05 +0200

    re-unify into one route all id ops for teams and users

--- lib/helpers/withMiddleware.ts
+++ lib/helpers/withMiddleware.ts
@@ -2,11 +2,12 @@ import { label } from "next-api-middleware";
 
 import { addRequestId } from "./addRequestid";
 import { captureErrors } from "./captureErrors";
-import { HTTP_POST, HTTP_DELETE, HTTP_PATCH, HTTP_GET } from "./httpMethods";
+import { HTTP_POST, HTTP_DELETE, HTTP_PATCH, HTTP_GET, HTTP_GET_DELETE_PATCH } from "./httpMethods";
 import { verifyApiKey } from "./verifyApiKey";
 
 const withMiddleware = label(
   {
+    HTTP_GET_DELETE_PATCH,
     HTTP_GET,
     HTTP_PATCH,
     HTTP_POST,
