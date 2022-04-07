fefc314d35d4b35c615e126df3ba8d7f3c0e897b
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2022-04-07 17:26:33 +0100

    Fixing api points not returning (#2419)
    Co-authored-by: Omar López <zomars@me.com>

--- packages/app-store/slackmessaging/api/add.ts
+++ packages/app-store/slackmessaging/api/add.ts
@@ -8,8 +8,7 @@ const scopes = ["commands", "users:read", "users:read.email"];
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (!req.session?.user?.id) {
-    res.status(401).json({ message: "You must be logged in to do this" });
-    return;
+    return res.status(401).json({ message: "You must be logged in to do this" });
   }
 
   if (req.method === "GET") {
@@ -31,7 +30,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const url = `https://slack.com/oauth/v2/authorize?${query}&user_`;
     // const url =
     //   "https://slack.com/oauth/v2/authorize?client_id=3194129032064.3178385871204&scope=chat:write,commands&user_scope=";
-    res.status(200).json({ url });
+    return res.status(200).json({ url });
   }
-  res.status(404).json({ error: "Not Found" });
+  return res.status(404).json({ error: "Not Found" });
 }
