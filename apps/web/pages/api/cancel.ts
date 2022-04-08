6fd52df4e3ff406683015703a0cd0bca8d9e4c67
Syed Ali Shahbaz <alishahbaz7@gmail.com>
2022-04-08 17:46:14 +0530

    cleanup

--- apps/web/pages/api/cancel.ts
+++ apps/web/pages/api/cancel.ts
@@ -67,8 +67,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       uid: true,
       eventTypeId: true,
       destinationCalendar: true,
-      dynamicEventSlugRef: true,
-      dynamicGroupSlugRef: true,
     },
   });
 
