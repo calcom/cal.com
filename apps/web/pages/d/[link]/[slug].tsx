091995070a73813454b2ad4ae6b839dd18b3e7d5
Syed Ali Shahbaz <alishahbaz7@gmail.com>
2022-04-13 16:34:35 +0530

    disposable model updated

---
+++ apps/web/pages/d/[link]/[slug].tsx
@@ -0,0 +1,194 @@
+import { Prisma } from "@prisma/client";
+import { UserPlan } from "@prisma/client";
+import { GetServerSidePropsContext } from "next";
+import { JSONObject } from "superjson/dist/types";
+
+import { useLocale } from "@calcom/lib/hooks/useLocale";
+
+import { asStringOrNull } from "@lib/asStringOrNull";
+import { getWorkingHours } from "@lib/availability";
+import prisma from "@lib/prisma";
+import { inferSSRProps } from "@lib/types/inferSSRProps";
+
+import AvailabilityPage from "@components/booking/pages/AvailabilityPage";
+
+import { ssrInit } from "@server/lib/ssr";
+
+export type AvailabilityPageProps = inferSSRProps<typeof getServerSideProps>;
+
+export default function Type(props: AvailabilityPageProps) {
+  return <AvailabilityPage {...props} />;
+}
+
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  const ssr = await ssrInit(context);
+  const link = asStringOrNull(context.query.link) || "";
+  const slug = asStringOrNull(context.query.slug) || "";
+  const dateParam = asStringOrNull(context.query.date);
+
+  const eventTypeSelect = Prisma.validator<Prisma.EventTypeSelect>()({
+    id: true,
+    title: true,
+    availability: true,
+    description: true,
+    length: true,
+    price: true,
+    currency: true,
+    periodType: true,
+    periodStartDate: true,
+    periodEndDate: true,
+    periodDays: true,
+    periodCountCalendarDays: true,
+    schedulingType: true,
+    userId: true,
+    schedule: {
+      select: {
+        availability: true,
+        timeZone: true,
+      },
+    },
+    hidden: true,
+    slug: true,
+    minimumBookingNotice: true,
+    beforeEventBuffer: true,
+    afterEventBuffer: true,
+    timeZone: true,
+    metadata: true,
+    slotInterval: true,
+    users: {
+      select: {
+        avatar: true,
+        name: true,
+        username: true,
+        hideBranding: true,
+        plan: true,
+        timeZone: true,
+      },
+    },
+  });
+
+  const disposableType = await prisma.disposableLink.findUnique({
+    where: {
+      link_slug: {
+        link,
+        slug,
+      },
+    },
+    select: {
+      title: true,
+      locations: true,
+      length: true,
+      users: {
+        select: {
+          avatar: true,
+          name: true,
+          username: true,
+          hideBranding: true,
+          plan: true,
+          timeZone: true,
+        },
+      },
+      userId: true,
+      eventTypes: {
+        select: eventTypeSelect,
+      },
+      availability: true,
+      booking: true,
+      timeZone: true,
+      expired: true,
+    },
+  });
+
+  const userId = disposableType?.userId || disposableType?.eventTypes[0].userId;
+
+  if (!userId)
+    return {
+      notFound: true,
+    };
+
+  const users = await prisma.user.findMany({
+    where: {
+      id: userId,
+    },
+    select: {
+      id: true,
+      username: true,
+      name: true,
+      email: true,
+      bio: true,
+      avatar: true,
+      startTime: true,
+      endTime: true,
+      timeZone: true,
+      weekStart: true,
+      availability: true,
+      hideBranding: true,
+      brandColor: true,
+      darkBrandColor: true,
+      defaultScheduleId: true,
+      allowDynamicBooking: true,
+      schedules: {
+        select: {
+          availability: true,
+          timeZone: true,
+          id: true,
+        },
+      },
+      theme: true,
+      plan: true,
+    },
+  });
+
+  if (!users || !users.length) {
+    return {
+      notFound: true,
+    };
+  }
+  const [user] = users;
+  const eventTypeObject = Object.assign({}, disposableType, {
+    metadata: {} as JSONObject,
+    periodStartDate: null,
+    periodEndDate: null,
+  });
+
+  const schedule = {
+    ...user.schedules.filter(
+      (schedule) => !user.defaultScheduleId || schedule.id === user.defaultScheduleId
+    )[0],
+  };
+
+  const timeZone = schedule.timeZone || disposableType.timeZone || user.timeZone;
+
+  const workingHours = getWorkingHours(
+    {
+      timeZone,
+    },
+    schedule.availability ||
+      (disposableType.availability.length ? disposableType.availability : user.availability)
+  );
+  eventTypeObject.schedule = null;
+  eventTypeObject.availability = [];
+
+  const profile = {
+    name: user.name || user.username,
+    image: user.avatar,
+    slug: user.username,
+    theme: user.theme,
+    weekStart: user.weekStart,
+    brandColor: user.brandColor,
+    darkBrandColor: user.darkBrandColor,
+  };
+
+  return {
+    props: {
+      isDynamicGroup: false,
+      profile,
+      plan: user.plan,
+      date: dateParam,
+      eventType: eventTypeObject,
+      workingHours,
+      trpcState: ssr.dehydrate(),
+      previousPage: context.req.headers.referer ?? null,
+    },
+  };
+};
--- packages/prisma/migrations/20220413110229_add_disposable_booking/migration.sql
+++ packages/prisma/migrations/20220413110229_add_disposable_booking/migration.sql
@@ -13,12 +13,10 @@ ALTER TABLE "Booking" ADD COLUMN     "disposableLinkId" INTEGER;
 -- CreateTable
 CREATE TABLE "DisposableLink" (
     "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
     "link" TEXT NOT NULL,
     "slug" TEXT NOT NULL,
-    "locations" JSONB,
-    "length" INTEGER NOT NULL,
     "userId" INTEGER,
+    "eventTypeId" INTEGER NOT NULL,
     "timeZone" TEXT,
     "expired" BOOLEAN NOT NULL DEFAULT false,
 
@@ -27,12 +25,6 @@ CREATE TABLE "DisposableLink" (
 
 -- CreateTable
 CREATE TABLE "_disposable_user" (
-    "A" INTEGER NOT NULL,
-    "B" INTEGER NOT NULL
-);
-
--- CreateTable
-CREATE TABLE "_disposable_eventtype" (
     "A" INTEGER NOT NULL,
     "B" INTEGER NOT NULL
 );
@@ -47,12 +39,6 @@ CREATE UNIQUE INDEX "_disposable_user_AB_unique" ON "_disposable_user"("A", "B")
 CREATE INDEX "_disposable_user_B_index" ON "_disposable_user"("B");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "_disposable_eventtype_AB_unique" ON "_disposable_eventtype"("A", "B");
-
--- CreateIndex
-CREATE INDEX "_disposable_eventtype_B_index" ON "_disposable_eventtype"("B");
-
--- CreateIndex
 CREATE UNIQUE INDEX "Booking_disposableLinkId_key" ON "Booking"("disposableLinkId");
 
 -- AddForeignKey
@@ -62,13 +48,10 @@ ALTER TABLE "Booking" ADD CONSTRAINT "Booking_disposableLinkId_fkey" FOREIGN KEY
 ALTER TABLE "Availability" ADD CONSTRAINT "Availability_disposableLinkId_fkey" FOREIGN KEY ("disposableLinkId") REFERENCES "DisposableLink"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "DisposableLink" ADD CONSTRAINT "DisposableLink_eventTypeId_fkey" FOREIGN KEY ("eventTypeId") REFERENCES "EventType"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "_disposable_user" ADD FOREIGN KEY ("A") REFERENCES "DisposableLink"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_disposable_user" ADD FOREIGN KEY ("B") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "_disposable_eventtype" ADD FOREIGN KEY ("A") REFERENCES "DisposableLink"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "_disposable_eventtype" ADD FOREIGN KEY ("B") REFERENCES "EventType"("id") ON DELETE CASCADE ON UPDATE CASCADE;
