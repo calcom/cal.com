9a74a45db9dec475b1d726a2c504db4fa0484ea6
Alan <alannnc@gmail.com>
2022-04-12 20:08:56 -0600

    Update lib no required

--- apps/web/lib/reschedule.tsx
+++ apps/web/lib/reschedule.tsx
@@ -1,7 +1,7 @@
-import EventManager from "@calcom/core/EventManager";
+// import EventManager from "@calcom/core/EventManager";
 
-export const rescheduleUpdateOriginalBooking = async (originalBookingUId: string) => {
-  // Use EventManager to conditionally use all needed integrations.
-  const updateManager = await EventManager.update(evt, originalBookingUId);
-  return updateManager.results;
-};
+// export const rescheduleUpdateOriginalBooking = async (originalBookingUId: string) => {
+//   // Use EventManager to conditionally use all needed integrations.
+//   const updateManager = await new EventManager().update(evt, originalBookingUId);
+//   return updateManager.results;
+// };
