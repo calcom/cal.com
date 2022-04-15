02855ef0202c51a93d61e2c9ac5d95ef1b1a1f6c
Agusti Fernandez Pardo <git@agusti.me>
2022-04-15 17:35:05 +0200

    disable cors

--- next.config.js
+++ next.config.js
@@ -10,6 +10,24 @@ const withTM = require("next-transpile-modules")([
 // use something like withPlugins([withTM], {}) if more plugins added later.
 
 module.exports = withTM({
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Content-Type, api_key, Authorization",
+          },
+        ],
+      },
+    ];
+  },
   async rewrites() {
     return [
       // This redirects requests recieved at / the root to the /api/ folder.
