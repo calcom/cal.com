d58c28f9a760b496442d28a1d456cb8b417cde7d
Agusti Fernandez Pardo <git@agusti.me>
2022-04-19 00:51:49 +0200

    Remove commented css import

--- apps/swagger/pages/_app.tsx
+++ apps/swagger/pages/_app.tsx
@@ -3,8 +3,6 @@ import "swagger-ui-react/swagger-ui.css";
 
 import "../styles/globals.css";
 
-// import "../styles/swagger-cal.css";
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
