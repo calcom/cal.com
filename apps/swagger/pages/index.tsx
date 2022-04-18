13b2e16c07e21afbd72e7992bbe2c395c221c921
Agusti Fernandez Pardo <git@agusti.me>
2022-04-19 00:58:24 +0200

    improve lib

--- apps/swagger/pages/index.tsx
+++ apps/swagger/pages/index.tsx
@@ -1,36 +1,12 @@
 import SwaggerUI from "swagger-ui-react";
 
-import { SnippedGenerator } from "../lib/snippets";
+import { SnippedGenerator, requestSnippets } from "@lib/snippets";
 
-const requestSnippets = {
-  generators: {
-    curl_bash: {
-      title: "cURL (bash)",
-      syntax: "bash",
-    },
-    curl_powershell: {
-      title: "cURL (PowerShell)",
-      syntax: "powershell",
-    },
-    curl_cmd: {
-      title: "cURL (CMD)",
-      syntax: "bash",
-    },
-    node: {
-      title: "Node",
-      syntax: "node",
-    },
-  },
-  defaultExpanded: true,
-  languages: ["node"],
-  // e.g. only show curl bash = ["curl_bash"]
-};
 export default function APIDocs() {
   return (
     <SwaggerUI
       url={process.env.NEXT_PUBLIC_SWAGGER_DOCS_URL || "https://api.cal.com/docs"}
       supportedSubmitMethods={["get", "post", "put", "delete", "patch"]}
-      // requestSnippets={requestSnippets}
       requestSnippetsEnabled={true}
       requestSnippets={requestSnippets}
       plugins={[SnippedGenerator]}
