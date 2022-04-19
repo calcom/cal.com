7a0b6c7fdb160eda9e01f0cc95c1a62a7f4a3ca1
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-04-19 16:22:45 -0400

    Add Jitsi add button

--- packages/app-store/components.tsx
+++ packages/app-store/components.tsx
@@ -21,6 +21,7 @@ export const InstallAppButtonMap = {
   zoomvideo: dynamic(() => import("./zoomvideo/components/InstallAppButton")),
   office365video: dynamic(() => import("./office365video/components/InstallAppButton")),
   wipemycalother: dynamic(() => import("./wipemycalother/components/InstallAppButton")),
+  jitsivideo: dynamic(() => import("./jitsivideo/components/InstallAppButton")),
 };
 
 export const InstallAppButton = (
