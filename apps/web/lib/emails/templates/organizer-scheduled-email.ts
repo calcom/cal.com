0f60230101b30657f9882009298c4c7172e71fb0
Andrea Timaran <andrea.timaran@kommit.co>
2022-04-26 09:47:21 -0500

    Eslint errors

--- apps/web/lib/emails/templates/organizer-scheduled-email.ts
+++ apps/web/lib/emails/templates/organizer-scheduled-email.ts
@@ -325,14 +325,12 @@ ${getRichDescription(this.calEvent)}
       providerName = this.calEvent.location;
     }
 
-    if(this.calEvent.zoomLink){
+    if (this.calEvent.zoomLink) {
       return `
       <p style="height: 6px"></p>
       <div style="line-height: 6px;">
         <p style="color: #494949;">${this.calEvent.organizer.language.translate("where")}</p>
-        <p style="color: #494949; font-weight: 400; line-height: 24px;">${
-          this.calEvent.zoomLink
-        }</p>
+        <p style="color: #494949; font-weight: 400; line-height: 24px;">${this.calEvent.zoomLink}</p>
       </div>
       `;
     }
