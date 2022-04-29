1a61f8d6d03b6aa587556a9f42d57d19db0fec3f
Agusti Fernandez Pardo <git@agusti.me>
2022-04-29 18:22:23 +0200

    fix: apiKeyAuth

--- apps/swagger/pages/index.tsx
+++ apps/swagger/pages/index.tsx
@@ -1,12 +1,15 @@
 import dynamic from "next/dynamic";
+import { SwaggerUI } from "swagger-ui-react";
 
 import { SnippedGenerator, requestSnippets } from "@lib/snippets";
 
-const SwaggerUI: any = dynamic(() => import("swagger-ui-react"), { ssr: false });
+const SwaggerUIDynamic: SwaggerUI & { url: string } = dynamic(() => import("swagger-ui-react"), {
+  ssr: false,
+});
 
 export default function APIDocs() {
   return (
-    <SwaggerUI
+    <SwaggerUIDynamic
       url={process.env.NEXT_PUBLIC_SWAGGER_DOCS_URL || "https://api.cal.com/docs"}
       persistAuthorization={true}
       supportedSubmitMethods={["get", "post", "delete", "put", "options", "patch"]}
