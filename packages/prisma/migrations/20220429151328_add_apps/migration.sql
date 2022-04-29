ec543380091adb01c7d3b64393c95acaad803377
zomars <zomars@me.com>
2022-04-29 09:36:16 -0600

    Adds App Model

--- packages/prisma/migrations/20220429151328_add_apps/migration.sql
+++ packages/prisma/migrations/20220429151328_add_apps/migration.sql
@@ -2,15 +2,15 @@
 CREATE TYPE "AppCategories" AS ENUM ('calendar', 'messaging', 'other', 'payment', 'video', 'web3');
 
 -- CreateTable
-CREATE TABLE "AvailableApps" (
+CREATE TABLE "App" (
     "slug" TEXT NOT NULL,
     "keys" JSONB,
     "categories" "AppCategories"[],
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "AvailableApps_pkey" PRIMARY KEY ("slug")
+    CONSTRAINT "App_pkey" PRIMARY KEY ("slug")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "AvailableApps_slug_key" ON "AvailableApps"("slug");
+CREATE UNIQUE INDEX "App_slug_key" ON "App"("slug");
