efb44bd9536b6fa58497664c10537c9db0e89e08
zomars <zomars@me.com>
2022-05-02 09:44:47 -0600

    Squashes app migrations

--- packages/prisma/migrations/20220501221906_adds_app_credential_relationship/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Credential" ADD COLUMN     "appId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "Credential" ADD CONSTRAINT "Credential_appId_fkey" FOREIGN KEY ("appId") REFERENCES "App"("slug") ON DELETE CASCADE ON UPDATE CASCADE;
