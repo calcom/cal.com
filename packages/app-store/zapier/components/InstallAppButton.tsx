8462c929be5e193beac615b5298bff909e2f449c
zomars <zomars@me.com>
2022-05-03 15:19:56 -0600

    Update InstallAppButton.tsx

--- packages/app-store/zapier/components/InstallAppButton.tsx
+++ packages/app-store/zapier/components/InstallAppButton.tsx
@@ -3,8 +3,7 @@ import type { InstallAppButtonProps } from "@calcom/app-store/types";
 import useAddAppMutation from "../../_utils/useAddAppMutation";
 
 export default function InstallAppButton(props: InstallAppButtonProps) {
-  // @ts-ignore TODO: deprecate App types in favor of DB slugs
-  const mutation = useAddAppMutation("zapier");
+  const mutation = useAddAppMutation("zapier_other");
 
   return (
     <>
