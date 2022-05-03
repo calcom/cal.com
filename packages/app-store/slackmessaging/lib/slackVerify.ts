20f8abe113eb48db0ba8a8e64deb71a90c99a3fc
zomars <zomars@me.com>
2022-05-03 17:29:32 -0600

    Update slackVerify.ts

--- packages/app-store/slackmessaging/lib/slackVerify.ts
+++ packages/app-store/slackmessaging/lib/slackVerify.ts
@@ -3,20 +3,24 @@ import dayjs from "dayjs";
 import { NextApiRequest, NextApiResponse } from "next";
 import { stringify } from "querystring";
 
-const signingSecret = process.env.SLACK_SIGNING_SECRET;
+import getAppKeysFromSlug from "../../_utils/getAppKeysFromSlug";
+
+let signingSecret = "";
 
 export default async function slackVerify(req: NextApiRequest, res: NextApiResponse) {
   const body = req.body;
   const timeStamp = req.headers["x-slack-request-timestamp"] as string; // Always returns a string and not a string[]
   const slackSignature = req.headers["x-slack-signature"] as string;
   const currentTime = dayjs().unix();
+  let { signing_secret } = await getAppKeysFromSlug("slack");
+  if (typeof signing_secret === "string") signingSecret = signing_secret;
 
   if (!timeStamp) {
     return res.status(400).json({ message: "Missing X-Slack-Request-Timestamp header" });
   }
 
   if (!signingSecret) {
-    return res.status(400).json({ message: "Missing process.env.SLACK_SIGNING_SECRET" });
+    return res.status(400).json({ message: "Missing Slack's signing_secret" });
   }
 
   if (Math.abs(currentTime - parseInt(timeStamp)) > 60 * 5) {
