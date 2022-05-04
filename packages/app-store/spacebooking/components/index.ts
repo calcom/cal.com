1bf3f33fbc30b3d3add7413b94c2f066537876b2
zomars <zomars@me.com>
2022-05-03 21:55:03 -0600

    Updated comment

--- packages/app-store/spacebooking/components/index.ts
+++ packages/app-store/spacebooking/components/index.ts
@@ -1,5 +1,5 @@
 import dynamic from "next/dynamic";
 
 export { default as InstallAppButton } from "./InstallAppButton";
-/** These are like 40kb that not every user needs */
+/** These are like 12kb that not every user needs */
 export const SpaceBookingSuccessPage = dynamic(() => import("./SpaceBookingSuccessPage"));
