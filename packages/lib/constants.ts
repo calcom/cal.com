0b82b8516698067f0f919f60da120d094d51ee22
zomars <zomars@me.com>
2022-05-05 16:29:17 -0600

    Adds console url to redirection whitelist

--- packages/lib/constants.ts
+++ packages/lib/constants.ts
@@ -2,6 +2,9 @@ export const WEBAPP_URL = process.env.NEXT_PUBLIC_WEBAPP_URL || `https://${proce
 /** @deprecated use `WEBAPP_URL` */
 export const BASE_URL = WEBAPP_URL;
 export const WEBSITE_URL = process.env.NEXT_PUBLIC_WEBSITE_URL || "https://cal.com";
+export const CONSOLE_URL = WEBAPP_URL.startsWith("http://localhost")
+  ? "http://localhost:3004"
+  : `https://console.cal.${process.env.VERCEL_ENV === "production" ? "com" : "dev"}`;
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
 export const TRIAL_LIMIT_DAYS = 14;
 export const HOSTED_CAL_FEATURES = process.env.HOSTED_CAL_FEATURES || BASE_URL === "https://app.cal.com";
