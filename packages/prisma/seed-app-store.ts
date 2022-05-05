1a06d9906be2cace88fabdf675fb772e55582300
zomars <zomars@me.com>
2022-05-05 16:34:26 -0600

    Fixes daily-video slug

--- packages/prisma/seed-app-store.ts
+++ packages/prisma/seed-app-store.ts
@@ -53,7 +53,7 @@ async function main() {
   }
   // Video apps
   if (process.env.DAILY_API_KEY) {
-    await createApp("dailyvideo", "dailyvideo", ["video"], "daily_video", {
+    await createApp("daily-video", "dailyvideo", ["video"], "daily_video", {
       api_key: process.env.DAILY_API_KEY,
       scale_plan: process.env.DAILY_SCALE_PLAN,
     });
