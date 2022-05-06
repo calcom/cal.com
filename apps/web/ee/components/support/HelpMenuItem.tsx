d815de3f29b79645b0fd7746991b7e41a79b6556
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-05-06 18:35:33 -0400

    Add menu items

--- apps/web/ee/components/support/HelpMenuItem.tsx
+++ apps/web/ee/components/support/HelpMenuItem.tsx
@@ -1,7 +1,7 @@
-import { ChatAltIcon } from "@heroicons/react/solid";
+import { ChatAltIcon, QuestionMarkCircleIcon, DocumentTextIcon, CodeIcon } from "@heroicons/react/solid";
+import * as Popover from "@radix-ui/react-popover";
 
 import { useLocale } from "@calcom/lib/hooks/useLocale";
-import { Tooltip } from "@calcom/ui/Tooltip";
 
 import classNames from "@lib/classNames";
 
@@ -11,18 +11,50 @@ export default function HelpMenuItem() {
   const { t } = useLocale();
 
   return (
-    <>
-      <button className="flex w-full px-4 py-2 text-sm font-medium text-neutral-700 hover:bg-gray-100 hover:text-gray-900">
-        <ChatAltIcon
-          className={classNames(
-            "text-neutral-400 group-hover:text-neutral-500",
-            "h-5 w-5 flex-shrink-0 ltr:mr-3"
-          )}
-          aria-hidden="true"
-        />
-        {t("help")}
-      </button>
-      <ContactMenuItem />
-    </>
+    <Popover.Root>
+      <Popover.Trigger asChild>
+        <button className="flex w-full px-4 py-2 text-sm font-medium text-neutral-700 hover:bg-gray-100 hover:text-gray-900">
+          <QuestionMarkCircleIcon
+            className={classNames(
+              "text-gray-500 group-hover:text-neutral-500",
+              "h-5 w-5 flex-shrink-0 ltr:mr-3"
+            )}
+            aria-hidden="true"
+          />
+
+          {t("help")}
+        </button>
+      </Popover.Trigger>
+      <Popover.Content side="right">
+        <div className="border-gray-300 bg-white p-5 shadow-sm ">
+          <p className="mb-1 text-neutral-500">RESOURCES</p>
+          <a
+            href="https://docs.cal.com/"
+            target="_blank"
+            className="flex w-full px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+            rel="noreferrer">
+            <DocumentTextIcon className="h-5 w-5 flex-shrink-0 ltr:mr-3" />
+            Support documentation
+          </a>
+          <a
+            href="https://developer.cal.com/"
+            target="_blank"
+            className="flex w-full px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+            rel="noreferrer">
+            <CodeIcon className="h-5 w-5 flex-shrink-0 ltr:mr-3" />
+            Developer documentation
+          </a>
+          <p className="mb-1 text-neutral-500">GET IN TOUCH</p>
+          <ChatAltIcon
+            className={classNames(
+              "text-neutral-400 group-hover:text-neutral-500",
+              "h-5 w-5 flex-shrink-0 ltr:mr-3"
+            )}
+            aria-hidden="true"
+          />
+          <ContactMenuItem />
+        </div>
+      </Popover.Content>
+    </Popover.Root>
   );
 }
