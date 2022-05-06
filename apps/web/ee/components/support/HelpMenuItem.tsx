768c5be786dbd72bd616c23051d3daf4d9a53d8e
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-05-06 09:42:33 -0400

    Seperate help menu item and contact menu item

--- apps/web/ee/components/support/HelpMenuItem.tsx
+++ apps/web/ee/components/support/HelpMenuItem.tsx
@@ -1,13 +1,28 @@
-import HelpscoutMenuItem from "@ee/lib/helpscout/HelpscoutMenuItem";
-import IntercomMenuItem from "@ee/lib/intercom/IntercomMenuItem";
-import ZendeskMenuItem from "@ee/lib/zendesk/ZendeskMenuItem";
+import { ChatAltIcon } from "@heroicons/react/solid";
+
+import { useLocale } from "@calcom/lib/hooks/useLocale";
+import { Tooltip } from "@calcom/ui/Tooltip";
+
+import classNames from "@lib/classNames";
+
+import ContactMenuItem from "./ContactMenuItem";
 
 export default function HelpMenuItem() {
+  const { t } = useLocale();
+
   return (
     <>
-      <IntercomMenuItem />
-      <ZendeskMenuItem />
-      <HelpscoutMenuItem />
+      <button className="flex w-full px-4 py-2 text-sm font-medium text-neutral-700 hover:bg-gray-100 hover:text-gray-900">
+        <ChatAltIcon
+          className={classNames(
+            "text-neutral-400 group-hover:text-neutral-500",
+            "h-5 w-5 flex-shrink-0 ltr:mr-3"
+          )}
+          aria-hidden="true"
+        />
+        {t("help")}
+      </button>
+      <ContactMenuItem />
     </>
   );
 }
