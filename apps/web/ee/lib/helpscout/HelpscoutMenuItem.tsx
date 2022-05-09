dd92ccdafd0c1c70cb83e403961a54a4aaaeddff
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-05-09 11:27:12 -0400

    Adjust contact support links

--- apps/web/ee/lib/helpscout/HelpscoutMenuItem.tsx
+++ apps/web/ee/lib/helpscout/HelpscoutMenuItem.tsx
@@ -22,20 +22,19 @@ export default function HelpscoutMenuItem() {
   else
     return (
       <>
-        <DropdownMenuItem>
-          <button
-            onClick={handleClick}
-            className="flex w-full px-4 py-2 text-sm font-medium text-neutral-700 hover:bg-gray-100 hover:text-gray-900">
-            <ChatAltIcon
-              className={classNames(
-                "text-neutral-400 group-hover:text-neutral-500",
-                "h-5 w-5 flex-shrink-0 ltr:mr-3"
-              )}
-              aria-hidden="true"
-            />
-            {t("help")}
-          </button>
-        </DropdownMenuItem>
+        <button
+          onClick={handleClick}
+          className="flex w-full px-4 py-2 text-sm font-medium text-neutral-700 hover:bg-gray-100 hover:text-gray-900">
+          <ChatAltIcon
+            className={classNames(
+              "text-neutral-400 group-hover:text-neutral-500",
+              "h-5 w-5 flex-shrink-0 ltr:mr-3"
+            )}
+            aria-hidden="true"
+          />
+          {t("share_feedback")}
+        </button>
+
         {active && <HelpScout color="#292929" icon="message" horizontalPosition="right" zIndex="1" />}
       </>
     );
