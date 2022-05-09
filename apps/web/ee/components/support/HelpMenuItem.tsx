199a59d33f2ed0fbc430b2ca587b6d379056613e
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-05-09 11:25:50 -0400

    Render contact only if support keys are present

--- apps/web/ee/components/support/HelpMenuItem.tsx
+++ apps/web/ee/components/support/HelpMenuItem.tsx
@@ -33,7 +33,12 @@ export default function HelpMenuItem() {
             target="_blank"
             className="flex w-full px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 hover:text-gray-900"
             rel="noreferrer">
-            <DocumentTextIcon className="h-5 w-5 flex-shrink-0 ltr:mr-3" />
+            <DocumentTextIcon
+              className={classNames(
+                "text-neutral-400 group-hover:text-neutral-500",
+                "h-5 w-5 flex-shrink-0 ltr:mr-3"
+              )}
+            />
             Support documentation
           </a>
           <a
@@ -41,17 +46,17 @@ export default function HelpMenuItem() {
             target="_blank"
             className="flex w-full px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 hover:text-gray-900"
             rel="noreferrer">
-            <CodeIcon className="h-5 w-5 flex-shrink-0 ltr:mr-3" />
+            <CodeIcon
+              className={classNames(
+                "text-neutral-400 group-hover:text-neutral-500",
+                "h-5 w-5 flex-shrink-0 ltr:mr-3"
+              )}
+            />
             Developer documentation
           </a>
-          <p className="mb-1 text-neutral-500">GET IN TOUCH</p>
-          <ChatAltIcon
-            className={classNames(
-              "text-neutral-400 group-hover:text-neutral-500",
-              "h-5 w-5 flex-shrink-0 ltr:mr-3"
-            )}
-            aria-hidden="true"
-          />
+          {process.env.NEXT_PUBLIC_INTERCOM_APP_ID ||
+            process.env.NEXT_PUBLIC_ZENDESK_KEY ||
+            (process.env.NEXT_PUBLIC_HELPSCOUT_KEY && <p className="mb-1 text-neutral-500">GET IN TOUCH</p>)}
           <ContactMenuItem />
         </div>
       </Popover.Content>
