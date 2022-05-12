93ef72648405835ebe3d0012ac5429b91df45851
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2022-05-12 12:01:40 +0100

    Updating checkbox field to reflect new designs

--- apps/web/components/ui/form/CheckboxField.tsx
+++ apps/web/components/ui/form/CheckboxField.tsx
@@ -1,36 +1,50 @@
 import React, { forwardRef, InputHTMLAttributes } from "react";
 
+import InfoBadge from "@components/ui/InfoBadge";
+
 type Props = InputHTMLAttributes<HTMLInputElement> & {
-  label: React.ReactNode;
+  label?: React.ReactNode;
   description: string;
+  infomationIconText?: string;
 };
 
-const CheckboxField = forwardRef<HTMLInputElement, Props>(({ label, description, ...rest }, ref) => {
-  return (
-    <div className="block items-center sm:flex">
-      <div className="min-w-48 mb-4 sm:mb-0">
-        <label htmlFor={rest.id} className="flex text-sm font-medium text-neutral-700">
-          {label}
-        </label>
-      </div>
-      <div className="w-full">
-        <div className="relative flex items-start">
-          <div className="flex h-5 items-center">
-            <input
-              {...rest}
-              ref={ref}
-              type="checkbox"
-              className="text-primary-600 focus:ring-primary-500 h-4 w-4 rounded border-gray-300"
-            />
+const CheckboxField = forwardRef<HTMLInputElement, Props>(
+  ({ label, description, infomationIconText, ...rest }, ref) => {
+    return (
+      <div className="block items-center sm:flex">
+        {label && (
+          <div className="min-w-48 mb-4 sm:mb-0">
+            <label htmlFor={rest.id} className="flex text-sm font-medium text-neutral-700">
+              {label}
+            </label>
           </div>
-          <div className="text-sm ltr:ml-3 rtl:mr-3">
-            <p className="text-neutral-900">{description}</p>
+        )}
+        <div className="w-full">
+          <div className="relative flex items-start">
+            <div className="flex h-5 items-center">
+              <input
+                {...rest}
+                ref={ref}
+                type="checkbox"
+                className="text-primary-600 focus:ring-primary-500 h-4 w-4 rounded border-gray-300"
+              />
+            </div>
+            <div className="text-sm ltr:ml-3 rtl:mr-3">
+              {!label ? (
+                <label htmlFor={rest.id} className="text-neutral-900">
+                  {description}
+                </label>
+              ) : (
+                <p className="text-neutral-900">{description}</p>
+              )}
+            </div>
           </div>
         </div>
+        {infomationIconText && <InfoBadge content={infomationIconText}></InfoBadge>}
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 CheckboxField.displayName = "CheckboxField";
 
