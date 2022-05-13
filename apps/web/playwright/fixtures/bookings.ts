11166e768fb96c200f215cb77e1ce5e05b81ade9
zomars <zomars@me.com>
2022-05-13 10:06:58 -0600

    Adds bookings fixture

--- apps/web/playwright/fixtures/bookings.ts
+++ apps/web/playwright/fixtures/bookings.ts
@@ -1,7 +1,15 @@
 import type { Page } from "@playwright/test";
-import type { Booking } from "@prisma/client";
+import type { Booking, Prisma } from "@prisma/client";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import short from "short-uuid";
+import { v5 as uuidv5, v4 as uuidv4 } from "uuid";
 
 import { prisma } from "@calcom/prisma";
+
+dayjs.extend(utc);
+
+const translator = short();
 
 type BookingFixture = ReturnType<typeof createBookingFixture>;
 
@@ -9,13 +17,47 @@ type BookingFixture = ReturnType<typeof createBookingFixture>;
 export const createBookingsFixture = (page: Page) => {
   let store = { bookings: [], page } as { bookings: BookingFixture[]; page: typeof page };
   return {
-    create: async () => {
+    create: async (
+      userId: number,
+      username: string | null,
+      eventTypeId = -1,
+      {
+        confirmed = true,
+        rescheduled = false,
+        paid = false,
+        status = "ACCEPTED",
+      }: Partial<Prisma.BookingCreateInput> = {}
+    ) => {
+      const startDate = dayjs().add(1, "day").toDate();
+      const seed = `${username}:${dayjs(startDate).utc().format()}:${new Date().getTime()}`;
+      const uid = translator.fromUUID(uuidv5(seed, uuidv5.URL));
       const booking = await prisma.booking.create({
         data: {
-          endTime: "",
-          startTime: "",
-          title: "",
-          uid: "",
+          uid: uid,
+          title: "30min",
+          startTime: startDate,
+          endTime: dayjs().add(1, "day").add(30, "minutes").toDate(),
+          user: {
+            connect: {
+              id: userId,
+            },
+          },
+          attendees: {
+            create: {
+              email: "attendee@example.com",
+              name: "Attendee Example",
+              timeZone: "Europe/London",
+            },
+          },
+          eventType: {
+            connect: {
+              id: eventTypeId,
+            },
+          },
+          confirmed,
+          rescheduled,
+          paid,
+          status,
         },
       });
       const bookingFixture = createBookingFixture(booking, store.page!);
@@ -24,7 +66,7 @@ export const createBookingsFixture = (page: Page) => {
     },
     get: () => store.bookings,
     delete: async (id: number) => {
-      const user = await prisma.booking.delete({
+      await prisma.booking.delete({
         where: { id },
       });
       store.bookings = store.bookings.filter((b) => b.id !== id);
@@ -32,16 +74,15 @@ export const createBookingsFixture = (page: Page) => {
   };
 };
 
-type JSONValue = string | number | boolean | { [x: string]: JSONValue } | Array<JSONValue>;
-
 // creates the single user fixture
 const createBookingFixture = (booking: Booking, page: Page) => {
   const store = { booking, page };
 
   // self is a reflective method that return the Prisma object that references this fixture.
   return {
-    id: booking.id,
+    id: store.booking.id,
+    uid: store.booking.uid,
     self: async () => (await prisma.booking.findUnique({ where: { id: store.booking.id } }))!,
-    delete: async (id: number) => (await prisma.booking.delete({ where: { id } }))!,
+    delete: async () => (await prisma.booking.delete({ where: { id: store.booking.id } }))!,
   };
 };
