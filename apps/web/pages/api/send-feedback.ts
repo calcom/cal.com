d876a6f4c6256ae6cf81b0ec7583ab1c59d52d71
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-05-13 14:17:31 -0400

    Send feedback email

--- apps/web/pages/api/send-feedback.ts
+++ apps/web/pages/api/send-feedback.ts
@@ -2,6 +2,8 @@ import dayjs from "dayjs";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 import { getSession } from "@lib/auth";
+import { sendFeedbackEmail } from "@lib/emails/email-manager";
+import FeedbackEmail from "@lib/emails/templates/feedback-email";
 import prisma from "@lib/prisma";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -10,14 +12,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(401).json({ message: "Not authenticated" });
   }
 
+  const feedback = {
+    userId: session.user.id,
+    rating: req.body.rating,
+    comment: req.body.comment,
+  };
+
   await prisma.feedback.create({
     data: {
-      userId: session.user.id,
+      ...feedback,
       date: dayjs().toISOString(),
-      rating: req.body.rating,
-      comment: req.body.comment,
     },
   });
+
+  if (process.env.SEND_FEEDBACK_EMAIL || feedback.comment) sendFeedbackEmail(feedback);
 
   res.status(200).end();
 }
