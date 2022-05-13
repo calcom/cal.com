198aa027f262e06965a1ce34127038c878fd59a6
zomars <zomars@me.com>
2022-05-13 10:07:49 -0600

    Updates fixtures

--- apps/web/playwright/lib/fixtures.ts
+++ apps/web/playwright/lib/fixtures.ts
@@ -1,14 +1,26 @@
 import { test as base } from "@playwright/test";
 
+import { createBookingsFixture } from "../fixtures/bookings";
+import { createPaymentsFixture } from "../fixtures/payments";
 import { createUsersFixture } from "../fixtures/users";
 
 interface Fixtures {
   users: ReturnType<typeof createUsersFixture>;
+  bookings: ReturnType<typeof createBookingsFixture>;
+  payments: ReturnType<typeof createPaymentsFixture>;
 }
 
 export const test = base.extend<Fixtures>({
   users: async ({ page }, use) => {
     const usersFixture = createUsersFixture(page);
     await use(usersFixture);
+  },
+  bookings: async ({ page }, use) => {
+    const bookingsFixture = createBookingsFixture(page);
+    await use(bookingsFixture);
+  },
+  payments: async ({ page }, use) => {
+    const payemntsFixture = createPaymentsFixture(page);
+    await use(payemntsFixture);
   },
 });
