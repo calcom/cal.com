c9e6d26ea9159680d97c3470ca2f901f1ad89a8e
zomars <zomars@me.com>
2022-05-18 10:04:19 -0600

    Update useIntercom.ts

--- apps/web/ee/lib/intercom/useIntercom.ts
+++ apps/web/ee/lib/intercom/useIntercom.ts
@@ -1,13 +1,12 @@
+import noop from "lodash/noop";
 import { useIntercom as _useIntercom } from "react-use-intercom";
 
 export const useIntercom =
   typeof window !== "undefined" && !!process.env.NEXT_PUBLIC_INTERCOM_APP_ID
     ? _useIntercom
     : () => ({
-        // eslint-disable-next-line @typescript-eslint/no-empty-function
-        boot: () => {},
-        // eslint-disable-next-line @typescript-eslint/no-empty-function
-        show: () => {},
+        boot: noop,
+        show: noop,
       });
 
 export default useIntercom;
