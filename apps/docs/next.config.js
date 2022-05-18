02db7e1d81f7ae17e86cd2ee494e2d608f3ad3ca
Omar López <zomars@me.com>
2022-05-18 09:10:57 -0600

    Update apps/docs/next.config.js

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,6 +1,5 @@
-import nextra from "nextra";
-
-const withNextra = nextra({
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.js",
   unstable_staticImage: true,
