957d6d2375c092878be05a96fb0f8866bc916871
Agusti Fernandez Pardo <git@agusti.me>
2022-05-18 19:10:16 +0200

    fix: locations or z null

--- lib/validations/event-type.ts
+++ lib/validations/event-type.ts
@@ -38,8 +38,8 @@ const schemaEventTypeCreateParams = z
     slug: z.string(),
     description: z.string().optional().nullable(),
     length: z.number().int(),
-    locations: jsonSchema.optional().nullable().or(z.null()),
-    metadata: z.any().optional().nullable().nullish(),
+    // locations: jsonSchema.optional().nullable().or(z.null()),
+    // metadata: z.any().optional().nullable().nullish(),
     recurringEvent: jsonSchema.optional().nullable().or(z.null()),
   })
   .strict();
@@ -85,4 +85,4 @@ export const schemaEventTypeReadPublic = EventType.pick({
   slotInterval: true,
   successRedirectUrl: true,
   description: true,
-}).merge(z.object({ metadata: z.any(), locations: z.any() }));
+}).merge(z.object({ metadata: z.any(), locations: jsonSchema.or(z.null()) }));
