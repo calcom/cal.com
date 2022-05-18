beeffcf5be779ade3243aed9400596d32576b795
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-05-18 10:55:46 -0400

    Add import statement

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,4 +1,6 @@
-const withNextra = require("nextra")({
+import nextra from "nextra";
+
+const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.js",
   unstable_staticImage: true,
