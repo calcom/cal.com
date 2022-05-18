b2d23f45f1ec27b8691b5aba3fa1da8eeef80674
zomars <zomars@me.com>
2022-05-17 22:09:06 -0600

    Revert "Linting"
    This reverts commit 1b59dacd644e20551e104a99685429e20e55215d.

--- packages/embeds/embed-snippet/src/index.ts
+++ packages/embeds/embed-snippet/src/index.ts
@@ -1,5 +1,3 @@
-/* eslint-disable prefer-rest-params */
-
 /**
  * As we want to keep control on the size of this snippet but we want some portion of it to be still readable.
  * So, write the code that you need directly but keep it short.
@@ -31,19 +29,18 @@ export interface CalWindow extends Window {
 
 export default function EmbedSnippet(url = EMBED_LIB_URL) {
   (function (C: CalWindow, A, L) {
-    const p = function (a: any, ar: any) {
+    let p = function (a: any, ar: any) {
       a.q.push(ar);
     };
-    const d = C.document;
+    let d = C.document;
     C.Cal =
       C.Cal ||
       function () {
-        const cal = C.Cal!;
-        const ar = arguments;
+        let cal = C.Cal!;
+        let ar = arguments;
         if (!cal.loaded) {
           cal.ns = {};
           cal.q = cal.q || [];
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
           //@ts-ignore
           d.head.appendChild(d.createElement("script")).src = A;
           cal.loaded = true;
