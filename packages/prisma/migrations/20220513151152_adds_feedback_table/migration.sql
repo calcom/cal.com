4a6c0a68e180bcd023a1a65c265f0f2dafdd06d8
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-05-18 16:26:40 -0400

    Change rating to strings

--- packages/prisma/migrations/20220513151152_adds_feedback_table/migration.sql
+++ packages/prisma/migrations/20220513151152_adds_feedback_table/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "Feedback" (
     "id" SERIAL NOT NULL,
     "date" TIMESTAMP(3) NOT NULL,
     "userId" INTEGER NOT NULL,
-    "rating" INTEGER NOT NULL,
+    "rating" TEXT NOT NULL,
     "comment" TEXT,
 
     CONSTRAINT "Feedback_pkey" PRIMARY KEY ("id")
