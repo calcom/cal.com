8396954e28809d0ec95617fad9cbb138758d541a
zomars <zomars@me.com>
2022-05-24 19:18:41 -0600

    Fixes paid bookings
    # Conflicts:
    #	apps/web/ee/components/stripe/Payment.tsx

--- packages/stripe/client.ts
+++ packages/stripe/client.ts
@@ -25,12 +25,13 @@ export function createPaymentLink(opts: {
   paymentUid: string;
   name?: Maybe<string>;
   date?: Maybe<string>;
+  email?: Maybe<string>;
   absolute?: boolean;
 }): string {
-  const { paymentUid, name, date, absolute = true } = opts;
+  const { paymentUid, name, email, date, absolute = true } = opts;
   let link = "";
   if (absolute) link = process.env.NEXT_PUBLIC_WEBSITE_URL!;
-  const query = stringify({ date, name });
+  const query = stringify({ date, name, email });
   return link + `/payment/${paymentUid}?${query}`;
 }
 
