874b0424c627961a8b77122bb64c4c8e96599d51
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2022-05-26 18:19:21 +0100

    Storybook Boilerplate setup

---
+++ apps/storybook/.storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+};
---
+++ apps/storybook/preview.js
@@ -0,0 +1,8 @@
+import * as NextImage from "next/image";
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
