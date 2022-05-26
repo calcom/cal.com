e8e36abc75d0fe7d804b318c3caeb2f606ceab45
Syed Ali Shahbaz <52925846+alishaz-polymath@users.noreply.github.com>
2022-05-26 12:32:04 +0530

    prettier fix

--- lib/validations/webhook.ts
+++ lib/validations/webhook.ts
@@ -3,9 +3,9 @@ import { z } from "zod";
 import { _WebhookModel as Webhook } from "@calcom/prisma/zod";
 
 export const WebhookTriggerEvents = {
-  BOOKING_CREATED: 'BOOKING_CREATED',
-  BOOKING_RESCHEDULED: 'BOOKING_RESCHEDULED',
-  BOOKING_CANCELLED: 'BOOKING_CANCELLED'
+  BOOKING_CREATED: "BOOKING_CREATED",
+  BOOKING_RESCHEDULED: "BOOKING_RESCHEDULED",
+  BOOKING_CANCELLED: "BOOKING_CANCELLED"
 };
 
 export const WEBHOOK_TRIGGER_EVENTS = [
