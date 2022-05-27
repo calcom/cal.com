eaa35457aea1d59177307f9291bea318c59770de
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2022-05-27 13:04:57 +0100

    First story

---
+++ apps/storybook/stories/Button.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { Button } from "@calcom/ui";
+
+export default {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: "Button",
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  color: "primary",
+};
