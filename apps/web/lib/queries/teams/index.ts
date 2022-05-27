541aef5a017a602334cc6d292c39ab7f2bc7406b
Alex van Andel <me@alexvanandel.com>
2022-05-27 16:07:12 +0100

    disabled any warning for intentional typing of AsyncReturnType

--- apps/web/lib/queries/teams/index.ts
+++ apps/web/lib/queries/teams/index.ts
@@ -2,9 +2,11 @@ import { Prisma, UserPlan } from "@prisma/client";
 
 import prisma from "@lib/prisma";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 type AsyncReturnType<T extends (...args: any) => Promise<any>> = T extends (...args: any) => Promise<infer R>
   ? R
-  : any;
+  : // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    any;
 
 export type TeamWithMembers = AsyncReturnType<typeof getTeamWithMembers>;
 
