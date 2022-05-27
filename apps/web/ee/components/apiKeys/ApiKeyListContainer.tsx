4a1e64b3a5d1d9aefcb3c5cae2ba361ba7df3fd8
Leo Giovanetti <hello@leog.me>
2022-05-27 18:01:14 -0300

    Adopting main changes

--- apps/web/ee/components/apiKeys/ApiKeyListContainer.tsx
+++ apps/web/ee/components/apiKeys/ApiKeyListContainer.tsx
@@ -24,63 +24,69 @@ function ApiKeyListContainer() {
   const [editModalOpen, setEditModalOpen] = useState(false);
   const [apiKeyToEdit, setApiKeyToEdit] = useState<(TApiKeys & { neverExpires?: boolean }) | null>(null);
   return (
-    <QueryCell
-      query={query}
-      customLoader={<SkeletonLoader />}
-      success={({ data }) => (
-        <div className="border-b border-gray-200 py-8 pl-2 pr-1">
-          <ShellSubHeading
-            className="mt-2"
-            title={t("api_keys")}
-            subtitle={t("api_keys_subtitle")}
-            actions={
-              <Button
-                color="secondary"
-                size="icon"
-                StartIcon={PlusIcon}
-                onClick={() => setNewApiKeyModal(true)}
-                data-testid="new_webhook"
-              />
-            }
+    <div className="border-b border-gray-200 py-8 pl-2 pr-1">
+      <ShellSubHeading
+        className="mt-2"
+        title={t("api_keys")}
+        subtitle={t("api_keys_subtitle")}
+        actions={
+          <Button
+            color="secondary"
+            size="icon"
+            StartIcon={PlusIcon}
+            onClick={() => setNewApiKeyModal(true)}
+            data-testid="new_webhook"
           />
-
-          {data.length > 0 && (
-            <List className="mt-6">
-              {data.map((item) => (
-                <ApiKeyListItem
-                  key={item.id}
-                  apiKey={item}
-                  onEditApiKey={() => {
-                    setApiKeyToEdit(item);
-                    setEditModalOpen(true);
-                  }}
-                />
-              ))}
-            </List>
-          )}
-
-          {/* New api key dialog */}
-          <Dialog open={newApiKeyModal} onOpenChange={(isOpen) => !isOpen && setNewApiKeyModal(false)}>
-            <DialogContent>
-              <ApiKeyDialogForm title={t("create_api_key")} handleClose={() => setNewApiKeyModal(false)} />
-            </DialogContent>
-          </Dialog>
-          {/* Edit api key dialog */}
-          <Dialog open={editModalOpen} onOpenChange={(isOpen) => !isOpen && setEditModalOpen(false)}>
-            <DialogContent>
-              {apiKeyToEdit && (
-                <ApiKeyDialogForm
-                  title={t("edit_api_key")}
-                  key={apiKeyToEdit.id}
-                  handleClose={() => setEditModalOpen(false)}
-                  defaultValues={apiKeyToEdit}
-                />
+        }
+      />
+      <LicenseRequired>
+        <QueryCell
+          query={query}
+          customLoader={<SkeletonLoader />}
+          success={({ data }) => (
+            <>
+              {data.length > 0 && (
+                <List className="mt-6">
+                  {data.map((item) => (
+                    <ApiKeyListItem
+                      key={item.id}
+                      apiKey={item}
+                      onEditApiKey={() => {
+                        setApiKeyToEdit(item);
+                        setEditModalOpen(true);
+                      }}
+                    />
+                  ))}
+                </List>
               )}
-            </DialogContent>
-          </Dialog>
-        </div>
-      )}
-    />
+
+              {/* New api key dialog */}
+              <Dialog open={newApiKeyModal} onOpenChange={(isOpen) => !isOpen && setNewApiKeyModal(false)}>
+                <DialogContent>
+                  <ApiKeyDialogForm
+                    title={t("create_api_key")}
+                    handleClose={() => setNewApiKeyModal(false)}
+                  />
+                </DialogContent>
+              </Dialog>
+              {/* Edit api key dialog */}
+              <Dialog open={editModalOpen} onOpenChange={(isOpen) => !isOpen && setEditModalOpen(false)}>
+                <DialogContent>
+                  {apiKeyToEdit && (
+                    <ApiKeyDialogForm
+                      title={t("edit_api_key")}
+                      key={apiKeyToEdit.id}
+                      handleClose={() => setEditModalOpen(false)}
+                      defaultValues={apiKeyToEdit}
+                    />
+                  )}
+                </DialogContent>
+              </Dialog>
+            </>
+          )}
+        />
+      </LicenseRequired>
+    </div>
   );
 }
 
--- apps/web/pages/settings/developer.tsx
+++ apps/web/pages/settings/developer.tsx
@@ -2,18 +2,15 @@ import { useLocale } from "@calcom/lib/hooks/useLocale";
 import ApiKeyListContainer from "@ee/components/apiKeys/ApiKeyListContainer";
 
 import SettingsShell from "@components/SettingsShell";
-import Shell from "@components/Shell";
 import WebhookListContainer from "@components/webhook/WebhookListContainer";
 
 export default function Settings() {
   const { t } = useLocale();
 
   return (
-    <Shell heading={t("developer")} subtitle={t("manage_developer_settings")}>
-      <SettingsShell>
-        <WebhookListContainer title={t("webhooks")} subtitle={t("receive_cal_meeting_data")} />
-        <ApiKeyListContainer />
-      </SettingsShell>
-    </Shell>
+    <SettingsShell heading={t("developer")} subtitle={t("manage_developer_settings")}>
+      <WebhookListContainer title={t("webhooks")} subtitle={t("receive_cal_meeting_data")} />
+      <ApiKeyListContainer />
+    </SettingsShell>
   );
 }
