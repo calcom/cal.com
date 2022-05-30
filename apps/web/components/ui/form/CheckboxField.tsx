0248d04489f9ecbf077b93175e73500c407f04c8
Alex van Andel <me@alexvanandel.com>
2022-05-30 14:46:24 +0100

    Fixed typo in CheckboxField and wrapped description in <label>

--- apps/web/components/ui/form/CheckboxField.tsx
+++ apps/web/components/ui/form/CheckboxField.tsx
@@ -6,46 +6,45 @@ type Props = InputHTMLAttributes<HTMLInputElement> & {
   label?: React.ReactNode;
   description: string;
   descriptionAsLabel?: boolean;
-  infomationIconText?: string;
+  informationIconText?: string;
 };
 
 const CheckboxField = forwardRef<HTMLInputElement, Props>(
-  ({ label, description, infomationIconText, descriptionAsLabel, ...rest }, ref) => {
+  ({ label, description, informationIconText, descriptionAsLabel, ...rest }, ref) => {
     return (
       <div className="block items-center sm:flex">
-        {label && !descriptionAsLabel && (
+        {label && (
           <div className="min-w-48 mb-4 sm:mb-0">
-            <label htmlFor={rest.id} className="flex text-sm font-medium text-neutral-700">
-              {label}
-            </label>
-          </div>
-        )}
-        {label && descriptionAsLabel && (
-          <div className="min-w-48 mb-4 sm:mb-0">
-            <span className="flex text-sm font-medium text-neutral-700">{label}</span>
+            {React.createElement(
+              descriptionAsLabel ? "div" : "label",
+              {
+                htmlFor: rest.id,
+                className: "flex text-sm font-medium text-neutral-700",
+              },
+              label
+            )}
           </div>
         )}
         <div className="w-full">
           <div className="relative flex items-start">
-            <div className="flex h-5 items-center">
-              <input
-                {...rest}
-                disabled={rest.disabled}
-                ref={ref}
-                type="checkbox"
-                className="text-primary-600 focus:ring-primary-500 h-4 w-4 rounded border-gray-300"
-              />
-            </div>
-            <div className="text-sm ltr:ml-3 rtl:mr-3">
-              {!label || descriptionAsLabel ? (
-                <label htmlFor={rest.id} className="text-neutral-700">
-                  {description}
-                </label>
-              ) : (
-                <p className="text-neutral-900">{description}</p>
-              )}
-            </div>
-            {infomationIconText && <InfoBadge content={infomationIconText}></InfoBadge>}
+            {React.createElement(
+              descriptionAsLabel ? "label" : "div",
+              {
+                className: "relative flex items-start",
+              },
+              <>
+                <div className="flex h-5 items-center">
+                  <input
+                    {...rest}
+                    ref={ref}
+                    type="checkbox"
+                    className="text-primary-600 focus:ring-primary-500 h-4 w-4 rounded border-gray-300"
+                  />
+                </div>
+                <span className="text-sm text-neutral-700 ltr:ml-3 rtl:mr-3">{description}</span>
+              </>
+            )}
+            {informationIconText && <InfoBadge content={informationIconText}></InfoBadge>}
           </div>
         </div>
       </div>
