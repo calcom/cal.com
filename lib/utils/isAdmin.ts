7f42cc84793fd59808a030aedcc44079c1315565
Agusti Fernandez Pardo <git@agusti.me>
2022-05-31 20:46:09 +0200

    fix: improve admin check using enum instead of string

--- lib/utils/isAdmin.ts
+++ lib/utils/isAdmin.ts
@@ -1,6 +1,7 @@
 import prisma from "@calcom/prisma";
+import { UserPermissionRole } from "@calcom/prisma/client";
 
 export const isAdminGuard = async (userId: number) => {
   const user = await prisma.user.findUnique({ where: { id: userId } });
-  return user?.role === "ADMIN";
+  return user?.role === UserPermissionRole.ADMIN;
 };
