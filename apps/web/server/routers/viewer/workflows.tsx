176256983a80fdb8e9444788449114a0710ceb12
CarinaWolli <wollencarina@gmail.com>
2022-05-31 14:50:52 +0200

    add mutation to create workflows

--- apps/web/server/routers/viewer/workflows.tsx
+++ apps/web/server/routers/viewer/workflows.tsx
@@ -1,14 +1,50 @@
 import { z } from "zod";
 
+import { WORKFLOW_TRIGGER_EVENTS } from "@lib/workflows/constants";
+import { WORKFLOW_ACTIONS } from "@lib/workflows/constants";
+
 import { createProtectedRouter } from "@server/createRouter";
 
-export const workflowsRouter = createProtectedRouter().query("list", {
-  async resolve({ ctx }) {
-    const workflows = await ctx.prisma.workflow.findMany({
-      where: {
-        userId: ctx.user.id,
-      },
-    });
-    return { workflows };
-  },
-});
+export const workflowsRouter = createProtectedRouter()
+  .query("list", {
+    async resolve({ ctx }) {
+      const workflows = await ctx.prisma.workflow.findMany({
+        where: {
+          userId: ctx.user.id,
+        },
+      });
+      return { workflows };
+    },
+  })
+  .mutation("create", {
+    input: z.object({
+      name: z.string(),
+      trigger: z.enum(WORKFLOW_TRIGGER_EVENTS),
+      action: z.enum(WORKFLOW_ACTIONS),
+    }),
+    async resolve({ ctx, input }) {
+      const { name, trigger, action } = input;
+      const userId = ctx.user.id;
+
+      try {
+        const workflow = await ctx.prisma.workflow.create({
+          data: {
+            name,
+            trigger,
+            userId,
+          },
+        });
+
+        await ctx.prisma.workflowStep.create({
+          data: {
+            stepNumber: 1,
+            action,
+            workflowId: workflow.id,
+          },
+        });
+        return { workflow };
+      } catch (e) {
+        throw e;
+      }
+    },
+  });
