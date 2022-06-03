0d9e49b7f69e16a3b923cc127b8ecc6a1eda2bda
CarinaWolli <wollencarina@gmail.com>
2022-06-03 11:52:18 +0200

    fix lint errors

--- apps/web/components/eventtype/EventTypeDescription.tsx
+++ apps/web/components/eventtype/EventTypeDescription.tsx
@@ -46,33 +46,33 @@ export const EventTypeDescription = ({ eventType, className }: EventTypeDescript
             {eventType.description.length > 100 && "..."}
           </h2>
         )}
-        <ul className="flex mt-2 flex-wrap sm:flex-nowrap">
-          <li className="flex mr-4 items-center whitespace-nowrap">
+        <ul className="mt-2 flex flex-wrap sm:flex-nowrap">
+          <li className="mr-4 flex items-center whitespace-nowrap">
             <ClockIcon className="mr-1.5 inline h-4 w-4 text-neutral-400" aria-hidden="true" />
             {eventType.length}m
           </li>
           {eventType.schedulingType ? (
-            <li className="flex mr-4 items-center whitespace-nowrap">
+            <li className="mr-4 flex items-center whitespace-nowrap">
               <UsersIcon className="mr-1.5 inline h-4 w-4 text-neutral-400" aria-hidden="true" />
               {eventType.schedulingType === SchedulingType.ROUND_ROBIN && t("round_robin")}
               {eventType.schedulingType === SchedulingType.COLLECTIVE && t("collective")}
             </li>
           ) : (
-            <li className="flex mr-4 items-center whitespace-nowrap">
+            <li className="mr-4 flex items-center whitespace-nowrap">
               <UserIcon className="mr-1.5 inline h-4 w-4 text-neutral-400" aria-hidden="true" />
               {t("1_on_1")}
             </li>
           )}
         </ul>
-        <ul className="flex mt-2">
+        <ul className="mt-2 flex">
           {recurringEvent?.count && recurringEvent.count > 0 && (
-            <li className="flex mr-4 items-center whitespace-nowrap">
+            <li className="mr-4 flex items-center whitespace-nowrap">
               <RefreshIcon className="mr-1.5 inline h-4 w-4 text-neutral-400" aria-hidden="true" />
               {t("repeats_up_to", { count: recurringEvent.count })}
             </li>
           )}
           {eventType.price > 0 && (
-            <li className="flex mr-4 items-center whitespace-nowrap">
+            <li className="mr-4 flex items-center whitespace-nowrap">
               <CreditCardIcon className="mr-1.5 inline h-4 w-4 text-neutral-400" aria-hidden="true" />
               <IntlProvider locale="en">
                 <FormattedNumber
