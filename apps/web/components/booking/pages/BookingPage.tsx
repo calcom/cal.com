abe5d53fc1591f23cf98cf644c6d58f6d98b727f
Carina Wollendorfer <30310907+CarinaWolli@users.noreply.github.com>
2022-06-08 16:02:09 +0200

    Disable dark mode on the success page only for the organizer  (#3001)
    * disable darkmode only for organizer
    
    * disable dark mode when user comes from user dashboard
    
    * move Theme to shell
    
    * fix build errors
    
    * sort dates for recurring bookings
    
    * new parameter if user comes from booking an event
    
    Co-authored-by: CarinaWolli <wollencarina@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- apps/web/components/booking/pages/BookingPage.tsx
+++ apps/web/components/booking/pages/BookingPage.tsx
@@ -166,6 +166,7 @@ const BookingPage = ({
           location,
           eventName: profile.eventName || "",
           bookingId: id,
+          isSuccessBookingPage: true,
         },
       });
     },
