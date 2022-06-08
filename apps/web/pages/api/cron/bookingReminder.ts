3a2224a4e4701a85fbc0278bfa3a7a412e91f8d1
Omar López <zomars@me.com>
2022-06-08 02:37:06 -0600

    Update bookingReminder.ts (#3007)

--- apps/web/pages/api/cron/bookingReminder.ts
+++ apps/web/pages/api/cron/bookingReminder.ts
@@ -29,6 +29,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         createdAt: {
           lte: dayjs().add(-interval, "minutes").toDate(),
         },
+        // Only send reminders if the event hasn't finished
+        endTime: { gte: new Date() },
       },
       select: {
         ...bookingMinimalSelect,
