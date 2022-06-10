ac19535353ec3623e8eb51b414d68acea348fb30
Sean Brydon <sean@cal.com>
2022-06-10 09:17:02 +0100

    Adding global selects

--- apps/web/pages/[user]/[type].tsx
+++ apps/web/pages/[user]/[type].tsx
@@ -6,6 +6,7 @@ import { locationHiddenFilter, LocationObject } from "@calcom/app-store/location
 import { parseRecurringEvent } from "@calcom/lib";
 import { getDefaultEvent, getGroupName, getUsernameList } from "@calcom/lib/defaultEvents";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
+import { availiblityPageEventTypeSelect } from "@calcom/prisma/selects";
 
 import { asStringOrNull } from "@lib/asStringOrNull";
 import { getWorkingHours } from "@lib/availability";
@@ -71,50 +72,6 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
     throw new Error(`File is not named [type]/[user]`);
   }
 
-  const eventTypeSelect = Prisma.validator<Prisma.EventTypeSelect>()({
-    id: true,
-    title: true,
-    availability: true,
-    description: true,
-    length: true,
-    price: true,
-    currency: true,
-    periodType: true,
-    periodStartDate: true,
-    periodEndDate: true,
-    periodDays: true,
-    periodCountCalendarDays: true,
-    locations: true,
-    schedulingType: true,
-    recurringEvent: true,
-    requiresConfirmation: true,
-    schedule: {
-      select: {
-        availability: true,
-        timeZone: true,
-      },
-    },
-    hidden: true,
-    slug: true,
-    minimumBookingNotice: true,
-    beforeEventBuffer: true,
-    afterEventBuffer: true,
-    timeZone: true,
-    metadata: true,
-    slotInterval: true,
-    seatsPerTimeSlot: true,
-    users: {
-      select: {
-        avatar: true,
-        name: true,
-        username: true,
-        hideBranding: true,
-        plan: true,
-        timeZone: true,
-      },
-    },
-  });
-
   const users = await prisma.user.findMany({
     where: {
       username: {
@@ -159,7 +116,14 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
             },
           ],
         },
-        select: eventTypeSelect,
+        select: {
+          ...availiblityPageEventTypeSelect,
+          users: {
+            select: {
+              id: false,
+            },
+          },
+        },
       },
     },
   });
@@ -185,7 +149,14 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
           },
         ],
       },
-      select: eventTypeSelect,
+      select: {
+        ...availiblityPageEventTypeSelect,
+        users: {
+          select: {
+            id: false,
+          },
+        },
+      },
     });
     if (!eventTypeBackwardsCompat) {
       return {
