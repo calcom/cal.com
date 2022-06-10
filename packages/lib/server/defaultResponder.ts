1d0f7e17ef5d37f8b5c2127cc79acc5ae41818b7
zomars <zomars@me.com>
2022-06-09 20:50:33 -0600

    Adds getServerErrorFromUnkown

--- packages/lib/server/defaultResponder.ts
+++ packages/lib/server/defaultResponder.ts
@@ -1,11 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { performance } from "perf_hooks";
-import Stripe from "stripe";
-import { ZodError } from "zod";
-
-import { HttpError } from "@calcom/lib/http-error";
 
 import { perfObserver } from ".";
+import { getServerErrorFromUnkown } from "./getServerErrorFromUnkown";
 
 type Handle<T> = (req: NextApiRequest, res: NextApiResponse) => Promise<T>;
 
@@ -19,24 +16,9 @@ function defaultResponder<T>(f: Handle<T>) {
       const result = await f(req, res);
       res.json(result);
     } catch (err) {
-      if (err instanceof HttpError) {
-        res.statusCode = err.statusCode;
-        res.json({ message: err.message });
-      } else if (err instanceof Stripe.errors.StripeInvalidRequestError) {
-        console.error("err", err);
-        res.statusCode = err.statusCode || 500;
-        res.json({ message: "Stripe error: " + err.message });
-      } else if (err instanceof ZodError && err.name === "ZodError") {
-        console.error("err", JSON.parse(err.message)[0].message);
-        res.statusCode = 400;
-        res.json({
-          message: "Validation errors: " + err.issues.map((i) => `—'${i.path}' ${i.message}`).join(". "),
-        });
-      } else {
-        console.error("err", err);
-        res.statusCode = 500;
-        res.json({ message: "Unknown error" });
-      }
+      const error = getServerErrorFromUnkown(err);
+      res.statusCode = error.statusCode;
+      res.json({ message: error.message });
     } finally {
       performance.mark("End");
       performance.measure("Measuring endpoint: " + req.url, "Start", "End");
