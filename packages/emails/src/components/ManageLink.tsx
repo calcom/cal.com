ad4bcfafce7a25a11504991d282d7ee689945881
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2022-06-12 16:33:14 +0100

    Manage Link showing for organisers (#3037)

--- packages/emails/src/components/ManageLink.tsx
+++ packages/emails/src/components/ManageLink.tsx
@@ -4,27 +4,29 @@ import type { CalendarEvent, Person } from "@calcom/types/Calendar";
 export function ManageLink(props: { calEvent: CalendarEvent; attendee: Person }) {
   // Only the original attendee can make changes to the event
   // Guests cannot
-  if (props.attendee.email !== props.calEvent.attendees[0].email) return null;
   const t = props.attendee.language.translate;
-
-  return (
-    <div
-      style={{
-        fontFamily: "Roboto, Helvetica, sans-serif",
-        fontSize: "16px",
-        fontWeight: 500,
-        lineHeight: "0px",
-        textAlign: "left",
-        color: "#3e3e3e",
-      }}>
-      <p>
-        <>{t("need_to_reschedule_or_cancel")}</>
-      </p>
-      <p style={{ fontWeight: 400, lineHeight: "24px" }}>
-        <a href={getCancelLink(props.calEvent)} style={{ color: "#3e3e3e" }}>
-          <>{t("manage_this_event")}</>
-        </a>
-      </p>
-    </div>
-  );
+  if(props.attendee.email === props.calEvent.attendees[0].email || props.calEvent.organizer.email  === props.attendee.email ){
+    return (
+      <div
+        style={{
+          fontFamily: "Roboto, Helvetica, sans-serif",
+          fontSize: "16px",        fontWeight: 500,
+          lineHeight: "0px",
+          textAlign: "left",
+          color: "#3e3e3e",
+        }}>
+        <p>
+          <>{t("need_to_reschedule_or_cancel")}</>
+        </p>
+        <p style={{ fontWeight: 400, lineHeight: "24px" }}>
+          <a href={getCancelLink(props.calEvent)} style={{ color: "#3e3e3e" }}>
+            <>{t("manage_this_event")}</>
+          </a>
+        </p>
+      </div>
+    );
+  }
+  
+  // Dont have the rights to the manage link 
+  return null
 }
