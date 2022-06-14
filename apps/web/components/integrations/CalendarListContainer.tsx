64309868c00e00da944c84cccc697eadd6d8069d
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-06-14 11:31:22 -0400

    Expire payment sessions and cancel payment intent

--- apps/web/components/integrations/CalendarListContainer.tsx
+++ apps/web/components/integrations/CalendarListContainer.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect } from "react";
+import { Fragment } from "react";
 import { useMutation } from "react-query";
 
 import { InstallAppButton } from "@calcom/app-store/components";
