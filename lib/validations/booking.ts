6349491ae7d711a9134c66e4e17f92fab96cbe3f
Agusti Fernandez Pardo <git@agusti.me>
2022-06-15 21:10:19 +0200

    feat: admin endpoints for bookings

--- lib/validations/booking.ts
+++ lib/validations/booking.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 import { _BookingModel as Booking } from "@calcom/prisma/zod";
 
 const schemaBookingBaseBodyParams = Booking.pick({
-  uid: true,
   userId: true,
   eventTypeId: true,
   title: true,
