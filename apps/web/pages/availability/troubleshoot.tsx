34cae662f3b44754951e68dd2ccfd275204a4a5f
zomars <zomars@me.com>
2022-06-15 21:27:14 -0600

    Revert "Update troubleshoot.tsx"
    This reverts commit 5dcd4399010b1bf7dccec23cf30c068d97ee7dda.

--- apps/web/pages/availability/troubleshoot.tsx
+++ apps/web/pages/availability/troubleshoot.tsx
@@ -74,7 +74,7 @@ const AvailabilityView = ({ user }: { user: User }) => {
             <Loader />
           ) : availability.length > 0 ? (
             availability.map((slot) => (
-              <div key={slot.start.toString()} className="overflow-hidden rounded-sm bg-neutral-100">
+              <div key={slot.start} className="overflow-hidden rounded-sm bg-neutral-100">
                 <div className="px-4 py-5 text-black sm:p-6">
                   {t("calendar_shows_busy_between")}{" "}
                   <span className="font-medium text-neutral-800" title={slot.start}>
