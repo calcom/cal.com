05a99cdfe46c98fe7fd12198414de0c6ec32560c
zomars <zomars@me.com>
2022-06-17 08:24:06 -0600

    Update Badge.tsx

--- packages/ui/Badge.tsx
+++ packages/ui/Badge.tsx
@@ -11,19 +11,30 @@ export type BadgeProps = {
 export const Badge = function Badge(props: BadgeProps) {
   const { variant, className, size = "default", StartIcon, ...passThroughProps } = props;
 
+  const classNameBySize = {
+    default: !StartIcon ? "h-5" : "",
+    lg: !StartIcon ? "h-6" : "",
+  };
+
+  const classNameByVariant = {
+    default: "bg-orange-100 text-orange-800",
+    warning: "bg-orange-100 text-orange-800",
+    orange: "bg-orange-100 text-orange-800",
+    success: "bg-green-100 text-green-800",
+    green: "bg-green-100 text-green-800",
+    gray: "bg-gray-200 text-gray-800",
+    blue: "bg-blue-100 text-blue-800",
+    red: "bg-red-100 text-red-800",
+    error: "bg-red-100 text-red-800",
+  };
+
   return (
     <div
       {...passThroughProps}
       className={classNames(
         "inline-flex items-center justify-center rounded py-0.5 px-[6px] text-sm font-semibold",
-        size === "default" && !StartIcon && "h-5 ",
-        size === "lg" && !StartIcon && "h-6",
-        (variant === "default" || variant == "warning" || variant == "orange") &&
-          "bg-orange-100 text-orange-800",
-        (variant === "success" || variant === "green") && "bg-green-100 text-green-800",
-        variant === "gray" && "bg-brand-200 text-gray-800",
-        variant === "blue" && "bg-blue-100 text-blue-800",
-        (variant === "red" || variant === "error") && "bg-red-100 text-red-800",
+        classNameBySize[size],
+        classNameByVariant[variant],
         StartIcon ? "w-15" : "w-[49px] ",
         className
       )}>
