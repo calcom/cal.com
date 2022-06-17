94c2ae141e8d908232c1a6cf72db221c67231988
zomars <zomars@me.com>
2022-06-17 08:30:46 -0600

    Update Badge.tsx

--- packages/ui/Badge.tsx
+++ packages/ui/Badge.tsx
@@ -2,38 +2,38 @@ import { Icon } from "react-feather";
 
 import classNames from "@calcom/lib/classNames";
 
+const classNameByVariant = {
+  default: "bg-orange-100 text-orange-800",
+  warning: "bg-orange-100 text-orange-800",
+  orange: "bg-orange-100 text-orange-800",
+  success: "bg-green-100 text-green-800",
+  green: "bg-green-100 text-green-800",
+  gray: "bg-gray-200 text-gray-800",
+  blue: "bg-blue-100 text-blue-800",
+  red: "bg-red-100 text-red-800",
+  error: "bg-red-100 text-red-800",
+};
+
+const classNameBySize = {
+  default: "h-5",
+  lg: "h-6",
+};
+
 export type BadgeProps = {
-  variant: "default" | "success" | "gray" | "blue" | "green" | "orange" | "red" | "warning" | "error";
-  size?: "default" | "lg";
+  variant: keyof typeof classNameByVariant;
+  size?: keyof typeof classNameBySize;
   StartIcon?: Icon;
 } & JSX.IntrinsicElements["div"];
 
 export const Badge = function Badge(props: BadgeProps) {
   const { variant, className, size = "default", StartIcon, ...passThroughProps } = props;
 
-  const classNameBySize = {
-    default: !StartIcon ? "h-5" : "",
-    lg: !StartIcon ? "h-6" : "",
-  };
-
-  const classNameByVariant = {
-    default: "bg-orange-100 text-orange-800",
-    warning: "bg-orange-100 text-orange-800",
-    orange: "bg-orange-100 text-orange-800",
-    success: "bg-green-100 text-green-800",
-    green: "bg-green-100 text-green-800",
-    gray: "bg-gray-200 text-gray-800",
-    blue: "bg-blue-100 text-blue-800",
-    red: "bg-red-100 text-red-800",
-    error: "bg-red-100 text-red-800",
-  };
-
   return (
     <div
       {...passThroughProps}
       className={classNames(
         "inline-flex items-center justify-center rounded py-0.5 px-[6px] text-sm font-semibold",
-        classNameBySize[size],
+        !StartIcon ? classNameBySize[size] : "",
         classNameByVariant[variant],
         StartIcon ? "w-15" : "w-[49px] ",
         className
