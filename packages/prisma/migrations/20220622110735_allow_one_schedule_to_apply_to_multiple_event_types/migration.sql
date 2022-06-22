0c3949e64eaabbb51d35e20b07f142aaaba3e330
Alex van Andel <me@alexvanandel.com>
2022-06-22 12:35:12 +0100

    Wrote migration query + change Schedule->EventType to EventType->Schedule

---
+++ packages/prisma/migrations/20220622110735_allow_one_schedule_to_apply_to_multiple_event_types/migration.sql
@@ -0,0 +1,29 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `eventTypeId` on the `Schedule` table. All the data in the column will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Schedule" DROP CONSTRAINT "Schedule_eventTypeId_fkey";
+
+-- DropIndex
+DROP INDEX "Schedule_eventTypeId_key";
+
+-- AlterTable
+ALTER TABLE "EventType" ADD COLUMN     "scheduleId" INTEGER;
+
+UPDATE "EventType"
+SET 
+  "scheduleId" = subquery.id 
+FROM (
+  SELECT id, "eventTypeId" FROM "Schedule"
+) AS subquery
+WHERE
+  "EventType".id = subquery."eventTypeId";
+
+-- AlterTable
+ALTER TABLE "Schedule" DROP COLUMN "eventTypeId";
+
+-- AddForeignKey
+ALTER TABLE "EventType" ADD CONSTRAINT "EventType_scheduleId_fkey" FOREIGN KEY ("scheduleId") REFERENCES "Schedule"("id") ON DELETE SET NULL ON UPDATE CASCADE;
