c3996221e17e582b3ab532309a46e0510821e687
Agusti Fernandez Pardo <git@agusti.me>
2022-06-23 19:16:12 +0200

    fix: don't require apiKey if already using customPrisma with calcom license key

--- lib/helpers/customPrisma.ts
+++ lib/helpers/customPrisma.ts
@@ -4,7 +4,6 @@ import { NextMiddleware } from "next-api-middleware";
 
 import { PRISMA_CLIENT_CACHING_TIME } from "@calcom/api/lib/constants";
 // import prismaAdmin from "@calcom/console/modules/common/utils/prisma";
-import { asStringOrUndefined } from "@calcom/lib/asStringOrNull";
 import { CONSOLE_URL } from "@calcom/lib/constants";
 import { prisma, customPrisma } from "@calcom/prisma";
 
@@ -18,14 +17,9 @@ export const customPrismaClient: NextMiddleware = async (req, res, next) => {
     req.prisma = prisma;
     await next();
   } else {
-    const id = asStringOrUndefined(key);
-
-    // If we have a key, we check if it is valid.
-    // const deployment = await prismaAdmin.deployment.findUnique({
-    //   where: { key },
-    // });
+    // If we have a key, we check if the deployment matching the key, has a databaseUrl value set.
     const databaseUrl = await fetch(
-      `${process.env.NEXT_PUBLIC_CONSOLE_URL || CONSOLE_URL}/api/deployments/database?key=${id}`
+      `${process.env.NEXT_PUBLIC_CONSOLE_URL || CONSOLE_URL}/api/deployments/database?key=${key}`
     )
       .then((res) => res.json())
       .then((res) => res.databaseUrl);
@@ -34,10 +28,11 @@ export const customPrismaClient: NextMiddleware = async (req, res, next) => {
       res.status(400).json({ error: "no databaseUrl set up at your instance yet" });
       return;
     }
+    // FIXME: Add some checks for the databaseUrl to make sure it is valid. (e.g. not a localhost)
     const hashedUrl = await hash(databaseUrl, 12);
 
     const cachedPrisma = cache.get(hashedUrl);
-
+    /* We cache each cusotm prisma client for 24h to avoid too many requests to the database. */
     if (!cachedPrisma) {
       cache.put(
         hashedUrl,
@@ -46,6 +41,13 @@ export const customPrismaClient: NextMiddleware = async (req, res, next) => {
       );
     }
     req.prisma = customPrisma({ datasources: { db: { url: databaseUrl } } });
+    /* @note:
+    In order to skip verifyApiKey for customPrisma requests, 
+    we pass isAdmin true, and userId 0, if we detect them later, 
+    we skip verifyApiKey logic and pass onto next middleware instead.
+     */
+    req.isAdmin = true;
+    req.userId = 0;
   }
   await next();
 };
