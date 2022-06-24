c92446ee6716e4c3a22ca37517ace1616fc93819
Alan <alannnc@gmail.com>
2022-06-24 15:27:56 -0600

    WIP stripe customer metedada sync

--- apps/web/server/routers/viewer.tsx
+++ apps/web/server/routers/viewer.tsx
@@ -11,9 +11,11 @@ import { sendFeedbackEmail } from "@calcom/emails";
 import { sendCancelledEmails } from "@calcom/emails";
 import { parseRecurringEvent, isPrismaObjOrUndefined } from "@calcom/lib";
 import { baseEventTypeSelect, bookingMinimalSelect } from "@calcom/prisma";
+import stripe from "@calcom/stripe/server";
 import { closePayments } from "@ee/lib/stripe/server";
 
 import { checkRegularUsername } from "@lib/core/checkRegularUsername";
+import hasKeyInMetadata from "@lib/hasKeyInMetadata";
 import jackson from "@lib/jackson";
 import prisma from "@lib/prisma";
 import { isTeamOwner } from "@lib/queries/teams";
@@ -701,12 +703,30 @@ const loggedInViewerRouter = createProtectedRouter()
         data.avatar = await resizeBase64Image(input.avatar);
       }
 
-      await prisma.user.update({
+      const updatedUser = await prisma.user.update({
         where: {
           id: user.id,
         },
         data,
+        select: {
+          id: true,
+          username: true,
+          email: true,
+          metadata: true,
+        },
       });
+
+      // Notify stripe about the change
+      if (updatedUser && updatedUser.metadata && hasKeyInMetadata(updatedUser, "stripeCustomerId")) {
+        const stripeCustomerId = `${updatedUser.metadata.stripeCustomerId}`;
+        await stripe.customers.update(stripeCustomerId, {
+          metadata: {
+            username: updatedUser.username,
+            email: updatedUser.email,
+            userId: updatedUser.id,
+          },
+        });
+      }
     },
   })
   .mutation("eventTypeOrder", {
