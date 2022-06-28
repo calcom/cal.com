d4706d679fb2e6c19e591af9c80ecfce89e4e6f2
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2022-06-28 13:11:00 +0100

    error && "text-red-800"

--- apps/storybook/stories/Checkbox.stories.tsx
+++ apps/storybook/stories/Checkbox.stories.tsx
@@ -9,13 +9,23 @@ export default {
 
 export const All = () => (
   <div className="flex flex-col space-y-2">
-    <Checkbox description="Default"></Checkbox>
-    <Checkbox description="Error" error></Checkbox>
-    <Checkbox description="Disabled" disabled></Checkbox>
-    <Checkbox description="Disabled" disabled checked></Checkbox>
+    <Checkbox label="Default" description="Toggle on and off something"></Checkbox>
+    <Checkbox label="Error" description="Toggle on and off something" error></Checkbox>
+    <Checkbox label="Disabled" description="Toggle on and off something" disabled></Checkbox>
+    <Checkbox label="Disabled Checked" description="Toggle on and off something" checked disabled></Checkbox>
+    <hr />
+    <Checkbox description="Default" descriptionAsLabel></Checkbox>
+    <Checkbox description="Error" descriptionAsLabel error></Checkbox>
+    <Checkbox description="Disabled" descriptionAsLabel disabled></Checkbox>
+    <Checkbox description="Disabled" descriptionAsLabel disabled checked></Checkbox>
   </div>
 );
 
-export const CheckboxField = () => <Checkbox description=" Default" />;
-export const CheckboxError = () => <Checkbox description=" Error" error />;
-export const CheckboxDisabled = () => <Checkbox description=" Disabled" disabled />;
+export const CheckboxField = () => <Checkbox description="Default" descriptionAsLabel></Checkbox>;
+export const CheckboxError = () => <Checkbox description="Error" descriptionAsLabel></Checkbox>;
+export const CheckboxDisabled = () => (
+  <>
+    <Checkbox description="Disabled" descriptionAsLabel disabled></Checkbox>
+    <Checkbox description="Disabled" descriptionAsLabel disabled checked></Checkbox>
+  </>
+);
--- packages/ui/form/Checkbox.tsx
+++ packages/ui/form/Checkbox.tsx
@@ -6,52 +6,61 @@ type Props = InputHTMLAttributes<HTMLInputElement> & {
   label?: React.ReactNode;
   description: string;
   descriptionAsLabel?: boolean;
+  informationIconText?: string;
   error?: boolean;
 };
 
 const CheckboxField = forwardRef<HTMLInputElement, Props>(
-  ({ label, description, descriptionAsLabel, error, ...rest }, ref) => {
+  ({ label, description, error, informationIconText, disabled, ...rest }, ref) => {
+    const descriptionAsLabel = !label || rest.descriptionAsLabel;
     return (
       <div className="block items-center sm:flex">
-        {label && !descriptionAsLabel && (
+        {label && (
           <div className="min-w-48 mb-4 sm:mb-0">
-            <label htmlFor={rest.id} className="flex text-sm font-medium text-neutral-700">
-              {label}
-            </label>
-          </div>
-        )}
-        {label && descriptionAsLabel && (
-          <div className="min-w-48 mb-4 sm:mb-0">
-            <span className="flex text-sm font-medium text-neutral-700">{label}</span>
+            {React.createElement(
+              descriptionAsLabel ? "div" : "label",
+              {
+                className: classNames("flex text-sm font-medium text-gray-900"),
+                ...(!descriptionAsLabel
+                  ? {
+                      htmlFor: rest.id,
+                    }
+                  : {}),
+              },
+              label
+            )}
           </div>
         )}
         <div className="w-full">
           <div className="relative flex items-start">
-            <div className="flex h-5 items-center">
-              <input
-                {...rest}
-                disabled={rest.disabled}
-                ref={ref}
-                type="checkbox"
-                className={classNames(
-                  " h-4 w-4 rounded focus:ring-1",
-                  !error
-                    ? "text-primary-600 focus:ring-primary-500 border-gray-300 hover:bg-gray-100 "
-                    : "border-red-800 text-red-800 checked:bg-red-800 focus:ring-red-800",
-                  rest.disabled &&
-                    "border-2 bg-white opacity-30 checked:bg-gray-900 checked:hover:bg-gray-900"
-                )}
-              />
-            </div>
-            <div className="text-sm ltr:ml-3 rtl:mr-3">
-              {!label || descriptionAsLabel ? (
-                <label htmlFor={rest.id} className="ml-2 text-neutral-700">
-                  {description}
-                </label>
-              ) : (
-                <p className="text-neutral-900">{description}</p>
-              )}
-            </div>
+            {React.createElement(
+              descriptionAsLabel ? "label" : "div",
+              {
+                className: classNames(
+                  "relative flex items-start",
+                  !error && descriptionAsLabel ? "text-gray-900" : "text-neutral-900",
+                  error && "text-red-800"
+                ),
+              },
+              <>
+                <div className="flex h-5 items-center">
+                  <input
+                    {...rest}
+                    ref={ref}
+                    type="checkbox"
+                    className={classNames(
+                      "text-primary-600 focus:ring-primary-500 mr-2 h-4 w-4 rounded border-gray-300 ",
+                      !error && disabled
+                        ? "bg-gray-300 checked:bg-gray-300"
+                        : "checked:bg-gray-800 hover:bg-gray-100",
+                      error && "border-red-800 checked:bg-red-800 hover:bg-red-400"
+                    )}
+                  />
+                </div>
+                <span className="text-sm ltr:ml-3 rtl:mr-3">{description}</span>
+              </>
+            )}
+            {/* {informationIconText && <InfoBadge content={informationIconText}></InfoBadge>} */}
           </div>
         </div>
       </div>
