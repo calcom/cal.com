ae18091d384f4f912ecb345957364d736d0c15bc
CarinaWolli <wollencarina@gmail.com>
2022-06-28 11:17:48 +0200

    fix text for sms reminder template

--- apps/web/lib/reminders/templates/smsReminderTemplate.ts
+++ apps/web/lib/reminders/templates/smsReminderTemplate.ts
@@ -8,14 +8,14 @@ const smsReminderTemplate = (
   name?: string
 ) => {
   const templateOne = `Hi${name ? ` ${name}` : ``}, this is a reminder that your meeting (${eventName}) with
-  ${attendee} at ${dayjs(startTime).format("YYYY MMM D h:mmA")} ${attendeeTimeZone}`;
+  ${attendee} is at ${dayjs(startTime).format("YYYY MMM D h:mmA")} ${attendeeTimeZone}`;
 
   //Twilio recomments message to be no longer than 320 characters
   if (templateOne.length <= 320) return templateOne;
 
-  const templateTwo = `Hi, this is a reminder that your meeting with ${attendee} at ${dayjs(startTime).format(
-    "MMM D h:mmA"
-  )}`;
+  const templateTwo = `Hi, this is a reminder that your meeting with ${attendee} is at ${dayjs(
+    startTime
+  ).format("MMM D h:mmA")}`;
 
   //Twilio supports up to 1600 characters
   if (templateTwo.length <= 1600) return templateTwo;
