4e2bd790fdc8fdb8f63149b5b4a49baf0cf8cc41
PhuongKieu <phuongkhn@zigvy.com>
2022-06-30 10:47:00 +0700

    fix bugs

--- pages/api/amili/availability/[userId].ts
+++ pages/api/amili/availability/[userId].ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextApiRequest, NextApiResponse } from "next";
