a6bc82065d1ec173df2e763c91c0d78cdc01c089
zomars <zomars@me.com>
2022-06-29 19:53:35 -0600

    Moved user page

--- apps/web/pages/[user]/index.tsx
+++ apps/web/pages/[user]/index.tsx
@@ -39,7 +39,7 @@ import { ssrInit } from "@server/lib/ssr";
 
 const EventTypeDescription = dynamic(() => import("@components/eventtype/EventTypeDescription"));
 const HeadSeo = dynamic(() => import("@components/seo/head-seo"));
-const CryptoSection = dynamic(() => import("../ee/components/web3/CryptoSection"));
+const CryptoSection = dynamic(() => import("../../ee/components/web3/CryptoSection"));
 
 interface EvtsToVerify {
   [evtId: string]: boolean;
