308c192912a2a774d572240ed4865bafacda9ee5
zomars <zomars@me.com>
2022-07-08 11:21:40 -0600

    Splits booking/[id] endpoint
    # Conflicts:
    #	pages/api/bookings/[id].ts

---
+++ pages/api/bookings/[id]/_auth-middleware.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest } from "next";
+
+import { HttpError } from "@calcom/lib/http-error";
+import { defaultResponder } from "@calcom/lib/server";
+
+import { schemaQueryIdParseInt } from "@lib/validations/shared/queryIdTransformParseInt";
+
+export async function authMiddleware(req: NextApiRequest) {
+  const { userId, prisma, isAdmin, query } = req;
+  const { id } = schemaQueryIdParseInt.parse(query);
+  const userWithBookings = await prisma.user.findUnique({
+    where: { id: userId },
+    include: { bookings: true },
+  });
+
+  if (!userWithBookings) throw new HttpError({ statusCode: 404, message: "User not found" });
+
+  const userBookingIds = userWithBookings.bookings.map((booking) => booking.id);
+
+  if (!isAdmin && !userBookingIds.includes(id)) {
+    throw new HttpError({ statusCode: 401, message: "You are not authorized" });
+  }
+}
+
+export default defaultResponder(authMiddleware);
---
+++ pages/api/bookings/[id]/_get.ts
@@ -0,0 +1,38 @@
+import type { NextApiRequest } from "next";
+
+import { defaultResponder } from "@calcom/lib/server";
+
+import { schemaBookingReadPublic } from "@lib/validations/booking";
+import { schemaQueryIdParseInt } from "@lib/validations/shared/queryIdTransformParseInt";
+
+/**
+ * @swagger
+ * /bookings/{id}:
+ *   get:
+ *     summary: Find a booking
+ *     operationId: getBookingById
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: ID of the booking to get
+ *     tags:
+ *     - bookings
+ *     responses:
+ *       200:
+ *         description: OK
+ *       401:
+ *        description: Authorization information is missing or invalid.
+ *       404:
+ *         description: Booking was not found
+ */
+export async function getHandler(req: NextApiRequest) {
+  const { prisma, query } = req;
+  const { id } = schemaQueryIdParseInt.parse(query);
+  const booking = await prisma.booking.findUnique({ where: { id } });
+  return { booking: schemaBookingReadPublic.parse(booking) };
+}
+
+export default defaultResponder(getHandler);
