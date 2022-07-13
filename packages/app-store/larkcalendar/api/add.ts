584918d29473c908a8071479f510f1090f0bd0de
zomars <zomars@me.com>
2022-07-13 16:46:29 -0600

    Refactors add endpoint

--- packages/app-store/larkcalendar/api/add.ts
+++ packages/app-store/larkcalendar/api/add.ts
@@ -1,47 +1,50 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest } from "next";
 import { stringify } from "querystring";
+import { z } from "zod";
 
 import { WEBAPP_URL } from "@calcom/lib/constants";
+import { defaultHandler, defaultResponder } from "@calcom/lib/server";
 
 import { encodeOAuthState } from "../../_utils/encodeOAuthState";
 import getAppKeysFromSlug from "../../_utils/getAppKeysFromSlug";
 import { LARK_HOST } from "../common";
 
-let app_id = "";
-let app_secret = "";
+const larkKeysSchema = z.object({
+  app_id: z.string(),
+  app_secret: z.string(),
+});
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "GET") {
-    const appKeys = await getAppKeysFromSlug("lark-calendar");
-    if (typeof appKeys.app_id === "string") app_id = appKeys.app_id;
-    if (!app_id) return res.status(400).json({ message: "lark app_id missing." });
-    if (typeof appKeys.app_secret === "string") app_secret = appKeys.app_secret;
-    if (!app_secret) return res.status(400).json({ message: "lark app_secret missing." });
+async function getHandler(req: NextApiRequest) {
+  const appKeys = await getAppKeysFromSlug("lark-calendar");
+  const { app_secret, app_id } = larkKeysSchema.parse(appKeys);
 
-    const state = encodeOAuthState(req);
+  const state = encodeOAuthState(req);
 
-    const params = {
+  const params = {
+    app_id,
+    redirect_uri: WEBAPP_URL + "/api/integrations/larkcalendar/callback",
+    state,
+  };
+
+  const query = stringify(params);
+
+  const url = `https://${LARK_HOST}/open-apis/authen/v1/index?${query}`;
+
+  // trigger app_ticket_immediately
+  fetch(`https://${LARK_HOST}/open-apis/auth/v3/app_ticket/resend`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify({
       app_id,
-      redirect_uri: WEBAPP_URL + "/api/integrations/larkcalendar/callback",
-      state,
-    };
+      app_secret,
+    }),
+  });
 
-    const query = stringify(params);
-
-    const url = `https://${LARK_HOST}/open-apis/authen/v1/index?${query}`;
-
-    // trigger app_ticket_immediately
-    fetch(`https://${LARK_HOST}/open-apis/auth/v3/app_ticket/resend`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json; charset=utf-8",
-      },
-      body: JSON.stringify({
-        app_id,
-        app_secret,
-      }),
-    });
-
-    res.status(200).json({ url });
-  }
+  return { url };
 }
+
+export default defaultHandler({
+  GET: Promise.resolve({ default: defaultResponder(getHandler) }),
+});
