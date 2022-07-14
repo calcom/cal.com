3f75658766b4ced62661fd7743a4e7b56f27c051
CarinaWolli <wollencarina@gmail.com>
2022-07-14 15:04:00 -0400

    fix timezone in email subject

--- apps/web/ee/lib/workflows/reminders/templates/emailReminderTemplate.ts
+++ apps/web/ee/lib/workflows/reminders/templates/emailReminderTemplate.ts
@@ -7,7 +7,9 @@ const emailReminderTemplate = (
   attendee: string,
   name: string
 ) => {
-  const templateSubject = `Reminder: ${eventName} at ${dayjs(startTime).format("YYYY MMM D h:mmA")}`;
+  const templateSubject = `Reminder: ${eventName} at ${dayjs(startTime)
+    .tz(attendeeTimeZone)
+    .format("YYYY MMM D h:mmA")}`;
 
   const templateBody = `Hi ${name},\n\nThis is a reminder that your meeting (${eventName}) with ${attendee} is on ${dayjs(
     startTime
