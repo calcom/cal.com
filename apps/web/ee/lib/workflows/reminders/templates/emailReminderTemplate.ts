53f81a17d4836f2a3762dfe2a3e4a7518ec63f22
CarinaWolli <wollencarina@gmail.com>
2022-07-14 15:02:48 -0400

    fix time zone in reminder template

--- apps/web/ee/lib/workflows/reminders/templates/emailReminderTemplate.ts
+++ apps/web/ee/lib/workflows/reminders/templates/emailReminderTemplate.ts
@@ -9,9 +9,11 @@ const emailReminderTemplate = (
 ) => {
   const templateSubject = `Reminder: ${eventName} at ${dayjs(startTime).format("YYYY MMM D h:mmA")}`;
 
-  const templateBody = `Hi ${name},\n\n this is a reminder that your meeting (${eventName}) with ${attendee} is on ${dayjs(
+  const templateBody = `Hi ${name},\n\nThis is a reminder that your meeting (${eventName}) with ${attendee} is on ${dayjs(
     startTime
-  ).format("YYYY MMM D")} at ${dayjs(startTime).format("h:mmA")} ${attendeeTimeZone}.`;
+  )
+    .tz(attendeeTimeZone)
+    .format("YYYY MMM D")} at ${dayjs(startTime).tz(attendeeTimeZone).format("h:mmA")} ${attendeeTimeZone}.`;
 
   const emailContent = { subject: templateSubject, body: templateBody };
 
--- apps/web/ee/lib/workflows/reminders/templates/smsReminderTemplate.ts
+++ apps/web/ee/lib/workflows/reminders/templates/smsReminderTemplate.ts
@@ -8,16 +8,16 @@ const smsReminderTemplate = (
   name?: string
 ) => {
   const templateOne = `Hi${name ? ` ${name}` : ``}, this is a reminder that your meeting (${eventName}) with
-  ${attendee} is on ${dayjs(startTime).format("MMM D")} at ${dayjs(startTime).format(
-    "h:mmA"
-  )} ${attendeeTimeZone}`;
+  ${attendee} is on ${dayjs(startTime).tz(attendeeTimeZone).format("MMM D")} at ${dayjs(attendeeTimeZone)
+    .tz()
+    .format("h:mmA")} ${attendeeTimeZone}`;
 
   //Twilio recomments message to be no longer than 320 characters
   if (templateOne.length <= 320) return templateOne;
 
-  const templateTwo = `Hi, this is a reminder that your meeting with ${attendee} is on ${dayjs(
-    startTime
-  ).format("MMM D")} at ${dayjs(startTime).format("h:mmA")}`;
+  const templateTwo = `Hi, this is a reminder that your meeting with ${attendee} is on ${dayjs(startTime)
+    .tz(attendeeTimeZone)
+    .format("MMM D")} at ${dayjs(startTime).tz(attendeeTimeZone).format("h:mmA")} ${attendeeTimeZone}`;
 
   //Twilio supports up to 1600 characters
   if (templateTwo.length <= 1600) return templateTwo;
