df3e3e8237fb664750dfa0b2f55723e8e0de6fed
Omar López <zomars@me.com>
2022-07-18 12:59:51 -0600

    Disables batching for i18n query (#3181)
    * Disables batching for i18n query
    
    * Makes i18n batch skip explicit

--- apps/web/pages/settings/profile.tsx
+++ apps/web/pages/settings/profile.tsx
@@ -501,7 +501,11 @@ function SettingsView(props: ComponentProps<typeof Settings> & { localeProp: str
   );
 }
 
-const WithQuery = withQuery(["viewer.public.i18n"]);
+/**
+ * i18n should never be clubbed with other queries, so that it's caching can be managed independently.
+ * We intend to not cache i18n query
+ **/
+const WithQuery = withQuery(["viewer.public.i18n"], { context: { skipBatch: true } });
 
 export default function Settings(props: Props) {
   const { t } = useLocale();
