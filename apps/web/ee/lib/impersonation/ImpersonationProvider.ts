245d7c731cf95a992566a4ff732d45f7c16986bd
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2022-07-20 16:40:29 +0100

    Update role selection
    Co-authored-by: Omar López <zomars@me.com>

--- apps/web/ee/lib/impersonation/ImpersonationProvider.ts
+++ apps/web/ee/lib/impersonation/ImpersonationProvider.ts
@@ -33,7 +33,7 @@ const ImpersonationProvider = CredentialsProvider({
             AND: [
               {
                 role: {
-                  not: "MEMBER",
+                  in: ["ADMIN", "OWNER"],
                 },
               },
               {
