d84aca9c6a695e634e305166411db2365e839247
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-07-25 13:43:41 -0400

    Add referral page sections

--- apps/web/pages/refer-a-friend.tsx
+++ apps/web/pages/refer-a-friend.tsx
@@ -16,6 +16,7 @@ const ReferAFriend = () => {
   const { data: referral, isLoading } = trpc.useQuery(["viewer.referrals"]);
 
   const [referralLink, setReferralLink] = useState<string>();
+  const [emails, setEmails] = useState<string>();
 
   useEffect(() => {
     if (referral) {
@@ -39,6 +40,24 @@ const ReferAFriend = () => {
             <div className="block sm:flex">
               <div className="mb-6 w-full sm:w-1/2">
                 <div>
+                  <Label htmlFor="username">Share via email</Label>
+                </div>
+                <div className="mt-1 flex rounded-md">
+                  <input
+                    value={emails}
+                    className="mt-1 mr-2 block w-full rounded-sm border-gray-300 p-1 focus:border-neutral-800 focus:ring-neutral-800 sm:text-sm"
+                    placeholder="Email addresses"
+                  />
+                  <Button className="mt-1">Send</Button>
+                </div>
+                <p className="mt-2 text-sm text-gray-500" id="email-description">
+                  Separate multiple emails with commas
+                </p>
+              </div>
+            </div>
+            <div className="block sm:flex">
+              <div className="mb-6 w-full sm:w-1/2">
+                <div>
                   <Label htmlFor="username">Share referral code</Label>
                 </div>
                 <div className="mt-1 flex rounded-md">
@@ -51,6 +70,20 @@ const ReferAFriend = () => {
                 </div>
               </div>
             </div>
+
+            <hr className="h-2 border-neutral-200" />
+
+            <h1 className="font-cal mb-1 text-xl font-bold capitalize tracking-wide text-gray-900">
+              Track your referrals
+            </h1>
+            <p className="text-sm text-neutral-500 ltr:mr-4 rtl:ml-4">
+              See how many friends have signed up with your referral
+            </p>
+
+            <hr className="h-2 border-neutral-200" />
+
+            <h3 className="font-cal mb-1 font-bold capitalize tracking-wide text-gray-900">Referral perks</h3>
+            <p className="text-sm text-neutral-500 ltr:mr-4 rtl:ml-4">Check out your referral perks</p>
           </>
         ) : (
           <Loader />
