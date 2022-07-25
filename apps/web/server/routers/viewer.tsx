e6522823730700090961069073b534625497b48c
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-07-25 12:55:25 -0400

    Generate referral pin

--- apps/web/server/routers/viewer.tsx
+++ apps/web/server/routers/viewer.tsx
@@ -1242,6 +1242,36 @@ const loggedInViewerRouter = createProtectedRouter()
         },
       });
     },
+  })
+  .query("referrals", {
+    async resolve({ ctx }) {
+      let referral = await prisma.user.findFirst({
+        where: {
+          id: ctx.user.id,
+        },
+        select: {
+          username: true,
+          referralPin: true,
+        },
+      });
+
+      if (!referral.referralPin) {
+        const referralPin = Math.floor(1000 + Math.random() * 9000);
+        await prisma.user.update({
+          where: {
+            id: ctx.user.id,
+          },
+          data: {
+            referralPin: referralPin,
+          },
+        });
+        referral = { ...referral, referralPin: referralPin };
+      }
+
+      console.log("🚀 ~ file: viewer.tsx ~ line 1256 ~ resolve ~ referral", referral);
+
+      return referral;
+    },
   });
 
 export const viewerRouter = createRouter()
