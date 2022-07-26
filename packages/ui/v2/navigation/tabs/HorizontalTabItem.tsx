1b6cb6bdceabc0eaba46dacaa46440ff727df20e
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2022-07-26 10:41:26 +0100

    Horizontal Tabs

--- packages/ui/v2/navigation/tabs/HorizontalTabItem.tsx
+++ packages/ui/v2/navigation/tabs/HorizontalTabItem.tsx
@@ -1,7 +1,7 @@
 import noop from "lodash/noop";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { FC, Fragment, MouseEventHandler } from "react";
+import { MouseEventHandler } from "react";
 
 import classNames from "@calcom/lib/classNames";
 
@@ -21,7 +21,7 @@ export type HorizontalTabItemProps = {
     }
 );
 
-const HorizontalTabItem: FC<HorizontalTabItemProps> = ({ name, href, tabName, ...props }) => {
+const HorizontalTabItem = ({ name, href, tabName, ...props }: HorizontalTabItemProps) => {
   const router = useRouter();
   let newHref = "";
   let isCurrent;
@@ -46,19 +46,18 @@ const HorizontalTabItem: FC<HorizontalTabItemProps> = ({ name, href, tabName, ..
     : noop;
 
   return (
-    <Fragment key={name}>
-      <Link key={name} href={props.disabled ? "#" : newHref}>
-        <a
-          onClick={onClick}
-          className={classNames(
-            isCurrent ? "bg-gray-200 text-gray-900" : " text-gray-600 hover:bg-gray-100 hover:text-gray-900",
-            "text-sm font-medium leading-4",
-            props.disabled && "pointer-events-none !opacity-30"
-          )}
-          aria-current={isCurrent ? "page" : undefined}
-        />
-      </Link>
-    </Fragment>
+    <Link key={name} href={props.disabled ? "#" : newHref}>
+      <a
+        onClick={onClick}
+        className={classNames(
+          isCurrent ? "bg-gray-200 text-gray-900" : " text-gray-600 hover:bg-gray-100 hover:text-gray-900",
+          "rounded-r-md py-[10px] px-4 text-sm font-medium leading-4",
+          props.disabled && "pointer-events-none !opacity-30"
+        )}
+        aria-current={isCurrent ? "page" : undefined}>
+        {name}
+      </a>
+    </Link>
   );
 };
 
--- packages/ui/v2/navigation/tabs/index.tsx
+++ packages/ui/v2/navigation/tabs/index.tsx
@@ -1,4 +1,4 @@
 export { default as VerticalTabItem } from "./VerticalTabItem";
 export { default as VerticalTabs } from "./VerticalTabs";
 export { default as HorizontalTabItem } from "./HorizontalTabItem";
-export { default as HorizontalTabs } from "./HorizontalTabItem";
+export { default as HorizontalTabs } from "./HorizontalTabs";
