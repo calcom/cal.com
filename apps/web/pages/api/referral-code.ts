03bf0e7cca29ee1051e3732d40661d82b59edd07
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-08-01 13:31:47 -0400

    Validate referral code

--- apps/web/pages/api/referral-code.ts
+++ apps/web/pages/api/referral-code.ts
@@ -4,9 +4,11 @@ import prisma from "@calcom/prisma";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
   const { referralCode } = req.body;
-  console.log("🚀 ~ file: referral-code.ts ~ line 7 ~ handler ~ referralCode", referralCode);
+
+  // Minimum length of a referral code is 5 characters
+  if (referralCode.length < 5) return res.status(418).json({ valid: false });
+
   const username = referralCode.slice(0, -4);
-  console.log("🚀 ~ file: referral-code.ts ~ line 9 ~ handler ~ userName", username);
 
   const referralPinQuery = await prisma.user.findUnique({
     where: {
@@ -16,13 +18,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       referralPin: true,
     },
   });
-  console.log("🚀 ~ file: referral-code.ts ~ line 19 ~ handler ~ referralPinQuery", referralPinQuery);
 
-  return res.status(200).json({ valid: true });
+  if (!referralPinQuery) return res.status(418).json({ valid: false });
 
-  //   if (username + referralPinQuery.referralPin === referralCode) {
-  //     return res.status(200).json({ valid: true });
-  //   } else {
-  //     return res.status(418).json({ valid: false });
-  //   }
+  if (username + referralPinQuery.referralPin === referralCode) {
+    return res.status(200).json({ valid: true });
+  } else {
+    return res.status(418).json({ valid: false });
+  }
 }
