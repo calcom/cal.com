69a8a9d82b3ef2adb2c12911edec63cad7dc2da0
alishaz-polymath <alishahbaz7@gmail.com>
2022-08-04 12:45:14 +0530

    added missing div closing tag

--- apps/web/pages/[user].tsx
+++ apps/web/pages/[user].tsx
@@ -62,37 +62,38 @@ export default function User(props: inferSSRProps<typeof getServerSideProps>) {
           <p className="mx-auto max-w-md">{t("user_dynamic_booking_disabled") as string}</p>
         </div>
       </div>
-    ) : (
-      <ul className="space-y-3">
-        {eventTypes.map((type, index) => (
-          <li
-            key={index}
-            className="hover:border-brand group relative rounded-sm border border-neutral-200 bg-white hover:bg-gray-50 dark:border-neutral-700 dark:bg-gray-800 dark:hover:border-neutral-600">
-            <Icon.FiArrowRight className="absolute right-3 top-3 h-4 w-4 text-black opacity-0 transition-opacity group-hover:opacity-100 dark:text-white" />
-            <Link href={getUsernameSlugLink({ users: props.users, slug: type.slug })}>
-              <a className="flex justify-between px-6 py-4" data-testid="event-type-link">
-                <div className="flex-shrink">
-                  <h2 className="font-cal font-semibold text-neutral-900 dark:text-white">{type.title}</h2>
-                  <EventTypeDescription className="text-sm" eventType={type} />
-                </div>
-                <div className="mt-1 self-center">
-                  <AvatarGroup
-                    border="border-2 border-white"
-                    truncateAfter={4}
-                    className="flex flex-shrink-0"
-                    size={10}
-                    items={props.users.map((user) => ({
-                      alt: user.name || "",
-                      image: user.avatar || "",
-                    }))}
-                  />
-                </div>
-              </a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    );
+    </div>
+  ) : (
+    <ul className="space-y-3">
+      {eventTypes.map((type, index) => (
+        <li
+          key={index}
+          className="hover:border-brand group relative rounded-sm border border-neutral-200 bg-white hover:bg-gray-50 dark:border-neutral-700 dark:bg-gray-800 dark:hover:border-neutral-600">
+          <Icon.FiArrowRight className="absolute right-3 top-3 h-4 w-4 text-black opacity-0 transition-opacity group-hover:opacity-100 dark:text-white" />
+          <Link href={getUsernameSlugLink({ users: props.users, slug: type.slug })}>
+            <a className="flex justify-between px-6 py-4" data-testid="event-type-link">
+              <div className="flex-shrink">
+                <h2 className="font-cal font-semibold text-neutral-900 dark:text-white">{type.title}</h2>
+                <EventTypeDescription className="text-sm" eventType={type} />
+              </div>
+              <div className="mt-1 self-center">
+                <AvatarGroup
+                  border="border-2 border-white"
+                  truncateAfter={4}
+                  className="flex flex-shrink-0"
+                  size={10}
+                  items={props.users.map((user) => ({
+                    alt: user.name || "",
+                    image: user.avatar || "",
+                  }))}
+                />
+              </div>
+            </a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
   const isEmbed = useIsEmbed();
   const eventTypeListItemEmbedStyles = useEmbedStyles("eventTypeListItem");
   const shouldAlignCentrallyInEmbed = useEmbedNonStylesConfig("align") !== "left";
