56de8ac2bdc9a5e47ca6f2b186a489c390fc2534
Alan <alannnc@gmail.com>
2022-08-05 12:17:20 -0600

    Fix for request-reschedule for team events

--- apps/web/pages/api/book/request-reschedule.ts
+++ apps/web/pages/api/book/request-reschedule.ts
@@ -54,6 +54,7 @@ const findUserDataByUserId = async (userId: number) => {
       locale: true,
       credentials: true,
       destinationCalendar: true,
+      teams: true,
     },
   });
 };
@@ -67,10 +68,10 @@ const handler = async (
     bookingId,
     rescheduleReason: cancellationReason,
   }: { bookingId: string; rescheduleReason: string; cancellationReason: string } = req.body;
-  let userOwner: Awaited<ReturnType<typeof findUserDataByUserId>>;
+  let user: Awaited<ReturnType<typeof findUserDataByUserId>>;
   try {
     if (session?.user?.id) {
-      userOwner = await findUserDataByUserId(session?.user.id);
+      user = await findUserDataByUserId(session?.user.id);
     } else {
       return res.status(501).end();
     }
@@ -84,6 +85,7 @@ const handler = async (
         startTime: true,
         endTime: true,
         eventTypeId: true,
+        eventType: true,
         location: true,
         attendees: true,
         references: true,
@@ -104,9 +106,18 @@ const handler = async (
       },
     });
 
-    if (bookingToReschedule.userId !== userOwner.id) throw new Error("UNAUTHORIZED");
+    const bookingBelongsToTeam = !!bookingToReschedule?.eventType?.teamId;
+    if (bookingBelongsToTeam && bookingToReschedule?.eventType?.teamId) {
+      const userTeamIds = user.teams.map((item) => item.teamId);
+      if (userTeamIds.indexOf(bookingToReschedule?.eventType?.teamId) === -1) {
+        throw new Error("UNAUTHORIZED: booking.belongs.to.team");
+      }
+    }
+    if (!bookingBelongsToTeam && bookingToReschedule.userId !== user.id) {
+      throw new Error("UNAUTHORIZED");
+    }
 
-    if (bookingToReschedule && userOwner) {
+    if (bookingToReschedule && user) {
       let event: Partial<EventType> = {};
       if (bookingToReschedule.eventTypeId) {
         event = await prisma.eventType.findFirst({
@@ -152,8 +163,8 @@ const handler = async (
         });
       };
 
-      const userOwnerTranslation = await getTranslation(userOwner.locale ?? "en", "common");
-      const [userOwnerAsPeopleType] = usersToPeopleType([userOwner], userOwnerTranslation);
+      const userTranslation = await getTranslation(user.locale ?? "en", "common");
+      const [userAsPeopleType] = usersToPeopleType([user], userTranslation);
 
       const builder = new CalendarEventBuilder();
       builder.init({
@@ -166,7 +177,7 @@ const handler = async (
           bookingToReschedule.attendees as unknown as PersonAttendeeCommonFields[],
           tAttendees
         ),
-        organizer: userOwnerAsPeopleType,
+        organizer: userAsPeopleType,
       });
 
       const director = new CalendarEventDirector();
@@ -182,7 +193,7 @@ const handler = async (
       // Handling calendar and videos cancellation
       // This can set previous time as available, until virtual calendar is done
       const credentialsMap = new Map();
-      userOwner.credentials.forEach((credential) => {
+      user.credentials.forEach((credential) => {
         credentialsMap.set(credential.type, credential);
       });
       const bookingRefsFiltered: BookingReference[] = bookingToReschedule.references.filter(
@@ -216,7 +227,7 @@ const handler = async (
         customInputs: isPrismaObjOrUndefined(bookingToReschedule.customInputs),
         startTime: bookingToReschedule?.startTime ? dayjs(bookingToReschedule.startTime).format() : "",
         endTime: bookingToReschedule?.endTime ? dayjs(bookingToReschedule.endTime).format() : "",
-        organizer: userOwnerAsPeopleType,
+        organizer: userAsPeopleType,
         attendees: usersToPeopleType(
           // username field doesn't exists on attendee but could be in the future
           bookingToReschedule.attendees as unknown as PersonAttendeeCommonFields[],
@@ -251,7 +262,9 @@ const handler = async (
 
     return res.status(200).json(bookingToReschedule);
   } catch (error) {
-    throw new Error("Error.request.reschedule");
+    if (error instanceof Error) {
+      throw new Error("Error.request.reschedule " + error?.message);
+    }
   }
 };
 
