3ed206df23cd7febe381c8f56b1984555a9d54db
zomars <zomars@me.com>
2022-08-05 18:08:05 -0600

    Squashed commit of the following:
    commit c378c647fe4822071cbdfc577fcfc950e53973df
    Author: zomars <zomars@me.com>
    Date:   Fri Aug 5 17:36:21 2022 -0600
    
        Cleanup
    
    commit a89cc7981df1c2aae96188466833490eb3b227a1
    Author: zomars <zomars@me.com>
    Date:   Fri Aug 5 17:17:57 2022 -0600
    
        redirect fixes
    
    commit a27979c2b3f06c9956ee9abfef1e82a5558b6411
    Author: zomars <zomars@me.com>
    Date:   Fri Aug 5 17:02:33 2022 -0600
    
        Fingers crossed
    
    commit c08a09e8d2ba3eb2561f418bb3b0370245f79bf7
    Author: zomars <zomars@me.com>
    Date:   Fri Aug 5 16:28:55 2022 -0600
    
        Update [...nextauth].tsx
    
    commit 4063bddb2ea3cbf3dda6be6cc10070ecc1eb578c
    Author: zomars <zomars@me.com>
    Date:   Fri Aug 5 16:05:21 2022 -0600
    
        Submodule sync
    
    commit e44dc73a3b751ba4cacc85abfe6680a668393c0d
    Author: zomars <zomars@me.com>
    Date:   Fri Aug 5 16:05:06 2022 -0600
    
        Upgrade next-auth
    
    commit 2b36e0a1b376083739ea06b793f1523e2f158a33
    Author: zomars <zomars@me.com>
    Date:   Fri Aug 5 15:23:03 2022 -0600
    
        Magic signup fixes
    
    commit 2cc81ca7e2f79a4e3fcab370e23ac29cbe3ee18d
    Author: Joe Au-Yeung <65426560+joeauyeung@users.noreply.github.com>
    Date:   Thu Aug 4 11:49:51 2022 -0400
    
        Properly parse list calendar call to MS Graph (#3698)
    
    commit 65887d8f9db8b399964fce38ec104ed24fcc48e6
    Author: Leo Giovanetti <hello@leog.me>
    Date:   Thu Aug 4 12:52:50 2022 -0300
    
        Fix app-store seeding (#3693) (#3697)
    
        Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    
        Co-authored-by: Hariom Balhara <hariombalhara@gmail.com>
        Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>
    
    commit 71594b962352519c4ed4ee6cf68d16aac9b71e26
    Author: Leo Giovanetti <hello@leog.me>
    Date:   Wed Aug 3 16:48:07 2022 -0300
    
        fixed dark mode profile page (#3650) (#3681)
    
        * fixed dark mode profile page
    
        * nit
    
        Co-authored-by: Peer Richelsen <peeroke@gmail.com>
    
    commit 78a5ae988274ff05f26309033821acc48e22823d
    Author: Leo Giovanetti <hello@leog.me>
    Date:   Wed Aug 3 14:05:02 2022 -0300
    
        Potential fix (#3671) (#3676)
    
    commit b006240b0c62e9e62944cb7184793f099358e2ec
    Merge: 6179b3fbe e7418f68a
    Author: zomars <zomars@me.com>
    Date:   Mon Aug 1 18:20:47 2022 -0600
    
        Merge branch 'main' into production
    
    commit 6179b3fbe06709f1cf3a4e54214a7dbb1543765e
    Author: zomars <zomars@me.com>
    Date:   Mon Aug 1 18:20:40 2022 -0600
    
        Fixes successRedirectUrl validation
    
    commit 14443099e0377e8d9af52f53f15c2f8b91b784d2
    Author: zomars <zomars@me.com>
    Date:   Mon Aug 1 17:59:47 2022 -0600
    
        successRedirectUrl fixes

--- apps/web/pages/api/auth/[...nextauth].tsx
+++ apps/web/pages/api/auth/[...nextauth].tsx
@@ -12,7 +12,7 @@ import path from "path";
 
 import checkLicense from "@calcom/features/ee/common/server/checkLicense";
 import ImpersonationProvider from "@calcom/features/ee/impersonation/lib/ImpersonationProvider";
-import { WEBAPP_URL, WEBSITE_URL } from "@calcom/lib/constants";
+import { WEBAPP_URL } from "@calcom/lib/constants";
 import { symmetricDecrypt } from "@calcom/lib/crypto";
 import { defaultCookies } from "@calcom/lib/default-cookies";
 import { serverConfig } from "@calcom/lib/serverConfig";
@@ -165,6 +165,11 @@ if (true) {
       maxAge: 10 * 60 * 60, // Magic links are valid for 10 min only
       // Here we setup the sendVerificationRequest that calls the email template with the identifier (email) and token to verify.
       sendVerificationRequest: ({ identifier, url }) => {
+        const originalUrl = new URL(url);
+        const webappUrl = new URL(WEBAPP_URL);
+        if (originalUrl.origin !== webappUrl.origin) {
+          url = url.replace(originalUrl.origin, webappUrl.origin);
+        }
         const emailFile = readFileSync(path.join(emailsDir, "confirm-email.html"), {
           encoding: "utf8",
         });
@@ -191,11 +196,12 @@ export default NextAuth({
   session: {
     strategy: "jwt",
   },
-  cookies: defaultCookies(WEBSITE_URL?.startsWith("https://")),
+  cookies: defaultCookies(WEBAPP_URL?.startsWith("https://")),
   pages: {
     signIn: "/auth/login",
     signOut: "/auth/logout",
     error: "/auth/error", // Error code passed in query string as ?error=
+    // verifyRequest: "/auth/verify-request",
     // newUser: "/auth/new", // New users will be directed here on first sign in (leave the property out if not of interest)
   },
   providers,
@@ -441,8 +447,8 @@ export default NextAuth({
     async redirect({ url, baseUrl }) {
       // Allows relative callback URLs
       if (url.startsWith("/")) return `${baseUrl}${url}`;
-      // Allows callback URLs on the same origin
-      else if (new URL(url).origin === new URL(baseUrl || WEBSITE_URL).origin) return url;
+      // Allows callback URLs on the same domain
+      else if (new URL(url).hostname === new URL(WEBAPP_URL).hostname) return url;
       return baseUrl;
     },
   },
