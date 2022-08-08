e9af0a0a0680859fccfb61ae8870b8c6f533a6a3
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2022-08-09 01:14:26 +0530

    fix: un comment icon

--- apps/web/components/booking/pages/BookingPage.tsx
+++ apps/web/components/booking/pages/BookingPage.tsx
@@ -608,7 +608,7 @@ const BookingPage = ({
                     {t("former_time")}
                   </p>
                   <p className="text-gray-500 line-through dark:text-white">
-                    {/* <Icon.FiCalendar className="mr-[10px] ml-[2px] -mt-1 inline-block h-4 w-4 text-gray-400" /> */}
+                    <Icon.FiCalendar className="mr-[10px] ml-[2px] -mt-1 inline-block h-4 w-4 text-gray-400" />
                     {typeof booking.startTime === "string" && parseDate(dayjs(booking.startTime), i18n)}
                   </p>
                 </div>
