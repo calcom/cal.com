6600db8e868aaecebab2a15a4659f1b09df6268a
Hariom Balhara <hariombalhara@gmail.com>
2022-08-08 10:25:28 +0530

    Fix Ping App

--- packages/app-store/ping/components/InstallAppButton.tsx
+++ packages/app-store/ping/components/InstallAppButton.tsx
@@ -1,9 +1,10 @@
 import type { InstallAppButtonProps } from "@calcom/app-store/types";
 
 import useAddAppMutation from "../../_utils/useAddAppMutation";
+import appConfig from "../config.json";
 
 export default function InstallAppButton(props: InstallAppButtonProps) {
-  const mutation = useAddAppMutation("ping_video");
+  const mutation = useAddAppMutation(appConfig.type);
 
   return (
     <>
