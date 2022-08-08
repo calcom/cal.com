09f0852f5d52c428607e5b433a90f47467f039d5
zomars <zomars@me.com>
2022-08-08 14:59:00 -0600

    Adds logic to let admins opt-in to V2

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,19 +1,16 @@
-import { getToken } from "next-auth/jwt";
 import { collectEvents } from "next-collect/server";
 // eslint-disable-next-line @next/next/no-server-import-in-page
 import { NextMiddleware, NextResponse } from "next/server";
 
 import { extendEventData, nextCollectBasicSettings } from "@calcom/lib/telemetry";
 
+const V2_WHITELIST = ["/settings/admin"];
+
 const middleware: NextMiddleware = async (req) => {
   const url = req.nextUrl;
-  const token = await getToken({ req });
 
-  /**
-   *  TODO: Add logic to only redirect certain pages if `v2` cookie is present.
-   * As of now, only admins can view the new v2 pages
-   **/
-  if (token?.role === "ADMIN" && url.pathname.startsWith(`/settings/admin`)) {
+  /** Display available V2 pages to users who opted-in to early access */
+  if (req.cookies.has("calcom-v2-early-access") && V2_WHITELIST.some((p) => url.pathname.startsWith(p))) {
     // rewrite to the current subdomain under the pages/sites folder
     url.pathname = `/v2${url.pathname}`;
   }
