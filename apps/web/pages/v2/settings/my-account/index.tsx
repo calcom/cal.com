ca172f4c4aa3cf0fedcdaddc020672d3807df612
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-08-16 15:31:26 -0400

    Create my account folder

---
+++ apps/web/pages/v2/settings/my-account/index.tsx
@@ -0,0 +1,13 @@
+import { getLayout } from "@calcom/ui/v2/core/layouts/AdminLayout";
+
+function AdminAppsView() {
+  return (
+    <>
+      <h1>Admin index</h1>
+    </>
+  );
+}
+
+AdminAppsView.getLayout = getLayout;
+
+export default AdminAppsView;
