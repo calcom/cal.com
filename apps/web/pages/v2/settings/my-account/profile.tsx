06a11e7b3357bfac127bf1f320a8d83b64747008
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-08-17 14:26:49 -0400

    Finish profile fields

--- apps/web/pages/v2/settings/my-account/profile.tsx
+++ apps/web/pages/v2/settings/my-account/profile.tsx
@@ -1,10 +1,14 @@
+import { signOut } from "next-auth/react";
+import { useState } from "react";
 import { useForm, Controller } from "react-hook-form";
 
 import { trpc } from "@calcom/trpc/react";
+import { Icon } from "@calcom/ui";
 import Avatar from "@calcom/ui/v2/core/Avatar";
 import { Button } from "@calcom/ui/v2/core/Button";
+import { Dialog, DialogTrigger, DialogContent } from "@calcom/ui/v2/core/Dialog";
 import Loader from "@calcom/ui/v2/core/Loader";
-import { TextField, Form } from "@calcom/ui/v2/core/form/fields";
+import { TextField, Form, Label } from "@calcom/ui/v2/core/form/fields";
 import { getLayout } from "@calcom/ui/v2/core/layouts/AdminLayout";
 
 // TODO show toast
@@ -12,6 +16,24 @@ import { getLayout } from "@calcom/ui/v2/core/layouts/AdminLayout";
 function ProfileView() {
   const { data: user, isLoading } = trpc.useQuery(["viewer.me"]);
   const mutation = trpc.useMutation("viewer.updateProfile");
+
+  const [deleteAccountOpen, setDeleteAccountOpen] = useState(false);
+
+  const deleteAccount = async () => {
+    await fetch("/api/user/me", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).catch((e) => {
+      console.error(`Error Removing user: ${user?.id}, email: ${user?.email} :`, e);
+    });
+    if (process.env.NEXT_PUBLIC_WEBAPP_URL === "https://app.cal.com") {
+      signOut({ callbackUrl: "/auth/logout?survey=true" });
+    } else {
+      signOut({ callbackUrl: "/auth/logout" });
+    }
+  };
 
   const formMethods = useForm({
     defaultValues: {
@@ -87,9 +109,34 @@ function ProfileView() {
             </div>
           )}
         />
-        <Button color="primary" className="ml-4">
+        <Button color="primary" className="mt-8">
           Update
         </Button>
+
+        <hr className="my-6 border-2 border-neutral-200" />
+
+        <Label>Danger Zone</Label>
+        {/* Delete account Dialog */}
+        <Dialog open={deleteAccountOpen} onOpenChange={setDeleteAccountOpen}>
+          <DialogTrigger asChild>
+            <Button color="destructive" className="mt-1 border-2" StartIcon={Icon.FiTrash2}>
+              Delete account
+            </Button>
+          </DialogTrigger>
+          <DialogContent
+            title="Delete Account"
+            description="Are you sure you want to delete your Cal.com account?"
+            type="confirmation"
+            actionText="Delete my account"
+            Icon={Icon.FiAlertTriangle}
+            actionOnClick={() => deleteAccount()}>
+            {/* Use trans component for translation */}
+            <p>
+              Anyone who you ve shared your account link with will no longer be able to book using it and any
+              preferences you have saved will be lost
+            </p>
+          </DialogContent>
+        </Dialog>
       </Form>
     </>
   );
