7e657fdbb76c297a2016f1274b9ea011dc897f5e
Alex van Andel <me@alexvanandel.com>
2022-08-17 23:42:34 +0100

    Minor fixup to auto-infer ensureArray +1

--- apps/web/lib/ensureArray.ts
+++
@@ -1,9 +0,0 @@
-export function ensureArray<T>(val: unknown): T[] {
-  if (Array.isArray(val)) {
-    return val;
-  }
-  if (typeof val === "undefined") {
-    return [];
-  }
-  return [val] as T[];
-}
