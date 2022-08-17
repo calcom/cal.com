abf3430a925cbb97b50a2d3134ef9bfcdb72c280
Hariom Balhara <hariombalhara@gmail.com>
2022-08-17 10:18:25 +0530

    Make tests stable

--- apps/web/playwright/change-username.test.ts
+++ apps/web/playwright/change-username.test.ts
@@ -40,9 +40,11 @@ test.describe("Change username on settings", () => {
     // Click on save button
     await page.click("[data-testid=update-username-btn-desktop]");
 
-    await page.click("[data-testid=save-username]");
-    // eslint-disable-next-line playwright/no-wait-for-timeout
-    await page.waitForTimeout(400);
+    await Promise.all([
+      page.waitForResponse("**/viewer.updateProfile*"),
+      page.click("[data-testid=save-username]"),
+    ]);
+
     const newUpdatedUser = await prisma.user.findFirst({
       where: {
         id: user.id,
