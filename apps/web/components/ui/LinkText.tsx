c90d04c7069aeb1d25a3d88180410d673a47e5d7
alannnc <alannnc@gmail.com>
2022-08-24 17:56:29 -0600

    Update apps/web/components/ui/LinkText.tsx
    Co-authored-by: Omar López <zomars@me.com>

--- apps/web/components/ui/LinkText.tsx
+++ apps/web/components/ui/LinkText.tsx
@@ -5,9 +5,10 @@ interface ILinkTextProps {
   children: React.ReactNode;
   classNameChildren?: string;
 }
-// This component had to be made in order to make i18n work with next/link
-// Reference: https://github.com/i18next/react-i18next/issues/1090#issuecomment-615426145
-
+/**
+ * This component had to be made in order to make i18n work with next/link
+ * @see https://github.com/i18next/react-i18next/issues/1090#issuecomment-615426145
+ **/
 export const LinkText = (props: ILinkTextProps) => {
   const { href, children, classNameChildren, ...moreProps } = props;
   return (
