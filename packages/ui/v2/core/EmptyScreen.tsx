d1e1d3b0157c901a36a4677e0b79b0000b05c7f1
Hariom Balhara <hariombalhara@gmail.com>
2022-08-25 19:13:53 +0530

    Fix UI

--- packages/ui/v2/core/EmptyScreen.tsx
+++ packages/ui/v2/core/EmptyScreen.tsx
@@ -26,7 +26,7 @@ export default function EmptyScreen({
         <div className="flex h-[72px] w-[72px] items-center justify-center rounded-full bg-gray-200 dark:bg-white">
           <Icon className="inline-block h-10 w-10 text-black dark:bg-gray-900 dark:text-gray-600" />
         </div>
-        <div className="max-w-[420px] text-center">
+        <div className="flex max-w-[420px] flex-col items-center">
           <h2 className="text-semibold font-cal mt-6 text-xl dark:text-gray-300">{headline}</h2>
           <p className="line-clamp-2 mt-3 mb-8 text-sm font-normal leading-6 text-gray-700 dark:text-gray-300">
             {description}
