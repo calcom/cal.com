ce36afb7c511cf8436d266436bc59b9dbd98529a
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-08-25 17:17:20 -0400

    Draft shell on md screen

--- apps/web/pages/v2/settings/my-account/appearance.tsx
+++ apps/web/pages/v2/settings/my-account/appearance.tsx
@@ -18,9 +18,9 @@ import Switch from "@calcom/ui/v2/core/Switch";
 import TimezoneSelect from "@calcom/ui/v2/core/TimezoneSelect";
 import ColorPicker from "@calcom/ui/v2/core/colorpicker";
 import Select from "@calcom/ui/v2/core/form/Select";
-import { TextField, Form, Label } from "@calcom/ui/v2/core/form/fields";
-import { getLayout } from "@calcom/ui/v2/core/layouts/AdminLayout";
+import { Form } from "@calcom/ui/v2/core/form/fields";
 import showToast from "@calcom/ui/v2/core/notfications";
+import SettingsShell from "@calcom/ui/v2/modules/settings/SettingsShell";
 
 import { getSession } from "@lib/auth";
 import { inferSSRProps } from "@lib/types/inferSSRProps";
@@ -46,91 +46,94 @@ const AppearanceView = (props: inferSSRProps<typeof getServerSideProps>) => {
   const formMethods = useForm();
 
   return (
-    <Form
-      form={formMethods}
-      handleSubmit={(values) => {
-        mutation.mutate({
-          ...values,
-          theme: values.theme.value,
-        });
-      }}>
-      <Controller
-        name="theme"
-        control={formMethods.control}
-        defaultValue={user.theme}
-        render={({ field: { value } }) => (
-          <>
-            <div className="flex items-center">
-              <div>
-                <p className="font-semibold">{t("follow_system_preferences")}</p>
-                <p className="text-gray-600">
-                  <Trans i18nKey="system_preference_description">
-                    Automatically adjust theme based on invitee system preferences. Note: This only applies to
-                    the booking pages.
-                  </Trans>
-                </p>
+    <SettingsShell heading={t("appearance")} subtitle={t("appearance_subtitle")}>
+      <Form
+        form={formMethods}
+        handleSubmit={(values) => {
+          mutation.mutate({
+            ...values,
+            theme: values.theme.value,
+          });
+        }}>
+        <Controller
+          name="theme"
+          control={formMethods.control}
+          defaultValue={user.theme}
+          render={({ field: { value } }) => (
+            <>
+              <div className="flex items-center">
+                <div>
+                  <p className="font-semibold">{t("follow_system_preferences")}</p>
+                  <p className="text-gray-600">
+                    <Trans i18nKey="system_preference_description">
+                      Automatically adjust theme based on invitee system preferences. Note: This only applies
+                      to the booking pages.
+                    </Trans>
+                  </p>
+                </div>
+                <Switch
+                  onCheckedChange={(checked) =>
+                    formMethods.setValue("theme", checked ? null : themeOptions[0])
+                  }
+                  checked={!value}
+                />
               </div>
-              <Switch
-                onCheckedChange={(checked) => formMethods.setValue("theme", checked ? null : themeOptions[0])}
-                checked={!value}
-              />
-            </div>
-            <div>
-              <Select
-                options={themeOptions}
-                onChange={(event) => {
-                  if (event) formMethods.setValue("theme", { ...event });
-                }}
-                isDisabled={!value}
-                defaultValue={value || themeOptions[0]}
-                value={value || themeOptions[0]}
-              />
-            </div>
-          </>
-        )}
-      />
+              <div>
+                <Select
+                  options={themeOptions}
+                  onChange={(event) => {
+                    if (event) formMethods.setValue("theme", { ...event });
+                  }}
+                  isDisabled={!value}
+                  defaultValue={value || themeOptions[0]}
+                  value={value || themeOptions[0]}
+                />
+              </div>
+            </>
+          )}
+        />
 
-      <hr className="border-1 my-8 border-neutral-200" />
-      <div className="mb-6 flex items-center">
-        <div>
-          <p className="font-semibold">{t("custom_brand_colors")}</p>
-          <p className="text-gray-600">{t("customize_your_brand_colors")}</p>
+        <hr className="border-1 my-8 border-neutral-200" />
+        <div className="mb-6 flex items-center">
+          <div>
+            <p className="font-semibold">{t("custom_brand_colors")}</p>
+            <p className="text-gray-600">{t("customize_your_brand_colors")}</p>
+          </div>
         </div>
-      </div>
 
-      <div className="flex justify-between">
-        <Controller
-          name="brandColor"
-          control={formMethods.control}
-          defaultValue={user.brandColor}
-          render={({ field: { value } }) => (
-            <div>
-              <p className="block text-sm font-medium text-gray-900">{t("light_brand_color")}</p>
-              <ColorPicker
-                defaultValue={user.brandColor}
-                onChange={(value) => formMethods.setValue("brandColor", value)}
-              />
-            </div>
-          )}
-        />
-        <Controller
-          name="darkBrandColor"
-          control={formMethods.control}
-          defaultValue={user.darkBrandColor}
-          render={({ field: { value } }) => (
-            <div>
-              <p className="block text-sm font-medium text-gray-900">{t("dark_brand_color")}</p>
-              <ColorPicker
-                defaultValue={user.darkBrandColor}
-                onChange={(value) => formMethods.setValue("darkBrandColor", value)}
-              />
-            </div>
-          )}
-        />
-      </div>
+        <div className="flex justify-between">
+          <Controller
+            name="brandColor"
+            control={formMethods.control}
+            defaultValue={user.brandColor}
+            render={({ field: { value } }) => (
+              <div>
+                <p className="block text-sm font-medium text-gray-900">{t("light_brand_color")}</p>
+                <ColorPicker
+                  defaultValue={user.brandColor}
+                  onChange={(value) => formMethods.setValue("brandColor", value)}
+                />
+              </div>
+            )}
+          />
+          <Controller
+            name="darkBrandColor"
+            control={formMethods.control}
+            defaultValue={user.darkBrandColor}
+            render={({ field: { value } }) => (
+              <div>
+                <p className="block text-sm font-medium text-gray-900">{t("dark_brand_color")}</p>
+                <ColorPicker
+                  defaultValue={user.darkBrandColor}
+                  onChange={(value) => formMethods.setValue("darkBrandColor", value)}
+                />
+              </div>
+            )}
+          />
+        </div>
 
-      {/* TODO future PR to preview brandColors */}
-      {/* <Button
+        {/* TODO future PR to preview brandColors */}
+        {/* <Button
         color="secondary"
         EndIcon={Icon.FiExternalLink}
         className="mt-6"
@@ -138,37 +141,38 @@ const AppearanceView = (props: inferSSRProps<typeof getServerSideProps>) => {
         Preview
       </Button> */}
 
-      <hr className="border-1 my-8 border-neutral-200" />
-      <Controller
-        name="hideBranding"
-        control={formMethods.control}
-        defaultValue={user.hideBranding}
-        render={({ field: { value } }) => (
-          <>
-            <div className="flex items-center">
-              <div>
-                <div className="flex items-center">
-                  <p className="mr-2 font-semibold">{t("disable_cal_branding")}</p>{" "}
-                  <Badge variant="gray">{t("pro")}</Badge>
+        <hr className="border-1 my-8 border-neutral-200" />
+        <Controller
+          name="hideBranding"
+          control={formMethods.control}
+          defaultValue={user.hideBranding}
+          render={({ field: { value } }) => (
+            <>
+              <div className="flex items-center">
+                <div>
+                  <div className="flex items-center">
+                    <p className="mr-2 font-semibold">{t("disable_cal_branding")}</p>{" "}
+                    <Badge variant="gray">{t("pro")}</Badge>
+                  </div>
+                  <p className="text-gray-600">{t("removes_cal_branding")}</p>
                 </div>
-                <p className="text-gray-600">{t("removes_cal_branding")}</p>
+                <Switch
+                  onCheckedChange={(checked) => formMethods.setValue("hideBranding", checked)}
+                  checked={value}
+                />
               </div>
-              <Switch
-                onCheckedChange={(checked) => formMethods.setValue("hideBranding", checked)}
-                checked={value}
-              />
-            </div>
-          </>
-        )}
-      />
-      <Button color="primary" className="mt-8">
-        {t("update")}
-      </Button>
-    </Form>
+            </>
+          )}
+        />
+        <Button color="primary" className="mt-8">
+          {t("update")}
+        </Button>
+      </Form>
+    </SettingsShell>
   );
 };
 
-AppearanceView.getLayout = getLayout;
+// AppearanceView.getLayout = getLayout;
 
 export default AppearanceView;
 
