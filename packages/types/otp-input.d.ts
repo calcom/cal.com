f0c528cb764ddd7c663bc0adb3f627954c66e521
Agusti Fernandez Pardo <git@agusti.me>
2022-08-25 18:41:07 +0200

    fix: remove react-otp-input types

--- packages/types/otp-input.d.ts
+++
@@ -1,8 +0,0 @@
-import { OtpInputProps as Props } from "react-otp-input";
-
-// necessary to extend OtpInputProps and accept autocomplete="code"
-declare module "react-otp-input" {
-  interface OtpInputProps extends Props {
-    autocomplete?: string;
-  }
-}
