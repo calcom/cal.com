24dc8ea9946dd03933a80d6e46a8b4e41f7c0dbf
Alan <alannnc@gmail.com>
2022-08-29 09:57:06 -0600

    Added Syntax error case and also default error returning

--- packages/lib/server/getServerErrorFromUnknown.ts
+++ packages/lib/server/getServerErrorFromUnknown.ts
@@ -33,6 +33,12 @@ export function getServerErrorFromUnknown(cause: unknown): HttpError {
       cause,
     });
   }
+  if (cause instanceof SyntaxError) {
+    return new HttpError({
+      statusCode: 500,
+      message: "Unexpected error, please reach out for our customer support.",
+    });
+  }
   if (cause instanceof PrismaClientKnownRequestError) {
     return new HttpError({ statusCode: 400, message: cause.message, cause });
   }
@@ -50,5 +56,8 @@ export function getServerErrorFromUnknown(cause: unknown): HttpError {
     return new Error(cause, { cause });
   }
 
-  return new HttpError({ statusCode: 500, message: `Unhandled error of type '${typeof cause}'` });
+  return new HttpError({
+    statusCode: 500,
+    message: `Unhandled error of type '${typeof cause}'. Please reach out for our customer support.`,
+  });
 }
