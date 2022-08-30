c5094a44dd2497c89f0460d08cab85196fbdf7ad
CarinaWolli <wollencarina@gmail.com>
2022-08-29 20:21:04 -0400

    code clean-up

--- packages/ui/form/PhoneInput.tsx
+++ packages/ui/form/PhoneInput.tsx
@@ -1,6 +1,5 @@
 import BasePhoneInput, { Props } from "react-phone-number-input/react-hook-form";
 import "react-phone-number-input/style.css";
-import { classNames } from "react-select/dist/declarations/src/utils";
 
 export type PhoneInputProps<FormValues> = Props<
   {
