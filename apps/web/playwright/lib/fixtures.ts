d27b7ab2c426d214896cbe397ec13bb4cdc3d90c
Omar López <zomars@me.com>
2022-09-01 20:00:48 -0600

    One playwright config to rule them all (#4072)

--- apps/web/playwright/lib/fixtures.ts
+++ apps/web/playwright/lib/fixtures.ts
@@ -1,13 +1,21 @@
 import { test as base } from "@playwright/test";
 
+import prisma from "@calcom/prisma";
+
 import { createBookingsFixture } from "../fixtures/bookings";
+import { createEmbedsFixture, createGetActionFiredDetails } from "../fixtures/embeds";
 import { createPaymentsFixture } from "../fixtures/payments";
+import { createServersFixture } from "../fixtures/servers";
 import { createUsersFixture } from "../fixtures/users";
 
 interface Fixtures {
   users: ReturnType<typeof createUsersFixture>;
   bookings: ReturnType<typeof createBookingsFixture>;
   payments: ReturnType<typeof createPaymentsFixture>;
+  addEmbedListeners: ReturnType<typeof createEmbedsFixture>;
+  getActionFiredDetails: ReturnType<typeof createGetActionFiredDetails>;
+  servers: ReturnType<typeof createServersFixture>;
+  prisma: typeof prisma;
 }
 
 /**
@@ -25,5 +33,20 @@ export const test = base.extend<Fixtures>({
   payments: async ({ page }, use) => {
     const payemntsFixture = createPaymentsFixture(page);
     await use(payemntsFixture);
+  },
+  addEmbedListeners: async ({ page }, use) => {
+    const embedsFixture = createEmbedsFixture(page);
+    await use(embedsFixture);
+  },
+  getActionFiredDetails: async ({ page }, use) => {
+    const getActionFiredDetailsFixture = createGetActionFiredDetails(page);
+    await use(getActionFiredDetailsFixture);
+  },
+  servers: async ({}, use) => {
+    const servers = createServersFixture();
+    await use(servers);
+  },
+  prisma: async ({}, use) => {
+    await use(prisma);
   },
 });
