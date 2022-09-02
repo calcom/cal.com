2c315f8583311549bb4836a1699a894d6210d909
zomars <zomars@me.com>
2022-09-01 19:43:23 -0600

    Workaround for SAML test

--- apps/web/playwright/saml.e2e.ts
+++ apps/web/playwright/saml.e2e.ts
@@ -1,14 +1,12 @@
 import { IS_SAML_LOGIN_ENABLED } from "../server/lib/constants";
+import { login } from "./fixtures/users";
 import { test } from "./lib/fixtures";
 
 test.describe("SAML tests", () => {
-  test.beforeEach(async ({ users }) => {
-    const proUser = await users.create();
-    await proUser.login();
-  });
-  test.afterEach(async ({ users }) => users.deleteAll());
-
   test("test SAML configuration UI with pro@example.com", async ({ page }) => {
+    // TODO: Figure out a way to use the users from fixtures here, right now we cannot set
+    // the SAML_ADMINS env variables dynamically
+    await login({ username: "pro", email: "pro@example.com", password: "pro" }, page);
     // eslint-disable-next-line playwright/no-skipped-test
     test.skip(!IS_SAML_LOGIN_ENABLED, "It should only run if SAML is enabled");
     // Try to go Security page
