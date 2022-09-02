f09db6efb6c1c6b0edcca564128dd6ada7496b02
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-09-02 11:05:20 -0400

    Revise sidebar

--- packages/ui/v2/core/layouts/SettingsLayout.tsx
+++ packages/ui/v2/core/layouts/SettingsLayout.tsx
@@ -1,100 +1,21 @@
 import React, { ComponentProps } from "react";
 
+import { classNames } from "@calcom/lib";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 
 import { Icon } from "../../../Icon";
 import { useMeta } from "../Meta";
 import Shell from "../Shell";
-import { VerticalTabItem } from "../navigation/tabs";
-import VerticalTabs from "../navigation/tabs/VerticalTabs";
-
-export const settingsTabs = [
-  {
-    name: "my_account",
-    href: "/settings/my-account",
-    icon: Icon.FiUser,
-    children: [
-      { name: "profile", href: "/v2/settings/my-account/profile" },
-      { name: "general", href: "/v2/settings/my-account/general" },
-      { name: "calendars", href: "/v2/settings/my-account/calendars" },
-      { name: "conferencing", href: "/v2/settings/my-account/conferencing" },
-      { name: "appearance", href: "/v2/settings/my-account/appearance" },
-      // TODO
-      // { name: "referrals", href: "/settings/my-account/referrals" },
-    ],
-  },
-  {
-    name: "security",
-    href: "/settings/security",
-    icon: Icon.FiKey,
-    children: [
-      //
-      { name: "password", href: "/v2/settings/security/password" },
-      { name: "2fa_auth", href: "/v2/settings/security/two-factor-auth" },
-    ],
-  },
-  {
-    name: "billing",
-    href: "/settings/billing",
-    icon: Icon.FiCreditCard,
-    children: [
-      //
-      { name: "invoices", href: "/v2/settings/billing" },
-    ],
-  },
-  {
-    name: "developer",
-    href: "/settings/developer",
-    icon: Icon.FiTerminal,
-    children: [
-      //
-      { name: "webhooks", href: "/v2/settings/developer/webhooks" },
-      { name: "api_keys", href: "/v2/settings/developer/api_keys" },
-      { name: "embeds", href: "/v2/settings/developer/embeds" },
-    ],
-  },
-  {
-    name: "teams",
-    href: "/settings/teams",
-    icon: Icon.FiUsers,
-    children: [],
-  },
-  {
-    name: "admin",
-    href: "/settings/admin",
-    icon: Icon.FiLock,
-    adminRequired: true,
-    children: [
-      //
-      { name: "impersonation", href: "/v2/settings/admin/impersonation" },
-      { name: "apps", href: "/v2/settings/admin/apps" },
-      { name: "users", href: "/v2/settings/admin/users" },
-    ],
-  },
-];
+import SettingsSidebarContainer from "../navigation/SettingsSidebarContainer";
 
 export default function SettingsLayout({
   children,
   ...rest
 }: { children: React.ReactNode } & ComponentProps<typeof Shell>) {
+  const { t } = useLocale();
   return (
-    <Shell
-      flexChildrenContainer
-      {...rest}
-      SidebarContainer={
-        <>
-          <VerticalTabs tabs={settingsTabs} className="hidden py-3 pl-3 lg:flex">
-            <VerticalTabItem
-              name="Settings"
-              href="/"
-              icon={Icon.FiArrowLeft}
-              textClassNames="text-md font-medium leading-none text-black"
-              className="mb-1"
-            />
-          </VerticalTabs>
-        </>
-      }>
-      <div className="flex-1 [&>*]:flex-1">
+    <Shell flexChildrenContainer {...rest} SidebarContainer={<SettingsSidebarContainer />}>
+      <div className="f}lex-1 [&>*]:flex-1">
         <div className="mt-8 justify-center px-4 sm:px-6 md:px-8">
           <ShellHeader />
           {children}
