93adf57d0004aec967a2b25921408cfa8bfbdf60
Jeroen Reumkens <hello@jeroenreumkens.nl>
2022-09-04 16:59:16 +0200

    #4110: Made vertical tabs in bookings and event types pages sticky. (#4145)
    Co-authored-by: Jeroen Reumkens <jeroen.reumkens@deptagency.com>

--- packages/ui/v2/core/navigation/tabs/VerticalTabs.tsx
+++ packages/ui/v2/core/navigation/tabs/VerticalTabs.tsx
@@ -1,16 +1,22 @@
 import { FC } from "react";
 
+import { classNames } from "@calcom/lib";
+
 import VerticalTabItem, { VerticalTabItemProps } from "./VerticalTabItem";
 
 export interface NavTabProps {
   tabs: VerticalTabItemProps[];
   children?: React.ReactNode;
   className?: string;
+  sticky?: boolean;
 }
 
-const NavTabs: FC<NavTabProps> = ({ tabs, className = "", ...props }) => {
+const NavTabs: FC<NavTabProps> = ({ tabs, className = "", sticky, ...props }) => {
   return (
-    <nav className={`no-scrollbar flex flex-col space-y-1 ${className}`} aria-label="Tabs" {...props}>
+    <nav
+      className={classNames(`no-scrollbar flex flex-col space-y-1 ${className}`, sticky && "sticky top-0")}
+      aria-label="Tabs"
+      {...props}>
       {props.children}
       {tabs.map((tab, idx) => (
         <VerticalTabItem {...tab} key={idx} />
