8d40ee27f9b34556d420dfc7d7b3bb6d8c466e83
nafees nazik <nafeesnazik21@gmail.com>
2022-09-07 20:44:46 +0530

    fix: make prop optional

--- apps/web/components/booking/BookingDescription.tsx
+++ apps/web/components/booking/BookingDescription.tsx
@@ -10,11 +10,11 @@ import type { AvailabilityPageProps } from "../../pages/[user]/[type]";
 interface Props {
   profile: AvailabilityPageProps["profile"];
   eventType: AvailabilityPageProps["eventType"];
-  isBookingPage: boolean;
+  isBookingPage?: boolean;
 }
 
 const BookingDescription: FC<Props> = (props) => {
-  const { profile, eventType, isBookingPage } = props;
+  const { profile, eventType, isBookingPage = false } = props;
   return (
     <>
       <UserAvatars
