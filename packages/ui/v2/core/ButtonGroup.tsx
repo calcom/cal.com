9c87bae13eb0deae65f85210e4c09b6013017b70
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2022-09-07 11:50:59 +0100

    Explain tailwind magic

--- packages/ui/v2/core/ButtonGroup.tsx
+++ packages/ui/v2/core/ButtonGroup.tsx
@@ -4,6 +4,15 @@ import classNames from "@calcom/lib/classNames";
 
 type Props = { children: React.ReactNode; combined?: boolean; containerProps?: JSX.IntrinsicElements["div"] };
 
+/**
+ * Breakdown of Tailwind Magic below
+ * [&_button]:border-l-0 [&_a]:border-l-0 -> Selects all buttons/a tags and applies a border left of 0
+ * [&>*:first-child]:rounded-l-md [&>*:first-child]:border-l -> Selects the first child of the content
+ * ounds the left side
+ * [&>*:last-child]:rounded-r-md -> Selects the last child of the content and rounds the right side
+ * We dont need to add border to the right as we never remove it
+ */
+
 export default function ButtonGroup({ children, combined = false, containerProps }: Props) {
   return (
     <div
