ddfde6990815395da8f77650bf5e4c49ed6a2a87
zomars <zomars@me.com>
2022-09-07 17:26:37 -0600

    WIP

--- packages/ui/v2/core/skeleton/index.tsx
+++ packages/ui/v2/core/skeleton/index.tsx
@@ -1,15 +1,19 @@
 import classNames from "@calcom/lib/classNames";
 
 type SkeletonBaseProps = {
+  /** @deprecated @see https://tailwindcss.com/docs/content-configuration#dynamic-class-names */
   width?: string;
+  /** @deprecated @see https://tailwindcss.com/docs/content-configuration#dynamic-class-names */
   height?: string;
   className?: string;
 };
 
 interface AvatarProps extends SkeletonBaseProps {
-  // Limit this cause we don't use avatars bigger than thi
-  width: "2" | "3" | "4" | "5" | "6" | "8" | "12";
-  height: "2" | "3" | "4" | "5" | "6" | "8" | "12";
+  // Limit this cause we don't use avatars bigger than this
+  /** @deprecated @see https://tailwindcss.com/docs/content-configuration#dynamic-class-names */
+  width?: "2" | "3" | "4" | "5" | "6" | "8" | "12";
+  /** @deprecated @see https://tailwindcss.com/docs/content-configuration#dynamic-class-names */
+  height?: "2" | "3" | "4" | "5" | "6" | "8" | "12";
 }
 
 interface SkeletonContainer {
@@ -19,23 +23,21 @@ interface SkeletonContainer {
 }
 
 const SkeletonAvatar: React.FC<AvatarProps> = ({ width, height, className }) => {
-  return (
-    <div
-      className={classNames(
-        `mt-1 rounded-full bg-gray-200 ltr:mr-2 rtl:ml-2 w-${width} h-${height}`,
-        className
-      )}
-    />
-  );
+  className = width ? `${className} w-${width}` : className;
+  // Using h-[number] wasn't working for some reason, using line-height instead
+  className = height ? `${className} h[${height}px]` : className;
+  return <div className={classNames(`mt-1 rounded-full bg-gray-200 ltr:mr-2 rtl:ml-2`, className)} />;
 };
 
 const SkeletonText: React.FC<SkeletonBaseProps> = ({ width = "", height = "", className = "" }) => {
+  /** @see https://tailwindcss.com/docs/content-configuration#dynamic-class-names */
   className = width ? `${className} w-${width}` : className;
-  className = height ? `${className} h-${height}` : className;
+  // Using h-[number] wasn't working for some reason, using line-height instead
+  className = height ? `${className} leading-[${height}px]` : className;
   return (
     <div
       className={classNames(
-        `dark:white-300 animate-pulse rounded-md bg-gray-300 empty:before:inline-block empty:before:content-[''] w-${width} h-${height}`,
+        `font-size-0 dark:white-300 animate-pulse rounded-md bg-gray-300 empty:before:inline-block empty:before:content-['']`,
         className
       )}
     />
