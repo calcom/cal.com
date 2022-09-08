15f36b5781887657f9efef26acb92d82aa861748
zomars <zomars@me.com>
2022-09-08 16:13:57 -0600

    Removes dynamic width in favor of classnames

--- packages/ui/v2/core/skeleton/index.tsx
+++ packages/ui/v2/core/skeleton/index.tsx
@@ -1,20 +1,8 @@
 import classNames from "@calcom/lib/classNames";
 
 type SkeletonBaseProps = {
-  /** @deprecated @see https://tailwindcss.com/docs/content-configuration#dynamic-class-names */
-  width?: string;
-  /** @deprecated @see https://tailwindcss.com/docs/content-configuration#dynamic-class-names */
-  height?: string;
   className?: string;
 };
-
-interface AvatarProps extends SkeletonBaseProps {
-  // Limit this cause we don't use avatars bigger than this
-  /** @deprecated @see https://tailwindcss.com/docs/content-configuration#dynamic-class-names */
-  width?: "2" | "3" | "4" | "5" | "6" | "8" | "12";
-  /** @deprecated @see https://tailwindcss.com/docs/content-configuration#dynamic-class-names */
-  height?: "2" | "3" | "4" | "5" | "6" | "8" | "12";
-}
 
 interface SkeletonContainer {
   as?: keyof JSX.IntrinsicElements;
@@ -22,18 +10,11 @@ interface SkeletonContainer {
   className?: string;
 }
 
-const SkeletonAvatar: React.FC<AvatarProps> = ({ width, height, className }) => {
-  className = width ? `${className} w-${width}` : className;
-  // Using h-[number] wasn't working for some reason, using line-height instead
-  className = height ? `${className} h[${height}px]` : className;
+const SkeletonAvatar: React.FC<SkeletonBaseProps> = ({ className }) => {
   return <div className={classNames(`mt-1 rounded-full bg-gray-200 ltr:mr-2 rtl:ml-2`, className)} />;
 };
 
-const SkeletonText: React.FC<SkeletonBaseProps> = ({ width = "", height = "", className = "" }) => {
-  /** @see https://tailwindcss.com/docs/content-configuration#dynamic-class-names */
-  className = width ? `${className} w-${width}` : className;
-  // Using h-[number] wasn't working for some reason, using line-height instead
-  className = height ? `${className} leading-[${height}px]` : className;
+const SkeletonText: React.FC<SkeletonBaseProps> = ({ className = "" }) => {
   return (
     <span
       className={classNames(
@@ -44,10 +25,10 @@ const SkeletonText: React.FC<SkeletonBaseProps> = ({ width = "", height = "", cl
   );
 };
 
-const SkeletonButton: React.FC<SkeletonBaseProps> = ({ width, height, className }) => {
+const SkeletonButton: React.FC<SkeletonBaseProps> = ({ className }) => {
   return (
     <SkeletonContainer>
-      <div className={classNames(`w-${width} h-${height} bg-gray-200`, className)} />
+      <div className={classNames(`bg-gray-200`, className)} />
     </SkeletonContainer>
   );
 };
