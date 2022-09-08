f6fd6ae6522748c74c7d91ba24fad2b25c9d186e
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2022-09-08 11:15:46 +0100

    Move permission container to features

--- packages/features/auth/PermissionContainer.tsx
+++ packages/features/auth/PermissionContainer.tsx
@@ -5,8 +5,6 @@ import { UserPermissionRole } from ".prisma/client";
 
 type AdminRequiredProps = {
   as?: keyof JSX.IntrinsicElements;
-  /** Use this if you want to skip the admin check - used when wrapping reusable components that might require an admin check  */
-  skip?: boolean;
   children?: React.ReactNode;
   /**Not needed right now but will be useful if we ever expand our permission roles */
   roleRequired?: UserPermissionRole;
@@ -15,14 +13,11 @@ type AdminRequiredProps = {
 export const PermissionContainer: FC<AdminRequiredProps> = ({
   children,
   as,
-  skip,
   roleRequired = "ADMIN",
   ...rest
 }) => {
   const session = useSession();
-  if (skip) {
-    return <>{children}</>;
-  }
+
   // Admin can do everything
   if (session.data?.user.role !== roleRequired || !UserPermissionRole.ADMIN) return null;
   const Component = as ?? Fragment;
