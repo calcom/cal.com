15c6fd09237ab204afbf9e0d30339085ffaf479e
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2022-09-08 11:16:46 +0100

    Fix export from v2

--- packages/ui/v2/core/index.ts
+++ packages/ui/v2/core/index.ts
@@ -30,4 +30,3 @@ export { default as HorizontalTabs, HorizontalTabItem } from "./navigation/tabs/
 export type { VerticalTabItemProps } from "./navigation/tabs/VerticalTabItem";
 export type { HorizontalTabItemProps } from "./navigation/tabs/HorizontalTabItem";
 export * from "./Portal";
-export * from "./PermissionContainer";
