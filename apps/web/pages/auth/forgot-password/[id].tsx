833d57e6d5c41706b2499f7bb750ca440affe7aa
Peer Richelsen <peer@cal.com>
2022-09-09 09:53:30 +0200

    using new v2 buttons in password reset

--- apps/web/pages/auth/forgot-password/[id].tsx
+++ apps/web/pages/auth/forgot-password/[id].tsx
@@ -7,6 +7,7 @@ import React, { useMemo } from "react";
 
 import dayjs from "@calcom/dayjs";
 import prisma from "@calcom/prisma";
+import { Button } from "@calcom/ui/v2";
 
 import { useLocale } from "@lib/hooks/useLocale";
 
@@ -106,7 +107,7 @@ export default function Page({ resetPasswordRequest, csrfToken }: Props) {
     <div className="flex min-h-screen flex-col justify-center bg-gray-50 py-12 sm:px-6 lg:px-8">
       <HeadSeo title={t("reset_password")} description={t("change_your_password")} />
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="mx-2 space-y-6 rounded-lg bg-white px-4 py-8 shadow sm:px-10">
+        <div className="mx-2 space-y-6 rounded-md border border-gray-200 bg-white px-4 py-8 sm:px-10">
           {isRequestExpired && <Expired />}
           {!isRequestExpired && !success && (
             <>
@@ -158,35 +159,9 @@ export default function Page({ resetPasswordRequest, csrfToken }: Props) {
                 </div>
 
                 <div>
-                  <button
-                    type="submit"
-                    disabled={loading}
-                    className={`flex w-full justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2 ${
-                      loading ? "cursor-not-allowed" : ""
-                    }`}>
-                    {loading && (
-                      <svg
-                        className="mr-3 -ml-1 h-5 w-5 animate-spin text-white"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24">
-                        <circle
-                          className="opacity-25"
-                          cx="12"
-                          cy="12"
-                          r="10"
-                          stroke="currentColor"
-                          strokeWidth="4"
-                        />
-                        <path
-                          className="opacity-75"
-                          fill="currentColor"
-                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                        />
-                      </svg>
-                    )}
+                  <Button loading={loading} color="primary" type="submit" disabled={loading}>
                     {t("submit")}
-                  </button>
+                  </Button>
                 </div>
               </form>
             </>
