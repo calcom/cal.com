c7732bc61d82f3c4642fa6398e98d5756ddf2268
Leo Giovanetti <hello@leog.me>
2022-09-09 10:49:42 -0300

    second try fixing e2e tests

--- apps/web/playwright/app-store.e2e.ts
+++ apps/web/playwright/app-store.e2e.ts
@@ -11,10 +11,8 @@ test.describe("App Store - Authed", () => {
     await page.goto("/apps");
     await page.click('[data-testid="app-store-category-calendar"]');
     await page.click('[data-testid="app-store-app-card-apple-calendar"]');
-    await Promise.all([
-      page.waitForResponse((resp) => resp.url().includes("/api/apps") && resp.status() === 200),
-      page.click('[data-testid="install-app-button"]'),
-    ]);
+    await page.waitForResponse((resp) => resp.url().includes("/api/apps") && resp.status() === 200);
+    await page.click('[data-testid="install-app-button"]');
     await expect(page.locator(`text=Connect to Apple Server`)).toBeVisible();
     await pro.delete();
   });
@@ -26,10 +24,8 @@ test.describe("App Store - Unauthed", () => {
     await page.waitForSelector("[data-testid=dashboard-shell]");
     await page.click('[data-testid="app-store-category-calendar"]');
     await page.click('[data-testid="app-store-app-card-apple-calendar"]');
-    await Promise.all([
-      page.waitForResponse((resp) => resp.url().includes("/api/apps") && resp.status() === 400),
-      page.click('[data-testid="install-app-button"]'),
-    ]);
+    await page.waitForResponse((resp) => resp.url().includes("/api/apps") && resp.status() === 400);
+    await page.click('[data-testid="install-app-button"]');
     await expect(page.locator(`[data-testid="login-form"]`)).toBeVisible();
   });
 });
