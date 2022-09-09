c257fd85195283bfe697c7998713fa08f7041186
Alex van Andel <me@alexvanandel.com>
2022-09-09 11:55:42 +0100

    Added some comments to improve readability

--- apps/web/playwright/onboarding.e2e.ts
+++ apps/web/playwright/onboarding.e2e.ts
@@ -12,9 +12,11 @@ test.describe("Onboarding", () => {
       const user = await users.create({ plan: UserPlan.TRIAL, completedOnboarding: false, name: null });
       await user.login();
 
-      await test.step("step 1", async () => {
-        await page.waitForURL("/getting-started");
+      // tests whether the user makes it to /getting-started
+      // after login with completedOnboarding false
+      await page.waitForURL("/getting-started");
 
+      await test.step("step 1", async () => {
         // Check required fields
         await page.locator("button[type=submit]").click();
         await expect(page.locator("data-testid=required")).toBeVisible();
@@ -27,6 +29,7 @@ test.describe("Onboarding", () => {
 
         await page.locator("button[type=submit]").click();
 
+        // should be on step 2 now.
         await expect(page).toHaveURL(/.*connected-calendar/);
 
         const userComplete = await user.self();
@@ -34,10 +37,9 @@ test.describe("Onboarding", () => {
       });
 
       await test.step("step 2", async () => {
-        // Second step
         const isDisabled = await page.locator("button[data-testid=save-calendar-button]").isDisabled();
         await expect(isDisabled).toBe(true);
-
+        // tests skip button, we don't want to test entire flow.
         await page.locator("button[data-testid=skip-step]").click();
 
         await expect(page).toHaveURL(/.*setup-availability/);
@@ -46,7 +48,7 @@ test.describe("Onboarding", () => {
       await test.step("step 3", async () => {
         const isDisabled = await page.locator("button[data-testid=save-availability]").isDisabled();
         await expect(isDisabled).toBe(false);
-
+        // same here, skip this step.
         await page.locator("button[data-testid=skip-step]").click();
 
         await expect(page).toHaveURL(/.*user-profile/);
@@ -54,7 +56,7 @@ test.describe("Onboarding", () => {
 
       await test.step("step 4", async () => {
         const finishButton = await page.locator("button[type=submit]");
-
+        // bio field is required, try to submit (and test whether that fails)
         await finishButton.click();
 
         const requiredBio = await page.locator("data-testid=required");
@@ -67,6 +69,7 @@ test.describe("Onboarding", () => {
 
         await finishButton.click();
 
+        // should redirect to /event-types after onboarding
         await page.waitForURL("/event-types");
 
         const userComplete = await user.self();
