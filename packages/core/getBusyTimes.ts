2c957441d2966e57b43bcdfda129bbff5f2e146c
isaiah dahl <isaiahdahl@me.com>
2022-09-09 08:58:31 -0700

    fix: don't make all roundRobin users busy when an event is booked.

--- packages/core/getBusyTimes.ts
+++ packages/core/getBusyTimes.ts
@@ -1,4 +1,4 @@
-import { BookingStatus, Credential, SelectedCalendar } from "@prisma/client";
+import { BookingStatus, Credential, SelectedCalendar, SchedulingType } from "@prisma/client";
 
 import { getBusyCalendarTimes } from "@calcom/core/CalendarManager";
 import logger from "@calcom/lib/logger";
@@ -26,13 +26,25 @@ export async function getBusyTimes(params: {
   const busyTimes: EventBusyDetails[] = await prisma.booking
     .findMany({
       where: {
-        eventType: {
-          users: {
-            some: {
-              id: userId,
-            },
+        AND: [
+          {
+            OR: [
+              /* If user is organizer */
+              { userId },
+              /* Or part of a collective booking */
+              {
+                eventType: {
+                  schedulingType: SchedulingType.COLLECTIVE,
+                  users: {
+                    some: {
+                      id: userId,
+                    },
+                  },
+                },
+              },
+            ],
           },
-        },
+        ],
         startTime: { gte: new Date(startTime) },
         endTime: { lte: new Date(endTime) },
         status: {
