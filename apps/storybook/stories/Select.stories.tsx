3ce687c0a4dc33298a7735165db5507b36165956
Syed Ali Shahbaz <52925846+alishaz-polymath@users.noreply.github.com>
2022-09-10 15:27:55 +0530

    V2 Multi-select (Team Select) (#4324)
    * --init
    
    * design improved
    
    * further fine tuning
    
    * more fixes
    
    * removed extra JSX tag
    
    * added story
    
    * NIT
    
    * revert to use of CheckedTeamSelect
    
    * Removes comments
    
    Co-authored-by: Peer Richelsen <peeroke@gmail.com>

--- apps/storybook/stories/Select.stories.tsx
+++ apps/storybook/stories/Select.stories.tsx
@@ -1,13 +1,24 @@
 import { ComponentMeta } from "@storybook/react";
 
 import { Select } from "@calcom/ui/v2";
+import MultiDropdownSelect from "@calcom/ui/v2/modules/event-types/MultiDropdownSelect";
 
 export default {
   title: "Form/Select",
   component: Select,
 } as ComponentMeta<typeof Select>;
 
-const testOptions = [
+const singleOptions = [
+  {
+    label: "Select",
+    value: 0,
+  },
+  ...[5, 10, 15, 20, 30, 45, 60, 90, 120].map((minutes) => ({
+    label: minutes + " " + "minutes",
+    value: minutes,
+  })),
+];
+const multiOptions = [
   {
     label: "Select",
     value: 0,
@@ -18,4 +29,5 @@ const testOptions = [
   })),
 ];
 
-export const Single = () => <Select options={testOptions} />;
+export const Single = () => <Select options={singleOptions} />;
+export const Multi = () => <MultiDropdownSelect options={multiOptions} />;
