e7108b6fb3b65846e35fa58190af93b2b1e30a89
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-09-11 15:08:42 -0400

    Fix API key page

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -8,6 +8,7 @@ import { extendEventData, nextCollectBasicSettings } from "@calcom/lib/telemetry
 const V2_WHITELIST = [
   "/settings/admin",
   "/settings/developer/webhooks",
+  "/settings/developer/api-keys",
   "/settings/my-account",
   "/settings/security",
   "/availability",
--- apps/web/pages/v2/settings/developer/api-keys.tsx
+++ apps/web/pages/v2/settings/developer/api-keys.tsx
@@ -8,10 +8,10 @@ import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { trpc } from "@calcom/trpc/react";
 import { Icon } from "@calcom/ui";
 import SkeletonLoader from "@calcom/ui/apps/SkeletonLoader";
-import Button from "@calcom/ui/v2/core/Button";
+import { EmptyScreen, Button } from "@calcom/ui/v2";
 import { Dialog, DialogContent } from "@calcom/ui/v2/core/Dialog";
 import Meta from "@calcom/ui/v2/core/Meta";
-import { getLayout } from "@calcom/ui/v2/core/layouts/AdminLayout";
+import { getLayout } from "@calcom/ui/v2/core/layouts/SettingsLayout";
 
 const ApiKeysView = () => {
   const { t } = useLocale();
@@ -23,6 +23,20 @@ const ApiKeysView = () => {
     undefined
   );
 
+  const NewApiKeyButton = () => {
+    return (
+      <Button
+        color="secondary"
+        StartIcon={Icon.FiPlus}
+        onClick={() => {
+          setApiKeyToEdit(undefined);
+          setApiKeyModal(true);
+        }}>
+        {t("new_api_key")}
+      </Button>
+    );
+  };
+
   return (
     <>
       <Meta title="api_keys" description="webhooks_description" />
@@ -32,29 +46,30 @@ const ApiKeysView = () => {
           {isLoading && <SkeletonLoader />}
           <div>
             {data?.length ? (
-              <div className="mt-6 mb-8 rounded-md border">
-                {data?.map((apiKey, index) => (
-                  <ApiKeyListItem
-                    key={apiKey.id}
-                    apiKey={apiKey}
-                    lastItem={data.length === index + 1}
-                    onEditClick={() => {
-                      setApiKeyToEdit(apiKey);
-                      setApiKeyModal(true);
-                    }}
-                  />
-                ))}
-              </div>
-            ) : null}
-            <Button
-              color="secondary"
-              StartIcon={Icon.FiPlus}
-              onClick={() => {
-                setApiKeyToEdit(undefined);
-                setApiKeyModal(true);
-              }}>
-              {t("new_api_key")}
-            </Button>
+              <>
+                <div className="mt-6 mb-8 rounded-md border">
+                  {data?.map((apiKey, index) => (
+                    <ApiKeyListItem
+                      key={apiKey.id}
+                      apiKey={apiKey}
+                      lastItem={data.length === index + 1}
+                      onEditClick={() => {
+                        setApiKeyToEdit(apiKey);
+                        setApiKeyModal(true);
+                      }}
+                    />
+                  ))}
+                </div>
+                <NewApiKeyButton />
+              </>
+            ) : (
+              <EmptyScreen
+                Icon={Icon.FiLink}
+                headline={t("create_first_api_key")}
+                description={t("create_first_api_key_description")}
+                buttonRaw={<NewApiKeyButton />}
+              />
+            )}
           </div>
         </>
       </LicenseRequired>
