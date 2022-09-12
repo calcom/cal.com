548e79fb715fd28fa125abc929ab69ca601fbfc7
CarinaWolli <wollencarina@gmail.com>
2022-09-12 13:39:02 -0400

    add mobile view for skeleton list

--- packages/features/ee/workflows/components/v2/SkeletonLoaderList.tsx
+++ packages/features/ee/workflows/components/v2/SkeletonLoaderList.tsx
@@ -3,7 +3,7 @@ import { SkeletonText } from "@calcom/ui/v2";
 
 function SkeletonLoader() {
   return (
-    <ul className="mt-20 animate-pulse divide-y divide-neutral-200 rounded-md border border-gray-200 bg-white sm:overflow-hidden">
+    <ul className="mt-5 animate-pulse divide-y divide-neutral-200 rounded-md border border-gray-200 bg-white sm:mt-20 sm:overflow-hidden">
       <SkeletonItem />
       <SkeletonItem />
       <SkeletonItem />
@@ -19,19 +19,19 @@ function SkeletonItem() {
       <div className="flex-grow truncate text-sm">
         <div className="flex">
           <div className="flex flex-col space-y-2">
-            <SkeletonText className="h-4 w-28" />
+            <SkeletonText className="h-4 w-16 sm:w-24" />
             <div className="flex">
               <Icon.FiBell className="mt-0.5 mr-1.5 inline h-4 w-4 text-gray-200" />
-              <SkeletonText className="mr-2 h-4 w-32" />
+              <SkeletonText className="mr-2 h-4 w-16 sm:w-28" />
               <Icon.FiLink className="mt-0.5 mr-1.5 inline h-4 w-4 text-gray-200" />
-              <SkeletonText className="h-4 w-40" />
+              <SkeletonText className="h-4 w-28 sm:w-36" />
             </div>
           </div>
         </div>
       </div>
-      <div className="mt-4 hidden flex-shrink-0 sm:mt-0 sm:ml-5 lg:flex">
+      <div className="mt-0 flex flex-shrink-0 sm:ml-5">
         <div className="flex justify-between space-x-2 rtl:space-x-reverse">
-          <SkeletonText className="h-8 w-16" />
+          <SkeletonText className="h-8 w-8 sm:w-16" />
         </div>
       </div>
     </li>
