ad482e6b180742b3a862dad681f5c4b545d94663
CarinaWolli <wollencarina@gmail.com>
2022-09-12 14:24:09 -0400

    mobile view for edit skeleton loader

--- packages/features/ee/workflows/components/v2/SkeletonLoaderEdit.tsx
+++ packages/features/ee/workflows/components/v2/SkeletonLoaderEdit.tsx
@@ -4,16 +4,16 @@ import { Icon } from "@calcom/ui/Icon";
 function SkeletonLoader() {
   return (
     <SkeletonContainer>
-      <div className="mt-20 flex">
-        <div className="mr-6 flex-none">
-          <SkeletonText className="h-4 w-16" />
-          <SkeletonText className="mt-2 mb-6 h-8 w-64" />
-          <SkeletonText className="h-4 w-16" />
-          <SkeletonText className="mt-2 h-8 w-64" />
-          <div className="my-7 border-transparent md:border-t md:border-gray-200" />
-          <SkeletonText className="mt-2 mb-6 h-8 w-40" />
+      <div className="mt-20 ml-2 md:flex">
+        <div className="mr-6 md:flex-none">
+          <SkeletonText className="h-4 w-28" />
+          <SkeletonText className="mt-2 mb-6 h-8 w-full md:w-64" />
+          <SkeletonText className="h-4 w-28" />
+          <SkeletonText className="mt-2 h-8 w-full md:w-64" />
+          <SkeletonText className="mt-8 hidden h-0.5 w-full md:block" />
+          <SkeletonText className="mt-8 mb-6 h-8 w-40" />
         </div>
-        <div className="flex-grow">
+        <div className="hidden flex-grow md:flex">
           <SkeletonText className="h-64 w-full" />
         </div>
       </div>
