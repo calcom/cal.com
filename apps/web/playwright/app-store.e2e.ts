34e7d2f40c32362166e7824eb837dfdf346b5a72
Leo Giovanetti <hello@leog.me>
2022-09-15 09:27:45 -0300

    Going directly to category page as a patch

--- apps/web/playwright/app-store.e2e.ts
+++ apps/web/playwright/app-store.e2e.ts
@@ -8,8 +8,7 @@ test.describe("App Store - Authed", () => {
   test("Browse apple-calendar and try to install", async ({ page, users }) => {
     const pro = await users.create();
     await pro.login();
-    await page.goto("/apps");
-    await page.click('[data-testid="app-store-category-calendar"] >> visible=true');
+    await page.goto("/apps/categories/calendar");
     await page.click(
       '[data-testid="app-store-app-card-apple-calendar"] [data-testid="app-store-app-card-details"]'
     );
@@ -22,9 +21,7 @@ test.describe("App Store - Authed", () => {
 
 test.describe("App Store - Unauthed", () => {
   test("Browse apple-calendar and try to install", async ({ page }) => {
-    await page.goto("/apps");
-    await page.waitForSelector("[data-testid=dashboard-shell]");
-    await page.click('[data-testid="app-store-category-calendar"] >> visible=true');
+    await page.goto("/apps/categories/calendar");
     await page.click(
       '[data-testid="app-store-app-card-apple-calendar"] [data-testid="app-store-app-card-details"]'
     );
