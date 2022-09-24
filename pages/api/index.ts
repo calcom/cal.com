d14c8404e708cc966d2049ffe6bddeabf2dd51a8
Peer Richelsen <peer@cal.com>
2022-09-24 13:23:10 +0000

    Update index.ts

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function CalcomApi(_: NextApiRequest, res: NextApiResponse) {
-  res.status(201).json({ message: "Welcome to Cal.com API - docs are at https://docs.cal.com/api" });
+  res.status(201).json({ message: "Welcome to Cal.com API - docs are at https://developer.cal.com/api" });
 }
