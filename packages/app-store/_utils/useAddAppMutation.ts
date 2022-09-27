564f55a9a316a9357bb18d52cc75207f91b57be3
Hariom Balhara <hariombalhara@gmail.com>
2022-09-27 13:46:47 +0530

    Make OmniInstallAppButton not redirect

--- packages/app-store/_utils/useAddAppMutation.ts
+++ packages/app-store/_utils/useAddAppMutation.ts
@@ -7,37 +7,42 @@ import { App } from "@calcom/types/App";
 import getInstalledAppPath from "./getInstalledAppPath";
 
 function useAddAppMutation(_type: App["type"] | null, options?: Parameters<typeof useMutation>[2]) {
-  const mutation = useMutation<unknown, Error, { type?: App["type"]; variant?: string; slug?: string } | "">(
-    async (variables) => {
-      let type: string | null | undefined;
-      if (variables === "") {
-        type = _type;
-      } else {
-        type = variables.type;
-      }
-      const state: IntegrationOAuthCallbackState = {
-        returnTo:
-          WEBAPP_URL +
-          getInstalledAppPath(
-            { variant: variables && variables.variant, slug: variables && variables.slug },
-            location.search
-          ),
-      };
-      const stateStr = encodeURIComponent(JSON.stringify(state));
-      const searchParams = `?state=${stateStr}`;
+  const mutation = useMutation<
+    unknown,
+    Error,
+    { type?: App["type"]; variant?: string; slug?: string; omni?: boolean } | ""
+  >(async (variables) => {
+    let type: string | null | undefined;
+    let omni;
+    if (variables === "") {
+      type = _type;
+    } else {
+      omni = variables.omni;
+      type = variables.type;
+    }
+    const state: IntegrationOAuthCallbackState = {
+      returnTo:
+        WEBAPP_URL +
+        getInstalledAppPath(
+          { variant: variables && variables.variant, slug: variables && variables.slug },
+          location.search
+        ),
+    };
+    const stateStr = encodeURIComponent(JSON.stringify(state));
+    const searchParams = `?state=${stateStr}`;
 
-      const res = await fetch(`/api/integrations/${type}/add` + searchParams);
+    const res = await fetch(`/api/integrations/${type}/add` + searchParams);
 
-      if (!res.ok) {
-        const errorBody = await res.json();
-        throw new Error(errorBody.message || "Something went wrong");
-      }
+    if (!res.ok) {
+      const errorBody = await res.json();
+      throw new Error(errorBody.message || "Something went wrong");
+    }
 
-      const json = await res.json();
+    const json = await res.json();
+    if (!omni) {
       window.location.href = json.url;
-    },
-    options
-  );
+    }
+  }, options);
 
   return mutation;
 }
--- packages/app-store/giphy/components/index.ts
+++ packages/app-store/giphy/components/index.ts
@@ -1,2 +1,1 @@
-export { default as InstallAppButton } from "./InstallAppButton";
 export { default as SelectGifInput } from "./SelectGifInput";
