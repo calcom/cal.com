5d49c3ff8e2b277e700710626ebf050e23cdb7a7
Leo Giovanetti <hello@leog.me>
2022-09-28 15:02:12 -0300

    Fix connected calendar list

--- packages/trpc/server/routers/viewer.tsx
+++ packages/trpc/server/routers/viewer.tsx
@@ -625,7 +625,7 @@ const loggedInViewerRouter = createProtectedRouter()
     async resolve({ ctx }) {
       const { user } = ctx;
       // get user's credentials + their connected integrations
-      const calendarCredentials = getCalendarCredentials(user.credentials, user.id);
+      const calendarCredentials = getCalendarCredentials(user.credentials);
 
       // get all the connected integrations' calendars (from third party)
       const connectedCalendars = await getConnectedCalendars(calendarCredentials, user.selectedCalendars);
