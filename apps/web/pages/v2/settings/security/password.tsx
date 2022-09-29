3d12b88639237908c6690f9f4b3a9196b0201e63
Alex van Andel <me@alexvanandel.com>
2022-09-29 10:29:40 +0100

    Use PasswordField

--- apps/web/pages/v2/settings/security/password.tsx
+++ apps/web/pages/v2/settings/security/password.tsx
@@ -7,7 +7,7 @@ import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { trpc } from "@calcom/trpc/react";
 import Button from "@calcom/ui/v2/core/Button";
 import Meta from "@calcom/ui/v2/core/Meta";
-import { Form, TextField } from "@calcom/ui/v2/core/form/fields";
+import { Form, PasswordField } from "@calcom/ui/v2/core/form/fields";
 import { getLayout } from "@calcom/ui/v2/core/layouts/SettingsLayout";
 import showToast from "@calcom/ui/v2/core/notifications";
 
@@ -68,20 +68,10 @@ const PasswordView = () => {
         <Form form={formMethods} handleSubmit={handleSubmit}>
           <div className="max-w-[38rem] sm:flex sm:space-x-4">
             <div className="flex-grow">
-              <TextField
-                {...register("oldPassword")}
-                label={t("old_password")}
-                type="password"
-                placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;"
-              />
+              <PasswordField {...register("oldPassword")} label={t("old_password")} />
             </div>
             <div className="flex-grow">
-              <TextField
-                {...register("newPassword")}
-                label={t("new_password")}
-                type="password"
-                placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;"
-              />
+              <PasswordField {...register("newPassword")} label={t("new_password")} />
             </div>
           </div>
           <p className="text-sm text-gray-600">
