63eeee8ace221af0781f2ba65a45897e23b98fbd
Jeroen Reumkens <hello@jeroenreumkens.nl>
2022-09-30 14:26:29 +0200

    Remove unnecessary boolean check
    Co-authored-by: alannnc <alannnc@gmail.com>

--- apps/web/components/booking/pages/AvailabilityPage.tsx
+++ apps/web/components/booking/pages/AvailabilityPage.tsx
@@ -224,7 +224,7 @@ function TimezoneDropdown({
   const [isTimeOptionsOpen, setIsTimeOptionsOpen] = useState(false);
 
   useEffect(() => {
-    handleToggle24hClock(getIs24hClockFromLocalStorage() === true);
+    handleToggle24hClock(!!getIs24hClockFromLocalStorage());
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
