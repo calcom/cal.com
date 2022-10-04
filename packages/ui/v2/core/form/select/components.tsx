460a90660065b12a2adb09ef0e28dd5605a0470e
Jeroen Reumkens <hello@jeroenreumkens.nl>
2022-10-04 15:49:20 +0200

    #4306: Fixed input text color in select dark mode.

--- packages/ui/v2/core/form/select/components.tsx
+++ packages/ui/v2/core/form/select/components.tsx
@@ -24,7 +24,10 @@ export const InputComponent = <
   return (
     <reactSelectComponents.Input
       // disables our default form focus hightlight on the react-select input element
-      inputClassName={classNames("focus:ring-0 focus:ring-offset-0", inputClassName)}
+      inputClassName={classNames(
+        "focus:ring-0 focus:ring-offset-0 dark:!text-darkgray-900 !text-black",
+        inputClassName
+      )}
       {...props}
     />
   );
