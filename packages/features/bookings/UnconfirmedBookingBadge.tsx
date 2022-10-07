10f19288f4dfbe48c1010483f5b54867dea91a5c
Peer Richelsen <peer@cal.com>
2022-10-07 18:52:02 +0100

    added link to unconfirmed bookings

--- packages/features/bookings/UnconfirmedBookingBadge.tsx
+++ packages/features/bookings/UnconfirmedBookingBadge.tsx
@@ -1,8 +1,17 @@
+import Link from "next/link";
+
 import { trpc } from "@calcom/trpc/react";
 import Badge from "@calcom/ui/v2/core/Badge";
 
 export default function UnconfirmedBookingBadge() {
   const { data: unconfirmedBookingCount } = trpc.useQuery(["viewer.bookingUnconfirmedCount"]);
   if (!unconfirmedBookingCount) return null;
-  else return <Badge variant="red">{unconfirmedBookingCount}</Badge>;
+  else
+    return (
+      <Link href="/bookings/unconfirmed">
+        <a>
+          <Badge variant="orange">{unconfirmedBookingCount}</Badge>
+        </a>
+      </Link>
+    );
 }
