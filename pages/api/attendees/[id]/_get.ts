d4a2b8e791978ae7e1f6a7af28e374eae7eaa8c8
zomars <zomars@me.com>
2022-10-07 13:08:25 -0600

    Refactors attendees' endpoints

---
+++ pages/api/attendees/[id]/_get.ts
@@ -0,0 +1,39 @@
+import type { NextApiRequest } from "next";
+
+import { defaultResponder } from "@calcom/lib/server";
+
+import { schemaAttendeeReadPublic } from "@lib/validations/attendee";
+import { schemaQueryIdParseInt } from "@lib/validations/shared/queryIdTransformParseInt";
+
+/**
+ * @swagger
+ * /attendees/{id}:
+ *   get:
+ *     operationId: getAttendeeById
+ *     summary: Find an attendee
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: ID of the attendee to get
+ *         example: 3
+ *     tags:
+ *     - attendees
+ *     responses:
+ *       200:
+ *         description: OK
+ *       401:
+ *        description: Authorization information is missing or invalid.
+ *       404:
+ *         description: Attendee was not found
+ */
+export async function getHandler(req: NextApiRequest) {
+  const { prisma, query } = req;
+  const { id } = schemaQueryIdParseInt.parse(query);
+  const attendee = await prisma.attendee.findUnique({ where: { id } });
+  return { attendee: schemaAttendeeReadPublic.parse(attendee) };
+}
+
+export default defaultResponder(getHandler);
---
+++ pages/api/attendees/_post.ts
@@ -0,0 +1,77 @@
+import { HttpError } from "@/../../packages/lib/http-error";
+import type { NextApiRequest } from "next";
+
+import { defaultResponder } from "@calcom/lib/server";
+
+import { schemaAttendeeCreateBodyParams, schemaAttendeeReadPublic } from "@lib/validations/attendee";
+
+/**
+ * @swagger
+ * /attendees:
+ *   post:
+ *     operationId: addAttendee
+ *     summary: Creates a new attendee
+ *     requestBody:
+ *       description: Create a new attendee related to one of your bookings
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - bookingId
+ *               - name
+ *               - email
+ *               - timeZone
+ *             properties:
+ *               bookingId:
+ *                 type: number
+ *                 example: 1
+ *               email:
+ *                 type: string
+ *                 example: email@example.com
+ *               name:
+ *                 type: string
+ *                 example: John Doe
+ *               timeZone:
+ *                 type: string
+ *                 example: Europe/London
+ *     tags:
+ *     - attendees
+ *     responses:
+ *       201:
+ *         description: OK, attendee created
+ *       400:
+ *        description: Bad request. Attendee body is invalid.
+ *       401:
+ *        description: Authorization information is missing or invalid.
+ */
+async function postHandler(req: NextApiRequest) {
+  const { userId, isAdmin, prisma } = req;
+  const body = schemaAttendeeCreateBodyParams.parse(req.body);
+
+  if (!isAdmin) {
+    const userBooking = await prisma.booking.findFirst({
+      where: { userId, id: body.bookingId },
+      select: { id: true },
+    });
+    // Here we make sure to only return attendee's of the user's own bookings.
+    if (!userBooking) throw new HttpError({ statusCode: 401, message: "Unauthorized" });
+  }
+
+  const data = await prisma.attendee.create({
+    data: {
+      email: body.email,
+      name: body.name,
+      timeZone: body.timeZone,
+      booking: { connect: { id: body.bookingId } },
+    },
+  });
+
+  return {
+    attendee: schemaAttendeeReadPublic.parse(data),
+    message: "Attendee created successfully",
+  };
+}
+
+export default defaultResponder(postHandler);
