a25bc0c6d3ea04825a9110f824f8152dba1e4ffb
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2022-10-07 10:44:26 +0100

    Update apps/web/pages/v2/event-types/[type]/index.tsx
    Co-authored-by: Omar López <zomars@me.com>

--- apps/web/pages/v2/event-types/[type]/index.tsx
+++ apps/web/pages/v2/event-types/[type]/index.tsx
@@ -16,7 +16,6 @@ import { CAL_URL } from "@calcom/lib/constants";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import prisma from "@calcom/prisma";
 import { trpc } from "@calcom/trpc/react";
-import { TRPCError } from "@calcom/trpc/server";
 import type { BookingLimit, RecurringEvent } from "@calcom/types/Calendar";
 import { Form } from "@calcom/ui/form/fields";
 import { showToast } from "@calcom/ui/v2";
