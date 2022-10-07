36a0ebfbadf7db444f2540f60729d9534dce2e45
Syed Ali Shahbaz <52925846+alishaz-polymath@users.noreply.github.com>
2022-10-07 12:46:35 +0530

    Adds safe json parse of the body
    It ensures that the body complies with the safe parsing of the JSON so that if the body sent is not a valid JSON, we convert it into an empty JSON. Would improve in handling the response of such cases as a follow up for improved UX

--- pages/api/schedules/index.ts
+++ pages/api/schedules/index.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 import { getAvailabilityFromSchedule, DEFAULT_SCHEDULE } from "@calcom/lib/availability";
 
+import safeParseJSON from "@lib/helpers/safeParseJSON";
 import { withMiddleware } from "@lib/helpers/withMiddleware";
 import { ScheduleResponse, SchedulesResponse } from "@lib/types";
 import {
@@ -14,7 +15,7 @@ async function createOrlistAllSchedules(
   { method, body, userId, isAdmin, prisma }: NextApiRequest,
   res: NextApiResponse<SchedulesResponse | ScheduleResponse>
 ) {
-  const safe = schemaScheduleBodyParams.safeParse(body);
+  const safe = schemaScheduleBodyParams.safeParse(safeParseJSON(body));
 
   if (!safe.success) {
     res.status(400).json({ message: "Bad request" });
