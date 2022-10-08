5ab8c7656820f333868efee6b5a24632e41073e9
Kiran <kiran@boxyhq.com>
2022-10-08 19:04:48 +0530

    Add the condition to check SAML SSO is enabled

--- apps/web/pages/v2/settings/security/sso.tsx
+++ apps/web/pages/v2/settings/security/sso.tsx
@@ -2,7 +2,9 @@ import { useRouter } from "next/router";
 
 import SAMLConfiguration from "@calcom/features/ee/sso/components/SAMLConfiguration";
 import { HOSTED_CAL_FEATURES } from "@calcom/lib/constants";
+import { trpc } from "@calcom/trpc/react";
 import Meta from "@calcom/ui/v2/core/Meta";
+import SkeletonLoader from "@calcom/ui/v2/core/apps/SkeletonLoader";
 import { getLayout } from "@calcom/ui/v2/core/layouts/SettingsLayout";
 
 const SAMLSSO = () => {
@@ -12,10 +14,22 @@ const SAMLSSO = () => {
     router.replace("/404");
   }
 
+  const teamsView = false;
+
+  const { data: saml, isLoading } = trpc.useQuery(["viewer.saml.access", { teamsView }], {
+    onError: () => {
+      router.push("/settings");
+    },
+  });
+
+  if (isLoading) {
+    return <SkeletonLoader />;
+  }
+
   return (
     <>
       <Meta title="SAML SSO" description="Allow team members to login using an Identity Provider." />
-      <SAMLConfiguration teamsView={false} teamId={null} />
+      {saml && saml.enabled ? <SAMLConfiguration teamsView={teamsView} teamId={null} /> : null}
     </>
   );
 };
