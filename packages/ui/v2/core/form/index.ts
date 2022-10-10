7ed21b692eee81925c94b6210cdd0042d2a0c284
Jeroen Reumkens <hello@jeroenreumkens.nl>
2022-10-10 20:50:43 +0200

    #4306: Added darkmode support for select by styling via tailwind (#4824)
    Co-authored-by: Peer Richelsen <peeroke@gmail.com>
    Co-authored-by: Hariom Balhara <hariombalhara@gmail.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- packages/ui/v2/core/form/index.ts
+++ packages/ui/v2/core/form/index.ts
@@ -2,6 +2,6 @@ export * from "./radio-area";
 export * from "./fields";
 export { default as Checkbox } from "./Checkbox";
 export { default as DatePicker } from "./DatePicker";
-export { default as Select, SelectWithValidation, SelectField } from "./Select";
+export { default as Select, SelectWithValidation, SelectField, getReactSelectProps } from "./select";
 
 export { default as FormStep } from "./FormStep";
