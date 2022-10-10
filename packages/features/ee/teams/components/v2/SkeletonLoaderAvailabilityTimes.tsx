74758edd6fce2321ef1577795a1849cd9bfc0a7e
CarinaWolli <wollencarina@gmail.com>
2022-10-10 15:02:10 +0200

    code clean up

--- packages/features/ee/teams/components/v2/SkeletonLoaderAvailabilityTimes.tsx
+++ packages/features/ee/teams/components/v2/SkeletonLoaderAvailabilityTimes.tsx
@@ -3,12 +3,10 @@ import { SkeletonContainer, SkeletonText } from "@calcom/ui";
 function SkeletonLoader() {
   return (
     <SkeletonContainer>
-      <div className="">
-        <div className="mr-3">
-          <SkeletonText className="h-4 w-28" />
-          <SkeletonText className="mt-3 h-11 w-full" />
-          <SkeletonText className="mt-2 h-11 w-full" />
-        </div>
+      <div className="mr-3">
+        <SkeletonText className="h-4 w-28" />
+        <SkeletonText className="mt-3 h-11 w-full" />
+        <SkeletonText className="mt-2 h-11 w-full" />
       </div>
     </SkeletonContainer>
   );
