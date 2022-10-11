53275c18cd537d122e28904b6318c4ab80e728a8
CarinaWolli <wollencarina@gmail.com>
2022-10-11 16:47:30 +0200

    add first version of template cards with logos

--- packages/app-store/zapier/components/AppSettings.tsx
+++ packages/app-store/zapier/components/AppSettings.tsx
@@ -1,3 +1,60 @@
+import TemplateCard from "./TemplateCard";
+
+export type Template = {
+  icon: string;
+  app: string;
+  text: string;
+};
+
+const templates: Template[] = [
+  {
+    icon: "gmail.svg",
+    app: "Gmail",
+    text: "Send emails via Gemail for scheduled Cal.com events",
+  },
+  {
+    icon: "googleSheets.svg",
+    app: "Google Sheets",
+    text: "Create Google Sheets rows for scheduled Cal.com events",
+  },
+  {
+    icon: "salesforce.svg",
+    app: "Salesforce",
+    text: "Create Salesforce leads from new bookings in Cal.com ",
+  },
+  {
+    icon: "todoist.svg",
+    app: "Todoist",
+    text: "Create Todoist tasks for scheduled Cal.com events",
+  },
+  {
+    icon: "gmail.svg",
+    app: "Gmail",
+    text: "Send emails via Gmail for rescheduled Cal.com events",
+  },
+  {
+    icon: "gmail.svg",
+    app: "Gmail",
+    text: "Send emails via Gmail for cancelled Cal.com events",
+  },
+  {
+    icon: "gmail.svg",
+    app: "Gmail",
+    text: "Send emails via Gmail after scheduled Cal.com meetings end",
+  },
+  {
+    icon: "googleCalendar.svg",
+    app: "Google Calendar",
+    text: "Add new Cal.com bookings to Google Calendar",
+  },
+];
+
 export default function AppSettings() {
-  return <div className="border-t border-gray-200">Zapier Templates</div>;
+  return (
+    <div className="grid grid-cols-2 gap-4 border-t border-gray-200 p-4">
+      {templates.map((template, index) => (
+        <TemplateCard key={index} template={template} />
+      ))}
+    </div>
+  );
 }
---
+++ packages/app-store/zapier/components/TemplateCard.tsx
@@ -0,0 +1,24 @@
+import { Template } from "zapier/components/AppSettings";
+
+import { Button } from "@calcom/ui/v2";
+
+export default function TemplateCard({ template }: { template: Template }) {
+  return (
+    <div className="flex items-center justify-center rounded-md border border-gray-200 p-4">
+      <div className="flex items-center justify-center">
+        <div className="bordre-gray-100 flex h-12 w-12 items-center justify-center rounded-md border p-1">
+          <img className="h-8" src={`/api/app-store/zapier/${template.icon}`} />
+        </div>
+      </div>
+      <div className="mx-4">
+        <p className="truncate text-sm font-medium text-neutral-900">{template.app}</p>
+        <p className="mt-[2px] text-xs text-gray-500 ">{template.text}</p>
+      </div>
+      <div className="ml-auto">
+        <Button color="secondary" className="w-[90px]">
+          Use Zap
+        </Button>
+      </div>
+    </div>
+  );
+}
