5510b8111e593340c1af7004fb88e45d5815f9cc
CarinaWolli <wollencarina@gmail.com>
2022-10-12 14:15:40 +0200

    fix mobile view

--- packages/app-store/zapier/components/TemplateCard.tsx
+++ packages/app-store/zapier/components/TemplateCard.tsx
@@ -4,20 +4,29 @@ import { Button } from "@calcom/ui/v2";
 
 export default function TemplateCard({ template }: { template: Template }) {
   return (
-    <div className="flex items-center justify-center rounded-md border border-gray-200 p-4">
-      <div className="flex items-center justify-center">
-        <div className="bordre-gray-100 flex h-12 w-12 items-center justify-center rounded-md border p-1">
-          <img className="h-8" src={`/api/app-store/zapier/${template.icon}`} />
+    <div className="rounded-md border border-gray-200 p-4 ">
+      <div className="flex">
+        <div className="flex items-center justify-center">
+          <div className="mr-4 flex h-12 w-12 items-center justify-center rounded-md border p-1 ">
+            <img className="h-8" src={`/api/app-store/zapier/${template.icon}`} />
+          </div>
+        </div>
+        <div className="mr-4">
+          <p className="truncate text-sm font-medium text-neutral-900">{template.app}</p>
+          <p className="mt-[2px] text-xs text-gray-500 ">{template.text}</p>
+        </div>
+        <div className="ml-auto hidden sm:block">
+          <Button color="secondary" className="w-[90px] " target="_blank" href={template.link}>
+            Use Zap
+          </Button>
         </div>
       </div>
-      <div className="mx-4">
-        <p className="truncate text-sm font-medium text-neutral-900">{template.app}</p>
-        <p className="mt-[2px] text-xs text-gray-500 ">{template.text}</p>
-      </div>
-      <div className="ml-auto">
-        <Button color="secondary" className="w-[90px]" target="_blank" href={template.link}>
-          Use Zap
-        </Button>
+      <div className="mt-2 block w-full sm:hidden">
+        <div className="float-right">
+          <Button color="secondary" className="w-[90px]" target="_blank" href={template.link}>
+            Use Zap
+          </Button>
+        </div>
       </div>
     </div>
   );
