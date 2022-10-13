74308ddefc95aac0c2876dcd981c0e7110aa4aa2
CarinaWolli <wollencarina@gmail.com>
2022-10-13 11:36:30 +0200

    code clean up

--- packages/app-store/_components/AppSettings.tsx
+++ packages/app-store/_components/AppSettings.tsx
@@ -2,10 +2,10 @@ import dynamic from "next/dynamic";
 
 import { DynamicComponent } from "./DynamicComponent";
 
-export const AppSetupMap = {
+export const AppSettingsMap = {
   zapier: dynamic(() => import("../zapier/components/AppSettings")),
 };
 
 export const AppSettings = (props: { slug: string }) => {
-  return <DynamicComponent<typeof AppSetupMap> componentMap={AppSetupMap} {...props} />;
+  return <DynamicComponent<typeof AppSettingsMap> componentMap={AppSettingsMap} {...props} />;
 };
