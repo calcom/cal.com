6d5d851b98b3e0b97b288dc3f2f50b887b3a5c09
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-10-14 14:13:04 -0400

    Query once router.query.id is read

--- packages/features/ee/teams/pages/team-members-view.tsx
+++ packages/features/ee/teams/pages/team-members-view.tsx
@@ -25,6 +25,7 @@ const MembersView = () => {
     onError: () => {
       router.push("/settings");
     },
+    enabled: !!router.query.id,
   });
 
   const {
@@ -35,6 +36,7 @@ const MembersView = () => {
     isFetchingNextPage,
   } = trpc.useInfiniteQuery(["viewer.teams.getMembers", { teamId: Number(router.query.id), limit: 5 }], {
     getNextPageParam: (lastPage) => lastPage.nextCursor,
+    enabled: !!router.query.id,
   });
 
   const [showMemberInvitationModal, setShowMemberInvitationModal] = useState(false);
