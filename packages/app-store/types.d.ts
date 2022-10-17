6f15b2bb3a9b12741dcf2f6dd8187161b5c3140f
Hariom Balhara <hariombalhara@gmail.com>
2022-10-17 21:47:07 +0530

    Make URL prop available to apps

--- packages/app-store/types.d.ts
+++ packages/app-store/types.d.ts
@@ -22,9 +22,11 @@ export interface InstallAppButtonProps {
   ) => JSX.Element;
   onChanged?: () => unknown;
 }
-
-export type EventTypeAppCardComponent = React.FC<{
+export type EventTypeAppCardComponentProps = {
   // Limit what data should be accessible to apps
-  eventType: Pick<z.infer<typeof _EventTypeModel>, "id", "title" | "description" | "teamId" | "length">;
+  eventType: Pick<z.infer<typeof _EventTypeModel>, "id" | "title" | "description" | "teamId" | "length"> & {
+    URL: string;
+  };
   app: inferQueryOutput<"viewer.apps">[number];
-}>;
+};
+export type EventTypeAppCardComponent = React.FC<EventTypeAppCardComponentProps>;
