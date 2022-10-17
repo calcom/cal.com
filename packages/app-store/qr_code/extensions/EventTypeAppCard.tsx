7224f121a1d1741fd0d89ece89588d744db2a8f3
Peer Richelsen <peer@cal.com>
2022-10-17 16:45:33 +0100

    added QR code settings, needs eventType.URL

--- packages/app-store/qr_code/extensions/EventTypeAppCard.tsx
+++ packages/app-store/qr_code/extensions/EventTypeAppCard.tsx
@@ -3,14 +3,32 @@ import { useState } from "react";
 import { useAppContextWithSchema } from "@calcom/app-store/EventTypeAppContext";
 import AppCard from "@calcom/app-store/_components/AppCard";
 import type { EventTypeAppCardComponent } from "@calcom/app-store/types";
-import { Icon } from "@calcom/ui";
+import { Tooltip } from "@calcom/ui/v2";
 
 import { appDataSchema } from "../zod";
 
 const EventTypeAppCard: EventTypeAppCardComponent = function EventTypeAppCard({ eventType, app }) {
   const [getAppData, setAppData] = useAppContextWithSchema<typeof appDataSchema>();
-  const isSunrise = getAppData("isSunrise");
   const [enabled, setEnabled] = useState(getAppData("enabled"));
+
+  const eventTypeURL = "https://cal.dev/pro/30min"; // TODO: eventType.URL
+
+  function QRCode({ size, data }: { size: number; data: string }) {
+    const QR_URL = "https://api.qrserver.com/v1/create-qr-code/?size=" + size + "&data=" + data;
+    return (
+      <Tooltip content={eventTypeURL}>
+        <a download href={QR_URL} target="_blank" rel="noreferrer">
+          <img
+            className="border hover:bg-gray-50 hover:shadow-sm"
+            style={{ padding: size / 16, borderRadius: size / 20 }}
+            width={size}
+            src={QR_URL}
+            alt={eventTypeURL}
+          />
+        </a>
+      </Tooltip>
+    );
+  }
 
   return (
     <AppCard
@@ -19,22 +37,15 @@ const EventTypeAppCard: EventTypeAppCardComponent = function EventTypeAppCard({ 
       switchOnClick={(e) => {
         if (!e) {
           setEnabled(false);
-          setAppData("isSunrise", false);
         } else {
           setEnabled(true);
-          setAppData("isSunrise", true);
         }
       }}
       switchChecked={enabled}>
-      <div className="mt-2 text-sm">
-        <div className="flex">
-          <span className="mr-2">{isSunrise ? <Icon.FiSunrise /> : <Icon.FiSunset />}</span>I am an AppCard
-          for Event with Title: {eventType.title}
-        </div>{" "}
-        <div className="mt-2">
-          Edit <span className="italic">packages/app-store/{app.slug}/extensions/EventTypeAppCard.tsx</span>{" "}
-          to play with me
-        </div>
+      <div className="max-w-60 flex items-baseline justify-between gap-2 text-sm ">
+        <QRCode size={256} data={eventTypeURL} />
+        <QRCode size={128} data={eventTypeURL} />
+        <QRCode size={64} data={eventTypeURL} />
       </div>
     </AppCard>
   );
