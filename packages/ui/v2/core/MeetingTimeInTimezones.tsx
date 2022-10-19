dc7fcbb9ff4a9483680f274e6c393720a79b21da
Jeroen Reumkens <hello@jeroenreumkens.nl>
2022-10-19 08:27:15 +0200

    #3334; Added stoppropagation to meeting timezones popover to prevent click bubbling to row click.

--- packages/ui/v2/core/MeetingTimeInTimezones.tsx
+++ packages/ui/v2/core/MeetingTimeInTimezones.tsx
@@ -52,14 +52,16 @@ const MeetingTimeInTimezones = ({
 
   return (
     <Popover.Root>
-      <Popover.Trigger className="popover-button ml-2 inline-flex h-5 w-5 items-center justify-center rounded-sm text-gray-900 transition-colors hover:bg-gray-200 focus:bg-gray-200">
+      <Popover.Trigger
+        onClick={preventBubbling}
+        className="popover-button ml-2 inline-flex h-5 w-5 items-center justify-center rounded-sm text-gray-900 transition-colors hover:bg-gray-200 focus:bg-gray-200">
         <Icon.FiGlobe />
       </Popover.Trigger>
       <Popover.Portal>
         <Popover.Content
-          onClick={(e) => e.stopPropagation()}
+          onClick={preventBubbling}
           side="top"
-          className="slideInBottom shadow-dropdown border-5 bg-brand-500 rounded-md border-gray-200 p-3 text-sm text-white shadow-sm">
+          className="popover-content slideInBottom shadow-dropdown border-5 bg-brand-500 rounded-md border-gray-200 p-3 text-sm text-white shadow-sm">
           {times.map((time) => (
             <span className="mt-2 block first:mt-0" key={time.timezone}>
               <span className="inline-flex align-baseline">
@@ -81,5 +83,12 @@ const MeetingTimeInTimezones = ({
 };
 
 MeetingTimeInTimezones.displayName = "MeetingTimeInTimezones";
+
+// Prevents propagation so the click on eg booking overview won't
+// bubble to the row of the table, causing a navigation to the
+// detaill page.
+const preventBubbling = (event: React.MouseEvent) => {
+  event.stopPropagation();
+};
 
 export default MeetingTimeInTimezones;
