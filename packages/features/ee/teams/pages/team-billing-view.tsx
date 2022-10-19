33ff213fe03227775a817c94c54cb5d542768fe0
Joe Au-Yeung <j.auyeung419@gmail.com>
2022-10-19 17:28:40 -0400

    Create settings page

--- packages/features/ee/teams/pages/team-billing-view.tsx
+++ packages/features/ee/teams/pages/team-billing-view.tsx
@@ -5,25 +5,33 @@ import { Controller, useForm } from "react-hook-form";
 import { classNames } from "@calcom/lib";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { trpc } from "@calcom/trpc/react";
+import { Icon } from "@calcom/ui";
 import { Button, Form, showToast, Switch } from "@calcom/ui/v2/core";
 import Meta from "@calcom/ui/v2/core/Meta";
 import { getLayout } from "@calcom/ui/v2/core/layouts/SettingsLayout";
 
 const BillingView = () => {
+  const { t } = useLocale();
+
   return (
     <>
       <Meta title="Team Billing" description="Manage billing for your team" />
 
-      <section className={classNames("flex flex-col sm:flex-row", className)}>
+      <div className="flex flex-col text-sm sm:flex-row">
         <div>
-          <h2 className="font-medium">Billing</h2>
-          <p>Billing details</p>
+          <h2 className="font-medium">{t("billing_manage_details_title")}</h2>
+          <p>{t("billing_manage_details_description")}</p>
         </div>
         <div className="flex-shrink-0 pt-3 sm:ml-auto sm:pt-0 sm:pl-3">
-          <p>Button</p>
+          <Button
+            color="primary"
+            href="/api/integrations/stripepayment/portal"
+            target="_blank"
+            EndIcon={Icon.FiExternalLink}>
+            {t("billing_portal")}
+          </Button>
         </div>
-      </section>
-      <hr className="border-neutral-200" />
+      </div>
     </>
   );
 };
