4b11cc0a6197f7ff1b7f61da69a503a9b03b0df2
Hariom Balhara <hariombalhara@gmail.com>
2022-10-19 15:38:32 +0530

    Fix Fallback message dark theme

--- packages/app-store/ee/routing-forms/pages/routing-link/[...appPages].tsx
+++ packages/app-store/ee/routing-forms/pages/routing-link/[...appPages].tsx
@@ -192,8 +192,8 @@ function RoutingForm({ form, profile, ...restProps }: inferSSRProps<typeof getSe
         ) : (
           <div className="mx-auto my-0 max-w-3xl md:my-24">
             <div className="w-full max-w-4xl ltr:mr-2 rtl:ml-2">
-              <div className="main -mx-4 rounded-md border border-neutral-200 bg-white p-4 py-6 sm:mx-0 sm:px-8">
-                <div>{customPageMessage}</div>
+              <div className="main dark:bg-darkgray-100 sm:dark:border-darkgray-300 -mx-4 rounded-md border border-neutral-200 bg-white p-4 py-6 sm:mx-0 sm:px-8">
+                <div className="dark:text-white">{customPageMessage}</div>
               </div>
             </div>
           </div>
