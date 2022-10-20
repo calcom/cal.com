19453a1a789916d1babf1e265ec6563da81a967f
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2022-10-20 17:15:52 +0530

    fix: mobile view added for /event-type header

--- apps/web/components/v2/eventtype/EventTypeSingleLayout.tsx
+++ apps/web/components/v2/eventtype/EventTypeSingleLayout.tsx
@@ -23,6 +23,7 @@ import {
 } from "@calcom/ui/v2";
 import ConfirmationDialogContent from "@calcom/ui/v2/core/ConfirmationDialogContent";
 import { Dialog } from "@calcom/ui/v2/core/Dialog";
+import Divider from "@calcom/ui/v2/core/Divider";
 import Dropdown, {
   DropdownMenuContent,
   DropdownMenuItem,
@@ -172,11 +173,11 @@ function EventTypeSingleLayout({
       subtitle={eventType.description || ""}
       CTA={
         <div className="flex items-center justify-end">
-          <div className="flex items-center rounded-md px-2 sm:hover:bg-gray-100">
+          <div className="hidden items-center rounded-md px-2 sm:flex sm:hover:bg-gray-100">
             <Skeleton
               as={Label}
               htmlFor="hiddenSwitch"
-              className="mt-2 hidden cursor-pointer self-center pr-2 sm:inline">
+              className="mt-2 hidden cursor-pointer self-center whitespace-nowrap pr-2 sm:inline">
               {t("hide_from_profile")}
             </Skeleton>
             <Switch
@@ -263,6 +264,24 @@ function EventTypeSingleLayout({
                   className="min-w-full">
                   {t("delete")}
                 </Button>
+              </DropdownMenuItem>
+              <Divider />
+              <DropdownMenuItem className="focus:ring-gray-100">
+                <div className="flex items-center rounded-md py-1.5 px-4 sm:hidden sm:hover:bg-gray-100">
+                  <Skeleton
+                    as={Label}
+                    htmlFor="hiddenSwitch"
+                    className="mt-2 inline cursor-pointer self-center pr-2 sm:hidden">
+                    {t("hide_from_profile")}
+                  </Skeleton>
+                  <Switch
+                    id="hiddenSwitch"
+                    defaultChecked={formMethods.getValues("hidden")}
+                    onCheckedChange={(e) => {
+                      formMethods.setValue("hidden", e);
+                    }}
+                  />
+                </div>
               </DropdownMenuItem>
             </DropdownMenuContent>
           </Dropdown>
