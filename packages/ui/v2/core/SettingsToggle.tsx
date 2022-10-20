8789528660698a01fe39aeacefb72d5d968aee67
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2022-10-20 15:51:10 +0100

    addTestId

--- packages/ui/v2/core/SettingsToggle.tsx
+++ packages/ui/v2/core/SettingsToggle.tsx
@@ -9,18 +9,34 @@ type Props = {
   title: string;
   description?: string;
   checked: boolean;
+  disabled?: boolean;
   onCheckedChange?: (checked: boolean) => void;
+  "data-testid"?: string;
 };
 
-function SettingsToggle({ checked, onCheckedChange, description, title, children }: Props) {
+function SettingsToggle({
+  checked,
+  onCheckedChange,
+  description,
+  title,
+  children,
+  disabled,
+  ...rest
+}: Props) {
   const [animateRef] = useAutoAnimate<HTMLDivElement>();
 
   return (
     <>
-      <div className="flex flex-col space-y-4 lg:flex-row lg:space-y-0 lg:space-x-4">
-        <fieldset className="block flex-col sm:flex">
+      <div className="flex w-full flex-col space-y-4 lg:flex-row lg:space-y-0 lg:space-x-4">
+        <fieldset className="block w-full flex-col sm:flex">
           <div className="flex space-x-3">
-            <Switch fitToHeight={true} checked={checked} onCheckedChange={onCheckedChange} />
+            <Switch
+              data-testid={rest["data-testid"]}
+              fitToHeight={true}
+              checked={checked}
+              onCheckedChange={onCheckedChange}
+              disabled={disabled}
+            />
 
             <div className="">
               <Label className="text-sm font-semibold leading-none text-black">{title}</Label>
