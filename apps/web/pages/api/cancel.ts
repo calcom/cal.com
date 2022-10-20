d4d502a35393c66a5f56a2b44a951115acae6567
Leo Giovanetti <hello@leog.me>
2022-10-20 19:18:30 -0300

    Cancel only for delete and post verbs

--- apps/web/pages/api/cancel.ts
+++ apps/web/pages/api/cancel.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest } from "next";
 
 import handleCancelBooking from "@calcom/features/bookings/lib/handleCancelBooking";
 import { getSession } from "@calcom/lib/auth";
-import { defaultResponder } from "@calcom/lib/server";
+import { defaultResponder, defaultHandler } from "@calcom/lib/server";
 
 async function handler(req: NextApiRequest & { userId?: number }) {
   const session = await getSession({ req });
@@ -11,4 +11,7 @@ async function handler(req: NextApiRequest & { userId?: number }) {
   return await handleCancelBooking(req);
 }
 
-export default defaultResponder(handler);
+export default defaultHandler({
+  DELETE: Promise.resolve({ default: defaultResponder(handler) }),
+  POST: Promise.resolve({ default: defaultResponder(handler) }),
+});
