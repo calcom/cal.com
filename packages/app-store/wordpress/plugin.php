4ff5d0807f436a495d7f73ce85e1a33665e00a91
Peer Richelsen <peer@cal.com>
2022-10-21 21:40:02 +0100

    added wordpress plugin

--- packages/app-store/wordpress/plugin.php
+++ packages/app-store/wordpress/plugin.php
@@ -11,12 +11,12 @@
  * Text Domain:       calcom-embed
  */
 
-function cal_embed_shortcode( $atts, $content = null) {
+function cal_shortcode( $atts, $content = null) {
 global $post;extract(shortcode_atts(array(
 'for' => $post->post_title,
 ), $atts));
 if(empty($content)) $content='Embed Cal.com';
 
-return '<script>(function (C, A, L){let p=function (a, ar){a.q.push(ar);}; let d=C.document; C.Cal=C.Cal || function (){let cal=C.Cal; let ar=arguments; if (!cal.loaded){cal.ns={}; cal.q=cal.q || []; d.head.appendChild(d.createElement("script")).src=A; cal.loaded=true;}if (ar[0]===L){const api=function (){p(api, arguments);}; const namespace=ar[1]; api.q=api.q || []; typeof namespace==="string" ? (cal.ns[namespace]=api) && p(api, ar) : p(cal, ar); return;}p(cal, ar);};})(window, "https://cal.com/embed.js", "init"); Cal("init") </script> <script>Cal("inline",{calLink: '.$content.', config:{name: "John Doe",},});</script>'
+return '<script>(function (C, A, L){let p=function (a, ar){a.q.push(ar);}; let d=C.document; C.Cal=C.Cal || function (){let cal=C.Cal; let ar=arguments; if (!cal.loaded){cal.ns={}; cal.q=cal.q || []; d.head.appendChild(d.createElement("script")).src=A; cal.loaded=true;}if (ar[0]===L){const api=function (){p(api, arguments);}; const namespace=ar[1]; api.q=api.q || []; typeof namespace==="string" ? (cal.ns[namespace]=api) && p(api, ar) : p(cal, ar); return;}p(cal, ar);};})(window, "https://cal.com/embed.js", "init"); Cal("init") </script> <script>Cal("inline",{calLink: '.$content.'});</script>'
 }
-add_shortcode('cal_embed', 'cal_embed_shortcode');
+add_shortcode('cal', 'cal_shortcode');
