42dbbf4773bf1891b1197795b8b5b8783b1150a0
zomars <zomars@me.com>
2022-10-25 12:48:28 -0600

    Removes team creation modal

--- apps/web/pages/teams/index.tsx
+++ apps/web/pages/teams/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+import { WEBAPP_URL } from "@calcom/lib/constants";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { trpc } from "@calcom/trpc/react";
 import { Icon } from "@calcom/ui/Icon";
@@ -9,12 +10,10 @@ import Button from "@calcom/ui/v2/core/Button";
 import EmptyScreen from "@calcom/ui/v2/core/EmptyScreen";
 
 import SkeletonLoaderTeamList from "@components/team/SkeletonloaderTeamList";
-import TeamCreateModal from "@components/team/TeamCreateModal";
 import TeamList from "@components/team/TeamList";
 
 function Teams() {
   const { t } = useLocale();
-  const [showCreateTeamModal, setShowCreateTeamModal] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
 
   const { data, isLoading } = trpc.useQuery(["viewer.teams.list"], {
@@ -25,22 +24,22 @@ function Teams() {
 
   const teams = data?.filter((m) => m.accepted) || [];
   const invites = data?.filter((m) => !m.accepted) || [];
+  const handleNewTeam = () => {
+    // Hey
+  };
 
   return (
     <Shell
       heading={t("teams")}
       subtitle={t("create_manage_teams_collaborative")}
       CTA={
-        <Button type="button" onClick={() => setShowCreateTeamModal(true)}>
+        <Button type="button" href={`${WEBAPP_URL}/settings/teams/new`}>
           <Icon.FiPlus className="inline-block h-3.5 w-3.5 text-white group-hover:text-black ltr:mr-2 rtl:ml-2" />
           {t("new")}
         </Button>
       }>
       <>
         {!!errorMessage && <Alert severity="error" title={errorMessage} />}
-        {showCreateTeamModal && (
-          <TeamCreateModal isOpen={showCreateTeamModal} onClose={() => setShowCreateTeamModal(false)} />
-        )}
         {invites.length > 0 && (
           <div className="mb-4">
             <h1 className="mb-2 text-lg font-medium">{t("open_invitations")}</h1>
@@ -54,11 +53,10 @@ function Teams() {
             headline={t("no_teams")}
             description={t("no_teams_description")}
             buttonRaw={
-              <Button color="secondary" onClick={() => setShowCreateTeamModal(true)}>
+              <Button color="secondary" href={`${WEBAPP_URL}/settings/teams/new`}>
                 {t("create_team")}
               </Button>
             }
-            buttonOnClick={() => setShowCreateTeamModal(true)}
           />
         )}
         {teams.length > 0 && <TeamList teams={teams} />}
