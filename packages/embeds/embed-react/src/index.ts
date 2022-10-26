07790c6da355351d7feae9c9bbdaf80c1cabf368
Hariom Balhara <hariombalhara@gmail.com>
2022-10-26 20:04:43 +0530

    Add support to derive embedJsUrl from calOrigin

--- packages/embeds/embed-react/src/index.ts
+++ packages/embeds/embed-react/src/index.ts
@@ -1,10 +1,29 @@
 import { CalWindow } from "@calcom/embed-core";
+import EmbedSnippet from "@calcom/embed-snippet";
 
-import Cal from "./Cal";
+import Cal, { getEmbedJsUrl } from "./Cal";
 
-export const getCalApi = (ns: string): Promise<CalWindow["Cal"]> =>
+/**
+ *  TODO: Run these tests and then delete the comment
+ * - Test getCalApi with ns
+ * - Test getCalApi without ns
+ */
+
+/**
+ * It can be used independently of the `Cal` component. So, a component can import and call just this method to get 'floating button popup' or 'element click popup working'
+ */
+export const getCalApi = ({
+  embedJsUrl,
+  calOrigin,
+  ns,
+}: {
+  embedJsUrl?: string;
+  calOrigin?: string;
+  ns?: string;
+}): Promise<CalWindow["Cal"]> =>
   new Promise(function tryReadingFromWindow(resolve) {
     let api = (window as CalWindow).Cal;
+    EmbedSnippet(getEmbedJsUrl({ embedJsUrl, calOrigin }));
 
     if (!api) {
       setTimeout(() => {
@@ -12,12 +31,18 @@ export const getCalApi = (ns: string): Promise<CalWindow["Cal"]> =>
       }, 50);
       return;
     }
+
     if (ns) {
+      api("init", ns, {
+        origin: calOrigin,
+      });
       api = api.ns && api.ns[ns];
-      if (!api) {
-        throw new Error(`Cal namespace:${ns} not found`);
-      }
+    } else {
+      api("init", {
+        origin: calOrigin,
+      });
     }
+
     resolve(api);
   });
 
