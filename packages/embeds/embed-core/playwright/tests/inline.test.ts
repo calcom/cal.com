f943bb3813f489a30817024e5eda0eff3966d952
Hariom Balhara <hariombalhara@gmail.com>
2022-10-27 14:54:50 +0530

    Restrjucture tests

--- packages/embeds/embed-core/playwright/tests/inline.test.ts
+++ packages/embeds/embed-core/playwright/tests/inline.test.ts
@@ -4,72 +4,46 @@ import { test } from "@calcom/web/playwright/lib/fixtures";
 
 import { bookFirstEvent, deleteAllBookingsByEmail, getEmbedIframe, todo } from "../lib/testUtils";
 
-test("Inline Iframe - Configured with Dark Theme", async ({
-  page,
-  getActionFiredDetails,
-  addEmbedListeners,
-}) => {
-  await deleteAllBookingsByEmail("embed-user@example.com");
-  await addEmbedListeners("");
-  await page.goto("/?only=ns:default");
-  const embedIframe = await getEmbedIframe({ page, pathname: "/pro" });
-  expect(embedIframe).toBeEmbedCalLink("", getActionFiredDetails, {
-    pathname: "/pro",
-    searchParams: {
-      theme: "dark",
-    },
+describe("Inline Embed", () => {
+  test("User Link configured with Dark Theme", async ({ page, getActionFiredDetails, addEmbedListeners }) => {
+    await deleteAllBookingsByEmail("embed-user@example.com");
+    await addEmbedListeners("");
+    await page.goto("/?only=ns:default");
+    const embedIframe = await getEmbedIframe({ page, pathname: "/pro" });
+    expect(embedIframe).toBeEmbedCalLink("", getActionFiredDetails, {
+      pathname: "/pro",
+      searchParams: {
+        theme: "dark",
+      },
+    });
+    expect(await page.screenshot()).toMatchSnapshot("event-types-list.png");
+    if (!embedIframe) {
+      throw new Error("Embed iframe not found");
+    }
+    await bookFirstEvent("pro", embedIframe, page);
+    await deleteAllBookingsByEmail("embed-user@example.com");
   });
-  expect(await page.screenshot()).toMatchSnapshot("event-types-list.png");
-  if (!embedIframe) {
-    throw new Error("Embed iframe not found");
-  }
-  await bookFirstEvent("pro", embedIframe, page);
-  await deleteAllBookingsByEmail("embed-user@example.com");
-});
 
-test("Ensure that on all pages - [user], [user]/[type], team/[slug], team/[slug]/book, UI styling works if these pages are directly linked in embed", async ({
-  page,
-  getActionFiredDetails,
-  addEmbedListeners,
-}) => {
-  await deleteAllBookingsByEmail("embed-user@example.com");
-  await addEmbedListeners("");
-  await page.goto("/?only=ns:fourth");
-  const embedIframe = await getEmbedIframe({ page, pathname: "/team/seeded-team" });
-  expect(embedIframe).toBeEmbedCalLink("", getActionFiredDetails, {
-    pathname: "/team/seeded-team",
+  test("Team Links List", async ({ page, getActionFiredDetails, addEmbedListeners }) => {
+    await deleteAllBookingsByEmail("embed-user@example.com");
+    await addEmbedListeners("fourth");
+    await page.goto("/?only=ns:fourth");
+
+    const embedIframe = await getEmbedIframe({ page, pathname: "/team/seeded-team" });
+    expect(embedIframe).toBeEmbedCalLink("", getActionFiredDetails, {
+      pathname: "/team/seeded-team",
+    });
+    expect(await page.screenshot()).toMatchSnapshot("event-types-list.png");
+    if (!embedIframe) {
+      throw new Error("Embed iframe not found");
+    }
+    await bookFirstEvent("team/seeded-team", embedIframe, page);
+    await deleteAllBookingsByEmail("embed-user@example.com");
   });
-  expect(await page.screenshot()).toMatchSnapshot("event-types-list.png");
-  if (!embedIframe) {
-    throw new Error("Embed iframe not found");
-  }
-  await bookFirstEvent("team/seeded-team", embedIframe, page);
-  await deleteAllBookingsByEmail("embed-user@example.com");
+
+  todo(
+    "Ensure that on all pages - [user], [user]/[type], team/[slug], team/[slug]/book, UI styling works if these pages are directly linked in embed"
+  );
+
+  todo("Check that UI Configuration doesn't work for Free Plan");
 });
-
-test("Ensure that on all pages - [user], [user]/[type], team/[slug], team/[slug]/book, UI styling works if these pages are directly linked in embed", async ({
-  page,
-  getActionFiredDetails,
-  addEmbedListeners,
-}) => {
-  await deleteAllBookingsByEmail("embed-user@example.com");
-  await addEmbedListeners("fourth");
-  await page.goto("/?only=ns:fourth");
-
-  const embedIframe = await getEmbedIframe({ page, pathname: "/team/seeded-team" });
-  expect(embedIframe).toBeEmbedCalLink("", getActionFiredDetails, {
-    pathname: "/team/seeded-team",
-  });
-  expect(await page.screenshot()).toMatchSnapshot("event-types-list.png");
-  if (!embedIframe) {
-    throw new Error("Embed iframe not found");
-  }
-  await bookFirstEvent("team/seeded-team", embedIframe, page);
-  await deleteAllBookingsByEmail("embed-user@example.com");
-});
-
-todo(
-  "Ensure that on all pages - [user], [user]/[type], team/[slug], team/[slug]/book, UI styling works if these pages are directly linked in embed"
-);
-
-todo("Check that UI Configuration doesn't work for Free Plan");
