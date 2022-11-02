f715034008da9c269a0d3e30e692edf364693977
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2022-11-02 13:33:30 +0000

    Add intro

--- apps/storybook_V2/.storybook/main.js
+++ apps/storybook_V2/.storybook/main.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
   stories: [
+    "../intro.stories.mdx",
     "../../../packages/ui/components/**/*.stories.mdx",
     "../../../packages/ui/components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
