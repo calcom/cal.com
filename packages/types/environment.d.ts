a786dea3a1328b4786118152b241cc67cf39d2de
maxi <maximilian.oehrlein@clicksports.de>
2022-11-02 08:23:40 +0100

    added two new variables for appName and support mail address. so everybody can change it via env

--- packages/types/environment.d.ts
+++ packages/types/environment.d.ts
@@ -60,5 +60,7 @@ declare namespace NodeJS {
      * Set it to "1" if you need to run E2E tests locally
      **/
     readonly NEXT_PUBLIC_IS_E2E: "1" | undefined;
+    readonly NEXT_PUBLIC_APP_NAME: string | "Cal";
+    readonly NEXT_PUBLIC_SUPPORT_MAIL_ADDRESS: string | "help@cal.com";
   }
 }
