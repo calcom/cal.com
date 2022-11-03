08856364b04d91af34bcbce9c9257378f46c9756
Jeroen Reumkens <hello@jeroenreumkens.nl>
2022-11-03 10:56:43 +0100

    Explained postcss pseudo plugin.

--- apps/storybook/postcss.config.js
+++ apps/storybook/postcss.config.js
@@ -2,6 +2,15 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    /**
+     * We use this postcss plugin to create custom css classes for
+     * any pseudo classes (eg hover, focus) mentioned below.
+     * This way you can eg show a hover state in Storybook by adding
+     * this classname .sb-pseudo--hover to the item.
+     *
+     * These styles will only be added in storybook, and will NOT
+     * end up in the final css bundle of apps using the components.
+     */
     "postcss-pseudo-companion-classes": {
       prefix: "sb-pseudo--",
       // We have to keep a restrictTo list here, because otherwise
