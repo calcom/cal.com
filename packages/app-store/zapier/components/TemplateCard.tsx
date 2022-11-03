7ee388d214e22309fbad259b2228ff508901f5c5
123om123 <omihridesh@gmail.com>
2022-11-03 15:20:39 -0400

    changed styles for Zap automation ui to match figma

--- packages/app-store/zapier/components/TemplateCard.tsx
+++ packages/app-store/zapier/components/TemplateCard.tsx
@@ -4,10 +4,10 @@ import { Template } from "./AppSettings";
 
 export default function TemplateCard({ template }: { template: Template }) {
   return (
-    <div className="rounded-md border border-gray-200 p-4 ">
-      <div className="flex items-center justify-center">
+    <div className="rounded-md border border-gray-200 bg-white p-4">
+      <div className="flex items-center justify-start">
         <div>
-          <div className="mr-4 flex h-12 w-12 items-center justify-center rounded-md border p-1 ">
+          <div className="mr-4 flex h-12 w-12 items-center justify-center rounded-md p-1">
             <img className="h-8" alt={template.app} src={`/api/app-store/zapier/${template.icon}`} />
           </div>
         </div>
@@ -17,11 +17,11 @@ export default function TemplateCard({ template }: { template: Template }) {
             <p className="mt-[2px] text-xs text-gray-500 ">{template.text}</p>
           </div>
         </div>
-        <div className="ml-auto hidden sm:block">
-          <Button color="secondary" className="w-[90px] " target="_blank" href={template.link}>
-            Use Zap
-          </Button>
-        </div>
+      </div>
+      <div className="float-right ml-auto mt-auto hidden sm:block">
+        <Button color="secondary" className="w-[90px] " target="_blank" href={template.link}>
+          Use Zap
+        </Button>
       </div>
       <div className="mt-2 block w-full sm:hidden">
         <div className="float-right">
