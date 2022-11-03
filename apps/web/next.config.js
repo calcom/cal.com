e6a5fe179b110e599fa23d7ee59f2ada7b1d5cbb
alannnc <alannnc@gmail.com>
2022-11-03 04:28:52 -0700

    feature/sentry.io for web (#5326)
    * Initial commits that install sentry.io on web app
    
    * Updates yarn.lock
    
    * Update yarn.lock
    
    Co-authored-by: Hariom Balhara <hariombalhara@gmail.com>

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,6 @@
 require("dotenv").config({ path: "../../.env" });
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { withSentryConfig } = require("@sentry/nextjs");
 
 const withTM = require("next-transpile-modules")([
   "@calcom/app-store",
@@ -228,6 +229,18 @@ const nextConfig = {
 
     return redirects;
   },
+  sentry: {
+    hideSourceMaps: true,
+  },
 };
 
-module.exports = () => plugins.reduce((acc, next) => next(acc), nextConfig);
+const sentryWebpackPluginOptions = {
+  silent: true, // Suppresses all logs
+};
+
+const moduleExports = () => plugins.reduce((acc, next) => next(acc), nextConfig);
+
+// Sentry should be the last thing to export to catch everything right
+module.exports = process.env.NEXT_PUBLIC_SENTRY_DSN
+  ? withSentryConfig(moduleExports, sentryWebpackPluginOptions)
+  : moduleExports;
