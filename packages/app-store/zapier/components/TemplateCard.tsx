44e6fbf569496ced1a4545637cc3531892f5aea9
123om123 <omihridesh@gmail.com>
2022-11-04 12:00:24 -0400

    fixed some styling issues

--- packages/app-store/zapier/components/TemplateCard.tsx
+++ packages/app-store/zapier/components/TemplateCard.tsx
@@ -4,7 +4,7 @@ import { Template } from "./AppSettings";
 
 export default function TemplateCard({ template }: { template: Template }) {
   return (
-    <div className="rounded-md border border-gray-200 bg-white p-4">
+    <div className="rounded-md border border-gray-200 bg-white p-5">
       <div className="flex items-center justify-start">
         <div>
           <div className="mr-4 flex h-12 w-12 items-center justify-center rounded-md p-1">
@@ -13,8 +13,8 @@ export default function TemplateCard({ template }: { template: Template }) {
         </div>
         <div className="mr-4 ">
           <div>
-            <p className="truncate text-sm font-medium text-neutral-900">{template.app}</p>
-            <p className="mt-[2px] text-xs text-gray-500 ">{template.text}</p>
+            <p className="truncate text-sm font-medium leading-4 text-neutral-900">{template.app}</p>
+            <p className="mt-[2px] text-sm text-gray-500">{template.text}</p>
           </div>
         </div>
       </div>
