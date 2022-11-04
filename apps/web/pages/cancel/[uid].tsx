0e67b2d63aac7ec27ff0c7200fd137107172e5bd
CarinaWolli <wollencarina@gmail.com>
2022-11-04 10:17:09 +0100

    redirect form old to new cancel page

---
+++ apps/web/pages/cancel/[uid].tsx
@@ -0,0 +1,24 @@
+import { GetServerSidePropsContext } from "next";
+import z from "zod";
+
+const querySchema = z.object({
+  uid: z.string(),
+  allRemainingBookings: z
+    .string()
+    .optional()
+    .transform((val) => (val ? JSON.parse(val) : false)),
+});
+
+export default function Type() {
+  return null;
+}
+
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  const { allRemainingBookings, uid } = querySchema.parse(context.query);
+  return {
+    redirect: {
+      permanent: false,
+      destination: `/success?uid=${uid}&allRemainingBookings=${allRemainingBookings}&cancel=true`,
+    },
+  };
+};
