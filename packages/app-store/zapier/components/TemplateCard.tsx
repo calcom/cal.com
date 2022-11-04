9ce23403a7b784b7aa9bd29df9ad604e978fb5b6
123om123 <omihridesh@gmail.com>
2022-11-04 12:46:53 -0400

    fixed issue with buttons being at different heights

--- packages/app-store/zapier/components/TemplateCard.tsx
+++ packages/app-store/zapier/components/TemplateCard.tsx
@@ -5,21 +5,25 @@ import { Template } from "./AppSettings";
 export default function TemplateCard({ template }: { template: Template }) {
   return (
     <div className="rounded-md border border-gray-200 bg-white p-5">
-      <div className="flex items-center justify-start">
+      <div className="min-h-14 flex items-center justify-start">
         <div>
           <div className="mr-4 flex h-12 w-12 items-center justify-center rounded-md p-1">
             <img className="h-8" alt={template.app} src={`/api/app-store/zapier/${template.icon}`} />
           </div>
         </div>
-        <div className="mr-4 ">
+        <div className="mr-4">
           <div>
             <p className="truncate text-sm font-medium leading-4 text-neutral-900">{template.app}</p>
             <p className="mt-[2px] text-sm text-gray-500">{template.text}</p>
           </div>
         </div>
       </div>
-      <div className="float-right ml-auto mt-4 hidden sm:block">
-        <Button color="secondary" className="w-[90px] " target="_blank" href={template.link}>
+      <div className="relative float-right ml-auto mt-4 hidden h-full sm:block">
+        <Button
+          color="secondary"
+          className="absolute bottom-0 right-0 w-[90px]"
+          target="_blank"
+          href={template.link}>
           Use Zap
         </Button>
       </div>
