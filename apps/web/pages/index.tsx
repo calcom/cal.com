056f2c33f4fcd28a10d26895b5ed27e81453d94e
Ben Kirchner <ben@mento.co>
2022-11-08 13:51:38 -0700

    Redirect to mento marketing on index for verification

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -9,7 +9,8 @@ function RedirectPage() {
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
   if (!session?.user?.id) {
-    return { redirect: { permanent: false, destination: "/auth/login" } };
+    return { redirect: { destination: "https://mento.co" } };
+    // return { redirect: { permanent: false, destination: "/auth/login" } };
   }
 
   return { redirect: { permanent: false, destination: "/event-types" } };
