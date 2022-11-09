05aa71298d08dc5f5632f9b21448154de7b11454
CarinaWolli <wollencarina@gmail.com>
2022-11-09 08:36:45 +0100

    add scroll

--- packages/app-store/ee/routing-forms/components/SingleForm.tsx
+++ packages/app-store/ee/routing-forms/components/SingleForm.tsx
@@ -318,64 +318,68 @@ function SingleForm({ form, appUrl, Page }: SingleFormComponentProps) {
       <Dialog open={isTestPreviewOpen} onOpenChange={setIsTestPreviewOpen}>
         <DialogContent type="creation" useOwnActionButtons={true} title={t("test_routing_form")}>
           <p className="text-sm text-gray-400">{t("test_preview_description")}</p>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              testRouting();
-            }}>
-            {form && <FormInputFields form={form} response={response} setResponse={setResponse} />}
-            <div>
-              {decidedAction && (
-                <div className="mt-5 rounded-md bg-gray-100 p-3">
-                  <div className="font-bold ">{t("route_to")}:</div>
-                  <div className="mt-2">
-                    {RoutingPages.map((page) => {
-                      if (page.value === decidedAction.type) {
-                        return <>{page.label}</>;
-                      }
-                    })}
-                    :{" "}
-                    {decidedAction.type === "customPageMessage" ? (
-                      <span className="text-gray-700">{decidedAction.value}</span>
-                    ) : decidedAction.type === "externalRedirectUrl" ? (
-                      <span className="text-gray-700 underline">
-                        <a
-                          target="_blank"
-                          href={
-                            decidedAction.value.includes("https://") ||
-                            decidedAction.value.includes("http://")
-                              ? decidedAction.value
-                              : `http://${decidedAction.value}`
-                          }
-                          rel="noreferrer">
-                          {decidedAction.value}
-                        </a>
-                      </span>
-                    ) : (
-                      <span className="text-gray-700 underline">
-                        <a target="_blank" href={`/${decidedAction.value}`} rel="noreferrer">
-                          {decidedAction.value}
-                        </a>
-                      </span>
-                    )}
+          <div>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                testRouting();
+              }}>
+              <div className="max-h-64 overflow-scroll px-1 sm:max-h-96">
+                {form && <FormInputFields form={form} response={response} setResponse={setResponse} />}
+              </div>
+              <div>
+                {decidedAction && (
+                  <div className="mt-5 rounded-md bg-gray-100 p-3">
+                    <div className="font-bold ">{t("route_to")}:</div>
+                    <div className="mt-2">
+                      {RoutingPages.map((page) => {
+                        if (page.value === decidedAction.type) {
+                          return <>{page.label}</>;
+                        }
+                      })}
+                      :{" "}
+                      {decidedAction.type === "customPageMessage" ? (
+                        <span className="text-gray-700">{decidedAction.value}</span>
+                      ) : decidedAction.type === "externalRedirectUrl" ? (
+                        <span className="text-gray-700 underline">
+                          <a
+                            target="_blank"
+                            href={
+                              decidedAction.value.includes("https://") ||
+                              decidedAction.value.includes("http://")
+                                ? decidedAction.value
+                                : `http://${decidedAction.value}`
+                            }
+                            rel="noreferrer">
+                            {decidedAction.value}
+                          </a>
+                        </span>
+                      ) : (
+                        <span className="text-gray-700 underline">
+                          <a target="_blank" href={`/${decidedAction.value}`} rel="noreferrer">
+                            {decidedAction.value}
+                          </a>
+                        </span>
+                      )}
+                    </div>
                   </div>
-                </div>
-              )}
-            </div>
-            <DialogFooter>
-              <DialogClose asChild>
-                <Button
-                  color="secondary"
-                  onClick={() => {
-                    setIsTestPreviewOpen(false);
-                    setDecidedAction(null);
-                  }}>
-                  {t("close")}
-                </Button>
-              </DialogClose>
-              <Button type="submit">{t("Test Routing")}</Button>
-            </DialogFooter>
-          </form>
+                )}
+              </div>
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button
+                    color="secondary"
+                    onClick={() => {
+                      setIsTestPreviewOpen(false);
+                      setDecidedAction(null);
+                    }}>
+                    {t("close")}
+                  </Button>
+                </DialogClose>
+                <Button type="submit">{t("Test Routing")}</Button>
+              </DialogFooter>
+            </form>
+          </div>
         </DialogContent>
       </Dialog>
     </>
