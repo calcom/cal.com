7e3aab88bae94273f5ea025751f3c5f7f11c3902
Om Ray <38233712+om-ray@users.noreply.github.com>
2022-11-11 13:24:01 -0500

    Update packages/lib/findDurationType.ts
    Co-authored-by: Omar López <zomars@me.com>

--- packages/lib/findDurationType.ts
+++ packages/lib/findDurationType.ts
@@ -1,11 +1,8 @@
 import { MINUTES_IN_DAY, MINUTES_IN_HOUR } from "./convertToNewDurationType";
 
 export default function findDurationType(value: number) {
-  if (value % MINUTES_IN_DAY === 0) {
-    return "days";
-  } else if (value % MINUTES_IN_HOUR === 0) {
-    return "hours";
-  } else {
-    return "minutes";
+  if (value % MINUTES_IN_DAY === 0) return "days";
+  if (value % MINUTES_IN_HOUR === 0) return "hours";
+  return "minutes";
   }
 }
