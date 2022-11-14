113216b3b9c7d2ebd013ea4ddecc49b0b3f2966a
maxi <maximilian.oehrlein@clicksports.de>
2022-11-14 09:01:33 +0100

    Updated AdminAppsView with dynamic translations. Added translations for german and english files only.

--- apps/web/pages/settings/admin/apps.tsx
+++ apps/web/pages/settings/admin/apps.tsx
@@ -1,11 +1,13 @@
+import { useLocale } from "@calcom/lib/hooks/useLocale";
 import Meta from "@calcom/ui/v2/core/Meta";
 import { getLayout } from "@calcom/ui/v2/core/layouts/AdminLayout";
 
 function AdminAppsView() {
+  const { t } = useLocale();
   return (
     <>
-      <Meta title="Apps" description="apps_description" />
-      <h1>App listing</h1>
+      <Meta title={t("apps")} description={t("apps_description")} />
+      <h1>{t("apps_listing")}</h1>
     </>
   );
 }
