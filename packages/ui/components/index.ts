9a79980cd8fd6727b8aeb9d67ca23d08b305834f
zomars <zomars@me.com>
2022-11-14 16:25:50 -0700

    Fixes

--- packages/ui/components/index.ts
+++ packages/ui/components/index.ts
@@ -4,5 +4,5 @@ export * from "./breadcrumb";
 export * from "./button";
 export * from "./buttonGroup";
 export * from "./form";
-export { TopBanner } from "./topBanner";
-export type { TopBannerProps } from "./topBanner";
+export { TopBanner } from "./top-banner";
+export type { TopBannerProps } from "./top-banner";
