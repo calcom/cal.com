3ca5de017b12d91f8b85aab0b8ae2196b24f5b8a
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2022-11-16 00:39:37 +0530

    fix: make locations option

--- packages/prisma/zod/custom/eventtype.ts
+++ packages/prisma/zod/custom/eventtype.ts
@@ -8,9 +8,10 @@ export const createEventTypeInput = _EventTypeModel
     length: true,
     teamId: true,
     schedulingType: true,
-    hidden: true
+    hidden: true,
+    locations: true
   })
-  .partial({ hidden: true })
+  .partial({ hidden: true,locations: true})
   .refine((data) => (data.teamId ? data.teamId && data.schedulingType : true), {
     path: ["schedulingType"],
     message: "You must select a scheduling type for team events",
