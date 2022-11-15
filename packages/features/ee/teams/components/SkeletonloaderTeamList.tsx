0b44efb584e225645e37719012ee0f7ea2c5b050
Jeroen Reumkens <hello@jeroenreumkens.nl>
2022-11-15 15:23:58 +0100

    Fixed skeletons for teams and workflows.

--- packages/features/ee/teams/components/SkeletonloaderTeamList.tsx
+++ packages/features/ee/teams/components/SkeletonloaderTeamList.tsx
@@ -19,18 +19,18 @@ function SkeletonItem() {
     <li className="group flex w-full items-center justify-between px-3 py-4">
       <div className="flex-grow truncate text-sm">
         <div className="flex justify-start space-x-2 px-2">
-          <SkeletonText className="h-3 w-3 rounded-full" />
-          <div className="space-y-2">
-            <SkeletonText className="h-1 w-8" />
-            <SkeletonText className="h-1 w-4" />
+          <SkeletonText className="h-10 w-10 rounded-full" />
+          <div className="flex flex-col space-y-2">
+            <SkeletonText className="h-4 w-32" />
+            <SkeletonText className="h-4 w-16" />
           </div>
         </div>
       </div>
       <div className="mt-4 hidden flex-shrink-0 pr-4 sm:mt-0 sm:ml-5 lg:flex">
         <div className="flex justify-between space-x-2 rtl:space-x-reverse">
-          <SkeletonText className="h-1 w-3" />
-          <SkeletonText className="h-1 w-1" />
-          <SkeletonText className="h-1 w-1" />
+          <SkeletonText className="h-4 w-12" />
+          <SkeletonText className="h-4 w-4" />
+          <SkeletonText className="h-4 w-4" />
         </div>
       </div>
     </li>
