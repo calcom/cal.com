073814fb3bccb528bfdbc4e391478e9eb78a2e96
Leo Giovanetti <hello@leog.me>
2022-11-17 13:30:24 -0300

    Testing rearranging exports

--- packages/app-store/sendgrid/api/check.ts
+++ packages/app-store/sendgrid/api/check.ts
@@ -1,27 +1,5 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import { defaultHandler } from "@calcom/lib/server";
 
-import Sendgrid from "@calcom/lib/Sendgrid";
-
-import checkSession from "../../_utils/auth";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
-    const { api_key } = req.body;
-    if (!api_key) res.status(400).json({ message: "No Api Key provided to check" });
-
-    checkSession(req);
-
-    const sendgrid: Sendgrid = new Sendgrid(api_key);
-
-    try {
-      const usernameInfo = await sendgrid.username();
-      if (usernameInfo.username) {
-        return res.status(200).end();
-      } else {
-        return res.status(404).end();
-      }
-    } catch (e) {
-      return res.status(500).json({ message: e });
-    }
-  }
-}
+export default defaultHandler({
+  POST: import("./_postCheck"),
+});
