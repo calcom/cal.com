fd6fd19e3dbaadfa0b98014adcca2c8f1119390e
askprateek <askpk123@gmail.com>
2022-11-18 14:50:52 +0530

    [UPDATED] UI of Sign in with Google Button.

--- packages/app-store/googlecalendar/components/InstallAppButton.tsx
+++ packages/app-store/googlecalendar/components/InstallAppButton.tsx
@@ -4,15 +4,25 @@ import useAddAppMutation from "../../_utils/useAddAppMutation";
 
 export default function InstallAppButton(props: InstallAppButtonProps) {
   const mutation = useAddAppMutation("google_calendar");
-
   return (
     <>
-      {props.render({
-        onClick() {
-          mutation.mutate("");
-        },
-        loading: mutation.isLoading,
-      })}
+      <button onClick={() => mutation.mutate("")}>
+        <img
+          alt="Sign in with Google"
+          src="https://teamepyc.github.io/cdn/pesto/btn_google_signin_dark_normal_web@2x.png"
+          style={{ height: 44 + "px" }}
+        />
+      </button>
     </>
   );
+  // return (
+  //   <>
+  //     {props.render({
+  //       onClick() {
+  //         mutation.mutate("");
+  //       },
+  //       loading: mutation.isLoading,
+  //     })}
+  //   </>
+  // );
 }
