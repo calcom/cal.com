76ba69800b896af6f815476040dbc1e106f7fd25
CarinaWolli <wollencarina@gmail.com>
2022-11-21 17:26:04 +0100

    code clean up

--- packages/features/ee/workflows/components/v2/SkeletonLoaderList.tsx
+++ packages/features/ee/workflows/components/v2/SkeletonLoaderList.tsx
@@ -3,13 +3,11 @@ import { SkeletonText } from "@calcom/ui/v2";
 
 function SkeletonLoader() {
   return (
-    <>
-      <ul className="mt-24 animate-pulse divide-y divide-neutral-200 rounded-md border border-gray-200 bg-white sm:overflow-hidden">
-        <SkeletonItem />
-        <SkeletonItem />
-        <SkeletonItem />
-      </ul>
-    </>
+    <ul className="mt-24 animate-pulse divide-y divide-neutral-200 rounded-md border border-gray-200 bg-white sm:overflow-hidden">
+      <SkeletonItem />
+      <SkeletonItem />
+      <SkeletonItem />
+    </ul>
   );
 }
 
