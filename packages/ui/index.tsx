d6996c0707dbe5305c55680994f57c19eff88b61
zomars <zomars@me.com>
2022-11-22 14:20:18 -0700

    Making explicit indexes

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,5 +1,30 @@
-export { default as Button } from "./Button";
+// export { default as Button } from "./Button";
+export {
+  Avatar,
+  AvatarGroup,
+  Breadcrumb,
+  BreadcrumbContainer,
+  BreadcrumbItem,
+  Button,
+  Checkbox,
+  EmailField,
+  EmailInput,
+  FieldsetLegend,
+  Form,
+  HintsOrErrors,
+  Input,
+  InputField,
+  InputGroupBox,
+  InputLeading,
+  Label,
+  PasswordField,
+  TextArea,
+  TextAreaField,
+  TextField,
+  TopBanner,
+} from "./components";
+export type { AvatarProps, BadgeProps, ButtonBaseProps, ButtonProps, TopBannerProps } from "./components";
 export { default as Select } from "./form/Select";
+export * from "./Icon";
 export { default as Loader } from "./Loader";
 export { default as Tooltip } from "./Tooltip";
-export * from "./Icon";
