2c7702cd7f737d77340c89f9a84586bbfeef61ef
Alan <alannnc@gmail.com>
2022-11-23 16:10:54 -0700

    Fix isDirty state for appearance settings

--- apps/web/pages/settings/my-account/appearance.tsx
+++ apps/web/pages/settings/my-account/appearance.tsx
@@ -43,9 +43,18 @@ const AppearanceView = () => {
 
   const utils = trpc.useContext();
   const { data: user, isLoading } = trpc.viewer.me.useQuery();
-  const formMethods = useForm();
+
+  const formMethods = useForm({
+    defaultValues: {
+      theme: user?.theme,
+      brandColor: user?.brandColor || "#292929",
+      darkBrandColor: user?.darkBrandColor || "#fafafa",
+      hideBranding: user?.hideBranding,
+    },
+  });
+
   const {
-    formState: { isSubmitting },
+    formState: { isSubmitting, isDirty },
   } = formMethods;
 
   const mutation = trpc.viewer.updateProfile.useMutation({
@@ -60,141 +69,136 @@ const AppearanceView = () => {
 
   if (isLoading) return <SkeletonLoader />;
 
-  const fields = formMethods.watch();
+  if (!user) return null;
 
-  if (user) {
-    const isDisabled =
-      isSubmitting ||
-      (fields.theme === user.theme &&
-        fields.brandColor === user.brandColor &&
-        fields.darkBrandColor === user.darkBrandColor &&
-        fields.hideBranding === user.hideBranding);
+  const isDisabled = isSubmitting || !isDirty;
 
-    return (
-      <Form
-        form={formMethods}
-        handleSubmit={(values) => {
-          mutation.mutate({
-            ...values,
-            // Radio values don't support null as values, therefore we convert an empty string
-            // back to null here.
-            theme: values.theme || null,
-          });
-        }}>
-        <Meta title="Appearance" description="Manage settings for your booking appearance" />
-        <div className="mb-6 flex items-center text-sm">
-          <div>
-            <p className="font-semibold">{t("theme")}</p>
-            <p className="text-gray-600">{t("theme_applies_note")}</p>
-          </div>
+  return (
+    <Form
+      form={formMethods}
+      handleSubmit={(values) => {
+        mutation.mutate({
+          ...values,
+          // Radio values don't support null as values, therefore we convert an empty string
+          // back to null here.
+          theme: values.theme || null,
+        });
+      }}>
+      <Meta title="Appearance" description="Manage settings for your booking appearance" />
+      <div className="mb-6 flex items-center text-sm">
+        <div>
+          <p className="font-semibold">{t("theme")}</p>
+          <p className="text-gray-600">{t("theme_applies_note")}</p>
         </div>
-        <div className="flex flex-col justify-between sm:flex-row">
-          <ThemeLabel
-            variant="system"
-            value={null}
-            label={t("theme_system")}
-            defaultChecked={user.theme === null}
-            register={formMethods.register}
-          />
-          <ThemeLabel
-            variant="light"
-            value="light"
-            label={t("theme_light")}
-            defaultChecked={user.theme === "light"}
-            register={formMethods.register}
-          />
-          <ThemeLabel
-            variant="dark"
-            value="dark"
-            label={t("theme_dark")}
-            defaultChecked={user.theme === "dark"}
-            register={formMethods.register}
-          />
-        </div>
+      </div>
+      <div className="flex flex-col justify-between sm:flex-row">
+        <ThemeLabel
+          variant="system"
+          value={null}
+          label={t("theme_system")}
+          defaultChecked={user.theme === null}
+          register={formMethods.register}
+        />
+        <ThemeLabel
+          variant="light"
+          value="light"
+          label={t("theme_light")}
+          defaultChecked={user.theme === "light"}
+          register={formMethods.register}
+        />
+        <ThemeLabel
+          variant="dark"
+          value="dark"
+          label={t("theme_dark")}
+          defaultChecked={user.theme === "dark"}
+          register={formMethods.register}
+        />
+      </div>
 
-        <hr className="border-1 my-8 border-neutral-200" />
-        <div className="mb-6 flex items-center text-sm">
-          <div>
-            <p className="font-semibold">{t("custom_brand_colors")}</p>
-            <p className="mt-0.5 leading-5 text-gray-600">{t("customize_your_brand_colors")}</p>
-          </div>
+      <hr className="border-1 my-8 border-neutral-200" />
+      <div className="mb-6 flex items-center text-sm">
+        <div>
+          <p className="font-semibold">{t("custom_brand_colors")}</p>
+          <p className="mt-0.5 leading-5 text-gray-600">{t("customize_your_brand_colors")}</p>
         </div>
+      </div>
 
-        <div className="block justify-between sm:flex">
-          <Controller
-            name="brandColor"
-            control={formMethods.control}
-            defaultValue={user.brandColor}
-            render={() => (
-              <div>
-                <p className="mb-2 block text-sm font-medium text-gray-900">{t("light_brand_color")}</p>
-                <ColorPicker
-                  defaultValue={user.brandColor}
-                  onChange={(value) => formMethods.setValue("brandColor", value)}
-                />
-              </div>
-            )}
-          />
-          <Controller
-            name="darkBrandColor"
-            control={formMethods.control}
-            defaultValue={user.darkBrandColor}
-            render={() => (
-              <div className="mt-6 sm:mt-0">
-                <p className="mb-2 block text-sm font-medium text-gray-900">{t("dark_brand_color")}</p>
-                <ColorPicker
-                  defaultValue={user.darkBrandColor}
-                  onChange={(value) => formMethods.setValue("darkBrandColor", value)}
-                />
-              </div>
-            )}
-          />
-        </div>
-        {/* TODO future PR to preview brandColors */}
-        {/* <Button
+      <div className="block justify-between sm:flex">
+        <Controller
+          name="brandColor"
+          control={formMethods.control}
+          defaultValue={user.brandColor}
+          render={() => (
+            <div>
+              <p className="mb-2 block text-sm font-medium text-gray-900">{t("light_brand_color")}</p>
+              <ColorPicker
+                defaultValue={user.brandColor}
+                onChange={(value) => formMethods.setValue("brandColor", value, { shouldDirty: true })}
+              />
+            </div>
+          )}
+        />
+        <Controller
+          name="darkBrandColor"
+          control={formMethods.control}
+          defaultValue={user.darkBrandColor}
+          render={() => (
+            <div className="mt-6 sm:mt-0">
+              <p className="mb-2 block text-sm font-medium text-gray-900">{t("dark_brand_color")}</p>
+              <ColorPicker
+                defaultValue={user.darkBrandColor}
+                onChange={(value) => formMethods.setValue("darkBrandColor", value, { shouldDirty: true })}
+              />
+            </div>
+          )}
+        />
+      </div>
+      {/* TODO future PR to preview brandColors */}
+      {/* <Button
         color="secondary"
         EndIcon={Icon.FiExternalLink}
         className="mt-6"
         onClick={() => window.open(`${WEBAPP_URL}/${user.username}/${user.eventTypes[0].title}`, "_blank")}>
         Preview
       </Button> */}
-        <hr className="border-1 my-8 border-neutral-200" />
-        <Controller
-          name="hideBranding"
-          control={formMethods.control}
-          defaultValue={user.hideBranding}
-          render={({ field: { value } }) => (
-            <>
-              <div className="flex w-full text-sm">
-                <div className="mr-1 flex-grow">
-                  <div className="flex items-center">
-                    <p className="mr-2 font-semibold">{t("disable_cal_branding")}</p>
-                    <Badge variant="gray">{t("pro")}</Badge>
-                  </div>
-                  <p className="mt-0.5  text-gray-600">{t("removes_cal_branding")}</p>
+      <hr className="border-1 my-8 border-neutral-200" />
+      <Controller
+        name="hideBranding"
+        control={formMethods.control}
+        defaultValue={user.hideBranding}
+        render={({ field: { value } }) => (
+          <>
+            <div className="flex w-full text-sm">
+              <div className="mr-1 flex-grow">
+                <div className="flex items-center">
+                  <p className="mr-2 font-semibold">{t("disable_cal_branding")}</p>
+                  <Badge variant="gray">{t("pro")}</Badge>
                 </div>
-                <div className="flex-none">
-                  <Switch
-                    id="hideBranding"
-                    onCheckedChange={(checked) => formMethods.setValue("hideBranding", checked)}
-                    checked={value}
-                  />
-                </div>
+                <p className="mt-0.5  text-gray-600">{t("removes_cal_branding")}</p>
               </div>
-            </>
-          )}
-        />
-        <Button
-          disabled={isDisabled}
-          type="submit"
-          loading={mutation.isLoading}
-          color="primary"
-          className="mt-8">
-          {t("update")}
-        </Button>
-      </Form>
-    );
-  }
+              <div className="flex-none">
+                <Switch
+                  id="hideBranding"
+                  onCheckedChange={(checked) =>
+                    formMethods.setValue("hideBranding", checked, { shouldDirty: true })
+                  }
+                  checked={value}
+                />
+              </div>
+            </div>
+          </>
+        )}
+      />
+      <Button
+        disabled={isDisabled}
+        type="submit"
+        loading={mutation.isLoading}
+        color="primary"
+        className="mt-8">
+        {t("update")}
+      </Button>
+    </Form>
+  );
 };
 
 AppearanceView.getLayout = getLayout;
