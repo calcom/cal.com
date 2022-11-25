93a14b190e8be8cb6f86be005cf5f75ee562e9f0
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2022-11-25 12:36:33 +0000

    Fix Impersonation Refresh Bug (#5679)
    Co-authored-by: Peer Richelsen <peeroke@gmail.com>

--- packages/ui/v2/core/layouts/AdminLayout.tsx
+++ packages/ui/v2/core/layouts/AdminLayout.tsx
@@ -17,7 +17,7 @@ export default function AdminLayout({
   // Force redirect on component level
   useEffect(() => {
     if (session.data && session.data.user.role !== UserPermissionRole.ADMIN) {
-      router.replace("/settings");
+      router.replace("/settings/my-account/profile");
     }
   }, [session, router]);
 
