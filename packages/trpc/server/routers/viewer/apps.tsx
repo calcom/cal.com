147b47dfe3be084bc75cce0812888f7a52a9c312
Joe <j.auyeung419@gmail.com>
2022-11-27 21:59:36 -0500

    Fix lark calendar metadata

--- packages/trpc/server/routers/viewer/apps.tsx
+++ packages/trpc/server/routers/viewer/apps.tsx
@@ -104,8 +104,6 @@ export const appsRouter = router({
     .mutation(async ({ ctx, input }) => {
       const { prisma } = ctx;
 
-      console.log(input);
-
       // Get app name from metadata
       const localApps = getLocalAppMetadata();
       const appMetadata = localApps.find((localApp) => localApp.slug === input.slug);
@@ -127,7 +125,6 @@ export const appsRouter = router({
           keys: undefined,
         },
       });
-      console.log("🚀 ~ file: apps.tsx ~ line 120 ~ .mutation ~ app", app);
 
       // If disabling an app then we need to alert users basesd on the app type
       if (input.enabled) {
