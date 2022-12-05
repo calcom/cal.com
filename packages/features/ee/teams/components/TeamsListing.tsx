35a7d5aedf72dc72c1b15c0c7746feebfb7ddc28
Peer Richelsen <peer@cal.com>
2022-12-05 23:06:24 +0100

    nit

--- packages/features/ee/teams/components/TeamsListing.tsx
+++ packages/features/ee/teams/components/TeamsListing.tsx
@@ -53,39 +53,37 @@ export function TeamsListing() {
       {!teams.length && !isLoading && (
         <>
           {isCalcom ? (
-            <>
-              <div className="max-w-[1118px]">
-                <div
-                  className="flex w-full justify-between overflow-hidden rounded-lg pt-4 pb-10 md:min-h-[295px] md:pt-10"
-                  style={{
-                    background: "url(/team-banner-background.jpg)",
-                    backgroundSize: "cover",
-                    backgroundRepeat: "no-repeat",
-                  }}>
-                  <div className="mt-3 px-14">
-                    <h1 className="font-cal text-3xl">{t("calcom_is_better_with_team")}</h1>
-                    <p className="my-4 max-w-sm text-gray-600">{t("add_your_team_members")}</p>
-                    <div className="space-y-2 sm:space-x-2">
-                      <Button color="primary" href={`${WEBAPP_URL}/settings/teams/new`}>
-                        {t("create_team")}
-                      </Button>
-                      <Button href="https://go.cal.com/teams-video" target="_blank" color="secondary">
-                        {t("learn_more")}
-                      </Button>
-                    </div>
+            <div className="max-w-[1118px]">
+              <div
+                className="flex w-full justify-between overflow-hidden rounded-lg pt-4 pb-10 md:min-h-[295px] md:pt-10"
+                style={{
+                  background: "url(/team-banner-background.jpg)",
+                  backgroundSize: "cover",
+                  backgroundRepeat: "no-repeat",
+                }}>
+                <div className="mt-3 px-14">
+                  <h1 className="font-cal text-3xl">{t("calcom_is_better_with_team")}</h1>
+                  <p className="my-4 max-w-sm text-gray-600">{t("add_your_team_members")}</p>
+                  <div className="space-y-2 sm:space-x-2">
+                    <Button color="primary" href={`${WEBAPP_URL}/settings/teams/new`}>
+                      {t("create_team")}
+                    </Button>
+                    <Button href="https://go.cal.com/teams-video" target="_blank" color="secondary">
+                      {t("learn_more")}
+                    </Button>
                   </div>
                 </div>
-                <div className="mt-6 grid-cols-3 md:grid md:gap-6">
-                  {features.map((feature) => (
-                    <div key={feature.title} className="mb-6 min-h-[180px] w-full rounded-md bg-gray-50 p-8">
-                      {feature.icon}
-                      <h2 className="font-cal mt-4 text-lg">{feature.title}</h2>
-                      <p className="text-gray-700">{feature.description}</p>
-                    </div>
-                  ))}
-                </div>
               </div>
-            </>
+              <div className="mt-6 grid-cols-3 md:grid md:gap-6">
+                {features.map((feature) => (
+                  <div key={feature.title} className="mb-6 min-h-[180px] w-full rounded-md bg-gray-50 p-8">
+                    {feature.icon}
+                    <h2 className="font-cal mt-4 text-lg">{feature.title}</h2>
+                    <p className="text-gray-700">{feature.description}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
           ) : (
             <EmptyScreen
               Icon={Icon.FiUsers}
