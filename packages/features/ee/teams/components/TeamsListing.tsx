f6946bd9f12fe8f6091273f6e6d8515331958cf9
Peer Richelsen <peer@cal.com>
2022-12-05 23:05:08 +0100

    show old emptyscreen for self-hosters

--- packages/features/ee/teams/components/TeamsListing.tsx
+++ packages/features/ee/teams/components/TeamsListing.tsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 
 import { WEBAPP_URL } from "@calcom/lib/constants";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
+import isCalcom from "@calcom/lib/isCalcom";
 import { trpc } from "@calcom/trpc/react";
-import { Alert, Button, Icon } from "@calcom/ui";
+import { Alert, Button, EmptyScreen, Icon } from "@calcom/ui";
 
 import SkeletonLoaderTeamList from "./SkeletonloaderTeamList";
 import TeamList from "./TeamList";
@@ -51,37 +52,52 @@ export function TeamsListing() {
       {isLoading && <SkeletonLoaderTeamList />}
       {!teams.length && !isLoading && (
         <>
-          <div className="max-w-[1118px]">
-            <div
-              className="flex w-full justify-between overflow-hidden rounded-lg pt-4 pb-10 md:min-h-[295px] md:pt-10"
-              style={{
-                background: "url(/team-banner-background.jpg)",
-                backgroundSize: "cover",
-                backgroundRepeat: "no-repeat",
-              }}>
-              <div className="mt-3 px-14">
-                <h1 className="font-cal text-3xl">{t("calcom_is_better_with_team")}</h1>
-                <p className="my-4 max-w-sm text-gray-600">{t("add_your_team_members")}</p>
-                <div className="space-y-2 sm:space-x-2">
-                  <Button color="primary" href={`${WEBAPP_URL}/settings/teams/new`}>
-                    {t("create_team")}
-                  </Button>
-                  <Button href="https://go.cal.com/teams-video" target="_blank" color="secondary">
-                    {t("learn_more")}
-                  </Button>
+          {isCalcom ? (
+            <>
+              <div className="max-w-[1118px]">
+                <div
+                  className="flex w-full justify-between overflow-hidden rounded-lg pt-4 pb-10 md:min-h-[295px] md:pt-10"
+                  style={{
+                    background: "url(/team-banner-background.jpg)",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                  }}>
+                  <div className="mt-3 px-14">
+                    <h1 className="font-cal text-3xl">{t("calcom_is_better_with_team")}</h1>
+                    <p className="my-4 max-w-sm text-gray-600">{t("add_your_team_members")}</p>
+                    <div className="space-y-2 sm:space-x-2">
+                      <Button color="primary" href={`${WEBAPP_URL}/settings/teams/new`}>
+                        {t("create_team")}
+                      </Button>
+                      <Button href="https://go.cal.com/teams-video" target="_blank" color="secondary">
+                        {t("learn_more")}
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-6 grid-cols-3 md:grid md:gap-6">
+                  {features.map((feature) => (
+                    <div key={feature.title} className="mb-6 min-h-[180px] w-full rounded-md bg-gray-50 p-8">
+                      {feature.icon}
+                      <h2 className="font-cal mt-4 text-lg">{feature.title}</h2>
+                      <p className="text-gray-700">{feature.description}</p>
+                    </div>
+                  ))}
                 </div>
               </div>
-            </div>
-            <div className="mt-6 grid-cols-3 md:grid md:gap-6">
-              {features.map((feature) => (
-                <div key={feature.title} className="mb-6 min-h-[180px] w-full rounded-md bg-gray-50 p-8">
-                  {feature.icon}
-                  <h2 className="font-cal mt-4 text-lg">{feature.title}</h2>
-                  <p className="text-gray-700">{feature.description}</p>
-                </div>
-              ))}
-            </div>
-          </div>
+            </>
+          ) : (
+            <EmptyScreen
+              Icon={Icon.FiUsers}
+              headline={t("no_teams")}
+              description={t("no_teams_description")}
+              buttonRaw={
+                <Button color="secondary" href={`${WEBAPP_URL}/settings/teams/new`}>
+                  {t("create_team")}
+                </Button>
+              }
+            />
+          )}
         </>
       )}
       {teams.length > 0 && <TeamList teams={teams} />}
