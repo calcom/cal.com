9bb83aba7eddf9c0bdcfd2d467eeb11dc89b02fa
Peer Richelsen <peeroke@gmail.com>
2022-12-09 12:05:42 +0100

    removed license consent (#5931)
    Co-authored-by: alannnc <alannnc@gmail.com>
    Co-authored-by: Alex van Andel <me@alexvanandel.com>

--- packages/lib/telemetry.ts
+++ packages/lib/telemetry.ts
@@ -96,7 +96,6 @@ export const extendEventData = (
     ipAddress: "",
     queryString: "",
     page_url: pageUrl,
-    licenseConsent: !!process.env.NEXT_PUBLIC_LICENSE_CONSENT,
     licensekey: process.env.CALCOM_LICENSE_KEY,
     isTeamBooking:
       original?.isTeamBooking === undefined
