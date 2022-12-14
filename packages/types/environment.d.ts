3b6901609c0cf11931a5f282ac049dd06397b10e
joshavant <joshavant@gmail.com>
2022-12-14 14:13:49 -0600

    Parameterize Stripe Payment Fee configs

--- packages/types/environment.d.ts
+++ packages/types/environment.d.ts
@@ -27,8 +27,8 @@ declare namespace NodeJS {
     readonly STRIPE_PRIVATE_KEY: string | undefined;
     readonly STRIPE_CLIENT_ID: string | undefined;
     readonly STRIPE_WEBHOOK_SECRET: string | undefined;
-    readonly PAYMENT_FEE_PERCENTAGE: number | undefined;
-    readonly PAYMENT_FEE_FIXED: number | undefined;
+    readonly STRIPE_PAYMENT_FEE_PERCENTAGE: number | undefined;
+    readonly STRIPE_PAYMENT_FEE_FIXED: number | undefined;
     readonly NEXT_PUBLIC_INTERCOM_APP_ID: string | undefined;
     readonly TANDEM_CLIENT_ID: string | undefined;
     readonly TANDEM_CLIENT_SECRET: string | undefined;
