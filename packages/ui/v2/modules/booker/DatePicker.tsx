0f4e75f9e337fb5ae09bf3f3aa722cf4a7bbc25b
Omar López <zomars@me.com>
2022-12-15 15:33:30 -0700

    Update packages/ui/v2/modules/booker/DatePicker.tsx

--- packages/ui/v2/modules/booker/DatePicker.tsx
+++ packages/ui/v2/modules/booker/DatePicker.tsx
@@ -229,7 +229,6 @@ const DatePicker = ({
           <button
             type="button"
             className="group p-1 opacity-50 hover:opacity-100"
-            w
             onClick={() => changeMonth(+1)}
             data-testid="incrementMonth">
             <ChevronRightIcon className="h-5 w-5" />
