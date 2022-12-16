53fd40738578280cb4642f34ed3bec847cc21a39
Carina Wollendorfer <30310907+CarinaWolli@users.noreply.github.com>
2022-12-16 22:13:57 +0100

    Hotfix/fix workflows (#6071)
    * Hotfix: metadata parsing
    
    * Hotfix: removed data64 avatar for 4mb payload limit
    
    * Hotfix: Installed Apps Tabs Switchi g (#5063) (#5064)
    
    * Hotfix: metadata parsing
    
    * Use Router query as tabs use shallow routing, data-fetching doesnt occur on switching tabs
    
    Co-authored-by: zomars <zomars@me.com>
    
    Co-authored-by: zomars <zomars@me.com>
    
    * Hotfix/email scheduling memory limit (#5839)
    
    * Hotfix: metadata parsing
    
    * Hotfix: removed data64 avatar for 4mb payload limit
    
    * Hotfix: Installed Apps Tabs Switchi g (#5063) (#5064)
    
    * Hotfix: metadata parsing
    
    * Use Router query as tabs use shallow routing, data-fetching doesnt occur on switching tabs
    
    Co-authored-by: zomars <zomars@me.com>
    
    Co-authored-by: zomars <zomars@me.com>
    
    * remove if and at to query
    
    Co-authored-by: zomars <zomars@me.com>
    Co-authored-by: Hariom Balhara <hariombalhara@gmail.com>
    Co-authored-by: CarinaWolli <wollencarina@gmail.com>
    
    * Fix/workflow email (#6069)
    
    * teams plan needed
    
    * disable
    
    * disable
    
    * type fix
    
    Co-authored-by: CarinaWolli <wollencarina@gmail.com>
    
    Co-authored-by: zomars <zomars@me.com>
    Co-authored-by: Hariom Balhara <hariombalhara@gmail.com>
    Co-authored-by: CarinaWolli <wollencarina@gmail.com>

--- packages/features/ee/workflows/lib/reminders/emailReminderManager.ts
+++ packages/features/ee/workflows/lib/reminders/emailReminderManager.ts
@@ -39,6 +39,7 @@ export const scheduleEmailReminder = async (
   workflowStepId: number,
   template: WorkflowTemplates
 ) => {
+  if (action === WorkflowActions.EMAIL_ADDRESS) return;
   const { startTime, endTime } = evt;
   const uid = evt.uid as string;
   const currentDate = dayjs();
@@ -76,10 +77,6 @@ export const scheduleEmailReminder = async (
       attendeeName = evt.organizer.name;
       timeZone = evt.attendees[0].timeZone;
       break;
-    case WorkflowActions.EMAIL_ADDRESS:
-      name = "";
-      attendeeName = evt.attendees[0].name;
-      timeZone = evt.organizer.timeZone;
   }
 
   let emailContent = {
