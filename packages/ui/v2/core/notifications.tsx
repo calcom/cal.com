72aede7c03f7a2fd6ba862d09264d9560643dffc
mehranShG <mehranfmpx@gmail.com>
2022-12-18 10:17:21 +0330

    refacore: refs #6064;replaced imports from react-feater with icon from calcom/ui

--- packages/ui/v2/core/notifications.tsx
+++ packages/ui/v2/core/notifications.tsx
@@ -1,7 +1,7 @@
 import classNames from "classnames";
-import {  Info } from "react-feather";
-import { Icon } from "@calcom/ui";
 import toast from "react-hot-toast";
+
+import { Icon } from "@calcom/ui";
 
 export function showToast(message: string, variant: "success" | "warning" | "error") {
   switch (variant) {
@@ -28,7 +28,7 @@ export function showToast(message: string, variant: "success" | "warning" | "err
               "animate-fade-in-up mb-2 flex h-9 items-center space-x-2 rounded-md bg-red-100 p-3 text-sm font-semibold text-red-900 shadow-md",
               t.visible && "animate-fade-in-up"
             )}>
-            <Info className="h-4 w-4" />
+            <Icon.FiInfo className="h-4 w-4" />
             <p>{message}</p>
           </div>
         ),
@@ -43,7 +43,7 @@ export function showToast(message: string, variant: "success" | "warning" | "err
               "animate-fade-in-up bg-brand-500 mb-2 flex h-9 items-center space-x-2 rounded-md p-3 text-sm font-semibold text-white shadow-md",
               t.visible && "animate-fade-in-up"
             )}>
-            <Info className="h-4 w-4" />
+            <Icon.FiInfo className="h-4 w-4" />
             <p>{message}</p>
           </div>
         ),
