ddcd0e5ead80c86f8de2dd3f5317c192635bcb24
zomars <zomars@me.com>
2022-12-20 12:20:29 -0700

    Update getStripeAppKeys.ts

--- packages/app-store/stripepayment/lib/getStripeAppKeys.ts
+++ packages/app-store/stripepayment/lib/getStripeAppKeys.ts
@@ -1,14 +1,4 @@
-import { z } from "zod";
-
 import getParsedAppKeysFromSlug from "../../_utils/getParsedAppKeysFromSlug";
+import { appKeysSchema } from "../zod";
 
-const stripeAppKeysSchema = z.object({
-  client_id: z.string().startsWith("ca_"),
-  client_secret: z.string().startsWith("sk_"),
-  public_key: z.string().startsWith("pk_"),
-  webhook_secret: z.string().startsWith("whsec_"),
-  payment_fee_fixed: z.number().int(),
-  payment_fee_percentage: z.number(),
-});
-
-export const getStripeAppKeys = () => getParsedAppKeysFromSlug("stripe", stripeAppKeysSchema);
+export const getStripeAppKeys = () => getParsedAppKeysFromSlug("stripe", appKeysSchema);
