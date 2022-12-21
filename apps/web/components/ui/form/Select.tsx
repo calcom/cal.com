e3699a019306b805ef0a19462c57e38fb9d23f33
Alex van Andel <me@alexvanandel.com>
2022-12-21 12:29:39 +0000

    Fix lint error

--- apps/web/components/ui/form/Select.tsx
+++ apps/web/components/ui/form/Select.tsx
@@ -1,13 +1,5 @@
 import React, { useCallback, useEffect, useState } from "react";
-import ReactSelect, {
-  components,
-  GroupBase,
-  Props,
-  InputProps,
-  SingleValue,
-  MultiValue,
-  OptionProps,
-} from "react-select";
+import ReactSelect, { components, GroupBase, Props, InputProps, SingleValue, MultiValue } from "react-select";
 
 import classNames from "@calcom/lib/classNames";
 import useTheme from "@calcom/lib/hooks/useTheme";
