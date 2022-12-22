d0a3d8d144549b7dc3dfe417ae8e2883f948d044
zomars <zomars@me.com>
2022-12-22 10:52:50 -0700

    Link fixes
    commit 56c1608deaf1921a80dc71bcbd107e772dd656f4
    Author: Omar López <zomars@me.com>
    Date:   Wed Dec 21 19:26:04 2022 -0700
    
        Update link.ts
    
    commit eab43d833515cabc7acf2f973c2d2837ac1a0332
    Author: zomars <zomars@me.com>
    Date:   Wed Dec 21 19:03:02 2022 -0700
    
        Prevents 500 on TRPC error
    
    commit 3a6970d5e5fab54136b379e1b2ccd7acf515a809
    Author: zomars <zomars@me.com>
    Date:   Wed Dec 21 18:15:26 2022 -0700
    
        Encoding fixes

--- apps/web/pages/api/link.ts
+++ apps/web/pages/api/link.ts
@@ -5,6 +5,7 @@ import { z } from "zod";
 import { symmetricDecrypt } from "@calcom/lib/crypto";
 import { defaultResponder } from "@calcom/lib/server";
 import prisma from "@calcom/prisma";
+import { TRPCError } from "@calcom/trpc/server";
 import { createContext } from "@calcom/trpc/server/createContext";
 import { viewerRouter } from "@calcom/trpc/server/routers/viewer";
 
@@ -27,7 +28,7 @@ const decryptedSchema = z.object({
 async function handler(req: NextApiRequest, res: NextApiResponse<Response>) {
   const { action, token, reason } = querySchema.parse(req.query);
   const { bookingUid, userId } = decryptedSchema.parse(
-    JSON.parse(symmetricDecrypt(token, process.env.CALENDSO_ENCRYPTION_KEY || ""))
+    JSON.parse(symmetricDecrypt(decodeURIComponent(token), process.env.CALENDSO_ENCRYPTION_KEY || ""))
   );
 
   /** We shape the session as required by tRPC router */
@@ -47,17 +48,24 @@ async function handler(req: NextApiRequest, res: NextApiResponse<Response>) {
     where: { uid: bookingUid },
   });
 
-  /** @see https://trpc.io/docs/server-side-calls */
-  const ctx = await createContext({ req, res }, sessionGetter);
-  const caller = viewerRouter.createCaller(ctx);
-  await caller.bookings.confirm({
-    bookingId: booking.id,
-    recurringEventId: booking.recurringEventId || undefined,
-    confirmed: action === DirectAction.ACCEPT,
-    reason,
-  });
+  try {
+    /** @see https://trpc.io/docs/server-side-calls */
+    const ctx = await createContext({ req, res }, sessionGetter);
+    const caller = viewerRouter.createCaller(ctx);
+    await caller.bookings.confirm({
+      bookingId: booking.id,
+      recurringEventId: booking.recurringEventId || undefined,
+      confirmed: action === DirectAction.ACCEPT,
+      reason,
+    });
+  } catch (e) {
+    let message = "Error confirming booking";
+    if (e instanceof TRPCError) message = e.message;
+    res.redirect(`/booking/${bookingUid}?error=${encodeURIComponent(message)}`);
+    return;
+  }
 
-  return res.redirect(`/booking/${bookingUid}`);
+  res.redirect(`/booking/${bookingUid}`);
 }
 
 export default defaultResponder(handler);
