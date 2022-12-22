befcd82232bd540b7d44077ad36776798f08f822
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2022-12-22 09:49:53 +0000

    Fix mobile mode

--- packages/ui/components/popover/index.tsx
+++ packages/ui/components/popover/index.tsx
@@ -14,10 +14,34 @@ export const AnimatedPopover = ({
   children: React.ReactNode;
 }) => {
   const [open, setOpen] = React.useState(false);
+  const ref = React.useRef<HTMLDivElement>(null);
+  // calculate which aligment to open the popover with based on which half of the screen it is on (left or right)
+  const [align, setAlign] = React.useState<"start" | "end">("start");
+  React.useEffect(() => {
+    const handleResize = () => {
+      const halfWidth = window.innerWidth / 2;
+      const { x, y } = ref?.current?.getBoundingClientRect() || {
+        x: 0,
+        y: 0,
+      };
+      if (x > halfWidth) {
+        setAlign("end");
+      } else {
+        setAlign("start");
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [setAlign]);
+
   return (
     <Popover.Root onOpenChange={setOpen} modal={true}>
       <Popover.Trigger asChild>
         <div
+          ref={ref}
           className="item-center  mb-2 flex h-9 justify-between whitespace-nowrap rounded-md border border-gray-300 py-2 px-3
           text-sm placeholder:text-gray-400 hover:cursor-pointer hover:border-gray-400
           focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-neutral-800 focus:ring-offset-1">
@@ -34,8 +58,12 @@ export const AnimatedPopover = ({
           </div>
         </div>
       </Popover.Trigger>
-      <Popover.Content side="bottom" align="end" asChild>
-        <div className="absolute z-50 mt-2 w-56 -translate-x-[228px] rounded-md bg-white  shadow-sm ring-1 ring-black ring-opacity-5 focus-within:outline-none">
+      <Popover.Content side="bottom" align={align} asChild>
+        <div
+          className={classNames(
+            "absolute z-50 mt-2 w-56  rounded-md bg-white  shadow-sm ring-1 ring-black ring-opacity-5 focus-within:outline-none",
+            align === "end" && "-translate-x-[228px]"
+          )}>
           {children}
         </div>
       </Popover.Content>
