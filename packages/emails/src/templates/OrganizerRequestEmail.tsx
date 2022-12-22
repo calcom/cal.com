dc58c342475c934db3cdc38f8982acea8d80c48b
zomars <zomars@me.com>
2022-12-22 10:52:50 -0700

    Link fixes
    commit 56c1608deaf1921a80dc71bcbd107e772dd656f4
    Author: Omar López <zomars@me.com>
    Date:   Wed Dec 21 19:26:04 2022 -0700
    
        Update link.ts
    
    commit eab43d833515cabc7acf2f973c2d2837ac1a0332
    Author: zomars <zomars@me.com>
    Date:   Wed Dec 21 19:03:02 2022 -0700
    
        Prevents 500 on TRPC error
    
    commit 3a6970d5e5fab54136b379e1b2ccd7acf515a809
    Author: zomars <zomars@me.com>
    Date:   Wed Dec 21 18:15:26 2022 -0700
    
        Encoding fixes

--- packages/emails/src/templates/OrganizerRequestEmail.tsx
+++ packages/emails/src/templates/OrganizerRequestEmail.tsx
@@ -7,7 +7,7 @@ import { OrganizerScheduledEmail } from "./OrganizerScheduledEmail";
 export const OrganizerRequestEmail = (props: React.ComponentProps<typeof OrganizerScheduledEmail>) => {
   const seedData = { bookingUid: props.calEvent.uid, userId: props.calEvent.organizer.id };
   const token = symmetricEncrypt(JSON.stringify(seedData), process.env.CALENDSO_ENCRYPTION_KEY || "");
-  const actionHref = `${WEBAPP_URL}/api/link/?token=${token}`;
+  const actionHref = `${WEBAPP_URL}/api/link/?token=${encodeURIComponent(token)}`;
   return (
     <OrganizerScheduledEmail
       title={
