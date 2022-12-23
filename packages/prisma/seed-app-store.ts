1044bee0e3430a4a7643daacb4133a9a52d3f130
joshavant <joshavant@gmail.com>
2022-12-23 14:30:09 -0500

    Cast Payment Fee params as Number

--- packages/prisma/seed-app-store.ts
+++ packages/prisma/seed-app-store.ts
@@ -281,8 +281,8 @@ export default async function main() {
     await createApp("stripe", "stripepayment", ["payment"], "stripe_payment", {
       client_id: process.env.STRIPE_CLIENT_ID,
       client_secret: process.env.STRIPE_PRIVATE_KEY,
-      payment_fee_fixed: process.env.PAYMENT_FEE_FIXED,
-      payment_fee_percentage: process.env.PAYMENT_FEE_PERCENTAGE,
+      payment_fee_fixed: Number(process.env.PAYMENT_FEE_FIXED),
+      payment_fee_percentage: Number(process.env.PAYMENT_FEE_PERCENTAGE),
       public_key: process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY,
       webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
     });
