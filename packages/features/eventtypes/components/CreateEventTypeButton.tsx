7857e833e0077e2302c23a8bc02a8153eba08168
Peer Richelsen <peer@cal.com>
2022-12-23 14:09:40 +0100

    introduced size="fab" prop to Button, added to CTA buttons

--- packages/features/eventtypes/components/CreateEventTypeButton.tsx
+++ packages/features/eventtypes/components/CreateEventTypeButton.tsx
@@ -157,6 +157,7 @@ export default function CreateEventTypeButton(props: CreateEventTypeBtnProps) {
           onClick={() => openModal(props.options[0])}
           data-testid="new-event-type"
           StartIcon={Icon.FiPlus}
+          size="fab"
           disabled={!props.canAddEvents}>
           {t("new")}
         </Button>
@@ -165,6 +166,7 @@ export default function CreateEventTypeButton(props: CreateEventTypeBtnProps) {
           <DropdownMenuTrigger asChild>
             <Button
               EndIcon={Icon.FiChevronDown}
+              size="fab"
               className="radix-state-open:bg-brand-500 radix-state-open:ring-2 radix-state-open:ring-brand-500 ring-offset-2">
               {t("new")}
             </Button>
--- packages/ui/v2/core/Dropdown.tsx
+++ packages/ui/v2/core/Dropdown.tsx
@@ -15,7 +15,7 @@ export const DropdownMenuTrigger = forwardRef<HTMLButtonElement, DropdownMenuTri
       {...props}
       className={
         props.asChild
-          ? classNames(className, "radix-state-open:bg-gray-100 rounded-md ring-0")
+          ? classNames(className, "rounded-md ring-0 sm:focus:ring-2")
           : `inline-flex items-center rounded-md bg-transparent px-3 py-2 text-sm font-medium text-gray-700 ring-0 hover:bg-gray-50 focus:bg-gray-100 group-hover:text-black ${className}`
       }
       ref={forwardedRef}
