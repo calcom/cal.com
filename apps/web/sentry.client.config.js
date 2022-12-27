60dbd677d2ee6c6f9649790cf12374075a1c24e3
tourlane-a-lanovyy <artem.lanovyy@tourlane.com>
2022-12-27 19:04:30 +0100

    chore(sentry): remove unused SENTRY_DSN env variable

--- apps/web/sentry.client.config.js
+++ apps/web/sentry.client.config.js
@@ -3,7 +3,7 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 import * as Sentry from "@sentry/nextjs";
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
 const ENVIRONMENT = process.env.SENTRY_ENVIRONMENT;
 
 Sentry.init({
