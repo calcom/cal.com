470209a2a4778a0bc7366b331026e647851d9132
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2023-01-05 11:07:15 +0000

    Fixing Icon Import

--- apps/web/components/apps/layouts/InstalledAppsLayout.tsx
+++ apps/web/components/apps/layouts/InstalledAppsLayout.tsx
@@ -3,8 +3,7 @@ import React, { ComponentProps } from "react";
 import AppCategoryNavigation from "@calcom/app-store/_components/AppCategoryNavigation";
 import { InstalledAppVariants } from "@calcom/app-store/utils";
 import { trpc } from "@calcom/trpc/react";
-import { Icon } from "@calcom/ui/Icon";
-import Shell from "@calcom/ui/v2/core/Shell";
+import { Icon, Shell } from "@calcom/ui";
 import type { HorizontalTabItemProps } from "@calcom/ui/v2/core/navigation/tabs/HorizontalTabItem";
 import type { VerticalTabItemProps } from "@calcom/ui/v2/core/navigation/tabs/VerticalTabItem";
 
