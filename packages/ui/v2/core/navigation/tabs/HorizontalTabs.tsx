90d3d864c67f0b0984b81c1466e6a76d29471f73
Peer Richelsen <peer@cal.com>
2023-01-06 12:53:39 +0100

    added space

--- packages/ui/v2/core/navigation/tabs/HorizontalTabs.tsx
+++ packages/ui/v2/core/navigation/tabs/HorizontalTabs.tsx
@@ -11,7 +11,10 @@ export interface NavTabProps {
 const HorizontalTabs = function ({ tabs, linkProps, actions, ...props }: NavTabProps) {
   return (
     <div className="mb-2 max-w-[calc(100%+40px)] lg:mb-5">
-      <nav className="no-scrollbar flex overflow-scroll rounded-md border p-1" aria-label="Tabs" {...props}>
+      <nav
+        className="no-scrollbar flex space-x-1 overflow-scroll rounded-md border p-1"
+        aria-label="Tabs"
+        {...props}>
         {tabs.map((tab, idx) => (
           <HorizontalTabItem {...tab} key={idx} {...linkProps} />
         ))}
