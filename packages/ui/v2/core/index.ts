ce518a16f89fa6fa4e475d544ee512f9d6150616
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2023-01-10 12:57:41 +0000

    Fix core export

--- packages/ui/v2/core/index.ts
+++ packages/ui/v2/core/index.ts
@@ -1,6 +1,2 @@
-export { default as Loader } from "../../components/skeleton/Loader";
-export { default as MeetingTimeInTimezones } from "../../components/popover/MeetingTimeInTimezones";
-export { default as SettingsToggle } from "../../components/form/switch/SettingsToggle";
 export { default as Shell } from "./Shell";
 export { default as Swatch } from "./Swatch";
-export { default as Switch } from "../../components/form/switch/Switch";
