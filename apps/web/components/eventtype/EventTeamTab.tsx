948eb22e265f4dada2da2b295995cf7fd423519d
Alex van Andel <me@alexvanandel.com>
2023-01-11 23:33:12 +0000

    DRY hostsFixed select

--- apps/web/components/eventtype/EventTeamTab.tsx
+++ apps/web/components/eventtype/EventTeamTab.tsx
@@ -1,12 +1,15 @@
 import { SchedulingType } from "@prisma/client/";
 import { EventTypeSetupProps, FormValues } from "pages/event-types/[type]";
 import { useMemo } from "react";
-import { Controller, useFormContext, useWatch } from "react-hook-form";
+import { Controller, useFormContext, useWatch, Control } from "react-hook-form";
+import { Options } from "react-select";
 
-import CheckedTeamSelect from "@calcom/features/eventtypes/components/CheckedTeamSelect";
+import CheckedTeamSelect, {
+  CheckedSelectOption,
+} from "@calcom/features/eventtypes/components/CheckedTeamSelect";
 import { WEBAPP_URL } from "@calcom/lib/constants";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
-import { Label, Select, TextField } from "@calcom/ui";
+import { Label, Select } from "@calcom/ui";
 
 interface IMemberToValue {
   id: number | null;
@@ -21,6 +24,55 @@ const mapUserToValue = ({ id, name, username, email }: IMemberToValue) => ({
   avatar: `${WEBAPP_URL}/${username}/avatar.png`,
   email,
 });
+
+const FixedHosts = ({
+  control,
+  labelText,
+  placeholder,
+  options = [],
+}: {
+  control: Control<FormValues>;
+  labelText: string;
+  placeholder: string;
+  options?: Options<CheckedSelectOption>;
+}) => {
+  return (
+    <div className="flex flex-col space-y-5 bg-gray-50 p-4">
+      <div>
+        <Label>{labelText}</Label>
+        <Controller
+          name="hostsFixed"
+          control={control}
+          render={({ field: { onChange, value } }) => {
+            return (
+              <CheckedTeamSelect
+                isDisabled={false}
+                onChange={(options) => {
+                  onChange(
+                    options.map((option) => ({
+                      isFixed: true,
+                      userId: parseInt(option.value, 10),
+                    }))
+                  );
+                }}
+                value={value
+                  .map(
+                    (host) =>
+                      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                      options.find((member) => member.value === host.userId.toString())!
+                  )
+                  .filter(Boolean)}
+                controlShouldRenderValue={false}
+                options={options}
+                placeholder={placeholder}
+              />
+            );
+          }}
+        />
+      </div>
+    </div>
+  );
+};
 
 export const EventTeamTab = ({
   eventType,
@@ -75,76 +127,26 @@ export const EventTeamTab = ({
           </div>
 
           {schedulingType === SchedulingType.COLLECTIVE && (
-            <div className="flex flex-col space-y-5 bg-gray-50 p-4">
-              <div>
-                <Label>{t("team")}</Label>
-                <Controller
-                  name="hostsFixed"
-                  control={formMethods.control}
-                  defaultValue={eventType.users.map((user) => user.id.toString())}
-                  render={({ field: { onChange, value } }) => (
-                    <CheckedTeamSelect
-                      isDisabled={false}
-                      onChange={(options) => onChange(options.map((user) => user.value))}
-                      value={value
-                        .map(
-                          (userId) =>
-                            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                            teamMembers.map(mapUserToValue).find((member) => member.value === userId)!
-                        )
-                        .filter(Boolean)}
-                      controlShouldRenderValue={false}
-                      options={teamMembers.map(mapUserToValue).filter((member) => {
-                        return member;
-                      })}
-                      placeholder={t("add_attendees")}
-                    />
-                  )}
-                />
-              </div>
-            </div>
+            <FixedHosts
+              options={teamMembers.map(mapUserToValue)}
+              placeholder={t("add_attendees")}
+              labelText={t("team")}
+              control={formMethods.control}
+            />
           )}
 
           {schedulingType === SchedulingType.ROUND_ROBIN && (
             <>
-              <div className="flex flex-col bg-gray-50 p-4">
-                <Label>{t("fixed_hosts")}</Label>
-                <Controller
-                  name="hostsFixed"
-                  control={formMethods.control}
-                  render={({ field: { onChange, value } }) => {
-                    return (
-                      <CheckedTeamSelect
-                        isDisabled={false}
-                        onChange={(options) => {
-                          onChange(
-                            options.map((option) => ({
-                              isFixed: true,
-                              userId: parseInt(option.value, 10),
-                            }))
-                          );
-                        }}
-                        value={value
-                          .map(
-                            (host) =>
-                              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                              teamMembers
-                                .map(mapUserToValue)
-                                .find((member) => member.value === host.userId.toString())!
-                          )
-                          .filter(Boolean)}
-                        controlShouldRenderValue={false}
-                        options={teamMembers.map(mapUserToValue).filter((member) => {
-                          return !formMethods
-                            .getValues("hosts")
-                            .find((host) => host.userId.toString() === member.value);
-                        })}
-                        placeholder={t("add_fixed_hosts")}
-                      />
-                    );
-                  }}
-                />
-              </div>
+              <FixedHosts
+                options={teamMembers.map(mapUserToValue).filter((member) => {
+                  return !formMethods
+                    .getValues("hosts")
+                    .find((host) => host.userId.toString() === member.value);
+                })}
+                placeholder={t("add_fixed_hosts")}
+                labelText={t("fixed_hosts")}
+                control={formMethods.control}
+              />
               <div className="flex flex-col space-y-5 bg-gray-50 p-4">
                 <div>
                   <Label>{t("round_robin_hosts")}</Label>
