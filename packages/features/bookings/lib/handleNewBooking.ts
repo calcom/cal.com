27179a62b1dbf4977d2761ca460f19425e38cfe9
Ben Kirchner <ben@mento.co>
2023-01-11 17:09:22 -0700

    Fix partials

--- packages/features/bookings/lib/handleNewBooking.ts
+++ packages/features/bookings/lib/handleNewBooking.ts
@@ -264,18 +264,12 @@ async function ensureAvailableUsers(
         recurringDatesInfo?.currentRecurringIndex === 0 &&
         recurringDatesInfo.allRecurringDates
       ) {
-        let allBookingDates = recurringDatesInfo.allRecurringDates.map((strDate) => new Date(strDate));
-
-        // CUSTOM_CODE First 3 Instances we check
-        allBookingDates = allBookingDates?.sort()?.slice(0, 3);
-
+        const allBookingDates = recurringDatesInfo.allRecurringDates.map((strDate) => new Date(strDate));
         // Go through each date for the recurring event and check if each one's availability
         // DONE: Decreased computational complexity from O(2^n) to O(n) by refactoring this loop to stop
         // running at the first unavailable time.
         let i = 0;
         while (!foundConflict && i < allBookingDates.length) {
-          console.log("check conflict", allBookingDates[i++]);
-
           foundConflict = checkForConflicts(bufferedBusyTimes, allBookingDates[i++], eventType.length);
         }
       } else {
