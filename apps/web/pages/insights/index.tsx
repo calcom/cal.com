49635a4c0063f65bae98172ed92ccce76cd43cf0
Peer Richelsen <peer@cal.com>
2023-01-16 18:19:09 +0100

    nit

--- apps/web/pages/insights/index.tsx
+++ apps/web/pages/insights/index.tsx
@@ -159,6 +159,15 @@ export default function InsightsPage() {
                   ]}
                 />
               </div>
+              <small className="block text-center text-gray-600">
+                Looking for more analytics?{" "}
+                <a
+                  className="text-blue-500 hover:underline"
+                  href="mailto:updates@cal.com?subject=Feature%20Request%3A%20More%20Analytics&body=Hey%20Cal.com%20Team%2C%20I%20love%20the%20analytics%20page%20but%20I%20am%20looking%20for%20...">
+                  Let us know
+                </a>
+                .
+              </small>
             </div>
           </>
         )}
