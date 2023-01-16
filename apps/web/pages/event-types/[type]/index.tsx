250fc34ed3c910df99926c0ee906201de59655cc
Alex van Andel <me@alexvanandel.com>
2023-01-16 10:34:26 +0000

    Added fallback to hostsFixed too which is used in COLLECTIVE

--- apps/web/pages/event-types/[type]/index.tsx
+++ apps/web/pages/event-types/[type]/index.tsx
@@ -204,8 +204,14 @@ const EventTypePage = (props: EventTypeSetupProps) => {
       metadata,
       hosts: !!eventType.hosts?.length
         ? eventType.hosts.filter((host) => !host.isFixed)
-        : eventType.users.map((user) => ({ userId: user.id })),
-      hostsFixed: eventType.hosts.filter((host) => host.isFixed),
+        : eventType.users
+            .filter(() => eventType.schedulingType === SchedulingType.ROUND_ROBIN)
+            .map((user) => ({ userId: user.id })),
+      hostsFixed: !!eventType.hosts?.length
+        ? eventType.hosts.filter((host) => host.isFixed)
+        : eventType.users
+            .filter(() => eventType.schedulingType === SchedulingType.COLLECTIVE)
+            .map((user) => ({ userId: user.id })),
     },
     resolver: zodResolver(
       z
