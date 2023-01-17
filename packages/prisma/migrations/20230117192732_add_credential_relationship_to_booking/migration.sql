7f78a8bdee1531b712f895a85f74418ca51961a3
Joe Au-Yeung <j.auyeung419@gmail.com>
2023-01-17 14:27:48 -0500

    Add credential relationship to booking reference

---
+++ packages/prisma/migrations/20230117192732_add_credential_relationship_to_booking/migration.sql
@@ -0,0 +1,2 @@
+-- AddForeignKey
+ALTER TABLE "BookingReference" ADD CONSTRAINT "BookingReference_credentialId_fkey" FOREIGN KEY ("credentialId") REFERENCES "Credential"("id") ON DELETE SET NULL ON UPDATE CASCADE;
