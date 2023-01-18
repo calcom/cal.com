95c8d4e43138ea55b547e248c54c77d7eb83c11f
nafees nazik <nafeesnazik21@gmail.com>
2023-01-18 22:44:12 +0530

    fix: flash

--- apps/web/components/apps/App.tsx
+++ apps/web/components/apps/App.tsx
@@ -314,7 +314,7 @@ export default function App(props: {
   const { t } = useLocale();
 
   return (
-    <Shell large isPublic heading={t("app_store")} backPath="/apps" withoutSeo>
+    <Shell large heading={t("app_store")} backPath="/apps" withoutSeo>
       <HeadSeo
         title={props.name}
         description={props.description}
