b5f274d6635f503ee6e10917b8df030df04c3f40
Alex van Andel <me@alexvanandel.com>
2023-01-18 10:40:06 +0000

    experiment: disco border  (#6531)
    * Had a play, looking good (now needs isLoading state or however we want to do that)
    
    * Also add tailwind config..
    
    * Don't show conic bg on lightmode
    
    * Slow down animation, show on light mode
    
    * Apply disco border both on light/dark mode & only on success page
    
    * 5s->8s animation speed, remove border crisping up the corners

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -2,5 +2,23 @@ const base = require("@calcom/config/tailwind-preset");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   ...base,
+  theme: {
+    ...base.theme,
+    extend: {
+      ...base.theme.extend,
+      backgroundImage: {
+        "gradient-conic": "conic-gradient(var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        disco: {
+          "0%": { transform: "translateY(-50%) rotate(0deg)" },
+          "100%": { transform: "translateY(-50%) rotate(360deg)" },
+        },
+      },
+      animation: {
+        disco: "disco 8s linear infinite",
+      },
+    },
+  },
   content: [...base.content],
 };
