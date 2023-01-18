e29e4f2ffcc7b8c5c4c6af4f7bb84c7f276ebeb0
CarinaWolli <wollencarina@gmail.com>
2023-01-18 12:19:40 -0500

    add back commented code

--- apps/web/lib/getNodeText.tsx
+++ apps/web/lib/getNodeText.tsx
@@ -1,6 +1,6 @@
 export const getNodeText = (node: string | JSX.Element | JSX.Element[]): string => {
-  // if (typeof node === 'string') return node
-  // if ( (node instanceof Array)) return node?.map(getNodeText).join('')
-  // if (typeof node === 'object' && node) return getNodeText(node.props.children)
+  if (typeof node === "string") return node;
+  if (node instanceof Array) return node.map(getNodeText).join("");
+  if (typeof node === "object" && node) return getNodeText(node.props.children);
   return "";
 };
