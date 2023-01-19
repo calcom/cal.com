4e6b4f07ae3d80000e026a5afc436e250533453e
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2023-01-19 11:39:05 +0000

    Add filter on YOUR bookings

--- packages/features/bookings/components/TeamFilter.tsx
+++ packages/features/bookings/components/TeamFilter.tsx
@@ -1,3 +1,5 @@
+import { useSession } from "next-auth/react";
+
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { trpc } from "@calcom/trpc/react";
 import { AnimatedPopover, Avatar, Icon } from "@calcom/ui";
@@ -6,6 +8,7 @@ import { useFilterQuery } from "../lib/useFilterQuery";
 
 export const TeamsMemberFilter = () => {
   const { t } = useLocale();
+  const session = useSession();
   const { data: query, pushItemToKey, removeItemByKeyAndValue, removeByKey } = useFilterQuery();
   const { data } = trpc.viewer.teams.list.useQuery();
 
@@ -30,9 +33,34 @@ export const TeamsMemberFilter = () => {
         <input
           id="allBookings"
           type="checkbox"
-          checked={!query.teamIds}
+          checked={!query.teamIds && !query.userIds?.includes(session.data?.user.id || 0)}
           onChange={() => {
             removeByKey("teamIds"); // Always clear on toggle  or not toggle (seems weird but when you know the behviour it works well )
+          }}
+          className="text-primary-600 focus:ring-primary-500 inline-flex h-4 w-4 place-self-center justify-self-end rounded border-gray-300 "
+        />
+      </div>
+      <div className="item-center flex px-4 py-[6px] focus-within:bg-gray-100 hover:cursor-pointer hover:bg-gray-50">
+        <div className="flex h-6 w-6 items-center justify-center ltr:mr-2 rtl:ml-2">
+          <Icon.FiUser className="h-5 w-5" />
+        </div>
+        <label
+          htmlFor="yourBookings"
+          className="mr-auto self-center truncate text-sm font-medium text-gray-700">
+          {t("your_bookings_filter_label")}
+        </label>
+
+        <input
+          id="yourBookings"
+          type="checkbox"
+          disabled={session.status === "loading"}
+          checked={query.userIds?.includes(session.data?.user.id || 0)}
+          onChange={(e) => {
+            if (e.target.checked) {
+              pushItemToKey("userIds", session.data?.user.id || 0);
+            } else if (!e.target.checked) {
+              removeItemByKeyAndValue("userIds", session.data?.user.id || 0);
+            }
           }}
           className="text-primary-600 focus:ring-primary-500 inline-flex h-4 w-4 place-self-center justify-self-end rounded border-gray-300 "
         />
