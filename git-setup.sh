4ffcdcab8b67b5c00fae7550632563cb7df2f73a
zomars <zomars@me.com>
2023-01-19 18:02:34 -0700

    Update .gitignore

--- git-setup.sh
+++ git-setup.sh
@@ -5,23 +5,31 @@ if [ $# -eq 0 ]; then
   echo "Usage: git-setup.sh <api,console,website>"
   exit 1
 fi
-
+# Loop through the requested modules
 for module in "$@"; do
   echo "Setting up '$module' module..."
-  # Set the project name, adding .git if necessary
+  # Set the project git URL
   project=$(echo "git@github.com:calcom/$module.git")
+  # Check if we have access to the module
   [ "$(git ls-remote $project 2>/dev/null)" ] && {
     echo "You have access to '${module}'"
+    # Create the .gitmodules file if it doesn't exist
     ([ -e ".gitmodules" ] || touch ".gitmodules") && [ ! -w ".gitmodules" ] && echo cannot write to .gitmodules && exit 1
+    # Prevents duplicate entries
+    git config -f .gitmodules --unset-all submodule.apps/$module.branch
+    # Add the submodule
     git submodule add --force git@github.com:calcom/$module.git apps/$module
+    # Set the default branch to main
     git config -f .gitmodules --add submodule.apps/$module.branch main
+    # Adding the subdmoule ignores the `.gitignore` so a reset is needed
+    git reset
   } || {
+    # If the module is the API, display a link to request access
     if [ "$module" == "api" ]; then
       echo "You don't have access to: '${module}' module. You can request access in: https://console.cal.com"
-      exit 0
     else
+      # If the module is not the API, display normal message
       echo "You don't have access to: '${module}' module."
-      exit 0
     fi
   }
 done
