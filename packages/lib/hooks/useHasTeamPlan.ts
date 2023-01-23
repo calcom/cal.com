077fe40d524805f56a6fd008f532642e1503672f
Hariom Balhara <hariombalhara@gmail.com>
2023-01-23 09:45:46 +0530

    Allow option to make a user part of team int ests

--- packages/lib/hooks/useHasTeamPlan.ts
+++ packages/lib/hooks/useHasTeamPlan.ts
@@ -3,8 +3,7 @@ import { trpc } from "@calcom/trpc/react";
 export function useHasTeamPlan() {
   const hasTeam = trpc.viewer.teams.hasTeamPlan.useQuery();
 
-  // TODO: Maybe later disable team plan for e2e tests and let the test create the team plan scenario
-  return process.env.NEXT_PUBLIC_IS_E2E || hasTeam.data?.hasTeamPlan || false;
+  return hasTeam.data?.hasTeamPlan || false;
 }
 
 export default useHasTeamPlan;
