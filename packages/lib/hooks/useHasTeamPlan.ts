ab163ef60eb3fe253b1433b67eb565416543a307
Hariom Balhara <hariombalhara@gmail.com>
2023-01-23 08:53:33 +0530

    Give team plan features to E2E tests

--- packages/lib/hooks/useHasTeamPlan.ts
+++ packages/lib/hooks/useHasTeamPlan.ts
@@ -3,7 +3,8 @@ import { trpc } from "@calcom/trpc/react";
 export function useHasTeamPlan() {
   const hasTeam = trpc.viewer.teams.hasTeamPlan.useQuery();
 
-  return hasTeam.data?.hasTeamPlan || false;
+  // TODO: Maybe later disable team plan for e2e tests and let the test create the team plan scenario
+  return process.env.NEXT_PUBLIC_IS_E2E || hasTeam.data?.hasTeamPlan || false;
 }
 
 export default useHasTeamPlan;
