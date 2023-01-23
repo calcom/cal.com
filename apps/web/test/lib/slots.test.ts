5a626939e935f0a19f939e324dc48ea93ffdbecf
tourlane-a-lanovyy <artem.lanovyy@tourlane.com>
2023-01-23 16:30:40 +0100

    chore(test): add edge case test data for slots

--- apps/web/test/lib/slots.test.ts
+++ apps/web/test/lib/slots.test.ts
@@ -126,17 +126,40 @@ describe("Tests the compact slot logic", () => {
     expect(result).toEqual([]);
   });
 
-  it("should return time slots that are not blocked by busy times", () => {
+  it.each([
+    {
+      busyTimes: [
+        { startTime: dayjs("2022-02-01 11:00"), endTime: dayjs("2022-02-01 12:00") },
+        { startTime: dayjs("2022-02-01 14:00"), endTime: dayjs("2022-02-01 15:00") },
+      ],
+      freeSlots: [
+        dayjs("2022-02-01 10:00"),
+        dayjs("2022-02-01 10:30"),
+        dayjs("2022-02-01 12:00"),
+        dayjs("2022-02-01 12:30"),
+        dayjs("2022-02-01 13:00"),
+        dayjs("2022-02-01 13:30"),
+        dayjs("2022-02-01 15:00"),
+        dayjs("2022-02-01 15:30"),
+        dayjs("2022-02-01 16:00"),
+        dayjs("2022-02-01 16:30"),
+      ],
+    },
+    {
+      busyTimes: [
+        { startTime: dayjs("2022-02-01 10:00"), endTime: dayjs("2022-02-01 16:00") },
+        { startTime: dayjs("2022-02-01 11:00"), endTime: dayjs("2022-02-01 12:00") },
+        { startTime: dayjs("2022-02-01 14:00"), endTime: dayjs("2022-02-01 15:00") },
+      ],
+      freeSlots: [dayjs("2022-02-01 16:00"), dayjs("2022-02-01 16:30")],
+    },
+  ])("should return time slots that are not blocked by busy times", ({ busyTimes, freeSlots }) => {
     const slotDay = dayjs("2022-02-01");
     const shiftStart = dayjs("2022-02-01 9:00");
     const shiftEnd = dayjs("2022-02-01 17:00");
     const days = [0, 1, 2, 3, 4, 5, 6];
     const minStartTime = dayjs("2022-02-01 10:00");
     const eventLength = 30;
-    const busyTimes = [
-      { startTime: dayjs("2022-02-01 11:00"), endTime: dayjs("2022-02-01 12:00") },
-      { startTime: dayjs("2022-02-01 14:00"), endTime: dayjs("2022-02-01 15:00") },
-    ];
     const result = getTimeSlotsCompact({
       slotDay,
       shiftStart,
@@ -146,18 +169,7 @@ describe("Tests the compact slot logic", () => {
       eventLength,
       busyTimes,
     });
-    expect(result).toEqual([
-      dayjs("2022-02-01 10:00"),
-      dayjs("2022-02-01 10:30"),
-      dayjs("2022-02-01 12:00"),
-      dayjs("2022-02-01 12:30"),
-      dayjs("2022-02-01 13:00"),
-      dayjs("2022-02-01 13:30"),
-      dayjs("2022-02-01 15:00"),
-      dayjs("2022-02-01 15:30"),
-      dayjs("2022-02-01 16:00"),
-      dayjs("2022-02-01 16:30"),
-    ]);
+    expect(result).toEqual(freeSlots);
   });
 
   it("should return an empty array if all the slots are blocked by busy times", () => {
