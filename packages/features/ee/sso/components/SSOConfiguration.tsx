0a21e0a4131991a5a50b1a800af87a392a43be42
Kiran <kiran@boxyhq.com>
2023-01-24 15:25:26 +0530

    Support OIDC

---
+++ packages/features/ee/sso/components/SSOConfiguration.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+import ConnectionInfo from "@calcom/ee/sso/components/ConnectionInfo";
+import LicenseRequired from "@calcom/features/ee/common/components/v2/LicenseRequired";
+import OIDCConnection from "@calcom/features/ee/sso/components/OIDCConnection";
+import SAMLConnection from "@calcom/features/ee/sso/components/SAMLConnection";
+import { useLocale } from "@calcom/lib/hooks/useLocale";
+import { trpc } from "@calcom/trpc/react";
+import { AppSkeletonLoader as SkeletonLoader, Meta, Alert } from "@calcom/ui";
+
+export default function SSOConfiguration({ teamId }: { teamId: number | null }) {
+  const [errorMessage, setErrorMessage] = useState("");
+  const { t } = useLocale();
+
+  const { data: connection, isLoading } = trpc.viewer.saml.get.useQuery(
+    { teamId },
+    {
+      onError: (err) => {
+        setErrorMessage(err.message);
+      },
+    }
+  );
+
+  if (isLoading) {
+    return <SkeletonLoader />;
+  }
+
+  if (errorMessage) {
+    return (
+      <>
+        <Meta title={t("saml_config")} description={t("saml_description")} />
+        <Alert severity="warning" message={t(errorMessage)} className="mb-4 " />
+      </>
+    );
+  }
+
+  // No connection found
+  if (!connection) {
+    return (
+      <LicenseRequired>
+        <div className="flex flex-col space-y-10">
+          <SAMLConnection teamId={teamId} />
+          <OIDCConnection teamId={teamId} />
+        </div>
+      </LicenseRequired>
+    );
+  }
+
+  return (
+    <LicenseRequired>
+      <div className="flex flex-col space-y-6">
+        {connection.type === "saml" ? <SAMLConnection teamId={teamId} /> : <OIDCConnection teamId={teamId} />}
+        <ConnectionInfo teamId={teamId} connection={connection} />
+      </div>
+    </LicenseRequired>
+  );
+}
