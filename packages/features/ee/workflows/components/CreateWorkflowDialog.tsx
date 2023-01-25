46eb2fe4c2d2868d4651d896137e8d09a9b4ee03
CarinaWolli <wollencarina@gmail.com>
2023-01-25 15:50:28 -0500

    continued work on workflow create dialog

--- packages/features/ee/workflows/components/CreateWorkflowDialog.tsx
+++ packages/features/ee/workflows/components/CreateWorkflowDialog.tsx
@@ -1,10 +1,41 @@
-import { Dialog, DialogContent } from "@calcom/ui";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Dialog, DialogContent, DialogHeader, Form } from "@calcom/ui";
+import type { MultiSelectCheckboxesOptionType as Option } from "@calcom/ui";
+import { FiZap } from "@calcom/ui/components/icon";
+
+type FormValues = {
+  name?: string;
+  activeOn: Option[];
+};
 
 export const CreateWorkflowDialog = () => {
+  const formSchema = z.object({
+    name: z.string(),
+    activeOn: z.object({ value: z.string(), label: z.string() }).array(),
+  });
+
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+  });
+
   return (
     <Dialog name="new">
-      <DialogContent type="creation" title="Create Workflow">
-        <>Create Workflow Dialog</>
+      <DialogContent type="creation">
+        <>
+          <div className="flex ">
+            <FiZap className="mt-1 mr-2.5" />
+            <div className="text-base font-medium ">Workflow in Cal.com</div>
+          </div>
+          <Form
+            form={form}
+            handleSubmit={(values) => {
+              console.log("Create Workflow");
+            }}
+          />
+        </>
       </DialogContent>
     </Dialog>
   );
