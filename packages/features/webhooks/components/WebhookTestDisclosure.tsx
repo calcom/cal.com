3928a42d10504d143e14c0a43ba3359ca734bb78
Joe Shajan <joeshajan1551@gmail.com>
2023-01-30 22:01:41 +0530

    webhook response style
    Co-authored-by: Carina Wollendorfer <30310907+CarinaWolli@users.noreply.github.com>

--- packages/features/webhooks/components/WebhookTestDisclosure.tsx
+++ packages/features/webhooks/components/WebhookTestDisclosure.tsx
@@ -33,7 +33,7 @@ export default function WebhookTestDisclosure() {
       <div className="space-y-0 rounded-md border-0 border-neutral-200 sm:mx-0 md:border">
         <div className="flex justify-between border-b p-4">
           <div className="flex items-center space-x-1">
-            <h3 className="font-sm self-center font-medium text-black">{t("webhook_response")}</h3>
+            <h3 className="self-center text-sm font-semibold leading-4">{t("webhook_response")}</h3>
             {mutation.data && (
               <Badge variant={mutation.data.ok ? "green" : "red"}>
                 {mutation.data.ok ? t("passed") : t("failed")}
