38db48438703c9d2b994201edf38904db926b0fb
joeshajan <joeshajan1551@gmail.com>
2023-01-31 21:32:28 +0530

    fix: webhook test border & test webhook font style

--- packages/features/webhooks/components/WebhookTestDisclosure.tsx
+++ packages/features/webhooks/components/WebhookTestDisclosure.tsx
@@ -20,7 +20,7 @@ export default function WebhookTestDisclosure() {
       <div className="flex justify-between">
         <div>
           <p className="text-sm font-semibold leading-5">{t("webhook_test")}</p>
-          <p className="font-sm mb-4 text-gray-600">{t("test_webhook")}</p>
+          <p className="mb-4 text-sm text-gray-600">{t("test_webhook")}</p>
         </div>
         <Button
           type="button"
@@ -31,7 +31,7 @@ export default function WebhookTestDisclosure() {
           {t("ping_test")}
         </Button>
       </div>
-      <div className="space-y-0 rounded-md border-0 border-neutral-200 sm:mx-0 md:border">
+      <div className="space-y-0 rounded-md  border border-neutral-200 sm:mx-0">
         <div className="flex justify-between border-b p-4">
           <div className="flex items-center space-x-1">
             <h3 className="self-center text-sm font-semibold leading-4">{t("webhook_response")}</h3>
