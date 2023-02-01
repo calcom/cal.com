19856ce048d78bb5c78ea22b1e9c12f7d8321422
CarinaWolli <wollencarina@gmail.com>
2023-02-01 13:30:20 -0500

    improve UI of filter

--- packages/features/ee/workflows/pages/index.tsx
+++ packages/features/ee/workflows/pages/index.tsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { useState, Dispatch, SetStateAction, useEffect } from "react";
 
 import Shell from "@calcom/features/shell/Shell";
+import { classNames } from "@calcom/lib";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { HttpError } from "@calcom/lib/http-error";
 import { trpc } from "@calcom/trpc/react";
@@ -107,11 +108,13 @@ function WorkflowsPage() {
         ) : (
           <>
             {allWorkflowsData?.workflows && allWorkflowsData.workflows.length > 0 && (
-              <Filter
-                profiles={query.data.profiles}
-                checked={checkedFilterItems}
-                setChecked={setCheckedFilterItems}
-              />
+              <div>
+                <Filter
+                  profiles={query.data.profiles}
+                  checked={checkedFilterItems}
+                  setChecked={setCheckedFilterItems}
+                />
+              </div>
             )}
             <WorkflowList
               workflows={filteredWorkflows}
@@ -144,88 +147,107 @@ const Filter = (props: {
     }>
   >;
 }) => {
-  const { t } = useLocale();
   const session = useSession();
   const userId = session.data?.user.id || 0;
   const userName = session.data?.user.name || "";
 
   const teams = props.profiles.filter((profile) => !!profile.teamId);
-  const teamNames = teams.map((profile) => profile.name);
   const { checked, setChecked } = props;
 
-  return (
-    <AnimatedPopover text={teamNames && teamNames.length > 0 ? `${teamNames.join(", ")}` : ""}>
-      <div className="item-center flex px-4 py-[6px] focus-within:bg-gray-100 hover:cursor-pointer hover:bg-gray-50">
-        <Avatar
-          imageSrc=""
-          size="sm"
-          alt={`${userName} Avatar`}
-          gravatarFallbackMd5="fallback"
-          className="self-center"
-          asChild
-        />
-        <label
-          htmlFor="yourWorkflows"
-          className="ml-2 mr-auto self-center truncate text-sm font-medium text-gray-700">
-          {userName}
-        </label>
+  const [noFilter, setNoFilter] = useState(true);
 
-        <input
-          id="yourWorkflows"
-          type="checkbox"
-          className="text-primary-600 focus:ring-primary-500 inline-flex h-4 w-4 place-self-center justify-self-end rounded border-gray-300 "
-          checked={!!checked.userId}
-          onChange={(e) => {
-            if (e.target.checked) {
-              setChecked({ userId: userId, teamIds: checked.teamIds });
-            } else if (!e.target.checked) {
-              setChecked({ userId: null, teamIds: checked.teamIds });
-            }
-          }}
-        />
-      </div>
-      {teams.map((profile) => (
-        <div
-          className="item-center flex px-4 py-[6px] focus-within:bg-gray-100 hover:cursor-pointer hover:bg-gray-50"
-          key={`${profile.teamId || 0}`}>
+  return (
+    <div className={classNames(noFilter ? "w-16" : "w-24")}>
+      <AnimatedPopover text={noFilter ? "All" : "Filtered"}>
+        <div className="item-center flex px-4 py-[6px] focus-within:bg-gray-100 hover:cursor-pointer hover:bg-gray-50">
           <Avatar
             imageSrc=""
             size="sm"
-            alt={`${profile.slug} Avatar`}
+            alt={`${userName} Avatar`}
             gravatarFallbackMd5="fallback"
             className="self-center"
             asChild
           />
           <label
-            htmlFor={profile.slug || ""}
-            className="ml-2 mr-auto select-none self-center truncate text-sm font-medium text-gray-700 hover:cursor-pointer">
-            {profile.slug}
+            htmlFor="yourWorkflows"
+            className="ml-2 mr-auto self-center truncate text-sm font-medium text-gray-700">
+            {userName}
           </label>
 
           <input
-            id={profile.slug || ""}
-            name={profile.slug || ""}
+            id="yourWorkflows"
             type="checkbox"
-            checked={checked.teamIds?.includes(profile.teamId || 0)}
+            className="text-primary-600 focus:ring-primary-500 inline-flex h-4 w-4 place-self-center justify-self-end rounded border-gray-300 "
+            checked={!!checked.userId}
             onChange={(e) => {
               if (e.target.checked) {
-                const updatedChecked = checked;
-                updatedChecked.teamIds.push(profile.teamId || 0);
-                setChecked({ userId: checked.userId, teamIds: [...updatedChecked.teamIds] });
-              } else if (!e.target.checked) {
-                const index = checked.teamIds.indexOf(profile.teamId || 0);
-                if (index !== -1) {
-                  const updatedChecked = checked;
-                  updatedChecked.teamIds.splice(index, 1);
-                  setChecked({ userId: checked.userId, teamIds: [...updatedChecked.teamIds] });
+                setChecked({ userId: userId, teamIds: checked.teamIds });
+                if (checked.teamIds.length === teams.length) {
+                  setNoFilter(true);
                 }
+              } else if (!e.target.checked) {
+                setChecked({ userId: null, teamIds: checked.teamIds });
+
+                setNoFilter(false);
               }
             }}
-            className="text-primary-600 focus:ring-primary-500 inline-flex h-4 w-4 place-self-center justify-self-end rounded border-gray-300 "
           />
         </div>
-      ))}
-    </AnimatedPopover>
+        {teams.map((profile) => (
+          <div
+            className="item-center flex px-4 py-[6px] focus-within:bg-gray-100 hover:cursor-pointer hover:bg-gray-50"
+            key={`${profile.teamId || 0}`}>
+            <Avatar
+              imageSrc=""
+              size="sm"
+              alt={`${profile.slug} Avatar`}
+              gravatarFallbackMd5="fallback"
+              className="self-center"
+              asChild
+            />
+            <label
+              htmlFor={profile.slug || ""}
+              className="ml-2 mr-auto select-none self-center truncate text-sm font-medium text-gray-700 hover:cursor-pointer">
+              {profile.slug}
+            </label>
+
+            <input
+              id={profile.slug || ""}
+              name={profile.slug || ""}
+              type="checkbox"
+              checked={checked.teamIds?.includes(profile.teamId || 0)}
+              onChange={(e) => {
+                if (e.target.checked) {
+                  const updatedChecked = checked;
+                  updatedChecked.teamIds.push(profile.teamId || 0);
+                  setChecked({ userId: checked.userId, teamIds: [...updatedChecked.teamIds] });
+
+                  if (checked.userId && updatedChecked.teamIds.length === teams.length) {
+                    setNoFilter(true);
+                  } else {
+                    setNoFilter(false);
+                  }
+                } else if (!e.target.checked) {
+                  const index = checked.teamIds.indexOf(profile.teamId || 0);
+                  if (index !== -1) {
+                    const updatedChecked = checked;
+                    updatedChecked.teamIds.splice(index, 1);
+                    setChecked({ userId: checked.userId, teamIds: [...updatedChecked.teamIds] });
+
+                    if (checked.userId && updatedChecked.teamIds.length === teams.length) {
+                      setNoFilter(true);
+                    } else {
+                      setNoFilter(false);
+                    }
+                  }
+                }
+              }}
+              className="text-primary-600 focus:ring-primary-500 inline-flex h-4 w-4 place-self-center justify-self-end rounded border-gray-300 "
+            />
+          </div>
+        ))}
+      </AnimatedPopover>
+    </div>
   );
 };
 
