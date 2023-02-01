ca7246c7b01c277225d9fd28c3314cab5886767a
Peer Richelsen <peer@cal.com>
2023-02-01 15:19:01 +0100

    mvp of team recommended steps, changes to <Card>

--- packages/features/ee/teams/components/TeamList.tsx
+++ packages/features/ee/teams/components/TeamList.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 
+import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { RouterOutputs, trpc } from "@calcom/trpc/react";
-import { showToast } from "@calcom/ui";
+import { Card, showToast } from "@calcom/ui";
+import { FiUserPlus, FiUsers, FiUnlock } from "@calcom/ui/components/icon";
 
 import TeamListItem from "./TeamListItem";
 
@@ -11,6 +13,8 @@ interface Props {
 
 export default function TeamList(props: Props) {
   const utils = trpc.useContext();
+
+  const { t } = useLocale();
 
   const [hideDropdown, setHideDropdown] = useState(false);
 
@@ -47,6 +51,60 @@ export default function TeamList(props: Props) {
           setHideDropdown={setHideDropdown}
         />
       ))}
+
+      {/* only show recommended steps when there is only one team */}
+      {props.teams.length === 1 && (
+        <>
+          {props.teams.map(
+            (team, i) =>
+              i === 0 && (
+                <div className="bg-gray-100 p-6">
+                  <h3 className="mb-4 text-sm font-semibold text-gray-900">{t("recommended_next_steps")}</h3>
+                  <div className="grid-col-1 grid gap-2 md:grid-cols-3">
+                    <Card
+                      icon={<FiUserPlus className="h-5 w-5 text-green-700" />}
+                      variant="basic"
+                      title={t("invite_team_member")}
+                      description="Meetings are better with the right team members there. Invite them now."
+                      actionButton={{
+                        href: "/settings/teams/" + team.id + "/members",
+                        child: t("invite"),
+                      }}
+                    />
+                    <Card
+                      icon={<FiUnlock className="h-5 w-5 text-blue-700" />}
+                      variant="basic"
+                      title={t("create_a_managed_event")}
+                      description={t("meetings_are_better_with_the_right")}
+                      actionButton={{
+                        href:
+                          "/event-types?dialog=new-eventtype&eventPage=team%2F&managed=true" +
+                          team.name +
+                          "&teamId=" +
+                          team.id,
+                        child: t("create"),
+                      }}
+                    />
+                    <Card
+                      icon={<FiUsers className="h-5 w-5 text-orange-700" />}
+                      variant="basic"
+                      title={t("collective_or_roundrobin")}
+                      description={t("book_your_team_members")}
+                      actionButton={{
+                        href:
+                          "/event-types?dialog=new-eventtype&eventPage=team%2F" +
+                          team.name +
+                          "&teamId=" +
+                          team.id,
+                        child: t("create"),
+                      }}
+                    />
+                  </div>
+                </div>
+              )
+          )}
+        </>
+      )}
     </ul>
   );
 }
