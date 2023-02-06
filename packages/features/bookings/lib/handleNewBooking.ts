390967990a6eb740619bdb1dd7bc578174bd1e43
Nafees Nazik <84864519+G3root@users.noreply.github.com>
2023-02-06 23:35:30 +0530

    fix: metadata not saved while creating a booking. (#6866)
    * feat: add metadata to booking creation
    
    * fix: bug

--- packages/features/bookings/lib/handleNewBooking.ts
+++ packages/features/bookings/lib/handleNewBooking.ts
@@ -744,6 +744,7 @@ async function handler(req: NextApiRequest & { userId?: number | undefined }) {
       location: evt.location,
       eventType: eventTypeRel,
       smsReminderNumber: reqBody.smsReminderNumber,
+      metadata: reqBody.metadata,
       attendees: {
         createMany: {
           data: evt.attendees.map((attendee) => {
@@ -1140,7 +1141,7 @@ async function handler(req: NextApiRequest & { userId?: number | undefined }) {
         uid: booking.uid,
       },
       data: {
-        metadata,
+        metadata: { ...(typeof booking.metadata === "object" && booking.metadata), ...metadata },
         references: {
           createMany: {
             data: referencesToCreate,
