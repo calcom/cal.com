d5f6586f24752e66bdf2fa3d99a6d9b51ec3606d
zomars <zomars@me.com>
2023-02-07 13:27:10 -0700

    Feedback

--- packages/ui/components/form/step/Steps.tsx
+++ packages/ui/components/form/step/Steps.tsx
@@ -1,27 +1,22 @@
 import classNames from "@calcom/lib/classNames";
-import { useLocale } from "@calcom/lib/hooks/useLocale";
 
 interface ISteps {
   maxSteps: number;
   currentStep: number;
   navigateToStep: (step: number) => void;
-  stepOfLabel?: (currentStep: number, maxSteps: number) => string;
+  stepLabel?: (currentStep: number, maxSteps: number) => string;
 }
 
 const Steps = (props: ISteps) => {
-  const { t } = useLocale();
   const {
     maxSteps,
     currentStep,
     navigateToStep,
-    stepOfLabel = (currentStep: number, maxSteps: number) =>
-      t("current_step_of_total", { currentStep: currentStep, maxSteps }),
+    stepLabel = (currentStep, totalSteps) => `Step ${currentStep} of ${totalSteps}`,
   } = props;
   return (
     <div className="mt-6 space-y-2">
-      <p className="text-xs font-medium text-gray-500 dark:text-white">
-        {stepOfLabel(currentStep, maxSteps)}
-      </p>
+      <p className="text-xs font-medium text-gray-500 dark:text-white">{stepLabel(currentStep, maxSteps)}</p>
       <div className="flex w-full space-x-2 rtl:space-x-reverse">
         {new Array(maxSteps).fill(0).map((_s, index) => {
           return index <= currentStep - 1 ? (
