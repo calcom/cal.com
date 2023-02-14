85f28a4064c34e2a3159ad91b65479ff82181cb9
zomars <zomars@me.com>
2023-02-14 13:35:34 -0700

    Update me.ts

--- apps/web/pages/api/me.ts
+++ apps/web/pages/api/me.ts
@@ -1,18 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
 
+import { performance } from "@calcom/lib/server/perfObserver";
+
 let isCold = true;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
-  const prePrismaDate = new Date();
+  const prePrismaDate = performance.now();
   const prisma = (await import("@calcom/prisma")).default;
-  const preSessionDate = new Date();
+  const preSessionDate = performance.now();
   const session = await getSession({ req });
   if (!session) return res.status(409).json({ message: "Unauthorized" });
-  const preUserDate = new Date();
+  const preUserDate = performance.now();
   const user = await prisma.user.findUnique({ where: { id: session.user.id } });
   if (!user) return res.status(404).json({ message: "No user found" });
-  const lastUpdate = new Date();
+  const lastUpdate = performance.now();
 
   res.setHeader("x-is-cold", isCold.toString());
   isCold = false;
@@ -20,11 +22,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   return res.status(200).json({
     message: `Hello ${user.name}`,
     prePrismaDate,
-    prismaDuration: `Prisma took ${preSessionDate.getMilliseconds() - prePrismaDate.getMilliseconds()}ms`,
+    prismaDuration: `Prisma took ${preSessionDate - prePrismaDate}ms`,
     preSessionDate,
-    sessionDuration: `Session took ${preUserDate.getMilliseconds() - preSessionDate.getMilliseconds()}ms`,
+    sessionDuration: `Session took ${preUserDate - preSessionDate}ms`,
     preUserDate,
-    userDuration: `User took ${lastUpdate.getMilliseconds() - preUserDate.getMilliseconds()}ms`,
+    userDuration: `User took ${lastUpdate - preUserDate}ms`,
     lastUpdate,
+    wasCold: isCold,
   });
 }
