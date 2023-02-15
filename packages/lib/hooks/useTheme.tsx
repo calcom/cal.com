29c02ad76a8e8c5d213a9a02cc81f3295ae08781
nafees nazik <nafeesnazik21@gmail.com>
2023-02-15 14:34:36 +0530

    refactor: useTheme hook

--- packages/lib/hooks/useTheme.tsx
+++ packages/lib/hooks/useTheme.tsx
@@ -1,29 +1,28 @@
 import { useTheme as useNextTheme } from "next-themes";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 import { useEmbedTheme } from "@calcom/embed-core/embed-iframe";
 import { Maybe } from "@calcom/trpc/server";
 
 // makes sure the ui doesn't flash
-export default function useTheme(theme?: Maybe<string>) {
-  theme = theme || "system";
-  const { resolvedTheme, setTheme, forcedTheme } = useNextTheme();
-  const [isReady, setIsReady] = useState<boolean>(false);
+export default function useTheme(theme?: null) {
+  let currentTheme: Maybe<string> = theme || "system";
+
+  const { resolvedTheme, setTheme, forcedTheme, theme: activeTheme } = useNextTheme();
   const embedTheme = useEmbedTheme();
   // Embed UI configuration takes more precedence over App Configuration
-  theme = embedTheme || theme;
+  currentTheme = embedTheme || theme;
 
   useEffect(() => {
-    if (theme) {
-      setTheme(theme);
+    if (currentTheme !== activeTheme && typeof currentTheme === "string") {
+      setTheme(currentTheme);
     }
-    setIsReady(true);
-  }, [theme, setTheme]);
+  }, [currentTheme, setTheme]);
 
   return {
     resolvedTheme,
     setTheme,
-    isReady,
     forcedTheme,
+    activeTheme,
   };
 }
