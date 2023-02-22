2009a994ccb77b194f517dcce069e3f22b17ac2b
warren <5959690+wrn14897@users.noreply.github.com>
2023-02-22 11:17:29 -0800

    rollback temp

--- packages/embeds/embed-core/playwright/config/playwright.config.ts
+++ packages/embeds/embed-core/playwright/config/playwright.config.ts
@@ -15,7 +15,7 @@ const config: PlaywrightTestConfig = {
   timeout: 60_000,
   reporter: [
     [CI ? "github" : "list"],
-    ["@deploysentinel/playwright"],
+    // ["@deploysentinel/playwright"],
     [
       "html",
       { outputFolder: path.join(__dirname, "..", "reports", "playwright-html-report"), open: "never" },
