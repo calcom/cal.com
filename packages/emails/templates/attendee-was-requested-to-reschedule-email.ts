357bcd797b2e31a71fa309407df3f15b548fc4b1
Bram Verstraten <bramverstraten@outlook.com>
2023-02-22 09:28:32 +0100

    Update attendee-was-requested-to-reschedule-email.ts

--- packages/emails/templates/attendee-was-requested-to-reschedule-email.ts
+++ packages/emails/templates/attendee-was-requested-to-reschedule-email.ts
@@ -22,7 +22,7 @@ export default class AttendeeWasRequestedToRescheduleEmail extends OrganizerSche
         filename: "event.ics",
         content: this.getiCalEventAsString(),
       },
-      from: `${APP_NAME} <${this.getMailerOptions().from}>`,
+      from: `${this.calEvent.organizer.name} <${this.getMailerOptions().from}>`,
       to: toAddresses.join(","),
       subject: `${this.t("requested_to_reschedule_subject_attendee", {
         eventType: this.calEvent.type,
