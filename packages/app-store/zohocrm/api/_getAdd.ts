611b8c7d567535a6c93805a864b91ec8b4ad3cce
Jatin Sandilya <7681067+jatinsandilya@users.noreply.github.com>
2023-02-23 23:12:28 +0530

    Update packages/app-store/zohocrm/api/_getAdd.ts
    Co-authored-by: Hariom Balhara <hariombalhara@gmail.com>

--- packages/app-store/zohocrm/api/_getAdd.ts
+++ packages/app-store/zohocrm/api/_getAdd.ts
@@ -14,7 +14,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (!client_id) return res.status(400).json({ message: "zohocrm client id missing." });
 
   const redirectUri = WEBAPP_URL + "/api/integrations/zohocrm/callback";
-  console.log("redirectUri", redirectUri);
   const url = `https://accounts.zoho.com/oauth/v2/auth?scope=ZohoCRM.modules.ALL,ZohoCRM.users.READ,AaaServer.profile.READ&client_id=${client_id}&response_type=code&access_type=offline&redirect_uri=${redirectUri}`;
   res.status(200).json({ url });
 }
