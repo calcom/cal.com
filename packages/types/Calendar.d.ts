50be9255cb8f839c2e8914e03a963c63b7dd5948
Joe Au-Yeung <65426560+joeauyeung@users.noreply.github.com>
2023-02-27 15:45:40 -0500

    Fix - add team members to emails (#7207)
    * On booking add team members & translation
    
    * Add team members to round robin create
    
    * Only update calendars on reschedule if there is a calendar reference
    
    * Send email on reschedules
    
    * Send team email on cancelled event
    
    * Add team members to calendar event description
    
    * Clean up
    
    * Convert other emails to organizer & teams
    
    * Type check fixes
    
    * More type fixes
    
    * Change organizer scheduled input to an object
    
    * early return updateCalendarEvent
    
    * Introduce team member type
    
    * Fix type errors
    
    * Put team members before attendees
    
    * Remove lodash cloneDeep
    
    * Update packages/core/EventManager.ts
    
    Co-authored-by: Omar López <zomars@me.com>
    
    * Remove booking select object
    
    * Revert "Remove booking select object"
    
    This reverts commit 9f121ff4ebbaec9a0643111f96c5337b930ddd5b.
    
    * Refactor email manager (#7270)
    
    Co-authored-by: zomars <zomars@me.com>
    
    * Type change
    
    * Remove conditional check for updateAllCalendarEvents
    
    ---------
    
    Co-authored-by: zomars <zomars@me.com>

--- packages/types/Calendar.d.ts
+++ packages/types/Calendar.d.ts
@@ -27,6 +27,13 @@ export type Person = {
   locale?: string;
 };
 
+export type TeamMember = {
+  name: string;
+  email: string;
+  timeZone: string;
+  language: { translate: TFunction; locale: string };
+};
+
 export type EventBusyDate = {
   start: Date | string;
   end: Date | string;
@@ -137,7 +144,7 @@ export interface CalendarEvent {
   description?: string | null;
   team?: {
     name: string;
-    members: string[];
+    members: TeamMember[];
   };
   location?: string | null;
   conferenceData?: ConferenceData;
