c72382f885d4c374cf42935c79d6918b7cfcd0dc
Alan <alannnc@gmail.com>
2023-02-27 13:58:15 -0700

    Added events timeline

--- packages/trpc/server/routers/viewer/analytics.tsx
+++ packages/trpc/server/routers/viewer/analytics.tsx
@@ -1,4 +1,4 @@
-import { Prisma } from "@prisma/client";
+import type { Prisma } from "@prisma/client";
 import { z } from "zod";
 
 import { TRPCError } from "@trpc/server";
@@ -111,4 +111,124 @@ export const analyticsRouter = router({
         },
       };
     }),
+  eventsTimeline: userBelongsToTeamProcedure
+    .input(
+      z.object({
+        teamId: z.coerce.number().optional(),
+        startDate: z.string(),
+        endDate: z.string(),
+        eventTypeId: z.coerce.number().optional(),
+        timeView: z.enum(["week", "month", "year"]),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { teamId, startDate, endDate, eventTypeId, timeView } = input;
+      const user = ctx.user;
+
+      // Just for type safety but authedProcedure should have already checked this
+      if (!user) {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+        });
+      }
+
+      let whereConditional: Prisma.BookingWhereInput = {
+        userId: user.id,
+      };
+
+      if (teamId && !!whereConditional) {
+        whereConditional = {
+          ...whereConditional,
+          eventType: {
+            teamId: teamId,
+          },
+        };
+      } else if (eventTypeId && !!whereConditional) {
+        whereConditional = {
+          ...whereConditional,
+          eventTypeId: eventTypeId,
+        };
+      }
+
+      const bookings = await ctx.prisma.booking.findMany({
+        where: {
+          ...whereConditional,
+          createdAt: {
+            gte: new Date(startDate),
+            lte: new Date(endDate),
+          },
+          eventType: {
+            teamId: teamId,
+          },
+        },
+        select: {
+          id: true,
+          eventType: true,
+        },
+      });
+
+      // Get timeline data
+      if (timeView) {
+        // switch (timeView) {
+        //   case "week":
+        //     return getWeekTimeline(booking);
+        //   case "month":
+        //     return getMonthTimeline(booking);
+        //   case "year":
+        //     return getYearTimeline(booking);
+        //   default:
+        //     return getWeekTimeline(booking);
+        // }
+        console.log({ bookings });
+      }
+
+      return [
+        {
+          Month: "Dec 15",
+          Created: 2890,
+          Completed: 2390,
+          Rescheduled: 500,
+          Cancelled: 100,
+        },
+        {
+          Month: "Dec 22",
+          Created: 1890,
+          Completed: 1590,
+          Rescheduled: 300,
+          Cancelled: 120,
+        },
+        {
+          Month: "Dec 29",
+          Created: 4890,
+          Completed: 4290,
+          Rescheduled: 800,
+          Cancelled: 300,
+        },
+        {
+          Month: "Jan 06",
+          Created: 3890,
+          Completed: 2400,
+          Rescheduled: 500,
+          Cancelled: 200,
+        },
+        {
+          Month: "Jan 13",
+          Created: 1890,
+          Completed: 1590,
+          Rescheduled: 200,
+          Cancelled: 50,
+        },
+      ];
+    }),
 });
+function getWeekTimeline() {
+  throw new Error("Function not implemented.");
+}
+
+function getMonthTimeline() {
+  throw new Error("Function not implemented.");
+}
+
+function getYearTimeline() {
+  throw new Error("Function not implemented.");
+}
