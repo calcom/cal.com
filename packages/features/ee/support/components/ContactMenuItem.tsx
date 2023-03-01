0b5ed1a78f7a3432a80658a8a3f03590d21d0514
nafees nazik <nafeesnazik21@gmail.com>
2023-03-01 15:54:07 +0530

    feat: add fresh chat menu item and render

--- packages/features/ee/support/components/ContactMenuItem.tsx
+++ packages/features/ee/support/components/ContactMenuItem.tsx
@@ -1,13 +1,15 @@
+import FreshChatMenuItem from "../lib/freshchat/FreshChatMenuItem";
 import HelpscoutMenuItem from "../lib/helpscout/HelpscoutMenuItem";
 import IntercomMenuItem from "../lib/intercom/IntercomMenuItem";
 import ZendeskMenuItem from "../lib/zendesk/ZendeskMenuItem";
 
-export default function HelpMenuItem() {
+export default function ContactMenuItem() {
   return (
     <>
       <IntercomMenuItem />
       <ZendeskMenuItem />
       <HelpscoutMenuItem />
+      <FreshChatMenuItem />
     </>
   );
 }
