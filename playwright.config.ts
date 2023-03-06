243b6f3a6e460d2e6b24615dcca013a99eada7ee
Alex van Andel <me@alexvanandel.com>
2023-03-06 10:22:02 +0000

    Allow for two retries of flaky tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ if (IS_EMBED_TEST) {
 
 const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
-  retries: 0,
+  retries: 2,
   workers: os.cpus().length,
   timeout: 60_000,
   maxFailures: headless ? 10 : undefined,
