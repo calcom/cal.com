fdb0a4a8cdb61fffa318fe9511bc549822729e5a
nafees nazik <nafeesnazik21@gmail.com>
2023-03-08 21:31:23 +0530

    fix: disable escaping characters

--- packages/core/event.ts
+++ packages/core/event.ts
@@ -17,6 +17,9 @@ export function getEventName(eventNameObj: EventNameObjectType, forAttendeeView 
       eventName: eventNameObj.eventType,
       host: eventNameObj.host,
       attendeeName: eventNameObj.attendeeName,
+      interpolation: {
+        escapeValue: false,
+      },
     });
 
   let eventName = eventNameObj.eventName;
