6b0451fb4ef7edc2aa894c5e70f6cf9a866ae37f
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2023-03-11 13:24:24 +0800

    Darkmode buttons and labels

--- packages/ui/components/form/inputs/Label.tsx
+++ packages/ui/components/form/inputs/Label.tsx
@@ -4,10 +4,7 @@ export function Label(props: JSX.IntrinsicElements["label"]) {
   return (
     <label
       {...props}
-      className={classNames(
-        "text-default dark:text-inverted mb-2 block text-sm font-medium leading-none",
-        props.className
-      )}>
+      className={classNames("text-default mb-2 block text-sm font-medium leading-none", props.className)}>
       {props.children}
     </label>
   );
