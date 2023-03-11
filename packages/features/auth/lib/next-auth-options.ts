46c18c8ab1a0d84cd3f38892cd81c7aaeda79366
Alex van Andel <me@alexvanandel.com>
2023-03-11 17:31:38 +0000

    Does case insensitive select, then overwrites email with IdP email

--- packages/features/auth/lib/next-auth-options.ts
+++ packages/features/auth/lib/next-auth-options.ts
@@ -327,6 +327,16 @@ function isNumber(n: string) {
 
 const calcomAdapter = CalComAdapter(prisma);
 
+const mapIdentityProvider = (providerName: string) => {
+  switch (providerName) {
+    case "saml-idp":
+    case "saml":
+      return IdentityProvider.SAML;
+    default:
+      return IdentityProvider.GOOGLE;
+  }
+};
+
 export const AUTH_OPTIONS: AuthOptions = {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
@@ -496,10 +506,7 @@ export const AUTH_OPTIONS: AuthOptions = {
       }
 
       if (account?.provider) {
-        let idP: IdentityProvider = IdentityProvider.GOOGLE;
-        if (account.provider === "saml" || account.provider === "saml-idp") {
-          idP = IdentityProvider.SAML;
-        }
+        const idP: IdentityProvider = mapIdentityProvider(account.provider);
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore-error TODO validate email_verified key on profile
         user.email_verified = user.email_verified || !!user.emailVerified || profile.email_verified;
@@ -508,7 +515,6 @@ export const AUTH_OPTIONS: AuthOptions = {
           return "/auth/error?error=unverified-email";
         }
 
-        // Only google oauth on this path
         const existingUser = await prisma.user.findFirst({
           include: {
             accounts: {
@@ -518,7 +524,7 @@ export const AUTH_OPTIONS: AuthOptions = {
             },
           },
           where: {
-            identityProvider: idP as IdentityProvider,
+            identityProvider: idP,
             identityProviderId: account.providerAccountId,
           },
         });
@@ -568,7 +574,12 @@ export const AUTH_OPTIONS: AuthOptions = {
         // a new account. If an account already exists with the incoming email
         // address return an error for now.
         const existingUserWithEmail = await prisma.user.findFirst({
-          where: { email: user.email },
+          where: {
+            email: {
+              equals: user.email,
+              mode: "insensitive",
+            },
+          },
         });
 
         if (existingUserWithEmail) {
@@ -588,8 +599,10 @@ export const AUTH_OPTIONS: AuthOptions = {
             !existingUserWithEmail.username
           ) {
             await prisma.user.update({
-              where: { email: user.email },
+              where: { email: existingUserWithEmail.email },
               data: {
+                // update the email to the IdP email
+                email: user.email,
                 // Slugify the incoming name and append a few random characters to
                 // prevent conflicts for users with the same name.
                 username: usernameSlug(user.name),
@@ -614,7 +627,8 @@ export const AUTH_OPTIONS: AuthOptions = {
           ) {
             await prisma.user.update({
               where: { email: existingUserWithEmail.email },
-              data: { password: null },
+              // also update email to the IdP email
+              data: { password: null, email: user.email },
             });
             if (existingUserWithEmail.twoFactorEnabled) {
               return loginWithTotp(existingUserWithEmail);
