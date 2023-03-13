48261f5276ba309a828d063a7e94b5bd2d3b671f
nafees nazik <nafeesnazik21@gmail.com>
2023-03-13 19:16:12 +0530

    fix: revert dangerouslySetInnerHTML

--- packages/lib/OgImages.tsx
+++ packages/lib/OgImages.tsx
@@ -235,7 +235,7 @@ export const Generic = ({ title, description }: GenericImageProps) => (
           {title}
         </div>
         <div tw="flex mt-3 w-[1040px]" style={{ fontFamily: "inter" }}>
-          <div dangerouslySetInnerHTML={{ __html: description }} />
+          {description}
         </div>
       </div>
     </div>
