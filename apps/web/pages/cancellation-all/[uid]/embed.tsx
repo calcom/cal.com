a6c5338e19fe7e1bf1cb39613fe553ce01d04fac
Ben Kirchner <ben@mento.co>
2023-03-15 13:42:20 -0600

    Full revert to main

--- apps/web/pages/cancellation-all/[uid]/embed.tsx
+++
@@ -1,7 +0,0 @@
-import withEmbedSsr from "@lib/withEmbedSsr";
-
-import { getServerSideProps as _getServerSideProps } from "../[uid]";
-
-export { default } from "../[uid]";
-
-export const getServerSideProps = withEmbedSsr(_getServerSideProps);
