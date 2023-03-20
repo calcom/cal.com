b9efdbd7056f07e5374df26e83d47dbe3606a89e
CarinaWolli <wollencarina@gmail.com>
2023-03-20 08:40:45 +0100

    fix type error and add yarn.lock

--- packages/prisma/middleware/eventTypeDescriptionParseAndSanitize.ts
+++ packages/prisma/middleware/eventTypeDescriptionParseAndSanitize.ts
@@ -6,6 +6,7 @@ import { md } from "@calcom/lib/markdownIt";
 
 export function parseAndSanitize(description: string) {
   const window = new JSDOM("").window;
+  // @ts-expect-error as suggested here: https://github.com/cure53/DOMPurify/issues/437#issuecomment-632021941
   const purify = DOMPurify(window);
   const parsedMarkdown = purify.sanitize(md.render(description));
   return parsedMarkdown;
