1fc68836f1cf01d169ca40a85a243b364851d2ed
nafees nazik <nafeesnazik21@gmail.com>
2023-03-22 01:14:45 +0530

    refactor: Intercom hook

--- packages/features/ee/support/lib/intercom/useIntercom.ts
+++ packages/features/ee/support/lib/intercom/useIntercom.ts
@@ -1,12 +1,35 @@
 import noop from "lodash/noop";
-import { useIntercom as _useIntercom } from "react-use-intercom";
+import { useIntercom as useIntercomLib } from "react-use-intercom";
+import { z } from "zod";
 
-export const useIntercom =
-  typeof window !== "undefined" && !!process.env.NEXT_PUBLIC_INTERCOM_APP_ID
-    ? _useIntercom
-    : () => ({
+import dayjs from "@calcom/dayjs";
+import { trpc } from "@calcom/trpc/react";
+
+// eslint-disable-next-line turbo/no-undeclared-env-vars
+export const isInterComEnabled = z.string().min(1).safeParse(process.env.NEXT_PUBLIC_INTERCOM_APP_ID).success;
+
+const useIntercomHook = isInterComEnabled
+  ? useIntercomLib
+  : () => {
+      return {
         boot: noop,
         show: noop,
-      });
+      };
+    };
+export const useIntercom = () => {
+  const hookData = useIntercomHook();
+  const { data } = trpc.viewer.me.useQuery();
+
+  const open = () => {
+    hookData.boot({
+      name: data?.name ?? "",
+      email: data?.email,
+      userId: String(data?.id),
+      createdAt: String(dayjs().unix()),
+    });
+    hookData.show();
+  };
+  return { ...hookData, open };
+};
 
 export default useIntercom;
