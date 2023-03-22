4cf508a4b7bf17ef58b4cd14c640db1c270e7f22
Alan <alannnc@gmail.com>
2023-03-22 12:03:28 -0700

    Add more translations

--- apps/web/pages/insights/components/KPICard.tsx
+++ apps/web/pages/insights/components/KPICard.tsx
@@ -1,5 +1,6 @@
 import { Card, Flex, Text, Metric, BadgeDelta } from "@tremor/react";
 
+import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { Tooltip } from "@calcom/ui";
 
 import { CalculateDeltaType, colors, valueFormatter } from "../index";
@@ -17,6 +18,7 @@ const KPICard = ({
   };
   previousDateRange: { startDate: string; endDate: string };
 }) => {
+  const { t } = useLocale();
   return (
     <Card key={title}>
       <Text>{title}</Text>
@@ -33,8 +35,12 @@ const KPICard = ({
             {Number(previousMetricData.deltaPrevious).toFixed(0)}%
           </Text>
 
-          <Tooltip content={`From: ${previousDateRange.startDate} To: ${previousDateRange.endDate}`}>
-            <small className="relative top-px text-xs text-gray-600">from last period</small>
+          <Tooltip
+            content={t("from_to_date_period", {
+              startDate: previousDateRange.startDate,
+              endDate: previousDateRange.endDate,
+            })}>
+            <small className="relative top-px text-xs text-gray-600">{t("from_last_period")}</small>
           </Tooltip>
         </Flex>
       </Flex>
