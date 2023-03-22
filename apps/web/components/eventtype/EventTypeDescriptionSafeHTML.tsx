b00271be203ef69086ffcd654a1b6810b1459ff9
Hariom Balhara <hariombalhara@gmail.com>
2023-03-22 08:53:36 +0530

    Now that eventType.descriptionAsSafeHTML is added at all the missing places, we can do away with ts-ignore and get type safety

--- apps/web/components/eventtype/EventTypeDescriptionSafeHTML.tsx
+++ apps/web/components/eventtype/EventTypeDescriptionSafeHTML.tsx
@@ -1,12 +1,11 @@
 export type EventTypeDescriptionSafeProps = {
-  eventType: { description: string | null };
+  eventType: { description: string | null; descriptionAsSafeHTML: string | null };
 };
 
 export const EventTypeDescriptionSafeHTML = ({ eventType }: EventTypeDescriptionSafeProps) => {
   const props: JSX.IntrinsicElements["div"] = { suppressHydrationWarning: true };
   if (eventType.description)
-    // @ts-expect-error: @see packages/prisma/middleware/eventTypeDescriptionParseAndSanitize.ts
-    props.dangerouslySetInnerHTML = { __html: eventType.descriptionAsSafeHTML };
+    props.dangerouslySetInnerHTML = { __html: eventType.descriptionAsSafeHTML || "" };
   return <div {...props} />;
 };
 
