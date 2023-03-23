afdba4d3d2d5a62daa1fe45347d68b880a4c9ae6
zomars <zomars@me.com>
2023-03-23 14:17:47 -0700

    Builds are now passing

--- packages/features/insights/filters/UsersListInTeam.tsx
+++ packages/features/insights/filters/UsersListInTeam.tsx
@@ -1,16 +1,22 @@
 import { useLocale } from "@calcom/lib/hooks/useLocale";
+import type { RouterOutputs } from "@calcom/trpc";
 import { trpc } from "@calcom/trpc";
 import { Select } from "@calcom/ui";
 
 import { useFilterContext } from "../context/provider";
 
+type User = RouterOutputs["viewer"]["insights"]["userList"][number];
+type Option = { value: number; label: string };
+
+const mapUserToOption = (user: User): Option => ({
+  value: user.id,
+  label: user.name ?? "",
+});
+
 export const UserListInTeam = () => {
   const { t } = useLocale();
   const { filter, setSelectedUserId } = useFilterContext();
-  const { selectedFilter } = filter;
-
-  const { selectedTeamId, selectedUserId } = filter;
-
+  const { selectedFilter, selectedTeamId, selectedUserId } = filter;
   const { data, isSuccess } = trpc.viewer.insights.userList.useQuery({
     teamId: selectedTeamId,
   });
@@ -18,33 +24,28 @@ export const UserListInTeam = () => {
   if (!selectedFilter?.includes("user")) return null;
   if (!selectedTeamId) return null;
 
-  const UserListOptions: any =
-    data?.map((item) => ({
-      value: item.id,
-      label: item.name ?? "",
-      option: item.name ?? "",
-    })) ?? ([] as { value: number; label: string }[]);
+  const userListOptions = data?.map(mapUserToOption) ?? ([] as { value: number; label: string }[]);
+  const selectedUser = data?.find((item) => item.id === selectedUserId);
+  const userValue = selectedUser ? mapUserToOption(selectedUser) : null;
 
   if (!isSuccess || data?.length === 0) return null;
 
   return (
-    <>
-      <Select
-        isSearchable={false}
-        className="mb-0 ml-2 h-[38px] w-40 min-w-[140px] capitalize md:min-w-[150px] md:max-w-[200px]"
-        defaultValue={selectedUserId === null ? data[0].id : selectedUserId}
-        options={UserListOptions}
-        onChange={(input: { value: number; label: string }) => {
-          if (input) {
-            setSelectedUserId(input.value);
-          }
-        }}
-        placeholder={
-          <div className="flex flex-row">
-            <p>{t("select_user")}</p>
-          </div>
+    <Select<Option>
+      isSearchable={false}
+      className="mb-0 ml-2 h-[38px] w-40 min-w-[140px] capitalize md:min-w-[150px] md:max-w-[200px]"
+      defaultValue={userValue}
+      options={userListOptions}
+      onChange={(input) => {
+        if (input) {
+          setSelectedUserId(input.value);
         }
-      />
-    </>
+      }}
+      placeholder={
+        <div className="flex flex-row">
+          <p>{t("select_user")}</p>
+        </div>
+      }
+    />
   );
 };
