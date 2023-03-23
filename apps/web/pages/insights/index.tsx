9d0b99ce16122f5ba0f227a5ed5cbba0c2dbc0bc
zomars <zomars@me.com>
2023-03-23 14:40:53 -0700

    Cleanup

--- apps/web/pages/insights/index.tsx
+++ apps/web/pages/insights/index.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-
-import dayjs from "@calcom/dayjs";
 import {
   AverageEventDurationChart,
   BookingKPICards,
@@ -9,8 +6,8 @@ import {
   MostBookedTeamMembersTable,
   PopularEventsTable,
 } from "@calcom/features/insights/components";
-import type { FilterContextType } from "@calcom/features/insights/context/provider";
-import { FilterProvider } from "@calcom/features/insights/context/provider";
+import { FiltersProvider } from "@calcom/features/insights/context/FiltersProvider";
+import { useFilterContext } from "@calcom/features/insights/context/provider";
 import { Filters } from "@calcom/features/insights/filters";
 import Shell from "@calcom/features/shell/Shell";
 import { UpgradeTip } from "@calcom/features/tips";
@@ -19,6 +16,23 @@ import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { trpc } from "@calcom/trpc";
 import { Button, ButtonGroup } from "@calcom/ui";
 import { FiRefreshCcw, FiUserPlus, FiUsers } from "@calcom/ui/components/icon";
+
+const Heading = () => {
+  const { t } = useLocale();
+  const {
+    filter: { selectedTeamName },
+  } = useFilterContext();
+  return (
+    <div className="min-w-52">
+      <p className="text-lg font-semibold">
+        {t("analytics_for_organisation", {
+          organisationName: selectedTeamName,
+        })}
+      </p>
+      <p>{t("subtitle_analytics")}</p>
+    </div>
+  );
+};
 
 export default function InsightsPage() {
   const { t } = useLocale();
@@ -40,22 +54,6 @@ export default function InsightsPage() {
       description: t("spot_popular_event_types_description"),
     },
   ];
-
-  const [dateRange, setDateRange] = useState<FilterContextType["filter"]["dateRange"]>([
-    dayjs().subtract(1, "month"),
-    dayjs(),
-    "t",
-  ]);
-
-  const [selectedTimeView, setSelectedTimeView] =
-    useState<FilterContextType["filter"]["selectedTimeView"]>("week");
-  const [selectedUserId, setSelectedUserId] = useState<FilterContextType["filter"]["selectedUserId"]>(null);
-  const [selectedTeamId, setSelectedTeamId] = useState<FilterContextType["filter"]["selectedTeamId"]>(null);
-  const [selectedEventTypeId, setSelectedEventTypeId] =
-    useState<FilterContextType["filter"]["selectedEventTypeId"]>(null);
-  const [selectedFilter, setSelectedFilter] = useState<FilterContextType["filter"]["selectedFilter"]>(null);
-  const [selectedTeamName, setSelectedTeamName] =
-    useState<FilterContextType["filter"]["selectedTeamName"]>(null);
 
   return (
     <div>
@@ -80,32 +78,9 @@ export default function InsightsPage() {
           {!user ? (
             <></>
           ) : (
-            <FilterProvider
-              value={{
-                filter: {
-                  dateRange,
-                  selectedTimeView,
-                  selectedUserId,
-                  selectedTeamId,
-                  selectedTeamName,
-                  selectedEventTypeId,
-                  selectedFilter,
-                },
-                setSelectedFilter: (filter) => setSelectedFilter(filter),
-                setDateRange: (dateRange) => setDateRange(dateRange),
-                setSelectedTimeView: (selectedTimeView) => setSelectedTimeView(selectedTimeView),
-                setSelectedUserId: (selectedUserId) => setSelectedUserId(selectedUserId),
-                setSelectedTeamId: (selectedTeamId) => setSelectedTeamId(selectedTeamId),
-                setSelectedTeamName: (selectedTeamName) => setSelectedTeamName(selectedTeamName),
-                setSelectedEventTypeId: (selectedEventTypeId) => setSelectedEventTypeId(selectedEventTypeId),
-              }}>
+            <FiltersProvider>
               <div className="mb-4 ml-auto flex w-full flex-wrap justify-between lg:flex-nowrap">
-                <div className="min-w-52">
-                  <p className="text-lg font-semibold">
-                    {t("analytics_for_organisation", { organisationName: selectedTeamName })}
-                  </p>
-                  <p>{t("subtitle_analytics")}</p>
-                </div>
+                <Heading />
                 <Filters />
               </div>
               <div className="mb-4 space-y-6">
@@ -132,7 +107,7 @@ export default function InsightsPage() {
                   </a>
                 </small>
               </div>
-            </FilterProvider>
+            </FiltersProvider>
           )}
         </UpgradeTip>
       </Shell>
