a67478fe8691e28c0caf6c22af1a22d4ad25d6cb
Omar López <zomars@me.com>
2023-03-23 14:02:55 -0700

    Revert "Fixes formatted description in email (#7696)"
    This reverts commit f6d7568d0a89dd500823ebeae62df61d86cc2f8d.

--- packages/lib/markdownIt.ts
+++ packages/lib/markdownIt.ts
@@ -1,3 +1,12 @@
 import MarkdownIt from "markdown-it";
 
 export const md = new MarkdownIt("default", { html: true, breaks: true, linkify: true });
+
+export function addListFormatting(html: string) {
+  return html
+    .replaceAll("<ul>", "<ul style='list-style-type: disc; list-style-position: inside; margin-left: 12px'>")
+    .replaceAll(
+      "<ol>",
+      "<ol style='list-style-type: decimal; list-style-position: inside; margin-left: 12px'>"
+    );
+}
