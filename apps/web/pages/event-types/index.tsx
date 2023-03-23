f6d7568d0a89dd500823ebeae62df61d86cc2f8d
Carina Wollendorfer <30310907+CarinaWolli@users.noreply.github.com>
2023-03-23 13:00:42 +0100

    Fixes formatted description in email (#7696)
    * fix description in email
    
    * add styling for lists
    
    * sanitize editor input before saving
    
    * sanitize eventTypeDescription
    
    * santize html when used dangerouslySetInnerHTML
    
    * fix server side sanitation
    
    * add missing formatting and sanitation
    
    * move @ts-expect-error to correct line
    
    * fix type error and add yarn.lock
    
    * fix build error
    
    * sanitize description for booking page and availability page
    
    * rename to markdownAndSanitize
    
    * always add list formatting
    
    * handle empty description in markdownAndSanitize for cleaner code
    
    * create function for clientside markdown rendering and sanitizing
    
    * fix type error
    
    * code clean up
    
    * Now that eventType.descriptionAsSafeHTML is added at all the missing places, we can do away with ts-ignore and get type safety
    
    * Remove unused variable
    
    * Remove one more ts-expect-error
    
    ---------
    
    Co-authored-by: CarinaWolli <wollencarina@gmail.com>
    Co-authored-by: Hariom Balhara <hariombalhara@gmail.com>
    Co-authored-by: Peer Richelsen <peeroke@gmail.com>

--- apps/web/pages/event-types/index.tsx
+++ apps/web/pages/event-types/index.tsx
@@ -13,6 +13,7 @@ import { APP_NAME, CAL_URL, WEBAPP_URL } from "@calcom/lib/constants";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import useMediaQuery from "@calcom/lib/hooks/useMediaQuery";
 import { useTypedQuery } from "@calcom/lib/hooks/useTypedQuery";
+import { markdownAndSanitize } from "@calcom/lib/markdownAndSanitizeClientSide";
 import type { RouterOutputs } from "@calcom/trpc/react";
 import { trpc, TRPCClientError } from "@calcom/trpc/react";
 import {
@@ -141,7 +142,7 @@ const Item = ({ type, group, readOnly }: { type: EventType; group: EventTypeGrou
       </div>
       <EventTypeDescription
         // @ts-expect-error FIXME: We have a type mismatch here @hariombalhara @sean-brydon
-        eventType={type}
+        eventType={{ ...type, descriptionAsSafeHTML: markdownAndSanitize(type.description) }}
         shortenDescription
       />
     </Link>
