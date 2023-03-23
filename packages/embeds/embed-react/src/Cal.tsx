5863822284e9cf748a2dd1384d6139c5016436d8
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-03-23 22:33:10 +0100

    bump import plugin

--- packages/embeds/embed-react/src/Cal.tsx
+++ packages/embeds/embed-react/src/Cal.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useEffect, useRef } from "react";
 
 import useEmbed from "./useEmbed";
--- packages/embeds/embed-react/src/useEmbed.ts
+++ packages/embeds/embed-react/src/useEmbed.ts
@@ -1,4 +1,5 @@
 "use client";
+
 import { useEffect, useState } from "react";
 
 import EmbedSnippet from "@calcom/embed-snippet";
