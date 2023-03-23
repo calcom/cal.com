1c38e5dcd8b7ead8d75a70a3ef0fed3335b448e9
Alex van Andel <me@alexvanandel.com>
2023-03-23 02:17:14 +0100

    Revert "Checking package size effects when we remove zod from middleware"
    This reverts commit ecc2038411c73fc562d57712d5c83d66925e7e13.

--- packages/lib/getIP.ts
+++ packages/lib/getIP.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest } from "next";
+import z from "zod";
 
 export function parseIpFromHeaders(value: string | string[]) {
   return Array.isArray(value) ? value[0] : value.split(",")[0];
@@ -15,11 +16,13 @@ export default function getIP(request: Request | NextApiRequest) {
   return xff ? parseIpFromHeaders(xff) : "127.0.0.1";
 }
 
+const banlistSchema = z.array(z.string());
+
 export function isIpInBanlist(request: Request | NextApiRequest) {
   const IP = getIP(request);
   const rawBanListJson = process.env.IP_BANLIST || "[]";
-  const banList = JSON.parse(rawBanListJson);
-  if (Array.isArray(banList) && banList.includes(IP)) {
+  const banList = banlistSchema.parse(JSON.parse(rawBanListJson));
+  if (banList.includes(IP)) {
     console.log(`Found banned IP: ${IP} in IP_BANLIST`);
     return true;
   }
