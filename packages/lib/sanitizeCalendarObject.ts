9938e6be6196c2849274fb02466cbda5e146a340
alishaz-polymath <alishahbaz7@gmail.com>
2023-03-24 23:59:42 +0530

    improves sanitizer function

--- packages/lib/sanitizeCalendarObject.ts
+++ packages/lib/sanitizeCalendarObject.ts
@@ -2,11 +2,14 @@ import type { DAVObject } from "tsdav";
 
 export const sanitizeCalendarObject = (obj: DAVObject) => {
   return obj.data
-    .replaceAll("\r\n", "\r")
-    .replaceAll("\r", "\r\n")
-    .replaceAll(/(: \r\n|:\r\n|\r\n:|\r\n :)/gm, ":")
-    .replaceAll(/(; \r\n|;\r\n|\r\n;|\r\n ;)/gm, ";")
-    .replaceAll(/(= \r\n|=\r\n|\r\n=|\r\n =)/gm, "=");
+    .replaceAll("\r\n", "\n")
+    .replaceAll("\r", "\n")
+    .replaceAll("\n ", "")
+    .replaceAll("\n\t", "")
+    .replaceAll("\n", "\r\n")
+    .replaceAll(/(:[ \t]*\r\n[ \t]*:)/gm, ":")
+    .replaceAll(/(;[ \t]*\r\n[ \t]*;)/gm, ";")
+    .replaceAll(/(=[ \t]*\r\n[ \t]*=)/gm, "=");
 };
 
 export default sanitizeCalendarObject;
