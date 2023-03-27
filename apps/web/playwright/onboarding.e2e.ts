f8dcdecfd514d50460a57748ec688ff753c18401
CarinaWolli <wollencarina@gmail.com>
2023-03-27 09:53:17 +0200

    fixes e2e test

--- apps/web/playwright/onboarding.e2e.ts
+++ apps/web/playwright/onboarding.e2e.ts
@@ -26,7 +26,13 @@ test.describe("Onboarding", () => {
         await page.locator("input[name=username]").fill("new user onboarding");
         await page.locator("input[name=name]").fill("new user 2");
         await page.locator("input[role=combobox]").click();
-        await page.locator("text=Europe/London GMT +0:00").click();
+        await page
+          .locator("*")
+          .filter((el: { textContent: string }) => {
+            return el.textContent.startsWith("Europe/London");
+          })
+          .first()
+          .click();
 
         await page.locator("button[type=submit]").click();
 
