16aa0dda71e09b6c6539a6a7b150b36d9b0265ce
zomars <zomars@me.com>
2023-03-27 08:43:31 -0700

    Rewrites only dynamic user pages

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,7 @@ require("dotenv").config({ path: "../../.env" });
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const { withSentryConfig } = require("@sentry/nextjs");
 const os = require("os");
+const glob = require("glob");
 
 const { withAxiom } = require("next-axiom");
 const { i18n } = require("./next-i18next.config");
@@ -67,6 +68,18 @@ if (process.env.ANALYZE === "true") {
 }
 
 plugins.push(withAxiom);
+
+/** Needed to rewrite public booking page, gets all static pages but [user] */
+const pages = glob
+  .sync("pages/**/[^_]*.{tsx,js,ts}", { cwd: __dirname })
+  .map((filename) =>
+    filename
+      .substr(6)
+      .replace(/(\.tsx|\.js|\.ts)/, "")
+      .replace(/\/.*/, "")
+  )
+  .filter((v, i, self) => self.indexOf(v) === i && !v.startsWith("[user]"));
+
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   i18n,
@@ -186,7 +199,7 @@ const nextConfig = {
         destination: process.env.NEXT_PUBLIC_EMBED_LIB_URL?,
       }, */
       {
-        source: "/:user/:type",
+        source: `/:user((?!${pages.join("|")}))/:type`,
         destination: "/:user/new-booker/:type",
         has: [{ type: "cookie", key: "new-booker-enabled" }],
       },
