bba83f8d469310561086db715a55abc0df8bf379
Joe Au-Yeung <j.auyeung419@gmail.com>
2023-03-29 20:36:10 -0400

    Set payment option on event type

--- packages/app-store/stripepayment/components/EventTypeAppCardInterface.tsx
+++ packages/app-store/stripepayment/components/EventTypeAppCardInterface.tsx
@@ -7,15 +7,20 @@ import AppCard from "@calcom/app-store/_components/AppCard";
 import type { EventTypeAppCardComponent } from "@calcom/app-store/types";
 import { WEBAPP_URL } from "@calcom/lib/constants";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
+import { Select } from "@calcom/ui";
 import { Alert, TextField } from "@calcom/ui";
 
+import { paymentOptions } from "../lib/constants";
 import type { appDataSchema } from "../zod";
+
+type Option = { value: string; label: string };
 
 const EventTypeAppCard: EventTypeAppCardComponent = function EventTypeAppCard({ app, eventType }) {
   const { asPath } = useRouter();
   const [getAppData, setAppData] = useAppContextWithSchema<typeof appDataSchema>();
   const price = getAppData("price");
   const currency = getAppData("currency");
+  const paymentOption = getAppData("paymentOption");
   const [requirePayment, setRequirePayment] = useState(getAppData("enabled"));
   const { t } = useLocale();
   const recurringEventDefined = eventType.recurringEvent?.count !== undefined;
@@ -68,6 +73,17 @@ const EventTypeAppCard: EventTypeAppCardComponent = function EventTypeAppCard({ 
                   setAppData("price", Number(e.target.value) * 100);
                 }}
                 value={price > 0 ? price / 100 : undefined}
+              />
+              <Select<Option>
+                defaultValue={
+                  paymentOptions.find((option) => paymentOption === option.value) || paymentOptions[0]
+                }
+                options={paymentOptions.map((option) => {
+                  return { ...option, label: t(option.label) || option.label };
+                })}
+                onChange={(input) => {
+                  if (input) setAppData("paymentOption", input.value);
+                }}
               />
             </div>
           )
