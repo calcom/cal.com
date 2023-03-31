fe7fb44431c1168a5a47e8f90d1075fb6baefa53
Efraín Rochín <roae.85@gmail.com>
2023-03-31 14:11:41 -0700

    add prisma to the app dir endpoint

--- apps/web/app/app-dir-cold-start/route.ts
+++ apps/web/app/app-dir-cold-start/route.ts
@@ -1,11 +1,16 @@
 import cache from "memory-cache";
 import { NextResponse } from "next/server";
 
+import prisma from "@calcom/prisma";
+
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const cacheKey = "experiment";
   const memory = cache.get(cacheKey);
   if (!memory) {
     cache.put(cacheKey, new Date());
   }
-  return NextResponse.json({ coldStart: !Boolean(memory) });
+  const totalUsers = await prisma.user.count();
+  return NextResponse.json({ coldStart: !Boolean(memory), totalUsers, date: new Date().toISOString() });
 }
