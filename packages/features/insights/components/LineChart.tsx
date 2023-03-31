65a7e8aacced4e7919825e2cb9b004f03bdce580
Sean Brydon <sean@cal.com>
2023-03-31 17:58:49 +0100

    Fix types

--- packages/features/insights/components/LineChart.tsx
+++ packages/features/insights/components/LineChart.tsx
@@ -1,5 +1,6 @@
-import type { LineChartProps } from "@tremor/react";
 import { LineChart as ExternalLineChart } from "@tremor/react";
+
+import type { LineChartProps } from "./tremor.types";
 
 // Honestly this is a mess. Why are all chart libraries in existance horrible to theme
 export const LineChart = (props: LineChartProps) => {
