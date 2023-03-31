70a34f73c773e29ad40cd21d8a41740e832182e4
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-03-31 14:43:50 +0200

    add typeguards to make sure all endpoints are covered in the approuter

--- packages/trpc/server/routers/_app.ts
+++ packages/trpc/server/routers/_app.ts
@@ -5,15 +5,21 @@ import { router } from "../trpc";
 import { viewerRouter } from "./viewer";
 import { slotsRouter } from "./viewer/slots";
 
+import type { Endpoint } from "../../react";
+
 /**
  * Create your application's root router
  * If you want to use SSG, you need export this
  * @link https://trpc.io/docs/ssg
  * @link https://trpc.io/docs/router
+ * 
+ * Make sure to "join" all the endpoint routers together
+ * so that the correct type can be inferred on the client.
+ * The `satisfies` will help make sure all endpoints are included.
  */
 export const appRouter = router({
   slots: slotsRouter,
   viewer: viewerRouter,
-});
+} satisfies Record<Endpoint, any>);
 
 export type AppRouter = typeof appRouter;
