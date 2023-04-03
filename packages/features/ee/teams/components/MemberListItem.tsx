cf4bd8a77d910c460aecfffcaa5a24625d4011b7
Peer Richelsen <peer@cal.com>
2023-04-03 14:08:17 +0200

    yarn format

--- packages/features/ee/teams/components/MemberListItem.tsx
+++ packages/features/ee/teams/components/MemberListItem.tsx
@@ -121,11 +121,12 @@ export default function MemberListItem(props: Props) {
                   data-email={props.member.email}>
                   {props.member.email}
                 </span>
-                {bookingLink && <>
-                  <span className="mx-2 block text-gray-600">•</span>
-                  <span className="block text-sm text-gray-600">{bookingLink}</span>
-                </>
-                }
+                {bookingLink && (
+                  <>
+                    <span className="mx-2 block text-gray-600">•</span>
+                    <span className="block text-sm text-gray-600">{bookingLink}</span>
+                  </>
+                )}
               </div>
             </div>
           </div>
