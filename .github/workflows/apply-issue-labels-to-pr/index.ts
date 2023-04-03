4597f075879cdd70ae111bf7e001d58b1bac1df9
tedspare <ted.spare@gmail.com>
2023-04-03 11:56:18 -0400

    Add workflow to sync issue labels to PR

---
+++ .github/workflows/apply-issue-labels-to-pr/index.ts
@@ -0,0 +1,82 @@
+const { Octokit } = require("@octokit/core");
+const { graphql: graphqlApi } = require("@octokit/graphql");
+
+async function applyLabelFromLinkedIssueToPR(pr, token) {
+  const graphql = graphqlApi.defaults({
+    headers: {
+      authorization: `token ${token}`,
+    },
+  });
+
+  const octokit = new Octokit({ auth: token });
+
+  const query = `
+    query GetLinkedIssues($owner: String!, $repo: String!, $prNumber: Int!) {
+      repository(owner: $owner, name: $repo) {
+        pullRequest(number: $prNumber) {
+          closingIssuesReferences(first: 10) {
+            nodes {
+              number
+              labels(first: 10) {
+                nodes {
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const data = await graphql(query, {
+    owner: pr.base.repo.owner.login,
+    repo: pr.base.repo.name,
+    prNumber: pr.number,
+  });
+
+  const linkedIssues =
+    data.repository.pullRequest.closingIssuesReferences.nodes;
+
+  if (linkedIssues.length === 0) {
+    console.log("No issue linked.");
+    return;
+  }
+
+  for (const issue of linkedIssues) {
+    const labels = issue.labels.nodes.map((label) => label.name);
+
+    if (labels.length === 0) {
+      console.log(`No labels found on the linked issue #${issue.number}.`);
+      continue;
+    }
+
+    await octokit.request(
+      "POST /repos/{owner}/{repo}/issues/{issue_number}/labels",
+      {
+        owner: pr.base.repo.owner.login,
+        repo: pr.base.repo.name,
+        issue_number: pr.number,
+        labels: labels,
+      }
+    );
+
+    console.log(
+      `Applied labels: ${labels.join(", ")} to PR#${
+        pr.number
+      } from linked issue #${issue.number}`
+    );
+  }
+}
+
+(async () => {
+  if (!process.env.PR_DATA) {
+    console.log("No PR data found.");
+    return;
+  }
+
+  const token = process.env.GITHUB_TOKEN;
+  const prData = JSON.parse(process.env.PR_DATA);
+
+  await applyLabelFromLinkedIssueToPR(prData, token);
+})();
