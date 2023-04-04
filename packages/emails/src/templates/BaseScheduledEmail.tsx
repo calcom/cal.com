5963a6dbeae1dc63844f6764632c3b95b1a5904e
Joe Au-Yeung <j.auyeung419@gmail.com>
2023-04-04 15:18:59 -0400

    Type fix

--- packages/emails/src/templates/BaseScheduledEmail.tsx
+++ packages/emails/src/templates/BaseScheduledEmail.tsx
@@ -80,7 +80,7 @@ export const BaseScheduledEmail = (
       <Info label={t("additional_notes")} description={props.calEvent.additionalNotes} withSpacer />
       {props.includeAppsStatus && <AppsStatus calEvent={props.calEvent} t={t} />}
       <UserFieldsResponses calEvent={props.calEvent} />
-      {props.calEvent?.paymentInfo && (
+      {props.calEvent.paymentInfo?.amount && (
         <Info
           label={props.calEvent.paymentInfo?.paymentOption === "HOLD" ? t("no_show_fee") : t("price")}
           description={new Intl.NumberFormat(props.attendee.language.locale, {
