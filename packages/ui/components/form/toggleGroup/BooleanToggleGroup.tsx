5f6f6c2e225e66da6478bfe2cf983b6862ed8d97
Sean Brydon <sean@cal.com>
2023-04-04 20:41:22 +0100

    Fix ui lint

--- packages/ui/components/form/toggleGroup/BooleanToggleGroup.tsx
+++ packages/ui/components/form/toggleGroup/BooleanToggleGroup.tsx
@@ -41,7 +41,7 @@ export const BooleanToggleGroup = function BooleanToggleGroup({
       value={yesNoValue}
       type="single"
       disabled={disabled}
-      className="flex space-x-2 rounded-md border border-subtle p-1 rtl:space-x-reverse"
+      className="border-subtle flex space-x-2 rounded-md border p-1 rtl:space-x-reverse"
       onValueChange={(yesNoValue: "yes" | "no") => {
         setYesNoValue(yesNoValue);
         onValueChange(boolean(yesNoValue));
