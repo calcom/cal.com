0c6e4261d5cc84bb38c620d7358677944b6932e8
Peer Richelsen <peer@cal.com>
2023-04-04 22:49:58 +0200

    fixed app store slider

--- packages/ui/components/apps/AllApps.tsx
+++ packages/ui/components/apps/AllApps.tsx
@@ -78,10 +78,10 @@ function CategoryTab({ selectedCategory, categories, searchText }: CategoryTabPr
       </h2>
       {leftVisible && (
         <button onClick={handleLeft} className="absolute bottom-0 flex md:left-1/2 md:-top-1">
-          <div className="bg-default flex h-10 w-5 items-center justify-end">
+          <div className="bg-default flex h-12 w-5 items-center justify-end">
             <FiChevronLeft className="text-subtle h-4 w-4" />
           </div>
-          <div className="flex h-10 w-5 bg-gradient-to-l from-transparent to-white" />
+          <div className="to-default flex h-12 w-5 bg-gradient-to-l from-transparent" />
         </button>
       )}
       <ul
@@ -93,8 +93,8 @@ function CategoryTab({ selectedCategory, categories, searchText }: CategoryTabPr
             router.replace(router.asPath.split("?")[0], undefined, { shallow: true });
           }}
           className={classNames(
-            selectedCategory === null ? "bg-inverted text-inverted" : "bg-muted text-emphasis",
-            "hover:bg-inverted rounded-md px-4 py-2.5 text-sm font-medium hover:cursor-pointer hover:text-gray-50"
+            selectedCategory === null ? "bg-emphasis text-default" : "bg-muted text-emphasis",
+            "hover:bg-emphasis rounded-md px-4 py-2.5 text-sm font-medium hover:cursor-pointer hover:text-gray-50"
           )}>
           {t("all_apps")}
         </li>
@@ -111,8 +111,8 @@ function CategoryTab({ selectedCategory, categories, searchText }: CategoryTabPr
               }
             }}
             className={classNames(
-              selectedCategory === cat ? "bg-inverted text-inverted" : "bg-muted text-emphasis",
-              "hover:bg-inverted rounded-md px-4 py-2.5 text-sm font-medium hover:cursor-pointer hover:text-gray-50"
+              selectedCategory === cat ? "bg-emphasis text-default" : "bg-muted text-emphasis",
+              "hover:bg-emphasis rounded-md px-4 py-2.5 text-sm font-medium hover:cursor-pointer hover:text-gray-50"
             )}>
             {cat[0].toUpperCase() + cat.slice(1)}
           </li>
@@ -120,8 +120,8 @@ function CategoryTab({ selectedCategory, categories, searchText }: CategoryTabPr
       </ul>
       {rightVisible && (
         <button onClick={handleRight} className="absolute bottom-0 right-0 flex md:-top-1">
-          <div className="to-default flex h-10 w-5 bg-gradient-to-r from-transparent" />
-          <div className="bg-default flex h-10 w-5 items-center justify-end">
+          <div className="to-default flex h-12 w-5 bg-gradient-to-r from-transparent" />
+          <div className="bg-default flex h-12 w-5 items-center justify-end">
             <FiChevronRight className="text-subtle h-4 w-4" />
           </div>
         </button>
