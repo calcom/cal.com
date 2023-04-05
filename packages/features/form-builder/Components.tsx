e1c64ffb04b2cb37fb24de1eb7c104e51ed87475
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2023-04-05 22:59:05 +0530

    fix: use optional chaing
    Signed-off-by: Udit Takkar <udit.07814802719@cse.mait.ac.in>

--- packages/features/form-builder/Components.tsx
+++ packages/features/form-builder/Components.tsx
@@ -329,7 +329,7 @@ export const Components: Record<BookingFieldType, Component> = {
                 <>
                   <Label>
                     {options[0].label}
-                    {!readOnly && optionsInputs[options[0].value].required ? (
+                    {!readOnly && optionsInputs[options[0].value]?.required ? (
                       <span className="ml-1 mb-1 text-sm font-medium leading-none dark:text-white">*</span>
                     ) : null}
                   </Label>
