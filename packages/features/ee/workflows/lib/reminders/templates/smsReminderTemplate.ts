239236d65c3f231e15a4e1151c8cad341791da55
CarinaWolli <wollencarina@gmail.com>
2023-04-05 13:05:20 +0200

    fix sms reminder template

--- packages/features/ee/workflows/lib/reminders/templates/smsReminderTemplate.ts
+++ packages/features/ee/workflows/lib/reminders/templates/smsReminderTemplate.ts
@@ -15,19 +15,19 @@ const smsReminderTemplate = (
   if (isEditingMode) {
     eventName = "{EVENT_NAME}";
     timeZone = "{TIMEZONE}";
-    startTime = "{EVENT_DATE_h mmA}";
+    startTime = "{START_TIME_h mmA}";
 
-    startTime = "{EVENT_TIME_MMM D h mmA}";
+    eventDate = "{EVENT_DATE_YYYY MMM D}";
     attendee = action === WorkflowActions.SMS_ATTENDEE ? "{ORGANIZER}" : "{ATTENDEE}";
     name = action === WorkflowActions.SMS_ATTENDEE ? "{ATTENDEE}" : "{ORGANIZER}";
   } else {
-    eventDate = dayjs(startTime).tz(timeZone).format("MMM D");
+    eventDate = dayjs(startTime).tz(timeZone).format("YYYY MMM D");
     startTime = dayjs(startTime).tz(timeZone).format("h:mmA");
   }
 
   const templateOne = `Hi${
     name ? ` ${name}` : ``
-  }, this is a reminder that your meeting (${eventName}) with ${attendee} is on ${eventDate} at ${startTime} ${timeZone}`;
+  }, this is a reminder that your meeting (${eventName}) with ${attendee} is on ${eventDate} at ${startTime} ${timeZone}.`;
 
   //Twilio recomments message to be no longer than 320 characters
   if (templateOne.length <= 320) return templateOne;
