353a209c6893aff2f70e7837a8948213155f9c15
Hariom Balhara <hariombalhara@gmail.com>
2023-04-06 11:08:36 +0530

    Fix tests

--- packages/features/form-builder/FormBuilder.tsx
+++ packages/features/form-builder/FormBuilder.tsx
@@ -550,7 +550,7 @@ const WithLabel = ({
       {field.type !== "boolean" && field.type !== "multiemail" && field.label && (
         <div className="mb-2 flex items-center">
           <Label className="!mb-0">
-            {field.label}
+            <span>{field.label}</span>
             <span className="ml-1 -mb-1 text-sm font-medium leading-none dark:text-white">
               {!readOnly && field.required ? "*" : ""}
             </span>
