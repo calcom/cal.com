76ebf0b95641916a170e4a9813eb7457d4f7a65f
Kiran K <mailtokirankk@gmail.com>
2023-04-11 17:12:54 +0530

    fix "window undefined" issue

--- apps/web/pages/auth/sso/direct.tsx
+++ apps/web/pages/auth/sso/direct.tsx
@@ -1,10 +1,12 @@
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 import { samlProductID, samlTenantID } from "@calcom/features/ee/sso/lib/saml";
 import { HOSTED_CAL_FEATURES } from "@calcom/lib/constants";
 
 import type { inferSSRProps } from "@lib/types/inferSSRProps";
+
 
 // This page is used to initiate the SAML authentication flow by redirecting to the SAML provider.
 // Accessible only on self-hosted Cal.com instances.
@@ -16,14 +18,16 @@ export default function Page({ samlTenantID, samlProductID }: inferSSRProps<type
     return;
   }
 
-  // Initiate SAML authentication flow
-  signIn(
-    "saml",
-    {
-      callbackUrl: "/",
-    },
-    { tenant: samlTenantID, product: samlProductID }
-  );
+  useEffect(() => {
+    // Initiate SAML authentication flow
+    signIn(
+      "saml",
+      {
+        callbackUrl: "/",
+      },
+      { tenant: samlTenantID, product: samlProductID }
+    );
+  }, []);
 
   return null;
 }
