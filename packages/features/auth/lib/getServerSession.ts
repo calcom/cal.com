5053689996f37c3315f0a420b573134607bfde1f
Efraín Rochín <roae.85@gmail.com>
2023-04-11 13:19:44 -0700

    returns getServerSession to its initial state

--- packages/features/auth/lib/getServerSession.ts
+++ packages/features/auth/lib/getServerSession.ts
@@ -1,16 +1,15 @@
 import type { GetServerSidePropsContext, NextApiRequest, NextApiResponse } from "next";
 import type { AuthOptions, Session } from "next-auth";
+import { getServerSession as getServerSessionInner } from "next-auth/next";
 
-//import { AUTH_OPTIONS } from "./next-auth-options";
+import { AUTH_OPTIONS } from "./next-auth-options";
 
 export async function getServerSession(options: {
   req: NextApiRequest | GetServerSidePropsContext["req"];
   res: NextApiResponse | GetServerSidePropsContext["res"];
   authOptions?: AuthOptions;
 }) {
-  const { req, res, authOptions = {} } = options;
-  // console.log("getServerSession", req.url);
-  const { getServerSession: getServerSessionInner } = await import("next-auth/next");
+  const { req, res, authOptions = AUTH_OPTIONS } = options;
 
   const session = await getServerSessionInner(req, res, authOptions);
 
