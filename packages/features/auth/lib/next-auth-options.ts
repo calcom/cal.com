49e52a67c4b51521a916eb9cd6a6c59ee602e5c9
zomars <zomars@me.com>
2023-04-11 18:31:49 -0700

    Test

--- packages/features/auth/lib/next-auth-options.ts
+++ packages/features/auth/lib/next-auth-options.ts
@@ -264,7 +264,7 @@ if (isSAMLLoginEnabled) {
         if (!code) {
           return null;
         }
-        const jackson = (await import("@calcom/features/ee/sso/lib/jackson")).default;
+        const { default: jackson } = await import("@calcom/features/ee/sso/lib/jackson");
         const { oauthController } = await jackson();
 
         // Fetch access token
