Aman-design/OneCal

feat: add isRecordingExist field and create api handler
Signed-off-by: Udit Takkar <udit.07814802719@cse.mait.ac.in>

apps/web/components/booking/BookingListItem.tsx
@@ -220,8 +220,7 @@ function BookingListItem(booking: BookingItemProps) {
       },
     });
   };
-  const showRecordingsButtons =
-    (booking.location === "integrations:daily" || booking?.location?.trim() === "") && isPast && isConfirmed;
+  const showRecordingsButtons = booking?.isRecordingExist && isPast && isConfirmed;
   return (
     <>
       <RescheduleDialog

calcom/cal.com

apps/web/components/booking/BookingListItem.tsx
@@ -256,10 +256,9 @@ function BookingListItem(booking: BookingItemProps) {
       },
     });
   };
-  const showRecordingsButtons =
-    (booking.location === "integrations:daily" || booking?.location?.trim() === "") && isPast && isConfirmed;
 
   const title = booking.title;
+  const showRecordingsButtons = booking?.isRecording && isPast && isConfirmed;
   return (
     <>
       <RescheduleDialog