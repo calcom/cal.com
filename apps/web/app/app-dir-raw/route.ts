711d391fca8374e477a636a5db8c964f0ae30141
Efraín Rochín <roae.85@gmail.com>
2023-04-13 12:33:18 -0700

    endpoint: count users without prisma

---
+++ apps/web/app/app-dir-raw/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+let cold = true;
+
+export async function GET() {
+  const coldStart = cold;
+  cold = false;
+  const startImport = performance.now();
+  const postgres = (await import("postgres")).default;
+  const endImport = performance.now();
+  const startConnection = performance.now();
+  const sql = postgres(process.env.DATABASE_URL || "");
+  const endConnection = performance.now();
+  const startCount = performance.now();
+  const [result] = await sql<Array<{ totalUSers?: number }>>`SELECT COUNT(id) as totalUsers FROM users`;
+  const endCount = performance.now();
+
+  return NextResponse.json({
+    coldStart,
+    ...result,
+    prismaImportTime: endImport - startImport,
+    prismaConnectionTime: endConnection - startConnection,
+    prismaQueryTime: endCount - startCount,
+    date: new Date().toISOString(),
+  });
+}
