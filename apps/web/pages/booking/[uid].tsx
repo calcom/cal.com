3a317a25dd494344a7299bd1c6c0902f7ab7e209
Peer Richelsen <peer@cal.com>
2023-04-14 15:45:37 +0200

    removed page_view tracker

--- apps/web/pages/booking/[uid].tsx
+++ apps/web/pages/booking/[uid].tsx
@@ -41,7 +41,7 @@ import { useLocale } from "@calcom/lib/hooks/useLocale";
 import useTheme from "@calcom/lib/hooks/useTheme";
 import { getEveryFreqFor } from "@calcom/lib/recurringStrings";
 import { maybeGetBookingUidFromSeat } from "@calcom/lib/server/maybeGetBookingUidFromSeat";
-import { collectPageParameters, telemetryEventTypes, useTelemetry } from "@calcom/lib/telemetry";
+import { useTelemetry } from "@calcom/lib/telemetry";
 import { getIs24hClockFromLocalStorage, isBrowserLocale24h } from "@calcom/lib/timeFormat";
 import { localStorage } from "@calcom/lib/webstorage";
 import prisma from "@calcom/prisma";
@@ -187,12 +187,12 @@ export default function Success(props: SuccessProps) {
       !bookingInfo.seatsReferences.some((reference) => reference.referenceUid === seatReferenceUid));
 
   const telemetry = useTelemetry();
-  useEffect(() => {
+  /*  useEffect(() => {
     if (top !== window) {
       //page_view will be collected automatically by _middleware.ts
       telemetry.event(telemetryEventTypes.embedView, collectPageParameters("/booking"));
     }
-  }, [telemetry]);
+  }, [telemetry]); */
 
   useEffect(() => {
     const users = eventType.users;
