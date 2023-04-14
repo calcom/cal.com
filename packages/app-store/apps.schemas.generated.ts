b24bfbc08fcd7794f94708fa1fd420ff2cf43d44
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2023-04-14 16:44:54 +0530

    fix: undo showRecordingsButton
    Signed-off-by: Udit Takkar <udit.07814802719@cse.mait.ac.in>

--- packages/app-store/apps.schemas.generated.ts
+++ packages/app-store/apps.schemas.generated.ts
@@ -25,6 +25,7 @@ import { appDataSchema as vital_zod_ts } from "./vital/zod";
 import { appDataSchema as wordpress_zod_ts } from "./wordpress/zod";
 import { appDataSchema as zapier_zod_ts } from "./zapier/zod";
 import { appDataSchema as zoomvideo_zod_ts } from "./zoomvideo/zod";
+
 export const appDataSchemas = {
   dailyvideo: dailyvideo_zod_ts,
   fathom: fathom_zod_ts,
