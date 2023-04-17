3ad8fb94d18cba69a4ff4154efc837dd6e51f3af
zomars <zomars@me.com>
2023-04-17 16:26:54 -0700

    Update disable.ts

--- apps/web/pages/api/auth/two-factor/totp/disable.ts
+++ apps/web/pages/api/auth/two-factor/totp/disable.ts
@@ -12,7 +12,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(405).json({ message: "Method not allowed" });
   }
 
-  const session = await getServerSession({ req });
+  const session = await getServerSession({ req, res });
   if (!session) {
     return res.status(401).json({ message: "Not authenticated" });
   }
