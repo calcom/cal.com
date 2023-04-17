18812f5c367b090923909db2aa2f066eb1a89713
zomars <zomars@me.com>
2023-04-17 15:53:17 -0700

    Removed unused interface

--- packages/trpc/server/routers/viewer/apps.tsx
+++ packages/trpc/server/routers/viewer/apps.tsx
@@ -12,19 +12,6 @@ import { TRPCError } from "@trpc/server";
 
 import { authedAdminProcedure, authedProcedure, router } from "../../trpc";
 
-interface FilteredApp {
-  name: string;
-  slug: string;
-  logo: string;
-  title?: string;
-  type: string;
-  description: string;
-  dirName: string;
-  keys: Prisma.JsonObject | null;
-  enabled: boolean;
-  isTemplate?: boolean;
-}
-
 export const appsRouter = router({
   listLocal: authedAdminProcedure
     .input(
