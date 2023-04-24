c4c9e7d67391404a25e1825f0997d015444075d3
Jeroen Reumkens <hello@jeroenreumkens.nl>
2023-04-24 14:35:18 +0200

    #CAL-1578 Fixed selected timezone alignment

--- packages/features/bookings/Booker/components/EventMeta.tsx
+++ packages/features/bookings/Booker/components/EventMeta.tsx
@@ -65,13 +65,13 @@ export const EventMeta = () => {
             )}
             <EventDetails event={event} />
             <EventMetaBlock
-              className="cursor-pointer [&_.current-timezone:before]:focus-within:opacity-100 [&_.current-timezone:before]:hover:opacity-100 [&_>svg]:mt-[4px]"
+              className="cursor-pointer [&_.current-timezone:before]:focus-within:opacity-100 [&_.current-timezone:before]:hover:opacity-100"
               contentClassName="relative"
               icon={Globe}>
               {bookerState === "booking" ? (
                 <>{timezone}</>
               ) : (
-                <span className="current-timezone before:bg-subtle flex items-center justify-center before:absolute before:inset-0 before:left-[-30px] before:top-[-3px] before:bottom-[-3px] before:w-[calc(100%_+_35px)] before:rounded-md before:py-3 before:opacity-0 before:transition-opacity">
+                <span className="current-timezone before:bg-subtle -mt-[2px] flex items-center justify-center before:absolute before:inset-0 before:left-[-30px] before:top-[-3px] before:bottom-[-3px] before:w-[calc(100%_+_35px)] before:rounded-md before:py-3 before:opacity-0 before:transition-opacity">
                   <TimezoneSelect
                     menuPosition="fixed"
                     classNames={{
