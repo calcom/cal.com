e71a7a5169e2bff84a4b51729a9c63fd16397fdc
CarinaWolli <wollencarina@gmail.com>
2023-04-25 14:11:25 +0200

    code clean up

--- packages/lib/slots.ts
+++ packages/lib/slots.ts
@@ -229,15 +229,9 @@ const getSlots = ({
   //handle full day overrides separately
   const fullDayOverrides = dateOverrides.filter((override) => {
     if (dayjs(override.start).isSame(dayjs(override.end))) {
-    }
-
-    const inviteeUtcOffset = dayjs(override.start.toString()).tz(timeZone).utcOffset();
-    const scheduleUtcOffset = dayjs(override.start.toString()).tz(override.timeZone).utcOffset();
-
-    const offset = inviteeUtcOffset - scheduleUtcOffset;
-
-    if (dayjs(override.start).day() === inviteeDate.day()) {
-      return true;
+      if (dayjs(override.start).day() === inviteeDate.day()) {
+        return true;
+      }
     }
   });
 
