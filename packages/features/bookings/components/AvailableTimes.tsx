30e03fbb89d41eeb4e3c8962896de756cc8ce095
Jeroen Reumkens <hello@jeroenreumkens.nl>
2023-04-25 16:02:59 +0200

    Update packages/features/bookings/components/AvailableTimes.tsx
    Co-authored-by: Peer Richelsen <peeroke@gmail.com>

--- packages/features/bookings/components/AvailableTimes.tsx
+++ packages/features/bookings/components/AvailableTimes.tsx
@@ -78,7 +78,7 @@ export const AvailableTimes = ({
               data-testid="time"
               data-time={slot.time}
               onClick={() => onTimeSelect(slot.time)}
-              className="mb-2 flex h-auto min-h-[36px] w-full flex-col justify-center py-2"
+              className="mb-2 flex h-auto min-h-9 w-full flex-col justify-center py-2"
               color="secondary">
               {dayjs.utc(slot.time).tz(timezone).format(timeFormat)}
               {bookingFull && <p className="text-sm">{t("booking_full")}</p>}
