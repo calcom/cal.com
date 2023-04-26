f8eb9553b99a5f6d7da417c57d4b64f6f48df766
Aditya Raj <rajaditya27145@gmail.com>
2023-04-27 03:41:38 +0530

    fix form inputs height to 36px

--- packages/config/tailwind-preset.js
+++ packages/config/tailwind-preset.js
@@ -111,6 +111,9 @@ module.exports = {
         sans: ["var(--font-inter)", ...fontFamily.sans],
         mono: ["Roboto Mono", "monospace"],
       },
+      height: {
+        9: "2.25rem",
+      },
       maxHeight: (theme) => ({
         0: "0",
         97: "25rem",
@@ -120,6 +123,7 @@ module.exports = {
       }),
       minHeight: (theme) => ({
         0: "0",
+        8.5: "2.125rem",
         ...theme("spacing"),
         full: "100%",
         screen: "100vh",
--- packages/ui/components/form/select/Select.tsx
+++ packages/ui/components/form/select/Select.tsx
@@ -51,7 +51,7 @@ export const Select = <
         dropdownIndicator: () => "text-default",
         control: (state) =>
           cx(
-            "bg-default border-default !min-h-9 text-sm leading-4 placeholder:text-sm placeholder:font-normal focus-within:ring-2 focus-within:ring-emphasis hover:border-emphasis rounded-md border ",
+            "bg-default border-default !min-h-9 h-9 text-sm leading-4 placeholder:text-sm placeholder:font-normal focus-within:ring-2 focus-within:ring-emphasis hover:border-emphasis rounded-md border ",
             state.isMulti
               ? variant === "checkbox"
                 ? "px-3 py-2"
@@ -59,7 +59,7 @@ export const Select = <
                 ? "p-1"
                 : "px-3 py-2"
               : "py-2 px-3",
-            props.isDisabled && "bg-gray-100",
+            props.isDisabled && "bg-muted",
             props.classNames?.control
           ),
         singleValue: () => cx("text-emphasis placeholder:text-muted", props.classNames?.singleValue),
