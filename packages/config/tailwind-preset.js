1427920f5016f4b53688108930ead71a9e496615
Aditya Raj <rajaditya27145@gmail.com>
2023-04-27 20:18:26 +0530

    replace minHeight class with height in input addOn

--- packages/config/tailwind-preset.js
+++ packages/config/tailwind-preset.js
@@ -120,7 +120,6 @@ module.exports = {
       }),
       minHeight: (theme) => ({
         0: "0",
-        8.5: "2.125rem",
         ...theme("spacing"),
         full: "100%",
         screen: "100vh",
