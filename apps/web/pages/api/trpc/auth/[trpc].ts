d1feec86265be0f868242bf04ae055c58d26ea47
Keith Williams <keithwillcode@gmail.com>
2023-04-28 18:01:01 -0300

    Created a reusable function for creating the API handler

--- apps/web/pages/api/trpc/auth/[trpc].ts
+++ apps/web/pages/api/trpc/auth/[trpc].ts
@@ -1,38 +1,4 @@
-/**
- * This file contains tRPC's HTTP response handler
- */
-import * as trpcNext from "@calcom/trpc/server/adapters/next";
-import { createContext as createTrpcContext } from "@calcom/trpc/server/createContext";
-import { createResponseMetaData } from "@calcom/trpc/server/createResponseMetaData";
+import { createNextApiHandler } from "@calcom/trpc/server/createNextApiHandler";
 import { authRouter } from "@calcom/trpc/server/routers/viewer/auth/_router";
 
-export default trpcNext.createNextApiHandler({
-  router: authRouter,
-  /**
-   * @link https://trpc.io/docs/context
-   */
-  createContext: ({ req, res }) => {
-    return createTrpcContext({ req, res });
-  },
-  /**
-   * @link https://trpc.io/docs/error-handling
-   */
-  onError({ error }) {
-    if (error.code === "INTERNAL_SERVER_ERROR") {
-      // send to bug reporting
-      console.error("Something went wrong", error);
-    }
-  },
-  /**
-   * Enable query batching
-   */
-  batching: {
-    enabled: true,
-  },
-  /**
-   * @link https://trpc.io/docs/caching#api-response-caching
-   */
-  responseMeta({ ctx, paths, type, errors }) {
-    return createResponseMetaData({ ctx, paths, type, errors });
-  },
-});
+export default createNextApiHandler(authRouter);
