207f5598506b9da661dd12f3c9d32336e362513f
Keyur Patel <keyur.patel@difx.io>
2023-04-29 19:18:58 -0400

    profile update refresh fixes

--- packages/features/settings/layouts/SettingsLayout.tsx
+++ packages/features/settings/layouts/SettingsLayout.tsx
@@ -105,14 +105,16 @@ const adminRequiredKeys = ["admin"];
 
 const useTabs = () => {
   const session = useSession();
+  const { data: user } = trpc.viewer.me.useQuery();
+  const { data: avatar } = trpc.viewer.avatar.useQuery();
 
   const isAdmin = session.data?.user.role === UserPermissionRole.ADMIN;
 
   tabs.map((tab) => {
-    if (tab.name === "my_account") {
-      tab.name = session.data?.user?.name || "my_account";
+    if (tab.href === "/settings/my-account") {
+      tab.name = user?.name || "my_account";
       tab.icon = undefined;
-      tab.avatar = WEBAPP_URL + "/" + session.data?.user?.username + "/avatar.png";
+      tab.avatar = avatar?.avatar || WEBAPP_URL + "/" + user?.username + "/avatar.png";
     }
     return tab;
   });
