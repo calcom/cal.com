ae9c700351719224ada37665819558119f9b56c9
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2023-04-30 23:05:28 +0530

    feat: lighten color from polished

--- packages/lib/getBrandColours.tsx
+++ packages/lib/getBrandColours.tsx
@@ -1,4 +1,4 @@
-import { rgb } from "polished";
+import { rgb, lighten as lightenColor } from "polished";
 
 import { useBrandColors } from "@calcom/embed-core/embed-iframe";
 
@@ -55,17 +55,7 @@ function rgbToHex(r: number, g: number, b: number): string {
 }
 
 function lighten(hex: string, intensity: number): string {
-  const color = hexToRgb(`#${hex}`);
-
-  if (!color) {
-    return "";
-  }
-
-  const r = Math.round(color.r + (255 - color.r) * intensity);
-  const g = Math.round(color.g + (255 - color.g) * intensity);
-  const b = Math.round(color.b + (255 - color.b) * intensity);
-
-  return rgbToHex(r, g, b);
+  return lightenColor(intensity, hex);
 }
 
 function darken(hex: string, intensity: number): string {
