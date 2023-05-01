d2988465b486f1b265f94548a533d7f132210623
Keith Williams <keithwillcode@gmail.com>
2023-05-01 20:42:34 -0300

    Reverted change in getSchedule test

--- apps/web/test/lib/getSchedule.test.ts
+++ apps/web/test/lib/getSchedule.test.ts
@@ -15,7 +15,8 @@ import { v4 as uuidv4 } from "uuid";
 import logger from "@calcom/lib/logger";
 import prisma from "@calcom/prisma";
 import type { BookingStatus } from "@calcom/prisma/client";
-import { getSchedule, type Slot } from "@calcom/trpc/server/routers/viewer/slots/types";
+import type { Slot } from "@calcom/trpc/server/routers/viewer/slots/types";
+import { getSchedule } from "@calcom/trpc/server/routers/viewer/slots/util";
 
 import { prismaMock, CalendarManagerMock } from "../../../../tests/config/singleton";
 
