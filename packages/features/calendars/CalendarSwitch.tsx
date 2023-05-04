f68637cc8ee130828051fd4868399df9169da838
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2023-05-04 18:53:37 +0100

    Add spinner to improve UX

--- packages/features/calendars/CalendarSwitch.tsx
+++ packages/features/calendars/CalendarSwitch.tsx
@@ -5,7 +5,7 @@ import { classNames } from "@calcom/lib";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { trpc } from "@calcom/trpc/react";
 import { showToast, Switch } from "@calcom/ui";
-import { ArrowLeft } from "@calcom/ui/components/icon";
+import { ArrowLeft, RotateCcw } from "@calcom/ui/components/icon";
 
 interface ICalendarSwitchProps {
   title: string;
@@ -92,6 +92,7 @@ const CalendarSwitch = (props: ICalendarSwitchProps) => {
           {t("adding_events_to")}
         </span>
       )}
+      {mutation.isLoading && <RotateCcw className="text-muted h-4 w-4 animate-spin ltr:ml-1 rtl:mr-1" />}
     </div>
   );
 };
