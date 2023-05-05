c967d92803e1c88f626c639ff5fa364cd6b0f03a
Keyur Patel <keyur.patel@difx.io>
2023-05-05 06:30:41 -0400

    fixed avatar loading issue

--- apps/web/pages/event-types/index.tsx
+++ apps/web/pages/event-types/index.tsx
@@ -20,6 +20,8 @@ import { HttpError } from "@calcom/lib/http-error";
 import { SchedulingType } from "@calcom/prisma/enums";
 import type { RouterOutputs } from "@calcom/trpc/react";
 import { trpc, TRPCClientError } from "@calcom/trpc/react";
+import useAvatarQuery from "@calcom/trpc/react/hooks/useAvatarQuery";
+import useMeQuery from "@calcom/trpc/react/hooks/useMeQuery";
 import {
   Avatar,
   AvatarGroup,
@@ -660,6 +662,8 @@ const EventTypeListHeading = ({
 }: EventTypeListHeadingProps): JSX.Element => {
   const { t } = useLocale();
   const router = useRouter();
+  const { data: loggedInUser } = useMeQuery();
+  const { data: avatar } = useAvatarQuery();
 
   const publishTeamMutation = trpc.viewer.teams.publish.useMutation({
     onSuccess(data) {
@@ -675,7 +679,11 @@ const EventTypeListHeading = ({
       <Avatar
         alt={profile?.name || ""}
         href={teamId ? `/settings/teams/${teamId}/profile` : "/settings/my-account/profile"}
-        imageSrc={`${WEBAPP_URL}/${profile.slug}/avatar.png` || undefined}
+        imageSrc={
+          loggedInUser?.username === profile.slug
+            ? avatar?.avatar
+            : `${WEBAPP_URL}/${profile.slug}/avatar.png` || undefined
+        }
         size="md"
         className="mt-1 inline-flex justify-center"
       />
