ca5ca4f36d0e499d9d66b04d14d061ed5b5997a9
Bharat saini <sainib601@gmail.com>
2023-05-06 08:46:13 +0530

    Booking not being cancelled on event deleting

--- packages/trpc/server/routers/viewer/eventTypes/delete.handler.ts
+++ packages/trpc/server/routers/viewer/eventTypes/delete.handler.ts
@@ -2,16 +2,38 @@ import { prisma } from "@calcom/prisma";
 
 import type { TrpcSessionUser } from "../../../trpc";
 import type { TDeleteInputSchema } from "./delete.schema";
+import handler, { CustomRequest } from "../../../../../features/bookings/lib/handleCancelBooking"
+
 
 type DeleteOptions = {
   ctx: {
     user: NonNullable<TrpcSessionUser>;
+    req: CustomRequest
   };
   input: TDeleteInputSchema;
 };
 
 export const deleteHandler = async ({ ctx: _ctx, input }: DeleteOptions) => {
   const { id } = input;
+
+  const event = await prisma.eventType.findFirst({
+    where:{
+      id,
+    },
+    select:{
+      bookings:true
+    }
+  })
+  
+  
+  if(event){
+
+    for(let element of event.bookings){
+        _ctx.req.body = { id : element.id , uid: element.uid}
+        handler(_ctx.req)
+    }
+
+  }
 
   await prisma.eventTypeCustomInput.deleteMany({
     where: {
