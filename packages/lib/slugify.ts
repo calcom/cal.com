f7a616e9c90d386732d63acf5f67e48569584ff3
Pranjal Goyal <pranjalgoyal13@gmail.com>
2023-05-07 19:47:30 +0530

    Updated The Slugify to accept username as emoji

--- packages/lib/slugify.ts
+++ packages/lib/slugify.ts
@@ -1,5 +1,5 @@
 export const slugify = (str: string) => {
-  return str.replace(/[^a-zA-Z0-9-]/g, "-").toLowerCase();
+  return str.replace(/[^a-zA-Z0-9-\p{Emoji}]/gu, "-").toLowerCase();
 };
 
 export default slugify;
