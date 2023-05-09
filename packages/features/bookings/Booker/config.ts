c0217b3629c1528af38298f078b6d7565fbbad44
Jeroen Reumkens <hello@jeroenreumkens.nl>
2023-05-09 11:21:46 +0200

    CAL-1623 CAL-1620: Improve animation for booker, and push mini calendar down in fullscreen views

--- packages/features/bookings/Booker/config.ts
+++ packages/features/bookings/Booker/config.ts
@@ -1,3 +1,7 @@
+import { cubicBezier, useAnimate } from "framer-motion";
+import { useReducedMotion } from "framer-motion";
+import { useEffect } from "react";
+
 import type { BookerLayout, BookerState } from "./types";
 
 // Framer motion fade in animation configs.
@@ -43,6 +47,7 @@ export const resizeAnimationConfig: ResizeAnimationConfig = {
   mobile: {
     default: {
       width: "100%",
+      minHeight: "0px",
       gridTemplateAreas: `
           "meta"
           "main"
@@ -54,28 +59,86 @@ export const resizeAnimationConfig: ResizeAnimationConfig = {
   small_calendar: {
     default: {
       width: "calc(var(--booker-meta-width) + var(--booker-main-width))",
+      minHeight: "450px",
+      height: "auto",
       gridTemplateAreas: `"meta main"`,
       gridTemplateColumns: "var(--booker-meta-width) var(--booker-main-width)",
     },
     selecting_time: {
-      width: "100%",
-      maxWidth: "calc(var(--booker-meta-width) + var(--booker-main-width) + var(--booker-timeslots-width))",
+      width: "calc(var(--booker-meta-width) + var(--booker-main-width) + var(--booker-timeslots-width))",
+      minHeight: "450px",
+      height: "auto",
       gridTemplateAreas: `"meta main timeslots"`,
       gridTemplateColumns: "var(--booker-meta-width) 1fr var(--booker-timeslots-width)",
     },
   },
   large_calendar: {
     default: {
-      width: "100%",
+      width: "100vw",
+      height: "100vh",
       gridTemplateAreas: `"meta main"`,
       gridTemplateColumns: "var(--booker-meta-width) 1fr",
     },
   },
   large_timeslots: {
     default: {
-      width: "100%",
+      width: "100vw",
+      height: "100vh",
       gridTemplateAreas: `"meta main"`,
       gridTemplateColumns: "var(--booker-meta-width) 1fr",
     },
   },
+};
+
+/**
+ * This hook returns a ref that should be set on the booker element.
+ * Based on that ref this hook animates the size of the booker element with framer motion.
+ * It also takes into account the prefers-reduced-motion setting, to not animate when that's set.
+ */
+export const useBookerResizeAnimation = (layout: BookerLayout, state: BookerState) => {
+  const prefersReducedMotion = useReducedMotion();
+  const [animationScope, animate] = useAnimate();
+
+  useEffect(() => {
+    const animationConfig = resizeAnimationConfig[layout][state] || resizeAnimationConfig[layout].default;
+
+    const animatedProperties = {
+      height: animationConfig?.height || "auto",
+    };
+
+    const nonAnimatedProperties = {
+      // Width is animated by the css class instead of via framer motion,
+      // because css is better at animating the calcs, framer motion might
+      // make some mistakes in that.
+      width: animationConfig?.width || "auto",
+      gridTemplateAreas: animationConfig?.gridTemplateAreas,
+      gridTemplateColumns: animationConfig?.gridTemplateColumns,
+      minHeight: animationConfig?.minHeight,
+    };
+
+    // We don't animate if users has set prefers-reduced-motion,
+    // or when the layout is mobile.
+    if (prefersReducedMotion || layout === "mobile") {
+      animate(
+        animationScope.current,
+        {
+          ...animatedProperties,
+          ...nonAnimatedProperties,
+        },
+        {
+          duration: 0,
+        }
+      );
+    } else {
+      animate(animationScope.current, nonAnimatedProperties, {
+        duration: 0,
+      });
+      animate(animationScope.current, animatedProperties, {
+        duration: 0.5,
+        ease: cubicBezier(0.4, 0, 0.2, 1),
+      });
+    }
+  }, [animate, animationScope, layout, prefersReducedMotion, state]);
+
+  return animationScope;
 };
