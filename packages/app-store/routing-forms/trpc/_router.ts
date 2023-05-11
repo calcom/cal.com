a2e18ae9ba9b9752436e90367de4e82f3a3b6508
Ansari <ansarithameem674@gmail.com>
2023-05-11 13:12:37 +0530

    deleted fields in duplicate form  🎭 (#8808)
    * deleted fields in duplicate form  🎭
    
    * Fixed duplicate deleted fields
    
    * Update packages/app-store/routing-forms/trpc/_router.ts
    
    ---------
    
    Co-authored-by: Hariom Balhara <hariombalhara@gmail.com>
    Co-authored-by: Keith Williams <keithwillcode@gmail.com>

--- packages/app-store/routing-forms/trpc/_router.ts
+++ packages/app-store/routing-forms/trpc/_router.ts
@@ -516,8 +516,7 @@ const appRoutingForms = router({
           // Duplicate just routes and fields
           // We don't want name, description and responses to be copied
           routes = routesParsed.data || [];
-          // FIXME: Deleted fields shouldn't come in duplicate
-          fields = fieldsParsed.data || [];
+          fields = fieldsParsed.data ? fieldsParsed.data.filter((f) => !f.deleted) : [];
         }
         return { routes, fields };
       }
