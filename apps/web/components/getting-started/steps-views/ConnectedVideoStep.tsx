afdcf4f78fe2142dc94baeeb71299b26753e1585
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2023-05-11 13:24:42 +0100

    Extract loader

--- apps/web/components/getting-started/steps-views/ConnectedVideoStep.tsx
+++ apps/web/components/getting-started/steps-views/ConnectedVideoStep.tsx
@@ -3,9 +3,10 @@ import { ArrowRightIcon } from "@heroicons/react/solid";
 import classNames from "@calcom/lib/classNames";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { trpc } from "@calcom/trpc/react";
-import { SkeletonAvatar, SkeletonText, SkeletonButton, List } from "@calcom/ui";
+import { List } from "@calcom/ui";
 
 import { AppConnectionItem } from "../components/AppConnectionItem";
+import { StepConnectionLoader } from "../components/StepConnectionLoader";
 
 interface ConnectedAppStepProps {
   nextStep: () => void;
@@ -47,19 +48,7 @@ const ConnectedVideoStep = (props: ConnectedAppStepProps) => {
         </List>
       )}
 
-      {isLoading && (
-        <ul className="bg-default divide-subtle border-subtle divide-y rounded-md border p-0 dark:bg-black">
-          {Array.from({ length: 4 }).map((_item, index) => {
-            return (
-              <li className="flex w-full flex-row justify-center border-b-0 py-6" key={index}>
-                <SkeletonAvatar className="mx-6 h-8 w-8 px-4" />
-                <SkeletonText className="ml-1 mr-4 mt-3 h-5 w-full" />
-                <SkeletonButton className="mr-6 h-8 w-20 rounded-md p-5" />
-              </li>
-            );
-          })}
-        </ul>
-      )}
+      {isLoading && <StepConnectionLoader />}
       <button
         type="button"
         data-testid="save-calendar-button"
