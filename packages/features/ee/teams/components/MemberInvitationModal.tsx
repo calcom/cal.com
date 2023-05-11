24d7a76c5b5b00111642762f99722bb89080942f
Sean Brydon <sean@cal.com>
2023-05-11 18:37:10 +0100

    Base UI work

--- packages/features/ee/teams/components/MemberInvitationModal.tsx
+++ packages/features/ee/teams/components/MemberInvitationModal.tsx
@@ -1,10 +1,11 @@
+import { PaperclipIcon, UserIcon, Users } from "lucide-react";
 import { Trans } from "next-i18next";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 
 import { IS_TEAM_BILLING_ENABLED } from "@calcom/lib/constants";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
-import type { MembershipRole } from "@calcom/prisma/enums";
+import { MembershipRole } from "@calcom/prisma/enums";
 import {
   Button,
   Checkbox as CheckboxField,
@@ -15,6 +16,8 @@ import {
   TextField,
   Label,
   ToggleGroup,
+  Select,
+  TextAreaField,
 } from "@calcom/ui";
 
 import type { PendingMember } from "../lib/types";
@@ -37,14 +40,31 @@ export interface NewMemberForm {
   sendInviteEmail: boolean;
 }
 
+type ModalMode = "INDIVIDUAL" | "BULK";
+
+const GoogleIcon = () => (
+  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g clip-path="url(#clip0_4178_176214)">
+      <path
+        d="M8.31875 15.36C4.26 15.36 0.9575 12.0588 0.9575 8.00001C0.9575 3.94126 4.26 0.640015 8.31875 0.640015C10.1575 0.640015 11.9175 1.32126 13.2763 2.55876L13.5238 2.78501L11.0963 5.21251L10.8713 5.02001C10.1588 4.41001 9.2525 4.07376 8.31875 4.07376C6.15375 4.07376 4.39125 5.83501 4.39125 8.00001C4.39125 10.165 6.15375 11.9263 8.31875 11.9263C9.88 11.9263 11.1138 11.1288 11.695 9.77001H7.99875V6.45626L15.215 6.46626L15.2688 6.72001C15.645 8.50626 15.3438 11.1338 13.8188 13.0138C12.5563 14.57 10.7063 15.36 8.31875 15.36Z"
+        fill="#6B7280"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_4178_176214">
+        <rect width="16" height="16" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
 export default function MemberInvitationModal(props: MemberInvitationModalProps) {
   const { t } = useLocale();
-
+  const [modalImportMode, setModalInputMode] = useState<ModalMode>("INDIVIDUAL");
   const options: MembershipRoleOption[] = useMemo(() => {
     return [
-      { value: "MEMBER", label: t("member") },
-      { value: "ADMIN", label: t("admin") },
-      { value: "OWNER", label: t("owner") },
+      { value: MembershipRole.MEMBER, label: t("member") },
+      { value: MembershipRole.ADMIN, label: t("admin") },
     ];
   }, [t]);
 
@@ -66,42 +86,93 @@ export default function MemberInvitationModal(props: MemberInvitationModalProps)
       }}>
       <DialogContent
         type="creation"
-        title={t("invite_new_member")}
+        title={t("invite_team_member")}
         description={
-          IS_TEAM_BILLING_ENABLED ? (
+          IS_TEAM_BILLING_ENABLED && (
             <span className="text-subtle text-sm leading-tight">
               <Trans i18nKey="invite_new_member_description">
                 Note: This will <span className="text-emphasis font-medium">cost an extra seat ($15/m)</span>{" "}
                 on your subscription.
               </Trans>
             </span>
-          ) : (
-            ""
           )
         }>
+        <div>
+          <Label className="sr-only" htmlFor="role">
+            {t("import_mode")}
+          </Label>
+          <ToggleGroup
+            isFullWidth={true}
+            onValueChange={(val) => setModalInputMode(val as ModalMode)}
+            defaultValue="INDIVIDUAL"
+            options={[
+              {
+                value: "INDIVIDUAL",
+                label: t("invite_team_individual_segment"),
+                iconLeft: <UserIcon />,
+              },
+              { value: "BULK", label: t("invite_team_bulk_segment"), iconLeft: <Users /> },
+            ]}
+          />
+        </div>
+
         <Form form={newMemberFormMethods} handleSubmit={(values) => props.onSubmit(values)}>
-          <div className="mt-6 space-y-6">
-            <Controller
-              name="emailOrUsername"
-              control={newMemberFormMethods.control}
-              rules={{
-                required: t("enter_email_or_username"),
-                validate: (value) => validateUniqueInvite(value) || t("member_already_invited"),
-              }}
-              render={({ field: { onChange }, fieldState: { error } }) => (
-                <>
-                  <TextField
-                    label={t("email_or_username")}
-                    id="inviteUser"
-                    name="inviteUser"
-                    placeholder="email@example.com"
-                    required
-                    onChange={(e) => onChange(e.target.value.trim().toLowerCase())}
-                  />
-                  {error && <span className="text-sm text-red-800">{error.message}</span>}
-                </>
-              )}
-            />
+          <div className="space-y-6">
+            {/* Indivdual Invite */}
+            {modalImportMode === "INDIVIDUAL" && (
+              <Controller
+                name="emailOrUsername"
+                control={newMemberFormMethods.control}
+                rules={{
+                  required: t("enter_email_or_username"),
+                  validate: (value) => validateUniqueInvite(value) || t("member_already_invited"),
+                }}
+                render={({ field: { onChange }, fieldState: { error } }) => (
+                  <>
+                    <TextField
+                      label={t("email_or_username")}
+                      id="inviteUser"
+                      name="inviteUser"
+                      placeholder="email@example.com"
+                      required
+                      onChange={(e) => onChange(e.target.value.trim().toLowerCase())}
+                    />
+                    {error && <span className="text-sm text-red-800">{error.message}</span>}
+                  </>
+                )}
+              />
+            )}
+            {/* Bulk Invite */}
+            {modalImportMode === "BULK" && (
+              <div className="bg-muted flex flex-col rounded-md p-4">
+                <TextAreaField
+                  name="emails"
+                  label="Invite via email"
+                  rows={4}
+                  placeholder="john@doe.com, alex@smith.com"
+                />
+                <div className="mt-1 mb-3 flex w-full items-center justify-center">
+                  <hr className="border-subtle border-1 h-px w-full" />
+                  <span className="bg-muted text-subtle px-2 py-1 leading-none">Or</span>
+                  <hr className="border-subtle border-1 h-px w-full" />
+                </div>
+
+                <Button
+                  type="button"
+                  color="secondary"
+                  StartIcon={GoogleIcon}
+                  className="justify-center gap-2">
+                  Import via Google Workspace
+                </Button>
+                <Button
+                  type="button"
+                  color="secondary"
+                  StartIcon={PaperclipIcon}
+                  className="mt-3 justify-center stroke-2">
+                  Upload a .csv file
+                </Button>
+              </div>
+            )}
             <Controller
               name="role"
               control={newMemberFormMethods.control}
@@ -109,17 +180,15 @@ export default function MemberInvitationModal(props: MemberInvitationModalProps)
               render={({ field: { onChange } }) => (
                 <div>
                   <Label className="text-emphasis font-medium" htmlFor="role">
-                    {t("role")}
+                    {t("invite_as")}
                   </Label>
-                  <ToggleGroup
-                    isFullWidth={true}
+                  <Select
                     id="role"
-                    onValueChange={onChange}
-                    defaultValue={options[0].value}
-                    options={[
-                      { value: "ADMIN", label: t("admin") },
-                      { value: "MEMBER", label: t("member") },
-                    ]}
+                    defaultValue={options[0]}
+                    options={options}
+                    onChange={(val) => {
+                      if (val) onChange(val.value);
+                    }}
                   />
                 </div>
               )}
@@ -138,7 +207,7 @@ export default function MemberInvitationModal(props: MemberInvitationModalProps)
               )}
             />
           </div>
-          <DialogFooter>
+          <DialogFooter showDivider>
             <Button
               type="button"
               color="minimal"
@@ -153,7 +222,7 @@ export default function MemberInvitationModal(props: MemberInvitationModalProps)
               color="primary"
               className="ms-2 me-2"
               data-testid="invite-new-member-button">
-              {t("invite")}
+              {t("send_invite")}
             </Button>
           </DialogFooter>
         </Form>
