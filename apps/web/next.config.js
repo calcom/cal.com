c0d5f3099a750a99c1021f5b0f81bd0b6fb543ba
Jeroen Reumkens <hello@jeroenreumkens.nl>
2023-05-18 09:05:37 +0200

    Updated nextjs config to redirect to correct embed url.

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -211,8 +211,18 @@ const nextConfig = {
         has: [{ type: "cookie", key: "new-booker-enabled" }],
       },
       {
+        source: `/:user((?!${pages.join("|")}).*)/:type/embed`,
+        destination: "/new-booker/:user/:type/embed",
+        has: [{ type: "cookie", key: "new-booker-enabled" }],
+      },
+      {
         source: "/team/:slug/:type",
         destination: "/new-booker/team/:slug/:type",
+        has: [{ type: "cookie", key: "new-booker-enabled" }],
+      },
+      {
+        source: "/team/:slug/:type/embed",
+        destination: "/new-booker/team/:slug/:type/embed",
         has: [{ type: "cookie", key: "new-booker-enabled" }],
       },
     ];
