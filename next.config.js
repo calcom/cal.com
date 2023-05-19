39a7a2240fe20b0edb72343370ffebaf56e3df5e
Alex van Andel <me@alexvanandel.com>
2023-05-19 20:00:04 +0200

    Remove modify-response middleware (it's huge and redundant) (#266)
    Remove custom types and now redundant express middlware.

--- next.config.js
+++ next.config.js
@@ -11,6 +11,32 @@ module.exports = withAxiom({
     "@calcom/prisma",
     "@calcom/trpc",
   ],
+  async headers() {
+    return [
+      {
+        source: "/docs",
+        headers: [
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, OPTIONS, PATCH, DELETE, POST, PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Content-Type, api_key, Authorization",
+          },
+        ],
+      },
+    ];
+  },
   async rewrites() {
     return {
       afterFiles: [
--- pages/api/docs.ts
+++ pages/api/docs.ts
@@ -1,6 +1,3 @@
-import modifyRes from "modify-response-middleware";
-import type { NextApiRequest, NextApiResponse } from "next";
-import { use } from "next-api-middleware";
 import { withSwagger } from "next-swagger-doc";
 
 import pjson from "~/package.json";
@@ -129,22 +126,4 @@ const swaggerHandler = withSwagger({
   apiFolder: "pages/api",
 });
 
-export default use(
-  modifyRes((content: string, _req: NextApiRequest, res: NextApiResponse) => {
-    // Add all headers here instead of next.config.js as it is throwing error( Cannot set headers after they are sent to the client) for OPTIONS method
-    // It is known to happen only in Dev Mode.
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, PATCH, DELETE, POST, PUT");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Content-Type, api_key, Authorization"
-    );
-    if (content) {
-      const parsed = JSON.parse(content);
-      // HACK: This is a hack to fix the swagger-ui issue with the extra channels property.
-      delete parsed.channels;
-      return Buffer.from(JSON.stringify(parsed));
-    }
-  })
-)(swaggerHandler());
+export default swaggerHandler();
