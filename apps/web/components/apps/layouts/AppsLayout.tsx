0be10b4580e7c58c0ef5effe6c0d02516d5c588d
Riyan Mohammad <107925299+Riyan911@users.noreply.github.com>
2023-05-21 11:52:22 +0530

    Update AppsLayout.tsx
    Removed extra line space to match prettier format

--- apps/web/components/apps/layouts/AppsLayout.tsx
+++ apps/web/components/apps/layouts/AppsLayout.tsx
@@ -20,8 +20,7 @@ export default function AppsLayout({ children, actions, emptyStore, ...rest }: A
   const router = useRouter();
   const isAdmin = session.data?.user.role === "ADMIN";
 
-  if (session.status === "loading") return <></>;
-  
+  if (session.status === "loading") return <></>;  
   return (
     <Shell {...rest} actions={actions?.("block")} hideHeadingOnMobile>
       <div className="flex flex-col xl:flex-row">
