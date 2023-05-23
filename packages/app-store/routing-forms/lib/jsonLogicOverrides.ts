49f23b6f072009805506277962c4aa17c5bbec36
Hariom Balhara <hariombalhara@gmail.com>
2023-05-23 16:34:18 +0530

    Fix bug in in operator when second arg is an array

--- packages/app-store/routing-forms/lib/jsonLogicOverrides.ts
+++ packages/app-store/routing-forms/lib/jsonLogicOverrides.ts
@@ -2,10 +2,26 @@
 import jsonLogic from "json-logic-js";
 
 // converts input to lowercase if string
-function normalize(input: any) {
-  return typeof input === "string" ? input.toLowerCase() : input;
+function normalize<T extends string | string[]>(input: T): T {
+  if (typeof input === "string") {
+    return input.toLowerCase() as T;
+  }
+  if (input instanceof Array) {
+    return input.map((item) => {
+      if (typeof item === "string") {
+        return item.toLowerCase();
+      }
+      // if array item is not a string, return it as is
+      return item;
+    }) as T;
+  }
+  return input;
 }
 
+/**
+ * Single Select equals and not equals uses it
+ * Short Text equals and not equals uses it
+ */
 jsonLogic.add_operation("==", function (a: any, b: any) {
   return normalize(a) == normalize(b);
 });
@@ -22,7 +38,12 @@ jsonLogic.add_operation("!=", function (a: any, b: any) {
   return normalize(a) != normalize(b);
 });
 
-jsonLogic.add_operation("in", function (a: any, b: any) {
+/**
+ * Multiselect "equals" and "not equals" uses it
+ * Singleselect "any in" and "not in" uses it
+ * Long Text/Short Text/Email/Phone "contains" also uses it.
+ */
+jsonLogic.add_operation("in", function (a: string, b: string | string[]) {
   const first = normalize(a);
   const second = normalize(b);
   if (!second || typeof second.indexOf === "undefined") return false;
