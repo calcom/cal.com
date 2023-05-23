84efda07e9a4c4150ee06878e056323fd9a90c74
Carina Wollendorfer <30310907+CarinaWolli@users.noreply.github.com>
2023-05-23 03:15:29 +0200

    Team webhooks (#8917)
    * allow event type specific webhooks for all event types
    
    * first version of team webhooks
    
    * add empty view
    
    * design fixes when no teams + invalidate query on delete/update
    
    * linke to new webhooks page with teamId in query
    
    * make one button with dropdown instead of a button for every team
    
    * add subtitle to dropdown
    
    * add avatar fallback
    
    * authorization when editing webhook
    
    * fix event type webhooks
    
    * fix authorization for delete handler
    
    * code clean up
    
    * fix disabled switch
    
    * add migration
    
    * fix subscriberUrlReservered function and fix authorization
    
    * fix type error
    
    * fix type error
    
    * fix switch not updating
    
    * make sure webhooks are triggered for the correct even types
    
    * code clean up
    
    * only show teams were user has write access
    
    * make webhooks read-only for members
    
    * fix comment
    
    * fix type error
    
    * fix webhook tests for team event types
    
    * implement feedback
    
    * code clean up from feedback
    
    * code clean up (feedback)
    
    * throw error if param missing in subscriberUrlReservered
    
    * handle null/undefined values in getWebhooks itself
    
    * better variable naming
    
    * better check if webhook is readonly
    
    * create assertPartOfTeamWithRequiredAccessLevel to remove duplicate code
    
    ---------
    
    Co-authored-by: CarinaWolli <wollencarina@gmail.com>
    Co-authored-by: alannnc <alannnc@gmail.com>

--- apps/web/pages/event-types/[type]/index.tsx
+++ apps/web/pages/event-types/[type]/index.tsx
@@ -39,8 +39,8 @@ import { EventLimitsTab } from "@components/eventtype/EventLimitsTab";
 import { EventRecurringTab } from "@components/eventtype/EventRecurringTab";
 import { EventSetupTab } from "@components/eventtype/EventSetupTab";
 import { EventTeamTab } from "@components/eventtype/EventTeamTab";
-import { EventTeamWebhooksTab } from "@components/eventtype/EventTeamWebhooksTab";
 import { EventTypeSingleLayout } from "@components/eventtype/EventTypeSingleLayout";
+import { EventWebhooksTab } from "@components/eventtype/EventWebhooksTab";
 import EventWorkflowsTab from "@components/eventtype/EventWorkfowsTab";
 
 import { ssrInit } from "@server/lib/ssr";
@@ -309,7 +309,7 @@ const EventTypePage = (props: EventTypeSetupProps) => {
         workflows={eventType.workflows.map((workflowOnEventType) => workflowOnEventType.workflow)}
       />
     ),
-    webhooks: <EventTeamWebhooksTab eventType={eventType} team={team} />,
+    webhooks: <EventWebhooksTab eventType={eventType} />,
   } as const;
 
   const handleSubmit = async (values: FormValues) => {
