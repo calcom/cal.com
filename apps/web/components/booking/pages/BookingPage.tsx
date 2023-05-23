c8f7daba4e0d0a5670c42bd116bdada808d5f558
zomars <zomars@me.com>
2023-05-23 16:12:05 -0700

    Formatting fixes

--- apps/web/components/booking/pages/BookingPage.tsx
+++ apps/web/components/booking/pages/BookingPage.tsx
@@ -641,7 +641,7 @@ const BookingPage = ({
                         {t("seats_available", {
                           count: currentSlotBooking
                             ? eventType.seatsPerTimeSlot - currentSlotBooking.attendees.length
-                            : eventType.seatsPerTimeSlot
+                            : eventType.seatsPerTimeSlot,
                         })}
                       </p>
                     </div>
