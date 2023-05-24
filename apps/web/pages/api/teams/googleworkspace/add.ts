d63e7372cb56a66891f78d4269c6b11ebc8ea8f5
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2023-05-24 02:01:31 +0100

    [Feat] Bulk Invite of Users + Google Workspace (#8969)
    * Base UI work
    
    * Bulk invite users
    
    * WIP workspace oauth implementation
    
    * Seperate components - add existing gcal check
    
    * Move callback session to getServerSession
    
    * Implementation of state redirect back to teams on login
    
    * Add callback to populate text field
    
    * Change error message
    
    * Redirect from callback and open modal
    
    * Fix bulk translations
    
    * Fix translations for google button
    
    * Delete Query
    
    * Feature flag this
    
    * Update packages/trpc/server/routers/viewer/teams/inviteMember.handler.ts
    
    * Check if Gcal in installed globally
    
    * Add new router
    
    * Add missing [trpc] route
    
    * Feedback
    
    * Update packages/trpc/server/routers/viewer/googleWorkspace/googleWorkspace.handler.ts
    
    * Typefixes
    
    * More typefixes
    
    ---------
    
    Co-authored-by: Hariom Balhara <hariombalhara@gmail.com>
    Co-authored-by: zomars <zomars@me.com>

---
+++ apps/web/pages/api/teams/googleworkspace/add.ts
@@ -0,0 +1,35 @@
+import { google } from "googleapis";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import getAppKeysFromSlug from "@calcom/app-store/_utils/getAppKeysFromSlug";
+import { WEBAPP_URL } from "@calcom/lib/constants";
+
+const scopes = [
+  "https://www.googleapis.com/auth/admin.directory.user.readonly",
+  "https://www.googleapis.com/auth/admin.directory.customer.readonly",
+];
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    // Get appKeys from google-calendar
+    const { client_id, client_secret } = await getAppKeysFromSlug("google-calendar");
+    if (!client_id || typeof client_id !== "string")
+      return res.status(400).json({ message: "Google client_id missing." });
+    if (!client_secret || typeof client_secret !== "string")
+      return res.status(400).json({ message: "Google client_secret missing." });
+
+    // use differnt callback to normal calendar connection
+    const redirect_uri = WEBAPP_URL + "/api/teams/googleworkspace/callback";
+    const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uri);
+
+    const authUrl = oAuth2Client.generateAuthUrl({
+      access_type: "offline",
+      scope: scopes,
+
+      prompt: "consent",
+      state: JSON.stringify({ teamId: req.query.teamId }),
+    });
+
+    res.status(200).json({ url: authUrl });
+  }
+}
