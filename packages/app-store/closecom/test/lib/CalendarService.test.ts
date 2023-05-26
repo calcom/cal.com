013eb7e1b0140e2989b50a651ea277ccb2b51b98
gitstart-calcom <gitstart@users.noreply.github.com>
2023-05-26 13:38:42 +0000

    solve conflicts

--- packages/app-store/closecom/test/lib/CalendarService.test.ts
+++ packages/app-store/closecom/test/lib/CalendarService.test.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { expect, vi, afterEach, test } from "vitest";
+
 import CloseCom from "@calcom/lib/CloseCom";
 import {
   getCloseComContactIds,
