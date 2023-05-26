6ea07d056d4adb657c342d41a315ce1842c59f25
CarinaWolli <wollencarina@gmail.com>
2023-05-26 17:58:22 +0200

    finish first e2e test

--- apps/web/playwright/workflow.e2e.ts
+++ apps/web/playwright/workflow.e2e.ts
@@ -1,40 +1,79 @@
 import { expect } from "@playwright/test";
 
+import dayjs from "@calcom/dayjs";
+import prisma from "@calcom/prisma";
+import { WorkflowMethods } from "@calcom/prisma/enums";
+
 import { test } from "./lib/fixtures";
+import { selectFirstAvailableTimeSlotNextMonth } from "./lib/testUtils";
 
 test.afterEach(({ users }) => users.deleteAll());
 
-test("add workflow with booking created trigger & test that booking an event triggers the workflow", async ({
-  page,
-  users,
-}) => {
-  const user = await users.create();
-  const [eventType] = user.eventTypes;
-  await user.login();
-  await page.goto(`/workflows`);
+test.describe("Workflow tests", () => {
+  test.describe("User Workflows", () => {
+    test("Create default reminder workflow & trigger when event type is booked", async ({ page, users }) => {
+      const user = await users.create();
+      const [eventType] = user.eventTypes;
+      await user.login();
+      await page.goto(`/workflows`);
 
-  await page.click('[data-testid="create-button"]');
+      await page.click('[data-testid="create-button"]');
 
-  // select first event type
-  await page.getByText("Select...").click();
-  await page.getByText(eventType.title, { exact: true }).click();
+      // select first event type
+      await page.getByText("Select...").click();
+      await page.getByText(eventType.title, { exact: true }).click();
 
-  // name workflow
-  await page.fill('[data-testid="workflow-name"]', "Test workflow");
+      // name workflow
+      await page.fill('[data-testid="workflow-name"]', "Test workflow");
 
-  // select when event is booked trigger
-  await page.locator("#trigger-select").click();
-  await page.getByText("When new event is booked", { exact: true }).click();
+      // save workflow
+      await page.click('[data-testid="save-workflow"]');
 
-  // save workflow
-  await page.click('[data-testid="save-workflow"]');
+      // check if workflow is saved
+      await expect(page.locator('[data-testid="workflow-list"] > li')).toHaveCount(1);
 
-  await expect(page.locator('[data-testid="workflow-list"] > li')).toHaveCount(1);
+      // book event type
+      await page.goto(`/${user.username}/${eventType.slug}`);
+      await selectFirstAvailableTimeSlotNextMonth(page);
 
-  // todo: book event type
+      await page.fill('[name="name"]', "Test");
+      await page.fill('[name="email"]', "test@example.com");
+      await page.press('[name="email"]', "Enter");
 
-  // todo: check if workflow triggered
+      // Make sure booking is completed
+      await expect(page.locator("[data-testid=success-page]")).toBeVisible();
+
+      const booking = await prisma.booking.findFirst({
+        where: {
+          eventTypeId: eventType.id,
+        },
+      });
+
+      // check if workflow triggered
+      const workflowReminders = await prisma.workflowReminder.findMany({
+        where: {
+          bookingUid: booking?.uid ?? "",
+        },
+      });
+
+      expect(workflowReminders).toHaveLength(1);
+
+      const scheduledDate = dayjs(booking?.startTime).subtract(1, "day").toDate();
+
+      expect(workflowReminders[0].method).toBe(WorkflowMethods.EMAIL);
+      expect(workflowReminders[0].scheduledDate.toISOString()).toBe(scheduledDate.toISOString());
+    });
+
+    //todo: test cancelling of booking and deleting workflow reminder
+    //todo: test the other triggers
+  });
+
+  /*
+  test.describe("Team Workflows", () => {
+    test("Admin can create and update team workflow", async ({ page, users }) => {
+      // managed-event-types.e2e.ts has as flow for creating a team (not sure if there is anything better)
+    });
+    test("Members can not create and update team workflows", async ({ page, users }) => {});
+  });
+  */
 });
-
-// Other possible tests:
-// admin can create and update team workflow, member can not update team workflow
