79132f20af00566efb000a5572511979bd9f93dc
headshigh <nischalgautam7200@gmail.com>
2023-05-29 08:26:50 +0545

    translation working

--- packages/features/form-builder/FormBuilder.tsx
+++ packages/features/form-builder/FormBuilder.tsx
@@ -483,7 +483,7 @@ export const FormBuilder = function FormBuilder({
                   fieldForm.getValues("editable") === "system" ||
                   fieldForm.getValues("editable") === "system-but-optional"
                 }
-                label="Identifier"
+                label={t("Identifier")}
               />
               <InputField
                 {...fieldForm.register("label")}
