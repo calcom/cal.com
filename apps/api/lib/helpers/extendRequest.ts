0ab287ac4ab424ecbf591310cd33770e9abd4943
Peer Richelsen <peeroke@gmail.com>
2023-05-29 20:00:22 +0100

    chore: temporarily delete /api folder
    reintroduce it once linter fixed

--- apps/api/lib/helpers/extendRequest.ts
+++
@@ -1,9 +0,0 @@
-import type { NextMiddleware } from "next-api-middleware";
-
-export const extendRequest: NextMiddleware = async (req, res, next) => {
-  req.pagination = {
-    take: 100,
-    skip: 0,
-  };
-  await next();
-};
--- apps/api/lib/validations/payment.ts
+++
@@ -1,5 +0,0 @@
-import { _PaymentModel as Payment } from "@calcom/prisma/zod";
-
-// FIXME: Payment seems a delicate endpoint, do we need to remove anything here?
-export const schemaPaymentBodyParams = Payment.omit({ id: true });
-export const schemaPaymentPublic = Payment.omit({ externalId: true });
