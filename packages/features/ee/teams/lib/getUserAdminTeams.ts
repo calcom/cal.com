47c4befa9dcb6bf55f1153081c0703c4ab265fd3
Joe Au-Yeung <j.auyeung419@gmail.com>
2023-05-31 08:01:28 -0400

    Add user query to getUserAdminTeams

--- packages/features/ee/teams/lib/getUserAdminTeams.ts
+++ packages/features/ee/teams/lib/getUserAdminTeams.ts
@@ -2,7 +2,7 @@ import { prisma } from "@calcom/prisma";
 import { MembershipRole } from "@calcom/prisma/enums";
 
 /** Get a user's team & orgs they are admins/owners of. Abstracted to a function to call in tRPC endpoint and SSR. */
-const getUserAdminTeams = async (userId: number) => {
+const getUserAdminTeams = async ({ userId, getUserInfo }: { userId: number; getUserInfo?: boolean }) => {
   const teams = await prisma.team.findMany({
     where: {
       members: {
@@ -19,9 +19,29 @@ const getUserAdminTeams = async (userId: number) => {
     },
   });
 
+  if (getUserInfo) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        id: true,
+        name: true,
+        avatar: true,
+      },
+    });
+
+    if (user) {
+      const userObject = { id: user.id, name: user.name || "Nameless", logo: user?.avatar, isUser: true };
+      teams.unshift(userObject);
+    }
+  }
+
   return teams;
 };
 
 export default getUserAdminTeams;
 
-export type UserAdminTeams = Awaited<Promise<ReturnType<typeof getUserAdminTeams>>> | [];
+export type UserAdminTeams =
+  | (Awaited<Promise<ReturnType<typeof getUserAdminTeams>>>[number] & { isUser?: boolean })[]
+  | [];
