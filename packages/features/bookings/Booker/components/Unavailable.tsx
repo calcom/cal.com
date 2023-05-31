855029703e7433e1a0b00a593e8c4a63ce7bfd9a
Jeroen Reumkens <hello@jeroenreumkens.nl>
2023-05-31 09:55:24 +0200

    Changed background from muted to default

--- packages/features/bookings/Booker/components/Unavailable.tsx
+++ packages/features/bookings/Booker/components/Unavailable.tsx
@@ -2,7 +2,7 @@ import { useLocale } from "@calcom/lib/hooks/useLocale";
 
 const UnAvailableMessage = ({ children, title }: { children: React.ReactNode; title: string }) => (
   <div className="mx-auto w-full max-w-2xl">
-    <div className="bg-muted border-subtle overflow-hidden rounded-lg border p-10">
+    <div className="border-subtle bg-default overflow-hidden rounded-lg border p-10">
       <h2 className="font-cal mb-4 text-3xl">{title}</h2>
       {children}
     </div>
