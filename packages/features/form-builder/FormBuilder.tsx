166555cf7634d5f66c0f947f39631e39299f7b2a
headshigh <nischalgautam7200@gmail.com>
2023-05-31 17:17:18 +0545

    Revert "translation working"
    This reverts commit 79132f20af00566efb000a5572511979bd9f93dc.

--- packages/features/form-builder/FormBuilder.tsx
+++ packages/features/form-builder/FormBuilder.tsx
@@ -483,7 +483,7 @@ export const FormBuilder = function FormBuilder({
                   fieldForm.getValues("editable") === "system" ||
                   fieldForm.getValues("editable") === "system-but-optional"
                 }
-                label={t("Identifier")}
+                label="Identifier"
               />
               <InputField
                 {...fieldForm.register("label")}
