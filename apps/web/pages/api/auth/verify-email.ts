f209e10a6fe7e7e1d27fcc862c23c134f8d3671b
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2023-05-31 09:33:00 +0100

    Update apps/web/pages/api/auth/verify-email.ts
    Co-authored-by: Omar López <zomars@me.com>

--- apps/web/pages/api/auth/verify-email.ts
+++ apps/web/pages/api/auth/verify-email.ts
@@ -23,7 +23,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 
   if (dayjs(foundToken?.expires).isBefore(dayjs())) {
-    res.status(401).json({ message: "Token expired" });
+    return res.status(401).json({ message: "Token expired" });
   }
 
   const user = await prisma.user.update({
