a0bf5b4067c41e513b07dfc260b1550f0a787137
Hariom Balhara <hariombalhara@gmail.com>
2023-06-02 02:11:30 +0530

    test: Smoke Tests for packaged embeds and build improvements (#9169)
    * Fix 2 Factor Auth
    
    * Add a sandbox to verify types of embed-react
    
    * Add fault types location
    
    * Fix type location again
    
    * Break types
    
    * Ensure that builds are done again when doing pbublish
    
    * Debug failure in CI
    
    * Make sure unit test files arent used by playwright
    
    * Fix embed-react test description
    
    * Update .github/workflows/e2e-embed-react.yml
    
    Co-authored-by: Omar López <zomars@me.com>
    
    * Remove unnecessary log
    
    ---------
    
    Co-authored-by: Alex van Andel <me@alexvanandel.com>
    Co-authored-by: Omar López <zomars@me.com>

--- packages/embeds/embed-react/playwright/tests/basic.e2e.ts
+++ packages/embeds/embed-react/playwright/tests/basic.e2e.ts
@@ -3,16 +3,22 @@ import { expect } from "@playwright/test";
 import { getEmbedIframe } from "@calcom/embed-core/playwright/lib/testUtils";
 import { test } from "@calcom/web/playwright/lib/fixtures";
 
-test("Inline Usage Snapshot", async ({ page, getActionFiredDetails, addEmbedListeners }) => {
-  //TODO: Do it with page.goto automatically
-  await addEmbedListeners("");
-  await page.goto("/");
-  const embedIframe = await getEmbedIframe({ page, pathname: "/pro" });
-  expect(embedIframe).toBeEmbedCalLink("", getActionFiredDetails, {
-    pathname: "/pro",
-    searchParams: {
-      theme: "dark",
-    },
+test.describe("Inline Embed", () => {
+  test("should verify that the iframe got created with correct URL", async ({
+    page,
+    getActionFiredDetails,
+    addEmbedListeners,
+  }) => {
+    //TODO: Do it with page.goto automatically
+    await addEmbedListeners("");
+    await page.goto("/");
+    const embedIframe = await getEmbedIframe({ page, pathname: "/pro" });
+    expect(embedIframe).toBeEmbedCalLink("", getActionFiredDetails, {
+      pathname: "/pro",
+      searchParams: {
+        theme: "dark",
+      },
+    });
+    // expect(await page.screenshot()).toMatchSnapshot("react-component-inline.png");
   });
-  // expect(await page.screenshot()).toMatchSnapshot("react-component-inline.png");
 });
