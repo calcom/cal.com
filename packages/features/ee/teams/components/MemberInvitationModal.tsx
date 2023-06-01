b6668a9a271783a25b238b3025da54df7039bef5
GitStart-Cal.com <121884634+gitstart-calcom@users.noreply.github.com>
2023-06-01 17:00:46 -0300

    Fix margin from invitation modal content

--- packages/features/ee/teams/components/MemberInvitationModal.tsx
+++ packages/features/ee/teams/components/MemberInvitationModal.tsx
@@ -104,7 +104,7 @@ export default function MemberInvitationModal(props: MemberInvitationModalProps)
         </div>
 
         <Form form={newMemberFormMethods} handleSubmit={(values) => props.onSubmit(values)}>
-          <div className="space-y-6 pb-7">
+          <div className="my-6 space-y-6">
             {/* Indivdual Invite */}
             {modalImportMode === "INDIVIDUAL" && (
               <Controller
@@ -214,21 +214,25 @@ export default function MemberInvitationModal(props: MemberInvitationModalProps)
               )}
             />
           </div>
+          <DialogFooter showDivider>
+            <Button
+              type="button"
+              color="minimal"
+              onClick={() => {
+                props.onExit();
+                newMemberFormMethods.reset();
+              }}>
+              {t("cancel")}
+            </Button>
+            <Button
+              type="submit"
+              color="primary"
+              className="ms-2 me-2"
+              data-testid="invite-new-member-button">
+              {t("send_invite")}
+            </Button>
+          </DialogFooter>
         </Form>
-        <DialogFooter showDivider>
-          <Button
-            type="button"
-            color="minimal"
-            onClick={() => {
-              props.onExit();
-              newMemberFormMethods.reset();
-            }}>
-            {t("cancel")}
-          </Button>
-          <Button type="submit" color="primary" className="ms-2 me-2" data-testid="invite-new-member-button">
-            {t("send_invite")}
-          </Button>
-        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
