e5f5f58994f286a399fe238482caa71b49792cb0
GitStart-Cal.com <121884634+gitstart-calcom@users.noreply.github.com>
2023-06-02 17:53:43 -0300

    Update FormBuilder.tsx to match figma

--- packages/features/form-builder/FormBuilder.tsx
+++ packages/features/form-builder/FormBuilder.tsx
@@ -544,14 +544,14 @@ export const FormBuilder = function FormBuilder({
                   );
                 }}
               />
+              <DialogFooter className="relative mt-8" showDivider>
+                <DialogClose color="secondary">{t("cancel")}</DialogClose>
+                <Button data-testid="field-add-save" type="submit" form="form-builder">
+                  {isFieldEditMode ? t("save") : t("add")}
+                </Button>
+              </DialogFooter>
             </Form>
           </div>
-          <DialogFooter className="relative rounded px-8 pb-6" showDivider>
-            <DialogClose color="secondary">{t("cancel")}</DialogClose>
-            <Button data-testid="field-add-save" type="submit" form="form-builder">
-              {isFieldEditMode ? t("save") : t("add")}
-            </Button>
-          </DialogFooter>
         </DialogContent>
       </Dialog>
     </div>
