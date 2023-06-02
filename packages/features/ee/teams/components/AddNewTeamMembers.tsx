3bfaba2d43644ac4acb6a0c51b5c91fe8b27d32c
Sean Brydon <sean@cal.com>
2023-06-02 15:20:25 +0100

    Handle updating subscription if orgId

--- packages/features/ee/teams/components/AddNewTeamMembers.tsx
+++ packages/features/ee/teams/components/AddNewTeamMembers.tsx
@@ -99,7 +99,6 @@ export const AddNewTeamMembersForm = ({
       </div>
       <MemberInvitationModal
         isOpen={memberInviteModal}
-        orgMembers={orgMembers}
         onExit={() => setMemberInviteModal(false)}
         onSubmit={(values) => {
           inviteMemberMutation.mutate({
