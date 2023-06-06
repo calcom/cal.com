7c8c2a94ca3779c3772e7c1290b0d9db399598e0
Peer Richelsen <peer@cal.com>
2023-06-06 11:09:04 +0100

    Update packages/features/form-builder/FormBuilder.tsx

--- packages/features/form-builder/FormBuilder.tsx
+++ packages/features/form-builder/FormBuilder.tsx
@@ -483,7 +483,7 @@ export const FormBuilder = function FormBuilder({
                   fieldForm.getValues("editable") === "system" ||
                   fieldForm.getValues("editable") === "system-but-optional"
                 }
-                label={t("Identifier")}
+                label={t("identifier")}
               />
               <InputField
                 {...fieldForm.register("label")}
