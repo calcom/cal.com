93fc036d2c466fd8121d4084c532e36a77d34cf0
Jeroen Reumkens <hello@jeroenreumkens.nl>
2023-06-08 14:32:17 +0200

    feat: Made booker layout settings more user friendly (#9397)
    * Made booker layout settings more user friendly
    
    * Fixed tyeps and default values in appearance form for booker layout
    
    * Enable all layouts by default

--- apps/web/pages/settings/my-account/appearance.tsx
+++ apps/web/pages/settings/my-account/appearance.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
+import type { z } from "zod";
 
 import { BookerLayoutSelector } from "@calcom/features/settings/BookerLayoutSelector";
 import ThemeLabel from "@calcom/features/settings/ThemeLabel";
@@ -9,6 +10,7 @@ import { checkWCAGContrastColor } from "@calcom/lib/getBrandColours";
 import { useHasPaidPlan } from "@calcom/lib/hooks/useHasPaidPlan";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { validateBookerLayouts } from "@calcom/lib/validateBookerLayouts";
+import type { userMetadata } from "@calcom/prisma/zod-utils";
 import { trpc } from "@calcom/trpc/react";
 import {
   Alert,
@@ -64,7 +66,7 @@ const AppearanceView = () => {
       brandColor: user?.brandColor || "#292929",
       darkBrandColor: user?.darkBrandColor || "#fafafa",
       hideBranding: user?.hideBranding,
-      defaultBookerLayouts: user?.defaultBookerLayouts,
+      metadata: user?.metadata as z.infer<typeof userMetadata>,
     },
   });
 
@@ -99,7 +101,7 @@ const AppearanceView = () => {
     <Form
       form={formMethods}
       handleSubmit={(values) => {
-        const layoutError = validateBookerLayouts(values.defaultBookerLayouts || null);
+        const layoutError = validateBookerLayouts(values?.metadata?.defaultBookerLayouts || null);
         if (layoutError) throw new Error(t(layoutError));
 
         mutation.mutate({
