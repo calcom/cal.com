16fc74fab72df8e47f7765c1c07a6ee29e2cf943
Efraín Rochín <roae.85@gmail.com>
2023-06-09 23:08:35 -0700

    moved viewport meta from _app.tsx to PageWrapper.tsx

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,5 +1,3 @@
-import Head from "next/head";
-
 import { trpc } from "@calcom/trpc/react";
 
 import type { AppProps } from "@lib/app-providers";
@@ -9,17 +7,7 @@ import "../styles/globals.css";
 function MyApp(props: AppProps) {
   const { Component, pageProps } = props;
   if (Component.PageWrapper !== undefined) return Component.PageWrapper(props);
-  return (
-    <>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default trpc.withTRPC(MyApp);
