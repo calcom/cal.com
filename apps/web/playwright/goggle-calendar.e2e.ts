c4390fb36d84a73b5c501ec15015934ce57226d7
Ninad3721 <ninadtrivedi25@gmail.com>
2023-06-12 19:56:02 +0530

    Added e2e testing for google calendar

---
+++ apps/web/playwright/goggle-calendar.e2e.ts
@@ -0,0 +1,32 @@
+import { expect } from "@playwright/test";
+
+import { GOOGLE_API_CREDENTIALS } from "../server/lib/constants";
+import { test } from "./lib/fixtures";
+
+test.describe("adding goggle calendar", async () => {
+  test.beforeEach(async ({ page, users }) => {
+    const user = await users.create();
+    await user.login();
+  });
+
+  test.afterAll(async ({ users }) => {
+    await users.deleteAll();
+  });
+
+  test("Google calendar got installed ", async ({ page }) => {
+    test.skip(!GOOGLE_API_CREDENTIALS, "It should only run if Google API credetials are present");
+    await page.goto("/apps/categories/calendar");
+    await page.waitForURL("/apps/categories/calendar");
+    await page.click("//a[@data-testid='app-store-app-card-google-calendar']");
+    await expect(page.locator("h1")).toHaveText("Google Calendar");
+    await expect(page.locator("//button[text()='Install another']")).toBeVisible();
+  });
+
+  test("Navigating to the installed apps", async ({ page }) => {
+    test.skip(!GOOGLE_API_CREDENTIALS, "It should only run if Google API credetials are present");
+    await page.goto("/apps/installed/calendar");
+    await page.waitForURL("/apps/installed/calendar");
+    await page.getByRole("menu", { name: "Add" }).click();
+    await expect(page.getByRole("button", { name: "Google Calendar Add Google Calendar" })).toBeVisible();
+  });
+});
