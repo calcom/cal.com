ca4a275f68973400c36cb5fc4eefdf74b97593cb
Jeroen Reumkens <hello@jeroenreumkens.nl>
2023-06-14 14:36:24 +0200

    Show correct timeformat for weekly view on left side time stamps

--- packages/features/calendars/weeklyview/components/horizontalLines/index.tsx
+++ packages/features/calendars/weeklyview/components/horizontalLines/index.tsx
@@ -1,6 +1,7 @@
 import { useId } from "react";
 
 import type dayjs from "@calcom/dayjs";
+import { useTimePreferences } from "@calcom/features/bookings/lib";
 
 export const HorizontalLines = ({
   hours,
@@ -11,6 +12,7 @@ export const HorizontalLines = ({
   numberOfGridStopsPerCell: number;
   containerOffsetRef: React.RefObject<HTMLDivElement>;
 }) => {
+  const { timeFormat } = useTimePreferences();
   const finalHour = hours[hours.length - 1].add(1, "hour").format("h A");
   const id = useId();
   return (
@@ -23,7 +25,7 @@ export const HorizontalLines = ({
       {hours.map((hour) => (
         <div key={`${id}-${hour.get("hour")}`}>
           <div className="text-muted sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5">
-            {hour.format("h A")}
+            {hour.format(timeFormat)}
           </div>
         </div>
       ))}
