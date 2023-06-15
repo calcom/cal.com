938d9b102eacca75d3edb617904543e7c18b88f6
Keith Williams <keithwillcode@gmail.com>
2023-06-15 20:11:51 -0300

    Added unit tests for CalendarManager->getCalendarCredentials

---
+++ packages/core/CalendarManager.test.ts
@@ -0,0 +1,31 @@
+import { describe, expect, it } from "vitest";
+
+import { getCalendarCredentials } from "./CalendarManager";
+
+describe("CalendarManager tests", () => {
+  describe("fn: getCalendarCredentials", () => {
+    it("should only return credentials for calendar apps", () => {
+        const googleCalendarCredentials = {
+            id: "1",
+            appId: "appIdOne",
+            type: "google_calendar",
+            userId: "3",
+            key: "google_calendar_key",
+            invalid: false
+        };
+
+        const credentials = [googleCalendarCredentials, {
+            id: "2",
+            appId: "appIdTwo",
+            type: "office365_video",
+            userId: "4",
+            key: "office365_video_key",
+            invalid: false
+        }];
+
+        const calendarCredentials = getCalendarCredentials(credentials);
+        expect(calendarCredentials).toHaveLength(1);
+        expect(calendarCredentials[0].credential).toBe(googleCalendarCredentials);
+    });
+  });
+});
