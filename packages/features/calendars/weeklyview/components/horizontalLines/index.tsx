5ba95729175b38b297f10846288205347e6523d8
Jeroen Reumkens <hello@jeroenreumkens.nl>
2023-06-15 14:39:58 +0530

    Force weekly view calendar hour labels to show 00 minutes, also for gmt x.5 timezones that are offset 30 mins.

--- packages/features/calendars/weeklyview/components/horizontalLines/index.tsx
+++ packages/features/calendars/weeklyview/components/horizontalLines/index.tsx
@@ -13,7 +13,8 @@ export const HorizontalLines = ({
   containerOffsetRef: React.RefObject<HTMLDivElement>;
 }) => {
   const { timeFormat } = useTimePreferences();
-  const finalHour = hours[hours.length - 1].add(1, "hour").format(timeFormat);
+  // We need to force the minute to zero, because otherwise in ex GMT+5.5, it would show :30 minute times (but at the positino of :00)
+  const finalHour = hours[hours.length - 1].add(1, "hour").minute(0).format(timeFormat);
   const id = useId();
   return (
     <div
@@ -25,7 +26,8 @@ export const HorizontalLines = ({
       {hours.map((hour) => (
         <div key={`${id}-${hour.get("hour")}`}>
           <div className="text-muted sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5">
-            {hour.format(timeFormat)}
+            {/* We need to force the minute to zero, because otherwise in ex GMT+5.5, it would show :30 minute times (but at the positino of :00) */}
+            {hour.minute(0).format(timeFormat)}
           </div>
         </div>
       ))}
