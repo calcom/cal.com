56d674a9f8d72c46f029ebbdbce99e589ffab501
Peer Richelsen <peer@cal.com>
2023-06-18 22:46:15 +0200

    lint

--- apps/web/pages/event-types/index.tsx
+++ apps/web/pages/event-types/index.tsx
@@ -799,7 +799,7 @@ const CTA = () => {
 
 const Actions = () => {
   return (
-    <div className="hidden md:flex items-center">
+    <div className="hidden items-center md:flex">
       <OrganizationEventTypeFilter />
       <VerticalDivider />
     </div>
