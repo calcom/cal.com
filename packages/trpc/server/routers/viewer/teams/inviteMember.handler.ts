a4ff5c2d570a9602b755dd67fb14ed1c036d333d
Sean Brydon <sean@cal.com>
2023-06-20 12:12:40 +0100

    Fix types

--- packages/trpc/server/routers/viewer/teams/inviteMember.handler.ts
+++ packages/trpc/server/routers/viewer/teams/inviteMember.handler.ts
@@ -6,8 +6,8 @@ import { sendTeamInviteEmail } from "@calcom/emails";
 import { updateQuantitySubscriptionFromStripe } from "@calcom/features/ee/teams/lib/payments";
 import { IS_TEAM_BILLING_ENABLED, WEBAPP_URL } from "@calcom/lib/constants";
 import { getTranslation } from "@calcom/lib/server/i18n";
+import { isTeamAdmin } from "@calcom/lib/server/queries";
 import { isOrganisationAdmin } from "@calcom/lib/server/queries/organisations";
-import { isTeamAdmin } from "@calcom/lib/server/queries/teams";
 import { prisma } from "@calcom/prisma";
 import type { Team, User } from "@calcom/prisma/client";
 import type { MembershipRole } from "@calcom/prisma/enums";
@@ -25,11 +25,23 @@ type InviteMemberOptions = {
   input: TInviteMemberInputSchema;
 };
 
-export const inviteMemberHandler = async ({ ctx, input }: InviteMemberOptions) => {
-  if (!(await isTeamAdmin(ctx.user?.id, input.teamId))) throw new TRPCError({ code: "UNAUTHORIZED" });
-  if (input.role === MembershipRole.OWNER && !(await isTeamOwner(ctx.user?.id, input.teamId)))
-    throw new TRPCError({ code: "UNAUTHORIZED" });
-  const translation = await getTranslation(input.language ?? "en", "common");
+async function checkPermissions({
+  userId,
+  teamId,
+  isOrg,
+}: {
+  userId: number;
+  teamId: number;
+  isOrg?: boolean;
+}) {
+  // Checks if the team they are inviteing to IS the org. Not a child team
+  if (isOrg) {
+    if (!(await isOrganisationAdmin(userId, teamId))) throw new TRPCError({ code: "UNAUTHORIZED" });
+  } else {
+    // TODO: do some logic here to check if the user is inviting a NEW user to a team that ISNT in the same org
+    if (!(await isTeamAdmin(userId, teamId))) throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+}
 
 async function getTeamOrThrow(teamId: number, isOrg?: boolean) {
   const team = await prisma.team.findFirst({
@@ -42,26 +54,21 @@ async function getTeamOrThrow(teamId: number, isOrg?: boolean) {
   });
 
   if (!team)
-    throw new TRPCError({ code: "NOT_FOUND", message: `${input.isOrg ? "Organization" : "Team"} not found` });
+    throw new TRPCError({ code: "NOT_FOUND", message: `${isOrg ? "Organization" : "Team"} not found` });
+  return team;
+}
 
 async function getEmailsToInvite(usernameOrEmail: string | string[]) {
   const emailsToInvite = Array.isArray(usernameOrEmail) ? usernameOrEmail : [usernameOrEmail];
 
-  if (emailsToInvite.length === 0)
+  if (emailsToInvite.length === 0) {
     throw new TRPCError({
       code: "BAD_REQUEST",
       message: "You must provide at least one email address to invite.",
     });
-
-    if (input.isOrg && invitee) {
-      throw new TRPCError({
-        code: "NOT_FOUND",
-        message: `Email ${usernameOrEmail} already exists, you can't invite existing users.`,
-      });
-    }
-
-    if (!invitee) {
-      // liberal email match
+  }
+  return emailsToInvite;
+}
 
 async function getUserToInviteOrThrowIfExists(usernameOrEmail: string, orgId: number, isOrg?: boolean) {
   // Check if user exists in ORG or exists all together
@@ -214,7 +221,7 @@ function checkIfUserIsInDifOrg(
 export const inviteMemberHandler = async ({ ctx, input }: InviteMemberOptions) => {
   const team = await getTeamOrThrow(input.teamId, input.isOrg);
 
-  await checkPermissions(ctx.user.id, input.teamId, input.isOrg);
+  await checkPermissions({ userId: ctx.user.id, teamId: input.teamId, isOrg: input.isOrg });
 
   const translation = await getTranslation(input.language ?? "en", "common");
 
