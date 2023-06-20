9adb4a24fac69fbba01747bb3a14bbb3cbdd6de3
Leo Giovanetti <hello@leog.me>
2023-06-20 16:04:19 -0300

    Renamed function in tests too

--- packages/features/test/orgDomains.test.ts
+++ packages/features/test/orgDomains.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from "vitest";
 
-import { orgDomainConfig, getOrgDomain } from "@calcom/features/ee/organizations/lib/orgDomains";
+import { orgDomainConfig, getOrgSlug } from "@calcom/features/ee/organizations/lib/orgDomains";
 
 import * as constants from "@calcom/lib/constants";
 
@@ -23,25 +23,25 @@ describe("Org Domains Utils", () => {
     });
   });
 
-  describe("getOrgDomain", () => {
+  describe("getOrgSlug", () => {
     it("should handle a prod web app url with a prod subdomain hostname", () => {
       Object.defineProperty(constants, 'WEBAPP_URL', {value:"https://app.cal.com"});
-      expect(getOrgDomain("acme.cal.com")).toEqual("acme");
+      expect(getOrgSlug("acme.cal.com")).toEqual("acme");
     });
 
     it("should handle a prod web app url with a staging subdomain hostname", () => {
       Object.defineProperty(constants, 'WEBAPP_URL', {value:"https://app.cal.com"});
-      expect(getOrgDomain("acme.cal.dev")).toEqual(null);
+      expect(getOrgSlug("acme.cal.dev")).toEqual(null);
     });
 
     it("should handle a local web app with port url with a local subdomain hostname", () => {
       Object.defineProperty(constants, 'WEBAPP_URL', {value:"http://app.cal.local:3000"});
-      expect(getOrgDomain("acme.cal.local:3000")).toEqual("acme");
+      expect(getOrgSlug("acme.cal.local:3000")).toEqual("acme");
     });
 
     it("should handle a local web app with port url with a non-local subdomain hostname", () => {
       Object.defineProperty(constants, 'WEBAPP_URL', {value:"http://app.cal.local:3000"});
-      expect(getOrgDomain("acme.cal.com:3000")).toEqual(null);
+      expect(getOrgSlug("acme.cal.com:3000")).toEqual(null);
     });
   })
 });
