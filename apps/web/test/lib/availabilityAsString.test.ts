12154f1c32999f9b7297c241957165f17d8180f9
Keith Williams <keithwillcode@gmail.com>
2023-06-22 18:29:59 +0200

    Replacing unicode space

--- apps/web/test/lib/availabilityAsString.test.ts
+++ apps/web/test/lib/availabilityAsString.test.ts
@@ -19,8 +19,9 @@ it("correctly handles 1 day", async () => {
     hour12: true
   });
 
-  expect(result.toString()).toBe("Mon, 9:00 AM - 5:00 PM");
+  expect(replaceUnicodeSpace(result)).toBe("Mon, 9:00 AM - 5:00 PM");
 });
+
 
 it("correctly handles all days", async () => {
   const availability = {
@@ -39,7 +40,7 @@ it("correctly handles all days", async () => {
     hour12: true
   });
 
-  expect(result.toString()).toBe("Mon - Sun, 9:00 AM - 5:00 PM");
+  expect(replaceUnicodeSpace(result)).toBe("Mon - Sun, 9:00 AM - 5:00 PM");
 });
 
 it("correctly handles staggered days", async () => {
@@ -59,7 +60,7 @@ it("correctly handles staggered days", async () => {
     hour12: true
   });
 
-  expect(result.toString()).toBe("Mon, Wed, Fri, Sun, 9:00 AM - 5:00 PM");
+  expect(replaceUnicodeSpace(result)).toBe("Mon, Wed, Fri, Sun, 9:00 AM - 5:00 PM");
 });
 
 it("correctly produces days and times - 12 hours", async () => {
@@ -79,7 +80,7 @@ it("correctly produces days and times - 12 hours", async () => {
     hour12: true
   });
 
-  expect(result.toString()).toBe("Mon - Wed, 9:00 AM - 5:00 PM");
+  expect(replaceUnicodeSpace(result)).toBe("Mon - Wed, 9:00 AM - 5:00 PM");
 });
 
 it("correctly produces days and times - 24 hours", async () => {
@@ -99,5 +100,9 @@ it("correctly produces days and times - 24 hours", async () => {
     hour12: false
   });
 
-  expect(result.toString()).toBe("Mon - Wed, 09:00 - 17:00");
+  expect(replaceUnicodeSpace(result)).toBe("Mon - Wed, 09:00 - 17:00");
 });
+
+function replaceUnicodeSpace(string) {
+  return string.replace('\u202f', ' ');
+}
