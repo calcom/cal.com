31dccf7f9d800ffab22f20d72c3d5c4a5c9f510c
Ninad3721 <ninadtrivedi25@gmail.com>
2023-06-23 20:29:12 +0530

    Added Condditional error message for admin password banner

--- packages/features/users/components/AdminPasswordBanner.tsx
+++ packages/features/users/components/AdminPasswordBanner.tsx
@@ -3,17 +3,19 @@ import Link from "next/link";
 
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { TopBanner } from "@calcom/ui";
+import { trpc } from "@calcom/trpc/react";
 
 function AdminPasswordBanner() {
   const { t } = useLocale();
   const { data } = useSession();
+  const { data: user, isLoading } = trpc.viewer.me.useQuery();
 
   if (data?.user.role !== "INACTIVE_ADMIN") return null;
 
   return (
     <>
       <TopBanner
-        text={t("invalid_admin_password", { user: data.user.username })}
+       text={user?.twoFactorEnabled ? t("invalid_admin_password", { user: data.user.username }) : t("invalid_admin_password_and_no_2FA", { user: data.user.username }) }
         variant="warning"
         actions={
           <Link href="/settings/security/password" className="border-b border-b-black">
