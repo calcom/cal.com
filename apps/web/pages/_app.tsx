c9325a66e5d4037be42ed0bd37478ea56bd9e36b
nafees nazik <nafeesnazik21@gmail.com>
2023-06-24 23:05:34 +0530

    feat: add progress indicator

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,8 +1,22 @@
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
+
 import { trpc } from "@calcom/trpc/react";
 
 import type { AppProps } from "@lib/app-providers";
 
 import "../styles/globals.css";
+
+const progress = new ProgressBar({
+  size: 2,
+  color: "#38bdf8",
+  className: "bar-of-progress",
+  delay: 100,
+});
+
+Router.events.on("routeChangeStart", () => progress.start());
+Router.events.on("routeChangeComplete", () => progress.finish());
+Router.events.on("routeChangeError", () => progress.finish());
 
 function MyApp(props: AppProps) {
   const { Component, pageProps } = props;
