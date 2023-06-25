70d98f9ace9628afe8895e548d2871bc046f065f
Sean Brydon <sean@cal.com>
2023-06-25 21:51:28 +0100

    Add single checkbox - add selection

--- packages/ui/components/data-table/__storybook__/columns.tsx
+++ packages/ui/components/data-table/__storybook__/columns.tsx
@@ -1,10 +1,30 @@
 import type { ColumnDef } from "@tanstack/react-table";
 
 import { Badge } from "../../badge";
+import { Checkbox } from "../../form";
 import type { FilterableItems } from "../DataTableToolbar";
 import type { DataTableUserStorybook } from "./data";
 
 export const columns: ColumnDef<DataTableUserStorybook>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+        className="translate-y-[2px]"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+        className="translate-y-[2px]"
+      />
+    ),
+  },
   {
     accessorKey: "username",
     header: "Username",
