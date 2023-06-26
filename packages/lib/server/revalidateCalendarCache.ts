d94176559dccc418235b40ce63267b41dd1220e6
Keith Williams <keithwillcode@gmail.com>
2023-06-26 19:52:00 +0200

    Removed remaining references to calendar-cache

--- packages/lib/server/revalidateCalendarCache.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiResponse } from "next";
-
-export const revalidateCalendarCache = (
-  revalidate: NextApiResponse["revalidate"],
-  username: string,
-  monthsToRevalidate = 4
-): Promise<void[]> => {
-  return true;
-};
