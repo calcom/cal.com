c16687e004e57bab14b104039a619e277fa5f574
Joe Au-Yeung <j.auyeung419@gmail.com>
2023-06-27 00:38:20 -0400

    Remove UserAdminTeams empty array union

--- packages/features/ee/teams/lib/getUserAdminTeams.ts
+++ packages/features/ee/teams/lib/getUserAdminTeams.ts
@@ -3,15 +3,13 @@ import type { Prisma } from "@prisma/client";
 import { prisma } from "@calcom/prisma";
 import { MembershipRole } from "@calcom/prisma/enums";
 
-export type UserAdminTeams =
-  | (Prisma.TeamGetPayload<{
-      select: {
-        id: true;
-        name: true;
-        logo: true;
-      };
-    }> & { isUser?: boolean })[]
-  | [];
+export type UserAdminTeams = (Prisma.TeamGetPayload<{
+  select: {
+    id: true;
+    name: true;
+    logo: true;
+  };
+}> & { isUser?: boolean })[];
 
 /** Get a user's team & orgs they are admins/owners of. Abstracted to a function to call in tRPC endpoint and SSR. */
 const getUserAdminTeams = async ({
