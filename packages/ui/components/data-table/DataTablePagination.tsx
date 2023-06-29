b278badeadea0ba41751dcbbbde3b51c2225e195
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2023-06-29 12:30:14 +0100

    Fix pagination footer - settings layout max -w

--- packages/ui/components/data-table/DataTablePagination.tsx
+++ packages/ui/components/data-table/DataTablePagination.tsx
@@ -15,25 +15,6 @@ export function DataTablePagination<TData>({ table }: DataTablePaginationProps<T
         selected.
       </div>
       <div className="flex items-center space-x-6 lg:space-x-8">
-        <div className="flex items-center space-x-2">
-          <p className="text-sm font-medium">Rows per page</p>
-          {/* <Select
-            value={`${table.getState().pagination.pageSize}`}
-            onValueChange={(value) => {
-              table.setPageSize(Number(value));
-            }}>
-            <SelectTrigger className="h-8 w-[70px]">
-              <SelectValue placeholder={table.getState().pagination.pageSize} />
-            </SelectTrigger>
-            <SelectContent side="top">
-              {[10, 20, 30, 40, 50].map((pageSize) => (
-                <SelectItem key={pageSize} value={`${pageSize}`}>
-                  {pageSize}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select> */}
-        </div>
         <div className="flex w-[100px] items-center justify-center text-sm font-medium">
           Page {table.getState().pagination.pageIndex} of {table.getPageCount()}
         </div>
