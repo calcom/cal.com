74d63722b74107142c0224b2ae732d5c461615af
Jemilu Mohammed <jemiluv8@gmail.com>
2023-07-02 02:18:34 +0000

    lint fix

--- apps/web/components/booking/pages/BookingPage.tsx
+++ apps/web/components/booking/pages/BookingPage.tsx
@@ -129,10 +129,7 @@ const BookingFields = ({
         }
 
         // We don't show `notes` field during reschedule
-        if (
-          (field.name === SystemField.Enum.notes) &&
-          !!rescheduleUid
-        ) {
+        if (field.name === SystemField.Enum.notes && !!rescheduleUid) {
           return null;
         }
 
