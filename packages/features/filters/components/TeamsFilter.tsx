6fb51db6ac076e7a32bbb5e90d40f85de6ce12f0
Ciarán Hanrahan <justciaranhanrahan@gmail.com>
2023-07-02 11:12:50 +0100

    chore: small changes to dropdown & main sidebar (#9719)

--- packages/features/filters/components/TeamsFilter.tsx
+++ packages/features/filters/components/TeamsFilter.tsx
@@ -127,13 +127,13 @@ type Props = InputHTMLAttributes<HTMLInputElement> & {
 
 export const FilterCheckboxField = forwardRef<HTMLInputElement, Props>(({ label, icon, ...rest }, ref) => {
   return (
-    <div className="hover:bg-muted flex items-center py-2 pl-3 pr-2.5 hover:cursor-pointer">
+    <div className="hover:bg-subtle flex items-center py-2 pl-3 pr-2.5 hover:cursor-pointer">
       <label className="flex w-full items-center justify-between hover:cursor-pointer">
         <div className="flex items-center">
           <div className="text-default flex h-4 w-4 items-center justify-center ltr:mr-2 rtl:ml-2">
             {icon}
           </div>
-          <label htmlFor={rest.id} className="text-default cursor-pointer truncate text-sm">
+          <label htmlFor={rest.id} className="text-default cursor-pointer truncate text-sm font-medium">
             {label}
           </label>
         </div>
