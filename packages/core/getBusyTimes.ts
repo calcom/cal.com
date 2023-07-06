3a6ad3a3bbb1721515246a9af1c38bd5a1eeb7bf
Alex van Andel <me@alexvanandel.com>
2023-07-07 01:31:39 +0200

    re-add seatsReferences to fix jest

--- packages/core/getBusyTimes.ts
+++ packages/core/getBusyTimes.ts
@@ -114,6 +114,8 @@ export async function getBusyTimes(params: {
     (aggregate: EventBusyDetails[], { id, startTime, endTime, eventType, title, seatsReferences }) => {
       // Seat references on the current event are non-blocking until the event is fully booked.
       if (
+        // this is purely for jest; prisma responses are []
+        seatsReferences &&
         // if this is a seated event and there are still seats available.
         seatsReferences.length < (eventType?.seatsPerTimeSlot || 0) &&
         // and this is the seated event, other event types should be blocked.
