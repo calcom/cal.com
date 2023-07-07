6be925adf77c8f4307a7b23306ef669639d6af3e
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2023-07-07 09:58:44 +0100

    Add pagination to handler

--- packages/trpc/server/routers/viewer/admin/listPaginated.handler.ts
+++ packages/trpc/server/routers/viewer/admin/listPaginated.handler.ts
@@ -1,9 +1,39 @@
-import type { TrpcSessionUser } from "../../../trpc";
+import { prisma } from "@calcom/prisma";
 
-type ListPaginatedOptions = {
+import type { TrpcSessionUser } from "../../../trpc";
+import type { TListMembersSchema } from "./listPaginated.schema";
+
+type GetOptions = {
   ctx: {
     user: NonNullable<TrpcSessionUser>;
   };
+  input: TListMembersSchema;
 };
 
-export const listPaginated = async ({ ctx }: ListPaginatedOptions) => {};
+export const listMembersHandler = async ({ ctx, input }: GetOptions) => {
+  const { cursor, limit } = input;
+
+  const getTotalUsers = await prisma.user.count();
+
+  const users = await prisma.user.findMany({
+    cursor: cursor ? { id: cursor } : undefined,
+    take: limit + 1, // We take +1 as itll be used for the next cursor
+    orderBy: {
+      id: "asc",
+    },
+  });
+
+  let nextCursor: typeof cursor | undefined = undefined;
+  if (users && users.length > limit) {
+    const nextItem = users.pop();
+    nextCursor = nextItem!.id;
+  }
+
+  return {
+    rows: users || [],
+    nextCursor,
+    meta: {
+      totalRowCount: getTotalUsers || 0,
+    },
+  };
+};
