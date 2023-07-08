d801fe85c3b9cccf20dabaf06815a2bc1794dac3
Pradumn <pradumn@tealfeed.com>
2023-07-08 11:04:31 +0530

    fix: import path of slugify in api

--- apps/api/lib/validations/event-type.ts
+++ apps/api/lib/validations/event-type.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
+import slugify from "@calcom/lib/slugify";
 import { _EventTypeModel as EventType, _HostModel } from "@calcom/prisma/zod";
 import { customInputSchema, eventTypeBookingFields } from "@calcom/prisma/zod-utils";
 
-import slugify from "~/../../packages/lib/slugify";
 import { Frequency } from "~/lib/types";
 
 import { jsonSchema } from "./shared/jsonSchema";
