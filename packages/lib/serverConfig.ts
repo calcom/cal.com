6ba51ae1d4bbcdc568320c715027efb2c7f6e1ed
Amitav Khandelwal <amitav13@users.noreply.github.com>
2023-07-09 12:15:55 +0530

    Fix for mailhog not working locally

--- packages/lib/serverConfig.ts
+++ packages/lib/serverConfig.ts
@@ -13,10 +13,13 @@ function detectTransport(): SendmailTransport.Options | SMTPConnection.Options |
     const transport = {
       host: process.env.EMAIL_SERVER_HOST,
       port,
-      auth: {
-        user: process.env.EMAIL_SERVER_USER,
-        pass: process.env.EMAIL_SERVER_PASSWORD,
-      },
+      auth:
+        process.env.EMAIL_SERVER_HOST === "localhost"
+          ? undefined
+          : {
+              user: process.env.EMAIL_SERVER_USER,
+              pass: process.env.EMAIL_SERVER_PASSWORD,
+            },
       secure: port === 465,
       tls: {
         rejectUnauthorized: isENVDev ? false : true,
