9c7428571d4ca13ba6b8eaf2d1f478900e2c23ea
Udit Takkar <53316345+Udit-takkar@users.noreply.github.com>
2023-07-12 01:56:30 +0530

    feat: added organizer daily video recording email (#9667)
    Co-authored-by: Shivam Kalra <shivamkalra98@gmail.com>

--- packages/emails/templates/organizer-daily-video-download-recording-email.ts
+++ packages/emails/templates/organizer-daily-video-download-recording-email.ts
@@ -1,29 +1,27 @@
-// TODO: We should find a way to keep App specific email templates within the App itself
 import type { TFunction } from "next-i18next";
 
-import type { CalendarEvent, Person } from "@calcom/types/Calendar";
+import { APP_NAME } from "@calcom/lib/constants";
+import type { CalendarEvent } from "@calcom/types/Calendar";
 
 import { renderEmail } from "../";
 import BaseEmail from "./_base-email";
 
-export default class AttendeeDailyVideoDownloadRecordingEmail extends BaseEmail {
+export default class OrganizerDailyVideoDownloadRecordingEmail extends BaseEmail {
   calEvent: CalendarEvent;
-  attendee: Person;
   downloadLink: string;
   t: TFunction;
 
-  constructor(calEvent: CalendarEvent, attendee: Person, downloadLink: string) {
+  constructor(calEvent: CalendarEvent, downloadLink: string) {
     super();
     this.name = "SEND_RECORDING_DOWNLOAD_LINK";
     this.calEvent = calEvent;
-    this.attendee = attendee;
     this.downloadLink = downloadLink;
-    this.t = attendee.language.translate;
+    this.t = this.calEvent.organizer.language.translate;
   }
   protected getNodeMailerPayload(): Record<string, unknown> {
     return {
-      to: `${this.attendee.name} <${this.attendee.email}>`,
-      from: `${this.calEvent.organizer.name} <${this.getMailerOptions().from}>`,
+      to: `${this.calEvent.organizer.email}>`,
+      from: `${APP_NAME} <${this.getMailerOptions().from}>`,
       replyTo: [...this.calEvent.attendees.map(({ email }) => email), this.calEvent.organizer.email],
       subject: `${this.t("download_recording_subject", {
         title: this.calEvent.title,
@@ -34,26 +32,26 @@ export default class AttendeeDailyVideoDownloadRecordingEmail extends BaseEmail 
         date: this.getFormattedDate(),
         downloadLink: this.downloadLink,
         language: this.t,
-        name: this.attendee.name,
+        name: this.calEvent.organizer.name,
       }),
     };
   }
 
   protected getTimezone(): string {
-    return this.attendee.timeZone;
+    return this.calEvent.organizer.timeZone;
   }
 
-  protected getInviteeStart(format: string) {
+  protected getOrganizerStart(format: string) {
     return this.getRecipientTime(this.calEvent.startTime, format);
   }
 
-  protected getInviteeEnd(format: string) {
+  protected getOrganizerEnd(format: string) {
     return this.getRecipientTime(this.calEvent.endTime, format);
   }
 
   protected getFormattedDate() {
-    return `${this.getInviteeStart("h:mma")} - ${this.getInviteeEnd("h:mma")}, ${this.t(
-      this.getInviteeStart("dddd").toLowerCase()
-    )}, ${this.t(this.getInviteeStart("MMMM").toLowerCase())} ${this.getInviteeStart("D, YYYY")}`;
+    return `${this.getOrganizerStart("h:mma")} - ${this.getOrganizerEnd("h:mma")}, ${this.t(
+      this.getOrganizerStart("dddd").toLowerCase()
+    )}, ${this.t(this.getOrganizerStart("MMMM").toLowerCase())} ${this.getOrganizerStart("D, YYYY")}`;
   }
 }
