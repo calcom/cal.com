640ff17206a829109cd6a0a32a4f01e231b5bff3
Alex van Andel <me@alexvanandel.com>
2023-07-11 16:29:07 +0200

    getBookingForReschedule rename didn't work

--- apps/web/pages/team/[slug]/[type].tsx
+++ apps/web/pages/team/[slug]/[type].tsx
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 import { Booker } from "@calcom/atoms";
 import { BookerSeo } from "@calcom/features/bookings/components/BookerSeo";
-import { getBookingByUidOrRescheduleUid } from "@calcom/features/bookings/lib/get-booking";
+import { getBookingForReschedule } from "@calcom/features/bookings/lib/get-booking";
 import type { GetBookingType } from "@calcom/features/bookings/lib/get-booking";
 import { classNames } from "@calcom/lib";
 import slugify from "@calcom/lib/slugify";
@@ -29,7 +29,7 @@ export default function Type({ slug, user, booking, away, isBrandingHidden }: Pa
       <Booker
         username={user}
         eventSlug={slug}
-        rescheduleBooking={booking}
+        bookingData={booking}
         isAway={away}
         hideBranding={isBrandingHidden}
         isTeamEvent
@@ -72,7 +72,7 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
 
   let booking: GetBookingType | null = null;
   if (rescheduleUid) {
-    booking = await getBookingByUidOrRescheduleUid(`${rescheduleUid}`);
+    booking = await getBookingForReschedule(`${rescheduleUid}`);
   }
 
   // We use this to both prefetch the query on the server,
