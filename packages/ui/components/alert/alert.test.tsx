0d89d1a109f3a682f3278cd5bcdcab21184587e4
gitstart-calcom <gitstart@users.noreply.github.com>
2023-07-12 21:28:15 +0000

    Update tests following the requirements

--- packages/ui/components/alert/alert.test.tsx
+++ packages/ui/components/alert/alert.test.tsx
@@ -5,53 +5,52 @@ import { User as UserIcon } from "@calcom/ui/components/icon";
 import { Alert } from "./Alert";
 
 describe("Tests for Alert component", () => {
-  it("Should render text", () => {
+  test("Should render text", () => {
     render(<Alert severity="info" title="I'm an Alert!" message="Hello World" />);
     expect(screen.getByText("I'm an Alert!")).toBeInTheDocument();
     expect(screen.getByText("Hello World")).toBeInTheDocument();
   });
 
-  it("Should have custom class name", () => {
+  test("Should have custom class name", () => {
     render(<Alert severity="info" className="component-class" />);
-    expect(screen.getByTestId("alert").classList.contains("component-class")).toBeTruthy();
+    expect(screen.getByTestId("alert").classList.toString()).toContain("component-class");
   });
 
-  it("Should render custom icon with class names", () => {
-    const { container } = render(
+  test("Should render custom icon with class names", () => {
+    render(
       <Alert severity="info" CustomIcon={UserIcon} customIconColor="icon-color" iconClassName="icon-class" />
     );
 
-    expect(container.querySelector("svg")?.classList.contains("icon-color")).toBeTruthy();
-    expect(container.querySelector("svg")?.classList.contains("icon-class")).toBeTruthy();
-    expect(screen.getByTestId("custom-icon")).toBeInTheDocument();
+    expect(screen.getByTestId("custom-icon")?.classList.toString()).toContain("icon-color");
+    expect(screen.getByTestId("custom-icon")?.classList.toString()).toContain("icon-class");
   });
 
-  it("Should render actions", () => {
+  test("Should render actions", () => {
     render(<Alert severity="info" actions={<button>Click Me</button>} />);
     expect(screen.getByText("Click Me")).toBeInTheDocument();
   });
 
-  it("Should render corresponding icon: error", () => {
+  test("Should render corresponding icon: error", () => {
     render(<Alert severity="error" />);
     expect(screen.getByTestId("x-circle")).toBeInTheDocument();
   });
 
-  it("Should render corresponding icon: warning", () => {
+  test("Should render corresponding icon: warning", () => {
     render(<Alert severity="warning" />);
     expect(screen.getByTestId("alert-triangle")).toBeInTheDocument();
   });
 
-  it("Should render corresponding icon: info", () => {
+  test("Should render corresponding icon: info", () => {
     render(<Alert severity="info" />);
     expect(screen.getByTestId("info")).toBeInTheDocument();
   });
 
-  it("Should render corresponding icon: neutral", () => {
+  test("Should render corresponding icon: neutral", () => {
     render(<Alert severity="neutral" />);
     expect(screen.getByTestId("neutral")).toBeInTheDocument();
   });
 
-  it("Should render corresponding icon: success", () => {
+  test("Should render corresponding icon: success", () => {
     render(<Alert severity="success" />);
     expect(screen.getByTestId("check-circle-2")).toBeInTheDocument();
   });
