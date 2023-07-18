e421a7e2515b65e538784ab09d9c944b37258b44
Syed Ali Shahbaz <52925846+alishaz-polymath@users.noreply.github.com>
2023-07-18 19:08:20 +0530

    Fix function comment

--- packages/lib/date-fns/index.ts
+++ packages/lib/date-fns/index.ts
@@ -31,10 +31,10 @@ export const formatTime = (
 };
 
 /**
- * Checks if a provided timeZone string is recognized as a valid timezone by the Intl API.
+ * Checks if a provided timeZone string is recognized as a valid timezone by dayjs.
  * 
  * @param {string} timeZone - The timezone string to be verified.
- * @returns {boolean} - Returns 'true' if the provided timezone string is recognized as a valid timezone by the Intl API. Otherwise, returns 'false'.
+ * @returns {boolean} - Returns 'true' if the provided timezone string is recognized as a valid timezone by dayjs. Otherwise, returns 'false'.
  * 
  */
 export const isSupportedTimeZone = (timeZone: string) => {
