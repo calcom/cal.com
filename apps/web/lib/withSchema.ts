ed63ff62bbb9334cc3f4f76ed70b7133e2579c6c
Sean Brydon <sean@cal.com>
2023-07-21 12:09:53 +0100

    withSchema validations for form data

---
+++ apps/web/lib/withSchema.ts
@@ -0,0 +1,16 @@
+import type { AnyZodObject, Schema } from "zod";
+import type { z } from "zod";
+
+export function withValidate<T extends AnyZodObject>(action: (data: z.infer<T>) => void, schema: Schema<T>) {
+  return async (formData: FormData) => {
+    // 'use server'
+
+    const parsedSchema = schema.safeParse(formData);
+
+    if (!parsedSchema.success) {
+      throw new Error("Invalid input.");
+    }
+
+    return action(parsedSchema.data);
+  };
+}
