797d3a7f987cd60bac9d35589ba2e5840daf13d8
Nafees Nazik <84864519+G3root@users.noreply.github.com>
2023-07-21 14:18:43 +0530

    fix: set up organization field validation (#10154)
    Co-authored-by: Carina Wollendorfer <30310907+CarinaWolli@users.noreply.github.com>

--- packages/features/ee/organizations/components/CreateANewOrganizationForm.tsx
+++ packages/features/ee/organizations/components/CreateANewOrganizationForm.tsx
@@ -223,8 +223,11 @@ export const CreateANewOrganizationForm = ({ slug }: { slug?: string }) => {
                   defaultValue={value}
                   onChange={(e) => {
                     const domain = extractDomainFromEmail(e?.target.value);
-                    newOrganizationFormMethods.setValue("adminEmail", e?.target.value);
-                    newOrganizationFormMethods.setValue("adminUsername", e?.target.value.split("@")[0]);
+                    newOrganizationFormMethods.setValue("adminEmail", e?.target.value.trim());
+                    newOrganizationFormMethods.setValue(
+                      "adminUsername",
+                      e?.target.value.split("@")[0].trim()
+                    );
                     if (newOrganizationFormMethods.getValues("slug") === "") {
                       newOrganizationFormMethods.setValue("slug", domain);
                     }
@@ -256,7 +259,7 @@ export const CreateANewOrganizationForm = ({ slug }: { slug?: string }) => {
                   label={t("organization_name")}
                   defaultValue={value}
                   onChange={(e) => {
-                    newOrganizationFormMethods.setValue("name", e?.target.value);
+                    newOrganizationFormMethods.setValue("name", e?.target.value.trim());
                     if (newOrganizationFormMethods.formState.touchedFields["slug"] === undefined) {
                       newOrganizationFormMethods.setValue("slug", slugify(e?.target.value));
                     }
