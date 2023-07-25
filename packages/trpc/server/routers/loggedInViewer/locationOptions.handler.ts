d1af0b4cca5b145edd2a121c794bddc8f3883079
Joe Au-Yeung <j.auyeung419@gmail.com>
2023-07-25 15:33:20 -0400

    Refactor where getLocationGroupedOptions is used

--- packages/trpc/server/routers/loggedInViewer/locationOptions.handler.ts
+++ packages/trpc/server/routers/loggedInViewer/locationOptions.handler.ts
@@ -1,8 +1,5 @@
 import { getLocationGroupedOptions } from "@calcom/app-store/utils";
-import getEnabledApps from "@calcom/lib/apps/getEnabledApps";
 import { getTranslation } from "@calcom/lib/server/i18n";
-import { prisma } from "@calcom/prisma";
-import { AppCategories } from "@calcom/prisma/enums";
 import type { TrpcSessionUser } from "@calcom/trpc/server/trpc";
 
 import type { TLocationOptionsInputSchema } from "./locationOptions.schema";
@@ -17,63 +14,9 @@ type LocationOptionsOptions = {
 export const locationOptionsHandler = async ({ ctx, input }: LocationOptionsOptions) => {
   const { teamId } = input;
 
-  let idToSearchObject = {};
-
-  if (teamId) {
-    // See if the team event belongs to an org
-    const org = await prisma.team.findFirst({
-      where: {
-        children: {
-          some: {
-            id: teamId,
-          },
-        },
-      },
-    });
-
-    if (org) {
-      idToSearchObject = {
-        teamId: {
-          in: [teamId, org.id],
-        },
-      };
-    } else {
-      idToSearchObject = { teamId };
-    }
-  } else {
-    idToSearchObject = { userId: ctx.user.id };
-  }
-
-  const credentials = await prisma.credential.findMany({
-    where: {
-      ...idToSearchObject,
-      app: {
-        categories: {
-          hasSome: [AppCategories.conferencing, AppCategories.video],
-        },
-      },
-    },
-    select: {
-      id: true,
-      type: true,
-      key: true,
-      userId: true,
-      teamId: true,
-      appId: true,
-      invalid: true,
-      team: {
-        select: {
-          name: true,
-        },
-      },
-    },
-  });
-
-  const integrations = await getEnabledApps(credentials, true);
-
   const t = await getTranslation(ctx.user.locale ?? "en", "common");
 
-  const locationOptions = getLocationGroupedOptions(integrations, t, true);
+  const locationOptions = await getLocationGroupedOptions(teamId ? { teamId } : { userId: ctx.user.id }, t);
   // If it is a team event then move the "use host location" option to top
   if (input.teamId) {
     const conferencingIndex = locationOptions.findIndex((option) => option.label === "Conferencing");
