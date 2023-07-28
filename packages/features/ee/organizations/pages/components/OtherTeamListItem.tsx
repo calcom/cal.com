23a4d8ec83177ee7452b7ffa2eeda834dbe9d9a0
alannnc <alannnc@gmail.com>
2023-07-28 10:28:04 -0700

    Update packages/features/ee/organizations/pages/components/OtherTeamListItem.tsx
    Co-authored-by: Omar López <zomars@me.com>

--- packages/features/ee/organizations/pages/components/OtherTeamListItem.tsx
+++ packages/features/ee/organizations/pages/components/OtherTeamListItem.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 import type { NewMemberForm } from "@calcom/ee/teams/components/MemberInvitationModal";
