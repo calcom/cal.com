49185863e0379922868e84e95f1a8fd91794fecc
zomars <zomars@me.com>
2023-07-28 15:32:47 -0700

    Update bookingSuccessRedirect.ts

--- packages/lib/bookingSuccessRedirect.ts
+++ packages/lib/bookingSuccessRedirect.ts
@@ -1,6 +1,21 @@
 import type { Booking, EventType } from "@prisma/client";
 import { useRouter, useSearchParams } from "next/navigation";
 
+function getNewSeachParams(args: {
+  query: Record<string, string | null | undefined | boolean>;
+  searchParams?: URLSearchParams;
+}) {
+  const { query, searchParams } = args;
+  const newSearchParams = new URLSearchParams(searchParams);
+  Object.entries(query).forEach(([key, value]) => {
+    if (value === null || value === undefined) {
+      return;
+    }
+    newSearchParams.append(key, String(value));
+  });
+  return newSearchParams;
+}
+
 export const useBookingSuccessRedirect = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -13,20 +28,14 @@ export const useBookingSuccessRedirect = () => {
     bookingUid: Booking["uid"] | undefined;
     query: Record<string, string | null | undefined | boolean>;
   }) => {
-    const newSearchParams = new URLSearchParams(searchParams);
-    Object.entries(query).forEach(([key, value]) => {
-      if (value === null || value === undefined) {
-        return;
-      }
-      newSearchParams.append(key, String(value));
-    });
-
     if (successRedirectUrl) {
       const url = new URL(successRedirectUrl);
       // Using parent ensures, Embed iframe would redirect outside of the iframe.
+      const newSearchParams = getNewSeachParams({ query, searchParams });
       window.parent.location.href = `${url.toString()}?${newSearchParams.toString()}`;
       return;
     }
+    const newSearchParams = getNewSeachParams({ query });
     return router.push(`/booking/${bookingUid}?${newSearchParams.toString()}`);
   };
 
