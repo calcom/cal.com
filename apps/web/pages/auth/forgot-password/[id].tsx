6ed574c4b133e709d3e3dc48016cbb671f74973d
varun thummar <Varun>
2023-07-29 21:39:50 +0530

    added validation for empty reset password field

--- apps/web/pages/auth/forgot-password/[id].tsx
+++ apps/web/pages/auth/forgot-password/[id].tsx
@@ -3,7 +3,7 @@ import { getCsrfToken } from "next-auth/react";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Link from "next/link";
 import type { CSSProperties } from "react";
-import React from "react";
+import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 
 import { useLocale } from "@calcom/lib/hooks/useLocale";
@@ -24,6 +24,8 @@ export default function Page({ requestId, isRequestExpired, csrfToken }: Props) 
   const formMethods = useForm<{ new_password: string }>();
   const success = formMethods.formState.isSubmitSuccessful;
   const loading = formMethods.formState.isSubmitting;
+  const [isEmpty, setIsEmpty] = useState<boolean>(true);
+  const passwordValue = formMethods.watch("new_password");
 
   const submitChangePassword = async ({ password, requestId }: { password: string; requestId: string }) => {
     const res = await fetch("/api/auth/reset-password", {
@@ -74,6 +76,14 @@ export default function Page({ requestId, isRequestExpired, csrfToken }: Props) 
       </>
     );
   };
+  useEffect(() => {
+    // disbale Reset Password button if password input field is empty
+    if (passwordValue) {
+      setIsEmpty(passwordValue.length === 0);
+      return;
+    }
+    setIsEmpty(true);
+  }, [passwordValue]);
 
   return (
     <AuthContainer
@@ -123,7 +133,7 @@ export default function Page({ requestId, isRequestExpired, csrfToken }: Props) 
                 loading={loading}
                 color="primary"
                 type="submit"
-                disabled={loading}
+                disabled={loading || isEmpty}
                 className="w-full justify-center">
                 {t("reset_password")}
               </Button>
