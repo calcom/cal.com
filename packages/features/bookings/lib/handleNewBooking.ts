3e2fabb679b025e77f5cf8bad52200a48ebfa82c
Monto <montodeveloper+7926@gmail.com>
2023-07-29 17:20:52 +0000

    add parameters to dinstinguish first and last attendee names in workflows

--- packages/features/bookings/lib/handleNewBooking.ts
+++ packages/features/bookings/lib/handleNewBooking.ts
@@ -923,6 +923,8 @@ async function handler(
     {
       email: bookerEmail,
       name: fullName,
+      firstName: (typeof bookerName === "object" && (bookerName.firstName as string)) || "",
+      lastName: (typeof bookerName === "object" && (bookerName.lastName as string)) || "",
       timeZone: reqBody.timeZone,
       language: { translate: tAttendees, locale: language ?? "en" },
     },
@@ -936,6 +938,8 @@ async function handler(
     guestArray.push({
       email: guest,
       name: "",
+      firstName: "",
+      lastName: "",
       timeZone: reqBody.timeZone,
       language: { translate: tGuests, locale: "en" },
     });
