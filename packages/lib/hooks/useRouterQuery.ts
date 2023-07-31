1dd6b6631b191c98b965b9dab1b4d3344d6de5c4
Hariom Balhara <hariombalhara@gmail.com>
2023-07-31 10:23:48 +0530

    Dont override multiple query params with same name

--- packages/lib/hooks/useRouterQuery.ts
+++ packages/lib/hooks/useRouterQuery.ts
@@ -1,6 +1,29 @@
 import { useSearchParams } from "next/navigation";
 
 /**
+ * An alternative to Object.fromEntries that allows duplicate keys.
+ */
+function fromEntriesWithDuplicateKeys(entries: ReturnType<ReturnType<typeof useSearchParams>["entries"]>) {
+  const result: Record<string, string | string[]> = {};
+  // Consider setting atleast ES2015 as target
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  for (const [key, value] of entries) {
+    if (result.hasOwnProperty(key)) {
+      let currentValue = result[key];
+      if (!Array.isArray(currentValue)) {
+        currentValue = [currentValue];
+      }
+      currentValue.push(value);
+      result[key] = currentValue;
+    } else {
+      result[key] = value;
+    }
+  }
+  return result;
+}
+
+/**
  * This hook returns the query object from the router. It is an attempt to
  * keep the original query object from the old useRouter hook.
  * At least until everything is properly migrated to the new router.
@@ -8,6 +31,6 @@ import { useSearchParams } from "next/navigation";
  */
 export const useRouterQuery = () => {
   const searchParams = useSearchParams();
-  const routerQuery = Object.fromEntries(searchParams.entries());
+  const routerQuery = fromEntriesWithDuplicateKeys(searchParams.entries());
   return routerQuery;
 };
