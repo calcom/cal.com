9344fe984a3afd49cb9b30a2e51877baa92e21a4
Joe Au-Yeung <j.auyeung419@gmail.com>
2023-07-31 15:38:45 -0400

    Remove category filter for credentials

--- packages/lib/getEventTypeById.ts
+++ packages/lib/getEventTypeById.ts
@@ -10,7 +10,7 @@ import getEnabledApps from "@calcom/lib/apps/getEnabledApps";
 import { CAL_URL } from "@calcom/lib/constants";
 import getPaymentAppData from "@calcom/lib/getPaymentAppData";
 import { getTranslation } from "@calcom/lib/server/i18n";
-import { SchedulingType, MembershipRole, AppCategories } from "@calcom/prisma/enums";
+import { SchedulingType, MembershipRole } from "@calcom/prisma/enums";
 import { customInputSchema, EventTypeMetaDataSchema } from "@calcom/prisma/zod-utils";
 
 import { TRPCError } from "@trpc/server";
@@ -228,9 +228,6 @@ export default async function getEventTypeById({
       userId,
       app: {
         enabled: true,
-        categories: {
-          hasSome: [AppCategories.conferencing, AppCategories.video],
-        },
       },
     },
     select: {
