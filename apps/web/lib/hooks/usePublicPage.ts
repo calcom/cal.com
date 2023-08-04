c073cf1a1142bb87d6d47d5928cb3130f6b03d85
Alan <alannnc@gmail.com>
2023-08-04 14:29:00 -0700

    Fix usePublicPage hook to use new router search params

--- apps/web/lib/hooks/usePublicPage.ts
+++ apps/web/lib/hooks/usePublicPage.ts
@@ -1,9 +1,12 @@
-import { usePathname } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 
 export default function usePublicPage() {
   const pathname = usePathname();
-  const isPublicPage = ["/[user]", "/booking", "/cancel", "/reschedule"].find((route) =>
-    pathname?.startsWith(route)
-  );
-  return isPublicPage;
+  const isPublicPage = ["/booking", "/cancel", "/reschedule"].some((route) => pathname?.startsWith(route));
+
+  const searchParams = useSearchParams();
+  const userParam = searchParams.get("user");
+  const teamParam = searchParams.get("team");
+
+  return !isPublicPage && (userParam || teamParam);
 }
