d97abb8a415e936540544207ab8d8f6bd3490f51
Hariom Balhara <hariombalhara@gmail.com>
2023-08-08 16:32:43 +0530

    Avoid state reset for Shell and thus avoid avatar refresh

--- apps/web/lib/hooks/usePublicPage.ts
+++ apps/web/lib/hooks/usePublicPage.ts
@@ -2,7 +2,7 @@ import { usePathname } from "next/navigation";
 
 export default function usePublicPage() {
   const pathname = usePathname();
-  const isPublicPage = ["/[user]", "/booking", "/cancel", "/reschedule"].find((route) =>
+  const isPublicPage = ["/[user]", "/booking/", "/cancel", "/reschedule"].find((route) =>
     pathname?.startsWith(route)
   );
   return isPublicPage;
