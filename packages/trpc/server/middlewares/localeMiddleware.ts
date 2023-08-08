320bf67c68009e19afcf36f3e1a831a105fb3ee8
Keith Williams <keithwillcode@gmail.com>
2023-08-08 13:37:41 +0100

    Removed tracing from middleware

--- packages/trpc/server/middlewares/localeMiddleware.ts
+++ packages/trpc/server/middlewares/localeMiddleware.ts
@@ -1,25 +1,13 @@
-import { tracer, context } from "@calcom/lib/server/otel-initializer";
-
 import sessionMiddleware from "./sessionMiddleware";
 
 const localeMiddleware = sessionMiddleware.unstable_pipe(async ({ ctx, next }) => {
-  const serverSideTranslationsImportSpan = tracer.startSpan(
-    "serverSideTranslationsImport",
-    undefined,
-    context.active()
-  );
   const { serverSideTranslations } = await import("next-i18next/serverSideTranslations");
-  serverSideTranslationsImportSpan.end();
-
   const { user } = ctx;
 
-  const serverSideTranslationsSpan = tracer.startSpan("serverSideTranslations", undefined, context.active());
   const i18n = await serverSideTranslations(
     user?.locale && user?.locale !== ctx.locale ? user.locale : ctx.locale,
     ["common", "vital"]
   );
-
-  serverSideTranslationsSpan.end();
 
   const locale = user?.locale || ctx.locale;
 
