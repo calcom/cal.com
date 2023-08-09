52eda56e9178efbb78fae6f88bbd84709dd27381
Matthias Nannt <mail@matthiasnannt.com>
2023-08-09 12:40:43 +0200

    move useFormbricks to formbricks lib file

--- packages/features/shell/Shell.tsx
+++ packages/features/shell/Shell.tsx
@@ -22,13 +22,7 @@ import VerifyEmailBanner from "@calcom/features/users/components/VerifyEmailBann
 import classNames from "@calcom/lib/classNames";
 import { APP_NAME, DESKTOP_APP_LINK, JOIN_DISCORD, ROADMAP, WEBAPP_URL } from "@calcom/lib/constants";
 import { getPlaceholderAvatar } from "@calcom/lib/defaultAvatarImage";
-import {
-  logoutFormbricks,
-  initFormbricks,
-  setFormbricksAttribute,
-  setFormbricksEmail,
-  setFormbricksUserId,
-} from "@calcom/lib/formbricks";
+import { logoutFormbricks, useFormbricks } from "@calcom/lib/formbricks";
 import getBrandColours from "@calcom/lib/getBrandColours";
 import { useIsomorphicLayoutEffect } from "@calcom/lib/hooks/useIsomorphicLayoutEffect";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
@@ -170,32 +164,6 @@ function AppTop({ setBannersHeight }: { setBannersHeight: Dispatch<SetStateActio
       <VerifyEmailBanner />
     </div>
   );
-}
-
-function useFormbricks() {
-  const { data: user, isLoading } = useMeQuery();
-  const { data: session, status } = useSession();
-
-  useEffect(() => {
-    if (!isLoading && user && session) {
-      initFormbricks();
-      // set Formbricks attributes for targeted surveys
-      setFormbricksUserId(user.id.toString());
-      setFormbricksEmail(user.email);
-      if (user?.name) {
-        setFormbricksAttribute("name", user.name);
-      }
-      if (user?.username) {
-        setFormbricksAttribute("username", user.username);
-      }
-      if (typeof session?.user?.belongsToActiveTeam !== "undefined") {
-        setFormbricksAttribute("belongsToActiveTeam", session.user.belongsToActiveTeam?.toString());
-      }
-      if (typeof user?.organization?.isOrgAdmin !== "undefined") {
-        setFormbricksAttribute("isOrganizationAdmin", user.organization?.isOrgAdmin.toString());
-      }
-    }
-  }, [isLoading, user, status]);
 }
 
 function useRedirectToOnboardingIfNeeded() {
