9b4a3692cca410b6ffe73698bedeea33b85a06c6
nicktrn <55853254+nicktrn@users.noreply.github.com>
2023-08-09 08:55:01 +0000

    feat: add support for apple travel time

--- packages/lib/CalendarService.ts
+++ packages/lib/CalendarService.ts
@@ -60,6 +60,29 @@ function getFileExtension(url: string): string {
   return fileName.substring(fileName.lastIndexOf(".") + 1);
 }
 
+const getTravelDurationInSeconds = (vevent: ICAL.Component, log: typeof logger) => {
+  const travelDuration: ICAL.Duration = vevent.getFirstPropertyValue("x-apple-travel-duration");
+  if (!travelDuration) return 0;
+
+  // we can't rely on this being a valid duration and it's painful to check, so just try and catch if anything throws
+  try {
+    const travelSeconds = travelDuration.toSeconds();
+    // integer validation as we can never be sure with ical.js
+    if (!Number.isInteger(travelSeconds)) return 0;
+    return travelSeconds;
+  } catch (e) {
+    log.error("[ERROR] invalid travelDuration?", e);
+    return 0;
+  }
+};
+
+const applyTravelDuration = (event: ICAL.Event, seconds: number) => {
+  if (seconds <= 0) return event;
+  event.startDate.second -= seconds;
+  event.endDate.second += seconds;
+  return event;
+};
+
 const convertDate = (date: string): DateArray =>
   dayjs(date)
     .utc()
@@ -386,6 +409,9 @@ export default abstract class BaseCalendarService implements Calendar {
           }
         }
         const vtimezone = vcalendar.getFirstSubcomponent("vtimezone");
+
+        // mutate event to add travel time either side
+        applyTravelDuration(event, getTravelDurationInSeconds(vevent, this.log));
 
         if (event.isRecurring()) {
           let maxIterations = 365;
