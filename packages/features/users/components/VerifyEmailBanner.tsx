16cd5b29161f5ecd89608257438d912c5da5492a
Carina Wollendorfer <30310907+CarinaWolli@users.noreply.github.com>
2023-08-09 15:37:25 -0400

    remove console.log

--- packages/features/users/components/VerifyEmailBanner.tsx
+++ packages/features/users/components/VerifyEmailBanner.tsx
@@ -17,7 +17,6 @@ function VerifyEmailBanner() {
   const session = useSession();
   const isLoggedIn = session?.data?.user;
 
-  console.log("isLoggedIn", !isLoggedIn);
 
   if (!isLoggedIn || isLoading || data?.isVerified || !flags["email-verification"]) return null;
 
