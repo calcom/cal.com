3e5170b6227036d4ef7d9a69729a448e8cf82d80
Hariom Balhara <hariombalhara@gmail.com>
2023-08-11 13:32:42 +0530

    Fix the root cause

--- packages/features/bookings/lib/getLocationOptionsForSelect.ts
+++ packages/features/bookings/lib/getLocationOptionsForSelect.ts
@@ -1,11 +1,8 @@
 import type { LocationObject } from "@calcom/app-store/locations";
-import { DefaultEventLocationTypeEnum } from "@calcom/app-store/locations";
 import { locationKeyToString } from "@calcom/app-store/locations";
 import { getEventLocationType } from "@calcom/app-store/locations";
 import type { useLocale } from "@calcom/lib/hooks/useLocale";
 import notEmpty from "@calcom/lib/notEmpty";
-
-import type I18nKeys from "../../../../apps/web/public/static/locales/en/common.json";
 
 export default function getLocationsOptionsForSelect(
   locations: LocationObject[],
@@ -22,19 +19,12 @@ export default function getLocationsOptionsForSelect(
       }
       const type = eventLocation.type;
 
-      // to prevent escaping of "http" when the link is displayed in the booking page
-      const i18nKey: keyof typeof I18nKeys = "link_meeting";
-      const shouldLinkDisplayedPublicly =
-        type === DefaultEventLocationTypeEnum.Link && locationString !== i18nKey;
-
       return {
-        label: shouldLinkDisplayedPublicly ? locationString : t(locationString),
+        // XYZ: is considered a namespace in i18next https://www.i18next.com/principles/namespaces and thus it get's cleaned up.
+        // Beacause there can be a URL in here, simply don't translate it if it starts with http: or https:. This would allow us to keep supporting namespaces if we plan to use them
+        label: locationString.search(/^https?:/) !== -1 ? locationString : t(locationString),
         value: type,
-        inputPlaceholder: t(eventLocation?.attendeeInputPlaceholder || "", {
-          interpolation: {
-            escapeValue: false,
-          },
-        }),
+        inputPlaceholder: t(eventLocation?.attendeeInputPlaceholder || ""),
       };
     })
     .filter(notEmpty);
