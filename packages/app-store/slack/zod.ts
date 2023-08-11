08f0a266272e0b8741824c63884ead060d753664
rkreddy99 <rreddy@e2clouds.com>
2023-08-11 20:16:59 +0530

    fix: type

--- packages/app-store/slack/zod.ts
+++ packages/app-store/slack/zod.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
 
+export const appDataSchema = z.object({});
+
 export const appKeysSchema = z.object({
   client_id: z.string().min(1),
   client_secret: z.string().min(1),
