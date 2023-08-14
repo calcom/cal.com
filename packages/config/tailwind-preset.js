1f0615b32e3b5c9960902f94d254f8abd00e5eb7
Peer Richelsen <peer@cal.com>
2023-08-14 08:42:12 +0200

    Revert "chore: remove tailwind-scrollbar warning (#10523)"
    This reverts commit e72c25f02e30e20e3c6beec5946641c7f2c74456.

--- packages/config/tailwind-preset.js
+++ packages/config/tailwind-preset.js
@@ -151,7 +151,7 @@ module.exports = {
   plugins: [
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
-    require("tailwind-scrollbar")({ nocompatible: true }),
+    require("tailwind-scrollbar"),
     require("tailwindcss-radix")(),
     require("@savvywombat/tailwindcss-grid-areas"),
     plugin(({ addVariant }) => {
@@ -168,6 +168,6 @@ module.exports = {
     }),
   ],
   variants: {
-    scrollbar: ["dark"],
+    scrollbar: ["rounded", "dark"],
   },
 };
