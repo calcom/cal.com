1d876f74980f9f148aae4fce187c8dadcf9b67b6
Kavinkumar <kavinkumar1543884@gmail.com>
2023-08-18 01:05:19 +0530

    Fix: typo for TeamAppearanceView

--- apps/web/pages/settings/organizations/teams/other/[id]/appearance.tsx
+++ apps/web/pages/settings/organizations/teams/other/[id]/appearance.tsx
@@ -1,9 +1,9 @@
-import TeamAppearenceView from "@calcom/features/ee/teams/pages/team-appearance-view";
+import TeamAppearanceView from "@calcom/features/ee/teams/pages/team-appearance-view";
 
 import type { CalPageWrapper } from "@components/PageWrapper";
 import PageWrapper from "@components/PageWrapper";
 
-const Page = TeamAppearenceView as CalPageWrapper;
+const Page = TeamAppearanceView as CalPageWrapper;
 Page.PageWrapper = PageWrapper;
 
 export default Page;
