ec9460605fc3dfaf4d27858d6b35543dc1c99319
Kavinkumar <kavinkumar1543884@gmail.com>
2023-08-18 11:21:44 +0530

    Fix: typo verifying

--- packages/features/ee/api-keys/lib/apiKeys.ts
+++ packages/features/ee/api-keys/lib/apiKeys.ts
@@ -1,6 +1,6 @@
 import { randomBytes, createHash } from "crypto";
 
-// Hash the API key to check against when veriying it. so we don't have to store the key in plain text.
+// Hash the API key to check against when verifying it. so we don't have to store the key in plain text.
 export const hashAPIKey = (apiKey: string): string => createHash("sha256").update(apiKey).digest("hex");
 
 // Generate a random API key. Prisma already makes sure it's unique. So no need to add salts like with passwords.
