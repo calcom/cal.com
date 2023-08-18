58402333595dceef979d12b6dbb39307da10e02a
Thibault Mathian <dev@thibault.sh>
2023-08-18 14:56:49 +0200

    fix: features query use placeholderData

--- packages/features/flags/hooks/index.ts
+++ packages/features/flags/hooks/index.ts
@@ -4,7 +4,8 @@ export function useFlags() {
   const query = trpc.viewer.features.map.useQuery(undefined, {
     initialData: process.env.NEXT_PUBLIC_IS_E2E
       ? { "managed-event-types": true, organizations: true, teams: true }
-      : {},
+      : undefined,
+    placeholderData: {},
   });
   return query.data;
 }
