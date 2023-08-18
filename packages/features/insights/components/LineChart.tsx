3db4e8c926392680fc994f5be8f561d8318c7963
Kavinkumar <kavinkumar1543884@gmail.com>
2023-08-18 11:12:45 +0530

    Fix: typo existence

--- packages/features/insights/components/LineChart.tsx
+++ packages/features/insights/components/LineChart.tsx
@@ -2,7 +2,7 @@ import { LineChart as ExternalLineChart } from "@tremor/react";
 
 import type { LineChartProps } from "./tremor.types";
 
-// Honestly this is a mess. Why are all chart libraries in existance horrible to theme
+// Honestly this is a mess. Why are all chart libraries in existence horrible to theme
 export const LineChart = (props: LineChartProps) => {
   return (
     <div className="dark:invert">
