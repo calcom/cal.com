83d543084f6df8aa2d41ed69a63989cbd5a3325d
Kavinkumar <kavinkumar1543884@gmail.com>
2023-08-18 09:16:52 +0530

    Fix: typo for omitting

--- apps/api/lib/validations/user.ts
+++ apps/api/lib/validations/user.ts
@@ -107,7 +107,7 @@ const schemaUserEditParams = z.object({
 });
 
 // @note: These are the values that are editable via PATCH method on the user Model,
-// merging both BaseBodyParams with RequiredParams, and omiting whatever we want at the end.
+// merging both BaseBodyParams with RequiredParams, and omitting whatever we want at the end.
 
 const schemaUserCreateParams = z.object({
   email: z.string().email(),
@@ -128,7 +128,7 @@ const schemaUserCreateParams = z.object({
 });
 
 // @note: These are the values that are editable via PATCH method on the user Model,
-// merging both BaseBodyParams with RequiredParams, and omiting whatever we want at the end.
+// merging both BaseBodyParams with RequiredParams, and omitting whatever we want at the end.
 export const schemaUserEditBodyParams = schemaUserBaseBodyParams
   .merge(schemaUserEditParams)
   .omit({})
