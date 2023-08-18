795dce40da7bfe7ef007e91a54e349ea84927a86
Kavinkumar <kavinkumar1543884@gmail.com>
2023-08-18 17:21:28 +0530

    Fix: typo Form

--- packages/features/bookings/lib/getBookingResponsesSchema.ts
+++ packages/features/bookings/lib/getBookingResponsesSchema.ts
@@ -72,7 +72,7 @@ function preprocess<T extends z.ZodType>({
           return;
         }
         const fieldTypeSchema = fieldTypesSchemaMap[field.type as keyof typeof fieldTypesSchemaMap];
-        // TODO: Move all the schemas along with their respective types to fieldTypeSchema, that would make schemas shared across Routing Forms builder and Booking Question Formm builder
+        // TODO: Move all the schemas along with their respective types to fieldTypeSchema, that would make schemas shared across Routing Forms builder and Booking Question Form builder
         if (fieldTypeSchema) {
           newResponses[field.name] = fieldTypeSchema.preprocess({
             response: value,
