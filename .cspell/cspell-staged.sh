c570eafc5ae449056d5b65cf23338b6eff210684
Kavinkumar <kavinkumar1543884@gmail.com>
2023-08-18 22:23:30 +0530

    Add flag to avoid error if no files are found for cspell

--- .cspell/cspell-staged.sh
+++ .cspell/cspell-staged.sh
@@ -4,7 +4,7 @@
 staged_files=$(git diff --name-only --relative --cached)
 
 if [ -n "$staged_files" ]; then
-    echo "$staged_files" | cspell --file-list stdin --config .cspell/cspell.json --no-progress --no-summary --show-suggestions
+    echo "$staged_files" | cspell --file-list stdin --config .cspell/cspell.json --no-progress --no-summary --show-suggestions --no-must-find-files
 
     if [ $? -ne 0 ]; then
         echo
