5993f8f3b47f8e620f4c82392948604081423122
Kavinkumar <kavinkumar1543884@gmail.com>
2023-08-18 17:23:46 +0530

    Fix: typo Rounds

--- packages/ui/components/buttonGroup/ButtonGroup.tsx
+++ packages/ui/components/buttonGroup/ButtonGroup.tsx
@@ -8,7 +8,7 @@ type Props = { children: React.ReactNode; combined?: boolean; containerProps?: J
  * Breakdown of Tailwind Magic below
  * [&_button]:border-l-0 [&_a]:border-l-0 -> Selects all buttons/a tags and applies a border left of 0
  * [&>*:first-child]:rounded-l-md [&>*:first-child]:border-l -> Selects the first child of the content
- * ounds the left side
+ * Rounds the left side
  * [&>*:last-child]:rounded-r-md -> Selects the last child of the content and rounds the right side
  * We dont need to add border to the right as we never remove it
  */
