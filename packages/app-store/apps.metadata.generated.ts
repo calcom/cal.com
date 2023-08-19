f80e9b255817461808ceba6aadb7f5b2eb85b16a
Jon@1599 <55296387+Jonathan1599@users.noreply.github.com>
2023-08-20 01:34:56 +0530

    feat: add basecamp integration to cal.com (#9195)
    * feat: installing the app works
    
    * Update yarn.lock
    
    * feat: /api/callback now gets user auth info from basecamp
    
    * feat: updated basecamp logo
    
    * feat: added project dropdown on event apps page
    
    * feat: basecamp event creation and deletion working
    
    * feat: basecamp event rescheduling now works
    
    * refactor(CalendarService): basecamp CaldendarService code clean up
    
    * refactor: code cleanup for basecamp app API
    
    * feat: updated event summary text sent to basecamp
    
    * chore: updated basecamp images and contact info
    
    * fix: fixed typescript errors and added logic to refresh tokens on event settings
    
    * refactor(CaldendarService): used refreshAccessToken from helpers.ts instead
    
    * chore: updated basecamp description
    
    * fix: fixed incorrect import
    
    * fix: accidentally deleted props to toggle app for event
    
    * chore: updated .env.appStore.example and added README for app
    
    * Update .env.appStore.example
    
    Co-authored-by: Leo Giovanetti <hello@leog.me>
    
    * feat: added basecamp userAgent in env instead of hardcoded value
    
    * feat: updated README to include how to set basecamp user agent env
    
    * fix: removed unused import
    
    * feat: used URLSearchParams to construct url params
    
    * fix: fixed typescript errors
    
    * chore: updated README to include an example on how to set basecamp user-agent
    
    * feat: using TRPC instead of REST
    
    * chore: removed old projects REST code
    
    ---------
    
    Co-authored-by: Leo Giovanetti <hello@leog.me>

--- packages/app-store/apps.metadata.generated.ts
+++ packages/app-store/apps.metadata.generated.ts
@@ -5,6 +5,7 @@
 import amie_config_json from "./amie/config.json";
 import { metadata as applecalendar__metadata_ts } from "./applecalendar/_metadata";
 import around_config_json from "./around/config.json";
+import basecamp3_config_json from "./basecamp3/config.json";
 import { metadata as caldavcalendar__metadata_ts } from "./caldavcalendar/_metadata";
 import campfire_config_json from "./campfire/config.json";
 import closecom_config_json from "./closecom/config.json";
@@ -73,6 +74,7 @@ export const appStoreMetadata = {
   amie: amie_config_json,
   applecalendar: applecalendar__metadata_ts,
   around: around_config_json,
+  basecamp3: basecamp3_config_json,
   caldavcalendar: caldavcalendar__metadata_ts,
   campfire: campfire_config_json,
   closecom: closecom_config_json,
---
+++ packages/app-store/basecamp3/trpc-router.ts
@@ -0,0 +1,1 @@
+export { default } from "./trpc/_router";
