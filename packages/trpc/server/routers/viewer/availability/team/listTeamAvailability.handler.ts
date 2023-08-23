7a063391f3ca40a9fe264f46c7d923759a845017
Sean Brydon <sean@cal.com>
2023-08-23 10:18:36 +0100

    Enable for teams -> refactor query

--- packages/trpc/server/routers/viewer/availability/team/listTeamAvailability.handler.ts
+++ packages/trpc/server/routers/viewer/availability/team/listTeamAvailability.handler.ts
@@ -1,3 +1,4 @@
+import type { Dayjs } from "@calcom/dayjs";
 import dayjs from "@calcom/dayjs";
 import type { DateRange } from "@calcom/lib/date-ranges";
 import { buildDateRanges } from "@calcom/lib/date-ranges";
@@ -15,25 +16,18 @@ type GetOptions = {
   input: TListTeamAvailaiblityScheme;
 };
 
-export const listTeamAvailabilityHandler = async ({ ctx, input }: GetOptions) => {
-  const organizationId = ctx.user.organizationId;
-
-  if (!organizationId) {
-    throw new TRPCError({ code: "NOT_FOUND", message: "User is not part of any organization." });
-  }
-
-  const { cursor, limit } = input;
-
-  const getTotalMembers = await prisma.membership.count({
+async function getTeamMembers({
+  teamId,
+  cursor,
+  limit,
+}: {
+  teamId: number;
+  cursor: number | null | undefined;
+  limit: number;
+}) {
+  return await prisma.membership.findMany({
     where: {
-      teamId: organizationId,
-    },
-  });
-
-  // I couldnt get this query to work direct on membership table
-  const teamMembers = await prisma.membership.findMany({
-    where: {
-      teamId: organizationId,
+      teamId,
       accepted: true,
     },
     select: {
@@ -55,6 +49,79 @@ export const listTeamAvailabilityHandler = async ({ ctx, input }: GetOptions) =>
       id: "asc",
     },
   });
+}
+
+type Member = Awaited<ReturnType<typeof getTeamMembers>>[number];
+
+async function buildMember(member: Member, dateFrom: Dayjs, dateTo: Dayjs) {
+  if (!member.user.defaultScheduleId) {
+    return {
+      id: member.user.id,
+      username: member.user.username,
+      email: member.user.email,
+      timeZone: member.user.timeZone,
+      role: member.role,
+      dateRanges: [] as DateRange[],
+    };
+  }
+
+  const schedule = await prisma.schedule.findUnique({
+    where: { id: member.user.defaultScheduleId },
+    select: { availability: true, timeZone: true },
+  });
+  const timeZone = schedule?.timeZone || member.user.timeZone;
+
+  const dateRanges = buildDateRanges({
+    dateFrom,
+    dateTo,
+    timeZone,
+    availability: schedule?.availability ?? [],
+  });
+
+  return {
+    id: member.user.id,
+    username: member.user.username,
+    email: member.user.email,
+    timeZone,
+    role: member.role,
+    dateRanges,
+  };
+}
+
+export const listTeamAvailabilityHandler = async ({ ctx, input }: GetOptions) => {
+  const teamId = input.teamId || ctx.user.organizationId;
+
+  if (!teamId) {
+    // Get all users TODO:
+    throw new TRPCError({ code: "NOT_FOUND", message: "User is not part of any organization or team." });
+  }
+
+  // check if user is part of team
+  const isMember = await prisma.membership.findFirst({
+    where: {
+      teamId,
+      userId: ctx.user.id,
+    },
+  });
+
+  if (!isMember) {
+    throw new TRPCError({ code: "NOT_FOUND", message: "User is not part of any organization or team." });
+  }
+
+  const { cursor, limit } = input;
+
+  const getTotalMembers = await prisma.membership.count({
+    where: {
+      teamId: teamId,
+    },
+  });
+
+  // I couldnt get this query to work direct on membership table
+  const teamMembers = await getTeamMembers({
+    teamId,
+    cursor,
+    limit,
+  });
 
   let nextCursor: typeof cursor | undefined = undefined;
   if (teamMembers && teamMembers.length > limit) {
@@ -65,40 +132,7 @@ export const listTeamAvailabilityHandler = async ({ ctx, input }: GetOptions) =>
   const dateFrom = dayjs(input.startDate).tz(input.loggedInUsersTz).subtract(1, "day");
   const dateTo = dayjs(input.endDate).tz(input.loggedInUsersTz).add(1, "day");
 
-  const buildMembers = teamMembers?.map(async (member) => {
-    if (!member.user.defaultScheduleId) {
-      return {
-        id: member.user.id,
-        username: member.user.username,
-        email: member.user.email,
-        timeZone: member.user.timeZone,
-        role: member.role,
-        dateRanges: [] as DateRange[],
-      };
-    }
-
-    const schedule = await prisma.schedule.findUnique({
-      where: { id: member.user.defaultScheduleId },
-      select: { availability: true, timeZone: true },
-    });
-    const timeZone = schedule?.timeZone || member.user.timeZone;
-
-    const dateRanges = buildDateRanges({
-      dateFrom,
-      dateTo,
-      timeZone,
-      availability: schedule?.availability ?? [],
-    });
-
-    return {
-      id: member.user.id,
-      username: member.user.username,
-      email: member.user.email,
-      timeZone,
-      role: member.role,
-      dateRanges,
-    };
-  });
+  const buildMembers = teamMembers?.map((member) => buildMember(member, dateFrom, dateTo));
 
   const members = await Promise.all(buildMembers);
 
