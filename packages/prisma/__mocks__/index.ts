9bbec71e17b367c570e05b2225b4b69d32676c6e
Joe Au-Yeung <j.auyeung419@gmail.com>
2023-08-22 21:46:04 -0400

    Create mock prisma client

---
+++ packages/prisma/__mocks__/index.ts
@@ -0,0 +1,11 @@
+import type { PrismaClient } from "@prisma/client";
+import { beforeEach } from "vitest";
+import { mockDeep, mockReset } from "vitest-mock-extended";
+
+beforeEach(() => {
+  mockReset(prisma);
+});
+
+const prisma = mockDeep<PrismaClient>();
+
+export default prisma;
