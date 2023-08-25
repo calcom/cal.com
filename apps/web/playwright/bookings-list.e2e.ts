5a0c980aa18dfa2852ed276bd6c621aafeb2c929
Hariom Balhara <hariombalhara@gmail.com>
2023-08-25 13:54:31 +0530

    Text change

--- apps/web/playwright/bookings-list.e2e.ts
+++ apps/web/playwright/bookings-list.e2e.ts
@@ -8,57 +8,57 @@ import { test } from "./lib/fixtures";
 test.afterEach(({ users }) => users.deleteAll());
 
 test.describe("Bookings", () => {
-  test.only("Upcoming bookings", async ({ page, users, bookings }) => {
-    const firstUser = await users.create();
-    const secondUser = await users.create();
-    const firstEventType = firstUser.eventTypes[0];
+  test.describe("Upcoming bookings", () => {
+    test("show attendee bookings and organizer bookings in asc order by startDate", async ({
+      page,
+      users,
+      bookings,
+    }) => {
+      const firstUser = await users.create();
+      const secondUser = await users.create();
 
-    const bookingWhereFirstUserIsOrganizerFixture = await createBooking({
-      title: "Booking as organizer",
-      bookingsFixture: bookings,
-      relativeDate: 3,
-      organizer: firstUser,
-      organizerEventType: firstUser.eventTypes[0],
-      attendees: [
-        { name: "First", email: "first@cal.com", timeZone: "Europe/Berlin" },
-        { name: "Second", email: "second@cal.com", timeZone: "Europe/Berlin" },
-        { name: "Third", email: "third@cal.com", timeZone: "Europe/Berlin" },
-      ],
+      const bookingWhereFirstUserIsOrganizerFixture = await createBooking({
+        title: "Booking as organizer",
+        bookingsFixture: bookings,
+        relativeDate: 3,
+        organizer: firstUser,
+        organizerEventType: firstUser.eventTypes[0],
+        attendees: [
+          { name: "First", email: "first@cal.com", timeZone: "Europe/Berlin" },
+          { name: "Second", email: "second@cal.com", timeZone: "Europe/Berlin" },
+          { name: "Third", email: "third@cal.com", timeZone: "Europe/Berlin" },
+        ],
+      });
+      const bookingWhereFirstUserIsOrganizer = await bookingWhereFirstUserIsOrganizerFixture.self();
+
+      const bookingWhereFirstUserIsAttendeeFixture = await createBooking({
+        title: "Booking as attendee",
+        bookingsFixture: bookings,
+        organizer: secondUser,
+        relativeDate: 2,
+        organizerEventType: secondUser.eventTypes[0],
+        attendees: [
+          { name: "OrganizerAsBooker", email: firstUser.email, timeZone: "Europe/Berlin" },
+          { name: "Second", email: "second@cal.com", timeZone: "Europe/Berlin" },
+          { name: "Third", email: "third@cal.com", timeZone: "Europe/Berlin" },
+        ],
+      });
+      const bookingWhereFirstUserIsAttendee = await bookingWhereFirstUserIsAttendeeFixture.self();
+
+      await firstUser.apiLogin();
+      await page.goto(`/bookings/upcoming`);
+      const upcomingBookings = page.locator('[data-testid="upcoming-bookings"]');
+      const firstUpcomingBooking = upcomingBookings.locator('[data-testid="booking-item"]').nth(0);
+      const secondUpcomingBooking = upcomingBookings.locator('[data-testid="booking-item"]').nth(1);
+      await expect(
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        firstUpcomingBooking.locator(`text=${bookingWhereFirstUserIsAttendee!.title}`)
+      ).toBeVisible();
+      await expect(
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        secondUpcomingBooking.locator(`text=${bookingWhereFirstUserIsOrganizer!.title}`)
+      ).toBeVisible();
     });
-    const bookingWhereFirstUserIsOrganizer = await bookingWhereFirstUserIsOrganizerFixture.self();
-
-    const bookingWhereFirstUserIsAttendeeFixture = await createBooking({
-      title: "Booking as attendee",
-      bookingsFixture: bookings,
-      organizer: secondUser,
-      relativeDate: 2,
-      organizerEventType: secondUser.eventTypes[0],
-      attendees: [
-        { name: "OrganizerAsBooker", email: firstUser.email, timeZone: "Europe/Berlin" },
-        { name: "Second", email: "second@cal.com", timeZone: "Europe/Berlin" },
-        { name: "Third", email: "third@cal.com", timeZone: "Europe/Berlin" },
-      ],
-    });
-    const bookingWhereFirstUserIsAttendee = await bookingWhereFirstUserIsAttendeeFixture.self();
-
-    await firstUser.apiLogin();
-    await page.goto(`/bookings/upcoming`);
-    const firstUpcomingBooking = page
-      .locator('[data-testid="upcoming-bookings"] [data-testid="booking-item"]')
-      .nth(0);
-    const secondUpcomingBooking = page
-      .locator('[data-testid="upcoming-bookings"] [data-testid="booking-item"]')
-      .nth(1);
-    await expect(
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      firstUpcomingBooking.locator(`text=${bookingWhereFirstUserIsAttendee!.title}`)
-    ).toBeVisible();
-    await expect(
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      secondUpcomingBooking.locator(`text=${bookingWhereFirstUserIsOrganizer!.title}`)
-    ).toBeVisible();
-
-    await page.pause();
   });
 });
 
