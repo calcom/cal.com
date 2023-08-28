27dd437eec8d6ad9b8ce86e4921f9cc21d050afd
tedspare <ted.spare@gmail.com>
2023-08-27 21:59:24 -0400

    Add comment and error-catching to agent route

--- apps/ai/src/app/api/agent/route.ts
+++ apps/ai/src/app/api/agent/route.ts
@@ -4,17 +4,34 @@ import { NextResponse } from "next/server";
 import agent from "../../../utils/agent";
 import sendEmail from "../../../utils/sendEmail";
 
+/**
+ * Launches a LangChain agent to process an incoming email,
+ * then sends the response to the user.
+ */
 export const POST = async (request: NextRequest) => {
   const json = await request.json();
 
-  const message = await agent(`${json.subject}\n\n${json.message}`, json.user);
+  const { message, subject, user } = json;
 
-  await sendEmail({
-    html: message,
-    subject: `Re: ${json.subject}`,
-    text: message,
-    to: json.user.email,
-  });
+  if (!message || !subject || !user) {
+    return new NextResponse("Missing fields", { status: 400 });
+  }
 
-  return new NextResponse("ok");
+  try {
+    const response = await agent(`${subject}\n\n${message}`, user);
+
+    await sendEmail({
+      html: response,
+      subject: `Re: ${subject}`,
+      text: response,
+      to: user.email,
+    });
+
+    return new NextResponse("ok");
+  } catch (error) {
+    return new NextResponse(
+      error.message || "Something went wrong. Please try again or reach out for help.",
+      { status: 500 }
+    );
+  }
 };
