3b32d59ec334ee1499b8f2cc8beeb952c21858a4
Ted Spare <ted.spare@gmail.com>
2023-08-29 19:16:24 -0400

    Update apps/ai/src/utils/verifyParseKey.ts
    Co-authored-by: nicktrn <55853254+nicktrn@users.noreply.github.com>

--- apps/ai/src/utils/verifyParseKey.ts
+++ apps/ai/src/utils/verifyParseKey.ts
@@ -4,7 +4,7 @@ import { env } from "../env.mjs";
 
 /**
  * Verifies that the request contains the correct parse key.
- * env.CAL_AI_KEY must be configured as a query param in the sendgrid inbound parse settings.
+ * env.PARSE_KEY must be configured as a query param in the sendgrid inbound parse settings.
  */
 export const verifyParseKey = (url: NextRequest["url"]) => {
   const verified = new URL(url).searchParams.get("parseKey") === env.PARSE_KEY;
