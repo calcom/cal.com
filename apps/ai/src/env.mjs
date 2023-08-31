8bf62643e06f65b55a1b7c88f504beab91e16a33
DexterStorey <36115192+DexterStorey@users.noreply.github.com>
2023-08-31 15:10:59 -0400

    resolve zod versions across child packages (#11052)

--- apps/ai/src/env.mjs
+++ apps/ai/src/env.mjs
@@ -23,6 +23,7 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    DATABASE_URL: process.env.DATABASE_URL,
   },
 
   /**
@@ -37,5 +38,6 @@ export const env = createEnv({
     NODE_ENV: z.enum(["development", "test", "production"]),
     OPENAI_API_KEY: z.string().min(1),
     SENDGRID_API_KEY: z.string().min(1),
+    DATABASE_URL: z.string().url(),
   },
 });
