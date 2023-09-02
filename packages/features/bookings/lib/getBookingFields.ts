5af854b64d81f15909047042890fbd7e33f1f351
Venkat Manoj Gowra <manojgowra24@gmail.com>
2023-09-02 18:14:57 +0530

    Fix Add_Guest_Placeholder
    Signed-off-by: Venkat Manoj Gowra <manojgowra24@gmail.com>

--- packages/features/bookings/lib/getBookingFields.ts
+++ packages/features/bookings/lib/getBookingFields.ts
@@ -251,6 +251,7 @@ export const ensureBookingInputsHaveSystemFields = ({
       type: "multiemail",
       editable: "system-but-optional",
       name: "guests",
+      defaultPlaceholder: "Email",
       required: false,
       hidden: disableGuests,
       sources: [
