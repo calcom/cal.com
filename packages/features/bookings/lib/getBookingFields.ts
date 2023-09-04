d122ce4f83d02a755c77e23ab52a1fbf66804b1f
Alan <alannnc@gmail.com>
2023-09-04 08:59:04 -0700

    fixes

--- packages/features/bookings/lib/getBookingFields.ts
+++ packages/features/bookings/lib/getBookingFields.ts
@@ -251,7 +251,7 @@ export const ensureBookingInputsHaveSystemFields = ({
       type: "multiemail",
       editable: "system-but-optional",
       name: "guests",
-      defaultPlaceholder: "enter_address",
+      defaultPlaceholder: "email",
       required: false,
       hidden: disableGuests,
       sources: [
