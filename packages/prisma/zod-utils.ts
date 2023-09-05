5f5b79139cf6f24b43d7ac4daceaf58ea8a3a06b
ABertaud <arthur.bertaud44@gmail.com>
2023-09-05 23:35:11 +0200

    add allTeamMembers to prisma props

--- packages/prisma/zod-utils.ts
+++ packages/prisma/zod-utils.ts
@@ -588,6 +588,7 @@ export const allManagedEventTypeProps: { [k in keyof Omit<Prisma.EventTypeSelect
   workflows: true,
   bookingFields: true,
   durationLimits: true,
+  allTeamMembers: true,
 };
 
 // All properties that are defined as unlocked based on all managed props
