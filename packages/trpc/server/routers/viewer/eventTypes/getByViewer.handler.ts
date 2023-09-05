9e5fb3aa3ec8573d7ac8519a9a86462e6d13477e
Alan <alannnc@gmail.com>
2023-09-05 13:29:04 -0700

    remove unused queries on user event types

--- packages/trpc/server/routers/viewer/eventTypes/getByViewer.handler.ts
+++ packages/trpc/server/routers/viewer/eventTypes/getByViewer.handler.ts
@@ -52,14 +52,6 @@ const userEventTypeSelect = Prisma.validator<Prisma.EventTypeSelect>()({
   users: {
     select: userSelect,
   },
-  // Temporarily putting this back for testing
-  children: {
-    include: {
-      users: {
-        select: userSelect,
-      },
-    },
-  },
   parentId: true,
   hosts: {
     select: {
@@ -74,13 +66,13 @@ const userEventTypeSelect = Prisma.validator<Prisma.EventTypeSelect>()({
 
 const teamEventTypeSelect = Prisma.validator<Prisma.EventTypeSelect>()({
   ...userEventTypeSelect,
-  // children: {
-  //   include: {
-  //     users: {
-  //       select: userSelect,
-  //     },
-  //   },
-  // },
+  children: {
+    include: {
+      users: {
+        select: userSelect,
+      },
+    },
+  },
 });
 
 export const compareMembership = (mship1: MembershipRole, mship2: MembershipRole) => {
