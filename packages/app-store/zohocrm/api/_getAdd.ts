0cdab1602c9d94227f2e53576d2c36ed46cc1b57
Joe Au-Yeung <j.auyeung419@gmail.com>
2023-09-05 17:25:44 -0400

    Clean up

--- packages/app-store/zohocrm/api/_getAdd.ts
+++ packages/app-store/zohocrm/api/_getAdd.ts
@@ -10,7 +10,6 @@ let client_id = "";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const appKeys = await getAppKeysFromSlug("zohocrm");
-  console.log("🚀 ~ file: _getAdd.ts:12 ~ handler ~ appKeys:", appKeys);
   if (typeof appKeys.client_id === "string") client_id = appKeys.client_id;
   if (!client_id) return res.status(400).json({ message: "zohocrm client id missing." });
   const state = encodeOAuthState(req);
@@ -24,14 +23,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     state,
     prompt: "consent",
   };
-  console.log("🚀 ~", WEBAPP_URL + "/api/integrations/zohocrm/callback");
 
-  // const redirectUri = WEBAPP_URL + "/api/integrations/zohocrm/callback";
-  // const url = `https://accounts.zoho.com/oauth/v2/auth?scope=ZohoCRM.modules.ALL,ZohoCRM.users.READ,AaaServer.profile.READ&client_id=${client_id}&response_type=code&access_type=offline&redirect_uri=${redirectUri}&state=${state}&prompt=consent`;
   const query = stringify(params);
   const url = `https://accounts.zoho.com/oauth/v2/auth?${query}`;
-
-  console.log("🚀 ~ file: _getAdd.ts:20 ~ handler ~ url:", url);
 
   res.status(200).json({ url });
 }
