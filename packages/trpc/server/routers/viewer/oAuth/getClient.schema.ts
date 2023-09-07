9b52d299fd68489891eef8a4789a0814f9ac4e66
CarinaWolli <wollencarina@gmail.com>
2023-09-07 13:09:44 -0400

    remove optional from clientId in getClient handler

--- packages/trpc/server/routers/viewer/oAuth/getClient.schema.ts
+++ packages/trpc/server/routers/viewer/oAuth/getClient.schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const ZGetClientInputSchema = z.object({
-  clientId: z.string().optional(),
+  clientId: z.string(),
 });
 
 export type TGetClientInputSchema = z.infer<typeof ZGetClientInputSchema>;
