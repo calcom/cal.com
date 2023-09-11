6f9a81ffcb3a8f39e0f5b3497528ca9de0001b7e
amanjaiswal7236 <124071449+amanjaiswal7236@users.noreply.github.com>
2023-09-11 16:34:08 +0530

    Update tailwind.config.js
    In the project's Tailwind CSS configuration file (typically tailwind.config.js), defined the text-emphasis class in the extend section of the configuration

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -2,6 +2,21 @@ const base = require("@calcom/config/tailwind-preset");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   ...base,
+  theme: {
+    extend: {
+      // Add your custom classes here
+      typography: {
+        'text-emphasis': {
+          css: {
+            // Define the styles for your custom class here
+            // For example:
+            fontWeight: 'bold',
+            color: '#ff0000', // Specify your desired color
+          },
+        },
+      },
+    },
+  },
   content: [...base.content, "../../node_modules/@tremor/**/*.{js,ts,jsx,tsx}"],
   plugins: [...base.plugins, require("tailwindcss-animate")],
 };
