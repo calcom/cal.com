3ebc5c419b3bcdd02673af67fec286d706218434
Keith Williams <keithwillcode@gmail.com>
2023-09-12 09:10:57 -0300

    Update bookingScenario.ts

--- apps/web/test/utils/bookingScenario.ts
+++ apps/web/test/utils/bookingScenario.ts
@@ -201,8 +201,7 @@ async function addBookings(bookings: InputBooking[], eventTypes: InputEventType[
             const firstConditionMatches =
               // eslint-disable-next-line @typescript-eslint/ban-ts-comment
               // @ts-ignore
-              statusIn.includes(booking.status) &&
-              (booking.userId === where.OR[0].userId || userIdIn.includes(booking.userId));
+              statusIn.includes(booking.status) && (booking.userId === where.OR[0].userId || userIdIn.includes(booking.userId));
 
             // We return this booking if either condition is met
             return firstConditionMatches;
