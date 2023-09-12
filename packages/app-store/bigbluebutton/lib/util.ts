6d30cde3ff0d1530a122e442e262e2de67ff8dcf
nicktrn <55853254+nicktrn@users.noreply.github.com>
2023-09-12 20:20:03 +0000

    chore: rename organizer to moderator

--- packages/app-store/bigbluebutton/lib/util.ts
+++ packages/app-store/bigbluebutton/lib/util.ts
@@ -5,10 +5,10 @@ import type { Person } from "@calcom/types/Calendar";
 export const applyMeetingUrlTemplate = (
   urlTemplate?: string | null,
   attendeeHash?: string | null,
-  isOrganizer?: boolean
+  isModerator?: boolean
 ) => {
   if (!attendeeHash || !urlTemplate) return urlTemplate;
-  return urlTemplate.replace("{ATTENDEE_HASH}", isOrganizer ? "" : attendeeHash);
+  return urlTemplate.replace("{ATTENDEE_HASH}", isModerator ? "" : attendeeHash);
 };
 
 export const hashAttendee = (attendee: Pick<Person, "name" | "email">) =>
