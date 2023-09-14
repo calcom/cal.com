54a5e50baad28c88a334f0aa4af03984e2288e92
CarinaWolli <wollencarina@gmail.com>
2023-09-14 13:36:20 -0400

    make sure only paying users can request verification codes

--- packages/trpc/server/routers/viewer/workflows/getWorkflowActionOptions.handler.ts
+++ packages/trpc/server/routers/viewer/workflows/getWorkflowActionOptions.handler.ts
@@ -1,5 +1,6 @@
 import { getWorkflowActionOptions } from "@calcom/features/ee/workflows/lib/getOptions";
 import { IS_SELF_HOSTED } from "@calcom/lib/constants";
+import hasKeyInMetadata from "@calcom/lib/hasKeyInMetadata";
 import { getTranslation } from "@calcom/lib/server/i18n";
 import type { TrpcSessionUser } from "@calcom/trpc/server/trpc";
 
@@ -16,10 +17,21 @@ type GetWorkflowActionOptionsOptions = {
 export const getWorkflowActionOptionsHandler = async ({ ctx }: GetWorkflowActionOptionsOptions) => {
   const { user } = ctx;
 
-  const { hasTeamPlan } = await hasTeamPlanHandler({ ctx });
+  const isCurrentUsernamePremium =
+    user && hasKeyInMetadata(user, "isPremium") ? !!user.metadata.isPremium : false;
+
+  let isTeamsPlan = false;
+  if (!isCurrentUsernamePremium) {
+    const { hasTeamPlan } = await hasTeamPlanHandler({ ctx });
+    isTeamsPlan = !!hasTeamPlan;
+  }
 
   const hasOrgsPlan = !!user.organizationId;
 
   const t = await getTranslation(ctx.user.locale, "common");
-  return getWorkflowActionOptions(t, IS_SELF_HOSTED || hasTeamPlan, IS_SELF_HOSTED || hasOrgsPlan);
+  return getWorkflowActionOptions(
+    t,
+    IS_SELF_HOSTED || isCurrentUsernamePremium || isTeamsPlan,
+    IS_SELF_HOSTED || hasOrgsPlan
+  );
 };
