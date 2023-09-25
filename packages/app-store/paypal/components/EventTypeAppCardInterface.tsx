f3f82182b33e1fae415a5d6604a5f41839fceffd
Lauris Skraucis <lauris@Lauriss-MacBook-Pro.local>
2023-09-25 12:32:14 +0200

    refactor: price input margin

--- packages/app-store/paypal/components/EventTypeAppCardInterface.tsx
+++ packages/app-store/paypal/components/EventTypeAppCardInterface.tsx
@@ -62,7 +62,7 @@ const EventTypeAppCard: EventTypeAppCardComponent = function EventTypeAppCard({ 
                   min="0.5"
                   type="number"
                   required
-                  className="block w-full rounded-sm border-gray-300 pl-2 pr-12 text-sm"
+                  className="block w-full rounded-sm border-gray-300 pl-2 text-sm"
                   placeholder="Price"
                   onChange={(e) => {
                     setAppData("price", Number(e.target.value) * 100);
