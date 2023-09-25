fb66df8c98263e820d4433aab98e53ddb488abed
Dhoni77 <aldrinmarz7@gmail.com>
2023-09-25 19:13:41 +0530

    feat(ui): Add tests for ToolTip Component

---
+++ packages/ui/components/tooltip/tooltip.test.tsx
@@ -0,0 +1,64 @@
+import { TooltipProvider } from "@radix-ui/react-tooltip";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { useState } from "react";
+
+import Tooltip from "./Tooltip";
+
+describe("Tests for Tooltip component", () => {
+  test("should render tooltip when defaultOpen prop is true", () => {
+    render(
+      <TooltipProvider>
+        <Tooltip content={<div>This is a tooltip</div>} defaultOpen={true}>
+          <button data-testid="btn">Hovered text</button>
+        </Tooltip>
+      </TooltipProvider>
+    );
+    expect(screen.getByText(/Hovered text/i)).toBeInTheDocument();
+  });
+
+  test("should render tooltip based on open state", () => {
+    function ToolTipExample() {
+      const [open, setOpen] = useState(true);
+      return (
+        <TooltipProvider>
+          <Tooltip
+            data-testid="tooltip"
+            content={<div>This is a tooltip</div>}
+            open={open}
+            onOpenChange={() => setOpen(false)}>
+            <button data-testid="btn">Hovered text</button>
+          </Tooltip>
+        </TooltipProvider>
+      );
+    }
+    render(<ToolTipExample />);
+    const tooltip = screen.getByTestId("tooltip");
+    const button = screen.getByTestId("btn");
+    expect(tooltip.getAttribute("data-state")).toEqual("instant-open");
+    fireEvent.click(button);
+    expect(tooltip.getAttribute("data-state")).toEqual("closed");
+  });
+
+  test("should render tooltip with given delay", async () => {
+    const { debug } = render(
+      <TooltipProvider>
+        <Tooltip content={<div>This is a tooltip</div>} delayDuration={50}>
+          <button data-testid="btn">Hovered text</button>
+        </Tooltip>
+      </TooltipProvider>
+    );
+    const button = screen.getByTestId("btn");
+    const user = userEvent.setup();
+    user.hover(button);
+    await waitFor(
+      () => {
+        debug();
+        expect(screen.getByTestId("btn").getAttribute("data-state")).toEqual("delayed-open");
+      },
+      {
+        timeout: 100,
+      }
+    );
+  });
+});
