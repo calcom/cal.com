58a2320401878873a49a75454cdb5976f453cd04
gitstart-calcom <gitstart-calcom@users.noreply.github.com>
2023-09-27 12:58:39 +0000

    Refactor

--- apps/web/playwright/booking/utils/bookingUtils.ts
+++ apps/web/playwright/booking/utils/bookingUtils.ts
@@ -8,143 +8,100 @@ const PHONE = "+55 (32) 983289947";
 const scheduleSuccessfullyText = "This meeting is scheduled";
 const reschedulePlaceholderText = "Let others know why you need to reschedule";
 
+interface QuestionActions {
+  [key: string]: () => Promise<void>;
+}
+
 type BookingOptions = {
   hasPlaceholder?: boolean;
   isReschedule?: boolean;
   isRequired?: boolean;
-  isCheckbox?: boolean;
-  isBoolean?: boolean;
-  isMultiEmails?: boolean;
-  isSelect?: boolean;
-  isMultiSelect?: boolean;
 };
 
-// Logs in a test user and navigates to the event types page.
 export const loginUser = async (page: Page, users: Fixtures["users"]) => {
   const pro = await users.create({ name: "testuser" });
   await pro.apiLogin();
   await page.goto("/event-types");
 };
 
-// Fills in booking details and confirms the booking, expecting a success message.
+const fillQuestion = async (
+  eventTypePage: Page,
+  questionType: string,
+  questionText: string,
+  options: BookingOptions
+) => {
+  const questionActions: QuestionActions = {
+    phone: async () => {
+      await eventTypePage.getByPlaceholder(`${questionText} test`).clear();
+      await eventTypePage.getByPlaceholder(`${questionText} test`).fill(PHONE);
+    },
+    multiemail: async () => {
+      await eventTypePage.getByRole("button", { name: `${questionText} test` }).click();
+      await eventTypePage.getByPlaceholder(`${questionText} test`).fill(EMAIL);
+      await eventTypePage.getByTestId("add-another-guest").last().click();
+      await eventTypePage.getByPlaceholder(`${questionText} test`).last().fill(EMAIL2);
+    },
+    checkbox: async () => {
+      await eventTypePage.getByLabel("Option 1").click();
+      await eventTypePage.getByLabel("Option 2").click();
+    },
+    multiselect: async () => {
+      await eventTypePage.locator("form svg").last().click();
+      await eventTypePage.getByTestId("select-option-Option 1").click();
+    },
+    boolean: async () => {
+      await eventTypePage.getByLabel(`${questionText} test`).check();
+    },
+    radio: async () => {
+      await eventTypePage.locator('[id="radio-test\\.option\\.0\\.radio"]').click();
+    },
+    select: async () => {
+      await eventTypePage.locator("form svg").last().click();
+      await eventTypePage.getByTestId("select-option-Option 1").click();
+    },
+    number: async () => {
+      await eventTypePage.getByPlaceholder(`${questionText} test`).click();
+      await eventTypePage.getByPlaceholder(`${questionText} test`).fill("123");
+    },
+    address: async () => {
+      await eventTypePage.getByPlaceholder(`${questionText} test`).click();
+      await eventTypePage.getByPlaceholder(`${questionText} test`).fill("address test");
+    },
+    textarea: async () => {
+      await eventTypePage.getByPlaceholder(`${questionText} test`).click();
+      await eventTypePage.getByPlaceholder(`${questionText} test`).fill("textarea test");
+    },
+    text: async () => {
+      await eventTypePage.getByPlaceholder(`${questionText} test`).click();
+      await eventTypePage.getByPlaceholder(`${questionText} test`).fill("text test");
+    },
+  };
+
+  if (options.isRequired || questionType !== "secondQuestion") {
+    if (questionActions[questionType]) {
+      await questionActions[questionType]();
+    }
+  }
+};
+
 export const fillAndConfirmBooking = async (
   eventTypePage: Page,
   placeholderText: string,
   question: string,
   fillText: string,
   secondQuestion: string,
-  hasPlaceholder = false,
-  isReschedule = false,
-  isRequired = true,
-  isCheckbox = false,
-  isBoolean = false,
-  isMultiEmails = false,
-  isSelect = false,
-  isMultiSelect = false
+  options: BookingOptions
 ) => {
-  const confirmButton = isReschedule ? "confirm-reschedule-button" : "confirm-book-button";
+  const confirmButton = options.isReschedule ? "confirm-reschedule-button" : "confirm-book-button";
 
   await expect(eventTypePage.getByText(`${secondQuestion} test`).first()).toBeVisible();
   await eventTypePage.getByPlaceholder(placeholderText).fill(fillText);
 
   // Fill the first question
-  switch (question) {
-    case "phone":
-      await eventTypePage.getByPlaceholder(`phone test`).clear();
-      await eventTypePage.getByPlaceholder(`phone test`).fill(PHONE);
-      break;
-    case "multiemail":
-      await eventTypePage.getByRole("button", { name: "multiemail test" }).click();
-      (await eventTypePage.getByPlaceholder("multiemail test").isHidden()) &&
-        (await eventTypePage.getByRole("button", { name: "multiemail test" }).click());
-      await eventTypePage.getByPlaceholder("multiemail test").fill(EMAIL);
-      await eventTypePage.getByTestId("add-another-guest").last().click();
-      await eventTypePage.getByPlaceholder("multiemail test").last().fill(EMAIL2);
-      break;
-    case "checkbox":
-      await eventTypePage.getByLabel("Option 1").click();
-      await eventTypePage.getByLabel("Option 2").click();
-      break;
-    case "multiselect":
-      await eventTypePage.locator("form svg").last().click();
-      await eventTypePage.getByTestId("select-option-Option 1").click();
-      break;
-    case "boolean":
-      await eventTypePage.getByLabel("boolean test").check();
-      break;
-    case "radio":
-      await eventTypePage.getByLabel("radio test").check();
-      break;
-    case "select":
-      await eventTypePage.locator("form svg").last().click();
-      await eventTypePage.getByTestId("select-option-Option 1").click();
-      break;
-    case "number":
-      await eventTypePage.getByPlaceholder(`number test`).click();
-      await eventTypePage.getByPlaceholder(`number test`).fill("123");
-      break;
-    case "address":
-      await eventTypePage.getByPlaceholder(`address test`).click();
-      await eventTypePage.getByPlaceholder(`address test`).fill("address test");
-      break;
-    case "textarea":
-      await eventTypePage.getByPlaceholder(`textarea test`).click();
-      await eventTypePage.getByPlaceholder(`textarea test`).fill("textarea test");
-      break;
-    case "text":
-      await eventTypePage.getByPlaceholder(`text test`).click();
-      await eventTypePage.getByPlaceholder(`text test`).fill("text test");
-      break;
-  }
+  await fillQuestion(eventTypePage, question, question, options);
 
   // Fill the second question
-  if (isRequired) {
-    if (secondQuestion === "phone") {
-      await eventTypePage.getByPlaceholder(`${secondQuestion} test`).clear();
-      await eventTypePage.getByPlaceholder(`${secondQuestion} test`).fill(PHONE);
-    } else {
-      // if is checkbox question and required check
-      if (isCheckbox) {
-        await eventTypePage.getByLabel("Option 1").check();
-        await eventTypePage.getByLabel("Option 2").check();
-      }
-
-      // if is boolean question and required check
-      if (isBoolean) {
-        await eventTypePage.getByLabel(`${secondQuestion} test`).check();
-      }
-
-      // if is multiemails question and required add two emails
-      if (isMultiEmails) {
-        await eventTypePage.getByRole("button", { name: "multiemail test" }).click();
-        await eventTypePage.getByPlaceholder("multiemail test").fill(EMAIL);
-        await eventTypePage.getByTestId("add-another-guest").last().click();
-        await eventTypePage.getByPlaceholder("multiemail test").nth(1).fill(EMAIL2);
-      }
-
-      if (isMultiSelect) {
-        await eventTypePage.locator("form svg").last().click();
-        await eventTypePage.getByTestId("select-option-Option 1").click();
-      }
-
-      // if is select or multiselect question and required click in all options, for select just the last option will be checked
-      if (isSelect) {
-        await eventTypePage.locator("form svg").last().click();
-        await eventTypePage.getByTestId("select-option-Option 1").click();
-      }
-
-      // if the question has placeholder fill, if is number fill as number
-      if (hasPlaceholder && !isMultiEmails) {
-        if (secondQuestion === "number") {
-          await eventTypePage.getByPlaceholder(`${secondQuestion} test`).click();
-          await eventTypePage.getByPlaceholder(`${secondQuestion} test`).fill("123");
-        } else {
-          await eventTypePage.getByPlaceholder(`${secondQuestion} test`).click();
-          await eventTypePage.getByPlaceholder(`${secondQuestion} test`).fill(secondQuestion);
-        }
-      }
-    }
-  }
+  await fillQuestion(eventTypePage, secondQuestion, secondQuestion, options);
 
   await eventTypePage.getByTestId(confirmButton).click();
   const scheduleSuccessfullyPage = eventTypePage.getByText(scheduleSuccessfullyText);
@@ -197,7 +154,6 @@ export const initialCommonSteps = async (
   await bookingPage.getByTestId("field-add-save").click();
   await expect(bookingPage.getByTestId(`field-${question}-test`)).toBeVisible();
   await expect(bookingPage.getByTestId(`field-${secondQuestion}-test`)).toBeVisible();
-
   await bookingPage.getByTestId("update-eventtype").click();
 
   // Go to booking page
@@ -207,27 +163,18 @@ export const initialCommonSteps = async (
 
   // Select the first available time
   await eventTypePage.getByTestId("time").first().click();
-
   fillAndConfirmBooking(
     eventTypePage,
     "Please share anything that will help prepare for our meeting.",
     question,
     message,
     secondQuestion,
-    options.hasPlaceholder,
-    options.isReschedule,
-    options.isRequired,
-    options.isCheckbox,
-    options.isBoolean,
-    options.isMultiEmails,
-    options.isSelect,
-    options.isMultiSelect
+    options
   );
 
   // Go to final steps
   await rescheduleAndCancel(eventTypePage);
 };
-
 const rescheduleAndCancel = async (eventTypePage: Page) => {
   await eventTypePage.getByText("Reschedule").click();
   await eventTypePage.getByTestId("time").first().click();
