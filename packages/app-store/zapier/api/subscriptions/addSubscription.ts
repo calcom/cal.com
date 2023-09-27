a269926a305bd302d9ac6d748a14b12e460c05c6
zomars <zomars@me.com>
2023-09-27 16:19:13 -0700

    fix: Nitpicks and DRYness

--- packages/app-store/zapier/api/subscriptions/addSubscription.ts
+++ packages/app-store/zapier/api/subscriptions/addSubscription.ts
@@ -1,41 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { validateAccountOrApiKey } from "zapier/lib/validateAccountOrApiKey";
 
-import isAuthorized from "@calcom/features/auth/lib/oAuthAuthorization";
-import findValidApiKey from "@calcom/features/ee/api-keys/lib/findValidApiKey";
 import { addSubscription } from "@calcom/features/webhooks/lib/scheduleTrigger";
 import { defaultHandler, defaultResponder } from "@calcom/lib/server";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const apiKey = req.query.apiKey as string;
-
-  let validKey;
-
-  if (apiKey) {
-    validKey = await findValidApiKey(apiKey, "zapier");
-    if (!validKey) {
-      return res.status(401).json({ message: "API key not valid" });
-    }
-  }
-
-  let authorizedAccount: {
-    id: number;
-    name: string | null;
-    isTeam: boolean;
-  } | null = null;
-
-  if (!apiKey) {
-    authorizedAccount = await isAuthorized(req, ["READ_BOOKING", "READ_PROFILE"]);
-  }
-
-  if (!authorizedAccount && !validKey) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-
   const { subscriberUrl, triggerEvent } = req.body;
-
+  const { account, appApiKey } = await validateAccountOrApiKey(req, ["READ_BOOKING", "READ_PROFILE"]);
   const createAppSubscription = await addSubscription({
-    appApiKey: validKey,
-    account: authorizedAccount,
+    appApiKey,
+    account,
     triggerEvent: triggerEvent,
     subscriberUrl: subscriberUrl,
     appId: "zapier",
--- packages/app-store/zapier/api/subscriptions/listBookings.ts
+++ packages/app-store/zapier/api/subscriptions/listBookings.ts
@@ -1,36 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { validateAccountOrApiKey } from "zapier/lib/validateAccountOrApiKey";
 
-import isAuthorized from "@calcom/features/auth/lib/oAuthAuthorization";
-import findValidApiKey from "@calcom/features/ee/api-keys/lib/findValidApiKey";
 import { listBookings } from "@calcom/features/webhooks/lib/scheduleTrigger";
 import { defaultHandler, defaultResponder } from "@calcom/lib/server";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const apiKey = req.query.apiKey as string;
-
-  let validKey;
-
-  if (apiKey) {
-    validKey = await findValidApiKey(apiKey, "zapier");
-
-    if (!validKey) {
-      return res.status(401).json({ message: "API key not valid" });
-    }
-  }
-
-  let authorizedAccount: {
-    id: number;
-    name: string | null;
-    isTeam: boolean;
-  } | null = null;
-
-  if (!apiKey) {
-    authorizedAccount = await isAuthorized(req, ["READ_BOOKING"]);
-  }
-  if (!authorizedAccount && !validKey) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-
+  const { account: authorizedAccount, appApiKey: validKey } = await validateAccountOrApiKey(req, [
+    "READ_BOOKING",
+  ]);
   const bookings = await listBookings(validKey, authorizedAccount);
 
   if (!bookings) {
