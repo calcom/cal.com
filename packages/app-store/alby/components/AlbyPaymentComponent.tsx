c98c12c01faf6f62d88098181e56a8a1c46473f2
Roland Bewick <roland.bewick@gmail.com>
2023-09-27 11:32:00 +0700

    chore: update TODOs

--- packages/app-store/alby/components/AlbyPaymentComponent.tsx
+++ packages/app-store/alby/components/AlbyPaymentComponent.tsx
@@ -123,7 +123,8 @@ export const AlbyPaymentComponent = (props: IAlbyPaymentComponentProps) => {
 type PaymentCheckerProps = PaymentPageProps;
 
 function PaymentChecker(props: PaymentCheckerProps) {
-  // FIXME: booking success is copied from packages/features/ee/payments/components/Payment.tsx
+  // TODO: move booking success code to a common lib function
+  // TODO: subscribe rather than polling
   const searchParams = useSearchParams();
   const bookingSuccessRedirect = useBookingSuccessRedirect();
   const utils = trpc.useContext();
