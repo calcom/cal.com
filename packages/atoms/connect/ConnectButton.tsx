af741a1b9ea7f03422ce764149a2213cdd24301b
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-09-29 13:39:54 +0530

    add callback for error

--- packages/atoms/connect/ConnectButton.tsx
+++ packages/atoms/connect/ConnectButton.tsx
@@ -6,6 +6,11 @@ import type { ConnectButtonProps } from "../types";
 // TODO: add default styling
 // also a cssClassAssembler function for custom styling
 
+// we accept two props: buttonText & onButtonClick
+// buttonText is for displaying buttonText
+// onButtonClick is a a method to handle onClick for button
+// it accepts another object which contains a callback in case of any error
+
 export function ConnectButton({ buttonText, onButtonClick }: ConnectButtonProps & AtomsGlobalConfigProps) {
   const [isProcessing, setIsProcessing] = useState<boolean>(false);
   const [errMsg, setErrMsg] = useState<string>("");
@@ -14,7 +19,14 @@ export function ConnectButton({ buttonText, onButtonClick }: ConnectButtonProps 
     e.preventDefault();
 
     setIsProcessing(true);
-    onButtonClick();
+    onButtonClick({
+      cb: (err) => {
+        setIsProcessing(false);
+        if (err?.error.message) {
+          setErrMsg(err.error.message);
+        }
+      },
+    });
   }
 
   return (
