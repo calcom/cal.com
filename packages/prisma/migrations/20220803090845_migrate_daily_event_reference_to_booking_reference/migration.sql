7e6ec88639ec6810df0fe43269d1084ea772d546
xonx <119700621+xonx4l@users.noreply.github.com>
2023-09-29 22:34:29 +0530

    Update migration.sql
    ISSUE-:11608

--- packages/prisma/migrations/20220803090845_migrate_daily_event_reference_to_booking_reference/migration.sql
+++ packages/prisma/migrations/20220803090845_migrate_daily_event_reference_to_booking_reference/migration.sql
@@ -1,4 +1,4 @@
 UPDATE "BookingReference"
-SET "meetingUrl" = "dailyurl", "meetingPassword" = "dailytoken"
+SET "meetingUrl" = TRIM("dailyurl", "meetingPassword") = "dailytoken"
 FROM "DailyEventReference"
 WHERE "DailyEventReference"."bookingId" = "BookingReference"."bookingId" AND "BookingReference"."type" = 'daily_video'
