ff3d77679dcafa103f1c0ff953caf7c9b58256fa
Bhargva <bhargavagonugunta123@gmail.com>
2023-09-30 12:21:06 +0530

    Fixes  booker: layout shift when changing months in  spinner

--- packages/features/bookings/Booker/config.ts
+++ packages/features/bookings/Booker/config.ts
@@ -75,7 +75,7 @@ export const resizeAnimationConfig: ResizeAnimationConfig = {
     selecting_time: {
       width: "calc(var(--booker-meta-width) + var(--booker-main-width) + var(--booker-timeslots-width))",
       minHeight: "450px",
-      height: "auto",
+      height: "500px",
       gridTemplateAreas: `
       "meta main timeslots"
       "meta main timeslots"
@@ -88,7 +88,7 @@ export const resizeAnimationConfig: ResizeAnimationConfig = {
     default: {
       width: "100vw",
       minHeight: "100vh",
-      height: "auto",
+      height: "500px",
       gridTemplateAreas: `
       "meta header header"
       "meta main main"
@@ -101,7 +101,7 @@ export const resizeAnimationConfig: ResizeAnimationConfig = {
     default: {
       width: "100vw",
       minHeight: "100vh",
-      height: "auto",
+      height: "500px",
       gridTemplateAreas: `
       "meta header header"
       "meta main main"
