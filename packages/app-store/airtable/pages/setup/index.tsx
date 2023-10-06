bf19ce9aa14698985f2cb0522fdc4287ce9f9152
nafees nazik <nafeesnazik21@gmail.com>
2023-10-06 23:08:02 +0530

    feat: add setup

--- packages/app-store/airtable/pages/setup/index.tsx
+++ packages/app-store/airtable/pages/setup/index.tsx
@@ -1,3 +1,100 @@
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { Toaster } from "react-hot-toast";
+
+import { useLocale } from "@calcom/lib/hooks/useLocale";
+import { trpc } from "@calcom/trpc";
+import { Button, PasswordField, showToast } from "@calcom/ui";
+
+import type { TAppKeysSchema } from "../../zod";
+
+const checkIsKeyValid = async (token: string) => {
+  const req = await fetch("https://api.airtable.com/v0/meta/whoami", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return req.ok;
+};
+
+type Inputs = TAppKeysSchema;
+
 export default function AirtableSetup() {
-  return <div>airtable setup page</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+    setError,
+  } = useForm<Inputs>();
+  const router = useRouter();
+  const { t } = useLocale();
+
+  const integrations = trpc.viewer.integrations.useQuery({ variant: "other", appId: "airtable" });
+
+  const [AirtableCredentials] = integrations.data?.items || [];
+  const [credentialId] = AirtableCredentials?.userCredentialIds || [-1];
+  const showContent = !!integrations.data && integrations.isSuccess && !!credentialId;
+
+  const saveKeysMutation = trpc.viewer.appsRouter.updateAppCredentials.useMutation({
+    onSuccess: () => {
+      showToast(t("keys_have_been_saved"), "success");
+      router.push("/event-types");
+    },
+    onError: (error) => {
+      showToast(error.message, "error");
+    },
+  });
+
+  const onSubmit = async (data: Inputs) => {
+    const isKeyValid = await checkIsKeyValid(data.personalAccessToken);
+
+    if (isKeyValid) {
+      saveKeysMutation.mutate({
+        credentialId,
+        key: data,
+      });
+    } else {
+      showToast("invalid key", "error");
+    }
+  };
+
+  if (integrations.isLoading) {
+    return <div className="absolute z-50 flex h-screen w-full items-center bg-gray-200" />;
+  }
+
+  return (
+    <div className="bg-default flex h-screen">
+      {showContent ? (
+        <div className="bg-default border-subtle m-auto max-w-[43em] overflow-auto rounded  border pb-10 md:p-10">
+          <div className="md:flex md:flex-row">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <PasswordField
+                {...register("personalAccessToken")}
+                autoComplete="off"
+                label="Airtable personal access token"
+              />
+              <div className="mt-5 flex flex-row justify-end">
+                <Button loading={isSubmitting} type="submit" color="primary">
+                  {t("save")}
+                </Button>
+              </div>
+            </form>
+          </div>
+        </div>
+      ) : (
+        <div className="ml-5 mt-5">
+          <div>{t("install_app_on_app_store", { appName: "Airtable" })}</div>
+          <div className="mt-3">
+            <Link href="/apps/make" passHref={true} legacyBehavior>
+              <Button>{t("go_to_app_store")}</Button>
+            </Link>
+          </div>
+        </div>
+      )}
+
+      <Toaster position="bottom-right" />
+    </div>
+  );
 }
--- packages/app-store/airtable/zod.ts
+++ packages/app-store/airtable/zod.ts
@@ -1,10 +1,16 @@
-import { z } from "zod";
+import * as z from "zod";
 
 import { eventTypeAppCardZod } from "@calcom/app-store/eventTypeAppCardZod";
 
 export const appDataSchema = eventTypeAppCardZod.merge(
   z.object({
     isSunrise: z.boolean(),
+    baseId: z.string().optional(),
+    tableId: z.string().optional(),
   })
 );
-export const appKeysSchema = z.object({});
+export const appKeysSchema = z.object({
+  personalAccessToken: z.string().min(1),
+});
+
+export type TAppKeysSchema = z.infer<typeof appKeysSchema>;
