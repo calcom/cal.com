62f108026f7f889f112395b8345389ee7c08190a
nafees nazik <nafeesnazik21@gmail.com>
2023-10-09 23:09:23 +0530

    feat: add app card

--- packages/app-store/airtable/components/EventTypeAppCardInterface.tsx
+++ packages/app-store/airtable/components/EventTypeAppCardInterface.tsx
@@ -2,37 +2,72 @@ import { useAppContextWithSchema } from "@calcom/app-store/EventTypeAppContext";
 import AppCard from "@calcom/app-store/_components/AppCard";
 import useIsAppEnabled from "@calcom/app-store/_utils/useIsAppEnabled";
 import type { EventTypeAppCardComponent } from "@calcom/app-store/types";
-import { Sunrise, Sunset } from "@calcom/ui/components/icon";
+import { trpc } from "@calcom/trpc/react";
 
+import { SelectField } from "../../../ui/components/form/select/Select";
 import type { appDataSchema } from "../zod";
 
 const EventTypeAppCard: EventTypeAppCardComponent = function EventTypeAppCard({ eventType, app }) {
   const { getAppData, setAppData } = useAppContextWithSchema<typeof appDataSchema>();
-  const isSunrise = getAppData("isSunrise");
-  const { enabled, updateEnabled } = useIsAppEnabled(app);
 
+  const baseId = getAppData("baseId");
+  const tableId = getAppData("tableId");
+  const { enabled, updateEnabled } = useIsAppEnabled(app);
+  const { data: bases } = trpc.viewer.appAirtable.bases.useQuery();
+  const { data: tables } = trpc.viewer.appAirtable.tables.useQuery(
+    { baseId: baseId as string },
+    {
+      enabled: typeof baseId === "string" && baseId !== "",
+    }
+  );
+
+  const basesOption = bases?.map((item) => ({
+    value: item.id,
+    label: item.name,
+  }));
+
+  const tablesOption = tables?.tables?.map((item) => ({
+    value: item.id,
+    label: item.name,
+  }));
   return (
     <AppCard
       app={app}
       switchOnClick={(e) => {
         if (!e) {
           updateEnabled(false);
-          setAppData("isSunrise", false);
         } else {
           updateEnabled(true);
-          setAppData("isSunrise", true);
         }
       }}
       switchChecked={enabled}
       teamId={eventType.team?.id || undefined}>
       <div className="mt-2 text-sm">
-        <div className="flex">
-          <span className="ltr:mr-2 rtl:ml-2">{isSunrise ? <Sunrise /> : <Sunset />}</span>I am an AppCard for
-          Event with Title: {eventType.title}
-        </div>{" "}
-        <div className="mt-2">
-          Edit <span className="italic">packages/app-store/{app.slug}/EventTypeAppCardInterface.tsx</span> to
-          play with me
+        <div className="flex w-full flex-col gap-y-4">
+          {basesOption && basesOption.length && (
+            <SelectField
+              className="w-full"
+              label="Airtable Base"
+              options={basesOption}
+              value={basesOption?.find((item) => item.value === baseId)}
+              onChange={(val) => {
+                setAppData("baseId", val.value);
+                setAppData("tableId", "");
+              }}
+            />
+          )}
+
+          {tablesOption && tablesOption.length && (
+            <SelectField
+              className="w-full"
+              label="Airtable Table"
+              options={tablesOption}
+              value={tablesOption?.find((item) => item.value === tableId)}
+              onChange={(val) => {
+                setAppData("tableId", val.value);
+              }}
+            />
+          )}
         </div>
       </div>
     </AppCard>
