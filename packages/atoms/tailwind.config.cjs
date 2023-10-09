801a2e5fda7bab59bd1f28a645cd9b3748278715
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-10-09 13:07:48 +0530

    fixes

--- packages/atoms/tailwind.config.cjs
+++ packages/atoms/tailwind.config.cjs
@@ -1,12 +1,16 @@
+const base = require("@calcom/config/tailwind-preset");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  ...base,
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+    "./bookings/**/*.tsx",
+  ],
   theme: {
     container: {
       center: true,
@@ -73,4 +77,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
