0cfbf7159eb3968b93098e43302d2c1029b357a3
Anik Dhabal Babu <81948346+anikdhabal@users.noreply.github.com>
2023-10-11 00:45:26 +0530

    fix position and add transition (#11799)

--- packages/ui/components/arrow-button/ArrowButton.tsx
+++ packages/ui/components/arrow-button/ArrowButton.tsx
@@ -10,13 +10,13 @@ export function ArrowButton(props: ArrowButtonProps) {
     <>
       {props.arrowDirection === "up" ? (
         <button
-          className="bg-default text-muted hover:text-emphasis border-default hover:border-emphasis invisible absolute left-[5px] -ml-4 -mt-4 mb-4 hidden h-6 w-6 scale-0 items-center justify-center rounded-md border p-1 transition-all group-hover:visible group-hover:scale-100 sm:ml-0 sm:flex lg:left-[36px]"
+          className="bg-default text-muted hover:text-emphasis border-default hover:border-emphasis invisible absolute left-[5px] -ml-4 -mt-4 mb-4 hidden h-6 w-6 scale-0 items-center justify-center rounded-md border p-1 transition-all group-hover:visible group-hover:scale-100 sm:ml-0 sm:flex lg:left-[12px]"
           onClick={props.onClick}>
           <ArrowUp className="h-5 w-5" />
         </button>
       ) : (
         <button
-          className="bg-default text-muted border-default hover:text-emphasis hover:border-emphasis invisible absolute left-[5px] -ml-4 mt-8 hidden h-6 w-6  scale-0 items-center justify-center rounded-md border p-1 transition-all  group-hover:visible group-hover:scale-100 sm:ml-0 sm:flex lg:left-[36px]"
+          className="bg-default text-muted border-default hover:text-emphasis hover:border-emphasis invisible absolute left-[5px] -ml-4 mt-8 hidden h-6 w-6  scale-0 items-center justify-center rounded-md border p-1 transition-all  group-hover:visible group-hover:scale-100 sm:ml-0 sm:flex lg:left-[12px]"
           onClick={props.onClick}>
           <ArrowDown className="h-5 w-5" />
         </button>
