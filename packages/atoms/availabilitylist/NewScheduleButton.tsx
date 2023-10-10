e62bfc18251b0b2fe0a9dead828901e89325c99a
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-10-10 22:36:24 +0530

    add dialog for new schedule

--- packages/atoms/availabilitylist/NewScheduleButton.tsx
+++ packages/atoms/availabilitylist/NewScheduleButton.tsx
@@ -1,9 +1,22 @@
+import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+
+import { Button } from "../src/components/ui/button";
 import { Form } from "./Form";
 
 export function NewScheduleButton({ name = "new-schedule" }: { name?: string }) {
   return (
     <div>
-      <Form />
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button>New</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Add new schedule</DialogTitle>
+            <Form />
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
