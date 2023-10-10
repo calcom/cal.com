837196610c10b0d7babf525c268c9c71a84412cc
Benny Joo <sldisek783@gmail.com>
2023-10-10 15:52:49 +0100

    wip

--- apps/web/app/event-types-1/layout.tsx
+++ apps/web/app/event-types-1/layout.tsx
@@ -2,6 +2,7 @@ import { headers } from "next/headers";
 import { type ReactElement } from "react";
 
 import { getLayout } from "@calcom/features/MainLayout";
+import { viewerRouter } from "@calcom/trpc/server/routers/viewer/_router";
 
 import PageWrapper from "@components/PageWrapperAppDir";
 
@@ -9,9 +10,14 @@ type EventTypesLayoutProps = {
   children: ReactElement;
 };
 
+const caller = viewerRouter.createCaller({});
+
 export default async function EventTypesLayout({ children }: EventTypesLayoutProps) {
   const h = headers();
   const nonce = h.get("x-nonce") ?? undefined;
+  console.log(caller);
+  const x = await caller.greeting();
+  console.log(x);
 
   return (
     // @ts-expect-error withTrpc expects AppProps
