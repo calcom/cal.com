9302f136c280fff8180cc277ef325c0ddff5e63a
Sean Brydon <sean@cal.com>
2023-10-10 11:28:17 +0100

    Fix timezone issues

--- packages/features/bookings/lib/useCheckOverlapWithOverlay.tsx
+++ packages/features/bookings/lib/useCheckOverlapWithOverlay.tsx
@@ -9,20 +9,36 @@ function getCurrentTime(date: Date) {
   return `${hours}:${minutes}`;
 }
 
-export function useCheckOverlapWithOverlay(start: Dayjs, selectedDuration: number | null) {
+export function useCheckOverlapWithOverlay(start: Dayjs, selectedDuration: number | null, offset: number) {
   const overlayBusyDates = useOverlayCalendarStore((state) => state.overlayBusyDates);
 
-  const overlappingTimeStart: string | null = null;
-  const overlappingTimeEnd: string | null = null;
+  let overlappingTimeStart: string | null = null;
+  let overlappingTimeEnd: string | null = null;
 
   const isOverlapping =
     overlayBusyDates &&
     overlayBusyDates.some((busyDate) => {
       const busyDateStart = dayjs(busyDate.start);
       const busyDateEnd = dayjs(busyDate.end);
-      const selectedEndTime = dayjs(start).add(selectedDuration ?? 0, "minute");
+      const selectedEndTime = dayjs(start.add(offset, "hours")).add(selectedDuration ?? 0, "minute");
 
-      const isOverlapping = selectedEndTime.isAfter(busyDateStart) && start < busyDateEnd;
+      const isOverlapping =
+        (selectedEndTime.isSame(busyDateStart) || selectedEndTime.isAfter(busyDateStart)) &&
+        start.add(offset, "hours") < busyDateEnd &&
+        selectedEndTime > busyDateStart;
+
+      if (isOverlapping) {
+        console.log({
+          isOverlapping,
+          start: start.format("HH:mm"),
+          selectedEndTime: selectedEndTime.format("HH:mm"),
+          busyDateStart: busyDateStart.format("HH:mm"),
+          busyDateEnd: busyDateEnd.format("HH:mm"),
+        });
+      }
+
+      overlappingTimeStart = isOverlapping ? getCurrentTime(busyDateStart.toDate()) : null;
+      overlappingTimeEnd = isOverlapping ? getCurrentTime(busyDateEnd.toDate()) : null;
 
       return isOverlapping;
     });
