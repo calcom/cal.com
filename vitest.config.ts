2faf24fb986c7a323ee9aa1ff9387bcc14033439
Hariom Balhara <hariombalhara@gmail.com>
2023-10-10 09:46:04 +0530

    test: Add collective scheduling tests (#11670)

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vitest/config";
 
 process.env.INTEGRATION_TEST_MODE = "true";
 
+// We can't set it during tests because it is used as soon as _metadata.ts is imported which happens before tests start running
+process.env.DAILY_API_KEY = "MOCK_DAILY_API_KEY";
+
 export default defineConfig({
   test: {
     coverage: {
