7adfe559ba821a35b014ae313fd4881850a1d3e4
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-10-10 14:47:17 +0530

    update list view

--- packages/atoms/availabilitylist/AvailabilityList.tsx
+++ packages/atoms/availabilitylist/AvailabilityList.tsx
@@ -1,14 +1,28 @@
-import { EmptyScreen } from " ./EmptyScreen";
+import { Clock } from "@calcom/ui/components/icon";
 
-export function AvailabilityList({ schedules }: []) {
+import { EmptyScreen } from "./EmptyScreen";
+
+export function AvailabilityList({ schedules }: { schedules: [] }) {
   return (
     <>
       {schedules.length === 0 ? (
         <div>
-          <EmptyScreen className="w-full" />
+          <EmptyScreen
+            Icon={Clock}
+            headline="Create an availability schedule"
+            subtitle="Creating availability schedules allows you to manage availability across event types. They can be applied to one or more event types."
+            className="w-full"
+          />
         </div>
       ) : (
-        <div>Render availability list here</div>
+        <div>
+          Render availability list here
+          <div>
+            {schedules.map((schedule) => (
+              <h1 key={schedule?.id}>Hi</h1>
+            ))}
+          </div>
+        </div>
       )}
     </>
   );
