de0bd85ea5f792c8f12a39a943d6f32377e0a73a
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-10-11 18:30:06 +0530

    add input and label for form component

---
+++ packages/atoms/src/components/ui/input.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+import * as React from "react";
+
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
+  return (
+    <input
+      type={type}
+      className={cn(
+        "border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        className
+      )}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+Input.displayName = "Input";
+
+export { Input };
