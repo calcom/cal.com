cac1e6880d0526a03ab142173f4ade48afc2cd92
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-10-13 12:58:41 +0530

    shift button prop types inside button component

--- packages/atoms/types.ts
+++ packages/atoms/types.ts
@@ -1,18 +1,7 @@
-import type React from "react";
-
 export interface AtomsGlobalConfigProps {
   /**
    * API endpoint for the Booker component to fetch data from,
    * defaults to https://cal.com
    */
   webAppUrl?: string;
-}
-
-export type SVGComponent = React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-
-export interface ConnectButtonProps {
-  buttonText?: string;
-  onButtonClick: (payload: { cb: (err: { error: { message: string } } | null) => void }) => Promise<void>;
-  stylesClassname?: string;
-  icon?: JSX.Element;
 }
