7ec99591905e1ca5c2bd2f78f2bfa9d1e6e35adf
Udit Takkar <udit222001@gmail.com>
2023-10-18 20:51:03 +0530

    chore: remove export

--- apps/web/components/dialog/EditLocationDialog.tsx
+++ apps/web/components/dialog/EditLocationDialog.tsx
@@ -42,7 +42,7 @@ interface ISetLocationDialog {
   teamId?: number;
 }
 
-export const LocationInput = (props: {
+const LocationInput = (props: {
   eventLocationType: EventLocationType;
   locationFormMethods: ReturnType<typeof useForm>;
   id: string;
