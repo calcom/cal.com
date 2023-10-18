37fd44f3aab31443daacf280dd4f19d7ba7307df
Dmytro Hryshyn <dev.dmytroh@gmail.com>
2023-10-18 19:30:46 +0300

    chore: server-side translations

--- apps/web/playwright/locale.e2e.ts
+++ apps/web/playwright/locale.e2e.ts
@@ -150,14 +150,14 @@ test.describe("unauthorized user sees correct translations (pt)", async () => {
 
 test.describe("unauthorized user sees correct translations (pt-br)", async () => {
   test.use({
-    locale: "pt-br",
+    locale: "pt-BR",
   });
 
   test("should use correct translations and html attributes", async ({ page }) => {
     await page.goto("/");
     await page.waitForLoadState("load");
 
-    await page.locator("html[lang=pt-br]").waitFor({ state: "attached" });
+    await page.locator("html[lang=pt-BR]").waitFor({ state: "attached" });
     await page.locator("html[dir=ltr]").waitFor({ state: "attached" });
 
     {
@@ -181,7 +181,7 @@ test.describe("unauthorized user sees correct translations (es-419)", async () =
     await page.goto("/");
     await page.waitForLoadState("load");
 
-    await page.locator("html[lang=es-419]").waitFor({ state: "attached" });
+    await page.locator("html[lang=es]").waitFor({ state: "attached" });
     await page.locator("html[dir=ltr]").waitFor({ state: "attached" });
 
     {
