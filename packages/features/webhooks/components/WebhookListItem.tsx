e7a74884ff4d1fe6e826cbe6c612bcda2ee1749f
Udit Takkar <udit222001@gmail.com>
2023-10-18 17:10:33 +0530

    fix: overflow

--- packages/features/webhooks/components/WebhookListItem.tsx
+++ packages/features/webhooks/components/WebhookListItem.tsx
@@ -71,9 +71,13 @@ export default function WebhookListItem(props: {
         "flex w-full justify-between p-4",
         props.lastItem ? "" : "border-subtle border-b"
       )}>
-      <div className="w-full truncate">
+      <div className="max-w-full truncate">
         <div className="flex">
-          <p className="text-emphasis truncate text-sm font-medium">{webhook.subscriberUrl}</p>
+          <Tooltip content={webhook.subscriberUrl}>
+            <p className="text-emphasis max-w-[600px] truncate text-sm font-medium">
+              {webhook.subscriberUrl}
+            </p>
+          </Tooltip>
           {!!props.readOnly && (
             <Badge variant="gray" className="ml-2 ">
               {t("readonly")}
