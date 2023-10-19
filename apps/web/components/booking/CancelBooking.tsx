7e5df3fbe5729cb409d200578a3bdc1f74dd2e1f
mhefuz8 <146924776+mhefuz8@users.noreply.github.com>
2023-10-19 10:03:45 +0530

    Refactor CancelBooking component to use useRef for cancelBookingRef

--- apps/web/components/booking/CancelBooking.tsx
+++ apps/web/components/booking/CancelBooking.tsx
@@ -1,8 +1,11 @@
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useCallback, useState } from "react";
-
+import React, { useCallback, useState, useRef } from "react";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
-import { collectPageParameters, telemetryEventTypes, useTelemetry } from "@calcom/lib/telemetry";
+import {
+  collectPageParameters,
+  telemetryEventTypes,
+  useTelemetry,
+} from "@calcom/lib/telemetry";
 import type { RecurringEvent } from "@calcom/types/Calendar";
 import { Button, TextArea } from "@calcom/ui";
 import { X } from "@calcom/ui/components/icon";
@@ -37,13 +40,42 @@ export default function CancelBooking(props: Props) {
   const telemetry = useTelemetry();
   const [error, setError] = useState<string | null>(booking ? null : t("booking_already_cancelled"));
 
-  const cancelBookingRef = useCallback((node: HTMLTextAreaElement) => {
-    if (node !== null) {
-      node.scrollIntoView({ behavior: "smooth" });
-      node.focus();
+  const cancelBookingRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const handleCancel = async () => {
+    setLoading(true);
+
+    telemetry.event(telemetryEventTypes.bookingCancelled, collectPageParameters());
+
+    try {
+      const res = await fetch("/api/cancel", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          uid: booking?.uid,
+          cancellationReason: cancellationReason,
+          allRemainingBookings,
+          // @NOTE: very important this shouldn't cancel with number ID use uid instead
+          seatReferenceUid,
+        }),
+      });
+
+      if (res.status >= 200 && res.status < 300) {
+        router.replace(asPath);
+      } else {
+        setLoading(false);
+        setError(
+          `${t("error_with_status_code_occurred", { status: res.status })} ${t("please_try_again")}`
+        );
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(t("an_error_occurred_please_try_again"));
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  };
+
   return (
     <>
       {error && (
@@ -63,54 +95,25 @@ export default function CancelBooking(props: Props) {
           <label className="text-default font-medium">{t("cancellation_reason")}</label>
           <TextArea
             data-testid="cancel_reason"
-            ref={cancelBookingRef}
+            ref={(node) => {
+              cancelBookingRef.current = node;
+              if (node) {
+                node.scrollIntoView({ behavior: "smooth" });
+                node.focus();
+              }
+            }}
             placeholder={t("cancellation_reason_placeholder")}
             value={cancellationReason}
             onChange={(e) => setCancellationReason(e.target.value)}
-            className="mb-4 mt-2 w-full "
+            className="mb-4 mt-2 w-full"
             rows={3}
           />
-          <div className="flex flex-col-reverse rtl:space-x-reverse ">
-            <div className="ml-auto flex w-full space-x-4 ">
-              <Button
-                className="ml-auto"
-                color="secondary"
-                onClick={() => props.setIsCancellationMode(false)}>
+          <div className="flex flex-col-reverse rtl:space-x-reverse">
+            <div className="ml-auto flex w-full space-x-4">
+              <Button className="ml-auto" color="secondary" onClick={() => props.setIsCancellationMode(false)}>
                 {t("nevermind")}
               </Button>
-              <Button
-                data-testid="confirm_cancel"
-                onClick={async () => {
-                  setLoading(true);
-
-                  telemetry.event(telemetryEventTypes.bookingCancelled, collectPageParameters());
-
-                  const res = await fetch("/api/cancel", {
-                    body: JSON.stringify({
-                      uid: booking?.uid,
-                      cancellationReason: cancellationReason,
-                      allRemainingBookings,
-                      // @NOTE: very important this shouldn't cancel with number ID use uid instead
-                      seatReferenceUid,
-                    }),
-                    headers: {
-                      "Content-Type": "application/json",
-                    },
-                    method: "POST",
-                  });
-
-                  if (res.status >= 200 && res.status < 300) {
-                    router.replace(asPath);
-                  } else {
-                    setLoading(false);
-                    setError(
-                      `${t("error_with_status_code_occured", { status: res.status })} ${t(
-                        "please_try_again"
-                      )}`
-                    );
-                  }
-                }}
-                loading={loading}>
+              <Button data-testid="confirm_cancel" onClick={handleCancel} loading={loading}>
                 {props.allRemainingBookings ? t("cancel_all_remaining") : t("cancel_event")}
               </Button>
             </div>
