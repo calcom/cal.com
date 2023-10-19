279b38b565a2562b603e18b95be2f2d71eeb907d
Joe Au-Yeung <j.auyeung419@gmail.com>
2023-10-19 15:33:59 -0400

    Add reschedule test

--- packages/app-store/googlecalendar/tests/google-calendar.e2e.ts
+++ packages/app-store/googlecalendar/tests/google-calendar.e2e.ts
@@ -1,19 +1,22 @@
 import { expect } from "@playwright/test";
 
+import dayjs from "@calcom/dayjs";
+import { APP_CREDENTIAL_SHARING_ENABLED } from "@calcom/lib/constants";
 import prisma from "@calcom/prisma";
+import type { Prisma } from "@calcom/prisma/client";
 import { test } from "@calcom/web/playwright/lib/fixtures";
-import { bookFirstEvent } from "@calcom/web/playwright/lib/testUtils";
+import { bookFirstEvent, selectSecondAvailableTimeSlotNextMonth } from "@calcom/web/playwright/lib/testUtils";
 
 import metadata from "../_metadata";
 import GoogleCalendarService from "../lib/CalendarService";
 
 test.describe("Google Calendar", async () => {
-  let qaUser;
-  let qaGCalCredential;
+  let qaUser: Prisma.UserGetPayload<{ select: { username: true } }> | null;
+  let qaGCalCredential: Prisma.CredentialGetPayload<{ select: { id: true } }> | null;
   test.beforeAll(async () => {
     let runIntegrationTest = false;
 
-    // TODO disable if credential syncing is in place
+    test.skip(!!APP_CREDENTIAL_SHARING_ENABLED, "Credential sharing enabled");
 
     if (process.env.E2E_TEST_CALCOM_QA_EMAIL && process.env.E2E_TEST_CALCOM_QA_PASSWORD) {
       qaGCalCredential = await prisma.credential.findFirst({
@@ -23,26 +26,34 @@ test.describe("Google Calendar", async () => {
           },
           type: metadata.type,
         },
+        select: {
+          id: true,
+        },
       });
 
       qaUser = await prisma.user.findFirst({
         where: {
           email: process.env.E2E_TEST_CALCOM_QA_EMAIL,
         },
+        select: {
+          username: true,
+        },
       });
 
-      if (qaGCalCredential) runIntegrationTest = true;
+      if (qaGCalCredential && qaUser) runIntegrationTest = true;
     }
 
     test.skip(!runIntegrationTest, "QA user not found");
   });
 
-  test("On new booking, event should be created on GCal", async ({ page, users }) => {
-    await page.goto(`/${qaUser.username}`);
+  test("On new booking, event should be created on GCal", async ({ page }) => {
+    await page.goto(`/${qaUser?.username}`);
     await bookFirstEvent(page);
 
     const bookingUrl = await page.url();
     const bookingUid = bookingUrl.match(/booking\/([^\/?]+)/);
+
+    expect(bookingUid).toBeTruthy();
 
     const gCalReference = await prisma.bookingReference.findFirst({
       where: {
@@ -50,29 +61,182 @@ test.describe("Google Calendar", async () => {
           uid: bookingUid[1],
         },
         type: metadata.type,
-        credentialId: qaGCalCredential.id,
-      },
-    });
-
-    const gCalCredentials = await prisma.credential.findFirst({
-      where: {
-        id: qaGCalCredential.id,
+        credentialId: qaGCalCredential?.id,
       },
       select: {
-        key: true,
+        uid: true,
+        booking: {
+          select: {
+            startTime: true,
+            endTime: true,
+            title: true,
+            attendees: {
+              select: {
+                email: true,
+              },
+            },
+            user: {
+              select: {
+                email: true,
+              },
+            },
+          },
+        },
+      },
+    });
+    expect(gCalReference).toBeTruthy();
+
+    // Need to refresh keys from DB
+    const refreshedCredential = await prisma.credential.findFirst({
+      where: {
+        id: qaGCalCredential?.id,
+      },
+      include: {
+        user: {
+          select: {
+            email: true,
+          },
+        },
       },
     });
 
-    const googleCalendarService = new GoogleCalendarService(gCalCredentials);
+    expect(refreshedCredential).toBeTruthy();
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-ignore
+    const googleCalendarService = new GoogleCalendarService(refreshedCredential);
 
     const authedCalendar = await googleCalendarService.authedCalendar();
 
-    const gCalEvent = await authedCalendar.events.get({
+    const gCalEventResponse = await authedCalendar.events.get({
       calendarId: "primary",
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-ignore
       eventId: gCalReference.uid,
     });
-    console.log("🚀 ~ file: google-calendar.e2e.ts:74 ~ test ~ gCalEvent:", gCalEvent);
 
-    expect(gCalEvent.status).toBe(200);
+    expect(gCalEventResponse.status).toBe(200);
+
+    const gCalEvent = gCalEventResponse.data;
+
+    if (!gCalReference) throw new Error("gCalReference not found");
+
+    // Ensure that the start and end times are matching
+    const startTimeMatches = dayjs(gCalReference.booking.startTime).isSame(
+      dayjs(gCalEvent.start?.dateTime).tz(gCalEvent.start.timeZone)
+    );
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-ignore
+    const endTimeMatches = dayjs(gCalReference.booking?.endTime).isSame(
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-ignore
+      dayjs(gCalEvent.end?.dateTime).tz(gCalEvent.end.timeZone)
+    );
+    expect(startTimeMatches && endTimeMatches).toBe(true);
+
+    // Ensure that the titles are matching
+    expect(gCalReference.booking.title).toBe(gCalEvent.summary);
+
+    // TODO ensure that the attendee is on the event
+    const bookingAttendee = gCalReference.booking?.attendees[0].email;
+    const attendeeInGCalEvent = gCalEvent.attendees?.find((attendee) => attendee.email === bookingAttendee);
+    expect(attendeeInGCalEvent).toBeTruthy();
+  });
+
+  test("On reschedule, event should be updated on GCal", async ({ page }) => {
+    // Reschedule the booking and check the gCalEvent's time is also changed
+    // On reschedule gCal UID stays the same
+    await page.goto(`/${qaUser?.username}`);
+    await bookFirstEvent(page);
+
+    const firstBookingUrl = await page.url();
+    const firstBookingUid = firstBookingUrl.match(/booking\/([^\/?]+)/);
+
+    // Need to find start and end times to compare with rescheduled GCal event
+    const firstBookingGCalReference = await prisma.bookingReference.findFirst({
+      where: {
+        booking: {
+          uid: firstBookingUid[1],
+        },
+        type: metadata.type,
+        credentialId: qaGCalCredential?.id,
+      },
+      select: {
+        uid: true,
+      },
+    });
+
+    // Refresh the credential from the DB
+    const refreshedCredential = await prisma.credential.findFirst({
+      where: {
+        id: qaGCalCredential?.id,
+      },
+      include: {
+        user: {
+          select: {
+            email: true,
+          },
+        },
+      },
+    });
+
+    // Grab the GCal event of the first booking
+    const googleCalendarService = new GoogleCalendarService(refreshedCredential);
+
+    const authedCalendar = await googleCalendarService.authedCalendar();
+
+    // Check that the original booking exists on GCal
+    const gCalEventResponse = await authedCalendar.events.get({
+      calendarId: "primary",
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-ignore
+      eventId: firstBookingGCalReference.uid,
+    });
+
+    expect(gCalEventResponse.status).toBe(200);
+
+    await page.locator('[data-testid="reschedule-link"]').click();
+
+    await selectSecondAvailableTimeSlotNextMonth(page);
+    await page.locator('[data-testid="confirm-reschedule-button"]').click();
+
+    await expect(page.locator("[data-testid=success-page]")).toBeVisible();
+
+    const rescheduledBookingUrl = await page.url();
+    const rescheduledBookingUid = rescheduledBookingUrl.match(/booking\/([^\/?]+)/);
+
+    // Get the rescheduled booking start and end times
+    const rescheduledBooking = await prisma.booking.findFirst({
+      where: {
+        uid: rescheduledBookingUid[1],
+      },
+      select: {
+        startTime: true,
+        endTime: true,
+      },
+    });
+
+    // The GCal event UID persists after reschedule but should get the rescheduled data
+    const gCalRescheduledEventResponse = await authedCalendar.events.get({
+      calendarId: "primary",
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-ignore
+      eventId: firstBookingGCalReference.uid,
+    });
+
+    expect(gCalRescheduledEventResponse.status).toBe(200);
+
+    const rescheduledGCalEvent = gCalRescheduledEventResponse.data;
+
+    // Ensure that the new start and end times are matching
+    const rescheduledStartTimeMatches = dayjs(rescheduledBooking.startTime).isSame(
+      dayjs(rescheduledGCalEvent.start?.dateTime).tz(rescheduledGCalEvent.start?.timeZone)
+    );
+    const rescheduledEndTimeMatches = dayjs(rescheduledBooking.endTime).isSame(
+      dayjs(rescheduledGCalEvent.end?.dateTime).tz(rescheduledGCalEvent.end.timeZone)
+    );
+    expect(rescheduledStartTimeMatches && rescheduledEndTimeMatches).toBe(true);
+
+    await page.waitForTimeout(10000);
   });
 });
