629650acf79f7d05efb8545a80fa9acbdc4294c3
Joe Au-Yeung <j.auyeung419@gmail.com>
2023-10-19 20:58:15 -0400

    Abstract common functions

--- packages/app-store/googlecalendar/tests/google-calendar.e2e.ts
+++ packages/app-store/googlecalendar/tests/google-calendar.e2e.ts
@@ -5,10 +5,10 @@ import { APP_CREDENTIAL_SHARING_ENABLED } from "@calcom/lib/constants";
 import prisma from "@calcom/prisma";
 import type { Prisma } from "@calcom/prisma/client";
 import { test } from "@calcom/web/playwright/lib/fixtures";
-import { bookFirstEvent, selectSecondAvailableTimeSlotNextMonth } from "@calcom/web/playwright/lib/testUtils";
+import { selectSecondAvailableTimeSlotNextMonth } from "@calcom/web/playwright/lib/testUtils";
 
 import metadata from "../_metadata";
-import GoogleCalendarService from "../lib/CalendarService";
+import { createBookingAndFetchGCalEvent, deleteBookingAndEvent } from "./testUtils";
 
 test.describe("Google Calendar", async () => {
   let qaUser: Prisma.UserGetPayload<{ select: { username: true } }> | null;
@@ -47,87 +47,21 @@ test.describe("Google Calendar", async () => {
   });
 
   test("On new booking, event should be created on GCal", async ({ page }) => {
-    await page.goto(`/${qaUser?.username}`);
-    await bookFirstEvent(page);
+    const { gCalEvent, gCalReference, booking, authedCalendar } = await createBookingAndFetchGCalEvent(
+      page,
+      qaGCalCredential,
+      qaUser?.username
+    );
 
-    const bookingUrl = await page.url();
-    const bookingUid = bookingUrl.match(/booking\/([^\/?]+)/);
-
-    expect(bookingUid).toBeTruthy();
-
-    const gCalReference = await prisma.bookingReference.findFirst({
-      where: {
-        booking: {
-          uid: bookingUid[1],
-        },
-        type: metadata.type,
-        credentialId: qaGCalCredential?.id,
-      },
-      select: {
-        uid: true,
-        booking: {
-          select: {
-            startTime: true,
-            endTime: true,
-            title: true,
-            attendees: {
-              select: {
-                email: true,
-              },
-            },
-            user: {
-              select: {
-                email: true,
-              },
-            },
-          },
-        },
-      },
-    });
-    expect(gCalReference).toBeTruthy();
-
-    // Need to refresh keys from DB
-    const refreshedCredential = await prisma.credential.findFirst({
-      where: {
-        id: qaGCalCredential?.id,
-      },
-      include: {
-        user: {
-          select: {
-            email: true,
-          },
-        },
-      },
-    });
-
-    expect(refreshedCredential).toBeTruthy();
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    //@ts-ignore
-    const googleCalendarService = new GoogleCalendarService(refreshedCredential);
-
-    const authedCalendar = await googleCalendarService.authedCalendar();
-
-    const gCalEventResponse = await authedCalendar.events.get({
-      calendarId: "primary",
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      //@ts-ignore
-      eventId: gCalReference.uid,
-    });
-
-    expect(gCalEventResponse.status).toBe(200);
-
-    const gCalEvent = gCalEventResponse.data;
-
-    if (!gCalReference) throw new Error("gCalReference not found");
+    // if (!gCalReference) throw new Error("gCalReference not found");
 
     // Ensure that the start and end times are matching
-    const startTimeMatches = dayjs(gCalReference.booking.startTime).isSame(
+    const startTimeMatches = dayjs(booking.startTime).isSame(
       dayjs(gCalEvent.start?.dateTime).tz(gCalEvent.start.timeZone)
     );
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     //@ts-ignore
-    const endTimeMatches = dayjs(gCalReference.booking?.endTime).isSame(
+    const endTimeMatches = dayjs(booking.endTime).isSame(
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       //@ts-ignore
       dayjs(gCalEvent.end?.dateTime).tz(gCalEvent.end.timeZone)
@@ -135,77 +69,24 @@ test.describe("Google Calendar", async () => {
     expect(startTimeMatches && endTimeMatches).toBe(true);
 
     // Ensure that the titles are matching
-    expect(gCalReference.booking.title).toBe(gCalEvent.summary);
+    expect(booking.title).toBe(gCalEvent.summary);
 
     // TODO ensure that the attendee is on the event
-    const bookingAttendee = gCalReference.booking?.attendees[0].email;
+    const bookingAttendee = booking?.attendees[0].email;
     const attendeeInGCalEvent = gCalEvent.attendees?.find((attendee) => attendee.email === bookingAttendee);
     expect(attendeeInGCalEvent).toBeTruthy();
 
-    // After test passes we can delete the booking and GCal event
-    await prisma.booking.delete({
-      where: {
-        uid: bookingUid[1],
-      },
-    });
-
-    await authedCalendar.events.delete({
-      calendarId: "primary",
-      eventId: gCalReference.uid,
-    });
+    await deleteBookingAndEvent(authedCalendar, booking.uid, gCalReference.uid);
   });
 
   test("On reschedule, event should be updated on GCal", async ({ page }) => {
     // Reschedule the booking and check the gCalEvent's time is also changed
     // On reschedule gCal UID stays the same
-    await page.goto(`/${qaUser?.username}`);
-    await bookFirstEvent(page);
-
-    const firstBookingUrl = await page.url();
-    const firstBookingUid = firstBookingUrl.match(/booking\/([^\/?]+)/);
-
-    // Need to find start and end times to compare with rescheduled GCal event
-    const gCalReference = await prisma.bookingReference.findFirst({
-      where: {
-        booking: {
-          uid: firstBookingUid[1],
-        },
-        type: metadata.type,
-        credentialId: qaGCalCredential?.id,
-      },
-      select: {
-        uid: true,
-      },
-    });
-
-    // Refresh the credential from the DB
-    const refreshedCredential = await prisma.credential.findFirst({
-      where: {
-        id: qaGCalCredential?.id,
-      },
-      include: {
-        user: {
-          select: {
-            email: true,
-          },
-        },
-      },
-    });
-
-    // Grab the GCal event of the first booking
-    const googleCalendarService = new GoogleCalendarService(refreshedCredential);
-
-    const authedCalendar = await googleCalendarService.authedCalendar();
-
-    // Check that the original booking exists on GCal
-    const gCalEventResponse = await authedCalendar.events.get({
-      calendarId: "primary",
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      //@ts-ignore
-      eventId: gCalReference.uid,
-    });
-
-    expect(gCalEventResponse.status).toBe(200);
+    const { gCalReference, booking, authedCalendar } = await createBookingAndFetchGCalEvent(
+      page,
+      qaGCalCredential,
+      qaUser?.username
+    );
 
     await page.locator('[data-testid="reschedule-link"]').click();
 
@@ -249,73 +130,24 @@ test.describe("Google Calendar", async () => {
     );
     expect(rescheduledStartTimeMatches && rescheduledEndTimeMatches).toBe(true);
 
-    // After test passes we can delete the booking and GCal event
+    // After test passes we can delete the bookings and GCal event
+    await deleteBookingAndEvent(authedCalendar, booking.uid, gCalReference.uid);
+
     await prisma.booking.delete({
       where: {
         uid: rescheduledBookingUid[1],
       },
     });
-
-    await authedCalendar.events.delete({
-      calendarId: "primary",
-      eventId: gCalReference.uid,
-    });
   });
 
   test("When canceling the booking, the GCal event should also be deleted", async ({ page }) => {
-    // Book an event
-    await page.goto(`/${qaUser?.username}`);
-    await bookFirstEvent(page);
+    const { gCalEvent, gCalReference, booking, authedCalendar } = await createBookingAndFetchGCalEvent(
+      page,
+      qaGCalCredential,
+      qaUser?.username
+    );
 
-    const bookingUrl = await page.url();
-    const bookingUid = bookingUrl.match(/booking\/([^\/?]+)/);
-    // Get the bookingUID and ensure that it exits on GCal
-
-    const gCalReference = await prisma.bookingReference.findFirst({
-      where: {
-        booking: {
-          uid: bookingUid[1],
-        },
-        type: metadata.type,
-        credentialId: qaGCalCredential?.id,
-      },
-      select: {
-        uid: true,
-      },
-    });
-
-    // Need to refresh keys from DB
-    const refreshedCredential = await prisma.credential.findFirst({
-      where: {
-        id: qaGCalCredential?.id,
-      },
-      include: {
-        user: {
-          select: {
-            email: true,
-          },
-        },
-      },
-    });
-
-    expect(refreshedCredential).toBeTruthy();
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    //@ts-ignore
-    const googleCalendarService = new GoogleCalendarService(refreshedCredential);
-
-    const authedCalendar = await googleCalendarService.authedCalendar();
-
-    const gCalEventResponse = await authedCalendar.events.get({
-      calendarId: "primary",
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      //@ts-ignore
-      eventId: gCalReference.uid,
-    });
-
-    expect(gCalEventResponse.status).toBe(200);
     // Cancel the booking
-
     await page.locator('[data-testid="cancel"]').click();
     await page.locator('[data-testid="confirm_cancel"]').click();
     // Query for the bookingUID and ensure that it doesn't exist on GCal
@@ -331,16 +163,7 @@ test.describe("Google Calendar", async () => {
 
     expect(canceledGCalEventResponse.data.status).toBe("cancelled");
 
-    // After test passes we can delete the booking and GCal event
-    await prisma.booking.delete({
-      where: {
-        uid: bookingUid[1],
-      },
-    });
-
-    await authedCalendar.events.delete({
-      calendarId: "primary",
-      eventId: gCalReference.uid,
-    });
+    // GCal API sees canceled events as already deleted
+    await deleteBookingAndEvent(authedCalendar, booking.uid);
   });
 });
