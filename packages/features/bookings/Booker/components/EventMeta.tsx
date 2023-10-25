f9ad99e5728e317739c0b744696f6925c0b0ed75
Siddharth Movaliya <siddraj1992@gmail.com>
2023-10-25 23:46:01 +0530

    feat: Lock timezone on booking page (#11891)
    Co-authored-by: CarinaWolli <wollencarina@gmail.com>

--- packages/features/bookings/Booker/components/EventMeta.tsx
+++ packages/features/bookings/Booker/components/EventMeta.tsx
@@ -105,6 +105,7 @@ export const EventMeta = () => {
               </EventMetaBlock>
             )}
             <EventDetails event={event} />
+
             <EventMetaBlock
               className="cursor-pointer [&_.current-timezone:before]:focus-within:opacity-100 [&_.current-timezone:before]:hover:opacity-100"
               contentClassName="relative max-w-[90%]"
@@ -112,7 +113,10 @@ export const EventMeta = () => {
               {bookerState === "booking" ? (
                 <>{timezone}</>
               ) : (
-                <span className="min-w-32 current-timezone before:bg-subtle -mt-[2px] flex h-6 max-w-full items-center justify-start before:absolute before:inset-0 before:bottom-[-3px] before:left-[-30px] before:top-[-3px] before:w-[calc(100%_+_35px)] before:rounded-md before:py-3 before:opacity-0 before:transition-opacity">
+                <span
+                  className={`min-w-32 current-timezone before:bg-subtle -mt-[2px] flex h-6 max-w-full items-center justify-start before:absolute before:inset-0 before:bottom-[-3px] before:left-[-30px] before:top-[-3px] before:w-[calc(100%_+_35px)] before:rounded-md before:py-3 before:opacity-0 before:transition-opacity ${
+                    event.lockTimeZoneToggleOnBookingPage ? "cursor-not-allowed" : ""
+                  }`}>
                   <TimezoneSelect
                     menuPosition="fixed"
                     classNames={{
@@ -124,6 +128,7 @@ export const EventMeta = () => {
                     }}
                     value={timezone}
                     onChange={(tz) => setTimezone(tz.value)}
+                    isDisabled={event.lockTimeZoneToggleOnBookingPage}
                   />
                 </span>
               )}
