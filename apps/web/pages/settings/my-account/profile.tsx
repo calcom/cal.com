273444d8e2a0fc7f55cb86b27dfbdf095b44de42
Udit Takkar <udit222001@gmail.com>
2023-10-26 17:26:44 +0530

    fix: padding in footer in profile

--- apps/web/pages/settings/my-account/profile.tsx
+++ apps/web/pages/settings/my-account/profile.tsx
@@ -283,8 +283,8 @@ const ProfileView = () => {
       />
 
       <div className="border-subtle mt-6 rounded-lg rounded-b-none border border-b-0 p-6">
-        <Label className="text-base font-semibold text-red-700">{t("danger_zone")}</Label>
-        <p className="text-subtle">{t("account_deletion_cannot_be_undone")}</p>
+        <Label className="mb-0 text-base font-semibold text-red-700">{t("danger_zone")}</Label>
+        <p className="text-subtle text-sm">{t("account_deletion_cannot_be_undone")}</p>
       </div>
       {/* Delete account Dialog */}
       <Dialog open={deleteAccountOpen} onOpenChange={setDeleteAccountOpen}>
