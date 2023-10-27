29ed797098236ea72dbc6fbd58b0f0709ac78f61
Benny Joo <sldisek783@gmail.com>
2023-10-27 19:29:27 +0100

    Add metadata for 3 root pages

--- apps/web/app/future/(layout)/availability/page.tsx
+++ apps/web/app/future/(layout)/availability/page.tsx
@@ -1,3 +1,39 @@
 import AvailabilityPage from "@pages/availability";
+import { headers } from "next/headers";
+
+import { constructGenericImage } from "@calcom/lib/OgImages";
+import { IS_CALCOM, WEBAPP_URL, APP_NAME, SEO_IMG_OGIMG } from "@calcom/lib/constants";
+import { getFixedT } from "@calcom/lib/server/getFixedT";
+
+import { preparePageMetadata } from "@lib/metadata";
+
+export const generateMetadata = async () => {
+  const h = headers();
+  const canonical = h.get("x-pathname") ?? "";
+  const locale = h.get("x-locale") ?? "en";
+
+  const t = await getFixedT(locale, "common");
+
+  const title = t("availability");
+  const description = t("configure_availability");
+
+  const metadataBase = new URL(IS_CALCOM ? "https://cal.com" : WEBAPP_URL);
+
+  const image =
+    SEO_IMG_OGIMG +
+    constructGenericImage({
+      title,
+      description,
+    });
+
+  return preparePageMetadata({
+    title,
+    canonical,
+    image,
+    description,
+    siteName: APP_NAME,
+    metadataBase,
+  });
+};
 
 export default AvailabilityPage;
--- apps/web/app/future/(layout)/workflows/page.tsx
+++ apps/web/app/future/(layout)/workflows/page.tsx
@@ -1,6 +1,42 @@
+import { headers } from "next/headers";
+
 import Workflows from "@calcom/features/ee/workflows/pages/index";
+import { constructGenericImage } from "@calcom/lib/OgImages";
+import { IS_CALCOM, WEBAPP_URL, APP_NAME, SEO_IMG_OGIMG } from "@calcom/lib/constants";
+import { getFixedT } from "@calcom/lib/server/getFixedT";
+
+import { preparePageMetadata } from "@lib/metadata";
 
 import type { CalPageWrapper } from "@components/PageWrapper";
+
+export const generateMetadata = async () => {
+  const h = headers();
+  const canonical = h.get("x-pathname") ?? "";
+  const locale = h.get("x-locale") ?? "en";
+
+  const t = await getFixedT(locale, "common");
+
+  const title = t("workflows");
+  const description = t("workflows_to_automate_notifications");
+
+  const metadataBase = new URL(IS_CALCOM ? "https://cal.com" : WEBAPP_URL);
+
+  const image =
+    SEO_IMG_OGIMG +
+    constructGenericImage({
+      title,
+      description,
+    });
+
+  return preparePageMetadata({
+    title,
+    canonical,
+    image,
+    description,
+    siteName: APP_NAME,
+    metadataBase,
+  });
+};
 
 const WorkflowsPage = Workflows as CalPageWrapper;
 
