d1c2fd2c9ca822a5ee89310822076bc58428e984
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-11-01 00:22:07 +0530

    add empty screen component to list view

--- packages/atoms/EventTypeList/EventTypeList.tsx
+++ packages/atoms/EventTypeList/EventTypeList.tsx
@@ -1,4 +1,25 @@
-export function EventTypeList() {
+import { CreateFirstEventTypeView } from "EventTypeList/components/empty-screen/createFirstEventType";
+import { EmptyEventTypeList } from "EventTypeList/components/empty-screen/emptyEventTypeList";
+
+export function EventTypeList({
+  group,
+  groupIndex,
+  readOnly,
+  types,
+}: {
+  group: any;
+  groupIndex: number;
+  readOnly: boolean;
+  types: any;
+}): JSX.Element {
+  if (!types.length) {
+    return group.teamId ? (
+      <EmptyEventTypeList group={group} />
+    ) : (
+      <CreateFirstEventTypeView slug={group.profile.slug ?? ""} />
+    );
+  }
+
   return (
     <div>
       <h1>Event type list goes here</h1>
