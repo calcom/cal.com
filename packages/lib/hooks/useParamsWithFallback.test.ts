cf5a29522c684ec266c0cf555f5791e59e8ee02c
Benny Joo <sldisek783@gmail.com>
2023-11-02 18:04:57 +0000

    fix: second solution by importing router from next/compat/router

--- packages/lib/hooks/useParamsWithFallback.test.ts
+++ packages/lib/hooks/useParamsWithFallback.test.ts
@@ -5,13 +5,13 @@ import { describe, expect, it } from "vitest";
 import { useParamsWithFallback } from "./useParamsWithFallback";
 
 describe("useParamsWithFallback hook", () => {
-  it("should return router.query when router.query exists", () => {
+  it("should return router.query when param is null", () => {
     vi.mock("next/navigation", () => ({
       useParams: vi.fn().mockReturnValue(null),
     }));
 
-    vi.mock("react", () => ({
-      useContext: vi.fn().mockReturnValue({ query: { id: 1 } }),
+    vi.mock("next/compat/router", () => ({
+      useRouter: vi.fn().mockReturnValue({ query: { id: 1 } }),
     }));
 
     const { result } = renderHook(() => useParamsWithFallback());
@@ -19,13 +19,13 @@ describe("useParamsWithFallback hook", () => {
     expect(result.current).toEqual({ id: 1 });
   });
 
-  it("should return useParams() when router is undefined", () => {
+  it("should return router.query when param is undefined", () => {
     vi.mock("next/navigation", () => ({
-      useParams: vi.fn().mockReturnValue({ id: 1 }),
+      useParams: vi.fn().mockReturnValue(undefined),
     }));
 
-    vi.mock("react", () => ({
-      useContext: vi.fn().mockReturnValue(undefined),
+    vi.mock("next/compat/router", () => ({
+      useRouter: vi.fn().mockReturnValue({ query: { id: 1 } }),
     }));
 
     const { result } = renderHook(() => useParamsWithFallback());
@@ -33,13 +33,27 @@ describe("useParamsWithFallback hook", () => {
     expect(result.current).toEqual({ id: 1 });
   });
 
-  it("should return useParams() when router is null", () => {
+  it("should return useParams() if it exists", () => {
     vi.mock("next/navigation", () => ({
       useParams: vi.fn().mockReturnValue({ id: 1 }),
     }));
 
-    vi.mock("react", () => ({
-      useContext: vi.fn().mockReturnValue(null),
+    vi.mock("next/compat/router", () => ({
+      useRouter: vi.fn().mockReturnValue(null),
+    }));
+
+    const { result } = renderHook(() => useParamsWithFallback());
+
+    expect(result.current).toEqual({ id: 1 });
+  });
+
+  it("should return useParams() if it exists", () => {
+    vi.mock("next/navigation", () => ({
+      useParams: vi.fn().mockReturnValue({ id: 1 }),
+    }));
+
+    vi.mock("next/compat/router", () => ({
+      useRouter: vi.fn().mockReturnValue({ query: { id: 2 } }),
     }));
 
     const { result } = renderHook(() => useParamsWithFallback());
--- packages/lib/hooks/useParamsWithFallback.ts
+++ packages/lib/hooks/useParamsWithFallback.ts
@@ -1,8 +1,7 @@
 "use client";
 
-import { RouterContext } from "next/dist/shared/lib/router-context";
+import { useRouter as useCompatRouter } from "next/compat/router";
 import { useParams } from "next/navigation";
-import { useContext } from "react";
 
 interface Params {
   [key: string]: string | string[];
@@ -12,14 +11,7 @@ interface Params {
  * This hook is a workaround until pages are migrated to app directory.
  */
 export function useParamsWithFallback(): Params {
-  const params = useParams();
-  // `Error: NextRouter was not mounted` is thrown if `useRouter` from `next/router` is called in App router.
-  // As can be seen in https://github.com/vercel/next.js/blob/e8a92a9507cff7d5f7b52701089d4b8141126a63/packages/next/src/client/router.ts#L132,
-  // `useRouter()` hook returns `React.useContext(RouterContext)`.
-  // Hence, We can directly use this value.
-  const router = useContext(RouterContext);
-  if (router) {
-    return (router.query ?? {}) as Params;
-  }
-  return params ?? {};
+  const params = useParams(); // always `null` in pages router
+  const router = useCompatRouter(); // always `null` in app router
+  return params ?? router?.query ?? {};
 }
