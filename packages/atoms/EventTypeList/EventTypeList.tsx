8e271b66202165bf497136940c5456cf09622aa0
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-11-03 23:44:10 +0530

    fix typings

--- packages/atoms/EventTypeList/EventTypeList.tsx
+++ packages/atoms/EventTypeList/EventTypeList.tsx
@@ -1,17 +1,21 @@
 import { CreateFirstEventTypeView } from "EventTypeList/components/empty-screen/createFirstEventType";
 import { EmptyEventTypeList } from "EventTypeList/components/empty-screen/emptyEventTypeList";
 
-export function EventTypeList({
-  group,
-  groupIndex,
-  readOnly,
-  types,
-}: {
-  group: any;
+import type { RouterOutputs } from "@calcom/trpc";
+
+type EventTypeGroups = RouterOutputs["viewer"]["eventTypes"]["getByViewer"]["eventTypeGroups"];
+
+export type EventTypeGroup = EventTypeGroups[number];
+export type EventType = EventTypeGroup["eventTypes"][number];
+
+type EventTypeListProps = {
+  group: EventTypeGroup;
   groupIndex: number;
   readOnly: boolean;
-  types: any;
-}): JSX.Element {
+  types: EventType[];
+};
+
+export function EventTypeList({ group, groupIndex, readOnly, types }: EventTypeListProps): JSX.Element {
   if (!types.length) {
     return group.teamId ? (
       <EmptyEventTypeList group={group} />
