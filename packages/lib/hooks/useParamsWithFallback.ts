08b84cd58a3f10283f2bb5a0132d7407d12eca88
Benny Joo <sldisek783@gmail.com>
2023-11-03 16:06:07 +0000

    fix return type

--- packages/lib/hooks/useParamsWithFallback.ts
+++ packages/lib/hooks/useParamsWithFallback.ts
@@ -2,6 +2,7 @@
 
 import { useRouter as useCompatRouter } from "next/compat/router";
 import { useParams } from "next/navigation";
+import type { ParsedUrlQuery } from "querystring";
 
 interface Params {
   [key: string]: string | string[];
@@ -10,7 +11,7 @@ interface Params {
 /**
  * This hook is a workaround until pages are migrated to app directory.
  */
-export function useParamsWithFallback(): Params {
+export function useParamsWithFallback(): Params | ParsedUrlQuery {
   const params = useParams(); // always `null` in pages router
   const router = useCompatRouter(); // always `null` in app router
   return params ?? router?.query ?? {};
