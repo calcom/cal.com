f2b20c36b86adbf2bc9b2ad7656985269cb1b1e5
supalarry <lauris.skraucis@gmail.com>
2023-11-03 08:54:27 +0100

    refactor: structure using folders to prepare for test files

--- packages/core/getAggregatedAvailability/date-range-utils/mergeOverlappingDateRanges/index.ts
+++ packages/core/getAggregatedAvailability/date-range-utils/mergeOverlappingDateRanges/index.ts
@@ -1,28 +1,6 @@
 import type { DateRange } from "@calcom/lib/date-ranges";
-import { intersect } from "@calcom/lib/date-ranges";
-import { SchedulingType } from "@calcom/prisma/enums";
 
-export const getAggregatedAvailability = (
-  userAvailability: { dateRanges: DateRange[]; user?: { isFixed?: boolean } }[],
-  schedulingType: SchedulingType | null
-): DateRange[] => {
-  const fixedHosts = userAvailability.filter(
-    ({ user }) => !schedulingType || schedulingType === SchedulingType.COLLECTIVE || user?.isFixed
-  );
-
-  const dateRangesToIntersect = fixedHosts.map((s) => s.dateRanges);
-
-  const unfixedHosts = userAvailability.filter(({ user }) => user?.isFixed !== true);
-  if (unfixedHosts.length) {
-    dateRangesToIntersect.push(unfixedHosts.flatMap((s) => s.dateRanges));
-  }
-
-  const availability = intersect(dateRangesToIntersect);
-
-  return mergeOverlappingDateRanges(availability);
-};
-
-function mergeOverlappingDateRanges(dateRanges: DateRange[]) {
+export function mergeOverlappingDateRanges(dateRanges: DateRange[]) {
   dateRanges.sort((a, b) => a.start.valueOf() - b.start.valueOf());
 
   const mergedDateRanges: DateRange[] = [];
