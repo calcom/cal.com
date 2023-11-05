0d5bfca2e37e776db68ea14b904fe4835a331f86
Aditya Kumar <callmecaprian@gmail.com>
2023-11-06 00:02:50 +0530

    change

--- apps/storybook/.storybook/YourTheme.js
+++ apps/storybook/.storybook/YourTheme.js
@@ -3,7 +3,7 @@ import { create } from "@storybook/theming";
 export default create({
   base: "light",
   colorPrimary: "#111827",
-  colorSecondary: "#111827",
+  colorSecondary: "#F9FAFC",
   appContentBg: "#F9FAFC",
   brandTitle: "Cal.com UI",
   brandUrl: "https://cal.com",
