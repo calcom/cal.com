a74e5f275523de5c3f8410a23af7ac964e33eb1b
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-11-08 18:27:58 +0530

    update view for mobile content

--- packages/atoms/event-type-list/components/controls/MobileDropdownContent.tsx
+++ packages/atoms/event-type-list/components/controls/MobileDropdownContent.tsx
@@ -1,21 +1,43 @@
 import { Button } from "@/components/ui/button";
-import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
-import { ExternalLink } from "lucide-react";
+import { DropdownMenuItem, DropdownMenuSeparator } from "@/components/ui/dropdown-menu";
+import { Switch } from "@/components/ui/switch";
+import type { EventTypeGroup } from "event-type-list";
+import { ExternalLink, Upload, Edit, Copy, Trash } from "lucide-react";
+
+import { Skeleton, Label } from "@calcom/ui";
 
 type MobileDropdownContentProps = {
+  group: EventTypeGroup;
   isManagedEventType: boolean;
-  calLink: string;
+  isChildrenManagedEventType: boolean;
+  isNativeShare: boolean;
+  isHidden: boolean;
+  readOnly: boolean;
   id: number;
-  onPreview: (link: string) => void;
-  onCopy: (link: string) => void;
+  onPreview: () => void;
+  onCopy: () => void;
+  onShare: () => void;
+  onEdit: () => void;
+  onDuplicate: () => void;
+  onDelete: () => void;
+  onMutate: () => void;
 };
 
 export function MobileDropdownContent({
+  group,
   isManagedEventType,
-  calLink,
+  isChildrenManagedEventType,
+  isNativeShare,
+  isHidden,
+  readOnly,
   id,
   onPreview,
   onCopy,
+  onShare,
+  onEdit,
+  onDuplicate,
+  onDelete,
+  onMutate,
 }: MobileDropdownContentProps) {
   return (
     <>
@@ -23,9 +45,7 @@ export function MobileDropdownContent({
         <>
           <DropdownMenuItem className="outline-none">
             <Button
-              onClick={() => {
-                onPreview(calLink);
-              }}
+              onClick={onPreview}
               data-testid="preview-link-button"
               type="button"
               className="w-full rounded-none">
@@ -38,13 +58,59 @@ export function MobileDropdownContent({
               data-testid={`event-type-duplicate-${id}`}
               type="button"
               className="w-full rounded-none text-left"
-              onClick={() => {
-                onCopy(calLink);
-              }}>
+              onClick={onCopy}>
               Copy link to event
             </Button>
           </DropdownMenuItem>
         </>
+      )}
+      {isNativeShare ? (
+        <DropdownMenuItem className="outline-none">
+          <Button
+            data-testid={`event-type-duplicate-${id}`}
+            type="button"
+            className="w-full rounded-none"
+            onClick={onShare}>
+            <Upload /> Share
+          </Button>
+        </DropdownMenuItem>
+      ) : null}
+      {readOnly && (
+        <DropdownMenuItem className="outline-none">
+          <Button type="button" className="w-full rounded-none" onClick={onEdit}>
+            <Edit /> Edit
+          </Button>
+        </DropdownMenuItem>
+      )}
+      {!isManagedEventType && !isChildrenManagedEventType && (
+        <DropdownMenuItem className="outline-none">
+          <Button data-testid={`event-type-duplicate-${id}`} onClick={onDuplicate}>
+            <Copy />
+            Duplicate
+          </Button>
+        </DropdownMenuItem>
+      )}
+      {(group.metadata?.readOnly === false || group.metadata.readOnly === null) &&
+        !isChildrenManagedEventType && (
+          <>
+            <DropdownMenuItem className="outline-none">
+              <Button variant="destructive" className="w-full rounded-none" onClick={onDelete}>
+                <Trash /> Delete
+              </Button>
+            </DropdownMenuItem>
+          </>
+        )}
+      <DropdownMenuSeparator />
+      {!isManagedEventType && (
+        <div className="hover:bg-subtle flex h-9 cursor-pointer flex-row items-center justify-between px-4 py-2">
+          <Skeleton
+            as={Label}
+            htmlFor="hiddenSwitch"
+            className="mt-2 inline cursor-pointer self-center pr-2 ">
+            {isHidden ? "Show on profile" : "Hide from profile"}
+          </Skeleton>
+          <Switch id="hiddenSwitch" name="Hidden" checked={isHidden} onCheckedChange={onMutate} />
+        </div>
       )}
     </>
   );
