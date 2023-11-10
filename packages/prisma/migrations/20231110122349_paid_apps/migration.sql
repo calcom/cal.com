ea6f818de70bb23a1f94ad41c40de02e8029b372
exception <erik@erosemberg.com>
2023-11-10 09:25:24 -0300

    chore: Ensure index

--- packages/prisma/migrations/20231110122349_paid_apps/migration.sql
+++ packages/prisma/migrations/20231110122349_paid_apps/migration.sql
@@ -2,3 +2,6 @@
 ALTER TABLE "Credential" ADD COLUMN     "billingCycleStart" INTEGER,
 ADD COLUMN     "paymentStatus" TEXT,
 ADD COLUMN     "subscriptionId" TEXT;
+
+-- CreateIndex
+CREATE INDEX "Credential_subscriptionId_idx" ON "Credential"("subscriptionId");
