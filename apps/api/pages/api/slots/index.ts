df106601bb445fb85ebede660d09de75aab87630
Keith Williams <keithwillcode@gmail.com>
2023-11-10 08:58:39 -0300

    Trying to set the memory directly in the handler file

--- apps/api/pages/api/slots/index.ts
+++ apps/api/pages/api/slots/index.ts
@@ -2,6 +2,10 @@ import { defaultHandler } from "@calcom/lib/server";
 
 import { withMiddleware } from "~/lib/helpers/withMiddleware";
 
+export const config = {
+  memory: 512,
+};
+
 export default withMiddleware()(
   defaultHandler({
     GET: import("./_get"),
