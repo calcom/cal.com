c8de1b7487a36a59d9d58b6064fdf65ecab92e16
Peer Richelsen <peer@cal.com>
2023-11-12 18:44:21 +0000

    added i18n

--- apps/web/pages/enterprise/index.tsx
+++ apps/web/pages/enterprise/index.tsx
@@ -2,7 +2,6 @@ import { getLayout } from "@calcom/features/MainLayout";
 import { ShellMain } from "@calcom/features/shell/Shell";
 import { UpgradeTip } from "@calcom/features/tips";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
-import { trpc } from "@calcom/trpc";
 import { Button, ButtonGroup } from "@calcom/ui";
 import { BarChart, CreditCard, Globe, Lock, Paintbrush, Users } from "@calcom/ui/components/icon";
 
@@ -10,49 +9,46 @@ import PageWrapper from "@components/PageWrapper";
 
 export default function EnterprisePage() {
   const { t } = useLocale();
-  const { data: user } = trpc.viewer.me.useQuery();
 
   const features = [
     {
       icon: <Globe className="h-5 w-5 text-red-500" />,
-      title: t("Branded Subdomain"),
-      description: t("Get your own branded subdomain, such as acme.cal.com"),
+      title: t("branded_subdomain"),
+      description: t("branded_subdomain_description"),
     },
     {
       icon: <BarChart className="h-5 w-5 text-blue-500" />,
-      title: t("Organization-wide Insights"),
-      description: t("Understand how your entire organization is spending time"),
+      title: t("org_insights"),
+      description: t("org_insights_description"),
     },
     {
       icon: <Paintbrush className="h-5 w-5 text-pink-500" />,
-      title: t("Extensive Whitelabeling"),
-      description: t("Customize your scheduling experience with your own logo, colors, and more"),
+      title: t("extensive_whitelabeling"),
+      description: t("extensive_whitelabeling_description"),
     },
     {
       icon: <Users className="h-5 w-5 text-orange-500" />,
-      title: t("Unlimited Teams"),
-      description: t("Add as many subteams as you need to your organization"),
+      title: t("unlimited_teams"),
+      description: t("unlimited_teams_description"),
     },
     {
       icon: <CreditCard className="h-5 w-5 text-green-500" />,
-      title: t("Unified Billing"),
-      description: t("Add a single credit card to pay for all your team's subscriptions"),
+      title: t("unified_billing"),
+      description: t("unified_billing_description"),
     },
     {
       icon: <Lock className="h-5 w-5 text-purple-500" />,
-      title: t("Advanced Managed Event Types"),
-      description: t("Add a single credit card to pay for all your team's subscriptions"),
+      title: t("advanced_managed_events"),
+      description: t("advanced_managed_events_description"),
     },
   ];
   return (
     <div>
-      <ShellMain heading="Enterprise" subtitle={t("Upgrade to Enterprise to create your Organization")}>
+      <ShellMain heading="Enterprise" subtitle={t("enterprise_description")}>
         <UpgradeTip
           plan="enterprise"
-          title={t("Create your Organization")}
-          description={t(
-            "Upgrade to Enterprise and receive a subdomain, unified billing, Insights, extensive whitelabeling and more"
-          )}
+          title={t("create_your_org")}
+          description={t("create_your_org_description")}
           features={features}
           background="/tips/enterprise"
           buttons={
