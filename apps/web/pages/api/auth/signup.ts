c973696be50b2f0adb3359d4b6699d2ad7b356f6
Sean Brydon <sean@cal.com>
2023-11-14 11:52:11 +0000

    user default handler

--- apps/web/pages/api/auth/signup.ts
+++ apps/web/pages/api/auth/signup.ts
@@ -5,15 +5,7 @@ import selfHostedSignupHandler from "@calcom/feature-auth/signup/handlers/selfHo
 import { type RequestWithUsernameStatus } from "@calcom/features/auth/signup/username";
 import { IS_CALCOM } from "@calcom/lib/constants";
 import { HttpError } from "@calcom/lib/http-error";
-
-function ensureReqIsPost(req: RequestWithUsernameStatus) {
-  if (req.method !== "POST") {
-    throw new HttpError({
-      statusCode: 405,
-      message: "Method not allowed",
-    });
-  }
-}
+import { defaultHandler } from "@calcom/lib/server";
 
 function ensureSignupIsEnabled() {
   if (process.env.NEXT_PUBLIC_DISABLE_SIGNUP === "true") {
@@ -24,7 +16,7 @@ function ensureSignupIsEnabled() {
   }
 }
 
-export default async function handler(req: RequestWithUsernameStatus, res: NextApiResponse) {
+async function handler(req: RequestWithUsernameStatus, res: NextApiResponse) {
   // Use a try catch instead of returning res every time
   try {
     ensureReqIsPost(req);
@@ -48,3 +40,7 @@ export default async function handler(req: RequestWithUsernameStatus, res: NextA
     return res.status(500).json({ message: "Internal server error" });
   }
 }
+
+export default defaultHandler({
+  POST: handler,
+});
