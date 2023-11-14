192ba227073fd3e748477e3dff94af5c3a2dea12
Peer Richelsen <peer@cal.com>
2023-11-14 16:00:19 +0000

    fixed mobile view

--- packages/features/bookings/Booker/Booker.tsx
+++ packages/features/bookings/Booker/Booker.tsx
@@ -378,37 +378,39 @@ export const InstantBooking = () => {
   const { t } = useLocale();
 
   return (
-    <div className=" bg-default border-subtle shadow-xs mx-2 flex items-center gap-3 rounded-xl border p-[6px] text-sm delay-1000">
-      <div className="relative flex items-center ps-1">
+    <div className=" bg-default border-subtle mx-2 block items-center gap-3 rounded-xl border p-[6px] text-sm shadow-sm delay-1000 sm:flex">
+      <div className="flex items-center gap-3 ps-1">
         {/* TODO: max. show 4 people here */}
-        <AvatarGroup
-          size="sm"
-          className="relative"
-          items={[
-            {
-              image: "https://cal.com/stakeholder/peer.jpg",
-              alt: "Peer",
-              title: "Peer Richelsen",
-            },
-            {
-              image: "https://cal.com/stakeholder/bailey.jpg",
-              alt: "Bailey",
-              title: "Bailey Pumfleet",
-            },
-            {
-              image: "https://cal.com/stakeholder/alex-van-andel.jpg",
-              alt: "Alex",
-              title: "Alex Van Andel",
-            },
-          ]}
-        />
-        <div className="border-muted absolute -bottom-0.5 -right-1 h-2 w-2 rounded-full border bg-green-500" />
+        <div className="relative">
+          <AvatarGroup
+            size="sm"
+            className="relative"
+            items={[
+              {
+                image: "https://cal.com/stakeholder/peer.jpg",
+                alt: "Peer",
+                title: "Peer Richelsen",
+              },
+              {
+                image: "https://cal.com/stakeholder/bailey.jpg",
+                alt: "Bailey",
+                title: "Bailey Pumfleet",
+              },
+              {
+                image: "https://cal.com/stakeholder/alex-van-andel.jpg",
+                alt: "Alex",
+                title: "Alex Van Andel",
+              },
+            ]}
+          />
+          <div className="border-muted absolute -bottom-0.5 -right-1 h-2 w-2 rounded-full border bg-green-500" />
+        </div>
+        <div>{t("dont_want_to_wait")}</div>
       </div>
-      <div className="hidden sm:block">{t("dont_want_to_wait")}</div>
-      <div>
+      <div className="mt-2 sm:mt-0">
         {/* TODO: onClick listener that takes data from the immediate time */}
-        <Button color="primary" size="sm" className="rounded-lg">
-          {t("call_immediately")}
+        <Button color="primary" size="sm" className="w-full justify-center rounded-lg sm:w-auto">
+          {t("connect_now")}
         </Button>
       </div>
     </div>
