5502f66cddfcdce646d67a47f46c8a9a17436909
Udit Takkar <udit222001@gmail.com>
2023-11-15 18:42:02 +0530

    chore

--- packages/features/bookings/lib/handleNewBooking.ts
+++ packages/features/bookings/lib/handleNewBooking.ts
@@ -632,7 +632,6 @@ async function handler(
 ) {
   const { userId } = req;
 
-  throw new Error("Not implemented");
   // handle dynamic user
   let eventType =
     !req.body.eventTypeId && !!req.body.eventTypeSlug
