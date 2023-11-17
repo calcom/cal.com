1e7ff9ce369978efbe14c5eb18ba4c5c368245eb
Peer Richelsen <peer@cal.com>
2023-11-17 23:52:15 +0000

    paid support wip

--- packages/features/ee/support/components/ContactMenuItem.tsx
+++ packages/features/ee/support/components/ContactMenuItem.tsx
@@ -1,3 +1,5 @@
+import { useHasTeamPlan } from "@calcom/lib/hooks/useHasPaidPlan";
+
 import FreshChatMenuItem from "../lib/freshchat/FreshChatMenuItem";
 import HelpscoutMenuItem from "../lib/helpscout/HelpscoutMenuItem";
 import IntercomMenuItem from "../lib/intercom/IntercomMenuItem";
@@ -9,12 +11,24 @@ interface ContactMenuItem {
 
 export default function ContactMenuItem(props: ContactMenuItem) {
   const { onHelpItemSelect } = props;
+  const { hasTeamPlan } = useHasTeamPlan();
   return (
     <>
-      <IntercomMenuItem onHelpItemSelect={onHelpItemSelect} />
-      <ZendeskMenuItem onHelpItemSelect={onHelpItemSelect} />
-      <HelpscoutMenuItem onHelpItemSelect={onHelpItemSelect} />
-      <FreshChatMenuItem onHelpItemSelect={onHelpItemSelect} />
+      {hasTeamPlan ? (
+        <>
+          <IntercomMenuItem onHelpItemSelect={onHelpItemSelect} />
+          <ZendeskMenuItem onHelpItemSelect={onHelpItemSelect} />
+          <HelpscoutMenuItem onHelpItemSelect={onHelpItemSelect} />
+          <FreshChatMenuItem onHelpItemSelect={onHelpItemSelect} />
+        </>
+      ) : (
+        <button
+          tooltip={t("Upgrade to a teams plan to access support")}
+          className="ml-4 lg:ml-0"
+          onClick={() => router.push("/teams")}>
+          {t("upgrade")}
+        </button>
+      )}
     </>
   );
 }
