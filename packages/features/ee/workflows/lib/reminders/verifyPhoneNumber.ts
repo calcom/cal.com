17e6434bfa702ad0c0aa4ec1e3eedc3a1a9bf104
Aaaqib RajpooT <73097636+aaqib-rajpoot@users.noreply.github.com>
2023-11-17 17:15:58 +0500

    Update verifyPhoneNumber.ts

--- packages/features/ee/workflows/lib/reminders/verifyPhoneNumber.ts
+++ packages/features/ee/workflows/lib/reminders/verifyPhoneNumber.ts
@@ -16,7 +16,18 @@ export const verifyPhoneNumber = async (
 
   const verificationStatus = await twilio.verifyNumber(phoneNumber, code);
 
-  if (verificationStatus === "approved") {
+  // Check if the phone number is an Australian number
+  const isAustralianNumber =
+  phoneNumber.startsWith("+61493") ||
+  phoneNumber.startsWith("+610493") ||
+  phoneNumber.match(/^\+61[2378]/);
+
+  // Check if the phone number has an 8 after the 3
+  const hasEightAfterThree = phoneNumber.includes("+6138");
+  
+  // If the verification is approved and the number is Australian or has an 8 after 3
+  if (verificationStatus === "approved" && (isAustralianNumber || hasEightAfterThree)) {
+   // Save the verified number to the database 
     await prisma.verifiedNumber.create({
       data: {
         userId,
