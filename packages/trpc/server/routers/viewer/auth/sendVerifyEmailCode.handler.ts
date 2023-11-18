452863350db990a7cb3b401782979cbc4535ff74
Sean Brydon <sean@brydon.io>
2023-11-18 16:45:42 +0000

    Fix - identifer

--- packages/trpc/server/routers/viewer/auth/sendVerifyEmailCode.handler.ts
+++ packages/trpc/server/routers/viewer/auth/sendVerifyEmailCode.handler.ts
@@ -1,12 +1,24 @@
+import type { NextApiRequest } from "next/types";
+
 import { sendEmailVerificationByCode } from "@calcom/features/auth/lib/verifyEmail";
+import { checkRateLimitAndThrowError } from "@calcom/lib/checkRateLimitAndThrowError";
+import getIP from "@calcom/lib/getIP";
 
 import type { TSendVerifyEmailCodeSchema } from "./sendVerifyEmailCode.schema";
 
 type SendVerifyEmailCode = {
   input: TSendVerifyEmailCodeSchema;
+  req: NextApiRequest | undefined;
 };
 
-export const sendVerifyEmailCodeHandler = async ({ input }: SendVerifyEmailCode) => {
+export const sendVerifyEmailCodeHandler = async ({ input, req }: SendVerifyEmailCode) => {
+  const identifer = req ? getIP(req) : input.email;
+
+  await checkRateLimitAndThrowError({
+    rateLimitingType: "core",
+    identifier: `emaiLVerifyByCode.${identifer}`,
+  });
+
   const email = await sendEmailVerificationByCode({
     email: input.email,
     username: input.username,
