ec1d49b269eb03543307d07f17d252c7d1b6601a
Peer Richelsen <peer@cal.com>
2023-11-20 14:21:01 +0000

    added upgrade box to intercom

--- packages/features/ee/support/components/ContactMenuItem.tsx
+++ packages/features/ee/support/components/ContactMenuItem.tsx
@@ -1,4 +1,10 @@
-import { useHasTeamPlan } from "@calcom/lib/hooks/useHasPaidPlan";
+import { useRouter } from "next/navigation";
+
+import { JOIN_DISCORD } from "@calcom/lib/constants";
+import { useHasPaidPlan } from "@calcom/lib/hooks/useHasPaidPlan";
+import { useLocale } from "@calcom/lib/hooks/useLocale";
+import { UpgradeTeamsBadge } from "@calcom/ui";
+import { ExternalLink } from "@calcom/ui/components/icon";
 
 import FreshChatMenuItem from "../lib/freshchat/FreshChatMenuItem";
 import HelpscoutMenuItem from "../lib/helpscout/HelpscoutMenuItem";
@@ -10,11 +16,13 @@ interface ContactMenuItem {
 }
 
 export default function ContactMenuItem(props: ContactMenuItem) {
+  const { t } = useLocale();
   const { onHelpItemSelect } = props;
-  const { hasTeamPlan } = useHasTeamPlan();
+  const router = useRouter();
+  const { hasPaidPlan } = useHasPaidPlan();
   return (
     <>
-      {hasTeamPlan ? (
+      {hasPaidPlan ? (
         <>
           <IntercomMenuItem onHelpItemSelect={onHelpItemSelect} />
           <ZendeskMenuItem onHelpItemSelect={onHelpItemSelect} />
@@ -22,14 +30,18 @@ export default function ContactMenuItem(props: ContactMenuItem) {
           <FreshChatMenuItem onHelpItemSelect={onHelpItemSelect} />
         </>
       ) : (
-        <button
-          tooltip={t("Upgrade to a teams plan to access support")}
-          onClick={() => router.push("/teams")}
-          className="hover:bg-subtle hover:text-emphasis text-default flex w-full px-5 py-2 pr-4 text-sm font-medium">
-          {t("contact_support")}
-          {t("upgrade")}
-        </button>
+        <div className=" hover:text-emphasis text-default flex w-full cursor-not-allowed justify-between px-5 py-2 pr-4 text-sm font-medium">
+          {t("premium_support")}
+          <UpgradeTeamsBadge />
+        </div>
       )}
+      <a
+        href={JOIN_DISCORD}
+        target="_blank"
+        className="hover:bg-subtle hover:text-emphasis text-default flex w-full px-5 py-2 pr-4 text-sm font-medium">
+        {t("community_support")}{" "}
+        <ExternalLink className="group-hover:text-subtle text-muted ml-1 mt-px h-4 w-4 flex-shrink-0 ltr:mr-3" />
+      </a>
     </>
   );
 }
