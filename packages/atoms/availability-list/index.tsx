041b97ed5eb586b332a1186fd61e187904111a4d
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-11-20 17:39:14 +0530

    remove props and bring all handlers inside of component, add conditions for api key validation messages and dispaly component based on api key status

--- packages/atoms/availability-list/index.tsx
+++ packages/atoms/availability-list/index.tsx
@@ -1,6 +1,9 @@
 import { NewScheduleButton } from "availability-list/components/new-schedule-button";
+import { useApiKey } from "cal-provider";
+import { useEffect, useState } from "react";
 
-import type { HttpError } from "@calcom/lib/http-error";
+import { HttpError } from "@calcom/lib/http-error";
+import { trpc } from "@calcom/trpc/react";
 import { Clock } from "@calcom/ui/components/icon";
 
 import { Availability } from "./components/availability";
@@ -23,25 +26,96 @@ export type Schedule = {
   timeZone: string | null;
 };
 
-type AvailabilityListProps = {
-  schedules: Schedule[] | [];
-  onCreateMutation: (values: {
-    onSucess: (schedule: Schedule) => void;
-    onError: (err: HttpError) => void;
-  }) => void;
-  updateMutation: ({ scheduleId, isDefault }: { scheduleId: number; isDefault: boolean }) => void;
-  duplicateMutation: ({ scheduleId }: { scheduleId: number }) => void;
-  deleteMutation: ({ scheduleId }: { scheduleId: number }) => void;
-};
+export function AvailabilityList() {
+  const key = useApiKey();
+  const utils = trpc.useContext();
+  const [isKeyPresent, setIsKeyPresent] = useState(false);
+  const [schedules, setSchedules] = useState<Schedule[] | []>([]);
 
-export function AvailabilityList({
-  schedules,
-  onCreateMutation,
-  updateMutation,
-  duplicateMutation,
-  deleteMutation,
-}: AvailabilityListProps) {
-  if (schedules.length === 0) {
+  const updateMutation = trpc.viewer.availability.schedule.update.useMutation({
+    onSuccess: async ({ schedule }) => {
+      await utils.viewer.availability.list.invalidate();
+      // show success toast
+    },
+    onError: (err) => {
+      if (err instanceof HttpError) {
+        const message = `${err.statusCode}: ${err.message}`;
+        // show error toast
+        // showToast(message, "error");
+      }
+    },
+  });
+
+  const deleteMutation = trpc.viewer.availability.schedule.delete.useMutation({
+    onMutate: async ({ scheduleId }) => {
+      await utils.viewer.availability.list.cancel();
+      const previousValue = utils.viewer.availability.list.getData();
+      if (previousValue) {
+        const filteredValue = previousValue.schedules.filter(({ id }) => id !== scheduleId);
+        utils.viewer.availability.list.setData(undefined, { ...previousValue, schedules: filteredValue });
+      }
+
+      return { previousValue };
+    },
+
+    onError: (err, variables, context) => {
+      if (context?.previousValue) {
+        utils.viewer.availability.list.setData(undefined, context.previousValue);
+      }
+      if (err instanceof HttpError) {
+        const message = `${err.statusCode}: ${err.message}`;
+        // Show error message toast here
+        //showToast(message, "error");
+      }
+    },
+    onSettled: () => {
+      utils.viewer.availability.list.invalidate();
+    },
+    onSuccess: () => {
+      // Show success toast here
+      // showToast(t("schedule_deleted_successfully"), "success");
+    },
+  });
+
+  const duplicateMutation = trpc.viewer.availability.schedule.duplicate.useMutation({
+    onSuccess: async ({ schedule }) => {
+      // router.push to schedule
+      // then show success toast
+    },
+    onError: (err) => {
+      if (err instanceof HttpError) {
+        const message = `${err.statusCode}: ${err.message}`;
+        // showToast(message, "error");
+        // show error toast
+      }
+    },
+  });
+
+  useEffect(() => {
+    async function getSchedules(key: string) {
+      // here we're supposed call the /schedules endpoint in v2 to get schedules
+      // since v2 is not ready yet thats why calling localhost for now
+      const response = await fetch(`/v2/schedules?apiKey=${key}`);
+      const data = await response.json();
+
+      setSchedules(data);
+    }
+
+    if (key !== "no_key" && key !== "invalid_key") {
+      setIsKeyPresent(true);
+      getSchedules(key);
+    }
+  }, [key]);
+
+  if (key === "no_key") {
+    return <>You havent entered a key</>;
+  }
+
+  if (key === "invalid_key") {
+    return <>This is not a valid key, please enter a valid key</>;
+  }
+
+  if (isKeyPresent && schedules.length === 0) {
     return (
       <div className="flex justify-center">
         <EmptyScreen
@@ -49,26 +123,28 @@ export function AvailabilityList({
           headline="Create an availability schedule"
           description="Creating availability schedules allows you to manage availability across event types. They can be applied to one or more event types."
           className="w-full"
-          buttonRaw={<NewScheduleButton createMutation={onCreateMutation} />}
+          buttonRaw={<NewScheduleButton />}
         />
       </div>
     );
   }
 
-  return (
-    <div className="border-subtle bg-default mb-16 overflow-hidden rounded-md border">
-      <ul className="divide-subtle divide-y" data-testid="schedules">
-        {schedules.map((schedule) => (
-          <Availability
-            key={schedule.id}
-            schedule={schedule}
-            isDeletable={schedules.length !== 1}
-            updateDefault={updateMutation}
-            deleteFunction={deleteMutation}
-            duplicateFunction={duplicateMutation}
-          />
-        ))}
-      </ul>
-    </div>
-  );
+  if (isKeyPresent && !!schedules) {
+    return (
+      <div className="border-subtle bg-default mb-16 overflow-hidden rounded-md border">
+        <ul className="divide-subtle divide-y" data-testid="schedules">
+          {schedules.map((schedule) => (
+            <Availability
+              key={schedule.id}
+              schedule={schedule}
+              isDeletable={schedules.length !== 1}
+              updateDefault={updateMutation.mutate}
+              deleteFunction={deleteMutation.mutate}
+              duplicateFunction={duplicateMutation.mutate}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
