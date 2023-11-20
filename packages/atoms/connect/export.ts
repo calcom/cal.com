a41e15049743090b41f97e78ee70ba5cd5238448
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-11-21 02:51:51 +0530

    update exports for connect component

--- packages/atoms/connect/export.ts
+++ packages/atoms/connect/export.ts
@@ -1,2 +1,4 @@
+export { ConnectCalendar } from "connect/connect-calendar";
+export { ConnectVideo } from "connect/connect-video";
 export { ConnectButton } from "./ConnectButton";
 export * from "../types";
--- packages/atoms/index.ts
+++ packages/atoms/index.ts
@@ -1,2 +1,5 @@
 export { Booker } from "./booker/Booker";
 export { ConnectButton } from "./connect/ConnectButton";
+export { CalProvider } from "./cal-provider/index";
+export { ConnectCalendar } from "connect/connect-calendar";
+export { ConnectVideo } from "connect/connect-video";
