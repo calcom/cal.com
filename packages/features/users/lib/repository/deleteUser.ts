0c55cbf79add014adad96fb1fff535ffe86ad8d2
Alex van Andel <me@alexvanandel.com>
2023-11-20 11:24:20 +0000

    fix: Better error reporting & some fixes to deleteUser API

---
+++ packages/features/users/lib/repository/deleteUser.ts
@@ -0,0 +1,19 @@
+import type { User } from "@prisma/client";
+
+import { deleteStripeCustomer } from "@calcom/app-store/stripepayment/lib/customer";
+import { deleteWebUser as syncServicesDeleteWebUser } from "@calcom/lib/sync/SyncServiceManager";
+import prisma from "@calcom/prisma";
+
+export default async function deleteUser(user: Pick<User, "id" | "email" | "metadata">) {
+  // If 2FA is disabled or totpCode is valid then delete the user from stripe and database
+  await deleteStripeCustomer(user).catch(console.warn);
+  // Remove my account
+  const deletedUser = await prisma.user.delete({
+    where: {
+      id: user.id,
+    },
+  });
+
+  // Sync Services
+  syncServicesDeleteWebUser(deletedUser);
+}
---
+++ packages/features/users/lib/repository/index.ts
@@ -0,0 +1,1 @@
+export { default as deleteUser } from "./deleteUser";
