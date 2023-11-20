716b89f40c8601f02cd45d20874402e07a35ee9e
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-11-20 17:31:56 +0530

    add cal provider to default export

--- packages/atoms/index.ts
+++ packages/atoms/index.ts
@@ -1,3 +1,4 @@
 export { Booker } from "./booker/Booker";
 export { AvailabilityList } from "./availability-list";
 export { Availability } from "./availability-list/components/availability";
+export { CalProvider } from "./cal-provider/index";
