81e9323fd92746ab342c98b8779ea6acb9b983ed
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-11-21 12:20:12 +0530

    add cal provider to index resolve merge conflicts

--- packages/atoms/index.ts
+++ packages/atoms/index.ts
@@ -1,3 +1,4 @@
 export { Booker } from "./booker/Booker";
 export { EventType } from "./event-type-list/components/event-type";
 export { EventTypeList } from "./event-type-list";
+export { CalProvider } from "./cal-provider/index";
