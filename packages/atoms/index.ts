e7247f0c6574f1c4736c128c89c4643947e10c98
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-11-21 12:27:48 +0530

    add event type and list to index

--- packages/atoms/index.ts
+++ packages/atoms/index.ts
@@ -1,2 +1,4 @@
 export { Booker } from "./booker/Booker";
 export { CalProvider } from "./cal-provider/index";
+export { EventType } from "./event-type-list/components/event-type";
+export { EventTypeList } from "./event-type-list";
