bfda55e8ade18c46c743c06db0702e52d67cde02
Shashank Kaul <kaulshashank96@gmail.com>
2023-11-21 14:00:39 +0530

    fix(api): Added correct include for `/teams/{teamId}/event-types`

--- apps/api/pages/api/teams/[teamId]/event-types/_get.ts
+++ apps/api/pages/api/teams/[teamId]/event-types/_get.ts
@@ -56,6 +56,14 @@ async function getHandler(req: NextApiRequest) {
             members: { some: { userId } },
           },
     },
+    include: {
+      customInputs: true,
+      team: { select: { slug: true } },
+      users: true,
+      hosts: { select: { userId: true, isFixed: true } },
+      owner: { select: { username: true, id: true } },
+      children: { select: { id: true, userId: true } },
+    },
   };
 
   const data = await prisma.eventType.findMany(args);
