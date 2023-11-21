cdcae40c7ab15494abdd61c8095b38caa09de1fb
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-11-21 12:22:19 +0530

    resolve merge conflicts

--- packages/atoms/index.ts
+++ packages/atoms/index.ts
@@ -1,4 +1,2 @@
 export { Booker } from "./booker/Booker";
-export { EventType } from "./event-type-list/components/event-type";
-export { EventTypeList } from "./event-type-list";
 export { CalProvider } from "./cal-provider/index";
