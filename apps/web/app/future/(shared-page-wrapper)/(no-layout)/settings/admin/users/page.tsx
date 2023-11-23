4723e8b90df9368e8f68d39cb76b2f4c9212b1ac
Benny Joo <sldisek783@gmail.com>
2023-11-23 14:38:58 +0000

    codemod: app-directory-boilerplate-calcom

---
+++ apps/web/app/future/(shared-page-wrapper)/(no-layout)/settings/admin/users/page.tsx
@@ -0,0 +1,10 @@
+import Page from "@pages/settings/admin/users/index";
+import { _generateMetadata } from "app/_utils";
+
+export const generateMetadata = async () =>
+  await _generateMetadata(
+    () => "",
+    () => ""
+  );
+
+export default Page;
