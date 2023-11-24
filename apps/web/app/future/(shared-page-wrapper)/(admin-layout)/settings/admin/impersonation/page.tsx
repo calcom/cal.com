36c44cce6744d4cc700a20b7dea6e147316c96c2
Benny Joo <sldisek783@gmail.com>
2023-11-24 10:05:50 +0000

    manual: add title/description metadata for static values

--- apps/web/app/future/(shared-page-wrapper)/(admin-layout)/settings/admin/impersonation/page.tsx
+++ apps/web/app/future/(shared-page-wrapper)/(admin-layout)/settings/admin/impersonation/page.tsx
@@ -3,8 +3,8 @@ import { _generateMetadata } from "app/_utils";
 
 export const generateMetadata = async () =>
   await _generateMetadata(
-    () => "",
-    () => ""
+    (t) => t("admin"),
+    (t) => t("impersonation")
   );
 
 export default Page;
--- apps/web/app/future/(shared-page-wrapper)/(admin-layout)/settings/admin/organizations/page.tsx
+++ apps/web/app/future/(shared-page-wrapper)/(admin-layout)/settings/admin/organizations/page.tsx
@@ -3,8 +3,8 @@ import { _generateMetadata } from "app/_utils";
 
 export const generateMetadata = async () =>
   await _generateMetadata(
-    () => "",
-    () => ""
+    (t) => t("organizations"),
+    (t) => t("orgs_page_description")
   );
 
 export default Page;
