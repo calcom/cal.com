df5f4d13fed82bb522f9d64e44b58fdf0bbb4d03
Benny Joo <sldisek783@gmail.com>
2023-11-24 13:16:05 +0000

    manual: add title/description metadata for dynamic value

--- apps/web/app/future/(shared-page-wrapper)/(admin-layout)/settings/admin/users/[id]/edit/page.tsx
+++ apps/web/app/future/(shared-page-wrapper)/(admin-layout)/settings/admin/users/[id]/edit/page.tsx
@@ -1,10 +1,28 @@
 import Page from "@pages/settings/admin/users/[id]/edit";
+import { trpc } from "app/_trpc/client";
+import { type Params } from "app/_types";
 import { _generateMetadata } from "app/_utils";
+import { z } from "zod";
 
-export const generateMetadata = async () =>
-  await _generateMetadata(
-    () => "",
-    () => ""
+const userIdSchema = z.object({ id: z.coerce.number() });
+
+export const generateMetadata = async ({ params }: { params: Params }) => {
+  const input = userIdSchema.safeParse(params);
+
+  let title = "";
+  if (!input.success) {
+    title = "Editing user";
+  } else {
+    // @ts-expect-error Property 'useSuspenseQuery' does not exist on type
+    const [data] = trpc.viewer.users.get.useSuspenseQuery({ userId: input.data.id });
+    const { user } = data;
+    title = `Editing user: ${user.username}`;
+  }
+
+  return await _generateMetadata(
+    () => title,
+    () => "Here you can edit a current user."
   );
+};
 
 export default Page;
