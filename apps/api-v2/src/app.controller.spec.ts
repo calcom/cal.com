040a4db061a38686e84cbf05d8d0a64a8c78df13
supalarry <lauris.skraucis@gmail.com>
2023-11-27 16:23:01 +0100

    init nest project

---
+++ apps/api-v2/src/app.controller.spec.ts
@@ -0,0 +1,23 @@
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import type { TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+});
