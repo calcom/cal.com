99285079c77781a54c84b59f5c3d0f8d131afb93
gitstart-calcom <gitstart-calcom@users.noreply.github.com>
2023-11-28 19:38:45 +0000

    add localize / refactor

--- apps/web/playwright/booking/collectiveEventType/collectiveEventType.e2e.ts
+++ apps/web/playwright/booking/collectiveEventType/collectiveEventType.e2e.ts
@@ -2,6 +2,7 @@ import { randomString } from "@calcom/lib/random";
 
 import { loginUser } from "../../fixtures/regularBookings";
 import { test } from "../../lib/fixtures";
+import { localize } from "../../lib/testUtils";
 
 test.describe("Create a Team, a Collective Event Type and Book a Meeting", () => {
   test.beforeEach(async ({ page, users }) => {
@@ -11,6 +12,7 @@ test.describe("Create a Team, a Collective Event Type and Book a Meeting", () =>
 
   test("Create Collective Event type and Book a Meeting", async ({ bookingPage }) => {
     const teamName = `Team example-${randomString(3)}`;
+    const backToBookingsText = (await localize("en"))("back_to_bookings");
 
     await bookingPage.createTeam(teamName);
     await bookingPage.createTeamEventType("test-collective", { isCollectiveType: true });
@@ -18,7 +20,7 @@ test.describe("Create a Team, a Collective Event Type and Book a Meeting", () =>
     await bookingPage.selectTimeSlot(eventTypePage);
 
     await bookingPage.confirmBooking(eventTypePage);
-    await bookingPage.goToPage("Back to bookings", eventTypePage);
+    await bookingPage.goToPage(backToBookingsText, eventTypePage);
     await bookingPage.assertLabelWithCorrectTeamName(eventTypePage, teamName);
     await bookingPage.assertBookingWithCorrectTitleAndDescription(eventTypePage, {
       profileName: "testuser",
@@ -35,6 +37,7 @@ test.describe("Create a Team, a Collective Event Type and Book a Meeting", () =>
 
   test("Create Collective Event type and Book a Meeting (with added guest)", async ({ bookingPage }) => {
     const teamName = `Team example-${randomString(3)}`;
+    const backToBookingsText = (await localize("en"))("back_to_bookings");
 
     await bookingPage.createTeam(teamName);
     await bookingPage.createTeamEventType("test-collective", { isCollectiveType: true });
@@ -42,7 +45,7 @@ test.describe("Create a Team, a Collective Event Type and Book a Meeting", () =>
     await bookingPage.selectTimeSlot(eventTypePage);
     await bookingPage.addGuests(eventTypePage, { guests: ["test@example.com"] });
     await bookingPage.confirmBooking(eventTypePage);
-    await bookingPage.goToPage("Back to bookings", eventTypePage);
+    await bookingPage.goToPage(backToBookingsText, eventTypePage);
     await bookingPage.assertLabelWithCorrectTeamName(eventTypePage, teamName);
     await bookingPage.assertBookingWithCorrectTitleAndDescription(eventTypePage, {
       profileName: "testuser",
