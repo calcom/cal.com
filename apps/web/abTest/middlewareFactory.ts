65809e1a6c74b0c75c6527dbeb32c49c50bde72b
Benny Joo <sldisek783@gmail.com>
2023-11-29 14:48:24 +0000

    manual: add ab test redirect

--- apps/web/abTest/middlewareFactory.ts
+++ apps/web/abTest/middlewareFactory.ts
@@ -8,6 +8,7 @@ const ROUTES: [URLPattern, boolean][] = [
   ["/settings/admin/:path*", process.env.APP_ROUTER_SETTINGS_ADMIN_ENABLED === "1"] as const,
   ["/apps/:slug", Boolean(process.env.APP_ROUTER_APPS_SLUG_ENABLED)] as const,
   ["/apps/:slug/setup", Boolean(process.env.APP_ROUTER_APPS_SLUG_SETUP_ENABLED)] as const,
+  ["/video/:path*", process.env.APP_ROUTER_VIDEO_ENABLED === "1"] as const,
 ].map(([pathname, enabled]) => [
   new URLPattern({
     pathname,
--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -103,12 +103,12 @@ export const config = {
     "/future/event-types/",
     "/settings/admin/:path*",
     "/future/settings/admin/:path*",
-
     "/apps/:slug/",
     "/future/apps/:slug/",
-
     "/apps/:slug/setup/",
     "/future/apps/:slug/setup/",
+    "/video/:path*",
+    "/future/video/:path*",
   ],
 };
 
