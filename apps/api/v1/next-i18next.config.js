d3e1b0fa6e9b92d8b5598477647d9a6f30ef3b9e
Morgan Vernay <morgan@cal.com>
2023-11-29 22:32:06 +0200

    fix config path after move to folder /v1

--- apps/api/v1/next-i18next.config.js
+++ apps/api/v1/next-i18next.config.js
@@ -4,7 +4,7 @@ const i18nConfig = require("@calcom/config/next-i18next.config");
 /** @type {import("next-i18next").UserConfig} */
 const config = {
   ...i18nConfig,
-  localePath: path.resolve("../web/public/static/locales"),
+  localePath: path.resolve("../../web/public/static/locales"),
 };
 
 module.exports = config;
