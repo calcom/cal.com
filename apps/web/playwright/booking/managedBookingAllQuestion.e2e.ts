f55817465ca7cfd24dfaa35294b0d8775795dfb5
GitStart-Cal.com <121884634+gitstart-calcom@users.noreply.github.com>
2023-11-29 18:24:06 -0300

    refactor

--- apps/web/playwright/booking/managedBookingAllQuestion.e2e.ts
+++ apps/web/playwright/booking/managedBookingAllQuestion.e2e.ts
@@ -1,12 +1,12 @@
 /* eslint-disable playwright/no-conditional-in-test */
-import { loginUser } from "../fixtures/regularBookings";
 import { test } from "../lib/fixtures";
 
 test.describe("Managed Booking With All Questions", () => {
   test.beforeEach(async ({ page, users, bookingPage }) => {
-    await loginUser(users);
+    const teamEventTitle = "testevent";
+    const userFixture = await users.create({ name: "testuser" }, { hasTeam: true, teamEventTitle });
+    await userFixture.apiLogin();
     await page.goto("/event-types");
-    await bookingPage.createTeam("Test Team");
     await bookingPage.createTeamEventType("Test Managed Event Type", { isManagedType: true });
     await bookingPage.goToTab("event_advanced_tab_title");
   });
