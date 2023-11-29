e88c04f4806fe76891e2e23443bc18d617dbe9ec
Henrik Stabil <dev@hardcoded.digital>
2023-11-29 02:05:10 +0100

    signup dark mode and corner fix

--- apps/web/pages/signup.tsx
+++ apps/web/pages/signup.tsx
@@ -245,9 +245,10 @@ export default function Signup({
           "--cal-brand-subtle": "#9CA3AF",
         } as CSSProperties
       }>
-      <div className="bg-muted 2xl:border-subtle grid max-h-[800px] w-full max-w-[1440px] grid-cols-1 grid-rows-1 lg:grid-cols-2 2xl:rounded-lg 2xl:border ">
+      <div className="bg-muted 2xl:border-subtle grid max-h-[800px] w-full max-w-[1440px] grid-cols-1 grid-rows-1 overflow-hidden lg:grid-cols-2 2xl:rounded-lg 2xl:border">
         <HeadSeo title={t("sign_up")} description={t("sign_up")} />
-        <div className="flex w-full flex-col px-4 py-6 sm:px-16 md:px-24 2xl:px-28">
+        {/* Left side */}
+        <div className="flex w-full flex-col justify-center px-4 py-6 sm:px-16 md:px-24 2xl:px-28">
           {/* Header */}
           {errors.apiError && (
             <Alert severity="error" message={errors.apiError?.message} data-testid="signup-error-message" />
@@ -415,7 +416,7 @@ export default function Signup({
               <Link href="/auth/login" className="text-emphasis hover:underline">
                 {t("already_have_account")}
               </Link>
-              <div className="text-subtle">
+              <div className="text-subtle mt-2">
                 By signing up, you agree to our{" "}
                 <Link className="text-emphasis hover:underline" href={`${WEBSITE_URL}/terms`}>
                   Terms of Service{" "}
@@ -428,6 +429,8 @@ export default function Signup({
             </div>
           </div>
         </div>
+
+        {/* Right side */}
         <div className="bg-subtle border-subtle hidden w-full flex-col justify-between rounded-l-2xl py-12 pl-12 lg:flex">
           {IS_CALCOM && (
             <div className="mb-12 mr-12 grid h-full w-full grid-cols-4 gap-4 ">
@@ -442,11 +445,7 @@ export default function Signup({
               </div>
             </div>
           )}
-          <div
-            className="rounded-2xl border-y border-l border-dashed border-[#D1D5DB5A] py-[6px] pl-[6px]"
-            style={{
-              backgroundColor: "rgba(236,237,239,0.9)",
-            }}>
+          <div className="dark:bg-darkgray-100/50 dark:border-darkgray-300 rounded-l-2xl border-y border-l border-dashed border-[#D1D5DB5A] bg-[rgba(236,237,239,0.9)] py-[6px] pl-[6px]">
             <img src="/mock-event-type-list.svg" alt="#" className="" />
           </div>
           <div className="mr-12 mt-8 grid h-full w-full grid-cols-3 gap-4 overflow-hidden">
@@ -625,5 +624,4 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   };
 };
 
-Signup.isThemeSupported = false;
 Signup.PageWrapper = PageWrapper;
