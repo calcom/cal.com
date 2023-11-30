57fe3adcc06f0d14bb755c419eed50fb0e783937
Morgan Vernay <morgan@cal.com>
2023-11-30 11:12:50 +0200

    fixup! fixup! fixup! chore: init platform packages

--- packages/platform/atoms/index.ts
+++ packages/platform/atoms/index.ts
@@ -1,5 +1,3 @@
 export { Booker } from "./booker/Booker";
 export { CalProvider } from "./cal-provider/index";
 export { ConnectToCal } from "./connect-to-cal-button/index";
-export * from "./src/components/ui/button";
-export * from "./src/lib/utils";
