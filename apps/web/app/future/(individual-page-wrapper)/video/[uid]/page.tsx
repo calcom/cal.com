6fd6ea49d4c253621e4bb25effa9776d7e421dcb
Benny Joo <sldisek783@gmail.com>
2023-11-30 16:02:10 +0000

    manual: refactor and add metadata

--- apps/web/app/future/(individual-page-wrapper)/video/[uid]/page.tsx
+++ apps/web/app/future/(individual-page-wrapper)/video/[uid]/page.tsx
@@ -1,7 +1,15 @@
 import OldPage from "@pages/video/[uid]";
+import { ssrInit } from "app/_trpc/ssrInit";
+import { type Params } from "app/_types";
 import { _generateMetadata } from "app/_utils";
-import type { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+import MarkdownIt from "markdown-it";
+import { type GetServerSidePropsContext } from "next";
 import { headers, cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { getServerSession } from "@calcom/features/auth/lib/getServerSession";
+import { APP_NAME } from "@calcom/lib/constants";
+import prisma, { bookingMinimalSelect } from "@calcom/prisma";
 
 import { buildLegacyCtx } from "@lib/buildLegacyCtx";
 
@@ -9,24 +17,107 @@ import PageWrapper from "@components/PageWrapperAppDir";
 
 export const generateMetadata = async () =>
   await _generateMetadata(
-    () => "",
-    () => ""
+    () => `${APP_NAME} Video`,
+    (t) => t("quick_video_meeting")
   );
 
 type PageProps = Readonly<{
   params: Params;
 }>;
 
+const md = new MarkdownIt("default", { html: true, breaks: true, linkify: true });
+
+async function getData(context: Omit<GetServerSidePropsContext, "res" | "resolvedUrl">) {
+  const booking = await prisma.booking.findUnique({
+    where: {
+      uid: typeof context?.params?.uid === "string" ? context.params.uid : "",
+    },
+    select: {
+      ...bookingMinimalSelect,
+      uid: true,
+      description: true,
+      isRecorded: true,
+      user: {
+        select: {
+          id: true,
+          timeZone: true,
+          name: true,
+          email: true,
+        },
+      },
+      references: {
+        select: {
+          uid: true,
+          type: true,
+          meetingUrl: true,
+          meetingPassword: true,
+        },
+        where: {
+          type: "daily_video",
+        },
+      },
+    },
+  });
+
+  if (!booking || booking.references.length === 0 || !booking.references[0].meetingUrl) {
+    return redirect("/video/no-meeting-found");
+  }
+
+  //daily.co calls have a 60 minute exit buffer when a user enters a call when it's not available it will trigger the modals
+  const now = new Date();
+  const exitDate = new Date(now.getTime() - 60 * 60 * 1000);
+
+  //find out if the meeting is in the past
+  const isPast = booking?.endTime <= exitDate;
+  if (isPast) {
+    return redirect(`/video/meeting-ended/${booking?.uid}`);
+  }
+
+  const bookingObj = Object.assign({}, booking, {
+    startTime: booking.startTime.toString(),
+    endTime: booking.endTime.toString(),
+  });
+
+  const session = await getServerSession({ req: context.req });
+
+  // set meetingPassword to null for guests
+  if (session?.user.id !== bookingObj.user?.id) {
+    bookingObj.references.forEach((bookRef: any) => {
+      bookRef.meetingPassword = null;
+    });
+  }
+
+  const ssr = await ssrInit();
+
+  return {
+    meetingUrl: bookingObj.references[0].meetingUrl ?? "",
+    ...(typeof bookingObj.references[0].meetingPassword === "string" && {
+      meetingPassword: bookingObj.references[0].meetingPassword,
+    }),
+    booking: {
+      ...bookingObj,
+      ...(bookingObj.description && { description: md.render(bookingObj.description) }),
+    },
+    dehydratedState: await ssr.dehydrate(),
+  };
+}
+
 const Page = async ({ params }: PageProps) => {
   const h = headers();
   const nonce = h.get("x-nonce") ?? undefined;
 
-  const legacyCtx = buildLegacyCtx(params, headers(), cookies());
+  const legacyCtx = buildLegacyCtx(headers(), cookies(), params);
+  // @ts-expect-error `req` of type '{ headers: ReadonlyHeaders; cookies: ReadonlyRequestCookies; }' is not assignable to `req` in `GetServerSidePropsContext`
   const props = await getData(legacyCtx);
 
   return (
-    <PageWrapper requiresLicense={false} nonce={nonce} themeBasis={null}>
-      <OldPage {...props} />
+    <PageWrapper
+      getLayout={null}
+      requiresLicense={false}
+      nonce={nonce}
+      themeBasis={null}
+      dehydratedState={props.dehydratedState}>
+      <OldPage {...props} trpcState={props.dehydratedState} />
     </PageWrapper>
   );
 };
--- apps/web/app/future/(individual-page-wrapper)/video/meeting-not-started/[uid]/page.tsx
+++ apps/web/app/future/(individual-page-wrapper)/video/meeting-not-started/[uid]/page.tsx
@@ -1,31 +1,66 @@
 import OldPage from "@pages/video/meeting-not-started/[uid]";
+import { type Params } from "app/_types";
 import { _generateMetadata } from "app/_utils";
-import type { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+import { type GetServerSidePropsContext } from "next";
 import { headers, cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+import prisma, { bookingMinimalSelect } from "@calcom/prisma";
 
 import { buildLegacyCtx } from "@lib/buildLegacyCtx";
 
 import PageWrapper from "@components/PageWrapperAppDir";
 
-export const generateMetadata = async () =>
-  await _generateMetadata(
-    () => "",
-    () => ""
-  );
-
 type PageProps = Readonly<{
   params: Params;
 }>;
+
+export const generateMetadata = async ({ params }: PageProps) => {
+  const booking = await prisma.booking.findUnique({
+    where: {
+      uid: typeof params?.uid === "string" ? params.uid : "",
+    },
+    select: bookingMinimalSelect,
+  });
+
+  return await _generateMetadata(
+    (t) => t("this_meeting_has_not_started_yet"),
+    () => booking?.title ?? ""
+  );
+};
+
+async function getData(context: Omit<GetServerSidePropsContext, "res" | "resolvedUrl">) {
+  const booking = await prisma.booking.findUnique({
+    where: {
+      uid: typeof context?.params?.uid === "string" ? context.params.uid : "",
+    },
+    select: bookingMinimalSelect,
+  });
+
+  if (!booking) {
+    return redirect("/video/no-meeting-found");
+  }
+
+  const bookingObj = Object.assign({}, booking, {
+    startTime: booking.startTime.toString(),
+    endTime: booking.endTime.toString(),
+  });
+
+  return {
+    booking: bookingObj,
+  };
+}
 
 const Page = async ({ params }: PageProps) => {
   const h = headers();
   const nonce = h.get("x-nonce") ?? undefined;
 
-  const legacyCtx = buildLegacyCtx(params, headers(), cookies());
+  const legacyCtx = buildLegacyCtx(headers(), cookies(), params);
+  // @ts-expect-error `req` of type '{ headers: ReadonlyHeaders; cookies: ReadonlyRequestCookies; }' is not assignable to `req` in `GetServerSidePropsContext`
   const props = await getData(legacyCtx);
 
   return (
-    <PageWrapper requiresLicense={false} nonce={nonce} themeBasis={null}>
+    <PageWrapper getLayout={null} requiresLicense={false} nonce={nonce} themeBasis={null}>
       <OldPage {...props} />
     </PageWrapper>
   );
