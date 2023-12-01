e771c0be29a2d72f1cc54f6888ca9a515772b48a
Peer Richelsen <peer@cal.com>
2023-12-01 18:40:47 +0000

    nit

--- packages/ui/components/image-uploader/ImageUploader.tsx
+++ packages/ui/components/image-uploader/ImageUploader.tsx
@@ -202,7 +202,7 @@ export default function ImageUploader({
                 type="file"
                 name={id}
                 placeholder={t("upload_image")}
-                className="text-default pointer-events-none absolute mt-4 cursor-pointer opacity-0 "
+                className="text-default pointer-events-none absolute mt-4 opacity-0 "
                 accept="image/*"
               />
               {t("choose_a_file")}
