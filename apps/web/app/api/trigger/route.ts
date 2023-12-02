31c1b2b5153223cdcceb2cb0e0e1b873ce9deb5b
Wlad Paiva <desenvolvedormg@gmail.com>
2023-12-02 18:43:49 -0300

    Update dependencies and remove unused code

--- apps/web/app/api/trigger/route.ts
+++ apps/web/app/api/trigger/route.ts
@@ -1,10 +1,16 @@
 import { createAppRoute } from "@trigger.dev/nextjs";
 
-import "../../../jobs";
-import { client } from "../../../trigger";
+import { client } from "@calcom/queues";
+import "@calcom/queues/jobs";
+
+let triggerRoute: ReturnType<typeof createAppRoute> | undefined;
+
+if (client) {
+  triggerRoute = createAppRoute(client);
+}
 
 //this route is used to send and receive data with Trigger.dev
-export const { POST, dynamic } = createAppRoute(client);
+export const { POST, dynamic } = triggerRoute || {};
 
 //uncomment this to set a higher max duration (it must be inside your plan limits). Full docs: https://vercel.com/docs/functions/serverless-functions/runtimes#max-duration
 //export const maxDuration = 60;
--- apps/web/jobs/index.ts
+++
@@ -1,3 +0,0 @@
-// export all your job files here
-
-export * from "./examples";
