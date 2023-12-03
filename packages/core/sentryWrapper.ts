bbe788b22eb027401f0ce53c40016d78056f1b5d
Brendan Woodward <brendanwoodward19@gmail.com>
2023-12-03 19:23:13 +0000

    add comment of when to use the wrapper

--- packages/core/sentryWrapper.ts
+++ packages/core/sentryWrapper.ts
@@ -1,5 +1,14 @@
 import * as Sentry from "@sentry/nextjs";
 
+/*
+WHEN TO USE
+We ran a script that performs a simple mathematical calculation within a loop of 1000000 iterations. 
+Our results were: Plain execution time: 441, Monitored execution time: 8094. 
+This suggests that using these wrappers within large loops can incur significant overhead and is thus not recommended.
+
+For smaller loops, the cost incurred may not be very significant on an absolute scale
+considering that a million monitored iterations only took roughly 8 seconds when monitored.
+*/
 const monitorCallback = async (cb: CallableFunction, ...args: any[]) => {
   // Attempt to retrieve the current transaction from Sentry's scope
   let transaction = Sentry.getCurrentHub().getScope()?.getTransaction();
@@ -8,7 +17,7 @@ const monitorCallback = async (cb: CallableFunction, ...args: any[]) => {
   if (!transaction) {
     transaction = Sentry.startTransaction({
       op: cb.name,
-      //   name: `Call to ${cb.name}`,
+      name: cb.name,
     });
   }
 
