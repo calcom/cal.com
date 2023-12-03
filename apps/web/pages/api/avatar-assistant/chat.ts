e9ba803a080c05a3ff5454f0631434a296354df3
Edwin Wong <ew20128@gmail.com>
2023-12-03 19:38:11 +0800

    feat: booking from ai

--- apps/web/pages/api/avatar-assistant/chat.ts
+++ apps/web/pages/api/avatar-assistant/chat.ts
@@ -13,7 +13,7 @@ const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY! });
 
 export default async function Chat(req: NextRequest) {
   // Extract the `messages` from the body of the request
-  const { messages, username, userId, userEventTypes, userTime } = await req.json();
+  const { messages, username, userId, userEventTypes, userTime, ...props } = await req.json();
 
   // eventTypes is a any[]
 
@@ -51,31 +51,56 @@ export default async function Chat(req: NextRequest) {
     experimental_onFunctionCall: async ({ name, arguments: args }, createFunctionCallMessages) => {
       // if you skip the function call and return nothing, the `function_call`
       // message will be sent to the client for it to handle
-      if (name === "get_user_availability") {
-        // Call a weather API here
-        const { dateFrom, dateTo } = GetUserAvailability.parse(args);
+      switch (name) {
+        case "get_user_availability":
+          // Call a weather API here
+          const { dateFrom, dateTo } = GetUserAvailability.parse(args);
 
-        console.log("calling", dateFrom, dateTo);
+          console.log("calling", dateFrom, dateTo, args);
 
-        const data = await fetchAvailability({
-          apiKey: process.env.CAL_API_KEY!,
-          userId: userId,
-          dateFrom: dateFrom,
-          dateTo: dateTo,
-          // eventTypeId: eventTypeId,
-        });
+          const data = await fetchAvailability({
+            apiKey: process.env.CAL_API_KEY!,
+            userId: userId,
+            dateFrom: dateFrom,
+            dateTo: dateTo,
+          });
 
-        console.log("User Availability", data);
+          console.log("User Availability", data);
 
-        // `createFunctionCallMessages` constructs the relevant "assistant" and "function" messages for you
-        const newMessages = createFunctionCallMessages(data as any);
-        return openai.chat.completions.create({
-          messages: [...messages, ...newMessages],
-          stream: true,
-          model: "gpt-4-0613",
-          // see "Recursive Function Calls" below
-          functions,
-        });
+          // `createFunctionCallMessages` constructs the relevant "assistant" and "function" messages for you
+          const newMessages = createFunctionCallMessages(data as any);
+          return openai.chat.completions.create({
+            messages: [...messages, ...newMessages],
+            stream: true,
+            model: "gpt-4-0613",
+            // see "Recursive Function Calls" below
+            functions,
+          });
+        case "create_booking":
+          const { end, eventTypeId, language, start, timeZone } = CreateBooking.parse(args);
+          const booking = await createBooking({
+            apiKey: process.env.CAL_API_KEY!,
+            userId,
+            user: {
+              username: username,
+              email: props.email,
+            },
+            eventTypeId,
+            start,
+            end,
+            timeZone,
+            language,
+          });
+          const bookingmessage = createFunctionCallMessages(booking as any);
+          return openai.chat.completions.create({
+            messages: [...messages, ...bookingmessage],
+            stream: true,
+            model: "gpt-4-0613",
+            // see "Recursive Function Calls" below
+            functions,
+          });
+        default:
+          return;
       }
     },
   });
@@ -89,6 +114,16 @@ const GetUserAvailability = z.object({
   dateFrom: z.string(),
   dateTo: z.string(),
   // eventTypeId: z.number(),
+});
+
+const CreateBooking = z.object({
+  end: z.string().describe("This should correspond to the event type's length, unless otherwise specified."),
+  eventTypeId: z.number(),
+  language: z.string(),
+  start: z.string(),
+  status: z.string().optional().describe("ACCEPTED, PENDING, CANCELLED or REJECTED"),
+  timeZone: z.string(),
+  title: z.string().optional(),
 });
 
 const functions: ChatCompletionCreateParams.Function[] = [
@@ -118,28 +153,40 @@ const functions: ChatCompletionCreateParams.Function[] = [
       ],
     },
   },
-  // {
-  //   name: "create_booking",
-  //   description: "Create a booking for the user.",
-  //   parameters: {
-  //     type: "object",
-  //     properties: {
-  //       dateFrom: {
-  //         type: "string",
-  //         description: "The start date of the range to check.",
-  //       },
-  //       dateTo: {
-  //         type: "string",
-  //         description: "The end date of the range to check.",
-  //       },
-  //       eventTypeId: {
-  //         type: "integer",
-  //         description: "The event type ID to check.",
-  //       },
-  //     },
-  //     required: ["dateFrom", "dateTo", "eventTypeId"],
-  //   },
-  // },
+  {
+    name: "create_booking",
+    description: "Create a booking for the user.",
+    parameters: {
+      type: "object",
+      properties: {
+        end: {
+          type: "string",
+          description: "The end date of the range to check.",
+        },
+        eventTypeId: {
+          type: "integer",
+          description: "The event type ID to check.",
+        },
+        language: {
+          type: "string",
+          description: "The event type ID to check.",
+        },
+        start: {
+          type: "string",
+          description: "The event type ID to check.",
+        },
+        timeZone: {
+          type: "string",
+          description: "The event type ID to check.",
+        },
+        title: {
+          type: "string",
+          description: "The event type ID to check.",
+        },
+      },
+      required: ["end", "eventTypeId", "language", "start", "timeZone", "title"],
+    },
+  },
 ];
 
 export type Availability = {
@@ -206,4 +253,72 @@ export const fetchAvailability = async ({
     timeZone: data.timeZone,
     workingHours: data.workingHours,
   };
+};
+
+const createBooking = async ({
+  apiKey,
+  userId,
+  user,
+  eventTypeId,
+  start,
+  end,
+  timeZone,
+  language,
+}: {
+  apiKey: string;
+  userId: number;
+  user: {
+    username: string;
+    email: string;
+  };
+  eventTypeId: number;
+  start: string;
+  end: string;
+  timeZone: string;
+  language: string;
+  title?: string;
+  status?: string;
+}): Promise<string | Error | { error: string }> => {
+  const params = {
+    apiKey,
+    userId: userId.toString(),
+  };
+
+  const urlParams = new URLSearchParams(params);
+
+  const url = `${process.env.BACKEND_URL}/bookings?${urlParams.toString()}`;
+
+  const responses = {
+    name: user.username,
+    email: user.email,
+  };
+
+  const response = await fetch(url, {
+    body: JSON.stringify({
+      eventTypeId,
+      start,
+      end,
+      responses,
+      metadata: {},
+      timeZone,
+      language,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+  });
+
+  // Let GPT handle this. This will happen when wrong event type id is used.
+  // if (response.status === 401) throw new Error("Unauthorized");
+
+  const data = await response.json();
+
+  if (response.status !== 200) {
+    return {
+      error: data.message,
+    };
+  }
+
+  return "Booking created";
 };
