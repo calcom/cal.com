37a2a5f7c25bb6ad78649b372aa8c6b09029e4a9
Abhijit Roy <roya51788@gmail.com>
2023-12-03 20:31:48 +0530

    fix:#12240

--- packages/app-store/plausible/components/EventTypeAppCardInterface.tsx
+++ packages/app-store/plausible/components/EventTypeAppCardInterface.tsx
@@ -2,47 +2,56 @@ import { useAppContextWithSchema } from "@calcom/app-store/EventTypeAppContext";
 import AppCard from "@calcom/app-store/_components/AppCard";
 import useIsAppEnabled from "@calcom/app-store/_utils/useIsAppEnabled";
 import type { EventTypeAppCardComponent } from "@calcom/app-store/types";
-import { TextField } from "@calcom/ui";
 
 import type { appDataSchema } from "../zod";
 
 const EventTypeAppCard: EventTypeAppCardComponent = function EventTypeAppCard({ app, eventType }) {
   const { getAppData, setAppData, disabled } = useAppContextWithSchema<typeof appDataSchema>();
+  const { enabled, updateEnabled } = useIsAppEnabled(app);
+
+  // Additional data retrieval
   const plausibleUrl = getAppData("PLAUSIBLE_URL");
   const trackingId = getAppData("trackingId");
-  const { enabled, updateEnabled } = useIsAppEnabled(app);
+
+  const handleToggleEnabled = (value: boolean) => {
+    updateEnabled(value);
+  };
 
   return (
     <AppCard
       app={app}
-      switchOnClick={(e) => {
-        updateEnabled(e);
-      }}
+      switchOnClick={handleToggleEnabled}
       switchChecked={enabled}
       teamId={eventType.team?.id || undefined}>
       <div className="flex flex-col gap-2">
-        <TextField
-          name="Plausible URL"
-          defaultValue="https://plausible.io/js/script.js"
-          placeholder="https://plausible.io/js/script.js"
-          value={plausibleUrl}
-          disabled={disabled}
-          onChange={(e) => {
-            setAppData("PLAUSIBLE_URL", e.target.value);
-          }}
-        />
-        <TextField
-          disabled={disabled}
-          name="Tracked Domain"
-          placeholder="yourdomain.com"
-          value={trackingId}
-          onChange={(e) => {
-            setAppData("trackingId", e.target.value);
-          }}
-        />
+        <label>
+          Activate Plausible
+          <input
+            type="checkbox"
+            checked={enabled}
+            onChange={(e) => handleToggleEnabled(e.target.checked)}
+            disabled={disabled}
+          />
+        </label>
+        <label>
+          Plausible URL
+          <input
+            type="text"
+            value={plausibleUrl}
+            onChange={(e) => setAppData("PLAUSIBLE_URL", e.target.value)}
+            disabled={disabled}
+          />
+        </label>
+        <label>
+          Tracked Domain
+          <input
+            type="text"
+            value={trackingId}
+            onChange={(e) => setAppData("trackingId", e.target.value)}
+            disabled={disabled}
+          />
+        </label>
       </div>
     </AppCard>
   );
 };
-
-export default EventTypeAppCard;
