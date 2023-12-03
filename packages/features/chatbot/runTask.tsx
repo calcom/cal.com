a9f58bf895dfc78b5af996d983bab72afeea543e
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-12-03 01:20:30 -0500

    resolve two promises in one await

--- packages/features/chatbot/runTask.tsx
+++ packages/features/chatbot/runTask.tsx
@@ -2,11 +2,16 @@ import runConversation from "./runnables/conversationRunnable";
 import runRouter from "./runnables/routerRunnable";
 
 const runTask = async (inputValue: string) => {
-  // Runnable 1: parse the url params
-  const urlParamsResponse: any = await runRouter(inputValue);
+  // const urlParamsResponse: any = await runRouter(inputValue);
 
-  // Runnable 2: talk to the user
-  const conversationResponse: any = await runConversation(inputValue);
+  // const conversationResponse: any = await runConversation(inputValue);
+
+  const [urlParamsResponse, conversationResponse] = await Promise.all([
+    // Runnable 1: parse the url params
+    runRouter(inputValue),
+    // Runnable 2: talk to the user
+    runConversation(inputValue),
+  ]);
 
   const outValue = {
     urlParamsResponse: urlParamsResponse,
