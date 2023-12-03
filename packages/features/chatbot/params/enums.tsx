563ddf268f389742fef3b16ddde8cd2e5b037e0b
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-12-03 10:38:14 -0500

    add external links (to continue)

--- packages/features/chatbot/params/enums.tsx
+++ packages/features/chatbot/params/enums.tsx
@@ -1,14 +1,16 @@
 const other = "None";
 
 export const ROUTER_ENUMS: string[] = [
-  "/workflows",
-  "/event-types",
   "/apps",
+  "/apps/routing-forms/forms",
+  "/auth/setup?step=1",
+  "/availability",
   "/bookings/upcoming",
   "/bookings/recurring",
   "/bookings/past",
   "/bookings/cancelled",
-  "/availability",
+  "/event-types",
+  "/insights",
   "/settings/my-account/profile",
   "/settings/my-account/general",
   "/settings/my-account/appearance",
@@ -16,11 +18,19 @@ export const ROUTER_ENUMS: string[] = [
   "/settings/security/password",
   "/settings/security/two-factor-auth",
   "/settings/security/impersonation",
-  "/auth/setup?step=1",
   "/settings/developer/webhooks",
   "/settings/developer/api-keys",
   "/settings/billing",
+  "/teams",
+  "/workflows",
   other,
 ];
+
+// export const EXTERNAL_ROUTER_ENUMS: string[] = [
+//   "https://cal.com/download",
+//   "https://discord.com/invite/calcom",
+//   "https://github.com/calcom/cal.com/milestones",
+//   "https://www.youtube.com/watch?v=jvaBafzVUQc",
+// ];
 
 export const ROUTER_MSGS: string[] = ROUTER_ENUMS.map((x) => `Going to ${x}`);
