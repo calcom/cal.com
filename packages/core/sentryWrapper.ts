14177df1fcf6283b525f0c8eef17511960e6a442
Brendan Woodward <brendanwoodward19@gmail.com>
2023-12-03 01:48:18 +0000

    Make Sentry Wrapper utilize parent transaction, if it exists.

--- packages/core/sentryWrapper.ts
+++ packages/core/sentryWrapper.ts
@@ -1,55 +1,37 @@
-// import * as Sentry from "@sentry/nextjs";
-// // Takes in a callable function and creates a flow to wrap the function with Sentry for tracking the endpoint
-// const monitorCallback = async (cb: CallableFunction, ...args: any[]) => {
-//   // Start the Sentry performance monitoring transaction
-//   const transaction = Sentry.startTransaction({
-//     op: cb.name,
-//     name: `Sentry performance monitoring for ${cb.name}`,
-//   });
-//   try {
-//     // Await the callback if it's asynchronous
-//     const result = await cb(...args);
-//     return result;
-//   } catch (error) {
-//     // Capture exceptions in Sentry
-//     Sentry.captureException(error);
-//     throw error;
-//   } finally {
-//     // Ensure the transaction is finished
-//     transaction.finish();
-//   }
-// };
-// export default monitorCallback;
 import * as Sentry from "@sentry/nextjs";
 
 const monitorCallback = async (cb: CallableFunction, ...args: any[]) => {
-  const transaction = Sentry.startTransaction({
-    op: cb.name,
-    name: `Sentry performance monitoring for ${cb.name}`,
-  });
+  // Attempt to retrieve the current transaction from Sentry's scope
+  let transaction = Sentry.getCurrentHub().getScope()?.getTransaction();
 
-  // Create a main span for the entire callback function
-  const mainSpan = transaction.startChild({
-    op: "function",
+  // Check if there's an existing transaction, if not, start a new one
+  if (!transaction) {
+    transaction = Sentry.startTransaction({
+      op: cb.name,
+      //   name: `Call to ${cb.name}`,
+    });
+  }
+
+  // Start a new span in the current transaction
+  const span = transaction.startChild({
+    op: cb.name,
     description: `Executing ${cb.name}`,
   });
 
   try {
-    // Start a child span for the actual operation
-    const operationSpan = mainSpan.startChild({
-      op: "operation",
-      description: "Performing operation",
-    });
-
     const result = await cb(...args);
-    operationSpan.finish(); // Finish the operation span
     return result;
   } catch (error) {
     Sentry.captureException(error);
     throw error;
   } finally {
-    mainSpan.finish(); // Finish the main span
-    transaction.finish();
+    // Finish the span
+    span.finish();
+
+    // If this was a new transaction, finish it
+    if (!Sentry.getCurrentHub().getScope()?.getTransaction()) {
+      transaction.finish();
+    }
   }
 };
 
