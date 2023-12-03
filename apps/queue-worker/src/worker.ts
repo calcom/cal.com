df8987b6f28d33bf23c484a0e1ff561eb487f5c3
Peter Mbanugo <p.mbanugo@yahoo.com>
2023-12-03 12:43:46 +0100

    chore: add Sentry for error tracking

--- apps/queue-worker/src/worker.ts
+++ apps/queue-worker/src/worker.ts
@@ -1,3 +1,5 @@
+import * as Sentry from "@sentry/node";
+import { ProfilingIntegration } from "@sentry/profiling-node";
 import { NativeConnection, Worker } from "@temporalio/worker";
 import "dotenv/config";
 
@@ -5,27 +7,32 @@ import * as activities from "@calcom/emails/email-manager";
 
 import { NAMESPACE, TASK_QUEUE_NAME } from "./shared";
 
+// eslint-disable-next-line turbo/no-undeclared-env-vars
+const { SENTRY_DSN } = process.env;
+if (SENTRY_DSN) {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    integrations: [new ProfilingIntegration()],
+    tracesSampleRate: 1.0,
+    profilesSampleRate: 1.0,
+  });
+  console.info("Sentry initialized");
+}
+
 async function run() {
-  // Step 1: Establish a connection with Temporal server.
-  //
-  // Worker code uses `@temporalio/worker.NativeConnection`.
-  // (But in your application code it's `@temporalio/client.Connection`.)
   const connection = await NativeConnection.connect({
     address: "localhost:7233",
     // TLS and gRPC metadata configuration goes here.
   });
-  // Step 2: Register Workflows and Activities with the Worker.
+
   const worker = await Worker.create({
     connection,
     namespace: NAMESPACE,
     taskQueue: TASK_QUEUE_NAME,
-    // Workflows are registered using a path as they run in a separate JS context.
     workflowsPath: require.resolve("@calcom/emails/email-workflow"),
     activities,
   });
 
-  // Step 3: Start accepting tasks on the queue
-  //
   // The worker runs until it encounters an unexepected error or the process receives a shutdown signal registered on
   // the SDK Runtime object.
   //
