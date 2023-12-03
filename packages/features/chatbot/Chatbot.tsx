0e643856e1be1b36cae5a40132ae4329200409f6
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-12-03 02:32:40 -0500

    merge frontend and backend and merge conflicts

--- packages/features/chatbot/Chatbot.tsx
+++ packages/features/chatbot/Chatbot.tsx
@@ -1,11 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
+import { useGetTheme } from "@calcom/lib/hooks/useTheme";
+
+import "./Chatbot.css";
 import runTask from "./runTask";
 
+type chatResponse = {
+  type: string;
+  message: string;
+};
+
 const FloatingIcon = () => {
+  const ref = useRef<HTMLDivElement>(null);
   const [isWindowOpen, setIsWindowOpen] = useState(false);
   const [inputValue, setInputValue] = useState("");
-  const [chatLog, setChatLog] = useState<string[]>([]);
+  const [chatLog, setChatLog] = useState<chatResponse[]>([]);
+  const { resolvedTheme, forcedTheme } = useGetTheme();
+  const { isLoading, setIsLoading } = useState(false);
+  const hasDarkTheme = !forcedTheme && resolvedTheme === "dark";
 
   const toggleWindow = () => {
     setIsWindowOpen(!isWindowOpen);
@@ -14,6 +26,20 @@ const FloatingIcon = () => {
   const handleInputChange = (e: any) => {
     setInputValue(e.target.value);
   };
+
+  const scrollToBottom = () => {
+    if (ref.current) {
+      const scrollHeight = ref.current.scrollHeight;
+      const clientHeight = ref.current.clientHeight;
+      const maxScrollTop = scrollHeight - clientHeight;
+      ref.current.scrollTop = maxScrollTop > 0 ? maxScrollTop : 0;
+    }
+  };
+
+  // Scroll to bottom when the component mounts or when content changes
+  useEffect(() => {
+    scrollToBottom();
+  }, [chatLog]);
 
   const handleConfirmClick = () => {
     // Handle the confirm button click, you can add your logic here
@@ -24,6 +50,40 @@ const FloatingIcon = () => {
     // Update the chatLog state with the new entry
     setChatLog([...chatLog, inputValue]);
     runTask(inputValue);
+
+    if (inputValue === "") return;
+
+    const responses: chatResponse[] = [];
+
+    const currentResponse: chatResponse = {
+      type: "human",
+      message: inputValue,
+    };
+    setChatLog([...chatLog, currentResponse]);
+    console.log(ref);
+    responses.push(currentResponse);
+
+    const chatResponse = Promise.resolve(runTask(inputValue));
+
+    chatResponse.then((value) => {
+      console.log("Response: ", value);
+
+      const aiResponse: chatResponse = {
+        type: "aiResponse",
+        message: value.url_param,
+      };
+
+      responses.push(aiResponse);
+      setChatLog([...chatLog, ...responses]);
+    });
+    // setChatLog([...chatLog, ...responses]);
+    setInputValue("");
+  };
+
+  const handleKeyDown = (event: any) => {
+    if (event.key === "Enter" && inputValue !== "") {
+      handleConfirmClick();
+    }
   };
 
   const handleWindowClick = (e: any) => {
@@ -31,19 +91,35 @@ const FloatingIcon = () => {
     e.stopPropagation();
   };
 
-  const renderChatLog = chatLog.map((val, idx) => {
-    return (
+  const renderChatLog = chatLog.map((val) => {
+    return val.type === "aiResponse" ? (
       <div
-        key={idx}
+        key={val.message}
         style={{
           width: "fit-content",
-          border: "1px solid white",
+          border: hasDarkTheme ? "2px solid white" : "2px solid black",
           margin: "1rem 0 1rem 0",
           borderRadius: "30px",
           padding: "0.5rem 1rem",
           maxWidth: "80%",
         }}>
-        {val}
+        {val.message}
+      </div>
+    ) : (
+      <div
+        key={val.message}
+        style={{ width: "100%", display: "flex", justifyContent: "flex-end", margin: "1rem 0 1rem 0" }}>
+        <div
+          style={{
+            width: "fit-content",
+            border: hasDarkTheme ? "2px solid white" : "2px solid black",
+
+            borderRadius: "30px",
+            padding: "0.5rem 1rem",
+            maxWidth: "80%",
+          }}>
+          {val.message}
+        </div>
       </div>
     );
   });
@@ -58,58 +134,58 @@ const FloatingIcon = () => {
         zIndex: 1000,
       }}
       onClick={toggleWindow}>
-      <div
-        style={{
-          backgroundColor: isWindowOpen ? "#ff6347" : "#007bff",
-          color: "#000",
-          borderRadius: "50%",
-          width: "40px",
-          height: "40px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          fontSize: "20px",
-          boxShadow: "0 2px 10px rgba(0, 0, 0, 0.2)",
-          transition: "background-color 0.3s ease",
-        }}>
-        &#x263C;
-      </div>
       {isWindowOpen && (
         <div
           style={{
-            position: "absolute",
-            bottom: "60px",
+            bottom: "10rem",
             right: "0",
             border: "1px solid #ccc",
-            borderRadius: "8px",
-            padding: "10px",
-            backgroundColor: "#000",
+            borderRadius: "50px",
+            padding: "20px",
+            backgroundColor: hasDarkTheme ? "#000" : "#fff",
             boxShadow: "0 2px 10px rgba(0, 0, 0, 0.2)",
             display: "block",
+            position: "fixed",
+            right: "20px",
+            bottom: "8rem",
           }}
           onClick={handleWindowClick} // Handle click within the window
         >
-          <div style={{ height: "20rem", width: "20rem" }}>
-            <div>{renderChatLog}</div>
-            <div style={{ width: "20rem", position: "absolute", bottom: "0", marginBottom: "10px" }}>
+          <div style={{ height: "60vh", width: "30vw", position: "relative" }}>
+            <div
+              ref={ref}
+              style={{
+                height: "90%",
+                overflow: "scroll",
+                paddingBottom: "5rem",
+              }}
+              className="chatblock">
+              {renderChatLog}
+            </div>
+            <div style={{ width: "100%", position: "absolute", bottom: "0", marginBottom: "10px" }}>
               <input
                 type="text"
                 value={inputValue}
                 onChange={handleInputChange}
                 placeholder="Enter text..."
                 style={{
-                  background: "black",
-                  border: "2px solid white",
+                  color: hasDarkTheme
+                    ? "whitevscode-file://vscode-app/usr/share/code/resources/app/out/vs/code/electron-sandbox/workbench/workbench.html"
+                    : "black",
+                  background: hasDarkTheme ? "black" : "white",
+                  border: hasDarkTheme ? "1px solid white" : "1px solid black",
                   borderRadius: "50px",
-                  padding: "1rem 2rem",
+                  padding: "1rem 4rem 1rem 2rem",
                   width: "100%",
                 }}
+                onKeyDown={handleKeyDown}
               />
               <button
+                className="chatbot-button"
                 onClick={handleConfirmClick}
                 style={{
-                  color: "black",
-                  background: "white",
+                  color: hasDarkTheme ? "black" : "white",
+                  background: hasDarkTheme ? "white" : "black",
                   borderRadius: "50px",
                   aspectRatio: "1",
                   height: "80%",
@@ -119,6 +195,7 @@ const FloatingIcon = () => {
                   display: "flex",
                   justifyContent: "center",
                   alignItems: "center",
+                  fontSize: "1.5rem",
                 }}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -140,6 +217,36 @@ const FloatingIcon = () => {
           </div>
         </div>
       )}
+      <div
+        className="chatbot-button"
+        style={{
+          backgroundColor:
+            (!isWindowOpen && hasDarkTheme) || (isWindowOpen && !hasDarkTheme) ? "white" : "black",
+          borderRadius: "50%",
+          width: "5rem",
+          height: "5rem",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          fontSize: "20px",
+          boxShadow: "0 2px 10px rgba(0, 0, 0, 0.2)",
+          transition: "background-color 0.3s ease",
+          position: "absolute",
+          right: "20px",
+          bottom: "20px",
+        }}>
+        {/* <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--> */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="3rem"
+          width="3.2rem"
+          viewBox="0 0 640 512"
+          style={{
+            fill: (hasDarkTheme && !isWindowOpen) || (isWindowOpen && !hasDarkTheme) ? "black" : "white",
+          }}>
+          <path d="M320 0c17.7 0 32 14.3 32 32V96H472c39.8 0 72 32.2 72 72V440c0 39.8-32.2 72-72 72H168c-39.8 0-72-32.2-72-72V168c0-39.8 32.2-72 72-72H288V32c0-17.7 14.3-32 32-32zM208 384c-8.8 0-16 7.2-16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H208zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H304zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H400zM264 256a40 40 0 1 0 -80 0 40 40 0 1 0 80 0zm152 40a40 40 0 1 0 0-80 40 40 0 1 0 0 80zM48 224H64V416H48c-26.5 0-48-21.5-48-48V272c0-26.5 21.5-48 48-48zm544 0c26.5 0 48 21.5 48 48v96c0 26.5-21.5 48-48 48H576V224h16z" />
+        </svg>
+      </div>
     </div>
   );
 };
