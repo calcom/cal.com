686d736039e568166494f1183e1a2e537ab7448c
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-12-04 13:16:50 +0530

    validate api keys to set error and key states for api keys

--- packages/atoms/cal-provider/index.tsx
+++ packages/atoms/cal-provider/index.tsx
@@ -1,15 +1,45 @@
 import type { ReactNode } from "react";
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState, useCallback, useEffect } from "react";
 
 type CalProviderProps = {
   apiKey: string;
   children: ReactNode;
 };
 
-const ApiKeyContext = createContext("");
+const ApiKeyContext = createContext({ key: "", error: "" });
 
 export const useApiKey = () => useContext(ApiKeyContext);
 
 export function CalProvider({ apiKey, children }: CalProviderProps) {
-  return <ApiKeyContext.Provider value={apiKey}>{children}</ApiKeyContext.Provider>;
+  const [key, setKey] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const verifyApiKey = useCallback(
+    async (key: string) => {
+      try {
+        // here we'll call the /me endpoint in v2 to get user profile
+        const response = await fetch(`/v2/me?apiKey=${key}`);
+
+        if (response.ok) {
+          setKey(apiKey);
+        }
+      } catch (error) {
+        console.error(error);
+        setErrorMessage("invalid api key");
+      }
+    },
+    [apiKey]
+  );
+
+  useEffect(() => {
+    if (apiKey.length === 0) {
+      setErrorMessage("no key value");
+    } else {
+      verifyApiKey(apiKey);
+    }
+  }, [verifyApiKey, apiKey]);
+
+  return (
+    <ApiKeyContext.Provider value={{ key: key, error: errorMessage }}>{children}</ApiKeyContext.Provider>
+  );
 }
