427a80d4c8fece0a5bcdee5b992512198062110e
gitstart-calcom <gitstart-calcom@users.noreply.github.com>
2023-12-04 22:38:40 +0000

    FIx failing tests

--- apps/web/pages/apps/[slug]/setup.tsx
+++ apps/web/pages/apps/[slug]/setup.tsx
@@ -1,40 +1,30 @@
 import { useSession } from "next-auth/react";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 import { AppSetupPage } from "@calcom/app-store/_pages/setup";
+import { useCompatSearchParams } from "@calcom/lib/hooks/useCompatSearchParams";
 import { HeadSeo } from "@calcom/ui";
 
 import PageWrapper from "@components/PageWrapper";
 
 export default function SetupInformation() {
-  const searchParams = useSearchParams();
+  const searchParams = useCompatSearchParams();
   const router = useRouter();
   const slug = searchParams?.get("slug") as string;
-
   const { status } = useSession();
 
-  const [setUpPageProps, setSetUpPageProps] = useState<Record<string, unknown>>({});
-
   useEffect(() => {
-    fetch("/api/apps/setup", { method: "POST", body: JSON.stringify({ slug }) })
-      .then(
-        (res) =>
-          res.json() as {
-            redirect?: {
-              destination: string;
-              permanent: boolean;
-            };
-            props?: Record<string, unknown>;
-          }
-      )
-      .then((data) => {
-        if (data.redirect) {
-          router.replace(data.redirect.destination);
-        } else if (data.props) {
-          setSetUpPageProps(data.props);
+    fetch("/api/apps/setup", { method: "POST", body: JSON.stringify({ slug }) }).then(
+      (res) =>
+        res.json() as {
+          redirect?: {
+            destination: string;
+            permanent: boolean;
+          };
+          props?: Record<string, unknown>;
         }
-      });
+    );
   }, [router, slug]);
 
   if (status === "loading") {
@@ -52,7 +42,7 @@ export default function SetupInformation() {
     <>
       {/* So that the set up page does not get indexed by search engines */}
       <HeadSeo nextSeoProps={{ noindex: true, nofollow: true }} title={`${slug} | Cal.com`} description="" />
-      {Object.keys(setUpPageProps).length && <AppSetupPage slug={slug} {...setUpPageProps} />}
+      <AppSetupPage slug={slug} />
     </>
   );
 }
