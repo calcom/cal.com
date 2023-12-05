add8f5fd90510968de97b934c71f8fc148f86684
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-12-05 12:35:27 +0530

    hooks for oauth clients data

--- apps/web/pages/settings/platform/oauth-clients/hooks/useOAuthClients.ts
+++ apps/web/pages/settings/platform/oauth-clients/hooks/useOAuthClients.ts
@@ -1,6 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 // hook to fetch oauth clients data
 // react-query to fetch
 // {loading, fetching, data} = useQuery to fetch data with react-query
+
 export const useOAuthClient = () => {
-  return true;
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["oauth-clients"],
+    queryFn: () => {
+      return axios
+        .get(
+          // The below url is only for testing for the time being
+          // correct one would have a v2 endpoint
+          `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=d83d78f5987434a1d8eae1541a4cbf38`
+        )
+        .then((res) => res.data);
+    },
+  });
+
+  return { isLoading, error, data };
 };
