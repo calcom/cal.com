24b1f8ed59f6d99f0ecc589c691efb0ac1f2ac3b
supalarry <lauris.skraucis@gmail.com>
2023-12-06 11:00:34 +0100

    exclude password from prisma read service

--- apps/api/v2/src/modules/prisma/prisma-read.service.ts
+++ apps/api/v2/src/modules/prisma/prisma-read.service.ts
@@ -1,25 +1,65 @@
 import type { OnModuleInit } from "@nestjs/common";
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Prisma } from "@prisma/client";
 
 @Injectable()
 export class PrismaReadService implements OnModuleInit {
-  public prisma: PrismaClient;
+  public prisma: ExtendedPrismaClient;
 
   constructor(private readonly configService: ConfigService) {
     const dbUrl = configService.get("db.readUrl", { infer: true });
 
-    this.prisma = new PrismaClient({
-      datasources: {
-        db: {
-          url: dbUrl,
-        },
-      },
-    });
+    this.prisma = getExtendedPrismaClient(dbUrl);
   }
 
   async onModuleInit() {
     this.prisma.$connect();
   }
+}
+
+type ExtendedPrismaClient = ReturnType<typeof getExtendedPrismaClient>;
+
+function getExtendedPrismaClient(dbUrl: string) {
+  const prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: dbUrl,
+      },
+    },
+  });
+
+  const extendedPrisma = prisma.$extends({
+    name: "exclude-user-password",
+    query: {
+      user: {
+        async findUnique({ args, query }) {
+          const withoutPassword = excludePassword(args) as Prisma.UserFindUniqueArgs;
+          return query(withoutPassword);
+        },
+        async findFirst({ args, query }) {
+          const withoutPassword = excludePassword(args);
+          return query(withoutPassword);
+        },
+        async findMany({ args, query }) {
+          const withoutPassword = excludePassword(args);
+          return query(withoutPassword);
+        },
+      },
+    },
+  });
+
+  return extendedPrisma;
+}
+
+function excludePassword(
+  args: Prisma.UserFindUniqueArgs | Prisma.UserFindFirstArgs | Prisma.UserFindManyArgs
+) {
+  const argsCopy = { ...args };
+
+  if (argsCopy.select?.password !== true) {
+    argsCopy.select = { ...argsCopy.select, password: false };
+  }
+
+  return argsCopy;
 }
