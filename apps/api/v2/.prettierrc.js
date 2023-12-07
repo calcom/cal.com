217ff9e21dd75c83312d1f703b31788bc4e90d74
Morgan Vernay <morgan@cal.com>
2023-12-07 09:49:03 +0200

    fix: prettier config

--- apps/api/v2/.prettierrc.js
+++ apps/api/v2/.prettierrc.js
@@ -2,5 +2,5 @@ const rootConfig = require("../../../packages/config/prettier-preset");
 
 module.exports = {
   ...rootConfig,
-  importOrderParserPlugins: ["typescript", "decorators-legacy"],
+  importOrderParserPlugins: ["typescript", "decorators"],
 };
