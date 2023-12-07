373ebe7d06fa21dbe8fd453f751875b302a8ea1c
Morgan Vernay <morgan@cal.com>
2023-12-07 11:51:20 +0200

    improve error code types

--- packages/platform/types/api.ts
+++ packages/platform/types/api.ts
@@ -1,9 +1,11 @@
-import type { ERROR_STATUS, SUCCESS_STATUS } from "@calcom/platform-constants";
+import type { ERROR_STATUS, SUCCESS_STATUS, API_ERROR_CODES } from "@calcom/platform-constants";
 
 export type ApiSuccessResponse<T> = { status: typeof SUCCESS_STATUS; data: T };
 
+export type API_ERROR_CODES_TYPE = (typeof API_ERROR_CODES)[number];
+
 export type ErrorType = {
-  code: string;
+  code: API_ERROR_CODES_TYPE;
   message?: string;
   details?: string;
 };
