85717679aa707f115c0fd8f44162cb24fb0c7d36
supalarry <lauris.skraucis@gmail.com>
2023-12-08 17:24:04 +0100

    refactor: check if user set in RolesGuard

--- apps/api/v2/src/modules/auth/guard/roles/index.ts
+++ apps/api/v2/src/modules/auth/guard/roles/index.ts
@@ -15,6 +15,6 @@ export class RolesGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const user = request.user;
 
-    return requiredRoles.includes(user.role);
+    return user && requiredRoles.includes(user.role);
   }
 }
