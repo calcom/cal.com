a656cf5464ab2c2f16f6ac8e8c03bd9382aeac26
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-12-09 18:35:13 +0530

    fix api key destructing

--- packages/platform/atoms/connect-to-cal-button/index.tsx
+++ packages/platform/atoms/connect-to-cal-button/index.tsx
@@ -6,18 +6,18 @@ import { ConnectButton } from "../connect-to-cal-button/Button";
 // they will have to login/signup and then will be redirected to the permission page where they can see required permissions for the oAuth clients and can choose to deny or accept
 
 export function ConnectToCal() {
-  const key = useApiKey();
+  const { key, error } = useApiKey();
 
   const handleClick = () => {
     // TODO: the url to redirect should include a client_id and redirect_uri
     window.location.href = `https://app.cal.com/auth/login?client_id=%${key}&redirect_uri=`;
   };
 
-  if (key === "no_key") {
+  if (error === "no_key") {
     return <>You havent entered a key</>;
   }
 
-  if (key === "invalid_key") {
+  if (error === "invalid_key") {
     return <>This is not a valid key, please enter a valid key</>;
   }
 
