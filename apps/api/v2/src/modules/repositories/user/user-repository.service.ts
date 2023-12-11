71f7f3e134ae8e95461a9c25462301de627b0a83
supalarry <lauris.skraucis@gmail.com>
2023-12-11 14:26:00 +0100

    refactor: remove unused type

--- apps/api/v2/src/modules/repositories/user/user-repository.service.ts
+++ apps/api/v2/src/modules/repositories/user/user-repository.service.ts
@@ -2,33 +2,6 @@ import { PrismaReadService } from "@/modules/prisma/prisma-read.service";
 import { Injectable } from "@nestjs/common";
 import type { User } from "@prisma/client";
 
-type UserRelations = {
-  eventTypes?: boolean;
-  credentials?: boolean;
-  teams?: boolean;
-  bookings?: boolean;
-  schedules?: boolean;
-  selectedCalendars?: boolean;
-  availability?: boolean;
-  webhooks?: boolean;
-  destinationCalendar?: boolean;
-  metadata?: boolean;
-  impersonatedUsers?: boolean;
-  impersonatedBy?: boolean;
-  apiKeys?: boolean;
-  accounts?: boolean;
-  sessions?: boolean;
-  Feedback?: boolean;
-  ownedEventTypes?: boolean;
-  workflows?: boolean;
-  routingForms?: boolean;
-  verifiedNumbers?: boolean;
-  hosts?: boolean;
-  organization?: boolean;
-  accessCodes?: boolean;
-  platformOAuthClients?: boolean;
-};
-
 @Injectable()
 export class UserRepository {
   constructor(private readonly dbRead: PrismaReadService) {}
