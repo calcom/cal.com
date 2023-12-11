1802b256b5a6e5c9b6a62138c8cb1b6f75ed401b
supalarry <lauris.skraucis@gmail.com>
2023-12-11 11:03:59 +0100

    feat: user repository relations

--- apps/api/v2/src/modules/repositories/user/user-repository.service.ts
+++ apps/api/v2/src/modules/repositories/user/user-repository.service.ts
@@ -2,24 +2,53 @@ import { PrismaReadService } from "@/modules/prisma/prisma-read.service";
 import { Injectable } from "@nestjs/common";
 import type { User } from "@prisma/client";
 
+type UserRelations = {
+  eventTypes?: boolean;
+  credentials?: boolean;
+  teams?: boolean;
+  bookings?: boolean;
+  schedules?: boolean;
+  selectedCalendars?: boolean;
+  availability?: boolean;
+  webhooks?: boolean;
+  destinationCalendar?: boolean;
+  metadata?: boolean;
+  impersonatedUsers?: boolean;
+  impersonatedBy?: boolean;
+  apiKeys?: boolean;
+  accounts?: boolean;
+  sessions?: boolean;
+  Feedback?: boolean;
+  ownedEventTypes?: boolean;
+  workflows?: boolean;
+  routingForms?: boolean;
+  verifiedNumbers?: boolean;
+  hosts?: boolean;
+  organization?: boolean;
+  accessCodes?: boolean;
+  platformOAuthClients?: boolean;
+};
+
 @Injectable()
 export class UserRepository {
   constructor(private readonly dbRead: PrismaReadService) {}
 
-  async findById(userId: number) {
+  async findById(userId: number, includeRelations?: UserRelations) {
     const user = await this.dbRead.prisma.user.findUniqueOrThrow({
       where: {
         id: userId,
       },
+      include: includeRelations,
     });
     return this.sanitize(user);
   }
 
-  async findByEmail(email: string) {
+  async findByEmail(email: string, includeRelations?: UserRelations) {
     const user = await this.dbRead.prisma.user.findUniqueOrThrow({
       where: {
         email,
       },
+      include: includeRelations,
     });
     return this.sanitize(user);
   }
