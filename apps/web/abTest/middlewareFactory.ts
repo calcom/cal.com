a568e1ac66f4d2a65345d813ab1b84da0100cb35
DmytroHryshyn <125881252+DmytroHryshyn@users.noreply.github.com>
2023-12-12 21:48:40 +0200

    chore: [app-router-migration-5] apps/installed page (#12618)
    Co-authored-by: zomars <zomars@me.com>

--- apps/web/abTest/middlewareFactory.ts
+++ apps/web/abTest/middlewareFactory.ts
@@ -6,6 +6,7 @@ import z from "zod";
 const ROUTES: [URLPattern, boolean][] = [
   ["/event-types", process.env.APP_ROUTER_EVENT_TYPES_ENABLED === "1"] as const,
   ["/settings/admin/:path*", process.env.APP_ROUTER_SETTINGS_ADMIN_ENABLED === "1"] as const,
+  ["/apps/installed/:category", process.env.APP_ROUTER_APPS_INSTALLED_CATEGORY_ENABLED === "1"] as const,
   ["/apps/:slug", process.env.APP_ROUTER_APPS_SLUG_ENABLED === "1"] as const,
   ["/apps/:slug/setup", process.env.APP_ROUTER_APPS_SLUG_SETUP_ENABLED === "1"] as const,
   ["/apps/categories", process.env.APP_ROUTER_APPS_CATEGORIES_ENABLED === "1"] as const,
--- apps/web/playwright/ab-tests-redirect.e2e.ts
+++ apps/web/playwright/ab-tests-redirect.e2e.ts
@@ -5,6 +5,33 @@ import { test } from "./lib/fixtures";
 test.describe.configure({ mode: "parallel" });
 
 test.describe("apps/ A/B tests", () => {
+  test("should point to the /future/apps/installed/[category]", async ({ page, users, context }) => {
+    await context.addCookies([
+      {
+        name: "x-calcom-future-routes-override",
+        value: "1",
+        url: "http://localhost:3000",
+      },
+    ]);
+    const user = await users.create();
+
+    await user.apiLogin();
+
+    await page.goto("/apps/installed/messaging");
+
+    await page.waitForLoadState();
+
+    const dataNextJsRouter = await page.evaluate(() =>
+      window.document.documentElement.getAttribute("data-nextjs-router")
+    );
+
+    expect(dataNextJsRouter).toEqual("app");
+
+    const locator = page.getByRole("heading", { name: "Messaging" });
+
+    await expect(locator).toBeVisible();
+  });
+
   test("should point to the /future/apps/[slug]", async ({ page, users, context }) => {
     await context.addCookies([
       {
