6dd6c5329df46ad38015b20764860a154b49baf9
ManavpreetSingh <119919977+manavpreetsingh@users.noreply.github.com>
2023-12-14 02:06:32 +0530

    fix: changed checkbox color

--- apps/web/components/ui/form/CheckboxField.tsx
+++ apps/web/components/ui/form/CheckboxField.tsx
@@ -49,7 +49,7 @@ const CheckboxField = forwardRef<HTMLInputElement, Props>(
                     {...rest}
                     ref={ref}
                     type="checkbox"
-                    className="text-primary-600 focus:ring-primary-500 border-default bg-default h-4 w-4 rounded"
+                    className="text-emphasis focus:ring-emphasis dark:text-muted border-default bg-default h-4 w-4 rounded"
                   />
                 </div>
                 <span className="ms-2 text-sm">{description}</span>
