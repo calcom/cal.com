aec1d81de2328c4b040f88dfc9ee2988f03ade3b
Keith Williams <keithwillcode@gmail.com>
2023-12-14 16:29:56 -0500

    chore: Disabling Sentry because of memory issues

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -568,13 +568,13 @@ const nextConfig = {
   },
 };
 
-if (!!process.env.NEXT_PUBLIC_SENTRY_DSN) {
-  nextConfig["sentry"] = {
-    autoInstrumentServerFunctions: true,
-    hideSourceMaps: true,
-  };
+// if (!!process.env.NEXT_PUBLIC_SENTRY_DSN) {
+//   nextConfig["sentry"] = {
+//     autoInstrumentServerFunctions: true,
+//     hideSourceMaps: true,
+//   };
 
-  plugins.push(withSentryConfig);
-}
+//   plugins.push(withSentryConfig);
+// }
 
 module.exports = () => plugins.reduce((acc, next) => next(acc), nextConfig);
--- apps/web/sentry.edge.config.ts
+++ apps/web/sentry.edge.config.ts
@@ -1,5 +1,1 @@
-import * as Sentry from "@sentry/nextjs";
-
-Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-});
+export {};
