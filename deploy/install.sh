ce05f47e79e143f5c30d4ca5a1722192a5d73cc1
Riju Mondal <pilotrijurocks@gmail.com>
2023-12-15 18:22:58 +0530

    fix: updated the openssl rand -base64 32 with openssl rans -base64 24 as per document

--- deploy/install.sh
+++ deploy/install.sh
@@ -4,7 +4,7 @@ set -xeuf -o pipefail
 cp .env.example .env
 
 sed -i 's|NEXTAUTH_SECRET=.*|NEXTAUTH_SECRET='"$(openssl rand -base64 32)"'|g' .env
-sed -i 's|CALENDSO_ENCRYPTION_KEY=.*|CALENDSO_ENCRYPTION_KEY='"$(openssl rand -base64 32)"'|g' .env
+sed -i 's|CALENDSO_ENCRYPTION_KEY=.*|CALENDSO_ENCRYPTION_KEY='"$(openssl rand -base64 24)"'|g' .env
 yarn
 
 ./deploy/codespaces/install.sh
