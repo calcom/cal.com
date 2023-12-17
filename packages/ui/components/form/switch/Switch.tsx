c1e95bae3d7b3f6e4df6a7d76f48e5bb31e9d549
H9660 <lohahussain0@gmail.com>
2023-12-17 11:37:37 +0530

    Added new style for buttons:#12836

--- packages/ui/components/form/switch/Switch.tsx
+++ packages/ui/components/form/switch/Switch.tsx
@@ -44,7 +44,7 @@ const Switch = (
           className={cx(
             isChecked ? "bg-brand-default dark:bg-emphasis" : "bg-emphasis",
             primitiveProps.disabled && "cursor-not-allowed",
-            "focus:ring-brand-default h-5 w-[34px] rounded-full shadow-none focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-neutral-800 focus:ring-offset-1",
+            "focus:ring-brand-default h-5 w-[34px] rounded-full shadow-none focus:border-neutral-300 focus:outline-none",
             props.className
           )}
           {...primitiveProps}>
