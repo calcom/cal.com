e50942343f96d025a38ab0587c3d9585ed510d90
Guilherme Santos <contato.guisantos07@gmail.com>
2023-12-18 23:50:51 +0000

    revert: changes on the by id route

--- apps/api/pages/api/event-types/[id]/_auth-middleware.ts
+++ apps/api/pages/api/event-types/[id]/_auth-middleware.ts
@@ -2,19 +2,14 @@ import type { NextApiRequest } from "next";
 
 import { HttpError } from "@calcom/lib/http-error";
 
-import { schemaQueryIdAsString } from "~/lib/validations/shared/queryIdString";
 import { schemaQueryIdParseInt } from "~/lib/validations/shared/queryIdTransformParseInt";
 
 async function authMiddleware(req: NextApiRequest) {
   const { userId, isAdmin, prisma } = req;
-  const idParse = schemaQueryIdParseInt.safeParse(req.query);
-  const { id: slug } = schemaQueryIdAsString.parse(req.query);
-
-  const id = idParse.success ? idParse.data.id : undefined;
-
+  const { id } = schemaQueryIdParseInt.parse(req.query);
   if (isAdmin) return;
   const eventType = await prisma.eventType.findFirst({
-    where: { OR: [{ id }, { slug }], AND: { users: { some: { id: userId } } } },
+    where: { id, users: { some: { id: userId } } },
   });
   if (!eventType) throw new HttpError({ statusCode: 403, message: "Forbidden" });
 }
