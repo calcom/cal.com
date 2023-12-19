21f29d95fa83dd9ac0d8e672857644ab98396308
exception <erik@erosemberg.com>
2023-12-19 11:31:23 -0300

    chore: Prepare for redis

--- apps/api/v2/src/modules/auth/guard/oauth/access-token.guard.ts
+++ apps/api/v2/src/modules/auth/guard/oauth/access-token.guard.ts
@@ -1,9 +1,9 @@
-import { PrismaReadService } from "@/modules/prisma/prisma-read.service";
+import { OAuthFlowService } from "@/modules/oauth/flow/oauth-flow.service";
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
 
 @Injectable()
 export class AccessTokenGuard implements CanActivate {
-  constructor(private readonly prismaReadService: PrismaReadService) {}
+  constructor(private readonly oauthFlowService: OAuthFlowService) {}
 
   canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
@@ -14,25 +14,6 @@ export class AccessTokenGuard implements CanActivate {
       throw new UnauthorizedException();
     }
 
-    return this.validateToken(bearer);
-  }
-
-  async validateToken(accessToken: string) {
-    // TODO: read from redis once there, fall back to prisma if not found.
-    const token = await this.prismaReadService.prisma.accessToken.findFirst({
-      where: {
-        secret: accessToken,
-      },
-    });
-
-    if (!token) {
-      throw new UnauthorizedException();
-    }
-
-    if (new Date() > token.expiresAt) {
-      throw new UnauthorizedException("Token is no longer valid.");
-    }
-
-    return true;
+    return this.oauthFlowService.validateAccessToken(bearer);
   }
 }
--- apps/api/v2/src/modules/oauth/flow/oauth-flow.service.ts
+++ apps/api/v2/src/modules/oauth/flow/oauth-flow.service.ts
@@ -1,13 +1,49 @@
-import { Injectable, Logger } from "@nestjs/common";
+import { PrismaReadService } from "@/modules/prisma/prisma-read.service";
+import { BadRequestException, Injectable, Logger, UnauthorizedException } from "@nestjs/common";
 import { AccessToken } from "@prisma/client";
 
 @Injectable()
 export class OAuthFlowService {
   private logger = new Logger("OAuthFlowService");
 
+  constructor(private readonly prismaReadService: PrismaReadService) {}
+
   async propagateAccessToken(accessToken: AccessToken) {
     this.logger.log("Propagating access token to redis", accessToken.secret);
     // TODO propagate
     return void 0;
+  }
+
+  async validateAccessToken(secret: string) {
+    // status can be "CACHE_HIT" or "CACHE_MISS", MISS will most likely mean the token has expired
+    // but we need to check the SQL db for it anyways.
+    const { status } = await this.readFromCache(secret);
+
+    if (status === "CACHE_HIT") {
+      return true;
+    }
+
+    const token = await this.prismaReadService.prisma.accessToken.findFirst({
+      where: {
+        secret,
+      },
+      select: {
+        expiresAt: true,
+      },
+    });
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    if (new Date() > token?.expiresAt) {
+      throw new BadRequestException("Token is expired");
+    }
+
+    return true;
+  }
+
+  private async readFromCache(secret: string) {
+    return { status: "CACHE_MISS" };
   }
 }
