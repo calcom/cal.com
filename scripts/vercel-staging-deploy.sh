ef6bf3d451e4c441ccdef868f8c3e98da3236781
zomars <zomars@me.com>
2023-12-19 13:16:33 -0700

    chore: add staging deploy script for Vercel

---
+++ scripts/vercel-staging-deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ "$VERCEL_ENV" == "preview" ]; then exit 1; else exit 0; fi
+if [ "$SKIP_APP_DIR" == "1" ]; then
+  echo "Skipping app directory build"
+  rm -rf \
+    apps/web/app/\
+    apps/web/components/PageWrapperAppDir.tsx\
+    apps/web/lib/app-providers-app-dir.tsx\
+    apps/web/.next/types/app/
+fi
