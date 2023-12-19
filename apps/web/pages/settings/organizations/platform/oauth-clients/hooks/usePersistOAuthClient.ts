26d7fa3def762107afda7ea0d77a69249c0e2fb2
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-12-19 14:38:44 +0530

    split oauth persisit hook into create and delete hooks

--- apps/web/pages/settings/organizations/platform/oauth-clients/hooks/usePersistOAuthClient.ts
+++ apps/web/pages/settings/organizations/platform/oauth-clients/hooks/usePersistOAuthClient.ts
@@ -1,14 +1,15 @@
 import { useMutation } from "@tanstack/react-query";
 
-import type { ApiSuccessResponse, CreateOAuthClientInput } from "@calcom/platform-types";
+import { SUCCESS_STATUS } from "@calcom/platform-constants";
+import type { ApiResponse, CreateOAuthClientInput, DeleteOAuthClientInput } from "@calcom/platform-types";
 
-interface IUsePersistOAuthClient {
+interface IPersistOAuthClient {
   onSuccess?: () => void;
   onError?: () => void;
 }
-// hook to update, save and delete oauth clients data
-export const usePersistOAuthClient = (
-  { onSuccess, onError }: IUsePersistOAuthClient = {
+
+export const useCreateOAuthClient = (
+  { onSuccess, onError }: IPersistOAuthClient = {
     onSuccess: () => {
       return;
     },
@@ -18,12 +19,11 @@ export const usePersistOAuthClient = (
   }
 ) => {
   const mutation = useMutation<
-    ApiSuccessResponse<{ client_id: string; client_secret: string }>,
+    ApiResponse<{ client_id: string; client_secret: string }>,
     unknown,
     CreateOAuthClientInput
   >({
     mutationFn: (data) => {
-      console.log(data);
       return fetch("/api/v2/oauth-clients", {
         method: "post",
         headers: { "Content-type": "application/json" },
@@ -31,18 +31,54 @@ export const usePersistOAuthClient = (
       }).then((res) => res.json());
     },
     onSuccess: (data, variables, context) => {
-      // I will fire first
-      console.log("succes", data);
-      onSuccess?.();
+      if (data.status === SUCCESS_STATUS) {
+        onSuccess?.();
+      } else {
+        // handle api errors
+        // error
+      }
     },
     onError: (error, variables, context) => {
-      // I will fire first
-      console.error("error", error);
       onError?.();
     },
-    onSettled: (data, error, variables, context) => {
-      // I will fire first
+  });
+
+  return mutation;
+};
+
+export const useDeleteOAuthClient = ({ onSuccess, onError }: IPersistOAuthClient) => {
+  const mutation = useMutation<
+    ApiResponse<{ client_id: string; client_secret: string }>,
+    unknown,
+    DeleteOAuthClientInput
+  >({
+    mutationFn: (data) => {
+      const { id } = data;
+      console.log("Data for delete oauth client input:", data, id);
+
+      return fetch(`/api/v2/oauth-clients/${id}`, {
+        method: "delete",
+        headers: { "Content-type": "application/json" },
+      }).then((res) => {
+        console.log("This is the response", res.json());
+
+        return res.json();
+      });
+    },
+    onSuccess: (data, variables, context) => {
+      if (data.status === SUCCESS_STATUS) {
+        console.log("Deleted client successfully");
+
+        onSuccess?.();
+      } else {
+        // handle api errors
+        // error
+      }
+    },
+    onError: (error, variables, context) => {
+      onError?.();
     },
   });
+
   return mutation;
 };
