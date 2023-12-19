587d37426f8a848e49f23a0d84e9dd7686c3d836
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-12-19 14:36:41 +0530

    update oauth client card view with client id and secret

--- apps/web/pages/settings/organizations/platform/oauth-clients/components/OAuthClientList.tsx
+++ apps/web/pages/settings/organizations/platform/oauth-clients/components/OAuthClientList.tsx
@@ -1,10 +1,10 @@
-// card that will be displayed for an OAuth clients
-// name, logo, onDelete and onUpdate props
-import React from "react";
+import { useDeleteOAuthClient } from "@pages/settings/organizations/platform/oauth-clients/hooks/usePersistOAuthClient";
+import { Clipboard } from "lucide-react";
+import React, { useState } from "react";
 
 import { classNames } from "@calcom/lib";
 import type { Avatar } from "@calcom/prisma/client";
-import { Button } from "@calcom/ui";
+import { Button, Input, showToast } from "@calcom/ui";
 
 type OAuthClientCardProps = {
   name: string;
@@ -12,6 +12,8 @@ type OAuthClientCardProps = {
   redirect_uris: string[];
   permissions: number;
   lastItem: boolean;
+  id: string;
+  secret: string;
 };
 
 export const OAuthClientList = ({
@@ -19,8 +21,17 @@ export const OAuthClientList = ({
   logo,
   redirect_uris,
   permissions,
+  id,
+  secret,
   lastItem,
 }: OAuthClientCardProps) => {
+  const [inputValue, setInputValue] = useState(secret);
+  const { mutateAsync, isLoading } = useDeleteOAuthClient({});
+
+  const handleDelete = (id: string) => {
+    mutateAsync({ id: id });
+  };
+
   return (
     <div
       className={classNames(
@@ -29,23 +40,44 @@ export const OAuthClientList = ({
       )}>
       <div>
         <div className="flex gap-1">
-          <p className="font-semibold">{name}</p>
+          <p className="font-semibold">
+            Client: <span className="font-normal">{name}</span>
+          </p>
         </div>
         {!!logo && (
           <div>
             <>{logo}</>
           </div>
         )}
-        <div className=" w-[85%] gap-1">
-          <span className="text-sm font-semibold">Redirect uris: </span>
-          {redirect_uris.map((item, index) => (redirect_uris.length === index + 1 ? `${item}` : `${item}, `))}
+        <div className="flex flex-col gap-2">
+          <div>Client id: {id}</div>
         </div>
-        <div className="border-subtle">
-          <span className="text-sm font-semibold">Permissions: </span>Read bookings, write bookings
+        <div className="flex items-center gap-2">
+          <div>Client secret:</div>
+          <div className="flex items-center rounded-md">
+            <Input className="" readOnly={true} type="password" value={inputValue} />
+            <Button
+              type="button"
+              onClick={() => {
+                navigator.clipboard.writeText(inputValue);
+                showToast("Client secret copied successfully", "success");
+              }}
+              StartIcon={Clipboard}
+            />
+          </div>
+        </div>
+        <div className="border-subtle text-sm">
+          <span className="font-semibold">Permissions: </span>Read bookings, write bookings
+        </div>
+        <div className="flex gap-1 text-sm">
+          <span className="font-semibold">Redirect uris: </span>
+          {redirect_uris.map((item, index) => (redirect_uris.length === index + 1 ? `${item}` : `${item}, `))}
         </div>
       </div>
       <div className="flex items-center">
-        <Button className="bg-red-500 text-white">Delete</Button>
+        <Button className="bg-red-500 text-white" onClick={() => handleDelete(id)}>
+          Delete
+        </Button>
       </div>
     </div>
   );
