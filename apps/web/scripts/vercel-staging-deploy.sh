a3443b4e76f26925ec8a31774bf7ade43d7f086d
zomars <zomars@me.com>
2023-12-19 14:00:04 -0700

    fix: deploy script

--- apps/web/scripts/vercel-staging-deploy.sh
+++ apps/web/scripts/vercel-staging-deploy.sh
@@ -3,9 +3,9 @@
 if [ "$SKIP_APP_DIR" == "1" ]; then
   echo "Skipping app directory build"
   rm -rf \
-    apps/web/app/\
-    apps/web/components/PageWrapperAppDir.tsx\
-    apps/web/lib/app-providers-app-dir.tsx\
-    apps/web/.next/types/app/
+    app/\
+    components/PageWrapperAppDir.tsx\
+    lib/app-providers-app-dir.tsx\
+    .next/types/app/
 fi
 if [ "$VERCEL_ENV" == "preview" ]; then exit 1; else exit 0; fi
