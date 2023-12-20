b8bf4d4e7848fa61626a48ea1b0219e1e0add883
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-12-20 16:12:09 +0530

    hook for deleting a schedule

---
+++ packages/platform/atoms/availability/hooks/useDeleteSchedule.ts
@@ -0,0 +1,42 @@
+import { useMutation } from "@tanstack/react-query";
+
+import { SUCCESS_STATUS } from "@calcom/platform-constants";
+import type { ApiResponse, DeleteScheduleInput } from "@calcom/platform-types";
+
+interface IPDeleteOAuthClient {
+  onSuccess?: () => void;
+  onError?: () => void;
+}
+
+export const useDeleteSchedule = (
+  { onSuccess, onError }: IPDeleteOAuthClient = {
+    onSuccess: () => {
+      return;
+    },
+    onError: () => {
+      return;
+    },
+  }
+) => {
+  const mutation = useMutation<ApiResponse<undefined>, unknown, DeleteScheduleInput>({
+    mutationFn: (data) => {
+      const { id, key } = data;
+      return fetch(`/api/v2/schedules/${id}?apiKey=${key}`, {
+        method: "delete",
+        headers: { "Content-type": "application/json" },
+      }).then((res) => res.json());
+    },
+    onSuccess: (data) => {
+      if (data.status === SUCCESS_STATUS) {
+        onSuccess?.();
+      } else {
+        onError?.();
+      }
+    },
+    onError: () => {
+      onError?.();
+    },
+  });
+
+  return mutation;
+};
