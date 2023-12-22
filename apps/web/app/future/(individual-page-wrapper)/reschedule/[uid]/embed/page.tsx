48c4e4b6b1cc63723057cb0f6fe9682d96687e85
Dmytro Hryshyn <dev.dmytroh@gmail.com>
2023-12-22 18:23:57 +0200

    fin

--- apps/web/app/future/(individual-page-wrapper)/reschedule/[uid]/embed/page.tsx
+++ apps/web/app/future/(individual-page-wrapper)/reschedule/[uid]/embed/page.tsx
@@ -1,14 +1,21 @@
 import type { Params } from "next/dist/shared/lib/router/utils/route-matcher";
-import { getData } from "../../page";
+import { cookies, headers } from "next/headers";
+
+import { buildLegacyCtx } from "@lib/buildLegacyCtx";
+import withEmbedSsr from "@lib/withEmbedSsr";
+
+import { getServerSideProps, withAppDir } from "../page";
 
 type PageProps = Readonly<{
-	params: Params;
+  params: Params;
 }>;
 
-const Page = ({ params }: PageProps) => {
-	await getData(params, true);
+const Page = async ({ params }: PageProps) => {
+  const legacyCtx = buildLegacyCtx(headers(), cookies(), params);
+  // @ts-expect-error Argument of type '{ query: Params; params: Params; req: { headers: ReadonlyHeaders; cookies: ReadonlyRequestCookies; }; }'
+  await withAppDir(withEmbedSsr(getServerSideProps))(legacyCtx);
 
-	return null;
+  return null;
 };
-	
+
 export default Page;
