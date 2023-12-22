846e88527fc4fb14629edb38ef0709ce0b048fa9
Morgan Vernay <morgan@cal.com>
2023-12-22 10:26:02 +0100

    fix: more entry points

--- packages/platform/examples/base/src/pages/_app.tsx
+++ packages/platform/examples/base/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-import { CalProvider } from "@calcom/platform-atoms";
+import { CalProvider } from "@calcom/platform-atoms/components";
 import "@calcom/platform-atoms/dist/globals.min.css";
 
 export default function App({ Component, pageProps }: AppProps) {
