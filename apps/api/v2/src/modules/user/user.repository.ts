e9136ce2c7db05a7e65f808fa9f372bab4c89e84
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-12-27 13:24:52 +0530

    add function to get user from db

--- apps/api/v2/src/modules/user/user.repository.ts
+++ apps/api/v2/src/modules/user/user.repository.ts
@@ -1,8 +1,16 @@
 import { PrismaReadService } from "@/modules/prisma/prisma-read.service";
-import { PrismaWriteService } from "@/modules/prisma/prisma-write.service";
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class UserRepository {
-  constructor(private readonly dbRead: PrismaReadService, private readonly dbWrite: PrismaWriteService) {}
+  constructor(private readonly dbRead: PrismaReadService) {}
+
+  async getUserInfo(clientId: string, clientSecret: string) {
+    return this.dbRead.prisma.user.findUnique({
+      where: {
+        id: clientId,
+        secret: clientSecret,
+      },
+    });
+  }
 }
