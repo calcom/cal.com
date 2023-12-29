e2795b1377fa14eb484daf5b00b80949078f7d21
Dmytro Hryshyn <dev.dmytroh@gmail.com>
2023-12-29 19:25:08 +0200

    remove-route-groups

---
+++ apps/web/app/future/settings/admin/page.tsx
@@ -0,0 +1,23 @@
+import LegacyPage from "@pages/settings/admin/index";
+import { _generateMetadata } from "app/_utils";
+import { headers } from "next/headers";
+
+import PageWrapper from "@components/PageWrapperAppDir";
+import { getLayout } from "@components/auth/layouts/AdminLayoutAppDir";
+
+export const generateMetadata = async () =>
+  await _generateMetadata(
+    () => "Admin",
+    () => "admin_description"
+  );
+
+export default async function Page() {
+  const h = headers();
+  const nonce = h.get("x-nonce") ?? undefined;
+
+  return (
+    <PageWrapper getLayout={getLayout} requiresLicense={false} nonce={nonce} themeBasis={null}>
+      <LegacyPage />
+    </PageWrapper>
+  );
+}
