b7e0f32f354c6245d19b126944f17716839ae90e
Riddhesh Mahajan <40472653+riddhesh-mahajan@users.noreply.github.com>
2024-01-02 16:11:39 +0530

    fix: ui flickering issue

--- apps/web/components/getting-started/steps-views/UserSettings.tsx
+++ apps/web/components/getting-started/steps-views/UserSettings.tsx
@@ -1,5 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect } from "react";
+import { useEffect, memo } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -20,114 +20,118 @@ interface IUserSettingsProps {
   hideUsername?: boolean;
 }
 
-const UserSettings = (props: IUserSettingsProps) => {
-  const { nextStep } = props;
-  const [user] = trpc.viewer.me.useSuspenseQuery();
-  const { t } = useLocale();
-  const { setTimezone: setSelectedTimeZone, timezone: selectedTimeZone } = useTimePreferences();
-  const telemetry = useTelemetry();
-  const userSettingsSchema = z.object({
-    name: z
-      .string()
-      .min(1)
-      .max(FULL_NAME_LENGTH_MAX_LIMIT, {
-        message: t("max_limit_allowed_hint", { limit: FULL_NAME_LENGTH_MAX_LIMIT }),
-      }),
-  });
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    watch,
-  } = useForm<z.infer<typeof userSettingsSchema>>({
-    defaultValues: {
-      name: user?.name || "",
-    },
-    reValidateMode: "onChange",
-    resolver: zodResolver(userSettingsSchema),
-  });
-
-  useEffect(() => {
-    telemetry.event(telemetryEventTypes.onboardingStarted);
-  }, [telemetry]);
-
-  const utils = trpc.useContext();
-  const onSuccess = async () => {
-    await utils.viewer.me.invalidate();
-    nextStep();
-  };
-  const mutation = trpc.viewer.updateProfile.useMutation({
-    onSuccess: onSuccess,
-  });
-
-  const onSubmit = handleSubmit((data) => {
-    mutation.mutate({
-      name: data.name,
-      timeZone: selectedTimeZone,
+// eslint-disable-next-line react/display-name
+const UserSettings = memo(
+  (props: IUserSettingsProps) => {
+    const { nextStep } = props;
+    const [user] = trpc.viewer.me.useSuspenseQuery();
+    const { t } = useLocale();
+    const { setTimezone: setSelectedTimeZone, timezone: selectedTimeZone } = useTimePreferences();
+    const telemetry = useTelemetry();
+    const userSettingsSchema = z.object({
+      name: z
+        .string()
+        .min(1)
+        .max(FULL_NAME_LENGTH_MAX_LIMIT, {
+          message: t("max_limit_allowed_hint", { limit: FULL_NAME_LENGTH_MAX_LIMIT }),
+        }),
     });
-  });
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+      watch,
+    } = useForm<z.infer<typeof userSettingsSchema>>({
+      defaultValues: {
+        name: user?.name || "",
+      },
+      reValidateMode: "onChange",
+      resolver: zodResolver(userSettingsSchema),
+    });
 
-  const watchName = watch("name");
-  useEffect(() => {
-    if (watchName || selectedTimeZone) {
-      props.onNameOrTimezoneChange(watchName, selectedTimeZone);
-    }
-  }, [watchName, selectedTimeZone]);
+    useEffect(() => {
+      telemetry.event(telemetryEventTypes.onboardingStarted);
+    }, [telemetry]);
 
-  return (
-    <form onSubmit={onSubmit}>
-      <div className="space-y-6">
-        {/* Username textfield: when not coming from signup */}
-        {!props.hideUsername && <UsernameAvailabilityField />}
+    const utils = trpc.useContext();
+    const onSuccess = async () => {
+      await utils.viewer.me.invalidate();
+      nextStep();
+    };
+    const mutation = trpc.viewer.updateProfile.useMutation({
+      onSuccess: onSuccess,
+    });
 
-        {/* Full name textfield */}
-        <div className="w-full">
-          <label htmlFor="name" className="text-default mb-2 block text-sm font-medium">
-            {t("full_name")}
-          </label>
-          <Input
-            {...register("name", {
-              required: true,
-            })}
-            id="name"
-            name="name"
-            type="text"
-            autoComplete="off"
-            autoCorrect="off"
-          />
-          {errors.name && (
-            <p data-testid="required" className="py-2 text-xs text-red-500">
-              {errors.name.message}
+    const onSubmit = handleSubmit((data) => {
+      mutation.mutate({
+        name: data.name,
+        timeZone: selectedTimeZone,
+      });
+    });
+
+    const watchName = watch("name");
+    useEffect(() => {
+      if (watchName || selectedTimeZone) {
+        props.onNameOrTimezoneChange(watchName, selectedTimeZone);
+      }
+    }, [watchName, selectedTimeZone]);
+
+    return (
+      <form onSubmit={onSubmit}>
+        <div className="space-y-6">
+          {/* Username textfield: when not coming from signup */}
+          {!props.hideUsername && <UsernameAvailabilityField />}
+
+          {/* Full name textfield */}
+          <div className="w-full">
+            <label htmlFor="name" className="text-default mb-2 block text-sm font-medium">
+              {t("full_name")}
+            </label>
+            <Input
+              {...register("name", {
+                required: true,
+              })}
+              id="name"
+              name="name"
+              type="text"
+              autoComplete="off"
+              autoCorrect="off"
+            />
+            {errors.name && (
+              <p data-testid="required" className="py-2 text-xs text-red-500">
+                {errors.name.message}
+              </p>
+            )}
+          </div>
+          {/* Timezone select field */}
+          <div className="w-full">
+            <label htmlFor="timeZone" className="text-default block text-sm font-medium">
+              {t("timezone")}
+            </label>
+
+            <TimezoneSelect
+              id="timeZone"
+              value={selectedTimeZone}
+              onChange={({ value }) => setSelectedTimeZone(value)}
+              className="mt-2 w-full rounded-md text-sm"
+            />
+
+            <p className="text-subtle mt-3 flex flex-row font-sans text-xs leading-tight">
+              {t("current_time")} {dayjs().tz(selectedTimeZone).format("LT").toString().toLowerCase()}
             </p>
-          )}
+          </div>
         </div>
-        {/* Timezone select field */}
-        <div className="w-full">
-          <label htmlFor="timeZone" className="text-default block text-sm font-medium">
-            {t("timezone")}
-          </label>
-
-          <TimezoneSelect
-            id="timeZone"
-            value={selectedTimeZone}
-            onChange={({ value }) => setSelectedTimeZone(value)}
-            className="mt-2 w-full rounded-md text-sm"
-          />
-
-          <p className="text-subtle mt-3 flex flex-row font-sans text-xs leading-tight">
-            {t("current_time")} {dayjs().tz(selectedTimeZone).format("LT").toString().toLowerCase()}
-          </p>
-        </div>
-      </div>
-      <Button
-        type="submit"
-        className="mt-8 flex w-full flex-row justify-center"
-        disabled={mutation.isLoading}>
-        {t("next_step_text")}
-        <ArrowRight className="ml-2 h-4 w-4 self-center" aria-hidden="true" />
-      </Button>
-    </form>
-  );
-};
+        <Button
+          type="submit"
+          className="mt-8 flex w-full flex-row justify-center"
+          disabled={mutation.isLoading}>
+          {t("next_step_text")}
+          <ArrowRight className="ml-2 h-4 w-4 self-center" aria-hidden="true" />
+        </Button>
+      </form>
+    );
+  },
+  () => true
+);
 
 export { UserSettings };
