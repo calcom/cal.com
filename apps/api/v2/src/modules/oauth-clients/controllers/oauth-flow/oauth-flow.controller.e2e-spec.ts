ddc6131296e6f6a659c2b1add68c641505816e6a
supalarry <lauris.skraucis@gmail.com>
2024-01-03 11:26:56 +0200

    oauth flow tests

--- apps/api/v2/src/modules/oauth-clients/controllers/oauth-flow/oauth-flow.controller.e2e-spec.ts
+++ apps/api/v2/src/modules/oauth-clients/controllers/oauth-flow/oauth-flow.controller.e2e-spec.ts
@@ -1,144 +1,175 @@
+import { bootstrap } from "@/app";
 import { AppModule } from "@/app.module";
 import { HttpExceptionFilter } from "@/filters/http-exception.filter";
 import { PrismaExceptionFilter } from "@/filters/prisma-exception.filter";
 import { ZodExceptionFilter } from "@/filters/zod-exception.filter";
 import { AuthModule } from "@/modules/auth/auth.module";
 import { OAuthAuthorizeInput } from "@/modules/oauth-clients/inputs/authorize.input";
+import { ExchangeAuthorizationCodeInput } from "@/modules/oauth-clients/inputs/exchange-code.input";
 import { OAuthClientModule } from "@/modules/oauth-clients/oauth-client.module";
 import { PrismaModule } from "@/modules/prisma/prisma.module";
 import { UsersModule } from "@/modules/users/users.module";
 import { INestApplication } from "@nestjs/common";
+import { NestExpressApplication } from "@nestjs/platform-express";
 import { Test, TestingModule } from "@nestjs/testing";
-import { Membership, PlatformOAuthClient, Team, User } from "@prisma/client";
+import { PlatformOAuthClient, Team, User } from "@prisma/client";
 import * as request from "supertest";
-import { MembershipRepositoryFixture } from "test/fixtures/repository/membership.repository.fixture";
 import { OAuthClientRepositoryFixture } from "test/fixtures/repository/oauth-client.repository.fixture";
 import { TeamRepositoryFixture } from "test/fixtures/repository/team.repository.fixture";
 import { UserRepositoryFixture } from "test/fixtures/repository/users.repository.fixture";
 import { withNextAuth } from "test/utils/withNextAuth";
 
-describe("OAuthFlow Endpoints", () => {
-  let app: INestApplication;
-  let oauthClientRepositoryFixture: OAuthClientRepositoryFixture;
-  let testClient: PlatformOAuthClient;
-  const userEmail = "user@api.com";
-  let authorizationCode: string | null;
-  let refreshToken: string;
-  let membership: Membership;
-  let organization: Team;
-  let teamRepositoryFixture: TeamRepositoryFixture;
-  let usersFixtures: UserRepositoryFixture;
-  let membershipFixtures: MembershipRepositoryFixture;
-  let user: User;
+import { X_CAL_SECRET_KEY } from "@calcom/platform-constants";
 
-  beforeAll(async () => {
-    const moduleRef: TestingModule = await withNextAuth(
-      userEmail,
-      Test.createTestingModule({
+describe("OAuthFlow Endpoints", () => {
+  describe("User Not Authenticated", () => {
+    let appWithoutAuth: INestApplication;
+
+    beforeAll(async () => {
+      const moduleRef = await Test.createTestingModule({
         providers: [PrismaExceptionFilter, HttpExceptionFilter, ZodExceptionFilter],
         imports: [AppModule, OAuthClientModule, UsersModule, AuthModule, PrismaModule],
-      })
-    ).compile();
-
-    app = moduleRef.createNestApplication();
-    await app.init();
-
-    // Setup OAuthClientRepositoryFixture
-    oauthClientRepositoryFixture = new OAuthClientRepositoryFixture(moduleRef);
-    teamRepositoryFixture = new TeamRepositoryFixture(moduleRef);
-    usersFixtures = new UserRepositoryFixture(moduleRef);
-    membershipFixtures = new MembershipRepositoryFixture(moduleRef);
-
-    // Create a test OAuth client
-    user = await usersFixtures.create({
-      email: userEmail,
+      }).compile();
+      appWithoutAuth = moduleRef.createNestApplication();
+      bootstrap(appWithoutAuth as NestExpressApplication);
+      await appWithoutAuth.init();
     });
-    organization = await teamRepositoryFixture.create({ name: "organization" });
-    membership = await membershipFixtures.addUserToOrg(user, organization, "OWNER", true);
-    testClient = await createOAuthClient(organization.id);
+
+    it(`POST /oauth/:clientId/authorize missing Cookie with user`, () => {
+      return request(appWithoutAuth.getHttpServer()).post("/api/v2/oauth/100/authorize").expect(401);
+    });
+
+    it(`POST /oauth/:clientId/exchange missing Authorization Bearer token`, () => {
+      return request(appWithoutAuth.getHttpServer()).post("/api/v2/oauth/100/exchange").expect(400);
+    });
+
+    it(`POST /oauth/:clientId/refresh missing ${X_CAL_SECRET_KEY} header with secret`, () => {
+      return request(appWithoutAuth.getHttpServer()).post("/api/v2/oauth/100/refresh").expect(401);
+    });
+
+    afterAll(async () => {
+      await appWithoutAuth.close();
+    });
   });
 
-  async function createOAuthClient(organizationId: number) {
-    const data = {
-      logo: "logo-url",
-      name: "name",
-      redirectUris: ["redirect-uri"],
-      permissions: 32,
-    };
-    const secret = "secret";
+  describe("User Authenticated", () => {
+    let app: INestApplication;
 
-    const client = await oauthClientRepositoryFixture.create(organizationId, data, secret);
-    return client;
-  }
+    let usersRepositoryFixtures: UserRepositoryFixture;
+    let organizationsRepositoryFixture: TeamRepositoryFixture;
+    let oAuthClientsRepositoryFixture: OAuthClientRepositoryFixture;
 
-  describe("Authorize Endpoint", () => {
-    it.only("POST /oauth/:clientId/authorize", async () => {
-      const body: OAuthAuthorizeInput = {
-        redirectUri: testClient.redirectUris[0],
+    let user: User;
+    let organization: Team;
+    let oAuthClient: PlatformOAuthClient;
+
+    let authorizationCode: string | null;
+    let refreshToken: string;
+
+    beforeAll(async () => {
+      const userEmail = "developer@platform.com";
+
+      const moduleRef: TestingModule = await withNextAuth(
+        userEmail,
+        Test.createTestingModule({
+          providers: [PrismaExceptionFilter, HttpExceptionFilter, ZodExceptionFilter],
+          imports: [AppModule, OAuthClientModule, UsersModule, AuthModule, PrismaModule],
+        })
+      ).compile();
+
+      app = moduleRef.createNestApplication();
+      await app.init();
+
+      oAuthClientsRepositoryFixture = new OAuthClientRepositoryFixture(moduleRef);
+      organizationsRepositoryFixture = new TeamRepositoryFixture(moduleRef);
+      usersRepositoryFixtures = new UserRepositoryFixture(moduleRef);
+
+      user = await usersRepositoryFixtures.create({
+        email: userEmail,
+      });
+      organization = await organizationsRepositoryFixture.create({ name: "organization" });
+      oAuthClient = await createOAuthClient(organization.id);
+    });
+
+    async function createOAuthClient(organizationId: number) {
+      const data = {
+        logo: "logo-url",
+        name: "name",
+        redirectUris: ["redirect-uri.com"],
+        permissions: 32,
       };
+      const secret = "secret";
 
-      const REDIRECT_STATUS = 302;
+      const client = await oAuthClientsRepositoryFixture.create(organizationId, data, secret);
+      return client;
+    }
 
-      const response = await request(app.getHttpServer())
-        .post(`/oauth/${testClient.id}/authorize`)
-        .send(body)
-        .expect(REDIRECT_STATUS);
+    describe("Authorize Endpoint", () => {
+      it("POST /oauth/:clientId/authorize", async () => {
+        const body: OAuthAuthorizeInput = {
+          redirectUri: oAuthClient.redirectUris[0],
+        };
 
-      console.log("asap response", JSON.stringify(response, null, 2));
+        const REDIRECT_STATUS = 302;
 
-      const baseUrl = "http://www.localhost/";
-      const redirectUri = new URL(response.header.location, baseUrl);
-      authorizationCode = redirectUri.searchParams.get("code");
-      expect(authorizationCode).toBeDefined();
+        const response = await request(app.getHttpServer())
+          .post(`/oauth/${oAuthClient.id}/authorize`)
+          .send(body)
+          .expect(REDIRECT_STATUS);
+
+        const baseUrl = "http://www.localhost/";
+        const redirectUri = new URL(response.header.location, baseUrl);
+        authorizationCode = redirectUri.searchParams.get("code");
+        expect(authorizationCode).toBeDefined();
+      });
     });
-  });
 
-  // describe('Exchange Endpoint', () => {
-  //   it('POST /oauth/:clientId/exchange', async () => {
-  //     const authorizationToken = 'Bearer exampleBearerToken';
-  //     const body = {
-  //       clientSecret: testClient.secret,
-  //       code: authorizationCode,
-  //     };
+    describe("Exchange Endpoint", () => {
+      it("POST /oauth/:clientId/exchange", async () => {
+        const authorizationToken = `Bearer ${authorizationCode}`;
+        const body: ExchangeAuthorizationCodeInput = {
+          clientSecret: oAuthClient.secret,
+        };
 
-  //     const response = await request(app.getHttpServer())
-  //       .post(`/oauth/${testClient.id}/exchange`)
-  //       .set('Authorization', authorizationToken)
-  //       .send(body)
-  //       .expect(200);
+        const response = await request(app.getHttpServer())
+          .post(`/oauth/${oAuthClient.id}/exchange`)
+          .set("Authorization", authorizationToken)
+          .send(body)
+          .expect(200);
 
-  //     expect(response.body).toHaveProperty('data');
-  //     expect(response.body.data).toHaveProperty('accessToken');
-  //     expect(response.body.data).toHaveProperty('refreshToken');
-  //     refreshToken = response.body.data.refreshToken;
-  //   });
-  // });
+        expect(response.body).toHaveProperty("data");
+        expect(response.body.data).toHaveProperty("accessToken");
+        expect(response.body.data).toHaveProperty("refreshToken");
+        refreshToken = response.body.data.refreshToken;
+      });
+    });
 
-  // describe('Refresh Token Endpoint', () => {
-  //   it('POST /oauth/:clientId/refresh', () => {
-  //     const secretKey = testClient.secret;
-  //     const body = {
-  //       refreshToken,
-  //     };
+    describe("Refresh Token Endpoint", () => {
+      it("POST /oauth/:clientId/refresh", () => {
+        const secretKey = oAuthClient.secret;
+        const body = {
+          refreshToken,
+        };
 
-  //     return request(app.getHttpServer())
-  //       .post(`/oauth/${testClient.id}/refresh`)
-  //       .set('x-cal-secret-key', secretKey)
-  //       .send(body)
-  //       .expect(200)
-  //       .then((response) => {
-  //         expect(response.body).toHaveProperty('data');
-  //         expect(response.body.data).toHaveProperty('accessToken');
-  //         expect(response.body.data).toHaveProperty('refreshToken');
-  //       });
-  //   });
-  // });
+        return request(app.getHttpServer())
+          .post(`/oauth/${oAuthClient.id}/refresh`)
+          .set("x-cal-secret-key", secretKey)
+          .send(body)
+          .expect(200)
+          .then((response) => {
+            expect(response.body).toHaveProperty("data");
+            expect(response.body.data).toHaveProperty("accessToken");
+            expect(response.body.data).toHaveProperty("refreshToken");
+          });
+      });
+    });
 
-  afterAll(async () => {
-    await oauthClientRepositoryFixture.delete(testClient.id);
-    await teamRepositoryFixture.delete(organization.id);
-    await usersFixtures.delete(user.id);
+    afterAll(async () => {
+      await oAuthClientsRepositoryFixture.delete(oAuthClient.id);
+      await organizationsRepositoryFixture.delete(organization.id);
+      await usersRepositoryFixtures.delete(user.id);
 
-    await app.close();
+      await app.close();
+    });
   });
 });
