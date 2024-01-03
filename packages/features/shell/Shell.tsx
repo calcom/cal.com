2d31d52d570013a122421f4631b56ca8486f648c
Riddhesh Mahajan <40472653+riddhesh-mahajan@users.noreply.github.com>
2024-01-03 21:28:57 +0530

    fix: skeleton width for settings menu in sidebar

--- packages/features/shell/Shell.tsx
+++ packages/features/shell/Shell.tsx
@@ -974,7 +974,7 @@ function SideBar({ bannersHeight, user }: SideBarProps) {
                     <div className="flex">{t(item.name)}</div>
                   </span>
                 ) : (
-                  <SkeletonText style={{ width: `${item.name.length * 10}px` }} className="h-[20px]" />
+                  <SkeletonText className="h-[20px] w-full" />
                 )}
               </ButtonOrLink>
             </Tooltip>
