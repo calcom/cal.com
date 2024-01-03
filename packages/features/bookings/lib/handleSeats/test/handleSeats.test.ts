d3657c03971ea62416835938cd161e98acd2d6f0
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-01-03 14:46:25 -0500

    Test when attendee already signs up for booking

--- packages/features/bookings/lib/handleSeats/test/handleSeats.test.ts
+++ packages/features/bookings/lib/handleSeats/test/handleSeats.test.ts
@@ -379,8 +379,8 @@ describe("handleSeats", () => {
         const handleNewBooking = (await import("@calcom/features/bookings/lib/handleNewBooking")).default;
 
         const booker = getBooker({
-          email: "booker@example.com",
-          name: "Booker",
+          email: "seat2@example.com",
+          name: "Seat 2",
         });
 
         const organizer = getOrganizer({
@@ -503,6 +503,117 @@ describe("handleSeats", () => {
             bookingId: 1,
           })
         );
+      });
+
+      test("If attendee is already a part of the booking then throw an error", async () => {
+        const handleNewBooking = (await import("@calcom/features/bookings/lib/handleNewBooking")).default;
+
+        const booker = getBooker({
+          email: "seat1@example.com",
+          name: "Seat 1",
+        });
+
+        const organizer = getOrganizer({
+          name: "Organizer",
+          email: "organizer@example.com",
+          id: 101,
+          schedules: [TestData.schedules.IstWorkHours],
+        });
+
+        const { dateString: plus1DateString } = getDate({ dateIncrement: 1 });
+        const bookingStartTime = `${plus1DateString}T04:00:00.000Z`;
+        const bookingUid = "abc123";
+        const bookingId = 1;
+
+        await createBookingScenario(
+          getScenarioData({
+            eventTypes: [
+              {
+                id: bookingId,
+                slug: "seated-event",
+                slotInterval: 45,
+                length: 45,
+                users: [
+                  {
+                    id: 101,
+                  },
+                ],
+                seatsPerTimeSlot: 3,
+                seatsShowAttendees: false,
+              },
+            ],
+            bookings: [
+              {
+                id: 1,
+                uid: bookingUid,
+                eventTypeId: 1,
+                status: BookingStatus.ACCEPTED,
+                startTime: bookingStartTime,
+                endTime: `${plus1DateString}T05:15:00.000Z`,
+                metadata: {
+                  videoCallUrl: "https://existing-daily-video-call-url.example.com",
+                },
+                references: [
+                  {
+                    type: appStoreMetadata.dailyvideo.type,
+                    uid: "MOCK_ID",
+                    meetingId: "MOCK_ID",
+                    meetingPassword: "MOCK_PASS",
+                    meetingUrl: "http://mock-dailyvideo.example.com",
+                    credentialId: null,
+                  },
+                ],
+                attendees: [
+                  getMockBookingAttendee({
+                    id: 1,
+                    name: "Seat 1",
+                    email: "seat1@example.com",
+                    // Booker's locale when the fresh booking happened earlier
+                    locale: "en",
+                    // Booker's timezone when the fresh booking happened earlier
+                    timeZone: "America/Toronto",
+                    bookingSeat: {
+                      referenceUid: "booking-seat-1",
+                      data: {},
+                    },
+                  }),
+                ],
+              },
+            ],
+            organizer,
+          })
+        );
+
+        mockSuccessfulVideoMeetingCreation({
+          metadataLookupKey: "dailyvideo",
+          videoMeetingData: {
+            id: "MOCK_ID",
+            password: "MOCK_PASS",
+            url: `http://mock-dailyvideo.example.com/meeting-1`,
+          },
+        });
+
+        const reqBookingUser = "seatedAttendee";
+
+        const mockBookingData = getMockRequestDataForBooking({
+          data: {
+            eventTypeId: 1,
+            responses: {
+              email: booker.email,
+              name: booker.name,
+              location: { optionValue: "", value: BookingLocations.CalVideo },
+            },
+            bookingUid: bookingUid,
+            user: reqBookingUser,
+          },
+        });
+
+        const { req } = createMockNextJsRequest({
+          method: "POST",
+          body: mockBookingData,
+        });
+
+        await expect(() => handleNewBooking(req)).rejects.toThrowError("Already signed up for this booking.");
       });
     });
   });
