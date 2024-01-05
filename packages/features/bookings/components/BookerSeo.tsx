c85266b7218840c05f86a7ff6d2a457329e15714
Peer Richelsen <peer@cal.com>
2024-01-05 12:56:37 +0000

    Update packages/features/bookings/components/BookerSeo.tsx

--- packages/features/bookings/components/BookerSeo.tsx
+++ packages/features/bookings/components/BookerSeo.tsx
@@ -38,7 +38,6 @@ export const BookerSeo = (props: BookerSeoProps) => {
   const profileName = event?.profile?.name ?? "";
   const profileImage = event?.profile?.image;
   const title = event?.title ?? "";
-  const description = event?.description ?? "";
   return (
     <HeadSeo
       title={`${rescheduleUid && !!bookingData ? t("reschedule") : ""} ${title} | ${profileName}`}
