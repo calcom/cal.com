fe2484a714d6a5ebf73f4a90347b0aaa6bd9c75a
Benny Joo <sldisek783@gmail.com>
2024-01-05 14:05:44 +0000

    manual: Change structure & Refactor to make code up to date

--- apps/web/app/future/insights/page.tsx
+++ apps/web/app/future/insights/page.tsx
@@ -1,12 +1,10 @@
-import OldPage from "@pages/insights/index";
+import LegacyPage from "@pages/insights/index";
 import { _generateMetadata } from "app/_utils";
-import { headers } from "next/headers";
+import { WithLayout } from "app/layoutHOC";
 import { notFound } from "next/navigation";
 
 import { getLayout } from "@calcom/features/MainLayoutAppDir";
 import { getFeatureFlagMap } from "@calcom/features/flags/server/utils";
-
-import PageWrapper from "@components/PageWrapperAppDir";
 
 export const generateMetadata = async () =>
   await _generateMetadata(
@@ -25,17 +23,4 @@ async function getData() {
   return {};
 }
 
-const Page = async () => {
-  const h = headers();
-  const nonce = h.get("x-nonce") ?? undefined;
-
-  await getData();
-
-  return (
-    <PageWrapper getLayout={getLayout} requiresLicense={false} nonce={nonce} themeBasis={null}>
-      <OldPage />
-    </PageWrapper>
-  );
-};
-
-export default Page;
+export default WithLayout({ getLayout, getData, Page: LegacyPage });
