cea09845ebd2807844e5970cf1ae54d2362d35bb
Omar López <zomars@me.com>
2024-01-09 09:08:53 -0700

    Discard changes to apps/web/app/layoutHOC.tsx

--- apps/web/app/layoutHOC.tsx
+++ apps/web/app/layoutHOC.tsx
@@ -1,5 +1,4 @@
 import type { LayoutProps, PageProps } from "app/_types";
-import { type GetServerSidePropsContext } from "next";
 import { cookies, headers } from "next/headers";
 
 import { buildLegacyCtx } from "@lib/buildLegacyCtx";
@@ -9,16 +8,14 @@ import PageWrapper from "@components/PageWrapperAppDir";
 type WithLayoutParams<T extends Record<string, any>> = {
   getLayout: ((page: React.ReactElement) => React.ReactNode) | null;
   Page?: (props: T) => React.ReactElement;
-  getData?: (arg: GetServerSidePropsContext) => Promise<T>;
+  getData?: (arg: ReturnType<typeof buildLegacyCtx>) => Promise<T>;
 };
 
 export function WithLayout<T extends Record<string, any>>({ getLayout, getData, Page }: WithLayoutParams<T>) {
   return async <P extends "P" | "L">(p: P extends "P" ? PageProps : LayoutProps) => {
     const h = headers();
     const nonce = h.get("x-nonce") ?? undefined;
-    const props = getData
-      ? await getData(buildLegacyCtx(h, cookies(), p.params) as unknown as GetServerSidePropsContext)
-      : ({} as T);
+    const props = getData ? await getData(buildLegacyCtx(h, cookies(), p.params)) : ({} as T);
 
     const children = "children" in p ? p.children : null;
 
