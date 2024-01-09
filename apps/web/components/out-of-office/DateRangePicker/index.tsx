8cf8c666985b69652526a5ba231b7b124450f231
Alan <alannnc@gmail.com>
2024-01-08 20:40:18 -0700

    Fix date picker dark mode

--- apps/web/components/out-of-office/DateRangePicker/index.tsx
+++ apps/web/components/out-of-office/DateRangePicker/index.tsx
@@ -5,7 +5,7 @@ import type { UseFormSetValue } from "react-hook-form";
 import dayjs from "@calcom/dayjs";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 
-import "./DateSelect.module.css";
+import "./DateSelect.css";
 
 interface IOutOfOfficeDateRangeSelectProps {
   dateRange: [Date | null, Date | null, null];
@@ -17,27 +17,30 @@ const OutOfOfficeDateRangePicker = (props: IOutOfOfficeDateRangeSelectProps) => 
   const { t } = useLocale();
   const { dateRange, setDateRange, setValue } = props;
   return (
-    <DateRangePicker
-      value={dateRange}
-      defaultValue={dateRange}
-      onValueChange={(datesArray) => {
-        const [start, end] = datesArray;
+    <div className="custom-date">
+      <DateRangePicker
+        value={dateRange}
+        defaultValue={dateRange}
+        onValueChange={(datesArray) => {
+          const [start, end] = datesArray;
 
-        if (start) {
-          setDateRange([start, end as Date | null, null]);
-        }
-        if (start && end) {
-          setValue("startDate", start.toISOString());
-          setValue("endDate", end.toISOString());
-        }
-      }}
-      options={undefined}
-      enableDropdown={false}
-      placeholder={t("select_date_range")}
-      enableYearPagination={true}
-      minDate={dayjs().startOf("d").toDate()}
-      maxDate={dayjs().add(2, "y").endOf("d").toDate()}
-    />
+          if (start) {
+            setDateRange([start, end as Date | null, null]);
+          }
+          if (start && end) {
+            setValue("startDate", start.toISOString());
+            setValue("endDate", end.toISOString());
+          }
+        }}
+        color="gray"
+        options={undefined}
+        enableDropdown={false}
+        placeholder={t("select_date_range")}
+        enableYearPagination={true}
+        minDate={dayjs().startOf("d").toDate()}
+        maxDate={dayjs().add(2, "y").endOf("d").toDate()}
+      />
+    </div>
   );
 };
 
