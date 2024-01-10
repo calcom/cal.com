57630cedd09409cb5b2e05a9dfbc3f902f90eb9f
Somay <somaychauhan98@gmail.com>
2024-01-10 15:35:08 +0530

    feat: implement flagsmith feature flags using flagsmith apis

--- packages/features/flags/server/utils.ts
+++ packages/features/flags/server/utils.ts
@@ -2,13 +2,35 @@ import type { PrismaClient } from "@calcom/prisma";
 
 import type { AppFlags } from "../config";
 
-export async function getFeatureFlagMap(prisma: PrismaClient) {
-  const flags = await prisma.feature.findMany({
-    orderBy: { slug: "asc" },
-    cacheStrategy: { swr: 300, ttl: 300 },
+const FLAGSMITH_ENVIRONMENT_ID = process.env.FLAGSMITH_ENVIRONMENT_ID;
+
+async function getFlagsmithFlags() {
+  const response = await fetch(`https://edge.api.flagsmith.com/api/v1/flags/`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "x-environment-key": FLAGSMITH_ENVIRONMENT_ID,
+    },
   });
-  return flags.reduce<AppFlags>((acc, flag) => {
-    acc[flag.slug as keyof AppFlags] = flag.enabled;
-    return acc;
-  }, {} as AppFlags);
+  return await response.json();
+}
+
+export async function getFeatureFlagMap(prisma: PrismaClient) {
+  if (!!FLAGSMITH_ENVIRONMENT_ID) {
+    const flags = await getFlagsmithFlags();
+    const res = flags.reduce<AppFlags>((acc, flag) => {
+      acc[flag.feature.name as keyof AppFlags] = flag.enabled;
+      return acc;
+    }, {} as AppFlags);
+    return res;
+  } else {
+    const flags = await prisma.feature.findMany({
+      orderBy: { slug: "asc" },
+      cacheStrategy: { swr: 300, ttl: 300 },
+    });
+    return flags.reduce<AppFlags>((acc, flag) => {
+      acc[flag.slug as keyof AppFlags] = flag.enabled;
+      return acc;
+    }, {} as AppFlags);
+  }
 }
