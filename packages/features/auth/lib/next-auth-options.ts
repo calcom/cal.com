6f2dfb5da9b380d88088ac916a7ea2237323343a
Somay <somaychauhan98@gmail.com>
2024-01-11 17:15:11 +0530

    feat: send user info to flagsmith on signin to identify

--- packages/features/auth/lib/next-auth-options.ts
+++ packages/features/auth/lib/next-auth-options.ts
@@ -57,6 +57,41 @@ export const checkIfUserBelongsToActiveTeam = <T extends UserTeams>(user: T) =>
     return metadata.success && metadata.data?.subscriptionId;
   });
 
+const setFlagsmithIdentity = async (user: { id: number; email: string; organization: string }) => {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  if (FLAGSMITH_ENVIRONMENT_ID) {
+    myHeaders.append("X-Environment-Key", FLAGSMITH_ENVIRONMENT_ID);
+  }
+
+  const raw = JSON.stringify({
+    identifier: user.email,
+    traits: [
+      {
+        trait_key: "organization",
+        trait_value: user.organization,
+      },
+      {
+        trait_key: "userId",
+        trait_value: user.id,
+      },
+    ],
+  });
+
+  try {
+    const res = await fetch("http://13.233.201.155:8000/api/v1/identities/", {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    });
+    const respose = await res.json();
+    console.log("rresposeresposeespose", respose);
+  } catch (e) {
+    return;
+  }
+};
+
 const checkIfUserShouldBelongToOrg = async (idP: IdentityProvider, email: string) => {
   const [orgUsername, apexDomain] = email.split("@");
   if (!ORGANIZATIONS_AUTOLINK || idP !== "GOOGLE") return { orgUsername, orgId: undefined };
@@ -583,6 +618,9 @@ export const AUTH_OPTIONS: AuthOptions = {
         return true;
       }
 
+      if (FLAGSMITH_ENVIRONMENT_ID) {
+        await setFlagsmithIdentity({ id: user.id, email: user.email, organization: user.org?.slug });
+      }
       // In this case we've already verified the credentials in the authorize
       // callback so we can sign the user in.
       // Only if provider is not saml-idp
