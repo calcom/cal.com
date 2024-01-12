ddb46ff05718d0171de20d93eb3508e46845415b
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-01-12 08:06:34 +0530

    update event type component view

--- packages/platform/atoms/event-type/index.tsx
+++ packages/platform/atoms/event-type/index.tsx
@@ -3,5 +3,12 @@ type EventTypeProp = {
 };
 
 export function EventType({ id }: EventTypeProp) {
-  return <>This returns the event type atom</>;
+  return (
+    <div className="hover:bg-muted flex w-full items-center justify-between transition">
+      <div className="group flex w-full max-w-full items-center justify-between overflow-hidden px-4 py-4 sm:px-6">
+        {}
+      </div>
+      <div>{}</div>
+    </div>
+  );
 }
