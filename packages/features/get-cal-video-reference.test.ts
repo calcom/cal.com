ba3137a072e40fccfdc2d03013742d321d18a8ab
Udit Takkar <udit222001@gmail.com>
2024-01-13 03:30:23 +0530

    chore

--- packages/features/get-cal-video-reference.test.ts
+++ packages/features/get-cal-video-reference.test.ts
@@ -7,14 +7,12 @@ describe("Cal Video", () => {
     expect(
       getCalVideoReference([
         {
-          id: 1,
           uid: "UID1",
           type: "daily_video",
           meetingId: "ID1",
           meetingPassword: "P1",
         },
         {
-          id: 2,
           uid: "UID2",
           type: "daily_video",
           meetingId: "ID2",
--- packages/features/get-cal-video-reference.ts
+++ packages/features/get-cal-video-reference.ts
@@ -1,6 +1,6 @@
-import type { BookingReference } from "@prisma/client";
+export type Reference = { uid: string; type: string; meetingUrl?: string; meetingPassword?: string };
 
-export const getCalVideoReference = (references: BookingReference[]) => {
+export const getCalVideoReference = (references: Reference[]) => {
   const videoReferences = references.filter((reference) => reference.type.includes("_video"));
   const latestVideoReference = videoReferences[videoReferences.length - 1];
   return latestVideoReference;
