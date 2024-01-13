82529e03c71e048950f2463d5cda9ca268cc5742
Hariom <hariombalhara@gmail.com>
2024-01-13 18:35:35 +0530

    Squash migrations

--- packages/prisma/migrations/20240104082605_sdf/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `away` on the `Profile` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Profile" DROP COLUMN "away";
--- packages/prisma/migrations/20240113130201_org_new_schema_migrations/migration.sql
+++ packages/prisma/migrations/20240113130201_org_new_schema_migrations/migration.sql
@@ -1,23 +1,5 @@
-/*
-  Warnings:
-
-  - You are about to drop the `OrgProfile` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE "OrgProfile" DROP CONSTRAINT "OrgProfile_organizationId_fkey";
-
--- DropForeignKey
-ALTER TABLE "OrgProfile" DROP CONSTRAINT "OrgProfile_userId_fkey";
-
 -- DropIndex
 DROP INDEX "users_username_idx";
-
--- AlterTable
-ALTER TABLE "Credential" ADD COLUMN     "ownedByOrganizationId" INTEGER;
-
--- DropTable
-DROP TABLE "OrgProfile";
 
 -- CreateTable
 CREATE TABLE "Profile" (
@@ -26,7 +8,6 @@ CREATE TABLE "Profile" (
     "userId" INTEGER NOT NULL,
     "organizationId" INTEGER NOT NULL,
     "username" TEXT NOT NULL,
-    "away" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -41,9 +22,6 @@ CREATE INDEX "Profile_organizationId_idx" ON "Profile"("organizationId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Profile_userId_organizationId_key" ON "Profile"("userId", "organizationId");
-
--- AddForeignKey
-ALTER TABLE "Credential" ADD CONSTRAINT "Credential_ownedByOrganizationId_fkey" FOREIGN KEY ("ownedByOrganizationId") REFERENCES "Team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
