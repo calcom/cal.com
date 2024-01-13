36b81ced5794ef8be67172ea35c8f048cf274ec5
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-01-13 22:05:11 +0530

    update common schedule settings toggle view

--- packages/platform/atoms/event-type/components/common-schedule-settings-toggle/index.tsx
+++ packages/platform/atoms/event-type/components/common-schedule-settings-toggle/index.tsx
@@ -1,3 +1,32 @@
-export function UseCommonScheduleSettingsToggle() {
-  return <>This is the commone schedules toggle</>;
+import type { FormValues } from "event-type/types";
+import { Controller, useFormContext } from "react-hook-form";
+
+import { SettingsToggle } from "@calcom/ui";
+
+type UseCommonScheduleSettingsToggleProps = {
+  eventType: any;
+};
+
+export function UseCommonScheduleSettingsToggle({ eventType }: UseCommonScheduleSettingsToggleProps) {
+  const { setValue } = useFormContext<FormValues>();
+
+  return (
+    <Controller
+      name="metadata.config.useHostSchedulesForTeamEvent"
+      render={({ field: { value, onChange } }) => (
+        <SettingsToggle
+          checked={!value}
+          onCheckedChange={(checked) => {
+            onChange(!checked);
+            if (!checked) {
+              setValue("schedule", null);
+            }
+          }}
+          title="Choose a common schedule"
+          description="Enable this if you want to use a common schedule between hosts. When disabled, each host will be booked based on their default schedule.">
+          {/* <EventTypeSchedule eventType={eventType} /> */}
+        </SettingsToggle>
+      )}
+    />
+  );
 }
