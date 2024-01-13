73004f7601691453391e4bfa9609dbe06ea3d686
supalarry <lauris.skraucis@gmail.com>
2024-01-13 13:18:47 +0100

    zod schema for request body

--- apps/api/lib/validations/team.ts
+++ apps/api/lib/validations/team.ts
@@ -7,11 +7,19 @@ export const schemaTeamBaseBodyParams = Team.omit({ id: true, createdAt: true })
   metadata: true,
 });
 
-const schemaTeamRequiredParams = z.object({});
+const schemaTeamRequiredParams = z.object({
+  name: z.string().max(255),
+});
 
 export const schemaTeamBodyParams = schemaTeamBaseBodyParams.merge(schemaTeamRequiredParams).strict();
 
 export const schemaTeamUpdateBodyParams = schemaTeamBodyParams.partial();
+
+const schemaOwnerId = z.object({
+  ownerId: z.number().optional(),
+});
+
+export const schemaTeamCreateBodyParams = schemaTeamBodyParams.merge(schemaOwnerId).strict();
 
 export const schemaTeamReadPublic = Team.omit({});
 
