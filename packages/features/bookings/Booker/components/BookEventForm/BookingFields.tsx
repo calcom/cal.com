aefb1966261880d0427502c0eab76e2d31136111
shravan20 <mrshravankumarb@gmail.com>
2024-01-15 11:18:38 +0530

    feat: Preview Product domain name rather than complete URL

--- packages/features/bookings/Booker/components/BookEventForm/BookingFields.tsx
+++ packages/features/bookings/Booker/components/BookEventForm/BookingFields.tsx
@@ -30,6 +30,13 @@ export const BookingFields = ({
   const currentView = rescheduleUid ? "reschedule" : "";
   const isInstantMeeting = useBookerStore((state) => state.isInstantMeeting);
 
+  const getDomain = (url: string): string => {
+    const urlParts: string[] = new URL(url).hostname.split(".");
+    const partsToKeep: number = urlParts.length === 4 ? 3 : 2;
+    const domainWithoutSubdomain: string = urlParts.slice(-partsToKeep).join(".");
+    return domainWithoutSubdomain;
+  };
+
   return (
     // TODO: It might make sense to extract this logic into BookingFields config, that would allow to quickly configure system fields and their editability in fresh booking and reschedule booking view
     // The logic here intends to make modifications to booking fields based on the way we want to specifically show Booking Form
@@ -105,10 +112,21 @@ export const BookingFields = ({
               optionInput.placeholder = option.inputPlaceholder;
             }
           });
-
+          console.log(field, "qweqwe");
           field.options = options.filter(
             (location): location is NonNullable<(typeof options)[number]> => !!location
           );
+
+          field.options.map((option) => {
+            option.label =
+              option.label.search(/^https?:/) !== -1 ? (
+                <a href={option.label} target="_blank">
+                  {getDomain(option.label)}{" "}
+                </a>
+              ) : (
+                option.label
+              );
+          });
         }
 
         if (field?.options) {
