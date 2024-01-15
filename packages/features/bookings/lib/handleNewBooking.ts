e5eb121ab75fdf0d5b5ffa1b4fef61107329e4cf
Udit Takkar <53316345+Udit-takkar@users.noreply.github.com>
2024-01-15 22:22:04 +0530

    fix: Reschedule a cancelled booking (#13170)
    * fix: Reschedule a cancelled booking
    
    * canceled instead of cancelled
    
    ---------
    
    Co-authored-by: Joe Au-Yeung <65426560+joeauyeung@users.noreply.github.com>
    Co-authored-by: Carina Wollendorfer <30310907+CarinaWolli@users.noreply.github.com>

--- packages/features/bookings/lib/handleNewBooking.ts
+++ packages/features/bookings/lib/handleNewBooking.ts
@@ -1224,6 +1224,13 @@ async function handler(
     if (!originalRescheduledBooking) {
       throw new HttpError({ statusCode: 404, message: "Could not find original booking" });
     }
+
+    if (
+      originalRescheduledBooking.status === BookingStatus.CANCELLED &&
+      !originalRescheduledBooking.rescheduled
+    ) {
+      throw new HttpError({ statusCode: 403, message: ErrorCode.CancelledBookingsCannotBeRescheduled });
+    }
   }
 
   //checks what users are available
