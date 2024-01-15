a7c80dd8b942e82190cf36337c68058a8fadc115
Sasha Mysak <reactionnnnn@gmail.com>
2024-01-15 16:51:01 +0100

    fix: remove redundant files

--- apps/web/app/future/auth/sso/[provider]/page.tsx
+++
@@ -1,9 +0,0 @@
-import Provider from "@pages/auth/sso/[provider]";
-import { withAppDir } from "app/AppDirSSRHOC";
-import { WithLayout } from "app/layoutHOC";
-
-export default WithLayout({
-  getLayout: null,
-  Page: Provider,
-  getData: withAppDir(getServerSideProps),
-})<"P">;
