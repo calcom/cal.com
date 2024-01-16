78fe5b1ebeef9988905d91138dd1c40ec17088d0
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-01-16 14:20:05 +0530

    refactor event availability tab

---
+++ packages/platform/atoms/event-type/components/single-value/index.tsx
@@ -0,0 +1,25 @@
+import type { AvailabilityOption } from "event-type/types";
+import type { OptionProps } from "react-select";
+import { components } from "react-select";
+
+import { Badge } from "@calcom/ui";
+
+export function SingleValue({ ...props }: OptionProps<AvailabilityOption>) {
+  const { label, isDefault, isManaged = false } = props.data;
+
+  return (
+    <components.Option {...props}>
+      <span>{label}</span>
+      {isDefault && (
+        <Badge variant="blue" className="ml-2">
+          Default
+        </Badge>
+      )}
+      {isManaged && (
+        <Badge variant="gray" className="ml-2">
+          Managed
+        </Badge>
+      )}
+    </components.Option>
+  );
+}
