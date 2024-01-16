dae2ae0d39bc75cb7cb3a9d7ee723c1478ac0262
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-01-16 14:20:44 +0530

    add helper to get forma

---
+++ packages/platform/atoms/event-type/lib/format.ts
@@ -0,0 +1,10 @@
+import dayjs from "@calcom/dayjs";
+
+const format = (date: Date, hour12: boolean) =>
+  Intl.DateTimeFormat(undefined, {
+    hour: "numeric",
+    minute: "numeric",
+    hourCycle: hour12 ? "h12" : "h24",
+  }).format(new Date(dayjs.utc(date).format("YYYY-MM-DDTHH:mm:ss")));
+
+export default format;
