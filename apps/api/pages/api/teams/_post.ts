5f0381ce83e5058edb4b45f284b99b005b3cc0ce
supalarry <lauris.skraucis@gmail.com>
2024-01-16 17:50:03 +0100

    Create teams as before if billing disabled

--- apps/api/pages/api/teams/_post.ts
+++ apps/api/pages/api/teams/_post.ts
@@ -5,7 +5,9 @@ import stripe from "@calcom/app-store/stripepayment/lib/server";
 import { IS_TEAM_BILLING_ENABLED, WEBAPP_URL } from "@calcom/lib/constants";
 import { HttpError } from "@calcom/lib/http-error";
 import { defaultResponder } from "@calcom/lib/server";
+import { MembershipRole } from "@calcom/prisma/enums";
 
+import { schemaMembershipPublic } from "~/lib/validations/membership";
 import { schemaTeamCreateBodyParams, schemaTeamReadPublic } from "~/lib/validations/team";
 
 /**
@@ -76,14 +78,6 @@ import { schemaTeamCreateBodyParams, schemaTeamReadPublic } from "~/lib/validati
  *        description: Authorization information is missing or invalid.
  */
 async function postHandler(req: NextApiRequest) {
-  if (!IS_TEAM_BILLING_ENABLED) {
-    const PRECONDITION_FAILED_STATUS_CODE = 412;
-    throw new HttpError({
-      statusCode: PRECONDITION_FAILED_STATUS_CODE,
-      message: "Team creation is unavailable because team billing is disabled.",
-    });
-  }
-
   const { prisma, body, userId, isAdmin } = req;
   const { ownerId, ...data } = schemaTeamCreateBodyParams.parse(body);
 
@@ -119,6 +113,27 @@ async function postHandler(req: NextApiRequest) {
     ...data,
     metadata: data.metadata === null ? {} : data.metadata || undefined,
   };
+
+  if (!IS_TEAM_BILLING_ENABLED) {
+    const team = await prisma.team.create({
+      data: {
+        ...cloneData,
+        createdAt: new Date(),
+        members: {
+          create: { userId: effectiveUserId, role: MembershipRole.OWNER, accepted: true },
+        },
+      },
+      include: { members: true },
+    });
+
+    req.statusCode = 201;
+
+    return {
+      team: schemaTeamReadPublic.parse(team),
+      owner: schemaMembershipPublic.parse(team.members[0]),
+      message: `Team created successfully. We also made user with ID=${ownerId} the owner of this team.`,
+    };
+  }
 
   const pendingPaymentTeam = await prisma.team.create({
     data: {
