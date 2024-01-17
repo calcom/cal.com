8467a9c53b47ac453d86731105be5be423d28bcb
DmytroHryshyn <125881252+DmytroHryshyn@users.noreply.github.com>
2024-01-17 14:46:45 +0200

    fix: fix build error caused by 'export' keyword before getData (#13276)

--- apps/web/app/future/org/[orgSlug]/[user]/page.tsx
+++ apps/web/app/future/org/[orgSlug]/[user]/page.tsx
@@ -2,6 +2,6 @@ import Page, { getServerSideProps, type PageProps } from "@pages/org/[orgSlug]/[
 import { withAppDir } from "app/AppDirSSRHOC";
 import { WithLayout } from "app/layoutHOC";
 
-export const getData = withAppDir<PageProps>(getServerSideProps);
+const getData = withAppDir<PageProps>(getServerSideProps);
 
 export default WithLayout({ getLayout: null, getData, isBookingPage: true, Page });
