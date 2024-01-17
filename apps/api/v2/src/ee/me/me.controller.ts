d1c400ce83bcd49d9431ced7353ee6277fd40853
supalarry <lauris.skraucis@gmail.com>
2024-01-17 10:23:51 +0100

    simplify

--- apps/api/v2/src/ee/me/me.controller.ts
+++ apps/api/v2/src/ee/me/me.controller.ts
@@ -1,7 +1,6 @@
 import { GetUser } from "@/modules/auth/decorators/get-user/get-user.decorator";
 import { AccessTokenGuard } from "@/modules/auth/guards/access-token/access-token.guard";
-import { UsersRepository } from "@/modules/users/users.repository";
-import { Controller, UseGuards, Get, NotFoundException } from "@nestjs/common";
+import { Controller, UseGuards, Get } from "@nestjs/common";
 import { User } from "@prisma/client";
 
 import { SUCCESS_STATUS } from "@calcom/platform-constants";
@@ -12,17 +11,9 @@ import { ApiResponse } from "@calcom/platform-types";
   version: "2",
 })
 export class MeController {
-  constructor(private readonly usersRepository: UsersRepository) {}
-
   @Get("/")
   @UseGuards(AccessTokenGuard)
-  async getMe(@GetUser("id") userId: number): Promise<ApiResponse<UserReturned>> {
-    const user = await this.usersRepository.findById(userId);
-
-    if (!user) {
-      throw new NotFoundException(`User with ID ${userId} not found.`);
-    }
-
+  async getMe(@GetUser() user: User): Promise<ApiResponse<UserReturned>> {
     const TWELVE_HOUR_TIME_FORMAT = 12;
 
     return {
--- apps/api/v2/src/ee/me/me.module.ts
+++ apps/api/v2/src/ee/me/me.module.ts
@@ -1,9 +1,7 @@
 import { MeController } from "@/ee/me/me.controller";
-import { UsersModule } from "@/modules/users/users.module";
 import { Module } from "@nestjs/common";
 
 @Module({
-  imports: [UsersModule],
   controllers: [MeController],
 })
 export class MeModule {}
