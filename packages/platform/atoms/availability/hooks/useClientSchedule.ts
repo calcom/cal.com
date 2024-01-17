de88a6187627065c5d5d5ed6b7b3b7dcda4361c3
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-01-17 13:36:32 +0530

    rename id parameter

--- packages/platform/atoms/availability/hooks/useClientSchedule.ts
+++ packages/platform/atoms/availability/hooks/useClientSchedule.ts
@@ -1,16 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 
-const useClientSchedule = (key: string, clientId?: string) => {
+const useClientSchedule = (key: string, id?: string) => {
   const { isLoading, error, data } = useQuery({
     queryKey: ["schedule"],
     queryFn: () => {
-      return fetch(
-        clientId ? `/v2/schedules/${clientId}?apiKey=${key}` : `/v2/schedules/defaultSchedule?apiKey=${key}`,
-        {
-          method: "get",
-          headers: { "Content-type": "application/json" },
-        }
-      ).then((res) => res.json());
+      return fetch(id ? `/v2/schedules/${id}?apiKey=${key}` : `/v2/schedules/defaultSchedule?apiKey=${key}`, {
+        method: "get",
+        headers: { "Content-type": "application/json" },
+      }).then((res) => res.json());
     },
   });
 
