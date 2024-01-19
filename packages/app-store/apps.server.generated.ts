beb5528062873c46df35d741c8dce92ae4a66c52
Gergő Móricz <mo.geryy@gmail.com>
2024-01-19 17:20:11 +0000

    feat: ics-feed proof of concept pt. 1

--- packages/app-store/apps.server.generated.ts
+++ packages/app-store/apps.server.generated.ts
@@ -29,6 +29,7 @@ export const apiHandlers = {
   gtm: import("./gtm/api"),
   hubspot: import("./hubspot/api"),
   huddle01video: import("./huddle01video/api"),
+  "ics-feed": import("./ics-feed/api"),
   intercom: import("./intercom/api"),
   jitsivideo: import("./jitsivideo/api"),
   larkcalendar: import("./larkcalendar/api"),
