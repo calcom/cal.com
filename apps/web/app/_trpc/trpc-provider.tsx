86cfdeab45948f892c8991edcc6c64104851e254
KATT <alexander@n1s.se>
2024-01-19 17:00:28 +0100

    .

--- apps/web/app/_trpc/trpc-provider.tsx
+++ apps/web/app/_trpc/trpc-provider.tsx
@@ -4,10 +4,10 @@ import { trpc } from "app/_trpc/client";
 import { useState } from "react";
 import superjson from "superjson";
 
-import { httpBatchLink } from "@calcom/trpc/client/links/httpBatchLink";
-import { httpLink } from "@calcom/trpc/client/links/httpLink";
-import { loggerLink } from "@calcom/trpc/client/links/loggerLink";
-import { splitLink } from "@calcom/trpc/client/links/splitLink";
+import { httpBatchLink } from "@calcom/trpc/client";
+import { httpLink } from "@calcom/trpc/client";
+import { loggerLink } from "@calcom/trpc/client";
+import { splitLink } from "@calcom/trpc/client";
 import { ENDPOINTS } from "@calcom/trpc/react/shared";
 
 export type Endpoint = (typeof ENDPOINTS)[number];
