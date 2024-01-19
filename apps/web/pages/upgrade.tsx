9955e4c161e09e2bd6b98e359cf0be8dacc74b49
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-01-19 14:31:54 -0500

    Show upgrade screen when org needs upgrade

--- apps/web/pages/upgrade.tsx
+++ apps/web/pages/upgrade.tsx
@@ -29,19 +29,12 @@ export default function UpgradePage() {
     },
   });
 
-  const membership = false;
+  const doesUserHaveOrgToUpgrade = trpc.viewer.organizations.checkIfOrgNeedsUpgrade.useQuery();
 
   return (
     <Shell hideHeadingOnMobile>
       <div className="max-w-screen-lg">
-        {membership ? (
-          <EmptyScreen
-            headline="You are all set"
-            description="You are already on the latest plan. Nothing to upgrade. Enjoy the new features and reach out to us with any questions!"
-            Icon={CheckCircle}
-            buttonRaw={<Button href="mailto:support@cal.com">{t("contact_support")}</Button>}
-          />
-        ) : (
+        {doesUserHaveOrgToUpgrade.data ? (
           <EmptyScreen
             headline="Your upgrade is here"
             description="Improve your scheduling experience by upgrading to the new plan and enjoy the new features."
@@ -54,6 +47,13 @@ export default function UpgradePage() {
                 {t("upgrade")}
               </Button>
             }
+          />
+        ) : (
+          <EmptyScreen
+            headline="You are all set"
+            description="You are already on the latest plan. Nothing to upgrade. Enjoy the new features and reach out to us with any questions!"
+            Icon={CheckCircle}
+            buttonRaw={<Button href="mailto:support@cal.com">{t("contact_support")}</Button>}
           />
         )}
       </div>
