b4bf137fe6179621a77ee3e27aeff478df8dfea5
Dmytro Hryshyn <dev.dmytroh@gmail.com>
2024-01-19 17:53:21 +0200

    fix

--- packages/trpc/server/routers/viewer/eventTypes/procedures/get.ts
+++ packages/trpc/server/routers/viewer/eventTypes/procedures/get.ts
@@ -2,9 +2,9 @@ import { ZGetInputSchema } from "../get.schema";
 import { eventOwnerProcedure } from "../util";
 
 export const get = eventOwnerProcedure.input(ZGetInputSchema).query(async ({ ctx, input }) => {
-  const handler = await import("../get.handler").then((mod) => mod.getHandler);
+  const handler = (await import("../get.handler")).getHandler;
 
-  return handler.get({
+  return handler({
     ctx,
     input,
   });
