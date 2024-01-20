126b26f1c14b1531ecc242255a294fc9979e6ec9
KATT <alexander@n1s.se>
2024-01-20 18:15:52 +0100

    .

--- packages/features/ee/teams/pages/team-members-view.tsx
+++ packages/features/ee/teams/pages/team-members-view.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import { useRouter, useEffect } from "next/navigation";
 import { useState } from "react";
 
 import { useCompatSearchParams } from "@calcom/lib/hooks/useCompatSearchParams";
