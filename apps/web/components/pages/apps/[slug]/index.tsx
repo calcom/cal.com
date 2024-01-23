40bda809e0f2ab285b1b5c01d69cba018b7aa1c1
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2024-01-23 16:17:43 +0100

    /apps/[slug]

---
+++ apps/web/components/pages/apps/[slug]/index.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import MarkdownIt from "markdown-it";
+import Link from "next/link";
+
+import { IS_PRODUCTION } from "@calcom/lib/constants";
+
+import type { AppProps } from "@lib/app-providers";
+import type { getStaticProps } from "@lib/apps/[slug]/getStaticProps";
+import type { inferSSRProps } from "@lib/types/inferSSRProps";
+
+import App from "@components/apps/App";
+
+const md = new MarkdownIt("default", { html: true, breaks: true });
+
+type SingleAppPageComponentFunction = ((
+  props: inferSSRProps<typeof getStaticProps>
+) => JSX.Element | null) & {
+  PageWrapper?: (props: AppProps) => JSX.Element;
+};
+
+const SingleAppPage: SingleAppPageComponentFunction = function (props: inferSSRProps<typeof getStaticProps>) {
+  // If it's not production environment, it would be a better idea to inform that the App is disabled.
+  if (props.isAppDisabled) {
+    if (!IS_PRODUCTION) {
+      // TODO: Improve disabled App UI. This is just a placeholder.
+      return (
+        <div className="p-2">
+          This App seems to be disabled. If you are an admin, you can enable this app from{" "}
+          <Link href="/settings/admin/apps" className="cursor-pointer text-blue-500 underline">
+            here
+          </Link>
+        </div>
+      );
+    }
+
+    // Disabled App should give 404 any ways in production.
+    return null;
+  }
+
+  const { source, data } = props;
+  return (
+    <App
+      name={data.name}
+      description={data.description}
+      isGlobal={data.isGlobal}
+      slug={data.slug}
+      variant={data.variant}
+      type={data.type}
+      logo={data.logo}
+      categories={data.categories ?? [data.category]}
+      author={data.publisher}
+      feeType={data.feeType || "usage-based"}
+      price={data.price || 0}
+      commission={data.commission || 0}
+      docs={data.docsUrl}
+      website={data.url}
+      email={data.email}
+      licenseRequired={data.licenseRequired}
+      teamsPlanRequired={data.teamsPlanRequired}
+      descriptionItems={source.data?.items as string[] | undefined}
+      isTemplate={data.isTemplate}
+      dependencies={data.dependencies}
+      concurrentMeetings={data.concurrentMeetings}
+      paid={data.paid}
+      //   tos="https://zoom.us/terms"
+      //   privacy="https://zoom.us/privacy"
+      body={
+        <>
+          <div dangerouslySetInnerHTML={{ __html: md.render(source.content) }} />
+        </>
+      }
+    />
+  );
+};
+
+export default SingleAppPage;
--- apps/web/pages/apps/[slug]/index.tsx
+++ apps/web/pages/apps/[slug]/index.tsx
@@ -1,75 +1,14 @@
 "use client";
 
 import { Prisma } from "@prisma/client";
-import MarkdownIt from "markdown-it";
 import type { GetStaticPaths } from "next";
-import Link from "next/link";
 
-import { IS_PRODUCTION } from "@calcom/lib/constants";
 import prisma from "@calcom/prisma";
 
 import { getStaticProps } from "@lib/apps/[slug]/getStaticProps";
-import type { inferSSRProps } from "@lib/types/inferSSRProps";
 
 import PageWrapper from "@components/PageWrapper";
-import App from "@components/apps/App";
-
-const md = new MarkdownIt("default", { html: true, breaks: true });
-
-function SingleAppPage(props: inferSSRProps<typeof getStaticProps>) {
-  // If it's not production environment, it would be a better idea to inform that the App is disabled.
-  if (props.isAppDisabled) {
-    if (!IS_PRODUCTION) {
-      // TODO: Improve disabled App UI. This is just a placeholder.
-      return (
-        <div className="p-2">
-          This App seems to be disabled. If you are an admin, you can enable this app from{" "}
-          <Link href="/settings/admin/apps" className="cursor-pointer text-blue-500 underline">
-            here
-          </Link>
-        </div>
-      );
-    }
-
-    // Disabled App should give 404 any ways in production.
-    return null;
-  }
-
-  const { source, data } = props;
-  return (
-    <App
-      name={data.name}
-      description={data.description}
-      isGlobal={data.isGlobal}
-      slug={data.slug}
-      variant={data.variant}
-      type={data.type}
-      logo={data.logo}
-      categories={data.categories ?? [data.category]}
-      author={data.publisher}
-      feeType={data.feeType || "usage-based"}
-      price={data.price || 0}
-      commission={data.commission || 0}
-      docs={data.docsUrl}
-      website={data.url}
-      email={data.email}
-      licenseRequired={data.licenseRequired}
-      teamsPlanRequired={data.teamsPlanRequired}
-      descriptionItems={source.data?.items as string[] | undefined}
-      isTemplate={data.isTemplate}
-      dependencies={data.dependencies}
-      concurrentMeetings={data.concurrentMeetings}
-      paid={data.paid}
-      //   tos="https://zoom.us/terms"
-      //   privacy="https://zoom.us/privacy"
-      body={
-        <>
-          <div dangerouslySetInnerHTML={{ __html: md.render(source.content) }} />
-        </>
-      }
-    />
-  );
-}
+import SingleAppPage from "@components/pages/apps/[slug]";
 
 export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
   let paths: { params: { slug: string } }[] = [];
@@ -91,8 +30,7 @@ export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
   };
 };
 
-export { getStaticProps };
-
 SingleAppPage.PageWrapper = PageWrapper;
 
 export default SingleAppPage;
+export { getStaticProps };
