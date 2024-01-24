cf00de99e8be264f3c27e8f347234cc81b9b61ae
zomars <zomars@me.com>
2024-01-24 13:44:10 -0700

    Update page.tsx

--- apps/web/app/future/booking/[uid]/embed/page.tsx
+++ apps/web/app/future/booking/[uid]/embed/page.tsx
@@ -1,10 +1,9 @@
-import OldPage from "@pages/booking/[uid]/embed";
 import { withAppDirSsr } from "app/WithAppDirSsr";
 import withEmbedSsrAppDir from "app/WithEmbedSSR";
 import { WithLayout } from "app/layoutHOC";
 
-import { type PageProps } from "~/bookings/views/bookings-single-view";
-import { getServerSideProps } from "~/bookings/views/bookings-single-view.getServerSideProps";
+import OldPage from "~/bookings/views/bookings-single-view";
+import { getServerSideProps, type PageProps } from "~/bookings/views/bookings-single-view.getServerSideProps";
 
 const getData = withAppDirSsr<PageProps>(getServerSideProps);
 
