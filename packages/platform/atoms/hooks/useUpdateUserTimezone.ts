9cbff75497dd2dac7884b4e54aed5bd59a0ae546
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-01-27 09:55:13 +0530

    use URL constructor for creating endpoints

--- packages/platform/atoms/hooks/useUpdateUserTimezone.ts
+++ packages/platform/atoms/hooks/useUpdateUserTimezone.ts
@@ -1,18 +1,26 @@
 import { useMutation } from "@tanstack/react-query";
 
+import { BASE_URL, API_VERSION, ENDPOINTS } from "@calcom/platform-constants";
 import type { ApiResponse } from "@calcom/platform-types";
+
+import { useApiKey } from "../hooks/useApiKeys";
 
 type updateTimezoneInput = {
   timeZone: string;
-  key: string;
 };
 
 export const useUpdateUserTimezone = () => {
+  const { key } = useApiKey();
+  const endpoint = new URL(BASE_URL);
+
+  endpoint.pathname = `${API_VERSION}/${ENDPOINTS.me}`;
+  endpoint.searchParams.set("apiKey", key);
+
   const mutation = useMutation<ApiResponse<undefined>, unknown, updateTimezoneInput>({
     mutationFn: (data) => {
-      const { key, timeZone } = data;
+      const { timeZone } = data;
 
-      return fetch(`/api/v2/me?apiKey=${key}`, {
+      return fetch(endpoint.toString(), {
         method: "patch",
         headers: { "Content-type": "application/json" },
         body: JSON.stringify({ timeZone }),
