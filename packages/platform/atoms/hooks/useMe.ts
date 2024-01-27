8fac8cebd341dcc31a1d16390a1f93661e263ceb
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-01-27 09:02:56 +0530

    use URL constructor for creating endpoints

--- packages/platform/atoms/hooks/useMe.ts
+++ packages/platform/atoms/hooks/useMe.ts
@@ -1,10 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 
+import { BASE_URL, API_VERSION, ENDPOINTS } from "@calcom/platform-constants";
+
 export const useMe = (key: string) => {
+  const endpoint = new URL(BASE_URL);
+
+  endpoint.pathname = `${API_VERSION}/${ENDPOINTS.me}`;
+  endpoint.searchParams.set("apiKey", key);
+
   const { data } = useQuery({
     queryKey: ["get-user-timezone"],
     queryFn: () => {
-      return fetch(`/v2/me?apiKey=${key}`, {
+      return fetch(endpoint.toString(), {
         method: "get",
         headers: { "Content-type": "application/json" },
       }).then((res) => res.json());
