f220f78122c9e04170837ca3730ca12e8cde38cd
sean-brydon <sean@cal.com>
2024-01-28 16:05:04 +0800

    feat: basecamp migration

--- packages/app-store/apps.browser.generated.tsx
+++ packages/app-store/apps.browser.generated.tsx
@@ -41,6 +41,7 @@ export const EventTypeAddonMap = {
 };
 export const EventTypeSettingsMap = {
   alby: dynamic(() => import("./alby/components/EventTypeAppSettingsInterface")),
+  basecamp3: dynamic(() => import("./basecamp3/components/EventTypeAppSettingsInterface")),
   fathom: dynamic(() => import("./fathom/components/EventTypeAppSettingsInterface")),
   ga4: dynamic(() => import("./ga4/components/EventTypeAppSettingsInterface")),
   giphy: dynamic(() => import("./giphy/components/EventTypeAppSettingsInterface")),
