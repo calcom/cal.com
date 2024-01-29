6590a55a4922800d279e1ef58ef4e3c2ea939a0f
supalarry <lauris.skraucis@gmail.com>
2024-01-29 12:46:40 +0100

    handle event type not found

--- apps/api/v2/src/ee/event-types/services/event-types.service.ts
+++ apps/api/v2/src/ee/event-types/services/event-types.service.ts
@@ -1,6 +1,6 @@
 import { PrismaReadService } from "@/modules/prisma/prisma-read.service";
 import { UsersRepository } from "@/modules/users/users.repository";
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { MembershipRole } from "@prisma/client";
 
 import { getEventTypeById } from "@calcom/platform-libraries";
@@ -24,7 +24,7 @@ export class EventTypesService {
       });
       return eventType;
     } catch (error) {
-      if (error instanceof Error) throw new Error(error.message);
+      throw new NotFoundException(`User with id ${userId} has no event type with id ${eventTypeId}`);
     }
   }
 }
