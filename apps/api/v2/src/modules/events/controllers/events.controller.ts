77c2de2677f6de6ffe212e1127c6c4134cab1708
Morgan Vernay <morgan@cal.com>
2024-01-29 15:52:43 +0200

    fix: fetch event type

--- apps/api/v2/src/modules/events/controllers/events.controller.ts
+++ apps/api/v2/src/modules/events/controllers/events.controller.ts
@@ -1,17 +1,22 @@
 import { GetPublicEventInput } from "@/modules/events/inputs/get-public-event.input";
+import { PrismaReadService } from "@/modules/prisma/prisma-read.service";
 import { Body, Controller, Get, VERSION_NEUTRAL, Version } from "@nestjs/common";
 
 import { EventService } from "@calcom/platform-libraries";
 import { ApiResponse } from "@calcom/platform-types";
+import type { PrismaClient } from "@calcom/prisma";
 
 @Controller("event")
 export class EventsController {
-  constructor(private readonly eventsService: EventService) {}
+  private eventService: EventService;
+  constructor(private readonly dbRead: PrismaReadService) {
+    this.eventService = new EventService(this.dbRead.prisma as unknown as PrismaClient);
+  }
 
   @Get("/")
   @Version(VERSION_NEUTRAL)
   async getPublicEvent(@Body() input: GetPublicEventInput): Promise<ApiResponse> {
-    const event = await this.eventsService.getPublicEvent(
+    const event = await this.eventService.getPublicEvent(
       input.username,
       input.eventSlug,
       input.isTeamEvent,
