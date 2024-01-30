b5f1288d5864173b1fbd9f66e9ef4dc58f8afde3
Rajiv Sahal <sahalrajiv-extc@atharvacoe.ac.in>
2024-01-30 17:15:13 +0530

    feat: user timezone management in `CalProvider` (#13361)
    * hooks to fetch and update user timezone
    
    * add react query package
    
    * add logic to check for change in user timezone and update it
    
    * hook to handle timezone changes
    
    * shift timezone handling logic into separate custom hook
    
    * rename variables
    
    * rename hook to get a user
    
    * call useTimezone hook inside of cal provider
    
    * minor refactors
    
    * add variables for api endpoints
    
    * use URL constructor for creating endpoints
    
    * key prop not needed
    
    * use URL constructor for creating endpoints

---
+++ packages/platform/atoms/hooks/useMe.ts
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { BASE_URL, API_VERSION, ENDPOINTS } from "@calcom/platform-constants";
+
+export const useMe = (key: string) => {
+  const endpoint = new URL(BASE_URL);
+
+  endpoint.pathname = `${API_VERSION}/${ENDPOINTS.me}`;
+  endpoint.searchParams.set("apiKey", key);
+
+  const { data } = useQuery({
+    queryKey: ["get-user-timezone"],
+    queryFn: () => {
+      return fetch(endpoint.toString(), {
+        method: "get",
+        headers: { "Content-type": "application/json" },
+      }).then((res) => res.json());
+    },
+  });
+
+  return data;
+};
