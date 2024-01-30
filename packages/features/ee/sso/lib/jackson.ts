cc34569efc81144a8feba19ae4bba0207f33a086
zomars <zomars@me.com>
2024-01-30 16:48:30 -0700

    chore: upgrades boxyhq jackson

--- packages/features/ee/sso/lib/jackson.ts
+++ packages/features/ee/sso/lib/jackson.ts
@@ -1,14 +1,14 @@
-import jackson from "@boxyhq/saml-jackson";
 import type {
   IConnectionAPIController,
   IOAuthController,
+  ISPSSOConfig,
   JacksonOption,
-  ISPSAMLConfig,
 } from "@boxyhq/saml-jackson";
+import jackson from "@boxyhq/saml-jackson";
 
 import { WEBAPP_URL } from "@calcom/lib/constants";
 
-import { samlDatabaseUrl, samlAudience, samlPath, oidcPath, clientSecretVerifier } from "./saml";
+import { clientSecretVerifier, oidcPath, samlAudience, samlDatabaseUrl, samlPath } from "./saml";
 
 // Set the required options. Refer to https://github.com/boxyhq/jackson#configuration for the full list
 const opts: JacksonOption = {
@@ -24,13 +24,17 @@ const opts: JacksonOption = {
   },
   idpEnabled: true,
   clientSecretVerifier,
+  ory: {
+    projectId: undefined,
+    sdkToken: undefined,
+  },
 };
 
 declare global {
   /* eslint-disable no-var */
   var connectionController: IConnectionAPIController | undefined;
   var oauthController: IOAuthController | undefined;
-  var samlSPConfig: ISPSAMLConfig | undefined;
+  var samlSPConfig: ISPSSOConfig | undefined;
   /* eslint-enable no-var */
 }
 
