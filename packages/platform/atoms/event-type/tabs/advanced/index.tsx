9ec6f6064dedd29cde72f9a6aa904917ede335aa
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-01-31 18:07:26 +0530

    advanced tab for availability settings

---
+++ packages/platform/atoms/event-type/tabs/advanced/index.tsx
@@ -0,0 +1,116 @@
+import { EventCalendarView } from "event-type/components/event-calendar-view";
+import { Edit } from "lucide-react";
+import { useState } from "react";
+import { Controller, useFormContext } from "react-hook-form";
+
+import getLocationsOptionsForSelect from "@calcom/features/bookings/lib/getLocationOptionsForSelect";
+import DestinationCalendarSelector from "@calcom/features/calendars/DestinationCalendarSelector";
+import useLockedFieldsManager from "@calcom/features/ee/managed-event-types/hooks/useLockedFieldsManager";
+import { FormBuilder } from "@calcom/features/form-builder/FormBuilder";
+import { BookerLayoutSelector } from "@calcom/features/settings/BookerLayoutSelector";
+import type { Prisma } from "@calcom/prisma/client";
+import { Button, Label, TextField } from "@calcom/ui";
+
+import type { FormValues } from "../../types";
+import type { EventTypeSetupProps } from "../event-setup/index";
+
+type AdvancedProps = {
+  eventType: Pick<EventTypeSetupProps, "eventType" | "team">;
+  team: Pick<EventTypeSetupProps, "eventType" | "team">;
+  userTheme: string | null | undefined;
+  userConnectedCalendars: [];
+};
+
+export function Advanced({ eventType, team, userTheme, userConnectedCalendars }: AdvancedProps) {
+  const formMethods = useFormContext<FormValues>();
+  const bookingFields: Prisma.JsonObject = {};
+  const selectedThemeIsDark =
+    userTheme === "dark" ||
+    (!userTheme && typeof document !== "undefined" && document.documentElement.classList.contains("dark"));
+  const { shouldLockDisableProps } = useLockedFieldsManager(
+    eventType,
+    "Members will not be able to edit this",
+    "This option was locked by the team admin"
+  );
+
+  const [showEventNameTip, setShowEventNameTip] = useState(false);
+  const [hashedLinkVisible, setHashedLinkVisible] = useState(!!eventType.hashedLink);
+  const [redirectUrlVisible, setRedirectUrlVisible] = useState(!!eventType.successRedirectUrl);
+  const [hashedUrl, setHashedUrl] = useState(eventType.hashedLink?.link);
+  const [requiresConfirmation, setRequiresConfirmation] = useState(eventType.requiresConfirmation);
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <div className="border-subtle space-y-6 rounded-lg border p-6">
+        {!!userConnectedCalendars.length && !team && (
+          <div className="flex flex-col">
+            <div className="flex justify-between">
+              <div>
+                <Label className="text-emphasis mb-0 font-medium">Add to calendar</Label>
+              </div>
+              <a
+                href="/apps/categories/calendar"
+                target="_blank"
+                className="hover:text-emphasis text-default text-sm">
+                Add another calendar
+              </a>
+            </div>
+            <Controller
+              control={formMethods.control}
+              name="destinationCalendar"
+              defaultValue={eventType.destinationCalendar || undefined}
+              render={({ field: { onChange, value } }) => (
+                <DestinationCalendarSelector
+                  destinationCalendar={eventType.destinationCalendar}
+                  value={value ? value.externalId : undefined}
+                  onChange={onChange}
+                  hidePlaceholder
+                  hideAdvancedText
+                />
+              )}
+            />
+            <p className="text-subtle text-sm">Select which calendar to add bookings to</p>
+          </div>
+        )}
+        <div className="w-full">
+          <TextField
+            label="Event name in calendar"
+            type="text"
+            {...shouldLockDisableProps("eventName")}
+            placeholder={eventNamePlaceholder}
+            defaultValue={eventType.eventName || ""}
+            {...formMethods.register("eventName")}
+            addOnSuffix={
+              <Button
+                color="minimal"
+                size="sm"
+                aria-label="edit custom name"
+                className="hover:stroke-3 hover:text-emphasis min-w-fit !py-0 px-0 hover:bg-transparent"
+                onClick={() => setShowEventNameTip((old) => !old)}>
+                <Edit className="h-4 w-4" />
+              </Button>
+            }
+          />
+        </div>
+      </div>
+      <EventCalendarView />
+
+      <BookerLayoutSelector fallbackToUserSettings isDark={selectedThemeIsDark} isOuterBorder={true} />
+
+      <div className="border-subtle space-y-6 rounded-lg border p-6">
+        <FormBuilder
+          title="Booking questions"
+          description="Customize the questions asked on the booking page"
+          addFieldLabel="Add a question"
+          formProp="bookingFields"
+          {...shouldLockDisableProps("bookingFields")}
+          dataStore={{
+            options: {
+              locations: getLocationsOptionsForSelect(eventType?.locations ?? [], t),
+            },
+          }}
+        />
+      </div>
+    </div>
+  );
+}
