12072ccbb391449192d98f99490513408b5d48c4
gabriel <gabpavao@outlook.com>
2024-02-02 14:41:16 -0300

    style: change background color

--- packages/features/sidebar/index.tsx
+++ packages/features/sidebar/index.tsx
@@ -3,5 +3,11 @@ import { TailwindSidebar } from "@funnelhub/sidebar";
 import { sidebarMenuData } from "./data";
 
 export const FunnelHubSidebar = () => {
-  return <TailwindSidebar menuData={sidebarMenuData} initialCollapsedState={true} className="!h-auto" />;
+  return (
+    <TailwindSidebar
+      menuData={sidebarMenuData}
+      initialCollapsedState={true}
+      className="border-r-gray-[#282939] !h-auto bg-[#14141F] hover:[&>a>div]:bg-[#BF0D51]"
+    />
+  );
 };
