a495d171f5989c741402daffc44b6d87374bb4d7
supalarry <lauris.skraucis@gmail.com>
2024-02-02 11:00:58 +0100

    fix: setup react query

--- packages/platform/atoms/cal-provider/CalProvider.tsx
+++ packages/platform/atoms/cal-provider/CalProvider.tsx
@@ -1,17 +1,11 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { ReactNode } from "react";
-import { useState } from "react";
 
-import { AtomsContext } from "../hooks/useAtomsContext";
-import { useOAuthClient } from "../hooks/useOAuthClient";
-import { useOAuthFlow } from "../hooks/useOAuthFlow";
-// import { useTimezone } from "../hooks/useTimezone";
-import { useUpdateUserTimezone } from "../hooks/useUpdateUserTimezone";
-import http from "../lib/http";
+import { BaseCalProvider } from "./BaseCalProvider";
 
 const queryClient = new QueryClient();
 
-type CalProviderProps = {
+export type CalProviderProps = {
   children?: ReactNode;
   clientId: string;
   accessToken: string;
@@ -19,49 +13,11 @@ type CalProviderProps = {
 };
 
 export function CalProvider({ clientId, accessToken, options, children }: CalProviderProps) {
-  const [error, setError] = useState<string>("");
-
-  const { mutateAsync } = useUpdateUserTimezone();
-  const handleTimezoneChange = async (currentTimezone: string) => {
-    await mutateAsync({ timeZone: currentTimezone });
-  };
-
-  const { isInit } = useOAuthClient({
-    clientId,
-    apiUrl: options.apiUrl,
-    refreshUrl: options.refreshUrl,
-    onError: setError,
-  });
-
-  const { isRefreshing, currentAccessToken } = useOAuthFlow({
-    accessToken,
-    refreshUrl: options.refreshUrl,
-    onError: setError,
-    clientId,
-  });
-
-  // userTimezone is using react query
-  // that is why it needs to be used inside of cal provider instead of here
-  // useTimezone(handleTimezoneChange);
-
-  return isInit ? (
-    <AtomsContext.Provider
-      value={{
-        clientId,
-        accessToken: currentAccessToken,
-        options,
-        error,
-        getClient: () => http,
-        isRefreshing: isRefreshing,
-        isInit: isInit,
-        isValidClient: Boolean(!error && clientId && isInit),
-        isAuth: Boolean(
-          isInit && !error && clientId && !isRefreshing && currentAccessToken && http.getAuthorizationHeader()
-        ),
-      }}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </AtomsContext.Provider>
-  ) : (
-    <>{children}</>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BaseCalProvider clientId={clientId} accessToken={accessToken} options={options}>
+        {children}
+      </BaseCalProvider>
+    </QueryClientProvider>
   );
 }
