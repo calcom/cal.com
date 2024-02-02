5a8cc090763c3144b80ed5a993ab033c0e9a32d3
Morgan Vernay <morgan@cal.com>
2024-02-02 15:36:46 +0200

    chore: getAvialableSlots and handleNewBooking in platform-libraries

--- packages/platform/libraries/index.ts
+++ packages/platform/libraries/index.ts
@@ -1,3 +1,9 @@
+import * as newBookingMethods from "@calcom/features/bookings/lib/handleNewBooking";
+
 export { getEventTypeById } from "@calcom/lib/getEventTypeById";
 export { EventService } from "@calcom/features/eventtypes/lib/event-service";
 export { getUsernameList } from "@calcom/lib/defaultEvents";
+
+const handleNewBooking = newBookingMethods.default;
+export { handleNewBooking };
+export { getAvailableSlots } from "@calcom/trpc/server/routers/viewer/slots/util";
