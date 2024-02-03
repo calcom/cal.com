9435eef8a04bacdfd7a9b6c7c8c48b468c8480a7
Hariom <hariombalhara@gmail.com>
2024-02-03 16:42:59 +0530

    Fix event-type listing not working

--- packages/lib/server/repository/eventType.ts
+++ packages/lib/server/repository/eventType.ts
@@ -87,48 +87,75 @@ export class EventTypeRepository {
   ) {
     if (!upId) return [];
     const lookupTarget = ProfileRepository.getLookupTarget(upId);
-    const eventTypeWhere = {
-      ...(lookupTarget.type === LookupTarget.User
-        ? {
-            userId: lookupTarget.id,
-          }
-        : {
-            profileId: lookupTarget.id,
-          }),
-      ...where,
+    const profileId = lookupTarget.type === LookupTarget.User ? null : lookupTarget.id;
+    const include = {
+      // TODO:  As required by getByViewHandler - Make it configurable
+      team: {
+        include: {
+          eventTypes: true,
+        },
+      },
+      hashedLink: true,
+      users: true,
+      children: {
+        include: {
+          users: true,
+        },
+      },
+      hosts: {
+        include: {
+          user: true,
+        },
+      },
     };
+
     log.debug(
       "findAllByUpId",
       safeStringify({
         upId,
         orderBy,
         argumentWhere: where,
-        where: eventTypeWhere,
       })
     );
-    return await prisma.eventType.findMany({
-      where: eventTypeWhere,
-      include: {
-        // TODO:  As required by getByViewHandler - Make it configurable
-        team: {
-          include: {
-            eventTypes: true,
-          },
+
+    if (!profileId) {
+      return await prisma.eventType.findMany({
+        where: {
+          userId: lookupTarget.id,
+          ...where,
         },
-        hashedLink: true,
-        users: true,
-        children: {
-          include: {
-            users: true,
-          },
+        include,
+        orderBy,
+      });
+    }
+
+    const profile = await ProfileRepository.findById(profileId);
+    if (profile?.movedFromUser) {
+      // Get all user events except those that belong to some other profile
+      return await prisma.eventType.findMany({
+        where: {
+          OR: [
+            {
+              userId: profile.movedFromUser.id,
+              profileId: null,
+            },
+            {
+              profileId: profileId,
+            },
+          ],
+          ...where,
         },
-        hosts: {
-          include: {
-            user: true,
-          },
+        include,
+        orderBy,
+      });
+    } else {
+      return await prisma.eventType.findMany({
+        where: {
+          profileId,
         },
-      },
-      orderBy,
-    });
+        include,
+        orderBy,
+      });
+    }
   }
 }
