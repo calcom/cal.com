f427b320231a9cc7bb7162a3e483b941c352d213
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-02-04 18:04:48 +0530

    replace fetch with axios client and fix pathname

--- packages/platform/atoms/hooks/useMe.ts
+++ packages/platform/atoms/hooks/useMe.ts
@@ -2,22 +2,23 @@ import { useQuery } from "@tanstack/react-query";
 
 import { BASE_URL, API_VERSION, V2_ENDPOINTS } from "@calcom/platform-constants";
 
+import http from "../lib/http";
 import { useAtomsContext } from "./useAtomsContext";
 
 export const useMe = () => {
+  const { accessToken } = useAtomsContext();
   const endpoint = new URL(BASE_URL);
 
-  endpoint.pathname = `${API_VERSION}/${V2_ENDPOINTS.me}`;
-
-  const { accessToken } = useAtomsContext();
+  endpoint.pathname = `api/${API_VERSION}/${V2_ENDPOINTS.me}`;
 
   const { data: me } = useQuery({
     queryKey: ["get-user-timezone"],
     queryFn: () => {
-      return fetch(endpoint.toString(), {
-        method: "get",
-        headers: { "Content-type": "application/json", Authorization: `Bearer ${accessToken}` },
-      }).then((res) => res.json());
+      return http
+        ?.get(endpoint.toString(), {
+          headers: { "Content-type": "application/json", Authorization: `Bearer ${accessToken}` },
+        })
+        .then((res) => res.data);
     },
   });
 
