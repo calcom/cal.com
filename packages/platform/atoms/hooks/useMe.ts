df43600407a2df50261f2a0090ab09ac2c883171
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-02-05 17:06:20 +0530

    excule headers in useMe request

--- packages/platform/atoms/hooks/useMe.ts
+++ packages/platform/atoms/hooks/useMe.ts
@@ -3,10 +3,8 @@ import { useQuery } from "@tanstack/react-query";
 import { BASE_URL, API_VERSION, V2_ENDPOINTS } from "@calcom/platform-constants";
 
 import http from "../lib/http";
-import { useAtomsContext } from "./useAtomsContext";
 
 export const useMe = () => {
-  const { accessToken } = useAtomsContext();
   const endpoint = new URL(BASE_URL);
 
   endpoint.pathname = `api/${API_VERSION}/${V2_ENDPOINTS.me}`;
@@ -14,11 +12,7 @@ export const useMe = () => {
   const { data: me } = useQuery({
     queryKey: ["get-user-timezone"],
     queryFn: () => {
-      return http
-        ?.get(endpoint.toString(), {
-          headers: { "Content-type": "application/json", Authorization: `Bearer ${accessToken}` },
-        })
-        .then((res) => res.data);
+      return http?.get(endpoint.toString()).then((res) => res.data);
     },
   });
 
