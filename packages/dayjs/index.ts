8ab45ca9d391df13f30ae5a36bd9ee4d76845cd1
Keith Williams <keithwillcode@gmail.com>
2024-02-05 12:47:43 -0300

    Updated the imports

--- packages/dayjs/index.ts
+++ packages/dayjs/index.ts
@@ -1,17 +1,17 @@
 /* eslint-disable @calcom/eslint/deprecated-imports */
 import dayjsBusinessTime from "dayjs-business-days2";
 
-import dayjs from "@calcom/dayjs";
-import customParseFormat from "@calcom/dayjs/plugin/customParseFormat";
-import duration from "@calcom/dayjs/plugin/duration";
-import isBetween from "@calcom/dayjs/plugin/isBetween";
-import isToday from "@calcom/dayjs/plugin/isToday";
-import localizedFormat from "@calcom/dayjs/plugin/localizedFormat";
-import minmax from "@calcom/dayjs/plugin/minMax";
-import relativeTime from "@calcom/dayjs/plugin/relativeTime";
-import timeZone from "@calcom/dayjs/plugin/timezone";
-import toArray from "@calcom/dayjs/plugin/toArray";
-import utc from "@calcom/dayjs/plugin/utc";
+import dayjs from "@calcom/dayjs-fork";
+import customParseFormat from "@calcom/dayjs-fork/plugin/customParseFormat";
+import duration from "@calcom/dayjs-fork/plugin/duration";
+import isBetween from "@calcom/dayjs-fork/plugin/isBetween";
+import isToday from "@calcom/dayjs-fork/plugin/isToday";
+import localizedFormat from "@calcom/dayjs-fork/plugin/localizedFormat";
+import minmax from "@calcom/dayjs-fork/plugin/minMax";
+import relativeTime from "@calcom/dayjs-fork/plugin/relativeTime";
+import timeZone from "@calcom/dayjs-fork/plugin/timezone";
+import toArray from "@calcom/dayjs-fork/plugin/toArray";
+import utc from "@calcom/dayjs-fork/plugin/utc";
 
 dayjs.extend(customParseFormat);
 dayjs.extend(dayjsBusinessTime);
