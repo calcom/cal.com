5e1fcb9b640cfb359d3ddb222ec3b9e713838c79
Keith Williams <keithwillcode@gmail.com>
2024-02-05 13:54:26 -0300

    Fixed import casing

--- packages/dayjs/index.ts
+++ packages/dayjs/index.ts
@@ -8,7 +8,7 @@ import isToday from "dayjs/plugin/isToday";
 import localizedFormat from "dayjs/plugin/localizedFormat";
 import minmax from "dayjs/plugin/minMax";
 import relativeTime from "dayjs/plugin/relativeTime";
-import timeZone from "dayjs/plugin/timeZone";
+import timeZone from "dayjs/plugin/timezone";
 import toArray from "dayjs/plugin/toArray";
 import utc from "dayjs/plugin/utc";
 
