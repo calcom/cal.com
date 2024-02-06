d1140f1f95ff168c4bd03c7de7e93b10a6e80629
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-02-06 14:19:13 +0530

    include availability settings into component exports

--- packages/platform/atoms/components.ts
+++ packages/platform/atoms/components.ts
@@ -1,2 +1,4 @@
 export { CalProvider } from "./cal-provider";
 export { GcalConnect } from "./gcal-connect";
+export { Availability } from "./availability/index";
+export { AvailabilitySettings } from "./availability/settings/index";
