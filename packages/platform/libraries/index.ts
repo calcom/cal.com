fc84bf45500e695c167d16a19f86d2e631ebee78
supalarry <lauris.skraucis@gmail.com>
2024-02-06 14:48:18 +0100

    extract connected calendars logic in re-usable platform-libraries function

--- packages/platform/libraries/index.ts
+++ packages/platform/libraries/index.ts
@@ -1,3 +1,3 @@
 export { getEventTypeById } from "@calcom/lib/getEventTypeById";
 export type { EventType } from "@calcom/lib/getEventTypeById";
-export { getConnectedDestinationCalendars } from "@calcom/trpc/server/routers/loggedInViewer/connectedCalendars.handler";
+export { getConnectedDestinationCalendars } from "@calcom/lib/getConnectedDestinationCalendars";
