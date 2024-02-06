be557d5a9d4384d66f29980951b0df86d97db904
supalarry <lauris.skraucis@gmail.com>
2024-02-06 16:49:03 +0100

    enable calendars endpoint

--- apps/api/v2/src/ee/calendars/controllers/calendars.controller.ts
+++ apps/api/v2/src/ee/calendars/controllers/calendars.controller.ts
@@ -1,21 +1,7 @@
 import { CalendarsService } from "@/ee/calendars/services/calendars.service";
-import { UpdateScheduleInput } from "@/ee/schedules/inputs/update-schedule.input";
-import { SchedulesService } from "@/ee/schedules/services/schedules.service";
-import { ScheduleResponse, schemaScheduleResponse } from "@/ee/schedules/zod/response/response";
 import { GetUser } from "@/modules/auth/decorators/get-user/get-user.decorator";
 import { AccessTokenGuard } from "@/modules/auth/guards/access-token/access-token.guard";
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  HttpCode,
-  HttpStatus,
-  Param,
-  Post,
-  Patch,
-  UseGuards,
-} from "@nestjs/common";
+import { Controller, Get, UseGuards } from "@nestjs/common";
 
 import { Calendars } from "@calcom/lib";
 import { SUCCESS_STATUS } from "@calcom/platform-constants";
