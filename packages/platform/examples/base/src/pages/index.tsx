a889c2320cb694a7fc4d69993e7c0b93b85feb21
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-02-07 13:13:45 +0530

    add availability atom in examples app

--- packages/platform/examples/base/src/pages/index.tsx
+++ packages/platform/examples/base/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 
 import { GcalConnect } from "@calcom/platform-atoms/components";
+import { Availability } from "@calcom/platform-atoms/components";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +10,7 @@ export default function Home() {
     <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <GcalConnect className="bg-orange-600 hover:bg-orange-700" />
+        <Availability />
       </div>
     </main>
   );
