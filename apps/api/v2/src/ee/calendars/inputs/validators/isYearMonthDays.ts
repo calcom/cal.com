babf73b1150ee9ea02867f0e26919419b23926bf
Morgan <33722304+ThyMinimalDev@users.noreply.github.com>
2024-02-07 14:05:20 +0200

    feat: overlay calendar busy dates events (#13581)
    * feat: overlay calendar busy times events
    
    * fixup! feat: overlay calendar busy times events
    
    * fixup! fixup! feat: overlay calendar busy times events
    
    * fixup! fixup! fixup! feat: overlay calendar busy times events

---
+++ apps/api/v2/src/ee/calendars/inputs/validators/isYearMonthDays.ts
@@ -0,0 +1,21 @@
+import { ValidatorConstraint, ValidatorConstraintInterface } from "class-validator";
+import { DateTime } from "luxon";
+
+@ValidatorConstraint({ name: "IsYearMonthDays", async: false })
+export class IsYearMonthDays implements ValidatorConstraintInterface {
+  validate(dateString: string): boolean {
+    try {
+      // Attempt to parse the date string
+      const dateTime = DateTime.fromFormat(dateString, "yyyy-MM-dd");
+
+      // Check if the parsed date is valid
+      return dateTime.isValid;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  defaultMessage() {
+    return "time format must be YEAR-MONTH-DAYS (e.g. 2022-01-01)";
+  }
+}
