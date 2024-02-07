9f7318f8fbde0870f41deb24199c2bbb84df0f08
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-02-07 13:10:21 +0530

    add platform wrappers in default exports

--- packages/platform/atoms/availability/index.ts
+++ packages/platform/atoms/availability/index.ts
@@ -1,3 +1,5 @@
 export { Availability } from "./Availability";
 export { AvailabilitySettings } from "./settings/AvailabilitySettings";
+export { PlatformAvailabilityWrapper } from "./wrappers";
+export { PlatformAvailabilitySettingsWrapper } from "./wrappers";
 export * from "../types";
