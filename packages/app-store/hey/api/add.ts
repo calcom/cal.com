67e252b1eb3735d0c3142219dedd34c5f38ae68c
Peer Richelsen <peer@cal.com>
2024-02-08 11:12:33 +0000

    added ics logic to hey

--- packages/app-store/hey/api/add.ts
+++ packages/app-store/hey/api/add.ts
@@ -1,16 +1,60 @@
-import { createDefaultInstallation } from "@calcom/app-store/_utils/installation";
-import type { AppDeclarativeHandler } from "@calcom/types/AppHandler";
+import type { NextApiRequest, NextApiResponse } from "next";
 
+import { symmetricEncrypt } from "@calcom/lib/crypto";
+import logger from "@calcom/lib/logger";
+import prisma from "@calcom/prisma";
+
+import getInstalledAppPath from "../../_utils/getInstalledAppPath";
 import appConfig from "../config.json";
+import { CalendarService } from "../lib";
 
-const handler: AppDeclarativeHandler = {
-  appType: appConfig.type,
-  variant: appConfig.variant,
-  slug: appConfig.slug,
-  supportsMultipleInstalls: false,
-  handlerType: "add",
-  createCredential: ({ appType, user, slug, teamId }) =>
-    createDefaultInstallation({ appType, user: user, slug, key: {}, teamId }),
-};
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    const { urls } = req.body;
+    // Get user
+    const user = await prisma.user.findFirstOrThrow({
+      where: {
+        id: req.session?.user?.id,
+      },
+      select: {
+        id: true,
+        email: true,
+      },
+    });
 
-export default handler;
+    const data = {
+      type: appConfig.type,
+      key: symmetricEncrypt(JSON.stringify({ urls }), process.env.CALENDSO_ENCRYPTION_KEY || ""),
+      userId: user.id,
+      teamId: null,
+      appId: appConfig.slug,
+      invalid: false,
+    };
+
+    try {
+      const dav = new CalendarService({
+        id: 0,
+        ...data,
+        user: { email: user.email },
+      });
+      const listedCals = await dav.listCalendars();
+
+      if (listedCals.length !== urls.length) {
+        throw new Error(`Listed cals and URLs mismatch: ${listedCals.length} vs. ${urls.length}`);
+      }
+
+      await prisma.credential.create({
+        data,
+      });
+    } catch (e) {
+      logger.error("Could not add ICS feeds", e);
+      return res.status(500).json({ message: "Could not add ICS feeds" });
+    }
+
+    return res.status(200).json({ url: getInstalledAppPath({ variant: "calendar", slug: "hey" }) });
+  }
+
+  if (req.method === "GET") {
+    return res.status(200).json({ url: "/apps/hey/setup" });
+  }
+}
