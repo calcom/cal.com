e7fa358302a906231d94795405611af71cecd840
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-02-08 18:29:52 +0530

    Replace event calendar view with user selected destination calendar

--- packages/platform/atoms/event-type/components/user-selected-destination-calendar/index.tsx
+++ packages/platform/atoms/event-type/components/user-selected-destination-calendar/index.tsx
@@ -1,6 +1,5 @@
 import { Edit } from "lucide-react";
-import { Controller } from "react-hook-form";
-import type { UseFormReturn } from "react-hook-form";
+import { Controller, type UseFormReturn } from "react-hook-form";
 
 import DestinationCalendarSelector from "@calcom/features/calendars/DestinationCalendarSelector";
 import { Label, TextField, Button } from "@calcom/ui";
@@ -8,40 +7,31 @@ import { Label, TextField, Button } from "@calcom/ui";
 import type { EventTypeSetupProps } from "../../tabs/event-setup/index";
 import type { FormValues } from "../../types";
 
-type EventCalendarViewProps = {
-  team: Pick<EventTypeSetupProps, "eventType" | "team">;
-  userConnectedCalendars: [];
-  formMethods: UseFormReturn<FormValues, any>;
-  destinationCalendarValue: {
-    id: number;
-    userId: number | null;
-    eventTypeId: number | null;
-    externalId: string;
-    integration: string;
-    credentialId: number | null;
-  } | null;
+type UserSelectedDestinationCalendarProps = {
+  eventType: Pick<EventTypeSetupProps, "eventType" | "team">;
   eventNamePlaceholder: string;
-  eventNameDefaultValue: string;
+  formMethods: UseFormReturn<FormValues, any>;
   shouldLockDisableProps: (fieldName: string) => {
     disabled: boolean;
     LockedIcon: false | JSX.Element;
   };
-  handleClick: () => void;
+  isConnectedConnectedPresent: boolean;
+  isTeamPresent: boolean;
+  toggleEventNameTip: () => void;
 };
 
-export function EventCalendarView({
-  team,
-  userConnectedCalendars,
+export function UserSelectedDestinationCalendar({
   formMethods,
-  destinationCalendarValue,
+  eventType,
   eventNamePlaceholder,
-  eventNameDefaultValue,
   shouldLockDisableProps,
-  handleClick,
-}: EventCalendarViewProps) {
+  isConnectedConnectedPresent,
+  isTeamPresent,
+  toggleEventNameTip,
+}: UserSelectedDestinationCalendarProps) {
   return (
     <div className="border-subtle space-y-6 rounded-lg border p-6">
-      {!!userConnectedCalendars.length && !team && (
+      {isConnectedConnectedPresent && isTeamPresent && (
         <div className="flex flex-col">
           <div className="flex justify-between">
             <div>
@@ -57,10 +47,10 @@ export function EventCalendarView({
           <Controller
             control={formMethods.control}
             name="destinationCalendar"
-            defaultValue={destinationCalendarValue || undefined}
+            defaultValue={eventType.destinationCalendar || undefined}
             render={({ field: { onChange, value } }) => (
               <DestinationCalendarSelector
-                destinationCalendar={destinationCalendarValue}
+                destinationCalendar={eventType.destinationCalendar}
                 value={value ? value.externalId : undefined}
                 onChange={onChange}
                 hidePlaceholder
@@ -77,7 +67,7 @@ export function EventCalendarView({
           type="text"
           {...shouldLockDisableProps("eventName")}
           placeholder={eventNamePlaceholder}
-          defaultValue={eventNameDefaultValue || ""}
+          defaultValue={eventType.eventName || ""}
           {...formMethods.register("eventName")}
           addOnSuffix={
             <Button
@@ -85,7 +75,7 @@ export function EventCalendarView({
               size="sm"
               aria-label="edit custom name"
               className="hover:stroke-3 hover:text-emphasis min-w-fit !py-0 px-0 hover:bg-transparent"
-              onClick={handleClick}>
+              onClick={toggleEventNameTip}>
               <Edit className="h-4 w-4" />
             </Button>
           }
