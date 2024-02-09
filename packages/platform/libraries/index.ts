56012debc0cc2a59d18257876bf6cc36fc0a0266
Morgan Vernay <morgan@cal.com>
2024-02-09 11:05:58 +0200

    feat: handle recurring and instant booking

--- packages/platform/libraries/index.ts
+++ packages/platform/libraries/index.ts
@@ -1,4 +1,5 @@
 import * as newBookingMethods from "@calcom/features/bookings/lib/handleNewBooking";
+import * as instantMeetingMethods from "@calcom/features/instant-meeting/handleInstantMeeting";
 
 export { getEventTypeById } from "@calcom/lib/getEventTypeById";
 export { getPublicEvent } from "@calcom/features/eventtypes/lib/getPublicEvent";
@@ -6,7 +7,11 @@ export { getUsernameList } from "@calcom/lib/defaultEvents";
 
 const handleNewBooking = newBookingMethods.default;
 export { handleNewBooking };
+const handleInstantMeeting = instantMeetingMethods.default;
+export { handleInstantMeeting };
 export { getAvailableSlots } from "@calcom/trpc/server/routers/viewer/slots/util";
+export { handleNewRecurringBooking } from "@calcom/features/bookings/lib/handleNewRecurringBooking";
+
 export type { EventType } from "@calcom/lib/getEventTypeById";
 export { getBusyCalendarTimes } from "@calcom/core/CalendarManager";
 export type {
