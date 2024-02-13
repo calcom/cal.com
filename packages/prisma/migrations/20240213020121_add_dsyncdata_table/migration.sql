de426e7c810fb63ff7a3d6c5d46c81edf8226334
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-02-13 13:44:33 -0500

    DB change for SCIM for orgs

--- packages/prisma/migrations/20240213020121_add_dsyncdata_table/migration.sql
+++ packages/prisma/migrations/20240213020121_add_dsyncdata_table/migration.sql
@@ -12,13 +12,13 @@ CREATE TABLE "DSyncData" (
     "id" SERIAL NOT NULL,
     "directoryId" TEXT NOT NULL,
     "tenant" TEXT NOT NULL,
-    "teamId" INTEGER,
+    "orgId" INTEGER,
 
     CONSTRAINT "DSyncData_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "DSyncData_teamId_key" ON "DSyncData"("teamId");
+CREATE UNIQUE INDEX "DSyncData_orgId_key" ON "DSyncData"("orgId");
 
 -- AddForeignKey
-ALTER TABLE "DSyncData" ADD CONSTRAINT "DSyncData_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "DSyncData" ADD CONSTRAINT "DSyncData_orgId_fkey" FOREIGN KEY ("orgId") REFERENCES "Team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
