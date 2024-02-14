6e85fba7736e89d18b2f0c7789a9614600e85c33
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-02-14 14:26:22 -0500

    Send email to reassigned and cancelled member

--- packages/trpc/server/routers/viewer/teams/roundRobin/roundRobinReassign.handler.ts
+++ packages/trpc/server/routers/viewer/teams/roundRobin/roundRobinReassign.handler.ts
@@ -1,9 +1,13 @@
 import dayjs from "@calcom/dayjs";
+import { sendRoundRobinCancelledEmails, sendRoundRobinScheduledEmails } from "@calcom/emails";
 import { ensureAvailableUsers } from "@calcom/features/bookings/lib/handleNewBooking";
 import logger from "@calcom/lib/logger";
 import { getLuckyUser } from "@calcom/lib/server";
+import { getTranslation } from "@calcom/lib/server/i18n";
+import { getTimeFormatStringFromUserTimeFormat } from "@calcom/lib/timeFormat";
 import { prisma } from "@calcom/prisma";
 import type { TrpcSessionUser } from "@calcom/trpc/server/trpc";
+import type { CalendarEvent } from "@calcom/types/Calendar";
 
 import { TRPCError } from "@trpc/server";
 
@@ -29,6 +33,8 @@ export const roundRobinReassignHandler = async ({ ctx, input }: RoundRobinReassi
     select: {
       id: true,
       availability: true,
+      slug: true,
+      description: true,
       hosts: {
         select: {
           user: {
@@ -42,8 +48,9 @@ export const roundRobinReassignHandler = async ({ ctx, input }: RoundRobinReassi
           },
         },
       },
-      users: {
+      team: {
         select: {
+          name: true,
           id: true,
         },
       },
@@ -55,22 +62,33 @@ export const roundRobinReassignHandler = async ({ ctx, input }: RoundRobinReassi
   }
 
   eventType.users = eventType.hosts.map((host) => ({ ...host.user }));
-  // console.log("🚀 ~ roundRobinReassignHandler ~ eventType.users:", eventType.users);
 
-  const booking = await prisma.booking.findUnique({
+  let booking = await prisma.booking.findUnique({
     where: {
       id: bookingId,
     },
     select: {
+      uid: true,
+      title: true,
       startTime: true,
       endTime: true,
+      userId: true,
+      customInputs: true,
+      responses: true,
       attendees: {
         select: {
           email: true,
+          name: true,
+          timeZone: true,
+          locale: true,
         },
       },
     },
   });
+
+  if (!booking) {
+    throw new TRPCError({ code: "NOT_FOUND" });
+  }
 
   const availableUsers = await ensureAvailableUsers(
     eventType,
@@ -81,23 +99,97 @@ export const roundRobinReassignHandler = async ({ ctx, input }: RoundRobinReassi
     },
     roundRobinReassignLogger
   );
-  // console.log("🚀 ~ roundRobinReassignHandler ~ availableUsers:", availableUsers);
 
   const luckyUser = await getLuckyUser("MAXIMIZE_AVAILABILITY", {
     availableUsers,
     eventTypeId: eventTypeId,
   });
 
-  console.log("🚀 ~ roundRobinReassignHandler ~ luckyUser:", luckyUser);
-
   // See if user is the assigned user or an attendee
   if (booking.userId === ctx.user.id) {
-    console.log("booking is connected to user");
+    booking = await prisma.booking.update({
+      where: {
+        id: bookingId,
+      },
+      data: {
+        userId: luckyUser.id,
+      },
+      include: {
+        attendees: true,
+      },
+    });
   } else if (booking.attendees.some((attendee) => attendee.email === ctx.user.email)) {
     console.log("booking is connected to attendee");
   } else {
     console.log("user is not in the booking");
   }
+
+  const t = await getTranslation(luckyUser.locale || "en", "common");
+
+  const teamMembers = await Promise.all(
+    eventType.hosts.map(async (teamMember) => {
+      const user = teamMember.user;
+      const tTeamMember = await getTranslation(user.locale ?? "en", "common");
+
+      return {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        timeZone: user.timeZone,
+        language: { translate: tTeamMember, locale: user.locale ?? "en" },
+      };
+    })
+  );
+
+  const attendeeList = await Promise.all(
+    booking.attendees.map(async (attendee) => {
+      const tAttendee = await getTranslation(attendee.locale ?? "en", "common");
+
+      return {
+        email: attendee.email,
+        name: attendee.name,
+        timeZone: attendee.timeZone,
+        language: { translate: tAttendee, locale: attendee.locale ?? "en" },
+      };
+    })
+  );
+
+  const evt: CalendarEvent = {
+    organizer: {
+      name: luckyUser.name || "",
+      email: luckyUser.email,
+      language: {
+        locale: luckyUser.locale || "en",
+        translate: t,
+      },
+      timeZone: luckyUser.timeZone,
+      timeFormat: getTimeFormatStringFromUserTimeFormat(luckyUser.timeFormat),
+    },
+    startTime: dayjs(booking.startTime).utc().format(),
+    endTime: dayjs(booking.endTime).utc().format(),
+    type: eventType.slug,
+    title: booking.title,
+    description: eventType.description,
+    attendees: attendeeList,
+    uid: booking.uid,
+    team: {
+      members: teamMembers,
+      name: eventType.team?.name,
+      id: eventType.team?.id,
+    },
+    customInputs: booking.customInputs,
+    userFieldsResponses: booking.responses,
+    // TODO complete this evt object
+  };
+
+  // Send to new RR host
+  sendRoundRobinScheduledEmails(evt, [
+    { ...luckyUser, language: { translate: t, locale: luckyUser.locale || "en" } },
+  ]);
+  // Send to cancelled RR host
+  sendRoundRobinCancelledEmails(evt, [
+    { ...ctx.user, language: { translate: t, locale: ctx.user.locale || "en" } },
+  ]);
 
   return;
 };
