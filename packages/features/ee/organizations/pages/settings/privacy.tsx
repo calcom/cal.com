c1d61eb0b9ea514d47a73987048fb8e03558188d
Peer Richelsen <peer@cal.com>
2024-02-14 18:59:05 +0000

    moved privacy setting to new tab

--- packages/features/ee/organizations/pages/settings/privacy.tsx
+++ packages/features/ee/organizations/pages/settings/privacy.tsx
@@ -1,32 +1,38 @@
 "use client";
 
 import LicenseRequired from "@calcom/features/ee/common/components/LicenseRequired";
+import MakeTeamPrivateSwitch from "@calcom/features/ee/teams/components/MakeTeamPrivateSwitch";
 import { getLayout } from "@calcom/features/settings/layouts/SettingsLayout";
-import { UserListTable } from "@calcom/features/users/components/UserTable/UserListTable";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { MembershipRole } from "@calcom/prisma/enums";
 import { trpc } from "@calcom/trpc/react";
 import { Meta } from "@calcom/ui";
 
-const MembersView = () => {
+const PrivacyView = () => {
   const { t } = useLocale();
   const { data: currentOrg } = trpc.viewer.organizations.listCurrent.useQuery();
 
+  const isInviteOpen = !currentOrg?.user.accepted;
   const isOrgAdminOrOwner =
     currentOrg &&
     (currentOrg.user.role === MembershipRole.OWNER || currentOrg.user.role === MembershipRole.ADMIN);
 
   return (
     <LicenseRequired>
-      <Meta title={t("organization_members")} description={t("organization_description")} />
+      <Meta title={t("privacy")} description={t("organization_description")} />
       <div>
-        {((currentOrg?.isPrivate && isOrgAdminOrOwner) || isOrgAdminOrOwner || !currentOrg?.isPrivate) && (
-          <UserListTable />
+        {currentOrg && isOrgAdminOrOwner && (
+          <MakeTeamPrivateSwitch
+            isOrg={true}
+            teamId={currentOrg.id}
+            isPrivate={currentOrg.isPrivate}
+            disabled={isInviteOpen}
+          />
         )}
       </div>
     </LicenseRequired>
   );
 };
-MembersView.getLayout = getLayout;
+PrivacyView.getLayout = getLayout;
 
-export default MembersView;
+export default PrivacyView;
