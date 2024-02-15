98c4f5e318db3e792d5a6d800285adfc4b2dce6a
VenkateshSircilla123 <sircillavenkatesh0@gmail.com>
2024-02-15 07:52:23 +0000

    top banner feat added

--- apps/web/components/AppListCard.tsx
+++ apps/web/components/AppListCard.tsx
@@ -10,6 +10,7 @@ import { useCompatSearchParams } from "@calcom/lib/hooks/useCompatSearchParams";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { useTypedQuery } from "@calcom/lib/hooks/useTypedQuery";
 import { Badge, ListItemText, Avatar } from "@calcom/ui";
+import { TopBanner } from "@calcom/ui";
 import { AlertCircle } from "@calcom/ui/components/icon";
 
 type ShouldHighlight =
@@ -85,52 +86,62 @@ export default function AppListCard(props: AppListCardProps) {
   }, [highlight, pathname, router, searchParams, shouldHighlight]);
 
   return (
-    <div className={classNames(highlight && "dark:bg-muted bg-yellow-100", className)}>
-      <div className="flex items-center gap-x-3 px-4 py-4 sm:px-6">
-        {logo ? (
-          <img
-            className={classNames(logo.includes("-dark") && "dark:invert", "h-10 w-10")}
-            src={logo}
-            alt={`${title} logo`}
+    <>
+      {invalidCredential && (
+        <div className="fixed left-0 top-0 mb-3 w-[100vw]">
+          <TopBanner
+            text={`Oh no! Looks like permission expired or was revoked. Please reinstall again. `}
+            variant="warning"
           />
-        ) : null}
-        <div className="flex grow flex-col gap-y-1 truncate">
-          <div className="flex items-center gap-x-2">
-            <h3 className="text-emphasis truncate text-sm font-semibold">{title}</h3>
+        </div>
+      )}
+      <div className={classNames(highlight && "dark:bg-muted bg-yellow-100", className)}>
+        <div className="flex items-center gap-x-3 px-4 py-4 sm:px-6">
+          {logo ? (
+            <img
+              className={classNames(logo.includes("-dark") && "dark:invert", "h-10 w-10")}
+              src={logo}
+              alt={`${title} logo`}
+            />
+          ) : null}
+          <div className="flex grow flex-col gap-y-1 truncate">
             <div className="flex items-center gap-x-2">
-              {isDefault && <Badge variant="green">{t("default")}</Badge>}
-              {isTemplate && <Badge variant="red">Template</Badge>}
+              <h3 className="text-emphasis truncate text-sm font-semibold">{title}</h3>
+              <div className="flex items-center gap-x-2">
+                {isDefault && <Badge variant="green">{t("default")}</Badge>}
+                {isTemplate && <Badge variant="red">Template</Badge>}
+              </div>
             </div>
+            <ListItemText component="p">{description}</ListItemText>
+            {invalidCredential && (
+              <div className="flex gap-x-2 pt-2">
+                <AlertCircle className="h-8 w-8 text-red-500 sm:h-4 sm:w-4" />
+                <ListItemText component="p" className="whitespace-pre-wrap text-red-500">
+                  {t("invalid_credential")}
+                </ListItemText>
+              </div>
+            )}
           </div>
-          <ListItemText component="p">{description}</ListItemText>
-          {invalidCredential && (
-            <div className="flex gap-x-2 pt-2">
-              <AlertCircle className="h-8 w-8 text-red-500 sm:h-4 sm:w-4" />
-              <ListItemText component="p" className="whitespace-pre-wrap text-red-500">
-                {t("invalid_credential")}
-              </ListItemText>
+          {credentialOwner && (
+            <div>
+              <Badge variant="gray">
+                <div className="flex items-center">
+                  <Avatar
+                    className="mr-2"
+                    alt={credentialOwner.name || "Nameless"}
+                    size="xs"
+                    imageSrc={getPlaceholderAvatar(credentialOwner.avatar, credentialOwner?.name as string)}
+                  />
+                  {credentialOwner.name}
+                </div>
+              </Badge>
             </div>
           )}
-        </div>
-        {credentialOwner && (
-          <div>
-            <Badge variant="gray">
-              <div className="flex items-center">
-                <Avatar
-                  className="mr-2"
-                  alt={credentialOwner.name || "Nameless"}
-                  size="xs"
-                  imageSrc={getPlaceholderAvatar(credentialOwner.avatar, credentialOwner?.name as string)}
-                />
-                {credentialOwner.name}
-              </div>
-            </Badge>
-          </div>
-        )}
 
-        {actions}
+          {actions}
+        </div>
+        {children && <div className="w-full">{children}</div>}
       </div>
-      {children && <div className="w-full">{children}</div>}
-    </div>
+    </>
   );
 }
