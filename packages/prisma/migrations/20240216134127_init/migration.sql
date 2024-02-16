c28ef60b39f28935c0af9f6fc178cfc028832635
Hitesh Sisara <hitesh.dev@sisara.in>
2024-02-16 21:44:00 +0530

    reverse yarnlock changes

--- packages/prisma/migrations/20240216134127_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `password` on the `users` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "users" DROP COLUMN "password";
