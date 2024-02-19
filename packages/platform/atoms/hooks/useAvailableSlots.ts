27daf815d5e62740267aede615bd249db9e243bf
Morgan Vernay <morgan@cal.com>
2024-02-19 16:51:00 +0200

    fixup! feat: platform use get available slots

--- packages/platform/atoms/hooks/useAvailableSlots.ts
+++ packages/platform/atoms/hooks/useAvailableSlots.ts
@@ -1,13 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 
 import type { AvailableSlotsType } from "@calcom/platform-libraries";
-import type { GetPublicEventInput, ApiResponse } from "@calcom/platform-types";
+import type { GetAvailableSlotsInput, ApiResponse } from "@calcom/platform-types";
 
 import http from "../lib/http";
 
 export const QUERY_KEY = "get-available-slots";
 
-export const useAvailableSlots = (props: GetPublicEventInput) => {
+export const useAvailableSlots = (props: GetAvailableSlotsInput) => {
   const availableSlots = useQuery({
     queryKey: [QUERY_KEY],
     queryFn: () => {
