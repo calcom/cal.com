ac10c47af8e45a7396ee0fcc7f693b57c716229c
Morgan Vernay <morgan@cal.com>
2024-02-21 10:24:07 +0200

    fixup! fixup! fixup! fixup! feat: platform use slots hooks

--- packages/platform/atoms/hooks/useReserveSlot.ts
+++ packages/platform/atoms/hooks/useReserveSlot.ts
@@ -4,8 +4,6 @@ import type { ReserveSlotInput, ApiResponse } from "@calcom/platform-types";
 
 import http from "../lib/http";
 
-export const QUERY_KEY = "reserve-slot";
-
 export const useReserveSlot = (props: ReserveSlotInput) => {
   const reserveSlot = useMutation({
     mutationFn: () => {
