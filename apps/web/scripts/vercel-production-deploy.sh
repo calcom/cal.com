0fae67f3140482e223ba26bd4e8197313a9c8b88
zomars <zomars@me.com>
2024-02-22 16:20:13 -0700

    test

--- apps/web/scripts/vercel-production-deploy.sh
+++ apps/web/scripts/vercel-production-deploy.sh
@@ -3,22 +3,26 @@
 # Only deploy to production
 if [ "$VERCEL_ENV" != "production" ]; then exit 0; fi
 
+checkRoute () {
+  if [ "$1" != '1' ]; then rm -rf $2; fi
+}
+
 # These conditionals are used to remove directories from the build that are not needed in production
 # This is to reduce the size of the build and prevent OOM errors
-if [ "$APP_ROUTER_EVENT_TYPES_ENABLED" != '1' ]; then              rm -rf app/future/event-types; fi
-if [ "$APP_ROUTER_SETTINGS_ADMIN_ENABLED" != '1' ]; then           rm -rf app/future/settings/admin; fi
-if [ "$APP_ROUTER_APPS_INSTALLED_CATEGORY_ENABLED" != '1' ]; then  rm -rf app/future/apps/installed; fi
-if [ "$APP_ROUTER_APPS_SLUG_ENABLED" != '1' ]; then                rm -rf app/future/apps/\[slug\]; fi
-if [ "$APP_ROUTER_APPS_SLUG_SETUP_ENABLED" != '1' ]; then          rm -rf app/future/apps/\[slug\]/setup; fi
-if [ "$APP_ROUTER_APPS_CATEGORIES_ENABLED" != '1' ]; then          rm -rf app/future/apps/categories; fi
-if [ "$APP_ROUTER_APPS_CATEGORIES_CATEGORY_ENABLED" != '1' ]; then rm -rf app/future/apps/categories/\[category\]; fi
-if [ "$APP_ROUTER_WORKFLOWS_ENABLED" != '1' ]; then                rm -rf app/future/workflows; fi
-if [ "$APP_ROUTER_SETTINGS_TEAMS_ENABLED" != '1' ]; then           rm -rf app/future/settings/teams; fi
-if [ "$APP_ROUTER_GETTING_STARTED_STEP_ENABLED" != '1' ]; then     rm -rf app/future/getting-started; fi
-if [ "$APP_ROUTER_APPS_ENABLED" != '1' ]; then                     rm -rf app/future/apps; fi
-if [ "$APP_ROUTER_BOOKINGS_STATUS_ENABLED" != '1' ]; then          rm -rf app/future/bookings; fi
-if [ "$APP_ROUTER_VIDEO_ENABLED" != '1' ]; then                    rm -rf app/future/video; fi
-if [ "$APP_ROUTER_TEAMS_ENABLED" != '1' ]; then                    rm -rf app/future/teams; fi
+checkRoute "$APP_ROUTER_EVENT_TYPES_ENABLED" app/future/event-types
+checkRoute "$APP_ROUTER_SETTINGS_ADMIN_ENABLED" app/future/settings/admin
+checkRoute "$APP_ROUTER_APPS_INSTALLED_CATEGORY_ENABLED" app/future/apps/installed
+checkRoute "$APP_ROUTER_APPS_SLUG_ENABLED" app/future/apps/\[slug\]
+checkRoute "$APP_ROUTER_APPS_SLUG_SETUP_ENABLED" app/future/apps/\[slug\]/setup
+checkRoute "$APP_ROUTER_APPS_CATEGORIES_ENABLED" app/future/apps/categories
+checkRoute "$APP_ROUTER_APPS_CATEGORIES_CATEGORY_ENABLED" app/future/apps/categories/\[category\]
+checkRoute "$APP_ROUTER_WORKFLOWS_ENABLED" app/future/workflows
+checkRoute "$APP_ROUTER_SETTINGS_TEAMS_ENABLED" app/future/settings/teams
+checkRoute "$APP_ROUTER_GETTING_STARTED_STEP_ENABLED" app/future/getting-started
+checkRoute "$APP_ROUTER_APPS_ENABLED" app/future/apps
+checkRoute "$APP_ROUTER_BOOKINGS_STATUS_ENABLED" app/future/bookings
+checkRoute "$APP_ROUTER_VIDEO_ENABLED" app/future/video
+checkRoute "$APP_ROUTER_TEAMS_ENABLED" app/future/teams
 
 # These are routes that don't have and environment variable to enable or disable them
 # Will stop removing gradually as we test and confirm that they are working
