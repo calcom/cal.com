881a7a3ca3915963814328fee9bc8917ea06a70a
Udit Takkar <udit222001@gmail.com>
2024-02-23 17:00:18 +0530

    feat: use redis to block requests

--- apps/web/pages/api/book/event.ts
+++ apps/web/pages/api/book/event.ts
@@ -1,3 +1,5 @@
+import { Redis } from "@upstash/redis";
+import { createHash } from "crypto";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 import { getServerSession } from "@calcom/features/auth/lib/getServerSession";
@@ -5,6 +7,12 @@ import handleNewBooking from "@calcom/features/bookings/lib/handleNewBooking";
 import { checkRateLimitAndThrowError } from "@calcom/lib/checkRateLimitAndThrowError";
 import getIP from "@calcom/lib/getIP";
 import { defaultResponder } from "@calcom/lib/server";
+
+const generateHash = (data: string): string => {
+  return createHash("sha256").update(data).digest("hex");
+};
+
+const redis = Redis.fromEnv();
 
 async function handler(req: NextApiRequest & { userId?: number }, res: NextApiResponse) {
   const userIp = getIP(req);
@@ -17,8 +25,31 @@ async function handler(req: NextApiRequest & { userId?: number }, res: NextApiRe
   const session = await getServerSession({ req, res });
   /* To mimic API behavior and comply with types */
   req.userId = session?.user?.id || -1;
-  const booking = await handleNewBooking(req);
-  return booking;
+
+  const requestBody = JSON.stringify({ ...req.body, userIp });
+  const idempotencyKey = generateHash(requestBody);
+
+  try {
+    const checkIfKeyExist = await redis.exists(idempotencyKey);
+    if (checkIfKeyExist) {
+      return res.status(429).json({ message: "Request Already Processing" });
+    }
+
+    await redis.set(idempotencyKey, "1");
+    const booking = await handleNewBooking(req);
+    await redis.del(idempotencyKey);
+
+    return booking;
+  } catch (err) {
+    // Extract error information
+    const responseData = { message: err?.message ?? "Internal Server Error" };
+    const statusCode = err?.statusCode ?? 500;
+
+    await redis.del(idempotencyKey);
+
+    // Send the exact error and status code in the response
+    return res.status(statusCode).json(responseData);
+  }
 }
 
 export default defaultResponder(handler);
