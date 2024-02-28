096e546259a28865a1e370a4afc6dcc2e290a70c
henricker <henriquevieira@alu.ufc.br>
2024-02-28 13:14:20 -0300

    feat: type

--- apps/web/server/lib/auth/login/getServerSideProps.tsx
+++ apps/web/server/lib/auth/login/getServerSideProps.tsx
@@ -1,5 +1,5 @@
 import type { GetServerSidePropsContext } from "next";
 
-export async function getServerSideProps(_: GetServerSidePropsContext) {
+export async function getServerSideProps(_: GetServerSidePropsContext): Promise<any> {
   return { redirect: { permanent: false, destination: process.env.NEXT_PUBLIC_FUNNELHUB_URL } };
 }
