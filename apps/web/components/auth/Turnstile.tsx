2de56fe044936f9669d692d40881f3d41392fbfe
sean-brydon <sean@cal.com>
2024-02-28 11:09:09 +0000

    fix: hide in e2e tests

--- apps/web/components/auth/Turnstile.tsx
+++ apps/web/components/auth/Turnstile.tsx
@@ -6,7 +6,7 @@ import { CLOUDFLARE_SITE_ID } from "@calcom/lib/constants";
 type Props = Omit<TurnstileProps, "sitekey">;
 
 export default function TurnstileWidget(props: Props) {
-  if (!CLOUDFLARE_SITE_ID) return null;
+  if (!CLOUDFLARE_SITE_ID || process.env.NEXT_PUBLIC_IS_E2E) return null;
 
   return <Turnstile {...props} sitekey={CLOUDFLARE_SITE_ID} theme="auto" />;
 }
