ba666704992cb0d4cc3f54bd6d9b34fb9cd1637f
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-03-03 16:01:49 -0500

    Require a userId in the param

--- apps/api/pages/api/credential-sync/_get.ts
+++ apps/api/pages/api/credential-sync/_get.ts
@@ -2,19 +2,18 @@ import type { NextApiRequest } from "next";
 
 import { defaultResponder } from "@calcom/lib/server";
 
-import { extractUserIdsFromQuery } from "~/lib/utils/extractUserIdsFromQuery";
 import { schemaCredentialGetParams } from "~/lib/validations/credential-sync";
 
 async function handler(req: NextApiRequest) {
-  const { userId: reqUserId, prisma } = req;
+  const { prisma } = req;
 
-  const { appSlug } = schemaCredentialGetParams.parse(req.query);
+  const { appSlug, userId: reqUserId } = schemaCredentialGetParams.parse(req.query);
 
-  const userId = req.query.userId ? extractUserIdsFromQuery(req)[0] : reqUserId;
+  const userId = parseInt(reqUserId);
 
   let credentials = await prisma.credential.findMany({
     where: {
-      userId: userId,
+      userId,
       ...(appSlug && { appId: appSlug }),
     },
     select: {
--- apps/api/pages/api/credential-sync/_post.ts
+++ apps/api/pages/api/credential-sync/_post.ts
@@ -4,15 +4,14 @@ import { appStoreMetadata } from "@calcom/app-store/appStoreMetaData";
 import { HttpError } from "@calcom/lib/http-error";
 import { defaultResponder } from "@calcom/lib/server";
 
-import { extractUserIdsFromQuery } from "~/lib/utils/extractUserIdsFromQuery";
 import { schemaCredentialPostParams } from "~/lib/validations/credential-sync";
 
 async function handler(req: NextApiRequest) {
-  const { userId: reqUserId, prisma } = req;
+  const { prisma } = req;
 
-  const { appSlug, key } = schemaCredentialPostParams.parse(req.query);
+  const { appSlug, key, userId: reqUserId } = schemaCredentialPostParams.parse(req.query);
 
-  const userId = req.query.userId ? extractUserIdsFromQuery(req)[0] : reqUserId;
+  const userId = parseInt(reqUserId);
 
   // Need to get app type
   const app = await prisma.app.findUnique({
