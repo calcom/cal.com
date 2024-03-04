e126062185dbf63e74a410de44c7202080510e7c
Nurudeen Soremekun <nurudeen.soremekun@gmail.com>
2024-03-04 17:59:24 +0100

    Update docker-entrypoint.sh to export DATABASE_DIRECT_URL

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 export DATABASE_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${DATABASE_HOST}/${POSTGRES_DB}"
+export DATABASE_DIRECT_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${DATABASE_HOST}/${POSTGRES_DB}"
 
 exec "$@"
