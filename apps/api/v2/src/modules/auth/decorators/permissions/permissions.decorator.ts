9cbf6d4ffef4b9f824f497fef400554075f7de9c
supalarry <lauris.skraucis@gmail.com>
2024-03-05 17:33:29 -0300

    feat: Permissions guard

--- apps/api/v2/src/modules/auth/decorators/permissions/permissions.decorator.ts
+++ apps/api/v2/src/modules/auth/decorators/permissions/permissions.decorator.ts
@@ -2,4 +2,4 @@ import { Reflector } from "@nestjs/core";
 
 import { PERMISSIONS } from "@calcom/platform-constants";
 
-export const Permissions = Reflector.createDecorator<typeof PERMISSIONS>();
+export const Permissions = Reflector.createDecorator<(typeof PERMISSIONS)[number][]>();
