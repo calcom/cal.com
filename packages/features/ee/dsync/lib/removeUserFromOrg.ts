f69998bfe7bcb9db5db347aeb72664f2179f1b5b
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-03-05 23:05:41 -0500

    Removed await from `removeMember`

--- packages/features/ee/dsync/lib/removeUserFromOrg.ts
+++ packages/features/ee/dsync/lib/removeUserFromOrg.ts
@@ -1,7 +1,7 @@
 import removeMember from "@calcom/features/ee/teams/lib/removeMember";
 
 const removeUserFromOrg = async ({ userId, orgId }: { userId: number; orgId: number }) => {
-  await removeMember({
+  removeMember({
     memberId: userId,
     teamId: orgId,
     isOrg: true,
