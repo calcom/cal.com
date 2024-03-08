1959d51a9745cbd43d7d32c975cbfa0235a02e28
sean-brydon <sean@cal.com>
2024-03-08 11:05:13 +0000

    feat: gtm push signup event

---
+++ packages/lib/gtm.ts
@@ -0,0 +1,9 @@
+declare const window: Window & { dataLayer: Record<string, unknown>[] };
+
+export const pushGTMEvent = (event: string, data?: Record<string, unknown>) => {
+  window.dataLayer?.push({
+    event,
+    ...data,
+  });
+  console.log("PUSHED");
+};
