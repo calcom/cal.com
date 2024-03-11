ca4b525196eca450c14dd0356969c9035d6bb98b
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-03-11 19:40:58 +0530

    make onTimeZoneChange handler optional

--- packages/platform/atoms/hooks/useTimezone.ts
+++ packages/platform/atoms/hooks/useTimezone.ts
@@ -3,14 +3,14 @@ import { useEffect } from "react";
 import { useMe } from "./useMe";
 
 export const useTimezone = (
-  onTimeZoneChange: (currentTimezone: string) => void,
+  onTimeZoneChange?: (currentTimezone: string) => void,
   currentTimezone: string = new Intl.DateTimeFormat().resolvedOptions().timeZone
 ) => {
   const { data: me, isLoading } = useMe();
   const preferredTimezone = me?.data?.timeZone ?? currentTimezone;
 
   useEffect(() => {
-    if (!isLoading && preferredTimezone && preferredTimezone !== currentTimezone) {
+    if (!isLoading && preferredTimezone && onTimeZoneChange && preferredTimezone !== currentTimezone) {
       // TODO: figure out how to do patch request correctly in axios
       // onTimeZoneChange(currentTimezone);
     }
