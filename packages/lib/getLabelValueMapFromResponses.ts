6c8dd486a0ec5191e4755bd38c5f282c7de9ea4a
Udit Takkar <udit222001@gmail.com>
2024-03-12 12:14:36 +0530

    fix: type err

--- packages/lib/getLabelValueMapFromResponses.ts
+++ packages/lib/getLabelValueMapFromResponses.ts
@@ -8,12 +8,13 @@ export default function getLabelValueMapFromResponses(calEvent: CalendarEvent) {
   const { customInputs, userFieldsResponses, responses, eventTypeId } = calEvent;
 
   const isDynamicEvent = !eventTypeId;
-  if (!!responses[TITLE] && !isDynamicEvent) {
-    userFieldsResponses[TITLE] = responses[TITLE];
-  }
 
   let labelValueMap: Record<string, z.infer<typeof bookingResponse>> = {};
   if (userFieldsResponses) {
+    if (!!responses?.[TITLE] && !isDynamicEvent) {
+      userFieldsResponses[TITLE] = responses[TITLE];
+    }
+
     for (const [, value] of Object.entries(userFieldsResponses)) {
       if (!value.label) {
         continue;
