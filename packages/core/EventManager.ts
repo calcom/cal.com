ccab1101a60aeda2b36572f440e707ef26326d85
Nurudeen Soremekun <nurudeen.soremekun@gmail.com>
2024-03-15 14:26:18 +0100

    Make reschedule use update and not delete and create

--- packages/core/EventManager.ts
+++ packages/core/EventManager.ts
@@ -382,50 +382,42 @@ export default class EventManager {
       // As the reschedule requires confirmation, we can't update the events and meetings to new time yet. So, just delete them and let it be handled when organizer confirms the booking.
       await this.deleteEventsAndMeetings({ booking, event });
     } else {
-      // if (changedOrganizer) {
-      log.debug("RescheduleOrganizerChanged: Deleting Event and Meeting for previous booking");
-      await this.deleteEventsAndMeetings({ booking, event });
+      if (changedOrganizer) {
+        log.debug("RescheduleOrganizerChanged: Deleting Event and Meeting for previous booking");
+        await this.deleteEventsAndMeetings({ booking, event });
 
-      log.debug("RescheduleOrganizerChanged: Creating Event and Meeting for new booking");
+        log.debug("RescheduleOrganizerChanged: Creating Event and Meeting for new booking");
 
-      const newEvent = { ...evt, destinationCalendar: newDestinationCalendar };
-      const createdEvent = await this.create(newEvent);
-      results.push(...createdEvent.results);
-      bookingReferenceChangedOrganizer.push(...createdEvent.referencesToCreate);
-      // }
-      // else {
-      changedOrganizer = true;
-      // the update approach below here is replaced with the delete and create above because it is brittle
-      // for example, it doesn't correctly handle the case where while the organiser is the same,
-      // the location type has changed, say from a non video type to a video type
-      // while that case could be specially captured, it will still lead to many duplicate logic regarding hadling booking references etc
-      // so why not just use a single source of truth
+        const newEvent = { ...evt, destinationCalendar: newDestinationCalendar };
+        const createdEvent = await this.create(newEvent);
+        results.push(...createdEvent.results);
+        bookingReferenceChangedOrganizer.push(...createdEvent.referencesToCreate);
+      } else {
+        // If the reschedule doesn't require confirmation, we can "update" the events and meetings to new time.
+        const isDedicated = evt.location ? isDedicatedIntegration(evt.location) : null;
+        // If and only if event type is a dedicated meeting, update the dedicated video meeting.
+        if (isDedicated) {
+          const result = await this.updateVideoEvent(evt, booking);
+          const [updatedEvent] = Array.isArray(result.updatedEvent)
+            ? result.updatedEvent
+            : [result.updatedEvent];
 
-      //   // If the reschedule doesn't require confirmation, we can "update" the events and meetings to new time.
-      //   const isDedicated = evt.location ? isDedicatedIntegration(evt.location) : null;
-      //   // If and only if event type is a dedicated meeting, update the dedicated video meeting.
-      //   if (isDedicated) {
-      //     const result = await this.updateVideoEvent(evt, booking);
-      //     const [updatedEvent] = Array.isArray(result.updatedEvent)
-      //       ? result.updatedEvent
-      //       : [result.updatedEvent];
+          if (updatedEvent) {
+            evt.videoCallData = updatedEvent;
+            evt.location = updatedEvent.url;
+          }
+          results.push(result);
+        }
 
-      //     if (updatedEvent) {
-      //       evt.videoCallData = updatedEvent;
-      //       evt.location = updatedEvent.url;
-      //     }
-      //     results.push(result);
-      //   }
-
-      //   const bookingCalendarReference = booking.references.find((reference) =>
-      //     reference.type.includes("_calendar")
-      //   );
-      //   // There was a case that booking didn't had any reference and we don't want to throw error on function
-      //   if (bookingCalendarReference) {
-      //     // Update all calendar events.
-      //     results.push(...(await this.updateAllCalendarEvents(evt, booking, newBookingId)));
-      //   }
-      // }
+        const bookingCalendarReference = booking.references.find((reference) =>
+          reference.type.includes("_calendar")
+        );
+        // There was a case that booking didn't had any reference and we don't want to throw error on function
+        if (bookingCalendarReference) {
+          // Update all calendar events.
+          results.push(...(await this.updateAllCalendarEvents(evt, booking, newBookingId)));
+        }
+      }
     }
     const bookingPayment = booking?.payment;
 
