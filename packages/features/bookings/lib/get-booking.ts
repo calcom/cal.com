ece74cbc495adb606ca60d3ee59ff4746a17da95
Shobana Chandrasekaran <c.shobana10@gmail.com>
2024-03-18 11:53:53 -0700

    fix: dynamic event length change on reschedule (#14110)
    * fixed dynamic event length change on reschedule
    
    * fixed dynamic event length change on reschedule
    
    * Update get-booking.ts
    
    Type check: added endTime
    
    * Update get-booking.ts
    
    * Fix: typeCheck error
    
    * Revert yarn.lock to its state before unintended changes

--- packages/features/bookings/lib/get-booking.ts
+++ packages/features/bookings/lib/get-booking.ts
@@ -54,6 +54,7 @@ async function getBooking(prisma: PrismaClient, uid: string, isSeatedEvent?: boo
       id: true,
       uid: true,
       startTime: true,
+      endTime: true,
       description: true,
       customInputs: true,
       responses: true,
@@ -85,6 +86,7 @@ async function getBooking(prisma: PrismaClient, uid: string, isSeatedEvent?: boo
     // @NOTE: had to do this because Server side cant return [Object objects]
     // probably fixable with json.stringify -> json.parse
     booking["startTime"] = (booking?.startTime as Date)?.toISOString() as unknown as Date;
+    booking["endTime"] = (booking?.endTime as Date)?.toISOString() as unknown as Date;
   }
 
   return booking;
@@ -223,6 +225,7 @@ export const getBookingForSeatedEvent = async (uid: string) => {
       id: true,
       uid: true,
       startTime: true,
+      endTime: true,
       attendees: {
         select: {
           id: true,
@@ -254,6 +257,7 @@ export const getBookingForSeatedEvent = async (uid: string) => {
     ...booking,
     // @NOTE: had to do this because Server side cant return [Object objects]
     startTime: booking.startTime.toISOString() as unknown as Date,
+    endTime: booking.endTime.toISOString() as unknown as Date,
     description: null,
     customInputs: null,
     responses: {},
