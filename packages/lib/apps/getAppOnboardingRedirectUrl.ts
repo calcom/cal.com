0621d2959073021bd5b9917338e99b22f0f50084
Somay Chauhan <somaychauhan98@gmail.com>
2024-03-18 23:36:29 +0530

    Update getAppOnboardingRedirectUrl.ts

--- packages/lib/apps/getAppOnboardingRedirectUrl.ts
+++ packages/lib/apps/getAppOnboardingRedirectUrl.ts
@@ -5,11 +5,9 @@ import { getAppOnboardingUrl } from "./getAppOnboardingUrl";
 export const getAppOnboardingRedirectUrl = (slug: string, teamId?: number, eventTypeId?: number) => {
   let url = "";
   if (eventTypeId) {
-    url = getAppOnboardingUrl({ slug, eventTypeId, step: AppOnboardingSteps.CONFIGURE_STEP });
-  } else if (teamId) {
-    url = getAppOnboardingUrl({ slug, teamId, step: AppOnboardingSteps.EVENT_TYPES_STEP });
+    url = getAppOnboardingUrl({ slug, eventTypeId, teamId, step: AppOnboardingSteps.CONFIGURE_STEP });
   } else {
-    url = getAppOnboardingUrl({ slug, step: AppOnboardingSteps.EVENT_TYPES_STEP });
+    url = getAppOnboardingUrl({ slug, teamId, step: AppOnboardingSteps.EVENT_TYPES_STEP });
   }
   return encodeURIComponent(url);
 };
