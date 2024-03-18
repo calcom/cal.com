87992b4a2e8498b3dc52ca7981db3150258fd8b8
sean-brydon <sean@cal.com>
2024-03-18 14:34:12 +0000

    tests: add tests for migrating existing teams

--- apps/web/playwright/organization/organization-creation.e2e.ts
+++ apps/web/playwright/organization/organization-creation.e2e.ts
@@ -119,7 +119,7 @@ test.describe("Organization", () => {
       await page.locator('input[name="teams.1.name"]').fill("Sales");
 
       // Finishing the creation wizard
-      await page.locator('button:text("Continue")').click();
+      await page.getByTestId("continue_or_checkout").click();
       await page.waitForURL("/event-types");
     });
 
@@ -236,7 +236,7 @@ test.describe("Organization", () => {
       await page.locator('input[name="teams.1.name"]').fill("Sales");
 
       // Finishing the creation wizard
-      await page.locator('button:text("Checkout")').click();
+      await page.getByTestId("continue_or_checkout").click();
     });
 
     await test.step("Login as org owner and pay", async () => {
@@ -258,6 +258,157 @@ test.describe("Organization", () => {
       const upgradeButtonHidden = await page.getByTestId("upgrade_org_banner_button");
 
       await expect(upgradeButtonHidden).toBeHidden();
+    });
+  });
+
+  test("User gets prompted with >=3 teams to upgrade & can transfer existing teams to org", async ({
+    page,
+    users,
+  }) => {
+    const numberOfTeams = 3;
+    const stringUUID = uuid();
+
+    const orgOwnerUsername = `owner-${stringUUID}`;
+
+    const targetOrgEmail = users.trackEmail({
+      username: orgOwnerUsername,
+      domain: `example.com`,
+    });
+    const orgOwnerUser = await users.create(
+      {
+        username: orgOwnerUsername,
+        email: targetOrgEmail,
+      },
+      { hasTeam: true, numberOfTeams }
+    );
+
+    await orgOwnerUser.apiLogin();
+
+    await page.goto("/teams");
+
+    await test.step("Has org self serve banner", async () => {
+      // These values are infered due to an existing user being signed
+      const selfServeButtonLocator = await page.getByTestId("setup_your_org_action_button");
+      await expect(selfServeButtonLocator).toBeVisible();
+
+      await selfServeButtonLocator.click();
+      await page.waitForURL("/settings/organizations/new");
+    });
+
+    await test.step("Basic info", async () => {
+      // These values are infered due to an existing user being signed
+      const slugLocator = await page.locator("input[name=slug]");
+      expect(await page.locator("input[name=name]").inputValue()).toBe("Example");
+      expect(await slugLocator.inputValue()).toBe("example");
+
+      await slugLocator.fill(`example-${stringUUID}`);
+
+      await page.locator("button[type=submit]").click();
+      await page.waitForLoadState("networkidle");
+    });
+
+    await test.step("About the organization", async () => {
+      // Choosing an avatar
+      await page.locator('button:text("Upload")').click();
+      const fileChooserPromise = page.waitForEvent("filechooser");
+      await page.getByText("Choose a file...").click();
+      const fileChooser = await fileChooserPromise;
+      await fileChooser.setFiles(path.join(__dirname, "../../public/apple-touch-icon.png"));
+      await page.locator('button:text("Save")').click();
+
+      // About text
+      await page.locator('textarea[name="about"]').fill("This is a testing org");
+      await page.locator("button[type=submit]").click();
+
+      // Waiting to be in next step URL
+      await page.waitForURL("/settings/organizations/*/onboard-members");
+    });
+
+    await test.step("On-board administrators", async () => {
+      await page.waitForSelector('[data-testid="pending-member-list"]');
+      expect(await page.getByTestId("pending-member-item").count()).toBe(1);
+
+      const adminEmail = users.trackEmail({ username: "rick", domain: `example.com` });
+
+      //can add members
+      await page.getByTestId("new-member-button").click();
+      await page.locator('[placeholder="email\\@example\\.com"]').fill(adminEmail);
+      await page.getByTestId("invite-new-member-button").click();
+      await expect(await page.locator(`li:has-text("${adminEmail}")`)).toBeVisible();
+      // TODO: Check if invited admin received the invitation email
+      // await expectInvitationEmailToBeReceived(
+      //   page,
+      //   emails,
+      //   adminEmail,
+      //   `${orgName}'s admin invited you to join the organization ${orgName} on Cal.com`
+      // );
+      await expect(page.getByTestId("pending-member-item")).toHaveCount(2);
+
+      // can remove members
+      await expect(page.getByTestId("pending-member-item")).toHaveCount(2);
+      const lastRemoveMemberButton = page.getByTestId("remove-member-button").last();
+      await lastRemoveMemberButton.click();
+      await page.waitForLoadState("networkidle");
+      await expect(page.getByTestId("pending-member-item")).toHaveCount(1);
+      await page.getByTestId("publish-button").click();
+      // Waiting to be in next step URL
+      await page.waitForURL("/settings/organizations/*/add-teams");
+    });
+
+    await test.step("Move existing teams to org", async () => {
+      // No easy way to get all team checkboxes so we fill all checkboxes on the page in
+      const foundCheckboxes = await page.locator('input[type="checkbox"]').all();
+      for (const locator of foundCheckboxes) {
+        await locator.check();
+      }
+      // Update slugs of existing teams
+      for (const id of [0, 1, 2]) {
+        const locator = page.locator(`input[name="moveTeams.${id}.newSlug"]`);
+        await locator.fill(`migratedTeam-${id}`);
+      }
+    });
+
+    await test.step("Create teams", async () => {
+      // Filling one team
+      await page.locator('input[name="teams.0.name"]').fill("Marketing");
+
+      // Adding another team
+      await page.locator('button:text("Add a team")').click();
+      await page.locator('input[name="teams.1.name"]').fill("Sales");
+
+      // Finishing the creation wizard
+      await page.getByTestId("continue_or_checkout").click();
+    });
+
+    await test.step("Login as org owner and pay", async () => {
+      // eslint-disable-next-line playwright/no-skipped-test
+      test.skip(!IS_STRIPE_ENABLED, "Skipping paying for org as stripe is disabled");
+
+      // Check that stripe checkout is present
+      const expectedUrl = "https://checkout.stripe.com";
+
+      await page.waitForURL((url) => url.href.startsWith(expectedUrl));
+      const url = page.url();
+
+      // Check that the URL matches the expected URL
+      expect(url).toContain(expectedUrl);
+
+      await fillStripeTestCheckout(page);
+      await page.waitForLoadState("networkidle");
+
+      const upgradeButtonHidden = await page.getByTestId("upgrade_org_banner_button");
+
+      await expect(upgradeButtonHidden).toBeHidden();
+    });
+
+    await test.step("Ensure correctnumberOfTeams are migrated", async () => {
+      // eslint-disable-next-line playwright/no-skipped-test
+      await page.goto("/teams");
+      await page.waitForLoadState("networkidle");
+      const teamListItems = await page.getByTestId("team-list-item-link").all();
+
+      // Number of teams migrated + the two created in the create teams step
+      expect(teamListItems.length).toBe(numberOfTeams + 2);
     });
   });
 });
