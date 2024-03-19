ff7b0a00cc62ca711cea2843737a499898f48e06
exception <erik@erosemberg.com>
2024-03-19 09:47:50 -0300

    chore: temporarily disable redis

--- apps/api/v2/src/app.module.ts
+++ apps/api/v2/src/app.module.ts
@@ -1,14 +1,11 @@
 import { AppLoggerMiddleware } from "@/app.logger.middleware";
 import appConfig from "@/config/app";
-import { AppConfig } from "@/config/type";
 import { AuthModule } from "@/modules/auth/auth.module";
 import { EndpointsModule } from "@/modules/endpoints.module";
 import { JwtModule } from "@/modules/jwt/jwt.module";
 import { PrismaModule } from "@/modules/prisma/prisma.module";
 import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { ThrottlerModule, seconds } from "@nestjs/throttler";
-import { ThrottlerStorageRedisService } from "nestjs-throttler-storage-redis";
+import { ConfigModule } from "@nestjs/config";
 
 import { AppController } from "./app.controller";
 
@@ -19,20 +16,20 @@ import { AppController } from "./app.controller";
       isGlobal: true,
       load: [appConfig],
     }),
-    ThrottlerModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService<AppConfig>) => ({
-        throttlers: [
-          {
-            name: "short",
-            ttl: seconds(10),
-            limit: 3,
-          },
-        ],
-        storage: new ThrottlerStorageRedisService(config.get("db.redisUrl", { infer: true })),
-      }),
-    }),
+    // ThrottlerModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   inject: [ConfigService],
+    //   useFactory: (config: ConfigService<AppConfig>) => ({
+    //     throttlers: [
+    //       {
+    //         name: "short",
+    //         ttl: seconds(10),
+    //         limit: 3,
+    //       },
+    //     ],
+    //     storage: new ThrottlerStorageRedisService(config.get("db.redisUrl", { infer: true })),
+    //   }),
+    // }),
     PrismaModule,
     EndpointsModule,
     AuthModule,
