046a3a08405dbfe044356869a141c2e94f9e9bff
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-03-19 14:54:16 -0400

    Add WEBAPP_URL_FOR_OAUTH to salesforce auth

--- packages/app-store/salesforce/api/callback.ts
+++ packages/app-store/salesforce/api/callback.ts
@@ -1,7 +1,7 @@
 import jsforce from "jsforce";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import { WEBAPP_URL } from "@calcom/lib/constants";
+import { WEBAPP_URL_FOR_OAUTH } from "@calcom/lib/constants";
 import { getSafeRedirectUrl } from "@calcom/lib/getSafeRedirectUrl";
 
 import getAppKeysFromSlug from "../../_utils/getAppKeysFromSlug";
@@ -33,7 +33,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const conn = new jsforce.Connection({
     clientId: consumer_key,
     clientSecret: consumer_secret,
-    redirectUri: `${WEBAPP_URL}/api/integrations/salesforce/callback`,
+    redirectUri: `${WEBAPP_URL_FOR_OAUTH}/api/integrations/salesforce/callback`,
   });
 
   const salesforceTokenInfo = await conn.oauth2.requestToken(code as string);
