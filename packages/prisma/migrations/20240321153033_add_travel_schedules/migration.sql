fd70b67e69d3d835ec2a5bf0b845596ea78aa118
CarinaWolli <wollencarina@gmail.com>
2024-03-21 11:31:52 -0400

    add indexes

--- packages/prisma/migrations/20240321153033_add_travel_schedules/migration.sql
+++ packages/prisma/migrations/20240321153033_add_travel_schedules/migration.sql
@@ -10,5 +10,11 @@ CREATE TABLE "TravelSchedule" (
     CONSTRAINT "TravelSchedule_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateIndex
+CREATE INDEX "TravelSchedule_startDate_idx" ON "TravelSchedule"("startDate");
+
+-- CreateIndex
+CREATE INDEX "TravelSchedule_endDate_idx" ON "TravelSchedule"("endDate");
+
 -- AddForeignKey
 ALTER TABLE "TravelSchedule" ADD CONSTRAINT "TravelSchedule_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
