61a8543e08385955431bd4578c25ac0f69ab29c2
Anwar Sadath <37188392+asadath1395@users.noreply.github.com>
2024-03-21 20:46:59 +0530

    feat: select team members to receive emails (#13957)
    * Add UI to select members to receive emails in routing form
    
    * Add backend changes to support frontend to select team members to get routing form updates to
    
    * Fix type error
    
    * Fix all the members of all the teams the user is part of shown instead of only the current team where the routing form is created
    
    * Fix type error
    
    * Refactor components to a shared folder
    
    * fix type error
    
    * Fix review comment: only include emails
    
    * Remove listMembers trpc call in frontend and get the data in formQuery.handler
    
    * Address review comments
    
    * Remove unrelated changes
    
    * Update SingleForm.tsx
    
    ---------
    
    Co-authored-by: Carina Wollendorfer <30310907+CarinaWolli@users.noreply.github.com>
    Co-authored-by: Joe Au-Yeung <65426560+joeauyeung@users.noreply.github.com>

--- packages/app-store/routing-forms/lib/getSerializableForm.ts
+++ packages/app-store/routing-forms/lib/getSerializableForm.ts
@@ -4,7 +4,7 @@ import type { z } from "zod";
 import { entityPrismaWhereClause } from "@calcom/lib/entityPermissionUtils";
 import { RoutingFormSettings } from "@calcom/prisma/zod-utils";
 
-import type { SerializableForm } from "../types/types";
+import type { SerializableForm, SerializableFormTeamMembers } from "../types/types";
 import type { zodRoutesView, zodFieldsView } from "../zod";
 import { zodFields, zodRoutes } from "../zod";
 import getConnectedForms from "./getConnectedForms";
@@ -58,6 +58,25 @@ export async function getSerializableForm<TForm extends App_RoutingForms_Form>({
     description: f.description,
   }));
   const finalFields = fields;
+  let teamMembers: SerializableFormTeamMembers[] = [];
+  if (form.teamId) {
+    teamMembers = await prisma.user.findMany({
+      where: {
+        teams: {
+          some: {
+            teamId: form.teamId,
+            accepted: true,
+          },
+        },
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        avatarUrl: true,
+      },
+    });
+  }
 
   // Ideally we should't have needed to explicitly type it but due to some reason it's not working reliably with VSCode TypeCheck
   const serializableForm: SerializableForm<TForm> = {
@@ -67,6 +86,7 @@ export async function getSerializableForm<TForm extends App_RoutingForms_Form>({
     routes,
     routers,
     connectedForms,
+    teamMembers,
     createdAt: form.createdAt.toString(),
     updatedAt: form.updatedAt.toString(),
   };
