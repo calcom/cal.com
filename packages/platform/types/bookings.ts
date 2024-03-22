3e25834e0301846827c0157e6e8375b00e76cbf3
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-03-22 20:05:54 +0530

    typings for booking input

---
+++ packages/platform/types/bookings.ts
@@ -0,0 +1,42 @@
+import { Type } from "class-transformer";
+import { ValidateNested, IsNumber, Min, Max, IsOptional, IsArray, IsEnum } from "class-validator";
+
+enum Status {
+  upcoming = "upcoming",
+  recurring = "recurring",
+  past = "past",
+  cancelled = "cancelled",
+  unconfirmed = "unconfirmed",
+}
+
+class Filters {
+  @IsArray()
+  @Type(() => Number)
+  teamsIds?: number[];
+
+  @IsArray()
+  @Type(() => Number)
+  userIds?: number[];
+
+  @IsEnum(Status)
+  status!: Status;
+
+  @IsArray()
+  @Type(() => Number)
+  eventTypeIds?: number[];
+}
+
+export class GetBookingsInput {
+  @ValidateNested({ each: true })
+  filters!: Filters;
+
+  @IsNumber()
+  @Min(1)
+  @Max(100)
+  @IsOptional()
+  limit?: number;
+
+  @IsNumber()
+  @IsOptional()
+  cursor?: number | null;
+}
