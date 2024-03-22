cd97c09e1821dc6d3693b031e14a8978c6a2cb4e
Omar López <zomars@me.com>
2024-03-22 13:24:44 -0700

    Discard changes to packages/features/bookings/lib/handleWebhookTrigger.ts

--- packages/features/bookings/lib/handleWebhookTrigger.ts
+++ packages/features/bookings/lib/handleWebhookTrigger.ts
@@ -1,7 +1,7 @@
-import { TaskerFactory } from "@calcom/features/tasker/tasker-factory";
-import type { GetSubscriberOptions } from "@calcom/features/webhooks/lib/getWebhooks";
 import getWebhooks from "@calcom/features/webhooks/lib/getWebhooks";
+import type { GetSubscriberOptions } from "@calcom/features/webhooks/lib/getWebhooks";
 import type { WebhookDataType } from "@calcom/features/webhooks/lib/sendPayload";
+import sendPayload from "@calcom/features/webhooks/lib/sendPayload";
 import logger from "@calcom/lib/logger";
 
 export async function handleWebhookTrigger(args: {
@@ -11,22 +11,19 @@ export async function handleWebhookTrigger(args: {
 }) {
   try {
     const subscribers = await getWebhooks(args.subscriberOptions);
-    const taskerFactory = new TaskerFactory();
-    const tasker = taskerFactory.createTasker();
+
     const promises = subscribers.map((sub) =>
-      tasker.create(
-        "sendWebhook",
-        JSON.stringify({
-          secretKey: sub.secret,
-          triggerEvent: args.eventTrigger,
-          createdAt: new Date().toISOString(),
-          webhook: sub,
-          data: args.webhookData,
-        })
+      sendPayload(sub.secret, args.eventTrigger, new Date().toISOString(), sub, args.webhookData).catch(
+        (e) => {
+          console.error(
+            `Error executing webhook for event: ${args.eventTrigger}, URL: ${sub.subscriberUrl}`,
+            e
+          );
+        }
       )
     );
-    await Promise.allSettled(promises);
+    await Promise.all(promises);
   } catch (error) {
-    logger.error("Error while scheduling webhooks", error);
+    logger.error("Error while sending webhook", error);
   }
 }
