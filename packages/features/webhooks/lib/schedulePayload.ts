b863099afe029916e87b27eaaee84ecba3bb2672
zomars <zomars@me.com>
2024-03-22 13:24:47 -0700

    Create schedulePayload.ts

---
+++ packages/features/webhooks/lib/schedulePayload.ts
@@ -0,0 +1,18 @@
+import type { Webhook } from "@prisma/client";
+import { TaskerFactory } from "tasker/tasker-factory";
+
+import type { WebhookDataType } from "./sendPayload";
+
+const schedulePayload = async (
+  secretKey: string | null,
+  triggerEvent: string,
+  createdAt: string,
+  webhook: Pick<Webhook, "subscriberUrl" | "appId" | "payloadTemplate">,
+  data: Omit<WebhookDataType, "createdAt" | "triggerEvent">
+) => {
+  const taskerFactory = new TaskerFactory();
+  const tasker = taskerFactory.createTasker();
+  await tasker.create("sendWebhook", JSON.stringify({ secretKey, triggerEvent, createdAt, webhook, data }));
+};
+
+export default schedulePayload;
