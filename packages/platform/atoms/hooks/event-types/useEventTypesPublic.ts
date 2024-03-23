da1005f1601400d1e2cc9fe447c55d2c80e4f24a
supalarry <laurisskraucis@gmail.com>
2024-03-23 16:47:49 +0100

    feat: hooks for public and private events

---
+++ packages/platform/atoms/hooks/event-types/useEventTypesPublic.ts
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { BASE_URL, API_VERSION, V2_ENDPOINTS, SUCCESS_STATUS } from "@calcom/platform-constants";
+import type { EventTypesPublic } from "@calcom/platform-libraries";
+import type { ApiResponse } from "@calcom/platform-types";
+
+import http from "../../lib/http";
+
+export const QUERY_KEY = "get-private-events";
+export const useEventTypesPublic = (username: string) => {
+  const endpoint = new URL(BASE_URL);
+
+  endpoint.pathname = `api/${API_VERSION}/${V2_ENDPOINTS.eventTypes}/${username}/public`;
+
+  return useQuery({
+    queryKey: [QUERY_KEY],
+    queryFn: () => {
+      return http?.get<ApiResponse<EventTypesPublic>>(endpoint.toString()).then((res) => {
+        if (res.data.status === SUCCESS_STATUS) {
+          return res.data;
+        }
+        throw new Error(res.data.error.message);
+      });
+    },
+  });
+};
