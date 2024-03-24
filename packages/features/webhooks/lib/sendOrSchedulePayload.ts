0c43f1095cf662a5a58c315b5ac1a9e7f0543321
zomars <zomars@me.com>
2024-03-24 11:44:31 -0700

    Update sendOrSchedulePayload.ts

--- packages/features/webhooks/lib/sendOrSchedulePayload.ts
+++ packages/features/webhooks/lib/sendOrSchedulePayload.ts
@@ -1,7 +1,13 @@
 import schedulePayload from "./sendOrSchedulePayload";
 import sendPayload from "./sendPayload";
 
-const sendOrSchedulePayload = async (...args: Parameters<typeof sendPayload>) => {
+type SendOrSchedulePayload = (...args: Parameters<typeof sendPayload>) => Promise<{
+  message?: string | undefined;
+  ok: boolean;
+  status: number;
+}>;
+
+const sendOrSchedulePayload: SendOrSchedulePayload = async (...args) => {
   // If Tasker is enabled, schedule the payload instead of sending it immediately
   if (process.env.TASKER_ENABLE_WEBHOOKS === "1") return schedulePayload(...args);
   return sendPayload(...args);
