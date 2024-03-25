9d3b021b4c4d4e729335fdf0988cf4d91d243d38
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-03-25 14:33:19 +0530

    refactor calendar switch to check if is platform or not

--- packages/features/calendars/CalendarSwitch.tsx
+++ packages/features/calendars/CalendarSwitch.tsx
@@ -1,13 +1,13 @@
 import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
 
-import { classNames } from "@calcom/lib";
+import { useIsPlatform } from "@calcom/atoms/monorepo";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { trpc } from "@calcom/trpc/react";
-import { showToast, Switch } from "@calcom/ui";
-import { ArrowLeft, RotateCw } from "@calcom/ui/components/icon";
+import { showToast } from "@calcom/ui";
+import { CalendarSwitchComponent } from "@calcom/ui";
 
-interface ICalendarSwitchProps {
+export type ICalendarSwitchProps = {
   title: string;
   externalId: string;
   type: string;
@@ -16,10 +16,18 @@ interface ICalendarSwitchProps {
   isLastItemInList?: boolean;
   destination?: boolean;
   credentialId: number;
-}
+};
+
 const CalendarSwitch = (props: ICalendarSwitchProps) => {
-  const { title, externalId, type, isChecked, name, isLastItemInList = false, credentialId } = props;
+  const isPlatform = useIsPlatform();
+
+  return !isPlatform ? <WebCalendarSwitch {...props} /> : <></>;
+};
+
+const WebCalendarSwitch = (props: ICalendarSwitchProps) => {
+  const { isChecked, title, credentialId, type, externalId } = props;
   const [checkedInternal, setCheckedInternal] = useState(isChecked);
+
   const utils = trpc.useContext();
   const { t } = useLocale();
   const mutation = useMutation({
@@ -63,30 +71,20 @@ const CalendarSwitch = (props: ICalendarSwitchProps) => {
       showToast(`Something went wrong when toggling "${title}"`, "error");
     },
   });
+
   return (
-    <div className={classNames("my-2 flex flex-row items-center")}>
-      <div className="flex pl-2">
-        <Switch
-          id={externalId}
-          checked={checkedInternal}
-          disabled={mutation.isPending}
-          onCheckedChange={async (isOn: boolean) => {
-            setCheckedInternal(isOn);
-            await mutation.mutate({ isOn });
-          }}
-        />
-      </div>
-      <label className="ml-3 text-sm font-medium leading-5" htmlFor={externalId}>
-        {name}
-      </label>
-      {!!props.destination && (
-        <span className="bg-subtle text-default ml-8 inline-flex items-center gap-1 rounded-md px-2 py-1 text-sm font-normal sm:ml-4">
-          <ArrowLeft className="h-4 w-4" />
-          {t("adding_events_to")}
-        </span>
-      )}
-      {mutation.isPending && <RotateCw className="text-muted h-4 w-4 animate-spin ltr:ml-1 rtl:mr-1" />}
-    </div>
+    <CalendarSwitchComponent
+      {...props}
+      isChecked={checkedInternal}
+      isLoading={mutation.isPending}
+      onCheckedChange={async (isOn: boolean) => {
+        setCheckedInternal(isOn);
+        await mutation.mutate({ isOn });
+      }}
+      translations={{
+        spanText: t("adding_events_to"),
+      }}
+    />
   );
 };
 
