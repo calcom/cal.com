c315e81fb850cdcf22519b36901c8b56a7caaa17
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-03-25 20:01:57 +0530

    abstract call into its own separate fn

--- apps/web/modules/bookings/lib/getBookingInfo.ts
+++ apps/web/modules/bookings/lib/getBookingInfo.ts
@@ -1,59 +1,9 @@
 import { getBookingWithResponses } from "@calcom/features/bookings/lib/get-booking";
-import prisma from "@calcom/prisma";
+
+import getUserBooking from "~/bookings/lib/getUserBooking";
 
 const getBookingInfo = async (uid: string) => {
-  const bookingInfoRaw = await prisma.booking.findFirst({
-    where: {
-      uid: uid,
-    },
-    select: {
-      title: true,
-      id: true,
-      uid: true,
-      description: true,
-      customInputs: true,
-      smsReminderNumber: true,
-      recurringEventId: true,
-      startTime: true,
-      endTime: true,
-      location: true,
-      status: true,
-      metadata: true,
-      cancellationReason: true,
-      responses: true,
-      rejectionReason: true,
-      userPrimaryEmail: true,
-      user: {
-        select: {
-          id: true,
-          name: true,
-          email: true,
-          username: true,
-          timeZone: true,
-        },
-      },
-      attendees: {
-        select: {
-          name: true,
-          email: true,
-          timeZone: true,
-        },
-      },
-      eventTypeId: true,
-      eventType: {
-        select: {
-          eventName: true,
-          slug: true,
-          timeZone: true,
-        },
-      },
-      seatsReferences: {
-        select: {
-          referenceUid: true,
-        },
-      },
-    },
-  });
+  const bookingInfoRaw = await getUserBooking(uid);
 
   if (!bookingInfoRaw) {
     return { bookingInfoRaw: undefined, bookingInfo: undefined };
