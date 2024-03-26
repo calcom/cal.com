0223db1f7cdf5e3d9cc213c5b3a4ba8a4168e56e
Lauris Skraucis <lauris.skraucis@gmail.com>
2024-03-26 09:35:17 +0100

    feat: v2 api endpoints and hooks to list private and public event types of user (#14192)
    * refactor: extract getEventTypeByViewer for private user dashboard event types
    
    * refactor: extract getEventTypesPublic for public event types to book
    
    * refactor: group event-types lib functions in single folder
    
    * refactor: rename getEventTypeByViewer to getEventTypesByViewer
    
    * feat: add event type helpers from lib to platform libraries
    
    * feat: v2 endpoint for private event types
    
    * driveby:fix: fixed date in docs instead of being re-generated
    
    * feature: endpoint to fetch public endpoints
    
    * fix: getting private event types
    
    * tests: fetch private event types
    
    * tests: fetch private event types
    
    * feat: hooks for public and private events
    
    * fix: yarn test
    
    * tests: IMO better mocking
    
    * refactor: track dynamic prop in react query
    
    * fix: distinct query key for public events
    
    * fix: username in query key and example app demo
    
    * fix: swagger for endpoint get public event input
    
    ---------
    
    Co-authored-by: Morgan Vernay <morgan@cal.com>
    Co-authored-by: Morgan <33722304+ThyMinimalDev@users.noreply.github.com>

--- packages/platform/atoms/index.ts
+++ packages/platform/atoms/index.ts
@@ -5,3 +5,4 @@ export { BookerPlatformWrapper as Booker } from "./booker";
 export { useIsPlatform } from "./hooks/useIsPlatform";
 export { useAtomsContext } from "./hooks/useAtomsContext";
 export { useConnectedCalendars } from "./hooks/useConnectedCalendars";
+export { useEventTypesPublic } from "./hooks/event-types/useEventTypesPublic";
