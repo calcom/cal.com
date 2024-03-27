aaa4834f2fb08f8cb0aa516d1f72de2ab1f9dc4c
Amit91848 <samit91848@gmail.com>
2024-03-27 18:32:54 +0000

    fix: unit test

--- packages/prisma/zod-utils.ts
+++ packages/prisma/zod-utils.ts
@@ -616,6 +616,7 @@ export const allManagedEventTypeProps: { [k in keyof Omit<Prisma.EventTypeSelect
   seatsPerTimeSlot: true,
   seatsShowAttendees: true,
   seatsShowAvailabilityCount: true,
+  forwardParamsSuccessRedirect: true,
   periodType: true,
   hashedLink: true,
   webhooks: true,
