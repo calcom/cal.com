8903fc81d99c545b08375ae38b5c197315eee66c
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-03-28 19:33:10 +0530

    add styling to  examples app

--- packages/platform/examples/base/src/pages/bookings.tsx
+++ packages/platform/examples/base/src/pages/bookings.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { useRouter } from "next/router";
 
 import { useGetBookings } from "@calcom/atoms";
+import dayjs from "@calcom/dayjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,25 +30,45 @@ export default function Bookings(props: { calUsername: string; calEmail: string 
       {isLoading && <p>Loading...</p>}
       {!isLoading &&
         (Boolean(upcomingBookings?.bookings.length) || Boolean(pastBookings?.bookings.length)) &&
-        [...pastBookings?.bookings, ...upcomingBookings?.bookings].map((booking) => (
-          <div
-            key={booking.id}
-            className="my-2 w-[440px] cursor-pointer overflow-hidden rounded shadow-md"
-            onClick={() => {
-              router.push(`/${booking.uid}`);
-            }}>
-            <div className="px-6 py-4">
-              <div className="text-md mb-0.5 font-semibold">
-                <p>{booking.title}</p>
+        [...pastBookings?.bookings, ...upcomingBookings?.bookings].map((booking) => {
+          const date = dayjs(booking.startTime).toDate();
+          const startTime = dayjs(booking?.startTime).format(12 === 12 ? "h:mma" : "HH:mm");
+          const endTime = dayjs(booking?.endTime).format(12 === 12 ? "h:mma" : "HH:mm");
+          const day = dayjs(date).format("dddd");
+          const month = dayjs(date).format("MMMM");
+
+          return (
+            <div
+              key={booking.id}
+              className="mx-10 my-2 flex w-[80vw] cursor-pointer items-center justify-between overflow-hidden rounded border-[0.8px] border-black py-4"
+              onClick={() => {
+                router.push(`/${booking.uid}`);
+              }}>
+              <div>
+                <div className="px-6">{`${day}, ${dayjs(booking.startTime).date()} ${month}`}</div>
+                <div className="px-6">
+                  <p>
+                    {startTime} - {endTime}
+                  </p>{" "}
+                  <p />
+                </div>
+              </div>
+              <div>
+                <div className="px-6">
+                  <div className="text-md mb-0.5 font-semibold">
+                    <p>{booking.title}</p>
+                  </div>
+                </div>
+                <div className="px-6">
+                  <p>
+                    {booking.user.name} and {booking.attendees[0].name}
+                  </p>{" "}
+                  <p />
+                </div>
               </div>
             </div>
-            <div className="px-6 pb-2">
-              <p>{booking.startTime}</p>
-              <p>{booking.endTime}</p>
-              <p>{booking.status}</p>
-            </div>
-          </div>
-        ))}
+          );
+        })}
     </main>
   );
 }
