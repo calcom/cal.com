63e2daadfd2e2c1707cb2dbd7d9eb9587d20a393
supalarry <laurisskraucis@gmail.com>
2024-04-02 16:56:00 +0200

    docs: GET /event-types/:id response

--- apps/api/v2/src/ee/event-types/controllers/event-types.controller.ts
+++ apps/api/v2/src/ee/event-types/controllers/event-types.controller.ts
@@ -1,8 +1,8 @@
 import { CreateEventTypeInput } from "@/ee/event-types/inputs/create-event-type.input";
 import { UpdateEventTypeInput } from "@/ee/event-types/inputs/update-event-type.input";
 import { CreateEventTypeOutput } from "@/ee/event-types/outputs/create-event-type.output";
+import { GetEventTypeOutput } from "@/ee/event-types/outputs/get-event-type.output";
 import { EventTypesService } from "@/ee/event-types/services/event-types.service";
-import { ForAtom } from "@/lib/atoms/decorators/for-atom.decorator";
 import { GetUser } from "@/modules/auth/decorators/get-user/get-user.decorator";
 import { Permissions } from "@/modules/auth/decorators/permissions/permissions.decorator";
 import { AccessTokenGuard } from "@/modules/auth/guards/access-token/access-token.guard";
@@ -26,11 +26,7 @@ import { EventType } from "@prisma/client";
 
 import { EventTypesByViewer } from "@calcom/lib";
 import { EVENT_TYPE_READ, EVENT_TYPE_WRITE, SUCCESS_STATUS } from "@calcom/platform-constants";
-import type {
-  EventType as AtomEventType,
-  EventTypesPublic,
-  UpdateEventTypeReturn,
-} from "@calcom/platform-libraries";
+import type { EventTypesPublic, UpdateEventTypeReturn } from "@calcom/platform-libraries";
 import { getEventTypesByViewer } from "@calcom/platform-libraries";
 import { ApiResponse, ApiSuccessResponse } from "@calcom/platform-types";
 
@@ -63,12 +59,9 @@ export class EventTypesController {
   @UseGuards(AccessTokenGuard)
   async getEventType(
     @Param("eventTypeId") eventTypeId: string,
-    @ForAtom() forAtom: boolean,
     @GetUser() user: UserWithProfile
-  ): Promise<ApiSuccessResponse<EventType | AtomEventType>> {
-    const eventType = forAtom
-      ? await this.eventTypesService.getUserEventTypeForAtom(user, Number(eventTypeId))
-      : await this.eventTypesService.getUserEventType(user.id, Number(eventTypeId));
+  ): Promise<GetEventTypeOutput> {
+    const eventType = await this.eventTypesService.getUserEventTypeForAtom(user, Number(eventTypeId));
 
     if (!eventType) {
       throw new NotFoundException(`Event type with id ${eventTypeId} not found`);
