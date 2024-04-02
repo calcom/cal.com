3b263e92820334c947e8a93405dcc4c2b97f24bc
Lauris Skraucis <lauris.skraucis@gmail.com>
2024-04-02 15:50:12 +0200

    feat: v2 event-types POST, PATCH, DELETE endpoints (#14237)
    * create event type: use createEventType from trpc handler
    
    * create event type: simplify handler input and create e2e test
    
    * update event type + test
    
    * delete end point
    
    * check if owner owns event type
    
    * fix: starting v2
    
    * fixes
    
    * remove unused enum
    
    * fix TS issue for update input
    
    * docs refresh
    
    * docs
    
    * docs
    
    * simplify event types docs
    
    * docs refactor
    
    * fix: check if user already has event type with existing slug
    
    * refactor update into a service
    
    * fix tests
    
    * remove unused import
    
    * update create event link
    
    * refactor: create event handler process passed locations
    
    * move input out of folder
    
    * reuse eventtype location for inputs
    
    * docs: example POST event-type return
    
    ---------
    
    Co-authored-by: Rajiv Sahal <sahalrajiv-extc@atharvacoe.ac.in>
    Co-authored-by: Morgan <33722304+ThyMinimalDev@users.noreply.github.com>

--- apps/api/v2/src/ee/event-types/services/event-types.service.ts
+++ apps/api/v2/src/ee/event-types/services/event-types.service.ts
@@ -1,26 +1,73 @@
 import { DEFAULT_EVENT_TYPES } from "@/ee/event-types/constants/constants";
 import { EventTypesRepository } from "@/ee/event-types/event-types.repository";
 import { CreateEventTypeInput } from "@/ee/event-types/inputs/create-event-type.input";
+import { UpdateEventTypeInput } from "@/ee/event-types/inputs/update-event-type.input";
 import { MembershipsRepository } from "@/modules/memberships/memberships.repository";
+import { PrismaWriteService } from "@/modules/prisma/prisma-write.service";
+import { SelectedCalendarsRepository } from "@/modules/selected-calendars/selected-calendars.repository";
 import { UserWithProfile, UsersRepository } from "@/modules/users/users.repository";
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { BadRequestException, ForbiddenException, Injectable, NotFoundException } from "@nestjs/common";
 
+import { createEventType, updateEventType } from "@calcom/platform-libraries";
 import { getEventTypesPublic, EventTypesPublic } from "@calcom/platform-libraries";
+import { EventType } from "@calcom/prisma/client";
 
 @Injectable()
 export class EventTypesService {
   constructor(
     private readonly eventTypesRepository: EventTypesRepository,
     private readonly membershipsRepository: MembershipsRepository,
-    private readonly usersRepository: UsersRepository
+    private readonly usersRepository: UsersRepository,
+    private readonly selectedCalendarsRepository: SelectedCalendarsRepository,
+    private readonly dbWrite: PrismaWriteService
   ) {}
 
-  async createUserEventType(userId: number, body: CreateEventTypeInput) {
-    return this.eventTypesRepository.createUserEventType(userId, body);
+  async createUserEventType(user: UserWithProfile, body: CreateEventTypeInput) {
+    await this.checkCanCreateEventType(user.id, body);
+    const eventTypeUser = await this.getUserToCreateEvent(user);
+    const { eventType } = await createEventType({
+      input: body,
+      ctx: {
+        user: eventTypeUser,
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        prisma: this.dbWrite.prisma,
+      },
+    });
+    return eventType;
+  }
+
+  async checkCanCreateEventType(userId: number, body: CreateEventTypeInput) {
+    const existsWithSlug = await this.eventTypesRepository.getUserEventTypeBySlug(userId, body.slug);
+    if (existsWithSlug) {
+      throw new BadRequestException("User already has an event type with this slug.");
+    }
+  }
+
+  async getUserToCreateEvent(user: UserWithProfile) {
+    const organizationId = user.movedToProfile?.organizationId || user.organizationId;
+    const isOrgAdmin = organizationId
+      ? await this.membershipsRepository.isUserOrganizationAdmin(user.id, organizationId)
+      : false;
+    const profileId = user.movedToProfile?.id || null;
+    return {
+      id: user.id,
+      organizationId: user.organizationId,
+      organization: { isOrgAdmin },
+      profile: { id: profileId },
+      metadata: user.metadata,
+    };
   }
 
   async getUserEventType(userId: number, eventTypeId: number) {
-    return this.eventTypesRepository.getUserEventType(userId, eventTypeId);
+    const eventType = await this.eventTypesRepository.getUserEventType(userId, eventTypeId);
+
+    if (!eventType) {
+      return null;
+    }
+
+    this.checkUserOwnsEventType(userId, eventType);
+    return eventType;
   }
 
   async getUserEventTypeForAtom(user: UserWithProfile, eventTypeId: number) {
@@ -30,7 +77,18 @@ export class EventTypesService {
       ? await this.membershipsRepository.isUserOrganizationAdmin(user.id, organizationId)
       : false;
 
-    return this.eventTypesRepository.getUserEventTypeForAtom(user, isUserOrganizationAdmin, eventTypeId);
+    const eventType = await this.eventTypesRepository.getUserEventTypeForAtom(
+      user,
+      isUserOrganizationAdmin,
+      eventTypeId
+    );
+
+    if (!eventType) {
+      return null;
+    }
+
+    this.checkUserOwnsEventType(user.id, eventType.eventType);
+    return eventType;
   }
 
   async getEventTypesPublicByUsername(username: string): Promise<EventTypesPublic> {
@@ -57,5 +115,52 @@ export class EventTypesService {
     ]);
 
     return defaultEventTypes;
+  }
+
+  async updateEventType(eventTypeId: number, body: UpdateEventTypeInput, user: UserWithProfile) {
+    this.checkCanUpdateEventType(user.id, eventTypeId);
+    const eventTypeUser = await this.getUserToUpdateEvent(user);
+    const { eventType } = await updateEventType({
+      input: { id: eventTypeId, ...body },
+      ctx: {
+        user: eventTypeUser,
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        prisma: this.dbWrite.prisma,
+      },
+    });
+
+    return eventType;
+  }
+
+  async checkCanUpdateEventType(userId: number, eventTypeId: number) {
+    const existingEventType = await this.getUserEventType(userId, eventTypeId);
+    if (!existingEventType) {
+      throw new NotFoundException(`Event type with id ${eventTypeId} not found`);
+    }
+    this.checkUserOwnsEventType(userId, existingEventType);
+  }
+
+  async getUserToUpdateEvent(user: UserWithProfile) {
+    const profileId = user.movedToProfile?.id || null;
+    const selectedCalendars = await this.selectedCalendarsRepository.getUserSelectedCalendars(user.id);
+    return { ...user, profile: { id: profileId }, selectedCalendars };
+  }
+
+  async deleteEventType(eventTypeId: number, userId: number) {
+    const existingEventType = await this.eventTypesRepository.getEventTypeById(eventTypeId);
+    if (!existingEventType) {
+      throw new NotFoundException(`Event type with ID=${eventTypeId} does not exist.`);
+    }
+
+    this.checkUserOwnsEventType(userId, existingEventType);
+
+    return this.eventTypesRepository.deleteEventType(eventTypeId);
+  }
+
+  checkUserOwnsEventType(userId: number, eventType: Pick<EventType, "id" | "userId">) {
+    if (userId !== eventType.userId) {
+      throw new ForbiddenException(`User with ID=${userId} does not own event type with ID=${eventType.id}`);
+    }
   }
 }
