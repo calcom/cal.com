9a9ea8154392ba96cc985143ac9d5ff4a79e6fca
zomars <zomars@me.com>
2024-04-02 17:07:45 -0700

    Testing something

--- packages/ui/components/icon/LazyIcon.tsx
+++ packages/ui/components/icon/LazyIcon.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 // find all lucide icons at https://lucide.dev/
 // github https://github.com/lucide-icons/lucide
 import type { LucideProps } from "lucide-react";
-import dynamicIconImports from "lucide-react/dynamicIconImports";
-import dynamic from "next/dynamic";
 import { memo } from "react";
+
+import { dynamicIconImports } from "./dynamicIconImports";
 
 export type IconName = keyof typeof dynamicIconImports;
 
@@ -11,9 +13,15 @@ interface IconProps extends Omit<LucideProps, "ref"> {
   name: IconName;
 }
 
-const LazyIcon = memo(({ name, ...props }: IconProps) => {
-  const LucideIcon = dynamic(dynamicIconImports[name]);
+const fallback = <div className="bg-emphasis h-4 w-4 animate-pulse rounded-lg" />;
 
+const LazyIcon = memo(({ name, ...props }: IconProps) => {
+  const LucideIcon = dynamicIconImports[name];
+
+  // This should never happen, but just in case
+  if (!LucideIcon) return fallback;
+
+  // @ts-expect-error - LucideIcon is a lazy loaded component
   return <LucideIcon {...props} />;
 });
 
