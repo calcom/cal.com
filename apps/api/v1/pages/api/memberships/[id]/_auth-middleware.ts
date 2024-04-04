467cfa28c013610368b4b198ae0d3859c4e0638b
Musyonchez <musyonchez@gmail.com>
2024-04-04 23:49:56 +0300

    Refactor: Add block braces to if statements in authMiddleware function for improved readability and error handling

--- apps/api/v1/pages/api/memberships/[id]/_auth-middleware.ts
+++ apps/api/v1/pages/api/memberships/[id]/_auth-middleware.ts
@@ -9,10 +9,14 @@ async function authMiddleware(req: NextApiRequest) {
   const { userId, isAdmin } = req;
   const { teamId } = membershipIdSchema.parse(req.query);
   // Admins can just skip this check
-  if (isAdmin) return;
+  if (isAdmin) {
+    return;
+  }
   // Only team members can modify a membership
   const membership = await prisma.membership.findFirst({ where: { userId, teamId } });
-  if (!membership) throw new HttpError({ statusCode: 403, message: "Forbidden" });
+  if (!membership) {
+    throw new HttpError({ statusCode: 403, message: "Forbidden" });
+  }
 }
 
 export default authMiddleware;
