3d5a6f56bdc7aad630bff0ce0e7d58e82f12d529
Musyonchez <musyonchez@gmail.com>
2024-04-04 22:30:56 +0300

    Refactor: Add block braces to if statements in authMiddleware function for improved readability and error handling

--- apps/api/v1/pages/api/booking-references/[id]/_auth-middleware.ts
+++ apps/api/v1/pages/api/booking-references/[id]/_auth-middleware.ts
@@ -9,12 +9,16 @@ async function authMiddleware(req: NextApiRequest) {
   const { userId, isAdmin } = req;
   const { id } = schemaQueryIdParseInt.parse(req.query);
   // Here we make sure to only return references of the user's own bookings if the user is not an admin.
-  if (isAdmin) return;
+  if (isAdmin) {
+    return;
+  }
   // Find all references where the user has bookings
   const bookingReference = await prisma.bookingReference.findFirst({
     where: { id, booking: { userId } },
   });
-  if (!bookingReference) throw new HttpError({ statusCode: 403, message: "Forbidden" });
+  if (!bookingReference) {
+    throw new HttpError({ statusCode: 403, message: "Forbidden" });
+  }
 }
 
 export default authMiddleware;
