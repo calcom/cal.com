6537981ddaa6172d32c7ead4b15be4d22c8d758c
Musyonchez <musyonchez@gmail.com>
2024-04-04 21:36:51 +0300

    Refactor: Inline immediately returned variable in verifyParseKey function for code simplification

--- apps/ai/src/utils/verifyParseKey.ts
+++ apps/ai/src/utils/verifyParseKey.ts
@@ -7,7 +7,5 @@ import { env } from "../env.mjs";
  * env.PARSE_KEY must be configured as a query param in the sendgrid inbound parse settings.
  */
 export const verifyParseKey = (url: NextRequest["url"]) => {
-  const verified = new URL(url).searchParams.get("parseKey") === env.PARSE_KEY;
-
-  return verified;
+  return new URL(url).searchParams.get("parseKey") === env.PARSE_KEY;
 };
