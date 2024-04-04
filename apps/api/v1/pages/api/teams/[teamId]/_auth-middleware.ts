247b081da52e067bc143b6ddb4526a9cb310ce8b
Musyonchez <musyonchez@gmail.com>
2024-04-05 00:43:16 +0300

    Refactor: Add block braces to if statements in teams/teamId/_auth-middleware.ts page for improved readability and error handling

--- apps/api/v1/pages/api/teams/[teamId]/_auth-middleware.ts
+++ apps/api/v1/pages/api/teams/[teamId]/_auth-middleware.ts
@@ -11,7 +11,9 @@ async function authMiddleware(req: NextApiRequest) {
   const { userId, isAdmin } = req;
   const { teamId } = schemaQueryTeamId.parse(req.query);
   /** Admins can skip the ownership verification */
-  if (isAdmin) return;
+  if (isAdmin) {
+    return;
+  }
   /** Non-members will see a 404 error which may or not be the desired behavior. */
   await prisma.team.findFirstOrThrow({
     where: { id: teamId, members: { some: { userId } } },
