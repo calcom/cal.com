94280ef74d6812dcaee07e197aef1fdfb62ae129
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-04-09 15:39:08 -0400

    Salesforce enable rr lead skip

--- packages/app-store/salesforce/components/EventTypeAppCardInterface.tsx
+++ packages/app-store/salesforce/components/EventTypeAppCardInterface.tsx
@@ -1,14 +1,21 @@
 import { usePathname } from "next/navigation";
 
+import { useAppContextWithSchema } from "@calcom/app-store/EventTypeAppContext";
 import AppCard from "@calcom/app-store/_components/AppCard";
 import useIsAppEnabled from "@calcom/app-store/_utils/useIsAppEnabled";
 import type { EventTypeAppCardComponent } from "@calcom/app-store/types";
 import { WEBAPP_URL } from "@calcom/lib/constants";
+import { SchedulingType } from "@calcom/prisma/enums";
+import { Switch } from "@calcom/ui";
+
+import type { appDataSchema } from "../zod";
 
 const EventTypeAppCard: EventTypeAppCardComponent = function EventTypeAppCard({ app, eventType }) {
   const pathname = usePathname();
 
+  const { getAppData, setAppData, disabled } = useAppContextWithSchema<typeof appDataSchema>();
   const { enabled, updateEnabled } = useIsAppEnabled(app);
+  const isRoundRobinLeadSkipEnabled = getAppData("roundRobinLeadSkip");
 
   return (
     <AppCard
@@ -19,7 +26,18 @@ const EventTypeAppCard: EventTypeAppCardComponent = function EventTypeAppCard({ 
         updateEnabled(e);
       }}
       switchChecked={enabled}
-    />
+      hideSettingsIcon>
+      <>
+        {eventType.schedulingType === SchedulingType.ROUND_ROBIN ? (
+          <Switch
+            label="Skip round robin assignment if contact exists in Salesforce"
+            labelOnLeading
+            checked={isRoundRobinLeadSkipEnabled}
+            onCheckedChange={(checked) => setAppData("roundRobinLeadSkip", checked)}
+          />
+        ) : null}
+      </>
+    </AppCard>
   );
 };
 
--- packages/app-store/salesforce/zod.ts
+++ packages/app-store/salesforce/zod.ts
@@ -2,7 +2,9 @@ import { z } from "zod";
 
 import { eventTypeAppCardZod } from "../eventTypeAppCardZod";
 
-export const appDataSchema = eventTypeAppCardZod;
+export const appDataSchema = eventTypeAppCardZod.extend({
+  roundRobinLeadSkip: z.boolean().optional(),
+});
 
 export const appKeysSchema = z.object({
   consumer_key: z.string().min(1),
