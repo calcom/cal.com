161000d1d425dac68ce6474fc7948fd3b5ec1697
Morgan Vernay <morgan@cal.com>
2024-04-10 10:38:55 +0300

    fixup! fixup! feat: enable platform emails

--- packages/lib/formatCalendarEvent.ts
+++ packages/lib/formatCalendarEvent.ts
@@ -1,22 +1,27 @@
+// eslint-disable-next-line no-restricted-imports
+import { cloneDeep } from "lodash";
+
 import type { CalendarEvent } from "@calcom/types/Calendar";
 
-// formatCalEvent to remove platformClientId from email addresses
-export const formatCalEvent = (calEvent: CalendarEvent) => {
-  const attendees = calEvent.platformClientId
-    ? calEvent.attendees.map((attendee) => ({
-        ...attendee,
-        email: attendee.email.replace(`+${calEvent.platformClientId}`, ""),
-      }))
-    : calEvent.attendees;
-  const organizer = calEvent.platformClientId
-    ? {
-        ...calEvent.organizer,
-        email: calEvent.organizer.email.replace(`+${calEvent.platformClientId}`, ""),
-      }
-    : calEvent.organizer;
-  return {
-    ...calEvent,
-    attendees,
-    organizer,
+// format CalEvent to remove platformClientId from email addresses
+const formatClientIdFromEmails = (calEvent: CalendarEvent, clientId: string) => {
+  const attendees = calEvent.attendees.map((attendee) => ({
+    ...attendee,
+    email: attendee.email.replace(`+${clientId}`, ""),
+  }));
+  const organizer = {
+    ...calEvent.organizer,
+    email: calEvent.organizer.email.replace(`+${clientId}`, ""),
   };
+  return [attendees, organizer];
+};
+
+export const formatCalEvent = (calEvent: CalendarEvent) => {
+  const clonedEvent = cloneDeep(calEvent);
+  if (clonedEvent.platformClientId) {
+    const [attendees, organizer] = formatClientIdFromEmails(clonedEvent, clonedEvent.platformClientId);
+    Object.assign(clonedEvent, { attendees, organizer });
+  }
+
+  return clonedEvent;
 };
