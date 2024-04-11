0c67f4940a5ad3683e64623785d6217b7d9a2b2d
Somay Chauhan <somaychauhan98@gmail.com>
2024-04-12 00:07:01 +0530

    Fix: Redirect only those apps that extend 'EventType' to the new app install flow (#14527)

--- packages/lib/apps/shouldRedirectToAppOnboarding.ts
+++ packages/lib/apps/shouldRedirectToAppOnboarding.ts
@@ -1,9 +1,6 @@
 import type { AppMeta } from "@calcom/types/App";
 
 export const shouldRedirectToAppOnboarding = (appMetadata: AppMeta) => {
-  // const appMetadata = appStoreMetadata[dirName as keyof typeof appStoreMetadata];
   const hasEventTypes = appMetadata?.extendsFeature == "EventType";
-  const isOAuth = appMetadata?.isOAuth;
-  const isCalendar = appMetadata?.category === "calendar";
-  return !isCalendar && (hasEventTypes || isOAuth);
+  return hasEventTypes;
 };
