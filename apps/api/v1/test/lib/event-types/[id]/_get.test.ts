e49657ff97e677745ed087a8efc2de12798b4d61
Keith Williams <keithwillcode@gmail.com>
2024-04-11 13:53:22 -0300

    Added test for checking user in team

--- apps/api/v1/test/lib/event-types/[id]/_get.test.ts
+++ apps/api/v1/test/lib/event-types/[id]/_get.test.ts
@@ -7,6 +7,7 @@ import { v4 as uuidv4 } from "uuid";
 import { describe, expect, test } from "vitest";
 
 import { buildEventType } from "@calcom/lib/test/builder";
+import { MembershipRole } from "@calcom/prisma/enums";
 
 import handler from "../../../../pages/api/event-types/[id]/_get";
 
@@ -17,7 +18,7 @@ describe("GET /api/event-types/[id]", () => {
   describe("Errors", () => {
     test("Returns 403 if user not admin/team member/event owner", async () => {
       const { req, res } = createMocks<CustomNextApiRequest, CustomNextApiResponse>({
-        method: "POST",
+        method: "GET",
         body: {},
         query: {
           id: 123456,
@@ -39,17 +40,18 @@ describe("GET /api/event-types/[id]", () => {
 
   describe("Success", async () => {
     test("Returns event type if user is admin", async () => {
+      const eventTypeId = 123456;
       const { req, res } = createMocks<CustomNextApiRequest, CustomNextApiResponse>({
-        method: "POST",
+        method: "GET",
         body: {},
         query: {
-          id: 123456,
+          id: eventTypeId,
         },
       });
 
       prismaMock.eventType.findUnique.mockResolvedValue(
         buildEventType({
-          id: 123456,
+          id: eventTypeId,
         })
       );
 
@@ -58,8 +60,56 @@ describe("GET /api/event-types/[id]", () => {
       await handler(req, res);
 
       expect(res.statusCode).toBe(200);
-      const responseData = JSON.parse(res._getData());
-      expect(responseData.event_type.id).toEqual(123456);
+      expect(JSON.parse(res._getData()).event_type.id).toEqual(eventTypeId);
+    });
+
+    test("Returns event type if user is in team associated with event type", async () => {
+      const eventTypeId = 123456;
+      const teamId = 9999;
+      const userId = uuidv4();
+      const { req, res } = createMocks<CustomNextApiRequest, CustomNextApiResponse>({
+        method: "GET",
+        body: {},
+        query: {
+          id: eventTypeId,
+        },
+      });
+
+      prismaMock.eventType.findUnique.mockResolvedValue(
+        buildEventType({
+          id: eventTypeId,
+          teamId,
+        })
+      );
+
+      prismaMock.team.findFirst.mockResolvedValue({
+        id: teamId,
+        members: [
+          {
+            userId,
+          },
+        ],
+      });
+
+      req.isAdmin = false;
+      req.userId = uuidv4();
+      await handler(req, res);
+
+      expect(res.statusCode).toBe(200);
+      expect(JSON.parse(res._getData()).event_type.id).toEqual(eventTypeId);
+      expect(prismaMock.team.findFirst).toHaveBeenCalledWith({
+        where: {
+          id: teamId,
+          members: {
+            some: {
+              userId: req.userId,
+              role: {
+                in: [MembershipRole.OWNER, MembershipRole.ADMIN, MembershipRole.MEMBER],
+              },
+            },
+          },
+        },
+      });
     });
   });
 });
