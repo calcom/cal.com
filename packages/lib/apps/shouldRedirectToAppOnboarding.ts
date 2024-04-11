711aceba0e99482ca91ad010d2d50e352a4fc735
Erik <erik@erosemberg.com>
2024-04-11 16:09:13 -0300

    Revert "Fix: Redirect only those apps that extend 'EventType' to the new app …" (#14539)
    This reverts commit 0c67f4940a5ad3683e64623785d6217b7d9a2b2d.

--- packages/lib/apps/shouldRedirectToAppOnboarding.ts
+++ packages/lib/apps/shouldRedirectToAppOnboarding.ts
@@ -1,6 +1,9 @@
 import type { AppMeta } from "@calcom/types/App";
 
 export const shouldRedirectToAppOnboarding = (appMetadata: AppMeta) => {
+  // const appMetadata = appStoreMetadata[dirName as keyof typeof appStoreMetadata];
   const hasEventTypes = appMetadata?.extendsFeature == "EventType";
-  return hasEventTypes;
+  const isOAuth = appMetadata?.isOAuth;
+  const isCalendar = appMetadata?.category === "calendar";
+  return !isCalendar && (hasEventTypes || isOAuth);
 };
