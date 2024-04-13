639fae207fd1f6aea4a4f11c326cbc02a19e5dcc
Keith Williams <keithwillcode@gmail.com>
2024-04-13 12:42:39 -0300

    fix: API v1 maintenance mode

---
+++ apps/api/v1/lib/helpers/checkIsInMaintenanceMode.ts
@@ -0,0 +1,20 @@
+import type { NextMiddleware } from "next-api-middleware";
+
+const safeGet = async <T = unknown>(key: string): Promise<T | undefined> => {
+  try {
+    return get<T>(key);
+  } catch (error) {
+    // Don't crash if EDGE_CONFIG env var is missing
+  }
+};
+
+export const config = { matcher: "/:path*" };
+
+export const checkIsInMaintenanceMode: NextMiddleware = async (req, res, next) => {
+  const isInMaintenanceMode = await safeGet<boolean>("isInMaintenanceMode");
+  if (!isInMaintenanceMode) return await next();
+  // If is in maintenance mode, return a 503 status code
+  return res
+    .status(503)
+    .json({ message: "API is currently under maintenance. Please try again at a later time." });
+};
