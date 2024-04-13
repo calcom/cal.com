79f00720a4147bd48587f3e38f702bfd60f466cc
Keith Williams <keithwillcode@gmail.com>
2024-04-13 16:32:20 -0300

    Using table aliases

--- packages/lib/event-types/getEventTypesPublic.ts
+++ packages/lib/event-types/getEventTypesPublic.ts
@@ -24,14 +24,14 @@ const getEventTypesWithHiddenFromDB = async (userId: number) => {
   const eventTypes = await prisma.$queryRaw`
     SELECT "t"."id", "t"."title", "t"."description", "t"."length", "t"."schedulingType"::text, "t"."recurringEvent", "t"."slug", "t"."hidden", "t"."price", "t"."currency", "t"."lockTimeZoneToggleOnBookingPage", "t"."requiresConfirmation", "t"."requiresBookerEmailVerification", "t"."metadata", "t"."position"
     FROM (
-      SELECT "public"."EventType"."id", "public"."EventType"."title", "public"."EventType"."description", "public"."EventType"."length", "public"."EventType"."schedulingType"::text, "public"."EventType"."recurringEvent", "public"."EventType"."slug", "public"."EventType"."hidden", "public"."EventType"."price", "public"."EventType"."currency", "public"."EventType"."lockTimeZoneToggleOnBookingPage", "public"."EventType"."requiresConfirmation", "public"."EventType"."requiresBookerEmailVerification", "public"."EventType"."metadata", "public"."EventType"."position"
-      FROM "public"."EventType"
-      WHERE "public"."EventType"."teamId" IS NULL AND "public"."EventType"."userId" = ${userId}
+      SELECT "et1"."id", "et1"."title", "et1"."description", "et1"."length", "et1"."schedulingType"::text, "et1"."recurringEvent", "et1"."slug", "et1"."hidden", "et1"."price", "et1"."currency", "et1"."lockTimeZoneToggleOnBookingPage", "et1"."requiresConfirmation", "et1"."requiresBookerEmailVerification", "et1"."metadata", "et1"."position"
+      FROM "public"."EventType" "et1"
+      WHERE "et1"."teamId" IS NULL AND "et1"."userId" = ${userId}
       UNION
-      SELECT "public"."EventType"."id", "public"."EventType"."title", "public"."EventType"."description", "public"."EventType"."length", "public"."EventType"."schedulingType"::text, "public"."EventType"."recurringEvent", "public"."EventType"."slug", "public"."EventType"."hidden", "public"."EventType"."price", "public"."EventType"."currency", "public"."EventType"."lockTimeZoneToggleOnBookingPage", "public"."EventType"."requiresConfirmation", "public"."EventType"."requiresBookerEmailVerification", "public"."EventType"."metadata", "public"."EventType"."position"
-      FROM "public"."EventType"
-      INNER JOIN "public"."_user_eventtype" "uet" on "uet"."A" = "public"."EventType"."id"
-      WHERE "uet"."B" = ${userId} AND "public"."EventType"."teamId" IS NULL
+      SELECT "et2"."id", "et2"."title", "et2"."description", "et2"."length", "et2"."schedulingType"::text, "et2"."recurringEvent", "et2"."slug", "et2"."hidden", "et2"."price", "et2"."currency", "et2"."lockTimeZoneToggleOnBookingPage", "et2"."requiresConfirmation", "et2"."requiresBookerEmailVerification", "et2"."metadata", "et2"."position"
+      FROM "public"."EventType" "et2"
+      INNER JOIN "public"."_user_eventtype" "uet" on "uet"."A" = "et2"."id"
+      WHERE "uet"."B" = ${userId} AND "et2"."teamId" IS NULL
     ) t
     ORDER BY "t"."position" DESC, "t"."id" ASC
   `;
