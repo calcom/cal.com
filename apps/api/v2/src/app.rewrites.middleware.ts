9b4928fb98c8f8885bc64cd8c8e6aa1f2d8c0479
Morgan Vernay <morgan@cal.com>
2024-04-13 13:51:21 +0300

    fixup! chore: remove /ee from apiv2 urls

--- apps/api/v2/src/app.rewrites.middleware.ts
+++ apps/api/v2/src/app.rewrites.middleware.ts
@@ -4,16 +4,12 @@ import { Request, Response } from "express";
 @Injectable()
 export class RewriterMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: () => void) {
-    console.log("REWIRTE MIDDLEWARE", req.url);
     if (req.url.startsWith("/api/v2")) {
-      console.log("REWRITING URL");
       req.url = req.url.replace("/api/v2", "/v2");
     }
     if (req.url.startsWith("/v2/ee")) {
-      console.log("REWRITING URL 2 ", req.url);
       req.url = req.url.replace("/v2/ee", "/v2");
     }
-    console.log("REWRITING DONE ", req.url);
     next();
   }
 }
