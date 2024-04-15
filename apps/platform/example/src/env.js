05198e37467c45172d786bd5eef3344a85eb8414
Richard Poelderl <richard.poelderl@gmail.com>
2024-04-15 04:07:48 +0200

    env: remove discord

--- apps/platform/example/src/env.js
+++ apps/platform/example/src/env.js
@@ -33,8 +33,6 @@ export const env = createEnv({
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
       process.env.VERCEL ? z.string() : z.string().url(),
     ),
-    DISCORD_CLIENT_ID: z.string(),
-    DISCORD_CLIENT_SECRET: z.string(),
 
     /**
      * [@calcom] These are the server environment variables to make our atoms work:
@@ -73,8 +71,6 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
     /** [@calcom] Make sure to add the calcom variables to your runtime environment variables, so that you can use them */
     CAL_SECRET: process.env.CAL_SECRET,
     NEXT_PUBLIC_CAL_API_URL: process.env.NEXT_PUBLIC_CAL_API_URL,
