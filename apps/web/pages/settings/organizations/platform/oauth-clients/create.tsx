71ce405b9b0c99e4d2e38132775bcb5cf0974c35
Morgan Vernay <morgan@cal.com>
2024-04-16 15:49:10 +0300

    fixup! Merge branch 'fix-no-email-booking-hide-empty-links' of github.com:calcom/cal.com into fix-no-email-booking-hide-empty-links

--- apps/web/pages/settings/organizations/platform/oauth-clients/create.tsx
+++ apps/web/pages/settings/organizations/platform/oauth-clients/create.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 import { getLayout } from "@calcom/features/settings/layouts/SettingsLayout";
