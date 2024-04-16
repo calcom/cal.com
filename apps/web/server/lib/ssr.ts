33b9e8f48c8477a274d946123cf661e30143c57b
Somay Chauhan <somaychauhan98@gmail.com>
2024-04-16 13:17:09 +0530

    fix: custom attributes not being synced in intercom

--- apps/web/server/lib/ssr.ts
+++ apps/web/server/lib/ssr.ts
@@ -81,6 +81,7 @@ export async function ssrInit(context: GetServerSidePropsContext, options?: { no
     // Provides a better UX to the users who have already upgraded.
     ssr.viewer.teams.hasTeamPlan.prefetch(),
     ssr.viewer.public.session.prefetch(),
+    ssr.viewer.loggedInRouter.me.prefetch(),
   ]);
 
   return ssr;
