58fb1fde6befb8a1dcb5373f54ed6b5d427d8f55
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-04-16 15:28:35 -0400

    Only create the event on the first destination calendar

---
+++ packages/app-store/_utils/calendars/processExternalId.ts
@@ -0,0 +1,18 @@
+import type { DestinationCalendar } from "@prisma/client";
+
+import { metadata as OutlookMetadata } from "../../office365calendar";
+
+/**
+ * When inviting attendees to a calendar event, sometimes the external ID is only used for internal purposes
+ * Need to process the correct external ID for the calendar service
+ */
+const processExternalId = (destinationCalendar: DestinationCalendar) => {
+  if (destinationCalendar.integration === OutlookMetadata.type) {
+    // Primary email should always be present for Outlook
+    return destinationCalendar.primaryEmail || destinationCalendar.externalId;
+  }
+
+  return destinationCalendar.externalId;
+};
+
+export default processExternalId;
