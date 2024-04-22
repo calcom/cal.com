14694de819f6afc08f633e12f76a14261444e58b
Alex van Andel <me@alexvanandel.com>
2024-04-22 17:59:57 +0100

    chore: Remove deprecated logo/avatar/away fields

---
+++ packages/prisma/migrations/20240419195414_drop_away_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `away` on the `users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "away";
---
+++ packages/prisma/migrations/20240422165853_drop_avatar_and_logo_fields/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `logo` on the `Team` table. All the data in the column will be lost.
+  - You are about to drop the column `avatar` on the `users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Team" DROP COLUMN "logo";
+
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "avatar";
