f6f6d21f91151eea62af22e9ba2e2bfb931a7f27
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-04-25 13:54:44 +0530

    dynamic routes for oauth client edit page

--- apps/web/pages/settings/platform/oauth-clients/[clientId]/edit/index.tsx
+++ apps/web/pages/settings/platform/oauth-clients/[clientId]/edit/index.tsx
@@ -1,12 +1,13 @@
+import { useParams } from "next/navigation";
+
 import Shell from "@calcom/features/shell/Shell";
-import { useCompatSearchParams } from "@calcom/lib/hooks/useCompatSearchParams";
 
 import PageWrapper from "@components/PageWrapper";
 import { EditOAuthClientForm } from "@components/settings/platform/oauth-clients/oauth-client-form/edit";
 
-export default function CreateOAuthClient() {
-  const searchParams = useCompatSearchParams();
-  const clientId = searchParams?.get("clientId") || "";
+export default function EditOAuthClient() {
+  const params = useParams<{ clientId: string }>();
+  const clientId = params?.clientId || "";
 
   return (
     <div>
@@ -15,7 +16,7 @@ export default function CreateOAuthClient() {
           <div className="border-subtle mx-auto block justify-between rounded-t-lg border px-4 py-6 sm:flex sm:px-6">
             <div className="flex w-full flex-col">
               <h1 className="font-cal text-emphasis mb-1 text-xl font-semibold leading-5 tracking-wide">
-                OAuth client {!!clientId ? "updation" : "creation"} form
+                OAuth client updation form
               </h1>
               <p className="text-default text-sm ltr:mr-4 rtl:ml-4">
                 This is the form to edit an existing OAuth client
@@ -29,4 +30,4 @@ export default function CreateOAuthClient() {
   );
 }
 
-CreateOAuthClient.PageWrapper = PageWrapper;
+EditOAuthClient.PageWrapper = PageWrapper;
