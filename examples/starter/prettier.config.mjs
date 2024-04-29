3005040df614a0481d21ef42a7a2ef58024dab42
Richard Poelderl <richard.poelderl@gmail.com>
2024-04-29 15:38:46 +0200

    use esm

---
+++ examples/starter/prettier.config.mjs
@@ -0,0 +1,14 @@
+/**
+ * @type {import('prettier').Config & import("@ianvs/prettier-plugin-sort-imports").PluginConfig}
+ */
+const config = {
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+};
+
+export default config;
