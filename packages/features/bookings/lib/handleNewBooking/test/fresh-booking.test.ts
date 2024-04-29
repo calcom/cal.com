f432a19bf327c39cbd0524736a024d09dca0b6a2
Alex van Andel <me@alexvanandel.com>
2024-04-29 17:31:02 +0100

    Revert "fix: invalid Cancelled / Rescheduled downstream calendar block (#14700)" (#14800)
    This reverts commit 0306ad737f28ab80be928f42d149684a49249ea8.

--- packages/features/bookings/lib/handleNewBooking/test/fresh-booking.test.ts
+++ packages/features/bookings/lib/handleNewBooking/test/fresh-booking.test.ts
@@ -2433,7 +2433,13 @@ describe("handleNewBooking", () => {
             email: "organizer@example.com",
             id: 101,
             schedules: [TestData.schedules.IstWorkHours],
-            credentials: [],
+            credentials: [getGoogleCalendarCredential()],
+            selectedCalendars: [TestData.selectedCalendars.google],
+            destinationCalendar: {
+              integration: "google_calendar",
+              externalId: "organizer@google-calendar.com",
+              primaryEmail: organizerOtherEmail,
+            },
           });
 
           await createBookingScenario(
@@ -2449,10 +2455,15 @@ describe("handleNewBooking", () => {
                       id: 101,
                     },
                   ],
+                  destinationCalendar: {
+                    integration: "google_calendar",
+                    externalId: "event-type-1@google-calendar.com",
+                    primaryEmail: organizerDestinationCalendarEmailOnEventType,
+                  },
                 },
               ],
               organizer,
-              apps: [TestData.apps["daily-video"]],
+              apps: [TestData.apps["google-calendar"], TestData.apps["daily-video"]],
             })
           );
 
@@ -2513,8 +2524,34 @@ describe("handleNewBooking", () => {
                 meetingPassword: "MOCK_PASS",
                 meetingUrl: "http://mock-dailyvideo.example.com/meeting-1",
               },
+              {
+                type: appStoreMetadata.googlecalendar.type,
+                uid: "MOCKED_GOOGLE_CALENDAR_EVENT_ID",
+                meetingId: "MOCKED_GOOGLE_CALENDAR_EVENT_ID",
+                meetingPassword: "MOCK_PASSWORD",
+                meetingUrl: "https://UNUSED_URL",
+              },
             ],
             iCalUID: createdBooking.iCalUID,
+          });
+
+          expectSuccessfulCalendarEventCreationInCalendar(calendarMock, {
+            calendarId: "event-type-1@google-calendar.com",
+            videoCallUrl: "http://mock-dailyvideo.example.com/meeting-1",
+          });
+
+          const iCalUID = expectICalUIDAsString(createdBooking.iCalUID);
+
+          expectSuccessfulBookingCreationEmails({
+            booking: {
+              uid: createdBooking.uid!,
+              urlOrigin: WEBSITE_URL,
+            },
+            booker,
+            organizer,
+            emails,
+            iCalUID,
+            destinationEmail: organizerDestinationCalendarEmailOnEventType,
           });
 
           await expect(async () => await handleNewBooking(req)).rejects.toThrowError(
