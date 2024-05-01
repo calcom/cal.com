b7f13fc50a840076e3ec1d547d8002397db32f18
sean-brydon <sean@cal.com>
2024-05-01 12:01:26 +0100

    (debug) push for debug

--- packages/trpc/server/routers/viewer/slots/handleNotificationWhenNoSlots.test.ts
+++ packages/trpc/server/routers/viewer/slots/handleNotificationWhenNoSlots.test.ts
@@ -1,21 +1,31 @@
+import i18nMock from "../../../../../../tests/libs/__mocks__/libServerI18n";
 import prismaMock from "../../../../../../tests/libs/__mocks__/prismaMock";
 
-import { vi, describe, it, expect, beforeAll, afterAll } from "vitest";
+import { vi, describe, it, beforeAll, afterAll, expect, beforeEach } from "vitest";
 
 import dayjs from "@calcom/dayjs";
+import * as CalcomEmails from "@calcom/emails";
 import { RedisService } from "@calcom/features/redis/RedisService";
 
 import { handleNotificationWhenNoSlots } from "./handleNotificationWhenNoSlots";
 
 vi.mock("@calcom/features/redis/RedisService", () => {
+  RedisService.prototype.lrange = vi.fn();
+  RedisService.prototype.lpush = vi.fn();
+  RedisService.prototype.expire = vi.fn();
   return {
-    RedisService: vi.fn().mockImplementation(() => ({
-      lrange: vi.fn().mockResolvedValue([""]),
-      lpush: vi.fn(),
-      expire: vi.fn(),
-    })),
+    RedisService,
   };
 });
+
+vi.mock("@calcom/features/flags/server/utils", () => {
+  // Mock kill switch to be false
+  return {
+    getFeatureFlag: vi.fn().mockResolvedValue(false),
+  };
+});
+
+vi.spyOn(CalcomEmails, "sendOrganizationAdminNoSlotsNotification");
 
 describe("handleNotificationWhenNoSlots", () => {
   beforeAll(() => {
@@ -24,16 +34,36 @@ describe("handleNotificationWhenNoSlots", () => {
     vi.stubEnv("UPSTASH_REDIS_REST_URL", "mocked_url");
   });
 
+  beforeEach(() => {
+    // Setup mocks
+    prismaMock.membership.findMany.mockResolvedValue([
+      {
+        user: {
+          email: "test@test.com",
+          locale: "en",
+        },
+      },
+    ]);
+
+    // @ts-expect-error FIXME - also type error in bookingScenario
+    i18nMock.getTranslation.mockImplementation(() => {
+      return new Promise((resolve) => {
+        const identityFn = (key: string) => key;
+        resolve(identityFn);
+      });
+    });
+  });
+
   afterAll(() => {
     vi.unstubAllEnvs();
+    vi.resetAllMocks();
   });
 
   it("Should send a notification if the org has them enabled", async () => {
-    // const redisService = mock<RedisService>();
     const redisService = new RedisService();
     const mocked = vi.mocked(redisService);
-    // Mocking the return values
-    mocked.lrange.mockResolvedValueOnce(["", ""]);
+
+    mocked.lrange.mockResolvedValueOnce([]);
     mocked.lpush.mockResolvedValueOnce(1);
     mocked.expire.mockResolvedValueOnce(1);
 
@@ -54,17 +84,35 @@ describe("handleNotificationWhenNoSlots", () => {
       isValidOrgDomain: true,
     };
 
-    const expiresSpy = vi.spyOn(redisService, "expire");
+    // Call the function
+    await handleNotificationWhenNoSlots({ eventDetails, orgDetails });
+
+    expect(CalcomEmails.sendOrganizationAdminNoSlotsNotification).not.toHaveBeenCalled();
+
+    await handleNotificationWhenNoSlots({ eventDetails, orgDetails });
+    expect(CalcomEmails.sendOrganizationAdminNoSlotsNotification).toHaveBeenCalled();
+  });
+  it("Should not send a notification if the org has them disabled", async () => {
+    prismaMock.team.findFirst.mockResolvedValue({
+      organizationSettings: {
+        adminGetsNoSlotsNotification: false,
+      },
+    });
+
+    // Define event and organization details
+    const eventDetails = {
+      username: "user1",
+      eventSlug: "event1",
+      startTime: dayjs(), // Mocking Dayjs format function
+    };
+    const orgDetails = {
+      currentOrgDomain: "org1",
+      isValidOrgDomain: true,
+    };
 
     // Call the function
     await handleNotificationWhenNoSlots({ eventDetails, orgDetails });
 
-    expect(expiresSpy).toHaveBeenCalled();
-
-    mocked.lrange.mockResolvedValueOnce([""]);
-
-    await handleNotificationWhenNoSlots({ eventDetails, orgDetails });
-    // We won't call this fn again as the count will have incremented
-    expect(expiresSpy).not.toHaveBeenCalled();
+    expect(CalcomEmails.sendEmail).not.toHaveBeenCalled();
   });
 });
