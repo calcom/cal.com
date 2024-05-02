a20fba52812809c50fbd99aa6106dae3288f5d18
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-05-02 11:34:41 +0530

    dashboard component containing oauth client list and managed user

---
+++ apps/web/components/settings/platform/dashboard/oauth-client-dropdown/index.tsx
@@ -0,0 +1,48 @@
+import type { PlatformOAuthClient } from "@calcom/prisma/client";
+import {
+  Button,
+  Dropdown,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownItem,
+} from "@calcom/ui";
+
+type OAuthClientsDropdownProps = {
+  oauthClients: PlatformOAuthClient[];
+  initialClientName: string;
+  handleChange: (clientId: string, clientName: string) => void;
+};
+
+export const OAuthClientsDropdown = ({
+  oauthClients,
+  initialClientName,
+  handleChange,
+}: OAuthClientsDropdownProps) => {
+  return (
+    <div>
+      <Dropdown modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button color="secondary">{initialClientName}</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          {oauthClients.map((client) => {
+            return (
+              <div key={client.id}>
+                {initialClientName !== client.name ? (
+                  <DropdownMenuItem className="outline-none">
+                    <DropdownItem type="button" onClick={() => handleChange(client.id, client.name)}>
+                      {client.name}
+                    </DropdownItem>
+                  </DropdownMenuItem>
+                ) : (
+                  <></>
+                )}
+              </div>
+            );
+          })}
+        </DropdownMenuContent>
+      </Dropdown>
+    </div>
+  );
+};
