cfe929e09360bd028456063f818675a39e7b6856
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-05-03 16:08:01 +0530

    fix type check

--- packages/platform/types/billing.ts
+++ packages/platform/types/billing.ts
@@ -1,8 +1,6 @@
 import { IsString } from "class-validator";
 
-type PlatformPlan = "STARTER" | "ESSENTIALS" | "SCALE" | "ENTERPRISE";
-
 export class SubscribeTeamInput {
   @IsString()
-  plan!: PlatformPlan;
+  plan!: string;
 }
