1b59544332b885853dc99ca0019de9e8efdc53cc
Udit Takkar <udit222001@gmail.com>
2024-05-03 17:08:01 +0530

    fix: omit

--- apps/web/test/utils/bookingScenario/bookingScenario.ts
+++ apps/web/test/utils/bookingScenario/bookingScenario.ts
@@ -1739,7 +1739,7 @@ export function getMockBookingReference(
 }
 
 export function getMockBookingAttendee(
-  attendee: Omit<Attendee, "bookingId", "phoneNumber"> & {
+  attendee: Omit<Attendee, "bookingId" | "phoneNumber"> & {
     bookingSeat?: AttendeeBookingSeatInput;
     phoneNumber?: string | null;
   }
