25a0a15cd2f38552a4b1ac51b32c2d2fc449d97d
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-05-03 13:10:53 -0400

    Clean up

--- packages/app-store/_utils/oauth/createOAuthAppCredential.ts
+++ packages/app-store/_utils/oauth/createOAuthAppCredential.ts
@@ -44,7 +44,7 @@ const createOAuthAppCredential = async (
 
     if (!team) throw new Error("User does not belong to the team");
 
-    const credential = await prisma.credential.create({
+    return await prisma.credential.create({
       data: {
         type: appData.type,
         key: key || {},
@@ -52,13 +52,11 @@ const createOAuthAppCredential = async (
         appId: appData.appId,
       },
     });
-
-    return credential;
   }
 
   await throwIfNotHaveAdminAccessToTeam({ teamId: state?.teamId ?? null, userId });
 
-  const credential = await prisma.credential.create({
+  return await prisma.credential.create({
     data: {
       type: appData.type,
       key: key || {},
@@ -66,8 +64,6 @@ const createOAuthAppCredential = async (
       appId: appData.appId,
     },
   });
-
-  return credential;
 };
 
 export default createOAuthAppCredential;
