256ff31bcbf35ae291a5f8fd46a9236e92e66175
Kartik Saini <kartik8942@gmail.com>
2024-05-05 19:50:31 +0530

    test

--- packages/core/getCalendarsEvents.test.ts
+++ packages/core/getCalendarsEvents.test.ts
@@ -128,7 +128,7 @@ describe("getCalendarsEvents", () => {
       false
     );
 
-    expect(getAvailabilitySpy).toHaveBeenCalledWith("2010-12-01", "2010-12-04", [selectedCalendar], false);
+    expect(getAvailabilitySpy).toHaveBeenCalledWith("2010-12-01", "2010-12-04", [selectedCalendar]);
     expect(result).toEqual([
       availability.map((av) => ({
         ...av,
@@ -192,18 +192,12 @@ describe("getCalendarsEvents", () => {
       false
     );
 
-    expect(getGoogleAvailabilitySpy).toHaveBeenCalledWith(
-      "2010-12-01",
-      "2010-12-04",
-      [selectedGoogleCalendar],
-      false
-    );
-    expect(getOfficeAvailabilitySpy).toHaveBeenCalledWith(
-      "2010-12-01",
-      "2010-12-04",
-      [selectedOfficeCalendar],
-      false
-    );
+    expect(getGoogleAvailabilitySpy).toHaveBeenCalledWith("2010-12-01", "2010-12-04", [
+      selectedGoogleCalendar,
+    ]);
+    expect(getOfficeAvailabilitySpy).toHaveBeenCalledWith("2010-12-01", "2010-12-04", [
+      selectedOfficeCalendar,
+    ]);
     expect(result).toEqual([
       googleAvailability.map((av) => ({
         ...av,
