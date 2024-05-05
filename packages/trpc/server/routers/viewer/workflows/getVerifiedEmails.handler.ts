52c6ba4ceed0f8dfe94a1f223fe9d9d9760b927b
Amit91848 <samit91848@gmail.com>
2024-05-05 17:36:42 +0000

    fix: check team before adding email

--- packages/trpc/server/routers/viewer/workflows/getVerifiedEmails.handler.ts
+++ packages/trpc/server/routers/viewer/workflows/getVerifiedEmails.handler.ts
@@ -1,5 +1,9 @@
+import type { VerifiedEmail } from "@prisma/client";
+
 import { prisma } from "@calcom/prisma";
 import type { TrpcSessionUser } from "@calcom/trpc/server/trpc";
+
+import { TRPCError } from "@trpc/server";
 
 import type { TGetVerifiedEmailsInputSchema } from "./getVerifiedEmails.schema";
 
@@ -12,11 +16,53 @@ type GetVerifiedEmailsOptions = {
 
 export const getVerifiedEmailsHandler = async ({ ctx, input }: GetVerifiedEmailsOptions) => {
   const { user } = ctx;
-  const verifiedEmails = await prisma.verifiedEmail.findMany({
+  const { teamId } = input;
+  let verifiedEmails: VerifiedEmail[] = [];
+
+  if (teamId) {
+    const team = await prisma.team.findFirst({
+      where: {
+        id: teamId,
+      },
+      select: {
+        members: {
+          include: {
+            user: {
+              select: {
+                id: true,
+                email: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    if (!team) {
+      throw new TRPCError({ code: "NOT_FOUND", message: "Team not found" });
+    }
+
+    const isTeamMember = team.members.some((member) => member.userId === user.id);
+
+    if (!isTeamMember) {
+      throw new TRPCError({ code: "FORBIDDEN", message: "You are not a member of this team" });
+    }
+
+    verifiedEmails = team.members.map((member) => ({
+      email: member.user.email,
+      userId: user.id,
+      teamId,
+      id: member.user.id,
+    }));
+  }
+
+  const emails = await prisma.verifiedEmail.findMany({
     where: {
       OR: [{ userId: user.id }, { teamId: input.teamId }],
     },
   });
+
+  verifiedEmails = verifiedEmails.concat(emails);
 
   return verifiedEmails;
 };
