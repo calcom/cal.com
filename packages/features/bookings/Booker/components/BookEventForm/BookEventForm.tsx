40d52f3425e45e73b63e0cdda231d5fc967e647d
VenkateshSircilla123 <113850972+VenkateshSircilla123@users.noreply.github.com>
2024-05-05 09:44:17 +0530

    translated by proceeding

--- packages/features/bookings/Booker/components/BookEventForm/BookEventForm.tsx
+++ packages/features/bookings/Booker/components/BookEventForm/BookEventForm.tsx
@@ -115,13 +115,12 @@ export const BookEventForm = ({
         {!isPlatform && IS_CALCOM && (
           <div className="text-subtle my-3 w-full text-xs opacity-80">
             <Trans i18nKey="signing_up_terms">
-              By proceeding, you agree to our{" "}
+              {t("by_proceeding_you_agree_to_our ")}
               <Link className="text-emphasis hover:underline" href={`${WEBSITE_URL}/terms`} target="_blank">
-                <a>Terms</a>
-              </Link>{" "}
-              and{" "}
+                <a>{t("terms")}</a>
+              </Link>{t(" and ")}
               <Link className="text-emphasis hover:underline" href={`${WEBSITE_URL}/privacy`} target="_blank">
-                <a>Privacy Policy</a>
+                <a>{t("privacy_policy")}</a>
               </Link>
               .
             </Trans>
