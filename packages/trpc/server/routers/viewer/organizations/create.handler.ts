577723bdce7ec6c1c6e2d6513f593cb2e61e6ce3
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-05-07 13:34:28 +0530

    outlining how to break the handler into smaller functions

--- packages/trpc/server/routers/viewer/organizations/create.handler.ts
+++ packages/trpc/server/routers/viewer/organizations/create.handler.ts
@@ -1,3 +1,4 @@
+import type { User, Team } from "@prisma/client";
 import { lookup } from "dns";
 
 import { getOrgFullOrigin } from "@calcom/ee/organizations/lib/orgDomains";
@@ -29,6 +30,14 @@ type CreateOptions = {
   input: TCreateInputSchema;
 };
 
+type LoggedInUserType = {
+  id: User["id"];
+  role: User["role"];
+  teams: {
+    team: { slug: Team["slug"] };
+  }[];
+};
+
 const getIPAddress = async (url: string): Promise<string> => {
   return new Promise((resolve, reject) => {
     lookup(url, (err, address) => {
@@ -36,6 +45,93 @@ const getIPAddress = async (url: string): Promise<string> => {
       resolve(address);
     });
   });
+};
+
+const checkUserIsAdmin = (
+  userRole: LoggedInUserType["role"],
+  loggedInUserEmail: string,
+  orgOwnerEmail: string
+) => {
+  const IS_USER_ADMIN = userRole === UserPermissionRole.ADMIN;
+  if (!ORG_SELF_SERVE_ENABLED && !IS_USER_ADMIN) {
+    throw new TRPCError({ code: "FORBIDDEN", message: "Only admins can create organizations" });
+  }
+
+  if (!IS_USER_ADMIN && loggedInUserEmail !== orgOwnerEmail) {
+    throw new TRPCError({
+      code: "FORBIDDEN",
+      message: "You can only create organization where you are the owner",
+    });
+  }
+
+  return IS_USER_ADMIN;
+};
+
+const checkLoginStatus = async (userId: number) => {
+  const loggedInUser = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      id: true,
+      role: true,
+      email: true,
+      teams: {
+        select: {
+          team: {
+            select: {
+              slug: true,
+            },
+          },
+        },
+      },
+    },
+  });
+  if (!loggedInUser) throw new TRPCError({ code: "UNAUTHORIZED", message: "You are not authorized." });
+
+  return loggedInUser;
+};
+
+const checkOrgPublishedTeams = (teams: LoggedInUserType["teams"], isAdmin: boolean) => {
+  const publishedTeams = teams.filter((team) => !!team.team.slug);
+  if (!isAdmin && publishedTeams.length < ORG_MINIMUM_PUBLISHED_TEAMS_SELF_SERVE) {
+    throw new TRPCError({ code: "FORBIDDEN", message: "You need to have atleast two published teams." });
+  }
+};
+
+const enrichUserProfile = async (
+  organizationId: number,
+  orgOwner: User
+  // availability: {
+  //   id: number;
+  //   userId: number | null;
+  //   eventTypeId: number | null;
+  //   days: number[];
+  //   startTime: Date;
+  //   endTime: Date;
+  //   date: Date | null;
+  //   scheduleId: number | null;
+  // }[]
+) => {
+  const user = await UserRepository.enrichUserWithItsProfile({
+    user: { ...orgOwner, organizationId },
+  });
+
+  // await prisma.availability.createMany({
+  //   data: availability.map((schedule) => ({
+  //     days: schedule.days,
+  //     startTime: schedule.startTime,
+  //     endTime: schedule.endTime,
+  //     userId: user.id,
+  //   })),
+  // });
+
+  return {
+    userId: user.id,
+    email: user.email,
+    organizationId: user.organizationId,
+    upId: user.profile.upId,
+  };
 };
 
 export const createHandler = async ({ input, ctx }: CreateOptions) => {
@@ -61,6 +157,7 @@ export const createHandler = async ({ input, ctx }: CreateOptions) => {
   });
   if (!loggedInUser) throw new TRPCError({ code: "UNAUTHORIZED", message: "You are not authorized." });
 
+  // start function checkUserIsAdmin from here
   const IS_USER_ADMIN = loggedInUser.role === UserPermissionRole.ADMIN;
 
   if (!ORG_SELF_SERVE_ENABLED && !IS_USER_ADMIN) {
@@ -73,12 +170,17 @@ export const createHandler = async ({ input, ctx }: CreateOptions) => {
       message: "You can only create organization where you are the owner",
     });
   }
+  // end function checkUserIsAdmin from here
 
+  // split between two functions
+
+  // start function checkOrgPublishedTeams from here
   const publishedTeams = loggedInUser.teams.filter((team) => !!team.team.slug);
 
   if (!IS_USER_ADMIN && publishedTeams.length < ORG_MINIMUM_PUBLISHED_TEAMS_SELF_SERVE) {
     throw new TRPCError({ code: "FORBIDDEN", message: "You need to have minimum published teams." });
   }
+  // end function checkOrgPublishedTeams from here
 
   let orgOwner = await prisma.user.findUnique({
     where: {
@@ -107,6 +209,8 @@ export const createHandler = async ({ input, ctx }: CreateOptions) => {
   const loggedInUserTranslation = await getTranslation(ctx.user.locale, "common");
   const inputLanguageTranslation = await getTranslation(input.language ?? "en", "common");
 
+  // this I think is only for orgs
+  // start here isOrgConfigured fn
   if (!isOrganizationConfigured) {
     // Otherwise, we proceed to send an administrative email to admins regarding
     // the need to configure DNS registry to support the newly created org
@@ -128,6 +232,7 @@ export const createHandler = async ({ input, ctx }: CreateOptions) => {
       console.warn("Organization created: subdomain not configured and couldn't notify adminnistrators");
     }
   }
+  // end here
 
   const autoAcceptEmail = orgOwnerEmail.split("@")[1];
 
@@ -143,6 +248,9 @@ export const createHandler = async ({ input, ctx }: CreateOptions) => {
   };
 
   // Create a new user and invite them as the owner of the organization
+  // new function will have to be created that checks if there is org owner or not
+  // there will be lots of small small functions inside of it
+  // persist organization function starts here
   if (!orgOwner) {
     const data = await OrganizationRepository.createWithNonExistentOwner({
       orgData,
@@ -175,6 +283,7 @@ export const createHandler = async ({ input, ctx }: CreateOptions) => {
       newLink: `${getOrgFullOrigin(slug, { protocol: true })}/${ownerProfile.username}`,
     });
 
+    // start function enrichUserProfile here
     const user = await UserRepository.enrichUserWithItsProfile({
       user: { ...orgOwner, organizationId: organization.id },
     });
@@ -185,6 +294,7 @@ export const createHandler = async ({ input, ctx }: CreateOptions) => {
       organizationId: user.organizationId,
       upId: user.profile.upId,
     };
+    // end function enrichUserProfile here
   } else {
     // If we are making the loggedIn user the owner of the organization and he is already a part of an organization, we don't allow it because multi-org is not supported yet
     const isLoggedInUserOrgOwner = orgOwner.id === loggedInUser.id;
@@ -202,6 +312,7 @@ export const createHandler = async ({ input, ctx }: CreateOptions) => {
       },
     });
 
+    // this repeats for both if else cases
     await sendOrganizationCreationEmail({
       language: inputLanguageTranslation,
       from: ctx.user.name ?? `${organization.name}'s admin`,
@@ -214,11 +325,16 @@ export const createHandler = async ({ input, ctx }: CreateOptions) => {
       newLink: `${getOrgFullOrigin(slug, { protocol: true })}/${ownerProfile.username}`,
     });
 
+    // this will be standalone line
     if (!organization.id) throw Error("User not created");
+    // start function enrichUserProfile here
     const user = await UserRepository.enrichUserWithItsProfile({
       user: { ...orgOwner, organizationId: organization.id },
     });
 
+    // this doenst come under function enrichUserProfile
+    // maybe create a separate fn for this
+    // createDefaultAvailability or sum
     await prisma.availability.createMany({
       data: availability.map((schedule) => ({
         days: schedule.days,
@@ -234,7 +350,9 @@ export const createHandler = async ({ input, ctx }: CreateOptions) => {
       organizationId: user.organizationId,
       upId: user.profile.upId,
     };
+    // end function enrichUserProfile here
   }
+  // persist organization function ends here
 
   // Sync Services: Close.com
   //closeComUpsertOrganizationUser(createTeam, ctx.user, MembershipRole.OWNER);
