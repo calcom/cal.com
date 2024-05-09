a27d43e629cec5610849db3198aec026b0177019
Anik Dhabal Babu <adhabal2002@gmail.com>
2024-05-09 20:58:14 +0000

    changes

--- apps/web/pages/signup.tsx
+++ apps/web/pages/signup.tsx
@@ -409,7 +409,7 @@ export default function Signup({
                 </Button>
               </Form>
               {/* Continue with Social Logins - Only for non-invite links */}
-              {token || (!isGoogleLoginEnabled && !isSAMLLoginEnabled) ? null : (
+              {!isGoogleLoginEnabled && !isSAMLLoginEnabled ? null : (
                 <div className="mt-6">
                   <div className="relative flex items-center">
                     <div className="border-subtle flex-grow border-t" />
@@ -420,91 +420,88 @@ export default function Signup({
                   </div>
                 </div>
               )}
-              {/* Social Logins - Only for non-invite links*/}
-              {!token && (
-                <div className="mt-6 flex flex-col gap-2 md:flex-row">
-                  {isGoogleLoginEnabled ? (
-                    <Button
-                      color="secondary"
-                      disabled={!!formMethods.formState.errors.username || premiumUsername}
-                      loading={isGoogleLoading}
-                      CustomStartIcon={
-                        <img
-                          className={classNames(
-                            "text-subtle  mr-2 h-4 w-4 dark:invert",
-                            premiumUsername && "opacity-50"
-                          )}
-                          src="/google-icon.svg"
-                          alt=""
-                        />
-                      }
-                      className={classNames(
-                        "w-full justify-center rounded-md text-center",
-                        formMethods.formState.errors.username ? "opacity-50" : ""
-                      )}
-                      onClick={async () => {
-                        setIsGoogleLoading(true);
-                        const username = formMethods.getValues("username");
-                        const baseUrl = process.env.NEXT_PUBLIC_WEBAPP_URL;
-                        const GOOGLE_AUTH_URL = `${baseUrl}/auth/sso/google`;
-                        if (username) {
-                          // If username is present we save it in query params to check for premium
-                          const searchQueryParams = new URLSearchParams();
-                          searchQueryParams.set("username", username);
-                          localStorage.setItem("username", username);
-                          router.push(`${GOOGLE_AUTH_URL}?${searchQueryParams.toString()}`);
-                          return;
-                        }
-                        router.push(GOOGLE_AUTH_URL);
-                      }}>
-                      Google
-                    </Button>
-                  ) : null}
-                  {isSAMLLoginEnabled ? (
-                    <Button
-                      color="secondary"
-                      disabled={
-                        !!formMethods.formState.errors.username ||
-                        !!formMethods.formState.errors.email ||
-                        premiumUsername ||
-                        isSubmitting ||
-                        isGoogleLoading
-                      }
-                      className={classNames(
-                        "w-full justify-center rounded-md text-center",
-                        formMethods.formState.errors.username && formMethods.formState.errors.email
-                          ? "opacity-50"
-                          : ""
-                      )}
-                      onClick={() => {
-                        if (!formMethods.getValues("username")) {
-                          formMethods.trigger("username");
-                        }
-                        if (!formMethods.getValues("email")) {
-                          formMethods.trigger("email");
-
-                          return;
-                        }
-                        const username = formMethods.getValues("username");
-                        if (!username) {
-                          showToast("error", t("username_required"));
-                          return;
-                        }
+              <div className="mt-6 flex flex-col gap-2 md:flex-row">
+                {isGoogleLoginEnabled ? (
+                  <Button
+                    color="secondary"
+                    disabled={!!formMethods.formState.errors.username || premiumUsername}
+                    loading={isGoogleLoading}
+                    CustomStartIcon={
+                      <img
+                        className={classNames(
+                          "text-subtle  mr-2 h-4 w-4 dark:invert",
+                          premiumUsername && "opacity-50"
+                        )}
+                        src="/google-icon.svg"
+                        alt=""
+                      />
+                    }
+                    className={classNames(
+                      "w-full justify-center rounded-md text-center",
+                      formMethods.formState.errors.username ? "opacity-50" : ""
+                    )}
+                    onClick={async () => {
+                      setIsGoogleLoading(true);
+                      const username = formMethods.getValues("username");
+                      const baseUrl = process.env.NEXT_PUBLIC_WEBAPP_URL;
+                      const GOOGLE_AUTH_URL = `${baseUrl}/auth/sso/google`;
+                      if (username) {
+                        // If username is present we save it in query params to check for premium
+                        const searchQueryParams = new URLSearchParams();
+                        searchQueryParams.set("username", username);
                         localStorage.setItem("username", username);
-                        const sp = new URLSearchParams();
-                        // @NOTE: don't remove username query param as it's required right now for stripe payment page
-                        sp.set("username", username);
-                        sp.set("email", formMethods.getValues("email"));
-                        router.push(
-                          `${process.env.NEXT_PUBLIC_WEBAPP_URL}/auth/sso/saml` + `?${sp.toString()}`
-                        );
-                      }}>
-                      <Icon name="shield-check" className="mr-2 h-5 w-5" />
-                      {t("saml_sso")}
-                    </Button>
-                  ) : null}
-                </div>
-              )}
+                        router.push(`${GOOGLE_AUTH_URL}?${searchQueryParams.toString()}`);
+                        return;
+                      }
+                      router.push(GOOGLE_AUTH_URL);
+                    }}>
+                    Google
+                  </Button>
+                ) : null}
+                {isSAMLLoginEnabled ? (
+                  <Button
+                    color="secondary"
+                    disabled={
+                      !!formMethods.formState.errors.username ||
+                      !!formMethods.formState.errors.email ||
+                      premiumUsername ||
+                      isSubmitting ||
+                      isGoogleLoading
+                    }
+                    className={classNames(
+                      "w-full justify-center rounded-md text-center",
+                      formMethods.formState.errors.username && formMethods.formState.errors.email
+                        ? "opacity-50"
+                        : ""
+                    )}
+                    onClick={() => {
+                      if (!formMethods.getValues("username")) {
+                        formMethods.trigger("username");
+                      }
+                      if (!formMethods.getValues("email")) {
+                        formMethods.trigger("email");
+
+                        return;
+                      }
+                      const username = formMethods.getValues("username");
+                      if (!username) {
+                        showToast("error", t("username_required"));
+                        return;
+                      }
+                      localStorage.setItem("username", username);
+                      const sp = new URLSearchParams();
+                      // @NOTE: don't remove username query param as it's required right now for stripe payment page
+                      sp.set("username", username);
+                      sp.set("email", formMethods.getValues("email"));
+                      router.push(
+                        `${process.env.NEXT_PUBLIC_WEBAPP_URL}/auth/sso/saml` + `?${sp.toString()}`
+                      );
+                    }}>
+                    <Icon name="shield-check" className="mr-2 h-5 w-5" />
+                    {t("saml_sso")}
+                  </Button>
+                ) : null}
+              </div>
             </div>
             {/* Already have an account & T&C */}
             <div className="mt-10 flex h-full flex-col justify-end text-xs">
