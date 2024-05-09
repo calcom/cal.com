ee33f105c16febdf586dac71a3f52a2df6aa70f3
Soumik Patra <smkgrand99@gmail.com>
2024-05-09 12:23:32 +0530

    commit 3

--- packages/app-store/cal-ai/api/_getAdd.ts
+++ packages/app-store/cal-ai/api/_getAdd.ts
@@ -4,16 +4,16 @@ import { defaultResponder } from "@calcom/lib/server";
 
 import checkSession from "../../_utils/auth";
 import { withPaidAppRedirect } from "../../_utils/paid-apps";
-import appConfig from "../config.json";
+import { metadata as appConfig } from "../_metadata";
 
 export async function getHandler(req: NextApiRequest, res: NextApiResponse) {
   const session = checkSession(req);
 
   const redirectUrl = await withPaidAppRedirect({
-    appPaidMode: appConfig.paid.mode,
+    appPaidMode: appConfig?.paid?.mode ?? "",
     appSlug: appConfig.slug,
     userId: session.user.id,
-    priceId: appConfig.paid.priceId,
+    priceId: appConfig?.paid?.priceId ?? "",
   });
 
   if (!redirectUrl) {
