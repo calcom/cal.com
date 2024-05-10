7e7e82b35916224220162ff66177c1f26e2115ad
Morgan <33722304+ThyMinimalDev@users.noreply.github.com>
2024-05-10 10:36:51 +0300

    fix: api v2 hotfix google calendar, exception details, billing check guard (#14967)

--- packages/platform/types/api.ts
+++ packages/platform/types/api.ts
@@ -17,7 +17,7 @@ export type API_ERROR_CODES_TYPE = (typeof API_ERROR_CODES)[number];
 export type ErrorType = {
   code: API_ERROR_CODES_TYPE | string;
   message?: string;
-  details?: string | string[];
+  details?: string | string[] | object;
 };
 
 export type ApiErrorResponse = {
