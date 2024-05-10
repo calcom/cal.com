affd4f57ac310550db89b942c23dbadabb68790c
vikrant845 <131388926+vikrant845@users.noreply.github.com>
2024-05-10 19:35:09 +0530

    fix: Fixed email embed for team when specific times are chosen embeds the wrong URL (#14975)
    * Fixed team embed url
    
    * Added comments
    
    ---------
    
    Co-authored-by: borawakevikrant <borawakevikrant@gmail.com>

--- packages/features/embed/Embed.tsx
+++ packages/features/embed/Embed.tsx
@@ -418,7 +418,13 @@ const EmailEmbedPreview = ({
                                   <tr style={{ height: "25px" }}>
                                     {selectedDateAndTime[key]?.length > 0 &&
                                       selectedDateAndTime[key].map((time) => {
-                                        const bookingURL = `${WEBSITE_URL}/${username}/${eventType.slug}?duration=${eventType.length}&date=${key}&month=${month}&slot=${time}`;
+                                        // If teamId is present on eventType and is not null, it means it is a team event.
+                                        // So we add 'team/' to the url.
+                                        const bookingURL = `${WEBSITE_URL}/${
+                                          eventType.teamId !== null ? "team/" : ""
+                                        }${username}/${eventType.slug}?duration=${
+                                          eventType.length
+                                        }&date=${key}&month=${month}&slot=${time}`;
                                         return (
                                           <td
                                             key={time}
