1be3f52c4aa9ed14c7a6446a280c840d7888e35c
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-05-10 15:32:34 +0530

    update returned user attributes

--- apps/web/components/settings/platform/hooks/useGetUserAttributes.ts
+++ apps/web/components/settings/platform/hooks/useGetUserAttributes.ts
@@ -3,9 +3,12 @@ import useMeQuery from "@lib/hooks/useMeQuery";
 
 export const useGetUserAttributes = () => {
   const { data: user, isLoading: isUserLoading } = useMeQuery();
-  const { data: userBillingData } = useCheckTeamBilling(user?.organizationId);
+  const { data: userBillingData, isFetching: isUserBillingDataLoading } = useCheckTeamBilling(
+    user?.organizationId
+  );
   const isPlatformUser = user?.organization.isPlatform;
   const isPaidUser = userBillingData?.valid;
+  const userOrgId = user?.organizationId;
 
-  return { isUserLoading, isPlatformUser, isPaidUser };
+  return { isUserLoading, isUserBillingDataLoading, isPlatformUser, isPaidUser, userBillingData, userOrgId };
 };
