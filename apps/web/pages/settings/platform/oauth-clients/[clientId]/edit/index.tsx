5bd21e159e43defb63d18c9393242b8533c75c2f
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-05-10 13:48:37 +0530

    add checks

--- apps/web/pages/settings/platform/oauth-clients/[clientId]/edit/index.tsx
+++ apps/web/pages/settings/platform/oauth-clients/[clientId]/edit/index.tsx
@@ -6,7 +6,9 @@ import { PERMISSIONS_GROUPED_MAP } from "@calcom/platform-constants";
 import { showToast } from "@calcom/ui";
 
 import { useOAuthClient } from "@lib/hooks/settings/platform/oauth-clients/useOAuthClients";
+import { useCheckTeamBilling } from "@lib/hooks/settings/platform/oauth-clients/usePersistOAuthClient";
 import { useUpdateOAuthClient } from "@lib/hooks/settings/platform/oauth-clients/usePersistOAuthClient";
+import useMeQuery from "@lib/hooks/useMeQuery";
 
 import PageWrapper from "@components/PageWrapper";
 import type { FormValues } from "@components/settings/platform/oauth-clients/oauth-client-form";
@@ -29,6 +31,14 @@ export default function EditOAuthClient() {
   const router = useRouter();
   const params = useParams<{ clientId: string }>();
   const clientId = params?.clientId || "";
+
+  const { data: user, isLoading } = useMeQuery();
+  const { data: userBillingData, isPending: isUserBillingDataLoading } = useCheckTeamBilling(
+    user?.organizationId
+  );
+  const isPlatformUser = user?.organization.isPlatform;
+  const isPaidUser = userBillingData?.valid;
+
   const { data, isFetched, isFetching, isError, refetch } = useOAuthClient(clientId);
   const { mutateAsync: update, isPending: isUpdating } = useUpdateOAuthClient({
     onSuccess: () => {
@@ -66,48 +76,60 @@ export default function EditOAuthClient() {
     });
   };
 
+  if (isLoading) return <div className="m-5">Loading...</div>;
+
+  if (isPlatformUser && isPaidUser) {
+    return (
+      <div>
+        <Shell title="OAuth client updation form" isPlatformUser={true}>
+          <div className="m-2 md:mx-14 md:mx-5">
+            <div className="border-subtle mx-auto block justify-between rounded-t-lg border px-4 py-6 sm:flex sm:px-6">
+              <div className="flex w-full flex-col">
+                <h1 className="font-cal text-emphasis mb-1 text-xl font-semibold leading-5 tracking-wide">
+                  OAuth client updation form
+                </h1>
+                <p className="text-default text-sm ltr:mr-4 rtl:ml-4">
+                  This is the form to edit an existing OAuth client
+                </p>
+              </div>
+            </div>
+            {(!Boolean(clientId) || (isFetched && !data)) && <p>OAuth Client not found.</p>}
+            {isFetched && !!data && (
+              <EditOAuthClientForm
+                defaultValues={{
+                  name: data?.name ?? "",
+                  areEmailsEnabled: data.areEmailsEnabled ?? false,
+                  redirectUris: data?.redirectUris?.map((uri) => ({ uri })) ?? [{ uri: "" }],
+                  bookingRedirectUri: data?.bookingRedirectUri ?? "",
+                  bookingCancelRedirectUri: data?.bookingCancelRedirectUri ?? "",
+                  bookingRescheduleRedirectUri: data?.bookingRescheduleRedirectUri ?? "",
+                  appsRead: hasAppsReadPermission(data?.permissions),
+                  appsWrite: hasAppsWritePermission(data?.permissions),
+                  bookingRead: hasBookingReadPermission(data?.permissions),
+                  bookingWrite: hasBookingWritePermission(data?.permissions),
+                  eventTypeRead: hasEventTypeReadPermission(data?.permissions),
+                  eventTypeWrite: hasEventTypeWritePermission(data?.permissions),
+                  profileRead: hasProfileReadPermission(data?.permissions),
+                  profileWrite: hasProfileWritePermission(data?.permissions),
+                  scheduleRead: hasScheduleReadPermission(data?.permissions),
+                  scheduleWrite: hasScheduleWritePermission(data?.permissions),
+                }}
+                onSubmit={onSubmit}
+                isPending={isUpdating}
+              />
+            )}
+            {isFetching && <p>Loading...</p>}
+            {isError && <p>Something went wrong.</p>}
+          </div>
+        </Shell>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <Shell title="OAuth client updation form" isPlatformUser={true}>
-        <div className="m-2 md:mx-14 md:mx-5">
-          <div className="border-subtle mx-auto block justify-between rounded-t-lg border px-4 py-6 sm:flex sm:px-6">
-            <div className="flex w-full flex-col">
-              <h1 className="font-cal text-emphasis mb-1 text-xl font-semibold leading-5 tracking-wide">
-                OAuth client updation form
-              </h1>
-              <p className="text-default text-sm ltr:mr-4 rtl:ml-4">
-                This is the form to edit an existing OAuth client
-              </p>
-            </div>
-          </div>
-          {(!Boolean(clientId) || (isFetched && !data)) && <p>OAuth Client not found.</p>}
-          {isFetched && !!data && (
-            <EditOAuthClientForm
-              defaultValues={{
-                name: data?.name ?? "",
-                areEmailsEnabled: data.areEmailsEnabled ?? false,
-                redirectUris: data?.redirectUris?.map((uri) => ({ uri })) ?? [{ uri: "" }],
-                bookingRedirectUri: data?.bookingRedirectUri ?? "",
-                bookingCancelRedirectUri: data?.bookingCancelRedirectUri ?? "",
-                bookingRescheduleRedirectUri: data?.bookingRescheduleRedirectUri ?? "",
-                appsRead: hasAppsReadPermission(data?.permissions),
-                appsWrite: hasAppsWritePermission(data?.permissions),
-                bookingRead: hasBookingReadPermission(data?.permissions),
-                bookingWrite: hasBookingWritePermission(data?.permissions),
-                eventTypeRead: hasEventTypeReadPermission(data?.permissions),
-                eventTypeWrite: hasEventTypeWritePermission(data?.permissions),
-                profileRead: hasProfileReadPermission(data?.permissions),
-                profileWrite: hasProfileWritePermission(data?.permissions),
-                scheduleRead: hasScheduleReadPermission(data?.permissions),
-                scheduleWrite: hasScheduleWritePermission(data?.permissions),
-              }}
-              onSubmit={onSubmit}
-              isPending={isUpdating}
-            />
-          )}
-          {isFetching && <p>Loading...</p>}
-          {isError && <p>Something went wrong.</p>}
-        </div>
+      <Shell isPlatformUser={true} hideHeadingOnMobile withoutMain={false} SidebarContainer={<></>}>
+        You are not subscribed to a Platform plan.
       </Shell>
     </div>
   );
