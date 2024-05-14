8bad5c898f4d82700e12f72b77056eb78aaccbb2
oliver <8559757+oliverqx@users.noreply.github.com>
2024-05-14 13:33:29 -0600

    fix: add parameter to handleAuditLogTrigger

--- packages/features/audit-logs/lib/handleAuditLogTrigger.ts
+++ packages/features/audit-logs/lib/handleAuditLogTrigger.ts
@@ -1,12 +1,12 @@
 import logger from "@calcom/lib/logger";
 
-import { getAuditLogManager } from "./AuditLogsManager";
+import { getAuditLogManager } from "./getAuditLogManager";
 
-export async function handleAuditLogTrigger() {
+export async function handleAuditLogTrigger(message: string) {
   try {
     const auditLogManager = await getAuditLogManager();
 
-    auditLogManager.report();
+    auditLogManager.report(message);
   } catch (error) {
     logger.error("Error while sending audit log", error);
   }
