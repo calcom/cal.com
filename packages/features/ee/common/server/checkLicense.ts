3cc7bb8fcc2ae511aa14391598c9abc3d4b86681
sean-brydon <sean@cal.com>
2024-05-14 09:51:10 +0100

    Remove schema as we validate on the backend with old+new key schemas

--- packages/features/ee/common/server/checkLicense.ts
+++ packages/features/ee/common/server/checkLicense.ts
@@ -6,18 +6,7 @@ import type { PrismaClient } from "@calcom/prisma";
 
 const CACHING_TIME = 86400000; // 24 hours in milliseconds
 
-const schemaLicenseKey = z
-  .string()
-  // .uuid() exists but I'd to fix the situation where the CALCOM_LICENSE_KEY is wrapped in quotes
-  .regex(/^\"?[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}\"?$/, {
-    message: "License key must follow UUID format: 8-4-4-4-12",
-  })
-  .transform((v) => {
-    // Remove the double quotes from the license key, as they 404 the fetch.
-    return v != null && v.length >= 2 && v.charAt(0) == '"' && v.charAt(v.length - 1) == '"'
-      ? v.substring(1, v.length - 1)
-      : v;
-  });
+const schemaLicenseKey = z.string();
 
 async function checkLicense(
   /** The prisma client to use (necessary for public API to handle custom prisma instances) */
