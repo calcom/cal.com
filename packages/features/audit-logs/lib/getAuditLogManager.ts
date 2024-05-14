a9abba76442cd9c1224a68fd748cafe5ff4e23dd
oliver <8559757+oliverqx@users.noreply.github.com>
2024-05-14 13:32:51 -0600

    fix: temporarily hard coded appKeys

--- packages/features/audit-logs/lib/getAuditLogManager.ts
+++ packages/features/audit-logs/lib/getAuditLogManager.ts
@@ -1,10 +1,11 @@
-import { AuditLogManagerDummy } from "./AuditLogsManager";
+import type { AuditLogsManager } from "@calcom/features/audit-logs/AuditLogsManager";
+import { AuditLogManagerDummy } from "@calcom/features/audit-logs/AuditLogsManager";
 
 export async function getAuditLogManager(): Promise<AuditLogsManager> {
   // based on app keys, return the proper audit logs manager
-  const appKeys: { apiKey: string; projectId: string } = (await getAppKeysFromSlug("boxyhq-retraced")) as any;
+  // const appKeys: { apiKey: string; projectId: string } = (await getAppKeysFromSlug("boxyhq-retraced")) as any;
 
   // at the moment boxyhq is the default audit logs manager
-  const boxy = new AuditLogManagerDummy(appKeys);
+  const boxy = new AuditLogManagerDummy({ apiKey: "", projectId: "" });
   return boxy;
 }
