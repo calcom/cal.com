b98ac4a397818c891dfb680bc236d8fb65fff9b3
oliver <8559757+oliverqx@users.noreply.github.com>
2024-05-14 20:21:41 -0600

    fix: typo in category name

--- packages/app-store-cli/src/components/AppCreateUpdateForm.tsx
+++ packages/app-store-cli/src/components/AppCreateUpdateForm.tsx
@@ -93,7 +93,7 @@ export const AppForm = ({
         { label: "CRM", value: "crm" },
         { label: "Messaging", value: "messaging" },
         { label: "Payment", value: "payment" },
-        { label: "Audit Log", value: "auditLog" },
+        { label: "Audit Log", value: "auditLogs" },
         { label: "Other", value: "other" },
       ],
       defaultValue: "",
