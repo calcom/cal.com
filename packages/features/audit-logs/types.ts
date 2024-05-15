7afd3b53b1b1f12424234aba50da0415c933e109
oliver <8559757+oliverqx@users.noreply.github.com>
2024-05-14 22:57:00 -0600

    remove: unnecessary comments

--- packages/features/audit-logs/types.ts
+++ packages/features/audit-logs/types.ts
@@ -35,13 +35,3 @@ export type AuditLogEvent = {
     [key: string]: string;
   };
 };
-
-// export class AuditLogManagerDummy implements AuditLogsManager {
-//   constructor(appKeys: { apiKey: string; projectId: string }) {
-//     console.log("Audit Log Manager Initiated");
-//   }
-//
-//   report(message: string) {
-//     console.log(message);
-//   }
-// }
