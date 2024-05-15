be8a3144bfd663f743b47b6f5c3b1bdee45fd050
oliver <8559757+oliverqx@users.noreply.github.com>
2024-05-14 22:52:58 -0600

    chore: add AuditLogEvent type

--- packages/features/audit-logs/types.ts
+++ packages/features/audit-logs/types.ts
@@ -2,6 +2,40 @@ export interface AuditLogsManager {
   report(message: string): void;
 }
 
+export type AuditLogEvent = {
+  action: string;
+  group?: {
+    id: string;
+    name?: string;
+  };
+  crud?: string;
+  created?: Date;
+  actor?: {
+    id: string;
+    name?: string;
+    href?: string;
+    fields?: {
+      [key: string]: string;
+    };
+  };
+  target?: {
+    id: string;
+    name?: string;
+    href?: string;
+    type?: string;
+    fields?: {
+      [key: string]: string;
+    };
+  };
+  source_ip?: string;
+  description?: string;
+  is_failure?: boolean;
+  is_anonymous?: boolean;
+  fields?: {
+    [key: string]: string;
+  };
+};
+
 // export class AuditLogManagerDummy implements AuditLogsManager {
 //   constructor(appKeys: { apiKey: string; projectId: string }) {
 //     console.log("Audit Log Manager Initiated");
