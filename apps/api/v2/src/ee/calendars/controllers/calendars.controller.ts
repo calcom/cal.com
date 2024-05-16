fd2bf7c739e1b9cc9fbb568a657eeb2bfb83d650
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-05-16 16:48:07 +0530

    update controllers

--- apps/api/v2/src/ee/calendars/controllers/calendars.controller.ts
+++ apps/api/v2/src/ee/calendars/controllers/calendars.controller.ts
@@ -2,12 +2,31 @@ import { GetBusyTimesOutput } from "@/ee/calendars/outputs/busy-times.output";
 import { ConnectedCalendarsOutput } from "@/ee/calendars/outputs/connected-calendars.output";
 import { CalendarsService } from "@/ee/calendars/services/calendars.service";
 import { GetUser } from "@/modules/auth/decorators/get-user/get-user.decorator";
+import { Permissions } from "@/modules/auth/decorators/permissions/permissions.decorator";
 import { AccessTokenGuard } from "@/modules/auth/guards/access-token/access-token.guard";
+import { PermissionsGuard } from "@/modules/auth/guards/permissions/permissions.guard";
+import { CredentialsRepository } from "@/modules/credentials/credentials.repository";
+import { SelectedCalendarsRepository } from "@/modules/selected-calendars/selected-calendars.repository";
+import { TokensRepository } from "@/modules/tokens/tokens.repository";
 import { UserWithProfile } from "@/modules/users/users.repository";
-import { Controller, Get, UseGuards, Query, HttpStatus, HttpCode, Req } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Query,
+  HttpStatus,
+  HttpCode,
+  Req,
+  Headers,
+  Redirect,
+  BadRequestException,
+  UnauthorizedException,
+} from "@nestjs/common";
 import { ApiTags as DocsTags } from "@nestjs/swagger";
 import { Request } from "express";
+import { z } from "zod";
 
+import { OFFICE_365_CALENDAR_TYPE, APPS_READ } from "@calcom/platform-constants";
 import { SUCCESS_STATUS } from "@calcom/platform-constants";
 import { CalendarBusyTimesInput } from "@calcom/platform-types";
 
@@ -15,11 +34,16 @@ import { CalendarBusyTimesInput } from "@calcom/platform-types";
   path: "/calendars",
   version: "2",
 })
-@UseGuards(AccessTokenGuard)
 @DocsTags("Calendars")
 export class CalendarsController {
-  constructor(private readonly calendarsService: CalendarsService) {}
+  constructor(
+    private readonly calendarsService: CalendarsService,
+    private readonly tokensRepository: TokensRepository,
+    private readonly credentialRepository: CredentialsRepository,
+    private readonly selectedCalendarsRepository: SelectedCalendarsRepository
+  ) {}
 
+  @UseGuards(AccessTokenGuard)
   @Get("/busy-times")
   async getBusyTimes(
     @Query() queryParams: CalendarBusyTimesInput,
@@ -57,12 +81,16 @@ export class CalendarsController {
     };
   }
 
+  @UseGuards(AccessTokenGuard)
   @Get("/office365/connect")
   @HttpCode(HttpStatus.OK)
   async redirect(
-    @Req() req: Request
+    @Req() req: Request,
+    @Headers("Authorization") authorization: string
   ): Promise<{ status: typeof SUCCESS_STATUS; data: { authUrl: Promise<string> } }> {
-    const redirectUrl = this.calendarsService.getRedirectUrl(req);
+    const accessToken = authorization.replace("Bearer ", "");
+    const origin = req.get("origin") ?? req.get("host");
+    const redirectUrl = this.calendarsService.getOffice365CalendarRedirectUrl(accessToken, origin ?? "");
 
     return {
       status: SUCCESS_STATUS,
@@ -72,15 +100,80 @@ export class CalendarsController {
 
   @Get("/office365/save")
   @HttpCode(HttpStatus.OK)
-  async save(): Promise<{ status: "success" }> {
+  @Redirect(undefined, 301)
+  async save(@Query("state") state: string, @Query("code") code: string): Promise<{ url: string }> {
+    // state params contains our user access token
+    const stateParams = new URLSearchParams(state);
+    const { accessToken, origin } = z
+      .object({ accessToken: z.string(), origin: z.string() })
+      .parse({ accessToken: stateParams.get("accessToken"), origin: stateParams.get("origin") });
+
+    // if code is not defined, user denied to authorize office 365 app, just redirect straight away
+    if (!code) {
+      return { url: origin };
+    }
+
+    const parsedCode = z.string().parse(code);
+
+    const ownerId = await this.tokensRepository.getAccessTokenOwnerId(accessToken);
+
+    if (!ownerId) {
+      throw new UnauthorizedException("Invalid Access token.");
+    }
+
+    const office365OAuthCredentials = await this.calendarsService.getOffice365OAuthCredentials(parsedCode);
+
+    const defaultCalendar = await this.calendarsService.getOffice365DefaultCalendar(accessToken);
+
+    if (defaultCalendar?.id) {
+      const credential = await this.credentialRepository.createAppCredential(
+        OFFICE_365_CALENDAR_TYPE,
+        office365OAuthCredentials,
+        ownerId
+      );
+
+      await this.selectedCalendarsRepository.createSelectedCalendar(
+        defaultCalendar.id,
+        credential.id,
+        ownerId,
+        OFFICE_365_CALENDAR_TYPE
+      );
+    }
+
     return {
-      status: SUCCESS_STATUS,
+      url: origin,
     };
   }
 
   @Get("/office365/check")
   @HttpCode(HttpStatus.OK)
-  async check(): Promise<{ status: "success" }> {
+  @UseGuards(AccessTokenGuard, PermissionsGuard)
+  @Permissions([APPS_READ])
+  async check(@GetUser("id") userId: number): Promise<{ status: "success" }> {
+    const office365CalendarCredentials = await this.credentialRepository.getByTypeAndUserId(
+      "office365_calendar",
+      userId
+    );
+
+    if (!office365CalendarCredentials) {
+      throw new BadRequestException("Credentials for office_365_calendar not found.");
+    }
+
+    if (office365CalendarCredentials.invalid) {
+      throw new BadRequestException("Invalid office 365 calendar credentials.");
+    }
+
+    const { connectedCalendars } = await this.calendarsService.getCalendars(userId);
+    const office365Calendar = connectedCalendars.find(
+      (cal: { integration: { type: string } }) => cal.integration.type === OFFICE_365_CALENDAR_TYPE
+    );
+    if (!office365Calendar) {
+      throw new UnauthorizedException("Office 365 calendar not connected.");
+    }
+    if (office365Calendar.error?.message) {
+      throw new UnauthorizedException(office365Calendar.error?.message);
+    }
+
     return {
       status: SUCCESS_STATUS,
     };
