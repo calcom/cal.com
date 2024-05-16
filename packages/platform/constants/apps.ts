af0df733f4c94e6e516565de67fcd5ddc652950d
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-05-16 16:43:07 +0530

    add calendar type and if office 365 calendar

--- packages/platform/constants/apps.ts
+++ packages/platform/constants/apps.ts
@@ -1,6 +1,9 @@
 export const GOOGLE_CALENDAR_TYPE = "google_calendar";
 export const GOOGLE_CALENDAR_ID = "google-calendar";
+export const OFFICE_365_CALENDAR_TYPE = "office365_calendar";
+export const OFFICE_365_CALENDAR_ID = "office365-calendar";
 
 export const APPS_TYPE_ID_MAPPING = {
   [GOOGLE_CALENDAR_TYPE]: GOOGLE_CALENDAR_ID,
+  [OFFICE_365_CALENDAR_TYPE]: OFFICE_365_CALENDAR_ID,
 } as const;
