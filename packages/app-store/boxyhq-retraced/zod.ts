0b4117e087eca79974ca422f3332c5c675de1201
oliver <8559757+oliverqx@users.noreply.github.com>
2024-05-16 12:16:22 -0600

    fix: remove settingsSchema from app template

--- packages/app-store/boxyhq-retraced/zod.ts
+++ packages/app-store/boxyhq-retraced/zod.ts
@@ -1,13 +1,5 @@
 import { z } from "zod";
 
-export const appSettingsSchema = z.object({
-  bookings: z.object({
-    modified: z.object({
-      reschedule: z.boolean(),
-    }),
-  }),
-});
-
 export const appKeysSchema = z.object({
   apiKey: z.string().min(1),
   projectId: z.string().min(1),
