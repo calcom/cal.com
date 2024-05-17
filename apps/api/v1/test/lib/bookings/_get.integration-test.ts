c9fc1ca27ec01c8eaee94913f47b89a7947ad128
Keith Williams <keithwillcode@gmail.com>
2024-05-17 10:53:16 -0400

    More tests for GET bookings

--- apps/api/v1/test/lib/bookings/_get.integration-test.ts
+++ apps/api/v1/test/lib/bookings/_get.integration-test.ts
@@ -15,10 +15,12 @@ const DefaultPagination = {
   skip: 0,
 };
 
-describe("GET /api/bookings", () => {
+describe("GET /api/bookings", async () => {
+  const proUser = await prisma.user.findFirstOrThrow({ where: { email: "pro@example.com" } });
+  const proUserBooking = await prisma.booking.findFirstOrThrow({ where: { userId: proUser.id } });
+
   it("Returns 403 when user has no permission to the bookings of another user", async () => {
     const memberUser = await prisma.user.findFirstOrThrow({ where: { email: "member2-acme@example.com" } });
-    const proUser = await prisma.user.findFirstOrThrow({ where: { email: "pro@example.com" } });
 
     const { req } = createMocks<CustomNextApiRequest, CustomNextApiResponse>({
       method: "GET",
@@ -35,9 +37,6 @@ describe("GET /api/bookings", () => {
   });
 
   it("Returns bookings for regular user", async () => {
-    const proUser = await prisma.user.findFirstOrThrow({ where: { email: "pro@example.com" } });
-    const booking = await prisma.booking.findFirstOrThrow({ where: { userId: proUser.id } });
-
     const { req } = createMocks<CustomNextApiRequest, CustomNextApiResponse>({
       method: "GET",
       pagination: DefaultPagination,
@@ -46,7 +45,40 @@ describe("GET /api/bookings", () => {
     req.userId = proUser.id;
 
     const responseData = await handler(req);
-    expect(responseData.bookings.find((b) => b.id === booking.id)).toBeDefined();
+    expect(responseData.bookings.find((b) => b.id === proUserBooking.id)).toBeDefined();
     expect(responseData.bookings.find((b) => b.userId !== proUser.id)).toBeUndefined();
+  });
+
+  it("Returns bookings for specified user when accessed by system-wide admin", async () => {
+    const { req } = createMocks<CustomNextApiRequest, CustomNextApiResponse>({
+      method: "GET",
+      pagination: DefaultPagination,
+      query: {
+        userId: proUser.id,
+      },
+    });
+
+    req.isSystemWideAdmin = true;
+
+    const responseData = await handler(req);
+    expect(responseData.bookings.find((b) => b.id === proUserBooking.id)).toBeDefined();
+    expect(responseData.bookings.find((b) => b.userId !== proUser.id)).toBeUndefined();
+  });
+
+  // TODO: This test is currently failing but seems to be because of a change in logic
+  // in this PR. Shouldn't a system admin get access to all bookings without having to
+  // pass in userIds?
+  it("Returns bookings for all users when accessed by system-wide admin", async () => {
+    const { req } = createMocks<CustomNextApiRequest, CustomNextApiResponse>({
+      method: "GET",
+      pagination: DefaultPagination,
+    });
+
+    req.isSystemWideAdmin = true;
+
+    const responseData = await handler(req);
+    const groupedUsers = [...new Set(responseData.bookings.map((item) => item.userId))];
+    expect(responseData.bookings.find((b) => b.id === proUserBooking.id)).toBeDefined();
+    expect(groupedUsers.length).toBeGreaterThan(2);
   });
 });
