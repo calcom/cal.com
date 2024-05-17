2c041b8e2256227d02ba8bcc72cb981622c5c993
Udit Takkar <udit222001@gmail.com>
2024-05-17 19:05:07 +0530

    test: add webhook cancelled test

--- apps/web/test/utils/bookingScenario/expects.ts
+++ apps/web/test/utils/bookingScenario/expects.ts
@@ -290,12 +290,16 @@ export function expectWebhookToHaveBeenCalledWith(
 
   if (parsedBody.payload) {
     if (data.payload) {
-      if (data.payload.metadata !== undefined) {
+      if (!!data.payload.metadata) {
         expect(parsedBody.payload.metadata).toEqual(expect.objectContaining(data.payload.metadata));
       }
-      if (data.payload.responses !== undefined)
+      if (!!data.payload.responses)
         expect(parsedBody.payload.responses).toEqual(expect.objectContaining(data.payload.responses));
-      const { responses: _1, metadata: _2, ...remainingPayload } = data.payload;
+
+      if (!!data.payload.organizer)
+        expect(parsedBody.payload.organizer).toEqual(expect.objectContaining(data.payload.organizer));
+
+      const { responses: _1, metadata: _2, organizer: _3, ...remainingPayload } = data.payload;
       expect(parsedBody.payload).toEqual(expect.objectContaining(remainingPayload));
     }
   }
