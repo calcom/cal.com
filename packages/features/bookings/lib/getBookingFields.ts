062c9e72b3b13b221edbc4afede883f2fe98b6a7
Udit Takkar <udit222001@gmail.com>
2024-05-21 23:28:03 +0530

    chore: change label

--- packages/features/bookings/lib/getBookingFields.ts
+++ packages/features/bookings/lib/getBookingFields.ts
@@ -201,7 +201,7 @@ export const ensureBookingInputsHaveSystemFields = ({
   ];
   if (isTeamEvent) {
     systemBeforeFields.splice(2, 0, {
-      defaultLabel: "attendee_phone_number",
+      defaultLabel: "phone_number",
       type: "phone",
       name: "attendeePhoneNumber",
       required: true,
