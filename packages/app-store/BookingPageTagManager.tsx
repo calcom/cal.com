36729c433b35f4490ebda55b8a1a651d5186f915
Hariom <hariombalhara@gmail.com>
2024-05-23 14:46:58 +0530

    Send all events to analytics apps

--- packages/app-store/BookingPageTagManager.tsx
+++ packages/app-store/BookingPageTagManager.tsx
@@ -1,28 +1,77 @@
 import Script from "next/script";
+import type { Tag } from "types";
 
 import { getEventTypeAppData } from "@calcom/app-store/_utils/getEventTypeAppData";
 import { appStoreMetadata } from "@calcom/app-store/bookerAppsMetaData";
+import { sdkActionManager } from "@calcom/lib/sdk-event";
+import type { AppMeta } from "@calcom/types/App";
 
 import type { appDataSchemas } from "./apps.schemas.generated";
 
+const PushEventPrefix = "cal_analytics_app_";
+
+// AnalyticApp has appData.tag always set
+type AnalyticApp = Omit<AppMeta, "appData"> & {
+  appData: Omit<NonNullable<AppMeta["appData"]>, "tag"> & {
+    tag: NonNullable<NonNullable<AppMeta["appData"]>["tag"]>;
+  };
+};
+
+// Setup listener for all events to push to analytics apps
+if (typeof window !== "undefined") {
+  sdkActionManager?.on("*", (event) => {
+    const { type: name, ...data } = event.detail;
+    // Don't push internal events to analytics apps
+    // They are meant for internal use like helping embed make some decisions
+    if (name.startsWith("__")) {
+      return;
+    }
+    Object.entries(window).forEach(([prop, value]) => {
+      if (!prop.startsWith(PushEventPrefix) || typeof value !== "function") {
+        return;
+      }
+      const pushEvent = window[prop as keyof typeof window];
+
+      pushEvent({
+        name,
+        data,
+      });
+    });
+  });
+}
 export default function BookingPageTagManager({
   eventType,
 }: {
   eventType: Parameters<typeof getEventTypeAppData>[0];
 }) {
+  const analyticsApps = Object.entries(appStoreMetadata).reduce(
+    (acc, entry) => {
+      const [appId, app] = entry;
+      const eventTypeAppData = getEventTypeAppData(eventType, appId as keyof typeof appDataSchemas);
+
+      if (!eventTypeAppData || !app.appData?.tag) {
+        return acc;
+      }
+
+      acc[appId] = {
+        meta: app as AnalyticApp,
+        eventTypeAppData: eventTypeAppData,
+      };
+      return acc;
+    },
+    {} as Record<
+      string,
+      {
+        meta: AnalyticApp;
+        eventTypeAppData: ReturnType<typeof getEventTypeAppData>;
+      }
+    >
+  );
+
   return (
     <>
-      {Object.entries(appStoreMetadata).map(([appId, app]) => {
-        const tag = app.appData?.tag;
-        if (!tag) {
-          return null;
-        }
-
-        const appData = getEventTypeAppData(eventType, appId as keyof typeof appDataSchemas);
-
-        if (!appData) {
-          return null;
-        }
+      {Object.entries(analyticsApps).map(([appId, { meta: app, eventTypeAppData }]) => {
+        const tag = app.appData.tag;
 
         const parseValue = <T extends string | undefined>(val: T): T => {
           if (!val) {
@@ -34,18 +83,19 @@ export default function BookingPageTagManager({
           let matches;
           while ((matches = regex.exec(val))) {
             const variableName = matches[1];
-            if (appData[variableName]) {
+            if (eventTypeAppData[variableName]) {
               // Replace if value is available. It can possible not be a template variable that just matches the regex.
               val = val.replace(
                 new RegExp(`{${variableName}}`, "g"),
-                appData[variableName]
+                eventTypeAppData[variableName]
               ) as NonNullable<T>;
             }
           }
           return val;
         };
 
-        return tag.scripts.map((script, index) => {
+        const pushEventScript = getPushEventScript({ tag, appId });
+        return tag.scripts.concat(pushEventScript ? [pushEventScript] : []).map((script, index) => {
           const parsedAttributes: NonNullable<(typeof tag.scripts)[number]["attrs"]> = {};
           const attrs = script.attrs || {};
           Object.entries(attrs).forEach(([name, value]) => {
@@ -72,3 +122,13 @@ export default function BookingPageTagManager({
     </>
   );
 }
+
+const getPushEventScript = ({ tag, appId }: { tag: Tag; appId: string }) => {
+  if (!tag.pushEventScript) {
+    return tag.pushEventScript;
+  }
+  return {
+    ...tag.pushEventScript,
+    content: tag.pushEventScript?.content?.replace("$pushEvent", `${PushEventPrefix}_${appId}`),
+  };
+};
