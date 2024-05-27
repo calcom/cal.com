1c5370aca06aff05f1fc5996ae79673fd7d9318e
oliver <8559757+oliverqx@users.noreply.github.com>
2024-05-27 09:19:47 -0700

    feat: create trpc middleware plugin

---
+++ packages/features/audit-logs/lib/trpc-plugin.ts
@@ -0,0 +1,28 @@
+import { handleAuditLogTrigger } from "@calcom/features/audit-logs/lib/handleAuditLogTrigger";
+import { AuditLogTriggerEvents } from "@calcom/features/audit-logs/types";
+import { AuditLogTriggerTargets } from "@calcom/prisma/enums";
+import type { UserFromSession } from "@calcom/trpc/server/middlewares/sessionMiddleware";
+import type { TrpcSessionUser } from "@calcom/trpc/server/trpc";
+
+import { experimental_trpcMiddleware } from "@trpc/server";
+
+export const auditLogMiddleware = experimental_trpcMiddleware<{
+  ctx: { user: NonNullable<TrpcSessionUser> }; // defaults to 'object' if not defined
+}>().create(async (opts) => {
+  const { user }: { user: UserFromSession } = opts.ctx;
+
+  await handleAuditLogTrigger({
+    event: {
+      action: AuditLogTriggerEvents[opts.path as keyof typeof AuditLogTriggerEvents],
+      actor: {
+        id: user.id,
+      },
+      target: {
+        name: AuditLogTriggerTargets.APPS,
+      },
+    },
+    userId: user.id,
+  });
+
+  return opts.next({ ctx: { user: user } });
+});
