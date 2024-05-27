cbc6b4d99a599c95a1b0dcaca1c349a7fbe5b456
Peer Richelsen <peeroke@gmail.com>
2024-05-27 11:35:52 +0200

    chore: added event-types link to settings (#14274)
    * added event-types link
    
    * fix: add item in settings layout
    
    ---------
    
    Co-authored-by: Udit Takkar <53316345+Udit-takkar@users.noreply.github.com>
    Co-authored-by: Udit Takkar <udit222001@gmail.com>

--- packages/features/settings/layouts/SettingsLayout.tsx
+++ packages/features/settings/layouts/SettingsLayout.tsx
@@ -319,6 +319,12 @@ const TeamListCollapsible = () => {
                     textClassNames="px-3 text-emphasis font-medium text-sm"
                     disableChevron
                   />
+                  <VerticalTabItem
+                    name={t("event_types_page_title")}
+                    href={`/event-types?teamIds=${team.id}`}
+                    textClassNames="px-3 text-emphasis font-medium text-sm"
+                    disableChevron
+                  />
                   {(team.role === MembershipRole.OWNER ||
                     team.role === MembershipRole.ADMIN ||
                     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -580,7 +586,6 @@ const SettingsSidebarContainer = ({
                                   textClassNames="px-3 text-emphasis font-medium text-sm"
                                   disableChevron
                                 />
-
                                 <>
                                   {/* TODO: enable appearance edit */}
                                   {/* <VerticalTabItem
