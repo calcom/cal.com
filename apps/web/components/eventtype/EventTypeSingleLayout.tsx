e3e135c61be0426ca85950e5c8a741c398e891a1
Sujit Ale Magar <128675355+Sujitale07@users.noreply.github.com>
2024-05-27 19:55:53 +0545

    fix: Event Type Toggle Shift (#15066)
    Co-authored-by: Udit Takkar <53316345+Udit-takkar@users.noreply.github.com>

--- apps/web/components/eventtype/EventTypeSingleLayout.tsx
+++ apps/web/components/eventtype/EventTypeSingleLayout.tsx
@@ -323,7 +323,7 @@ function EventTypeSingleLayout({
               <div
                 className={classNames(
                   "sm:hover:bg-muted hidden cursor-pointer items-center rounded-md",
-                  formMethods.watch("hidden") ? "px-2" : "",
+                  formMethods.watch("hidden") ? "pl-2" : "",
                   "lg:flex"
                 )}>
                 {formMethods.watch("hidden") && (
