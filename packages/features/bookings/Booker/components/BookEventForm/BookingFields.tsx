54f8dfda3b751e9b7193bc5a21aafe8c0a5c8a1c
Anik Dhabal Babu <adhabal2002@gmail.com>
2024-05-29 14:23:07 +0000

    update

--- packages/features/bookings/Booker/components/BookEventForm/BookingFields.tsx
+++ packages/features/bookings/Booker/components/BookEventForm/BookingFields.tsx
@@ -85,7 +85,7 @@ export const BookingFields = ({
 
         // Attendee location field can be edited during reschedule
         if (field.name === SystemField.Enum.location) {
-          if (locationResponse?.value === "attendeeInPerson" || "phone" || "somewhereElse") {
+          if (["attendeeInPerson", "phone", "somewhereElse"].includes(locationResponse?.value)) {
             readOnly = false;
           }
         }
