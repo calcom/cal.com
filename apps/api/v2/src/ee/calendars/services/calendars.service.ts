d28333fbf67cc61a1343e53b34a1b47a7885f71c
Rajiv Sahal <rajivsahal@Rajivs-MacBook-Pro.local>
2024-05-30 16:42:39 +0530

    cleanup

--- apps/api/v2/src/ee/calendars/services/calendars.service.ts
+++ apps/api/v2/src/ee/calendars/services/calendars.service.ts
@@ -6,7 +6,6 @@ import {
 import { PrismaReadService } from "@/modules/prisma/prisma-read.service";
 import { PrismaWriteService } from "@/modules/prisma/prisma-write.service";
 import { UsersRepository } from "@/modules/users/users.repository";
-import type { Calendar as OfficeCalendar } from "@microsoft/microsoft-graph-types-beta";
 import {
   Injectable,
   InternalServerErrorException,
@@ -16,10 +15,8 @@ import {
 import { ConfigService } from "@nestjs/config";
 import { User } from "@prisma/client";
 import { DateTime } from "luxon";
-import { stringify } from "querystring";
 import { z } from "zod";
 
-import { OFFICE_365_CALENDAR_ID } from "@calcom/platform-constants";
 import { getConnectedDestinationCalendars } from "@calcom/platform-libraries";
 import { getBusyCalendarTimes } from "@calcom/platform-libraries";
 import { Calendar } from "@calcom/platform-types";
@@ -28,7 +25,6 @@ import { PrismaClient } from "@calcom/prisma";
 @Injectable()
 export class CalendarsService {
   private oAuthCalendarResponseSchema = z.object({ client_id: z.string(), client_secret: z.string() });
-  private redirectUri = `${this.config.get("api.url")}/calendars/office365/save`;
 
   constructor(
     private readonly usersRepository: UsersRepository,
@@ -140,69 +136,5 @@ export class CalendarsService {
     }
 
     return { client_id, client_secret };
-  }
-
-  async getOffice365CalendarRedirectUrl(accessToken: string, origin: string) {
-    const { client_id } = await this.getAppKeys(OFFICE_365_CALENDAR_ID);
-
-    const scopes = ["User.Read", "Calendars.Read", "Calendars.ReadWrite", "offline_access"];
-    const params = {
-      response_type: "code",
-      scope: scopes.join(" "),
-      client_id,
-      prompt: "select_account",
-      redirect_uri: this.redirectUri,
-      state: `accessToken=${accessToken}&origin=${origin}`,
-    };
-
-    const query = stringify(params);
-
-    const url = `https://login.microsoftonline.com/common/oauth2/v2.0/authorize?${query}`;
-
-    return url;
-  }
-
-  async getOffice365OAuthCredentials(code: string) {
-    const scopes = ["offline_access", "Calendars.Read", "Calendars.ReadWrite"];
-    const { client_id, client_secret } = await this.getAppKeys(OFFICE_365_CALENDAR_ID);
-
-    const toUrlEncoded = (payload: Record<string, string>) =>
-      Object.keys(payload)
-        .map((key) => `${key}=${encodeURIComponent(payload[key])}`)
-        .join("&");
-
-    const body = toUrlEncoded({
-      client_id,
-      grant_type: "authorization_code",
-      code,
-      scope: scopes.join(" "),
-      redirect_uri: this.redirectUri,
-      client_secret,
-    });
-
-    const response = await fetch("https://login.microsoftonline.com/common/oauth2/v2.0/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-      },
-      body,
-    });
-
-    const responseBody = await response.json();
-
-    return responseBody;
-  }
-
-  async getOffice365DefaultCalendar(accessToken: string): Promise<OfficeCalendar> {
-    const response = await fetch("https://graph.microsoft.com/v1.0/me/calendar", {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-        "Content-Type": "application/json",
-      },
-    });
-    const responseBody = await response.json();
-
-    return responseBody as OfficeCalendar;
   }
 }
