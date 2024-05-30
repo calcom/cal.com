3cc149c04b652233d381deabdd996d2e696dc538
Rajiv Sahal <rajivsahal@Rajivs-MacBook-Pro.local>
2024-05-30 16:43:37 +0530

    make all calendar endpoints dynamic

--- apps/api/v2/src/ee/calendars/controllers/calendars.controller.ts
+++ apps/api/v2/src/ee/calendars/controllers/calendars.controller.ts
@@ -23,13 +23,12 @@ import {
   Headers,
   Redirect,
   BadRequestException,
-  UnauthorizedException,
 } from "@nestjs/common";
 import { ApiTags as DocsTags } from "@nestjs/swagger";
 import { Request } from "express";
 import { z } from "zod";
 
-import { OFFICE_365_CALENDAR_TYPE, APPS_READ } from "@calcom/platform-constants";
+import { APPS_READ } from "@calcom/platform-constants";
 import {
   SUCCESS_STATUS,
   CALENDARS,
@@ -112,84 +111,51 @@ export class CalendarsController {
     }
   }
 
-  @Get("/office365/save")
+  @Get("/:calendar/save")
   @HttpCode(HttpStatus.OK)
   @Redirect(undefined, 301)
-  async save(@Query("state") state: string, @Query("code") code: string): Promise<{ url: string }> {
+  async save(
+    @Query("state") state: string,
+    @Query("code") code: string,
+    @Param("calendar") calendar: string
+  ): Promise<{ url: string }> {
     // state params contains our user access token
     const stateParams = new URLSearchParams(state);
     const { accessToken, origin } = z
       .object({ accessToken: z.string(), origin: z.string() })
       .parse({ accessToken: stateParams.get("accessToken"), origin: stateParams.get("origin") });
 
-    // if code is not defined, user denied to authorize office 365 app, just redirect straight away
-    if (!code) {
-      return { url: origin };
+    switch (calendar) {
+      case MICROSOFT_OUTLOOK_CALENDAR:
+        return await this.outlookService.save(code, accessToken, origin);
+      case GOOGLE_CALENDAR:
+        return await this.googleCalendarService.save(code, accessToken, origin);
+      default:
+        throw new BadRequestException(
+          "Invalid calendar type, available calendars are: ",
+          CALENDARS.join(", ")
+        );
     }
-
-    const parsedCode = z.string().parse(code);
-
-    const ownerId = await this.tokensRepository.getAccessTokenOwnerId(accessToken);
-
-    if (!ownerId) {
-      throw new UnauthorizedException("Invalid Access token.");
-    }
-
-    const office365OAuthCredentials = await this.calendarsService.getOffice365OAuthCredentials(parsedCode);
-
-    const defaultCalendar = await this.calendarsService.getOffice365DefaultCalendar(accessToken);
-
-    if (defaultCalendar?.id) {
-      const credential = await this.credentialRepository.createAppCredential(
-        OFFICE_365_CALENDAR_TYPE,
-        office365OAuthCredentials,
-        ownerId
-      );
-
-      await this.selectedCalendarsRepository.createSelectedCalendar(
-        defaultCalendar.id,
-        credential.id,
-        ownerId,
-        OFFICE_365_CALENDAR_TYPE
-      );
-    }
-
-    return {
-      url: origin,
-    };
   }
 
-  @Get("/office365/check")
+  @Get("/:calendar/check")
   @HttpCode(HttpStatus.OK)
   @UseGuards(AccessTokenGuard, PermissionsGuard)
   @Permissions([APPS_READ])
-  async check(@GetUser("id") userId: number): Promise<{ status: "success" }> {
-    const office365CalendarCredentials = await this.credentialRepository.getByTypeAndUserId(
-      "office365_calendar",
-      userId
-    );
-
-    if (!office365CalendarCredentials) {
-      throw new BadRequestException("Credentials for office_365_calendar not found.");
+  async check(
+    @GetUser("id") userId: number,
+    @Param("calendar") calendar: string
+  ): Promise<{ status: string }> {
+    switch (calendar) {
+      case MICROSOFT_OUTLOOK_CALENDAR:
+        return await this.outlookService.check(userId);
+      case GOOGLE_CALENDAR:
+        return await this.googleCalendarService.check(userId);
+      default:
+        throw new BadRequestException(
+          "Invalid calendar type, available calendars are: ",
+          CALENDARS.join(", ")
+        );
     }
-
-    if (office365CalendarCredentials.invalid) {
-      throw new BadRequestException("Invalid office 365 calendar credentials.");
-    }
-
-    const { connectedCalendars } = await this.calendarsService.getCalendars(userId);
-    const office365Calendar = connectedCalendars.find(
-      (cal: { integration: { type: string } }) => cal.integration.type === OFFICE_365_CALENDAR_TYPE
-    );
-    if (!office365Calendar) {
-      throw new UnauthorizedException("Office 365 calendar not connected.");
-    }
-    if (office365Calendar.error?.message) {
-      throw new UnauthorizedException(office365Calendar.error?.message);
-    }
-
-    return {
-      status: SUCCESS_STATUS,
-    };
   }
 }
