80fc77748a15bbf8b14e44d70892c4d713e44662
Rajiv Sahal <rajivsahal@Rajivs-MacBook-Pro.local>
2024-05-30 16:44:15 +0530

    calendar app constants

--- packages/platform/constants/apps.ts
+++ packages/platform/constants/apps.ts
@@ -2,6 +2,10 @@ export const GOOGLE_CALENDAR_TYPE = "google_calendar";
 export const GOOGLE_CALENDAR_ID = "google-calendar";
 export const OFFICE_365_CALENDAR_TYPE = "office365_calendar";
 export const OFFICE_365_CALENDAR_ID = "office365-calendar";
+export const GOOGLE_CALENDAR = "google";
+export const MICROSOFT_OUTLOOK_CALENDAR = "google";
+export const APPLE_CALENDAR = "apple";
+export const CALENDARS = [GOOGLE_CALENDAR, APPLE_CALENDAR, MICROSOFT_OUTLOOK_CALENDAR] as const;
 
 export const APPS_TYPE_ID_MAPPING = {
   [GOOGLE_CALENDAR_TYPE]: GOOGLE_CALENDAR_ID,
