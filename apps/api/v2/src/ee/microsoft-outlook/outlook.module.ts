bbd9bdf838a6f77975428e600453daf0efbfe5cc
Rajiv Sahal <rajivsahal@Rajivs-MacBook-Pro.local>
2024-05-30 16:48:17 +0530

    cleanup

--- apps/api/v2/src/ee/microsoft-outlook/outlook.module.ts
+++
@@ -1,7 +0,0 @@
-import { MicrosoftOutlookService } from "@/ee/microsoft-outlook/outlook.service";
-import { Module } from "@nestjs/common";
-
-@Module({
-  exports: [MicrosoftOutlookService],
-})
-export class MicrosoftOutlookModule {}
