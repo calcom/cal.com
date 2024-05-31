254296d163c0012d1740903b043752c99b62333e
sean-brydon <sean@cal.com>
2024-05-31 10:26:51 +0100

    restore checkLicense from main

--- packages/features/ee/common/server/checkLicense.ts
+++ packages/features/ee/common/server/checkLicense.ts
@@ -1,11 +1,23 @@
 import cache from "memory-cache";
 import { z } from "zod";
 
+import { CONSOLE_URL } from "@calcom/lib/constants";
 import type { PrismaClient } from "@calcom/prisma";
 
 const CACHING_TIME = 86400000; // 24 hours in milliseconds
 
-const schemaLicenseKey = z.string();
+const schemaLicenseKey = z
+  .string()
+  // .uuid() exists but I'd to fix the situation where the CALCOM_LICENSE_KEY is wrapped in quotes
+  .regex(/^\"?[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}\"?$/, {
+    message: "License key must follow UUID format: 8-4-4-4-12",
+  })
+  .transform((v) => {
+    // Remove the double quotes from the license key, as they 404 the fetch.
+    return v != null && v.length >= 2 && v.charAt(0) == '"' && v.charAt(v.length - 1) == '"'
+      ? v.substring(1, v.length - 1)
+      : v;
+  });
 
 async function checkLicense(
   /** The prisma client to use (necessary for public API to handle custom prisma instances) */
@@ -21,13 +33,7 @@ async function checkLicense(
     licenseKey = deployment?.licenseKey ?? undefined;
   }
   if (!licenseKey) return false;
-
-  /**
-   * The console URL for this will be used for any user that has not upgraded to latest as it proxies
-   * to our private-api
-   * const url = `${CONSOLE_URL}/api/license?key=${schemaLicenseKey.parse(licenseKey)}`;
-   */
-  const url = `${process.env.CALCOM_PRIVATE_API_ROUTE}/api/license/${schemaLicenseKey.parse(licenseKey)}`;
+  const url = `${CONSOLE_URL}/api/license?key=${schemaLicenseKey.parse(licenseKey)}`;
   const cachedResponse = cache.get(url);
   if (cachedResponse) {
     return cachedResponse;
