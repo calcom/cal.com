40864658243aebef8cf33b89f407c152fbc7a4de
Rajiv Sahal <rajivsahal@Rajivs-MacBook-Pro.local>
2024-06-01 21:49:10 +0530

    rename microsoft calendar type

--- packages/platform/constants/apps.ts
+++ packages/platform/constants/apps.ts
@@ -3,9 +3,9 @@ export const GOOGLE_CALENDAR_ID = "google-calendar";
 export const OFFICE_365_CALENDAR_TYPE = "office365_calendar";
 export const OFFICE_365_CALENDAR_ID = "office365-calendar";
 export const GOOGLE_CALENDAR = "google";
-export const MICROSOFT_OUTLOOK_CALENDAR = "google";
+export const OFFICE_365_CALENDAR = "office365";
 export const APPLE_CALENDAR = "apple";
-export const CALENDARS = [GOOGLE_CALENDAR, APPLE_CALENDAR, MICROSOFT_OUTLOOK_CALENDAR] as const;
+export const CALENDARS = [GOOGLE_CALENDAR, APPLE_CALENDAR, OFFICE_365_CALENDAR] as const;
 
 export const APPS_TYPE_ID_MAPPING = {
   [GOOGLE_CALENDAR_TYPE]: GOOGLE_CALENDAR_ID,
