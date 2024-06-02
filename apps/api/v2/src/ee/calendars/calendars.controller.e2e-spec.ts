772924331b43e47ad0ee2d39eead2c1becca4caf
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2024-06-03 03:10:35 +0530

    some more e2e tests including  google calendar

--- apps/api/v2/src/ee/calendars/calendars.controller.e2e-spec.ts
+++ apps/api/v2/src/ee/calendars/calendars.controller.e2e-spec.ts
@@ -17,6 +17,12 @@ import { TeamRepositoryFixture } from "test/fixtures/repository/team.repository.
 import { TokensRepositoryFixture } from "test/fixtures/repository/tokens.repository.fixture";
 import { UserRepositoryFixture } from "test/fixtures/repository/users.repository.fixture";
 
+import {
+  GOOGLE_CALENDAR,
+  OFFICE_365_CALENDAR,
+  GOOGLE_CALENDAR_TYPE,
+  GOOGLE_CALENDAR_ID,
+} from "@calcom/platform-constants";
 import { OFFICE_365_CALENDAR_ID, OFFICE_365_CALENDAR_TYPE } from "@calcom/platform-constants";
 
 const CLIENT_REDIRECT_URI = "http://localhost:5555";
@@ -47,6 +53,7 @@ describe("Platform Calendars Endpoints", () => {
   let credentialsRepositoryFixture: CredentialsRepositoryFixture;
   let user: User;
   let office365Credentials: Credential;
+  let googleCalendarCredentials: Credential;
   let accessTokenSecret: string;
   let refreshTokenSecret: string;
 
@@ -102,16 +109,16 @@ describe("Platform Calendars Endpoints", () => {
     expect(user).toBeDefined();
   });
 
-  it(`/GET/v2/calendars/office365/connect: it should respond 401 with invalid access token`, async () => {
+  it(`/GET/v2/calendars/${OFFICE_365_CALENDAR}/connect: it should respond 401 with invalid access token`, async () => {
     await request(app.getHttpServer())
-      .get(`/v2/calendars/office365/connect`)
+      .get(`/v2/calendars/${OFFICE_365_CALENDAR}/connect`)
       .set("Authorization", `Bearer invalid_access_token`)
       .expect(401);
   });
 
-  it(`/GET/v2/calendars/office365/connect: it should redirect to auth-url for office 365 calendar oauth with valid access token `, async () => {
+  it(`/GET/v2/calendars/${OFFICE_365_CALENDAR}/connect: it should redirect to auth-url for office 365 calendar oauth with valid access token `, async () => {
     const response = await request(app.getHttpServer())
-      .get(`/v2/calendars/office365/connect`)
+      .get(`/v2/calendars/${OFFICE_365_CALENDAR}/connect`)
       .set("Authorization", `Bearer ${accessTokenSecret}`)
       .set("Origin", CLIENT_REDIRECT_URI)
       .expect(200);
@@ -119,35 +126,53 @@ describe("Platform Calendars Endpoints", () => {
     expect(data.authUrl).toBeDefined();
   });
 
-  it(`/GET/v2/calendars/office365/save: without access token`, async () => {
-    await request(app.getHttpServer())
-      .get(
-        `/v2/calendars/office365/save?state=accessToken=${accessTokenSecret}&code=4/0AfJohXmBuT7QVrEPlAJLBu4ZcSnyj5jtDoJqSW_riPUhPXQ70RPGkOEbVO3xs-OzQwpPQw&scope=User.Read%20Calendars.Read%20Calendars.ReadWrite%20offline_access`
-      )
-      .expect(400);
+  it(`/GET/v2/calendars/${GOOGLE_CALENDAR}/connect: it should redirect to auth-url for google calendar oauth with valid access token `, async () => {
+    const response = await request(app.getHttpServer())
+      .get(`/v2/calendars/${GOOGLE_CALENDAR}/connect`)
+      .set("Authorization", `Bearer ${accessTokenSecret}`)
+      .set("Origin", CLIENT_REDIRECT_URI)
+      .expect(200);
+    const data = response.body.data;
+    expect(data.authUrl).toBeDefined();
   });
 
-  it(`/GET/v2/calendars/office365/save: without origin`, async () => {
+  it(`/GET/v2/calendars/random-calendar/connect: it should respond 400 with a message saying the calendar type is invalid`, async () => {
     await request(app.getHttpServer())
-      .get(
-        `/v2/calendars/office365/save?state=accessToken=${accessTokenSecret}&code=4/0AfJohXmBuT7QVrEPlAJLBu4ZcSnyj5jtDoJqSW_riPUhPXQ70RPGkOEbVO3xs-OzQwpPQw&scope=User.Read%20Calendars.Read%20Calendars.ReadWrite%20offline_access`
-      )
-      .expect(400);
-  });
-
-  it(`/GET/v2/calendars/office365/check without access token`, async () => {
-    await request(app.getHttpServer()).get(`/v2/calendars/office365/check`).expect(401);
-  });
-
-  it(`/GET/v2/calendars/office365/check with no credentials`, async () => {
-    await request(app.getHttpServer())
-      .get(`/v2/calendars/office365/check`)
+      .get(`/v2/calendars/random-calendar/connect`)
       .set("Authorization", `Bearer ${accessTokenSecret}`)
       .set("Origin", CLIENT_REDIRECT_URI)
       .expect(400);
   });
 
-  it(`/GET/v2/calendars/office365/check with access token, origin and office365 credentials`, async () => {
+  it(`/GET/v2/calendars/${OFFICE_365_CALENDAR}/save: without access token`, async () => {
+    await request(app.getHttpServer())
+      .get(
+        `/v2/calendars/${OFFICE_365_CALENDAR}/save?state=accessToken=${accessTokenSecret}&code=4/0AfJohXmBuT7QVrEPlAJLBu4ZcSnyj5jtDoJqSW_riPUhPXQ70RPGkOEbVO3xs-OzQwpPQw&scope=User.Read%20Calendars.Read%20Calendars.ReadWrite%20offline_access`
+      )
+      .expect(400);
+  });
+
+  it(`/GET/v2/calendars/${OFFICE_365_CALENDAR}/save: without origin`, async () => {
+    await request(app.getHttpServer())
+      .get(
+        `/v2/calendars/${OFFICE_365_CALENDAR}/save?state=accessToken=${accessTokenSecret}&code=4/0AfJohXmBuT7QVrEPlAJLBu4ZcSnyj5jtDoJqSW_riPUhPXQ70RPGkOEbVO3xs-OzQwpPQw&scope=User.Read%20Calendars.Read%20Calendars.ReadWrite%20offline_access`
+      )
+      .expect(400);
+  });
+
+  it(`/GET/v2/calendars/${OFFICE_365_CALENDAR}/check without access token`, async () => {
+    await request(app.getHttpServer()).get(`/v2/calendars/${OFFICE_365_CALENDAR}/check`).expect(401);
+  });
+
+  it(`/GET/v2/calendars/${OFFICE_365_CALENDAR}/check with no credentials`, async () => {
+    await request(app.getHttpServer())
+      .get(`/v2/calendars/${OFFICE_365_CALENDAR}/check`)
+      .set("Authorization", `Bearer ${accessTokenSecret}`)
+      .set("Origin", CLIENT_REDIRECT_URI)
+      .expect(400);
+  });
+
+  it(`/GET/v2/calendars/${OFFICE_365_CALENDAR}/check with access token, origin and office365 credentials`, async () => {
     office365Credentials = await credentialsRepositoryFixture.create(
       OFFICE_365_CALENDAR_TYPE,
       {},
@@ -155,7 +180,21 @@ describe("Platform Calendars Endpoints", () => {
       OFFICE_365_CALENDAR_ID
     );
     await request(app.getHttpServer())
-      .get(`/v2/calendars/office365/check`)
+      .get(`/v2/calendars/${OFFICE_365_CALENDAR}/check`)
+      .set("Authorization", `Bearer ${accessTokenSecret}`)
+      .set("Origin", CLIENT_REDIRECT_URI)
+      .expect(200);
+  });
+
+  it(`/GET/v2/calendars/${GOOGLE_CALENDAR}/check with access token, origin and google calendar credentials`, async () => {
+    googleCalendarCredentials = await credentialsRepositoryFixture.create(
+      GOOGLE_CALENDAR_TYPE,
+      {},
+      user.id,
+      GOOGLE_CALENDAR_ID
+    );
+    await request(app.getHttpServer())
+      .get(`/v2/calendars/${GOOGLE_CALENDAR}/check`)
       .set("Authorization", `Bearer ${accessTokenSecret}`)
       .set("Origin", CLIENT_REDIRECT_URI)
       .expect(200);
@@ -165,6 +204,7 @@ describe("Platform Calendars Endpoints", () => {
     await oauthClientRepositoryFixture.delete(oAuthClient.id);
     await teamRepositoryFixture.delete(organization.id);
     await credentialsRepositoryFixture.delete(office365Credentials.id);
+    await credentialsRepositoryFixture.delete(googleCalendarCredentials.id);
     await userRepositoryFixture.deleteByEmail(user.email);
     await app.close();
   });
