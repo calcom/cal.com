5c851f9a549dd8a65b2d41813069526c604c0bf6
Rajiv Sahal <sahalrajiv-extc@atharvacoe.ac.in>
2024-06-03 17:17:57 +0530

    feat: outlook 365 calendar endpoints (#15077)
    * init endpoints for office 365 syncing
    
    * get web app url for oauth
    
    * update typings
    
    * nethods for office 365 calendar
    
    * update office 365 connect endpoint
    
    * update packages
    
    * add calendar type and if office 365 calendar
    
    * add SelectedCalendarsRepository and TokensModule
    
    * add functions for getting oauth credentials, getting default calendars and redirect url for office 365 calendar
    
    * update controllers
    
    * e2e tests for office 365 calendar endpoints
    
    * fixup! Merge branch 'main' into rajiv/cal-3544-outlookoffice365-integration-connect-atom
    
    * fixup! fixup! Merge branch 'main' into rajiv/cal-3544-outlookoffice365-integration-connect-atom
    
    * fix merge conflict
    
    * update typings
    
    * abstract microsoft outlook into its own module
    
    * add one common interface for calendar apps
    
    * make calendars dynamic
    
    * abstract every calendar app into its own service
    
    * cleanup
    
    * make all calendar endpoints dynamic
    
    * calendar app constants
    
    * cleanup
    
    * remove unused repositories
    
    * rename microsoft calendar type
    
    * fix typings
    
    * add google and outlook calendar service to calendars module
    
    * fixup
    
    * some more e2e tests including  google calendar
    
    * chore: small type change and apple calendar not implemented yet
    
    ---------
    
    Co-authored-by: Morgan <33722304+ThyMinimalDev@users.noreply.github.com>
    Co-authored-by: Morgan Vernay <morgan@cal.com>
    Co-authored-by: Rajiv Sahal <rajivsahal@Rajivs-MacBook-Pro.local>

--- apps/api/v2/src/ee/calendars/controllers/calendars.controller.ts
+++ apps/api/v2/src/ee/calendars/controllers/calendars.controller.ts
@@ -1,24 +1,47 @@
 import { GetBusyTimesOutput } from "@/ee/calendars/outputs/busy-times.output";
 import { ConnectedCalendarsOutput } from "@/ee/calendars/outputs/connected-calendars.output";
 import { CalendarsService } from "@/ee/calendars/services/calendars.service";
+import { GoogleCalendarService } from "@/ee/calendars/services/gcal.service";
+import { OutlookService } from "@/ee/calendars/services/outlook.service";
 import { GetUser } from "@/modules/auth/decorators/get-user/get-user.decorator";
+import { Permissions } from "@/modules/auth/decorators/permissions/permissions.decorator";
 import { AccessTokenGuard } from "@/modules/auth/guards/access-token/access-token.guard";
+import { PermissionsGuard } from "@/modules/auth/guards/permissions/permissions.guard";
 import { UserWithProfile } from "@/modules/users/users.repository";
-import { Controller, Get, UseGuards, Query } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Query,
+  HttpStatus,
+  HttpCode,
+  Req,
+  Param,
+  Headers,
+  Redirect,
+  BadRequestException,
+} from "@nestjs/common";
 import { ApiTags as DocsTags } from "@nestjs/swagger";
+import { Request } from "express";
+import { z } from "zod";
 
-import { SUCCESS_STATUS } from "@calcom/platform-constants";
-import { CalendarBusyTimesInput } from "@calcom/platform-types";
+import { APPS_READ } from "@calcom/platform-constants";
+import { SUCCESS_STATUS, CALENDARS, GOOGLE_CALENDAR, OFFICE_365_CALENDAR } from "@calcom/platform-constants";
+import { ApiResponse, CalendarBusyTimesInput } from "@calcom/platform-types";
 
 @Controller({
   path: "/calendars",
   version: "2",
 })
-@UseGuards(AccessTokenGuard)
 @DocsTags("Calendars")
 export class CalendarsController {
-  constructor(private readonly calendarsService: CalendarsService) {}
+  constructor(
+    private readonly calendarsService: CalendarsService,
+    private readonly outlookService: OutlookService,
+    private readonly googleCalendarService: GoogleCalendarService
+  ) {}
 
+  @UseGuards(AccessTokenGuard)
   @Get("/busy-times")
   async getBusyTimes(
     @Query() queryParams: CalendarBusyTimesInput,
@@ -54,5 +77,71 @@ export class CalendarsController {
       status: SUCCESS_STATUS,
       data: calendars,
     };
+  }
+
+  @UseGuards(AccessTokenGuard)
+  @Get("/:calendar/connect")
+  @HttpCode(HttpStatus.OK)
+  async redirect(
+    @Req() req: Request,
+    @Headers("Authorization") authorization: string,
+    @Param("calendar") calendar: string
+  ): Promise<ApiResponse<{ authUrl: string }>> {
+    switch (calendar) {
+      case OFFICE_365_CALENDAR:
+        return await this.outlookService.connect(authorization, req);
+      case GOOGLE_CALENDAR:
+        return await this.googleCalendarService.connect(authorization, req);
+      default:
+        throw new BadRequestException(
+          "Invalid calendar type, available calendars are: ",
+          CALENDARS.join(", ")
+        );
+    }
+  }
+
+  @Get("/:calendar/save")
+  @HttpCode(HttpStatus.OK)
+  @Redirect(undefined, 301)
+  async save(
+    @Query("state") state: string,
+    @Query("code") code: string,
+    @Param("calendar") calendar: string
+  ): Promise<{ url: string }> {
+    // state params contains our user access token
+    const stateParams = new URLSearchParams(state);
+    const { accessToken, origin } = z
+      .object({ accessToken: z.string(), origin: z.string() })
+      .parse({ accessToken: stateParams.get("accessToken"), origin: stateParams.get("origin") });
+
+    switch (calendar) {
+      case OFFICE_365_CALENDAR:
+        return await this.outlookService.save(code, accessToken, origin);
+      case GOOGLE_CALENDAR:
+        return await this.googleCalendarService.save(code, accessToken, origin);
+      default:
+        throw new BadRequestException(
+          "Invalid calendar type, available calendars are: ",
+          CALENDARS.join(", ")
+        );
+    }
+  }
+
+  @Get("/:calendar/check")
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(AccessTokenGuard, PermissionsGuard)
+  @Permissions([APPS_READ])
+  async check(@GetUser("id") userId: number, @Param("calendar") calendar: string): Promise<ApiResponse> {
+    switch (calendar) {
+      case OFFICE_365_CALENDAR:
+        return await this.outlookService.check(userId);
+      case GOOGLE_CALENDAR:
+        return await this.googleCalendarService.check(userId);
+      default:
+        throw new BadRequestException(
+          "Invalid calendar type, available calendars are: ",
+          CALENDARS.join(", ")
+        );
+    }
   }
 }
--- packages/platform/constants/apps.ts
+++ packages/platform/constants/apps.ts
@@ -1,6 +1,14 @@
 export const GOOGLE_CALENDAR_TYPE = "google_calendar";
 export const GOOGLE_CALENDAR_ID = "google-calendar";
+export const OFFICE_365_CALENDAR_TYPE = "office365_calendar";
+export const OFFICE_365_CALENDAR_ID = "office365-calendar";
+export const GOOGLE_CALENDAR = "google";
+export const OFFICE_365_CALENDAR = "office365";
+export const APPLE_CALENDAR = "apple";
+// APPLE_CALENDAR is not implemented yet
+export const CALENDARS = [GOOGLE_CALENDAR, OFFICE_365_CALENDAR] as const;
 
 export const APPS_TYPE_ID_MAPPING = {
   [GOOGLE_CALENDAR_TYPE]: GOOGLE_CALENDAR_ID,
+  [OFFICE_365_CALENDAR_TYPE]: OFFICE_365_CALENDAR_ID,
 } as const;
