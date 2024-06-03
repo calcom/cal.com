268a167258ba7dabec8e8bd3c4e07c1734e66e74
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-06-03 16:34:38 -0400

    Introduce mockDatabaseClient class

--- tests/libs/__mocks__/mockDatabaseClient.ts
+++ tests/libs/__mocks__/mockDatabaseClient.ts
@@ -1,9 +1,41 @@
 import prismock from "./prisma";
 
 export default class MockDatabaseClient {
-  async writeCredentialToMockDb(credential) {
+  public async writeCredential(credential) {
     await prismock.credential.create({
       data: credential,
+    });
+  }
+
+  public async getCredential(credentialId) {
+    return await prismock.credential.findFirst({
+      where: {
+        id: credentialId,
+      },
+    });
+  }
+
+  public async writeApp(appData: {
+    slug: string;
+    categories: string[];
+    keys: any;
+    dirName: string;
+    enabled: boolean;
+  }) {
+    await prismock.app.create({
+      data: {
+        ...appData,
+      },
+    });
+  }
+
+  public async getEventTypes(eventTypeIds) {
+    return await prismock.eventType.findMany({
+      where: {
+        id: {
+          in: eventTypeIds,
+        },
+      },
     });
   }
 }
