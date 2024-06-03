b3eb45822d8f04274946a27baeb3b6eb40519f0b
Morgan Vernay <morgan@cal.com>
2024-06-03 13:21:58 +0300

    chore: remove timezone controller subversion poc

--- apps/api/v2/src/modules/timezones/controllers/timezones.controller.ts
+++ apps/api/v2/src/modules/timezones/controllers/timezones.controller.ts
@@ -1,4 +1,4 @@
-import { VERSION_2024_04_15_VALUE } from "@/lib/api-versions";
+import { API_VERSIONS_VALUES } from "@/lib/api-versions";
 import { TimezonesService } from "@/modules/timezones/services/timezones.service";
 import { Controller, Get } from "@nestjs/common";
 import { ApiTags as DocsTags } from "@nestjs/swagger";
@@ -9,7 +9,7 @@ import { ApiResponse } from "@calcom/platform-types";
 
 @Controller({
   path: "/v2/timezones",
-  version: VERSION_2024_04_15_VALUE,
+  version: API_VERSIONS_VALUES,
 })
 @DocsTags("Timezones")
 export class TimezonesController {
--- apps/api/v2/src/modules/timezones/timezones.module.ts
+++ apps/api/v2/src/modules/timezones/timezones.module.ts
@@ -1,5 +1,4 @@
 import { RedisModule } from "@/modules/redis/redis.module";
-import { TimezonesController as TimezonesController20240521 } from "@/modules/timezones/controllers/2024-05-21/timezones.controller";
 import { TimezonesController } from "@/modules/timezones/controllers/timezones.controller";
 import { TimezonesService } from "@/modules/timezones/services/timezones.service";
 import { Module } from "@nestjs/common";
@@ -7,7 +6,7 @@ import { Module } from "@nestjs/common";
 @Module({
   imports: [RedisModule],
   providers: [TimezonesService],
-  controllers: [TimezonesController, TimezonesController20240521],
+  controllers: [TimezonesController],
   exports: [TimezonesService],
 })
 export class TimezoneModule {}
