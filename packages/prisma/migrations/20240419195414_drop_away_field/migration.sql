9e317576bade8d1dbd5097204e49c0cb1bdfe5ac
Alex van Andel <me@alexvanandel.com>
2024-06-04 23:24:10 +0100

    Remove the avatar/logo drops for staggered release

--- packages/prisma/migrations/20240419195414_drop_away_field/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `away` on the `users` table. All the data in the column will be lost.
-
-*/
--- AlterTable
--- ALTER TABLE "users" DROP COLUMN "away";
