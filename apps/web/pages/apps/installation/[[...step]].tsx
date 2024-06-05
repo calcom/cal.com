78ea4d2f6846b9ebb3172e953da7564475b25def
Alex van Andel <me@alexvanandel.com>
2024-06-05 17:44:31 +0100

    chore: Remove deprecated logo/avatar/away fields (#14705)
    * chore: Remove deprecated logo/avatar/away fields
    
    * Use right variables for accounts-step-card
    
    * Remove more deprecated logo/avatar usage
    
    * Dont drop the fields until all usages are gone
    
    * Remove the avatar/logo drops for staggered release
    
    * fix: Typescript blindspot (unvalidated include fields)
    
    * fix: Further updates (mostly to API) typescript missed
    
    ---------
    
    Co-authored-by: Udit Takkar <53316345+Udit-takkar@users.noreply.github.com>
    Co-authored-by: Bailey Pumfleet <bailey@pumfleet.co.uk>
    Co-authored-by: Carina Wollendorfer <30310907+CarinaWolli@users.noreply.github.com>
    Co-authored-by: Peer Richelsen <peeroke@gmail.com>
    Co-authored-by: Keith Williams <keithwillcode@gmail.com>

--- apps/web/pages/apps/installation/[[...step]].tsx
+++ apps/web/pages/apps/installation/[[...step]].tsx
@@ -474,7 +474,7 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
     const personalAccount = {
       id: user.id,
       name: user.name,
-      avatar: user.avatarUrl,
+      avatarUrl: user.avatarUrl,
       alreadyInstalled: appInstalls.some((install) => !Boolean(install.teamId) && install.userId === user.id),
     };
 
