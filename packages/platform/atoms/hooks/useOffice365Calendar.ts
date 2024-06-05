24961f0781d8af2195aad26e109544a0df897298
Rajiv Sahal <rajivsahal@Rajivs-MacBook-Pro.local>
2024-06-05 14:57:32 +0530

    refactors

--- packages/platform/atoms/hooks/useOffice365Calendar.ts
+++ packages/platform/atoms/hooks/useOffice365Calendar.ts
@@ -1,8 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
 import type { OnCheckErrorType } from "hooks/useGcal";
 import { useState, useEffect } from "react";
 
-import type { ApiErrorResponse } from "@calcom/platform-types";
+import type { ApiResponse } from "@calcom/platform-types";
+import { type ApiErrorResponse } from "@calcom/platform-types";
 
+import { SUCCESS_STATUS, ERROR_STATUS } from "../../constants/api";
 import http from "../lib/http";
 
 export interface useOffice365CalendarProps {
@@ -10,19 +13,41 @@ export interface useOffice365CalendarProps {
   onCheckError?: OnCheckErrorType;
 }
 
+export const QUERY_KEY = ["get-office-365-redirect-uri"];
+
+export const useGetRedirectUrl = (calendar: string) => {
+  const authUrl = useQuery({
+    queryKey: QUERY_KEY,
+    staleTime: Infinity,
+    enabled: false,
+    queryFn: () => {
+      return http
+        ?.get<ApiResponse<{ authUrl: string }>>(`/calendars/${calendar}/connect`)
+        .then(({ data: responseBody }) => {
+          if (responseBody.status === SUCCESS_STATUS) {
+            return responseBody.data.authUrl;
+          }
+          if (responseBody.status === ERROR_STATUS) throw new Error(responseBody.error.message);
+          return "";
+        });
+    },
+  });
+
+  return authUrl;
+};
+
 export const useOffice365Calendar = ({ isAuth, onCheckError }: useOffice365CalendarProps) => {
   const [allowConnect, setAllowConnect] = useState<boolean>(false);
   const [checked, setChecked] = useState<boolean>(false);
 
-  const redirectToOffice365OAuth = () => {
-    http
-      ?.get("/calendars/office365/connect")
-      .then(({ data: responseBody }) => {
-        if (responseBody.data?.authUrl) {
-          window.location.href = responseBody.data.authUrl;
-        }
-      })
-      .catch(console.error);
+  const { refetch } = useGetRedirectUrl("office365");
+
+  const redirectToOffice365OAuth = async () => {
+    const redirectUri = await refetch();
+
+    if (redirectUri.data) {
+      window.location.href = redirectUri.data;
+    }
   };
 
   useEffect(() => {
