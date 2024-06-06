7894dee47423007d9b9d5d6c48379c05049997db
Rajiv Sahal <rajivsahal@Rajivs-MacBook-Pro.local>
2024-06-07 01:14:03 +0530

    get isPlatformUser boolean from user and then redirect user accordingly

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { IncomingMessage } from "http";
+import type { NextApiRequest } from "next";
 import type { AppContextType } from "next/dist/shared/lib/utils";
 import { useRouter, usePathname } from "next/navigation";
 import React, { useEffect } from "react";
@@ -14,7 +15,7 @@ function MyApp(props: AppProps) {
   const { Component, pageProps } = props;
   const pathName = usePathname();
 
-  const isPlatformUser = pageProps?.session?.isPlatformUser;
+  const isPlatformUser = pageProps.isPlatformUser;
 
   useEffect(() => {
     if (isPlatformUser === true && pathName && !pathName.startsWith("/settings/platform")) {
@@ -33,14 +34,18 @@ declare global {
 }
 
 MyApp.getInitialProps = async (ctx: AppContextType) => {
-  const { req } = ctx.ctx;
+  const { req, res } = ctx.ctx;
 
   let newLocale = "en";
+  let userSession = null;
 
   if (req) {
     const { getLocale } = await import("@calcom/features/auth/lib/getLocale");
+    const { getServerSession } = await import("@calcom/features/auth/lib/getServerSession");
+
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     newLocale = await getLocale(req as IncomingMessage & { cookies: Record<string, any> });
+    userSession = await getServerSession({ req: req as NextApiRequest, res });
   } else if (typeof window !== "undefined" && window.calNewLocale) {
     newLocale = window.calNewLocale;
   }
@@ -48,6 +53,7 @@ MyApp.getInitialProps = async (ctx: AppContextType) => {
   return {
     pageProps: {
       newLocale,
+      isPlatformUser: userSession?.isPlatformUser ?? false,
     },
   };
 };
