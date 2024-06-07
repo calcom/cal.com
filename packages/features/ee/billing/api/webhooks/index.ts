b62ec7c715205d42004208af3077eca9c328c53c
zomars <zomars@me.com>
2024-06-06 18:32:22 -0700

    Update index.ts

--- packages/features/ee/billing/api/webhooks/index.ts
+++ packages/features/ee/billing/api/webhooks/index.ts
@@ -3,8 +3,10 @@ import { defaultHandler, defaultResponder } from "@calcom/lib/server";
 import { stripeWebhookHandler } from "./__handler";
 
 export default defaultHandler({
-  GET: Promise.resolve({
+  // We only need to handle POST requests
+  POST: Promise.resolve({
     default: defaultResponder(
+      // We handle each Stripe webhook event type with it's own lazy loaded handler
       stripeWebhookHandler({
         "payment_intent.succeeded": import("./_payment_intent.succeeded"),
         "customer.subscription.deleted": import("./_customer.subscription.deleted"),
