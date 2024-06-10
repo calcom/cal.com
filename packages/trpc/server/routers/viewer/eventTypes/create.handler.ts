20fdb50081ae684723a5af78436068c0b09509a4
Somay Chauhan <somaychauhan98@gmail.com>
2024-06-10 15:27:18 +0530

    creating default event-types should set the user's default location

--- packages/trpc/server/routers/viewer/eventTypes/create.handler.ts
+++ packages/trpc/server/routers/viewer/eventTypes/create.handler.ts
@@ -121,17 +121,16 @@ async function getDefaultLocations(user: User): Promise<EventTypeLocation[]> {
   const defaultConferencingData = userMetadataSchema.parse(user.metadata)?.defaultConferencingApp;
   const appKeys = await getAppKeysFromSlug("daily-video");
 
-  if (typeof appKeys.api_key === "string") {
-    return [{ type: DailyLocationType }];
-  }
-
   if (defaultConferencingData && defaultConferencingData.appSlug !== "daily-video") {
     const credentials = await getUsersCredentials(user);
     const foundApp = getApps(credentials, true).filter(
       (app) => app.slug === defaultConferencingData.appSlug
     )[0]; // There is only one possible install here so index [0] is the one we are looking for ;
     const locationType = foundApp?.locationOption?.value ?? DailyLocationType; // Default to Daily if no location type is found
-    return [{ type: locationType, link: defaultConferencingData.appLink }];
+    const credentialId = foundApp.credential.id ?? undefined;
+    return [{ type: locationType, link: defaultConferencingData.appLink, credentialId }];
+  } else if (typeof appKeys.api_key === "string") {
+    return [{ type: DailyLocationType }];
   }
 
   return [];
