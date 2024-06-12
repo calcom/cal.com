3037cfb344f31a87c913db8387900b5bc16f4c40
oliver <8559757+oliverqx@users.noreply.github.com>
2024-06-11 21:49:25 -0700

    feat: add flattenObject
    - avoided recursion so its a loop by choice

--- packages/features/audit-logs/utils.ts
+++ packages/features/audit-logs/utils.ts
@@ -8,3 +8,39 @@ export function getHref(
   baseUrlParsed.searchParams.set(activeSettingsOption.credentialId, activeSettingsOption.activeOption);
   return baseUrlParsed.toString();
 }
+
+function getPropertyByPath(obj, path) {
+  const parts = path.split(".");
+  let current = obj;
+  for (const part of parts) {
+    if (current[part] === undefined) {
+      return undefined;
+    }
+    current = current[part];
+  }
+  return current;
+}
+
+export function flattenObject(obj) {
+  const flattened = {};
+
+  Object.keys(obj).forEach((key) => {
+    let searchNext = [key];
+    while (searchNext.length > 0) {
+      const discovered = [];
+
+      searchNext.forEach((key) => {
+        const value = getPropertyByPath(obj, key);
+        if (typeof value === "object" && value !== null && !Array.isArray(value)) {
+          discovered.push(...Object.keys(value).map((value) => `${key}.${value}`));
+        } else {
+          flattened[key] = value;
+        }
+      });
+
+      searchNext = discovered;
+    }
+  });
+
+  return flattened;
+}
