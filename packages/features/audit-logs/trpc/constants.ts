d48b6b3c958876cfc14b53aa4d4bd0a6bc474f77
oliver <8559757+oliverqx@users.noreply.github.com>
2024-06-11 21:45:16 -0700

    feat: add pathToAuditLogEvent
    based on path this will return the events metadata, should be moved to core later

--- packages/features/audit-logs/trpc/constants.ts
+++ packages/features/audit-logs/trpc/constants.ts
@@ -1,3 +1,27 @@
-export const pathToAuditLogEvent = {
-  updateAppCredentials: "SYSTEM_UPDATE",
+import { CRUD } from "@calcom/features/audit-logs/types";
+import {
+  AuditLogTriggerTargets,
+  AuditLogSystemTriggerEvents,
+  AuditLogAppTriggerEvents,
+} from "@calcom/prisma/enums";
+
+export const pathToAuditLogEvent: Record<string, any> = {
+  // updateAppCredentials: {
+  //   action: AuditLogSystemTriggerEvents.SYSTEM_CREDENTIALS_UPDATED,
+  //   description: "App keys have been updated",
+  //   crud: CRUD.UPDATE,
+  //   target: AuditLogTriggerTargets.SYSTEM,
+  // },
+  updateAppCredentials: {
+    action: AuditLogAppTriggerEvents.APP_KEYS_UPDATED,
+    description: "App keys have been updated",
+    crud: CRUD.UPDATE,
+    target: AuditLogTriggerTargets.APPS,
+  },
+  systemMisc: {
+    action: AuditLogSystemTriggerEvents.SYSTEM_MISC,
+    description: "AuditLog implementation specific action was performed.",
+    crud: CRUD.UPDATE,
+    target: AuditLogTriggerTargets.SYSTEM,
+  },
 };
