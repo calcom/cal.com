9c710a36d95d56ef278287ec999e4e2cfd6829ca
oliver <8559757+oliverqx@users.noreply.github.com>
2024-06-11 21:38:19 -0700

    chore: remove credentialSettingsSchema

--- packages/app-store/templates/audit-log-implementation/zod.ts
+++ packages/app-store/templates/audit-log-implementation/zod.ts
@@ -4,15 +4,9 @@ export const appKeysSchema = z.object({
   apiKey: z.string().min(1),
   projectId: z.string().min(1),
   endpoint: z.string().min(1),
+  disabledEvents: z.array(),
 });
 
 export type AppKeys = z.infer<typeof appKeysSchema>;
 
 export const appDataSchema = z.object({});
-
-export const credentialSettingsSchema = z.object({
-  disabledEvents: z.array(z.string()),
-  templateSetup: z.boolean(),
-});
-
-export type CredentialSettings = z.infer<typeof credentialSettingsSchema>;
