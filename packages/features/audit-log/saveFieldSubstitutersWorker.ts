963effb7a1b178c59c0e4e54bbacfcebf8a36d2f
TaduJR <nattysec26@gmail.com>
2024-06-13 22:07:16 +0300

    implement substituter worker thread

--- packages/features/audit-log/saveFieldSubstitutersWorker.ts
+++ packages/features/audit-log/saveFieldSubstitutersWorker.ts
@@ -1,3 +1,5 @@
+import { isMainThread, Worker, workerData } from "node:worker_threads";
+
 import { FieldSubstituterOption } from "./types/TFieldSubstituterInput";
 import type TFieldSubstituterInput from "./types/TFieldSubstituterInput";
 import {
@@ -8,27 +10,48 @@ import {
   substituteTeamDelete,
 } from "./util/saveFieldSubstituter";
 
-async function saveFieldSubstitutersWorker(input: TFieldSubstituterInput) {
-  switch (input.triggeredEvent) {
-    case FieldSubstituterOption.EventTypeDelete:
-      await substituteEventTypeDelete(input.deletedEventType);
-      break;
-    case FieldSubstituterOption.EventTypeDeleteMany:
-      await substituteEventTypeDeleteMany(input.deletedEventTypes);
-      break;
-    case FieldSubstituterOption.UserUpdate:
-      await substituteUserUpdate(input.prevUser, input.updatedUser);
-      break;
-    case FieldSubstituterOption.UserDelete:
-      await substituteUserDelete(input.deletedUser);
-      break;
-    case FieldSubstituterOption.TeamDelete:
-      await substituteTeamDelete(input.deletedTeam);
-      break;
-    default:
-      // console.warn(`Unhandled audit log event type: ${input.triggeredEvent}`);
-      break;
-  }
+let saveFieldSubstitutersWorker: (input: TFieldSubstituterInput) => Promise<void> = async (
+  _input: TFieldSubstituterInput
+) => {
+  // Default implementation that does nothing
+};
+
+if (isMainThread) {
+  saveFieldSubstitutersWorker = (input: TFieldSubstituterInput) => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(__filename, { workerData: input });
+      worker.on("message", resolve);
+      worker.on("error", reject);
+      worker.on("exit", (code) => {
+        if (code !== 0) {
+          reject(new Error(`Worker stopped with exit code ${code}`));
+        }
+      });
+    });
+  };
+} else {
+  (async () => {
+    switch (workerData.triggeredEvent) {
+      case FieldSubstituterOption.EventTypeDelete:
+        await substituteEventTypeDelete(workerData.deletedEventType);
+        break;
+      case FieldSubstituterOption.EventTypeDeleteMany:
+        await substituteEventTypeDeleteMany(workerData.deletedEventTypes);
+        break;
+      case FieldSubstituterOption.UserUpdate:
+        await substituteUserUpdate(workerData.prevUser, workerData.updatedUser);
+        break;
+      case FieldSubstituterOption.UserDelete:
+        await substituteUserDelete(workerData.deletedUser);
+        break;
+      case FieldSubstituterOption.TeamDelete:
+        await substituteTeamDelete(workerData.deletedTeam);
+        break;
+      default:
+        // console.warn(`Unhandled audit log event type: ${workerData.triggeredEvent}`);
+        break;
+    }
+  })();
 }
 
 export default saveFieldSubstitutersWorker;
