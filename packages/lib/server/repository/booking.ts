d940ab529e0b47016384001010f5d043eaaa7244
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-06-14 14:23:49 -0400

    Add doesUserIdHaveAccessToBooking for user

--- packages/lib/server/repository/booking.ts
+++ packages/lib/server/repository/booking.ts
@@ -8,4 +8,32 @@ export class BookingRepository {
       },
     });
   }
+
+  /** Determines if the user is the organizer, team admin, or org admin that the booking was created under */
+  static async doesUserIdHaveAccessToBooking({ userId, bookingId }: { userId: number; bookingId: number }) {
+    const booking = await prisma.booking.findFirst({
+      where: {
+        id: bookingId,
+      },
+      select: {
+        userId: true,
+        eventType: {
+          select: {
+            teamId: true,
+          },
+        },
+      },
+    });
+
+    if (!booking) return false;
+
+    if (userId === booking.userId) return true;
+
+    // If the booking doesn't belong to the user and there's no team then return early
+    if (!booking.eventType.teamId) return false;
+
+    // TODO add checks for team and org
+
+    return;
+  }
 }
