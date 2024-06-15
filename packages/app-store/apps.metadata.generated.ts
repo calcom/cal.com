e205253150eca6c93b8af830dd74acb8ddc07972
Anik Dhabal Babu <adhabal2002@gmail.com>
2024-06-15 18:29:24 +0000

    feat: Dialpad conferencing

--- packages/app-store/apps.metadata.generated.ts
+++ packages/app-store/apps.metadata.generated.ts
@@ -18,6 +18,7 @@ import cron_config_json from "./cron/config.json";
 import { metadata as dailyvideo__metadata_ts } from "./dailyvideo/_metadata";
 import deel_config_json from "./deel/config.json";
 import demodesk_config_json from "./demodesk/config.json";
+import dialpad_config_json from "./dialpad/config.json";
 import discord_config_json from "./discord/config.json";
 import eightxeight_config_json from "./eightxeight/config.json";
 import element_call_config_json from "./element-call/config.json";
@@ -107,6 +108,7 @@ export const appStoreMetadata = {
   dailyvideo: dailyvideo__metadata_ts,
   deel: deel_config_json,
   demodesk: demodesk_config_json,
+  dialpad: dialpad_config_json,
   discord: discord_config_json,
   eightxeight: eightxeight_config_json,
   "element-call": element_call_config_json,
