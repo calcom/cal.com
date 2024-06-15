24d993ed83bd547e320c5c2a3f191f0d27aa1a12
Shaik-Sirajuddin <sirajudddinshaik30@gmail.com>
2024-06-15 22:38:36 +0530

    fix_event_type_created_with_calvideo_instead_of_user_default_conferencing_app

--- packages/trpc/server/routers/viewer/eventTypes/create.handler.ts
+++ packages/trpc/server/routers/viewer/eventTypes/create.handler.ts
@@ -119,11 +119,6 @@ export const createHandler = async ({ ctx, input }: CreateOptions) => {
 
 async function getDefaultLocations(user: User): Promise<EventTypeLocation[]> {
   const defaultConferencingData = userMetadataSchema.parse(user.metadata)?.defaultConferencingApp;
-  const appKeys = await getAppKeysFromSlug("daily-video");
-
-  if (typeof appKeys.api_key === "string") {
-    return [{ type: DailyLocationType }];
-  }
 
   if (defaultConferencingData && defaultConferencingData.appSlug !== "daily-video") {
     const credentials = await getUsersCredentials(user);
@@ -132,6 +127,12 @@ async function getDefaultLocations(user: User): Promise<EventTypeLocation[]> {
     )[0]; // There is only one possible install here so index [0] is the one we are looking for ;
     const locationType = foundApp?.locationOption?.value ?? DailyLocationType; // Default to Daily if no location type is found
     return [{ type: locationType, link: defaultConferencingData.appLink }];
+  }
+
+  const appKeys = await getAppKeysFromSlug("daily-video");
+
+  if (typeof appKeys.api_key === "string") {
+    return [{ type: DailyLocationType }];
   }
 
   return [];
