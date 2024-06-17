3a864b8a1a69b59fd1d75d252ef15a8561d7143b
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-06-17 09:42:07 -0400

    Check user permission tRPC route

--- packages/trpc/server/routers/viewer/teams/roundRobin/roundRobinReassign.handler.ts
+++ packages/trpc/server/routers/viewer/teams/roundRobin/roundRobinReassign.handler.ts
@@ -1,5 +1,8 @@
 import { roundRobinReassignment } from "@calcom/features/ee/round-robin/roundRobinReassignment";
+import { BookingRepository } from "@calcom/lib/server/repository/booking";
 import type { TrpcSessionUser } from "@calcom/trpc/server/trpc";
+
+import { TRPCError } from "@trpc/server";
 
 import type { TRoundRobinReassignInputSchema } from "./roundRobinReassign.schema";
 
@@ -11,7 +14,14 @@ type RoundRobinReassignOptions = {
 };
 
 export const roundRobinReassignHandler = async ({ input }: RoundRobinReassignOptions) => {
-  const { bookingId } = input;
+  const { ctx, bookingId } = input;
+
+  // Check if user has access to change booking
+  const isAllowed = await BookingRepository.doesUserIdHaveAccessToBooking({ userId: ctx.user.id, bookingId });
+
+  if (!isAllowed) {
+    throw new TRPCError({ code: "FORBIDDEN", message: "You do not have permission" });
+  }
 
   await roundRobinReassignment({ bookingId });
 };
