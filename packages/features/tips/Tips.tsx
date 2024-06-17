62a0d1b59fcc68f13b7bb1ea464ce0f29c4ab638
Vijay <vijayraghav22@gmail.com>
2024-06-17 19:24:55 +0530

    randomize tips

--- packages/features/tips/Tips.tsx
+++ packages/features/tips/Tips.tsx
@@ -111,7 +111,16 @@ export const tips = [
   },
 ];
 
-const reversedTips = tips.slice(0).reverse();
+function shuffle<T>(array: T[]): T[] {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+  }
+  return array;
+}
+
+const shuffledTips = shuffle(tips);
+const reversedTips = shuffledTips.slice(0).reverse();
 
 function Tips() {
   const { t } = useLocale();
