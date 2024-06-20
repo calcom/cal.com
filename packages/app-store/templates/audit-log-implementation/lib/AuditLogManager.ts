403c3271c8413f812adc92833a87303173a62e84
oliver <8559757+oliverqx@users.noreply.github.com>
2024-06-20 16:39:29 -0700

    fix: remove unused arguments and add custom interceptor

--- packages/app-store/templates/audit-log-implementation/lib/AuditLogManager.ts
+++ packages/app-store/templates/audit-log-implementation/lib/AuditLogManager.ts
@@ -5,6 +5,7 @@ import type {
   GenericAuditLogClient,
 } from "@calcom/features/audit-logs/types";
 import logger from "@calcom/lib/logger";
+import { AuditLogSystemTriggerEvents } from "@calcom/prisma/enums";
 
 import config from "../config.json";
 import type { AppKeys } from "../zod";
@@ -13,7 +14,7 @@ const log = logger.getSubLogger({ prefix: ["AuditLogManager", config.slug] });
 export default class GenericAuditLogManager implements AuditLogsManager {
   private client: undefined | GenericAuditLogClient;
 
-  constructor(appKeys: AppKeys, id: number) {
+  constructor(appKeys: AppKeys) {
     log.silly("Initializing GenericAuditLogManager");
 
     // This is where your audit log client goes. Should be edited.
@@ -27,6 +28,9 @@ export default class GenericAuditLogManager implements AuditLogsManager {
 
   public async reportEvent(event: AuditLogEvent) {
     log.silly("Reporting event.");
+    if (event.action === AuditLogSystemTriggerEvents.SYSTEM_MISC) {
+      event.action = event.fields.implementationAction as string;
+    }
     // Here you can intercept the event before its sent by your client.
     this.client?.reportEvent(event);
   }
