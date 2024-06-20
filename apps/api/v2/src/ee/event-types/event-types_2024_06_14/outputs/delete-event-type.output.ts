5ac9ed5ed4a7b35de81b29244a7706616f3f0334
supalarry <laurisskraucis@gmail.com>
2024-06-20 09:56:14 +0200

    refactor: use IsIn instead of IsEnum

--- apps/api/v2/src/ee/event-types/event-types_2024_06_14/outputs/delete-event-type.output.ts
+++ apps/api/v2/src/ee/event-types/event-types_2024_06_14/outputs/delete-event-type.output.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { ApiProperty as DocsProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
-import { IsEnum, IsInt, IsString } from "class-validator";
+import { IsIn, IsInt, IsString } from "class-validator";
 
 import { SUCCESS_STATUS, ERROR_STATUS } from "@calcom/platform-constants";
 import { CREATE_EVENT_LENGTH_EXAMPLE, CREATE_EVENT_TITLE_EXAMPLE } from "@calcom/platform-types";
@@ -25,7 +25,7 @@ class DeleteData_2024_06_14 {
 
 export class DeleteEventTypeOutput_2024_06_14 {
   @ApiProperty({ example: SUCCESS_STATUS, enum: [SUCCESS_STATUS, ERROR_STATUS] })
-  @IsEnum([SUCCESS_STATUS, ERROR_STATUS])
+  @IsIn([SUCCESS_STATUS, ERROR_STATUS])
   status!: typeof SUCCESS_STATUS | typeof ERROR_STATUS;
 
   @Type(() => DeleteData_2024_06_14)
--- apps/api/v2/src/ee/event-types/event-types_2024_06_14/outputs/update-event-type.output.ts
+++ apps/api/v2/src/ee/event-types/event-types_2024_06_14/outputs/update-event-type.output.ts
@@ -1,13 +1,13 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
-import { IsEnum, IsNotEmptyObject, ValidateNested } from "class-validator";
+import { IsIn, IsNotEmptyObject, ValidateNested } from "class-validator";
 
 import { SUCCESS_STATUS, ERROR_STATUS } from "@calcom/platform-constants";
 import { EventTypeOutput_2024_06_14 } from "@calcom/platform-types";
 
 export class UpdateEventTypeOutput_2024_06_14 {
   @ApiProperty({ example: SUCCESS_STATUS, enum: [SUCCESS_STATUS, ERROR_STATUS] })
-  @IsEnum([SUCCESS_STATUS, ERROR_STATUS])
+  @IsIn([SUCCESS_STATUS, ERROR_STATUS])
   status!: typeof SUCCESS_STATUS | typeof ERROR_STATUS;
 
   @ApiProperty({
