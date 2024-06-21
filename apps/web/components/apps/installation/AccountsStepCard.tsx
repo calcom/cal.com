b3a15f361184b89f93bc796b89691a7db8ea26da
Somay Chauhan <somaychauhan98@gmail.com>
2024-06-21 11:50:39 +0530

    undo team select step

--- apps/web/components/apps/installation/AccountsStepCard.tsx
+++ apps/web/components/apps/installation/AccountsStepCard.tsx
@@ -1,13 +1,13 @@
 import type { TTeams } from "@pages/apps/installation/[[...step]]";
 import type { FC } from "react";
-import React from "react";
+import React, { useState } from "react";
 
 import { classNames } from "@calcom/lib";
+import { getPlaceholderAvatar } from "@calcom/lib/defaultAvatarImage";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import type { User } from "@calcom/prisma/client";
 import { StepCard } from "@calcom/ui";
-
-import AccountSelector from "@components/apps/installation/AccountSelector";
+import { Avatar } from "@calcom/ui";
 
 export type PersonalAccountProps = Pick<User, "id" | "avatarUrl" | "name"> & { alreadyInstalled: boolean };
 
@@ -17,6 +17,52 @@ type AccountStepCardProps = {
   onSelect: (id?: number) => void;
   loading: boolean;
   installableOnTeams: boolean;
+};
+
+type AccountSelectorProps = {
+  avatar?: string;
+  name: string;
+  alreadyInstalled: boolean;
+  onClick?: () => void;
+  loading: boolean;
+  testId: string;
+};
+
+const AccountSelector: FC<AccountSelectorProps> = ({
+  avatar,
+  alreadyInstalled,
+  name,
+  onClick,
+  loading,
+  testId,
+}) => {
+  const { t } = useLocale();
+  const [selected, setSelected] = useState(false);
+  return (
+    <div
+      className={classNames(
+        "hover:bg-muted flex cursor-pointer flex-row items-center gap-2 p-1",
+        (alreadyInstalled || loading) && "cursor-not-allowed",
+        selected && loading && "bg-muted animate-pulse"
+      )}
+      data-testid={testId}
+      onClick={() => {
+        if (!alreadyInstalled && !loading && onClick) {
+          setSelected(true);
+          onClick();
+        }
+      }}>
+      <Avatar
+        alt={avatar || ""}
+        imageSrc={getPlaceholderAvatar(avatar, name)} // if no image, use default avatar
+        size="sm"
+      />
+      <div className="text-md pt-0.5 font-medium text-gray-500">
+        {name}
+        {alreadyInstalled ? <span className="ml-1 text-sm text-gray-400">{t("already_installed")}</span> : ""}
+      </div>
+    </div>
+  );
 };
 
 export const AccountsStepCard: FC<AccountStepCardProps> = ({
