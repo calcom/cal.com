b6789cccd8999ee730d90d004d61628fe02b4fd9
Keith Williams <keithwillcode@gmail.com>
2024-06-24 18:50:04 -0300

    chore: Cache timezones by calcom version

--- packages/trpc/server/routers/publicViewer/cityTimezones.schema.ts
+++ packages/trpc/server/routers/publicViewer/cityTimezones.schema.ts
@@ -1,1 +1,7 @@
-export {};
+import { z } from "zod";
+
+export const cityTimezonesSchema = z.object({
+  CalComVersion: z.string(),
+});
+
+export type I18nInputSchema = z.infer<typeof cityTimezonesSchema>;
