6709e09f24548ec675c9401c1408840eb4ec1ace
sean-brydon <sean@cal.com>
2024-06-24 09:39:32 +0100

    revese billingPeriod

--- packages/features/ee/teams/lib/payments.ts
+++ packages/features/ee/teams/lib/payments.ts
@@ -182,7 +182,7 @@ export const purchaseTeamOrOrgSubscription = async (input: {
         unit_amount: yearlyPrice, // Stripe expects the amount in cents
         // Use the same currency as in the fixed price to avoid hardcoding it.
         currency: currency,
-        recurring: { interval: billingPeriod === "MONTHLY" ? "month" : "year" }, // Define your subscription interval
+        recurring: { interval: billingPeriod === "ANNUALLY" ? "year" : "month" }, // Define your subscription interval
         product: typeof product === "string" ? product : product.id,
         tax_behavior: "exclusive",
       });
