b57bee884ad42d6eddd8a3653621e26d10b55616
Morgan Vernay <morgan@cal.com>
2024-06-27 14:48:50 +0200

    fixup! fixup! Merge branch 'main' into feat-api-v2-org-teams-endpoints

--- apps/api/v2/src/modules/organizations/controllers/organizations-teams.controller.ts
+++ apps/api/v2/src/modules/organizations/controllers/organizations-teams.controller.ts
@@ -6,6 +6,7 @@ import { IsOrgGuard } from "@/modules/auth/guards/organizations/is-org.guard";
 import { RolesGuard } from "@/modules/auth/guards/roles/roles.guard";
 import { IsTeamInOrg } from "@/modules/auth/guards/teams/is-team-in-org.guard";
 import { CreateOrgTeamDto } from "@/modules/organizations/inputs/create-organization-team.input";
+import { OrgTeamOutputDto } from "@/modules/organizations/outputs/organization-team.output";
 import { OrganizationsTeamsService } from "@/modules/organizations/services/organizations-teams.service";
 import {
   Controller,
@@ -20,6 +21,7 @@ import {
   Body,
 } from "@nestjs/common";
 import { ApiTags as DocsTags } from "@nestjs/swagger";
+import { plainToClass } from "class-transformer";
 
 import { SUCCESS_STATUS } from "@calcom/platform-constants";
 import { ApiResponse, SkipTakePagination } from "@calcom/platform-types";
@@ -29,76 +31,76 @@ import { Team } from "@calcom/prisma/client";
   path: "/v2/organizations/:orgId/teams",
   version: API_VERSIONS_VALUES,
 })
-@UseGuards(ApiAuthGuard, IsOrgGuard)
+@UseGuards(ApiAuthGuard, IsOrgGuard, RolesGuard)
 @DocsTags("Organizations Teams")
 export class OrganizationsTeamsController {
   constructor(private organizationsTeamsService: OrganizationsTeamsService) {}
 
   @Get()
-  @UseGuards(RolesGuard)
+  @UseGuards()
   @Roles("ORG_ADMIN")
   async getAllTeams(
     @Param("orgId", ParseIntPipe) orgId: number,
     @Query() queryParams: SkipTakePagination
-  ): Promise<ApiResponse<Team[]>> {
+  ): Promise<ApiResponse<OrgTeamOutputDto[]>> {
     const { skip, take } = queryParams;
     const teams = await this.organizationsTeamsService.getPaginatedOrgTeams(orgId, skip ?? 0, take ?? 250);
     return {
       status: SUCCESS_STATUS,
-      data: teams,
+      data: teams.map((team) => plainToClass(OrgTeamOutputDto, team, { strategy: "excludeAll" })),
     };
   }
 
-  @UseGuards(IsTeamInOrg, RolesGuard)
+  @UseGuards(IsTeamInOrg)
   @Roles("TEAM_ADMIN")
   @Get("/:teamId")
-  async getTeam(@GetTeam() team: Team): Promise<ApiResponse<Team>> {
+  async getTeam(@GetTeam() team: Team): Promise<ApiResponse<OrgTeamOutputDto>> {
     return {
       status: SUCCESS_STATUS,
-      data: team,
+      data: plainToClass(OrgTeamOutputDto, team, { strategy: "excludeAll" }),
     };
   }
 
-  @UseGuards(IsTeamInOrg, RolesGuard)
+  @UseGuards(IsTeamInOrg)
   @Roles("ORG_ADMIN")
   @Delete("/:teamId")
   async deleteTeam(
     @Param("orgId", ParseIntPipe) orgId: number,
     @Param("teamId", ParseIntPipe) teamId: number
-  ): Promise<ApiResponse<Team>> {
+  ): Promise<ApiResponse<OrgTeamOutputDto>> {
     const team = await this.organizationsTeamsService.deleteOrgTeam(orgId, teamId);
     return {
       status: SUCCESS_STATUS,
-      data: team,
+      data: plainToClass(OrgTeamOutputDto, team, { strategy: "excludeAll" }),
     };
   }
 
-  @UseGuards(IsTeamInOrg, RolesGuard)
+  @UseGuards(IsTeamInOrg)
   @Roles("ORG_ADMIN")
   @Patch("/:teamId")
   async updateTeam(
     @Param("orgId", ParseIntPipe) orgId: number,
     @Param("teamId", ParseIntPipe) teamId: number,
     @Body() body: CreateOrgTeamDto
-  ): Promise<ApiResponse<Team>> {
+  ): Promise<ApiResponse<OrgTeamOutputDto>> {
     const team = await this.organizationsTeamsService.updateOrgTeam(orgId, teamId, body);
     return {
       status: SUCCESS_STATUS,
-      data: team,
+      data: plainToClass(OrgTeamOutputDto, team, { strategy: "excludeAll" }),
     };
   }
 
   @Post()
-  @UseGuards(RolesGuard)
+  @UseGuards()
   @Roles("ORG_ADMIN")
   async createTeam(
     @Param("orgId", ParseIntPipe) orgId: number,
     @Body() body: CreateOrgTeamDto
-  ): Promise<ApiResponse<Team>> {
+  ): Promise<ApiResponse<OrgTeamOutputDto>> {
     const team = await this.organizationsTeamsService.createOrgTeam(orgId, body);
     return {
       status: SUCCESS_STATUS,
-      data: team,
+      data: plainToClass(OrgTeamOutputDto, team, { strategy: "excludeAll" }),
     };
   }
 }
