e260809b0821857af88a48461419d5cf5972f5b7
Morgan <33722304+ThyMinimalDev@users.noreply.github.com>
2024-06-27 18:04:37 +0200

    feat: organizations teams endpoints apiv2 (#15586)
    * feat: organizations teams endpoints apiv2
    
    * fixup! Merge branch 'main' into feat-api-v2-org-teams-endpoints
    
    * chore: add roles guard to endpoints
    
    * add tests
    
    * fixup! Merge branch 'main' into feat-api-v2-org-teams-endpoints
    
    * fixup! fixup! Merge branch 'main' into feat-api-v2-org-teams-endpoints
    
    * fixup! fixup! fixup! Merge branch 'main' into feat-api-v2-org-teams-endpoints

--- apps/api/v2/src/modules/auth/guards/teams/is-team-in-org.guard.ts
+++ apps/api/v2/src/modules/auth/guards/teams/is-team-in-org.guard.ts
@@ -1,12 +1,18 @@
-import { OrganizationsRepository } from "@/modules/organizations/organizations.repository";
-import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from "@nestjs/common";
+import { OrganizationsTeamsRepository } from "@/modules/organizations/repositories/organizations-teams.repository";
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  NotFoundException,
+} from "@nestjs/common";
 import { Request } from "express";
 
 import { Team } from "@calcom/prisma/client";
 
 @Injectable()
 export class IsTeamInOrg implements CanActivate {
-  constructor(private organizationsRepository: OrganizationsRepository) {}
+  constructor(private organizationsTeamsRepository: OrganizationsTeamsRepository) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<Request & { team: Team }>();
@@ -21,11 +27,15 @@ export class IsTeamInOrg implements CanActivate {
       throw new ForbiddenException("No team id found in request params.");
     }
 
-    const team = await this.organizationsRepository.findOrgTeam(Number(orgId), Number(teamId));
+    const team = await this.organizationsTeamsRepository.findOrgTeam(Number(orgId), Number(teamId));
 
     if (team && !team.isOrganization && team.parentId === Number(orgId)) {
       request.team = team;
       return true;
+    }
+
+    if (!team) {
+      throw new NotFoundException(`Team (${teamId}) not found.`);
     }
 
     return false;
