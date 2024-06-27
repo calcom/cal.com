1cdecc0c05578710943e10a2872ea700c2f56d36
Morgan <33722304+ThyMinimalDev@users.noreply.github.com>
2024-06-27 18:04:37 +0200

    feat: organizations teams endpoints apiv2 (#15586)
    * feat: organizations teams endpoints apiv2
    
    * fixup! Merge branch 'main' into feat-api-v2-org-teams-endpoints
    
    * chore: add roles guard to endpoints
    
    * add tests
    
    * fixup! Merge branch 'main' into feat-api-v2-org-teams-endpoints
    
    * fixup! fixup! Merge branch 'main' into feat-api-v2-org-teams-endpoints
    
    * fixup! fixup! fixup! Merge branch 'main' into feat-api-v2-org-teams-endpoints

--- apps/api/v2/src/modules/organizations/organizations.module.ts
+++ apps/api/v2/src/modules/organizations/organizations.module.ts
@@ -1,6 +1,8 @@
 import { MembershipsRepository } from "@/modules/memberships/memberships.repository";
 import { OrganizationsTeamsController } from "@/modules/organizations/controllers/organizations-teams.controller";
 import { OrganizationsRepository } from "@/modules/organizations/organizations.repository";
+import { OrganizationsTeamsRepository } from "@/modules/organizations/repositories/organizations-teams.repository";
+import { OrganizationsTeamsService } from "@/modules/organizations/services/organizations-teams.service";
 import { OrganizationsService } from "@/modules/organizations/services/organizations.service";
 import { PrismaModule } from "@/modules/prisma/prisma.module";
 import { StripeModule } from "@/modules/stripe/stripe.module";
@@ -8,8 +10,14 @@ import { Module } from "@nestjs/common";
 
 @Module({
   imports: [PrismaModule, StripeModule],
-  providers: [OrganizationsRepository, OrganizationsService, MembershipsRepository],
-  exports: [OrganizationsService, OrganizationsRepository],
+  providers: [
+    OrganizationsRepository,
+    OrganizationsTeamsRepository,
+    OrganizationsService,
+    OrganizationsTeamsService,
+    MembershipsRepository,
+  ],
+  exports: [OrganizationsService, OrganizationsRepository, OrganizationsTeamsRepository],
   controllers: [OrganizationsTeamsController],
 })
 export class OrganizationsModule {}
