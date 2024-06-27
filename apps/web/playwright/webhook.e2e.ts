098bbc46ce4fe3ade749279384da2b6f4d49ced9
Udit Takkar <udit222001@gmail.com>
2024-06-27 18:52:42 +0530

    fix: webhook end to end test

--- apps/web/playwright/webhook.e2e.ts
+++ apps/web/playwright/webhook.e2e.ts
@@ -609,7 +609,7 @@ test.describe("FORM_SUBMITTED", async () => {
     });
 
     await user.apiLogin();
-    const webhookReceiver = await webhooks.createReceiver();
+    const webhookReceiver = await webhooks.createTeamReceiver();
     await page.waitForLoadState("networkidle");
 
     const form = await routingForms.create({
