619f54e98f546ef1073d3fa88179a294571e67fd
Udit Takkar <udit222001@gmail.com>
2024-06-28 19:14:09 +0530

    refactor: create booking factor

--- packages/features/bookings/lib/handleNewBooking/createBooking.ts
+++ packages/features/bookings/lib/handleNewBooking/createBooking.ts
@@ -243,6 +243,7 @@ function buildNewBookingData(params: {
   if (reqBodyRecurringEventId) {
     newBookingData.recurringEventId = reqBodyRecurringEventId;
   }
+
   if (originalRescheduledBooking) {
     newBookingData.metadata = {
       ...(typeof originalRescheduledBooking.metadata === "object" && originalRescheduledBooking.metadata),
