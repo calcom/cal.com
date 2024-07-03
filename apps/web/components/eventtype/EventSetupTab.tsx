6cda4ef5656409b9d80440a87e8841d604dcd0e5
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2024-07-03 22:12:19 +0530

    revert the changes

--- apps/web/components/eventtype/EventSetupTab.tsx
+++ apps/web/components/eventtype/EventSetupTab.tsx
@@ -417,8 +417,16 @@ export const EventSetupTab = (
               <div className="mr-1.5 h-3 w-3">
                 <Icon name="check" className="h-3 w-3" />
               </div>
-              <Trans i18nKey="requires_google_calendar">
-                <p>This app requires a Google Calendar connection</p>
+              <Trans i18nKey="event_type_requres_google_cal">
+                <p>
+                  The “Add to calendar” for this event type needs to be a Google Calendar for Meet to work.
+                  Change it{" "}
+                  <Link
+                    href={`${WEBAPP_URL}/event-types/${formMethods.getValues("id")}?tabName=advanced`}
+                    className="underline">
+                    here.
+                  </Link>{" "}
+                </p>
               </Trans>
             </div>
           )}
