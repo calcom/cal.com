e2d96349bb48d95afdfeac7d69ab0bb0f2f6d2aa
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2024-07-03 22:52:56 +0530

    add translation key for "requires_google_calendar_to_install"

--- apps/web/components/eventtype/EventSetupTab.tsx
+++ apps/web/components/eventtype/EventSetupTab.tsx
@@ -417,7 +417,7 @@ export const EventSetupTab = (
               <div className="mr-1.5 h-3 w-3">
                 <Icon name="check" className="h-3 w-3" />
               </div>
-              <Trans i18nKey="event_type_requres_google_cal">
+              <Trans i18nKey="requires_google_calendar_to_install">
                 <p>
                   The “Add to calendar” for this event type needs to be a Google Calendar for Meet to work.
                   Change it{" "}
