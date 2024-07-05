5709fd861d6f7d8b1e6acc80605705a2ac31f212
Vijay <vijayraghav22@gmail.com>
2024-07-05 22:53:31 +0530

    On Delete set null for schedule
    if schedule is deleted, no need to delete the host

--- packages/prisma/migrations/20240628185532_/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Host" DROP CONSTRAINT "Host_scheduleId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Host" ADD CONSTRAINT "Host_scheduleId_fkey" FOREIGN KEY ("scheduleId") REFERENCES "Schedule"("id") ON DELETE CASCADE ON UPDATE CASCADE;
