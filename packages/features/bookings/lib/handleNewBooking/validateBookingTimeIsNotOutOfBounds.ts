717084b6befe0b5d7bf922952a569b122c454e4f
Udit Takkar <udit222001@gmail.com>
2024-07-05 23:03:42 +0530

    refactor: handleNewBooking

---
+++ packages/features/bookings/lib/handleNewBooking/validateBookingTimeIsNotOutOfBounds.ts
@@ -0,0 +1,64 @@
+import type { Logger } from "tslog";
+
+import { getUTCOffsetByTimezone } from "@calcom/lib/date-fns";
+import { HttpError } from "@calcom/lib/http-error";
+import isOutOfBounds, { BookingDateInPastError } from "@calcom/lib/isOutOfBounds";
+import type { EventType } from "@calcom/prisma/client";
+
+type ValidateBookingTimeEventType = Pick<
+  EventType,
+  | "periodType"
+  | "periodDays"
+  | "periodEndDate"
+  | "periodStartDate"
+  | "periodCountCalendarDays"
+  | "minimumBookingNotice"
+  | "eventName"
+>;
+
+export const validateBookingTimeIsNotOutOfBounds = async <T extends ValidateBookingTimeEventType>(
+  reqBodyStartTime: string,
+  reqBodyTimeZone: string,
+  eventType: T,
+  logger: Logger<unknown>
+) => {
+  const user = eventType.users.find((user) => user.id === eventType.userId);
+  const userSchedule = user?.schedules.find((schedule) => schedule.id === user?.defaultScheduleId);
+  const eventTimeZone = eventType.schedule?.timeZone ?? userSchedule?.timeZone;
+
+  let timeOutOfBounds = false;
+  try {
+    timeOutOfBounds = isOutOfBounds(
+      reqBodyStartTime,
+      {
+        periodType: eventType.periodType,
+        periodDays: eventType.periodDays,
+        periodEndDate: eventType.periodEndDate,
+        periodStartDate: eventType.periodStartDate,
+        periodCountCalendarDays: eventType.periodCountCalendarDays,
+        bookerUtcOffset: getUTCOffsetByTimezone(reqBody.timeZone) ?? 0,
+        eventUtcOffset: eventTimeZone ? getUTCOffsetByTimezone(eventTimeZone) ?? 0 : 0,
+      },
+      eventType.minimumBookingNotice
+    );
+  } catch (error) {
+    logger.warn({
+      message: "NewBooking: Unable set timeOutOfBounds. Using false. ",
+    });
+    if (error instanceof BookingDateInPastError) {
+      logger.info(`Booking eventType ${eventTypeId} failed`, JSON.stringify({ error }));
+      throw new HttpError({ statusCode: 400, message: error.message });
+    }
+  }
+
+  if (timeOutOfBounds) {
+    const error = {
+      errorCode: "BookingTimeOutOfBounds",
+      message: `EventType '${eventType.eventName}' cannot be booked at this time.`,
+    };
+    logger.warn({
+      message: `NewBooking: EventType '${eventType.eventName}' cannot be booked at this time.`,
+    });
+    throw new HttpError({ statusCode: 400, message: error.message });
+  }
+};
