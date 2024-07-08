1383301c67b793335713e866e685494baf8d2293
Rajiv Sahal <rajivsahal@Rajivs-MacBook-Pro.local>
2024-07-08 16:57:28 +0200

    update booker embed wrapper

--- packages/platform/atoms/booker-embed/BookerEmbedPlatformWrapper.tsx
+++ packages/platform/atoms/booker-embed/BookerEmbedPlatformWrapper.tsx
@@ -1,19 +1,16 @@
-// import { CalProvider } from "../../atoms/cal-provider/CalProvider";
-// import type { BookerPlatformWrapperAtomProps } from "../booker/BookerPlatformWrapper";
-// import { BookerEmbed } from "./BookerEmbed";
-// // need to wrap this around a cal provider where accessToken, clientId and optios would be preset by us
-// export const BookerEmbedPlatformWrapper = (props: BookerPlatformWrapperAtomProps) => {
-//   return (
-//     <CalProvider>
-//       <BookerEmbed {...props} />
-//     </CalProvider>
-//   );
-// };
+import { CalProvider } from "../../atoms/cal-provider/CalProvider";
+import type { BookerPlatformWrapperAtomProps } from "../booker/BookerPlatformWrapper";
+import { BookerEmbed } from "./BookerEmbed";
 
-export const BookerEmbedPlatformWrapper = () => {
-  console.log(import.meta.env.VITE_BOOKER_EMBED_OAUTH_CLIENT_ID, "env for booker embed oauth client id");
-  console.log(import.meta.env.VITE_BOOKER_EMBED_API_URL, "env for booker embed api url");
-  console.log(import.meta.env.VITE_BOOKER_EMBED_OAUTH_CLIENT_ID, "env for booker ember oauth client id");
-
-  return <>Hello world, this is just to test booker embed</>;
+export const BookerEmbedPlatformWrapper = (props: BookerPlatformWrapperAtomProps) => {
+  return (
+    <CalProvider
+      clientId={import.meta.env.VITE_BOOKER_EMBED_OAUTH_CLIENT_ID}
+      options={{
+        apiUrl: import.meta.env.VITE_BOOKER_EMBED_API_URL,
+        refreshUrl: import.meta.env.VITE_BOOKER_EMBED_REFRESH_URL,
+      }}>
+      <BookerEmbed {...props} />
+    </CalProvider>
+  );
 };
