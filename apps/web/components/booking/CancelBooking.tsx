6d1aac483d99b2dfbc4e1f9b7265c9fd12409b18
monk007 <157161434+bdzb@users.noreply.github.com>
2024-07-08 14:26:06 +0530

    Update CancelBooking.tsx

--- apps/web/components/booking/CancelBooking.tsx
+++ apps/web/components/booking/CancelBooking.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
 import { sdkActionManager } from "@calcom/embed-core/embed-iframe";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
