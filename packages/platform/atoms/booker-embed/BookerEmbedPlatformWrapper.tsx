9a945817df4d7805bf103978556e660567734034
Rajiv Sahal <rajivsahal@Rajivs-MacBook-Pro.local>
2024-07-08 17:11:27 +0200

    fixup: refresh url not needed

--- packages/platform/atoms/booker-embed/BookerEmbedPlatformWrapper.tsx
+++ packages/platform/atoms/booker-embed/BookerEmbedPlatformWrapper.tsx
@@ -8,7 +8,6 @@ export const BookerEmbedPlatformWrapper = (props: BookerPlatformWrapperAtomProps
       clientId={import.meta.env.VITE_BOOKER_EMBED_OAUTH_CLIENT_ID}
       options={{
         apiUrl: import.meta.env.VITE_BOOKER_EMBED_API_URL,
-        refreshUrl: import.meta.env.VITE_BOOKER_EMBED_REFRESH_URL,
       }}>
       <BookerEmbed {...props} />
     </CalProvider>
