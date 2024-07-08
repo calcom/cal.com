dfcd0f837b0bb9180e1bdee3aee597f9c9dd960d
Keith Williams <keithwillcode@gmail.com>
2024-07-08 12:53:48 +0100

    Skopping telemetry to try and get it to build

--- apps/web/instrumentation.ts
+++ apps/web/instrumentation.ts
@@ -4,12 +4,14 @@ export function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     Sentry.init({
       dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+      telemetry: false,
     });
   }
 
   if (process.env.NEXT_RUNTIME === "edge") {
     Sentry.init({
       dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+      telemetry: false,
     });
   }
 }
