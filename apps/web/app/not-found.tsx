ffcd8043c10513d271a043b1f1bc0292396e3c33
Keith Williams <keithwillcode@gmail.com>
2024-07-09 09:59:43 +0100

    fixing tests

--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div>
+    <div data-testid="404-page">
       <h1>404 - Page Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
     </div>
