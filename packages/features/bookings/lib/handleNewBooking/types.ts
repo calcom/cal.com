2330d551ed3f52146516eb05c5f6053cb0b31f15
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-07-09 12:50:34 -0400

    Type fix

--- packages/features/bookings/lib/handleNewBooking/types.ts
+++ packages/features/bookings/lib/handleNewBooking/types.ts
@@ -55,7 +55,7 @@ export interface IEventTypePaymentCredentialType {
 export type IsFixedAwareUser = User & {
   isFixed: boolean;
   credentials: CredentialPayload[];
-  organization: { slug: string };
+  organization?: { slug: string };
   priority?: number;
 };
 
