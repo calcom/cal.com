0a22ad97ab3efaa18a71788a58b45aceccabcfff
Keith Williams <keithwillcode@gmail.com>
2024-07-09 07:22:59 +0100

    Attempt to build

--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -2,14 +2,8 @@ import NotFoundPage from "@pages/404";
 import { WithLayout } from "app/layoutHOC";
 import type { GetStaticPropsContext } from "next";
 
-import { getTranslations } from "@server/lib/getTranslations";
-
 const getData = async (context: GetStaticPropsContext) => {
-  const i18n = await getTranslations(context);
-
-  return {
-    i18n,
-  };
+  return {};
 };
 
 export const dynamic = "force-static";
