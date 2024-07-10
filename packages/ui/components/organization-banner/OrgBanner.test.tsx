f1d936d4c29af0e1cc8ffc650c05f8ad394eef26
Jake Hebert <jacobshebert@gmail.com>
2024-07-10 12:19:51 -0500

    fix: add alt to OrgBanner in test

--- packages/ui/components/organization-banner/OrgBanner.test.tsx
+++ packages/ui/components/organization-banner/OrgBanner.test.tsx
@@ -5,7 +5,7 @@ import { OrgBanner } from "./OrgBanner";
 
 describe("tests for UserAvatar component", () => {
   test("Should render the UsersAvatar Correctly", () => {
-    const { getByTestId } = render(<OrgBanner data-testid="user-avatar-test" />);
+    const { getByTestId } = render(<OrgBanner alt="Test Org Banner" data-testid="user-avatar-test" />);
     const orgBanner = getByTestId("org-banner-test");
 
     expect(orgBanner).toBeInTheDocument();
