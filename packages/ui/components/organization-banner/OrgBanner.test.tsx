e18755df7c498339f558d5439d42bb5575b81be0
Jake Hebert <jacobshebert@gmail.com>
2024-07-10 12:29:16 -0500

    fix: failing OrgBanner test, added neg test

--- packages/ui/components/organization-banner/OrgBanner.test.tsx
+++ packages/ui/components/organization-banner/OrgBanner.test.tsx
@@ -3,11 +3,18 @@ import { render } from "@testing-library/react";
 
 import { OrgBanner } from "./OrgBanner";
 
-describe("tests for UserAvatar component", () => {
-  test("Should render the UsersAvatar Correctly", () => {
-    const { getByTestId } = render(<OrgBanner alt="Test Org Banner" data-testid="user-avatar-test" />);
+describe("tests for OrgBanner component", () => {
+  test("Should render the OrgBanner correctly when supplied imageSrc", () => {
+    const { getByTestId } = render(
+      <OrgBanner alt="Test Org Banner" data-testid="org-banner-test" imageSrc="/logo.svg" />
+    );
     const orgBanner = getByTestId("org-banner-test");
 
     expect(orgBanner).toBeInTheDocument();
+  });
+  test("Should not render the OrgBanner when no imageSrc is supplied", () => {
+    const { getByTestId } = render(<OrgBanner alt="Test Org Banner" data-testid="org-banner-test" />);
+
+    expect(() => getByTestId("org-banner-test")).toThrow();
   });
 });
