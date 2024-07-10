922265f63436d6b9fe6412b90ff1e111f50bb7d3
Nick Holden <nick.r.holden@gmail.com>
2024-07-10 12:05:14 -0400

    Improve redirect after OAuth flow

--- packages/app-store/campsite/api/callback.ts
+++ packages/app-store/campsite/api/callback.ts
@@ -1,13 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 import { WEBAPP_URL } from "@calcom/lib/constants";
+import { getSafeRedirectUrl } from "@calcom/lib/getSafeRedirectUrl";
 import prisma from "@calcom/prisma";
 
 import getAppKeysFromSlug from "../../_utils/getAppKeysFromSlug";
 import getInstalledAppPath from "../../_utils/getInstalledAppPath";
 import createOAuthAppCredential from "../../_utils/oauth/createOAuthAppCredential";
+import { decodeOAuthState } from "../../_utils/oauth/decodeOAuthState";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const state = decodeOAuthState(req);
   const userId = req.session?.user.id;
   if (!userId) {
     return res.status(404).json({ message: "No user found" });
@@ -64,5 +67,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     req
   );
 
-  res.redirect(getInstalledAppPath({ variant: "conferencing", slug: "campsite" }));
+  res.redirect(
+    getSafeRedirectUrl(state?.returnTo) ?? getInstalledAppPath({ variant: "conferencing", slug: "campsite" })
+  );
 }
