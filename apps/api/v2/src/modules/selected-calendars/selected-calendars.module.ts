f10d16365634b1199c40265f202091c0255b9492
Ryukemeister <sahalrajiv6900@gmail.com>
2024-07-16 14:20:19 +0200

    include selected calendars controller in selected calendars module

--- apps/api/v2/src/modules/selected-calendars/selected-calendars.module.ts
+++ apps/api/v2/src/modules/selected-calendars/selected-calendars.module.ts
@@ -1,10 +1,12 @@
 import { PrismaModule } from "@/modules/prisma/prisma.module";
+import { SelectedCalendarsController } from "@/modules/selected-calendars/controllers/selected-calendars.controller";
 import { SelectedCalendarsRepository } from "@/modules/selected-calendars/selected-calendars.repository";
 import { Module } from "@nestjs/common";
 
 @Module({
   imports: [PrismaModule],
   providers: [SelectedCalendarsRepository],
+  controllers: [SelectedCalendarsController],
   exports: [SelectedCalendarsRepository],
 })
 export class SelectedCalendarsModule {}
