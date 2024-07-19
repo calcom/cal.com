88cca99ddd87bc662ff9f7ea2c2e8554ecb39050
krishnendu <krishnendu.1923CO1112@kiet.edu>
2024-07-19 16:17:07 +0530

    fix: #15832

--- packages/features/embed/lib/EmbedTabs.tsx
+++ packages/features/embed/lib/EmbedTabs.tsx
@@ -44,7 +44,7 @@ export const tabs = [
             data-testid="embed-code"
             ref={ref as typeof ref & MutableRefObject<HTMLTextAreaElement>}
             name="embed-code"
-            className="text-default bg-default h-[calc(100%-50px)] font-mono"
+            className="text-default h-[calc(100%-50px)] bg-gray-100 font-mono"
             style={{ resize: "none", overflow: "auto" }}
             readOnly
             value={`<!-- Cal ${embedType} embed code begins -->\n${
@@ -96,7 +96,7 @@ export const tabs = [
             data-testid="embed-react"
             ref={ref as typeof ref & MutableRefObject<HTMLTextAreaElement>}
             name="embed-react"
-            className="text-default bg-default selection:bg-subtle h-[calc(100%-50px)] font-mono"
+            className="text-default selection:bg-subtle h-[calc(100%-50px)] bg-gray-100 font-mono"
             readOnly
             style={{ resize: "none", overflow: "auto" }}
             value={`/* First make sure that you have installed the package */
