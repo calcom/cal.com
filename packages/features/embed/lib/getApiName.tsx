4d434fba31913c89aedabbf204484a0e9eebe3ff
Hariom <hariombalhara@gmail.com>
2024-07-19 14:51:07 +0530

    Update e2e

--- packages/features/embed/lib/getApiName.tsx
+++ packages/features/embed/lib/getApiName.tsx
@@ -1,4 +1,10 @@
-function getApiNameWithNamespace({ namespace, mainApiName }: { namespace: string; mainApiName: string }) {
+export function getApiNameWithNamespace({
+  namespace,
+  mainApiName,
+}: {
+  namespace: string;
+  mainApiName: string;
+}) {
   const isAValidVariableName = /^[a-zA-Z_$][a-zA-Z_$0-9]*$/.test(namespace);
   // Try to use dot notation if possible because it's more readable otherwise use bracket notation
   return isAValidVariableName ? `${mainApiName}.ns.${namespace}` : `${mainApiName}.ns["${namespace}"]`;
