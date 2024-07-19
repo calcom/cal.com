b7964c863d13f5629d26d9b66c777916cc102ce4
Hariom <hariombalhara@gmail.com>
2024-07-19 13:28:22 +0530

    fix: Fix React cal api namespaced usage

--- packages/features/embed/lib/getApiName.tsx
+++ packages/features/embed/lib/getApiName.tsx
@@ -1,14 +1,23 @@
-export function getApiName({
-  namespace,
-  mainApiName = "Cal",
-}: {
-  namespace: string | null;
-  mainApiName?: string;
-}) {
-  if (!namespace) {
-    return mainApiName;
-  }
+function getApiNameWithNamespace({ namespace, mainApiName }: { namespace: string; mainApiName: string }) {
   const isAValidVariableName = /^[a-zA-Z_$][a-zA-Z_$0-9]*$/.test(namespace);
   // Try to use dot notation if possible because it's more readable otherwise use bracket notation
   return isAValidVariableName ? `${mainApiName}.ns.${namespace}` : `${mainApiName}.ns["${namespace}"]`;
+}
+
+function getApiNameWithoutNamespace({ mainApiName }: { mainApiName: string }) {
+  return mainApiName;
+}
+
+export function getApiNameForReactSnippet({ mainApiName }: { mainApiName: string }) {
+  return getApiNameWithoutNamespace({ mainApiName });
+}
+
+export function getApiNameForVanillaJsSnippet({
+  namespace,
+  mainApiName,
+}: {
+  namespace: string;
+  mainApiName: string;
+}) {
+  return getApiNameWithNamespace({ mainApiName, namespace });
 }
