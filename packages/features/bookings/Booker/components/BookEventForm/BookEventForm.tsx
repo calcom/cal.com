444f43c5a8f3daf43015b0144e6e73d41ec62a99
Felippe Fernandes <lfelippeeng@gmail.com>
2024-07-20 19:11:55 -0300

    Fix incorrect policy links

--- packages/features/bookings/Booker/components/BookEventForm/BookEventForm.tsx
+++ packages/features/bookings/Booker/components/BookEventForm/BookEventForm.tsx
@@ -156,7 +156,7 @@ export const BookEventForm = ({
                 <Link
                   className="text-emphasis hover:underline"
                   key="privacy"
-                  href={`${WEBSITE_URL}/politica-de-privacidade`}
+                  href={`${WEBSITE_URL}/politicas-de-privacidade`}
                   target="_blank">
                   Políticas de privacidade
                 </Link>,
