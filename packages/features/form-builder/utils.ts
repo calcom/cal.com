e30c774eaf564d44f181cddc70035e729a9c800c
Felippe Fernandes <lfelippeeng@gmail.com>
2024-07-20 19:33:13 -0300

    Fix try to replace undefined value

--- packages/features/form-builder/utils.ts
+++ packages/features/form-builder/utils.ts
@@ -140,6 +140,7 @@ const validateCPF = (cpf: string) => {
 };
 
 export const cpfMask = (value: string) => {
+  if (!value) return { isValid: false, value: "" };
   const onlyNumbers = value.replace(/[^\d]/g, "");
 
   const cpfIsValid = validateCPF(onlyNumbers);
