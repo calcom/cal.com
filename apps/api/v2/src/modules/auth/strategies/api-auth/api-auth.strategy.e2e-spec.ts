95d190078bdbdd07f4fdd413f893aa1070daf1bc
supalarry <laurisskraucis@gmail.com>
2024-07-22 15:08:35 +0200

    feat: ApiAuthStrategy handle oauth credentials

--- apps/api/v2/src/modules/auth/strategies/api-auth/api-auth.strategy.e2e-spec.ts
+++ apps/api/v2/src/modules/auth/strategies/api-auth/api-auth.strategy.e2e-spec.ts
@@ -7,6 +7,7 @@ import { OAuthClientRepository } from "@/modules/oauth-clients/oauth-client.repo
 import { OAuthFlowService } from "@/modules/oauth-clients/services/oauth-flow.service";
 import { PrismaReadService } from "@/modules/prisma/prisma-read.service";
 import { PrismaWriteService } from "@/modules/prisma/prisma-write.service";
+import { ProfilesModule } from "@/modules/profiles/profiles.module";
 import { TokensRepository } from "@/modules/tokens/tokens.repository";
 import { UsersRepository } from "@/modules/users/users.repository";
 import { ExecutionContext, HttpException } from "@nestjs/common";
@@ -17,10 +18,13 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { PlatformOAuthClient, Team, User } from "@prisma/client";
 import { ApiKeysRepositoryFixture } from "test/fixtures/repository/api-keys.repository.fixture";
 import { OAuthClientRepositoryFixture } from "test/fixtures/repository/oauth-client.repository.fixture";
+import { ProfileRepositoryFixture } from "test/fixtures/repository/profiles.repository.fixture";
 import { TeamRepositoryFixture } from "test/fixtures/repository/team.repository.fixture";
 import { TokensRepositoryFixture } from "test/fixtures/repository/tokens.repository.fixture";
 import { UserRepositoryFixture } from "test/fixtures/repository/users.repository.fixture";
 import { MockedRedisService } from "test/mocks/mock-redis-service";
+
+import { X_CAL_CLIENT_ID, X_CAL_SECRET_KEY } from "@calcom/platform-constants";
 
 import { ApiAuthStrategy } from "./api-auth.strategy";
 
@@ -33,10 +37,15 @@ describe("ApiAuthStrategy", () => {
   let oAuthClient: PlatformOAuthClient;
   let apiKeysRepositoryFixture: ApiKeysRepositoryFixture;
   let oAuthClientRepositoryFixture: OAuthClientRepositoryFixture;
+  let profilesRepositoryFixture: ProfileRepositoryFixture;
+
   const validApiKeyEmail = "api-key-user-email@example.com";
   const validAccessTokenEmail = "access-token-user-email@example.com";
+  const validOAuthEmail = "oauth-user@example.com";
+
   let validApiKeyUser: User;
   let validAccessTokenUser: User;
+  let validOAuthUser: User;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -46,6 +55,7 @@ describe("ApiAuthStrategy", () => {
           isGlobal: true,
           load: [appConfig],
         }),
+        ProfilesModule,
       ],
       providers: [
         MockedRedisService,
@@ -71,6 +81,7 @@ describe("ApiAuthStrategy", () => {
     apiKeysRepositoryFixture = new ApiKeysRepositoryFixture(module);
     teamRepositoryFixture = new TeamRepositoryFixture(module);
     oAuthClientRepositoryFixture = new OAuthClientRepositoryFixture(module);
+    profilesRepositoryFixture = new ProfileRepositoryFixture(module);
     organization = await teamRepositoryFixture.create({ name: "organization" });
     validApiKeyUser = await userRepositoryFixture.create({
       email: validApiKeyEmail,
@@ -78,6 +89,18 @@ describe("ApiAuthStrategy", () => {
     validAccessTokenUser = await userRepositoryFixture.create({
       email: validAccessTokenEmail,
     });
+
+    validOAuthUser = await userRepositoryFixture.create({
+      email: validOAuthEmail,
+    });
+
+    await profilesRepositoryFixture.create({
+      uid: "asd-asd",
+      username: validOAuthEmail,
+      user: { connect: { id: validOAuthUser.id } },
+      organization: { connect: { id: organization.id } },
+    });
+
     const data = {
       logo: "logo-url",
       name: "name",
@@ -135,6 +158,12 @@ describe("ApiAuthStrategy", () => {
       if (user) expect(user.id).toEqual(validApiKeyUser.id);
     });
 
+    it("should return user associated with valid OAuth client", async () => {
+      const user = await strategy.oAuthClientStrategy(oAuthClient.id, oAuthClient.secret);
+      expect(user).toBeDefined();
+      expect(user.id).toEqual(validOAuthUser.id);
+    });
+
     it("should throw 401 if api key is invalid", async () => {
       const context: ExecutionContext = {
         switchToHttp: () => ({
@@ -158,7 +187,55 @@ describe("ApiAuthStrategy", () => {
       }
     });
 
-    it("should throw 401 if Authorization header does not contain auth token", async () => {
+    it("should throw 401 if OAuth ID is invalid", async () => {
+      const context: ExecutionContext = {
+        switchToHttp: () => ({
+          getRequest: () => ({
+            headers: {
+              [X_CAL_CLIENT_ID]: `${oAuthClient.id}gibberish`,
+              [X_CAL_SECRET_KEY]: `secret`,
+            },
+            get: (key: string) =>
+              ({ Authorization: `Bearer cal_test_badkey1234`, origin: "http://localhost:3000" }[key]),
+          }),
+        }),
+      } as ExecutionContext;
+      const request = context.switchToHttp().getRequest();
+
+      try {
+        await strategy.authenticate(request);
+      } catch (error) {
+        if (error instanceof HttpException) {
+          expect(error.getStatus()).toEqual(401);
+        }
+      }
+    });
+
+    it("should throw 401 if OAuth secret is invalid", async () => {
+      const context: ExecutionContext = {
+        switchToHttp: () => ({
+          getRequest: () => ({
+            headers: {
+              [X_CAL_CLIENT_ID]: `${oAuthClient.id}`,
+              [X_CAL_SECRET_KEY]: `gibberish`,
+            },
+            get: (key: string) =>
+              ({ Authorization: `Bearer cal_test_badkey1234`, origin: "http://localhost:3000" }[key]),
+          }),
+        }),
+      } as ExecutionContext;
+      const request = context.switchToHttp().getRequest();
+
+      try {
+        await strategy.authenticate(request);
+      } catch (error) {
+        if (error instanceof HttpException) {
+          expect(error.getStatus()).toEqual(401);
+        }
+      }
+    });
+
+    it("should throw 401 if request does not contain Bearer token nor OAuth client credentials", async () => {
       const context: ExecutionContext = {
         switchToHttp: () => ({
           getRequest: () => ({
@@ -176,5 +253,13 @@ describe("ApiAuthStrategy", () => {
         }
       }
     });
+  });
+
+  afterAll(async () => {
+    await oAuthClientRepositoryFixture.delete(oAuthClient.id);
+    await userRepositoryFixture.delete(validApiKeyUser.id);
+    await userRepositoryFixture.delete(validAccessTokenUser.id);
+    await userRepositoryFixture.delete(validOAuthUser.id);
+    await teamRepositoryFixture.delete(organization.id);
   });
 });
