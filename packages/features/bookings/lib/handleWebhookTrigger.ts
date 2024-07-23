eecbae5b2a4cc67829e7ea85ffe90e49a80fa76d
CarinaWolli <wollencarina@gmail.com>
2024-07-23 11:51:08 +0200

    code clean up

--- packages/features/bookings/lib/handleWebhookTrigger.ts
+++ packages/features/bookings/lib/handleWebhookTrigger.ts
@@ -1,11 +1,7 @@
 import getWebhooks from "@calcom/features/webhooks/lib/getWebhooks";
 import type { GetSubscriberOptions } from "@calcom/features/webhooks/lib/getWebhooks";
 import sendPayload from "@calcom/features/webhooks/lib/sendOrSchedulePayload";
-import {
-  isEventPayload,
-  isOOOEntryPayload,
-  type WebhookPayloadType,
-} from "@calcom/features/webhooks/lib/sendPayload";
+import { isEventPayload, type WebhookPayloadType } from "@calcom/features/webhooks/lib/sendPayload";
 import logger from "@calcom/lib/logger";
 import { safeStringify } from "@calcom/lib/safeStringify";
 
@@ -20,24 +16,12 @@ export async function handleWebhookTrigger(args: {
     const promises = subscribers.map((sub) =>
       sendPayload(sub.secret, args.eventTrigger, new Date().toISOString(), sub, args.webhookData).catch(
         (e) => {
-          let bookingId, oooEntryId, bookingUid, oooEntryUUID;
-
-          if (isOOOEntryPayload(args.webhookData)) {
-            ({ id: oooEntryId, uuid: oooEntryUUID } = args.webhookData.oooEntry);
-          }
-
           if (isEventPayload(args.webhookData)) {
-            ({ bookingId, uid: bookingUid } = args.webhookData);
+            logger.error(
+              `Error executing webhook for event: ${args.eventTrigger}, URL: ${sub.subscriberUrl}, booking id: ${args.webhookData.bookingId}, booking uid: ${args.webhookData.uid}`,
+              safeStringify(e)
+            );
           }
-
-          const idString = bookingId ? `booking id:${bookingId}` : `ooo entry id:${oooEntryId}`;
-
-          const uidOrUUIDString = bookingUid ? `booking uid:${bookingId}` : `ooo entry UUID:${oooEntryUUID}`;
-
-          logger.error(
-            `Error executing webhook for event: ${args.eventTrigger}, URL: ${sub.subscriberUrl}, ${idString}, ${uidOrUUIDString}`,
-            safeStringify(e)
-          );
         }
       )
     );
