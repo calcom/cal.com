bfac183de318d42d7b056fc1efd074c63972501f
supalarry <laurisskraucis@gmail.com>
2024-07-25 09:28:34 +0200

    feat: useTeamEventType and useTeamEventTypes hooks

---
+++ packages/platform/atoms/hooks/event-types/public/useTeamEventTypes.ts
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { SUCCESS_STATUS } from "@calcom/platform-constants";
+import type { TeamEventTypeOutput_2024_06_14 } from "@calcom/platform-types";
+import type { ApiResponse, ApiSuccessResponse } from "@calcom/platform-types";
+
+import http from "../../../lib/http";
+
+export const QUERY_KEY = "use-team-event-types";
+export const useTeamEventTypes = (orgId: number, teamId: number) => {
+  const pathname = `/organizations/${orgId}/teams/${teamId}/event-types`;
+
+  return useQuery({
+    queryKey: [QUERY_KEY, orgId, teamId],
+    queryFn: () => {
+      return http?.get<ApiResponse<TeamEventTypeOutput_2024_06_14[]>>(pathname).then((res) => {
+        if (res.data.status === SUCCESS_STATUS) {
+          return (res.data as ApiSuccessResponse<TeamEventTypeOutput_2024_06_14[]>).data;
+        }
+        throw new Error(res.data.error.message);
+      });
+    },
+  });
+};
