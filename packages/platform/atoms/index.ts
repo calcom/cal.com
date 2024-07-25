62b94f205518073938ced2e04e30309557cb3b84
supalarry <laurisskraucis@gmail.com>
2024-07-25 14:35:40 +0200

    feat: BookerPlatformWrapper enable team events by exposting orgId and teamId props

--- packages/platform/atoms/index.ts
+++ packages/platform/atoms/index.ts
@@ -6,6 +6,7 @@ export { useIsPlatform } from "./hooks/useIsPlatform";
 export { useAtomsContext } from "./hooks/useAtomsContext";
 export { useConnectedCalendars } from "./hooks/useConnectedCalendars";
 export { useEventTypes } from "./hooks/event-types/public/useEventTypes";
+export { useTeamEventTypes } from "./hooks/event-types/public/useTeamEventTypes";
 export { useEventType as useEvent } from "./hooks/event-types/public/useEventType";
 export { useEventTypeById } from "./hooks/event-types/private/useEventTypeById";
 export { useCancelBooking } from "./hooks/useCancelBooking";
