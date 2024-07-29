f0c7a7f994ca57ff943fb98f97abe032aceb7035
Souptik Datta <souptikdatta2001@gmail.com>
2024-07-29 17:39:45 +0530

    refactor: Create separate test for edit ooo event
    Signed-off-by: Souptik Datta <souptikdatta2001@gmail.com>

--- apps/web/playwright/out-of-office.e2e.ts
+++ apps/web/playwright/out-of-office.e2e.ts
@@ -30,7 +30,7 @@ test.describe("Out of office", () => {
     await expect(page.locator(`data-testid=table-redirect-n-a`)).toBeVisible();
   });
 
-  test("User can configure and edit booking redirect", async ({ page, users }) => {
+  test.skip("User can configure booking redirect", async ({ page, users }) => {
     const user = await users.create({ name: "userOne" });
     const userTo = await users.create({ name: "userTwo" });
     const userToSecond = await users.create({ name: "userThree" });
@@ -90,6 +90,68 @@ test.describe("Out of office", () => {
 
     // expect table-redirect-toUserId to be visible
     await expect(page.locator(`data-testid=table-redirect-${userTo.username}`)).toBeVisible();
+  });
+
+  test("User can edit out of office entry", async ({ page, users }) => {
+    const user = await users.create({ name: "userOne" });
+    const userTo = await users.create({ name: "userTwo" });
+    const userToSecond = await users.create({ name: "userThree" });
+
+    const team = await prisma.team.create({
+      data: {
+        name: "test-insights",
+        slug: `test-insights-${Date.now()}-${randomString(5)}}`,
+      },
+    });
+
+    // create memberships
+    await prisma.membership.createMany({
+      data: [
+        {
+          userId: user.id,
+          teamId: team.id,
+          accepted: true,
+          role: "ADMIN",
+        },
+        {
+          userId: userTo.id,
+          teamId: team.id,
+          accepted: true,
+          role: "ADMIN",
+        },
+        {
+          userId: userToSecond.id,
+          teamId: team.id,
+          accepted: true,
+          role: "ADMIN",
+        },
+      ],
+    });
+
+    // Skip creating the ooo entry through front-end as we can assume that it has already been tested above.
+    const uuid = uuidv4();
+    await prisma.outOfOfficeEntry.create({
+      data: {
+        start: dayjs().startOf("day").toDate(),
+        end: dayjs().startOf("day").add(1, "w").toDate(),
+        uuid,
+        user: { connect: { id: user.id } },
+        toUser: { connect: { id: userTo.id } },
+        createdAt: new Date(),
+        reason: {
+          connect: {
+            id: 1,
+          },
+        },
+      },
+    });
+
+    await user.apiLogin();
+
+    await page.goto(`/settings/my-account/out-of-office`);
+
+    // expect table-redirect-toUserId to be visible
+    await expect(page.locator(`data-testid=table-redirect-${userTo.username}`)).toBeVisible();
 
     // Open the edit modal and change redirect user and note.
     await page.getByTestId(`ooo-edit-${userTo.username}`).click();
@@ -99,8 +161,8 @@ test.describe("Out of office", () => {
 
     await page.getByTestId("team_username_select").click();
 
-    await page.locator("#react-select-5-input").fill("userThree");
-    await page.locator("#react-select-5-input").press("Enter");
+    await page.locator("#react-select-3-input").fill("userThree");
+    await page.locator("#react-select-3-input").press("Enter");
 
     // send request
     await page.getByTestId("create-or-edit-entry-ooo-redirect").click();
