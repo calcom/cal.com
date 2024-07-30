a829a0850da4c098bef4215944fcd4abe16d3735
Rajesh Jonnalagadda <rajeshjonnalagadda48@gmail.com>
2024-07-30 14:30:06 +0530

    fix: field value valdiation at initilisation and add the  unit test cases for that change

--- packages/features/bookings/Booker/components/hooks/useInitialFormValues.ts
+++ packages/features/bookings/Booker/components/hooks/useInitialFormValues.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import type { z, ZodSchema } from "zod";
-import { z as _z } from "zod";
+import type { ZodSchema } from "zod";
+import { z as _z, z } from "zod";
 
 import { useBookerStore } from "@calcom/features/bookings/Booker/store";
 import type getBookingResponsesSchema from "@calcom/features/bookings/lib/getBookingResponsesSchema";
@@ -28,18 +28,18 @@ type UseInitialFormValuesProps = {
 };
 
 //basically it will partially test the input values against the schema. validation are not needed at the initial stage
-function getFieldSchema(field: Field): ZodSchema<unknown> {
+export function getFieldSchema(field: Field): ZodSchema<unknown> {
   switch (field.type) {
     case "name":
       return _z.string().optional();
     case "email":
-      return _z.string().email({ message: "Invalid email address" }).optional();
+      return _z.string().optional();
     case "text":
       return _z.string().optional();
     case "textarea":
       return _z.string().optional();
     case "multiemail":
-      return _z.array(_z.string().email({ message: "Invalid email address" })).optional();
+      return _z.union([_z.string(), _z.array(_z.string())]).optional();
     case "address":
       return _z.string().optional();
     case "phone":
@@ -61,20 +61,54 @@ function getFieldSchema(field: Field): ZodSchema<unknown> {
         .optional();
     case "radioInput":
     case "select":
-    case "multiselect":
-    case "checkbox":
     case "radio":
       if (field.options && field.options.length > 0) {
-        // Convert options to tuple
         const values = field.options.map((opt) => opt.value) as [string, ...string[]];
-        return _z.enum(values).optional();
+        return _z.union([_z.enum(values), _z.string()]).optional();
       } else {
         return _z.any().optional();
+      }
+    case "multiselect":
+    case "checkbox":
+      if (field.options && field.options.length > 0) {
+        const validValues = field.options.map((opt) => opt.value) as [string, ...string[]];
+        return z
+          .union([
+            z
+              .array(z.string())
+              .refine((arr) => arr.some((val) => validValues.includes(val)), {
+                message: "Invalid option in array",
+              })
+              .transform((arr) => arr.filter((val) => validValues.includes(val))),
+
+            z
+              .string()
+              .refine((val) => validValues.includes(val), {
+                message: "Invalid single option",
+              })
+              .optional(),
+          ])
+          .optional();
+      } else {
+        return _z.union([_z.array(_z.string()).optional(), _z.string()]).optional();
       }
     // Add more cases as needed
     default:
       return _z.any().optional();
   }
+}
+
+export function checkParseQueryValues(field: Field, parsedQuery: Record<string, unknown> | undefined) {
+  if (!parsedQuery || !field) return { ignore: true };
+
+  if (!parsedQuery[field.name]) {
+    return { ignore: true };
+  }
+
+  const schema = getFieldSchema(field);
+  if (!schema) return { ignore: true };
+  const parsedResponses = schema.safeParse(parsedQuery[field.name]);
+  return { success: parsedResponses.success, value: parsedResponses?.data };
 }
 
 export function useInitialFormValues({
@@ -96,18 +130,6 @@ export function useInitialFormValues({
   const formValues = useBookerStore((state) => state.formValues);
 
   //currently while initializing the form we are checking if the query params are valid and if not, we are returning default values
-  function checkParseQueryValues(field: Field, parsedQuery: Record<string, unknown> | undefined) {
-    if (!parsedQuery || !field) return true;
-
-    if (!parsedQuery[field.name]) {
-      return true;
-    }
-
-    const schema = getFieldSchema(field);
-    if (!schema) return true;
-    const parsedResponses = schema.safeParse(parsedQuery[field.name]);
-    return parsedResponses.success;
-  }
 
   useEffect(() => {
     (async function () {
@@ -153,9 +175,10 @@ export function useInitialFormValues({
         };
 
         const responses = eventType.bookingFields.reduce((responses, field) => {
+          const result = checkParseQueryValues(field, parsedQuery);
           let value;
-          if (checkParseQueryValues(field, parsedQuery)) {
-            value = parsedQuery[field.name] || undefined;
+          if (result.success) {
+            value = result.value || undefined;
           }
           return {
             ...responses,
@@ -184,9 +207,10 @@ export function useInitialFormValues({
       };
 
       const responses = eventType.bookingFields.reduce((responses, field) => {
+        const result = checkParseQueryValues(field, parsedQuery);
         let value;
-        if (checkParseQueryValues(field, bookingData?.responses)) {
-          value = bookingData?.responses[field.name];
+        if (result.success) {
+          value = result.value || undefined;
         }
         return {
           ...responses,
--- packages/ui/components/formbuilder/FormBuilderField.test.tsx
+++ packages/ui/components/formbuilder/FormBuilderField.test.tsx
@@ -2,6 +2,7 @@ import type { Control } from "react-hook-form";
 import { useFormContext } from "react-hook-form";
 import { describe, expect, vi } from "vitest";
 
+import { checkParseQueryValues } from "@calcom/features/bookings/Booker/components/hooks/useInitialFormValues";
 import { useShouldBeDisabledDueToPrefill } from "@calcom/features/form-builder/FormBuilderField";
 
 import type { RhfFormField } from "./testUtils";
@@ -9,9 +10,23 @@ import type { RhfFormField } from "./testUtils";
 // Mock dependencies
 vi.mock("@hookform/error-message", () => ({ ErrorMessage: () => null }));
 
+//positive and negativev test cases
+const multiOptionsFieldSearchParams = {
+  "test-multiselect-p-n": ["Option 1", "Option 2"], // Mock search params
+  "test-multiselect-n-n": ["Option 1", "Option 3"], // Mock search params
+  "test-checkbox-p-n": ["Option 1", "Option 2"], // Mock search params
+  "test-checkbox-n-n": ["Option 1", "Option 3"], // Mock search params
+  "test-multiemail-p-n": ["test123@gmail.com", "test1234@gmail.com"], // Mock search params
+
+  "n-test-multiselect-p-n": "Option 3", // Mock search params
+  "n-test-multiselect-n-n": ["Option 4", "Option 3"], // Mock search params
+  "n-test-checkbox-p-n": "Option 3", // Mock search params
+  "n-test-checkbox-n-n": ["Option 4", "Option 3"], // Mock search params
+};
+
 const searchParams = {
   "test-email-n": "test123@gmail", // Mock search params
-  "test-phone-n": "+919951881732", // Mock search params
+  "test-phone-n": "+919952882732", // Mock search params
   "test-address-n": "test123", // Mock search params
   "test-text-n": "test123", // Mock search params
   "test-textarea-n": "test123", // Mock search params
@@ -21,12 +36,18 @@ const searchParams = {
   "test-radio-n": "Option 1", // Mock search params
   "test-boolean-n": "true", // Mock search params
   "test-checkbox-n": "Option 1", // Mock search params
-  "test-number-n": 123, // Mock search params
+  "test-number-n": "123", // Mock search params
+  ...multiOptionsFieldSearchParams,
 };
 
 vi.mock("@calcom/lib/hooks/useRouterQuery", () => ({
   useRouterQuery: () => ({ ...searchParams }),
 }));
+
+const options = [
+  { label: "Option 1", value: "Option 1" },
+  { label: "Option 2", value: "Option 2" },
+];
 
 const questionTypes = [
   { questionType: "email", label: "Email Field" },
@@ -35,13 +56,18 @@ const questionTypes = [
   { questionType: "text", label: "Short Text Field" },
   { questionType: "number", label: "Number Field" },
   { questionType: "textarea", label: "Long Text Field" },
-  { questionType: "select", label: "Select Field" },
-  { questionType: "multiselect", label: "MultiSelect Field" },
-  { questionType: "multiemail", label: "Multiple Emails Field" },
-  { questionType: "checkbox", label: "CheckBox Group Field" },
-  { questionType: "radio", label: "Radio Group Field" },
-  { questionType: "boolean", label: "Checkbox Field" },
+  { questionType: "select", label: "Select Field", options },
+  { questionType: "radio", label: "Radio Group Field", options },
+  { questionType: "boolean", label: "Checkbox Field", options },
 ];
+
+const multiOptionsQuestions = [
+  { questionType: "multiselect", label: "MultiSelect Field", options },
+  { questionType: "multiemail", label: "Multiple Emails Field" },
+  { questionType: "checkbox", label: "CheckBox Group Field", options },
+];
+
+const finalQuestionTypes = [...questionTypes, ...multiOptionsQuestions];
 
 const buildFormStateWithError = (questionType: string) => {
   return {
@@ -51,6 +77,19 @@ const buildFormStateWithError = (questionType: string) => {
       },
     },
   };
+};
+
+const getFormInitialValueForField = (
+  field: RhfFormField,
+  searchParams: Record<string, string | string[]>
+) => {
+  if (!searchParams) return {};
+  const result = checkParseQueryValues(field, searchParams);
+  if (result.success) {
+    return { [field.name]: result.value };
+  }
+
+  return { [field.name]: undefined };
 };
 
 // Mock `react-hook-form` module
@@ -71,7 +110,7 @@ describe("FormBuilderField: Disable on Prefill Behavior", () => {
     vi.resetAllMocks();
   });
 
-  for (const { questionType, label } of questionTypes) {
+  for (const { questionType, label, options } of finalQuestionTypes) {
     test(`should return false when 'disableOnPrefill' is 'true' but there are errors for ${label} (${questionType})`, () => {
       const field = {
         value: "",
@@ -81,13 +120,14 @@ describe("FormBuilderField: Disable on Prefill Behavior", () => {
         label: `test-${questionType}-l`,
         placeholder: `test-${questionType}-p`,
         required: true,
+        options,
       } as RhfFormField;
       const formState = buildFormStateWithError(questionType);
       const mockFormContext = {
         formState,
         control: {} as Control,
         getValues: () => ({
-          responses: { ...searchParams },
+          responses: getFormInitialValueForField(field, searchParams),
         }),
       };
       // Mock `useFormContext` for this specific test
@@ -98,7 +138,7 @@ describe("FormBuilderField: Disable on Prefill Behavior", () => {
       expect(shouldBeDisabled).toBe(false);
     });
   }
-  for (const { questionType, label } of questionTypes) {
+  for (const { questionType, label, options } of finalQuestionTypes) {
     test(`should return 'false' when 'disableOnPrefill' is 'false' for ${label} (${questionType})`, () => {
       const field = {
         value: "",
@@ -108,6 +148,7 @@ describe("FormBuilderField: Disable on Prefill Behavior", () => {
         label: `test-${questionType}-l`,
         placeholder: `test-${questionType}-p`,
         required: true,
+        options,
       } as RhfFormField;
 
       const formState = buildFormStateWithError(questionType);
@@ -115,7 +156,7 @@ describe("FormBuilderField: Disable on Prefill Behavior", () => {
         formState,
         control: {} as Control,
         getValues: () => ({
-          responses: { ...searchParams },
+          responses: getFormInitialValueForField(field, searchParams),
         }),
       };
       // Mock `useFormContext` for this specific test
@@ -125,8 +166,8 @@ describe("FormBuilderField: Disable on Prefill Behavior", () => {
       expect(shouldBeDisabled).toBe(false);
     });
   }
-  for (const { questionType, label } of questionTypes) {
-    test(`should return 'true' when 'disableOnPrefill' is true, there are no errors, and the URL search parameter 
+  for (const { questionType, label, options } of finalQuestionTypes) {
+    test(`should return 'true' when 'disableOnPrefill' is true, there are no errors, and the URL search parameter
       value matches the form value for ${label} (${questionType})`, () => {
       const field = {
         value: "",
@@ -135,15 +176,17 @@ describe("FormBuilderField: Disable on Prefill Behavior", () => {
         name: `test-${questionType}-n`,
         label: `test-${questionType}-l`,
         placeholder: `test-${questionType}-p`,
+        options,
         required: true,
       } as RhfFormField;
       //no errors
       const formState = {};
+      const formValues = getFormInitialValueForField(field, searchParams);
       const mockFormContext = {
         formState,
         control: {} as Control,
         getValues: () => ({
-          responses: { ...searchParams },
+          responses: formValues,
         }),
       };
       // Mock `useFormContext` for this specific test
@@ -153,31 +196,128 @@ describe("FormBuilderField: Disable on Prefill Behavior", () => {
     });
   }
 
-  for (const { questionType, label } of questionTypes) {
-    test(`should return 'false' when 'disableOnPrefill' is true and there is a mismatch between the URL
-       search parameter value and the form value for ${label} (${questionType})`, () => {
+  for (const { questionType, label, options } of multiOptionsQuestions) {
+    test(`should return 'true' when 'disableOnPrefill' is true and there is a match between the URL
+       search parameter value and the form value for ${label} (${questionType})(positive case)`, () => {
       const field = {
         value: "",
         disableOnPrefill: true,
         type: questionType,
-        name: `test-${questionType}-n`,
-        label: `test-${questionType}-l`,
-        placeholder: `test-${questionType}-p`,
+        name: `test-${questionType}-p-n`,
+        label: `test-${questionType}-p-l`,
+        placeholder: `test-${questionType}-p-p`,
         required: true,
+        options,
       } as RhfFormField;
       //no errors
       const formState = {};
+      const defaultValues = getFormInitialValueForField(field, searchParams);
       const mockFormContext = {
         formState,
         control: {} as Control,
         getValues: () => ({
-          responses: {},
+          responses: defaultValues,
         }),
       };
       // Mock `useFormContext` for this specific test
       (vi.mocked(useFormContext) as any).mockReturnValue(mockFormContext);
       const shouldBeDisabled = useShouldBeDisabledDueToPrefill(field);
-      expect(shouldBeDisabled).toBe(false);
+      expect(shouldBeDisabled).toBe(true);
+    });
+  }
+
+  for (const { questionType, label, options } of multiOptionsQuestions) {
+    test(`should return 'true' when 'disableOnPrefill' is true and there is a mismatch between the URL
+       search parameter value and the form value for ${label} (${questionType})(positive negative case)`, () => {
+      if (questionType !== "multiemail") {
+        const field = {
+          value: "",
+          disableOnPrefill: true,
+          type: questionType,
+          name: `test-${questionType}-n-n`,
+          label: `test-${questionType}-n-l`,
+          placeholder: `test-${questionType}-n-p`,
+          required: true,
+          options,
+        } as RhfFormField;
+        //no errors
+        const formState = {};
+        const defaultValues = getFormInitialValueForField(field, searchParams);
+        const mockFormContext = {
+          formState,
+          control: {} as Control,
+          getValues: () => ({
+            responses: defaultValues,
+          }),
+        };
+        // Mock `useFormContext` for this specific test
+        (vi.mocked(useFormContext) as any).mockReturnValue(mockFormContext);
+        const shouldBeDisabled = useShouldBeDisabledDueToPrefill(field);
+        expect(shouldBeDisabled).toBe(true);
+      }
+    });
+  }
+
+  for (const { questionType, label, options } of multiOptionsQuestions) {
+    test(`should return 'false' when 'disableOnPrefill' is true and there is a mismatch between the URL
+       search parameter value and the form value for ${label} (${questionType})(negative case)`, () => {
+      if (questionType !== "multiemail") {
+        const field = {
+          value: "",
+          disableOnPrefill: true,
+          type: questionType,
+          name: `n-test-${questionType}-p-n`,
+          label: `n-test-${questionType}-p-l`,
+          placeholder: `n-test-${questionType}-p-p`,
+          required: true,
+          options,
+        } as RhfFormField;
+        //no errors
+        const formState = {};
+        const defaultValues = getFormInitialValueForField(field, searchParams);
+        const mockFormContext = {
+          formState,
+          control: {} as Control,
+          getValues: () => ({
+            responses: defaultValues,
+          }),
+        };
+        // Mock `useFormContext` for this specific test
+        (vi.mocked(useFormContext) as any).mockReturnValue(mockFormContext);
+        const shouldBeDisabled = useShouldBeDisabledDueToPrefill(field);
+        expect(shouldBeDisabled).toBe(false);
+      }
+    });
+  }
+
+  for (const { questionType, label, options } of multiOptionsQuestions) {
+    test(`should return 'false' when 'disableOnPrefill' is true and there is a mismatch between the URL
+       search parameter value and the form value for ${label} (${questionType})(negative case 2)`, () => {
+      if (questionType === "multiemail") {
+        const field = {
+          value: "",
+          disableOnPrefill: true,
+          type: questionType,
+          name: `n-test-${questionType}-n-n`,
+          label: `n-test-${questionType}-n-l`,
+          placeholder: `n-test-${questionType}-n-p`,
+          required: true,
+          options,
+        } as RhfFormField;
+        //no errors
+        const formState = {};
+        const mockFormContext = {
+          formState,
+          control: {} as Control,
+          getValues: () => ({
+            responses: getFormInitialValueForField(field, searchParams),
+          }),
+        };
+        // Mock `useFormContext` for this specific test
+        (vi.mocked(useFormContext) as any).mockReturnValue(mockFormContext);
+        const shouldBeDisabled = useShouldBeDisabledDueToPrefill(field);
+        expect(shouldBeDisabled).toBe(false);
+      }
     });
   }
 });
