325f160396c5574bbd727f8b24898366ab493be9
Morgan Vernay <morgan@cal.com>
2024-08-02 12:50:45 +0300

    chore: describe payload template

--- apps/api/v2/src/modules/webhooks/outputs/webhook.output.ts
+++ apps/api/v2/src/modules/webhooks/outputs/webhook.output.ts
@@ -12,6 +12,17 @@ export class WebhookOutputDto {
 
   @IsString()
   @Expose()
+  @ApiProperty({
+    description:
+      "The template of the payload that will be sent to the subscriberUrl, check cal.com/docs/core-features/webhooks for more information",
+    example: JSON.stringify({
+      content: "A new event has been scheduled",
+      type: "{{type}}",
+      name: "{{title}}",
+      organizer: "{{organizer.name}}",
+      booker: "{{attendees.0.name}}",
+    }),
+  })
   readonly payloadTemplate!: string;
 
   @IsEnum(WebhookTriggerEvents)
