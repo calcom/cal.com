831c50c81a0f4ce1d7337bf2fe48a169b2e22d89
Anik Dhabal Babu <adhabal2002@gmail.com>
2024-08-02 16:43:57 +0000

    add test

--- packages/features/bookings/lib/handleNewBooking.ts
+++ packages/features/bookings/lib/handleNewBooking.ts
@@ -595,14 +595,15 @@ async function handler(
         const freeUsers = luckyUserPool.filter(
           (user) => !luckyUsers.concat(notAvailableLuckyUsers).find((existing) => existing.id === user.id)
         );
-        const originalBookingUserId = originalRescheduledBooking && originalRescheduledBooking.userId;
+        const originalRescheduledBookingUserId =
+          originalRescheduledBooking && originalRescheduledBooking.userId;
         const isSameRoundRobinHost =
-          !!originalBookingUserId &&
+          !!originalRescheduledBookingUserId &&
           eventType.schedulingType === SchedulingType.ROUND_ROBIN &&
           eventType.rescheduleWithSameRoundRobinHost;
 
         const newLuckyUser = isSameRoundRobinHost
-          ? freeUsers.find((user) => user.id === originalBookingUserId)
+          ? freeUsers.find((user) => user.id === originalRescheduledBookingUserId)
           : await getLuckyUser("MAXIMIZE_AVAILABILITY", {
               // find a lucky user that is not already in the luckyUsers array
               availableUsers: freeUsers,
