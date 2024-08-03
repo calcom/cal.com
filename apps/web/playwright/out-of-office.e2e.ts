bd34e6504585c278e68fea708c339a5e110bca80
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2024-08-03 11:37:30 +0000

    chore: Update TimeZoneEnum with additional time zones
    This commit updates the TimeZoneEnum in the types.ts file to include additional time zones. The new time zones added are:
    - ID: Asia/Jakarta (UTC+7)
    - SAMOA: Etc/GMT+12 (UTC-12)
    - AMERICAN_SAMOA: Pacific/Pago_Pago (UTC-11)
    - HAWAII: Pacific/Honolulu (UTC-10)
    - ALASKA: America/Anchorage (UTC-9)
    - PACIFIC_USA: America/Los_Angeles (UTC-8)
    - CENTRAL_USA: America/Chicago (UTC-6)
    - EASTERN_USA: America/New_York (UTC-5)
    - VENEZUELA: America/Caracas (UTC-4)
    - ARGENTINA: America/Buenos_Aires (UTC-3)
    - BRAZIL: America/Noronha (UTC-2)
    - AZORES: Atlantic/Azores (UTC-1)
    - FRANCE: Europe/Paris (UTC+1)
    - GREECE: Europe/Athens (UTC+2)
    - RUSSIA: Europe/Moscow (UTC+3)
    - UAE: Asia/Dubai (UTC+4)
    - PAKISTAN: Asia/Karachi (UTC+5)
    - BANGLADESH: Asia/Dhaka (UTC+6)
    - SINGAPORE: Asia/Singapore (UTC+8)
    - JAPAN: Asia/Tokyo (UTC+9)
    - AUSTRALIA: Australia/Sydney (UTC+10)
    - SOLOMON_ISLANDS: Pacific/Guadalcanal (UTC+11)
    - NEW_ZEALAND: Pacific/Auckland (UTC+12)
    
    The purpose of this update is to provide a comprehensive list of time zones for users to choose from when setting their time zone in the out-of-office feature.

--- apps/web/playwright/out-of-office.e2e.ts
+++ apps/web/playwright/out-of-office.e2e.ts
@@ -5,84 +5,89 @@ import dayjs from "@calcom/dayjs";
 import { randomString } from "@calcom/lib/random";
 import prisma from "@calcom/prisma";
 
+import { TimeZoneEnum } from "./fixtures/types";
 import { test } from "./lib/fixtures";
 
 test.describe.configure({ mode: "parallel" });
 test.afterEach(({ users }) => users.deleteAll());
 
 test.describe("Out of office", () => {
-  test("User can create out of office entry", async ({ page, users }) => {
-    const user = await users.create({ name: "userOne" });
+  // NOTE: 2 tests below are known to fail due to the timezone issue in the past @see #16001
+  const timeZoneEnumValues = Object.values(TimeZoneEnum);
+  timeZoneEnumValues.forEach((timeZone) => {
+    test(`[${timeZone}] User can create out of office entry`, async ({ page, users }) => {
+      const user = await users.create({ name: "userOne", timeZone });
 
-    await user.apiLogin();
+      await user.apiLogin();
 
-    await page.goto("/settings/my-account/out-of-office");
+      await page.goto("/settings/my-account/out-of-office");
 
-    await page.getByTestId("add_entry_ooo").click();
-    await page.getByTestId("reason_select").click();
+      await page.getByTestId("add_entry_ooo").click();
+      await page.getByTestId("reason_select").click();
 
-    await page.getByTestId("select-option-4").click();
+      await page.getByTestId("select-option-4").click();
 
-    await page.getByTestId("notes_input").click();
-    await page.getByTestId("notes_input").fill("Demo notes");
-    await page.getByTestId("create-entry-ooo-redirect").click();
+      await page.getByTestId("notes_input").click();
+      await page.getByTestId("notes_input").fill("Demo notes");
+      await page.getByTestId("create-entry-ooo-redirect").click();
 
-    await expect(page.locator(`data-testid=table-redirect-n-a`)).toBeVisible();
-  });
-
-  test("User can configure booking redirect", async ({ page, users }) => {
-    const user = await users.create({ name: "userOne" });
-    const userTo = await users.create({ name: "userTwo" });
-
-    const team = await prisma.team.create({
-      data: {
-        name: "test-insights",
-        slug: `test-insights-${Date.now()}-${randomString(5)}}`,
-      },
+      await expect(page.locator(`data-testid=table-redirect-n-a`)).toBeVisible();
     });
 
-    // create memberships
-    await prisma.membership.createMany({
-      data: [
-        {
-          userId: user.id,
-          teamId: team.id,
-          accepted: true,
-          role: "ADMIN",
+    test(`[${timeZone}] User can configure booking redirect`, async ({ page, users }) => {
+      const user = await users.create({ name: "userOne", timeZone });
+      const userTo = await users.create({ name: "userTwo", timeZone });
+
+      const team = await prisma.team.create({
+        data: {
+          name: "test-insights",
+          slug: `test-insights-${Date.now()}-${randomString(5)}}`,
         },
-        {
-          userId: userTo.id,
-          teamId: team.id,
-          accepted: true,
-          role: "ADMIN",
-        },
-      ],
+      });
+
+      // create memberships
+      await prisma.membership.createMany({
+        data: [
+          {
+            userId: user.id,
+            teamId: team.id,
+            accepted: true,
+            role: "ADMIN",
+          },
+          {
+            userId: userTo.id,
+            teamId: team.id,
+            accepted: true,
+            role: "ADMIN",
+          },
+        ],
+      });
+
+      await user.apiLogin();
+
+      await page.goto(`/settings/my-account/out-of-office`);
+
+      await page.getByTestId("add_entry_ooo").click();
+      await page.getByTestId("reason_select").click();
+
+      await page.getByTestId("select-option-4").click();
+
+      await page.getByTestId("notes_input").click();
+      await page.getByTestId("notes_input").fill("Demo notes");
+
+      await page.getByTestId("profile-redirect-switch").click();
+
+      await page.getByTestId("team_username_select").click();
+
+      await page.locator("#react-select-3-input").fill("user");
+      await page.locator("#react-select-3-input").press("Enter");
+
+      // send request
+      await page.getByTestId("create-entry-ooo-redirect").click();
+
+      // expect table-redirect-toUserId to be visible
+      await expect(page.locator(`data-testid=table-redirect-${userTo.username}`)).toBeVisible();
     });
-
-    await user.apiLogin();
-
-    await page.goto(`/settings/my-account/out-of-office`);
-
-    await page.getByTestId("add_entry_ooo").click();
-    await page.getByTestId("reason_select").click();
-
-    await page.getByTestId("select-option-4").click();
-
-    await page.getByTestId("notes_input").click();
-    await page.getByTestId("notes_input").fill("Demo notes");
-
-    await page.getByTestId("profile-redirect-switch").click();
-
-    await page.getByTestId("team_username_select").click();
-
-    await page.locator("#react-select-3-input").fill("user");
-    await page.locator("#react-select-3-input").press("Enter");
-
-    // send request
-    await page.getByTestId("create-entry-ooo-redirect").click();
-
-    // expect table-redirect-toUserId to be visible
-    await expect(page.locator(`data-testid=table-redirect-${userTo.username}`)).toBeVisible();
   });
 
   test("Profile redirection", async ({ page, users }) => {
