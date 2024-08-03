1adb032d460a7e8784981ae42823b40d116574ac
Snehal Baghel <snehalbaghel@Snehals-MacBook-Pro.local>
2024-08-03 22:53:32 +0530

    fix: ssr translation on no-meeting, signup pages

--- apps/web/pages/video/no-meeting-found.tsx
+++ apps/web/pages/video/no-meeting-found.tsx
@@ -1,6 +1,19 @@
+import type { GetServerSidePropsContext } from "next";
+
 import PageWrapper, { type CalPageWrapper } from "@components/PageWrapper";
 
+import { ssrInit } from "@server/lib/ssr";
+
 import NoMeetingFound from "~/videos/views/videos-no-meeting-found-single-view";
+
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  const ssr = await ssrInit(context);
+  return {
+    props: {
+      trpcState: ssr.dehydrate(),
+    },
+  };
+};
 
 const NoMeetingFoundPage = NoMeetingFound as unknown as CalPageWrapper;
 
