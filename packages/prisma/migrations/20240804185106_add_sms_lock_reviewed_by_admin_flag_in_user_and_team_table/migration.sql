6e6866e3846cd7bb56e39a011bb66d536cc9c035
TaduJR <nattysec26@gmail.com>
2024-08-04 21:57:06 +0300

    test

--- packages/prisma/migrations/20240804185106_add_sms_lock_reviewed_by_admin_flag_in_user_and_team_table/migration.sql
+++ packages/prisma/migrations/20240804185106_add_sms_lock_reviewed_by_admin_flag_in_user_and_team_table/migration.sql
@@ -2,4 +2,4 @@
 ALTER TABLE "Team" ADD COLUMN     "smsLockReviewedByAdmin" BOOLEAN NOT NULL DEFAULT false;
 
 -- AlterTable
-ALTER TABLE "users" ADD COLUMN     "smsLockReviewByAdmin" BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE "users" ADD COLUMN     "smsLockReviewedByAdmin" BOOLEAN NOT NULL DEFAULT false;
