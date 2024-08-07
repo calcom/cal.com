f2233de82239433432214e958d6a975a683c9826
Mihir867 <Mihirkantthakur867@gmail.com>
2024-08-07 18:18:10 +0530

    Better UI for the setup page

--- packages/app-store/make/pages/setup/index.tsx
+++ packages/app-store/make/pages/setup/index.tsx
@@ -133,12 +133,22 @@ export default function MakeSetup({ inviteLink }: InferGetServerSidePropsType<ty
           </div>
         </div>
       ) : (
-        <div className="ml-5 mt-5">
-          <div>{t("install_make_app")}</div>
-          <div className="mt-3">
-            <Link href="/apps/make" passHref={true} legacyBehavior>
-              <Button>{t("go_to_app_store")}</Button>
-            </Link>
+        <div className="flex h-full w-full flex-col  items-center justify-center rounded-md bg-white p-10  text-black">
+          <div className="flex w-1/3 flex-col items-center rounded-lg border border-[#E5E7EB] p-10 shadow-md">
+            <div className="flex h-16 w-16 items-center justify-center rounded-full bg-gray-100">
+              <Icon name="circle-alert" className="h-10 w-10 text-gray-800" />
+            </div>
+            <div className="mb-2 mt-4 text-2xl  text-2xl font-bold">App not installed </div>
+            <h2 className="mt-4 text-lg text-[##4B5563] ">{t("install_make_app")}</h2>
+
+            <div className="mt-5">
+              <Link href="/apps/make" passHref>
+                <Button className="border">
+                  {t("go_to_app_store")}
+                  <Icon name="arrow-up-right" />
+                </Button>
+              </Link>
+            </div>
           </div>
         </div>
       )}
