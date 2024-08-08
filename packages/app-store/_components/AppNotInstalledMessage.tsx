df806d42b1ba0ecfbf2663d7ce431b1534bb19ba
Amit Sharma <74371312+Amit91848@users.noreply.github.com>
2024-08-08 12:51:57 +0530

    fix: breaking on small screen, design refactor

--- packages/app-store/_components/AppNotInstalledMessage.tsx
+++ packages/app-store/_components/AppNotInstalledMessage.tsx
@@ -7,19 +7,19 @@ export default function AppNotInstalledMessage({ appName }: { appName: string })
   const { t } = useLocale();
 
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center bg-white px-6 text-black">
-      <div className="m-10 flex w-2/5 flex-col items-center rounded-xl border border-[#E5E7EB] px-8 py-14 text-center shadow-md">
-        <div className="rounded-full bg-gray-100 p-3">
-          <Icon name="circle-alert" className="h-10 w-10 flex-shrink-0 p-0.5 font-extralight text-gray-800" />
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="bg-default flex max-w-lg flex-col items-center rounded-xl border px-8 py-14 text-center shadow-md">
+        <div className="mb-5 rounded-full bg-gray-100 p-3  dark:bg-[#292929] ">
+          <Icon name="circle-alert" className="h-9 w-9 p-0.5" strokeWidth={1.5} />
         </div>
-        <h3 className="font-cal my-6 text-2xl font-normal leading-none">{t("app_not_installed")}</h3>
-        <p className="font-normal text-[#4B5563]">{t("visit_our_app_store")}</p>
+        <h3 className="font-cal mb-2 text-2xl font-semibold">{t("app_not_installed")}</h3>
+        <p className="text-subtle px-1 leading-normal">{t("visit_our_app_store")}</p>
 
-        <div className="mt-8">
+        <div className="mt-5">
           <Link href={`/apps/${appName}`} passHref={true} legacyBehavior>
-            <Button className="border">
+            <Button type="button" color="secondary">
               {t("go_to_app_store")}
-              <Icon name="arrow-up-right" />
+              <Icon name="arrow-up-right" className="ml-1" size={20} />
             </Button>
           </Link>
         </div>
