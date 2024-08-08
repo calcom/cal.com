dc786eefbc213d200119163035d3b73b715c37d6
Morgan Vernay <morgan@cal.com>
2024-08-08 18:12:36 +0300

    fixup! fixup! fixup! fixup! fixup! Merge branch 'main' into platform-oauth-client-webhooks

--- apps/api/v2/src/modules/oauth-clients/inputs/update-oauth-client.input.ts
+++ apps/api/v2/src/modules/oauth-clients/inputs/update-oauth-client.input.ts
@@ -31,21 +31,4 @@ export class UpdateOAuthClientInput {
   @IsOptional()
   @IsBoolean()
   areEmailsEnabled?: boolean;
-
-  @IsEnum(WebhookTriggerEvents)
-  @ApiProperty({
-    description: "List of events that will trigger the webhook",
-    example: [WebhookTriggerEvents["BOOKING_CREATED"], WebhookTriggerEvents["BOOKING_CANCELLED"]],
-    enum: WebhookTriggerEvents,
-  })
-  eventTriggers?: WebhookTriggerEvents;
-
-  @IsUrl()
-  @IsOptional()
-  @ApiProperty({
-    description: "Url to send the webhook to",
-    example: "https://example.com",
-    enum: WebhookTriggerEvents,
-  })
-  subscriberUrl?: string;
 }
