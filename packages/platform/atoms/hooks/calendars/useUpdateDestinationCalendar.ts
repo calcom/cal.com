46fe5acb9f957acee95cf8824935d0030d240709
Ryukemeister <sahalrajiv6900@gmail.com>
2024-08-09 13:27:53 +0530

    hook to update destination calendar

---
+++ packages/platform/atoms/hooks/calendars/useUpdateDestinationCalendar.ts
@@ -0,0 +1,59 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { SUCCESS_STATUS } from "@calcom/platform-constants";
+import type { ApiErrorResponse, ApiResponse } from "@calcom/platform-types";
+
+import http from "../../lib/http";
+import { QUERY_KEY } from "../useConnectedCalendars";
+
+interface IUseUpdateDestinationCalendar {
+  onSuccess?: (res: ApiResponse) => void;
+  onError?: (err: ApiErrorResponse | Error) => void;
+}
+
+export const useUpdateDestinationCalendar = (
+  { onSuccess, onError }: IUseUpdateDestinationCalendar = {
+    onSuccess: () => {
+      return;
+    },
+    onError: () => {
+      return;
+    },
+  }
+) => {
+  const queryClient = useQueryClient();
+  const updatedDestinationCalendar = useMutation<
+    ApiResponse<{
+      status: string;
+      data: {
+        userId: number;
+        integration: string;
+        externalId: string;
+        credentialId: number | null;
+      };
+    }>,
+    unknown,
+    { integration: string; externalId: string }
+  >({
+    mutationFn: (data) => {
+      return http.post(`/destination-calendar`, data).then((res) => {
+        return res.data;
+      });
+    },
+    onSuccess: (data) => {
+      if (data.status === SUCCESS_STATUS) {
+        onSuccess?.(data);
+      } else {
+        onError?.(data);
+      }
+    },
+    onError: (err) => {
+      onError?.(err as ApiErrorResponse);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY] });
+    },
+  });
+
+  return updatedDestinationCalendar;
+};
