9e468d093ad337f9fc1c3e36a16f41992abde229
Ryukemeister <sahalrajiv6900@gmail.com>
2024-08-12 18:48:36 +0530

    update input dtos

--- apps/api/v2/src/modules/destination-calendars/inputs/destination-calendars.input.ts
+++ apps/api/v2/src/modules/destination-calendars/inputs/destination-calendars.input.ts
@@ -2,16 +2,25 @@ import { ApiProperty } from "@nestjs/swagger";
 import { Expose } from "class-transformer";
 import { IsString } from "class-validator";
 
+import {
+  APPLE_CALENDAR_TYPE,
+  GOOGLE_CALENDAR_TYPE,
+  OFFICE_365_CALENDAR_TYPE,
+} from "@calcom/platform-constants";
+
 export class DestinationCalendarsInputBodyDto {
   @IsString()
   @Expose()
   @ApiProperty({
-    example: "apple_calendar",
+    example: APPLE_CALENDAR_TYPE,
     description: "The calendar service you want to integrate, as returned by the /calendars endpoint",
-    type: "string",
+    enum: [APPLE_CALENDAR_TYPE, GOOGLE_CALENDAR_TYPE, OFFICE_365_CALENDAR_TYPE],
     required: true,
   })
-  readonly integration!: string;
+  readonly integration!:
+    | typeof APPLE_CALENDAR_TYPE
+    | typeof GOOGLE_CALENDAR_TYPE
+    | typeof OFFICE_365_CALENDAR_TYPE;
 
   @IsString()
   @Expose()
