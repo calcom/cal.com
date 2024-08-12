ef58a00cb6ae937b82e8726c03624a4b81a1add9
sean-brydon <sean@cal.com>
2024-08-12 11:16:31 +0100

    fix: handle unique slug error

--- packages/trpc/server/routers/viewer/attributes/create.handler.ts
+++ packages/trpc/server/routers/viewer/attributes/create.handler.ts
@@ -1,5 +1,7 @@
 import slugify from "@calcom/lib/slugify";
 import prisma from "@calcom/prisma";
+import type { Attribute } from "@calcom/prisma/client";
+import { Prisma } from "@calcom/prisma/client";
 
 import { TRPCError } from "@trpc/server";
 
@@ -30,14 +32,23 @@ const createAttributesHandler = async ({ input, ctx }: GetOptions) => {
   const optionsWithoutDuplicates = Array.from(new Set(options));
   const typeHasOptions = typesWithOptions.includes(input.type);
 
-  const attributes = await prisma.attribute.create({
-    data: {
-      slug,
-      name: input.name,
-      type: input.type,
-      teamId: org.id,
-    },
-  });
+  let attributes: Attribute;
+  try {
+    attributes = await prisma.attribute.create({
+      data: {
+        slug,
+        name: input.name,
+        type: input.type,
+        teamId: org.id,
+      },
+    });
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2002") {
+      throw new Error("A record with that name already exists. Please choose another one.");
+    } else {
+      throw error; // Re-throw the error if it's not a unique constraint violation
+    }
+  }
 
   // Only assign options for the types that have options
   // TEXT/NUMBER don't have options
