86fc92ac32175800bfb2d8f4e2881209eabe1d12
Udit Takkar <udit222001@gmail.com>
2024-08-13 13:43:10 +0530

    chore: remove

--- packages/features/ee/teams/components/MemberListItem.tsx
+++ packages/features/ee/teams/components/MemberListItem.tsx
@@ -97,7 +97,6 @@ export default function MemberListItem(props: Props) {
     },
     async onSuccess() {
       await utils.viewer.teams.get.invalidate();
-      await utils.viewer.teams.invalidate();
       await utils.viewer.eventTypes.invalidate();
       await utils.viewer.organizations.listMembers.invalidate();
       await utils.viewer.organizations.getMembers.invalidate();
