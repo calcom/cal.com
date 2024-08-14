e80a6e428aa2f7861081427c03196d91d61f10af
Anik Dhabal Babu <81948346+anikdhabal@users.noreply.github.com>
2024-08-15 00:53:40 +0530

    fix type error

--- packages/prisma/zod/custom/eventtype.ts
+++ packages/prisma/zod/custom/eventtype.ts
@@ -33,7 +33,7 @@ export const createEventTypeInput = z.object({
     title: z.string().min(1),
     description: z.string(),
     length: z.number(),
-    teamId: z.number().optional(),
+    teamId: z.number().optional().nullable(),
   }).strict();
 
 export type EventTypeLocation = (z.infer<typeof imports.eventTypeLocations>)[number];
