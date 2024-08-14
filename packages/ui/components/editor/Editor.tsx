d0c422eaac1323534e35a4aefd652ff72a300364
unknown <adhabal2002@gmail.com>
2024-08-14 13:08:21 +0530

    add unit test for the plugin and changes

--- packages/ui/components/editor/Editor.tsx
+++ packages/ui/components/editor/Editor.tsx
@@ -71,7 +71,7 @@ export const Editor = (props: TextEditorProps) => {
   const editable = props.editable ?? true;
   return (
     <div className="editor rounded-md">
-      <LexicalComposer initialConfig={{ ...editorConfig, editable }}>
+      <LexicalComposer initialConfig={{ ...editorConfig }}>
         <div className="editor-container hover:border-emphasis focus-within:ring-brand-default rounded-md p-0 transition focus-within:ring-2">
           <ToolbarPlugin
             getText={props.getText}
@@ -89,7 +89,6 @@ export const Editor = (props: TextEditorProps) => {
             <RichTextPlugin
               contentEditable={
                 <ContentEditable
-                  data-testid="editor-input"
                   readOnly={!editable}
                   style={{ height: props.height }}
                   className="editor-input"
