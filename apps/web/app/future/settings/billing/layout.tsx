ae55765e1dd69e08e43bd12d8f1067c5d70590f7
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-08-14 22:14:35 -0400

    Add billing to app router

---
+++ apps/web/app/future/settings/billing/layout.tsx
@@ -0,0 +1,20 @@
+import { getFixedT } from "app/_utils";
+import { getServerSession } from "next-auth";
+
+import { AUTH_OPTIONS } from "@calcom/feature-auth/lib/next-auth-options";
+import SettingsLayout from "@calcom/features/settings/layouts/SettingsLayoutAppDir";
+
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  const session = await getServerSession(AUTH_OPTIONS);
+
+  const t = await getFixedT(session.user.locale || "en");
+
+  return (
+    <SettingsLayout
+      title={t("billing")}
+      description={t("manage_billing_description")}
+      borderInShellHeader={true}>
+      {children}
+    </SettingsLayout>
+  );
+}
