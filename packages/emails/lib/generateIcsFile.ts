3579134e0369472434efddaaaf12033672304261
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-08-16 12:46:35 -0400

    Add role enum and only disable for organizers

--- packages/emails/lib/generateIcsFile.ts
+++ packages/emails/lib/generateIcsFile.ts
@@ -1,26 +1,38 @@
+import type { EventStatus } from "ics";
 import type { TFunction } from "next-i18next";
 
 import type { CalendarEvent } from "@calcom/types/Calendar";
 
 import generateIcsString from "./generateIcsString";
 
+export enum GenerateIcsRole {
+  ATTENDEE = "attendee",
+  ORGANIZER = "organizer",
+}
+
 export default function generateIcsFile({
   calEvent,
   title,
   subtitle,
   role,
+  status,
   t,
   isRequestReschedule,
 }: {
   calEvent: CalendarEvent;
   title: string;
   subtitle: string;
-  role: "attendee" | "organizer";
+  role: GenerateIcsRole;
+  status: EventStatus;
   t?: TFunction;
   isRequestReschedule?: boolean;
 }) {
   // O365 deletes emails if the calendar event is selected. Currently no option to disable this on the web
-  if (calEvent.destinationCalendar && calEvent.destinationCalendar[0].integration === "office365_calendar")
+  if (
+    role !== GenerateIcsRole.ATTENDEE &&
+    calEvent.destinationCalendar &&
+    calEvent.destinationCalendar[0].integration === "office365_calendar"
+  )
     return null;
 
   return {
@@ -30,7 +42,7 @@ export default function generateIcsFile({
       title,
       subtitle,
       role,
-      status: "CONFIRMED",
+      status,
       t,
       isRequestReschedule,
     }),
