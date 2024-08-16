3f5d7b8e5425b6954ce9eeb65682b46000b3c3b2
Souptik Datta <souptikdatta2001@gmail.com>
2024-08-16 22:03:08 +0530

    refactor: Remove redundant migrations
    Signed-off-by: Souptik Datta <souptikdatta2001@gmail.com>

--- packages/prisma/migrations/20240723105132_add_single_use_links_to_event_types/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "HashedLink" ADD COLUMN     "destroyOnUse" BOOLEAN NOT NULL DEFAULT false;
--- packages/prisma/migrations/20240816134807_remove_destroy_on_use_field_from_hashed_link_table/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `destroyOnUse` on the `HashedLink` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "HashedLink" DROP COLUMN "destroyOnUse";
