b004587262e8221083bafbe9a0c515e7becaa7b3
Thefcraft <76678136+thefcraft@users.noreply.github.com>
2024-08-20 14:09:33 +0530

    fix: textarea ux in Editor.tsx #16225 (#16231)
    * fix: textarea ux in Editor.tsx #16225
    
    * fix: textarea ux in Editor.tsx #16225
    
    * fix: textarea ux in Editor.tsx #16225
    
    * fix: textarea ux in Editor.tsx #16225
    
    * fix: textarea ux in Editor.tsx #16225
    
    * fix and update
    
    * remove space
    
    ---------
    
    Co-authored-by: unknown <adhabal2002@gmail.com>
    Co-authored-by: Anik Dhabal Babu <81948346+anikdhabal@users.noreply.github.com>
    Co-authored-by: Udit Takkar <53316345+Udit-takkar@users.noreply.github.com>

--- packages/ui/components/editor/Editor.tsx
+++ packages/ui/components/editor/Editor.tsx
@@ -37,6 +37,7 @@ export type TextEditorProps = {
   excludedToolbarItems?: string[];
   variables?: string[];
   height?: string;
+  maxHeight?: string;
   placeholder?: string;
   disableLists?: boolean;
   updateTemplate?: boolean;
@@ -84,8 +85,8 @@ export const Editor = (props: TextEditorProps) => {
             setFirstRender={props.setFirstRender}
           />
           <div
-            className={classNames("editor-inner scroll-bar", !editable && "!bg-subtle")}
-            style={{ height: props.height }}>
+            className={classNames("editor-inner scroll-bar overflow-x-hidden", !editable && "!bg-subtle")}
+            style={{ height: props.height, maxHeight: props.maxHeight }}>
             <RichTextPlugin
               contentEditable={
                 <ContentEditable
