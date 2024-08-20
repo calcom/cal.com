d69dd93eb1f0f5109c78bec5a5dfcdabfcb5d232
Ryukemeister <sahalrajiv6900@gmail.com>
2024-08-21 01:30:14 +0530

    better naming

--- apps/api/v2/src/modules/stripe/utils/newStripeInstance.ts
+++ apps/api/v2/src/modules/stripe/utils/newStripeInstance.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
 const stripePrivateKey = process.env.STRIPE_PRIVATE_KEY || "";
-export const stripe = new Stripe(stripePrivateKey, {
+export const stripeInstance = new Stripe(stripePrivateKey, {
   apiVersion: "2020-08-27",
 });
