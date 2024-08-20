4948aaa29fba4567a3b2de60941271874ab270d4
Benny Joo <sldisek783@gmail.com>
2024-08-20 18:57:53 -0400

    settings/billing page is missing in future

---
+++ apps/web/app/future/settings/billing/page.tsx
@@ -0,0 +1,10 @@
+import Page from "@pages/settings/billing/index";
+import { _generateMetadata } from "app/_utils";
+
+export const generateMetadata = async () =>
+  await _generateMetadata(
+    (t) => t("billing"),
+    (t) => t("manage_billing_description")
+  );
+
+export default Page;
