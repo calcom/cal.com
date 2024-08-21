323de9bde295d6605720f126b08f9524ccf820a3
Ryukemeister <sahalrajiv6900@gmail.com>
2024-08-21 20:20:11 +0530

    fix naming

--- apps/api/v2/src/modules/stripe/utils/newStripeInstance.ts
+++ apps/api/v2/src/modules/stripe/utils/newStripeInstance.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-const stripePrivateKey = process.env.STRIPE_API_KEY || "";
-export const stripeInstance = new Stripe(stripePrivateKey, {
+const stripeApiKey = process.env.STRIPE_API_KEY || "";
+export const stripeInstance = new Stripe(stripeApiKey, {
   apiVersion: "2020-08-27",
 });
