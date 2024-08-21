63417c74b90363e9a5344e36c1074a75f3acc792
Ryukemeister <sahalrajiv6900@gmail.com>
2024-08-21 20:19:23 +0530

    replace STRIPE_PRIVATE_KEY with STRIPE_API_KEY

--- apps/api/v2/src/modules/stripe/utils/newStripeInstance.ts
+++ apps/api/v2/src/modules/stripe/utils/newStripeInstance.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-const stripePrivateKey = process.env.STRIPE_PRIVATE_KEY || "";
+const stripePrivateKey = process.env.STRIPE_API_KEY || "";
 export const stripeInstance = new Stripe(stripePrivateKey, {
   apiVersion: "2020-08-27",
 });
