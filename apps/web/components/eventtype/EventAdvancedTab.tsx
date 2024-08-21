2add9ba7c1a2a27ed0830ef70de602a034a5b2a0
Souptik Datta <souptikdatta2001@gmail.com>
2024-08-21 15:00:31 +0530

    fix: Address feedbacks
    Signed-off-by: Souptik Datta <souptikdatta2001@gmail.com>

--- apps/web/components/eventtype/EventAdvancedTab.tsx
+++ apps/web/components/eventtype/EventAdvancedTab.tsx
@@ -1,4 +1,3 @@
-import { useAutoAnimate } from "@formkit/auto-animate/react";
 import dynamic from "next/dynamic";
 import type { EventTypeSetupProps } from "pages/event-types/[type]";
 import { useState } from "react";
@@ -39,101 +38,14 @@ import {
   SettingsToggle,
   Switch,
   TextField,
-  Tooltip,
   showToast,
   ColorPicker,
 } from "@calcom/ui";
 
+import { SingleUseLinksController } from "@components/eventtype/SingleUseLinksController";
+
 import RequiresConfirmationController from "./RequiresConfirmationController";
 import { DisableAllEmailsSetting } from "./settings/DisableAllEmailsSetting";
-
-const SingleUseLinksManager = ({
-  team,
-  bookerUrl,
-  disabled,
-}: Pick<EventTypeSetupProps["eventType"], "team" | "bookerUrl"> & { disabled: boolean }) => {
-  const formMethods = useFormContext<FormValues>();
-  const { t } = useLocale();
-  const [animateRef] = useAutoAnimate<HTMLUListElement>();
-  return (
-    <Controller
-      name="singleUseLinks"
-      control={formMethods.control}
-      render={({ field: { value, onChange } }) => {
-        if (!value) {
-          value = [];
-        }
-        const addSingleUseLink = () => {
-          const newSingleUseLink = generateHashedLink(formMethods.getValues("users")[0]?.id ?? team?.id);
-          if (!value) value = [];
-          value.push(newSingleUseLink);
-          onChange(value);
-        };
-
-        const removeSingleUseLink = (index: number) => {
-          if (!value) value = [];
-          value.splice(index, 1);
-          onChange(value);
-        };
-
-        return (
-          <ul ref={animateRef}>
-            {value &&
-              value.map((val: string, key: number) => {
-                const singleUseURL = `${bookerUrl}/d/${val}/${formMethods.getValues("slug")}`;
-                return (
-                  <li data-testid="add-single-use-link" className="mb-4 flex items-center" key={val}>
-                    <TextField
-                      data-testid={`single-use-link-${key}`}
-                      containerClassName="w-full"
-                      disabled
-                      labelSrOnly
-                      type="text"
-                      defaultValue={singleUseURL}
-                      addOnSuffix={
-                        <Tooltip content={t("copy_to_clipboard")}>
-                          <Button
-                            color="minimal"
-                            size="sm"
-                            type="button"
-                            className="hover:stroke-3 hover:text-emphasis min-w-fit !py-0 px-0 hover:bg-transparent"
-                            aria-label="copy link"
-                            onClick={() => {
-                              navigator.clipboard.writeText(singleUseURL);
-                              showToast(t("single_use_link_copied"), "success");
-                            }}>
-                            <Icon name="copy" className="ml-1 h-4 w-4" />
-                          </Button>
-                        </Tooltip>
-                      }
-                    />
-                    {value && value.length > 1 && (
-                      <Button
-                        data-testid={`remove-single-use-link-${key}`}
-                        variant="icon"
-                        StartIcon="trash-2"
-                        color="destructive"
-                        className="ml-2 border-none"
-                        onClick={() => removeSingleUseLink(key)}
-                      />
-                    )}
-                  </li>
-                );
-              })}
-            <Button
-              color="minimal"
-              StartIcon="plus"
-              onClick={addSingleUseLink}
-              disabled={disabled}
-              data-testid="add-single-use-link-button">
-              {t("add_a_single_use_link")}
-            </Button>
-          </ul>
-        );
-      }}
-    />
-  );
-};
 
 const CustomEventTypeModal = dynamic(() => import("@components/eventtype/CustomEventTypeModal"));
 
@@ -489,7 +401,7 @@ export const EventAdvancedTab = ({ eventType, team }: Pick<EventTypeSetupProps, 
               }}>
               {!isManagedEventType && (
                 <div className="border-subtle rounded-b-lg border border-t-0 p-6">
-                  <SingleUseLinksManager
+                  <SingleUseLinksController
                     team={team}
                     bookerUrl={eventType.bookerUrl}
                     disabled={shouldLockDisableProps("singleUseLinks").disabled}
