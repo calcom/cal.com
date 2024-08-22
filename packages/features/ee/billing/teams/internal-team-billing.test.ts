15b984012d147dfc3f8d0b389cdff7f2e5194b1d
zomars <zomars@me.com>
2024-08-22 14:45:19 -0700

    Update internal-team-billing.test.ts

--- packages/features/ee/billing/teams/internal-team-billing.test.ts
+++ packages/features/ee/billing/teams/internal-team-billing.test.ts
@@ -75,18 +75,18 @@ describe("TeamBilling", async () => {
   });
 });
 
-test("TODO: Internal billing is called when team billing is enabled", async () => {
+test.skip("TODO: Internal billing is called when team billing is enabled", async () => {
   expect(true).toBe(false);
 });
 
-test("Subscription is cancelled when team is deleted", async () => {
+test.skip("TODO: Subscription is cancelled when team is deleted", async () => {
   expect(true).toBe(false);
 });
 
-test("Team is marked as pending payment when renewal charge fails", async () => {
+test.skip("TODO: Team is marked as pending payment when renewal charge fails", async () => {
   expect(true).toBe(false);
 });
 
-test("Team is deleted when X amount of days pass with pending payment", async () => {
+test.skip("TODO: Team is deleted when X amount of days pass with pending payment", async () => {
   expect(true).toBe(false);
 });
