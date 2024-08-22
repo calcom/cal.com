e9dd68f54ace6a981d523471dbf8db87eba3a3c5
Felippe Fernandes <lfelippeeng@gmail.com>
2024-08-22 18:36:06 -0300

    Fix router param

--- packages/ui/components/logo/Logo.tsx
+++ packages/ui/components/logo/Logo.tsx
@@ -20,6 +20,8 @@ export default function Logo({
   const { pathname } = useRouter();
   const isLogin = pathname.includes("auth/login");
 
+  console.log("isMobile", isMobile);
+
   const logoStyle = { width: isMobile ? "230px" : "300px", height: "auto" };
 
   return (
