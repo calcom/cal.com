4559a303b0818a63090e47ac20cd4222e202992c
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-08-21 20:00:47 -0400

    Type fix

--- apps/web/test/utils/bookingScenario/expects.ts
+++ apps/web/test/utils/bookingScenario/expects.ts
@@ -81,7 +81,7 @@ declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace jest {
     interface Matchers<R> {
-      toHaveEmail(expectedEmail: ExpectedEmail, to: string): R;
+      toHaveEmail(expectedEmail: ExpectedEmail, to: string, subject?: string): R;
     }
   }
 }
