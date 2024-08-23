1cbb9168af5a756f3fa0335081405560a355bfde
Ryukemeister <sahalrajiv6900@gmail.com>
2024-08-23 12:15:07 +0530

    revert stripe controllers commit

--- apps/api/v2/src/modules/stripe/inputs/stripe.input.ts
+++
@@ -1,7 +0,0 @@
-import { IsString, IsOptional } from "class-validator";
-
-export class StripeConnectQueryParamsInputDto {
-  @IsString()
-  @IsOptional()
-  readonly redir?: string;
-}
--- apps/api/v2/src/modules/stripe/utils/getReturnToValueFromQueryState.ts
+++
@@ -1,9 +0,0 @@
-export const getReturnToValueFromQueryState = (queryState: string | string[] | undefined) => {
-  let returnTo = "";
-  try {
-    returnTo = JSON.parse(`${queryState}`).returnTo;
-  } catch (error) {
-    console.info("No 'returnTo' in req.query.state");
-  }
-  return returnTo;
-};
