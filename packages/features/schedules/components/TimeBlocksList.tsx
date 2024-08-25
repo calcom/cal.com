fa59a3864254097c1d7268ada7ab3266f40ea0f8
Kartik Saini <kartik8942@gmail.com>
2024-08-25 22:34:52 +0530

    Show error when trying to submit empty time block

--- packages/features/schedules/components/TimeBlocksList.tsx
+++ packages/features/schedules/components/TimeBlocksList.tsx
@@ -1,9 +1,9 @@
-import { Controller } from "react-hook-form";
+import { Controller, useFormContext } from "react-hook-form";
 import type { Control } from "react-hook-form";
 
 import type { AvailabilityFormValues } from "@calcom/atoms/availability/types";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
-import { Button, TextField } from "@calcom/ui";
+import { Button, TextField, InputError } from "@calcom/ui";
 
 const TimeBlocksList = ({
   fields,
@@ -15,39 +15,52 @@ const TimeBlocksList = ({
   control: Control<AvailabilityFormValues>;
 }) => {
   const { t } = useLocale();
+  const methods = useFormContext() as ReturnType<typeof useFormContext> | null;
+  if (!methods) return null;
+  const { formState } = methods;
+
+  if (fields.length === 0) {
+    return <div />;
+  }
+
   return (
     <div>
-      {fields.length > 0 ? (
-        fields.map((field, index) => (
+      {fields.map((field, index) => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        const fieldErrors: FieldErrors<T> | undefined = formState.errors.timeBlocks?.[index]?.value;
+        return (
           <div key={field.id} className="mb-2 flex-col items-center space-y-2">
             {index !== 0 && (
               <div>
                 <p>{t("or")}</p>
               </div>
             )}
-            <div className="flex items-center space-x-4">
-              <div className="w-full">
-                <Controller
-                  control={control}
-                  name={`timeBlocks.${index}.value`}
-                  render={({ field }) => (
-                    <TextField label={false} placeholder="Time block" type="standard" {...field} />
-                  )}
+            <div>
+              <div className="flex items-center space-x-4">
+                <div className="w-full">
+                  <Controller
+                    control={control}
+                    name={`timeBlocks.${index}.value`}
+                    rules={{ required: t("time_block_empty_error") }}
+                    render={({ field }) => (
+                      <TextField label={false} placeholder="Time block" type="standard" {...field} />
+                    )}
+                  />
+                </div>
+                <Button
+                  StartIcon="x"
+                  variant="icon"
+                  color="secondary"
+                  aria-label="remove"
+                  onClick={() => remove(index)}
                 />
               </div>
-              <Button
-                StartIcon="x"
-                variant="icon"
-                color="secondary"
-                aria-label="remove"
-                onClick={() => remove(index)}
-              />
+              {fieldErrors && <InputError message={fieldErrors.message} />}
             </div>
           </div>
-        ))
-      ) : (
-        <div />
-      )}
+        );
+      })}
     </div>
   );
 };
