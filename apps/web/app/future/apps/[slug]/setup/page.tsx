67132e7939bd5830bf99703a1282e478208366a3
Benny Joo <sldisek783@gmail.com>
2024-08-25 20:52:28 -0400

    migrate apps/[slug]/setup

--- apps/web/app/future/apps/[slug]/setup/page.tsx
+++ apps/web/app/future/apps/[slug]/setup/page.tsx
@@ -1,11 +1,12 @@
-import Page from "@pages/apps/[slug]/setup";
 import { withAppDirSsr } from "app/WithAppDirSsr";
 import { _generateMetadata } from "app/_utils";
 import { WithLayout } from "app/layoutHOC";
-import type { InferGetServerSidePropsType } from "next";
 
 import { getServerSideProps } from "@calcom/app-store/_pages/setup/_getServerSideProps";
 import { APP_NAME } from "@calcom/lib/constants";
+import type { inferSSRProps } from "@calcom/types/inferSSRProps";
+
+import Page from "~/apps/[slug]/setup/setup-view";
 
 export const generateMetadata = async ({ params }: { params: Record<string, string | string[]> }) => {
   return await _generateMetadata(
@@ -14,7 +15,7 @@ export const generateMetadata = async ({ params }: { params: Record<string, stri
   );
 };
 
-type T = InferGetServerSidePropsType<typeof getServerSideProps>;
+type T = inferSSRProps<typeof getServerSideProps>;
 
 const getData = withAppDirSsr<T>(getServerSideProps);
 
