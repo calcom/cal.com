ede315c108ed8b12cce56084ac16ca80e0560b39
Benny Joo <sldisek783@gmail.com>
2024-08-27 00:58:36 -0400

    fix settings/platform main view

--- apps/web/app/future/settings/platform/page.tsx
+++ apps/web/app/future/settings/platform/page.tsx
@@ -1,11 +1,16 @@
 import { _generateMetadata } from "app/_utils";
+import { WithLayout } from "app/layoutHOC";
 
 import PlatformView from "~/settings/platform/platform-view";
 
-export const generateMetadata = async () =>
-  await _generateMetadata(
-    (t) => t(""),
-    (t) => t("")
+export const generateMetadata = async () => {
+  return await _generateMetadata(
+    () => "Platform",
+    () => "Manage everything related to platform."
   );
+};
 
-export default PlatformView;
+export default WithLayout({
+  getLayout: null,
+  Page: PlatformView,
+});
