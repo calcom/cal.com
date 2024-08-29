fed9d5112e4e6a48896609157d9e2a438e3ad5d0
Benny Joo <sldisek783@gmail.com>
2024-08-29 00:28:28 -0400

    fix import of apps/installation

--- apps/web/pages/apps/installation/[[...step]].tsx
+++ apps/web/pages/apps/installation/[[...step]].tsx
@@ -2,12 +2,11 @@ import PageWrapper from "@components/PageWrapper";
 
 import type { OnboardingPageProps } from "~/apps/installation/[[...step]]/step-view";
 import StepView from "~/apps/installation/[[...step]]/step-view";
-import { getServerSideProps } from "~/apps/installation/[[...step]]/step-view.getServerSideProps";
 
 const Page = (props: OnboardingPageProps) => <StepView {...props} />;
 
 Page.PageWrapper = PageWrapper;
 
-export { getServerSideProps };
+export { getServerSideProps } from "@lib/apps/installation/[[...step]]/getServerSideProps";
 
 export default Page;
