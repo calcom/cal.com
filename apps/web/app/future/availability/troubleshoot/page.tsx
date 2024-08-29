8b4c5567a808137e0491f3a697edda7872f6d4e1
Benny Joo <sldisek783@gmail.com>
2024-08-29 18:27:47 -0400

    migrate availability/troubleshoot page

---
+++ apps/web/app/future/availability/troubleshoot/page.tsx
@@ -0,0 +1,15 @@
+import { _generateMetadata } from "app/_utils";
+import { WithLayout } from "app/layoutHOC";
+
+import { getLayout } from "@calcom/features/troubleshooter/layout";
+
+import Troubleshoot from "~/availability/troubleshoot/troubleshoot-view";
+
+export const generateMetadata = async () => {
+  return await _generateMetadata(
+    (t) => t("troubleshoot"),
+    (t) => t("troubleshoot_availability")
+  );
+};
+
+export default WithLayout({ getLayout, Page: Troubleshoot });
