731e4310ad626eff8ec50ab56d450801475eb6f3
zomars <zomars@me.com>
2024-08-29 13:37:47 -0700

    refactor: more feedback

--- packages/core/builders/CalendarEvent/director.ts
+++ packages/core/builders/CalendarEvent/director.ts
@@ -44,7 +44,7 @@ export class CalendarEventDirector {
      * By default we don't want to allow reschedule for cancelled bookings.
      */
     allowRescheduleForCancelledBooking?: boolean;
-  }): Promise<void> {
+  } = {}): Promise<void> {
     if (this.existingBooking && this.existingBooking.eventTypeId && this.existingBooking.uid) {
       await this.builder.buildEventObjectFromInnerClass(this.existingBooking.eventTypeId);
       await this.builder.buildUsersFromInnerClass();
