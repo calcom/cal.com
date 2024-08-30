8a64badd445cd88180277d2c197ad68b4b0b764a
Benny Joo <sldisek783@gmail.com>
2024-08-30 00:37:14 -0400

    use params, not searchParams in generateMetadata

--- apps/web/app/future/availability/[schedule]/page.tsx
+++ apps/web/app/future/availability/[schedule]/page.tsx
@@ -4,9 +4,9 @@ import { WithLayout } from "app/layoutHOC";
 
 import { AvailabilitySettingsWebWrapper } from "~/availability/[schedule]/schedule-view";
 
-export const generateMetadata = async ({ searchParams }: PageProps) => {
+export const generateMetadata = async ({ params }: PageProps) => {
   const { trpc } = await import("@calcom/trpc");
-  const scheduleId = searchParams?.schedule ? Number(searchParams.schedule) : -1;
+  const scheduleId = params?.schedule ? Number(params.schedule) : -1;
 
   const { data: schedule } = trpc.viewer.availability.schedule.get.useQuery(
     { scheduleId },
