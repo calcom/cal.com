9279861351f98e56bb60313cafebaa467ed6331c
zomars <zomars@me.com>
2024-08-29 18:18:44 -0700

    fixes

--- apps/web/playwright/icons.e2e.ts
+++ apps/web/playwright/icons.e2e.ts
@@ -2,7 +2,11 @@ import { expect } from "@playwright/test";
 
 import { test } from "./lib/fixtures";
 
-test("Icons render properly", async ({ page }) => {
+test("Icons render properly", async ({ page }, testInfo) => {
   await page.goto("/icons");
-  await expect(page).toHaveScreenshot();
+  // To avoid different filenames between local and CI runs
+  testInfo.snapshotSuffix = "";
+  await expect(page).toHaveScreenshot("icons.png", {
+    fullPage: true,
+  });
 });
