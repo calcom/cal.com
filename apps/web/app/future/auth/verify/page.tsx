d40e75a1305874ed4b1cdbbaf48d1ccfb9e86763
Benny Joo <sldisek783@gmail.com>
2024-08-31 13:49:12 -0400

    auth/verify: extract client component into /modules

--- apps/web/app/future/auth/verify/page.tsx
+++ apps/web/app/future/auth/verify/page.tsx
@@ -1,6 +1,5 @@
-import VerifyPage from "@pages/auth/verify";
 import { withAppDirSsr } from "app/WithAppDirSsr";
-import type { PageProps } from "app/_types";
+import type { PageProps as _PageProps } from "app/_types";
 import { _generateMetadata } from "app/_utils";
 import { WithLayout } from "app/layoutHOC";
 import { z } from "zod";
@@ -9,15 +8,16 @@ import { StripeRepository } from "@calcom/lib/server/repository/stripe";
 
 import { getServerSideProps } from "@server/lib/auth/verify/getServerSideProps";
 
+import type { PageProps } from "~/auth/verify-view";
+import VerifyPage from "~/auth/verify-view";
+
 const querySchema = z.object({
   stripeCustomerId: z.string().optional(),
   sessionId: z.string().optional(),
   t: z.string().optional(),
 });
 
-export const generateMetadata = async ({ params }: PageProps) => {
-  console.log("PARAMS123", params);
-
+export const generateMetadata = async ({ params }: _PageProps) => {
   const { sessionId, stripeCustomerId } = querySchema.parse(params);
 
   if (!stripeCustomerId && !sessionId) {
@@ -34,9 +34,9 @@ export const generateMetadata = async ({ params }: PageProps) => {
 
   const { hasPaymentFailed } = data;
 
-  hasPaymentFailed ? "Your payment failed" : sessionId ? "Payment successful!" : `Verify your email`;
   return await _generateMetadata(
-    () => "",
+    () =>
+      hasPaymentFailed ? "Your payment failed" : sessionId ? "Payment successful!" : `Verify your email`,
     () => ""
   );
 };
@@ -44,5 +44,5 @@ export const generateMetadata = async ({ params }: PageProps) => {
 export default WithLayout({
   getLayout: null,
   Page: VerifyPage,
-  getData: withAppDirSsr(getServerSideProps),
+  getData: withAppDirSsr<PageProps>(getServerSideProps),
 })<"P">;
