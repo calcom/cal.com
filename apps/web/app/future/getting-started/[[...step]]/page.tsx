04ceb8dcbf9d0ead8a56ec928d86b9a602b0b954
Benny Joo <sldisek783@gmail.com>
2024-08-31 16:07:41 -0400

    getting-started: extract into /modules

--- apps/web/app/future/getting-started/[[...step]]/page.tsx
+++ apps/web/app/future/getting-started/[[...step]]/page.tsx
@@ -1,7 +1,8 @@
-import Page from "@pages/getting-started/[[...step]]";
 import { withAppDirSsr } from "app/WithAppDirSsr";
 import { WithLayout } from "app/layoutHOC";
 
 import { getServerSideProps } from "@lib/getting-started/[[...step]]/getServerSideProps";
+
+import Page from "~/getting-started/[[...step]]/onboarding-view";
 
 export default WithLayout({ getLayout: null, getData: withAppDirSsr(getServerSideProps), Page });
