7afe48923b707452d37c9d77a206bd7ac8f23264
zomars <zomars@me.com>
2024-08-30 17:32:43 -0700

    reverted suffix removal

--- apps/web/playwright/icons.e2e.ts
+++ apps/web/playwright/icons.e2e.ts
@@ -4,8 +4,6 @@ import { test } from "./lib/fixtures";
 
 test("Icons render properly", async ({ page }, testInfo) => {
   await page.goto("/icons");
-  // To avoid different filenames between local and CI runs
-  testInfo.snapshotSuffix = "";
   await expect(page).toHaveScreenshot("icons.png", {
     fullPage: true,
   });
