4d07bf7484a3266de4ffe622065d45e654cdaeba
Benny Joo <sldisek783@gmail.com>
2024-08-31 13:41:50 -0400

    auth/verify: Add metadata in app router

--- apps/web/app/future/auth/verify/page.tsx
+++ apps/web/app/future/auth/verify/page.tsx
@@ -1,8 +1,45 @@
 import VerifyPage from "@pages/auth/verify";
 import { withAppDirSsr } from "app/WithAppDirSsr";
+import type { PageProps } from "app/_types";
+import { _generateMetadata } from "app/_utils";
 import { WithLayout } from "app/layoutHOC";
+import { z } from "zod";
+
+import { StripeRepository } from "@calcom/lib/server/repository/stripe";
 
 import { getServerSideProps } from "@server/lib/auth/verify/getServerSideProps";
+
+const querySchema = z.object({
+  stripeCustomerId: z.string().optional(),
+  sessionId: z.string().optional(),
+  t: z.string().optional(),
+});
+
+export const generateMetadata = async ({ params }: PageProps) => {
+  console.log("PARAMS123", params);
+
+  const { sessionId, stripeCustomerId } = querySchema.parse(params);
+
+  if (!stripeCustomerId && !sessionId) {
+    return await _generateMetadata(
+      () => "",
+      () => ""
+    );
+  }
+
+  const data = await StripeRepository.getCheckoutSession({
+    stripeCustomerId,
+    checkoutSessionId: sessionId,
+  });
+
+  const { hasPaymentFailed } = data;
+
+  hasPaymentFailed ? "Your payment failed" : sessionId ? "Payment successful!" : `Verify your email`;
+  return await _generateMetadata(
+    () => "",
+    () => ""
+  );
+};
 
 export default WithLayout({
   getLayout: null,
