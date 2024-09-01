0dfa574c6899769e993188544bcfe7428ecb4251
Benny Joo <sldisek783@gmail.com>
2024-08-31 22:22:50 -0400

    more: extract client component into /modules folder

--- apps/web/app/future/more/page.tsx
+++ apps/web/app/future/more/page.tsx
@@ -1,4 +1,5 @@
-import Page from "@pages/more";
 import { WithLayout } from "app/layoutHOC";
+
+import Page from "~/more/more-page-view";
 
 export default WithLayout({ getLayout: null, Page })<"P">;
