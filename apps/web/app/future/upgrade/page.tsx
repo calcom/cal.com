9c988801ddb79082d9ea24b39f01d539821501d4
Benny Joo <sldisek783@gmail.com>
2024-08-31 22:45:17 -0400

    upgrade: extract client component into /modules folder

--- apps/web/app/future/upgrade/page.tsx
+++ apps/web/app/future/upgrade/page.tsx
@@ -1,14 +1,12 @@
 import { _generateMetadata } from "app/_utils";
 import { WithLayout } from "app/layoutHOC";
 
-import { APP_NAME } from "@calcom/lib/constants";
-
-import LegacyPage from "~/maintenance/maintenance-view";
+import LegacyPage from "~/upgrade/upgrade-view";
 
 export const generateMetadata = async () =>
   await _generateMetadata(
-    (t) => t("under_maintenance"),
-    (t) => t("under_maintenance_description", { appName: APP_NAME })
+    () => "Your upgrade is here",
+    () => "Improve your scheduling experience by upgrading to the new plan and enjoy the new features."
   );
 
 export default WithLayout({ getLayout: null, Page: LegacyPage })<"P">;
