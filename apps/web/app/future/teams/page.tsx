114ed5fffe8ac2d24acadc6602e6019a4f958b54
Benny Joo <sldisek783@gmail.com>
2024-09-02 19:46:43 -0400

    extract /teams/* into /modules

--- apps/web/app/future/teams/page.tsx
+++ apps/web/app/future/teams/page.tsx
@@ -1,9 +1,10 @@
-import Page from "@pages/teams/index";
 import { withAppDirSsr } from "app/WithAppDirSsr";
 import { _generateMetadata } from "app/_utils";
 import { WithLayout } from "app/layoutHOC";
 
 import { getServerSideProps } from "@lib/teams/getServerSideProps";
+
+import Page from "~/teams/teams-view";
 
 export const generateMetadata = async () =>
   await _generateMetadata(
