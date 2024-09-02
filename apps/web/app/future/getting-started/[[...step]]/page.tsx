254af71091ebfb9ee211a344a6b8d7d36f94b30b
Benny Joo <sldisek783@gmail.com>
2024-09-01 21:16:19 -0400

    fix type-check

--- apps/web/app/future/getting-started/[[...step]]/page.tsx
+++ apps/web/app/future/getting-started/[[...step]]/page.tsx
@@ -3,6 +3,7 @@ import { WithLayout } from "app/layoutHOC";
 
 import { getServerSideProps } from "@lib/getting-started/[[...step]]/getServerSideProps";
 
+import type { PageProps } from "~/getting-started/[[...step]]/onboarding-view";
 import Page from "~/getting-started/[[...step]]/onboarding-view";
 
-export default WithLayout({ getLayout: null, getData: withAppDirSsr(getServerSideProps), Page });
+export default WithLayout({ getLayout: null, getData: withAppDirSsr<PageProps>(getServerSideProps), Page });
