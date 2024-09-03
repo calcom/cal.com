3e2e1acf0e6b8fe5716bc3e7530c7da679076231
Benny Joo <sldisek783@gmail.com>
2024-09-03 01:47:39 -0400

    fix type page metadata

--- apps/web/app/future/team/[slug]/[type]/page.tsx
+++ apps/web/app/future/team/[slug]/[type]/page.tsx
@@ -4,6 +4,7 @@ import { _generateMetadata } from "app/_utils";
 import { WithLayout } from "app/layoutHOC";
 import { cookies, headers } from "next/headers";
 
+import { orgDomainConfig } from "@calcom/features/ee/organizations/lib/orgDomains";
 import { EventRepository } from "@calcom/lib/server/repository/event";
 
 import { buildLegacyCtx } from "@lib/buildLegacyCtx";
@@ -14,15 +15,15 @@ import LegacyPage, { type PageProps as LegacyPageProps } from "~/team/type-view"
 export const generateMetadata = async ({ params, searchParams }: PageProps) => {
   const legacyCtx = buildLegacyCtx(headers(), cookies(), params, searchParams);
   const props = await getData(legacyCtx);
-  const { eventData, user: username, slug: eventSlug, booking } = props;
-  const entity = eventData.entity;
+  const { user: username, slug: eventSlug, booking } = props;
+  const { currentOrgDomain, isValidOrgDomain } = orgDomainConfig(legacyCtx.req, legacyCtx.params?.orgSlug);
 
   const event = await EventRepository.getPublicEvent({
     username,
     eventSlug,
-    isTeamEvent: false,
-    org: entity.orgSlug ?? null,
-    fromRedirectOfNonOrgLink: entity.fromRedirectOfNonOrgLink,
+    isTeamEvent: true,
+    org: isValidOrgDomain ? currentOrgDomain : null,
+    fromRedirectOfNonOrgLink: legacyCtx.query.orgRedirection === "true",
   });
 
   const profileName = event?.profile?.name ?? "";
