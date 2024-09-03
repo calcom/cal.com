68e6fffff07e6e091e3ce35967a68483dc79195c
Benny Joo <sldisek783@gmail.com>
2024-09-02 23:58:34 -0400

    fix embed pages

--- apps/web/pages/team/[slug]/[type]/embed.tsx
+++ apps/web/pages/team/[slug]/[type]/embed.tsx
@@ -1,8 +1,14 @@
-"use client";
-
 import { getServerSideProps as _getServerSideProps } from "@lib/team/[slug]/[type]/getServerSideProps";
 import withEmbedSsr from "@lib/withEmbedSsr";
 
-export { default } from "~/team/type-view";
+import PageWrapper from "@components/PageWrapper";
+
+import TypePage, { type PageProps } from "~/team/type-view";
 
 export const getServerSideProps = withEmbedSsr(_getServerSideProps);
+
+const Page = (props: PageProps) => <TypePage {...props} />;
+
+Page.PageWrapper = PageWrapper;
+
+export default Page;
--- apps/web/pages/team/[slug]/embed.tsx
+++ apps/web/pages/team/[slug]/embed.tsx
@@ -1,8 +1,14 @@
-"use client";
-
 import { getServerSideProps as _getServerSideProps } from "@lib/team/[slug]/getServerSideProps";
 import withEmbedSsr from "@lib/withEmbedSsr";
 
-export { default } from "~/team/team-view";
+import PageWrapper from "@components/PageWrapper";
+
+import TeamPage, { type PageProps } from "~/team/team-view";
 
 export const getServerSideProps = withEmbedSsr(_getServerSideProps);
+
+const Page = (props: PageProps) => <TeamPage {...props} />;
+
+Page.PageWrapper = PageWrapper;
+
+export default Page;
