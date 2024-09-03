a341d90d14f45d58142467bcc4830814bec36479
Benny Joo <sldisek783@gmail.com>
2024-09-02 23:14:08 -0400

    fix metadata for type page

--- apps/web/app/future/team/[slug]/[type]/embed/page.tsx
+++ apps/web/app/future/team/[slug]/[type]/embed/page.tsx
@@ -1,11 +1,40 @@
 import { withAppDirSsr } from "app/WithAppDirSsr";
 import withEmbedSsrAppDir from "app/WithEmbedSSR";
+import type { PageProps as _PageProps } from "app/_types";
+import { _generateMetadata } from "app/_utils";
 import { WithLayout } from "app/layoutHOC";
+import { cookies, headers } from "next/headers";
 
+import { EventRepository } from "@calcom/lib/server/repository/event";
+
+import { buildLegacyCtx } from "@lib/buildLegacyCtx";
 import { getServerSideProps } from "@lib/team/[slug]/[type]/getServerSideProps";
 
 import type { PageProps } from "~/team/type-view";
 import LegacyPage from "~/team/type-view";
+
+export const generateMetadata = async ({ params, searchParams }: _PageProps) => {
+  const legacyCtx = buildLegacyCtx(headers(), cookies(), params, searchParams);
+  const props = await getData(legacyCtx);
+  const { eventData, user: username, slug: eventSlug, booking } = props;
+  const entity = eventData.entity;
+
+  const event = await EventRepository.getPublicEvent({
+    username,
+    eventSlug,
+    isTeamEvent: false,
+    org: entity.orgSlug ?? null,
+    fromRedirectOfNonOrgLink: entity.fromRedirectOfNonOrgLink,
+  });
+
+  const profileName = event?.profile?.name ?? "";
+  const title = event?.title ?? "";
+
+  return await _generateMetadata(
+    (t) => `${booking?.uid && !!booking ? t("reschedule") : ""} ${title} | ${profileName}`,
+    (t) => `${booking?.uid ? t("reschedule") : ""} ${title}`
+  );
+};
 
 const getData = withAppDirSsr<PageProps>(getServerSideProps);
 const getEmbedData = withEmbedSsrAppDir(getData);
--- apps/web/app/future/team/[slug]/[type]/page.tsx
+++ apps/web/app/future/team/[slug]/[type]/page.tsx
@@ -4,6 +4,8 @@ import { _generateMetadata } from "app/_utils";
 import { WithLayout } from "app/layoutHOC";
 import { cookies, headers } from "next/headers";
 
+import { EventRepository } from "@calcom/lib/server/repository/event";
+
 import { buildLegacyCtx } from "@lib/buildLegacyCtx";
 import { getServerSideProps } from "@lib/team/[slug]/[type]/getServerSideProps";
 
@@ -12,19 +14,16 @@ import LegacyPage, { type PageProps as LegacyPageProps } from "~/team/type-view"
 export const generateMetadata = async ({ params, searchParams }: PageProps) => {
   const legacyCtx = buildLegacyCtx(headers(), cookies(), params, searchParams);
   const props = await getData(legacyCtx);
-  const { eventData, user, slug, booking } = props;
+  const { eventData, user: username, slug: eventSlug, booking } = props;
   const entity = eventData.entity;
-  const { trpc } = await import("@calcom/trpc");
-  const { data: event } = trpc.viewer.public.event.useQuery(
-    {
-      username: user,
-      eventSlug: slug,
-      isTeamEvent: false,
-      org: entity.orgSlug ?? null,
-      fromRedirectOfNonOrgLink: entity.fromRedirectOfNonOrgLink,
-    },
-    { refetchOnWindowFocus: false }
-  );
+
+  const event = await EventRepository.getPublicEvent({
+    username,
+    eventSlug,
+    isTeamEvent: false,
+    org: entity.orgSlug ?? null,
+    fromRedirectOfNonOrgLink: entity.fromRedirectOfNonOrgLink,
+  });
 
   const profileName = event?.profile?.name ?? "";
   const title = event?.title ?? "";
