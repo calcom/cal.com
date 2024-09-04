3bf481ae15b6069289b16f5c824047effeb9d56b
Morgan Vernay <morgan@cal.com>
2024-09-04 19:30:19 +0300

    fixup! Merge branch 'main' into fix-use-platform-me-less-spammy

--- apps/web/components/settings/platform/hooks/usePlatformMe.ts
+++ apps/web/components/settings/platform/hooks/usePlatformMe.ts
@@ -7,12 +7,6 @@ export const usePlatformMe = () => {
   const platformMeQuery = useQuery<UserResponse>({
     queryKey: [QUERY_KEY],
     staleTime: 300000,
-    retry: (failureCount, error) => {
-      if (error.message === "500") {
-        return false;
-      }
-      return failureCount < 3; // Retry up to 3 times for other errors
-    },
     queryFn: async () => {
       const response = await fetch(`/api/v2/me`, {
         method: "get",
