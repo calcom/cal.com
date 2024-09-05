f186a3449f78e9aeab998910c41219875946597b
Keith Williams <keithwillcode@gmail.com>
2024-09-05 10:10:05 -0300

    Reverted unneeded change

--- packages/features/insights/server/events.ts
+++ packages/features/insights/server/events.ts
@@ -51,7 +51,7 @@ class EventsInsights {
       (aggregate: { [x: string]: number }, item) => {
         if (typeof item.timeStatus === "string") {
           aggregate[item.timeStatus] = item._count._all;
-          aggregate._all += item._count._all;
+          aggregate["_all"] += item._count._all;
         }
         return aggregate;
       },
