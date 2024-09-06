884ce47d9b10c2ff81461d7de0f35df3a7a34de0
Benny Joo <sldisek783@gmail.com>
2024-09-06 13:00:42 -0400

    fix metadata

--- apps/web/app/future/connect-and-join/page.tsx
+++ apps/web/app/future/connect-and-join/page.tsx
@@ -5,8 +5,8 @@ import LegacyPage from "~/connect-and-join/connect-and-join-view";
 
 export const generateMetadata = async () => {
   return await _generateMetadata(
-    (t) => t("instant_tab_title"),
-    (t) => t("uprade_to_create_instant_bookings")
+    () => "Connect and Join",
+    () => ""
   );
 };
 
