a06919bd793d8f3c9ea9135bc5f6dce575bbf2cc
Benny Joo <sldisek783@gmail.com>
2024-09-06 12:59:45 -0400

    add metadata for connect and join

--- apps/web/app/future/connect-and-join/page.tsx
+++ apps/web/app/future/connect-and-join/page.tsx
@@ -1,5 +1,13 @@
+import { _generateMetadata } from "app/_utils";
 import { WithLayout } from "app/layoutHOC";
 
 import LegacyPage from "~/connect-and-join/connect-and-join-view";
+
+export const generateMetadata = async () => {
+  return await _generateMetadata(
+    (t) => t("instant_tab_title"),
+    (t) => t("uprade_to_create_instant_bookings")
+  );
+};
 
 export default WithLayout({ getLayout: null, Page: LegacyPage })<"P">;
