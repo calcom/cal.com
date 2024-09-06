ef855fd20654445555fe18d60863c7d8ec19481b
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-09-06 17:47:38 -0400

    Remove meta instance from billing page route

--- apps/web/pages/settings/billing/index.tsx
+++ apps/web/pages/settings/billing/index.tsx
@@ -1,14 +1,21 @@
 import { getLayout } from "@calcom/features/settings/layouts/SettingsLayout";
+import { useLocale } from "@calcom/lib/hooks/useLocale";
+import { Meta } from "@calcom/ui";
 
 import PageWrapper from "@components/PageWrapper";
 
 import BillingView from "~/settings/billing/billing-view";
 
-const Page = new Proxy<{
-  (): JSX.Element;
-  PageWrapper?: typeof PageWrapper;
-  getLayout?: typeof getLayout;
-}>(BillingView, {});
+const Page = () => {
+  const { t } = useLocale();
+
+  return (
+    <>
+      <Meta title={t("billing")} description={t("manage_billing_description")} borderInShellHeader={true} />
+      <BillingView />
+    </>
+  );
+};
 
 Page.getLayout = getLayout;
 Page.PageWrapper = PageWrapper;
