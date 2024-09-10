7609fb01937bee444be9a4e54372118cf1df9c61
Benny Joo <sldisek783@gmail.com>
2024-09-10 18:44:08 -0400

    fix

--- packages/lib/server/repository/stripe.ts
+++ packages/lib/server/repository/stripe.ts
@@ -1,28 +1,8 @@
-import { z } from "zod";
-
 import stripe from "@calcom/app-store/stripepayment/lib/server";
-
-export const ZStripeCheckoutSessionInputSchema = z
-  .object({
-    stripeCustomerId: z.string().optional(),
-    checkoutSessionId: z.string().optional(),
-  })
-  .superRefine((arg, ctx) => {
-    if (!arg.checkoutSessionId && !arg.stripeCustomerId) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: "Missing checkoutSessionId or stripeCustomerId",
-      });
-    }
-    if (arg.checkoutSessionId && arg.stripeCustomerId) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.custom,
-        message: "Both checkoutSessionId and stripeCustomerId provided",
-      });
-    }
-  });
-
-export type TStripeCheckoutSessionInputSchema = z.infer<typeof ZStripeCheckoutSessionInputSchema>;
+import {
+  ZStripeCheckoutSessionInputSchema,
+  type TStripeCheckoutSessionInputSchema,
+} from "@calcom/trpc/server/routers/publicViewer/stripeCheckoutSession.schema";
 
 export class StripeRepository {
   static async getCheckoutSession(input: TStripeCheckoutSessionInputSchema) {
--- packages/trpc/server/routers/publicViewer/stripeCheckoutSession.schema.ts
+++ packages/trpc/server/routers/publicViewer/stripeCheckoutSession.schema.ts
@@ -1,3 +1,23 @@
-export { ZStripeCheckoutSessionInputSchema } from "@calcom/lib/server/repository/stripe";
+import { z } from "zod";
 
-export type { TStripeCheckoutSessionInputSchema } from "@calcom/lib/server/repository/stripe";
+export const ZStripeCheckoutSessionInputSchema = z
+  .object({
+    stripeCustomerId: z.string().optional(),
+    checkoutSessionId: z.string().optional(),
+  })
+  .superRefine((arg, ctx) => {
+    if (!arg.checkoutSessionId && !arg.stripeCustomerId) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Missing checkoutSessionId or stripeCustomerId",
+      });
+    }
+    if (arg.checkoutSessionId && arg.stripeCustomerId) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Both checkoutSessionId and stripeCustomerId provided",
+      });
+    }
+  });
+
+export type TStripeCheckoutSessionInputSchema = z.infer<typeof ZStripeCheckoutSessionInputSchema>;
