b6c46430de56c6981c26ff4b88d7e2c01d5cb404
Benny Joo <sldisek783@gmail.com>
2024-09-10 00:49:06 -0400

    do not use PageWrapper directly

--- apps/web/app/future/availability/[schedule]/page.tsx
+++ apps/web/app/future/availability/[schedule]/page.tsx
@@ -1,14 +1,13 @@
 import type { PageProps } from "app/_types";
 import { _generateMetadata } from "app/_utils";
+import { WithLayout } from "app/layoutHOC";
 import { getServerSession } from "next-auth";
-import { headers } from "next/headers";
+import { notFound } from "next/navigation";
 
 import { AUTH_OPTIONS } from "@calcom/features/auth/lib/next-auth-options";
 import { ScheduleRepository } from "@calcom/lib/server/repository/schedule";
 import { TravelScheduleRepository } from "@calcom/lib/server/repository/travelSchedule";
 import { UserRepository } from "@calcom/lib/server/repository/user";
-
-import PageWrapper from "@components/PageWrapperAppDir";
 
 import { AvailabilitySettingsWebWrapper } from "~/availability/[schedule]/schedule-view";
 
@@ -30,40 +29,38 @@ export const generateMetadata = async ({ params }: PageProps) => {
 };
 
 const Page = async ({ params }: PageProps) => {
-  const h = headers();
-  const nonce = h.get("x-nonce") ?? undefined;
   const session = await getServerSession(AUTH_OPTIONS);
   const userId = session?.user?.id ?? -1;
-  const userData = await UserRepository.findById({
-    id: userId,
-    select: {
-      timeZone: true,
-      defaultScheduleId: true,
-    },
-  });
+  let userData, schedule, travelSchedules;
+
+  try {
+    userData = await UserRepository.findById({
+      id: userId,
+      select: {
+        timeZone: true,
+        defaultScheduleId: true,
+      },
+    });
+  } catch (e) {
+    notFound();
+  }
 
   const scheduleId = params?.schedule ? Number(params.schedule) : -1;
-  const [schedule, travelSchedules] = await Promise.all([
-    ScheduleRepository.findDetailedScheduleById({
+  try {
+    schedule = await ScheduleRepository.findDetailedScheduleById({
       scheduleId,
       isManagedEventType: false,
       userId,
       timeZone: userData?.timeZone ?? "Europe/London",
       defaultScheduleId: userData?.defaultScheduleId ?? -1,
-    }),
-    TravelScheduleRepository.findTravelSchedulesByUserId(userId),
-  ]);
+    });
+  } catch (e) {}
 
-  return (
-    <PageWrapper
-      getLayout={null}
-      requiresLicense={false}
-      nonce={nonce}
-      themeBasis={null}
-      isBookingPage={false}>
-      <AvailabilitySettingsWebWrapper schedule={schedule} travelSchedules={travelSchedules} />
-    </PageWrapper>
-  );
+  try {
+    travelSchedules = await TravelScheduleRepository.findTravelSchedulesByUserId(userId);
+  } catch (e) {}
+
+  return <AvailabilitySettingsWebWrapper schedule={schedule} travelSchedules={travelSchedules} />;
 };
 
-export default Page;
+export default WithLayout({ ServerPage: Page });
--- apps/web/app/future/availability/page.tsx
+++ apps/web/app/future/availability/page.tsx
@@ -1,11 +1,9 @@
 import { _generateMetadata } from "app/_utils";
+import { WithLayout } from "app/layoutHOC";
 import { getServerSession } from "next-auth";
-import { headers } from "next/headers";
 
 import { AUTH_OPTIONS } from "@calcom/features/auth/lib/next-auth-options";
 import { OrganizationRepository } from "@calcom/lib/server/repository/organization";
-
-import PageWrapper from "@components/PageWrapperAppDir";
 
 import AvailabilityPage from "~/availability/availability-view";
 
@@ -17,8 +15,6 @@ export const generateMetadata = async () => {
 };
 
 const Page = async () => {
-  const h = headers();
-  const nonce = h.get("x-nonce") ?? undefined;
   const session = await getServerSession(AUTH_OPTIONS);
   const userId = session?.user?.id ?? -1;
   const orgId = session?.user?.org?.id ?? -1;
@@ -31,16 +27,7 @@ const Page = async () => {
     });
   } catch {}
 
-  return (
-    <PageWrapper
-      getLayout={null}
-      requiresLicense={false}
-      nonce={nonce}
-      themeBasis={null}
-      isBookingPage={false}>
-      <AvailabilityPage currentOrg={currentOrg} />
-    </PageWrapper>
-  );
+  return <AvailabilityPage currentOrg={currentOrg} />;
 };
 
-export default Page;
+export default WithLayout({ ServerPage: Page });
