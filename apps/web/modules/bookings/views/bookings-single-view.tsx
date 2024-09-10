134788d5ef036f3d5fca4eafe38d9a1dc777baf3
Alex van Andel <me@alexvanandel.com>
2024-09-10 14:10:22 +0100

    fix: Add missing URL encoding to scheduledBy

--- apps/web/modules/bookings/views/bookings-single-view.tsx
+++ apps/web/modules/bookings/views/bookings-single-view.tsx
@@ -703,7 +703,9 @@ export default function Success(props: PageProps) {
                                   <span className="underline" data-testid="reschedule-link">
                                     <Link
                                       href={`/reschedule/${seatReferenceUid || bookingInfo?.uid}${
-                                        currentUserEmail ? `?rescheduledBy=${currentUserEmail}` : ""
+                                        currentUserEmail
+                                          ? `?rescheduledBy=${encodeURIComponent(currentUserEmail)}`
+                                          : ""
                                       }`}
                                       legacyBehavior>
                                       {t("reschedule")}
