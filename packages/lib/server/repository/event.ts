cbce1d1e0f7c35247f6fb98886422fe684ff4af8
Benny Joo <sldisek783@gmail.com>
2024-09-10 20:11:46 -0400

    revert unneeded changes

--- packages/lib/server/repository/event.ts
+++ packages/lib/server/repository/event.ts
@@ -1,21 +1,7 @@
-import { z } from "zod";
-
 import { getPublicEvent } from "@calcom/features/eventtypes/lib/getPublicEvent";
 import prisma from "@calcom/prisma";
 
-export const ZEventInputSchema = z.object({
-  username: z.string(),
-  eventSlug: z.string(),
-  isTeamEvent: z.boolean().optional(),
-  org: z.string().nullable(),
-  /**
-   * Informs that the event request has been sent from a page that was reached by a redirect from non-org link(i.e. app.cal.com/username redirected to acme.cal.com/username)
-   * Based on this decision like whether to allow unpublished organization's event to be served or not can be made.
-   */
-  fromRedirectOfNonOrgLink: z.boolean().optional().default(false),
-});
-
-export type TEventInputSchema = z.infer<typeof ZEventInputSchema>;
+import type { TEventInputSchema } from "../../../trpc/server/routers/publicViewer/event.schema";
 
 export class EventRepository {
   static async getPublicEvent(input: TEventInputSchema) {
