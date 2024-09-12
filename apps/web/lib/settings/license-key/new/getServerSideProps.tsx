35b8f5a298f3af01bb047d9cbeda49a165a779de
zomars <zomars@me.com>
2024-09-12 11:24:07 -0700

    Update getServerSideProps.tsx

--- apps/web/lib/settings/license-key/new/getServerSideProps.tsx
+++ apps/web/lib/settings/license-key/new/getServerSideProps.tsx
@@ -1,5 +1,4 @@
 import type { GetServerSidePropsContext } from "next";
-import type { NextApiRequest } from "next/types";
 
 import { getServerSession } from "@calcom/feature-auth/lib/getServerSession";
 import { getOptions } from "@calcom/feature-auth/lib/next-auth-options";
@@ -8,7 +7,7 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
   const session = await getServerSession({
     req: context.req,
     res: context.res,
-    authOptions: getOptions(context.req as NextApiRequest),
+    authOptions: getOptions({ getDclid: () => context.req.cookies.dclid }),
   });
   // Disable this check if we ever make this self serve.
   if (session?.user.role !== "ADMIN") {
