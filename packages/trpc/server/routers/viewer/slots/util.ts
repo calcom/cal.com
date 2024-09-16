f1fb7e133f32eb39a5f519c49169ac8cf7348681
Syed Ali Shahbaz <52925846+alishaz-polymath@users.noreply.github.com>
2024-09-16 10:02:29 +0400

    fix init

--- packages/trpc/server/routers/viewer/slots/util.ts
+++ packages/trpc/server/routers/viewer/slots/util.ts
@@ -38,6 +38,8 @@ import type { GetScheduleOptions } from "./getSchedule.handler";
 import type { TGetScheduleInputSchema } from "./getSchedule.schema";
 import { handleNotificationWhenNoSlots } from "./handleNotificationWhenNoSlots";
 
+const log = logger.getSubLogger({ prefix: ["[slots/util]"] });
+
 export const checkIfIsAvailable = ({
   time,
   busy,
@@ -135,7 +137,7 @@ export async function getEventType(
   organizationDetails: { currentOrgDomain: string | null; isValidOrgDomain: boolean }
 ) {
   const { eventTypeSlug, usernameList, isTeamEvent } = input;
-  logger.info("getEventType", safeStringify({ usernameList, eventTypeSlug, isTeamEvent, organizationDetails }));
+  log.info("getEventType", safeStringify({ usernameList, eventTypeSlug, isTeamEvent, organizationDetails }));
   const eventTypeId =
     input.eventTypeId ||
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -839,7 +841,7 @@ async function getUserIdFromUsername(
   organizationDetails: { currentOrgDomain: string | null; isValidOrgDomain: boolean }
 ) {
   const { currentOrgDomain, isValidOrgDomain } = organizationDetails;
-  logger.info("getUserIdFromUsername", safeStringify({ organizationDetails, username }));
+  log.info("getUserIdFromUsername", safeStringify({ organizationDetails, username }));
   const [user] = await UserRepository.findUsersByUsername({
     usernameList: [username],
     orgSlug: isValidOrgDomain ? currentOrgDomain : null,
