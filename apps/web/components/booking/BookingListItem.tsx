3c91a818c33c1ca7075341be55535bedb6b11257
Udit Takkar <udit222001@gmail.com>
2024-09-18 13:38:58 +0530

    chore: remove condition

--- apps/web/components/booking/BookingListItem.tsx
+++ apps/web/components/booking/BookingListItem.tsx
@@ -598,7 +598,7 @@ function BookingListItem(booking: BookingItemProps) {
         <td className="flex w-full flex-col flex-wrap items-end justify-end space-x-2 space-y-2 py-4 pl-4 text-right text-sm font-medium ltr:pr-4 rtl:pl-4 sm:flex-row sm:flex-nowrap sm:items-start sm:space-y-0 sm:pl-0">
           {isUpcoming && !isCancelled ? (
             <>
-              {isPending && userId === booking.user?.id && <TableActions actions={pendingActions} />}
+              {isPending && <TableActions actions={pendingActions} />}
               {isConfirmed && <TableActions actions={bookedActions} />}
               {isRejected && <div className="text-subtle text-sm">{t("rejected")}</div>}
             </>
