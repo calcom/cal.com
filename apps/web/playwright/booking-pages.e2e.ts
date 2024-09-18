c8b210d4bae1020896d1927076a6587a4575fdb3
unknown <adhabal2002@gmail.com>
2024-09-18 15:44:10 +0530

    fix and update

--- apps/web/playwright/booking-pages.e2e.ts
+++ apps/web/playwright/booking-pages.e2e.ts
@@ -28,15 +28,12 @@ test("check SSR and OG - User Event Type", async ({ page, users }) => {
   const user = await users.create({
     name,
   });
-  const [response] = await Promise.all([
-    // This promise resolves to the main resource response
-    page.waitForResponse(
-      (response) => response.url().includes(`/${user.username}/30-min`) && response.status() === 200
-    ),
-
-    // Trigger the page navigation
-    page.goto(`/${user.username}/30-min`),
-  ]);
+  const responsePromise = page.waitForResponse(
+    (response) => response.url().includes(`/${user.username}/30-min`) && response.status() === 200
+  );
+  await page.goto(`/${user.username}/30-min`);
+  await page.content();
+  const response = await responsePromise;
   const ssrResponse = await response.text();
   const document = new JSDOM(ssrResponse).window.document;
 
