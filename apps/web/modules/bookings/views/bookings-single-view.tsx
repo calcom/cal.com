551e0a84b91359c4604031e04df9997a546bb93c
Felippe Fernandes <lfelippeeng@gmail.com>
2024-09-19 14:41:00 -0300

    strategy to continue in light mode

--- apps/web/modules/bookings/views/bookings-single-view.tsx
+++ apps/web/modules/bookings/views/bookings-single-view.tsx
@@ -256,11 +256,14 @@ export default function Success(props: PageProps) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const html = document.querySelector("html");
+
   useEffect(() => {
-    const html = document.querySelector("html");
-    html?.setAttribute("data-testid", "dark-mode");
-    html?.classList.remove("dark");
-  }, []);
+    const hasDarkMode = html?.classList.contains("dark");
+    if (hasDarkMode) {
+      html?.classList.remove("dark");
+    }
+  }, [html]);
 
   const sendFeedback = async (rating: string, comment: string) => {
     mutation.mutate({ bookingUid: bookingInfo.uid, rating: rateValue, comment: comment });
