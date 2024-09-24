0333a251f8dd399e6a32a61b5bbe8645489d62ed
Benny Joo <sldisek783@gmail.com>
2024-09-24 15:38:58 -0400

    Revert "feat: Update UI of signup / login pages (#16710)"
    This reverts commit b879aeef0983585d2aa00c5b2e42a87dbdc7fbb4.

--- apps/web/playwright/organization/organization-invitation.e2e.ts
+++ apps/web/playwright/organization/organization-invitation.e2e.ts
@@ -297,10 +297,9 @@ test.describe("Organization", () => {
       await test.step("Signing up with the previous username of the migrated user - shouldn't be allowed", async () => {
         await page.goto("/signup");
         await expect(page.locator("text=Create your account")).toBeVisible();
-        await expect(page.locator('[data-testid="continue-with-email-button"]')).toBeVisible();
-        await page.locator('[data-testid="continue-with-email-button"]').click();
+        const usernameInput = page.locator('input[name="username"]');
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        await page.locator('input[name="username"]').fill(existingUser.username!);
+        await usernameInput.fill(existingUser.username!);
         await page
           .locator('input[name="email"]')
           .fill(`${existingUser.username}-differnet-email@example.com`);
