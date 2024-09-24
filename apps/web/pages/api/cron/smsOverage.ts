ba4193ba9afa082f70bf4e9c67d8776975956fd2
CarinaWolli <wollencarina@gmail.com>
2024-09-24 17:06:59 -0400

    add empty cron job

---
+++ apps/web/pages/api/cron/smsOverage.ts
@@ -0,0 +1,7 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  //get the previous month
+  // get all smsCreditCounts from the prev month that have overage charges
+  //create stripe invoice
+}
