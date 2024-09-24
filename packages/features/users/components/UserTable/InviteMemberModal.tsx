7deae42edee9b2c4764473608d3369da6b71d3bb
Ryukemeister <sahalrajiv6900@gmail.com>
2024-09-25 01:56:16 +0530

    use data from useMe query instead of useSession

--- packages/features/users/components/UserTable/InviteMemberModal.tsx
+++ packages/features/users/components/UserTable/InviteMemberModal.tsx
@@ -1,9 +1,9 @@
-import { useSession } from "next-auth/react";
 import type { Dispatch } from "react";
 
 import MemberInvitationModal from "@calcom/features/ee/teams/components/MemberInvitationModal";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { trpc } from "@calcom/trpc";
+import useMeQuery from "@calcom/trpc/react/hooks/useMeQuery";
 import { showToast } from "@calcom/ui";
 
 import type { Action } from "./UserListTable";
@@ -13,9 +13,9 @@ interface Props {
 }
 
 export function InviteMemberModal(props: Props) {
-  const { data: session } = useSession();
   const utils = trpc.useUtils();
   const { t, i18n } = useLocale();
+  const { data: userData } = useMeQuery();
   const inviteMemberMutation = trpc.viewer.teams.inviteMember.useMutation({
     async onSuccess(data) {
       props.dispatch({ type: "CLOSE_MODAL" });
@@ -45,9 +45,9 @@ export function InviteMemberModal(props: Props) {
     },
   });
 
-  if (!session?.user.org?.id) return null;
+  const orgId = userData?.organizationId ?? userData?.profiles[0].organizationId;
 
-  const orgId = session.user.org.id;
+  if (!orgId) return null;
 
   return (
     <MemberInvitationModal
