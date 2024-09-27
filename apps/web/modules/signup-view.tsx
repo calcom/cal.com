e805c0f00ae77d8fc4b02255311f9dc7bfcda8c9
Benny Joo <sldisek783@gmail.com>
2024-09-27 14:43:30 -0400

    fix spacing in signup (#16850)

--- apps/web/modules/signup-view.tsx
+++ apps/web/modules/signup-view.tsx
@@ -335,7 +335,7 @@ export default function Signup({
         <div className="bg-muted 2xl:border-subtle grid w-full max-w-[1440px] grid-cols-1 grid-rows-1 overflow-hidden lg:grid-cols-2 2xl:rounded-[20px] 2xl:border 2xl:py-6">
           <HeadSeo title={t("sign_up")} description={t("sign_up")} />
           {/* Left side */}
-          <div className="ml-auto mr-auto mt-0 flex w-full max-w-xl flex-col px-4 pt-6 sm:px-16 md:px-20 lg:mt-12 2xl:px-28">
+          <div className="ml-auto mr-auto mt-0 flex w-full max-w-xl flex-col px-4 pt-6 sm:px-16 md:px-20 lg:mt-24 2xl:px-28">
             {displayBackButton && (
               <div className="flex w-fit lg:-mt-12">
                 <Button
@@ -504,7 +504,7 @@ export default function Signup({
               </div>
             )}
             {!displayEmailForm && (
-              <div className="mt-24">
+              <div className="mt-12">
                 {/* Upper Row */}
                 <div className="mt-6 flex flex-col gap-2 md:flex-row">
                   {isGoogleLoginEnabled ? (
@@ -595,7 +595,7 @@ export default function Signup({
                     {t("sign_in")}
                   </Link>
                 </div>
-                <div className="text-subtle ">
+                <div className="text-subtle">
                   <Trans
                     i18nKey="signing_up_terms"
                     components={[
