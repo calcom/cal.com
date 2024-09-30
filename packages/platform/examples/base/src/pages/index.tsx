1d203dc5665844713a8f6c85525dc9003ed73375
Rajiv Sahal <sahalrajiv-extc@atharvacoe.ac.in>
2024-09-30 17:03:18 +0530

    feat: frontend to handle multiple calendars for connect atoms (#16527)
    * handle multiple calendars in connect atoms
    
    * resolve merge conflicts
    
    * fixup
    
    * frontend for multiple calendars
    
    * fixup
    
    * remove comment
    
    * add missing props
    
    * add prop for multi calendar view
    
    * add multi calendar support
    
    * fixup

--- packages/platform/examples/base/src/pages/index.tsx
+++ packages/platform/examples/base/src/pages/index.tsx
@@ -25,17 +25,21 @@ export default function Home(props: { calUsername: string; calEmail: string }) {
               className="h-[40px] bg-gradient-to-r from-[#8A2387] via-[#E94057] to-[#F27121] text-center text-base font-semibold text-transparent text-white hover:bg-orange-700"
             />
             <Connect.OutlookCalendar
+              isMultiCalendar={true}
               redir="http://localhost:4321/calendars"
               className="h-[40px] bg-gradient-to-r from-[#8A2387] via-[#E94057] to-[#F27121] text-center text-base font-semibold text-transparent text-white hover:bg-orange-700"
             />
-            <Connect.AppleCalendar className="h-[40px] bg-gradient-to-r from-[#8A2387] via-[#E94057] to-[#F27121] text-center text-base font-semibold text-transparent text-white hover:bg-orange-700" />
+            <Connect.AppleCalendar
+              isMultiCalendar={true}
+              className="h-[40px] bg-gradient-to-r from-[#8A2387] via-[#E94057] to-[#F27121] text-center text-base font-semibold text-transparent text-white hover:bg-orange-700"
+            />
           </div>
         </div>
         <div className="hidden lg:block">
           <img
             width="76%"
             height="76%"
-            className=" rounded-lg shadow-2xl"
+            className="rounded-lg shadow-2xl"
             alt="cover image"
             src="https://images.unsplash.com/photo-1506784365847-bbad939e9335?q=80&w=2668&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           />
