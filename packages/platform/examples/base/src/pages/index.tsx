700a583a04e96c2dcb47276066b0df834bc15b57
Ryukemeister <sahalrajiv6900@gmail.com>
2024-09-30 18:09:50 +0530

    resolve merge conflicts

--- packages/platform/examples/base/src/pages/index.tsx
+++ packages/platform/examples/base/src/pages/index.tsx
@@ -25,10 +25,14 @@ export default function Home(props: { calUsername: string; calEmail: string }) {
               className="h-[40px] bg-gradient-to-r from-[#8A2387] via-[#E94057] to-[#F27121] text-center text-base font-semibold text-transparent text-white hover:bg-orange-700"
             />
             <Connect.OutlookCalendar
+              isMultiCalendar={true}
               redir="http://localhost:4321/calendars"
               className="h-[40px] bg-gradient-to-r from-[#8A2387] via-[#E94057] to-[#F27121] text-center text-base font-semibold text-transparent text-white hover:bg-orange-700"
             />
-            <Connect.AppleCalendar className="h-[40px] bg-gradient-to-r from-[#8A2387] via-[#E94057] to-[#F27121] text-center text-base font-semibold text-transparent text-white hover:bg-orange-700" />
+            <Connect.AppleCalendar
+              isMultiCalendar={true}
+              className="h-[40px] bg-gradient-to-r from-[#8A2387] via-[#E94057] to-[#F27121] text-center text-base font-semibold text-transparent text-white hover:bg-orange-700"
+            />
             <StripeConnect
               className="h-[40px] bg-gradient-to-r from-[#E94057] via-[#E94057] to-[#E94057] text-center text-base font-semibold text-transparent text-white hover:bg-orange-700"
               errorRedir="http://localhost:4321/availability"
