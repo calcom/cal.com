492d8a51da56b37cfbfe2f74cc7ead5339964dd7
Alex van Andel <me@alexvanandel.com>
2024-10-01 17:08:13 +0100

    Remove Sentry from direct import in instrumentation file

--- apps/web/instrumentation.ts
+++ apps/web/instrumentation.ts
@@ -1,15 +1,9 @@
-import * as Sentry from "@sentry/nextjs";
-
-export function register() {
+export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    Sentry.init({
-      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-    });
+    await import("./sentry.server.config");
   }
 
   if (process.env.NEXT_RUNTIME === "edge") {
-    Sentry.init({
-      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-    });
+    await import("./sentry.edge.config");
   }
 }
