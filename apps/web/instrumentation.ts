0ca2d0b6fd5f17ee20bcc2c788a99cb88c24a034
Alex van Andel <me@alexvanandel.com>
2024-10-01 17:12:43 +0100

    chore: Exclude Sentry configs when not needed

--- apps/web/instrumentation.ts
+++ apps/web/instrumentation.ts
@@ -1,9 +1,9 @@
 export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
+  if (process.env.NEXT_PUBLIC_SENTRY_DSN && process.env.NEXT_RUNTIME === "nodejs") {
     await import("./sentry.server.config");
   }
 
-  if (process.env.NEXT_RUNTIME === "edge") {
+  if (process.env.NEXT_PUBLIC_SENTRY_DSN && process.env.NEXT_RUNTIME === "edge") {
     await import("./sentry.edge.config");
   }
 }
