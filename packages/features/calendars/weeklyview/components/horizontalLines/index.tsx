9363c54b7eeca65437159b851038727d2617027c
Rohit Funde <rohit@ventii.jp>
2024-10-01 17:08:01 +0900

    Update index.tsx

--- packages/features/calendars/weeklyview/components/horizontalLines/index.tsx
+++ packages/features/calendars/weeklyview/components/horizontalLines/index.tsx
@@ -13,27 +13,25 @@ export const HorizontalLines = ({
   containerOffsetRef: React.RefObject<HTMLDivElement>;
 }) => {
   const { timeFormat } = useTimePreferences();
-  // We need to force the minute to zero, because otherwise in ex GMT+5.5, it would show :30 minute times (but at the positino of :00)
   const finalHour = hours[hours.length - 1].add(1, "hour").minute(0).format(timeFormat);
   const id = useId();
 
   return (
     <div
-      className=" divide-default pointer-events-none relative z-[60] col-start-1 col-end-2 row-start-1 grid divide-y"
+      className="divide-default pointer-events-none relative z-[60] col-start-1 col-end-2 row-start-1 grid divide-y"
       style={{
         gridTemplateRows: `repeat(${hours.length}, minmax(var(--gridDefaultSize),1fr)`,
       }}>
-      <div className="row-end-1 h-[--calendar-offset-top] " ref={containerOffsetRef} />
+      <div className="row-end-1 h-[--calendar-offset-top]" ref={containerOffsetRef} />
       {hours.map((hour) => (
-        <div key={`${id}-${hour.get("hour")}`}>
-          <div className="text-muted sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 rtl:-mr-14">
-            {/* We need to force the minute to zero, because otherwise in ex GMT+5.5, it would show :30 minute times (but at the positino of :00) */}
+        <div key={`${id}-${hour.get("hour")}`} className="relative">
+          <div className="text-muted absolute left-0 top-0 z-20 -ml-14 flex h-full w-14 items-center justify-end pr-2 text-xs leading-5 rtl:-mr-14">
             {hour.minute(0).format(timeFormat)}
           </div>
         </div>
       ))}
-      <div key={`${id}-${finalHour}`}>
-        <div className="text-muted sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 rtl:-mr-14">
+      <div key={`${id}-${finalHour}`} className="relative">
+        <div className="text-muted absolute left-0 top-0 z-20 -ml-14 flex h-full w-14 items-center justify-end pr-2 text-xs leading-5 rtl:-mr-14">
           {finalHour}
         </div>
       </div>
