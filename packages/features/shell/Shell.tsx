f13c3f4f5feb1f2794d0cd899579e3b5d3d67825
sean-brydon <sean@cal.com>
2024-10-01 11:14:56 +0100

    Navigation Item

--- packages/features/shell/Shell.tsx
+++ packages/features/shell/Shell.tsx
@@ -13,7 +13,6 @@ import { useRedirectToOnboardingIfNeeded } from "@calcom/features/auth/lib/hooks
 import UnconfirmedBookingBadge from "@calcom/features/bookings/UnconfirmedBookingBadge";
 import { getOrgFullOrigin } from "@calcom/features/ee/organizations/lib/orgDomains";
 import { useBootIntercom } from "@calcom/features/ee/support/lib/intercom/useIntercom";
-import { useFlagMap } from "@calcom/features/flags/context/provider";
 import { KBarContent, KBarRoot, KBarTrigger } from "@calcom/features/kbar/Kbar";
 import TimezoneChangeDialog from "@calcom/features/settings/TimezoneChangeDialog";
 import classNames from "@calcom/lib/classNames";
@@ -29,7 +28,6 @@ import { useCopy } from "@calcom/lib/hooks/useCopy";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { ButtonState, useNotifications } from "@calcom/lib/hooks/useNotifications";
 import { useRefreshData } from "@calcom/lib/hooks/useRefreshData";
-import { isKeyInObject } from "@calcom/lib/isKeyInObject";
 import { trpc } from "@calcom/trpc/react";
 import {
   Avatar,
@@ -56,6 +54,7 @@ import { useOrgBranding } from "../ee/organizations/context/provider";
 import { TeamInviteBadge } from "./TeamInviteBadge";
 import { BannerContainer } from "./banners/LayoutBanner";
 import { useBanners } from "./banners/useBanners";
+import { NavigationItem, MobileNavigationItem, MobileNavigationMoreItem } from "./navigation/NavigationItem";
 import { useAppTheme } from "./useAppTheme";
 import { UserDropdown } from "./user-dropdown/UserDropdown";
 
@@ -350,81 +349,6 @@ const Navigation = ({ isPlatformNavigation = false }: { isPlatformNavigation?: b
   );
 };
 
-function useShouldDisplayNavigationItem(item: NavigationItemType) {
-  const flags = useFlagMap();
-  if (isKeyInObject(item.name, flags)) return flags[item.name];
-  return true;
-}
-
-const defaultIsCurrent: NavigationItemType["isCurrent"] = ({ isChild, item, pathname }) => {
-  return isChild ? item.href === pathname : item.href ? pathname?.startsWith(item.href) ?? false : false;
-};
-
-const NavigationItem: React.FC<{
-  index?: number;
-  item: NavigationItemType;
-  isChild?: boolean;
-}> = (props) => {
-  const { item, isChild } = props;
-  const { t, isLocaleReady } = useLocale();
-  const pathname = usePathname();
-  const isCurrent: NavigationItemType["isCurrent"] = item.isCurrent || defaultIsCurrent;
-  const current = isCurrent({ isChild: !!isChild, item, pathname });
-  const shouldDisplayNavigationItem = useShouldDisplayNavigationItem(props.item);
-
-  if (!shouldDisplayNavigationItem) return null;
-
-  return (
-    <Fragment>
-      <Tooltip side="right" content={t(item.name)} className="lg:hidden">
-        <Link
-          data-test-id={item.name}
-          href={item.href}
-          aria-label={t(item.name)}
-          target={item.target}
-          className={classNames(
-            "todesktop:py-[7px] text-default group flex items-center rounded-md px-2 py-1.5 text-sm font-medium transition",
-            item.child ? `[&[aria-current='page']]:!bg-transparent` : `[&[aria-current='page']]:bg-emphasis`,
-            isChild
-              ? `[&[aria-current='page']]:text-emphasis [&[aria-current='page']]:bg-emphasis hidden h-8 pl-16 lg:flex lg:pl-11 ${
-                  props.index === 0 ? "mt-0" : "mt-px"
-                }`
-              : "[&[aria-current='page']]:text-emphasis mt-0.5 text-sm",
-            isLocaleReady
-              ? "hover:bg-subtle todesktop:[&[aria-current='page']]:bg-emphasis todesktop:hover:bg-transparent hover:text-emphasis"
-              : ""
-          )}
-          aria-current={current ? "page" : undefined}>
-          {item.icon && (
-            <Icon
-              name={item.isLoading ? "rotate-cw" : item.icon}
-              className={classNames(
-                "todesktop:!text-blue-500 mr-2 h-4 w-4 flex-shrink-0 rtl:ml-2 md:ltr:mx-auto lg:ltr:mr-2 [&[aria-current='page']]:text-inherit",
-                item.isLoading && "animate-spin"
-              )}
-              aria-hidden="true"
-              aria-current={current ? "page" : undefined}
-            />
-          )}
-          {isLocaleReady ? (
-            <span
-              className="hidden w-full justify-between truncate text-ellipsis lg:flex"
-              data-testid={`${item.name}-test`}>
-              {t(item.name)}
-              {item.badge && item.badge}
-            </span>
-          ) : (
-            <SkeletonText className="h-[20px] w-full" />
-          )}
-        </Link>
-      </Tooltip>
-      {item.child &&
-        isCurrent({ pathname, isChild, item }) &&
-        item.child.map((item, index) => <NavigationItem index={index} key={item.name} item={item} isChild />)}
-    </Fragment>
-  );
-};
-
 function MobileNavigationContainer({ isPlatformNavigation = false }: { isPlatformNavigation?: boolean }) {
   const { status } = useSession();
   if (status !== "authenticated") return null;
@@ -449,64 +373,6 @@ const MobileNavigation = ({ isPlatformNavigation = false }: { isPlatformNavigati
       {/* add padding to content for mobile navigation*/}
       <div className="block pt-12 md:hidden" />
     </>
-  );
-};
-
-const MobileNavigationItem: React.FC<{
-  item: NavigationItemType;
-  isChild?: boolean;
-}> = (props) => {
-  const { item, isChild } = props;
-  const pathname = usePathname();
-  const { t, isLocaleReady } = useLocale();
-  const isCurrent: NavigationItemType["isCurrent"] = item.isCurrent || defaultIsCurrent;
-  const current = isCurrent({ isChild: !!isChild, item, pathname });
-  const shouldDisplayNavigationItem = useShouldDisplayNavigationItem(props.item);
-
-  if (!shouldDisplayNavigationItem) return null;
-  return (
-    <Link
-      key={item.name}
-      href={item.href}
-      target={item.target}
-      className="[&[aria-current='page']]:text-emphasis hover:text-default text-muted relative my-2 min-w-0 flex-1 overflow-hidden rounded-md !bg-transparent p-1 text-center text-xs font-medium focus:z-10 sm:text-sm"
-      aria-current={current ? "page" : undefined}>
-      {item.badge && <div className="absolute right-1 top-1">{item.badge}</div>}
-      {item.icon && (
-        <Icon
-          name={item.icon}
-          className="[&[aria-current='page']]:text-emphasis  mx-auto mb-1 block h-5 w-5 flex-shrink-0 text-center text-inherit"
-          aria-hidden="true"
-          aria-current={current ? "page" : undefined}
-        />
-      )}
-      {isLocaleReady ? <span className="block truncate">{t(item.name)}</span> : <SkeletonText />}
-    </Link>
-  );
-};
-
-const MobileNavigationMoreItem: React.FC<{
-  item: NavigationItemType;
-  isChild?: boolean;
-}> = (props) => {
-  const { item } = props;
-  const { t, isLocaleReady } = useLocale();
-  const shouldDisplayNavigationItem = useShouldDisplayNavigationItem(props.item);
-
-  if (!shouldDisplayNavigationItem) return null;
-
-  return (
-    <li className="border-subtle border-b last:border-b-0" key={item.name}>
-      <Link href={item.href} className="hover:bg-subtle flex items-center justify-between p-5 transition">
-        <span className="text-default flex items-center font-semibold ">
-          {item.icon && (
-            <Icon name={item.icon} className="h-5 w-5 flex-shrink-0 ltr:mr-3 rtl:ml-3" aria-hidden="true" />
-          )}
-          {isLocaleReady ? t(item.name) : <SkeletonText />}
-        </span>
-        <Icon name="arrow-right" className="text-subtle h-5 w-5" />
-      </Link>
-    </li>
   );
 };
 
