282bf48953ebfc8c8c840198057aab29eb87d144
CarinaWolli <wollencarina@gmail.com>
2024-10-03 13:04:33 -0400

    update migration

--- packages/prisma/migrations/20241003170314_add_sms_credit_limits/migration.sql
+++ packages/prisma/migrations/20241003170314_add_sms_credit_limits/migration.sql
@@ -6,20 +6,11 @@ DROP INDEX "WorkflowReminder_cancelled_scheduledDate_idx";
 
 -- AlterTable
 ALTER TABLE "Team" ADD COLUMN     "smsCreditAllocationType" "SmsCreditAllocationType" NOT NULL DEFAULT 'SPECIFIC',
-ADD COLUMN     "smsCreditAllocationValue" INTEGER DEFAULT 50;
+ADD COLUMN     "smsCreditAllocationValue" INTEGER DEFAULT 50,
+ADD COLUMN     "smsOverageLimit" INTEGER NOT NULL DEFAULT 0;
 
 -- AlterTable
 ALTER TABLE "WorkflowReminder" ADD COLUMN     "smsCredits" INTEGER;
-
--- CreateTable
-CREATE TABLE "SmsCountryCredits" (
-    "id" TEXT NOT NULL,
-    "iso" TEXT NOT NULL,
-    "country" TEXT NOT NULL,
-    "credits" INTEGER NOT NULL DEFAULT 0,
-
-    CONSTRAINT "SmsCountryCredits_pkey" PRIMARY KEY ("id")
-);
 
 -- CreateTable
 CREATE TABLE "SmsCreditCount" (
@@ -28,14 +19,12 @@ CREATE TABLE "SmsCreditCount" (
     "teamId" INTEGER NOT NULL,
     "credits" INTEGER NOT NULL DEFAULT 0,
     "month" TIMESTAMP(3) NOT NULL,
+    "overageCharges" INTEGER NOT NULL DEFAULT 0,
     "limitReached" BOOLEAN,
     "warningSent" BOOLEAN,
 
     CONSTRAINT "SmsCreditCount_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "SmsCountryCredits_id_key" ON "SmsCountryCredits"("id");
 
 -- AddForeignKey
 ALTER TABLE "SmsCreditCount" ADD CONSTRAINT "SmsCreditCount_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
