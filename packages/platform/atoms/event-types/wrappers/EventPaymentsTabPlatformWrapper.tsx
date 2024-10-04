a33b418827d7adc4a86deb763194a4e5b7679912
Ryukemeister <sahalrajiv6900@gmail.com>
2024-10-04 22:40:41 +0530

    add stripe form

--- packages/platform/atoms/event-types/wrappers/EventPaymentsTabPlatformWrapper.tsx
+++ packages/platform/atoms/event-types/wrappers/EventPaymentsTabPlatformWrapper.tsx
@@ -1,12 +1,51 @@
+import { useState } from "react";
 import { useFormContext } from "react-hook-form";
 
+import { useAppContextWithSchema } from "@calcom/app-store/EventTypeAppContext";
 import type { FormValues } from "@calcom/features/eventtypes/lib/types";
+import type { EventTypeSetupProps } from "@calcom/features/eventtypes/lib/types";
+import { Switch } from "@calcom/ui";
+import EventTypeAppSettingsWrapper from "@calcom/web/components/apps/installation/EventTypeAppSettingsWrapper";
 
-const EventPaymentsTabPlatformWrapper = () => {
+import { StripeConnect } from "../../connect/stripe/StripeConnect";
+
+const EventPaymentsTabPlatformWrapper = ({ eventType }: { eventType: EventTypeSetupProps["eventType"] }) => {
   const formMethods = useFormContext<FormValues>();
-  formMethods.getValues("metadata.apps.stripe.enabled");
+  const enabledStripe = formMethods.getValues("metadata.apps.stripe.enabled");
+  const [isSwitchActive, setIsSwitchActive] = useState(enabledStripe ?? false);
+  const { LockedIcon } = useAppContextWithSchema();
 
-  return <div>This is the event payments tab to be used for platform!</div>;
+  return (
+    <div>
+      This is the event payments tab to be used for platform!
+      <div>
+        <StripeConnect label="Add" redir={window.location.href} />
+      </div>
+      <div className="flex w-[100%] items-center justify-between gap-5">
+        <div>
+          <h1> Stripe app switch</h1>
+        </div>
+        <div>
+          <Switch
+            LockedIcon={LockedIcon}
+            checked={isSwitchActive}
+            onCheckedChange={(enabled) => {
+              setIsSwitchActive(enabled);
+              formMethods.setValue("metadata.apps.stripe.enabled", enabled, { shouldDirty: true });
+            }}
+            data-testid="stripe-app-switch"
+          />
+        </div>
+      </div>
+      <EventTypeAppSettingsWrapper
+        eventType={eventType}
+        slug="stripe"
+        credentialId={19}
+        categories={["payment"]}
+        userName="ifxofushkq-cm1oxillv0001sbtzkm3ksqdt-example"
+      />
+    </div>
+  );
 };
 
 export default EventPaymentsTabPlatformWrapper;
