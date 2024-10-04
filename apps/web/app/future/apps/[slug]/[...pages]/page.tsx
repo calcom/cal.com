1ff5b712d570e13c6d2fe4398a7caa33ee1202d0
Benny Joo <sldisek783@gmail.com>
2024-10-03 22:39:23 -0400

    fix

--- apps/web/app/future/apps/[slug]/[...pages]/page.tsx
+++ apps/web/app/future/apps/[slug]/[...pages]/page.tsx
@@ -1,16 +1,38 @@
 import { withAppDirSsr } from "app/WithAppDirSsr";
+import { PageProps } from "app/_types";
 import { _generateMetadata } from "app/_utils";
 import { WithLayout } from "app/layoutHOC";
 import type { GetServerSidePropsResult } from "next";
+import { cookies, headers } from "next/headers";
+import { notFound } from "next/navigation";
+import z from "zod";
 
 import { getServerSideProps } from "@lib/apps/[slug]/[...pages]/getServerSideProps";
+import { buildLegacyCtx } from "@lib/buildLegacyCtx";
 
 import LegacyPage, { getLayout } from "~/apps/[slug]/[...pages]/pages-view";
 
-export const generateMetadata = async () => {
+const paramsSchema = z.object({
+  slug: z.string(),
+  pages: z.array(z.string()),
+});
+
+export const generateMetadata = async ({ params, searchParams }: PageProps) => {
+  const p = paramsSchema.safeParse(params);
+
+  if (!p.success) {
+    return notFound();
+  }
+
+  const legacyContext = buildLegacyCtx(headers(), cookies(), params, searchParams);
+  const data = await getData(legacyContext);
+  const form = "form" in data ? (data.form as { name?: string; description?: string }) : null;
+  const formName = form?.name;
+  const formDescription = form?.description;
+
   return await _generateMetadata(
-    (t) => t("routing_forms"),
-    (t) => t("routing_forms_description")
+    (t) => formName ?? t("routing_forms"),
+    (t) => formDescription ?? t("routing_forms_description")
   );
 };
 
