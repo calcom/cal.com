bd09e62c76ad110fc39aeff76aa5e2fde0221007
Felippe Fernandes <lfelippeeng@gmail.com>
2024-10-05 10:32:29 -0300

    Fix supabase route

--- apps/api/v2/src/app.controller.ts
+++ apps/api/v2/src/app.controller.ts
@@ -1,14 +1,33 @@
-import { getEnv } from "@/env";
-import { Controller, Get, Version, VERSION_NEUTRAL } from "@nestjs/common";
-import { ApiTags as DocsTags, ApiExcludeController as DocsExcludeController } from "@nestjs/swagger";
+import { ApiAuthGuard } from "@/modules/auth/guards/api-auth/api-auth.guard";
+import { Controller, Get, Param, UseGuards, Version, VERSION_NEUTRAL } from "@nestjs/common";
+
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY!;
+const SUPABASE_URL = process.env.SUPABASE_ANON_KEY!;
 
 @Controller()
-@DocsTags("Health - development only")
-@DocsExcludeController(getEnv("NODE_ENV") === "production")
 export class AppController {
   @Get("health")
   @Version(VERSION_NEUTRAL)
   getHealth(): "OK" {
     return "OK";
+  }
+
+  @Get("/supabase")
+  @UseGuards(ApiAuthGuard)
+  @Version(VERSION_NEUTRAL)
+  async getSupabase(@Param("scope") scope: string, @Param("eq") eq: string): Promise<any> {
+    const supabaseResponse = await fetch(`${SUPABASE_URL}?scope=${scope}&eq=${eq}`, {
+      headers: {
+        apikey: SUPABASE_ANON_KEY,
+      },
+    });
+
+    return await supabaseResponse.json();
+  }
+
+  @Get()
+  @Version(VERSION_NEUTRAL)
+  getRoot(): string {
+    return `{"message":"Welcome to Cal.com API V2 - docs are at https://developer.cal.com/api}`;
   }
 }
