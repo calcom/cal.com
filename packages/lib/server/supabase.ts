72cf65109617847ce836a8db65128f9c98e17a8b
Felippe Fernandes <lfelippeeng@gmail.com>
2024-10-06 18:49:33 -0300

    Revert "Fix supabase route"
    This reverts commit 99faa5abe3537ac22113e3739ae9007a039050c5.

--- packages/lib/server/supabase.ts
+++ packages/lib/server/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = process.env.SUPABASE_BASE_URL!;
-const SUPABASE_KEY = process.env.SUPABASE_ANON_KEY!;
+const SUPABASE_URL = process.env.SUPABASE_ANON_KEY!;
+const SUPABASE_KEY = process.env.SUPABASE_BASE_URL!;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
