0e61dc9aa7cd43dd22bf56bab29949f8f877bdac
Benny Joo <sldisek783@gmail.com>
2024-10-07 15:51:17 -0400

    chore: apps/installed remove pages router

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -64,7 +64,7 @@ const middleware = async (req: NextRequest): Promise<NextResponse<unknown>> => {
     requestHeaders.set("x-csp-enforce", "true");
   }
 
-  if (url.pathname.startsWith("/future/apps/installed")) {
+  if (url.pathname.startsWith("/apps/installed")) {
     const returnTo = req.cookies.get("return-to")?.value;
     if (returnTo !== undefined) {
       requestHeaders.set("Set-Cookie", "return-to=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT");
@@ -169,7 +169,6 @@ export const config = {
     "/settings/admin/:path*",
     "/future/settings/admin/:path*",
     "/apps/installed/:category/",
-    "/future/apps/installed/:category/",
     "/apps/:slug/",
     "/future/apps/:slug/",
     "/apps/:slug/setup/",
