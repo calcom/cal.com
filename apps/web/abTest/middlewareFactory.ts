1a60a51333ff9cf7a61cefa91911018fbf588b4b
Benny Joo <sldisek783@gmail.com>
2024-10-07 15:57:45 -0400

    chore: apps main page remove pages router

--- apps/web/abTest/middlewareFactory.ts
+++ apps/web/abTest/middlewareFactory.ts
@@ -28,7 +28,6 @@ const ROUTES: [URLPattern, boolean][] = [
   ["/workflows/:path*", process.env.APP_ROUTER_WORKFLOWS_ENABLED === "1"] as const,
   ["/settings/teams/:path*", process.env.APP_ROUTER_SETTINGS_TEAMS_ENABLED === "1"] as const,
   ["/getting-started/:step", process.env.APP_ROUTER_GETTING_STARTED_STEP_ENABLED === "1"] as const,
-  ["/apps", process.env.APP_ROUTER_APPS_ENABLED === "1"] as const,
   ["/bookings/:status", process.env.APP_ROUTER_BOOKINGS_STATUS_ENABLED === "1"] as const,
   ["/booking/:path*", process.env.APP_ROUTER_BOOKING_ENABLED === "1"] as const,
   ["/video/:path*", process.env.APP_ROUTER_VIDEO_ENABLED === "1"] as const,
