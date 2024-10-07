671bf5c4bbe130ce82c4e389088da3d437af4aab
Ryukemeister <sahalrajiv6900@gmail.com>
2024-10-07 14:20:54 +0530

    testing EventTypeAppCard component

--- packages/platform/atoms/event-types/wrappers/EventPaymentsTabPlatformWrapper.tsx
+++ packages/platform/atoms/event-types/wrappers/EventPaymentsTabPlatformWrapper.tsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 import { useFormContext } from "react-hook-form";
 
 import { useAppContextWithSchema } from "@calcom/app-store/EventTypeAppContext";
+import { EventTypeAppCard } from "@calcom/app-store/_components/EventTypeAppCardInterface";
+import type { EventTypeAppsList } from "@calcom/app-store/utils";
 import type { FormValues } from "@calcom/features/eventtypes/lib/types";
 import type { EventTypeSetupProps } from "@calcom/features/eventtypes/lib/types";
-import { Switch } from "@calcom/ui";
-import EventTypeAppSettingsWrapper from "@calcom/web/components/apps/installation/EventTypeAppSettingsWrapper";
+import useAppsData from "@calcom/lib/hooks/useAppsData";
 
+//import { Switch } from "@calcom/ui";
+// import EventTypeAppSettingsWrapper from "@calcom/web/components/apps/installation/EventTypeAppSettingsWrapper";
 import { StripeConnect } from "../../connect/stripe/StripeConnect";
 
 const EventPaymentsTabPlatformWrapper = ({ eventType }: { eventType: EventTypeSetupProps["eventType"] }) => {
@@ -15,13 +18,44 @@ const EventPaymentsTabPlatformWrapper = ({ eventType }: { eventType: EventTypeSe
   const [isSwitchActive, setIsSwitchActive] = useState(enabledStripe ?? false);
   const { LockedIcon } = useAppContextWithSchema();
 
+  const { getAppDataGetter, getAppDataSetter, eventTypeFormMetadata } = useAppsData();
+  const app = {
+    appData: null,
+    dirName: "stripepayment",
+    __template: "",
+    name: "Stripe",
+    description:
+      "A Saas company a payment processing software, and application programming interfaces for e-commerce websites and mobile applications.",
+    installed: true,
+    slug: "stripe",
+    category: "payment",
+    categories: ["payment"],
+    logo: "/app-store/stripepayment/icon.svg",
+    publisher: "Cal.com",
+    title: "Stripe",
+    type: "stripe_payment",
+    url: "https://cal.com/",
+    docsUrl: "https://stripe.com/docs",
+    variant: "payment",
+    extendsFeature: "EventType",
+    email: "help@cal.com",
+    isOAuth: true,
+    locationOption: null,
+    enabled: true,
+    userCredentialIds: [],
+    invalidCredentialIds: [],
+    teams: [],
+    isInstalled: true,
+    isSetupAlready: true,
+  };
+
   return (
     <div>
       This is the event payments tab to be used for platform!
       <div>
         <StripeConnect label="Add" redir={window.location.href} />
       </div>
-      <div className="flex w-[100%] items-center justify-between gap-5">
+      {/* <div className="flex w-[100%] items-center justify-between gap-5">
         <div>
           <h1> Stripe app switch</h1>
         </div>
@@ -36,13 +70,21 @@ const EventPaymentsTabPlatformWrapper = ({ eventType }: { eventType: EventTypeSe
             data-testid="stripe-app-switch"
           />
         </div>
-      </div>
-      <EventTypeAppSettingsWrapper
+      </div> */}
+      {/* <EventTypeAppSettingsWrapper
         eventType={eventType}
         slug="stripe"
         credentialId={19}
         categories={["payment"]}
         userName="ifxofushkq-cm1oxillv0001sbtzkm3ksqdt-example"
+      /> */}
+      <EventTypeAppCard
+        getAppData={getAppDataGetter(app.slug as EventTypeAppsList)}
+        setAppData={getAppDataSetter(app.slug as EventTypeAppsList, app.categories, app.userCredentialIds[0])}
+        key={app.slug}
+        app={app}
+        eventType={eventType}
+        eventTypeFormMetadata={eventTypeFormMetadata}
       />
     </div>
   );
