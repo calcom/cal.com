8bd4d0fec067913c61023b40cd3c56baa4631e45
Lauris Skraucis <lauris.skraucis@gmail.com>
2024-10-08 11:20:24 +0200

    Update input.service.ts

--- apps/api/v2/src/ee/bookings/2024-08-13/services/input.service.ts
+++ apps/api/v2/src/ee/bookings/2024-08-13/services/input.service.ts
@@ -179,7 +179,7 @@ export class InputBookingsService_2024_08_13 {
     // note(Lauris): timeBetween 0=yearly, 1=monthly and 2=weekly
     const timeBetween = occurrence.freq;
 
-    if (inputBooking.recurrenceCount && inputBooking?.recurrenceCount > occurrence.count) {
+    if (inputBooking.recurrenceCount && inputBooking.recurrenceCount > occurrence.count) {
       throw new BadRequestException(
         "Provided recurrence count is higher than the event type's recurring event count."
       );
