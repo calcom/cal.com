e1a8f4f45c3e09b5416863be9b95dcc9c276f8b0
Keith Williams <keithwillcode@gmail.com>
2024-10-10 12:36:02 -0300

    Just to build

--- apps/web/sentry.server.config.ts
+++ apps/web/sentry.server.config.ts
@@ -2,5 +2,5 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  tracesSampleRate: process.env.SENTRY_TRACES_SAMPLE_RATE ?? 0.0,
+  tracesSampleRate: process.env.SENTRY_TRACES_SAMPLE_RATE ?? 0.0, // Just build please
 });
