ce4d8c95d65a52d8aa4a42be9d60ab1d54f5bf36
zomars <zomars@me.com>
2024-10-14 16:59:46 -0700

    Update emailSchema.ts

--- packages/lib/emailSchema.ts
+++ packages/lib/emailSchema.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 
+/** @see https://github.com/colinhacks/zod/issues/3155#issuecomment-2060045794 */
 export const emailRegex =
   /^(?!\.)(?!.*\.\.)([A-Z0-9_+-\.']*)[A-Z0-9_+'-]@([A-Z0-9][A-Z0-9\-]*\.)+[A-Z]{2,}$/i;
 
