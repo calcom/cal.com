a48d190af3f565dd9bcbdaf7b4d60c4afe4af3f0
Vijay <vijayraghav22@gmail.com>
2024-10-15 22:06:00 +0530

    feat: #15416 Expand round robin availability beyond default schedule (#15618)
    * add feature to change host schedule
    
    * update for type-check:ci
    
    * Update SingleForm.tsx for linterr
    
    * update as per design review
    
    * update for rerendering
    
    * migration files
    
    * On Delete set null for schedule
    
    if schedule is deleted, no need to delete the host
    
    * updated schema
    
    * added testcases
    
    * checking permissions before
    
    * updated for comments
    
    * update for initial value
    
    * updated and cleanup
    
    * set initial value
    
    * update after merge
    
    * updated nit style
    
    * update for console warning and added comments
    
    * nit/chore
    
    * chore
    
    * update for typechecks
    
    * update for scheduleId param after merge
    
    * update after merge or new changes in metadata.config
    
    * update after weights functionality
    
    * updated description text and column name as per review suggestion
    
    * resolved merge conflicts
    
    * fix typecheck due to changes in getScheduleSchema
    
    * update after merge having refactor
    
    * update refactored EventTypeWebWrapper after merge
    
    * updated props
    
    * updated trpc err code and added log
    
    * update due to refactor in EventAvailabilityTab
    
    * perf: Optimise TS type + select only the data that's needed for a specific function
    
    ---------
    
    Co-authored-by: Amit Sharma <74371312+Amit91848@users.noreply.github.com>
    Co-authored-by: Syed Ali Shahbaz <52925846+alishaz-polymath@users.noreply.github.com>
    Co-authored-by: Alex van Andel <me@alexvanandel.com>

--- packages/app-store/routing-forms/lib/getSerializableForm.ts
+++ packages/app-store/routing-forms/lib/getSerializableForm.ts
@@ -81,6 +81,7 @@ export async function getSerializableForm<TForm extends App_RoutingForms_Form>({
         name: true,
         email: true,
         avatarUrl: true,
+        defaultScheduleId: true,
       },
     });
   }
