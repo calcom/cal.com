2920f1ce4942d815633ff0b92a4876aca91ec90b
Keith Williams <keithwillcode@gmail.com>
2024-10-20 15:43:49 -0300

    Removed the extra integrations tracing config

--- apps/web/sentry.server.config.ts
+++ apps/web/sentry.server.config.ts
@@ -1,11 +1,6 @@
 import * as Sentry from "@sentry/nextjs";
-import { Integrations } from "@sentry/tracing";
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   tracesSampleRate: process.env.SENTRY_TRACES_SAMPLE_RATE ?? 0.0,
-  integrations: [
-    new Integrations.Postgres(), // Optional: For Postgres performance tracing
-    new Integrations.Http({ tracing: true }), // For HTTP performance tracing
-  ],
 });
