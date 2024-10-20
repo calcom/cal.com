3c9cdc103a3207b197aa60cb250160531ea48f60
Keith Williams <keithwillcode@gmail.com>
2024-10-20 11:30:33 -0300

    perf: Add Sentry perf tracing

--- apps/web/sentry.server.config.ts
+++ apps/web/sentry.server.config.ts
@@ -1,6 +1,11 @@
 import * as Sentry from "@sentry/nextjs";
+import { Integrations } from "@sentry/tracing";
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  tracesSampleRate: process.env.SENTRY_TRACES_SAMPLE_RATE ?? 0.0, // Just build please
+  tracesSampleRate: process.env.SENTRY_TRACES_SAMPLE_RATE ?? 0.0,
+  integrations: [
+    new Integrations.Postgres(), // Optional: For Postgres performance tracing
+    new Integrations.Http({ tracing: true }), // For HTTP performance tracing
+  ],
 });
