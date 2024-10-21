c11b66bc1cdba6ca74b60d5b0612547ebca03e36
Morgan Vernay <morgan@cal.com>
2024-10-21 10:21:17 +0300

    chore: better request/response logging apiv2

--- apps/api/v2/src/middleware/request-ids/request-id.middleware.ts
+++ apps/api/v2/src/middleware/request-ids/request-id.middleware.ts
@@ -1,11 +1,28 @@
-import { Injectable, NestMiddleware } from "@nestjs/common";
+import { Injectable, NestMiddleware, Logger } from "@nestjs/common";
 import { Request, Response, NextFunction } from "express";
 import { v4 as uuid } from "uuid";
 
 @Injectable()
 export class RequestIdMiddleware implements NestMiddleware {
+  private readonly logger = new Logger("RequestIdMiddleware - NestMiddleware");
+
   use(req: Request, res: Response, next: NextFunction) {
-    req.headers["X-Request-Id"] = uuid();
+    const requestId = uuid();
+    req.headers["X-Request-Id"] = requestId;
+    const { method, url, headers, body: requestBody } = req;
+
+    this.logger.log(
+      JSON.stringify({
+        requestId,
+        method,
+        url,
+        headers,
+        requestBody,
+        timestamp: new Date().toISOString(),
+        message: "Incoming Request",
+      })
+    );
+
     next();
   }
 }
