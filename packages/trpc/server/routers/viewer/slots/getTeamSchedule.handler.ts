c79008e0801fde1ea702a5b9e9df33447ac5c3b3
Keith Williams <keithwillcode@gmail.com>
2024-10-23 15:29:11 -0300

    Type fixes

--- packages/trpc/server/routers/viewer/slots/getTeamSchedule.handler.ts
+++ packages/trpc/server/routers/viewer/slots/getTeamSchedule.handler.ts
@@ -1,17 +1,17 @@
 import type { IncomingMessage } from "http";
 
-import type { TGetScheduleInputSchema } from "./getTeamSchedule.schema";
+import type { TGetTeamScheduleInputSchema } from "./getTeamSchedule.schema";
 import { getAvailableSlots } from "./util";
 
-export type GetScheduleOptions = {
+export type GetTeamScheduleOptions = {
   ctx?: ContextForGetSchedule;
-  input: TGetScheduleInputSchema;
+  input: TGetTeamScheduleInputSchema;
 };
 
 interface ContextForGetSchedule extends Record<string, unknown> {
   req?: (IncomingMessage & { cookies: Partial<{ [key: string]: string }> }) | undefined;
 }
 
-export const getTeamScheduleHandler = async ({ ctx, input }: GetScheduleOptions) => {
+export const getTeamScheduleHandler = async ({ ctx, input }: GetTeamScheduleOptions) => {
   return await getAvailableSlots({ ctx, input });
 };
