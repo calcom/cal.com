e6459f86fb3cc90bd6dbdb7c30a69621b9cde147
Ryukemeister <sahalrajiv6900@gmail.com>
2024-10-25 03:41:38 +0530

    update stripe module

--- apps/api/v2/src/modules/stripe/stripe.module.ts
+++ apps/api/v2/src/modules/stripe/stripe.module.ts
@@ -4,13 +4,14 @@ import { PrismaModule } from "@/modules/prisma/prisma.module";
 import { StripeController } from "@/modules/stripe/controllers/stripe.controller";
 import { StripeService } from "@/modules/stripe/stripe.service";
 import { TokensRepository } from "@/modules/tokens/tokens.repository";
+import { UsersRepository } from "@/modules/users/users.repository";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [ConfigModule, PrismaModule],
   exports: [StripeService],
-  providers: [StripeService, AppsRepository, CredentialsRepository, TokensRepository],
+  providers: [StripeService, AppsRepository, CredentialsRepository, TokensRepository, UsersRepository],
   controllers: [StripeController],
 })
 export class StripeModule {}
