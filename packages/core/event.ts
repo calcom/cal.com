80051646c357ec21ba1125a1b4a22d6bf7f499f3
Anik Dhabal Babu <adhabal2002@gmail.com>
2024-10-24 21:57:53 +0000

    add test

--- packages/core/event.ts
+++ packages/core/event.ts
@@ -63,14 +63,14 @@ export function getEventName(eventNameObj: EventNameObjectType, forAttendeeView 
     .replaceAll("{ATTENDEE}", attendeeName)
     .replaceAll("{HOST}", eventNameObj.host)
     .replaceAll("{HOST/ATTENDEE}", forAttendeeView ? eventNameObj.host : attendeeName)
-    .replaceAll("{Event duration}", `${String(eventNameObj.eventDuration)} mins`);
+    .replaceAll("{Event duration}", `${String(eventNameObj.eventDuration)} mins`)
+    .replaceAll(
+      "{Scheduler first name}",
+      attendeeName === eventNameObj.t("scheduler") ? "{Scheduler first name}" : attendeeName.split(" ")[0]
+    );
 
   const { bookingFields } = eventNameObj || {};
   const { name } = bookingFields || {};
-
-  if (bookingFields) {
-    dynamicEventName = dynamicEventName.replaceAll("{Scheduler first name}", attendeeName.split(" ")[0]);
-  }
 
   if (name && typeof name === "object" && !Array.isArray(name) && typeof name.lastName === "string") {
     dynamicEventName = dynamicEventName.replaceAll("{Scheduler last name}", name.lastName.toString());
