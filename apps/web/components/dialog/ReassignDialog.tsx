caab18964396b6c451430538be62e1353ea51d74
sean-brydon <sean@cal.com>
2024-10-24 10:39:27 +0100

    add logic for confirmation modal on unavailable host

--- apps/web/components/dialog/ReassignDialog.tsx
+++ apps/web/components/dialog/ReassignDialog.tsx
@@ -1,6 +1,6 @@
 import { useAutoAnimate } from "@formkit/auto-animate/react";
 import type { Dispatch, SetStateAction } from "react";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useForm } from "react-hook-form";
 import { components } from "react-select";
 
@@ -16,6 +16,7 @@ import {
   DialogClose,
   DialogContent,
   DialogFooter,
+  ConfirmationDialogContent,
   showToast,
   Select,
   RadioGroup as RadioArea,
@@ -99,12 +100,19 @@ export const ReassignDialog = ({ isOpenDialog, setIsOpenDialog, teamId, bookingI
     },
   });
 
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
   const handleSubmit = (values: FormValues) => {
     if (values.reassignType === "round_robin") {
       roundRobinReassignMutation.mutate({ teamId, bookingId });
     } else {
       if (values.teamMemberId) {
-        roundRobinManualReassignMutation.mutate({ bookingId, teamMemberId: values.teamMemberId });
+        const selectedMember = teamMemberOptions.find((member) => member.value === values.teamMemberId);
+        if (selectedMember && selectedMember.status === "unavailable") {
+          setShowConfirmation(true);
+        } else {
+          roundRobinManualReassignMutation.mutate({ bookingId, teamMemberId: values.teamMemberId });
+        }
       }
     }
   };
@@ -112,79 +120,98 @@ export const ReassignDialog = ({ isOpenDialog, setIsOpenDialog, teamId, bookingI
   const watchedReassignType = form.watch("reassignType");
 
   return (
-    <Dialog
-      open={isOpenDialog}
-      onOpenChange={(open) => {
-        setIsOpenDialog(open);
-      }}>
-      <DialogContent
-        title={t("reassign_round_robin_host")}
-        description={t("reassign_to_another_rr_host")}
-        enableOverflow>
-        {/* TODO add team member reassignment*/}
-        <Form form={form} handleSubmit={handleSubmit} ref={animationParentRef}>
-          <RadioArea.Group
-            onValueChange={(val) => {
-              form.setValue("reassignType", val as "team_member" | "round_robin");
-            }}
-            className={classNames("mt-1 flex flex-col gap-4")}>
-            <RadioArea.Item
-              value="round_robin"
-              className={classNames("w-full text-sm")}
-              classNames={{ container: classNames("w-full") }}>
-              <strong className="mb-1 block">{t("round_robin")}</strong>
-              <p>{t("round_robin_reassign_description")}</p>
-            </RadioArea.Item>
-            <RadioArea.Item
-              value="team_member"
-              className={classNames("text-sm")}
-              classNames={{ container: classNames("w-full") }}>
-              <strong className="mb-1 block">{t("team_member_round_robin_reassign")}</strong>
-              <p>{t("team_member_round_robin_reassign_description")}</p>
-            </RadioArea.Item>
-          </RadioArea.Group>
+    <>
+      <Dialog
+        open={isOpenDialog}
+        onOpenChange={(open) => {
+          setIsOpenDialog(open);
+        }}>
+        <DialogContent
+          title={t("reassign_round_robin_host")}
+          description={t("reassign_to_another_rr_host")}
+          enableOverflow>
+          {/* TODO add team member reassignment*/}
+          <Form form={form} handleSubmit={handleSubmit} ref={animationParentRef}>
+            <RadioArea.Group
+              onValueChange={(val) => {
+                form.setValue("reassignType", val as "team_member" | "round_robin");
+              }}
+              className={classNames("mt-1 flex flex-col gap-4")}>
+              <RadioArea.Item
+                value="round_robin"
+                className={classNames("w-full text-sm")}
+                classNames={{ container: classNames("w-full") }}>
+                <strong className="mb-1 block">{t("round_robin")}</strong>
+                <p>{t("round_robin_reassign_description")}</p>
+              </RadioArea.Item>
+              <RadioArea.Item
+                value="team_member"
+                className={classNames("text-sm")}
+                classNames={{ container: classNames("w-full") }}>
+                <strong className="mb-1 block">{t("team_member_round_robin_reassign")}</strong>
+                <p>{t("team_member_round_robin_reassign_description")}</p>
+              </RadioArea.Item>
+            </RadioArea.Group>
 
-          {watchedReassignType === "team_member" && (
-            <div className="mb-2">
-              <Label className="text-emphasis mt-6">{t("select_team_member")}</Label>
-              <Select
-                value={teamMemberOptions?.find((option) => option.value === form.getValues("teamMemberId"))}
-                options={teamMemberOptions}
-                onChange={(event) => {
-                  form.setValue("teamMemberId", event?.value);
-                }}
-                components={{
-                  Option: ({ children, ...props }) => {
-                    const status = props.data?.status;
-                    return (
-                      <components.Option {...props}>
-                        <div className="flex items-center gap-2">
-                          <div
-                            className={classNames(
-                              "h-2 w-2 rounded-full",
-                              status === "available" ? "bg-green-500" : "bg-red-500"
-                            )}
-                          />
-                          {children}
-                        </div>
-                      </components.Option>
-                    );
-                  },
-                }}
-              />
-            </div>
-          )}
-          <DialogFooter>
-            <DialogClose />
-            <Button
-              type="submit"
-              data-testid="rejection-confirm"
-              loading={roundRobinReassignMutation.isPending || roundRobinManualReassignMutation.isPending}>
-              {t("reassign")}
-            </Button>
-          </DialogFooter>
-        </Form>
-      </DialogContent>
-    </Dialog>
+            {watchedReassignType === "team_member" && (
+              <div className="mb-2">
+                <Label className="text-emphasis mt-6">{t("select_team_member")}</Label>
+                <Select
+                  value={teamMemberOptions?.find((option) => option.value === form.getValues("teamMemberId"))}
+                  options={teamMemberOptions}
+                  onChange={(event) => {
+                    form.setValue("teamMemberId", event?.value);
+                  }}
+                  components={{
+                    Option: ({ children, ...props }) => {
+                      const status = props.data?.status;
+                      return (
+                        <components.Option {...props}>
+                          <div className="flex items-center gap-2">
+                            <div
+                              className={classNames(
+                                "h-2 w-2 rounded-full",
+                                status === "available" ? "bg-green-500" : "bg-red-500"
+                              )}
+                            />
+                            {children}
+                          </div>
+                        </components.Option>
+                      );
+                    },
+                  }}
+                />
+              </div>
+            )}
+            <DialogFooter>
+              <DialogClose />
+              <Button
+                type="submit"
+                data-testid="rejection-confirm"
+                loading={roundRobinReassignMutation.isPending || roundRobinManualReassignMutation.isPending}>
+                {t("reassign")}
+              </Button>
+            </DialogFooter>
+          </Form>
+        </DialogContent>
+      </Dialog>
+
+      <Dialog open={showConfirmation} onOpenChange={setShowConfirmation}>
+        <ConfirmationDialogContent
+          variety="warning"
+          open={showConfirmation}
+          onOpenChange={setShowConfirmation}
+          title={t("confirm_reassign_unavailable")}
+          confirmBtnText={t("yes_reassign")}
+          cancelBtnText={t("cancel")}
+          onConfirm={() => {
+            const teamMemberId = form.getValues("teamMemberId");
+            roundRobinManualReassignMutation.mutate({ bookingId, teamMemberId });
+            setShowConfirmation(false);
+          }}>
+          {t("reassign_unavailable_team_member_description")}
+        </ConfirmationDialogContent>
+      </Dialog>
+    </>
   );
 };
