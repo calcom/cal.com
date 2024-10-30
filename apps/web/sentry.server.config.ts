05be5fc13c55c4c6e0d98edc6c9aa9fe5ae202e0
Keith Williams <keithwillcode@gmail.com>
2024-10-30 10:21:21 -0300

    perf: Add ability to configure Sentry maxSpans

--- apps/web/sentry.server.config.ts
+++ apps/web/sentry.server.config.ts
@@ -2,5 +2,6 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  maxSpans: parseInt(process.env.SENTRY_MAX_SPANS ?? "1000") || 1000,
   tracesSampleRate: parseFloat(process.env.SENTRY_TRACES_SAMPLE_RATE ?? "0.0") || 0.0,
 });
