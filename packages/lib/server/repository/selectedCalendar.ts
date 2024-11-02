1b3608d96122f963573934b5d14e7c1ac2800308
hbjORbj <sldisek783@gmail.com>
2024-11-02 14:47:36 -0400

    make credential repository and selected calendar repository more generic

--- packages/lib/server/repository/selectedCalendar.ts
+++ packages/lib/server/repository/selectedCalendar.ts
@@ -1,11 +1,17 @@
 import { prisma } from "@calcom/prisma";
 
+type SelectedCalendarCreateInput = {
+  credentialId: number;
+  userId: number;
+  externalId: string;
+  integration: string;
+};
+
 export class SelectedCalendarRepository {
-  static async createGoogleCalendar(data: { credentialId: number; userId: number; externalId: string }) {
+  static async create(data: SelectedCalendarCreateInput) {
     return await prisma.selectedCalendar.create({
       data: {
         ...data,
-        integration: "google_calendar",
       },
     });
   }
