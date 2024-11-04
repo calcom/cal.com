fffa2c75490ab353d18aeac56e90433d102b0b24
Felippe Fernandes <lfelippeeng@gmail.com>
2024-11-03 21:40:54 -0300

    Fix incorrect import

--- packages/platform/atoms/hooks/useAtomsContext.ts
+++ packages/platform/atoms/hooks/useAtomsContext.ts
@@ -2,7 +2,7 @@
 
 import { createContext, useContext } from "react";
 
-import type { translationKeys, CalProviderLanguagesType } from "../cal-provider/CalProvider";
+import type { ptBrTranslationKeys, CalProviderLanguagesType } from "../cal-provider/CalProvider";
 import type http from "../lib/http";
 
 export interface IAtomsContextOptions {
@@ -26,7 +26,7 @@ export interface IAtomsContext {
     language: CalProviderLanguagesType;
     defaultLocale: CalProviderLanguagesType;
     locales: CalProviderLanguagesType[];
-    exists: (key: translationKeys | string) => boolean;
+    exists: (key: ptBrTranslationKeys | string) => boolean;
   };
   organizationId: number;
 }
