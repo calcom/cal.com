59ba8a6031f3cdcfeca078577e6a80d806b56842
CodeShakingSheep <19874562+CodeShakingSheep@users.noreply.github.com>
2024-11-05 19:04:51 -0500

    Add hints + docsUrl for Nextcloud Talk app

--- packages/app-store/nextcloudtalk/zod.ts
+++ packages/app-store/nextcloudtalk/zod.ts
@@ -1,10 +1,15 @@
 import { z } from "zod";
 
 export const appKeysSchema = z.object({
-  nextcloudTalkHost: z.string(),
-  nextcloudTalkPattern: z.string().optional(),
-  nextcloudTalkClientId: z.string(),
-  nextcloudTalkClientSecret: z.string(),
+  nextcloudTalkHost: z.string().describe("Your Nextcloud base URL"),
+  nextcloudTalkPattern: z
+    .string()
+    .optional()
+    .describe(
+      "Pattern according to which your created calls are named. You can use the following placeholders: {uuid}, {Title}, {Event Type Title}, {Scheduler}, {Organizer}, {Location} and {Team}"
+    ),
+  nextcloudTalkClientId: z.string().describe("Your Nextcloud OAuth Client's ID"),
+  nextcloudTalkClientSecret: z.string().describe("Your Nextcloud OAuth Client's secret"),
 });
 
 export const appDataSchema = z.object({});
