0cbac4e4b6251dc26b62e8e3535fff69a5c68d23
CodeShakingSheep <19874562+CodeShakingSheep@users.noreply.github.com>
2024-11-05 19:06:18 -0500

    Add hints for Jitsi app

--- packages/app-store/jitsivideo/zod.ts
+++ packages/app-store/jitsivideo/zod.ts
@@ -1,8 +1,13 @@
 import { z } from "zod";
 
 export const appKeysSchema = z.object({
-  jitsiHost: z.string().optional(),
-  jitsiPathPattern: z.string().optional(),
+  jitsiHost: z.string().optional().describe("If you self-host Jitsi enter your Jitsi base URL here"),
+  jitsiPathPattern: z
+    .string()
+    .optional()
+    .describe(
+      "Pattern according to which your created calls are named. You can use the following placeholders: {uuid}, {Title}, {Event Type Title}, {Scheduler}, {Organizer}, {Location} and {Team}"
+    ),
 });
 
 export const appDataSchema = z.object({});
