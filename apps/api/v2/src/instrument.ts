07867a131675a89e1876ec4859c0eac4dd71fe70
Morgan Vernay <morgan@cal.com>
2024-11-06 20:13:47 +0200

    chore: add sentry performance tracking api-v2

--- apps/api/v2/src/instrument.ts
+++ apps/api/v2/src/instrument.ts
@@ -1,12 +1,15 @@
 import { getEnv } from "@/env";
 import * as Sentry from "@sentry/node";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 if (process.env.SENTRY_DSN) {
   // Ensure to call this before requiring any other modules!
   Sentry.init({
     dsn: getEnv("SENTRY_DSN"),
-    // Add Performance Monitoring by setting tracesSampleRate
-    // We recommend adjusting this value in production
-    // todo: Evaluate? tracesSampleRate: 1.0
+    integrations: [nodeProfilingIntegration()],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0,
   });
 }
