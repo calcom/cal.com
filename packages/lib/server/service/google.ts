c3c03d9f461cb9818851b3c078cdaa6e135d5657
hbjORbj <sldisek783@gmail.com>
2024-11-07 14:32:38 -0500

    remove wrong log

--- packages/lib/server/service/google.ts
+++ packages/lib/server/service/google.ts
@@ -66,11 +66,6 @@ export class GoogleService {
 
         allCalendars = [...allCalendars, ...(response.data.items ?? [])];
         pageToken = response.data.nextPageToken;
-
-        logger.error("Google Calendar pagination failed in the middle", {
-          totalSoFar: allCalendars.length,
-          hasMorePages: !!pageToken,
-        });
       } while (pageToken);
 
       return allCalendars;
