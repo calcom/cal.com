de9e0f9f4448f960cf85b276e1109678585bd10f
Somay Chauhan <somaychauhan98@gmail.com>
2024-11-07 15:56:30 +0530

    feat: added custom classnames to event-recurring tab

--- packages/platform/atoms/event-types/wrappers/EventRecurringWebWrapper.tsx
+++ packages/platform/atoms/event-types/wrappers/EventRecurringWebWrapper.tsx
@@ -1,9 +1,7 @@
+import type { EventRecurringTabProps } from "@calcom/features/eventtypes/components/tabs/recurring/EventRecurringTab";
 import { EventRecurringTab } from "@calcom/features/eventtypes/components/tabs/recurring/EventRecurringTab";
-import type { EventTypeSetupProps } from "@calcom/features/eventtypes/lib/types";
 
-export type EventRecurringWebWrapperProps = Pick<EventTypeSetupProps, "eventType">;
-
-const EventRecurringWebWrapper = (props: EventRecurringWebWrapperProps) => {
+const EventRecurringWebWrapper = (props: EventRecurringTabProps) => {
   return <EventRecurringTab {...props} />;
 };
 
