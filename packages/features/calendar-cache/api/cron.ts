092da047878cd7f6a85ed707cc0fcf3cf6747eb4
Omar López <zomars@me.com>
2024-11-07 20:22:55 -0700

    WIP

--- packages/features/calendar-cache/api/cron.ts
+++ packages/features/calendar-cache/api/cron.ts
@@ -2,6 +2,7 @@ import type { NextRequest } from "next/server";
 
 import { HttpError } from "@calcom/lib/http-error";
 import { defaultResponder } from "@calcom/lib/server/defaultResponder.appDir";
+import prisma from "@calcom/prisma";
 
 const validateRequest = (req: NextRequest) => {
   const searchParams = req.nextUrl.searchParams;
@@ -16,6 +17,50 @@ export const GET = defaultResponder(async (request: NextRequest) => {
   validateRequest(request);
   // const calendarCache = await CalendarCache.init();
   // calendarCache.re();
+  // Get all selected calendars from users that belong to a team that has calendar cache enabled
+  const oneDayInMS = 24 * 60 * 60 * 1000;
+  const expiresInADay = String(new Date().getTime() + oneDayInMS);
+  const selectedCalendars = await prisma.selectedCalendar.findMany({
+    where: {
+      user: {
+        teams: {
+          some: {
+            team: {
+              features: {
+                some: {
+                  featureId: "calendar-cache",
+                },
+              },
+            },
+          },
+        },
+      },
+      // RN we only support google calendar subscriptions for now
+      integration: "google_calendar",
+      AND: [
+        {
+          OR: [{ googleChannelExpiration: null }, { googleChannelExpiration: { gt: expiresInADay } }],
+        },
+      ],
+    },
+    // select: {
+    //   credentialId: true,
+    //   externalId: true,
+    // },
+  });
 
+  console.log({
+    expiresInADay,
+    selectedCalendars,
+    expired: selectedCalendars.map((sc) => {
+      if (!sc.googleChannelExpiration) return null;
+      console.log({
+        humanReadableExpireDate: new Date(parseInt(sc.googleChannelExpiration, 10)).toLocaleString(),
+        humanReadableCurrentDate: new Date(parseInt(expiresInADay, 10)).toLocaleString(),
+      });
+      return sc.googleChannelExpiration < expiresInADay;
+    }),
+  });
+  // TODO: Credentials can be installed on a whole team, check for selected calendars on the team
   return { success: true };
 });
