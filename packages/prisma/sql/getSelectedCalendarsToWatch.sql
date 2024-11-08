4b991646414bbe25d52836badbeab6ed6a1b749b
Omar López <zomars@me.com>
2024-11-08 09:30:44 -0700

    Update getSelectedCalendarsToWatch.sql

--- packages/prisma/sql/getSelectedCalendarsToWatch.sql
+++ packages/prisma/sql/getSelectedCalendarsToWatch.sql
@@ -5,7 +5,7 @@ FROM
 	"SelectedCalendar" sc
 	LEFT JOIN "users" AS u ON u.id = sc. "userId"
 	LEFT JOIN "Membership" AS m ON m. "userId" = u.id
-	LEFT JOIN "Team" AS t ON m. "userId" = u.id
+	LEFT JOIN "Team" AS t ON t.id = m."teamId"
 	LEFT JOIN "TeamFeatures" AS tf ON tf. "teamId" = t.id
 WHERE
 	-- Only get calendars for teams where cache is enabled
@@ -18,7 +18,7 @@ WHERE
 		OR (
 			-- Or is a calendar that is about to expire
       		sc."googleChannelExpiration" IS NOT NULL
-      		-- We substract one day in senconds to renew a day befor expiration
+      		-- We substract one day in senconds to renew a day before expiration
       		AND to_timestamp(sc."googleChannelExpiration"::bigint / 1000 - 86400)::date < CURRENT_TIMESTAMP
     	)
   	);
