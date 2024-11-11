f387e1391b7e51d1450774bbdf39cd2fa4e09ae4
Benny Joo <sldisek783@gmail.com>
2024-11-10 19:54:40 -0500

    add e2e test for email signup

--- apps/web/playwright/onboarding.e2e.ts
+++ apps/web/playwright/onboarding.e2e.ts
@@ -10,10 +10,9 @@ test.describe.configure({ mode: "serial" });
 
 test.afterEach(({ users }) => users.deleteAll());
 test.describe("Onboarding v3", () => {
+  test.afterEach(({ users }) => users.deleteAll());
   test.describe("Google Sign Up", () => {
-    test.afterEach(({ users }) => users.deleteAll());
-
-    test("Personal Account Flow (1-step) @test", async ({ page, users }) => {
+    test("Personal Account Flow (1-step)", async ({ page, users }) => {
       const user = await users.create({
         completedOnboarding: false,
         name: null,
@@ -37,13 +36,79 @@ test.describe("Onboarding v3", () => {
       expect(await user.confirmCompletedOnboarding()).toEqual(true);
     });
 
-    test("Team Account Flow (4-step) @test", async ({ page, users }) => {
+    test("Team Account Flow (4-step)", async ({ page, users }) => {
       test.skip(!IS_TEAM_BILLING_ENABLED, "Skipping paying for Teams as Stripe is disabled");
 
       const user = await users.create({
         completedOnboarding: false,
         name: null,
         identityProvider: "GOOGLE",
+      });
+      await user.apiLogin();
+      await page.goto("/getting-started");
+
+      // Verify landing on first screen
+      await page.waitForURL("/getting-started");
+
+      await page.getByText("With my team").click();
+      await page.locator("button[type=submit]").click();
+      await page.waitForURL("/settings/teams/new");
+
+      await page.locator('input[name="name"]').fill("NewTeamName");
+      await page.click("[type=submit]");
+
+      await fillStripeTestCheckout(page);
+      await page.waitForURL(/\/settings\/teams\/(\d+)\/onboard-members.*$/i);
+      await page.locator("[data-testid=publish-button]").click();
+      await page.waitForURL(/\/settings\/teams\/(\d+)\/event-type*$/i);
+      await page.locator("[data-testid=handle-later-button]").click();
+      await page.waitForURL(/\/settings\/teams\/(\d+)\/profile$/i);
+      expect(await user.confirmCompletedOnboarding()).toEqual(true);
+    });
+  });
+
+  test.describe("Email Sign Up", () => {
+    test("Personal Account Flow (3-step)", async ({ page, users }) => {
+      const user = await users.create({
+        completedOnboarding: false,
+        name: null,
+        identityProvider: "CAL",
+      });
+      await user.apiLogin();
+      await page.goto("/getting-started");
+
+      // Verify landing on first screen
+      await page.waitForURL("/getting-started");
+
+      // Click "For personal use" button
+      await page.getByText("For personal use").click();
+
+      // Click "Continue" button
+      await page.locator("button[type=submit]").click();
+
+      // Connected Calendar screen
+      await expect(page).toHaveURL(/.*connected-calendar/);
+      expect(await page.locator("button[data-testid=save-calendar-button]").isDisabled()).toBe(true);
+      // tests skip button, we don't want to test entire flow.
+      await page.locator("button[data-testid=skip-step]").click();
+
+      // Connected Video screen
+      await expect(page).toHaveURL(/.*connected-video/);
+      expect(await page.locator("button[data-testid=save-video-button]").isDisabled()).toBe(false);
+      await page.locator("button[data-testid=save-video-button]").click();
+
+      await page.waitForURL("/event-types");
+
+      expect(await user.confirmCompletedOnboarding()).toEqual(true);
+    });
+
+    test("Team Account Flow (4-step)", async ({ page, users }) => {
+      test.skip(!IS_TEAM_BILLING_ENABLED, "Skipping paying for Teams as Stripe is disabled");
+
+      const user = await users.create({
+        completedOnboarding: false,
+        name: null,
+        identityProvider: "CAL",
       });
       await user.apiLogin();
       await page.goto("/getting-started");
