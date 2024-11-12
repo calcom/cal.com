97922464f09a15af09759c779713cf138e338098
Eunjae Lee <hey@eunjae.dev>
2024-11-12 10:26:06 +0100

    fix declaration file

--- packages/ui/components/data-table/tanstack.d.ts
+++ packages/ui/components/data-table/tanstack.d.ts
@@ -1,6 +1,6 @@
-import "@tanstack/react-table";
+import "@tanstack/table-core";
 
-declare module "@tanstack/react-table" {
+declare module "@tanstack/table-core" {
   interface ColumnMeta<TData extends RowData, TValue> {
     sticky?: {
       position: "left" | "right";
