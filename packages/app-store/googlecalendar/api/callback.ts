f766b66c20a41baac30ce5dec108af4d16488c55
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-11-13 10:39:51 -0500

    Fix merge error

--- packages/app-store/googlecalendar/api/callback.ts
+++ packages/app-store/googlecalendar/api/callback.ts
@@ -12,6 +12,7 @@ import { getSafeRedirectUrl } from "@calcom/lib/getSafeRedirectUrl";
 import { HttpError } from "@calcom/lib/http-error";
 import { defaultHandler, defaultResponder } from "@calcom/lib/server";
 import { CredentialRepository } from "@calcom/lib/server/repository/credential";
+import { GoogleService } from "@calcom/lib/server/service/google";
 import { Prisma } from "@calcom/prisma/client";
 
 import getInstalledAppPath from "../../_utils/getInstalledAppPath";
