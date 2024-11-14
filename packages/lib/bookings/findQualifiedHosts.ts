700bb63cdc04b662af0b7229363c26bd0ae99545
Alex van Andel <me@alexvanandel.com>
2024-11-14 16:14:41 +0000

    feat: Implements maxLeadThreshold which puts a maximum to the amount of lead a given host is allowed to be ahead

---
+++ packages/lib/bookings/findQualifiedHosts.ts
@@ -0,0 +1,43 @@
+import type { Host } from "@calcom/prisma/client";
+import { SchedulingType } from "@calcom/prisma/client";
+
+import { filterHostsByLeadThreshold } from "./filterHostsByLeadThreshold";
+
+export const findQualifiedHosts = async <
+  T extends { email: string; id: number } & Record<string, unknown>
+>(eventType: {
+  id: number;
+  maxLeadThreshold: number | null;
+  hosts?: (Pick<Host, "isFixed" | "createdAt"> & {
+    user: T;
+  })[];
+  users: T[];
+  schedulingType: SchedulingType | null;
+}): Promise<
+  {
+    isFixed: boolean;
+    email: string;
+    user: T;
+  }[]
+> => {
+  const hosts =
+    eventType.hosts?.length && eventType.schedulingType
+      ? await filterHostsByLeadThreshold({
+          eventTypeId: eventType.id,
+          hosts: eventType.hosts.map((host) => ({
+            isFixed: host.isFixed,
+            createdAt: host.createdAt,
+            email: host.user.email,
+            user: host.user,
+          })),
+          maxLeadThreshold: eventType.maxLeadThreshold,
+        })
+      : eventType.users.map((user) => {
+          return {
+            isFixed: !eventType.schedulingType || eventType.schedulingType === SchedulingType.COLLECTIVE,
+            email: user.email,
+            user: user,
+          };
+        });
+  return hosts;
+};
