e423ed80b0492e5d9bd4ac40f327285b3b66a69a
supalarry <laurisskraucis@gmail.com>
2024-11-15 06:52:59 +0100

    refactor: getCanceledSubsequentBookings

--- apps/api/v2/src/ee/bookings/2024-08-13/services/bookings.service.ts
+++ apps/api/v2/src/ee/bookings/2024-08-13/services/bookings.service.ts
@@ -307,19 +307,25 @@ export class BookingsService_2024_08_13 {
 
     const bookingRequest = await this.inputService.createCancelBookingRequest(request, bookingUid, body);
     await handleCancelBooking(bookingRequest);
+
     if ("cancelSubsequentBookings" in body && body.cancelSubsequentBookings) {
-      const booking = await this.bookingsRepository.getByUid(bookingUid);
-      const recurringBookingUid = booking?.recurringEventId;
-      if (!recurringBookingUid) {
-        return this.getBooking(bookingUid);
-      }
-      const bookings = (await this.getBooking(recurringBookingUid)) as any[];
-      if (Array.isArray(bookings)) {
-        return bookings.filter((booking) => booking.status === "cancelled");
-      }
-      return bookings;
+      return this.getCanceledSubsequentBookings(bookingUid);
     }
+
     return this.getBooking(bookingUid);
+  }
+
+  private async getCanceledSubsequentBookings(bookingUid: string) {
+    const booking = await this.bookingsRepository.getByUid(bookingUid);
+    const recurringBookingUid = booking?.recurringEventId;
+    if (!recurringBookingUid) {
+      throw new BadRequestException(
+        `Booking with bookingUid=${bookingUid} is not part of a recurring booking.`
+      );
+    }
+
+    const bookings = (await this.getBooking(recurringBookingUid)) as any[];
+    return bookings.filter((booking) => booking.status === "cancelled");
   }
 
   async markAbsent(bookingUid: string, bookingOwnerId: number, body: MarkAbsentBookingInput_2024_08_13) {
