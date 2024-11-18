25beedc8659634709b457ea13e2f8ff07875ced0
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-11-18 14:53:31 -0500

    Add assignment reason table

---
+++ packages/prisma/migrations/20241118195318_add_assignment_reason/migration.sql
@@ -0,0 +1,22 @@
+-- CreateEnum
+CREATE TYPE "AssignmentReasonEnum" AS ENUM ('ROUTING_FORM_ROUTING', 'ROUTING_FORM_ROUTING_FALLBACK', 'REASSIGNED', 'REROUTED', 'SALESFORCE_ASSIGNMENT');
+
+-- CreateTable
+CREATE TABLE "AssignmentReason" (
+    "id" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "bookingId" INTEGER NOT NULL,
+    "reasonEnum" "AssignmentReasonEnum" NOT NULL,
+    "reasonString" TEXT NOT NULL,
+
+    CONSTRAINT "AssignmentReason_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AssignmentReason_id_key" ON "AssignmentReason"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AssignmentReason_bookingId_key" ON "AssignmentReason"("bookingId");
+
+-- AddForeignKey
+ALTER TABLE "AssignmentReason" ADD CONSTRAINT "AssignmentReason_bookingId_fkey" FOREIGN KEY ("bookingId") REFERENCES "Booking"("id") ON DELETE CASCADE ON UPDATE CASCADE;
