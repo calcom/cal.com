160ddf78c1f61fce2113298cec11c56945e6dafa
unknown <adhabal2002@gmail.com>
2024-11-18 19:42:13 +0530

    revert

--- packages/features/eventtypes/components/tabs/assignment/EventTeamAssignmentTab.tsx
+++ packages/features/eventtypes/components/tabs/assignment/EventTeamAssignmentTab.tsx
@@ -176,6 +176,7 @@ const FixedHosts = ({
           childrenClassName="lg:ml-0">
           <div className="border-subtle flex flex-col gap-6 rounded-bl-md rounded-br-md border border-t-0 px-6">
             <AddMembersWithSwitch
+              data-testid="fixed-hosts-select"
               teamMembers={teamMembers}
               value={value}
               onChange={onChange}
