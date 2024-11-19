12f319874d5d8a575466eed6780e46d6bb15dc5a
Omar López <zomars@me.com>
2024-11-19 10:56:20 -0700

    Update features.repository.mock.ts

--- packages/features/flags/features.repository.mock.ts
+++ packages/features/flags/features.repository.mock.ts
@@ -1,12 +1,10 @@
-import type { AppFlags } from "flags/config";
-
 import type { IFeaturesRepository } from "./features.repository.interface";
 
 export class MockFeaturesRepository implements IFeaturesRepository {
   async checkIfUserHasFeature(userId: number, slug: string) {
     return slug === "mock-feature";
   }
-  async checkIfFeatureIsEnabledGlobally(slug: keyof AppFlags) {
+  async checkIfFeatureIsEnabledGlobally() {
     return true;
   }
 }
