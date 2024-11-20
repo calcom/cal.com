d0967a801eeab334beeb4c48b111183bd230a316
Eunjae Lee <hey@eunjae.dev>
2024-11-20 09:07:08 +0100

    fix: fix date comparison for OOO

--- packages/trpc/server/routers/loggedInViewer/outOfOffice.handler.ts
+++ packages/trpc/server/routers/loggedInViewer/outOfOffice.handler.ts
@@ -30,16 +30,16 @@ export const outOfOfficeCreateOrUpdate = async ({ ctx, input }: TBookingRedirect
     throw new TRPCError({ code: "BAD_REQUEST", message: "start_date_and_end_date_required" });
   }
 
-  const inputStartTime = dayjs(startDate).startOf("day");
-  const inputEndTime = dayjs(endDate).endOf("day");
+  const startTimeUtc = dayjs.utc(startDate).add(input.offset, "minute").startOf("day");
+  const endTimeUtc = dayjs.utc(endDate).add(input.offset, "minute").endOf("day");
 
   // If start date is after end date throw error
-  if (inputStartTime.isAfter(inputEndTime)) {
+  if (startTimeUtc.isAfter(endTimeUtc)) {
     throw new TRPCError({ code: "BAD_REQUEST", message: "start_date_must_be_before_end_date" });
   }
 
   // If start date is before to today throw error
-  if (inputStartTime.isBefore(dayjs().startOf("day").subtract(1, "day"))) {
+  if (startTimeUtc.isBefore(dayjs().startOf("day").subtract(1, "day"))) {
     throw new TRPCError({ code: "BAD_REQUEST", message: "start_date_must_be_in_the_future" });
   }
 
@@ -87,24 +87,24 @@ export const outOfOfficeCreateOrUpdate = async ({ ctx, input }: TBookingRedirect
           OR: [
             {
               start: {
-                lt: inputEndTime.toISOString(), //existing start is less than or equal to input end time
+                lt: endTimeUtc.toISOString(), //existing start is less than or equal to input end time
               },
               end: {
-                gt: inputStartTime.toISOString(), //existing end is greater than or equal to input start time
+                gt: startTimeUtc.toISOString(), //existing end is greater than or equal to input start time
               },
             },
             {
               //existing start is within the new input range
               start: {
-                gt: inputStartTime.toISOString(),
-                lt: inputEndTime.toISOString(),
+                gt: startTimeUtc.toISOString(),
+                lt: endTimeUtc.toISOString(),
               },
             },
             {
               //existing end is within the new input range
               end: {
-                gt: inputStartTime.toISOString(),
-                lt: inputEndTime.toISOString(),
+                gt: startTimeUtc.toISOString(),
+                lt: endTimeUtc.toISOString(),
               },
             },
           ],
@@ -135,17 +135,11 @@ export const outOfOfficeCreateOrUpdate = async ({ ctx, input }: TBookingRedirect
       OR: [
         // Outside of range
         {
-          AND: [
-            { start: { lte: inputEndTime.toISOString() } },
-            { end: { gte: inputStartTime.toISOString() } },
-          ],
+          AND: [{ start: { lte: endTimeUtc.toISOString() } }, { end: { gte: startTimeUtc.toISOString() } }],
         },
         // Inside of range
         {
-          AND: [
-            { start: { gte: inputStartTime.toISOString() } },
-            { end: { lte: inputEndTime.toISOString() } },
-          ],
+          AND: [{ start: { gte: startTimeUtc.toISOString() } }, { end: { lte: endTimeUtc.toISOString() } }],
         },
       ],
     },
@@ -155,8 +149,6 @@ export const outOfOfficeCreateOrUpdate = async ({ ctx, input }: TBookingRedirect
   if (existingOutOfOfficeEntry) {
     throw new TRPCError({ code: "BAD_REQUEST", message: "booking_redirect_infinite_not_allowed" });
   }
-  const startTimeUtc = dayjs.utc(startDate).add(input.offset, "minute").startOf("day").toISOString();
-  const endTimeUtc = dayjs.utc(endDate).add(input.offset, "minute").endOf("day").toISOString();
 
   // Get the existing redirected user from existing out of office entry to send that user appropriate email.
   const previousOutOfOfficeEntry = await prisma.outOfOfficeEntry.findUnique({
@@ -181,8 +173,8 @@ export const outOfOfficeCreateOrUpdate = async ({ ctx, input }: TBookingRedirect
     },
     create: {
       uuid: uuidv4(),
-      start: startTimeUtc,
-      end: endTimeUtc,
+      start: startTimeUtc.toISOString(),
+      end: endTimeUtc.toISOString(),
       notes: input.notes,
       userId: ctx.user.id,
       reasonId: input.reasonId,
@@ -191,8 +183,8 @@ export const outOfOfficeCreateOrUpdate = async ({ ctx, input }: TBookingRedirect
       updatedAt: new Date(),
     },
     update: {
-      start: startTimeUtc,
-      end: endTimeUtc,
+      start: startTimeUtc.toISOString(),
+      end: endTimeUtc.toISOString(),
       notes: input.notes,
       userId: ctx.user.id,
       reasonId: input.reasonId,
