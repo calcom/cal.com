ac9e9008a5adb85d9b1644f3d2d9625e60186eaf
Khushal Malhotra <93102837+khushal-malhotra@users.noreply.github.com>
2024-11-24 18:29:08 +0530

    refactor: remove unnecessary comments
    Co-authored-by: Praash <99237795+Praashh@users.noreply.github.com>

--- packages/features/auth/lib/validPassword.ts
+++ packages/features/auth/lib/validPassword.ts
@@ -1,13 +1,11 @@
 export function validPassword(password: string) {
   if (password.length < 7) return false;
 
-  // Match uppercase characters (Unicode aware) using \p{Lu}
+
   if (!/\p{Lu}/u.test(password)) return false;
 
-  // Match lowercase characters (Unicode aware) using \p{Ll}
   if (!/\p{Ll}/u.test(password)) return false;
 
-  // Match at least one digit
   if (!/\d+/.test(password)) return false;
 
   return true;
