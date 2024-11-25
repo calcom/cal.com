fcd69ae7ef88f73fcba0960e55e683d7afa8f4b9
hbjORbj <sldisek783@gmail.com>
2024-11-25 05:14:33 -0500

    fix attribute pages

--- apps/web/app/settings/(settings-layout)/organizations/attributes/create/page.tsx
+++ apps/web/app/settings/(settings-layout)/organizations/attributes/create/page.tsx
@@ -1,11 +1,24 @@
-import { _generateMetadata } from "app/_utils";
+import { _generateMetadata, getFixedT } from "app/_utils";
 
-import Page from "@calcom/ee/organizations/pages/settings/attributes/attributes-create-view";
+import OrgAttributesCreatePage from "@calcom/ee/organizations/pages/settings/attributes/attributes-create-view";
+import { getServerSessionForAppDir } from "@calcom/features/auth/lib/get-server-session-for-app-dir";
+import SettingsHeader from "@calcom/features/settings/appDir/SettingsHeader";
 
 export const generateMetadata = async () =>
   await _generateMetadata(
-    () => "Attribute",
-    () => "Create an attribute for your team members"
+    (t) => t("attribute"),
+    (t) => t("create_attribute_description")
   );
+
+const Page = async () => {
+  const session = await getServerSessionForAppDir();
+  const t = await getFixedT(session?.user.locale || "en");
+
+  return (
+    <SettingsHeader title={t("attribute")} description={t("create_attribute_description")}>
+      <OrgAttributesCreatePage />
+    </SettingsHeader>
+  );
+};
 
 export default Page;
