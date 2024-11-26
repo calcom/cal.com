0ed1bc32bb85bc0b2c34e70f4dc5ebf3c9157afc
Benny Joo <sldisek783@gmail.com>
2024-11-26 08:33:42 -0500

    reduce

--- apps/web/playwright/icons.e2e.ts
+++ apps/web/playwright/icons.e2e.ts
@@ -5,7 +5,7 @@ import { test } from "./lib/fixtures";
 test("Icons render properly", async ({ page }, testInfo) => {
   await page.goto("/icons");
   await expect(page).toHaveScreenshot("icons.png", {
-    maxDiffPixels: 50000, // Allow some pixel differences (e.g., linux vs mac)
+    maxDiffPixels: 45000, // Allow some pixel differences (e.g., linux vs mac)
     fullPage: true,
   });
 });
