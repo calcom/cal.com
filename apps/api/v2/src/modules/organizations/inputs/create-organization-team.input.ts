a288f8ef992ee9d734ba62b401e354f501cabb8d
Ryukemeister <sahalrajiv6900@gmail.com>
2024-11-27 20:46:24 +0530

    update docs

--- apps/api/v2/src/modules/organizations/inputs/create-organization-team.input.ts
+++ apps/api/v2/src/modules/organizations/inputs/create-organization-team.input.ts
@@ -1,16 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsBoolean, IsOptional, IsString, IsUrl, Length } from "class-validator";
 
 export class CreateOrgTeamDto {
   @IsString()
   @Length(1)
+  @ApiProperty({ description: "Name of the team", example: "CalTeam" })
   readonly name!: string;
 
   @IsOptional()
   @IsString()
+  @ApiProperty({ description: "Team slug", example: "caltel" })
   readonly slug?: string;
 
   @IsOptional()
   @IsUrl()
+  @ApiProperty({
+    example: "https://cal.com/api/avatar/d95949bc-ccb1-400f-acf6-045c51a16856.png",
+    description: `URL of the teams logo image`,
+  })
   readonly logoUrl?: string;
 
   @IsOptional()
@@ -59,6 +66,10 @@ export class CreateOrgTeamDto {
 
   @IsOptional()
   @IsUrl()
+  @ApiProperty({
+    example: "https://cal.com/api/avatar/d95949bc-ccb1-400f-acf6-045c51a16856.png",
+    description: `URL of the teams banner image which is shown on booker`,
+  })
   readonly bannerUrl?: string;
 
   @IsOptional()
@@ -67,10 +78,17 @@ export class CreateOrgTeamDto {
 
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    example: "America/New_York",
+    description: `Timezone is used to create teams's default schedule from Monday to Friday from 9AM to 5PM. It will default to Europe/London if not passed.`,
+  })
   readonly timeZone?: string = "Europe/London";
 
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    example: "Monday",
+  })
   readonly weekStart?: string = "Sunday";
 
   @IsOptional()
