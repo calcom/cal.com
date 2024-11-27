c3f635a2735732b1f3e05e1f35ed0e6a86be0744
Eunjae Lee <hey@eunjae.dev>
2024-11-27 15:27:49 +0100

    revert change

--- packages/platform/atoms/availability/AvailabilitySettings.tsx
+++ packages/platform/atoms/availability/AvailabilitySettings.tsx
@@ -230,7 +230,7 @@ const SmallScreenSideBar = ({ open, children }: { open: boolean; children: JSX.E
     <div
       className={classNames(
         open
-          ? "fadeIn fixed inset-0 z-50 bg-neutral-800 bg-opacity-70 transition-opacity sm:hidden dark:bg-opacity-70"
+          ? "fadeIn fixed inset-0 z-50 bg-neutral-800 bg-opacity-70 transition-opacity dark:bg-opacity-70 sm:hidden"
           : ""
       )}>
       <div
@@ -384,7 +384,7 @@ export function AvailabilitySettings({
               <div
                 className={classNames(
                   openSidebar
-                    ? "fadeIn fixed inset-0 z-50 bg-neutral-800 bg-opacity-70 transition-opacity sm:hidden dark:bg-opacity-70"
+                    ? "fadeIn fixed inset-0 z-50 bg-neutral-800 bg-opacity-70 transition-opacity dark:bg-opacity-70 sm:hidden"
                     : ""
                 )}>
                 <div
@@ -452,8 +452,8 @@ export function AvailabilitySettings({
                     )}
                   </div>
 
-                  <div className="col-span-3 min-w-40 space-y-2 px-2 py-4 lg:col-span-1">
-                    <div className="w-full pr-4 sm:ml-0 sm:mr-36 sm:p-0 xl:max-w-80">
+                  <div className="min-w-40 col-span-3 space-y-2 px-2 py-4 lg:col-span-1">
+                    <div className="xl:max-w-80 w-full pr-4 sm:ml-0 sm:mr-36 sm:p-0">
                       <div>
                         <Skeleton
                           as={Label}
@@ -570,8 +570,8 @@ export function AvailabilitySettings({
               />
             )}
           </div>
-          <div className="col-span-3 hidden min-w-40 space-y-2 md:block lg:col-span-1">
-            <div className="w-full pr-4 sm:ml-0 sm:mr-36 sm:p-0 xl:max-w-80">
+          <div className="min-w-40 col-span-3 hidden space-y-2 md:block lg:col-span-1">
+            <div className="xl:max-w-80 w-full pr-4 sm:ml-0 sm:mr-36 sm:p-0">
               <div>
                 <Skeleton
                   as={Label}
