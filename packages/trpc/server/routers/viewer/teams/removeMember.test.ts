a38defa1c24c4de74cd6a910b82632c6e2c1bde9
Udit Takkar <udit222001@gmail.com>
2024-11-28 20:14:59 +0530

    fix: test

--- packages/trpc/server/routers/viewer/teams/removeMember.test.ts
+++ packages/trpc/server/routers/viewer/teams/removeMember.test.ts
@@ -1,18 +1,18 @@
-import prismaMock from "../../../../../../tests/libs/__mocks__/prismaMock";
+import prismaMock from "../../../../../../tests/libs/__mocks__/prisma";
 
 import {
   createBookingScenario,
   TestData,
   getOrganizer,
   getScenarioData,
-  createOrganization,
   Timezones,
+  createOrganization,
 } from "@calcom/web/test/utils/bookingScenario/bookingScenario";
 import { setupAndTeardown } from "@calcom/web/test/utils/bookingScenario/setupAndTeardown";
 
 import { describe, test, expect } from "vitest";
 
-import { MembershipRole, SchedulingType } from "@calcom/prisma/enums";
+import { SchedulingType, MembershipRole } from "@calcom/prisma/enums";
 
 import type { TrpcSessionUser } from "../../../trpc";
 import removeMember from "./removeMember.handler";
@@ -20,117 +20,163 @@ import removeMember from "./removeMember.handler";
 describe("removeMember", () => {
   setupAndTeardown();
 
-  test("should remove a member from a team", async () => {
-    const org = await createOrganization({ name: "acme", slug: "acme" });
+  describe("should remove a member from a team", () => {
+    test(`1) Should remove a member from a team
+          2) Should remove the member from hosts
+      `, async () => {
+      const org = await createOrganization({
+        name: "Test Org",
+        slug: "testorg",
+      });
 
-    const organizer = getOrganizer({
-      name: "Organizer",
-      email: "organizer@example.com",
-      id: 101,
-      organizationId: org.id,
-      schedules: [TestData.schedules.IstWorkHours],
-      teams: [
-        {
-          team: { id: 2, name: "Team 1", slug: "team-1", parentId: org.id },
-          membership: { accepted: true, role: MembershipRole.ADMIN },
-        },
-        {
-          team: { id: org.id, name: org.name, slug: org.slug, parentId: undefined },
-          membership: { accepted: true, role: MembershipRole.OWNER },
-        },
-      ],
-    });
+      // Create the child team
+      const childTeam = {
+        id: 202,
+        name: "Team 1",
+        slug: "team-1",
+        parentId: org.id,
+      };
 
-    const otherTeamMembers = [
-      {
-        name: "Other Team Member 1",
-        username: "other-team-member-1",
-        timeZone: Timezones["+5:30"],
-        defaultScheduleId: null,
-        email: "other-team-member-1@example.com",
-        id: 102,
-        schedules: [TestData.schedules.IstEveningShift],
+      const organizer = getOrganizer({
+        name: "Organizer",
+        email: "organizer@example.com",
+        id: 101,
+        organizationId: org.id,
+        schedules: [TestData.schedules.IstWorkHours],
         teams: [
           {
-            team: { id: 2, name: "Team 1", slug: "team-1", parentId: org.id },
-            membership: { accepted: true },
+            membership: {
+              accepted: true,
+              role: MembershipRole.ADMIN,
+            },
+            team: {
+              id: org.id,
+              name: "Test Org",
+              slug: "testorg",
+            },
           },
           {
-            team: { id: org.id, name: org.name, slug: org.slug, parentId: undefined },
-            membership: { accepted: true },
+            membership: {
+              accepted: true,
+              role: MembershipRole.ADMIN,
+            },
+            team: {
+              id: childTeam.id,
+              name: "Team 1",
+              slug: "team-1",
+              parentId: org.id,
+            },
           },
         ],
-      },
-    ];
+      });
 
-    await createBookingScenario(
-      getScenarioData(
+      const otherTeamMembers = [
         {
-          eventTypes: [
+          name: "Other Team Member 1",
+          username: "other-team-member-1",
+          timeZone: Timezones["+5:30"],
+          defaultScheduleId: null,
+          email: "other-team-member-1@example.com",
+          id: 102,
+          organizationId: org.id,
+          schedules: [TestData.schedules.IstEveningShift],
+          teams: [
             {
-              id: 1,
-              slug: "round-robin-event",
-              schedulingType: SchedulingType.ROUND_ROBIN,
-              length: 30,
-              hosts: [
-                {
-                  userId: 101,
-                  isFixed: false,
-                },
-                {
-                  userId: 102,
-                  isFixed: false,
-                },
-              ],
+              membership: {
+                accepted: true,
+                role: MembershipRole.MEMBER,
+              },
+              team: {
+                id: org.id,
+                name: "Test Org",
+                slug: "testorg",
+              },
+            },
+            {
+              membership: {
+                accepted: true,
+                role: MembershipRole.MEMBER,
+              },
+              team: {
+                id: childTeam.id,
+                name: "Team 1",
+                slug: "team-1",
+                parentId: org.id,
+              },
             },
           ],
-          organizer,
-          usersApartFromOrganizer: otherTeamMembers,
-          apps: [TestData.apps["daily-video"]],
         },
-        org
-      )
-    );
+      ];
 
-    //Logged in user is admin of the org
-    const ctx = {
-      user: {
-        id: organizer.id,
-        name: organizer.name,
-      } as NonNullable<TrpcSessionUser>,
-    };
+      await createBookingScenario(
+        getScenarioData(
+          {
+            eventTypes: [
+              {
+                id: 1,
+                teamId: childTeam.id,
+                schedulingType: SchedulingType.ROUND_ROBIN,
+                length: 30,
+                hosts: [
+                  {
+                    userId: 101,
+                    isFixed: false,
+                  },
+                  {
+                    userId: 102,
+                    isFixed: false,
+                  },
+                ],
+              },
+            ],
+            organizer,
+            usersApartFromOrganizer: otherTeamMembers,
+            apps: [TestData.apps["daily-video"]],
+          },
+          org
+        )
+      );
 
-    await removeMember({
-      ctx,
-      input: {
-        teamIds: [org.id],
-        memberIds: [102],
-        isOrg: true,
-      },
+      //Logged in user is admin of the org
+      const ctx = {
+        user: {
+          id: organizer.id,
+          name: organizer.name,
+        } as NonNullable<TrpcSessionUser>,
+      };
+
+      await removeMember({
+        ctx,
+        input: {
+          teamIds: [org.id],
+          memberIds: [102],
+          isOrg: true,
+        },
+      });
+
+      //Check if the remaining memberships are correct
+      const remainingMemberships = await prismaMock.membership.findMany({
+        where: {
+          teamId: childTeam.id,
+        },
+        select: {
+          userId: true,
+        },
+      });
+
+      expect(remainingMemberships.length).toBe(1);
+
+      //Check if the event type has the correct hosts
+      const eventType = await prismaMock.eventType.findUnique({
+        where: {
+          id: 1,
+        },
+        select: {
+          hosts: true,
+        },
+      });
+
+      expect(eventType?.hosts?.length).toBe(1);
     });
-
-    //Check if the remaining memberships are correct
-    const remainingMemberships = await prismaMock.membership.findMany({
-      where: {
-        teamId: 2,
-      },
-      select: {
-        userId: true,
-      },
-    });
-
-    expect(remainingMemberships.length).toBe(1);
-
-    //Check if the event type has the correct hosts
-    const eventType = await prismaMock.eventType.findUnique({
-      where: {
-        id: 1,
-      },
-      select: {
-        hosts: true,
-      },
-    });
-
-    expect(eventType?.hosts?.length).toBe(1);
   });
 });
