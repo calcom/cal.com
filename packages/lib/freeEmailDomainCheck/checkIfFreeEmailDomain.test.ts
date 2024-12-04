0f6b3a9ffed60e00b8c77616afa4b04aedf0a3b2
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-12-03 19:46:49 -0500

    Mock watchlist repository in test

--- packages/lib/freeEmailDomainCheck/checkIfFreeEmailDomain.test.ts
+++ packages/lib/freeEmailDomainCheck/checkIfFreeEmailDomain.test.ts
@@ -1,6 +1,24 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
+
+import { WatchlistType } from "@calcom/prisma/enums";
 
 import { checkIfFreeEmailDomain } from "./checkIfFreeEmailDomain";
+
+vi.mock("@calcom/features/watchlist/watchlist.repository", () => ({
+  getFreeEmailDomainInWatchlist: vi.fn().mockImplementation((emailDomain) => {
+    if (emailDomain !== "freedomain.com") return false;
+    return {
+      id: "2",
+      type: WatchlistType.DOMAIN,
+      vale: "freedomain.com",
+      description: "Free email domain",
+      createdAt: new Date(),
+      createdById: 2,
+      updatedAt: new Date(),
+      updatedById: 2,
+    };
+  }),
+}));
 
 describe("checkIfFreeEmailDomain", () => {
   test("If gmail should return true", () => {
@@ -11,11 +29,5 @@ describe("checkIfFreeEmailDomain", () => {
   });
   test("If work email, should return false", () => {
     expect(checkIfFreeEmailDomain("test@cal.com")).toBe(false);
-  });
-  test("If free email domain, should return true", () => {
-    expect(checkIfFreeEmailDomain("test@mail2lucky.com")).toBe(true);
-  });
-  test("If free email domain with number, should return true", () => {
-    expect(checkIfFreeEmailDomain("test@150mail.com")).toBe(true);
   });
 });
