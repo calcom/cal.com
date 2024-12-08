8ab1950a42d7c6446230656d168ae529196d64e0
hbjORbj <sldisek783@gmail.com>
2024-12-08 15:22:41 -0500

    improve test

--- apps/web/playwright/eventType/ai-translation.e2e.ts
+++ apps/web/playwright/eventType/ai-translation.e2e.ts
@@ -1,6 +1,6 @@
 import { expect } from "@playwright/test";
 import { loginUser } from "playwright/fixtures/regularBookings";
-import { createNewEventType } from "playwright/lib/testUtils";
+import { createNewEventType, doOnOrgDomain } from "playwright/lib/testUtils";
 
 import { MembershipRole } from "@calcom/prisma/enums";
 
@@ -17,48 +17,67 @@ test.describe("AI Translation - Event Type", () => {
   });
 
   test("Toggle should not be clickable for team plan members", async ({ page, users, bookingPage }) => {
-    const pro = await users.create(
+    const user = await users.create(
       { name: "testuser" },
       {
         hasTeam: true,
         teamRole: MembershipRole.MEMBER,
       }
     );
-    await pro.apiLogin();
+    await user.apiLogin();
     await page.goto("/event-types");
     await bookingPage.goToEventType("30 min");
     expect(await bookingPage.getAITranslationToggleDisabled()).toBe(true);
   });
 
-  test("Toggle should be clickable for org members", async ({ page, users, bookingPage }) => {
-    const pro = await users.create(
-      { name: "testuser" },
-      {
-        isOrg: true,
-        hasTeam: true,
-        teamRole: MembershipRole.MEMBER,
-      }
-    );
-    await pro.apiLogin();
+  test("Toggle should be clickable for org members", async ({ page, orgs, users, bookingPage }) => {
+    const org = await orgs.create({
+      name: "acme",
+    });
+    const user = await users.create({
+      organizationId: org.id,
+      roleInOrganization: MembershipRole.MEMBER,
+    });
+    await user.apiLogin();
     await page.goto("/event-types");
-    await createNewEventType(page, { eventTitle: "5 min" });
+    await bookingPage.goToEventType("30 min");
     expect(await bookingPage.getAITranslationToggleDisabled()).toBe(false);
   });
 
-  test("Enable the AI translation and test in booking page", async ({ page, users, bookingPage }) => {
-    const pro = await users.create(
-      { name: "testuser" },
-      {
-        isOrg: true,
-        hasTeam: true,
-        teamRole: MembershipRole.MEMBER,
-      }
-    );
-    await pro.apiLogin();
+  test("Enable the AI translation and test in booking page", async ({
+    page,
+    orgs,
+    users,
+    bookingPage,
+    context,
+  }) => {
+    const org = await orgs.create({
+      name: "acme",
+    });
+    const user = await users.create({
+      organizationId: org.id,
+      roleInOrganization: MembershipRole.MEMBER,
+    });
+    await user.apiLogin();
     await page.goto("/event-types");
     await createNewEventType(page, { eventTitle: "5 min", eventDescription: "A quick 5 minute chat." });
     expect(await bookingPage.getAITranslationToggleDisabled()).toBe(false);
     await bookingPage.toggleAITranslation();
     await bookingPage.updateEventType();
+
+    await user.logout();
+    await context.setExtraHTTPHeaders({
+      "Accept-Language": "ko-KR",
+    });
+    await doOnOrgDomain(
+      {
+        orgSlug: org.slug,
+        page,
+      },
+      async () => {
+        await page.goto("/5-min");
+        await expect(page.locator('[data-testid="event-meta-description"] p')).toHaveText("빠른 5분 대화.");
+      }
+    );
   });
 });
