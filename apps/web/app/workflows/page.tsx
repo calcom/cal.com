221df0d84adeffc2b277439d059286cad40bfaea
hbjORbj <sldisek783@gmail.com>
2024-12-10 15:50:29 -0500

    disable server side fetching for now

--- apps/web/app/workflows/page.tsx
+++ apps/web/app/workflows/page.tsx
@@ -2,10 +2,10 @@ import type { PageProps } from "app/_types";
 import { _generateMetadata } from "app/_utils";
 import { WithLayout } from "app/layoutHOC";
 
-import { getServerSessionForAppDir } from "@calcom/feature-auth/lib/get-server-session-for-app-dir";
+// import { getServerSessionForAppDir } from "@calcom/feature-auth/lib/get-server-session-for-app-dir";
+// import { getTeamsFiltersFromQuery } from "@calcom/features/filters/lib/getTeamsFiltersFromQuery";
+// import { WorkflowRepository } from "@calcom/lib/server/repository/workflow";
 import LegacyPage from "@calcom/features/ee/workflows/pages/index";
-import { getTeamsFiltersFromQuery } from "@calcom/features/filters/lib/getTeamsFiltersFromQuery";
-import { WorkflowRepository } from "@calcom/lib/server/repository/workflow";
 
 export const generateMetadata = async () =>
   await _generateMetadata(
@@ -15,22 +15,26 @@ export const generateMetadata = async () =>
 
 const Page = async ({ params, searchParams }: PageProps) => {
   // FIXME: Refactor me once next-auth endpoint is migrated to App Router
-  const session = await getServerSessionForAppDir();
-  const user = session?.user;
+  // const session = await getServerSessionForAppDir();
+  // const user = session?.user;
 
-  const filters = getTeamsFiltersFromQuery({ ...searchParams, ...params });
+  // const filters = getTeamsFiltersFromQuery({ ...searchParams, ...params });
 
-  let filteredList;
-  try {
-    filteredList = await WorkflowRepository.getFilteredList({
-      userId: user?.id,
-      input: {
-        filters,
-      },
-    });
-  } catch (err) {}
+  // let filteredList;
+  // try {
+  //   filteredList = await WorkflowRepository.getFilteredList({
+  //     userId: user?.id,
+  //     input: {
+  //       filters,
+  //     },
+  //   });
+  // } catch (err) {}
 
-  return <LegacyPage filteredList={filteredList} />;
+  return (
+    <LegacyPage
+    //  filteredList={filteredList}
+    />
+  );
 };
 
 export default WithLayout({ getLayout: null, ServerPage: Page })<"P">;
