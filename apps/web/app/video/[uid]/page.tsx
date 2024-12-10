0d03d9bec20b1653af052531120e3bdd17b8ea04
Benny Joo <sldisek783@gmail.com>
2024-12-10 18:53:35 -0500

    add correct metadata to video/[uid]

--- apps/web/app/video/[uid]/page.tsx
+++ apps/web/app/video/[uid]/page.tsx
@@ -1,19 +1,38 @@
 import { withAppDirSsr } from "app/WithAppDirSsr";
-import { _generateMetadata } from "app/_utils";
+import { getTranslate } from "app/_utils";
 import { WithLayout } from "app/layoutHOC";
 
-import { APP_NAME } from "@calcom/lib/constants";
+import { APP_NAME, SEO_IMG_OGIMG_VIDEO, WEBSITE_URL } from "@calcom/lib/constants";
 
 import { getServerSideProps } from "@lib/video/[uid]/getServerSideProps";
 
 import type { PageProps } from "~/videos/views/videos-single-view";
 import VideosSingleView from "~/videos/views/videos-single-view";
 
-export const generateMetadata = async () =>
-  await _generateMetadata(
-    () => `${APP_NAME} Video`,
-    (t) => t("quick_video_meeting")
-  );
+export const generateMetadata = async () => {
+  const t = await getTranslate();
+  return {
+    title: `${APP_NAME} Video`,
+    description: t("quick_video_meeting"),
+    openGraph: {
+      title: `${APP_NAME} Video`,
+      description: t("quick_video_meeting"),
+      url: `${WEBSITE_URL}/video`,
+      images: [
+        {
+          url: SEO_IMG_OGIMG_VIDEO,
+        },
+      ],
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `${APP_NAME} Video`,
+      description: t("quick_video_meeting"),
+      images: [SEO_IMG_OGIMG_VIDEO],
+    },
+  };
+};
 
 const getData = withAppDirSsr<PageProps>(getServerSideProps);
 
