4a900d1315a6affd7e2d8701052ec61aee013664
hbjORbj <sldisek783@gmail.com>
2024-12-13 04:02:37 -0500

    fix saml-idp

--- apps/web/app/auth/saml-idp/page.tsx
+++ apps/web/app/auth/saml-idp/page.tsx
@@ -1,1 +1,18 @@
-export { default } from "~/auth/saml-idp/saml-idp-view";
+import type { PageProps } from "app/_types";
+import { notFound } from "next/navigation";
+import { z } from "zod";
+
+import SamlIdpClient from "~/auth/saml-idp/saml-idp-view";
+
+const querySchema = z.object({
+  code: z.string(),
+});
+
+export default async function SamlIdpPage({ params, searchParams }: PageProps) {
+  const parsed = querySchema.safeParse({ ...params, ...searchParams });
+  if (!parsed.success) {
+    notFound();
+  }
+
+  return <SamlIdpClient code={parsed.data.code} />;
+}
