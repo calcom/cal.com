4dc0680ec1443cbe79420426cd8e28a571ef6d18
Vishnu Kumar <vishnukumart96@gmail.com>
2024-12-13 16:13:30 +0530

    fix: routing page link and icon

--- packages/features/shell/navigation/Navigation.tsx
+++ packages/features/shell/navigation/Navigation.tsx
@@ -87,10 +87,10 @@ const getNavigationItems = (orgBranding: OrganizationBranding): NavigationItemTy
     icon: "ellipsis",
   },
   {
-    name: "routing_forms",
-    href: "/apps/routing-forms/forms",
-    icon: "file-text",
-    isCurrent: ({ pathname }) => pathname?.startsWith("/apps/routing-forms/") ?? false,
+    name: "routing",
+    href: "/routing/forms",
+    icon: "split",
+    isCurrent: ({ pathname }) => pathname?.startsWith("/routing/forms") ?? false,
     moreOnMobile: true,
   },
   {
