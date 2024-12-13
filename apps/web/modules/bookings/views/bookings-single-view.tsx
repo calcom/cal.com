645985c783fec5cc940b6b53be6c39d5a30660a6
Anik Dhabal Babu <81948346+anikdhabal@users.noreply.github.com>
2024-12-13 20:59:02 +0530

    fix: Invalid time zone specified: Etc/Unknown (#18121)
    * fix: Invalid time zone specified: Etc/Unknown
    
    * update
    
    * Update PaymentPage.tsx
    
    * update
    
    * update one more file
    
    * address review
    
    * update test
    
    ---------
    
    Co-authored-by: Udit Takkar <53316345+Udit-takkar@users.noreply.github.com>

--- apps/web/modules/bookings/views/bookings-single-view.tsx
+++ apps/web/modules/bookings/views/bookings-single-view.tsx
@@ -31,7 +31,7 @@ import {
   SystemField,
   TITLE_FIELD,
 } from "@calcom/features/bookings/lib/SystemField";
-import { APP_NAME } from "@calcom/lib/constants";
+import { APP_NAME, CURRENT_TIMEZONE } from "@calcom/lib/constants";
 import {
   formatToLocalizedDate,
   formatToLocalizedTime,
@@ -258,9 +258,7 @@ export default function Success(props: PageProps) {
   }, [telemetry]); */
 
   useEffect(() => {
-    setDate(
-      date.tz(localStorage.getItem("timeOption.preferredTimeZone") || dayjs.tz.guess() || "Europe/London")
-    );
+    setDate(date.tz(localStorage.getItem("timeOption.preferredTimeZone") || CURRENT_TIMEZONE));
     setIs24h(props?.userTimeFormat ? props.userTimeFormat === 24 : !!getIs24hClockFromLocalStorage());
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [eventType, needsConfirmation]);
