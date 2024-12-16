caa10a1c7871f7295760629ca26186bf641cd0b9
Somay Chauhan <somaychauhan98@gmail.com>
2024-12-16 17:20:06 +0530

    update decorators 4

--- apps/api/v2/src/modules/organizations/inputs/update-organization-membership.input.ts
+++ apps/api/v2/src/modules/organizations/inputs/update-organization-membership.input.ts
@@ -1,17 +1,19 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import { MembershipRole } from "@prisma/client";
 import { IsBoolean, IsOptional, IsEnum } from "class-validator";
 
 export class UpdateOrgMembershipDto {
   @IsOptional()
   @IsBoolean()
+  @ApiPropertyOptional({ type: Boolean, default: false })
   readonly accepted?: boolean = false;
 
   @IsEnum(MembershipRole)
-  @ApiProperty({ enum: ["MEMBER", "OWNER", "ADMIN"] })
+  @ApiProperty({ enum: ["MEMBER", "OWNER", "ADMIN"], required: true, default: "MEMBER" })
   readonly role?: MembershipRole = MembershipRole.MEMBER;
 
   @IsOptional()
   @IsBoolean()
+  @ApiPropertyOptional({ type: Boolean, default: false })
   readonly disableImpersonation?: boolean = false;
 }
