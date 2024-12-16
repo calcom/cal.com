666f4b0caca9a1ed095eec4c5b83af72753b808c
Keith Williams <keithwillcode@gmail.com>
2024-12-16 16:47:13 +0100

    Added the sentry wrapper that enables perf tracing

--- apps/web/pages/api/trpc/bookings/[trpc].ts
+++ apps/web/pages/api/trpc/bookings/[trpc].ts
@@ -1,4 +1,6 @@
+import { wrapApiHandlerWithSentry } from "@sentry/nextjs";
+
 import { createNextApiHandler } from "@calcom/trpc/server/createNextApiHandler";
 import { bookingsRouter } from "@calcom/trpc/server/routers/viewer/bookings/_router";
 
-export default createNextApiHandler(bookingsRouter);
+export default wrapApiHandlerWithSentry(createNextApiHandler(bookingsRouter), "/api/trpc/bookings/[trpc]");
