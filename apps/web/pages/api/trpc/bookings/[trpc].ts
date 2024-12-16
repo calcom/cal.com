504ac4e95e926687878a47642782d794f89e7685
Keith Williams <keithwillcode@gmail.com>
2024-12-16 17:16:08 +0100

    Fixed where we wrap sentry

--- apps/web/pages/api/trpc/bookings/[trpc].ts
+++ apps/web/pages/api/trpc/bookings/[trpc].ts
@@ -1,6 +1,4 @@
-import { wrapApiHandlerWithSentry } from "@sentry/nextjs";
-
 import { createNextApiHandler } from "@calcom/trpc/server/createNextApiHandler";
 import { bookingsRouter } from "@calcom/trpc/server/routers/viewer/bookings/_router";
 
-export default wrapApiHandlerWithSentry(createNextApiHandler(bookingsRouter), "/api/trpc/bookings/[trpc]");
+export default createNextApiHandler(bookingsRouter);
