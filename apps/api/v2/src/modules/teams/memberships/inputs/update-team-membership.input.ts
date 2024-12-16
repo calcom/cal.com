b07dc43a76483516424cb875584b402866052938
Somay Chauhan <somaychauhan98@gmail.com>
2024-12-16 17:34:53 +0530

    update decorators 4

--- apps/api/v2/src/modules/teams/memberships/inputs/update-team-membership.input.ts
+++ apps/api/v2/src/modules/teams/memberships/inputs/update-team-membership.input.ts
@@ -1,17 +1,20 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import { MembershipRole } from "@prisma/client";
 import { IsBoolean, IsOptional, IsEnum } from "class-validator";
 
 export class UpdateTeamMembershipInput {
   @IsOptional()
   @IsBoolean()
+  @ApiPropertyOptional({ type: Boolean, default: false })
   readonly accepted?: boolean = false;
 
+  @IsOptional()
   @IsEnum(MembershipRole)
-  @ApiProperty({ enum: ["MEMBER", "OWNER", "ADMIN"] })
+  @ApiPropertyOptional({ enum: ["MEMBER", "OWNER", "ADMIN"], default: MembershipRole.MEMBER })
   readonly role?: MembershipRole = MembershipRole.MEMBER;
 
   @IsOptional()
   @IsBoolean()
+  @ApiPropertyOptional({ type: Boolean, default: false })
   readonly disableImpersonation?: boolean = false;
 }
