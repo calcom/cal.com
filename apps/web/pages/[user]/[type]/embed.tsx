c67b23fa19ed4b8471ba7955d827df34d58b0126
hbjORbj <sldisek783@gmail.com>
2024-12-18 12:22:11 -0500

    remove pages router for /user pages

--- apps/web/pages/[user]/[type]/embed.tsx
+++
@@ -1,7 +0,0 @@
-import withEmbedSsr from "@lib/withEmbedSsr";
-
-import { getServerSideProps as _getServerSideProps } from "@server/lib/[user]/[type]/getServerSideProps";
-
-export const getServerSideProps = withEmbedSsr(_getServerSideProps);
-
-export { default } from "./index";
