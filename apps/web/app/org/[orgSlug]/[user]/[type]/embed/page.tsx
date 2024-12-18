756fc52fcc1b530d3ac7c1629b898d744ecc579d
hbjORbj <sldisek783@gmail.com>
2024-12-18 00:23:02 -0500

    fix orgSlug/user/type pages

--- apps/web/app/org/[orgSlug]/[user]/[type]/embed/page.tsx
+++ apps/web/app/org/[orgSlug]/[user]/[type]/embed/page.tsx
@@ -1,12 +1,14 @@
-import { type PageProps } from "@pages/org/[orgSlug]/[user]/[type]";
-import Page from "@pages/org/[orgSlug]/[user]/[type]/embed";
 import { withAppDirSsr } from "app/WithAppDirSsr";
 import withEmbedSsrAppDir from "app/WithEmbedSSR";
 import { WithLayout } from "app/layoutHOC";
 
 import { getServerSideProps } from "@lib/org/[orgSlug]/[user]/[type]/getServerSideProps";
 
-const getData = withAppDirSsr<PageProps>(getServerSideProps);
+import { Page, type OrgTypePageProps } from "../page";
+
+export { generateMetadata } from "../page";
+
+const getData = withAppDirSsr<OrgTypePageProps>(getServerSideProps);
 const getEmbedData = withEmbedSsrAppDir(getData);
 
-export default WithLayout({ getLayout: null, getData: getEmbedData, isBookingPage: true, Page });
+export default WithLayout({ getLayout: null, getData: getEmbedData, isBookingPage: true, ServerPage: Page });
--- apps/web/app/org/[orgSlug]/[user]/[type]/page.tsx
+++ apps/web/app/org/[orgSlug]/[user]/[type]/page.tsx
@@ -1,9 +1,53 @@
-import Page, { type PageProps } from "@pages/org/[orgSlug]/[user]/[type]";
 import { withAppDirSsr } from "app/WithAppDirSsr";
+import type { PageProps } from "app/_types";
+import { _generateMetadata } from "app/_utils";
 import { WithLayout } from "app/layoutHOC";
+import { cookies, headers } from "next/headers";
 
+import { orgDomainConfig } from "@calcom/features/ee/organizations/lib/orgDomains";
+import { EventRepository } from "@calcom/lib/server/repository/event";
+
+import { buildLegacyCtx } from "@lib/buildLegacyCtx";
 import { getServerSideProps } from "@lib/org/[orgSlug]/[user]/[type]/getServerSideProps";
 
-const getData = withAppDirSsr<PageProps>(getServerSideProps);
+import type { PageProps as TeamTypePageProps } from "~/team/type-view";
+import TeamTypePage from "~/team/type-view";
+import UserTypePage from "~/users/views/users-type-public-view";
+import type { PageProps as UserTypePageProps } from "~/users/views/users-type-public-view";
 
-export default WithLayout({ getLayout: null, getData, Page });
+export type OrgTypePageProps = UserTypePageProps | TeamTypePageProps;
+const getData = withAppDirSsr<OrgTypePageProps>(getServerSideProps);
+
+export const generateMetadata = async ({ params, searchParams }: PageProps) => {
+  const legacyCtx = buildLegacyCtx(headers(), cookies(), params, searchParams);
+  const props = await getData(legacyCtx);
+
+  const { booking, user: username, slug: eventSlug } = props;
+  const rescheduleUid = booking?.uid;
+  const { currentOrgDomain, isValidOrgDomain } = orgDomainConfig(legacyCtx.req, legacyCtx.params?.orgSlug);
+
+  const event = await EventRepository.getPublicEvent({
+    username,
+    eventSlug,
+    isTeamEvent: false,
+    org: isValidOrgDomain ? currentOrgDomain : null,
+    fromRedirectOfNonOrgLink: legacyCtx.query.orgRedirection === "true",
+  });
+
+  const profileName = event?.profile?.name ?? "";
+  const title = event?.title ?? "";
+
+  return await _generateMetadata(
+    (t) => `${rescheduleUid && !!booking ? t("reschedule") : ""} ${title} | ${profileName}`,
+    (t) => `${rescheduleUid ? t("reschedule") : ""} ${title}`
+  );
+};
+
+export const Page = async (props: OrgTypePageProps) => {
+  if ((props as TeamTypePageProps)?.teamId) {
+    return <TeamTypePage {...(props as TeamTypePageProps)} />;
+  }
+  return <UserTypePage {...(props as UserTypePageProps)} />;
+};
+
+export default WithLayout({ getLayout: null, getData, ServerPage: Page, isBookingPage: true });
