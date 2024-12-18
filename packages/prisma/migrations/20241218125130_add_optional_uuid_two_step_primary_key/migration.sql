c1065e0e551536dee4310f25ba0ffa07cd74fba5
Alex van Andel <me@alexvanandel.com>
2024-12-18 13:50:47 +0000

    revert unique

--- packages/prisma/migrations/20241218125130_add_optional_uuid_two_step_primary_key/migration.sql
+++ packages/prisma/migrations/20241218125130_add_optional_uuid_two_step_primary_key/migration.sql
@@ -1,5 +1,5 @@
 -- AlterTable
-ALTER TABLE "SelectedCalendar" ADD COLUMN     "id" TEXT UNIQUE;
+ALTER TABLE "SelectedCalendar" ADD COLUMN     "id" TEXT;
 
 -- Update all historical rows (id) to `gen_random_uuid`
 UPDATE "SelectedCalendar" SET "id" = gen_random_uuid() WHERE "id" IS NULL;
