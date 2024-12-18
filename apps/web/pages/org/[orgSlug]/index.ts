704a6675638ec8c4082ff1e5bddd56d9dfb905b9
hbjORbj <sldisek783@gmail.com>
2024-12-17 23:33:00 -0500

    update imports

--- apps/web/pages/org/[orgSlug]/index.ts
+++ apps/web/pages/org/[orgSlug]/index.ts
@@ -1,1 +1,2 @@
-export { getServerSideProps, default } from "@pages/team/[slug]";
+export { default } from "~/team/team-view";
+export { getServerSideProps } from "@lib/team/[slug]/getServerSideProps";
