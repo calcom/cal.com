a4b7f3563e95922a4adb504e0fea01ab5f8b695b
Benny Joo <sldisek783@gmail.com>
2024-12-20 16:58:18 -0500

    fix test

--- apps/web/playwright/manage-booking-questions.e2e.ts
+++ apps/web/playwright/manage-booking-questions.e2e.ts
@@ -231,7 +231,7 @@ test.describe("Manage Booking Questions", () => {
   });
 
   test.describe("For Team EventType", () => {
-    test("Do a booking with a user added question and verify a few thing in b/w", async ({
+    test("Do a booking with a user added question and verify a few thing in b/w @test", async ({
       page,
       users,
       context,
@@ -258,7 +258,7 @@ test.describe("Manage Booking Questions", () => {
 
       await test.step("Go to First Team Event", async () => {
         await page.getByTestId(`horizontal-tab-${team?.name}`).click();
-        await page.waitForLoadState("networkidle");
+        await page.waitForSelector("[data-testid=event-types] li a");
 
         const $eventTypes = page.locator("[data-testid=event-types]").locator("li a");
         const firstEventTypeElement = $eventTypes.first();
