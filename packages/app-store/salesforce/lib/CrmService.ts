8fff823714c7bb7b15ba1e13d2b1cabf04e1faff
Joe <j.auyeung419@gmail.com>
2024-12-20 01:16:57 -0500

    Refactor prio contacts when creating events on leads

--- packages/app-store/salesforce/lib/CrmService.ts
+++ packages/app-store/salesforce/lib/CrmService.ts
@@ -379,14 +379,13 @@ export default class SalesforceCRMService implements CRM {
       // If falling back to contacts, check for the contact before returning the leads or empty array
       if (
         appOptions.createEventOn === SalesforceRecordEnum.LEAD &&
-        appOptions.createEventOnLeadCheckForContact &&
-        !forRoundRobinSkip
+        appOptions.createEventOnLeadCheckForContact && !forRoundRobinSkip
       ) {
         // Get any matching contacts
         const contactSearch = await conn.query(
-          `SELECT Id, Email, OwnerId, Owner.Email FROM ${
-            SalesforceRecordEnum.CONTACT
-          } WHERE Email IN ('${emailArray.join("','")}')`
+          `SELECT Id, Email, OwnerId, Owner.Email FROM ${SalesforceRecordEnum.CONTACT} WHERE Email IN ('${emailArray.join(
+            "','"
+          )}')`
         );
 
         if (contactSearch?.records?.length > 0) {
