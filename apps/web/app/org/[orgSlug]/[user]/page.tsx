8b6d226d6ae1d2c6ef7e8225a3d96df08fc0307e
hbjORbj <sldisek783@gmail.com>
2024-12-22 01:04:38 -0500

    fix OG images for org/user and team/slug

--- apps/web/app/org/[orgSlug]/[user]/page.tsx
+++ apps/web/app/org/[orgSlug]/[user]/page.tsx
@@ -4,6 +4,11 @@ import { _generateMetadata } from "app/_utils";
 import { WithLayout } from "app/layoutHOC";
 import { cookies, headers } from "next/headers";
 
+import { getServerSessionForAppDir } from "@calcom/features/auth/lib/get-server-session-for-app-dir";
+import { constructMeetingImage } from "@calcom/lib/OgImages";
+import { SEO_IMG_OGIMG } from "@calcom/lib/constants";
+import { getOrgOrTeamAvatar } from "@calcom/lib/defaultAvatarImage";
+
 import { buildLegacyCtx } from "@lib/buildLegacyCtx";
 import { getServerSideProps } from "@lib/org/[orgSlug]/[user]/getServerSideProps";
 
@@ -18,17 +23,48 @@ const getData = withAppDirSsr<OrgPageProps>(getServerSideProps);
 export const generateMetadata = async ({ params, searchParams }: PageProps) => {
   const legacyCtx = buildLegacyCtx(headers(), cookies(), params, searchParams);
   const props = await getData(legacyCtx);
-
+  let metadata, meeting;
   if ((props as TeamPageProps)?.team) {
-    return await _generateMetadata(
-      (t) => (props as TeamPageProps).team.name || t("nameless_team"),
-      (t) => (props as TeamPageProps).team.name || t("nameless_team")
+    const { team, markdownStrippedBio } = props as TeamPageProps;
+    meeting = {
+      title: markdownStrippedBio ?? "",
+      profile: {
+        name: `${team.name}`,
+        image: getOrgOrTeamAvatar(team),
+      },
+    };
+
+    metadata = await _generateMetadata(
+      (t) => team.name ?? t("nameless_team"),
+      (t) => team.name ?? t("nameless_team")
     );
+  } else {
+    const { profile, markdownStrippedBio } = props as UserPageProps;
+    metadata = await _generateMetadata(
+      () => profile.name,
+      () => markdownStrippedBio
+    );
+    const session = await getServerSessionForAppDir();
+    const user = session?.user;
+    meeting = {
+      title: markdownStrippedBio,
+      profile: { name: `${profile.name}`, image: user?.avatarUrl ?? null },
+      users: [
+        {
+          username: `${profile.username ?? ""}`,
+          name: `${profile.name ?? ""}`,
+        },
+      ],
+    };
   }
-  return await _generateMetadata(
-    (t) => (props as UserPageProps).profile.name,
-    (t) => (props as UserPageProps).markdownStrippedBio
-  );
+  const image = SEO_IMG_OGIMG + constructMeetingImage(meeting);
+  return {
+    ...metadata,
+    openGraph: {
+      ...metadata.openGraph,
+      images: [image],
+    },
+  };
 };
 
 export const Page = async (props: OrgPageProps) => {
--- apps/web/app/team/[slug]/page.tsx
+++ apps/web/app/team/[slug]/page.tsx
@@ -4,6 +4,10 @@ import { _generateMetadata } from "app/_utils";
 import { WithLayout } from "app/layoutHOC";
 import { cookies, headers } from "next/headers";
 
+import { constructMeetingImage } from "@calcom/lib/OgImages";
+import { SEO_IMG_OGIMG } from "@calcom/lib/constants";
+import { getOrgOrTeamAvatar } from "@calcom/lib/defaultAvatarImage";
+
 import { buildLegacyCtx } from "@lib/buildLegacyCtx";
 import { getServerSideProps } from "@lib/team/[slug]/getServerSideProps";
 
@@ -11,12 +15,30 @@ import type { PageProps } from "~/team/team-view";
 import LegacyPage from "~/team/team-view";
 
 export const generateMetadata = async ({ params, searchParams }: _PageProps) => {
-  const props = await getData(buildLegacyCtx(headers(), cookies(), params, searchParams));
-
-  return await _generateMetadata(
-    (t) => props.team.name || t("nameless_team"),
-    (t) => props.team.name || t("nameless_team")
+  const { team, markdownStrippedBio } = await getData(
+    buildLegacyCtx(headers(), cookies(), params, searchParams)
   );
+
+  const meeting = {
+    title: markdownStrippedBio ?? "",
+    profile: {
+      name: `${team.name}`,
+      image: getOrgOrTeamAvatar(team),
+    },
+  };
+  const image = SEO_IMG_OGIMG + constructMeetingImage(meeting);
+
+  const metadata = await _generateMetadata(
+    (t) => team.name || t("nameless_team"),
+    (t) => team.name || t("nameless_team")
+  );
+  return {
+    ...metadata,
+    openGraph: {
+      ...metadata.openGraph,
+      images: [image],
+    },
+  };
 };
 
 const getData = withAppDirSsr<PageProps>(getServerSideProps);
