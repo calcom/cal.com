d7acf671dee58590ae0955bc0e9210667e9fdb7a
hbjORbj <sldisek783@gmail.com>
2024-12-22 00:49:01 -0500

    fix OG image for [user]

--- apps/web/app/[user]/page.tsx
+++ apps/web/app/[user]/page.tsx
@@ -4,6 +4,10 @@ import { _generateMetadata } from "app/_utils";
 import { WithLayout } from "app/layoutHOC";
 import { headers, cookies } from "next/headers";
 
+import { getServerSessionForAppDir } from "@calcom/feature-auth/lib/get-server-session-for-app-dir";
+import { constructMeetingImage } from "@calcom/lib/OgImages";
+import { SEO_IMG_OGIMG } from "@calcom/lib/constants";
+
 import { buildLegacyCtx } from "@lib/buildLegacyCtx";
 
 import { getServerSideProps } from "@server/lib/[user]/getServerSideProps";
@@ -15,10 +19,31 @@ export const generateMetadata = async ({ params, searchParams }: PageProps) => {
   const props = await getData(buildLegacyCtx(headers(), cookies(), params, searchParams));
 
   const { profile, markdownStrippedBio } = props;
-  return await _generateMetadata(
+  const metadata = await _generateMetadata(
     () => profile.name,
     () => markdownStrippedBio
   );
+  const session = await getServerSessionForAppDir();
+  const user = session?.user;
+
+  const meeting = {
+    title: markdownStrippedBio,
+    profile: { name: `${profile.name}`, image: user?.avatarUrl ?? null },
+    users: [
+      {
+        username: `${profile.username ?? ""}`,
+        name: `${profile.name ?? ""}`,
+      },
+    ],
+  };
+  const image = SEO_IMG_OGIMG + constructMeetingImage(meeting);
+  return {
+    ...metadata,
+    openGraph: {
+      ...metadata.openGraph,
+      images: [image],
+    },
+  };
 };
 
 const getData = withAppDirSsr<LegacyPageProps>(getServerSideProps);
