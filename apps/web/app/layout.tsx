883a0f503e0ce80baa85f440678ffbd889b433e8
hbjORbj <sldisek783@gmail.com>
2024-12-22 19:59:48 -0500

    index and follow must be true by default

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -26,8 +26,8 @@ export const generateMetadata = () =>
     twitterCreator: "@calcom",
     twitterSite: "@calcom",
     robots: {
-      index: false,
-      follow: false,
+      index: true,
+      follow: true,
     },
   });
 
