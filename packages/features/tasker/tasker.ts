7b653e133a85becc75e3ebf84c1f7303e03f13f3
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-12-23 23:24:11 -0500

    Change type to expect string from tasker

--- packages/features/tasker/tasker.ts
+++ packages/features/tasker/tasker.ts
@@ -17,7 +17,7 @@ type TaskPayloads = {
   translateEventTypeData: z.infer<
     typeof import("./tasks/translateEventTypeData").ZTranslateEventDataPayloadSchema
   >;
-  createCRMEvent: z.infer<typeof import("./tasks/crm/schema").createCRMEventSchema>;
+  createCRMEvent: string;
 };
 export type TaskTypes = keyof TaskPayloads;
 export type TaskHandler = (payload: string) => Promise<void>;
