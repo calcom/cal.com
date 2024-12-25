fafe33ba7d9c3358067004e940bead7362899c42
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-12-25 01:03:48 -0500

    Handle no enabled credentials and no actions

--- packages/app-store/routing-forms/pages/incomplete-booking/[...appPages].tsx
+++ packages/app-store/routing-forms/pages/incomplete-booking/[...appPages].tsx
@@ -1,11 +1,13 @@
 import Shell from "@calcom/features/shell/Shell";
 import { trpc } from "@calcom/trpc/react";
 import type { inferSSRProps } from "@calcom/types/inferSSRProps";
+import { EmptyScreen } from "@calcom/ui";
 
 import SingleForm, {
   getServerSidePropsForSingleFormView as getServerSideProps,
 } from "../../components/SingleForm";
 import type { RoutingFormWithResponseCount } from "../../components/SingleForm";
+import { enabledIncompleteBookingApps } from "../../lib/enabledIncompleteBookingApps";
 
 function Page({ form }: { form: RoutingFormWithResponseCount }) {
   const { data, isLoading } = trpc.viewer.appRoutingForms.getIncompleteBookingSettings.useQuery({
@@ -15,7 +17,23 @@ function Page({ form }: { form: RoutingFormWithResponseCount }) {
   if (isLoading) {
     return <div>Loading...</div>;
   }
-  return <h1>Incomplete Booking</h1>;
+
+  // Check to see if the user has any compatable credentials
+  if (
+    !data?.credentials.some((credential) => enabledIncompleteBookingApps.includes(credential?.appId ?? ""))
+  ) {
+    return <div>No apps installed that support this feature</div>;
+  }
+
+  if (data?.incompleteBookingActions.length === 0) {
+    return (
+      <EmptyScreen
+        Icon="file-text"
+        headline="Create new action"
+        description="Create a new action when the form is filled but a booking was created"
+      />
+    );
+  }
 }
 
 export default function IncompleteBookingPage({
