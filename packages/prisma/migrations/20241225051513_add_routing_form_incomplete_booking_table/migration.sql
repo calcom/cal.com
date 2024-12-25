eaaacc2d54032155eabe944db90f91e525ded182
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-12-25 00:50:00 -0500

    Add booking incomplete actions table

---
+++ packages/prisma/migrations/20241225051513_add_routing_form_incomplete_booking_table/migration.sql
@@ -0,0 +1,15 @@
+-- CreateEnum
+CREATE TYPE "IncompleteBookingActionType" AS ENUM ('SALESFORCE');
+
+-- CreateTable
+CREATE TABLE "App_RoutingForms_IncompleteBooking_Actions" (
+    "id" SERIAL NOT NULL,
+    "formId" TEXT NOT NULL,
+    "actionType" "IncompleteBookingActionType" NOT NULL,
+    "data" JSONB NOT NULL,
+
+    CONSTRAINT "App_RoutingForms_IncompleteBooking_Actions_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "App_RoutingForms_IncompleteBooking_Actions" ADD CONSTRAINT "App_RoutingForms_IncompleteBooking_Actions_formId_fkey" FOREIGN KEY ("formId") REFERENCES "App_RoutingForms_Form"("id") ON DELETE CASCADE ON UPDATE CASCADE;
