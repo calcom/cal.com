9065a4b73d8e63149f550a10b1650ff5e54b127d
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-12-25 22:51:20 -0500

    Add new write record entries

--- packages/app-store/routing-forms/pages/incomplete-booking/[...appPages].tsx
+++ packages/app-store/routing-forms/pages/incomplete-booking/[...appPages].tsx
@@ -1,7 +1,15 @@
+import { useState, useEffect } from "react";
+import type z from "zod";
+
+import { WhenToWriteToRecord, SalesforceFieldType } from "@calcom/app-store/salesforce/lib/enums";
+import type { writeToRecordDataSchema as salesforceWriteToRecordDataSchema } from "@calcom/app-store/salesforce/zod";
+import { routingFormIncompleteBookingDataSchema as salesforceRoutingFormIncompleteBookingDataSchema } from "@calcom/app-store/salesforce/zod";
 import Shell from "@calcom/features/shell/Shell";
+import { useLocale } from "@calcom/lib/hooks/useLocale";
+import { IncompleteBookingActionType } from "@calcom/prisma/enums";
 import { trpc } from "@calcom/trpc/react";
 import type { inferSSRProps } from "@calcom/types/inferSSRProps";
-import { EmptyScreen } from "@calcom/ui";
+import { Switch, InputField, Button, Select, showToast } from "@calcom/ui";
 
 import SingleForm, {
   getServerSidePropsForSingleFormView as getServerSideProps,
@@ -9,10 +17,60 @@ import SingleForm, {
 import type { RoutingFormWithResponseCount } from "../../components/SingleForm";
 import { enabledIncompleteBookingApps } from "../../lib/enabledIncompleteBookingApps";
 
+interface IncompleteBookingActionData {
+  field: string;
+  fieldType: string;
+  value: string;
+  whenToWrite: WhenToWriteToRecord;
+}
+
 function Page({ form }: { form: RoutingFormWithResponseCount }) {
+  const { t } = useLocale();
   const { data, isLoading } = trpc.viewer.appRoutingForms.getIncompleteBookingSettings.useQuery({
     formId: form.id,
   });
+
+  const [incompleteBookingWriteToRecordEntries, setIncompleteBookingWriteToRecordEntries] = useState<
+    z.infer<typeof salesforceWriteToRecordDataSchema> | []
+  >([]);
+
+  // Handle just Salesforce for now but need to expand this to other apps
+  const [salesforceActionEnabled, setSalesforceActionEnabled] = useState<boolean>(false);
+
+  const fieldTypeOptions = [{ label: t("text"), value: SalesforceFieldType.TEXT }];
+
+  const [selectedFieldType, setSelectedFieldType] = useState(fieldTypeOptions[0]);
+
+  const whenToWriteToRecordOptions = [
+    { label: t("on_every_booking"), value: WhenToWriteToRecord.EVERY_BOOKING },
+    { label: t("only_if_field_is_empty"), value: WhenToWriteToRecord.FIELD_EMPTY },
+  ];
+
+  const [selectedWhenToWrite, setSelectedWhenToWrite] = useState(whenToWriteToRecordOptions[0]);
+
+  const [newSalesforceAction, setNewSalesforceAction] = useState({
+    field: "",
+    fieldType: selectedFieldType.value,
+    value: "",
+    whenToWrite: WhenToWriteToRecord.FIELD_EMPTY,
+  });
+
+  useEffect(() => {
+    const salesforceAction = data?.incompleteBookingActions.find(
+      (action) => action.actionType === IncompleteBookingActionType.SALESFORCE
+    );
+
+    if (salesforceAction) {
+      setSalesforceActionEnabled(salesforceAction.enabled);
+
+      const parsedSalesforceActionData = salesforceRoutingFormIncompleteBookingDataSchema.safeParse(
+        salesforceAction.data
+      );
+      if (parsedSalesforceActionData.success) {
+        setIncompleteBookingWriteToRecordEntries(parsedSalesforceActionData.data?.writeToRecordObject ?? []);
+      }
+    }
+  }, [data]);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -25,15 +83,161 @@ function Page({ form }: { form: RoutingFormWithResponseCount }) {
     return <div>No apps installed that support this feature</div>;
   }
 
-  if (data?.incompleteBookingActions.length === 0) {
-    return (
-      <EmptyScreen
-        Icon="file-text"
-        headline="Create new action"
-        description="Create a new action when the form is filled but a booking was created"
-      />
-    );
-  }
+  return (
+    <>
+      <div className="bg-default border-subtle rounded-md border p-8">
+        <Switch
+          labelOnLeading
+          label="Write to Salesforce contact/lead record"
+          checked={salesforceActionEnabled}
+          onCheckedChange={(checked) => {
+            setSalesforceActionEnabled(checked);
+          }}
+        />
+        {salesforceActionEnabled ? (
+          <div className="ml-2 mt-2">
+            <div className="grid grid-cols-5 gap-4">
+              <div>{t("field_name")}</div>
+              <div>{t("field_type")}</div>
+              <div>{t("value")}</div>
+              <div>{t("when_to_write")}</div>
+            </div>
+            <div>
+              {incompleteBookingWriteToRecordEntries.map((action) => (
+                <div className="mt-2 grid grid-cols-5 gap-4" key={action.field}>
+                  <div>
+                    <InputField value={action.field} readOnly />
+                  </div>
+                  <div>
+                    <Select
+                      value={fieldTypeOptions.find((option) => option.value === action.fieldType)}
+                      isDisabled={true}
+                    />
+                  </div>
+                  <div>
+                    <InputField value={action.value} readOnly />
+                  </div>
+                  <div>
+                    <Select
+                      value={whenToWriteToRecordOptions.find((option) => option.value === action.whenToWrite)}
+                      isDisabled={true}
+                    />
+                  </div>
+                  <div>
+                    <Button
+                      StartIcon="trash"
+                      variant="icon"
+                      color="destructive"
+                      onClick={() => {
+                        const newActions = incompleteBookingWriteToRecordEntries.filter(
+                          (action) => action.field !== action.field
+                        );
+                        setIncompleteBookingWriteToRecordEntries(newActions);
+                      }}
+                    />
+                  </div>
+                </div>
+              ))}
+              <div className="mt-2 grid grid-cols-5 gap-4">
+                <div>
+                  <InputField
+                    value={newSalesforceAction.field}
+                    onChange={(e) =>
+                      setNewSalesforceAction({
+                        ...newSalesforceAction,
+                        field: e.target.value,
+                      })
+                    }
+                  />
+                </div>
+                <div>
+                  <Select
+                    options={fieldTypeOptions}
+                    value={selectedFieldType}
+                    onChange={(e) => {
+                      if (e) {
+                        setSelectedFieldType(e);
+                        setNewSalesforceAction({
+                          ...newSalesforceAction,
+                          fieldType: e.value,
+                        });
+                      }
+                    }}
+                  />
+                </div>
+                <div>
+                  <InputField
+                    value={newSalesforceAction.value}
+                    onChange={(e) =>
+                      setNewSalesforceAction({
+                        ...newSalesforceAction,
+                        value: e.target.value,
+                      })
+                    }
+                  />
+                </div>
+                <div>
+                  <Select
+                    options={whenToWriteToRecordOptions}
+                    value={selectedWhenToWrite}
+                    onChange={(e) => {
+                      if (e) {
+                        setSelectedWhenToWrite(e);
+                        setNewSalesforceAction({
+                          ...newSalesforceAction,
+                          whenToWrite: e.value,
+                        });
+                      }
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+            <Button
+              className="mt-2"
+              size="sm"
+              disabled={
+                !(
+                  newSalesforceAction.field &&
+                  newSalesforceAction.fieldType &&
+                  newSalesforceAction.value &&
+                  newSalesforceAction.whenToWrite
+                )
+              }
+              onClick={() => {
+                if (
+                  Object.keys(incompleteBookingWriteToRecordEntries).includes(
+                    newSalesforceAction.field.trim()
+                  )
+                ) {
+                  showToast("Field already exists", "error");
+                  return;
+                }
+
+                setIncompleteBookingWriteToRecordEntries([
+                  ...incompleteBookingWriteToRecordEntries,
+                  {
+                    field: newSalesforceAction.field,
+                    fieldType: newSalesforceAction.fieldType,
+                    value: newSalesforceAction.value,
+                    whenToWrite: newSalesforceAction.whenToWrite,
+                  },
+                ]);
+
+                setNewSalesforceAction({
+                  field: "",
+                  fieldType: selectedFieldType.value,
+                  value: "",
+                  whenToWrite: WhenToWriteToRecord.FIELD_EMPTY,
+                });
+              }}>
+              {t("add_new_field")}
+            </Button>
+          </div>
+        ) : null}
+      </div>
+    </>
+  );
 }
 
 export default function IncompleteBookingPage({
