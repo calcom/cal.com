79b746e7248eb02ab3ce71ef8f265ace809359a1
Joe Au-Yeung <j.auyeung419@gmail.com>
2025-01-03 14:12:41 -0500

    Create create input type

---
+++ packages/features/bookings/listener/types.d.ts
@@ -0,0 +1,37 @@
+import type { DestinationCalendar } from "@prisma/client";
+import type { Prisma } from "@prisma/client";
+import type { TFunction } from "next-i18next";
+
+import type { EventNameObjectType } from "@calcom/core/event";
+import type { getAllCredentials } from "@calcom/features/bookings/lib/getAllCredentialsForUsersOnEvent/getAllCredentials";
+import type { getEventTypesFromDB } from "@calcom/features/bookings/lib/handleNewBooking/getEventTypesFromDB";
+import type { CalendarEvent, AppsStatus } from "@calcom/types/Calendar";
+
+export type BookingListenerCreateInput = {
+  evt: CalendarEvent;
+  allCredentials: Awaited<ReturnType<typeof getAllCredentials>>;
+  organizerUser: {
+    id: number;
+    email: string;
+    destinationCalendar: DestinationCalendar | null;
+    username: string | null;
+  };
+  eventType: Awaited<ReturnType<typeof getEventTypesFromDB>>;
+  tOrganizer: TFunction;
+  booking: {
+    id: number;
+    startTime: Date;
+    endTime: Date;
+    location?: string | null;
+    appsStatus?: AppsStatus[];
+    iCalUID: string | null;
+    description: string | null;
+    customInputs: Prisma.JsonValue | null;
+    metadata: Prisma.JsonValue | null;
+    smsReminderNumber?: string | null;
+  };
+  eventNameObject: EventNameObjectType;
+  teamId?: number | null;
+  platformClientId?: string;
+  bookerUrl: string;
+};
