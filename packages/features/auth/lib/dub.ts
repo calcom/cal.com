cb12220de1c9294c4aaaa89a9c841555a08e2a01
Steven Tey <stevensteel97@gmail.com>
2025-01-02 20:23:26 -0800

    [FEAT] Use dub.customer to issue dual-sided incentives

--- packages/features/auth/lib/dub.ts
+++ packages/features/auth/lib/dub.ts
@@ -3,3 +3,14 @@ import { Dub } from "dub";
 export const dub = new Dub({
   token: process.env.DUB_API_KEY,
 });
+
+// fetch Dub customer using their external ID (ID in our database)
+export const getDubCustomer = async (userId: string) => {
+  try {
+    return await dub.customers.get({
+      id: `ext_${userId}`,
+    });
+  } catch (error) {
+    return null;
+  }
+};
