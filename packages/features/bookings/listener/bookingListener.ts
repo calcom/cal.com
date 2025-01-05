aab70beda125d5f20f7695178a6e0fcfb97a1868
Joe <j.auyeung419@gmail.com>
2025-01-04 22:09:05 -0500

    Skip trigger if booking created through platform

--- packages/features/bookings/listener/bookingListener.ts
+++ packages/features/bookings/listener/bookingListener.ts
@@ -6,7 +6,7 @@ import type { BookingListenerCreateInput } from "./types";
 class BookingListener {
   static async create(bookingListenerCreateInput: BookingListenerCreateInput) {
     console.log("🚀 ~ BookingListener ~ TRIGGER_PROJECT_ID:", process.env.TRIGGER_PROJECT_ID);
-    if (process.env.TRIGGER_PROJECT_ID) {
+    if (process.env.TRIGGER_PROJECT_ID && !bookingListenerCreateInput.platformClientId) {
       await tasker.create("bookingListener-create", { bookingId: bookingListenerCreateInput.booking.id });
       return;
     }
