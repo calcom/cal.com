19753fa8806d4f22c5254e9187b7ef1c21dab405
hbjORbj <sldisek783@gmail.com>
2025-01-05 21:15:09 -0500

    fix type check in apps/[slug]

--- apps/web/app/apps/[slug]/setup/page.tsx
+++ apps/web/app/apps/[slug]/setup/page.tsx
@@ -2,15 +2,25 @@ import { withAppDirSsr } from "app/WithAppDirSsr";
 import type { PageProps as _PageProps } from "app/_types";
 import { _generateMetadata } from "app/_utils";
 import { WithLayout } from "app/layoutHOC";
+import { notFound } from "next/navigation";
+import { z } from "zod";
 
 import { getServerSideProps } from "@calcom/app-store/_pages/setup/_getServerSideProps";
 
 import Page, { type PageProps } from "~/apps/[slug]/setup/setup-view";
 
-export const generateMetadata = async ({ params, searchParams }: _PageProps) => {
-  const { slug } = { ...params, ...searchParams };
+const paramsSchema = z.object({
+  slug: z.string(),
+});
+
+export const generateMetadata = async ({ params }: _PageProps) => {
+  const p = paramsSchema.safeParse(params);
+
+  if (!p.success) {
+    return notFound();
+  }
   const metadata = await _generateMetadata(
-    () => `${slug}`,
+    () => `${p.data.slug}`,
     () => ""
   );
   return {
