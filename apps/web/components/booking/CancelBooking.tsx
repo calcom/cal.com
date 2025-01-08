d0dc731eb372cade275144cc11a3cd57ad43a52a
Felippe Fernandes <lfelippeeng@gmail.com>
2025-01-08 09:28:09 -0300

    feat: cancelBooking errors log

--- apps/web/components/booking/CancelBooking.tsx
+++ apps/web/components/booking/CancelBooking.tsx
@@ -3,8 +3,9 @@ import { useCallback, useState } from "react";
 
 import { sdkActionManager } from "@calcom/embed-core/embed-iframe";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
+import { collectPageParameters, telemetryEventTypes, useTelemetry } from "@calcom/lib/telemetry";
 import type { RecurringEvent } from "@calcom/types/Calendar";
-import { Button } from "@calcom/ui";
+import { Button, Icon, TextArea } from "@calcom/ui";
 
 type Props = {
   booking: {
@@ -34,8 +35,6 @@ type Props = {
   };
 };
 
-// TODO
-
 export default function CancelBooking(props: Props) {
   const [cancellationReason, setCancellationReason] = useState<string>("");
   const { t } = useLocale();
@@ -54,7 +53,7 @@ export default function CancelBooking(props: Props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  console.error({
+  console.log({
     cancellationReason,
     props,
     error,
@@ -66,19 +65,19 @@ export default function CancelBooking(props: Props) {
     <>
       {error && (
         <div className="mt-8">
-          {/* <div className="bg-error mx-auto flex h-12 w-12 items-center justify-center rounded-full">
+          <div className="bg-error mx-auto flex h-12 w-12 items-center justify-center rounded-full">
             <Icon name="x" className="h-6 w-6 text-red-600" />
           </div>
           <div className="mt-3 text-center sm:mt-5">
             <h3 className="text-emphasis text-lg font-medium leading-6" id="modal-title">
               {error}
             </h3>
-          </div> */}
+          </div>
         </div>
       )}
       {!error && (
         <div className="mt-5 sm:mt-6">
-          {/* <label className="text-default font-medium">{t("cancellation_reason")}</label>
+          <label className="text-default font-medium">{t("cancellation_reason")}</label>
           <TextArea
             data-testid="cancel_reason"
             ref={cancelBookingRef}
@@ -95,53 +94,55 @@ export default function CancelBooking(props: Props) {
                 color="secondary"
                 onClick={() => props.setIsCancellationMode(false)}>
                 Voltar
-              </Button> */}
-          <Button
-            data-testid="confirm_cancel"
-            onClick={async () => {
-              setLoading(true);
+              </Button>
+              <Button
+                data-testid="confirm_cancel"
+                onClick={async () => {
+                  setLoading(true);
 
-              // telemetry.event(telemetryEventTypes.bookingCancelled, collectPageParameters());
+                  telemetry.event(telemetryEventTypes.bookingCancelled, collectPageParameters());
 
-              const res = await fetch("/api/cancel", {
-                body: JSON.stringify({
-                  uid: booking?.uid,
-                  cancellationReason: cancellationReason,
-                  allRemainingBookings,
-                  // @NOTE: very important this shouldn't cancel with number ID use uid instead
-                  seatReferenceUid,
-                  cancelledBy: currentUserEmail,
-                }),
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                method: "POST",
-              });
+                  const res = await fetch("/api/cancel", {
+                    body: JSON.stringify({
+                      uid: booking?.uid,
+                      cancellationReason: cancellationReason,
+                      allRemainingBookings,
+                      // @NOTE: very important this shouldn't cancel with number ID use uid instead
+                      seatReferenceUid,
+                      cancelledBy: currentUserEmail,
+                    }),
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    method: "POST",
+                  });
 
-              const bookingWithCancellationReason = {
-                ...(bookingCancelledEventProps.booking as object),
-                cancellationReason,
-              } as unknown;
+                  const bookingWithCancellationReason = {
+                    ...(bookingCancelledEventProps.booking as object),
+                    cancellationReason,
+                  } as unknown;
 
-              if (res.status >= 200 && res.status < 300) {
-                // tested by apps/web/playwright/booking-pages.e2e.ts
-                sdkActionManager?.fire("bookingCancelled", {
-                  ...bookingCancelledEventProps,
-                  booking: bookingWithCancellationReason,
-                });
-                router.refresh();
-              } else {
-                setLoading(false);
-                setError(
-                  `${t("error_with_status_code_occured", { status: res.status })} ${t("please_try_again")}`
-                );
-              }
-            }}
-            loading={loading}>
-            {props.allRemainingBookings ? t("cancel_all_remaining") : t("cancel_event")}
-          </Button>
-          {/* </div>
-          </div> */}
+                  if (res.status >= 200 && res.status < 300) {
+                    // tested by apps/web/playwright/booking-pages.e2e.ts
+                    sdkActionManager?.fire("bookingCancelled", {
+                      ...bookingCancelledEventProps,
+                      booking: bookingWithCancellationReason,
+                    });
+                    router.refresh();
+                  } else {
+                    setLoading(false);
+                    setError(
+                      `${t("error_with_status_code_occured", { status: res.status })} ${t(
+                        "please_try_again"
+                      )}`
+                    );
+                  }
+                }}
+                loading={loading}>
+                {props.allRemainingBookings ? t("cancel_all_remaining") : t("cancel_event")}
+              </Button>
+            </div>
+          </div>
         </div>
       )}
     </>
