fa86928fbd23c41e591fef9f54862e8b418adabe
Kartik Saini <kartik8942@gmail.com>
2025-01-13 21:49:48 +0530

    feat: use tasker cron job for scheduled webhook triggers

--- packages/features/tasker/api/cron.ts
+++ packages/features/tasker/api/cron.ts
@@ -1,14 +1,18 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
+import prisma from "@calcom/prisma";
+
 import tasker from "..";
+import { handleWebhookScheduledTriggers } from "../../webhooks/lib/handleWebhookScheduledTriggers";
 
 async function handler(request: NextRequest) {
   const authHeader = request.headers.get("authorization");
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     return new Response("Unauthorized", { status: 401 });
   }
-  await tasker.processQueue();
+  // Concurrent execution
+  await Promise.allSettled([tasker.processQueue(), handleWebhookScheduledTriggers(prisma)]);
   return NextResponse.json({ success: true });
 }
 
