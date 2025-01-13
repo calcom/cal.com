b20912e7a939ed04b073adf469b4468530d4fe1b
hbjORbj <sldisek783@gmail.com>
2025-01-13 07:40:48 -0500

    fix

--- apps/web/app/auth/new/page.tsx
+++ apps/web/app/auth/new/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
-export default async function Page() {
+export default function Page() {
   redirect(process.env.NEXT_PUBLIC_WEBAPP_URL || "https://app.cal.com");
 }
