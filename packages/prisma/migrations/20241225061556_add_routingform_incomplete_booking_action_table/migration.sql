316a3662aa2f18d9f96dc6c1d8627b0997c79abb
Joe <j.auyeung419@gmail.com>
2025-01-12 21:30:25 -0500

    Revert "Add `credentialId` to action record"
    This reverts commit 579f9ff4167b65aa987659e4e8f8c26f9e773317.

--- packages/prisma/migrations/20241225061556_add_routingform_incomplete_booking_action_table/migration.sql
+++ packages/prisma/migrations/20241225061556_add_routingform_incomplete_booking_action_table/migration.sql
@@ -8,13 +8,9 @@ CREATE TABLE "App_RoutingForms_IncompleteBooking_Actions" (
     "actionType" "IncompleteBookingActionType" NOT NULL,
     "data" JSONB NOT NULL,
     "enabled" BOOLEAN NOT NULL DEFAULT true,
-    "credentialId" INTEGER,
 
     CONSTRAINT "App_RoutingForms_IncompleteBooking_Actions_pkey" PRIMARY KEY ("id")
 );
 
 -- AddForeignKey
 ALTER TABLE "App_RoutingForms_IncompleteBooking_Actions" ADD CONSTRAINT "App_RoutingForms_IncompleteBooking_Actions_formId_fkey" FOREIGN KEY ("formId") REFERENCES "App_RoutingForms_Form"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "App_RoutingForms_IncompleteBooking_Actions" ADD CONSTRAINT "App_RoutingForms_IncompleteBooking_Actions_credentialId_fkey" FOREIGN KEY ("credentialId") REFERENCES "Credential"("id") ON DELETE CASCADE ON UPDATE CASCADE;
