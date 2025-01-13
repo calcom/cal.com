b4e7a0293f3f77a1af67df5a1d1549254abcd580
Joe <j.auyeung419@gmail.com>
2025-01-13 14:22:35 -0500

    Fix table name

--- packages/prisma/migrations/20250113170733_add_routingforms_incompletebooking_action_table/migration.sql
+++ packages/prisma/migrations/20250113170733_add_routingforms_incompletebooking_action_table/migration.sql
@@ -2,7 +2,7 @@
 CREATE TYPE "IncompleteBookingActionType" AS ENUM ('SALESFORCE');
 
 -- CreateTable
-CREATE TABLE "AppRouting_Forms_IncompleteBookingActions" (
+CREATE TABLE "App_RoutingForms_IncompleteBookingActions" (
     "id" SERIAL NOT NULL,
     "formId" TEXT NOT NULL,
     "actionType" "IncompleteBookingActionType" NOT NULL,
@@ -10,8 +10,8 @@ CREATE TABLE "AppRouting_Forms_IncompleteBookingActions" (
     "enabled" BOOLEAN NOT NULL DEFAULT true,
     "credentialId" INTEGER,
 
-    CONSTRAINT "AppRouting_Forms_IncompleteBookingActions_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "App_RoutingForms_IncompleteBookingActions_pkey" PRIMARY KEY ("id")
 );
 
 -- AddForeignKey
-ALTER TABLE "AppRouting_Forms_IncompleteBookingActions" ADD CONSTRAINT "AppRouting_Forms_IncompleteBookingActions_formId_fkey" FOREIGN KEY ("formId") REFERENCES "App_RoutingForms_Form"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "App_RoutingForms_IncompleteBookingActions" ADD CONSTRAINT "App_RoutingForms_IncompleteBookingActions_formId_fkey" FOREIGN KEY ("formId") REFERENCES "App_RoutingForms_Form"("id") ON DELETE CASCADE ON UPDATE CASCADE;
