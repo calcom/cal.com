2c3743df4796bf7a14e262b9fd76ea4f2e22a9fd
hbjORbj <sldisek783@gmail.com>
2025-01-14 04:39:58 -0500

    fix

--- packages/features/ee/payments/components/PaymentPage.tsx
+++ packages/features/ee/payments/components/PaymentPage.tsx
@@ -41,7 +41,7 @@ const AlbyPaymentComponent = dynamic(
 );
 
 const PaymentPage: FC<PaymentPageProps> = (props) => {
-  const { t } = useLocale();
+  const { t, i18n } = useLocale();
   const [is24h, setIs24h] = useState(isBrowserLocale24h());
   const [date, setDate] = useState(dayjs.utc(props.booking.startTime));
   const [timezone, setTimezone] = useState<string | null>(null);
