8a44bf3165f1bce30f5fc12a5ea15b77c62dc61d
Kartik Saini <kartik8942@gmail.com>
2025-01-14 19:09:28 +0530

    add reassigned subtite

--- packages/emails/src/templates/OrganizerCancelledEmail.tsx
+++ packages/emails/src/templates/OrganizerCancelledEmail.tsx
@@ -2,10 +2,12 @@ import { OrganizerScheduledEmail } from "./OrganizerScheduledEmail";
 
 export const OrganizerCancelledEmail = (props: React.ComponentProps<typeof OrganizerScheduledEmail>) => {
   const title = props.reassigned ? "event_request_reassigned" : "event_request_cancelled";
+  const subtitle = props.reassigned ? "event_reassigned_subtitle" : "";
   const subject = props.reassigned ? "event_reassigned_subject" : "event_cancelled_subject";
   return (
     <OrganizerScheduledEmail
       title={title}
+      subtitle={subtitle}
       headerType="xCircle"
       subject={subject}
       callToAction={null}
