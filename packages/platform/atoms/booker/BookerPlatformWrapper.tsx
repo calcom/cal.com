3007d084260e57f011bfd8ddb6bc2416a94ccc0a
Ryukemeister <sahalrajiv6900@gmail.com>
2025-01-17 15:14:41 +0530

    redirect on success from event types

--- packages/platform/atoms/booker/BookerPlatformWrapper.tsx
+++ packages/platform/atoms/booker/BookerPlatformWrapper.tsx
@@ -304,6 +304,10 @@ export const BookerPlatformWrapper = (
     onSuccess: (data) => {
       schedule.refetch();
       props.onCreateBookingSuccess?.(data);
+
+      if (!!event.data?.successRedirectUrl) {
+        window.location.href = event.data.successRedirectUrl;
+      }
     },
     onError: props.onCreateBookingError,
   });
