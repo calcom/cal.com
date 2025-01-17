015edd3c9b0adddfe3c3fec8f94ab93857ebbf8b
Benny Joo <sldisek783@gmail.com>
2025-01-17 06:46:59 -0500

    chore: migrate `/auth/new` (#18732)
    * move out of /future dir
    
    * remove legacy pages

--- apps/web/abTest/middlewareFactory.ts
+++ apps/web/abTest/middlewareFactory.ts
@@ -13,7 +13,6 @@ const ROUTES: [URLPattern, boolean][] = [
   ["/auth/forgot-password/:path*", process.env.APP_ROUTER_AUTH_FORGOT_PASSWORD_ENABLED === "1"] as const,
   ["/auth/login", process.env.APP_ROUTER_AUTH_LOGIN_ENABLED === "1"] as const,
   ["/auth/logout", process.env.APP_ROUTER_AUTH_LOGOUT_ENABLED === "1"] as const,
-  ["/auth/new", process.env.APP_ROUTER_AUTH_NEW_ENABLED === "1"] as const,
   ["/auth/saml-idp", process.env.APP_ROUTER_AUTH_SAML_ENABLED === "1"] as const,
   ["/auth/platform/:path*", process.env.APP_ROUTER_AUTH_PLATFORM_ENABLED === "1"] as const,
   ["/auth/oauth2/:path*", process.env.APP_ROUTER_AUTH_OAUTH2_ENABLED === "1"] as const,
