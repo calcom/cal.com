1fc3b0a8258198b7f9d8a3bd1b7af347cf4bfa0f
Ryukemeister <sahalrajiv6900@gmail.com>
2025-01-17 18:36:13 +0530

    fixup

--- packages/platform/atoms/hooks/useAvailableSlots.ts
+++ packages/platform/atoms/hooks/useAvailableSlots.ts
@@ -24,6 +24,7 @@ export const useAvailableSlots = ({
       rest.usernameList,
       rest.routedTeamMemberIds,
       rest.skipContactOwner,
+      rest.shouldServeCache,
     ],
     queryFn: () => {
       return http
