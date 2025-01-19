c6657e92ddbfbd703de5e2cab79b5b218106e629
tanayvaswani <vaswani.tanay9@gmail.com>
2025-01-19 22:01:17 +0000

    chore: technical debt clearing - remove getServerSideProps for /event-types/[type]

--- apps/web/app/event-types/[type]/page.tsx
+++ apps/web/app/event-types/[type]/page.tsx
@@ -1,24 +1,62 @@
-import { withAppDirSsr } from "app/WithAppDirSsr";
 import type { PageProps } from "app/_types";
 import { _generateMetadata } from "app/_utils";
 import { WithLayout } from "app/layoutHOC";
+import type { GetServerSidePropsContext } from "next";
 import { cookies, headers } from "next/headers";
+import { notFound, redirect } from "next/navigation";
 
+import { EventType } from "@calcom/atoms/monorepo";
+import { getServerSessionForAppDir } from "@calcom/features/auth/lib/get-server-session-for-app-dir";
+import logger from "@calcom/lib/logger";
+import { safeStringify } from "@calcom/lib/safeStringify";
+
+import { asStringOrThrow } from "@lib/asStringOrNull";
 import { buildLegacyCtx } from "@lib/buildLegacyCtx";
-import { getServerSideProps } from "@lib/event-types/[type]/getServerSideProps";
 
-import EventTypePageWrapper from "~/event-types/views/event-types-single-view";
+import { ssrInit } from "@server/lib/ssr";
 
 export const generateMetadata = async ({ params, searchParams }: PageProps) => {
   const legacyCtx = buildLegacyCtx(headers(), cookies(), params, searchParams);
-  const { eventType } = await getData(legacyCtx);
+  const eventType = await getEventTypeById(parseInt(asStringOrThrow(legacyCtx.query.type)), legacyCtx);
 
   return await _generateMetadata(
-    (t) => `${eventType.title} | ${t("event_type")}`,
+    (t) => `${eventType?.title ?? "Not found"} | ${t("event_type")}`,
     () => ""
   );
 };
 
-const getData = withAppDirSsr(getServerSideProps);
+const getEventTypeById = async (eventTypeId: number, context: GetServerSidePropsContext) => {
+  const ssr = await ssrInit(context);
 
-export default WithLayout({ getLayout: null, getData, Page: EventTypePageWrapper })<"P">;
+  await ssr.viewer.eventTypes.get.prefetch({ id: eventTypeId });
+  try {
+    const { eventType } = await ssr.viewer.eventTypes.get.fetch({ id: eventTypeId });
+    return eventType;
+  } catch (e: unknown) {
+    logger.error(safeStringify(e));
+    // reject, user has no access to this event type.
+    return null;
+  }
+};
+
+const Page = async ({ params, searchParams }: PageProps) => {
+  const context = buildLegacyCtx(headers(), cookies(), params, searchParams);
+  const session = await getServerSessionForAppDir();
+  if (!session?.user?.id) {
+    redirect("/auth/login");
+  }
+
+  const typeParam = parseInt(asStringOrThrow(context.query.type));
+  if (Number.isNaN(typeParam)) {
+    notFound();
+  }
+
+  const eventType = await getEventTypeById(typeParam, context);
+  if (!eventType) {
+    return redirect("/event-types");
+  }
+
+  return <EventType {...eventType} id={typeParam} />;
+};
+
+export default WithLayout({ ServerPage: Page })<"P">;
