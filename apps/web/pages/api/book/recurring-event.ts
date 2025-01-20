0b4b4ff5ccb99d4800fe39705b2405b99be111de
sean-brydon <sean@cal.com>
2025-01-20 13:24:44 +0000

    add to recuring event

--- apps/web/pages/api/book/recurring-event.ts
+++ apps/web/pages/api/book/recurring-event.ts
@@ -6,11 +6,19 @@ import type { BookingResponse } from "@calcom/features/bookings/types";
 import { checkRateLimitAndThrowError } from "@calcom/lib/checkRateLimitAndThrowError";
 import getIP from "@calcom/lib/getIP";
 import { defaultResponder } from "@calcom/lib/server";
+import { checkCfTurnstileToken } from "@calcom/lib/server/checkCfTurnstileToken";
 
 // @TODO: Didn't look at the contents of this function in order to not break old booking page.
 
 async function handler(req: NextApiRequest & { userId?: number }, res: NextApiResponse) {
   const userIp = getIP(req);
+
+  if (process.env.NEXT_PUBLIC_CLOUDFLARE_USE_TURNSTILE_IN_BOOKER === "1") {
+    await checkCfTurnstileToken({
+      token: req.body["cfToken"] as string,
+      remoteIp: userIp,
+    });
+  }
 
   await checkRateLimitAndThrowError({
     rateLimitingType: "core",
