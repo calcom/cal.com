fbcf4c63ec760e615a0cba5d1b02a77f07312c0c
hbjORbj <sldisek783@gmail.com>
2025-01-20 19:42:33 -0500

    remove files

--- apps/web/server/lib/[user]/[type]/getServerSideProps.appDir.ts
+++
@@ -1,5 +0,0 @@
-import { withAppDirCsrf } from "@calcom/features/csrf/with-app-dir-csrf";
-
-import { getServerSideProps as _getServerSideProps } from "./getServerSideProps";
-
-export const getServerSideProps = withAppDirCsrf(_getServerSideProps);
