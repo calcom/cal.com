01d2d58679c6197ced24964f1058989e02ff41f1
Joe Au-Yeung <j.auyeung419@gmail.com>
2025-01-23 13:17:15 -0500

    Update migration to set previous records to true

--- packages/prisma/migrations/20250123170615_add_workflowstep_safe/migration.sql
+++ packages/prisma/migrations/20250123170615_add_workflowstep_safe/migration.sql
@@ -1,2 +1,5 @@
 -- AlterTable
 ALTER TABLE "WorkflowStep" ADD COLUMN     "safe" BOOLEAN NOT NULL DEFAULT false;
+
+-- Update existing records to set safe to true
+UPDATE "WorkflowStep" SET "safe" = true;
