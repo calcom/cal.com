1620b8570804dd6a05f7a23ade9236b5c4ca0d7c
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2025-01-23 13:16:56 +0000

    Update packages/features/bookings/lib/handleNewBooking/getEventTypesFromDB.ts

--- packages/features/bookings/lib/handleNewBooking/getEventTypesFromDB.ts
+++ packages/features/bookings/lib/handleNewBooking/getEventTypesFromDB.ts
@@ -175,8 +175,6 @@ export const getEventTypesFromDB = async (eventTypeId: number) => {
 
   const usersWithSelectedCalendars = users.map((user) => withSelectedCalendars(user));
 
-  console.log(restEventType.teamId);
-
   return {
     ...restEventType,
     hosts: hostsWithSelectedCalendars,
