023d97335a70797e92e27d8768cf9812ee43737e
Ajinkya Nikam <ajinkyanikam464@gmail.com>
2025-01-23 19:47:35 +0000

    ui_changes

--- packages/ui/components/dropdown/Dropdown.tsx
+++ packages/ui/components/dropdown/Dropdown.tsx
@@ -60,6 +60,7 @@ type DropdownMenuItemProps = ComponentProps<(typeof DropdownMenuPrimitive)["Chec
 export const DropdownMenuItem = forwardRef<HTMLDivElement, DropdownMenuItemProps>(
   ({ className = "", ...props }, forwardedRef) => (
     <DropdownMenuPrimitive.Item
+      style={{ marginTop: "0px", marginBottom: "0px" }}
       className={`focus:ring-brand-800 hover:bg-subtle hover:text-emphasis text-default text-sm ring-inset first-of-type:rounded-t-[inherit] last-of-type:rounded-b-[inherit] focus:outline-none focus:ring-1 ${className}`}
       {...props}
       ref={forwardedRef}
