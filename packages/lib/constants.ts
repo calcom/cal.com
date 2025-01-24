0700a53c87e6735e86bddfd62ac3f18501e229e1
Joe Au-Yeung <j.auyeung419@gmail.com>
2025-01-23 23:41:55 -0500

    Fix `IS_SELF_HOSTED`

--- packages/lib/constants.ts
+++ packages/lib/constants.ts
@@ -55,7 +55,9 @@ export const CONSOLE_URL =
   process.env.NODE_ENV !== "production"
     ? `https://console.cal.dev`
     : `https://console.cal.com`;
-export const IS_SELF_HOSTED = false;
+export const IS_SELF_HOSTED = !(
+  new URL(WEBAPP_URL).hostname.endsWith(".cal.dev") || new URL(WEBAPP_URL).hostname.endsWith(".cal.com")
+);
 export const EMBED_LIB_URL = process.env.NEXT_PUBLIC_EMBED_LIB_URL || `${WEBAPP_URL}/embed/embed.js`;
 export const TRIAL_LIMIT_DAYS = 14;
 
