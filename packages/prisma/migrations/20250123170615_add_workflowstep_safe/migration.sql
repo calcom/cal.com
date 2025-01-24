c314420b77b873868d7dc7230425b3ec6f62c6ee
Joe Au-Yeung <j.auyeung419@gmail.com>
2025-01-24 00:28:11 -0500

    Revert "Make `safe` optional in schema"
    This reverts commit d0964702affa87c35562300301473d25635c565b.

--- packages/prisma/migrations/20250123170615_add_workflowstep_safe/migration.sql
+++ packages/prisma/migrations/20250123170615_add_workflowstep_safe/migration.sql
@@ -1,5 +1,5 @@
 -- AlterTable
-ALTER TABLE "WorkflowStep" ADD COLUMN     "safe" BOOLEAN DEFAULT false;
+ALTER TABLE "WorkflowStep" ADD COLUMN     "safe" BOOLEAN NOT NULL DEFAULT false;
 
 -- Update existing records to set safe to true
 UPDATE "WorkflowStep" SET "safe" = true;
