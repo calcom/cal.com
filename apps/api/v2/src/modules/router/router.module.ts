718f6c8b1ea32f7e209f1789bb72b1957241af39
Morgan Vernay <morgan@cal.com>
2025-01-27 13:55:10 +0200

    expose router endpoint

--- apps/api/v2/src/modules/router/router.module.ts
+++ apps/api/v2/src/modules/router/router.module.ts
@@ -1,10 +1,12 @@
 import { PrismaModule } from "@/modules/prisma/prisma.module";
 import { RouterController } from "@/modules/router/controllers/router.controller";
+import { RoutingRepository } from "@/modules/router/router.repository";
+import { RouterService } from "@/modules/router/services/router.service";
 import { Module } from "@nestjs/common";
 
 @Module({
   imports: [PrismaModule],
-  providers: [],
+  providers: [RouterService, RoutingRepository],
   exports: [],
   controllers: [RouterController],
 })
--- apps/api/v2/src/modules/router/services/router.service.ts
+++ apps/api/v2/src/modules/router/services/router.service.ts
@@ -1,11 +1,8 @@
-import { OrganizationsRepository } from "@/modules/organizations/organizations.repository";
 import { Injectable, Logger } from "@nestjs/common";
 
 @Injectable()
 export class RouterService {
   private logger = new Logger("BillingService");
-
-  constructor(private readonly teamsRepository: OrganizationsRepository) {}
 
   hasEmbedPath(pathWithQuery: string): boolean {
     const onlyPath = pathWithQuery.split("?")[0];
