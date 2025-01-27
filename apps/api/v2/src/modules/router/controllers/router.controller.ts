b6e02ebda18818a3c4d4c068eb05719aa7618eb8
Morgan Vernay <morgan@cal.com>
2025-01-27 19:54:02 +0200

    fixup! fixup! reuse logic

--- apps/api/v2/src/modules/router/controllers/router.controller.ts
+++ apps/api/v2/src/modules/router/controllers/router.controller.ts
@@ -1,26 +1,26 @@
 import { API_VERSIONS_VALUES } from "@/lib/api-versions";
-import { Controller, Get, Req, NotFoundException, Res, Query } from "@nestjs/common";
+import { Controller, Get, Req, NotFoundException, Res, Query, Param } from "@nestjs/common";
 import { ApiTags as DocsTags, ApiExcludeController as DocsExcludeController } from "@nestjs/swagger";
 import { Request, Response } from "express";
-import z from "zod";
 
-import { getRoutedUrl } from "@calcom/platform-libraries-1.2.3";
+import { getRoutedUrl } from "@calcom/platform-libraries";
 import { ApiResponse } from "@calcom/platform-types";
 
 @Controller({
   path: "/v2/router",
   version: API_VERSIONS_VALUES,
 })
-@DocsTags("Routing forms")
+@DocsTags("Router controller")
 @DocsExcludeController(true)
 export class RouterController {
-  @Get("/")
-  async getRouterResponse(
+  @Get("/forms/:formId")
+  async getRoutingFormResponse(
     @Req() request: Request,
     @Res() res: Response,
+    @Param("formId") formId: string,
     @Query() query: Record<string, string>
   ): Promise<void | ApiResponse<unknown>> {
-    const routedUrlData = await getRoutedUrl({ req: request, query });
+    const routedUrlData = await getRoutedUrl({ req: request, query: { ...query, form: formId } });
 
     if (routedUrlData?.notFound) {
       throw new NotFoundException("Route not found. Please check the provided form parameter.");
