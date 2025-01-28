001a8a4e88cc4cc924657ca9f7b3c787bab0e0de
Amrit Rai <iamamrit27@gmail.com>
2025-01-28 12:27:36 +0530

    feat: Enhance routing forms response handling and route matching

--- packages/app-store/routing-forms/lib/handleResponse.ts
+++ packages/app-store/routing-forms/lib/handleResponse.ts
@@ -1,4 +1,5 @@
 import { Prisma } from "@prisma/client";
+import type { GetServerSidePropsContext } from "next";
 import { z } from "zod";
 
 import { emailSchema } from "@calcom/lib/emailSchema";
@@ -9,11 +10,11 @@ import { prisma } from "@calcom/prisma";
 import type { App_RoutingForms_Form } from "@calcom/prisma/client";
 import { RoutingFormSettings } from "@calcom/prisma/zod-utils";
 import { TRPCError } from "@calcom/trpc/server";
-import type { ZResponseInputSchema } from "@calcom/trpc/server/routers/viewer/routing-forms/response.schema";
 
 import isRouter from "../lib/isRouter";
 import { onFormSubmission } from "../trpc/utils";
 import type { FormResponse, SerializableForm } from "../types/types";
+import { findMatchingRoute } from "./processRoute";
 
 type Form = SerializableForm<
   App_RoutingForms_Form & {
@@ -29,23 +30,64 @@ type Form = SerializableForm<
 
 const moduleLogger = logger.getSubLogger({ prefix: ["routing-forms/lib/handleResponse"] });
 
-export const handleResponse = async ({
-  response,
-  form,
-  // Unused but probably should be used
-  // formFillerId,
-  chosenRouteId,
-  isPreview,
-}: {
-  response: z.infer<typeof ZResponseInputSchema>["response"];
-  form: Form;
-  formFillerId: string;
-  chosenRouteId: string | null;
-  isPreview: boolean;
-}) => {
+export const handleResponse = async (
+  context: Pick<GetServerSidePropsContext, "query"> & {
+    req: { body: unknown } & GetServerSidePropsContext["req"];
+  }
+) => {
+  let serializableFormWithFields: Form | null = null;
+
   try {
+    if (!context.query.formId) {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+      });
+    }
+
+    const formId = context.query.formId;
+    const form = (await prisma.app_RoutingForms_Form.findUnique({
+      where: {
+        id: formId as string,
+      },
+      select: {
+        id: true,
+        fields: true,
+        routes: true,
+        teamId: true,
+        user: {
+          select: {
+            id: true,
+            email: true,
+          },
+        },
+        team: {
+          select: {
+            parentId: true,
+          },
+        },
+      },
+    })) as unknown as Form;
+
+    if (!form) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+      });
+    }
+
+    const response = (
+      typeof context.req.body === "string" ? JSON.parse(context.req.body) : context.req.body
+    ) as Record<string, { value: string | number | string[]; label: string }>;
+
+    const matchingRoute = findMatchingRoute({ form, response });
+
+    if (!matchingRoute) {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "No matching route found",
+      });
+    }
+
     if (!form.fields) {
-      // There is no point in submitting a form that doesn't have fields defined
       throw new TRPCError({
         code: "BAD_REQUEST",
       });
@@ -53,12 +95,12 @@ export const handleResponse = async ({
 
     const formTeamId = form.teamId;
     const formOrgId = form.team?.parentId ?? null;
-    const serializableFormWithFields = {
+    serializableFormWithFields = {
       ...form,
-      fields: form.fields,
+      fields: form.fields || [],
     };
 
-    const missingFields = serializableFormWithFields.fields
+    const missingFields = (serializableFormWithFields.fields || [])
       .filter((field) => !(field.required ? response[field.id]?.value : true))
       .map((f) => f.label);
 
@@ -68,10 +110,9 @@ export const handleResponse = async ({
         message: `Missing required fields ${missingFields.join(", ")}`,
       });
     }
-    const invalidFields = serializableFormWithFields.fields
+    const invalidFields = (serializableFormWithFields.fields || [])
       .filter((field) => {
         const fieldValue = response[field.id]?.value;
-        // The field isn't required at this point. Validate only if it's set
         if (!fieldValue) {
           return false;
         }
@@ -117,7 +158,7 @@ export const handleResponse = async ({
       userWithEmails = userEmails.map((userEmail) => userEmail.user.email);
     }
 
-    const chosenRoute = serializableFormWithFields.routes?.find((route) => route.id === chosenRouteId);
+    const chosenRoute = serializableFormWithFields.routes?.find((route) => route.id === matchingRoute.id);
     let teamMemberIdsMatchingAttributeLogic: number[] | null = null;
     let timeTaken: Record<string, number | null> = {};
     if (chosenRoute) {
@@ -164,39 +205,22 @@ export const handleResponse = async ({
       // It currently happens for a Router route. Such a route id isn't present in the form.routes
     }
 
-    let dbFormResponse;
-    if (!isPreview) {
-      dbFormResponse = await prisma.app_RoutingForms_FormResponse.create({
-        data: {
-          // TODO: Why do we not save formFillerId available in the input?
-          // formFillerId,
-          formId: form.id,
-          response: response,
-          chosenRouteId,
-        },
-      });
-
-      await onFormSubmission(
-        { ...serializableFormWithFields, userWithEmails },
-        dbFormResponse.response as FormResponse,
-        dbFormResponse.id,
-        chosenRoute ? ("action" in chosenRoute ? chosenRoute.action : undefined) : undefined
-      );
-    } else {
-      moduleLogger.debug("Dry run mode - Form response not stored and also webhooks and emails not sent");
-      // Create a mock response for dry run
-      dbFormResponse = {
-        id: 0,
+    const dbFormResponse = await prisma.app_RoutingForms_FormResponse.create({
+      data: {
         formId: form.id,
-        response,
-        chosenRouteId,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      };
-    }
+        response: response,
+        chosenRouteId: matchingRoute.id,
+      },
+    });
+
+    await onFormSubmission(
+      { ...serializableFormWithFields, userWithEmails, fields: form.fields || [] },
+      dbFormResponse.response as FormResponse,
+      dbFormResponse.id,
+      chosenRoute ? ("action" in chosenRoute ? chosenRoute.action : undefined) : undefined
+    );
 
     return {
-      isPreview: !!isPreview,
       formResponse: dbFormResponse,
       teamMembersMatchingAttributeLogic: teamMemberIdsMatchingAttributeLogic,
       attributeRoutingConfig: chosenRoute
@@ -207,13 +231,26 @@ export const handleResponse = async ({
       timeTaken,
     };
   } catch (e) {
+    if (e instanceof TRPCError) {
+      return {
+        error: true,
+        message: e.message,
+        status: 400,
+      };
+    }
     if (e instanceof Prisma.PrismaClientKnownRequestError) {
       if (e.code === "P2002") {
-        throw new TRPCError({
-          code: "CONFLICT",
-        });
+        return {
+          error: true,
+          message: "Duplicate submission",
+          status: 409,
+        };
       }
     }
-    throw e;
+    return {
+      error: true,
+      message: "An unexpected error occurred",
+      status: 500,
+    };
   }
 };
