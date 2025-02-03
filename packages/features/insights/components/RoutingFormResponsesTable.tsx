7f86f69528b1f07c831b0bcbece1f71c33751016
Eunjae Lee <hey@eunjae.dev>
2025-02-03 15:22:56 +0100

    fix: remove unused attribute columns for /insights/routing (#19059)

--- packages/features/insights/components/RoutingFormResponsesTable.tsx
+++ packages/features/insights/components/RoutingFormResponsesTable.tsx
@@ -29,7 +29,7 @@ import classNames from "@calcom/lib/classNames";
 import { useCopy } from "@calcom/lib/hooks/useCopy";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { BookingStatus } from "@calcom/prisma/enums";
-import { RoutingFormFieldType } from "@calcom/routing-forms/lib/FieldTypes";
+import { RoutingFormFieldType, isValidRoutingFormFieldType } from "@calcom/routing-forms/lib/FieldTypes";
 import { trpc, type RouterOutputs } from "@calcom/trpc";
 import {
   Badge,
@@ -258,7 +258,7 @@ export function RoutingFormResponsesTable() {
     useInsightsParameters();
 
   const {
-    data: headers,
+    data: headersRaw,
     isLoading: isHeadersLoading,
     isSuccess: isHeadersSuccess,
   } = trpc.viewer.insights.routingFormResponsesHeaders.useQuery({
@@ -267,6 +267,11 @@ export function RoutingFormResponsesTable() {
     isAll,
     routingFormId,
   });
+
+  const headers = useMemo(() => {
+    if (!headersRaw) return;
+    return headersRaw.filter((header) => header.label && isValidRoutingFormFieldType(header.type));
+  }, [headersRaw]);
 
   const { data: forms } = trpc.viewer.insights.getRoutingFormsForFilters.useQuery({
     userId,
