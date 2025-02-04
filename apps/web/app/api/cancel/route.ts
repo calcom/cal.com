59a16096d7b27236388fca34e279c9ea7232a361
hbjORbj <sldisek783@gmail.com>
2025-02-04 02:38:28 -0500

    Add app/api/cancel/route.ts

---
+++ apps/web/app/api/cancel/route.ts
@@ -0,0 +1,35 @@
+import { cookies, headers } from "next/headers";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+import { getServerSession } from "@calcom/features/auth/lib/getServerSession";
+import handleCancelBooking from "@calcom/features/bookings/lib/handleCancelBooking";
+
+import { buildLegacyRequest } from "@lib/buildLegacyCtx";
+
+async function handler(req: NextRequest) {
+  const session = await getServerSession({ req: buildLegacyRequest(headers(), cookies()) });
+  let appDirRequestBody;
+  try {
+    appDirRequestBody = await req.json();
+  } catch (error) {
+    return NextResponse.json({ success: false, message: "Invalid JSON" }, { status: 400 });
+  }
+
+  const result = await handleCancelBooking({
+    appDirRequestBody,
+    userId: session?.user?.id || -1,
+  });
+
+  const statusCode = result.success ? 200 : 400;
+
+  return NextResponse.json(result, { status: statusCode });
+}
+
+export async function DELETE(req: NextRequest) {
+  return handler(req);
+}
+
+export async function POST(req: NextRequest) {
+  return handler(req);
+}
