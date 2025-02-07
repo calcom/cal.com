4e0d12e9a0db16d9e1e23188c5150bb906c11fd3
Joe Au-Yeung <j.auyeung419@gmail.com>
2025-02-07 16:14:20 -0500

    Add user _post test

---
+++ apps/api/v1/test/lib/users/_post.test.ts
@@ -0,0 +1,90 @@
+import prismock from "../../../../../../tests/libs/__mocks__/prisma";
+
+import type { Request, Response } from "express";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { createMocks } from "node-mocks-http";
+import { describe, test, expect, vi } from "vitest";
+
+import handler from "../../../pages/api/users/_post";
+
+type CustomNextApiRequest = NextApiRequest & Request;
+type CustomNextApiResponse = NextApiResponse & Response;
+
+vi.mock("@calcom/lib/server/i18n", () => {
+  return {
+    getTranslation: (key: string) => {
+      return () => key;
+    },
+  };
+});
+
+vi.stubEnv("CALCOM_LICENSE_KEY", undefined);
+
+describe("POST /api/users", () => {
+  test("should throw 401 if not system-wide admin", async () => {
+    const { req, res } = createMocks<CustomNextApiRequest, CustomNextApiResponse>({
+      method: "POST",
+      body: {
+        email: "test@example.com",
+        username: "test",
+      },
+    });
+    req.isSystemWideAdmin = false;
+
+    await handler(req, res);
+
+    expect(res.statusCode).toBe(401);
+  });
+  test("should throw a 400 if no email is provided", async () => {
+    const { req, res } = createMocks<CustomNextApiRequest, CustomNextApiResponse>({
+      method: "POST",
+      body: {
+        username: "test",
+      },
+    });
+    req.isSystemWideAdmin = true;
+
+    await handler(req, res);
+
+    expect(res.statusCode).toBe(400);
+  });
+  test("should throw a 400 if no username is provided", async () => {
+    const { req, res } = createMocks<CustomNextApiRequest, CustomNextApiResponse>({
+      method: "POST",
+      body: {
+        email: "test@example.com",
+      },
+    });
+    req.isSystemWideAdmin = true;
+
+    await handler(req, res);
+
+    expect(res.statusCode).toBe(400);
+  });
+  test("should create user successfully", async () => {
+    const { req, res } = createMocks<CustomNextApiRequest, CustomNextApiResponse>({
+      method: "POST",
+      body: {
+        email: "test@example.com",
+        username: "test",
+      },
+      prisma: prismock,
+    });
+    req.isSystemWideAdmin = true;
+
+    await handler(req, res);
+
+    expect(res.statusCode).toBe(200);
+
+    const response = JSON.parse(res._getData());
+
+    expect(response.user).toEqual(
+      expect.objectContaining({
+        email: "test@example.com",
+        username: "test",
+        locked: false,
+        organizationId: null,
+      })
+    );
+  });
+});
