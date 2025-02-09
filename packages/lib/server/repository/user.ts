86200eceb327304cca727cf7adb5fcd8f58716ea
Joe Au-Yeung <j.auyeung419@gmail.com>
2025-02-08 21:42:34 -0500

    Explicitly pass `locked` prop

--- packages/lib/server/repository/user.ts
+++ packages/lib/server/repository/user.ts
@@ -614,6 +614,7 @@ export class UserRepository {
           },
         },
         creationSource,
+        locked,
         ...(organizationIdValue
           ? {
               organizationId: organizationIdValue,
