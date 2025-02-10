b9e84a2a380fb7a775e5ac25e5219982da2dbf52
hbjORbj <sldisek783@gmail.com>
2025-02-10 13:46:53 -0500

    remove client component not needed

--- apps/web/app/(use-page-wrapper)/apps/routing-forms/[...pages]/page.tsx
+++ apps/web/app/(use-page-wrapper)/apps/routing-forms/[...pages]/page.tsx
@@ -3,10 +3,10 @@ import type { PageProps as ServerPageProps } from "app/_types";
 import { _generateMetadata } from "app/_utils";
 import { cookies, headers } from "next/headers";
 
+import LayoutHandler from "@calcom/app-store/routing-forms/pages/layout-handler/[...appPages]";
+
 import { getServerSideProps } from "@lib/apps/routing-forms/[...pages]/getServerSideProps";
 import { buildLegacyCtx } from "@lib/buildLegacyCtx";
-
-import LegacyPage from "~/apps/routing-forms/[...pages]/pages-view";
 
 export const generateMetadata = async ({ params, searchParams }: ServerPageProps) => {
   const legacyContext = buildLegacyCtx(headers(), cookies(), params, searchParams);
@@ -26,8 +26,13 @@ const getData = withAppDirSsr<any>(getServerSideProps);
 const ServerPage = async ({ params, searchParams }: ServerPageProps) => {
   const context = buildLegacyCtx(headers(), cookies(), params, searchParams);
   const props = await getData(context);
+  const pages = Array.isArray(params.pages) ? params.pages : params.pages?.split("/") ?? [];
 
-  return <LegacyPage {...props} />;
+  const componentProps = {
+    ...props,
+    pages: pages.slice(1),
+  };
+  return <LayoutHandler {...componentProps} />;
 };
 
 export default ServerPage;
