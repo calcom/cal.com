1bd09a68af617471dfaa79cca454298609dad8b8
Somay Chauhan <somaychauhan98@gmail.com>
2025-02-10 18:27:16 +0530

    Update useSkipConfirmStep.ts

--- packages/features/bookings/Booker/components/hooks/useSkipConfirmStep.ts
+++ packages/features/bookings/Booker/components/hooks/useSkipConfirmStep.ts
@@ -3,8 +3,8 @@ import { useState, useEffect } from "react";
 import type { UseBookingFormReturnType } from "@calcom/features/bookings/Booker/components/hooks/useBookingForm";
 import { useBookerStore } from "@calcom/features/bookings/Booker/store";
 import { getBookingResponsesSchemaWithOptionalChecks } from "@calcom/features/bookings/lib/getBookingResponsesSchema";
-import type { BookerEvent } from "@calcom/features/bookings/types";
 
+import type { BookerEvent } from "../../../types";
 import type { BookerState } from "../../types";
 
 const useSkipConfirmStep = (
