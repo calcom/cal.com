35d7d3fdfe2060232c4f34eaa0cc9b0511070b43
harshit078 <harshitsingh467@gmail.com>
2025-02-12 01:25:58 +0530

    Added Sonner instead of react-show-toast and updated files

--- packages/ui/components/toast/showToast.tsx
+++ packages/ui/components/toast/showToast.tsx
@@ -1,110 +1,80 @@
-import classNames from "classnames";
-import type { ToastOptions, Toast } from "react-hot-toast";
-import toast from "react-hot-toast";
+import { toast } from "sonner";
 
 import { Icon } from "../icon";
 
-type IToast = {
-  message: string;
-  toastVisible: boolean;
-  toastId: string;
-  onClose: (toastId: string) => void;
-};
-
-export const SuccessToast = ({ message, toastVisible, onClose, toastId }: IToast) => (
-  <button
-    className={classNames(
-      "data-testid-toast-success bg-brand-default text-brand mb-2 flex h-auto space-x-2 rounded-md p-3 text-sm font-semibold shadow-md rtl:space-x-reverse md:max-w-sm",
-      toastVisible && "animate-fade-in-up cursor-pointer"
-    )}
-    onClick={() => onClose(toastId)}>
-    <span className="mt-0.5">
-      <Icon name="check" className="h-4 w-4" />
-    </span>
-    <p data-testid="toast-success" className="text-left">
-      {message}
-    </p>
-  </button>
-);
-
-export const ErrorToast = ({ message, toastVisible, onClose, toastId }: IToast) => (
-  <button
-    className={classNames(
-      "animate-fade-in-up bg-error text-error text-brand mb-2 flex h-auto space-x-2 rounded-md p-3 text-sm font-semibold shadow-md rtl:space-x-reverse md:max-w-sm",
-      toastVisible && "animate-fade-in-up cursor-pointer"
-    )}
-    onClick={() => onClose(toastId)}>
-    <span className="mt-0.5">
-      <Icon name="info" className="h-4 w-4" />
-    </span>
-    <p data-testid="toast-error" className="text-left">
-      {message}
-    </p>
-  </button>
-);
-
-export const WarningToast = ({ message, toastVisible, onClose, toastId }: IToast) => (
-  <button
-    className={classNames(
-      "animate-fade-in-up bg-brand-default text-brand mb-2 flex h-auto space-x-2 rounded-md p-3 text-sm font-semibold shadow-md rtl:space-x-reverse md:max-w-sm",
-      toastVisible && "animate-fade-in-up cursor-pointer"
-    )}
-    onClick={() => onClose(toastId)}>
-    <span className="mt-0.5">
-      <Icon name="info" className="h-4 w-4" />
-    </span>
-    <p data-testid="toast-warning" className="text-left">
-      {message}
-    </p>
-  </button>
-);
-
-export const DefaultToast = ({ message, toastVisible, onClose, toastId }: IToast) => (
-  <button
-    className={classNames(
-      "animate-fade-in-up bg-brand-default text-brand mb-2 flex h-auto space-x-2 rounded-md p-3 text-sm font-semibold shadow-md rtl:space-x-reverse md:max-w-sm",
-      toastVisible && "animate-fade-in-up cursor-pointer"
-    )}
-    onClick={() => onClose(toastId)}>
-    <span className="mt-0.5">
-      <Icon name="check" className="h-4 w-4" />
-    </span>
-    <p data-testid="toast-default" className="text-left">
-      {message}
-    </p>
-  </button>
-);
-
 const TOAST_VISIBLE_DURATION = 6000;
 
-type ToastVariants = "success" | "warning" | "error";
+export type ToastOptions = {
+  duration?: number;
+};
 
+export type ToastVariants = "success" | "warning" | "error";
+
+export const SuccessToast = ({ message }: { message: string }) => (
+  <div className="bg-brand-default text-brand mb-2 flex h-auto space-x-2 rounded-md p-3 text-sm font-semibold shadow-md md:max-w-sm">
+    <span className="mt-0.5">
+      <Icon name="check" className="h-4 w-4" />
+    </span>
+    <p>{message}</p>
+  </div>
+);
+
+export const ErrorToast = ({ message }: { message: string }) => (
+  <div className="bg-error text-error mb-2 flex h-auto space-x-2 rounded-md p-3 text-sm font-semibold shadow-md md:max-w-sm">
+    <span className="mt-0.5">
+      <Icon name="info" className="h-4 w-4" />
+    </span>
+    <p>{message}</p>
+  </div>
+);
+
+export const WarningToast = ({ message }: { message: string }) => (
+  <div className="bg-brand-default text-brand mb-2 flex h-auto space-x-2 rounded-md p-3 text-sm font-semibold shadow-md md:max-w-sm">
+    <span className="mt-0.5">
+      <Icon name="info" className="h-4 w-4" />
+    </span>
+    <p>{message}</p>
+  </div>
+);
+
+export const DefaultToast = ({ message }: { message: string }) => (
+  <div className="bg-brand-default text-brand mb-2 flex h-auto space-x-2 rounded-md p-3 text-sm font-semibold shadow-md md:max-w-sm">
+    <span className="mt-0.5">
+      <Icon name="check" className="h-4 w-4" />
+    </span>
+    <p>{message}</p>
+  </div>
+);
+
+/**
+ * @param variant - Type of notification ("success", "warning", or "error")
+ * @param message -  message to display
+ * @param options - Duration
+ */
 export function showToast(
-  message: string,
   variant: ToastVariants,
-  // Options or duration (duration for backwards compatibility reasons)
+  message: string,
   options: number | ToastOptions = TOAST_VISIBLE_DURATION
 ) {
-  //
-  const _options: ToastOptions = typeof options === "number" ? { duration: options } : options;
-  if (!_options.duration) _options.duration = TOAST_VISIBLE_DURATION;
-  if (!_options.position) _options.position = "bottom-center";
+  let duration: number = TOAST_VISIBLE_DURATION;
+  if (typeof options === "number") {
+    duration = options;
+  } else if (typeof options === "object" && options.duration) {
+    duration = options.duration;
+  }
 
-  const onClose = (toastId: string) => {
-    toast.remove(toastId);
-  };
-  const toastElements: { [x in ToastVariants]: (t: Toast) => JSX.Element } = {
-    success: (t) => (
-      <SuccessToast message={message} toastVisible={t.visible} onClose={onClose} toastId={t.id} />
-    ),
-    error: (t) => <ErrorToast message={message} toastVisible={t.visible} onClose={onClose} toastId={t.id} />,
-    warning: (t) => (
-      <WarningToast message={message} toastVisible={t.visible} onClose={onClose} toastId={t.id} />
-    ),
-  };
-  return toast.custom(
-    toastElements[variant] ||
-      ((t) => <DefaultToast message={message} toastVisible={t.visible} onClose={onClose} toastId={t.id} />),
-    _options
-  );
+  switch (variant) {
+    case "success":
+      toast.success(message, { duration });
+      break;
+    case "error":
+      toast.error(message, { duration });
+      break;
+    case "warning":
+      toast(message, { duration, icon: "⚠️" });
+      break;
+    default:
+      toast(message, { duration });
+      break;
+  }
 }
