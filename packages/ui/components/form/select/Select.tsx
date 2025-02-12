a85da1e8ec975ae39f53faff981a2256b4fcab6f
Sean Brydon <sean@brydon.io>
2025-02-12 11:23:42 +0100

    use badge design for select pills

--- packages/ui/components/form/select/Select.tsx
+++ packages/ui/components/form/select/Select.tsx
@@ -98,10 +98,8 @@ export const Select = <
           cx("text-default placeholder:text-muted flex gap-1", innerClassNames?.valueContainer),
         multiValue: () =>
           cx(
-            "font-medium inline-flex items-center justify-center rounded bg-emphasis text-emphasis  leading-none ",
-            size == "sm"
-              ? "px-1.5 py-[1px] text-[10px] rounded-md"
-              : "py-1 px-1.5 text-xs leading-none rounded-lg"
+            "font-medium inline-flex items-center justify-center rounded bg-emphasis text-emphasis leading-none text-xs",
+            size == "sm" ? "px-1.5 py-[1px] rounded-md" : "py-1 px-1.5 leading-none rounded-lg"
           ),
         menu: () =>
           cx(
