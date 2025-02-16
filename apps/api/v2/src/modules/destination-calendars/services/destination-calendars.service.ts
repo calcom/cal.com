a87d6fcb32c61b02899edf0355bebe5e23576736
Morgan Vernay <morgan@cal.com>
2025-02-16 16:10:32 +0200

    fixup! try fix set destination calendar api v2

--- apps/api/v2/src/modules/destination-calendars/services/destination-calendars.service.ts
+++ apps/api/v2/src/modules/destination-calendars/services/destination-calendars.service.ts
@@ -58,8 +58,8 @@ export class DestinationCalendarsService {
       externalId,
       userId,
       primaryEmail ?? null,
-      credentialId,
-      domainWideDelegationCredentialId
+      dwdCalendar ? undefined : credentialId,
+      dwdCalendar ? domainWideDelegationCredentialId : undefined
     );
 
     return {
