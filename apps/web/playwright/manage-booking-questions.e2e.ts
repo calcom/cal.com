fd651133c271074c734ef4c8520426c20ef25583
Anik Dhabal Babu <81948346+anikdhabal@users.noreply.github.com>
2025-02-16 15:57:34 +0530

    fix flake (#19306)

--- apps/web/playwright/manage-booking-questions.e2e.ts
+++ apps/web/playwright/manage-booking-questions.e2e.ts
@@ -258,6 +258,7 @@ test.describe("Manage Booking Questions", () => {
       await test.step("Go to First Team Event", async () => {
         const locator = page.getByTestId(`horizontal-tab-${team?.name}`);
         await locator.click();
+        await expect(locator).toHaveAttribute("aria-current", "page");
         const $eventTypes = page.locator("[data-testid=event-types]").locator("li a");
         const firstEventTypeElement = $eventTypes.first();
 
