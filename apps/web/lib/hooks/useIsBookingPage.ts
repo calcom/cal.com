bfe78622b58341ab8695beeef0088b2863a9c57a
hbjORbj <sldisek783@gmail.com>
2025-02-17 10:13:58 -0500

    update useIsBookingPage

--- apps/web/lib/hooks/useIsBookingPage.ts
+++ apps/web/lib/hooks/useIsBookingPage.ts
@@ -7,6 +7,7 @@ export default function useIsBookingPage(): boolean {
   const isBookingPage = [
     "/booking",
     "/cancel",
+    "/team",
     "/reschedule",
     "/d", // Private Link of booking page
     "/apps/routing-forms/routing-link", // Routing Form page
