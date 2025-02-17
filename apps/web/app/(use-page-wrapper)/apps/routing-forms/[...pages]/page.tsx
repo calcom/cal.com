d874ac54da9bb3934e7a4ce73623e7b0bec4a736
Hariom Balhara <hariombalhara@gmgmail.com>
2025-02-17 09:07:53 +0530

    Fix title for Routing Form public link and also remove any

--- apps/web/app/(use-page-wrapper)/apps/routing-forms/[...pages]/page.tsx
+++ apps/web/app/(use-page-wrapper)/apps/routing-forms/[...pages]/page.tsx
@@ -3,31 +3,45 @@ import type { PageProps as ServerPageProps } from "app/_types";
 import { _generateMetadata } from "app/_utils";
 import { cookies, headers } from "next/headers";
 
+import type { routingServerSidePropsConfig } from "@calcom/app-store/routing-forms/pages/app-routing.config";
 import LayoutHandler from "@calcom/app-store/routing-forms/pages/layout-handler/[...appPages]";
 import Shell from "@calcom/features/shell/Shell";
 
 import { getServerSideProps } from "@lib/apps/routing-forms/[...pages]/getServerSideProps";
 import { buildLegacyCtx } from "@lib/buildLegacyCtx";
 
-export const generateMetadata = async () => {
+const normalizePages = (pages: string[] | string | undefined) => {
+  const normalizedPages = Array.isArray(pages) ? pages : pages?.split("/") ?? [];
+  return {
+    mainPage: normalizedPages[0],
+    subPages: normalizedPages.slice(1),
+  };
+};
+
+export const generateMetadata = async ({ params }: { params: { pages: string[] } }) => {
+  const { mainPage } = normalizePages(params.pages);
   return await _generateMetadata(
-    (t) => t("routing_forms"),
+    // TODO: Need to show the actual form name instead of "Form"
+    (t) => (mainPage === "routing-link" ? `Form | Cal.com Forms` : `${t("routing_forms")} | Cal.com Forms`),
     () => ""
   );
 };
 
-const getData = withAppDirSsr<any>(getServerSideProps);
+type GetServerSidePropsResult =
+  (typeof routingServerSidePropsConfig)[keyof typeof routingServerSidePropsConfig];
+const getData = withAppDirSsr<GetServerSidePropsResult>(getServerSideProps);
 
 const ServerPage = async ({ params, searchParams }: ServerPageProps) => {
   const context = buildLegacyCtx(headers(), cookies(), params, searchParams);
   const props = await getData(context);
-  const pages = Array.isArray(params.pages) ? params.pages : params.pages?.split("/") ?? [];
+  const { mainPage, subPages } = normalizePages(params.pages);
 
   const componentProps = {
     ...props,
-    pages: pages.slice(1),
+    pages: subPages,
   };
-  if (pages[0] === "routing-link") {
+
+  if (mainPage === "routing-link") {
     return <LayoutHandler {...componentProps} />;
   }
 
