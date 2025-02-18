9f641ecd9a22840d3d042f5aa2a065e6e7d22d6f
hbjORbj <sldisek783@gmail.com>
2025-02-18 04:42:28 -0500

    update layout

--- apps/web/app/(use-page-wrapper)/layout.tsx
+++ apps/web/app/(use-page-wrapper)/layout.tsx
@@ -1,3 +1,4 @@
+import { checkIfBookingPage } from "app/_utils";
 import { cookies, headers } from "next/headers";
 
 import { buildLegacyCtx } from "@lib/buildLegacyCtx";
@@ -9,11 +10,17 @@ import { ssrInit } from "@server/lib/ssr";
 export default async function PageWrapperLayout({ children }: { children: React.ReactNode }) {
   const h = headers();
   const nonce = h.get("x-nonce") ?? undefined;
+  const isBookingPage = checkIfBookingPage();
   const context = buildLegacyCtx(headers(), cookies(), {}, {});
   const ssr = await ssrInit(context);
 
   return (
-    <PageWrapper requiresLicense={false} nonce={nonce} themeBasis={null} dehydratedState={ssr.dehydrate()}>
+    <PageWrapper
+      isBookingPage={isBookingPage}
+      requiresLicense={false}
+      nonce={nonce}
+      themeBasis={null}
+      dehydratedState={ssr.dehydrate()}>
       {children}
     </PageWrapper>
   );
