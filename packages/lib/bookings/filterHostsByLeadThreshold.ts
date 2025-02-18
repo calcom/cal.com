e81b7923c3eb5bb4991102648d45fa03ab64d5f8
Alex van Andel <me@alexvanandel.com>
2025-02-18 14:59:48 +0000

    fix: Fairness when routing (#18550)
    * Partial push of new filters
    
    * Let's always return the passed host array when a filter doesn't need to be applied
    
    * fix: isFixed typing in same round robin host check
    
    * Add main bulk of rewritten logic for routed fairness support
    
    * Remove console.log
    
    * Add getAttributeWithEnabledWeights to AttributeRepository
    
    * Add OOO entry repository
    
    * Use getOrderedListOfLuckyUsers in lead threshold filtering
    
    * Shortfall already includes weight
    
    * test against minShortfall
    
    * first attempt to fix fallbacks
    
    * Added testcase updates
    
    * Partially fix filterHosts test
    
    * Some test updates
    
    * Removed email field from normalization
    
    * fix no available users found error
    
    * fix unit test
    
    * remove fallback from same host reschedule return
    
    * fixes and improvements for findQualifiedHosts
    
    * include fallback logic in handleNewBooking
    
    * use fallback in handleNewBooking
    
    * fix for contact owner in handleNewBooking
    
    * WIP - comment
    
    * pass routingFormResponseId
    
    * fix type error
    
    * pass routingFormResponse
    
    * fixes in treshold logic
    
    * remove reason
    
    * merge qualified and fallback availabilities
    
    * fix type error
    
    * add and fix tests
    
    * clean up test file
    
    * fix filterHostsBySameRoundRobinHost tests
    
    * fix findQualifiedHosts.test.ts
    
    * fix getRoutedUsers.test.ts
    
    * fix tests
    
    * fix isRerouting check
    
    * fix start time for two weeks check
    
    * fix fallback hosts
    
    * fix test
    
    * fallback hosts should also include qualified hosts
    
    * fix type error
    
    * fix typos
    
    * add contact owner to fallbacks
    
    * fix typo
    
    * fix qualifiedRRUsers
    
    * set fixed users when no hosts
    
    * add tets + code clean up
    
    * add test for reschedule with same host
    
    * fix typo
    
    * add more tests for findQualifiedHosts
    
    * remove skip from test
    
    * add comment
    
    * remove unused code
    
    * fix fallback users in loadAndValidateUsers
    
    * improve naming for fallback
    
    * rename missed fallback variable
    
    * fix reschedule with same rr host in handleNewBooking
    
    * fixes for fallback slots
    
    * add more tests + small fixes for contact owner
    
    * fix tests and type errors
    
    * add logs for fallback
    
    * add type:json to logger
    
    * Fix small merge issue
    
    * fix end time if two weeks are cut off
    
    * add more tests for contact owner availability check
    
    * add new logic for two weeks fallback
    
    * fix reschedule same RR host test
    
    ---------
    
    Co-authored-by: CarinaWolli <wollencarina@gmail.com>
    Co-authored-by: Peer Richelsen <peeroke@gmail.com>
    Co-authored-by: Carina Wollendorfer <30310907+CarinaWolli@users.noreply.github.com>

--- packages/lib/bookings/filterHostsByLeadThreshold.ts
+++ packages/lib/bookings/filterHostsByLeadThreshold.ts
@@ -1,129 +1,156 @@
-import { BookingRepository } from "@calcom/lib/server/repository/booking";
+import logger from "@calcom/lib/logger";
+import type { SelectedCalendar } from "@calcom/prisma/client";
+import type { CredentialPayload } from "@calcom/types/Credential";
 
-const START_OF_MONTH = new Date(Date.UTC(new Date().getUTCFullYear(), new Date().getUTCMonth(), 1));
+import type { RoutingFormResponse } from "../server/getLuckyUser";
+import { getOrderedListOfLuckyUsers } from "../server/getLuckyUser";
 
 export const errorCodes = {
   MAX_LEAD_THRESHOLD_FALSY: "Max lead threshold should be null or > 1, not 0.",
 } as const;
 
-function getMostRecentDate(dates: Date[]): Date {
-  if (dates.length === 0)
-    throw new Error("Array of date length provided in getMostRecentDate should not be empty.");
+type BaseUser = {
+  id: number;
+  email: string;
+  credentials: CredentialPayload[];
+  userLevelSelectedCalendars: SelectedCalendar[];
+} & Record<string, unknown>;
 
-  return dates.reduce((mostRecent, current) => (current > mostRecent ? current : mostRecent));
+type BaseHost<User extends BaseUser> = {
+  isFixed: boolean;
+  createdAt: Date;
+  priority?: number | null;
+  weight?: number | null;
+  weightAdjustment?: number | null;
+  user: User;
+};
+
+type PerUserData = Awaited<ReturnType<typeof getOrderedListOfLuckyUsers>>["perUserData"];
+type WeightedPerUserData = Omit<PerUserData, "weights" | "calibrations" | "bookingShortfalls"> & {
+  weights: NonNullable<PerUserData["weights"]>;
+  calibrations: NonNullable<PerUserData["calibrations"]>;
+  bookingShortfalls: NonNullable<PerUserData["bookingShortfalls"]>;
+};
+
+const log = logger.getSubLogger({ name: "filterHostsByLeadThreshold" });
+
+function filterHostsByLeadThresholdWithWeights(perUserData: WeightedPerUserData, maxLeadThreshold: number) {
+  const filteredUserIds: number[] = [];
+  // negative shortfall means the host should receive negative bookings, so they are overbooked
+  const maxShortfall = Math.max(...Object.values(perUserData.bookingShortfalls)); // least amount of bookings
+
+  for (const userIdStr in perUserData.bookingShortfalls) {
+    const shortfall = perUserData.bookingShortfalls[userIdStr];
+    // if user's shortfall is more than
+    if (maxShortfall - shortfall > maxLeadThreshold) {
+      log.debug(
+        `Host ${userIdStr} has been filtered out because the amount of bookings made him exceed the thresholds. Shortfall: ${shortfall}, Max Shortfall: ${maxShortfall}`
+      );
+    } else {
+      filteredUserIds.push(parseInt(userIdStr, 10));
+    }
+  }
+  return filteredUserIds;
 }
 
-const computeLeadOffsets = async <T = Record<string, unknown>>({
-  hosts,
-  eventTypeId,
-}: {
-  eventTypeId: number;
-  hosts: (T & {
-    isFixed: false;
-    createdAt: Date;
-    user: {
-      id: number;
-      email: string;
-    };
-  })[];
-}) => {
-  if (!hosts.length) return [];
-  // use either the beginning of the month, of the most recently added host; whichever is most recent
-  const startDate = getMostRecentDate([...hosts.map(({ createdAt }) => createdAt), START_OF_MONTH]);
-  // we need booking data now, this cannot be queried ahead of time as it requires knowing the most recent host date
-  // data only available after the initial call.
-  const bookingCounts = await BookingRepository.groupByActiveBookingCounts({
-    startDate,
-    users: hosts.map((host) => ({
-      ...host.user,
-    })),
-    eventTypeId,
-  });
-  const { minBookingCount, bookingCountMap } = bookingCounts.reduce(
-    (
-      acc: {
-        minBookingCount: number;
-        bookingCountMap: Record<number, number>;
-      },
-      booking
-    ) => {
-      // satisfy TS, obviously as we've where'd on userId it cannot be null but try telling TS that..
-      if (!booking.userId) return acc;
-      acc.minBookingCount = Math.min(acc.minBookingCount, booking._count._all || Infinity);
-      acc.bookingCountMap[booking.userId] = booking._count._all;
-      return acc;
-    },
-    {
-      minBookingCount: bookingCounts.length < hosts.length ? 0 : Infinity,
-      bookingCountMap: {},
+function filterHostsByLeadThresholdWithoutWeights(perUserData: PerUserData, maxLeadThreshold: number) {
+  const filteredUserIds: number[] = [];
+
+  const bookingsArray = Object.values(perUserData.bookingsCount);
+  const minBookings = Math.min(...bookingsArray);
+
+  for (const userIdStr in perUserData.bookingsCount) {
+    const bookingsCount = perUserData.bookingsCount[userIdStr];
+    if (bookingsCount - minBookings > maxLeadThreshold) {
+      log.debug(
+        `Host ${userIdStr} has been filtered out because the given data made them exceed the thresholds. BookingsCount: ${bookingsCount}, MinBookings: ${minBookings}`
+      );
+    } else {
+      filteredUserIds.push(parseInt(userIdStr, 10));
+      log.debug(
+        `Host Allowed ${userIdStr} has been filtered out because the given data made them exceed the thresholds. BookingsCount: ${bookingsCount}, MinBookings: ${minBookings}, MaxLeadThreshold: ${maxLeadThreshold}`
+      );
     }
-  );
-
-  return hosts.map((host) => {
-    const leadOffset = (bookingCountMap[host.user.id] || 0) - minBookingCount;
-    return {
-      ...host,
-      leadOffset,
-    };
-  });
-};
-
-export const _filterHostByLeadThreshold = ({
-  host,
-  maxLeadThreshold,
-}: {
-  host: { leadOffset: number };
-  maxLeadThreshold: number;
-}) => {
-  // it's possible that maxLeadThreshold is given as 0, which makes '0' sense.
-  if (!maxLeadThreshold) {
-    throw new Error(errorCodes.MAX_LEAD_THRESHOLD_FALSY);
   }
-  return host.leadOffset < maxLeadThreshold; // leadOffset is 0-indexed.
-};
+
+  return filteredUserIds;
+}
 
 /*
  * Filter the hosts by lead threshold, disqualifying hosts that have exceeded the maximum
  *
+ * NOTE: This function cleans up the leadOffset value so can't be used afterwards.
+ *
  * @throws errorCodes.MAX_LEAD_THRESHOLD_FALSY
  */
-export const filterHostsByLeadThreshold = async <T = Record<string, unknown>>({
+export const filterHostsByLeadThreshold = async <T extends BaseHost<BaseUser>>({
   hosts,
   maxLeadThreshold,
-  eventTypeId,
+  eventType,
+  routingFormResponse,
 }: {
-  hosts: ({ isFixed: boolean; createdAt: Date; user: { id: number; email: string } } & T)[];
+  hosts: T[];
   maxLeadThreshold: number | null;
-  eventTypeId: number;
-}): Promise<Omit<T, "leadOffset">[]> => {
-  if (maxLeadThreshold === null) return hosts;
-  // Calculate offsets for non-fixed hosts only once
-  const computedRoundRobinHosts = await computeLeadOffsets<T>({
-    eventTypeId,
-    hosts: hosts.filter((host) => !host.isFixed).map((host) => ({ ...host, isFixed: false as const })),
+  eventType: {
+    id: number;
+    isRRWeightsEnabled: boolean;
+    team: {
+      parentId?: number | null;
+    } | null;
+  };
+  routingFormResponse: RoutingFormResponse | null;
+}) => {
+  if (maxLeadThreshold === 0) {
+    throw new Error(errorCodes.MAX_LEAD_THRESHOLD_FALSY);
+  }
+  if (maxLeadThreshold === null) {
+    return hosts; // don't apply filter.
+  }
+
+  // this needs the routing forms response too, because it needs to know what queue we are in
+  const orderedLuckyUsers = await getOrderedListOfLuckyUsers({
+    availableUsers: [
+      {
+        ...hosts[0].user,
+        weight: hosts[0].weight ?? null,
+        priority: hosts[0].priority ?? null,
+      },
+      ...hosts.slice(1).map((host) => ({
+        ...host.user,
+        weight: host.weight ?? null,
+        priority: host.priority ?? null,
+      })),
+    ],
+    eventType,
+    allRRHosts: hosts,
+    routingFormResponse,
   });
-  // Track indices of non-fixed hosts for easy mapping back to the original order
-  let roundRobinIndex = 0;
-  return hosts
-    .map((host) => {
-      if (host.isFixed) {
-        // Return fixed hosts as they are
-        return host as Omit<T, "leadOffset">;
-      } else {
-        // Apply lead threshold filtering on round-robin hosts
-        const roundRobinHost = { ...computedRoundRobinHosts[roundRobinIndex++] };
-        if (
-          _filterHostByLeadThreshold({
-            host: roundRobinHost,
-            maxLeadThreshold,
-          })
-        ) {
-          // cleanup with destructure to remove leadOffset
-          const { leadOffset, ...roundRobinHostWithoutLeadOffset } = roundRobinHost;
-          return roundRobinHostWithoutLeadOffset;
-        }
-        return null;
-      }
-    })
-    .filter((host): host is Omit<T, "leadOffset"> => host !== null);
+
+  const perUserData = orderedLuckyUsers["perUserData"];
+
+  let filteredUserIds: number[];
+  if (eventType.isRRWeightsEnabled) {
+    // Check if any of the required data is null
+    if (
+      perUserData.calibrations === null ||
+      perUserData.weights === null ||
+      perUserData.bookingShortfalls === null
+    ) {
+      throw new Error("Calibrations, weights, or booking shortfalls are null");
+    }
+    filteredUserIds = filterHostsByLeadThresholdWithWeights(
+      {
+        bookingsCount: perUserData.bookingsCount,
+        bookingShortfalls: perUserData.bookingShortfalls,
+        calibrations: perUserData.calibrations,
+        weights: perUserData.weights,
+      },
+      maxLeadThreshold
+    );
+  } else {
+    filteredUserIds = filterHostsByLeadThresholdWithoutWeights(perUserData, maxLeadThreshold);
+  }
+
+  const filteredHosts = hosts.filter((host) => filteredUserIds.includes(host.user.id));
+  return filteredHosts;
 };
