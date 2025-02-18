1b0ae4ec3b63d14934753d8f21eafd126d107818
Joe Au-Yeung <j.auyeung419@gmail.com>
2025-02-18 09:23:19 -0500

    Use `wrapGetServerSIdePropsWIthSentry` in router getServerSideProps

--- apps/web/server/lib/router/getServerSideProps.ts
+++ apps/web/server/lib/router/getServerSideProps.ts
@@ -1,7 +1,11 @@
+import { wrapGetServerSidePropsWithSentry } from "@sentry/nextjs";
 import type { GetServerSidePropsContext } from "next";
 
 import { getRoutedUrl } from "@calcom/lib/server";
 
-export const getServerSideProps = async function getServerSideProps(context: GetServerSidePropsContext) {
-  return getRoutedUrl(context);
-};
+export const getServerSideProps = wrapGetServerSidePropsWithSentry(async function getServerSideProps(
+  context: GetServerSidePropsContext
+) {
+  return await getRoutedUrl(context);
+},
+"/router");
