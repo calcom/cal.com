0956a1970c7af7593549900ec3d09afec577afbc
hbjORbj <sldisek783@gmail.com>
2025-02-19 07:01:13 -0500

    Remove enterprise page client component

--- apps/web/app/(use-page-wrapper)/enterprise/page.tsx
+++ apps/web/app/(use-page-wrapper)/enterprise/page.tsx
@@ -1,6 +1,8 @@
-import { _generateMetadata } from "app/_utils";
+import { _generateMetadata, getTranslate } from "app/_utils";
 
-import EnterprisePage from "@components/EnterprisePage";
+import Shell from "@calcom/features/shell/Shell";
+import { UpgradeTip } from "@calcom/features/tips";
+import { Button, ButtonGroup, Icon } from "@calcom/ui";
 
 export const generateMetadata = async () =>
   await _generateMetadata(
@@ -9,7 +11,65 @@ export const generateMetadata = async () =>
   );
 
 async function ServerPage() {
-  return <EnterprisePage />;
+  const t = await getTranslate();
+  const features = [
+    {
+      icon: <Icon name="globe" className="h-5 w-5 text-red-500" />,
+      title: t("branded_subdomain"),
+      description: t("branded_subdomain_description"),
+    },
+    {
+      icon: <Icon name="chart-bar" className="h-5 w-5 text-blue-500" />,
+      title: t("org_insights"),
+      description: t("org_insights_description"),
+    },
+    {
+      icon: <Icon name="paintbrush" className="h-5 w-5 text-pink-500" />,
+      title: t("extensive_whitelabeling"),
+      description: t("extensive_whitelabeling_description"),
+    },
+    {
+      icon: <Icon name="users" className="h-5 w-5 text-orange-500" />,
+      title: t("unlimited_teams"),
+      description: t("unlimited_teams_description"),
+    },
+    {
+      icon: <Icon name="credit-card" className="h-5 w-5 text-green-500" />,
+      title: t("unified_billing"),
+      description: t("unified_billing_description"),
+    },
+    {
+      icon: <Icon name="lock" className="h-5 w-5 text-purple-500" />,
+      title: t("advanced_managed_events"),
+      description: t("advanced_managed_events_description"),
+    },
+  ];
+  return (
+    <div>
+      <Shell heading={t("enterprise")} subtitle={t("enterprise_description")} withoutSeo={true}>
+        <UpgradeTip
+          plan="enterprise"
+          title={t("create_your_org")}
+          description={t("create_your_org_description")}
+          features={features}
+          background="/tips/enterprise"
+          buttons={
+            <div className="space-y-2 rtl:space-x-reverse sm:space-x-2">
+              <ButtonGroup>
+                <Button color="primary" href="https://i.cal.com/sales/enterprise?duration=25" target="_blank">
+                  {t("contact_sales")}
+                </Button>
+                <Button color="minimal" href="https://cal.com/enterprise" target="_blank">
+                  {t("learn_more")}
+                </Button>
+              </ButtonGroup>
+            </div>
+          }>
+          <>Create Org</>
+        </UpgradeTip>
+      </Shell>
+    </div>
+  );
 }
 
 export default ServerPage;
