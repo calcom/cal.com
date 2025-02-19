0c9bad001eb690f516e14083d7a6a6bf5458e112
Eunjae Lee <hey@eunjae.dev>
2025-02-19 11:31:30 +0100

    fix type erorr

--- packages/lib/bookings/getAllUserBookings.ts
+++ packages/lib/bookings/getAllUserBookings.ts
@@ -12,7 +12,7 @@ type SortOptions = {
 };
 type GetOptions = {
   ctx: {
-    user: { id: number; email: string };
+    user: { id: number; email: string; name: string };
     prisma: PrismaClient;
   };
   bookingListingByStatus: InputByStatus[];
