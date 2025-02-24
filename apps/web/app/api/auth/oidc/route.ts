b0673545a87f4a36c46d1ca5111d3054ac0638ce
hbjORbj <sldisek783@gmail.com>
2025-02-24 13:52:28 -0500

    migrate oidc

---
+++ apps/web/app/api/auth/oidc/route.ts
@@ -0,0 +1,42 @@
+// app/api/auth/callback/route.ts
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+import jackson from "@calcom/features/ee/sso/lib/jackson";
+import { HttpError } from "@calcom/lib/http-error";
+import { apiRouteMiddleware } from "@calcom/lib/server/apiRouteMiddleware";
+
+// This is the callback endpoint for the OIDC provider
+// A team must set this endpoint in the OIDC provider's configuration
+async function handler(req: NextRequest) {
+  const { searchParams } = req.nextUrl;
+  const code = searchParams.get("code");
+  const state = searchParams.get("state");
+
+  if (!code || !state) {
+    return NextResponse.json({ message: "Code and state are required" }, { status: 400 });
+  }
+
+  const { oauthController } = await jackson();
+
+  try {
+    const { redirect_url } = await oauthController.oidcAuthzResponse({ code, state });
+
+    if (!redirect_url) {
+      throw new HttpError({
+        message: "No redirect URL found",
+        statusCode: 500,
+      });
+    }
+
+    return NextResponse.redirect(redirect_url, 302);
+  } catch (err) {
+    const { message, statusCode = 500 } = err as HttpError;
+
+    return NextResponse.json({ message }, { status: statusCode });
+  }
+}
+
+const getHandler = apiRouteMiddleware((req: NextRequest) => handler(req));
+
+export { getHandler as GET };
