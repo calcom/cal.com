2b301c2c9a1fd0654d354b7a51a6a71999b86acc
hbjORbj <sldisek783@gmail.com>
2025-02-24 13:57:38 -0500

    migrate saml apis

---
+++ apps/web/app/api/auth/saml/authorize/route.ts
@@ -0,0 +1,27 @@
+import type { OAuthReq } from "@boxyhq/saml-jackson";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+import jackson from "@calcom/features/ee/sso/lib/jackson";
+import type { HttpError } from "@calcom/lib/http-error";
+import { apiRouteMiddleware } from "@calcom/lib/server/apiRouteMiddleware";
+
+async function handler(req: NextRequest) {
+  const { oauthController } = await jackson();
+
+  try {
+    const { redirect_url } = await oauthController.authorize(
+      Object.fromEntries(req.nextUrl.searchParams) as unknown as OAuthReq
+    );
+
+    return NextResponse.redirect(redirect_url as string, 302);
+  } catch (err) {
+    const { message, statusCode = 500 } = err as HttpError;
+
+    return NextResponse.json({ message }, { status: statusCode });
+  }
+}
+
+const getHandler = apiRouteMiddleware((req: NextRequest) => handler(req));
+
+export { getHandler as GET };
---
+++ apps/web/app/api/auth/saml/userinfo/route.ts
@@ -0,0 +1,36 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import z from "zod";
+
+import jackson from "@calcom/features/ee/sso/lib/jackson";
+import { HttpError } from "@calcom/lib/http-error";
+import { apiRouteMiddleware } from "@calcom/lib/server/apiRouteMiddleware";
+
+const extractAuthToken = (req: NextRequest) => {
+  const authHeader = req.headers.get("authorization");
+  const parts = (authHeader || "").split(" ");
+  if (parts.length > 1) return parts[1];
+
+  // check for query param
+  let arr: string[] = [];
+  const { access_token } = requestQuery.parse(Object.fromEntries(req.nextUrl.searchParams));
+  arr = arr.concat(access_token);
+  if (arr[0].length > 0) return arr[0];
+
+  throw new HttpError({ statusCode: 401, message: "Unauthorized" });
+};
+
+const requestQuery = z.object({
+  access_token: z.string(),
+});
+
+async function handler(req: NextRequest) {
+  const { oauthController } = await jackson();
+  const token = extractAuthToken(req);
+  const userInfo = await oauthController.userInfo(token);
+  return NextResponse.json(userInfo);
+}
+
+const getHandler = apiRouteMiddleware((req: NextRequest) => handler(req));
+
+export { getHandler as GET };
