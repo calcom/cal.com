bfa5c8024f2b3787f9ca7ee7f7d70fb5e210f96e
hbjORbj <sldisek783@gmail.com>
2025-02-25 08:28:00 -0500

    create separate file for types

--- packages/trpc/server/routers/_app.ts
+++ packages/trpc/server/routers/_app.ts
@@ -1,11 +1,20 @@
+/**
+ * This file contains the root router of your tRPC-backend
+ */
 import { router } from "../trpc";
 import { getViewerRouter } from "./viewer/_router";
 
+export { AppRouter } from "./viewer/_router.types";
+
+/**
+ * Create your application's root router
+ * If you want to use SSG, you need export this
+ * @link https://trpc.io/docs/ssg
+ * @link https://trpc.io/docs/router
+ */
 export async function createAppRouter() {
   const viewerRouter = await getViewerRouter();
   return router({
     viewer: viewerRouter,
   });
 }
-
-export type AppRouter = Awaited<typeof createAppRouter>;
