f4a66dd9acca7eaf9362a6189b74f9b72227f78f
hbjORbj <sldisek783@gmail.com>
2025-02-26 15:13:22 -0500

    fix build

--- apps/api/v1/pages/api/users/[userId]/_patch.ts
+++ apps/api/v1/pages/api/users/[userId]/_patch.ts
@@ -1,8 +1,8 @@
 import type { NextApiRequest } from "next";
 
 import { HttpError } from "@calcom/lib/http-error";
-import { defaultResponder } from "@calcom/lib/server/defaultResponder";
 import { uploadAvatar } from "@calcom/lib/server/avatar";
+import { defaultResponder } from "@calcom/lib/server/defaultResponder";
 import prisma from "@calcom/prisma";
 import type { Prisma } from "@calcom/prisma/client";
 
