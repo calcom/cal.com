0a1104fbb39640925206210b15e644cd6fca6213
Somay Chauhan <somaychauhan98@gmail.com>
2025-02-26 15:18:27 +0530

    Update transformApiEventTypeForAtom.ts

--- packages/platform/atoms/event-types/atom-api-transformers/transformApiEventTypeForAtom.ts
+++ packages/platform/atoms/event-types/atom-api-transformers/transformApiEventTypeForAtom.ts
@@ -329,7 +329,7 @@ function getLocations(locations: EventTypeOutput_2024_06_14["locations"]) {
 function isAtomSupportedLocation(
   location: EventTypeOutput_2024_06_14["locations"][number]
 ): location is InputLocation_2024_06_14 {
-  const supportedIntegrations = ["cal-video", "google-meet", "msteams"];
+  const supportedIntegrations = ["cal-video", "google-meet"];
 
   return (
     location.type === "address" ||
