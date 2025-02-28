375d815116addcf4aed8f84b3070daa352540af1
Eunjae Lee <hey@eunjae.dev>
2025-02-28 16:29:44 +0100

    fix type error

--- packages/features/data-table/hooks/useFilterValue.ts
+++ packages/features/data-table/hooks/useFilterValue.ts
@@ -1,13 +1,13 @@
 import { useMemo } from "react";
 import type { z } from "zod";
 
-import type { FilterValueSchema, ColumnFilterType } from "../lib/types";
+import type { FilterValueSchema, ColumnFilterType, ZFilterValue } from "../lib/types";
 import { useDataTable } from "./useDataTable";
 
-export function useFilterValue<T extends ColumnFilterType, TSchema extends FilterValueSchema<T>>(
-  columnId: string,
-  schema: TSchema
-) {
+export function useFilterValue<
+  T extends ColumnFilterType,
+  TSchema extends FilterValueSchema<T> | typeof ZFilterValue
+>(columnId: string, schema: TSchema) {
   const { activeFilters } = useDataTable();
   return useMemo(() => {
     const value = activeFilters.find((filter) => filter.f === columnId)?.v;
