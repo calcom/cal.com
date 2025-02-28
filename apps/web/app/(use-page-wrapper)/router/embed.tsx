0501be7c3b8eb76e25451c7dcc093b131949f9c6
sean-brydon <sean@cal.com>
2025-02-28 17:04:18 +0000

    migrate embed page + fix dynamic imports

--- apps/web/app/(use-page-wrapper)/router/embed.tsx
+++ apps/web/app/(use-page-wrapper)/router/embed.tsx
@@ -22,13 +22,15 @@ export default async function RouterPage({ params, searchParams }: _PageProps) {
   }
 
   return (
-    <div className="mx-auto my-0 max-w-3xl md:my-24">
-      <div className="w-full max-w-4xl ltr:mr-2 rtl:ml-2">
-        <div className="text-default bg-default -mx-4 rounded-sm border border-neutral-200 p-4 py-6 sm:mx-0 sm:px-8">
-          <div>{result.props?.message}</div>
+    <>
+      <div className="mx-auto my-0 max-w-3xl md:my-24">
+        <div className="w-full max-w-4xl ltr:mr-2 rtl:ml-2">
+          <div className="text-default bg-default -mx-4 rounded-sm border border-neutral-200 p-4 py-6 sm:mx-0 sm:px-8">
+            <div>{message}</div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
