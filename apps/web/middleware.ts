6db58589166e380451d7ab88249dbf6808f3fff1
hbjORbj <sldisek783@gmail.com>
2025-02-28 19:25:42 -0500

    fix

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -166,7 +166,6 @@ export const config = {
   // Next.js Doesn't support spread operator in config matcher, so, we must list all paths explicitly here.
   // https://github.com/vercel/next.js/discussions/42458
   matcher: [
-    "/",
     "/403",
     "/500",
     "/icons",
