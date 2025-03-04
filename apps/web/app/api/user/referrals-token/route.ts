de088be475c41e97f38c995773e2ea5dfb433691
Nizzy <nizabizaher@gmail.com>
2025-03-04 14:04:24 -0500

    get user

--- apps/web/app/api/user/referrals-token/route.ts
+++ apps/web/app/api/user/referrals-token/route.ts
@@ -1,26 +1,25 @@
+import { headers, cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 import { dub } from "@calcom/feature-auth/lib/dub";
+import { getServerSession } from "@calcom/feature-auth/lib/getServerSession";
+
+import { buildLegacyRequest } from "@lib/buildLegacyCtx";
 
 export const GET = async () => {
-  const session = {
-    user: {
-      id: "123",
-      name: "John Doe",
-      email: "john.doe@example.com",
-      username: "john",
-      image: "https://example.com/image.png",
-    },
-  };
+  const session = await getServerSession({ req: buildLegacyRequest(headers(), cookies()) });
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
   const { publicToken } = await dub.embedTokens.referrals({
     programId: "prog_mODHMDrJPWlkpT7uzsUASFhK",
-    tenantId: session.user.id,
+    tenantId: session.user.id.toString(),
     partner: {
-      name: session.user.name,
-      email: session.user.email,
-      username: session.user.username,
-      image: session.user.image || null,
-      tenantId: session.user.id,
+      name: session?.user.name || "",
+      email: session?.user.email || "",
+      username: session?.user.username || "",
+      image: session?.user.image || null,
+      tenantId: session?.user.id.toString() || "",
     },
   });
 
