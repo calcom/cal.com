5a93d013223626b490facd3c9f1db69bff5194b6
Anik Dhabal Babu <adhabal2002@gmail.com>
2025-03-04 15:40:44 +0000

    fix: daterange picker side

--- packages/ui/components/form/date-range-picker/DateRangePicker.tsx
+++ packages/ui/components/form/date-range-picker/DateRangePicker.tsx
@@ -84,10 +84,7 @@ export function DatePickerWithRange({
         </Popover.Trigger>
         <Popover.Content
           className="bg-default text-emphasis z-50 w-auto rounded-md border p-0 outline-none"
-          align="start"
-          sideOffset={4}
-          side="bottom"
-          avoidCollisions={false}>
+          align="start">
           {calendar}
         </Popover.Content>
       </Popover.Root>
