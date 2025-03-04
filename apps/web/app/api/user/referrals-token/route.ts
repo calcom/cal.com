1e267c01bfed84839775eaf6f8c6badb3f7dc24d
Steven Tey <stevensteel97@gmail.com>
2025-03-03 22:07:26 -0800

    Update route.ts

--- apps/web/app/api/user/referrals-token/route.ts
+++ apps/web/app/api/user/referrals-token/route.ts
@@ -8,15 +8,17 @@ export const GET = async () => {
       id: "123",
       name: "John Doe",
       email: "john.doe@example.com",
+      username: "john",
       image: "https://example.com/image.png",
     },
   };
   const { publicToken } = await dub.embedTokens.referrals({
-    programId: "prog_d8pl69xXCv4AoHNT281pHQdo",
+    programId: "prog_mODHMDrJPWlkpT7uzsUASFhK",
     tenantId: session.user.id,
     partner: {
       name: session.user.name,
       email: session.user.email,
+      username: session.user.username,
       image: session.user.image || null,
       tenantId: session.user.id,
     },
