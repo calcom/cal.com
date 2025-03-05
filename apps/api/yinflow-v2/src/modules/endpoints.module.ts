dd8b80e65531f3068b32896ab18c374429923928
Felippe Fernandes <lfelippeeng@gmail.com>
2025-03-04 22:05:59 -0300

    fix: directus module

--- apps/api/yinflow-v2/src/modules/endpoints.module.ts
+++ apps/api/yinflow-v2/src/modules/endpoints.module.ts
@@ -1,10 +1,10 @@
-import { DirectusModule } from "@/modules/directus/directus.module";
 import type { MiddlewareConsumer, NestModule } from "@nestjs/common";
 import { Module } from "@nestjs/common";
 
 import { PlatformEndpointsModule } from "../ee/platform-endpoints-module";
 import { BillingModule } from "./billing/billing.module";
 import { DestinationCalendarsModule } from "./destination-calendars/destination-calendars.module";
+import { DirectusModule } from "./directus/directus.module";
 import { OAuthClientModule } from "./oauth-clients/oauth-client.module";
 import { TimezoneModule } from "./timezones/timezones.module";
 import { UsersModule } from "./users/users.module";
