1b8c5b67d4da7cd5db31bf38d171400baac5b40f
Morgan Vernay <morgan@cal.com>
2025-03-07 12:02:15 +0200

    fix: barrel file platform libraries example

--- packages/platform/libraries/index.ts
+++ packages/platform/libraries/index.ts
@@ -207,8 +207,8 @@ export type {
   InternalLocation,
 } from "@calcom/lib/event-types/transformers";
 
-export { parseBookingLimit, parseEventTypeColor } from "@calcom/lib";
-
+export { parseEventTypeColor } from "@calcom/lib";
+export { parseBookingLimit } from "@calcom/lib/intervalLimits/isBookingLimits";
 export { parseRecurringEvent } from "@calcom/lib/isRecurringEvent";
 export { dynamicEvent } from "@calcom/lib/defaultEvents";
 
