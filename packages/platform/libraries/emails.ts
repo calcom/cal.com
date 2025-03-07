ab00a4fe9ba50caa8781b004caef3e7bace2621b
Morgan Vernay <morgan@cal.com>
2025-03-07 14:28:29 +0200

    refactor: extract emails from libraries barrel file

---
+++ packages/platform/libraries/emails.ts
@@ -0,0 +1,33 @@
+import AttendeeCancelledEmail from "@calcom/emails/templates/attendee-cancelled-email";
+import AttendeeDeclinedEmail from "@calcom/emails/templates/attendee-declined-email";
+import AttendeeRequestEmail from "@calcom/emails/templates/attendee-request-email";
+import AttendeeRescheduledEmail from "@calcom/emails/templates/attendee-rescheduled-email";
+import AttendeeScheduledEmail from "@calcom/emails/templates/attendee-scheduled-email";
+import AttendeeUpdatedEmail from "@calcom/emails/templates/attendee-updated-email";
+import OrganizerCancelledEmail from "@calcom/emails/templates/organizer-cancelled-email";
+import OrganizerReassignedEmail from "@calcom/emails/templates/organizer-reassigned-email";
+import OrganizerRequestEmail from "@calcom/emails/templates/organizer-request-email";
+import OrganizerRescheduledEmail from "@calcom/emails/templates/organizer-rescheduled-email";
+import OrganizerScheduledEmail from "@calcom/emails/templates/organizer-scheduled-email";
+
+export { AttendeeScheduledEmail };
+
+export { OrganizerScheduledEmail };
+
+export { AttendeeDeclinedEmail };
+
+export { AttendeeCancelledEmail };
+
+export { OrganizerCancelledEmail };
+
+export { OrganizerReassignedEmail };
+
+export { OrganizerRescheduledEmail };
+
+export { AttendeeRescheduledEmail };
+
+export { AttendeeUpdatedEmail };
+
+export { OrganizerRequestEmail };
+
+export { AttendeeRequestEmail };
