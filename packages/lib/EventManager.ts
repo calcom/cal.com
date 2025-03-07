b593a5d3c12a39b587d7cada18a9dc4238522b7d
Anik Dhabal Babu <81948346+anikdhabal@users.noreply.github.com>
2025-03-07 22:45:33 +0530

    Update EventManager.ts

--- packages/lib/EventManager.ts
+++ packages/lib/EventManager.ts
@@ -181,6 +181,7 @@ export default class EventManager {
   public async create(event: CalendarEvent): Promise<CreateUpdateResult> {
     const evt = processLocation(event);
 
+    log.warn(`Falling back to cal video if no location is set: ${evt.location}`);
     // Fallback to cal video if no location is set
     if (!evt.location) {
       // See if cal video is enabled & has keys
@@ -197,7 +198,6 @@ export default class EventManager {
       const calVideoKeys = calVideoKeysSchema.safeParse(calVideo?.keys);
 
       if (calVideo?.enabled && calVideoKeys.success) evt["location"] = "integrations:daily";
-      log.warn(`Falling back to cal video if no location is set: ${evt.location}`);
     }
 
     // Fallback to Cal Video if Google Meet is selected w/o a Google Cal
