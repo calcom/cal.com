e50f1cfceda9dd900d303183630e8ea5e5ba10c5
Neon-20 <rajveer23pr@gmail.com>
2025-03-08 16:50:08 +0530

    feat: added react-atom to embed generator and renamed others

--- packages/features/embed/lib/EmbedTabs.tsx
+++ packages/features/embed/lib/EmbedTabs.tsx
@@ -74,9 +74,9 @@ export const tabs = [
     }),
   },
   {
-    name: "React",
+    name: "React (Standard)",
     href: "embedTabName=embed-react",
-    "data-testid": "react",
+    "data-testid": "react-standard",
     icon: "code" as const,
     type: "code",
     Component: forwardRef<
@@ -111,6 +111,56 @@ export const tabs = [
   // npm install @calcom/embed-react
   ${getEmbedTypeSpecificString({
     embedFramework: "react",
+    embedType,
+    calLink,
+    previewState,
+    embedCalOrigin,
+    namespace,
+  })}
+  `}
+          />
+        </>
+      );
+    }),
+  },
+  {
+    name: "React (Atom)",
+    href: "embedTabName=embed-react-atom",
+    "data-testid": "react-atom",
+    icon: "code" as const,
+    type: "code",
+    Component: forwardRef<
+      HTMLTextAreaElement | HTMLIFrameElement | null,
+      { embedType: EmbedType; calLink: string; previewState: PreviewState; namespace: string }
+    >(function EmbedReactAtom({ embedType, calLink, previewState, namespace }, ref) {
+      const { t } = useLocale();
+      const embedCalOrigin = useEmbedCalOrigin();
+
+      if (ref instanceof Function || !ref) {
+        return null;
+      }
+      if (ref.current && !(ref.current instanceof HTMLTextAreaElement)) {
+        return null;
+      }
+      return (
+        <>
+          <small className="text-subtle flex py-2">{t("create_update_react_component")}</small>
+          <TextArea
+            data-testid="embed-react-atom"
+            ref={ref as typeof ref & MutableRefObject<HTMLTextAreaElement>}
+            name="embed-react-atom"
+            className="text-default bg-default h-[calc(100%-50px)] font-mono"
+            readOnly
+            style={{ resize: "none", overflow: "auto" }}
+            value={`/* First make sure that you have installed the package */
+
+  /* If you are using yarn */
+  // yarn add @calcom/embed-react
+
+  /* If you are using npm */
+  // npm install @calcom/embed-react
+  ${getEmbedTypeSpecificString({
+    embedFramework: "react-atom",
     embedType,
     calLink,
     previewState,
