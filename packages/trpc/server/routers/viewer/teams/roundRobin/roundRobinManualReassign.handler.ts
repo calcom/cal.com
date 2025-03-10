95fbb096f20b0e10180776f1de4cf2499c25c2b6
sean-brydon <sean@cal.com>
2025-03-10 09:24:13 +0000

    fix trpc session circle dep

--- packages/trpc/server/routers/viewer/teams/roundRobin/roundRobinManualReassign.handler.ts
+++ packages/trpc/server/routers/viewer/teams/roundRobin/roundRobinManualReassign.handler.ts
@@ -1,6 +1,6 @@
 import { roundRobinManualReassignment } from "@calcom/features/ee/round-robin/roundRobinManualReassignment";
 import { BookingRepository } from "@calcom/lib/server/repository/booking";
-import type { TrpcSessionUser } from "@calcom/trpc/server/trpc";
+import type { TrpcSessionUser } from "@calcom/trpc/server/types";
 
 import { TRPCError } from "@trpc/server";
 
