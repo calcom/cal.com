72a1afb2162b407abe125383c67ab620d63b2edc
hbjORbj <sldisek783@gmail.com>
2025-03-10 13:39:46 -0400

    remove i18n from next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -183,12 +183,6 @@ const nextConfig = {
     optimizePackageImports: ["@calcom/ui"],
     turbo: {},
   },
-  i18n: {
-    ...i18n,
-    defaultLocale: "en",
-    locales: ["en"],
-    localeDetection: false,
-  },
   productionBrowserSourceMaps: process.env.SENTRY_DISABLE_CLIENT_SOURCE_MAPS === "0",
   /* We already do type check on GH actions */
   typescript: {
