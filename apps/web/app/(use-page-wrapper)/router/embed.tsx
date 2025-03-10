8b8e48989a90065534c2fa8ce4f4c62f21b87a61
Hariom Balhara <hariombalhara@gmail.com>
2025-03-10 11:08:48 +0530

    Add comment back

--- apps/web/app/(use-page-wrapper)/router/embed.tsx
+++ apps/web/app/(use-page-wrapper)/router/embed.tsx
@@ -1,1 +1,3 @@
+// We are not using withEmbedSsr here because page.tsx already has the logic to append /embed to the redirect URL and it ends up as /embed/embed if we use withEmbedSsr here
+// TODO: We should use withEmbedSsr here and remove the logic from page.tsx
 export { default } from "./page";
