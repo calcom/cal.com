34a594043f3a3954ea4c7e5a68d9dac9103d1479
hbjORbj <sldisek783@gmail.com>
2025-03-10 16:59:04 -0400

    add comments

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -177,7 +177,10 @@ const orgDomainMatcherConfig = {
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined,
-  serverExternalPackages: ["next-i18next", "deasync", "http-cookie-agent", "rest-facade", "superagent-proxy"],
+  serverExternalPackages: ["next-i18next", 
+    "deasync", "http-cookie-agent", // Dependencies of @ewsjs/xhr
+    "rest-facade", "superagent-proxy" // Dependencies of @tryvital/vital-node
+  ],
   experimental: {
     // externalize server-side node_modules with size > 1mb, to improve dev mode performance/RAM usage
     optimizePackageImports: ["@calcom/ui"],
