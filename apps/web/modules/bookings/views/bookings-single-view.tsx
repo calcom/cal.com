d04008293c561b4a33f211df545e6bb16e655a76
amrit <iamamrit27@gmail.com>
2025-03-13 10:44:10 +0530

    fix: ensure correct dymanic values are passed in getTitle()

--- apps/web/modules/bookings/views/bookings-single-view.tsx
+++ apps/web/modules/bookings/views/bookings-single-view.tsx
@@ -308,19 +308,14 @@ export default function Success(props: PageProps) {
       const isAttendee = bookingInfo.attendees.find((attendee) => attendee.email === session?.user?.email);
       const attendee = bookingInfo.attendees[0]?.name || bookingInfo.attendees[0]?.email || "Nameless";
       const host = bookingInfo.user.name || bookingInfo.user.email;
-      if (isHost) {
-        return t(`${titlePrefix}emailed_host_and_attendee${titleSuffix}`, {
-          host,
-          attendee,
+
+      if (isHost || isAttendee) {
+        return t(`round_robin_emailed_you_and_attendees${titleSuffix}`, {
+          user: isHost ? attendee : host,
         });
       }
-      if (isAttendee) {
-        return t(`${titlePrefix}emailed_host_and_attendee${titleSuffix}`, {
-          host,
-          attendee,
-        });
-      }
-      return t(`${titlePrefix}emailed_host_and_attendee${titleSuffix}`, {
+
+      return t(`round_robin_emailed_host_and_attendee${titleSuffix}`, {
         host,
         attendee,
       });
