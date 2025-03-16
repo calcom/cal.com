612445be8715cbe25eab9910d9a303b2db242454
Keith Williams <keithwillcode@gmail.com>
2025-03-16 07:57:24 -0300

    Removed used of importHandler

--- packages/trpc/server/routers/viewer/i18n/_router.tsx
+++ packages/trpc/server/routers/viewer/i18n/_router.tsx
@@ -1,14 +1,10 @@
 import publicProcedure from "../../../procedures/publicProcedure";
-import { importHandler, router } from "../../../trpc";
+import { router } from "../../../trpc";
 import { i18nInputSchema } from "./i18n.schema";
-
-const NAMESPACE = "viewer";
-
-const namespaced = (s: string) => `${NAMESPACE}.${s}`;
 
 export const i18nRouter = router({
   get: publicProcedure.input(i18nInputSchema).query(async (opts) => {
-    const handler = await importHandler(namespaced("i18n"), () => import("./i18n.handler"));
+    const handler = (await import("./i18n.handler")).i18nHandler;
     return handler(opts);
   }),
 });
