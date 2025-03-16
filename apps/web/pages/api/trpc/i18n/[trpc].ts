ce3639f2a402bda37043b28521def0a8ea31482d
Keith Williams <keithwillcode@gmail.com>
2025-03-16 09:11:14 -0300

    Added the namespace to the cache rule for it to work

--- apps/web/pages/api/trpc/i18n/[trpc].ts
+++ apps/web/pages/api/trpc/i18n/[trpc].ts
@@ -1,4 +1,4 @@
 import { createNextApiHandler } from "@calcom/trpc/server/createNextApiHandler";
 import { i18nRouter } from "@calcom/trpc/server/routers/viewer/i18n/_router";
 
-export default createNextApiHandler(i18nRouter, true);
+export default createNextApiHandler(i18nRouter, true, "i18n");
