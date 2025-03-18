5c0612eca6127905c67fe54a72e26ed01f9adbc1
Keith Williams <keithwillcode@gmail.com>
2025-03-18 16:30:49 -0300

    Added back the files to help with type generation

---
+++ packages/trpc/server/routers/_app.ts
@@ -0,0 +1,17 @@
+/**
+ * This file contains the root router of your tRPC-backend
+ */
+import { router } from "../trpc";
+import { viewerRouter } from "./viewer/_router";
+
+/**
+ * Create your application's root router
+ * If you want to use SSG, you need export this
+ * @link https://trpc.io/docs/ssg
+ * @link https://trpc.io/docs/router
+ */
+export const appRouter = router({
+  viewer: viewerRouter,
+});
+
+export type AppRouter = typeof appRouter;
