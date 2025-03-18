11a8038407e0c5e17963255a2d3b3f5f64b79294
Tushar <tusharbhatt0135@gmail.com>
2025-03-18 22:16:00 +0530

    fix: spacing-issue

--- packages/app-store/_components/AppCategoryNavigation.tsx
+++ packages/app-store/_components/AppCategoryNavigation.tsx
@@ -31,7 +31,7 @@ const AppCategoryNavigation = ({
   const appCategories = useMemo(() => getAppCategories(baseURL, useQueryParam), [baseURL, useQueryParam]);
 
   return (
-    <div className={cs("flex flex-col gap-x-6 md:p-0 xl:flex-row", classNames?.root ?? className)}>
+    <div className={cs("flex flex-col md:p-0 xl:flex-row xl:space-x-6", classNames?.root ?? className)}>
       <div className="hidden xl:block">
         <VerticalTabs tabs={appCategories} sticky linkShallow itemClassname={classNames?.verticalTabsItem} />
       </div>
