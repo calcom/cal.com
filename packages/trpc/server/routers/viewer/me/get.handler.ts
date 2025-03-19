3c1297aa72a2a3293b015a3484af96556c522b12
sean-brydon <55134778+sean-brydon@users.noreply.github.com>
2025-03-19 08:50:22 +0000

    fix: calcom trpc sessio circle dep (#19893)
    * fix trpc session circle dep
    
    * fixes trpc session cirlce dep
    
    * fix relative imports
    
    * fix more imports to use types and not trpc
    
    * fix exports
    
    * Fixed types
    
    ---------
    
    Co-authored-by: Keith Williams <keithwillcode@gmail.com>

--- packages/trpc/server/routers/viewer/me/get.handler.ts
+++ packages/trpc/server/routers/viewer/me/get.handler.ts
@@ -6,7 +6,7 @@ import { UserRepository } from "@calcom/lib/server/repository/user";
 import prisma from "@calcom/prisma";
 import { IdentityProvider, MembershipRole } from "@calcom/prisma/enums";
 import { userMetadata } from "@calcom/prisma/zod-utils";
-import type { TrpcSessionUser } from "@calcom/trpc/server/trpc";
+import type { TrpcSessionUser } from "@calcom/trpc/server/types";
 
 import type { TGetInputSchema } from "./get.schema";
 
