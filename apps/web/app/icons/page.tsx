8540294a58606827cb076be433a18a84c8ef1c90
hbjORbj <sldisek783@gmail.com>
2025-03-19 12:47:05 -0400

    fix

--- apps/web/app/icons/page.tsx
+++ apps/web/app/icons/page.tsx
@@ -2,7 +2,7 @@ import { _generateMetadata, getTranslate } from "app/_utils";
 import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 
-import { IconSprites } from "@calcom/ui/components/icon/IconSprites";
+import { IconSprites } from "@calcom/ui/components/icon";
 import type { IconName } from "@calcom/ui/components/icon";
 
 import { lucideIconList } from "../../../../packages/ui/components/icon/icon-list.mjs";
