b0bf2748963c3c2acf027be34bb02723e36c9961
Benny Joo <sldisek783@gmail.com>
2025-03-20 06:26:12 -0400

    perf: Remove `packages/ui/index.ts` barrel file (#20243)
    * remove packages/ui/index.ts
    
    * Update tests

--- apps/web/components/dialog/__tests__/RerouteDialog.test.tsx
+++ apps/web/components/dialog/__tests__/RerouteDialog.test.tsx
@@ -82,15 +82,9 @@ vi.mock("@calcom/web/lib/hooks/useRouterQuery", () => ({
   }),
 }));
 
-vi.mock("@calcom/ui", async (importOriginal) => {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  const actual = await importOriginal<any>("@calcom/ui");
-  return {
-    ...actual,
-    Tooltip: vi.fn(({ children }) => <div data-testid="mock-tooltip">{children}</div>),
-  };
-});
+vi.mock("@calcom/ui/components/tooltip", () => ({
+  Tooltip: vi.fn(({ children }) => <div data-testid="mock-tooltip">{children}</div>),
+}));
 
 vi.mock("@calcom/trpc/react", () => ({
   trpc: {
