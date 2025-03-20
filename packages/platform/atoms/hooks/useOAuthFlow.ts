729eac3eee9f81c19b5368bbae4b1666884221fd
Morgan Vernay <morgan@cal.com>
2025-03-20 11:44:50 +0200

    fix: axios error response interceptor

--- packages/platform/atoms/hooks/useOAuthFlow.ts
+++ packages/platform/atoms/hooks/useOAuthFlow.ts
@@ -42,7 +42,7 @@ export const useOAuthFlow = ({ accessToken, refreshUrl, clientId, onError, onSuc
 
               setIsRefreshing(false);
             }
-            return Promise.reject(err.response);
+            return Promise.reject(err);
           })
         : "";
 
