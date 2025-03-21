fcdccd85cebef1060cde346e81feb2a960bbfe21
Omar López <zomars@me.com>
2025-03-21 11:26:41 -0700

    Update layout.tsx

--- apps/web/app/(use-page-wrapper)/(main-nav)/layout.tsx
+++ apps/web/app/(use-page-wrapper)/(main-nav)/layout.tsx
@@ -1,6 +1,5 @@
 import { setUser as SentrySetUser } from "@sentry/nextjs";
 import { cookies, headers } from "next/headers";
-import { redirect } from "next/navigation";
 import React from "react";
 
 import { getServerSession } from "@calcom/features/auth/lib/getServerSession";
@@ -10,12 +9,7 @@ import { buildLegacyRequest } from "@lib/buildLegacyCtx";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const session = await getServerSession({ req: buildLegacyRequest(await headers(), await cookies()) });
-
-  if (!session?.user?.id) {
-    redirect("/auth/login");
-  }
-
-  SentrySetUser({ id: session.user.id });
+  if (session?.user?.id) SentrySetUser({ id: session.user.id });
 
   return (
     <Shell withoutSeo={true} withoutMain={true}>
