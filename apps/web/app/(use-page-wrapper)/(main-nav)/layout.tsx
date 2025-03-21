5bc37c9a9d14c2c4dc6ec3c85f3315e0cc305f02
Omar López <zomars@me.com>
2025-03-21 09:22:43 -0700

    Added webapp

--- apps/web/app/(use-page-wrapper)/(main-nav)/layout.tsx
+++ apps/web/app/(use-page-wrapper)/(main-nav)/layout.tsx
@@ -1,6 +1,22 @@
+import { setUser as SentrySetUser } from "@sentry/nextjs";
+import { cookies, headers } from "next/headers";
+import { redirect } from "next/navigation";
+import React from "react";
+
+import { getServerSession } from "@calcom/features/auth/lib/getServerSession";
 import Shell from "@calcom/features/shell/Shell";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+import { buildLegacyRequest } from "@lib/buildLegacyCtx";
+
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await getServerSession({ req: buildLegacyRequest(await headers(), await cookies()) });
+
+  if (!session?.user?.id) {
+    redirect("/auth/login");
+  }
+
+  SentrySetUser({ id: session.user.id });
+
   return (
     <Shell withoutSeo={true} withoutMain={true}>
       {children}
