014861d9b9c7f1206701e91a662232391b33306e
Felippe Fernandes <lfelippeeng@gmail.com>
2025-03-21 14:32:29 -0300

    fix: remove ioredis

--- apps/api/yinflow-v2/src/app.module.ts
+++ apps/api/yinflow-v2/src/app.module.ts
@@ -1,10 +1,7 @@
-import { ThrottlerStorageRedisService } from "@nest-lab/throttler-storage-redis";
-import { BullModule } from "@nestjs/bull";
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { APP_GUARD, APP_INTERCEPTOR, APP_FILTER } from "@nestjs/core";
-import { seconds, ThrottlerModule } from "@nestjs/throttler";
-import { SentryModule, SentryGlobalFilter } from "@sentry/nestjs/setup";
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from "@nestjs/core";
+import { SentryGlobalFilter, SentryModule } from "@sentry/nestjs/setup";
 
 import { AppController } from "./app.controller";
 import appConfig from "./config/app";
@@ -21,7 +18,6 @@ import { EndpointsModule } from "./modules/endpoints.module";
 import { JwtModule } from "./modules/jwt/jwt.module";
 import { PrismaModule } from "./modules/prisma/prisma.module";
 import { RedisModule } from "./modules/redis/redis.module";
-import { RedisService } from "./modules/redis/redis.service";
 
 @Module({
   imports: [
@@ -33,27 +29,6 @@ import { RedisService } from "./modules/redis/redis.service";
     }),
 
     RedisModule,
-    BullModule.forRoot({
-      redis: `${process.env.REDIS_URL}${process.env.NODE_ENV === "production" ? "?tls=true" : ""}`,
-    }),
-    ThrottlerModule.forRootAsync({
-      imports: [RedisModule],
-      inject: [RedisService],
-      useFactory: (redisService: RedisService) => ({
-        // note(Lauris): IMPORTANT: rate limiting is enforced by CustomThrottlerGuard, but we need to have at least one
-        // entry in the throttlers array otherwise CustomThrottlerGuard is not invoked at all. If we specify only ThrottlerModule
-        // without .forRootAsync then throttler options are not passed to CustomThrottlerGuard containing redis connection etc.
-        // So we need to specify at least one dummy throttler here and CustomThrottlerGuard is actually handling the default and custom rate limits.
-        throttlers: [
-          {
-            name: "dummy",
-            ttl: seconds(60),
-            limit: 120,
-          },
-        ],
-        storage: new ThrottlerStorageRedisService(redisService.redis),
-      }),
-    }),
     PrismaModule,
     EndpointsModule,
     AuthModule,
@@ -64,13 +39,6 @@ import { RedisService } from "./modules/redis/redis.service";
     {
       provide: APP_FILTER,
       useClass: SentryGlobalFilter,
-    },
-    {
-      provide: ThrottlerStorageRedisService,
-      useFactory: (redisService: RedisService) => {
-        return new ThrottlerStorageRedisService(redisService.redis);
-      },
-      inject: [RedisService],
     },
     {
       provide: APP_INTERCEPTOR,
