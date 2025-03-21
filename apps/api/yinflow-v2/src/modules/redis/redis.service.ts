efb02c4e26068677b2eb0c2783dee879a9347663
Felippe Fernandes <lfelippeeng@gmail.com>
2025-03-21 09:55:42 -0300

    fix: remove redis

--- apps/api/yinflow-v2/src/modules/redis/redis.service.ts
+++ apps/api/yinflow-v2/src/modules/redis/redis.service.ts
@@ -1,26 +1,4 @@
-import { Injectable, OnModuleDestroy, Logger } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { Redis } from "ioredis";
-
-import { AppConfig } from "../../config/type";
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class RedisService implements OnModuleDestroy {
-  public redis: Redis;
-  private readonly logger = new Logger("RedisService");
-
-  constructor(readonly configService: ConfigService<AppConfig>) {
-    const dbUrl = configService.get<string>("db.redisUrl", { infer: true });
-    if (!dbUrl) throw new Error("Misconfigured Redis, halting.");
-
-    this.redis = new Redis(dbUrl);
-  }
-
-  async onModuleDestroy() {
-    try {
-      await this.redis.quit();
-    } catch (err) {
-      this.logger.error(err);
-    }
-  }
-}
+export class RedisService {}
