5dfcdaa4a658db1312a9824fb6be413acc4c708f
Omar López <zomars@me.com>
2025-03-24 10:49:21 -0700

    Update captureUserId.ts

--- apps/api/v1/lib/helpers/captureUserId.ts
+++ apps/api/v1/lib/helpers/captureUserId.ts
@@ -2,9 +2,7 @@ import { setUser as SentrySetUser } from "@sentry/nextjs";
 import type { NextMiddleware } from "next-api-middleware";
 
 export const captureUserId: NextMiddleware = async (req, res, next) => {
-  if (!req.userId) return res.status(401).json({ message: "No userId provided" });
-
-  SentrySetUser({ id: req.userId });
+  if (req.userId) SentrySetUser({ id: req.userId });
 
   await next();
 };
