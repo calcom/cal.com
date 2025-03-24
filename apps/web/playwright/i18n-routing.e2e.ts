3281b6093a44e2d1e62c95e6de8999d6793fcf25
hbjORbj <sldisek783@gmail.com>
2025-03-24 18:27:32 -0400

    update e2e test

--- apps/web/playwright/i18n-routing.e2e.ts
+++ apps/web/playwright/i18n-routing.e2e.ts
@@ -2,26 +2,30 @@ import { expect } from "@playwright/test";
 
 import { test } from "./lib/fixtures";
 
-test.describe("/en/* pages must not 404", () => {
-  test("/en/login shouldn't 404", async ({ page }) => {
-    const response = await page.goto(`/en/login`);
-    expect(response?.status()).not.toBe(404);
-  });
+test.describe("Locale-specific pages must not 404", () => {
+  for (const locale of ["en", "fr", "es"]) {
+    test.describe(`/${locale}/* pages`, () => {
+      test(`/${locale}/login shouldn't 404`, async ({ page }) => {
+        const response = await page.goto(`/${locale}/login`);
+        expect(response?.status()).not.toBe(404);
+      });
 
-  test("/en/auth/login shouldn't 404", async ({ page }) => {
-    const response = await page.goto(`/en/auth/login`);
-    expect(response?.status()).not.toBe(404);
-  });
+      test(`/${locale}/auth/login shouldn't 404`, async ({ page }) => {
+        const response = await page.goto(`/${locale}/auth/login`);
+        expect(response?.status()).not.toBe(404);
+      });
 
-  test("/en/[user] page shouldn't 404", async ({ page, users }) => {
-    const user = await users.create();
-    const response = await page.goto(`/en/${user.username}`);
-    expect(response?.status()).not.toBe(404);
-  });
+      test(`/${locale}/[user] page shouldn't 404`, async ({ page, users }) => {
+        const user = await users.create();
+        const response = await page.goto(`/${locale}/${user.username}`);
+        expect(response?.status()).not.toBe(404);
+      });
 
-  test("/en/[user]/[type] page shouldn't 404", async ({ page, users }) => {
-    const user = await users.create();
-    const response = await page.goto(`/en/${user.username}/30-min`);
-    expect(response?.status()).not.toBe(404);
-  });
+      test(`/${locale}/[user]/[type] page shouldn't 404`, async ({ page, users }) => {
+        const user = await users.create();
+        const response = await page.goto(`/${locale}/${user.username}/30-min`);
+        expect(response?.status()).not.toBe(404);
+      });
+    });
+  }
 });
