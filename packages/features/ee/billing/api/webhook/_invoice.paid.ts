abf6a3c3ef2398f30442a14c2bf732e2479393ad
sean-brydon <sean@cal.com>
2025-03-24 16:18:02 +0000

    no throw 202

--- packages/features/ee/billing/api/webhook/_invoice.paid.ts
+++ packages/features/ee/billing/api/webhook/_invoice.paid.ts
@@ -1,6 +1,5 @@
 import logger from "@calcom/lib/logger";
 
-import { HttpCode } from "./__handler";
 import type { LazyModule, SWHMap } from "./__handler";
 
 type Data = SWHMap["invoice.paid"]["data"];
@@ -30,10 +29,16 @@ const stripeWebhookProductHandler = (handlers: Handlers) => async (data: Data) =
   }
 
   const handlerGetter = handlers[productId as keyof typeof handlers];
-  if (!handlerGetter) throw new HttpCode(202, `No product handler found for product: ${productId}`);
+  if (!handlerGetter) {
+    log.error(`Skipping product: ${productId} because no handler found`);
+    return { success: true };
+  }
   const handler = (await handlerGetter())?.default;
   // auto catch unsupported Stripe products.
-  if (!handler) throw new HttpCode(202, `No product handler found for product: ${productId}`);
+  if (!handler) {
+    log.error(`Skipping product: ${productId} because no handler found`);
+    return { success: true };
+  }
   return await handler(data);
 };
 
