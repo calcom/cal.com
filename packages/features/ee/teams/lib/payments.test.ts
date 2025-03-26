25afba840aab0c7b7608b17d52a664aad6e63840
sean-brydon <sean@cal.com>
2025-03-26 08:14:45 +0000

    fix enum

--- packages/features/ee/teams/lib/payments.test.ts
+++ packages/features/ee/teams/lib/payments.test.ts
@@ -3,6 +3,7 @@ import prismock from "../../../../../tests/libs/__mocks__/prisma";
 import { describe, expect, it, vi, beforeEach, afterEach } from "vitest";
 
 import stripe from "@calcom/features/ee/payments/server/stripe";
+import { BillingPeriod } from "@calcom/prisma/zod-utils";
 
 import {
   getTeamWithPaymentMetadata,
@@ -182,7 +183,7 @@ describe("purchaseTeamOrOrgSubscription", () => {
         userId: user.id,
         isOrg: true,
         pricePerSeat: 100,
-        billingPeriod: "MONTHLY",
+        billingPeriod: BillingPeriod.MONTHLY,
       })
     ).toEqual({ url: "SESSION_URL" });
 
@@ -253,7 +254,7 @@ describe("purchaseTeamOrOrgSubscription", () => {
         userId: user.id,
         isOrg: true,
         pricePerSeat: 100,
-        billingPeriod: "ANNUALLY",
+        billingPeriod: BillingPeriod.ANNUALLY,
       })
     ).toEqual({ url: "SESSION_URL" });
 
@@ -324,7 +325,8 @@ describe("purchaseTeamOrOrgSubscription", () => {
         seatsToChargeFor,
         userId: user.id,
         isOrg: true,
-        billingPeriod: "ANNUALLY",
+        billingPeriod: BillingPeriod.ANNUALLY,
+        pricePerSeat: null,
       })
     ).toEqual({ url: "SESSION_URL" });
 
