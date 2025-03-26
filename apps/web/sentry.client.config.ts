4b3d894d14edd7abc6baa76575c2e10447bd7be7
Hariom Balhara <hariombalhara@gmail.com>
2025-03-26 14:06:24 +0530

    use sentry for logging

--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -1,1 +1,14 @@
-export {};
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  debug: !!process.env.SENTRY_DEBUG,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+
+  // Disable performance monitoring
+  tracesSampleRate: parseFloat(process.env.NEXT_PUBLIC_SENTRY_TRACES_SAMPLE_RATE_CLIENT ?? "0.0") || 0.0,
+
+  // Disable session replay - It could be super costly performance wise, so enable separately
+  replaysSessionSampleRate: 0,
+  // It could be costly too to track, so enable separately
+  replaysOnErrorSampleRate: 0,
+});
--- packages/features/bookings/Booker/utils/logEvent.ts
+++ packages/features/bookings/Booker/utils/logEvent.ts
@@ -1,7 +1,8 @@
+import * as Sentry from "@sentry/nextjs";
+
 // Consider making it an hook, that would retrieve useful data from useBookerStore and send along with the event
 export const logEvent = (eventName: string, data: Record<string, unknown>) => {
-  fetch("/api/log", {
-    method: "POST",
-    body: JSON.stringify({ eventName, data }),
+  Sentry.captureMessage(eventName, {
+    extra: data,
   });
 };
