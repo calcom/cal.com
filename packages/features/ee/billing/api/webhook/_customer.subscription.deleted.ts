a4170334a449d6157c50f9dff70ae16b415bd598
Alex van Andel <me@alexvanandel.com>
2025-03-26 18:01:22 +0000

    fix: Implement backwards compatible stripe change to customer.subscription.deleted

--- packages/features/ee/billing/api/webhook/_customer.subscription.deleted.ts
+++ packages/features/ee/billing/api/webhook/_customer.subscription.deleted.ts
@@ -9,10 +9,23 @@ const STRIPE_TEAM_PRODUCT_ID = process.env.STRIPE_TEAM_PRODUCT_ID || "";
 
 const stripeWebhookProductHandler = (handlers: Handlers) => async (data: Data) => {
   const subscription = data.object;
-  // @ts-expect-error - we know subscription.plan.product is defined when unsubscribing
-  const productId = subscription.plan.product; // prod_xxxxx
-
-  const handlerGetter = handlers[productId];
+  let productId: string;
+  // @ts-expect-error - support legacy just in case.
+  if (subscription.plan) {
+    // @ts-expect-error - we know subscription.plan.product is defined when unsubscribing
+    productId = subscription.plan.product; // prod_xxxxx
+  } else {
+    const subscriptionItem = subscription.items?.data?.[0];
+    if (!subscriptionItem) {
+      throw new Error("Subscription item and plan missing");
+    }
+    const product = subscription.items.data[0]?.price.product;
+    productId = typeof product === "string" ? product : product.id;
+  }
+  if (typeof productId !== "string") {
+    throw new Error(`Product ID ${productId} is not a string`);
+  }
+  const handlerGetter = handlers[productId as any];
   if (!handlerGetter) throw new HttpCode(202, `No product handler found for product: ${productId}`);
   const handler = (await handlerGetter())?.default;
   // auto catch unsupported Stripe products.
