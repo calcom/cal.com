c3f89daf57d31d40e9d8a21ee97278fd66a47e71
Morgan Vernay <morgan@cal.com>
2025-03-27 20:36:50 +0200

    chore: refactor react imports

--- packages/platform/atoms/src/components/ui/button.tsx
+++ packages/platform/atoms/src/components/ui/button.tsx
@@ -1,6 +1,7 @@
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
-import * as React from "react";
+import { forwardRef } from "react";
+import type { ButtonHTMLAttributes } from "react";
 
 import { cn } from "../../lib/utils";
 
@@ -36,12 +37,12 @@ const buttonVariants: buttonVariantsType = cva(
 );
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
     return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
