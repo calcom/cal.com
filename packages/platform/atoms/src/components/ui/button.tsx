45a664e12011189d64984196c9573810ea3e2449
Morgan Vernay <morgan@cal.com>
2025-03-27 20:32:19 +0200

    type button cva

--- packages/platform/atoms/src/components/ui/button.tsx
+++ packages/platform/atoms/src/components/ui/button.tsx
@@ -4,7 +4,12 @@ import * as React from "react";
 
 import { cn } from "../../lib/utils";
 
-const buttonVariants = cva(
+type buttonVariantsType = (props: {
+  variant: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link" | null | undefined;
+  size: "default" | "sm" | "lg" | "icon" | null | undefined;
+  className: string | undefined;
+}) => string;
+const buttonVariants: buttonVariantsType = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
