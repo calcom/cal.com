52a8c680b049fd2d16550264ff3bb8f9b6d8c681
CarinaWolli <wollencarina@gmail.com>
2025-03-31 17:04:01 +0300

    make uuid optional

--- packages/prisma/migrations/20250331140235_add_reference_uid_to_task/migration.sql
+++ packages/prisma/migrations/20250331140235_add_reference_uid_to_task/migration.sql
@@ -2,14 +2,13 @@
   Warnings:
 
   - A unique constraint covering the columns `[uuid]` on the table `WorkflowReminder` will be added. If there are existing duplicate values, this will fail.
-  - The required column `uuid` was added to the `WorkflowReminder` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
 
 */
 -- AlterTable
 ALTER TABLE "Task" ADD COLUMN     "referenceUid" TEXT;
 
 -- AlterTable
-ALTER TABLE "WorkflowReminder" ADD COLUMN     "uuid" TEXT NOT NULL;
+ALTER TABLE "WorkflowReminder" ADD COLUMN     "uuid" TEXT;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "WorkflowReminder_uuid_key" ON "WorkflowReminder"("uuid");
