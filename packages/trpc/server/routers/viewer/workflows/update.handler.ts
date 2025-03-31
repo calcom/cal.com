80939ea1a7bf4096a48f5624531df941af149c45
Joe <j.auyeung419@gmail.com>
2025-03-31 11:07:36 -0400

    Update logic

--- packages/trpc/server/routers/viewer/workflows/update.handler.ts
+++ packages/trpc/server/routers/viewer/workflows/update.handler.ts
@@ -394,7 +394,11 @@ export const updateHandler = async ({ ctx, input }: UpdateOptions) => {
           sender: newStep.sender,
           numberVerificationPending: false,
           includeCalendarEvent: newStep.includeCalendarEvent,
-          verifiedAt: SCANNING_WORKFLOW_STEPS && didBodyChange ? null : oldStep.verifiedAt ?? new Date(),
+          verifiedAt: !SCANNING_WORKFLOW_STEPS
+            ? new Date()
+            : didBodyChange
+            ? null
+            : oldStep.verifiedAt ?? new Date(),
         },
       });
 
