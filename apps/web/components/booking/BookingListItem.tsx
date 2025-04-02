795588b9c5d3b7c769bceff363b51ed2546a27ed
Tushar <tusharbhatt0135@gmail.com>
2025-04-02 21:59:57 +0530

    fix:tests

--- apps/web/components/booking/BookingListItem.tsx
+++ apps/web/components/booking/BookingListItem.tsx
@@ -238,27 +238,7 @@ function BookingListItem(booking: BookingItemProps) {
   ];
 
   const editBookingActions: ActionType[] = [
-    ...(isBookingInPast && booking.eventType.allowReschedulingPastBookings
-      ? [
-          {
-            id: "reschedule",
-            icon: "clock" as const,
-            label: t("reschedule_booking"),
-            href: `/reschedule/${booking.uid}${
-              booking.seatsReferences.length ? `?seatReferenceUid=${getSeatReferenceUid()}` : ""
-            }`,
-          },
-          {
-            id: "reschedule_request",
-            icon: "send" as const,
-            iconClassName: "rotate-45 w-[16px] -translate-x-0.5 ",
-            label: t("send_reschedule_request"),
-            onClick: () => {
-              setIsOpenRescheduleDialog(true);
-            },
-          },
-        ]
-      : booking.eventType?.disableRescheduling
+    ...(isBookingInPast && !booking.eventType.allowReschedulingPastBookings
       ? []
       : [
           {
@@ -345,26 +325,25 @@ function BookingListItem(booking: BookingItemProps) {
       icon: attendeeList.length === 1 && attendeeList[0].noShow ? "eye" : ("eye-off" as const),
     });
   }
+
   let bookedActions: ActionType[] = [
+    {
+      id: "cancel",
+      label: isTabRecurring && isRecurring ? t("cancel_all_remaining") : t("cancel_event"),
+      /* When cancelling we need to let the UI and the API know if the intention is to
+               cancel all remaining bookings or just that booking instance. */
+      href: `/booking/${booking.uid}?cancel=true${
+        isTabRecurring && isRecurring ? "&allRemainingBookings=true" : ""
+      }${booking.seatsReferences.length ? `&seatReferenceUid=${getSeatReferenceUid()}` : ""}
+      `,
+      icon: "x" as const,
+    },
     {
       id: "edit_booking",
       label: t("edit"),
       actions: editBookingActions,
     },
   ];
-
-  if (!booking.eventType.disableCancelling) {
-    bookedActions.unshift({
-      id: "cancel",
-      label: isTabRecurring && isRecurring ? t("cancel_all_remaining") : t("cancel_event"),
-      /* When cancelling we need to let the UI and the API know if the intention is to cancel all remaining bookings or just that booking instance. */
-      href: `/booking/${booking.uid}?cancel=true${
-        isTabRecurring && isRecurring ? "&allRemainingBookings=true" : ""
-      }${booking.seatsReferences.length ? `&seatReferenceUid=${getSeatReferenceUid()}` : ""}
-    `,
-      icon: "x" as const,
-    });
-  }
 
   const chargeCardActions: ActionType[] = [
     {
@@ -378,11 +357,14 @@ function BookingListItem(booking: BookingItemProps) {
     },
   ];
 
-  if (isTabRecurring && isRecurring) {
+  const isDisabledCancelling = booking.eventType.disableCancelling;
+  const isDisabledRescheduling = booking.eventType.disableRescheduling;
+
+  if (isDisabledRescheduling || (isTabRecurring && isRecurring)) {
     bookedActions = bookedActions.filter((action) => action.id !== "edit_booking");
   }
 
-  if (isBookingInPast && isPending && !isConfirmed) {
+  if (isDisabledCancelling || (isBookingInPast && isPending && !isConfirmed)) {
     bookedActions = bookedActions.filter((action) => action.id !== "cancel");
   }
 
