a31454c3f500642cbc28a438c2624f2bae806c9b
supalarry <laurisskraucis@gmail.com>
2025-04-04 11:17:06 +0200

    fix: pass orgId to getBookings

--- apps/api/v2/src/modules/organizations/users/bookings/controllers/organizations-users-bookings-controller.ts
+++ apps/api/v2/src/modules/organizations/users/bookings/controllers/organizations-users-bookings-controller.ts
@@ -9,7 +9,7 @@ import { IsOrgGuard } from "@/modules/auth/guards/organizations/is-org.guard";
 import { RolesGuard } from "@/modules/auth/guards/roles/roles.guard";
 import { IsUserInOrg } from "@/modules/auth/guards/users/is-user-in-org.guard";
 import { OrganizationUsersBookingsService } from "@/modules/organizations/users/bookings/services/organization-users-bookings.service";
-import { Controller, UseGuards, Get, Query } from "@nestjs/common";
+import { Controller, UseGuards, Get, Query, ParseIntPipe, Param } from "@nestjs/common";
 import { ApiOperation, ApiTags as DocsTags } from "@nestjs/swagger";
 
 import { SUCCESS_STATUS } from "@calcom/platform-constants";
@@ -31,9 +31,14 @@ export class OrganizationsUsersBookingsController {
   @ApiOperation({ summary: "Get all bookings of an organization user" })
   async getOrganizationUserBookings(
     @GetUser() user: User,
+    @Param("orgId", ParseIntPipe) orgId: number,
     @Query() query: GetBookingsInput_2024_08_13
   ): Promise<GetBookingsOutput_2024_08_13> {
-    const bookings = await this.organizationUsersBookingsService.getOrganizationUserBookings(user, query);
+    const bookings = await this.organizationUsersBookingsService.getOrganizationUserBookings(
+      orgId,
+      user,
+      query
+    );
 
     return {
       status: SUCCESS_STATUS,
--- apps/api/v2/src/modules/organizations/users/bookings/services/organization-users-bookings.service.ts
+++ apps/api/v2/src/modules/organizations/users/bookings/services/organization-users-bookings.service.ts
@@ -8,7 +8,11 @@ import { User } from "@calcom/prisma/client";
 export class OrganizationUsersBookingsService {
   constructor(private readonly bookingsService: BookingsService_2024_08_13) {}
 
-  async getOrganizationUserBookings(user: User, queryParams: GetBookingsInput_2024_08_13) {
-    return await this.bookingsService.getBookings(queryParams, user);
+  async getOrganizationUserBookings(
+    orgId: number,
+    user: Pick<User, "id" | "email">,
+    queryParams: GetBookingsInput_2024_08_13
+  ) {
+    return await this.bookingsService.getBookings(queryParams, { orgId, id: user.id, email: user.email });
   }
 }
