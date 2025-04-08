098bdea982fc9ff2856158c01b8fef8067cd8461
Ryukemeister <sahalrajiv6900@gmail.com>
2025-04-09 01:30:43 +0530

    update billing module

--- apps/api/v2/src/modules/billing/billing.module.ts
+++ apps/api/v2/src/modules/billing/billing.module.ts
@@ -1,3 +1,4 @@
+import { BookingsRepository_2024_08_13 } from "@/ee/bookings/2024-08-13/bookings.repository";
 import { BillingProcessor } from "@/modules/billing/billing.processor";
 import { BillingRepository } from "@/modules/billing/billing.repository";
 import { BillingController } from "@/modules/billing/controllers/billing.controller";
@@ -5,10 +6,12 @@ import { BillingConfigService } from "@/modules/billing/services/billing.config.
 import { BillingService } from "@/modules/billing/services/billing.service";
 import { ManagedOrganizationsBillingService } from "@/modules/billing/services/managed-organizations.billing.service";
 import { MembershipsModule } from "@/modules/memberships/memberships.module";
+import { OAuthClientRepository } from "@/modules/oauth-clients/oauth-client.repository";
 import { OrganizationsModule } from "@/modules/organizations/organizations.module";
 import { PrismaModule } from "@/modules/prisma/prisma.module";
 import { StripeModule } from "@/modules/stripe/stripe.module";
 import { UsersModule } from "@/modules/users/users.module";
+import { UsersRepository } from "@/modules/users/users.repository";
 import { BullModule } from "@nestjs/bull";
 import { Module } from "@nestjs/common";
 
@@ -33,6 +36,9 @@ import { Module } from "@nestjs/common";
     BillingRepository,
     BillingProcessor,
     ManagedOrganizationsBillingService,
+    UsersRepository,
+    OAuthClientRepository,
+    BookingsRepository_2024_08_13,
   ],
   exports: [BillingService, BillingRepository, ManagedOrganizationsBillingService],
   controllers: [BillingController],
