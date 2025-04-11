3d86b62224b9f31aa78dd799516eeb868efcfa00
Joe <j.auyeung419@gmail.com>
2025-04-11 15:10:48 -0400

    Remove dotenv

--- packages/app-store/salesforce/codegen.ts
+++ packages/app-store/salesforce/codegen.ts
@@ -1,7 +1,4 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
-import { config as dotenvconfig } from "dotenv";
-
-dotenvconfig({ path: "./.env" });
 
 const config: CodegenConfig = {
   schema: "./schema.graphql",
