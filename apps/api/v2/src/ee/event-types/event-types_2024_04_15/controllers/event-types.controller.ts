3e0012e9edc5d4a716aa6d5822e66e8b75dc7edd
hbjORbj <sldisek783@gmail.com>
2025-04-12 19:47:02 -0400

    fix

--- apps/api/v2/src/ee/event-types/event-types_2024_04_15/controllers/event-types.controller.ts
+++ apps/api/v2/src/ee/event-types/event-types_2024_04_15/controllers/event-types.controller.ts
@@ -40,7 +40,6 @@ import { ApiExcludeController as DocsExcludeController } from "@nestjs/swagger";
 
 import { EVENT_TYPE_READ, EVENT_TYPE_WRITE, SUCCESS_STATUS } from "@calcom/platform-constants";
 import { getPublicEvent, getEventTypesByViewer } from "@calcom/platform-libraries-0.0.2";
-import { PrismaClient } from "@calcom/prisma";
 
 @Controller({
   path: "/v2/event-types",
