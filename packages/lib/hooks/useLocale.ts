ed036e97b0ea2c1179fec5b484c42f3237884a64
Vijay <vijayraghav22@gmail.com>
2025-04-15 21:59:09 +0530

    nit

--- packages/lib/hooks/useLocale.ts
+++ packages/lib/hooks/useLocale.ts
@@ -17,17 +17,8 @@ const useClientLocale = (namespace: Parameters<typeof useTranslation>[0] = "comm
   const context = useAtomsContext();
   const { i18n, t } = useTranslation(namespace);
   const isLocaleReady = Object.keys(i18n).length > 0;
-
   if (context?.clientId) {
-    return {
-      i18n: context.i18n,
-      t: context.t,
-      isLocaleReady: true,
-    } as unknown as {
-      i18n: ReturnType<typeof useTranslation>["i18n"];
-      t: ReturnType<typeof useTranslation>["t"];
-      isLocaleReady: boolean;
-    };
+    return { i18n: context.i18n, t: context.t, isLocaleReady: true } as unknown as useLocaleReturnType;
   }
   return {
     i18n,
