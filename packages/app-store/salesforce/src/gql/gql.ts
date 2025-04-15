02121f6832c71737542d103c3a0b7f5565d518fd
Joe <j.auyeung419@gmail.com>
2025-04-15 00:17:41 -0400

    Create own types folder for production

--- packages/app-store/salesforce/src/gql/gql.ts
+++ packages/app-store/salesforce/src/gql/gql.ts
@@ -1,7 +1,8 @@
 /* eslint-disable */
+// import * as types from './graphql';
 import { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/core";
 
-import * as types from "./graphql";
+import * as types from "./types";
 
 /**
  * Map of all GraphQL operations in the project.
