798549240244ce4d5c32b30f21fb77b7e8237819
Joe <j.auyeung419@gmail.com>
2025-04-14 23:13:53 -0400

    Adjust artifactDirectory path

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -194,7 +194,7 @@ const nextConfig = {
     swcPlugins: [
       [
         "@graphql-codegen/client-preset-swc-plugin",
-        { artifactDirectory: "../../src/gql", gqlTagName: "graphql" },
+        { artifactDirectory: "./src/gql/", gqlTagName: "graphql" },
       ],
     ],
   },
