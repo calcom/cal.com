f4c36beca91a42b7505017acbab83b39991dda9c
Morgan Vernay <morgan@cal.com>
2025-04-16 20:10:27 +0300

    adding missing input file

--- apps/api/v2/src/modules/organizations/users/index/inputs/get-organization-users.input.ts
+++ apps/api/v2/src/modules/organizations/users/index/inputs/get-organization-users.input.ts
@@ -1,3 +1,36 @@
 import { GetUsersInput } from "@/modules/users/inputs/get-users.input";
+import { ApiPropertyOptional } from "@nestjs/swagger";
+import { Transform } from "class-transformer";
+import { Expose } from "class-transformer";
+import { IsOptional, IsArray, ArrayMinSize, IsString, IsIn } from "class-validator";
 
-export class GetOrganizationsUsersInput extends GetUsersInput {}
+export class GetOrganizationsUsersInput extends GetUsersInput {
+  @Expose()
+  @IsOptional()
+  @Transform(({ value }) => {
+    if (typeof value === "string") {
+      return value.split(",").map((optId: string) => optId);
+    }
+    return value;
+  })
+  @Expose()
+  @ApiPropertyOptional({
+    type: [String],
+    description: "Filter by assigned attribute option ids. ids must be separated by a comma.",
+    example: "?assignedOptionIds=aaaaaaaa-bbbb-cccc-dddd-eeeeee1eee,aaaaaaaa-bbbb-cccc-dddd-eeeeee2eee",
+  })
+  @IsArray()
+  @IsString({ each: true })
+  @ArrayMinSize(1, { message: "assignedOptionIds must contain at least 1 attribute option id" })
+  assignedOptionIds?: string[];
+
+  @ApiPropertyOptional({
+    type: String,
+    description: "Query operator used to filter assigned options, AND by default.",
+    example: "NONE",
+  })
+  @IsOptional()
+  @IsString()
+  @IsIn(["OR", "AND", "NONE"])
+  attributeQueryOperator: "AND" | "OR" | "NONE" = "AND"; // Default value
+}
