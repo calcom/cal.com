9bc703a437cfb47d37c595410bd205be6142ceff
Hariom Balhara <hariombalhara@gmail.com>
2025-04-16 19:10:51 +0530

    feat: Embed - Show skeleton loader when pageType is provided (#20646)
    * mvp done
    
    * wip
    
    * fix ts errors and other code improvements
    
    * fix ts errors
    
    * ensure mobile layout support
    
    * Make skeleton responsive on screen resize
    
    * refactor
    
    * Add test for EmbedElement
    
    * make skeleton closer to pixel perfect
    
    * Address PR feedback
    
    * Remove PR_TODO
    
    ---------
    
    Co-authored-by: Benny Joo <sldisek783@gmail.com>
    Co-authored-by: amrit <iamamrit27@gmail.com>

--- packages/features/bookings/Booker/Booker.tsx
+++ packages/features/bookings/Booker/Booker.tsx
@@ -38,9 +38,17 @@ import { useIsQuickAvailabilityCheckFeatureEnabled } from "./components/hooks/us
 import { fadeInLeft, getBookerSizeClassNames, useBookerResizeAnimation } from "./config";
 import framerFeatures from "./framer-features";
 import { useBookerStore } from "./store";
-import type { BookerProps, WrappedBookerProps } from "./types";
+import type { BookerProps, WrappedBookerProps, BookerState } from "./types";
 import { isBookingDryRun } from "./utils/isBookingDryRun";
 import { isTimeSlotAvailable } from "./utils/isTimeslotAvailable";
+
+function updateEmbedBookerState({ bookerState }: { bookerState: BookerState }) {
+  // Ensure that only after the bookerState is reflected, we update the embedIsBookerReady
+  if (typeof window !== "undefined") {
+    (window as Window & { _embedBookerState?: "initializing" | "done" })._embedBookerState =
+      bookerState && bookerState !== "loading" ? "done" : "initializing";
+  }
+}
 
 const BookerComponent = ({
   username,
@@ -177,6 +185,8 @@ const BookerComponent = ({
     CLOUDFLARE_USE_TURNSTILE_IN_BOOKER === "1" &&
     (bookerState === "booking" || (bookerState === "selecting_time" && skipConfirmStep))
   );
+
+  updateEmbedBookerState({ bookerState });
 
   useEffect(() => {
     if (event.isPending) return setBookerState("loading");
