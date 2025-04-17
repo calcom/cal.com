fe509f82918f4448534097e3a12ad6b7b22ebd35
Omar López <zomars@me.com>
2025-04-16 18:53:47 -0700

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -719,7 +719,7 @@ if (!!process.env.NEXT_PUBLIC_SENTRY_DSN) {
       // For all available options, see:
       // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
       // Upload a larger set of source maps for prettier stack traces (increases build time)
-      widenClientFileUpload: true,
+      widenClientFileUpload: false,
       // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
       // This can increase your server load as well as your hosting bill.
       // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
