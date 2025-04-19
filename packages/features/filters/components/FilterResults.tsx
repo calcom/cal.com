110f43e68c8ab7adb9c79edb71fa0b59a79e3e53
Tushar <tusharbhatt0135@gmail.com>
2025-04-19 19:50:43 +0530

    fix type-check

--- packages/features/filters/components/FilterResults.tsx
+++ packages/features/filters/components/FilterResults.tsx
@@ -1,14 +1,17 @@
 export function FilterResults({
   queryRes,
+  SkeletonLoader,
   noResultsScreen,
   emptyScreen,
   children,
 }: {
-  queryRes: { data: { totalCount: number; filtered: unknown[] } | undefined };
+  queryRes: { isPending?: boolean; data: { totalCount: number; filtered: unknown[] } | undefined };
+  SkeletonLoader?: React.FC;
   noResultsScreen: React.ReactNode;
   emptyScreen: React.ReactNode;
   children: React.ReactNode;
 }) {
+  if (queryRes?.isPending && SkeletonLoader) return <SkeletonLoader />;
   if (!queryRes.data?.totalCount) return <>{emptyScreen}</>;
 
   return queryRes.data?.totalCount ? (
