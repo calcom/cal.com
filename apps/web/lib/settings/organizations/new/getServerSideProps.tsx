eb6102835a5285deb7a4243ef3a25829fdd98836
Ryukemeister <sahalrajiv6900@gmail.com>
2025-04-19 20:01:04 +0530

    redirect to login if no user session

--- apps/web/lib/settings/organizations/new/getServerSideProps.tsx
+++ apps/web/lib/settings/organizations/new/getServerSideProps.tsx
@@ -1,9 +1,24 @@
 import type { GetServerSidePropsResult } from "next";
+import type { GetServerSidePropsContext } from "next";
+import { getSession } from "next-auth/react";
 
 import { getFeatureFlag } from "@calcom/features/flags/server/utils";
 import prisma from "@calcom/prisma";
 
-export const getServerSideProps = async (): Promise<GetServerSidePropsResult<{ isOrg: boolean }>> => {
+export const getServerSideProps = async (
+  ctx: GetServerSidePropsContext
+): Promise<GetServerSidePropsResult<{ isOrg: boolean }>> => {
+  const session = await getSession(ctx);
+
+  if (!session || !session.user) {
+    return {
+      redirect: {
+        destination: `/login?callbackUrl=${ctx.req.headers["x-url"]}`,
+        permanent: false,
+      },
+    };
+  }
+
   const organizations = await getFeatureFlag(prisma, "organizations");
   // Check if organizations are enabled
   if (!organizations) {
