00e63907a3f0b204c0d53452697bfe1f90cea4a1
Evan Liu <evanliu.dev@gmail.com>
2025-04-20 14:33:12 +1200

    fix: header overlaps in month view on short viewport (#20749)
    * fix: header overlaps in month view on short viewport
    
    * refactor: Merge classes into same condition
    
    * fix: Remove booker margin top if isEmbed
    
    ---------
    
    Co-authored-by: Anik Dhabal Babu <81948346+anikdhabal@users.noreply.github.com>
    Co-authored-by: Tushar Bhatt <95581504+TusharBhatt1@users.noreply.github.com>
    Co-authored-by: amrit <iamamrit27@gmail.com>

--- packages/features/bookings/Booker/Booker.tsx
+++ packages/features/bookings/Booker/Booker.tsx
@@ -324,6 +324,7 @@ const BookerComponent = ({
           // In a popup embed, if someone clicks outside the main(having main class or main tag), it closes the embed
           "main",
           "text-default flex min-h-full w-full flex-col items-center",
+          layout === BookerLayouts.MONTH_VIEW && !isEmbed && "my-20 ",
           layout === BookerLayouts.MONTH_VIEW ? "overflow-visible" : "overflow-clip",
           `${customClassNames?.bookerWrapper}`
         )}>
