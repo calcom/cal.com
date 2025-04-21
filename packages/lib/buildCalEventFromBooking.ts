a87fd72d0dbff071481dc74bea1a6496a1bd822a
unknown <adhabal2002@gmail.com>
2025-04-21 14:46:31 +0530

    type error

--- packages/lib/buildCalEventFromBooking.ts
+++ packages/lib/buildCalEventFromBooking.ts
@@ -36,6 +36,7 @@ type EventType = {
   recurringEvent: Prisma.JsonValue | null;
   seatsPerTimeSlot: number | null;
   seatsShowAttendees: boolean | null;
+  customReplyToEmail: string | null;
 };
 
 type Booking = {
