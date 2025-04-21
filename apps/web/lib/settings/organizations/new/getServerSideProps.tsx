df859bd3548e305fed79b147ffa6d784b8985ea4
Ryukemeister <sahalrajiv6900@gmail.com>
2025-04-21 19:59:16 +0530

    remove callback url since the same function is used by org and platform both

--- apps/web/lib/settings/organizations/new/getServerSideProps.tsx
+++ apps/web/lib/settings/organizations/new/getServerSideProps.tsx
@@ -3,19 +3,17 @@ import type { GetServerSidePropsContext } from "next";
 import { getSession } from "next-auth/react";
 
 import { getFeatureFlag } from "@calcom/features/flags/server/utils";
-import { WEBAPP_URL } from "@calcom/lib/constants";
 import prisma from "@calcom/prisma";
 
 export const getServerSideProps = async (
   ctx: GetServerSidePropsContext
 ): Promise<GetServerSidePropsResult<{ isOrg: boolean }>> => {
   const session = await getSession(ctx);
-  const callbackUrl = `${WEBAPP_URL}/settings/platform/new`;
 
   if (!session || !session.user) {
     return {
       redirect: {
-        destination: `/login?callbackUrl=${callbackUrl}`,
+        destination: `/login`,
         permanent: false,
       },
     };
