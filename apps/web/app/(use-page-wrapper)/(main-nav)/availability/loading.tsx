b0de40c7494fe681f3bb53e5166117ed1af7a66a
hbjORbj <sldisek783@gmail.com>
2025-04-22 10:00:15 -0300

    rename

--- apps/web/app/(use-page-wrapper)/(main-nav)/availability/loading.tsx
+++ apps/web/app/(use-page-wrapper)/(main-nav)/availability/loading.tsx
@@ -1,4 +1,4 @@
-import AvailabilityLoader from "app/(use-page-wrapper)/(main-nav)/availability/clientLoader";
+import AvailabilityLoader from "app/(use-page-wrapper)/(main-nav)/availability/skeleton";
 
 export default function Loading() {
   return <AvailabilityLoader />;
