7f8e5a3fa8fd67fd040f06f37378c86c5a16fcae
hbjORbj <sldisek783@gmail.com>
2025-04-22 17:37:32 -0300

    update

--- apps/web/app/(use-page-wrapper)/(main-nav)/availability/page.tsx
+++ apps/web/app/(use-page-wrapper)/(main-nav)/availability/page.tsx
@@ -3,11 +3,9 @@ import type { PageProps } from "app/_types";
 import { _generateMetadata, getTranslate } from "app/_utils";
 import { notFound } from "next/navigation";
 
-import { checkAdminOrOwner } from "@calcom/features/auth/lib/checkAdminOrOwner";
 import { AvailabilitySliderTable } from "@calcom/features/timezone-buddy/components/AvailabilitySliderTable";
 import { availabilityRouter } from "@calcom/trpc/server/routers/viewer/availability/_router";
 import { meRouter } from "@calcom/trpc/server/routers/viewer/me/_router";
-import { viewerOrganizationsRouter } from "@calcom/trpc/server/routers/viewer/organizations/_router";
 
 import { AvailabilityList, AvailabilityCTA } from "~/availability/availability-view";
 
@@ -27,25 +25,14 @@ const Page = async ({ searchParams: _searchParams }: PageProps) => {
   const searchParams = await _searchParams;
   const t = await getTranslate();
   try {
-    const [meCaller, orgCaller, availabilityCaller] = await Promise.all([
+    const [meCaller, availabilityCaller] = await Promise.all([
       createRouterCaller(meRouter),
-      createRouterCaller(viewerOrganizationsRouter),
       createRouterCaller(availabilityRouter),
     ]);
 
     const me = await meCaller.get();
     const organizationId = me.organization?.id ?? me.profiles[0]?.organizationId;
-    let currentOrgs = null;
-    let canViewTeamAvailability = me.isTeamAdminOrOwner;
-
-    if (organizationId) {
-      currentOrgs = await orgCaller.listCurrent();
-      const isOrgAdminOrOwner = currentOrgs && checkAdminOrOwner(currentOrgs.user.role);
-      const isOrgAndPrivate = currentOrgs?.isOrganization && currentOrgs.isPrivate;
-      canViewTeamAvailability = isOrgAdminOrOwner || !isOrgAndPrivate;
-    }
-
-    const isTeamView = searchParams?.type === "team" && canViewTeamAvailability;
+    const isTeamView = searchParams?.type === "team";
     const availabilities = !isTeamView ? await availabilityCaller.list() : null;
 
     return (
@@ -56,7 +43,7 @@ const Page = async ({ searchParams: _searchParams }: PageProps) => {
           <AvailabilityCTA
             toggleGroupOptions={[
               { value: "mine", label: t("my_availability") },
-              ...(canViewTeamAvailability ? [{ value: "team", label: t("team_availability") }] : []),
+              { value: "team", label: t("team_availability") },
             ]}
           />
         }>
