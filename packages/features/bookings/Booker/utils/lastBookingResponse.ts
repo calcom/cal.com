c01a43cd6d00f2acc2f1559984e67d4a648fe52d
Hariom Balhara <hariombalhara@gmail.com>
2025-04-25 21:26:28 +0530

    remove dead code to fix ts error

--- packages/features/bookings/Booker/utils/lastBookingResponse.ts
+++ packages/features/bookings/Booker/utils/lastBookingResponse.ts
@@ -1,15 +1,16 @@
 import logger from "@calcom/lib/logger";
 import { localStorage } from "@calcom/lib/webstorage";
 
-const responsesToStore = ["email", "name"];
+// const responsesToStore = ["email", "name"];
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const setLastBookingResponse = (responses: Record<string, unknown> | null) => {
   return;
-  if (!responses) return;
-  const prevResponse = Object.fromEntries(
-    Object.entries(responses).filter(([key]) => responsesToStore.includes(key))
-  );
-  localStorage.setItem("lastBookingResponse", JSON.stringify(prevResponse));
+  // if (!responses) return;
+  // const prevResponse = Object.fromEntries(
+  //   Object.entries(responses).filter(([key]) => responsesToStore.includes(key))
+  // );
+  // localStorage.setItem("lastBookingResponse", JSON.stringify(prevResponse));
 };
 
 export const getLastBookingResponse = () => {
