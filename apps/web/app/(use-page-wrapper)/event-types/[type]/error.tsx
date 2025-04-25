fd589c58aa710c637887273b5cca9719ba44b5b3
hbjORbj <sldisek783@gmail.com>
2025-04-25 16:12:37 -0300

    address

--- apps/web/app/(use-page-wrapper)/event-types/[type]/error.tsx
+++ apps/web/app/(use-page-wrapper)/event-types/[type]/error.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import Shell from "@calcom/features/shell/Shell";
+import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { Alert } from "@calcom/ui/components/alert";
 
 export default function Error() {
+  const { t } = useLocale();
   return (
     <Shell>
-      <Alert severity="error" title="Something went wrong" />
+      <Alert severity="error" title={t("something_went_wrong")} />
     </Shell>
   );
 }
