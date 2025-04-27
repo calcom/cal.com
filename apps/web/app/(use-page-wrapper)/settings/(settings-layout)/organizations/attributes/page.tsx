24e85dc876b02b5f65b7f73ed5ed6aa92bfe9115
amrit <iamamrit27@gmail.com>
2025-04-27 10:35:39 +0530

    perf: server-side render org attributes page with initial data, add skeleton loading state

--- apps/web/app/(use-page-wrapper)/settings/(settings-layout)/organizations/attributes/page.tsx
+++ apps/web/app/(use-page-wrapper)/settings/(settings-layout)/organizations/attributes/page.tsx
@@ -1,7 +1,10 @@
+import { createRouterCaller } from "app/_trpc/context";
 import { _generateMetadata, getTranslate } from "app/_utils";
+import { redirect } from "next/navigation";
 
 import OrgSettingsAttributesPage from "@calcom/ee/organizations/pages/settings/attributes/attributes-list-view";
 import SettingsHeader from "@calcom/features/settings/appDir/SettingsHeader";
+import { appRouter } from "@calcom/trpc/server/routers/_app";
 
 export const generateMetadata = async () =>
   await _generateMetadata(
@@ -14,10 +17,24 @@ export const generateMetadata = async () =>
 
 const Page = async () => {
   const t = await getTranslate();
+  const routerCaller = await createRouterCaller(appRouter);
+  const user = await routerCaller.viewer.me.get();
+
+  if (!user) {
+    redirect("/auth/login?callbackUrl=/settings/organizations/attributes");
+  }
+
+  const isAdminOrOwner = user.organization?.isOrgAdmin ?? false;
+
+  if (!user.organizationId || !isAdminOrOwner) {
+    redirect("/");
+  }
+
+  const attributes = await routerCaller.viewer.attributes.list();
 
   return (
     <SettingsHeader title={t("attributes")} description={t("attribute_meta_description")}>
-      <OrgSettingsAttributesPage />
+      <OrgSettingsAttributesPage initialAttributes={attributes} />
     </SettingsHeader>
   );
 };
