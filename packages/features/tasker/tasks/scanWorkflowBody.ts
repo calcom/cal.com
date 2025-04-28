50edcc437cd056a5a4a963eb340f4a6a81dd8fe9
Joe Au-Yeung <j.auyeung419@gmail.com>
2025-04-28 14:16:19 -0400

    WIP using Iffy to scan comments

--- packages/features/tasker/tasks/scanWorkflowBody.ts
+++ packages/features/tasker/tasks/scanWorkflowBody.ts
@@ -1,11 +1,8 @@
-import { AkismetClient } from "akismet-api";
-import type { Comment } from "akismet-api";
 import z from "zod";
 
 import { getTemplateBodyForAction } from "@calcom/features/ee/workflows/lib/actionHelperFunctions";
 import compareReminderBodyToTemplate from "@calcom/features/ee/workflows/lib/compareReminderBodyToTemplate";
 import { lockUser, LockReason } from "@calcom/lib/autoLock";
-import { WEBAPP_URL } from "@calcom/lib/constants";
 import logger from "@calcom/lib/logger";
 import { getTranslation } from "@calcom/lib/server/i18n";
 import { getTimeFormatStringFromUserTimeFormat } from "@calcom/lib/timeFormat";
@@ -20,8 +17,8 @@ export const scanWorkflowBodySchema = z.object({
 const log = logger.getSubLogger({ prefix: ["[tasker] scanWorkflowBody"] });
 
 export async function scanWorkflowBody(payload: string) {
-  if (!process.env.AKISMET_API_KEY) {
-    log.info("AKISMET_API_KEY not set, skipping scan");
+  if (!process.env.IFFY_API_KEY) {
+    log.info("IFFY_API_KEY not set, skipping scan");
     return;
   }
 
@@ -48,7 +45,7 @@ export async function scanWorkflowBody(payload: string) {
     },
   });
 
-  const client = new AkismetClient({ key: process.env.AKISMET_API_KEY, blog: WEBAPP_URL });
+  // const client = new AkismetClient({ key: process.env.AKISMET_API_KEY, blog: WEBAPP_URL });
 
   for (const workflowStep of workflowSteps) {
     if (!workflowStep.reminderBody) {
@@ -93,12 +90,17 @@ export async function scanWorkflowBody(payload: string) {
       continue;
     }
 
-    const comment: Comment = {
-      user_ip: "127.0.0.1",
-      content: workflowStep.reminderBody,
-    };
+    // const comment: Comment = {
+    //   user_ip: "127.0.0.1",
+    //   content: workflowStep.reminderBody,
+    // };
 
-    const isSpam = await client.checkSpam(comment);
+    // const isSpam = await client.checkSpam(comment);
+
+    const response = await iffyScanBody(workflowStep.reminderBody, workflowStep.id);
+    console.log("🚀 ~ file: scanWorkflowBody.ts:101 ~ scanWorkflowBody ~ response:", response);
+
+    const isSpam = false;
 
     if (isSpam) {
       if (workflowStep.workflow.user?.whitelistWorkflows) {
@@ -160,3 +162,26 @@ export async function scanWorkflowBody(payload: string) {
     teamId: workflow.team?.id || null,
   });
 }
+
+const iffyScanBody = async (body: string, workflowStepId: number) => {
+  try {
+    const response = await fetch("https://api.iffy.com/api/v1/moderate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.IFFY_API_KEY}`,
+      },
+      body: JSON.stringify({
+        clientId: process.env.IFFY_CLIENT_NAME,
+        name: "Workflow",
+        entity: "Body",
+        content: body,
+        passthrough: true,
+      }),
+    });
+    console.log("🚀 ~ file: scanWorkflowBody.ts:177 ~ iffyScanBody ~ response:", response);
+    // return await response.json();
+  } catch (error) {
+    log.error(`Error scanning workflow body for workflow step ${workflowStepId}:`, error);
+  }
+};
