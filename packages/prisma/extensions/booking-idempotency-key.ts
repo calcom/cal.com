0ea588f51f917e2867cceb26edda44a9375216dc
Joe <j.auyeung419@gmail.com>
2025-04-30 13:17:59 -0400

    Use userId instead of attendee email

--- packages/prisma/extensions/booking-idempotency-key.ts
+++ packages/prisma/extensions/booking-idempotency-key.ts
@@ -8,19 +8,10 @@ export function bookingIdempotencyKeyExtension() {
     query: {
       booking: {
         async create({ args, query }) {
-          const uniqueEmailJoinInput: string[] = [];
-          if (args.data.attendees?.create && !Array.isArray(args.data.attendees?.create)) {
-            uniqueEmailJoinInput.push(args.data.attendees?.create.email);
-          }
-          if (args.data.attendees?.createMany && Array.isArray(args.data.attendees?.createMany.data)) {
-            uniqueEmailJoinInput.push(...args.data.attendees?.createMany.data.map((record) => record.email));
-          }
           const idempotencyKey = uuidv5(
             `${
               args.data.eventType?.connect?.id
-            }.${args.data.startTime.valueOf()}.${args.data.endTime.valueOf()}.${uniqueEmailJoinInput.join(
-              ","
-            )}`,
+            }.${args.data.startTime.valueOf()}.${args.data.endTime.valueOf()}.${args.data?.userId}`,
             uuidv5.URL
           );
           args.data.idempotencyKey = idempotencyKey;
