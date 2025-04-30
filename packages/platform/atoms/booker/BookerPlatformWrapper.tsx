e9f86f3b45f7c76f3ef5bdb7f850d2551aaa4d33
Alex van Andel <me@alexvanandel.com>
2025-04-30 23:31:08 +0100

    fix: cal.cache not working

--- packages/platform/atoms/booker/BookerPlatformWrapper.tsx
+++ packages/platform/atoms/booker/BookerPlatformWrapper.tsx
@@ -292,14 +292,14 @@ export const BookerPlatformWrapper = (
     const skipContactOwner = searchParams.get("cal.skipContactOwner") === "true";
 
     const _cacheParam = searchParams?.get("cal.cache");
-    const shouldServeCache = _cacheParam ? _cacheParam === "true" : undefined;
+    const _shouldServeCache = _cacheParam ? _cacheParam === "true" : undefined;
     const isBookingDryRun =
       searchParams?.get("cal.isBookingDryRun")?.toLowerCase() === "true" ||
       searchParams?.get("cal.sandbox")?.toLowerCase() === "true";
     setRoutingParams({
       ...(skipContactOwner ? { skipContactOwner } : {}),
       ...(routedTeamMemberIds ? { routedTeamMemberIds } : {}),
-      ...(shouldServeCache ? { shouldServeCache } : {}),
+      ...(_shouldServeCache ? { _shouldServeCache } : {}),
       ...(isBookingDryRun ? { isBookingDryRun } : {}),
     });
   }, [routingFormSearchParams]);
