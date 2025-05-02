358957740097b44236c1cc94d106d1a6759eb871
Somay Chauhan <somaychauhan98@gmail.com>
2025-05-02 12:34:38 +0530

    refactor: move schedule transform logic from lib to input service

--- packages/platform/libraries/schedules.ts
+++ packages/platform/libraries/schedules.ts
@@ -1,13 +1,6 @@
-import {
-  transformApiScheduleOverrides,
-  transformApiScheduleAvailability,
-} from "@calcom/lib/schedules/transformers/from-api";
-
 export {
   ScheduleRepository,
   type FindDetailedScheduleByIdReturnType,
 } from "@calcom/lib/server/repository/schedule";
 
 export { updateSchedule, type UpdateScheduleResponse } from "@calcom/lib/schedules/updateSchedule";
-
-export { transformApiScheduleOverrides, transformApiScheduleAvailability };
