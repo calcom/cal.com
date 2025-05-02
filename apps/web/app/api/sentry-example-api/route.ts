d7e986a200abb0aa07d3512a4af8023f39485350
Omar López <zomars@me.com>
2025-05-02 15:25:03 -0700

    Delete apps/web/app/api/sentry-example-api/route.ts

--- apps/web/app/api/sentry-example-api/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export const dynamic = "force-dynamic";
-
-// A faulty API route to test Sentry's error monitoring
-export function GET() {
-  throw new Error("Sentry Example API Route Error");
-  return NextResponse.json({ data: "Testing Sentry Error..." });
-}
