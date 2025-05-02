a091235f31f9460b8f177fee9a49b38a34d2f7d1
Somay Chauhan <somaychauhan98@gmail.com>
2025-05-02 11:44:07 +0530

    Refactor: add new endpoints for atoms/schedules

--- packages/platform/libraries/schedules.ts
+++ packages/platform/libraries/schedules.ts
@@ -3,4 +3,11 @@ import {
   transformApiScheduleAvailability,
 } from "@calcom/lib/schedules/transformers/from-api";
 
+export {
+  ScheduleRepository,
+  type FindDetailedScheduleByIdReturnType,
+} from "@calcom/lib/server/repository/schedule";
+
+export { updateSchedule, type UpdateScheduleResponse } from "@calcom/lib/schedules/updateSchedule";
+
 export { transformApiScheduleOverrides, transformApiScheduleAvailability };
