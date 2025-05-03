6595a954a477f14285a8098487ef4e250cab5f59
Morgan Vernay <morgan@cal.com>
2025-05-03 15:37:38 +0300

    kysely use connection string

--- packages/kysely/index.ts
+++ packages/kysely/index.ts
@@ -6,16 +6,9 @@ import type { DB, Booking } from "./types.ts";
 export type { DB, Booking };
 
 const connectionString = process.env.DATABASE_URL ?? "postgresql://postgres:@localhost:5450/calendso";
-const parsedUrl = new URL(connectionString);
-const poolOptions = {
-  database: parsedUrl.pathname?.slice(1) || undefined, // Remove leading '/'
-  host: parsedUrl.hostname || "localhost",
-  user: parsedUrl.username || undefined, // Use undefined if not provided
-  password: parsedUrl.password || undefined, // Use undefined if not provided
-  port: parsedUrl.port ? parseInt(parsedUrl.port, 10) : 5432, // Default PG port is 5432
-};
+
 const dialect = new PostgresDialect({
-  pool: new Pool(poolOptions),
+  pool: new Pool({ connectionString }),
 });
 
 const db = new Kysely<DB>({
