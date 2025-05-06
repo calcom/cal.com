df84f736d04bbc5b0009aae1f7c794666d780dac
CarinaWolli <wollencarina@gmail.com>
2025-05-06 13:50:09 +0200

    fix stripe import

--- packages/features/ee/billing/api/webhook/_checkout.session.completed.ts
+++ packages/features/ee/billing/api/webhook/_checkout.session.completed.ts
@@ -1,4 +1,4 @@
-import stripe from "@calcom/app-store/stripepayment/lib/server";
+import stripe from "@calcom/features/ee/payments/server/stripe";
 import prisma from "@calcom/prisma";
 
 import type { SWHMap } from "./__handler";
