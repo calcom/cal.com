1d05f190315605fc67945efd5911c0175df73752
Morgan Vernay <morgan@cal.com>
2025-05-07 23:55:38 +0300

    add decorators for pagination

--- packages/platform/types/bookings/2024-08-13/outputs/get-bookings.output.ts
+++ packages/platform/types/bookings/2024-08-13/outputs/get-bookings.output.ts
@@ -1,4 +1,5 @@
 import { ApiProperty, ApiExtraModels, getSchemaPath } from "@nestjs/swagger";
+import { Type } from "class-transformer";
 import { IsBoolean, IsEnum, IsInt, Min, ValidateNested } from "class-validator";
 
 import { SUCCESS_STATUS, ERROR_STATUS } from "@calcom/platform-constants";
@@ -8,43 +9,6 @@ import {
   GetSeatedBookingOutput_2024_08_13,
   RecurringBookingOutput_2024_08_13,
 } from "@calcom/platform-types";
-
-@ApiExtraModels(
-  BookingOutput_2024_08_13,
-  RecurringBookingOutput_2024_08_13,
-  GetSeatedBookingOutput_2024_08_13,
-  GetRecurringSeatedBookingOutput_2024_08_13
-)
-export class GetBookingsOutput_2024_08_13 {
-  @ApiProperty({ example: SUCCESS_STATUS, enum: [SUCCESS_STATUS, ERROR_STATUS] })
-  @IsEnum([SUCCESS_STATUS, ERROR_STATUS])
-  status!: typeof SUCCESS_STATUS | typeof ERROR_STATUS;
-
-  @ApiProperty({
-    type: "array",
-    items: {
-      oneOf: [
-        { $ref: getSchemaPath(BookingOutput_2024_08_13) },
-        { $ref: getSchemaPath(RecurringBookingOutput_2024_08_13) },
-        { $ref: getSchemaPath(GetSeatedBookingOutput_2024_08_13) },
-        { $ref: getSchemaPath(GetRecurringSeatedBookingOutput_2024_08_13) },
-      ],
-    },
-    description:
-      "Array of booking data, which can contain either BookingOutput objects or RecurringBookingOutput objects",
-  })
-  @ValidateNested({ each: true })
-  data!: (
-    | BookingOutput_2024_08_13
-    | RecurringBookingOutput_2024_08_13
-    | GetSeatedBookingOutput_2024_08_13
-    | GetRecurringSeatedBookingOutput_2024_08_13
-  )[];
-
-  error?: Error;
-
-  pagination!: PaginationMetaDto;
-}
 
 class PaginationMetaDto {
   @ApiProperty({
@@ -64,7 +28,7 @@ class PaginationMetaDto {
   })
   @IsInt()
   @Min(0)
-  remainingItems!: number; // Added this field
+  remainingItems!: number;
 
   @ApiProperty({
     description: "The maximum number of items requested per page.",
@@ -106,4 +70,42 @@ class PaginationMetaDto {
   })
   @IsBoolean()
   hasPreviousPage!: boolean;
+}
+
+@ApiExtraModels(
+  BookingOutput_2024_08_13,
+  RecurringBookingOutput_2024_08_13,
+  GetSeatedBookingOutput_2024_08_13,
+  GetRecurringSeatedBookingOutput_2024_08_13
+)
+export class GetBookingsOutput_2024_08_13 {
+  @ApiProperty({ example: SUCCESS_STATUS, enum: [SUCCESS_STATUS, ERROR_STATUS] })
+  @IsEnum([SUCCESS_STATUS, ERROR_STATUS])
+  status!: typeof SUCCESS_STATUS | typeof ERROR_STATUS;
+
+  @ApiProperty({
+    type: "array",
+    items: {
+      oneOf: [
+        { $ref: getSchemaPath(BookingOutput_2024_08_13) },
+        { $ref: getSchemaPath(RecurringBookingOutput_2024_08_13) },
+        { $ref: getSchemaPath(GetSeatedBookingOutput_2024_08_13) },
+        { $ref: getSchemaPath(GetRecurringSeatedBookingOutput_2024_08_13) },
+      ],
+    },
+    description:
+      "Array of booking data, which can contain either BookingOutput objects or RecurringBookingOutput objects",
+  })
+  data!: (
+    | BookingOutput_2024_08_13
+    | RecurringBookingOutput_2024_08_13
+    | GetSeatedBookingOutput_2024_08_13
+    | GetRecurringSeatedBookingOutput_2024_08_13
+  )[];
+  error?: Error;
+
+  @ApiProperty({ type: () => PaginationMetaDto }) // Crucial for Swagger
+  @Type(() => PaginationMetaDto) // Crucial for class-transformer if you need to transform plain to class
+  @ValidateNested()
+  pagination!: PaginationMetaDto;
 }
