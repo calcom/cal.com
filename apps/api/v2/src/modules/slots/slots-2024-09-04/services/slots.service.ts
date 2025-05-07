08046dec1fe4a7974e8544d047c1f8b91e3b379b
supalarry <laurisskraucis@gmail.com>
2025-05-07 10:17:12 +0200

    fix: check overlap when updating slots

--- apps/api/v2/src/modules/slots/slots-2024-09-04/services/slots.service.ts
+++ apps/api/v2/src/modules/slots/slots-2024-09-04/services/slots.service.ts
@@ -124,17 +124,7 @@ export class SlotsService_2024_09_04 {
 
     const reservationDuration = input.reservationDuration ?? DEFAULT_RESERVATION_DURATION;
 
-    const overlappingReservation = await this.slotsRepository.getOverlappingSlotReservation(
-      input.eventTypeId,
-      startDate.toISO(),
-      endDate.toISO()
-    );
-
-    if (overlappingReservation) {
-      throw new UnprocessableEntityException(
-        `This time slot is already reserved by another user. Please choose a different time.`
-      );
-    }
+    await this.checkSlotOverlap(input.eventTypeId, startDate.toISO(), endDate.toISO());
 
     if (eventType.userId) {
       const slot = await this.slotsRepository.createSlot(
@@ -163,6 +153,20 @@ export class SlotsService_2024_09_04 {
     );
 
     return this.slotsOutputService.getReservationSlotCreated(slot, reservationDuration);
+  }
+
+  private async checkSlotOverlap(eventTypeId: number, startDate: string, endDate: string) {
+    const overlappingReservation = await this.slotsRepository.getOverlappingSlotReservation(
+      eventTypeId,
+      startDate,
+      endDate
+    );
+
+    if (overlappingReservation) {
+      throw new UnprocessableEntityException(
+        `This time slot is already reserved by another user. Please choose a different time.`
+      );
+    }
   }
 
   validateSlotDuration(eventType: EventType, inputSlotDuration: number) {
@@ -268,6 +272,8 @@ export class SlotsService_2024_09_04 {
     }
 
     const reservationDuration = input.reservationDuration ?? DEFAULT_RESERVATION_DURATION;
+
+    await this.checkSlotOverlap(input.eventTypeId, startDate.toISO(), endDate.toISO());
 
     const slot = await this.slotsRepository.updateSlot(
       eventType.id,
