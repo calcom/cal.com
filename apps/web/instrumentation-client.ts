2c39ab86c82dfd304a58755dc97eaff454d50178
hbjORbj <sldisek783@gmail.com>
2025-05-08 13:46:55 -0400

    comment out replay related configs

--- apps/web/instrumentation-client.ts
+++ apps/web/instrumentation-client.ts
@@ -4,14 +4,14 @@ Sentry.init({
   debug: !!process.env.SENTRY_DEBUG,
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   tracesSampleRate: parseFloat(process.env.SENTRY_TRACES_SAMPLE_RATE ?? "0.0") || 0.0,
-  // Controls the percentage of user sessions that are recorded as replays regardless of whether an error occurs
-  replaysSessionSampleRate: parseFloat(process.env.SENTRY_REPLAYS_SESSION_SAMPLE_RATE ?? "0.0") || 0.0,
-  // Controls the percentage of sessions where a replay is recorded only when an error is captured by Sentry
-  replaysOnErrorSampleRate: parseFloat(process.env.SENTRY_REPLAYS_ON_ERROR_SAMPLE_RATE ?? "0.0") || 0.0,
+  // // Controls the percentage of user sessions that are recorded as replays regardless of whether an error occurs
+  // replaysSessionSampleRate: parseFloat(process.env.SENTRY_REPLAYS_SESSION_SAMPLE_RATE ?? "0.0") || 0.0,
+  // // Controls the percentage of sessions where a replay is recorded only when an error is captured by Sentry
+  // replaysOnErrorSampleRate: parseFloat(process.env.SENTRY_REPLAYS_ON_ERROR_SAMPLE_RATE ?? "0.0") || 0.0,
+  // integrations: [Sentry.replayIntegration()],
   _experiments: {
     maxSpans: parseInt(process.env.SENTRY_MAX_SPANS ?? "1000") || 1000,
   },
-  integrations: [Sentry.replayIntegration()],
   beforeSend(event) {
     event.tags = {
       ...event.tags,
