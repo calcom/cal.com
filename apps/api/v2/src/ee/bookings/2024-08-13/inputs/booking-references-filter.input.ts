34b97290be7929214f18461de80d50af4eda42be
Somay Chauhan <somaychauhan98@gmail.com>
2025-05-08 11:34:19 +0530

    feat: add enum validation for booking reference types in filter input

--- apps/api/v2/src/ee/bookings/2024-08-13/inputs/booking-references-filter.input.ts
+++ apps/api/v2/src/ee/bookings/2024-08-13/inputs/booking-references-filter.input.ts
@@ -1,13 +1,23 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsOptional, IsString } from "class-validator";
+import { IsEnum, IsOptional } from "class-validator";
+
+export enum BookingReferenceType {
+  GOOGLE_CALENDAR = "google_calendar",
+  OFFICE365_CALENDAR = "office365_calendar",
+  DAILY_VIDEO = "daily_video",
+  GOOGLE_VIDEO = "google_video",
+  OFFICE365_VIDEO = "office365_video",
+  ZOOM_VIDEO = "zoom_video",
+}
 
 export class BookingReferencesFilterInput_2024_08_13 {
   @ApiProperty({
     description: "Filter booking references by type",
     required: false,
-    example: "zoom",
+    enum: BookingReferenceType,
+    example: "google_calendar",
   })
   @IsOptional()
-  @IsString()
-  type?: string;
+  @IsEnum(BookingReferenceType)
+  type?: BookingReferenceType;
 }
