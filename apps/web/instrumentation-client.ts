a41c4223b107d424ba8ea7e37609e6e3e1f2557c
hbjORbj <sldisek783@gmail.com>
2025-05-08 11:17:52 -0400

    update

--- apps/web/instrumentation-client.ts
+++ apps/web/instrumentation-client.ts
@@ -3,9 +3,11 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   debug: !!process.env.SENTRY_DEBUG,
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  tracesSampleRate: parseFloat(process.env.SENTRY_TRACES_SAMPLE_RATE ?? "0.0"),
-  replaysSessionSampleRate: parseFloat(process.env.SENTRY_REPLAYS_SESSION_SAMPLE_RATE ?? "0.0"), // Capture 10% of sessions for replay
-  replaysOnErrorSampleRate: parseFloat(process.env.SENTRY_REPLAYS_ON_ERROR_SAMPLE_RATE ?? "0.0"), // Capture replays for all error events
+  tracesSampleRate: parseFloat(process.env.SENTRY_TRACES_SAMPLE_RATE ?? "0.0") || 0.0,
+  // Controls the percentage of user sessions that are recorded as replays regardless of whether an error occurs
+  replaysSessionSampleRate: parseFloat(process.env.SENTRY_REPLAYS_SESSION_SAMPLE_RATE ?? "0.0") || 0.0,
+  // Controls the percentage of sessions where a replay is recorded only when an error is captured by Sentry
+  replaysOnErrorSampleRate: parseFloat(process.env.SENTRY_REPLAYS_ON_ERROR_SAMPLE_RATE ?? "0.0") || 0.0,
   _experiments: {
     maxSpans: parseInt(process.env.SENTRY_MAX_SPANS ?? "1000") || 1000,
   },
