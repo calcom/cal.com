089d93bac5b61ce38054969d3e454c758f1c3f80
hbjORbj <sldisek783@gmail.com>
2025-05-08 10:59:23 -0400

    update

--- apps/web/instrumentation-client.ts
+++ apps/web/instrumentation-client.ts
@@ -1,11 +1,15 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   // Adds request headers and IP for users, for more info visit:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
-  tracesSampleRate: 1.0,
+  debug: !!process.env.SENTRY_DEBUG,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  tracesSampleRate: parseFloat(process.env.SENTRY_TRACES_SAMPLE_RATE ?? "0.0") || 0.0,
+  _experiments: {
+    maxSpans: parseInt(process.env.SENTRY_MAX_SPANS ?? "1000") || 1000,
+  },
   beforeSend(event) {
     event.tags = {
       ...event.tags,
