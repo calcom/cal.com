e5485ba4280bb96d55540cc8a388395575491974
Omar López <zomars@me.com>
2025-05-08 15:27:56 -0700

    Update sentry.server.config.ts

--- apps/web/sentry.server.config.ts
+++ apps/web/sentry.server.config.ts
@@ -4,4 +4,5 @@ Sentry.init({
   debug: !!process.env.SENTRY_DEBUG,
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   tracesSampleRate: parseFloat(process.env.SENTRY_TRACES_SAMPLE_RATE ?? "0.0") || 0.0,
+  integrations: [Sentry.prismaIntegration()],
 });
