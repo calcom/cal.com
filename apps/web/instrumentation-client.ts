be37bf011dc45fa836e473008509ff9f450a8afa
hbjORbj <sldisek783@gmail.com>
2025-05-08 11:10:14 -0400

    do not collect pii

--- apps/web/instrumentation-client.ts
+++ apps/web/instrumentation-client.ts
@@ -1,9 +1,6 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  // Adds request headers and IP for users, for more info visit:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
   debug: !!process.env.SENTRY_DEBUG,
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   tracesSampleRate: parseFloat(process.env.SENTRY_TRACES_SAMPLE_RATE ?? "0.0"),
