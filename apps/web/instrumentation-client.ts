66f04ebb3ee4c765736533128178dd5fdea5475b
Omar López <zomars@me.com>
2025-05-08 22:01:01 -0700

    Update instrumentation-client.ts

--- apps/web/instrumentation-client.ts
+++ apps/web/instrumentation-client.ts
@@ -21,7 +21,7 @@ Sentry.init({
   replaysOnErrorSampleRate: 1.0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  debug: !!process.env.SENTRY_DEBUG,
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
