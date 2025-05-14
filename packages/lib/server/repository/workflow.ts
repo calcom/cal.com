cdbe2d644236284e654aba0851145a1a7aad4282
Omar López <zomars@me.com>
2025-05-14 14:26:09 -0700

    Update workflow.ts

--- packages/lib/server/repository/workflow.ts
+++ packages/lib/server/repository/workflow.ts
@@ -22,44 +22,42 @@ export const ZGetInputSchema = z.object({
 
 export type TGetInputSchema = z.infer<typeof ZGetInputSchema>;
 
-const { include: includedFields } = Prisma.validator<Prisma.WorkflowInclude>()({
-  include: {
-    activeOn: {
-      select: {
-        eventType: {
-          select: {
-            id: true,
-            title: true,
-            parentId: true,
-            _count: {
-              select: {
-                children: true,
-              },
+const includedFields = Prisma.validator<Prisma.WorkflowInclude>()({
+  activeOn: {
+    select: {
+      eventType: {
+        select: {
+          id: true,
+          title: true,
+          parentId: true,
+          _count: {
+            select: {
+              children: true,
             },
           },
         },
       },
     },
-    activeOnTeams: {
-      select: {
-        team: {
-          select: {
-            id: true,
-            name: true,
-          },
+  },
+  activeOnTeams: {
+    select: {
+      team: {
+        select: {
+          id: true,
+          name: true,
         },
       },
     },
-    steps: true,
-    team: {
-      select: {
-        id: true,
-        slug: true,
-        name: true,
-        members: true,
-        logoUrl: true,
-        isOrganization: true,
-      },
+  },
+  steps: true,
+  team: {
+    select: {
+      id: true,
+      slug: true,
+      name: true,
+      members: true,
+      logoUrl: true,
+      isOrganization: true,
     },
   },
 });
