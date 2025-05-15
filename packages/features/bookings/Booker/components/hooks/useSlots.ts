ce721b3ac22a8c721334988c07a9fa8b41c84206
supalarry <laurisskraucis@gmail.com>
2025-05-15 18:09:33 +0200

    dont touch web app

--- packages/features/bookings/Booker/components/hooks/useSlots.ts
+++ packages/features/bookings/Booker/components/hooks/useSlots.ts
@@ -101,7 +101,6 @@ export const useSlots = (event: { id: number; length: number } | null) => {
       setSlotReservationId(data.uid);
     },
   });
-  const seatedEventData = useBookerStore((state) => state.seatedEventData);
   const removeSelectedSlot = trpc.viewer.slots.removeSelectedSlotMark.useMutation({
     trpc: { context: { skipBatch: true } },
   });
@@ -137,7 +136,6 @@ export const useSlots = (event: { id: number; length: number } | null) => {
         eventTypeId,
         slotUtcEndDate: dayjs(timeSlotToBeBooked).utc().add(eventDuration, "minutes").toISOString(),
         _isDryRun: isBookingDryRun(searchParams),
-        bookingUid: seatedEventData.bookingUid || undefined,
       });
     }
   };
