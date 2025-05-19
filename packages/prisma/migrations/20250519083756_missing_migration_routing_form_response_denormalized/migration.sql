0628bd267ac1094e5fa81851e3ce2e931d4ff107
Eunjae Lee <hey@eunjae.dev>
2025-05-19 11:23:45 +0200

    updates

--- packages/prisma/migrations/20250519083756_missing_migration_routing_form_response_denormalized/migration.sql
+++ packages/prisma/migrations/20250519083756_missing_migration_routing_form_response_denormalized/migration.sql
@@ -6,15 +6,3 @@ CREATE INDEX "App_RoutingForms_FormResponse_routedToBookingUid_idx" ON "App_Rout
 
 -- CreateIndex
 CREATE INDEX "DelegationCredential_enabled_idx" ON "DelegationCredential"("enabled");
-
--- DropForeignKey
-ALTER TABLE "RoutingFormResponseField" DROP CONSTRAINT IF EXISTS "RoutingFormResponseField_responseId_fkey";
-
--- DropForeignKey
-ALTER TABLE "RoutingFormResponseField" DROP CONSTRAINT IF EXISTS "RoutingFormResponseField_response_fkey";
-
--- AddForeignKey
-ALTER TABLE "RoutingFormResponseField" ADD CONSTRAINT "RoutingFormResponseField_response_fkey" FOREIGN KEY ("responseId") REFERENCES "App_RoutingForms_FormResponse"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "RoutingFormResponseField" ADD CONSTRAINT "RoutingFormResponseField_responseId_fkey" FOREIGN KEY ("responseId") REFERENCES "RoutingFormResponseDenormalized"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
