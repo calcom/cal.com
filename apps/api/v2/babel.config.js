8e32b9c8535f26dc252f7972cc3b88e4db1c6db0
supalarry <laurisskraucis@gmail.com>
2025-05-19 15:56:03 +0200

    fix: v2 tests

---
+++ apps/api/v2/babel.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current",
+        },
+      },
+    ],
+    "@babel/preset-typescript",
+  ],
+};
