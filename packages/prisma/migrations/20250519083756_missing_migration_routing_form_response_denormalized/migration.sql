876940afe56dda5b5b1c36c595c683b2dbf32a03
Eunjae Lee <hey@eunjae.dev>
2025-05-19 10:54:02 +0200

    more changes

--- packages/prisma/migrations/20250519083756_missing_migration_routing_form_response_denormalized/migration.sql
+++ packages/prisma/migrations/20250519083756_missing_migration_routing_form_response_denormalized/migration.sql
@@ -1,12 +1,6 @@
 -- DropForeignKey
 ALTER TABLE "RoutingFormResponseDenormalized" DROP CONSTRAINT "RoutingFormResponseDenormalized_eventTypeId_fkey";
 
--- DropForeignKey
-ALTER TABLE "RoutingFormResponseField" DROP CONSTRAINT "RoutingFormResponseField_response_fkey";
-
--- AlterTable
-ALTER TABLE "RoutingFormResponseField" ALTER COLUMN "responseId" DROP NOT NULL;
-
 -- CreateIndex
 CREATE INDEX "App_RoutingForms_FormResponse_routedToBookingUid_idx" ON "App_RoutingForms_FormResponse"("routedToBookingUid");
 
@@ -14,7 +8,4 @@ CREATE INDEX "App_RoutingForms_FormResponse_routedToBookingUid_idx" ON "App_Rout
 CREATE INDEX "DelegationCredential_enabled_idx" ON "DelegationCredential"("enabled");
 
 -- AddForeignKey
-ALTER TABLE "RoutingFormResponseField" ADD CONSTRAINT "RoutingFormResponseField_response_fkey" FOREIGN KEY ("responseId") REFERENCES "App_RoutingForms_FormResponse"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "RoutingFormResponseField" ADD CONSTRAINT "RoutingFormResponseField_responseId_fkey" FOREIGN KEY ("responseId") REFERENCES "RoutingFormResponseDenormalized"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "RoutingFormResponseField" ADD CONSTRAINT "RoutingFormResponseField_responseId_fkey" FOREIGN KEY ("responseId") REFERENCES "RoutingFormResponseDenormalized"("id") ON DELETE CASCADE ON UPDATE CASCADE;
