b2cf172a84fe8953f9497bf6e43874f476fbc04b
Somay Chauhan <somaychauhan98@gmail.com>
2025-05-19 12:12:44 +0530

    chore: configure babel and jest for node module transpilation in api v2

---
+++ apps/api/v2/babel.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current",
+        },
+      },
+    ],
+    "@babel/preset-typescript",
+  ],
+};
