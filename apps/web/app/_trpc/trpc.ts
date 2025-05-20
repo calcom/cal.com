c765b297ddaf5e4fd9f648496e27f548cd61f9eb
Eunjae Lee <hey@eunjae.dev>
2025-05-20 15:00:50 +0200

    chore: fix import path of AppRouter

--- apps/web/app/_trpc/trpc.ts
+++ apps/web/app/_trpc/trpc.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import type { AppRouter } from "@calcom/trpc/types/server/routers/_app";
+import type { AppRouter } from "@calcom/trpc/server/routers/_app";
 
 import { createTRPCReact } from "@trpc/react-query";
 
