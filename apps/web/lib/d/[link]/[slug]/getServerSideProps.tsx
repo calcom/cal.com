6e6df42bcfdc6ff8bd2fb5103c82c290e2ab5372
hbjORbj <sldisek783@gmail.com>
2025-05-20 17:33:17 -0400

    fix

--- apps/web/lib/d/[link]/[slug]/getServerSideProps.tsx
+++ apps/web/lib/d/[link]/[slug]/getServerSideProps.tsx
@@ -6,6 +6,7 @@ import { getServerSession } from "@calcom/features/auth/lib/getServerSession";
 import { getBookingForReschedule, getMultipleDurationValue } from "@calcom/features/bookings/lib/get-booking";
 import type { GetBookingType } from "@calcom/features/bookings/lib/get-booking";
 import { orgDomainConfig } from "@calcom/features/ee/organizations/lib/orgDomains";
+import { EventRepository } from "@calcom/lib/server/repository/event";
 import { UserRepository } from "@calcom/lib/server/repository/user";
 import slugify from "@calcom/lib/slugify";
 import prisma from "@calcom/prisma";
