93196501bea384a2dd28b8c8ade03924b777575e
CarinaWolli <wollencarina@gmail.com>
2025-05-20 10:08:10 +0200

    improve rr reset interval label

--- packages/features/ee/teams/components/RoundRobinResetInterval.tsx
+++ packages/features/ee/teams/components/RoundRobinResetInterval.tsx
@@ -30,8 +30,8 @@ const RoundRobinResetInterval = () => {
         return (
           <>
             <div className="">
-              <h4 className="text-emphasis text-sm font-semibold leading-5">{t("rr_reset_interval")}</h4>
-              <p className="text-default text-sm ">{t("rr_reset_interval_description")}</p>
+              <h4 className="text-emphasis text-sm font-semibold leading-5">{t("reset_interval")}</h4>
+              <p className="text-default text-sm">{t("rr_reset_interval_description")}</p>
               <div className="mt-4 w-48">
                 <Select
                   options={intervalOptions}
