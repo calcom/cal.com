37f5e0908eb270c2a790d484d68662f294332372
Alex van Andel <me@alexvanandel.com>
2025-05-22 00:23:55 +0100

    Remove custom 'zod' booking model, it does not belong with Prisma

--- apps/api/v1/lib/validations/booking.ts
+++ apps/api/v1/lib/validations/booking.ts
@@ -1,15 +1,14 @@
 import { z } from "zod";
 
-import {
-  _AttendeeModel,
-  _BookingModel as Booking,
-  _EventTypeModel,
-  _PaymentModel,
-  _TeamModel,
-  _UserModel,
-} from "@calcom/prisma/zod";
+import { routingFormResponseInDbSchema } from "@calcom/app-store/routing-forms/zod";
+import { timeZoneSchema } from "@calcom/lib/dayjs/timeZone.schema";
 import { iso8601 } from "@calcom/prisma/zod-utils";
-import { extendedBookingCreateBody } from "@calcom/prisma/zod/custom/booking";
+import { AttendeeSchema } from "@calcom/prisma/zod/modelSchema/AttendeeSchema";
+import { BookingSchema as Booking } from "@calcom/prisma/zod/modelSchema/BookingSchema";
+import { EventTypeSchema } from "@calcom/prisma/zod/modelSchema/EventTypeSchema";
+import { PaymentSchema } from "@calcom/prisma/zod/modelSchema/PaymentSchema";
+import { TeamSchema } from "@calcom/prisma/zod/modelSchema/TeamSchema";
+import { UserSchema } from "@calcom/prisma/zod/modelSchema/UserSchema";
 
 import { schemaQueryUserId } from "./shared/queryUserId";
 
@@ -26,6 +25,101 @@ const schemaBookingBaseBodyParams = Booking.pick({
   cancelledBy: true,
   createdAt: true,
 }).partial();
+
+export const bookingCreateBodySchema = z.object({
+  end: z.string().optional(),
+  eventTypeId: z.number(),
+  eventTypeSlug: z.string().optional(),
+  rescheduleUid: z.string().optional(),
+  recurringEventId: z.string().optional(),
+  rescheduledBy: z.string().email({ message: "Invalid email" }).optional(),
+  start: z.string(),
+  timeZone: timeZoneSchema,
+  user: z.union([z.string(), z.array(z.string())]).optional(),
+  language: z.string(),
+  bookingUid: z.string().optional(),
+  metadata: z.record(z.string()),
+  hasHashedBookingLink: z.boolean().optional(),
+  hashedLink: z.string().nullish(),
+  seatReferenceUid: z.string().optional(),
+  orgSlug: z.string().optional(),
+  teamMemberEmail: z.string().nullish(),
+  crmOwnerRecordType: z.string().nullish(),
+  routedTeamMemberIds: z.array(z.number()).nullish(),
+  routingFormResponseId: z.number().optional(),
+  skipContactOwner: z.boolean().optional(),
+  crmAppSlug: z.string().nullish().optional(),
+  cfToken: z.string().nullish().optional(),
+
+  /**
+   * Holds the corrected responses of the Form for a booking, provided during rerouting
+   */
+  reroutingFormResponses: routingFormResponseInDbSchema.optional(),
+  /**
+   * Used to identify if the booking is a dry run.
+   */
+  _isDryRun: z.boolean().optional(),
+  /** Whether to override the cache */
+  _shouldServeCache: z.boolean().optional(),
+  tracking: z
+    .object({
+      utm_source: z.string().optional(),
+      utm_medium: z.string().optional(),
+      utm_campaign: z.string().optional(),
+      utm_term: z.string().optional(),
+      utm_content: z.string().optional(),
+    })
+    .optional(),
+});
+
+export type BookingCreateBody = z.input<typeof bookingCreateBodySchema>;
+
+export const extendedBookingCreateBody = bookingCreateBodySchema.merge(
+  z.object({
+    noEmail: z.boolean().optional(),
+    recurringCount: z.number().optional(),
+    allRecurringDates: z
+      .array(
+        z.object({
+          start: z.string(),
+          end: z.string(),
+        })
+      )
+      .optional(),
+    currentRecurringIndex: z.number().optional(),
+    appsStatus: z
+      .array(
+        z.object({
+          appName: z.string(),
+          success: z.number(),
+          failures: z.number(),
+          type: z.string(),
+          errors: z.string().array(),
+          warnings: z.string().array().optional(),
+        })
+      )
+      .optional(),
+    luckyUsers: z.array(z.number()).optional(),
+    customInputs: z.undefined().optional(),
+  })
+);
+
+// It has only the legacy props that are part of `responses` now. The API can still hit old props
+export const bookingCreateSchemaLegacyPropsForApi = z.object({
+  email: z.string(),
+  name: z.string(),
+  guests: z.array(z.string()).optional(),
+  notes: z.string().optional(),
+  location: z.string(),
+  smsReminderNumber: z.string().optional().nullable(),
+  rescheduleReason: z.string().optional(),
+  customInputs: z.array(z.object({ label: z.string(), value: z.union([z.string(), z.boolean()]) })),
+});
+
+// This is the schema that is used for the API. It has all the legacy props that are part of `responses` now.
+export const bookingCreateBodySchemaForApi = extendedBookingCreateBody.merge(
+  bookingCreateSchemaLegacyPropsForApi.partial()
+);
 
 export const schemaBookingCreateBodyParams = extendedBookingCreateBody.merge(schemaQueryUserId.partial());
 
@@ -72,17 +166,16 @@ export const schemaBookingEditBodyParams = schemaBookingBaseBodyParams
   .merge(schemaBookingEditParams)
   .omit({ uid: true });
 
-const teamSchema = _TeamModel.pick({
+const teamSchema = TeamSchema.pick({
   name: true,
   slug: true,
 });
 
 export const schemaBookingReadPublic = Booking.extend({
-  eventType: _EventTypeModel
-    .pick({
-      title: true,
-      slug: true,
-    })
+  eventType: EventTypeSchema.pick({
+    title: true,
+    slug: true,
+  })
     .merge(
       z.object({
         team: teamSchema.nullish(),
@@ -91,7 +184,7 @@ export const schemaBookingReadPublic = Booking.extend({
     .nullish(),
   attendees: z
     .array(
-      _AttendeeModel.pick({
+      AttendeeSchema.pick({
         id: true,
         email: true,
         name: true,
@@ -100,17 +193,15 @@ export const schemaBookingReadPublic = Booking.extend({
       })
     )
     .optional(),
-  user: _UserModel
-    .pick({
-      email: true,
-      name: true,
-      timeZone: true,
-      locale: true,
-    })
-    .nullish(),
+  user: UserSchema.pick({
+    email: true,
+    name: true,
+    timeZone: true,
+    locale: true,
+  }).nullish(),
   payment: z
     .array(
-      _PaymentModel.pick({
+      PaymentSchema.pick({
         id: true,
         success: true,
         paymentOption: true,
