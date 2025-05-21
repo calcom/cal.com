187f64f92687bc8d85f926c8491ea7fe7f71b2c3
Sean Brydon <sean@cal.com>
2025-05-21 09:16:40 +0100

    use app dir to fetch team feature flags

---
+++ packages/features/flags/hooks/useIsFeatureEnabledForTeam.ts
@@ -0,0 +1,19 @@
+import type { AppFlags } from "../config";
+
+type TeamFeatures = Record<number, Record<keyof AppFlags, boolean>> | null;
+
+export const useIsFeatureEnabledForTeam = ({
+  teamFeatures,
+  teamId,
+  feature,
+}: {
+  teamFeatures?: TeamFeatures;
+  teamId?: number;
+  feature: keyof AppFlags;
+}) => {
+  if (!teamId || !teamFeatures) {
+    return false;
+  }
+
+  return teamFeatures[teamId][feature];
+};
