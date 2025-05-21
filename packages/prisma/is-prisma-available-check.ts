32cda9479370ce0e9a3af5e4603a9a0b8578b771
Alex van Andel <me@alexvanandel.com>
2025-05-21 01:37:19 +0100

    Updated isPrismaAvailableCheck that doesn't crash on import

--- packages/prisma/is-prisma-available-check.ts
+++ packages/prisma/is-prisma-available-check.ts
@@ -1,16 +1,17 @@
-import prisma from ".";
 import { Prisma } from "./client";
 
-export async function isPrismaAvailableCheck() {
+export async function isPrismaAvailableCheck(): Promise<boolean> {
   try {
-    await prisma.$queryRaw`SELECT 1`;
+    const { PrismaClient } = await import("./client");
+    const prisma = new PrismaClient();
+
+    await prisma.$queryRawUnsafe("SELECT 1");
+    await prisma.$disconnect();
     return true;
   } catch (e: unknown) {
     if (e instanceof Prisma.PrismaClientInitializationError) {
-      // Database might not available at build time.
       return false;
-    } else {
-      throw e;
     }
+    throw e;
   }
 }
