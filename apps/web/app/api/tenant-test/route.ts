cd3ac7f0092dfe24a46ccb037ccfcd1f8d6a2c56
Omar López <zomars@me.com>
2025-05-21 20:53:25 -0700

    Delete route.ts

--- apps/web/app/api/tenant-test/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-import { prisma } from "@calcom/prisma";
-import { withPrismaRoute } from "@calcom/prisma/store/withPrismaRoute";
-
-export const GET = withPrismaRoute(async (req) => {
-  const user = await prisma.user.findFirst({ where: { id: 1 }, select: { id: true, name: true } });
-  return NextResponse.json({ user });
-});
