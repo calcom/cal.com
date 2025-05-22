72755fbe3722f308b9daf9551d39503c7bf6840f
Udit Takkar <udit222001@gmail.com>
2025-05-22 13:49:20 +0530

    chore: change schema

--- packages/prisma/migrations/20250522081447_add_cal_video_settings/migration.sql
+++ packages/prisma/migrations/20250522081447_add_cal_video_settings/migration.sql
@@ -5,14 +5,13 @@ CREATE TABLE "CalVideoSettings" (
     "disableRecordingForGuests" BOOLEAN NOT NULL DEFAULT false,
     "redirectUrlOnExit" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "CalVideoSettings_pkey" PRIMARY KEY ("eventTypeId")
 );
 
 -- CreateIndex
 CREATE INDEX "CalVideoSettings_eventTypeId_idx" ON "CalVideoSettings"("eventTypeId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "CalVideoSettings_eventTypeId_key" ON "CalVideoSettings"("eventTypeId");
 
 -- AddForeignKey
 ALTER TABLE "CalVideoSettings" ADD CONSTRAINT "CalVideoSettings_eventTypeId_fkey" FOREIGN KEY ("eventTypeId") REFERENCES "EventType"("id") ON DELETE CASCADE ON UPDATE CASCADE;
