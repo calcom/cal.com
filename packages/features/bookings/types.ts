a2897c521b2bdae4ef824f7e78aeb37e571e7b23
Alex van Andel <me@alexvanandel.com>
2025-05-22 03:11:19 +0100

    More fixes, final fix remaining is CompleteEventType

--- packages/features/bookings/types.ts
+++ packages/features/bookings/types.ts
@@ -1,11 +1,11 @@
 import type { ErrorOption, FieldPath } from "react-hook-form";
 
 import type { SchedulingType } from "@calcom/prisma/client";
-import type { BookingCreateBody } from "@calcom/prisma/zod/custom/booking";
 import type { RouterOutputs } from "@calcom/trpc/react";
 import type { AppsStatus } from "@calcom/types/Calendar";
 
 import type { DatePickerProps } from "../calendars/DatePicker";
+import type { BookingCreateBody } from "./lib/bookingCreateBodySchema";
 
 export type PublicEvent = NonNullable<RouterOutputs["viewer"]["public"]["event"]>;
 
