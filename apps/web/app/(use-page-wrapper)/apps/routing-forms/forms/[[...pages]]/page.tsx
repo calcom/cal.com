314245fef73962d9bf77c95b56a66445ef366516
hbjORbj <sldisek783@gmail.com>
2025-05-23 16:33:36 -0400

    fix

--- apps/web/app/(use-page-wrapper)/apps/routing-forms/forms/[[...pages]]/page.tsx
+++ apps/web/app/(use-page-wrapper)/apps/routing-forms/forms/[[...pages]]/page.tsx
@@ -3,13 +3,14 @@ import { _generateMetadata } from "app/_utils";
 import Forms from "@calcom/app-store/routing-forms/pages/forms/[...appPages]";
 
 export const generateMetadata = async ({ params }: { params: Promise<{ pages: string[] }> }) => {
-  console.log((await params).pages);
+  const pages = (await params).pages;
+
   return await _generateMetadata(
     (t) => `${t("routing_forms")} | Cal.com Forms`,
     () => "",
     undefined,
     undefined,
-    `/routing/forms/${(await params).pages.join("/")}`
+    `/routing/forms/${pages?.length > 0 ? pages.join("/") : ""}`
   );
 };
 
