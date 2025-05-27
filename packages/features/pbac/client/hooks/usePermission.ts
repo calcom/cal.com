4c25cef183cd5562924c06004e82ad4f96a4abed
Sean Brydon <sean@cal.com>
2025-05-27 10:19:30 +0100

    move hooks out of infra into client with provider

--- packages/features/pbac/client/hooks/usePermission.ts
+++ packages/features/pbac/client/hooks/usePermission.ts
@@ -1,5 +1,5 @@
 import type { PermissionString } from "../../domain/types/permission-registry";
-import { usePermissionStore } from "../store/permission-store";
+import { usePermissionStore } from "../../infrastructure/store/permission-store";
 
 export function usePermission(teamId: number, permission: PermissionString) {
   const hasPermission = usePermissionStore((state) => state.hasPermission(teamId, permission));
