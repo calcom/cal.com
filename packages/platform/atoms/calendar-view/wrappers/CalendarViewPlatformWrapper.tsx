93371c4f0e27564bb565b447bd187a5c2e91232c
Ryukemeister <sahalrajiv6900@gmail.com>
2025-05-27 14:51:47 +0530

    update calendar view

--- packages/platform/atoms/calendar-view/wrappers/CalendarViewPlatformWrapper.tsx
+++ packages/platform/atoms/calendar-view/wrappers/CalendarViewPlatformWrapper.tsx
@@ -1,7 +1,18 @@
-import { useMemo } from "react";
+import { useMemo, useEffect, useState } from "react";
+import { shallow } from "zustand/shallow";
 
+import dayjs from "@calcom/dayjs";
 import type { BookerProps } from "@calcom/features/bookings/Booker";
+// import { LargeCalendar } from "./components/LargeCalendar";
+import { LargeCalendar } from "@calcom/features/bookings/Booker/components/LargeCalendar";
 import { useBookerLayout } from "@calcom/features/bookings/Booker/components/hooks/useBookerLayout";
+import { useBookerStore, useInitializeBookerStore } from "@calcom/features/bookings/Booker/store";
+import { useTimePreferences } from "@calcom/features/bookings/lib";
+import { useTimesForSchedule } from "@calcom/features/schedules/lib/use-schedule/useTimesForSchedule";
+import { getRoutedTeamMemberIdsFromSearchParams } from "@calcom/lib/bookings/getRoutedTeamMemberIdsFromSearchParams";
+import { getUsernameList } from "@calcom/lib/defaultEvents";
+import type { RoutingFormSearchParams } from "@calcom/platform-types";
+import { BookerLayouts } from "@calcom/prisma/zod-utils";
 
 // import {
 //   transformApiEventTypeForAtom,
@@ -11,8 +22,10 @@ import {
   transformApiEventTypeForAtom,
   transformApiTeamEventTypeForAtom,
 } from "../../event-types/atom-api-transformers/transformApiEventTypeForAtom";
+import { useGetBookingForReschedule } from "../../hooks/bookings/useGetBookingForReschedule";
 import { useEventType } from "../../hooks/event-types/public/useEventType";
 import { useTeamEventType } from "../../hooks/event-types/public/useTeamEventType";
+import { useAvailableSlots } from "../../hooks/useAvailableSlots";
 
 type CalendarViewPlatformWrapperProps = {
   username: string;
@@ -30,10 +43,27 @@ type CalendarViewPlatformWrapperProps = {
     rescheduleReason?: string;
   } & Record<string, string | string[]>;
   entity?: BookerProps["entity"];
+  rescheduleUid?: string;
+  duration?: number | null;
+  teamMemberEmail?: string | null;
+  routingFormSearchParams: RoutingFormSearchParams | undefined;
+  bookingUid?: string;
+  allowUpdatingUrlParams?: boolean;
+  crmAppSlug?: string | null;
+  crmOwnerRecordType?: string | null;
+  view?: "MONTH_VIEW" | "WEEK_VIEW" | "COLUMN_VIEW";
 };
 
 export const CalendarViewPlatformWrapper = (props: CalendarViewPlatformWrapperProps) => {
-  const { username, eventSlug, isTeamEvent, teamId, hostsLimit, defaultFormValues } = props;
+  const {
+    username,
+    eventSlug,
+    isTeamEvent,
+    teamId,
+    hostsLimit,
+    defaultFormValues,
+    view = "MONTH_VIEW",
+  } = props;
 
   const { isSuccess, isError, isPending, data } = useEventType(username, eventSlug, isTeamEvent);
   const {
@@ -85,7 +115,126 @@ export const CalendarViewPlatformWrapper = (props: CalendarViewPlatformWrapperPr
     props.hostsLimit,
   ]);
 
-  const bookerLayout = useBookerLayout(event.data);
+  const bookerLayout = useBookerLayout(event.data?.profile?.bookerLayouts);
+  const { extraDays } = bookerLayout;
+  const setSelectedDuration = useBookerStore((state) => state.setSelectedDuration);
+  const [bookerState, setBookerState] = useBookerStore((state) => [state.state, state.setState], shallow);
+  const selectedDate = useBookerStore((state) => state.selectedDate);
+  const date = dayjs(selectedDate).format("YYYY-MM-DD");
+  const monthCount =
+    ((bookerLayout.layout !== BookerLayouts.WEEK_VIEW && bookerState === "selecting_time") ||
+      bookerLayout.layout === BookerLayouts.COLUMN_VIEW) &&
+    dayjs(date).add(1, "month").month() !==
+      dayjs(date).add(bookerLayout.columnViewExtraDays.current, "day").month()
+      ? 2
+      : undefined;
+  const month = useBookerStore((state) => state.month);
+  const [dayCount] = useBookerStore((state) => [state.dayCount, state.setDayCount], shallow);
+
+  const prefetchNextMonth =
+    (bookerLayout.layout === BookerLayouts.WEEK_VIEW &&
+      !!bookerLayout.extraDays &&
+      dayjs(date).month() !== dayjs(date).add(bookerLayout.extraDays, "day").month()) ||
+    (bookerLayout.layout === BookerLayouts.COLUMN_VIEW &&
+      dayjs(date).month() !== dayjs(date).add(bookerLayout.columnViewExtraDays.current, "day").month());
+
+  const [startTime, endTime] = useTimesForSchedule({
+    month,
+    monthCount,
+    dayCount,
+    prefetchNextMonth,
+    selectedDate,
+  });
+  const selectedDuration = useBookerStore((state) => state.selectedDuration);
+
+  useEffect(() => {
+    setSelectedDuration(props.duration ?? null);
+  }, [props.duration]);
+
+  const { timezone } = useTimePreferences();
+  const isDynamic = useMemo(() => {
+    return getUsernameList(username ?? "").length > 1;
+  }, [username]);
+
+  const [routingParams, setRoutingParams] = useState<{
+    routedTeamMemberIds?: number[];
+    _shouldServeCache?: boolean;
+    skipContactOwner?: boolean;
+    isBookingDryRun?: boolean;
+  }>({});
+
+  useEffect(() => {
+    const searchParams = props.routingFormSearchParams
+      ? new URLSearchParams(props.routingFormSearchParams)
+      : new URLSearchParams(window.location.search);
+
+    const routedTeamMemberIds = getRoutedTeamMemberIdsFromSearchParams(searchParams);
+    const skipContactOwner = searchParams.get("cal.skipContactOwner") === "true";
+
+    const _cacheParam = searchParams?.get("cal.cache");
+    const _shouldServeCache = _cacheParam ? _cacheParam === "true" : undefined;
+    const isBookingDryRun =
+      searchParams?.get("cal.isBookingDryRun")?.toLowerCase() === "true" ||
+      searchParams?.get("cal.sandbox")?.toLowerCase() === "true";
+    setRoutingParams({
+      ...(skipContactOwner ? { skipContactOwner } : {}),
+      ...(routedTeamMemberIds ? { routedTeamMemberIds } : {}),
+      ...(_shouldServeCache ? { _shouldServeCache } : {}),
+      ...(isBookingDryRun ? { isBookingDryRun } : {}),
+    });
+  }, [props.routingFormSearchParams]);
+  const bookingData = useBookerStore((state) => state.bookingData);
+  const setBookingData = useBookerStore((state) => state.setBookingData);
+  const layout = BookerLayouts[view];
+
+  useGetBookingForReschedule({
+    uid: props.rescheduleUid ?? props.bookingUid ?? "",
+    onSuccess: (data) => {
+      setBookingData(data);
+    },
+  });
+
+  useInitializeBookerStore({
+    ...props,
+    // props.teamMemberEmail,
+    // crmAppSlug,
+    // crmOwnerRecordType,
+    eventId: event.data?.id,
+    rescheduleUid: props.rescheduleUid ?? null,
+    bookingUid: props.bookingUid ?? null,
+    layout: layout,
+    org: props.entity?.orgSlug,
+    username,
+    bookingData,
+    isPlatform: true,
+    allowUpdatingUrlParams: props.allowUpdatingUrlParams ?? false,
+  });
+
+  const schedule = useAvailableSlots({
+    usernameList: getUsernameList(username),
+    eventTypeId: event?.data?.id ?? 0,
+    startTime,
+    endTime,
+    timeZone: timezone,
+    duration: selectedDuration ?? undefined,
+    rescheduleUid: props.rescheduleUid,
+    teamMemberEmail: props.teamMemberEmail ?? undefined,
+    ...(props.isTeamEvent
+      ? {
+          isTeamEvent: props.isTeamEvent,
+          teamId: teamId,
+        }
+      : {}),
+    enabled:
+      Boolean(teamId || username) &&
+      Boolean(month) &&
+      Boolean(timezone) &&
+      (props.isTeamEvent ? !isTeamPending : !isPending) &&
+      Boolean(event?.data?.id),
+    orgSlug: props.entity?.orgSlug ?? undefined,
+    eventTypeSlug: isDynamic ? "dynamic" : eventSlug || "",
+    ...routingParams,
+  });
 
   console.log("----");
   console.log(data, "individual event data");
@@ -96,5 +245,12 @@ export const CalendarViewPlatformWrapper = (props: CalendarViewPlatformWrapperPr
   console.log(teamEventTypeData, "team event data".toLocaleUpperCase());
   console.log("----");
 
-  return <div>This is the calendar view atom!</div>;
+  return (
+    <LargeCalendar
+      extraDays={extraDays}
+      schedule={schedule.data}
+      isLoading={schedule.isPending}
+      event={event}
+    />
+  );
 };
