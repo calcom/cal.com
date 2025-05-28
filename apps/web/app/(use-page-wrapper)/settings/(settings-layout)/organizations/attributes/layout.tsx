c11cf94ae2d1f4074717d568ec80afd95a966059
hbjORbj <sldisek783@gmail.com>
2025-05-28 18:15:51 -0400

    feat: add permission security to attributes pages

---
+++ apps/web/app/(use-page-wrapper)/settings/(settings-layout)/organizations/attributes/layout.tsx
@@ -0,0 +1,19 @@
+import { cookies, headers } from "next/headers";
+
+import { checkAdminOrOwner } from "@calcom/features/auth/lib/checkAdminOrOwner";
+import { getServerSession } from "@calcom/features/auth/lib/getServerSession";
+
+import { buildLegacyRequest } from "@lib/buildLegacyCtx";
+
+const AttributesLayout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await getServerSession({ req: buildLegacyRequest(await headers(), await cookies()) });
+  const isOrgAdminOrOwner = checkAdminOrOwner(session?.user?.org?.role);
+
+  if (!isOrgAdminOrOwner) {
+    redirect("/settings/organizations/profile");
+  }
+
+  return children;
+};
+
+export default AttributesLayout;
