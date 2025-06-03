8b32b8d17fdf8d9489aa008295ac1c929eba25a2
sean-brydon <sean@cal.com>
2025-06-03 10:44:22 +0100

    try export prisma singleton to stop too many clients

--- packages/prisma/e2e.ts
+++ packages/prisma/e2e.ts
@@ -1,3 +1,11 @@
 import { getPrismaClient } from "./store/prismaStore";
 
-export const prisma = getPrismaClient();
+let prisma: ReturnType<typeof getPrismaClient>;
+export const getPrisma = () => {
+  if (!prisma) {
+    prisma = getPrismaClient();
+  }
+  return prisma;
+};
+// Only export one instance
+export { prisma };
