ef5f53255642b60e956738e89f6da5cf74ea4471
Hariom Balhara <hariombalhara@gmail.com>
2025-06-06 15:35:00 +0530

    wip

--- packages/features/calendar-sync/crons/calendar-subscriptions.ts
+++ packages/features/calendar-sync/crons/calendar-subscriptions.ts
@@ -76,10 +76,8 @@ const createOrRenewThirdPartySubscription = async (
 
   log.debug(`Attempting to ${logPrefix} subscription for CalendarSubscription ${subscription.id}`);
 
-  console.log("Calling getCalendar", getCalendar);
   // Create or renew the subscription with the provider (e.g., Google Calendar watch)
   const calendarService = await getCalendar(credentialForCalendarCache);
-  console.log("calendarService1", calendarService);
   if (!calendarService) {
     log.error(
       `Calendar service not found for CalendarSubscription ${subscription.id} (credential ${subscription.credentialId}, externalId ${subscription.externalCalendarId})`
@@ -87,7 +85,6 @@ const createOrRenewThirdPartySubscription = async (
     throw new Error("CalendarService couldn't be initialized");
   }
 
-  console.log("calendarService", calendarService);
   if (!calendarService.subscribeToCalendar) {
     log.error(
       `subscribeToCalendar is not implemented for CalendarSubscription ${subscription.id} (credential ${subscription.credentialId}, externalId ${subscription.externalCalendarId})`
