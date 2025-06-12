008dbdbb51aa662239faa2cdf9ee0000c777a290
Joe Au-Yeung <j.auyeung419@gmail.com>
2025-06-11 23:23:01 -0400

    Create `MaxActiveBookingsPerBookerController` and offer reschedule option

---
+++ packages/features/eventtypes/components/tabs/limits/MaxActiveBookingsPerBookerController.tsx
@@ -0,0 +1,69 @@
+import { Controller, useFormContext } from "react-hook-form";
+
+import type { FormValues } from "@calcom/features/eventtypes/lib/types";
+import { useLocale } from "@calcom/lib/hooks/useLocale";
+import classNames from "@calcom/ui/classNames";
+import { SettingsToggle } from "@calcom/ui/components/form";
+import { TextField, CheckboxField } from "@calcom/ui/components/form";
+
+export default function MaxActiveBookingsPerBookerController() {
+  const { t } = useLocale();
+  const formMethods = useFormContext<FormValues>();
+
+  const maxActiveBookingPerBookerOfferReschedule = formMethods.watch(
+    "maxActiveBookingPerBookerOfferReschedule"
+  );
+  return (
+    <Controller
+      name="maxActiveBookingsPerBooker"
+      render={({ field: { onChange, value } }) => {
+        const isChecked = value && value > 0;
+        return (
+          <SettingsToggle
+            labelClassName={classNames("text-sm")}
+            toggleSwitchAtTheEnd={true}
+            switchContainerClassName={classNames(
+              "border-subtle mt-6 rounded-lg border py-6 px-4 sm:px-6",
+              isChecked && "rounded-b-none"
+            )}
+            childrenClassName={classNames("lg:ml-0")}
+            title={t("booker_booking_limit")}
+            description={t("booker_booking_limit_description")}
+            checked={isChecked}
+            onCheckedChange={(active) => {
+              if (active) {
+                onChange(1);
+              } else {
+                onChange(null);
+              }
+            }}>
+            <div className="border-subtle rounded-b-lg border border-t-0 p-6">
+              <TextField
+                required
+                type="number"
+                value={value}
+                onChange={(e) => {
+                  onChange(Math.max(Number(e.target.value), 1));
+                }}
+                min={1}
+                containerClassName={classNames("max-w-80")}
+                addOnSuffix="bookings"
+                data-testid="booker-booking-limit-input"
+              />
+              <CheckboxField
+                checked={maxActiveBookingPerBookerOfferReschedule}
+                descriptionAsLabel
+                description={t("offer_to_reschedule_last_booking")}
+                onChange={(e) => {
+                  formMethods.setValue("maxActiveBookingPerBookerOfferReschedule", e.target.checked, {
+                    shouldDirty: true,
+                  });
+                }}
+              />
+            </div>
+          </SettingsToggle>
+        );
+      }}
+    />
+  );
+}
