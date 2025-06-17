cc3016433641084aed40f4e25f503f9c57bdfc74
hbjORbj <sldisek783@gmail.com>
2025-06-17 12:05:57 -0400

    wip

---
+++ trigger/example.ts
@@ -0,0 +1,16 @@
+import { logger, task, wait } from "@trigger.dev/sdk/v3";
+
+export const helloWorldTask = task({
+  id: "hello-world",
+  // Set an optional maxDuration to prevent tasks from running indefinitely
+  maxDuration: 300, // Stop executing after 300 secs (5 mins) of compute
+  run: async (payload: any, { ctx }) => {
+    logger.log("Hello, world!", { payload, ctx });
+
+    await wait.for({ seconds: 5 });
+
+    return {
+      message: "Hello, world!",
+    }
+  },
+});
