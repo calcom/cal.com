df7a0b258da08eaa2a6f52799c7cda018435cd8b
Sean Brydon <sean@cal.com>
2025-06-17 08:02:57 +0100

    seperate migraations to batches

--- packages/prisma/migrations/20250522083627_update_memberships_to_new_roles/migration.sql
+++ packages/prisma/migrations/20250522083627_update_memberships_to_new_roles/migration.sql
@@ -143,37 +143,3 @@ CREATE TRIGGER membership_role_change_trigger
     FOR EACH ROW
     EXECUTE FUNCTION update_membership_custom_role();
 
--- -- Update existing memberships to have the correct customRoleId in batches
--- DO $$
--- DECLARE
---     batch_size INTEGER := 5000;
---     max_id INTEGER;
---     current_id INTEGER := 0;
---     affected_rows INTEGER;
--- BEGIN
---     -- Get the maximum id we need to process
---     SELECT COALESCE(MAX(id), 0) INTO max_id FROM "Membership" WHERE "customRoleId" IS NULL;
-
---     -- Process in batches until we've covered all records
---     LOOP
---         UPDATE "Membership"
---         SET "customRoleId" =
---             CASE role
---                 WHEN 'OWNER' THEN 'owner_role'
---                 WHEN 'ADMIN' THEN 'admin_role'
---                 WHEN 'MEMBER' THEN 'member_role'
---             END
---         WHERE id > current_id
---         AND id <= current_id + batch_size
---         AND "customRoleId" IS NULL;
-
---         GET DIAGNOSTICS affected_rows = ROW_COUNT;
-
---         EXIT WHEN current_id >= max_id;
-
---         current_id := current_id + batch_size;
-
---         -- Optional: Add a small delay between batches if needed
---         -- PERFORM pg_sleep(0.1);
---     END LOOP;
--- END $$;
