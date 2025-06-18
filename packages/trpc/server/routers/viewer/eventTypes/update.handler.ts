649a9100cd888f0bda5e91c206c88ea3ef07cffb
Alex van Andel <me@alexvanandel.com>
2025-06-18 13:30:58 +0100

    Create injectable objects for Membership + Schedule (future proofing)

--- packages/trpc/server/routers/viewer/eventTypes/update.handler.ts
+++ packages/trpc/server/routers/viewer/eventTypes/update.handler.ts
@@ -13,6 +13,8 @@ import { validateIntervalLimitOrder } from "@calcom/lib/intervalLimits/validateI
 import logger from "@calcom/lib/logger";
 import { getTranslation } from "@calcom/lib/server/i18n";
 import { CalVideoSettingsRepository } from "@calcom/lib/server/repository/calVideoSettings";
+import { MembershipRepository } from "@calcom/lib/server/repository/membership";
+import { ScheduleRepository } from "@calcom/lib/server/repository/schedule";
 import { validateBookerLayouts } from "@calcom/lib/validateBookerLayouts";
 import type { PrismaClient } from "@calcom/prisma";
 import { WorkflowTriggerEvents } from "@calcom/prisma/client";
@@ -339,34 +341,27 @@ export const updateHandler = async ({ ctx, input }: UpdateOptions) => {
     };
   }
 
+  const membershipRepo = new MembershipRepository(ctx.prisma);
+
   if (restrictionScheduleId) {
     // Verify that the user owns the restriction schedule or is a team member
-    const restrictionSchedule = await ctx.prisma.schedule.findUnique({
-      where: {
-        id: restrictionScheduleId,
-      },
-      select: {
-        userId: true,
-      },
+    const scheduleRepo = new ScheduleRepository(ctx.prisma);
+    const restrictionSchedule = await scheduleRepo.findScheduleByIdForOwnershipCheck({
+      scheduleId: restrictionScheduleId,
     });
-
     // If the user doesn't own the schedule, check if they're a team member
     if (restrictionSchedule?.userId !== ctx.user.id) {
-      if (!teamId) {
+      if (!teamId || !restrictionSchedule) {
         throw new TRPCError({
           code: "UNAUTHORIZED",
           message: "The restriction schedule is not owned by you or your team",
         });
       }
-      const teamMemberCount = await ctx.prisma.membership.count({
-        where: {
-          teamId,
-          userId: restrictionSchedule?.userId,
-          accepted: true,
-        },
+      const hasMembership = await membershipRepo.hasMembership({
+        teamId,
+        userId: restrictionSchedule.userId,
       });
-
-      if (teamMemberCount === 0) {
+      if (!hasMembership) {
         throw new TRPCError({
           code: "UNAUTHORIZED",
           message: "The restriction schedule is not owned by you or your team",
@@ -394,14 +389,7 @@ export const updateHandler = async ({ ctx, input }: UpdateOptions) => {
 
   if (teamId && hosts) {
     // check if all hosts can be assigned (memberships that have accepted invite)
-    const memberships =
-      (await ctx.prisma.membership.findMany({
-        where: {
-          teamId,
-          accepted: true,
-        },
-      })) || [];
-    const teamMemberIds = memberships.map((membership) => membership.userId);
+    const teamMemberIds = await membershipRepo.listAcceptedTeamMemberIds({ teamId });
     // guard against missing IDs, this may mean a member has just been removed
     // or this request was forged.
     // we let this pass through on organization sub-teams
