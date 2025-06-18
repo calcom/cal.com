9a3d351129a2067d05a54e0b3cf2d4ce95646f44
heykandoi <abhikandoi2000@gmail.com>
2025-06-18 18:45:08 +0530

    fix: add api.bodyParser.sizeLimit of 5mb for image uploads

--- apps/web/pages/api/trpc/organizations/[trpc].ts
+++ apps/web/pages/api/trpc/organizations/[trpc].ts
@@ -2,3 +2,11 @@ import { createNextApiHandler } from "@calcom/trpc/server/createNextApiHandler";
 import { viewerOrganizationsRouter } from "@calcom/trpc/server/routers/viewer/organizations/_router";
 
 export default createNextApiHandler(viewerOrganizationsRouter);
+
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "5mb", // default body size limit of 1MB is not enough for images
+    },
+  },
+};
