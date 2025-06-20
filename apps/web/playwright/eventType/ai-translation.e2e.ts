7aef544dd60453c8c00ec16b247f091912a46456
hbjORbj <sldisek783@gmail.com>
2025-06-20 12:04:55 -0400

    simplify

--- apps/web/playwright/eventType/ai-translation.e2e.ts
+++ apps/web/playwright/eventType/ai-translation.e2e.ts
@@ -1,6 +1,5 @@
 import { expect } from "@playwright/test";
 import { loginUser } from "playwright/fixtures/regularBookings";
-import { setupOrgMember, createNewUserEventType } from "playwright/lib/testUtils";
 
 import { MembershipRole } from "@calcom/prisma/enums";
 
@@ -42,31 +41,5 @@ test.describe("AI Translation - Event Type", () => {
     await page.goto("/event-types");
     await bookingPage.goToEventType("30 min");
     expect(await bookingPage.getAITranslationToggleDisabled()).toBe(false);
-  });
-});
-
-test.describe("AI Translation - Booking Page", () => {
-  test.use({
-    locale: "ko-KR",
-  });
-  test("Enable the AI translation and test in browser in Korean (ko-KR)", async ({
-    page,
-    users,
-    bookingPage,
-  }) => {
-    const { orgMember, org } = await setupOrgMember(users);
-
-    await page.goto("/event-types");
-    await createNewUserEventType(page, {
-      eventTitle: "5 min",
-      username: orgMember.username ?? undefined,
-    });
-    expect(await bookingPage.getAITranslationToggleDisabled()).toBe(false);
-    await bookingPage.toggleAITranslation();
-    await bookingPage.updateEventTypeDescription("A quick 5 minute chat.");
-    await bookingPage.updateEventType();
-    await orgMember.logout(); // logging out because user locale overrides the browser locale otherwise
-    await page.goto(`/${orgMember.username}/5-min`);
-    await expect(page.locator('[data-testid="event-meta-description"] p')).toHaveText("빠른 5분 대화.");
   });
 });
