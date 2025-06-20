5b5cc6034f197cdaac67fd90d7d6ce60c0501b9c
unknown <adhabal2002@gmail.com>
2025-06-20 10:46:56 +0530

    update

--- packages/lib/hooks/useBookerUrl.ts
+++ packages/lib/hooks/useBookerUrl.ts
@@ -1,12 +1,12 @@
 import { useOrgBranding } from "@calcom/features/ee/organizations/context/provider";
 import { WEBAPP_URL, WEBSITE_URL } from "@calcom/lib/constants";
 
-export const useBookerUrl = (options?: { excludeWebsiteUrl?: boolean }) => {
+export const useBookerUrl = () => {
   const orgBranding = useOrgBranding();
-
-  if (options?.excludeWebsiteUrl) {
-    return orgBranding?.fullDomain ?? WEBAPP_URL;
-  }
-
   return orgBranding?.fullDomain ?? WEBSITE_URL ?? WEBAPP_URL;
+};
+
+export const useEmbedBookerUrl = () => {
+  const orgBranding = useOrgBranding();
+  return orgBranding?.fullDomain ?? WEBAPP_URL;
 };
