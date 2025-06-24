56ebac3c98d3dc19a8b2246043999235341f13f8
romit <romitgabani@icloud.com>
2025-06-24 15:24:17 +0530

    fixed team event form ui

--- packages/features/eventtypes/components/CreateEventTypeDialog.tsx
+++ packages/features/eventtypes/components/CreateEventTypeDialog.tsx
@@ -116,6 +116,7 @@ export default function CreateEventTypeDialog({
       name="new"
       clearQueryParamsOnClose={["eventPage", "type", "description", "title", "length", "slug", "locations"]}>
       <DialogContent
+        className="overflow-hidden"
         type="creation"
         enableOverflow
         title={teamId ? t("add_new_team_event_type") : t("add_new_event_type")}
