a1d3c6b7f773d546769d901598a872a325aae7d2
Udit Takkar <udit222001@gmail.com>
2025-06-26 23:08:43 +0530

    perf: optimize listWithTeamHandler query

--- packages/trpc/server/routers/viewer/eventTypes/listWithTeam.handler.ts
+++ packages/trpc/server/routers/viewer/eventTypes/listWithTeam.handler.ts
@@ -9,20 +9,19 @@ type ListWithTeamOptions = {
 };
 
 export const listWithTeamHandler = async ({ ctx }: ListWithTeamOptions) => {
+  const userTeamIds = await prisma.membership.findMany({
+    where: {
+      userId: ctx.user.id,
+      accepted: true,
+    },
+    select: {
+      teamId: true,
+    },
+  });
+
   return await prisma.eventType.findMany({
     where: {
-      OR: [
-        { userId: ctx.user.id },
-        {
-          team: {
-            members: {
-              some: {
-                userId: ctx.user.id,
-              },
-            },
-          },
-        },
-      ],
+      OR: [{ userId: ctx.user.id }, { teamId: { in: userTeamIds.map((membership) => membership.teamId) } }],
     },
     select: {
       id: true,
