a5d9dfea4f18a9158d83615019482039f04929d3
Anik Dhabal Babu <adhabal2002@gmail.com>
2025-07-02 06:05:25 +0000

    add commnet

--- packages/features/bookings/lib/handleCancelBooking.ts
+++ packages/features/bookings/lib/handleCancelBooking.ts
@@ -89,6 +89,12 @@ async function handler(input: CancelBookingInput) {
     arePlatformEmailsEnabled,
   } = input;
 
+  /**
+   * Important: We prevent cancelling an already cancelled booking.
+   * A booking could have been CANCELLED due to a reschedule,
+   * in which case we simply update the existing calendar event and meeting.
+   * We want to avoid deleting them by a subsequent cancellation attempt.
+   */
   if (bookingToDelete.status === BookingStatus.CANCELLED) {
     throw new HttpError({
       statusCode: 400,
