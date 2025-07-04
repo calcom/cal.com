a702fc68339a46fce8c8c1224df7d9819085089e
CarinaWolli <wollencarina@gmail.com>
2025-07-04 10:18:40 +0200

    clean up migrations

--- packages/prisma/migrations/20250704081718_add_host_groups/migration.sql
+++ packages/prisma/migrations/20250704081718_add_host_groups/migration.sql
@@ -1,9 +1,9 @@
 -- AlterTable
-ALTER TABLE "Host" ADD COLUMN     "hostGroupId" INTEGER;
+ALTER TABLE "Host" ADD COLUMN     "groupId" TEXT;
 
 -- CreateTable
 CREATE TABLE "HostGroup" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "eventTypeId" INTEGER,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -19,7 +19,7 @@ CREATE INDEX "HostGroup_name_idx" ON "HostGroup"("name");
 CREATE INDEX "HostGroup_eventTypeId_idx" ON "HostGroup"("eventTypeId");
 
 -- AddForeignKey
-ALTER TABLE "Host" ADD CONSTRAINT "Host_hostGroupId_fkey" FOREIGN KEY ("hostGroupId") REFERENCES "HostGroup"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Host" ADD CONSTRAINT "Host_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "HostGroup"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "HostGroup" ADD CONSTRAINT "HostGroup_eventTypeId_fkey" FOREIGN KEY ("eventTypeId") REFERENCES "EventType"("id") ON DELETE CASCADE ON UPDATE CASCADE;
