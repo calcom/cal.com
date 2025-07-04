9ae4e1b83a1e17b76c46ff4ab65a185bb62de9cc
Devanshu Sharma <devanshusharma658@gmail.com>
2025-07-04 17:38:59 +0530

    feat: filter team members by MEMBER role in EventTeamAssignmentTabPlatformWrapper

--- packages/platform/atoms/event-types/wrappers/EventTeamAssignmentTabPlatformWrapper.tsx
+++ packages/platform/atoms/event-types/wrappers/EventTeamAssignmentTabPlatformWrapper.tsx
@@ -2,12 +2,17 @@ import {
   EventTeamAssignmentTab,
   type EventTeamAssignmentTabBaseProps,
 } from "@calcom/features/eventtypes/components/tabs/assignment/EventTeamAssignmentTab";
+import { MembershipRole } from "@calcom/prisma/enums";
 
 const EventTeamAssignmentTabPlatformWrapper = (
   props: Omit<EventTeamAssignmentTabBaseProps, "isSegmentApplicable">
 ) => {
-  // todo: implement attributes for platform orgs for segment
-  return <EventTeamAssignmentTab {...props} isSegmentApplicable={false} />;
+  // Filter team members to only include those with MEMBER role
+  const filteredTeamMembers = props.teamMembers.filter(
+    (member) => member.membership === MembershipRole.MEMBER
+  );
+
+  return <EventTeamAssignmentTab {...props} teamMembers={filteredTeamMembers} isSegmentApplicable={false} />;
 };
 
 export default EventTeamAssignmentTabPlatformWrapper;
