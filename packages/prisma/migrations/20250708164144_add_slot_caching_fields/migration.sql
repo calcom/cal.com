0f753a93f01e66c5b4c321cd9104aae5df35060b
Keith Williams <keithwillcode@gmail.com>
2025-07-08 15:40:12 -0300

    Delete packages/prisma/migrations/20250708164144_add_slot_caching_fields/migration.sql

--- packages/prisma/migrations/20250708164144_add_slot_caching_fields/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "CalendarCache" ADD COLUMN     "availableCount" INTEGER,
-ADD COLUMN     "cacheType" TEXT DEFAULT 'calendar',
-ADD COLUMN     "eventTypeId" INTEGER,
-ADD COLUMN     "totalSlots" INTEGER;
-
--- CreateIndex
-CREATE INDEX "CalendarCache_eventTypeId_cacheType_idx" ON "CalendarCache"("eventTypeId", "cacheType");
