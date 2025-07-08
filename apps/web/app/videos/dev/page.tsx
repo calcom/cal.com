2c3bc16d8bfb46136a46ab051a5a1b46974e5013
devkumar4 <dev796699@gmail.com>
2025-07-08 07:14:28 +0000

    feat: redesign LogInOverlay modal UI (#21754)

---
+++ apps/web/app/videos/dev/page.tsx
@@ -0,0 +1,11 @@
+// apps/web/app/videos/dev/page.tsx
+
+"use client";
+
+import { DevLoginOverlayUI } from "~/videos/views/videos-single-view";
+
+
+
+export default function DevLoginOverlay() {
+  return <DevLoginOverlayUI/>;
+}
