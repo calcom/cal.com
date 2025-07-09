5c4b8202884e098f56e0db65ae1aca5e5305549e
cal.com <morgan@cal.com>
2025-07-09 18:34:23 +0300

    remove useless comment from prisma module

--- packages/prisma/prisma.module.ts
+++ packages/prisma/prisma.module.ts
@@ -6,6 +6,6 @@ import { prisma, readonlyPrisma } from "./index";
 
 export const prismaModule = createModule();
 
-prismaModule.bind(DI_TOKENS.PRISMA_CLIENT).toFactory(() => prisma, "singleton"); // Use toFactory for safety as discussed
+prismaModule.bind(DI_TOKENS.PRISMA_CLIENT).toFactory(() => prisma, "singleton");
 
 prismaModule.bind(DI_TOKENS.READ_ONLY_PRISMA_CLIENT).toFactory(() => readonlyPrisma, "singleton");
