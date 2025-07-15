4b3c4c24a342b1c2aedaed71a296c1ce23d29e2f
Alex van Andel <me@alexvanandel.com>
2025-07-15 02:00:21 +0100

    Extract routing form response parser to seperate util

--- packages/lib/server/service/routingForm/RoutingFormResponseDataFactory.ts
+++ packages/lib/server/service/routingForm/RoutingFormResponseDataFactory.ts
@@ -1,9 +1,7 @@
-import { zodNonRouterField } from "@calcom/app-store/routing-forms/zod";
-import { routingFormResponseInDbSchema } from "@calcom/app-store/routing-forms/zod";
 import type logger from "@calcom/lib/logger";
 
 import type { RoutingFormResponseRepositoryInterface } from "../../repository/RoutingFormResponseRepository.interface";
-import type { RoutingFormResponseData } from "./responseData/types";
+import { parseRoutingFormResponse } from "./responseData/parseRoutingFormResponse";
 
 interface Dependencies {
   logger: typeof logger;
@@ -13,19 +11,19 @@ interface Dependencies {
 export class RoutingFormResponseDataFactory {
   constructor(private readonly deps: Dependencies) {}
 
-  async createWithBookingId(bookingId: string) {
+  async createWithBookingUid(bookingUid: string) {
     const log = this.deps.logger.getSubLogger({
       prefix: ["[routingFormFieldService]", { bookingUid }],
     });
 
-    const formResponse = await this.deps.routingFormResponseRepo.findByBookingUid(bookingId);
+    const formResponse = await this.deps.routingFormResponseRepo.findByBookingUid(bookingUid);
 
     if (!formResponse) {
       log.error("Form response not found");
       throw new Error("Form response not found");
     }
 
-    return this.parseResponseData(formResponse.response);
+    return parseRoutingFormResponse(formResponse.response, formResponse.form.fields);
   }
 
   async createWithResponseId(responseId: number) {
@@ -33,32 +31,13 @@ export class RoutingFormResponseDataFactory {
       prefix: ["[routingFormFieldService]", { responseId }],
     });
 
-    const formResponse = await this.deps.routingFormResponseRepo.findByResponseId(responseId);
+    const formResponse = await this.deps.routingFormResponseRepo.findById(responseId);
 
     if (!formResponse) {
       log.error("Form response not found");
       throw new Error("Form response not found");
     }
 
-    return this.parseResponseData(formResponse.response);
-  }
-
-  private parseResponseData(rawResponse: string): RoutingFormResponseData {
-    const response = routingFormResponseInDbSchema.safeParse(rawResponse);
-    if (!response.success) {
-      log.error("Form response not found");
-      throw new Error("Form response not found");
-    }
-
-    const fields = zodNonRouterField.array().safeParse(formResponse.form.fields);
-    if (!fields.success) {
-      log.error("Form fields not found");
-      throw new Error("Form fields not found");
-    }
-
-    return {
-      response: response.data,
-      fields: fields.data,
-    } satisfies RoutingFormResponseData;
+    return parseRoutingFormResponse(formResponse.response, formResponse.form.fields);
   }
 }
