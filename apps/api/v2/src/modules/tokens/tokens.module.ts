56adf7a41669601da10330d62a6a7c9de69dd106
supalarry <laurisskraucis@gmail.com>
2025-07-17 12:09:14 +0200

    fix: v2 ignore permissions check for 3rd party oauth

--- apps/api/v2/src/modules/tokens/tokens.module.ts
+++ apps/api/v2/src/modules/tokens/tokens.module.ts
@@ -1,10 +1,11 @@
 import { PrismaModule } from "@/modules/prisma/prisma.module";
 import { TokensRepository } from "@/modules/tokens/tokens.repository";
+import { TokensService } from "@/modules/tokens/tokens.service";
 import { Module } from "@nestjs/common";
 
 @Module({
   imports: [PrismaModule],
-  providers: [TokensRepository],
-  exports: [TokensRepository],
+  providers: [TokensRepository, TokensService],
+  exports: [TokensRepository, TokensService],
 })
 export class TokensModule {}
