c92907f8faa1c9116bbf7f913228ebc29fef4112
Volnei Munhoz <volnei.munhoz@gmail.com>
2025-07-19 15:24:27 -0300

    Remove unused files

--- packages/ui/components/editor/plugins/EditablePlugin.tsx
+++
@@ -1,7 +0,0 @@
-import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-
-export default function EditablePlugin({ editable }: { editable: boolean }) {
-  const [editor] = useLexicalComposerContext();
-  editor.setEditable(editable);
-  return null;
-}
