9c78e8c3ee931e57530cdf1746f0110c084be1f2
Volnei Munhoz <volnei.munhoz@gmail.com>
2025-07-20 18:59:45 -0300

    fix ts types for disableRescheduling and disableCancelling

--- packages/features/CalendarEventBuilder.ts
+++ packages/features/CalendarEventBuilder.ts
@@ -53,8 +53,8 @@ export class CalendarEventBuilder {
     seatsShowAttendees?: boolean | null;
     seatsShowAvailabilityCount?: boolean | null;
     customReplyToEmail?: string | null;
-    disableRescheduling?: boolean | null;
-    disableCancelling?: boolean | null;
+    disableRescheduling?: boolean;
+    disableCancelling?: boolean;
   }) {
     this.event = {
       ...this.event,
@@ -70,8 +70,8 @@ export class CalendarEventBuilder {
       seatsShowAttendees: eventType.seatsPerTimeSlot ? eventType.seatsShowAttendees : true,
       seatsShowAvailabilityCount: eventType.seatsPerTimeSlot ? eventType.seatsShowAvailabilityCount : true,
       customReplyToEmail: eventType.customReplyToEmail,
-      disableRescheduling: eventType.disableRescheduling,
-      disableCancelling: eventType.disableCancelling,
+      disableRescheduling: eventType.disableRescheduling || false, // default false
+      disableCancelling: eventType.disableCancelling || false, // default false
     };
     return this;
   }
