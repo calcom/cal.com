689cc2ce9571267d3f2b7f0071aa5615ba4c46f7
Kartik Saini <41051387+kart1ka@users.noreply.github.com>
2025-07-21 08:56:10 +0530

    chore

--- packages/emails/src/templates/OrganizerAttendeeCancelledSeatEmail.tsx
+++ packages/emails/src/templates/OrganizerAttendeeCancelledSeatEmail.tsx
@@ -2,15 +2,13 @@ import { OrganizerScheduledEmail } from "./OrganizerScheduledEmail";
 
 export const OrganizerAttendeeCancelledSeatEmail = (
   props: React.ComponentProps<typeof OrganizerScheduledEmail>
-) => {
-  return (
-    <OrganizerScheduledEmail
-      title="attendee_no_longer_attending"
-      headerType="xCircle"
-      subject="event_cancelled_subject"
-      callToAction={null}
-      attendeeCancelled={!props.isCancelledByHost}
-      {...props}
-    />
-  );
-};
+) => (
+  <OrganizerScheduledEmail
+    title="attendee_no_longer_attending"
+    headerType="xCircle"
+    subject="event_cancelled_subject"
+    callToAction={null}
+    attendeeCancelled={!props.isCancelledByHost}
+    {...props}
+  />
+);
