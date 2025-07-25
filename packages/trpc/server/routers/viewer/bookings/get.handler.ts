5dd25496aca1d57c9cac59bf5a2c12f173d9e143
unknown <adhabal2002@gmail.com>
2025-07-25 10:58:02 +0530

    update

--- packages/trpc/server/routers/viewer/bookings/get.handler.ts
+++ packages/trpc/server/routers/viewer/bookings/get.handler.ts
@@ -503,6 +503,20 @@ export async function getBookings({
                     "varchar" // Or 'text' - use the actual SQL data type
                   )
                   .as("schedulingType"),
+                jsonArrayFrom(
+                  eb
+                    .selectFrom("Host")
+                    .select((eb) => [
+                      "Host.userId",
+                      jsonObjectFrom(
+                        eb
+                          .selectFrom("users")
+                          .select(["users.id", "users.email"])
+                          .whereRef("Host.userId", "=", "users.id")
+                      ).as("user"),
+                    ])
+                    .whereRef("Host.eventTypeId", "=", "EventType.id")
+                ).as("hosts"),
                 "EventType.length",
                 jsonObjectFrom(
                   eb
@@ -640,13 +654,28 @@ export async function getBookings({
     })
   );
 
+  const checkIfUserIsHost = (userId: number, booking: (typeof plainBookings)[number]) => {
+    if (booking.user?.id === userId) {
+      return true;
+    }
+
+    if (!booking.eventType?.hosts || booking.eventType.hosts.length === 0) {
+      return false;
+    }
+
+    const attendeeEmails = new Set(booking.attendees.map((attendee) => attendee.email));
+
+    return booking.eventType.hosts.some(({ user: hostUser }) => {
+      return hostUser?.id === userId && attendeeEmails.has(hostUser.email);
+    });
+  };
   const bookings = await Promise.all(
     plainBookings.map(async (booking) => {
       // If seats are enabled, the event is not set to show attendees, and the current user is not the host, filter out attendees who are not the current user
       if (
         booking.seatsReferences.length &&
         !booking.eventType?.seatsShowAttendees &&
-        user.id !== booking.user?.id
+        !checkIfUserIsHost(user.id, booking)
       ) {
         booking.attendees = booking.attendees.filter((attendee) => attendee.email === user.email);
       }
