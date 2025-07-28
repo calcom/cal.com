8d0b64e96e2ca74a02373360c2ca7a5bee59ae15
CarinaWolli <wollencarina@gmail.com>
2025-07-28 12:25:19 +0200

    fix datePicker tests

--- packages/features/calendars/__tests__/DatePicker.test.tsx
+++ packages/features/calendars/__tests__/DatePicker.test.tsx
@@ -1,4 +1,6 @@
+import { TooltipProvider } from "@radix-ui/react-tooltip";
 import { render } from "@testing-library/react";
+import React from "react";
 
 import dayjs from "@calcom/dayjs";
 import { PeriodType } from "@calcom/prisma/enums";
@@ -13,18 +15,20 @@ describe("Tests for DatePicker Component", () => {
   test("Should render correctly with default date", async () => {
     const testDate = dayjs("2024-02-20");
     const { getByTestId } = render(
-      <DatePicker
-        onChange={noop}
-        browsingDate={testDate}
-        locale="en"
-        periodData={{
-          periodType: PeriodType.UNLIMITED,
-          periodDays: null,
-          periodCountCalendarDays: false,
-          periodStartDate: null,
-          periodEndDate: null,
-        }}
-      />
+      <TooltipProvider>
+        <DatePicker
+          onChange={noop}
+          browsingDate={testDate}
+          locale="en"
+          periodData={{
+            periodType: PeriodType.UNLIMITED,
+            periodDays: null,
+            periodCountCalendarDays: false,
+            periodStartDate: null,
+            periodEndDate: null,
+          }}
+        />
+      </TooltipProvider>
     );
 
     const selectedMonthLabel = getByTestId("selected-month-label");
@@ -35,7 +39,9 @@ describe("Tests for DatePicker Component", () => {
     const testDate = dayjs("2024-02-20");
     const minDate = dayjs("2025-02-10");
     const { getByTestId } = render(
-      <DatePicker onChange={noop} browsingDate={testDate} minDate={minDate.toDate()} locale="en" />
+      <TooltipProvider>
+        <DatePicker onChange={noop} browsingDate={testDate} minDate={minDate.toDate()} locale="en" />
+      </TooltipProvider>
     );
 
     const selectedMonthLabel = getByTestId("selected-month-label");
@@ -46,7 +52,9 @@ describe("Tests for DatePicker Component", () => {
     const testDate = dayjs("2025-03-20");
     const minDate = dayjs("2025-02-10");
     const { getByTestId } = render(
-      <DatePicker onChange={noop} browsingDate={testDate} minDate={minDate} locale="en" />
+      <TooltipProvider>
+        <DatePicker onChange={noop} browsingDate={testDate} minDate={minDate.toDate()} locale="en" />
+      </TooltipProvider>
     );
 
     const selectedMonthLabel = getByTestId("selected-month-label");
