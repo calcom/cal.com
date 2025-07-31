9206cb2e5b48912bba3e80c604d8370d589b81d6
Alex van Andel <me@alexvanandel.com>
2025-08-01 00:16:34 +0100

    chore: Add node_modules/dist to ignore + convert config to .ts

---
+++ apps/api/v2/jest.config.ts
@@ -0,0 +1,20 @@
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const config: JestConfigWithTsJest = {
+  moduleFileExtensions: ["json", "ts"], // removed 'js'
+  rootDir: ".",
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+    "test/(.*)": "<rootDir>/test/$1",
+  },
+  testEnvironment: "node",
+  testRegex: ".*\\.spec\\.ts$",
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  setupFiles: ["<rootDir>/test/setEnvVars.ts"],
+  testPathIgnorePatterns: ["/dist/", "/node_modules/"],
+  transformIgnorePatterns: ["/dist/", "/node_modules/"],
+};
+
+export default config;
