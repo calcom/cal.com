78cae7aebc6cf21e9e243416dd5bbbef34dc3ebc
Omar López <zomars@me.com>
2025-07-30 22:08:39 -0700

    Remove calendar DI modules
    Deleted dependency injection modules for calendar event and subscription services and repositories. This likely reflects a refactor or removal of related features or a change in DI strategy.

--- packages/lib/di/modules/calendarSubscription.ts
+++
@@ -1,9 +0,0 @@
-import { createModule } from "@evyweb/ioctopus";
-
-import { CalendarSubscriptionRepository } from "@calcom/features/calendar-cache-sql/CalendarSubscriptionRepository";
-import { DI_TOKENS } from "@calcom/lib/di/tokens";
-
-export const calendarSubscriptionRepositoryModule = createModule();
-calendarSubscriptionRepositoryModule
-  .bind(DI_TOKENS.CALENDAR_SUBSCRIPTION_REPOSITORY)
-  .toClass(CalendarSubscriptionRepository, [DI_TOKENS.PRISMA_CLIENT]);
