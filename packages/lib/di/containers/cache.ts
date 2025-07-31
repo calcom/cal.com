c9bf853a7a6b412f0121fb7d5cc557696f5892d1
cal.com <morgan@cal.com>
2025-07-31 13:31:41 +0300

    chore: bump platform libs

--- packages/lib/di/containers/cache.ts
+++ packages/lib/di/containers/cache.ts
@@ -4,9 +4,11 @@ import { DI_TOKENS } from "@calcom/lib/di/tokens";
 
 import type { CacheService } from "../../../features/calendar-cache/lib/getShouldServeCache";
 import { cacheModule } from "../modules/cache";
+import { featuresRepositoryModule } from "../modules/features";
 
 const container = createContainer();
 container.load(DI_TOKENS.CACHE_SERVICE_MODULE, cacheModule);
+container.load(DI_TOKENS.FEATURES_REPOSITORY_MODULE, featuresRepositoryModule);
 
 export function getCacheService() {
   return container.get<CacheService>(DI_TOKENS.CACHE_SERVICE);
