4155cad1c4dd86ba51017288ba2d5c075dde6d3a
arjun3492 <arjun.kohli.330@gmail.com>
2025-08-05 00:18:52 +0530

    added necessary components for ui

--- packages/calid/hooks/use-toast.tsx
+++ packages/calid/hooks/use-toast.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import type { ToastActionElement, ToastProps } from "../ui/components/toast/toast";
+import type { ToastActionElement, ToastProps } from "../ui/components/toast";
 
 const TOAST_LIMIT = 1;
 const TOAST_REMOVE_DELAY = 1000000;
