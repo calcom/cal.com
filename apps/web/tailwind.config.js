fcccfd15f2e8c7aa9700df4f54fe57cfe20fb8d0
Bandhan Majumder <bandhanmajumder16@gmail.com>
2025-08-07 18:58:55 +0530

    move plainChat and contactForm from lib to components

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -2,10 +2,6 @@ const base = require("@calcom/config/tailwind-preset");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   ...base,
-  content: [
-    ...base.content,
-    "../../packages/app-store/routing-forms/**/*.{js,ts,jsx,tsx}",
-    "../../apps/web/lib/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: [...base.content, "../../packages/app-store/routing-forms/**/*.{js,ts,jsx,tsx}"],
   plugins: [...base.plugins, require("tailwindcss-animate")],
 };
