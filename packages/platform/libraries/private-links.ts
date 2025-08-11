955a0e40281272102390f440b147909073fc9cee
Syed Ali Shahbaz <alishahbaz7@gmail.com>
2025-08-11 06:31:17 +0400

    remove unnecessary comments

--- packages/platform/libraries/private-links.ts
+++ packages/platform/libraries/private-links.ts
@@ -1,5 +1,2 @@
-// Thin barrel for private-links utils used by API v2
-// Expose generateHashedLink and isLinkExpired from core lib under a stable platform-libraries import path
 export { generateHashedLink } from "@calcom/lib/generateHashedLink";
 export { isLinkExpired } from "@calcom/lib/hashedLinksUtils";
-
