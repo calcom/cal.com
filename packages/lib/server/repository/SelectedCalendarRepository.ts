2ecef2e63549acfa9c9508f3157ef97b42c71f13
Omar López <zomars@me.com>
2025-08-11 18:23:09 -0700

    Update SelectedCalendarRepository.ts

--- packages/lib/server/repository/SelectedCalendarRepository.ts
+++ packages/lib/server/repository/SelectedCalendarRepository.ts
@@ -4,11 +4,17 @@ import { SelectedCalendarRepository as StaticSelectedCalendarRepository } from "
 
 export interface ISelectedCalendarRepository {
   findFirst(args: { where: Prisma.SelectedCalendarWhereInput }): Promise<{ id: string } | null>;
+  findMany(args: { where: Prisma.SelectedCalendarWhereInput }): Promise<Array<{ id: string }>>;
 }
 
 export class SelectedCalendarRepository implements ISelectedCalendarRepository {
   async findFirst(args: { where: Prisma.SelectedCalendarWhereInput }): Promise<{ id: string } | null> {
     const result = await StaticSelectedCalendarRepository.findFirst(args);
     return result ? { id: result.id } : null;
+  }
+
+  async findMany(args: { where: Prisma.SelectedCalendarWhereInput }): Promise<Array<{ id: string }>> {
+    const results = await StaticSelectedCalendarRepository.findMany(args);
+    return results.map((r) => ({ id: r.id }));
   }
 }
