5ea5b485e269d39f225eb0b203eca5b7a5842349
Syed Ali Shahbaz <alishahbaz7@gmail.com>
2025-08-13 08:09:05 +0400

    remove unnecessary comments

--- apps/api/v2/src/ee/event-types-private-links/event-types-private-links.module.ts
+++ apps/api/v2/src/ee/event-types-private-links/event-types-private-links.module.ts
@@ -1,16 +1,14 @@
-import { Module } from "@nestjs/common";
-import { TokensModule } from "@/modules/tokens/tokens.module";
-import { PrismaModule } from "@/modules/prisma/prisma.module";
 import { EventTypesModule_2024_06_14 } from "@/ee/event-types/event-types_2024_06_14/event-types.module";
-// org-scoped dependencies removed; org controller will live in OrganizationsModule
 import { EventTypeOwnershipGuard } from "@/modules/event-types/guards/event-type-ownership.guard";
+import { PrismaModule } from "@/modules/prisma/prisma.module";
+import { TokensModule } from "@/modules/tokens/tokens.module";
+import { Module } from "@nestjs/common";
 
 import { EventTypesPrivateLinksController } from "./controllers/event-types-private-links.controller";
-// org/team-scoped controller is registered in OrganizationsModule
+import { PrivateLinksRepository } from "./private-links.repository";
 import { PrivateLinksInputService } from "./services/private-links-input.service";
 import { PrivateLinksOutputService } from "./services/private-links-output.service";
 import { PrivateLinksService } from "./services/private-links.service";
-import { PrivateLinksRepository } from "./private-links.repository";
 
 @Module({
   imports: [TokensModule, PrismaModule, EventTypesModule_2024_06_14],
@@ -25,5 +23,3 @@ import { PrivateLinksRepository } from "./private-links.repository";
   exports: [PrivateLinksService],
 })
 export class EventTypesPrivateLinksModule {}
-
-
