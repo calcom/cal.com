b1c2a9bd00e0ef6811f603d09bbc2ce418643bbc
hbjORbj <sldisek783@gmail.com>
2025-08-19 21:39:19 +0900

    add comment

--- apps/web/app/api/social/og/image/route.tsx
+++ apps/web/app/api/social/og/image/route.tsx
@@ -7,7 +7,7 @@ import { Meeting, App, Generic } from "@calcom/lib/OgImages";
 import { WEBAPP_URL } from "@calcom/lib/constants";
 
 export const runtime = "edge";
-export const revalidate = false;
+export const revalidate = false; // this results in indefinite caching, meaning that a new deployment will not invalidate the cache
 
 const meetingSchema = z.object({
   imageType: z.literal("meeting"),
