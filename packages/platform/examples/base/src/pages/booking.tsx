4a0ddb6deec67643019a19e949dc4816e19c07ff
Somay Chauhan <somaychauhan98@gmail.com>
2025-08-20 15:39:29 +0530

    udpate

--- packages/platform/examples/base/src/pages/booking.tsx
+++ packages/platform/examples/base/src/pages/booking.tsx
@@ -137,7 +137,7 @@ export default function Bookings(props: { calUsername: string; calEmail: string 
               hostsLimit={3}
               allowUpdatingUrlParams={true}
               silentlyHandleCalendarFailures={false}
-              hideEventMeta={true}
+              // hideEventMetadata={true}
             />
           </>
         )}
