2e41a8fb597ca7ad4874011c3093d8c889aff3b6
Alex van Andel <me@alexvanandel.com>
2025-08-21 13:52:14 +0100

    wip getSchedule call fixups

--- packages/features/schedules/lib/use-schedule/useSchedule.ts
+++ packages/features/schedules/lib/use-schedule/useSchedule.ts
@@ -132,15 +132,6 @@ export const useSchedule = ({
     eventTypeId: eventId ?? undefined,
   });
 
-  const generateHash = (string) => {
-    let hash = 0;
-    for (const char of string) {
-      hash = (hash << 5) - hash + char.charCodeAt(0);
-      hash |= 0; // Constrain to 32bit integer
-    }
-    return hash;
-  };
-
   const schedule = trpc.viewer.slots.getSchedule.useQuery(input, {
     ...options,
     // Only enable if we're not using API V2
