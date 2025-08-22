f59575fb06d823c18ac02e92afa0d808412fc9f9
Joe Au-Yeung <j.auyeung419@gmail.com>
2025-08-22 12:48:41 -0400

    Type fies

--- packages/app-store/caldavcalendar/lib/CalendarService.ts
+++ packages/app-store/caldavcalendar/lib/CalendarService.ts
@@ -1,4 +1,4 @@
-import { CalendarService } from "@calcom/lib/CalendarService";
+import CalendarService from "@calcom/lib/CalendarService";
 import type { CredentialPayload } from "@calcom/types/Credential";
 
 export default class CalDavCalendarService extends CalendarService {
