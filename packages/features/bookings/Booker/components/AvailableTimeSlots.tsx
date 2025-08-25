5ef14567115cf482d009cbf51445bace931f392a
Alex van Andel <me@alexvanandel.com>
2025-08-25 01:23:08 +0100

    Fix missing import

--- packages/features/bookings/Booker/components/AvailableTimeSlots.tsx
+++ packages/features/bookings/Booker/components/AvailableTimeSlots.tsx
@@ -1,3 +1,4 @@
+import type { UseScheduleForEventReturnType } from "bookings/Booker/hooks/useScheduleForEvent";
 import { useCallback, useMemo, useRef } from "react";
 
 import dayjs from "@calcom/dayjs";
@@ -14,6 +15,7 @@ import { BookerLayouts } from "@calcom/prisma/zod-utils";
 import classNames from "@calcom/ui/classNames";
 
 import { AvailableTimesHeader } from "../../components/AvailableTimesHeader";
+import { getQueryParam } from "../utils/query-param";
 
 type AvailableTimeSlotsProps = {
   extraDays?: number;
