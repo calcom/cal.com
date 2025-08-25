4a12107718254f0c1e61bac70c80ffb3d2b4b3fa
Anik Dhabal Babu <adhabal2002@gmail.com>
2025-08-25 13:53:15 +0000

    restrict write operstion only

--- packages/prisma/zod-utils.ts
+++ packages/prisma/zod-utils.ts
@@ -372,11 +372,29 @@ export enum BillingPeriod {
   ANNUALLY = "ANNUALLY",
 }
 
-export const teamMetadataSchema = z
-  .object({
-    defaultConferencingApp: schemaDefaultConferencingApp.optional(),
-    requestedSlug: z.string().or(z.null()),
-    paymentId: z.string(),
+const baseTeamMetadataSchema = z.object({
+  defaultConferencingApp: schemaDefaultConferencingApp.optional(),
+  requestedSlug: z.string().or(z.null()),
+  paymentId: z.string(),
+  subscriptionId: z.string().nullable(),
+  subscriptionItemId: z.string().nullable(),
+  orgSeats: z.number().nullable(),
+  orgPricePerSeat: z.number().nullable(),
+  migratedToOrgFrom: z
+    .object({
+      teamSlug: z.string().or(z.null()).optional(),
+      lastMigrationTime: z.string().optional(),
+      reverted: z.boolean().optional(),
+      lastRevertTime: z.string().optional(),
+    })
+    .optional(),
+  billingPeriod: z.nativeEnum(BillingPeriod).optional(),
+});
+
+export const teamMetadataSchema = baseTeamMetadataSchema.partial().nullable();
+
+export const teamMetadataStrictSchema = baseTeamMetadataSchema
+  .extend({
     subscriptionId: z
       .string()
       .refine((val) => val.startsWith("sub_"), {
@@ -389,17 +407,6 @@ export const teamMetadataSchema = z
         message: "subscriptionItemId must start with 'si_'",
       })
       .nullable(),
-    orgSeats: z.number().nullable(),
-    orgPricePerSeat: z.number().nullable(),
-    migratedToOrgFrom: z
-      .object({
-        teamSlug: z.string().or(z.null()).optional(),
-        lastMigrationTime: z.string().optional(),
-        reverted: z.boolean().optional(),
-        lastRevertTime: z.string().optional(),
-      })
-      .optional(),
-    billingPeriod: z.nativeEnum(BillingPeriod).optional(),
   })
   .partial()
   .nullable();
