396c447c66ea7249bcc240e3835a02190452a56e
Joe Au-Yeung <j.auyeung419@gmail.com>
2025-08-26 13:22:04 -0400

    Refactor `setDefaultConferencingApp` to use metadata from DB

--- packages/app-store/_utils/setDefaultConferencingApp.ts
+++ packages/app-store/_utils/setDefaultConferencingApp.ts
@@ -1,57 +1,60 @@
 import type { LocationObject } from "@calcom/app-store/locations";
-import { getAppFromSlug } from "@calcom/app-store/utils";
 import { getBulkUserEventTypes } from "@calcom/lib/event-types/getBulkEventTypes";
+import { AppRepository } from "@calcom/lib/server/repository/app/PrismaAppRepository";
 import prisma from "@calcom/prisma";
 import { userMetadata } from "@calcom/prisma/zod-utils";
 
 const setDefaultConferencingApp = async (userId: number, appSlug: string) => {
   const eventTypes = await getBulkUserEventTypes(userId);
   const eventTypeIds = eventTypes.eventTypes.map((item) => item.id);
-  const foundApp = getAppFromSlug(appSlug);
+  const appRepository = new AppRepository();
+  const [foundApp, user] = await Promise.all([
+    appRepository.getMetadataFromSlug(appSlug),
+    prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        metadata: true,
+        credentials: true,
+      },
+    }),
+  ]);
   const appType = foundApp?.appData?.location?.type;
 
   if (!appType) {
     return;
   }
 
-  const user = await prisma.user.findUnique({
-    where: {
-      id: userId,
-    },
-    select: {
-      metadata: true,
-      credentials: true,
-    },
-  });
-
   const currentMetadata = userMetadata.parse(user?.metadata);
   const credentialId = user?.credentials.find((item) => item.appId == appSlug)?.id;
 
   //Update the default conferencing app for the user.
-  await prisma.user.update({
-    where: {
-      id: userId,
-    },
-    data: {
-      metadata: {
-        ...currentMetadata,
-        defaultConferencingApp: {
-          appSlug,
+  await Promise.all([
+    prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        metadata: {
+          ...currentMetadata,
+          defaultConferencingApp: {
+            appSlug,
+          },
         },
       },
-    },
-  });
-
-  await prisma.eventType.updateMany({
-    where: {
-      id: {
-        in: eventTypeIds,
+    }),
+    prisma.eventType.updateMany({
+      where: {
+        id: {
+          in: eventTypeIds,
+        },
+        userId,
       },
-      userId,
-    },
-    data: {
-      locations: [{ type: appType, credentialId }] as LocationObject[],
-    },
-  });
+      data: {
+        locations: [{ type: appType, credentialId }] as LocationObject[],
+      },
+    }),
+  ]);
 };
 export default setDefaultConferencingApp;
