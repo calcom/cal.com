7cdf18f264ad53b40b12751b189fb85599d7a39e
Keith Williams <keithwillcode@gmail.com>
2025-08-26 09:33:34 -0300

    Trying to fix tests

--- setupVitest.ts
+++ setupVitest.ts
@@ -11,21 +11,45 @@ fetchMocker.enableMocks();
 
 expect.extend(matchers);
 
+class MockExchangeCalendarService implements CalendarService {
+  constructor() {}
+  async createEvent() {
+    return {
+      uid: "mock",
+      id: "mock",
+      password: "",
+      type: "",
+      url: "",
+      additionalInfo: {},
+    };
+  }
+  async updateEvent() {
+    return {
+      uid: "mock",
+      id: "mock",
+      password: "",
+      type: "",
+      url: "",
+      additionalInfo: {},
+    };
+  }
+  async deleteEvent() {}
+  async getAvailability() {
+    return [];
+  }
+  async listCalendars() {
+    return [];
+  }
+}
+
 vi.mock("@calcom/exchangecalendar/lib/CalendarService", () => ({
-  default: class MockExchangeCalendarService {
-    constructor() {}
-    async createEvent() {
-      return { uid: "mock", id: "mock", password: "", type: "", url: "", additionalInfo: {} };
-    }
-    async updateEvent() {
-      return { uid: "mock", id: "mock", password: "", type: "", url: "", additionalInfo: {} };
-    }
-    async deleteEvent() {}
-    async getAvailability() {
-      return [];
-    }
-    async listCalendars() {
-      return [];
-    }
-  },
+  default: MockExchangeCalendarService,
+}));
+
+vi.mock("@calcom/exchange2013calendar/lib/CalendarService", () => ({
+  default: MockExchangeCalendarService,
+}));
+
+vi.mock("@calcom/exchange2016calendar/lib/CalendarService", () => ({
+  default: MockExchangeCalendarService,
 }));
