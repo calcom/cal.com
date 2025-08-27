18c2e2fae383c86e77dda3778c249337f7d721e9
Eunjae Lee <hey@eunjae.dev>
2025-08-27 16:43:55 +0200

    re-order charts

--- apps/web/modules/insights/insights-view.tsx
+++ apps/web/modules/insights/insights-view.tsx
@@ -93,10 +93,9 @@ function InsightsPageContent() {
 
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
           <MostCancelledBookingsTables />
-          <RecentNoShowGuestsChart />
           <HighestNoShowHostTable />
           <div className="sm:col-span-2">
-            <PopularEventsTable />
+            <RecentNoShowGuestsChart />
           </div>
         </div>
 
@@ -105,6 +104,12 @@ function InsightsPageContent() {
           <LowestRatedMembersTable />
           <div className="sm:col-span-2">
             <RecentFeedbackTable />
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
+          <div className="sm:col-span-2">
+            <PopularEventsTable />
           </div>
         </div>
 
