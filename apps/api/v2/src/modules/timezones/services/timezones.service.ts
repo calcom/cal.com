e7f6cb7db10a1f6d73347d49ad5bb7c614571313
Volnei Munhoz <volnei.munhoz@gmail.com>
2025-08-29 11:47:15 +0000

    chore: remove unucessary dependency

--- apps/api/v2/src/modules/timezones/services/timezones.service.ts
+++ apps/api/v2/src/modules/timezones/services/timezones.service.ts
@@ -1,4 +1,3 @@
-import { RedisService } from "@/modules/redis/redis.service";
 import { Injectable } from "@nestjs/common";
 
 import { cityTimezonesHandler } from "@calcom/platform-libraries";
@@ -6,8 +5,6 @@ import type { CityTimezones } from "@calcom/platform-libraries";
 
 @Injectable()
 export class TimezonesService {
-  constructor(private readonly redisService: RedisService) {}
-
   async getCityTimeZones(): Promise<CityTimezones> {
     return await cityTimezonesHandler();
   }
