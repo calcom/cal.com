4b0b59d97a21b2a0993999836d4db5a7cec42c09
Ryukemeister <sahalrajiv6900@gmail.com>
2025-09-01 02:06:39 +0530

    update playwright config

--- packages/platform/examples/base/playwright.config.ts
+++ packages/platform/examples/base/playwright.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig, devices } from "@playwright/test";
 import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, ".env.tests.ci") });
 
 const DEFAULT_EXPECT_TIMEOUT = process.env.CI ? 30000 : 120000;
 const DEFAULT_TEST_TIMEOUT = process.env.CI ? 60000 : 240000;
