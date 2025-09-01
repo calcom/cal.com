69969d40c87c68749af523043f9e7e0b49ae5161
Angelina Nguyen <nangelina1412@gmail.com>
2025-09-01 18:00:17 +1000

    add descriptive user-agent to kyzonAxiosInstance, so KYZON can whitelist its requests

--- packages/app-store/kyzon-space/lib/axios.ts
+++ packages/app-store/kyzon-space/lib/axios.ts
@@ -5,4 +5,5 @@ export const kyzonBaseUrl = "https://kyzonsolutions.com/api/cloud";
 export const kyzonAxiosInstance = axios.create({
   baseURL: kyzonBaseUrl,
   timeout: 10000, // align with other axios instances to enforce a 10 s timeout
+  headers: { "User-Agent": "Cal.com-KYZON-Space/1.0" },
 });
