b6035236361f138413caab6640e9b3c864cbf200
hbjORbj <sldisek783@gmail.com>
2025-09-01 11:12:53 +0900

    fallback

--- apps/web/app/api/social/og/image/route.tsx
+++ apps/web/app/api/social/og/image/route.tsx
@@ -113,7 +113,7 @@ async function handler(req: NextRequest) {
       case "app": {
         try {
           const svgHashesModule = await import("@calcom/web/public/app-store/svg-hashes.json");
-          const SVG_HASHES = svgHashesModule.default;
+          const SVG_HASHES = svgHashesModule.default ?? {};
           const { name, description, slug } = appSchema.parse({
             name: searchParams.get("name"),
             description: searchParams.get("description"),
