3b7d97b194bb29f2a686863d0ecf1cc862e2b818
Volnei Munhoz <volnei.munhoz@gmail.com>
2025-09-02 12:24:09 -0300

    chore: add eslint rules to avoid importing from app-store (#23518)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,18 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: ["./packages/config/eslint-preset.js"],
+  overrides: [
+    {
+      files: ["packages/lib/**/*.{ts,tsx,js,jsx}", "packages/prisma/**/*.{ts,tsx,js,jsx}"],
+      rules: {
+        "no-restricted-imports": [
+          "warn",
+          {
+            paths: ["@calcom/app-store"],
+            patterns: ["@calcom/app-store/*"],
+          },
+        ],
+      },
+    },
+  ],
 };
