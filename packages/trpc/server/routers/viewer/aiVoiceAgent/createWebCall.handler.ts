8f2cd9391c181209e57c072b9d213d352157c93a
Udit Takkar <udit222001@gmail.com>
2025-09-03 14:14:01 +0530

    fix: web call

--- packages/trpc/server/routers/viewer/aiVoiceAgent/createWebCall.handler.ts
+++ packages/trpc/server/routers/viewer/aiVoiceAgent/createWebCall.handler.ts
@@ -2,7 +2,8 @@ import { createDefaultAIPhoneServiceProvider } from "@calcom/features/calAIPhone
 import logger from "@calcom/lib/logger";
 import { PrismaAgentRepository } from "@calcom/lib/server/repository/PrismaAgentRepository";
 
-// import { TRPCError } from "@trpc/server";
+import { TRPCError } from "@trpc/server";
+
 import type { TrpcSessionUser } from "../../../types";
 import type { TCreateWebCallInputSchema } from "./createWebCall.schema";
 
@@ -41,11 +42,11 @@ export const createWebCallHandler = async ({ ctx, input }: CreateWebCallHandlerO
     });
   } catch (error) {
     logger.error(`Failed to update tools for agent ${agent.providerAgentId}: ${error}`);
-    // throw new TRPCError({
-    //   code: "UNAUTHORIZED",
-    //   message:
-    //     "You are not authorized to create web call with this event type. Failed to update tools for agent",
-    // });
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message:
+        "You are not authorized to create web call with this event type. Failed to update tools for agent",
+    });
   }
 
   return await aiService.createWebCall({
