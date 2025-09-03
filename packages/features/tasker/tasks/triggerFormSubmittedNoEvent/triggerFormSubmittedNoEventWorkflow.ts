cfe2068c1c368a3c1f009f8a3fb13ccedd13a913
CarinaWolli <wollencarina@gmail.com>
2025-09-03 14:49:29 +0200

    implement triggerFormSubmittedNoEventWorkflow

--- packages/features/tasker/tasks/triggerFormSubmittedNoEvent/triggerFormSubmittedNoEventWorkflow.ts
+++ packages/features/tasker/tasks/triggerFormSubmittedNoEvent/triggerFormSubmittedNoEventWorkflow.ts
@@ -1,48 +1,35 @@
 import { z } from "zod";
 
-import { scheduleWorkflowReminders } from "@calcom/ee/workflows/lib/reminders/reminderScheduler";
 import logger from "@calcom/lib/logger";
-import { WorkflowTriggerEvents, WorkflowActions, WorkflowTemplates, TimeUnit } from "@calcom/prisma/enums";
+import { WorkflowService } from "@calcom/lib/server/service/workflows";
+import { ZWorkflow } from "@calcom/trpc/server/routers/viewer/workflows/getAllActiveWorkflows.schema";
 
 import { shouldTriggerFormSubmittedNoEvent } from "./formSubmissionValidation";
 
 export const ZTriggerFormSubmittedNoEventWorkflowPayloadSchema = z.object({
   responseId: z.number(),
   responses: z.any(),
-  formId: z.string(),
-  workflow: z.object({
-    id: z.number(),
-    name: z.string(),
-    teamId: z.number().nullable(),
-    trigger: z.nativeEnum(WorkflowTriggerEvents),
-    time: z.number().nullable(),
-    timeUnit: z.nativeEnum(TimeUnit).nullable(),
-    userId: z.number().nullable(),
-    steps: z.array(
-      z.object({
-        id: z.number(),
-        action: z.nativeEnum(WorkflowActions),
-        sendTo: z.string().nullable(),
-        template: z.nativeEnum(WorkflowTemplates),
-        reminderBody: z.string().nullable(),
-        emailSubject: z.string().nullable(),
-        sender: z.string().nullable(),
-        includeCalendarEvent: z.boolean(),
-        numberVerificationPending: z.boolean(),
-        numberRequired: z.boolean().nullable(),
-        verifiedAt: z.date().optional().nullable(),
-      })
-    ),
+  form: z.object({
+    id: z.string(),
+    userId: z.number(),
+    teamId: z.number().nullable().optional(),
+    fields: z.array(z.object({ type: z.string(), identifier: z.string().optional() })).optional(),
+    user: z.object({
+      email: z.string(),
+      timeFormat: z.number().nullable(),
+      locale: z.string().nullable(),
+    }),
   }),
+  workflow: ZWorkflow,
 });
 
 export async function triggerFormSubmittedNoEventWorkflow(payload: string): Promise<void> {
-  const { responseId, formId, responses, workflow } = ZTriggerFormSubmittedNoEventWorkflowPayloadSchema.parse(
+  const { responseId, form, responses, workflow } = ZTriggerFormSubmittedNoEventWorkflowPayloadSchema.parse(
     JSON.parse(payload)
   );
 
   const shouldTrigger = await shouldTriggerFormSubmittedNoEvent({
-    formId,
+    formId: form.id,
     responseId,
     responses,
   });
@@ -50,11 +37,11 @@ export async function triggerFormSubmittedNoEventWorkflow(payload: string): Prom
   if (!shouldTrigger) return;
 
   try {
-    await scheduleWorkflowReminders({
+    WorkflowService.scheduleFormWorkflows({
       workflows: [workflow],
-      smsReminderNumber: null, // we need to pass this here and get it from repsonses
-      calendarEvent: null, //we need to pass responses here instead
-      hideBranding: false, // we need to get that from team or user
+      responses,
+      responseId,
+      form: form,
     });
   } catch (error) {
     logger.error("Error while triggering form submitted no event workflows", JSON.stringify({ error }));
