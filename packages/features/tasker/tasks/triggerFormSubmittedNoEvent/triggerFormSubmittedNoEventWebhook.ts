55fe85a8b686d8cb0b31afb329358a10f14d5002
hbjORbj <sldisek783@gmail.com>
2025-09-06 00:34:15 +0900

    fix

--- packages/features/tasker/tasks/triggerFormSubmittedNoEvent/triggerFormSubmittedNoEventWebhook.ts
+++ packages/features/tasker/tasks/triggerFormSubmittedNoEvent/triggerFormSubmittedNoEventWebhook.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 import incompleteBookingActionFunctions from "@calcom/app-store/routing-forms/lib/incompleteBooking/actionFunctions";
-import type { FORM_SUBMITTED_WEBHOOK_RESPONSES } from "@calcom/app-store/routing-forms/trpc/utils";
 import { sendGenericWebhookPayload } from "@calcom/features/webhooks/lib/sendPayload";
 import prisma from "@calcom/prisma";
+import type { FORM_SUBMITTED_WEBHOOK_RESPONSES } from "@calcom/trpc/server/routers/apps/routing-forms/utils";
 
 export type ResponseData = {
   responseId: number;
