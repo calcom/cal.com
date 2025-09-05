4482f11bd657b547431e62f6687b18a035cce6d9
hbjORbj <sldisek783@gmail.com>
2025-09-05 23:59:45 +0900

    delete

--- packages/app-store/routing-forms/trpc/procedures/forms.ts
+++
@@ -1,8 +0,0 @@
-import authedProcedure from "@calcom/trpc/server/procedures/authedProcedure";
-
-import { ZFormsInputSchema } from "../forms.schema";
-
-export const forms = authedProcedure.input(ZFormsInputSchema).query(async ({ ctx, input }) => {
-  const handler = (await import("../forms.handler")).default;
-  return handler({ ctx, input });
-});
