44e244cfce3ad8ff0942777cc0559805240e4fdb
Shashwat Singh <shashwat123student@gmail.com>
2025-09-06 16:04:13 +0530

    fixing type errors

--- packages/trpc/server/routers/viewer/calendars/setDestinationReminder.schema.ts
+++ packages/trpc/server/routers/viewer/calendars/setDestinationReminder.schema.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const ZUpdateDestinationCalendarReminderInputSchema = z.object({
   credentialId: z.number(),
-  integration: z.literal("google_calendar"),
-  defaultReminder: z.union([z.literal(10), z.literal(30), z.literal(60)]),
+  integration: z.string(),
+  defaultReminder: z.number(),
 });
 
 export type TSetDestinationCalendarReminderSchema = z.infer<
