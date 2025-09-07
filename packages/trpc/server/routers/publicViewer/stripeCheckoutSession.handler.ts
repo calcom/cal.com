a62f1b6bfa6eb90bd4bfa3290bfb7a76b47eb086
hbjORbj <sldisek783@gmail.com>
2025-09-07 20:52:43 +0900

    define business logic directly in trpc handler

--- packages/trpc/server/routers/publicViewer/stripeCheckoutSession.handler.ts
+++ packages/trpc/server/routers/publicViewer/stripeCheckoutSession.handler.ts
@@ -1,13 +1,71 @@
-import { StripeService } from "@calcom/lib/server/service/stripe";
+import stripe from "@calcom/features/ee/payments/server/stripe";
 
-import type { TStripeCheckoutSessionInputSchema } from "./stripeCheckoutSession.schema";
+import {
+  ZStripeCheckoutSessionInputSchema,
+  type TStripeCheckoutSessionInputSchema,
+} from "./stripeCheckoutSession.schema";
 
 type StripeCheckoutSessionOptions = {
   input: TStripeCheckoutSessionInputSchema;
 };
 
+async function getCheckoutSession(input: TStripeCheckoutSessionInputSchema) {
+  const { checkoutSessionId, stripeCustomerId } = input;
+
+  // Moved the following data checks to superRefine
+  ZStripeCheckoutSessionInputSchema.parse(input);
+
+  let customerId: string;
+  let isPremiumUsername = false;
+  let hasPaymentFailed = false;
+  if (checkoutSessionId) {
+    try {
+      const session = await stripe.checkout.sessions.retrieve(checkoutSessionId);
+      if (typeof session.customer !== "string") {
+        return {
+          valid: false,
+        };
+      }
+      customerId = session.customer;
+      isPremiumUsername = true;
+      hasPaymentFailed = session.payment_status !== "paid";
+    } catch (e) {
+      return {
+        valid: false,
+      };
+    }
+  } else {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    customerId = stripeCustomerId!;
+  }
+
+  try {
+    const customer = await stripe.customers.retrieve(customerId);
+    if (customer.deleted) {
+      return {
+        valid: false,
+      };
+    }
+
+    return {
+      valid: true,
+      hasPaymentFailed,
+      isPremiumUsername,
+      customer: {
+        username: customer.metadata.username,
+        email: customer.metadata.email,
+        stripeCustomerId: customerId,
+      },
+    };
+  } catch (e) {
+    return {
+      valid: false,
+    };
+  }
+}
+
 export const stripeCheckoutSessionHandler = async ({ input }: StripeCheckoutSessionOptions) => {
-  return await StripeService.getCheckoutSession(input);
+  return await getCheckoutSession(input);
 };
 
 export default stripeCheckoutSessionHandler;
