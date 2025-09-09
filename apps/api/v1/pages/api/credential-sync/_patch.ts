0a3ecc5c747c685826a0f7a1037d3e92860f3d4a
Alex van Andel <me@alexvanandel.com>
2025-09-09 03:26:27 +0100

    Run eslint formatter

--- apps/api/v1/pages/api/credential-sync/_patch.ts
+++ apps/api/v1/pages/api/credential-sync/_patch.ts
@@ -5,6 +5,7 @@ import { symmetricDecrypt } from "@calcom/lib/crypto";
 import { defaultResponder } from "@calcom/lib/server/defaultResponder";
 import { prisma } from "@calcom/prisma";
 import type { Prisma } from "@calcom/prisma/client";
+
 import { schemaCredentialPatchParams, schemaCredentialPatchBody } from "~/lib/validations/credential-sync";
 
 /**
