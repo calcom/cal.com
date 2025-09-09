d998b1ec736711bc77a2fd1902a939be55496bd4
Sean Brydon <sean@cal.com>
2025-09-09 08:08:24 +0100

    return all constants from mock

--- packages/features/pbac/services/__tests__/permission-check.service.test.ts
+++ packages/features/pbac/services/__tests__/permission-check.service.test.ts
@@ -14,9 +14,13 @@ vi.mock("../../infrastructure/repositories/PermissionRepository");
 vi.mock("@calcom/features/flags/features.repository");
 vi.mock("@calcom/lib/server/repository/membership");
 vi.mock("../permission.service");
-vi.mock("@calcom/lib/constants", () => ({
-  IS_CALCOM: true,
-}));
+vi.mock("@calcom/lib/constants", async (importOriginal) => {
+  const actual = await importOriginal();
+  return {
+    ...actual,
+    IS_CALCOM: true,
+  };
+});
 
 type MockRepository = {
   [K in keyof IPermissionRepository]: Mock;
