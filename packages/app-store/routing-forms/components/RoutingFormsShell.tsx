e18debe8422e871e0e0ecd174b36c079caebe781
Eunjae Lee <hey@eunjae.dev>
2025-09-10 14:52:44 +0200

    remove unnecessary hook

--- packages/app-store/routing-forms/components/RoutingFormsShell.tsx
+++ packages/app-store/routing-forms/components/RoutingFormsShell.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useRedirectToLoginIfUnauthenticated } from "@calcom/features/auth/lib/hooks/useRedirectToLoginIfUnauthenticated";
-import { useRedirectToOnboardingIfNeeded } from "@calcom/features/auth/lib/hooks/useRedirectToOnboardingIfNeeded";
 
 interface RoutingFormsShellProps {
   children: React.ReactNode;
@@ -9,7 +8,6 @@ interface RoutingFormsShellProps {
 
 export default function RoutingFormsShell({ children }: RoutingFormsShellProps) {
   useRedirectToLoginIfUnauthenticated();
-  useRedirectToOnboardingIfNeeded();
 
   return <>{children}</>;
 }
