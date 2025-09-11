9156848503c804b6dfcd43763f0e877cbdc5a629
Benny Joo <sldisek783@gmail.com>
2025-09-10 22:18:59 -0400

    refactor: circular deps between app store and lib [2] (#23734)
    * mv getDefaultLocations
    
    * fix
    
    * 2 more utils
    
    * update imports
    
    * fix
    
    * move getConnectedApps
    
    * move handlePaymentSuccess
    
    * update imports
    
    * fix import
    
    * publish platform libraries

--- packages/app-store/_utils/getDefaultLocations.ts
+++ packages/app-store/_utils/getDefaultLocations.ts
@@ -1,16 +1,16 @@
-import getAppKeysFromSlug from "@calcom/app-store/_utils/getAppKeysFromSlug";
-import { DailyLocationType } from "@calcom/app-store/constants";
-import getApps from "@calcom/app-store/utils";
 import { getUsersCredentialsIncludeServiceAccountKey } from "@calcom/lib/server/getUsersCredentials";
+import type { Prisma } from "@calcom/prisma/client";
 import { userMetadata as userMetadataSchema } from "@calcom/prisma/zod-utils";
 import type { EventTypeLocation } from "@calcom/prisma/zod/custom/eventtype";
-import type { TrpcSessionUser } from "@calcom/trpc/server/types";
 
-type SessionUser = NonNullable<TrpcSessionUser>;
+import { DailyLocationType } from "../constants";
+import getApps from "../utils";
+import getAppKeysFromSlug from "./getAppKeysFromSlug";
+
 type User = {
-  id: SessionUser["id"];
-  email: SessionUser["email"];
-  metadata: SessionUser["metadata"];
+  id: number;
+  email: string;
+  metadata: Prisma.JsonValue;
 };
 
 export async function getDefaultLocations(user: User): Promise<EventTypeLocation[]> {
