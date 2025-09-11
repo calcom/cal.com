34d5e92306c827fb714b06a46fa0f4f76c735fe4
Ryukemeister <sahalrajiv6900@gmail.com>
2025-09-11 11:59:22 +0530

    fix: import variables from env instead of separate files

--- packages/platform/examples/base/playwright.config.ts
+++ packages/platform/examples/base/playwright.config.ts
@@ -2,7 +2,9 @@ import { defineConfig, devices } from "@playwright/test";
 import dotenv from "dotenv";
 import path from "path";
 
-dotenv.config({ path: path.resolve(__dirname, ".env.tests.ci") });
+const envPath = process.env.CI ? path.resolve(__dirname, ".env") : path.resolve(__dirname, ".env.local");
+
+dotenv.config({ path: envPath });
 
 const DEFAULT_EXPECT_TIMEOUT = process.env.CI ? 30000 : 120000;
 const DEFAULT_TEST_TIMEOUT = process.env.CI ? 60000 : 240000;
