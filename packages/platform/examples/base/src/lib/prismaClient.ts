5eb228837db75b9c0e25ed9cbb711a1a4ddb1c58
cal.com <morgan@cal.com>
2025-09-11 12:04:23 +0300

    fix: atoms e2e

--- packages/platform/examples/base/src/lib/prismaClient.ts
+++ packages/platform/examples/base/src/lib/prismaClient.ts
@@ -1,7 +1,7 @@
 // prisma client of example app
 //  using local prisma db, not related to the cal.com monorepo prisma client
 // eslint-disable-next-line
-import { PrismaClient } from "@calcom/prisma";
+import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
   return new PrismaClient();
