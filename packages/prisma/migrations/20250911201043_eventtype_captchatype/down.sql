1054dc771bfbc18db4c41a735ec8e6815b3b89f6
arjun3492 <arjun.kohli.330@gmail.com>
2025-09-14 23:08:56 +0530

    fix:migration history post merge

--- packages/prisma/migrations/20250911201043_eventtype_captchatype/down.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "EventType" DROP COLUMN "captchaType";
-
--- DropEnum
-DROP TYPE "CaptchaType";
-
--- packages/prisma/migrations/20250911201043_eventtype_captchatype/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "CaptchaType" AS ENUM ('off', 'low', 'medium', 'high');
-
--- AlterTable
-ALTER TABLE "EventType" ADD COLUMN     "captchaType" "CaptchaType" DEFAULT 'off';
