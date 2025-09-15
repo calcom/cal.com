bdc106445a008b498b1d51eb138fcd5b710f0abc
Hariom Balhara <hariombalhara@gmail.com>
2025-09-15 16:41:49 +0530

    Add BookingValidationService

--- packages/features/bookings/lib/handleNewBooking/checkIfBookerEmailIsBlocked.ts
+++ packages/features/bookings/lib/handleNewBooking/checkIfBookerEmailIsBlocked.ts
@@ -1,13 +1,15 @@
 import { extractBaseEmail } from "@calcom/lib/extract-base-email";
 import { HttpError } from "@calcom/lib/http-error";
-import prisma from "@calcom/prisma";
+import type { UserRepository } from "@calcom/lib/server/repository/user";
 
 export const checkIfBookerEmailIsBlocked = async ({
   bookerEmail,
   loggedInUserId,
+  userRepository,
 }: {
   bookerEmail: string;
   loggedInUserId?: number;
+  userRepository: UserRepository;
 }) => {
   const baseEmail = extractBaseEmail(bookerEmail);
   const blacklistedGuestEmails = process.env.BLACKLISTED_GUEST_EMAILS
@@ -17,37 +19,11 @@ export const checkIfBookerEmailIsBlocked = async ({
   const blacklistedEmail = blacklistedGuestEmails.find(
     (guestEmail: string) => guestEmail.toLowerCase() === baseEmail.toLowerCase()
   );
-
   if (!blacklistedEmail) {
     return false;
   }
 
-  const user = await prisma.user.findFirst({
-    where: {
-      OR: [
-        {
-          email: baseEmail,
-          emailVerified: {
-            not: null,
-          },
-        },
-        {
-          secondaryEmails: {
-            some: {
-              email: baseEmail,
-              emailVerified: {
-                not: null,
-              },
-            },
-          },
-        },
-      ],
-    },
-    select: {
-      id: true,
-      email: true,
-    },
-  });
+  const user = await userRepository.findVerifiedUserByEmail({ email: baseEmail });
 
   if (!user) {
     throw new HttpError({ statusCode: 403, message: "Cannot use this email to create the booking." });
