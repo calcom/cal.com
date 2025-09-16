a85bbb81e2e6f0fb109c8aa769d757855001aac3
arjun3492 <arjun.kohli.330@gmail.com>
2025-09-16 10:43:17 +0530

    updated admin gtm

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -178,17 +178,17 @@ class MyDocument extends Document<Props> {
               strategy="afterInteractive"
               dangerouslySetInnerHTML={{
                 __html: `
-          (function(w,d,s,l,i){
-            w[l]=w[l]||[];
-            w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-            var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),
-            dl=l!='dataLayer'?'&l='+l:'';
-            j.async=true;
-            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-            f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-KNB8Q7R4');
-        `,
+        (function(w,d,s,l,i){
+          w[l]=w[l]||[];
+          w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+          var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),
+          dl=l!='dataLayer'?'&l='+l:'';
+          j.async=true;
+          j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+          f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MJWK3XXZ');
+      `,
               }}
             />
           )}
@@ -344,7 +344,7 @@ class MyDocument extends Document<Props> {
           {allowScript && (
             <noscript>
               <iframe
-                src="https://www.googletagmanager.com/ns.html?id=GTM-KNB8Q7R4"
+                src="https://www.googletagmanager.com/ns.html?id=GTM-MJWK3XXZ"
                 height="0"
                 width="0"
                 style={{ display: "none", visibility: "hidden" }}
