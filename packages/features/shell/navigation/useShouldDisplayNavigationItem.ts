7fe80dd3ddb5a1e10a86286c5944b23e15dd3ab3
Eunjae Lee <hey@eunjae.dev>
2025-09-16 12:17:08 +0200

    Revert "feat: implement generalized navigation permission system with PBAC (#23706)" (#23867)
    This reverts commit 34b62f7516b04293ac6e09ab8ecf896f8d750d47.

--- packages/features/shell/navigation/useShouldDisplayNavigationItem.ts
+++ packages/features/shell/navigation/useShouldDisplayNavigationItem.ts
@@ -1,27 +1,10 @@
 import { useFlagMap } from "@calcom/features/flags/context/provider";
 import { isKeyInObject } from "@calcom/lib/isKeyInObject";
 
-import {
-  useNavigationPermissions,
-  type NavigationItemName,
-} from "../permissions/NavigationPermissionsProvider";
 import type { NavigationItemType } from "./NavigationItem";
 
 export function useShouldDisplayNavigationItem(item: NavigationItemType) {
   const flags = useFlagMap();
-  const { permissions: navigationPermissions, isLoading } = useNavigationPermissions();
-
-  if (isKeyInObject(item.name, flags) && flags[item.name] === false) {
-    return false;
-  }
-
-  if (isLoading) {
-    return true;
-  }
-
-  if (item.name in navigationPermissions) {
-    return navigationPermissions[item.name as NavigationItemName];
-  }
-
+  if (isKeyInObject(item.name, flags)) return flags[item.name];
   return true;
 }
