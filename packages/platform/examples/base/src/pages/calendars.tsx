6552d1accfd8791110f58ae3c9fa277395788531
Ryukemeister <sahalrajiv6900@gmail.com>
2025-09-17 19:21:23 +0530

    fix: add calendar view page in examples app

--- packages/platform/examples/base/src/pages/calendars.tsx
+++ packages/platform/examples/base/src/pages/calendars.tsx
@@ -1,7 +1,7 @@
 import { Navbar } from "@/components/Navbar";
 import { Inter } from "next/font/google";
 
-import { CalendarView } from "@calcom/atoms";
+import { CalendarSettings } from "@calcom/atoms";
 
 const inter = Inter({ subsets: ["latin"] });
 
