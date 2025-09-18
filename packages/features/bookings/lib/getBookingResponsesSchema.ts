9bbeab45926e046591f3159167b53fa6be570036
hbjORbj <sldisek783@gmail.com>
2025-09-18 18:49:21 +0900

    wip

--- packages/features/bookings/lib/getBookingResponsesSchema.ts
+++ packages/features/bookings/lib/getBookingResponsesSchema.ts
@@ -3,11 +3,8 @@ import z from "zod";
 
 import type { ALL_VIEWS } from "@calcom/features/form-builder/schema";
 import { dbReadResponseSchema, fieldTypesSchemaMap } from "@calcom/features/form-builder/schema";
-import {
-  bookingResponses,
-  emailSchemaRefinement,
-  type eventTypeBookingFields,
-} from "@calcom/prisma/zod-utils";
+import type { eventTypeBookingFields } from "@calcom/prisma/zod-utils";
+import { bookingResponses, emailSchemaRefinement } from "@calcom/prisma/zod-utils";
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 type View = ALL_VIEWS | (string & {});
