96e9c4f3745eeaace433e5e34220957f06a40209
Anik Dhabal Babu <adhabal2002@gmail.com>
2025-09-18 10:12:13 +0530

    update

--- packages/features/ee/round-robin/roundRobinReassignment.ts
+++ packages/features/ee/round-robin/roundRobinReassignment.ts
@@ -32,7 +32,7 @@ import logger from "@calcom/lib/logger";
 import { getTranslation } from "@calcom/lib/server/i18n";
 import { getTimeFormatStringFromUserTimeFormat } from "@calcom/lib/timeFormat";
 import { prisma } from "@calcom/prisma";
-import { userMetadata as userMetadataSchema, bookingMetadataSchema } from "@calcom/prisma/zod-utils";
+import { userMetadata as userMetadataSchema } from "@calcom/prisma/zod-utils";
 import type { EventTypeMetadata, PlatformClientParams } from "@calcom/prisma/zod-utils";
 import type { CalendarEvent } from "@calcom/types/Calendar";
 
@@ -304,9 +304,6 @@ export const roundRobinReassignment = async ({
         },
       })
     : null;
-
-  const parsedMetadata = bookingMetadataSchema.safeParse(booking.metadata ?? null);
-  const bookingMetadata = parsedMetadata.success ? parsedMetadata.data : null;
 
   const evt: CalendarEvent = {
     organizer: {
