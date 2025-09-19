07c92b3f60c6061d2148a5158637f7fb1859cb47
Anik Dhabal Babu <adhabal2002@gmail.com>
2025-09-19 23:15:02 +0530

    add migration

--- packages/prisma/migrations/20250919132724_add_event_type_timestamps/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "EventType" ADD COLUMN     "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN     "updatedAt" TIMESTAMP(3);
--- packages/prisma/migrations/20250919170653_fix_event_type_timestamps_no_default/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "EventType" ALTER COLUMN "createdAt" DROP DEFAULT;
