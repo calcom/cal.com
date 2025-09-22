6fe3d59cef6af5a2324117f8e1248cf97788eefc
hbjORbj <sldisek783@gmail.com>
2025-09-22 17:19:22 +0900

    mv getCalendarsEvents

--- packages/features/calendars/lib/getCalendarsEvents.ts
+++ packages/features/calendars/lib/getCalendarsEvents.ts
@@ -1,4 +1,5 @@
 import { getCalendar } from "@calcom/app-store/_utils/getCalendar";
+import { symmetricDecrypt } from "@calcom/lib/crypto";
 import { isDelegationCredential } from "@calcom/lib/delegationCredential";
 import logger from "@calcom/lib/logger";
 import { getPiiFreeSelectedCalendar, getPiiFreeCredential } from "@calcom/lib/piiFreeData";
@@ -6,8 +7,6 @@ import { safeStringify } from "@calcom/lib/safeStringify";
 import { performance } from "@calcom/lib/server/perfObserver";
 import type { EventBusyDate, SelectedCalendar } from "@calcom/types/Calendar";
 import type { CredentialForCalendarService } from "@calcom/types/Credential";
-
-import { symmetricDecrypt } from "./crypto";
 
 const log = logger.getSubLogger({ prefix: ["getCalendarsEvents"] });
 
