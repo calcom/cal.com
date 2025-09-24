34ea798f55c938441bc41928822e92080d55ee84
Amit Sharma <74371312+Amit91848@users.noreply.github.com>
2025-09-24 17:00:03 +0530

    changes fixes

--- packages/app-store/deel/zod.ts
+++ packages/app-store/deel/zod.ts
@@ -8,5 +8,5 @@ export const appKeysSchema = z.object({
   client_id: z.string().min(1),
   client_secret: z.string().min(1),
   redirect_uris: z.string().min(1),
-  webhook_signing_key: z.string().min(1),
+  webhook_signing_key: z.string().nullish(),
 });
