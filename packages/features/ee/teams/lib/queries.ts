9748d55dc1bd08cabe82e55d6aba43b90b03aa03
hbjORbj <sldisek783@gmail.com>
2025-09-25 17:20:54 +0900

    Remove isTeamMember

--- packages/features/ee/teams/lib/queries.ts
+++ packages/features/ee/teams/lib/queries.ts
@@ -385,27 +385,6 @@ export async function getTeamWithoutMembers(args: {
   };
 }
 
-export async function isTeamOwner(userId: number, teamId: number) {
-  return !!(await prisma.membership.findFirst({
-    where: {
-      userId,
-      teamId,
-      accepted: true,
-      role: "OWNER",
-    },
-  }));
-}
-
-export async function isTeamMember(userId: number, teamId: number) {
-  return !!(await prisma.membership.findFirst({
-    where: {
-      userId,
-      teamId,
-      accepted: true,
-    },
-  }));
-}
-
 export function generateNewChildEventTypeDataForDB({
   eventType,
   userId,
