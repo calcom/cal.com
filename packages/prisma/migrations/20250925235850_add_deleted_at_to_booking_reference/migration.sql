fcc09c9523d9bada58767a8536813778fee3dd5f
Alex van Andel <me@alexvanandel.com>
2025-09-26 03:21:34 +0100

    Remove generated warning, N/A

--- packages/prisma/migrations/20250925235850_add_deleted_at_to_booking_reference/migration.sql
+++ packages/prisma/migrations/20250925235850_add_deleted_at_to_booking_reference/migration.sql
@@ -1,9 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `sid` on the `users` table. All the data in the column will be lost.
-
-*/
 -- AlterTable
 ALTER TABLE "BookingReference" ADD COLUMN     "deletedAt" TIMESTAMP(3);
 
