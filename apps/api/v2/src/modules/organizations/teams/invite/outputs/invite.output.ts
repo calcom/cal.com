2c2afd127e8ba2db8e1758b521e27c7ca295a887
Udit Takkar <udit222001@gmail.com>
2025-09-26 14:01:59 +0530

    chore; udate summary

--- apps/api/v2/src/modules/organizations/teams/invite/outputs/invite.output.ts
+++ apps/api/v2/src/modules/organizations/teams/invite/outputs/invite.output.ts
@@ -3,15 +3,15 @@ import { ApiProperty } from "@nestjs/swagger";
 export class InviteDataDto {
   @ApiProperty({
     description:
-      "Opaque invitation token associated with this team. It can be shared with prospective members; when used via the webapp signup or teams page, it allows the recipient to accept the invitation and join the team/organization.",
+      "Unique invitation token for this team. Share this token with prospective members to allow them to join the team/organization.",
     example: "f6a5c8b1d2e34c7f90a1b2c3d4e5f6a5b7c8d9e0f1a2b3c4d5e6f7a8b9c0d1e2",
   })
   token!: string;
 
   @ApiProperty({
     description:
-      "Invitation URL constructed for the token. In organization context it points to signup with callback; otherwise it points to the teams page to accept the invite.",
-    example: "https://app.cal.com/signup?token=f6a5...&callbackUrl=/getting-started",
+      "Complete invitation URL that can be shared with prospective members. Opens the signup page with the token and redirects to getting started after signup.",
+    example: "http://app.cal.com/signup?token=f6a5c8b1d2e34c7f90a1b2c3d4e5f6a5b7c8d9e0f1a2b3c4d5e6f7a8b9c0d1e2&callbackUrl=/getting-started",
   })
   inviteLink!: string;
 }
