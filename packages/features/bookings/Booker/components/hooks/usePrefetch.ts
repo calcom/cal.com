50c3e53ec1c945fa40436012d7aff69947c54002
Ryukemeister <sahalrajiv6900@gmail.com>
2025-09-27 22:53:16 +0530

    fix: move over logic to determine prefetch next month into its own function

--- packages/features/bookings/Booker/components/hooks/usePrefetch.ts
+++ packages/features/bookings/Booker/components/hooks/usePrefetch.ts
@@ -1,8 +1,8 @@
 import dayjs from "@calcom/dayjs";
 import type { BookerState } from "@calcom/features/bookings/Booker/types";
-import { BookerLayouts } from "@calcom/prisma/zod-utils";
 
 import { getPrefetchMonthCount } from "../../utils/getPrefetchMonthCount";
+import { isPrefetchNextMonthEnabled } from "../../utils/isPrefetchNextMonthEnabled";
 
 interface UsePrefetchParams {
   date: string;
@@ -16,32 +16,12 @@ interface UsePrefetchParams {
 }
 
 export const usePrefetch = ({ date, month, bookerLayout, bookerState }: UsePrefetchParams) => {
-  const dateMonth = dayjs(date).month();
   const monthAfterAdding1Month = dayjs(date).add(1, "month").month();
-  const monthAfterAddingExtraDays = dayjs(date).add(bookerLayout.extraDays, "day").month();
   const monthAfterAddingExtraDaysColumnView = dayjs(date)
     .add(bookerLayout.columnViewExtraDays.current, "day")
     .month();
 
-  const isValidDate = dayjs(date).isValid();
-  const twoWeeksAfter = dayjs(month).startOf("month").add(2, "week");
-  const isSameMonth = dayjs().isSame(dayjs(month), "month");
-  const isAfter2Weeks = dayjs().isAfter(twoWeeksAfter);
-
-  const prefetchNextMonth =
-    (bookerLayout.layout === BookerLayouts.WEEK_VIEW &&
-      !!bookerLayout.extraDays &&
-      !isNaN(dateMonth) &&
-      !isNaN(monthAfterAddingExtraDays) &&
-      dateMonth !== monthAfterAddingExtraDays) ||
-    (bookerLayout.layout === BookerLayouts.COLUMN_VIEW &&
-      !isNaN(dateMonth) &&
-      !isNaN(monthAfterAddingExtraDaysColumnView) &&
-      dateMonth !== monthAfterAddingExtraDaysColumnView) ||
-    ((bookerLayout.layout === BookerLayouts.MONTH_VIEW || bookerLayout.layout === "mobile") &&
-      (!isValidDate || isSameMonth) &&
-      isAfter2Weeks);
-
+  const prefetchNextMonth = isPrefetchNextMonthEnabled(bookerLayout.layout, date, month);
   const monthCount = getPrefetchMonthCount(
     bookerLayout.layout,
     bookerState,
