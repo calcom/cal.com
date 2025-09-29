8ffef26c9b8412d7a80f1a8c64987a9363bf5533
Volnei Munhoz <volnei.munhoz@gmail.com>
2025-09-29 18:45:55 -0300

    removes from api v1

--- apps/api/v1/eslint.config.mjs
+++ apps/api/v1/eslint.config.mjs
@@ -1,11 +1,3 @@
-import { forbid } from "@calcom/eslint-config/base";
 import { nextJsConfig } from "@calcom/eslint-config/next-js";
 
-export default [
-  ...nextJsConfig,
-  forbid({
-    from: "../../tests/libs/__mocks__/prismaMock.ts",
-    target: ".",
-    message: "Please don't use prismaMock",
-  }),
-];
+export default nextJsConfig;
