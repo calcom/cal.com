72b51280a92d65f9de977ae0d1a224f8fee4bb15
Dhairyashil <dhairyashil10101010@gmail.com>
2025-10-02 16:04:03 +0530

    remove comments

--- apps/web/lib/sanitizeUsername.ts
+++ apps/web/lib/sanitizeUsername.ts
@@ -3,7 +3,7 @@ export const sanitizeUsername = (username: string): string => {
   if (!username) return "";
 
   return username
-    .replace(/^https?:\/\//i, "") // Remove any URL scheme (http, https)
-    .replace(/[^a-zA-Z0-9-_.+*]/g, "") // Remove invalid characters
-    .toLowerCase(); // Convert to lowercase
+    .replace(/^https?:\/\//i, "")
+    .replace(/[^a-zA-Z0-9-_.+*]/g, "")
+    .toLowerCase();
 };
