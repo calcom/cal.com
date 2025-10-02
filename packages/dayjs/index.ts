45e65bd1c4ab88013b707720edda99b29ff3b6f5
Ryukemeister <sahalrajiv6900@gmail.com>
2025-10-02 22:09:10 +0530

    revert isoWeek import for dayjs

--- packages/dayjs/index.ts
+++ packages/dayjs/index.ts
@@ -4,7 +4,6 @@ import customParseFormat from "dayjs/plugin/customParseFormat";
 import duration from "dayjs/plugin/duration";
 import isBetween from "dayjs/plugin/isBetween";
 import isToday from "dayjs/plugin/isToday";
-import isoWeek from "dayjs/plugin/isoWeek";
 import localizedFormat from "dayjs/plugin/localizedFormat";
 import minmax from "dayjs/plugin/minMax";
 import relativeTime from "dayjs/plugin/relativeTime";
@@ -25,7 +24,6 @@ dayjs.extend(toArray);
 dayjs.extend(utc);
 dayjs.extend(minmax);
 dayjs.extend(duration);
-dayjs.extend(isoWeek);
 
 export type Dayjs = dayjs.Dayjs;
 
