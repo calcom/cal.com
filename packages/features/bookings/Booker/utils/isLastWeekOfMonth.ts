3eebe6588ddbb9d5e1542c6c9c202f5141ebb236
Ryukemeister <sahalrajiv6900@gmail.com>
2025-10-02 21:55:54 +0530

    fix: dont use isoWeek from dayjs

--- packages/features/bookings/Booker/utils/isLastWeekOfMonth.ts
+++ packages/features/bookings/Booker/utils/isLastWeekOfMonth.ts
@@ -3,9 +3,7 @@ import dayjs from "@calcom/dayjs";
 export const isLastWeekOfMonth = (date: string): boolean => {
   const dateObj = dayjs(date);
   const currentMonth = dateObj.month();
-  // we wanna use ISO Week plugin here as it provides consistent, standardized week semantics
-  // thsi also aligns with international standards
-  const endOfWeek = dateObj.endOf("isoWeek");
+  const endOfWeek = dateObj.endOf("week");
 
   // if the end of current week is in a different month, we're in the last week
   return endOfWeek.month() !== currentMonth;
