3f183ba4e7b36b515c8343893392b9f2f16a62a3
Ryukemeister <sahalrajiv6900@gmail.com>
2025-10-02 21:32:23 +0530

    fix: type errors

--- packages/features/bookings/Booker/utils/isPrefetchNextMonthEnabled.ts
+++ packages/features/bookings/Booker/utils/isPrefetchNextMonthEnabled.ts
@@ -1,19 +1,15 @@
-import type { BookerLayouts } from "@calcom/prisma/zod-utils";
+import { BookerLayouts } from "@calcom/prisma/zod-utils";
 
 import { isLastWeekOfMonth } from "./isLastWeekOfMonth";
 import { isMonthViewPrefetchEnabled } from "./isMonthViewPrefetchEnabled";
 
-export const isPrefetchNextMonthEnabled = (
-  bookerLayout: BookerLayouts | "mobile",
-  date: string,
-  month: string | null
-) => {
-  if (bookerLayout === "week_view" || bookerLayout === "column_view") {
+export const isPrefetchNextMonthEnabled = (bookerLayout: string, date: string, month: string | null) => {
+  if (bookerLayout === BookerLayouts.WEEK_VIEW || bookerLayout === BookerLayouts.COLUMN_VIEW) {
     const shouldPrefetchNextMonth = isLastWeekOfMonth(date);
     return shouldPrefetchNextMonth;
   }
 
-  if (bookerLayout === "month_view" || bookerLayout === "mobile") {
+  if (bookerLayout === BookerLayouts.MONTH_VIEW || bookerLayout === "mobile") {
     const shouldPrefetchNextMonth = isMonthViewPrefetchEnabled(date, month);
     return shouldPrefetchNextMonth;
   }
