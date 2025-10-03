d03df1603fa59884cb2a2e5ce93c3db3794f5079
Anshumancanrock <109489361+Anshumancanrock@users.noreply.github.com>
2025-10-04 01:29:16 +0530

    Update Editor.tsx

--- packages/ui/components/editor/Editor.tsx
+++ packages/ui/components/editor/Editor.tsx
@@ -63,7 +63,7 @@ export const Editor = (props: TextEditorProps) => {
     <div className="editor rounded-md">
       {props.label && <label className="mb-1 block text-sm font-medium leading-6">{props.label}</label>}
       <LexicalComposer initialConfig={{ ...editorConfig }}>
-        <div className="editor-container hover:border-emphasis focus-within:shadow-outline-gray-focused !rounded-lg p-0 transition">
+        <div className="editor-container border border-default hover:border-emphasis focus-within:border-subtle focus-within:ring-brand-default rounded-lg p-0 transition focus-within:ring-2 focus-within:ring-offset-0">
           <ToolbarPlugin
             getText={props.getText}
             setText={props.setText}
