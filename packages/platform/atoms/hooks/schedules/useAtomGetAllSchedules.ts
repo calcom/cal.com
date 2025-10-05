66a3b467e75d2ff6e74093573fc0419a3a567344
Ryukemeister <sahalrajiv6900@gmail.com>
2025-10-06 02:41:47 +0530

    init: list schedules atom

--- packages/platform/atoms/hooks/schedules/useAtomGetAllSchedules.ts
+++ packages/platform/atoms/hooks/schedules/useAtomGetAllSchedules.ts
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { SUCCESS_STATUS } from "@calcom/platform-constants";
+import type { ApiResponse } from "@calcom/platform-types";
+import type { GetAvailabilityListHandlerReturn } from "@calcom/trpc/server/routers/viewer/availability/list.handler";
 
 import http from "../../lib/http";
 import { useAtomsContext } from "../useAtomsContext";
@@ -11,10 +13,10 @@ export const useAtomGetAllSchedules = () => {
   const pathname = "atoms/schedules/all";
   const { isInit, accessToken } = useAtomsContext();
 
-  const { isLoading, error, data } = useQuery({
+  const { isLoading, error, data, refetch } = useQuery({
     queryKey: [QUERY_KEY],
     queryFn: () => {
-      return http.get(pathname).then((res) => {
+      return http.get<ApiResponse<GetAvailabilityListHandlerReturn>>(pathname).then((res) => {
         if (res.data.status === SUCCESS_STATUS) {
           return res.data.data;
         }
@@ -24,5 +26,5 @@ export const useAtomGetAllSchedules = () => {
     enabled: isInit && !!accessToken,
   });
 
-  return { isLoading, error, data };
+  return { isLoading, error, data, refetch };
 };
---
+++ packages/platform/atoms/list-schedules/wrappers/ListSchedulesPlatformWrapper.tsx
@@ -0,0 +1,117 @@
+import { AtomsWrapper } from "@/components/atoms-wrapper";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+
+import { ScheduleListItem } from "@calcom/features/schedules/components/ScheduleListItem";
+import { getTransformedSchedules } from "@calcom/lib/schedules/transformers/getTransformedSchedles";
+import { EmptyScreen } from "@calcom/ui/components/empty-screen";
+
+import { useAtomDuplicateSchedule } from "../../hooks/schedules/useAtomDuplicateSchedule";
+import { useAtomGetAllSchedules } from "../../hooks/schedules/useAtomGetAllSchedules";
+import { useAtomUpdateSchedule } from "../../hooks/schedules/useAtomUpdateSchedule";
+import useDeleteSchedule from "../../hooks/schedules/useDeleteSchedule";
+import { useMe } from "../../hooks/useMe";
+import { useToast } from "../../src/components/ui/use-toast";
+
+interface ListSchedulesPlatformWrapperProps {
+  getRedirectUrl?: (scheduleId: number) => string;
+}
+
+export const ListSchedulesPlatformWrapper = ({ getRedirectUrl }: ListSchedulesPlatformWrapperProps = {}) => {
+  const [animationParentRef] = useAutoAnimate<HTMLUListElement>();
+  const { toast } = useToast();
+
+  const {
+    data: userSchedules,
+    isLoading: isLoadingSchedules,
+    refetch: refetchSchedules,
+  } = useAtomGetAllSchedules();
+  const { data: user, isLoading: isUserLoading } = useMe();
+
+  const { mutate: updateSchedule } = useAtomUpdateSchedule({
+    onSuccess: () => {
+      toast({
+        description: "Schedule updated successfully",
+      });
+      refetchSchedules();
+    },
+    onError: (err) => {
+      toast({
+        description: `Could not update schedule: ${err.error.message}`,
+      });
+    },
+  });
+
+  const { mutate: duplicateSchedule } = useAtomDuplicateSchedule({
+    onSuccess: () => {
+      toast({
+        description: "Schedule updated successfully",
+      });
+      refetchSchedules();
+    },
+    onError: (err) => {
+      toast({
+        description: `Could not duplicate schedule: ${err.error.message}`,
+      });
+    },
+  });
+
+  const { mutate: deleteSchedule } = useDeleteSchedule({
+    onSuccess: () => {
+      toast({
+        description: "Schedule deleted successfully",
+      });
+      refetchSchedules();
+    },
+    onError: (err) => {
+      toast({
+        description: `Could not delete schedule: ${err.error.message}`,
+      });
+    },
+  });
+
+  const transformedSchedules = getTransformedSchedules(userSchedules?.schedules ?? []);
+
+  if (isLoadingSchedules || isUserLoading) return <>Loading...</>;
+
+  if (!isLoadingSchedules && transformedSchedules?.length === 0)
+    return (
+      <EmptyScreen
+        Icon="clock"
+        headline="No schedules present"
+        description="Create a new schedule to get started"
+        className="w-full"
+      />
+    );
+
+  return (
+    <AtomsWrapper>
+      <div className="border-subtle bg-default overflow-hidden rounded-md border">
+        <ul className="divide-subtle divide-y" data-testid="schedules" ref={animationParentRef}>
+          {transformedSchedules?.map((schedule) => (
+            <ScheduleListItem
+              displayOptions={{
+                hour12: user?.data.timeFormat ? user.data.timeFormat === 12 : undefined,
+                timeZone: user?.data.timeZone,
+                weekStart: user?.data.weekStart || "Sunday",
+              }}
+              key={schedule.id}
+              schedule={schedule}
+              isDeletable={transformedSchedules.length !== 1}
+              redirectUrl={getRedirectUrl ? getRedirectUrl(schedule.id) : ""}
+              updateDefault={() => {
+                updateSchedule({
+                  scheduleId: schedule.id,
+                  body: {
+                    isDefault: true,
+                  },
+                });
+              }}
+              deleteFunction={async () => await deleteSchedule({ id: schedule.id })}
+              duplicateFunction={() => duplicateSchedule({ scheduleId: schedule.id })}
+            />
+          ))}
+        </ul>
+      </div>
+    </AtomsWrapper>
+  );
+};
