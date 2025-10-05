1e94556cb92d6fbd8e0a960f647f311e9ad1c57e
Shaunak Das <shaundj20@gmail.com>
2025-10-05 13:45:26 +0530

    feat: add membership repo

--- apps/api/v2/src/lib/services/check-booking-limits.service.ts
+++ apps/api/v2/src/lib/services/check-booking-limits.service.ts
@@ -1,12 +1,14 @@
 import { PrismaBookingRepository } from "@/lib/repositories/prisma-booking.repository";
+import { PrismaMembershipRepository } from "@/lib/repositories/prisma-membership.repository";
 import { Injectable } from "@nestjs/common";
 
 import { CheckBookingLimitsService as BaseCheckBookingLimitsService } from "@calcom/platform-libraries/bookings";
 
 @Injectable()
 export class CheckBookingLimitsService extends BaseCheckBookingLimitsService {
-  constructor(bookingRepository: PrismaBookingRepository) {
+  constructor(bookingRepository: PrismaBookingRepository, membershipRepository: PrismaMembershipRepository) {
     super({
+      membershipRepo: membershipRepository,
       bookingRepo: bookingRepository,
     });
   }
