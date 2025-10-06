e4f21cd132b47781098c97fe9e90d781f0241bfb
Joe Au-Yeung <j.auyeung419@gmail.com>
2025-10-06 16:11:54 -0400

    Change repository to expect credentialId

--- packages/app-store/salesforce/lib/repository/PrismaSalesforceFieldCacheRepository.ts
+++ packages/app-store/salesforce/lib/repository/PrismaSalesforceFieldCacheRepository.ts
@@ -1,31 +1,38 @@
 import { PrismaClient } from "@prisma/client";
 
+import { SalesforceRecordEnum } from "../enums";
+import type { ISalesforceFieldCacheRepository } from "../repository/SalesforceFieldCacheRepository.interface";
 import { SalesforceField } from "../types";
 
-export class PrismaSalesforceFieldCacheRepository {
+export class PrismaSalesforceFieldCacheRepository implements ISalesforceFieldCacheRepository {
   constructor(private readonly prisma: PrismaClient) {}
-
-  async create({ eventTypeId, fields }: { eventTypeId: string; fields: SalesforceField[] }) {
+  async create(args: { credentialId: number; recordType: SalesforceRecordEnum; fields: SalesforceField[] }) {
     return this.prisma.app_Salesforce_RecordFieldCache.create({
       data: {
-        eventTypeId,
-        fields,
+        ...args,
       },
     });
   }
 
-  async getByEventTypeId(eventTypeId: string) {
+  async getByCredentialId(args: { credentialId: number; recordType: SalesforceRecordEnum }) {
     return this.prisma.app_Salesforce_RecordFieldCache.findUnique({
       where: {
-        eventTypeId,
+        credentialId_recordType: {
+          credentialId: args.credentialId,
+          recordType: args.recordType,
+        },
       },
     });
   }
 
-  async update({ eventTypeId, fields }: { eventTypeId: string; fields: SalesforceField[] }) {
+  async update(args: { credentialId: number; recordType: SalesforceRecordEnum; fields: SalesforceField[] }) {
+    const { credentialId, recordType, fields } = args;
     return this.prisma.app_Salesforce_RecordFieldCache.update({
       where: {
-        eventTypeId,
+        credentialId_recordType: {
+          credentialId,
+          recordType,
+        },
       },
       data: {
         fields,
