16a33e88f980690c1938787487bd30595c661bad
Syed Ali Shahbaz <52925846+alishaz-polymath@users.noreply.github.com>
2025-10-06 23:20:35 +0400

    Create migration.sql

---
+++ packages/prisma/migrations/20251006191654_drop_watchlist_deleted_column_index/migration.sql
@@ -0,0 +1,14 @@
+-- DropIndex
+DROP INDEX "Watchlist_organizationId_isGlobal_idx";
+
+-- DropIndex
+DROP INDEX "Watchlist_source_idx";
+
+-- DropIndex
+DROP INDEX "WatchlistAudit_id_key";
+
+-- DropIndex
+DROP INDEX "WatchlistEventAudit_eventTypeId_timestamp_idx";
+
+-- DropIndex
+DROP INDEX "WatchlistEventAudit_watchlistId_timestamp_idx";
