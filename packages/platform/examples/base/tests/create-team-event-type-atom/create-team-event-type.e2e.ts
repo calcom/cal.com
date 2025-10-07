8c124414430839cf9ad5c25b3f9997180b614dfc
Ryukemeister <sahalrajiv6900@gmail.com>
2025-10-07 10:58:52 +0200

    refactor: create event type and create team event type test

--- packages/platform/examples/base/tests/create-team-event-type-atom/create-team-event-type.e2e.ts
+++ packages/platform/examples/base/tests/create-team-event-type-atom/create-team-event-type.e2e.ts
@@ -1,7 +1,5 @@
 import { test, expect } from "@playwright/test";
 
-import { generateRandomText } from "../../src/lib/generateRandomText";
-
 test("create team event using CreateTeamEventTypeAtom", async ({ page }) => {
   await page.goto("/");
 
@@ -30,47 +28,6 @@ test("create team event using CreateTeamEventTypeAtom", async ({ page }) => {
   await expect(page.locator('p:has-text("/platform-team-e2e-event")')).toBeVisible();
 
   await page.locator('[data-testid="team-event-type-card"] h1:has-text("Platform team e2e event")').click();
-
-  await expect(page.locator('[data-testid="event-type-settings-atom"]')).toBeVisible();
-
-  await expect(page.locator('[data-testid="vertical-tab-availability"]')).toBeVisible();
-  await page.locator('[data-testid="vertical-tab-availability"]').click();
-
-  await expect(page.locator('[data-testid="vertical-tab-assignment"]')).toBeVisible();
-  await page.locator('[data-testid="vertical-tab-assignment"]').click();
-
-  await expect(page.locator('[data-testid="vertical-tab-event_limit_tab_title"]')).toBeVisible();
-  await page.locator('[data-testid="vertical-tab-event_limit_tab_title"]').click();
-
-  await expect(page.locator('[data-testid="vertical-tab-event_advanced_tab_title"]')).toBeVisible();
-  await page.locator('[data-testid="vertical-tab-event_advanced_tab_title"]').click();
-
-  await expect(page.locator('[data-testid="vertical-tab-recurring"]')).toBeVisible();
-  await page.locator('[data-testid="vertical-tab-recurring"]').click();
-
-  await expect(page.locator('[data-testid="vertical-tab-payments"]')).toBeVisible();
-  await page.locator('[data-testid="vertical-tab-payments"]').click();
-
-  await expect(page.locator('[data-testid="vertical-tab-basics"]')).toBeVisible();
-  await page.locator('[data-testid="vertical-tab-basics"]').click();
-
-  await expect(page.locator('textarea[name="description"]')).toBeVisible();
-  await page.fill('textarea[name="description"]', generateRandomText());
-
-  await page.locator('[data-testid="update-eventtype"]').click();
-
-  await expect(page.locator('h1:has-text("Platform team e2e event")')).toBeVisible();
-  await expect(page.locator('p:has-text("/platform-team-e2e-event")')).toBeVisible();
-
-  await page.locator('[data-testid="team-event-type-card"] h1:has-text("Platform team e2e event")').click();
-
-  await expect(page.locator('[data-testid="event-type-settings-atom"]')).toBeVisible();
-
-  await page.locator('button[type="button"][tooltip="Delete"]').waitFor({ state: "visible" });
-  await page.locator('button[type="button"][tooltip="Delete"]').click();
-
-  await page.locator('[data-testid="dialog-creation"]').waitFor({ state: "visible" });
-  await page.locator('[data-testid="dialog-confirmation"]').click();
 
   await expect(page.locator("body")).toBeVisible();
 });
