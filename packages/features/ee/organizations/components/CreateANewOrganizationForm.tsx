348ed020fb2ccf5615f1afa73cc3f854debbe8ea
Sean Brydon <sean@cal.com>
2025-10-10 13:32:54 +0100

    Fix

--- packages/features/ee/organizations/components/CreateANewOrganizationForm.tsx
+++ packages/features/ee/organizations/components/CreateANewOrganizationForm.tsx
@@ -246,7 +246,7 @@ const CreateANewOrganizationFormChild = ({ session }: { session: Ensure<SessionC
           />
         </div>
 
-        {isBillingEnabled && isAdmin && (
+        {isAdmin && (
           <>
             <section className="grid grid-cols-2 gap-2">
               <div className="w-full">
