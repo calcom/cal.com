78b2fed93fd10387692dc23a9c888fdeeddbd5a0
Ryukemeister <sahalrajiv6900@gmail.com>
2025-10-10 15:27:28 +0200

    fix: merge conflicts

--- packages/platform/libraries/schedules.ts
+++ packages/platform/libraries/schedules.ts
@@ -4,10 +4,4 @@ export {
 } from "@calcom/lib/server/repository/schedule";
 
 export { updateSchedule, type UpdateScheduleResponse } from "@calcom/lib/schedules/updateSchedule";
-export { UserAvailabilityService } from "@calcom/lib/getUserAvailability";
-
-export {
-  createHandler as createScheduleHandler,
-  type CreateScheduleHandlerReturn,
-} from "@calcom/trpc/server/routers/viewer/availability/schedule/create.handler";
-export { ZCreateInputSchema as CreateAtomScheduleDto } from "@calcom/trpc/server/routers/viewer/availability/schedule/create.schema";
+export { UserAvailabilityService } from "@calcom/features/availability/lib/getUserAvailability";
