2779d39381b8461f65b92893f5f6f1f9aad10cfa
Joe Au-Yeung <j.auyeung419@gmail.com>
2025-10-10 15:36:43 -0400

    Fix typo in file import

--- packages/features/ee/billing/stripe-billing-service.ts
+++ packages/features/ee/billing/stripe-billing-service.ts
@@ -1,6 +1,9 @@
 import Stripe from "stripe";
 
+import logger from "@calcom/lib/logger";
+
 import type { BillingService } from "./billing-service";
+import { SubscriptionStatus } from "./repository/IBillingRepository";
 
 export class StripeBillingService implements BillingService {
   private stripe: Stripe;
@@ -210,4 +213,32 @@ export class StripeBillingService implements BillingService {
 
     return { subscriptionStart, subscriptionTrialEnd, subscriptionEnd };
   }
+
+  static mapStripeStatusToCalStatus = ({
+    stripeStatus,
+    subscriptionId,
+  }: {
+    stripeStatus: string;
+    subscriptionId: string;
+  }) => {
+    const log = logger.getSubLogger({ prefix: ["mapStripeStatusToCalStatus"] });
+    const statusMap: Record<string, SubscriptionStatus> = {
+      active: SubscriptionStatus.ACTIVE,
+      past_due: SubscriptionStatus.PAST_DUE,
+      canceled: SubscriptionStatus.CANCELLED,
+      cancelled: SubscriptionStatus.CANCELLED,
+      trialing: SubscriptionStatus.TRIALING,
+      incomplete: SubscriptionStatus.INCOMPLETE,
+      incomplete_expired: SubscriptionStatus.INCOMPLETE_EXPIRED,
+      unpaid: SubscriptionStatus.UNPAID,
+      paused: SubscriptionStatus.PAUSED,
+    };
+
+    const status = statusMap[stripeStatus];
+    if (!status) {
+      log.warn(`Unhandled status for ${stripeStatus} and sub id ${subscriptionId}`);
+    }
+
+    return status || SubscriptionStatus.ACTIVE;
+  };
 }
