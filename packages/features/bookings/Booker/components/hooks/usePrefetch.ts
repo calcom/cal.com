7e5140e5d56903aae2817ff20411351c41898d0a
Ryukemeister <sahalrajiv6900@gmail.com>
2025-10-13 10:39:45 +0200

    fix: bring back the orginal logic we had in usePrefetch but in a simplified way

--- packages/features/bookings/Booker/components/hooks/usePrefetch.ts
+++ packages/features/bookings/Booker/components/hooks/usePrefetch.ts
@@ -16,12 +16,22 @@ interface UsePrefetchParams {
 }
 
 export const usePrefetch = ({ date, month, bookerLayout, bookerState }: UsePrefetchParams) => {
+  const dateMonth = dayjs(date).month();
   const monthAfterAdding1Month = dayjs(date).add(1, "month").month();
+  const monthAfterAddingExtraDays = dayjs(date).add(bookerLayout.extraDays, "day").month();
   const monthAfterAddingExtraDaysColumnView = dayjs(date)
     .add(bookerLayout.columnViewExtraDays.current, "day")
     .month();
 
-  const prefetchNextMonth = isPrefetchNextMonthEnabled(bookerLayout.layout, date, month);
+  const prefetchNextMonth = isPrefetchNextMonthEnabled(
+    bookerLayout.layout,
+    date,
+    dateMonth,
+    monthAfterAddingExtraDays,
+    monthAfterAddingExtraDaysColumnView,
+    month,
+    bookerLayout.extraDays
+  );
   const monthCount = getPrefetchMonthCount(
     bookerLayout.layout,
     bookerState,
--- packages/features/bookings/Booker/utils/isPrefetchNextMonthEnabled.ts
+++ packages/features/bookings/Booker/utils/isPrefetchNextMonthEnabled.ts
@@ -1,16 +1,29 @@
 import { BookerLayouts } from "@calcom/prisma/zod-utils";
 
-import { isLastWeekOfMonth } from "./isLastWeekOfMonth";
+import { isMonthChange } from "./isMonthChange";
 import { isMonthViewPrefetchEnabled } from "./isMonthViewPrefetchEnabled";
 
-export const isPrefetchNextMonthEnabled = (bookerLayout: string, date: string, month: string | null) => {
-  if (bookerLayout === BookerLayouts.WEEK_VIEW || bookerLayout === BookerLayouts.COLUMN_VIEW) {
-    const shouldPrefetchNextMonth = isLastWeekOfMonth(date);
-    return shouldPrefetchNextMonth;
+export const isPrefetchNextMonthEnabled = (
+  bookerLayout: string,
+  date: string,
+  dateMonth: number,
+  monthAfterAddingExtraDays: number,
+  monthAfterAddingExtraDaysColumnView: number,
+  month: string | null,
+  extraDays?: number
+) => {
+  if (bookerLayout === BookerLayouts.WEEK_VIEW) {
+    return !!extraDays && isMonthChange(dateMonth, monthAfterAddingExtraDays);
+  }
+
+  if (bookerLayout === BookerLayouts.COLUMN_VIEW) {
+    return isMonthChange(dateMonth, monthAfterAddingExtraDaysColumnView);
   }
 
   if (bookerLayout === BookerLayouts.MONTH_VIEW || bookerLayout === "mobile") {
     const shouldPrefetchNextMonth = isMonthViewPrefetchEnabled(date, month);
     return shouldPrefetchNextMonth;
   }
+
+  return false;
 };
