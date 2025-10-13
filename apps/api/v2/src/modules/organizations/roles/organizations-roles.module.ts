56b6815a9de6e145a846433fb807a8e501947cf6
supalarry <laurisskraucis@gmail.com>
2025-10-13 17:06:56 +0200

    fix: starting v2

--- apps/api/v2/src/modules/organizations/roles/organizations-roles.module.ts
+++ apps/api/v2/src/modules/organizations/roles/organizations-roles.module.ts
@@ -1,3 +1,8 @@
+import { MembershipsModule } from "@/modules/memberships/memberships.module";
+import { OrganizationsRepository } from "@/modules/organizations/index/organizations.repository";
+import { PrismaModule } from "@/modules/prisma/prisma.module";
+import { RedisModule } from "@/modules/redis/redis.module";
+import { StripeModule } from "@/modules/stripe/stripe.module";
 import { Module } from "@nestjs/common";
 
 import { RoleService } from "@calcom/platform-libraries/pbac";
@@ -7,8 +12,9 @@ import { OrganizationsRolesOutputService } from "./services/organizations-roles-
 import { OrganizationsRolesService } from "./services/organizations-roles.service";
 
 @Module({
-  imports: [],
+  imports: [StripeModule, PrismaModule, RedisModule, MembershipsModule],
   providers: [
+    OrganizationsRepository,
     {
       provide: RoleService,
       useFactory: () => new RoleService(),
