b402262f466ffc3d5d23316eec6d6197d183177d
Ryukemeister <sahalrajiv6900@gmail.com>
2025-10-15 01:25:02 +0200

    update platform libraries schedule

--- packages/platform/libraries/schedules.ts
+++ packages/platform/libraries/schedules.ts
@@ -10,7 +10,13 @@ export {
   createHandler as createScheduleHandler,
   type CreateScheduleHandlerReturn,
 } from "@calcom/trpc/server/routers/viewer/availability/schedule/create.handler";
-
-export { ZCreateInputSchema as CreateScheduleSchema } from "@calcom/trpc/server/routers/viewer/availability/schedule/create.schema";
 export { ZCreateInputSchema as CreateScheduleSchema } from "@calcom/trpc/server/routers/viewer/availability/schedule/create.schema";
 
+export {
+  listHandler as getAvailabilityListHandler,
+  type GetAvailabilityListHandlerReturn,
+} from "@calcom/trpc/server/routers/viewer/availability/list.handler";
+export {
+  duplicateHandler as duplicateScheduleHandler,
+  type DuplicateScheduleHandlerReturn,
+} from "@calcom/trpc/server/routers/viewer/availability/schedule/duplicate.handler";
