fdb5b8012a82718d04fd710f5fe7ff3e0f7f0612
Ryukemeister <sahalrajiv6900@gmail.com>
2025-10-14 11:55:58 +0200

    fix: resolve merge conflicts

--- packages/platform/libraries/schedules.ts
+++ packages/platform/libraries/schedules.ts
@@ -4,13 +4,4 @@ export {
 } from "@calcom/lib/server/repository/schedule";
 
 export { updateSchedule, type UpdateScheduleResponse } from "@calcom/lib/schedules/updateSchedule";
-export { UserAvailabilityService } from "@calcom/lib/getUserAvailability";
-
-export {
-  listHandler as getAvailabilityListHandler,
-  type GetAvailabilityListHandlerReturn,
-} from "@calcom/trpc/server/routers/viewer/availability/list.handler";
-export {
-  duplicateHandler as duplicateScheduleHandler,
-  type DuplicateScheduleHandlerReturn,
-} from "@calcom/trpc/server/routers/viewer/availability/schedule/duplicate.handler";
+export { UserAvailabilityService } from "@calcom/features/availability/lib/getUserAvailability";
