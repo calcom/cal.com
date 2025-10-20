9cd0d39c8970e069fd55e70791bb1b6be970f9bc
Sean Brydon <sean@cal.com>
2025-10-20 12:48:38 +0100

    Revert all packages/* changes to original branch point

--- packages/features/ee/organizations/lib/onboarding/index.ts
+++
@@ -1,5 +0,0 @@
-export { OrganizationOnboardingFactory } from "./OrganizationOnboardingFactory";
-export type { IOrganizationOnboardingService } from "./IOrganizationOnboardingService";
-export type { CreateOnboardingIntentInput, OnboardingIntentResult } from "./types";
-export { BillingEnabledOnboardingService } from "./BillingEnabledOnboardingService";
-export { SelfHostedOnboardingService } from "./SelfHostedOnboardingService";
--- packages/prisma/migrations/20251005102651_add_onboarding_v3_feature_flag/migration.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO
-  "Feature" (slug, enabled, description, "type")
-VALUES
-  (
-    'onboarding-v3',
-    false,
-    'Enable onboarding v3 for all users',
-    'EXPERIMENT'
-  ) ON CONFLICT (slug) DO NOTHING;
