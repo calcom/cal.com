183fbf3fadff4993492c0f3e47a4ec84aafcfff5
Alex van Andel <me@alexvanandel.com>
2025-10-23 14:34:46 +0100

    chore: Adds a users.uuid column

---
+++ packages/prisma/migrations/20251023133244_add_user_uuid_column/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[uuid]` on the table `users` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."users" ADD COLUMN     "uuid" UUID;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_uuid_key" ON "public"."users"("uuid");
