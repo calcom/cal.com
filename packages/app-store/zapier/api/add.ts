8ea2c68d462b49c3423a821698cc16b40eed91d1
Dhairyashil <dhairyashil10101010@gmail.com>
2025-10-23 14:17:26 +0530

    refactor: remove-zapier-setup-screen

--- packages/app-store/zapier/api/add.ts
+++ packages/app-store/zapier/api/add.ts
@@ -35,5 +35,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(500);
   }
 
-  return res.status(200).json({ url: "/apps/zapier/setup" });
+  return res.status(200).json({ url: "https://zapier.com/apps/calcom/integrations" });
 }
