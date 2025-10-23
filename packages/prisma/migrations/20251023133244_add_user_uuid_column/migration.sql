47b398e31d3305e89cd1af6014e31c2481f09a10
Alex van Andel <me@alexvanandel.com>
2025-10-23 14:38:12 +0100

    Add uuid backfill

--- packages/prisma/migrations/20251023133244_add_user_uuid_column/migration.sql
+++ packages/prisma/migrations/20251023133244_add_user_uuid_column/migration.sql
@@ -9,3 +9,6 @@ ALTER TABLE "public"."users" ADD COLUMN     "uuid" UUID;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_uuid_key" ON "public"."users"("uuid");
+
+-- Backfill
+UPDATE "public"."users" SET uuid = gen_random_uuid() WHERE uuid IS NULL;
