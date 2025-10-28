22a2f44adc4ef299cc6b857c0abcf993ca517b9d
Syed Ali Shahbaz <alishahbaz7@gmail.com>
2025-10-28 16:49:44 +0530

    fix

--- apps/web/components/booking/BookingListItem.tsx
+++ apps/web/components/booking/BookingListItem.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { Controller, useFieldArray, useForm } from "react-hook-form";
@@ -555,8 +556,10 @@ function BookingListItem(booking: BookingItemProps) {
                             className="text-sm leading-6 text-blue-600 hover:underline dark:text-blue-400">
                             <div className="flex items-center gap-2">
                               {provider?.iconUrl && (
-                                <img
+                                <Image
                                   src={provider.iconUrl}
+                                  width={16}
+                                  height={16}
                                   className="h-4 w-4 rounded-sm"
                                   alt={`${provider?.label} logo`}
                                 />
@@ -1214,8 +1217,8 @@ const NoShowAttendeesDialog = ({
 
   return (
     <Dialog open={isOpen} onOpenChange={() => setIsOpen(false)}>
-      <DialogContent title={t("mark_as_no_show_title")} description={t("no_show_description")}>
-        {noShowAttendees.map((attendee) => (
+      <DialogContent title={t("mark_as_no_show_title")} description={t("no_show_description")} enableOverflow>
+        {noShowAttendees.map((attendee, index) => (
           <form
             key={attendee.id}
             onSubmit={(e) => {
@@ -1225,7 +1228,10 @@ const NoShowAttendeesDialog = ({
                 attendees: [{ email: attendee.email, noShow: !attendee.noShow }],
               });
             }}>
-            <div className="bg-muted flex items-center justify-between rounded-md px-4 py-2">
+            <div
+              className={`bg-muted flex items-center justify-between px-4 py-2 ${
+                index === 0 ? "rounded-t-md" : ""
+              } ${index === noShowAttendees.length - 1 ? "rounded-b-md" : "border-subtle border-b"}`}>
               <span className="text-emphasis flex flex-col text-sm">
                 {attendee.name}
                 {attendee.email && <span className="text-muted">({attendee.email})</span>}
