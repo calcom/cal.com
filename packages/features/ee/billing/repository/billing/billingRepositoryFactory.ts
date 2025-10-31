5412960b3fa746979c3cef754ceb9d8f97f24792
Joe Au-Yeung <j.auyeung419@gmail.com>
2025-10-30 21:18:47 -0400

    Return stub BillingRepository if billing is not enabled

--- packages/features/ee/billing/repository/billing/billingRepositoryFactory.ts
+++ packages/features/ee/billing/repository/billing/billingRepositoryFactory.ts
@@ -2,9 +2,14 @@ import { prisma } from "@calcom/prisma";
 
 import { PrismaOrganizationBillingRepository } from "./PrismaOrganizationBillingRepository";
 import { PrismaTeamBillingRepository } from "./PrismaTeamBillingRepository";
+import { StubBillingRepository } from "./StubBillingRepository";
 
 export class BillingRepositoryFactory {
-  static getRepository(isOrganization: boolean) {
+  static getRepository(isOrganization: boolean, isBillingEnabled: boolean) {
+    if (!isBillingEnabled) {
+      return new StubBillingRepository();
+    }
+
     if (isOrganization) {
       return new PrismaOrganizationBillingRepository(prisma);
     }
