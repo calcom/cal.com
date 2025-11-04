39c4461a1038a0586b30033ac7f989e5c2f5a392
Hariom Balhara <hariombalhara@gmail.com>
2025-11-04 16:59:48 +0530

    refactor(di): refactor logger service with DI module and update dependent containers

--- packages/features/bookings/di/BookingEventHandlerService.container.ts
+++ packages/features/bookings/di/BookingEventHandlerService.container.ts
@@ -1,6 +1,4 @@
 import { createContainer } from "@calcom/features/di/di";
-import { loggerServiceModule } from "@calcom/features/di/shared/services/logger.service";
-import { SHARED_TOKENS } from "@calcom/features/di/shared/shared.tokens";
 
 import {
   type BookingEventHandlerService,
@@ -9,10 +7,9 @@ import {
 
 const container = createContainer();
 
-export function getBookingEventHandlerService(): BookingEventHandlerService {
-  // Load logger module
-  container.load(SHARED_TOKENS.LOGGER, loggerServiceModule);
-  
+export function getBookingEventHandlerService() {
+
+  // Load the booking event handler module
   bookingEventHandlerServiceModule.loadModule(container);
 
   return container.get<BookingEventHandlerService>(bookingEventHandlerServiceModule.token);
