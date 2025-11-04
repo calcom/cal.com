887bb150ced9c4c16564344d3a2d9ddd831c818a
Sean Brydon <sean@cal.com>
2025-11-04 11:03:26 +0000

    remove onboarding card frame

--- apps/web/modules/onboarding/personal/_components/OnboardingCard.tsx
+++ apps/web/modules/onboarding/personal/_components/OnboardingCard.tsx
@@ -12,31 +12,29 @@ type OnboardingCardProps = {
 
 export const OnboardingCard = ({ title, subtitle, children, footer, isLoading }: OnboardingCardProps) => {
   return (
-    <div className="bg-muted border-muted relative flex h-full w-full flex-col rounded-xl border p-1">
-      <div className="rounded-inherit flex w-full flex-col items-start overflow-clip">
-        {/* Card Header */}
-        <div className="flex w-full gap-1.5 px-5 py-4">
-          <div className="flex w-full flex-col gap-1">
-            <h1 className="font-cal text-xl font-semibold leading-6">{title}</h1>
-            <p className="text-subtle text-sm font-medium leading-tight">{subtitle}</p>
-          </div>
+    <div className="relative flex h-full w-full flex-col">
+      {/* Card Header */}
+      <div className="flex w-full gap-1.5 px-5 py-4">
+        <div className="flex w-full flex-col gap-1">
+          <h1 className="font-cal text-xl font-semibold leading-6">{title}</h1>
+          <p className="text-subtle text-sm font-medium leading-tight">{subtitle}</p>
         </div>
-
-        {/* Content */}
-        <div className="flex w-full flex-col gap-4">
-          {isLoading ? (
-            <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
-              <SkeletonText className="h-40 w-full" />
-              <SkeletonText className="h-40 w-full" />
-            </div>
-          ) : (
-            children
-          )}
-        </div>
-
-        {/* Footer */}
-        <div className="flex w-full items-center justify-end gap-1 px-5 py-4">{footer}</div>
       </div>
+
+      {/* Content */}
+      <div className="flex w-full flex-col gap-4">
+        {isLoading ? (
+          <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
+            <SkeletonText className="h-40 w-full" />
+            <SkeletonText className="h-40 w-full" />
+          </div>
+        ) : (
+          children
+        )}
+      </div>
+
+      {/* Footer */}
+      <div className="flex w-full items-center justify-end gap-1 px-5 py-4">{footer}</div>
     </div>
   );
 };
