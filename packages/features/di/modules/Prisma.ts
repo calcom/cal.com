7ea9bbea97fb36ce6accb1f089fdd713036ef297
Joe Au-Yeung <j.auyeung419@gmail.com>
2025-11-07 12:04:07 -0500

    Undo changes made to Prisma module

--- packages/features/di/modules/Prisma.ts
+++ packages/features/di/modules/Prisma.ts
@@ -1,13 +1,13 @@
 import { type Container, createModule } from "@calcom/features/di/di";
 import { DI_TOKENS } from "@calcom/features/di/tokens";
-import { prisma, readonlyPrisma, type PrismaClient } from "@calcom/prisma";
+import { prisma, readonlyPrisma } from "@calcom/prisma";
 
 export const prismaModule = createModule();
 const token = DI_TOKENS.PRISMA_CLIENT;
 const readOnlyToken = DI_TOKENS.READ_ONLY_PRISMA_CLIENT;
 const moduleToken = DI_TOKENS.PRISMA_MODULE;
-prismaModule.bind(token).toFactory((): PrismaClient => prisma, "singleton");
-prismaModule.bind(readOnlyToken).toFactory((): PrismaClient => readonlyPrisma, "singleton");
+prismaModule.bind(token).toFactory(() => prisma, "singleton");
+prismaModule.bind(readOnlyToken).toFactory(() => readonlyPrisma, "singleton");
 
 export const moduleLoader = {
   token,
