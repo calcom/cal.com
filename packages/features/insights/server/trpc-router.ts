458ec5b8f6d229c1e002187b1c9704fa4fa00a21
Eunjae Lee <hey@eunjae.dev>
2025-11-08 16:23:18 +0100

    remove log

--- packages/features/insights/server/trpc-router.ts
+++ packages/features/insights/server/trpc-router.ts
@@ -857,7 +857,6 @@ export const insightsRouter = router({
   routingFormResponses: userBelongsToTeamProcedure
     .input(insightsRoutingServicePaginatedInputSchema)
     .query(async ({ ctx, input }) => {
-      console.log("💡 TEST", JSON.stringify(input, null, 2));
       const insightsRoutingService = createInsightsRoutingService(ctx, input);
       return await insightsRoutingService.getTableData({
         sorting: input.sorting,
