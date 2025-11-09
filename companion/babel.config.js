2a4d5be82be16849694664e765298372e00a596d
Abhijeet Singh <asingh9829@gmail.com>
2025-11-10 01:34:30 +0530

    init nativewind

---
+++ companion/babel.config.js
@@ -0,0 +1,10 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind", reanimated: false }],
+      "nativewind/babel",
+
+    ],
+  };
+};
