8090168f7089fbd55e726cece2a85aa624e73ccb
Alex van Andel <me@alexvanandel.com>
2025-11-10 10:30:35 +0000

    fix: APIv1 race conditions re. permissions checking

--- apps/api/v1/test/lib/utils/isAdmin.integration-test.ts
+++ apps/api/v1/test/lib/utils/isAdmin.integration-test.ts
@@ -13,29 +13,6 @@ type CustomNextApiResponse = NextApiResponse & Response;
 
 describe("isAdmin guard", () => {
   beforeAll(async () => {
-    const acmeOrg = await prisma.team.findFirst({
-      where: {
-        slug: "acme",
-        isOrganization: true,
-      },
-    });
-
-    if (acmeOrg) {
-      await prisma.organizationSettings.upsert({
-        where: {
-          organizationId: acmeOrg.id,
-        },
-        update: {
-          isAdminAPIEnabled: true,
-        },
-        create: {
-          organizationId: acmeOrg.id,
-          orgAutoAcceptEmail: "acme.com",
-          isAdminAPIEnabled: true,
-        },
-      });
-    }
-
     const dunderOrg = await prisma.team.findFirst({
       where: {
         slug: "dunder-mifflin",
