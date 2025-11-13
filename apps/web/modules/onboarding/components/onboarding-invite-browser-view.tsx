54f9f5af843f04089d0ec741f04aa1a3f1a30ee4
Sean Brydon <sean@cal.com>
2025-11-13 09:55:02 +0000

    WIP

--- apps/web/modules/onboarding/components/onboarding-invite-browser-view.tsx
+++ apps/web/modules/onboarding/components/onboarding-invite-browser-view.tsx
@@ -9,6 +9,12 @@ type OnboardingInviteBrowserViewProps = {
   teamName?: string;
 };
 
+const EmptyStateTeams = Array.from({ length: 9 }, () => ({
+  name: "team_member",
+  email: "name@example.com",
+  team: "Team",
+}));
+
 export const OnboardingInviteBrowserView = ({ teamName }: OnboardingInviteBrowserViewProps) => {
   const { data: user } = trpc.viewer.me.get.useQuery();
   const { teamBrand } = useOnboardingStore();
@@ -20,46 +26,41 @@ export const OnboardingInviteBrowserView = ({ teamName }: OnboardingInviteBrowse
   const teamAvatar = teamBrand.logo || null;
 
   return (
-    <div className="bg-default border-subtle hidden h-full w-full flex-col rounded-l-2xl border xl:flex">
-      {/* Browser header */}
-      <div className="border-subtle flex min-w-0 shrink-0 items-center gap-3 rounded-t-2xl border-b bg-white p-3">
-        {/* Navigation buttons */}
-        <div className="flex shrink-0 items-center gap-4 opacity-50">
-          <div className="h-3 w-3 rounded-full bg-gray-300" />
-          <div className="h-3 w-3 rounded-full bg-gray-300" />
-          <div className="h-3 w-3 rounded-full bg-gray-300" />
-        </div>
-        <div className="bg-muted flex w-full items-center gap-2 rounded-[32px] px-3 py-2">
-          <div className="h-3 w-3 rounded-full bg-gray-400" />
-          <p className="text-subtle text-xs font-medium leading-tight">mail.example.com</p>
-        </div>
-      </div>
-
+    <div className="bg-default border-subtle bg-muted hidden h-full w-full flex-col overflow-hidden rounded-l-2xl border xl:flex">
       {/* Content */}
-      <div className="bg-muted h-full pl-8 pt-8">
-        <div className="bg-default border-subtle flex h-full w-full flex-col overflow-hidden rounded-tl-2xl border-l border-t">
-          {/* Email Header */}
-          <div className="border-subtle bg-muted flex flex-col gap-4 border-b p-8">
-            <div className="flex flex-col items-start gap-4">
-              <Avatar
-                size="lg"
-                imageSrc={teamAvatar || undefined}
-                alt={displayTeamName}
-                className="border-default h-12 w-12 border-2"
-              />
-              <div className="flex w-full flex-col items-start gap-1">
-                <h2 className="text-emphasis font-cal w-full text-left text-xl font-semibold leading-tight">
-                  {displayInviterName} invited you to join {displayTeamName}
-                </h2>
-                <p className="text-subtle text-left text-sm font-normal leading-tight">
-                  We're emailing you all the details
-                </p>
-              </div>
+      <div className="h-full px-6 pt-6">
+        <div className="bg-default border-subtle flex flex-col gap-4 rounded-2xl border p-8">
+          <div className="flex flex-col items-start gap-4">
+            <Avatar
+              size="lg"
+              imageSrc={teamAvatar || undefined}
+              alt={displayTeamName}
+              className="border-default h-12 w-12 border-2"
+            />
+            <div className="flex w-full flex-col items-start gap-1">
+              <h2 className="text-emphasis font-cal w-full text-left text-xl font-semibold leading-tight">
+                {displayInviterName} invited you to join {displayTeamName}
+              </h2>
+              <p className="text-subtle text-left text-sm font-normal leading-tight">
+                We're emailing you all the details
+              </p>
             </div>
           </div>
+        </div>
 
-          {/* Email Body */}
-          <div className="bg-default flex-1 rounded-bl-2xl" />
+        {/* Email Body */}
+        <div className="bg-default border-subtle mt-3 grid grid-cols-3 gap-4 rounded-t-2xl border p-6 opacity-60">
+          {EmptyStateTeams.map((team) => (
+            <div
+              key={team.name}
+              className="bg-default border-subtle flex aspect-square flex-col items-center justify-center gap-2 rounded-lg border p-4">
+              <Avatar size="sm" imageSrc={team.avatar} alt={team.name} />
+              <div className="flex flex-col items-center gap-1">
+                <p className="text-default text-sm font-semibold leading-tight">{team.name}</p>
+                <p className="text-subtle text-xs font-medium leading-tight">{team.email}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
