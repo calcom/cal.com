4b9b12c01807045d8a85c992ab4f0fc8b681229a
PG <pranavghorpade61@gmail.com>
2025-11-16 18:49:22 +0000

    chore: remove prisma migrations again

--- packages/prisma/migrations/20251115064352_add_custom_reminder_to_destination_calendar/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."DestinationCalendar" ADD COLUMN     "customReminder" INTEGER DEFAULT 10;
--- packages/prisma/migrations/20251115085651_reminder_minutes/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."DestinationCalendar" ADD COLUMN     "reminderMinutes" INTEGER;
