87372b7fda5c307883d2408724aff824c2a07c33
Spandan-Mishra <spandanmishra69@gmail.com>
2025-11-17 16:54:52 +0530

    fix: made playground page use same layout as other pages

--- apps/web/app/(use-page-wrapper)/settings/(admin-layout)/admin/playground/page.tsx
+++ apps/web/app/(use-page-wrapper)/settings/(admin-layout)/admin/playground/page.tsx
@@ -1,4 +1,7 @@
 import Link from "next/link";
+import SettingsHeader from "@calcom/features/settings/appDir/SettingsHeader";
+import { _generateMetadata, getTranslate } from "app/_utils";
+import { Table } from "@calcom/ui/components/table";
 
 const LINKS = [
   {
@@ -15,20 +18,41 @@ const LINKS = [
   },
 ];
 
-export default function Page() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold">Playground</h1>
+const { Cell, Header, Row, ColumnTitle } = Table;
 
-      <ul className="mt-8">
+export const generateMetadata = async () =>
+  await _generateMetadata(
+    (t) => t("playground"),
+    (t) => t("admin_playground_description"),
+    undefined,
+    undefined,
+    "/settings/admin/playground"
+  );
+
+const Page = async () => {
+  const t = await getTranslate();
+  return (
+    <SettingsHeader
+      title={t("playground")}
+      description={t("admin_playground_description")}
+    >
+      <Table>
+        <Header>
+          <ColumnTitle widthClassNames="w-auto">Feature</ColumnTitle>
+        </Header>
         {LINKS.map((link) => (
-          <li key={link.title}>
-            <Link href={link.href} className="list-item list-disc font-medium underline">
-              {link.title} →
+          <Row key={link.title}>
+            <Cell widthClassNames="w-auto">
+              <Link href={link.href} className="font-medium">
+              {link.title}
             </Link>
-          </li>
+            </Cell>
+          </Row>
         ))}
-      </ul>
-    </div>
+      </Table>
+        
+    </SettingsHeader>
   );
 }
+
+export default Page;
