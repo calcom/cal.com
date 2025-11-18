8978f2459d577628f025eb46f54bb24eeafb10a9
Sean Brydon <sean@cal.com>
2025-11-18 13:14:42 +0000

    Make banner smaller

--- apps/web/modules/onboarding/components/onboarding-organization-browser-view.tsx
+++ apps/web/modules/onboarding/components/onboarding-organization-browser-view.tsx
@@ -112,7 +112,7 @@ export const OnboardingOrganizationBrowserView = ({
             <div className="border-subtle flex flex-col border-b">
               <div className="relative">
                 {/* Banner Image */}
-                <div className="border-subtle relative h-40 w-full overflow-hidden rounded-t-xl border-b">
+                <div className="border-subtle relative h-36 w-full overflow-hidden rounded-t-xl border-b">
                   {bannerUrl ? (
                     <img src={bannerUrl} alt={name || ""} className="h-full w-full object-cover" />
                   ) : (
