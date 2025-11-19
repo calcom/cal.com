f25fe986727312e7d2a0f869881ba457297bdc8c
Udit Takkar <udit222001@gmail.com>
2025-11-20 01:34:17 +0530

    chore: use org

--- packages/trpc/server/routers/loggedInViewer/connectAndJoin.handler.ts
+++ packages/trpc/server/routers/loggedInViewer/connectAndJoin.handler.ts
@@ -213,7 +213,7 @@ export const Handler = async ({ ctx, input }: Options) => {
     eventTypeId: eventType?.id,
     videoCallData,
     customReplyToEmail: eventType?.customReplyToEmail,
-    organizationId: user.profile?.organizationId ?? null,
+    organizationId: user?.organizationId ?? null,
     team: updatedBooking.eventType?.team
       ? {
           name: updatedBooking.eventType.team.name,
