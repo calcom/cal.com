ae73fb1daa8841b92556d9ec809661776cf3393c
supalarry <laurisskraucis@gmail.com>
2025-11-20 16:51:42 +0100

    fix: e2e

--- apps/api/v2/src/ee/bookings/2024-04-15/controllers/reserved-slot-bookings.e2e-spec.ts
+++ apps/api/v2/src/ee/bookings/2024-04-15/controllers/reserved-slot-bookings.e2e-spec.ts
@@ -265,8 +265,8 @@ describe("Reserved Slot Bookings Endpoints 2024-04-15", () => {
 
         it("should create booking with reservedSlotUid from cookie and remove selected slot", async () => {
           const reservedSlotUid = `reserved-slot-${randomString()}`;
-          const startTime = new Date("2040-05-23T09:30:00.000Z");
-          const endTime = new Date("2040-05-23T10:30:00.000Z");
+          const startTime = new Date("2040-05-24T11:30:00.000Z");
+          const endTime = new Date("2040-05-24T12:30:00.000Z");
 
           await createReservedSlot(user.id, recurringEventTypeId, reservedSlotUid, startTime, endTime);
 
@@ -303,8 +303,8 @@ describe("Reserved Slot Bookings Endpoints 2024-04-15", () => {
 
         it("should create booking with reservedSlotUid from request body and remove selected slot", async () => {
           const reservedSlotUid = `reserved-slot-${randomString()}`;
-          const startTime = new Date("2040-05-21T11:30:00.000Z");
-          const endTime = new Date("2040-05-21T12:30:00.000Z");
+          const startTime = new Date("2040-05-25T11:30:00.000Z");
+          const endTime = new Date("2040-05-25T12:30:00.000Z");
 
           await createReservedSlot(user.id, recurringEventTypeId, reservedSlotUid, startTime, endTime);
 
