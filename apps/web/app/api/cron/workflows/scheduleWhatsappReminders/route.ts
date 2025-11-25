f550ec5b93f59270b888ca081194abde30f835fb
hbjORbj <sldisek783@gmail.com>
2025-11-25 17:00:36 +0900

    fix

--- apps/web/app/api/cron/workflows/scheduleWhatsappReminders/route.ts
+++ apps/web/app/api/cron/workflows/scheduleWhatsappReminders/route.ts
@@ -1,10 +1,10 @@
 import { defaultResponderForAppDir } from "app/api/defaultResponderForAppDir";
 
 import { CreditService } from "@calcom/features/ee/billing/credit-service";
-import { makeHandler } from "@calcom/features/ee/workflows/api/scheduleWhatsappReminders";
+import { handler } from "@calcom/features/ee/workflows/api/scheduleWhatsappReminders";
 
 const creditService = new CreditService();
 const creditCheckFn = ({ userId, teamId }: { userId?: number | null; teamId?: number | null }) =>
   creditService.hasAvailableCredits({ userId, teamId });
 
-export const POST = defaultResponderForAppDir(makeHandler({ creditCheckFn }));
+export const POST = defaultResponderForAppDir(handler({ creditCheckFn }));
