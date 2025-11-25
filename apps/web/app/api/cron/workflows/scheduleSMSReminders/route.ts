d72f73029b46496083f76242000b6059a00d57a6
hbjORbj <sldisek783@gmail.com>
2025-11-25 17:01:33 +0900

    refactor

--- apps/web/app/api/cron/workflows/scheduleSMSReminders/route.ts
+++ apps/web/app/api/cron/workflows/scheduleSMSReminders/route.ts
@@ -4,7 +4,4 @@ import { CreditService } from "@calcom/features/ee/billing/credit-service";
 import { handler } from "@calcom/features/ee/workflows/api/scheduleSMSReminders";
 
 const creditService = new CreditService();
-const creditCheckFn = ({ userId, teamId }: { userId?: number | null; teamId?: number | null }) =>
-  creditService.hasAvailableCredits({ userId, teamId });
-
-export const POST = defaultResponderForAppDir(handler({ creditCheckFn }));
+export const POST = defaultResponderForAppDir(handler({ creditCheckFn: creditService.hasAvailableCredits }));
