6676c48653b5407696a34d1d0cb7316885e82b6e
Anik Dhabal Babu <81948346+anikdhabal@users.noreply.github.com>
2025-11-28 13:28:05 +0530

    Fix variable index lookup in substituteVariables.ts

--- packages/app-store/routing-forms/lib/substituteVariables.ts
+++ packages/app-store/routing-forms/lib/substituteVariables.ts
@@ -40,7 +40,7 @@ export const substituteVariables = (
         const humanReadableString = humanReadableValues.toString();
 
         const variablePattern = `{${variable}}`;
-        const variableIndex = routeValue.indexOf(variablePattern);
+        const variableIndex = eventTypeUrl.indexOf(variablePattern);
         const isInQueryString = queryStringIndex !== -1 && variableIndex > queryStringIndex;
 
         let valueToSubstitute: string;
