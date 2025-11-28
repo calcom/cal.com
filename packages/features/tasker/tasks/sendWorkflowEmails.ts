72c990c7904c37cdabbd4f70a5b6717ea4cb1c10
Joe Au-Yeung <j.auyeung419@gmail.com>
2025-11-28 00:12:08 -0500

    Use services in tasker

--- packages/features/tasker/tasks/sendWorkflowEmails.ts
+++ packages/features/tasker/tasks/sendWorkflowEmails.ts
@@ -1,6 +1,12 @@
 import { z } from "zod";
 
 import { sendCustomWorkflowEmail } from "@calcom/emails/workflow-email-service";
+import { CalendarEventBuilder } from "@calcom/features/CalendarEventBuilder";
+import { BookingRepository } from "@calcom/features/bookings/repositories/BookingRepository";
+import { BookingSeatRepository } from "@calcom/features/bookings/repositories/BookingSeatRepository";
+import { EmailWorkflowService } from "@calcom/features/ee/workflows/lib/service/EmailWorkflowService";
+import { WorkflowReminderRepository } from "@calcom/features/ee/workflows/repositories/WorkflowReminderRepository";
+import { prisma } from "@calcom/prisma";
 
 export const ZSendWorkflowEmailsSchemaEager = z.object({
   to: z.array(z.string()),
@@ -32,6 +38,32 @@ export const ZSendWorkflowEmailsSchema = z.union([
 
 export async function sendWorkflowEmails(payload: string): Promise<void> {
   const mailData = ZSendWorkflowEmailsSchema.parse(JSON.parse(payload));
+
+  // Generate workflow body to send
+  if ("bookingUid" in mailData && "workflowReminderId" in mailData) {
+    const bookingRepository = new BookingRepository(prisma);
+    const booking = await bookingRepository.getBookingForCalEventBuilderFromUid(mailData.bookingUid);
+
+    if (!booking) {
+      throw new Error("Booking not found");
+    }
+
+    const calendarEvent = (await CalendarEventBuilder.fromBooking(booking, {})).build();
+
+    if (!calendarEvent) {
+      throw new Error("Calendar event could not be built");
+    }
+    const workflowReminderRepository = new WorkflowReminderRepository(prisma);
+    const bookingSeatRepository = new BookingSeatRepository(prisma);
+    const emailWorkflowService = new EmailWorkflowService(workflowReminderRepository, bookingSeatRepository);
+
+    await emailWorkflowService.handleSendEmailWorkflowTask({
+      evt: calendarEvent,
+      workflowReminderId: mailData.workflowReminderId,
+    });
+
+    return;
+  }
 
   await Promise.all(
     mailData.to.map((to) =>
