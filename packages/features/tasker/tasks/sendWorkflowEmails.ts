23443d9fa7881b19048a4fafb850684635457e6c
Joe Au-Yeung <j.auyeung419@gmail.com>
2025-11-27 20:09:57 -0500

    `sendWorkflowEmails` tasker to accept lazy payload

--- packages/features/tasker/tasks/sendWorkflowEmails.ts
+++ packages/features/tasker/tasks/sendWorkflowEmails.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 import { sendCustomWorkflowEmail } from "@calcom/emails/workflow-email-service";
 
-export const ZSendWorkflowEmailsSchema = z.object({
+export const ZSendWorkflowEmailsSchemaEager = z.object({
   to: z.array(z.string()),
   subject: z.string(),
   html: z.string(),
@@ -19,6 +19,16 @@ export const ZSendWorkflowEmailsSchema = z.object({
     )
     .optional(),
 });
+
+const ZSendWorkflowEmailsSchemaLazy = z.object({
+  bookingUid: z.string(),
+  workflowStepId: z.number(),
+});
+
+export const ZSendWorkflowEmailsSchema = z.union([
+  ZSendWorkflowEmailsSchemaEager,
+  ZSendWorkflowEmailsSchemaLazy,
+]);
 
 export async function sendWorkflowEmails(payload: string): Promise<void> {
   const mailData = ZSendWorkflowEmailsSchema.parse(JSON.parse(payload));
