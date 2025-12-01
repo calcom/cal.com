af8adfe1f48457e0e29a7cc1326741577526bc30
Mauryan Kansara <mauryankansaramk@gmail.com>
2025-12-01 22:06:46 +0530

    fix: include location in handleCreateBooking payload

---
+++ packages/lib/bookings/getLocationValue.ts
@@ -0,0 +1,27 @@
+import logger from "@calcom/lib/logger";
+
+export const getLocationValue = (
+  location: string | undefined,
+  responseLocation: string | { value: unknown } | undefined
+): string | undefined => {
+  if (location) return location;
+
+  if (typeof responseLocation === "string") return responseLocation;
+
+  if (
+    responseLocation &&
+    typeof responseLocation === "object" &&
+    "value" in responseLocation &&
+    typeof responseLocation.value === "string"
+  ) {
+    return responseLocation.value;
+  }
+
+  logger.warn(
+    `getLocationValue - location ${location} and response location ${JSON.stringify(
+      responseLocation
+    )} were passed but the type is not supported.`
+  );
+
+  return undefined;
+};
