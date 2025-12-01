8944ccbc47af9739354775f679a0e12baba98b74
hbjORbj <sldisek783@gmail.com>
2025-12-01 16:27:09 +0900

    revert

--- packages/trpc/server/trpc.ts
+++ packages/trpc/server/trpc.ts
@@ -13,10 +13,5 @@ export const tRPCContext = initTRPC.context<typeof createContextInner>().create(
 export const router = tRPCContext.router;
 export const mergeRouters = tRPCContext.mergeRouters;
 export const middleware = tRPCContext.middleware;
-/**
- * Base procedure with error mapping middleware.
- * This converts ErrorWithCode thrown by feature layer code into TRPCError,
- * allowing features to remain transport-agnostic.
- */
 export const procedure = tRPCContext.procedure;
 export const createCallerFactory = tRPCContext.createCallerFactory;
