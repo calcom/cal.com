7023b195d42ef3002ea2a57e9476ddaf0e179b59
SleepyAsync <hoangphu9685@gmail.com>
2025-12-01 23:46:30 +0700

    fix: prevent overlap in EditableHeading (GH-25498)

--- packages/ui/components/editable-heading/EditableHeading.tsx
+++ packages/ui/components/editable-heading/EditableHeading.tsx
@@ -22,8 +22,10 @@ export const EditableHeading = function EditableHeading({
   return (
     <div className="group pointer-events-auto relative truncate" onClick={enableEditing}>
       <div className={classNames(!disabled && "cursor-pointer", "flex items-center")}>
-        <label className="min-w-8 relative inline-block">
-          <span className="whitespace-pre text-xl tracking-normal text-transparent">{value}&nbsp;</span>
+        <label className="relative inline-block max-w-full">
+          <span className="block max-w-full whitespace-pre text-xl tracking-normal text-transparent pr-5">
+            {value}&nbsp;
+          </span>
           <input
             {...passThroughProps}
             disabled={disabled}
@@ -33,7 +35,7 @@ export const EditableHeading = function EditableHeading({
             className={classNames(
               !disabled &&
                 "hover:text-default focus:text-emphasis cursor-pointer focus:outline-none focus:ring-0",
-              "text-emphasis absolute left-0 top-0 w-full truncate border-none bg-transparent p-0 align-top text-xl ",
+              "text-emphasis absolute left-0 top-0 w-full truncate border-none bg-transparent p-0 pr-5 align-top text-xl",
               passThroughProps.className
             )}
             onFocus={(e) => {
@@ -47,7 +49,10 @@ export const EditableHeading = function EditableHeading({
             onChange={(e) => onChange && onChange(e.target.value)}
           />
           {!isEditing && isReady && !disabled && (
-            <Icon name="pencil" className="text-subtle group-hover:text-subtle -mt-px ml-1 inline  h-3 w-3" />
+            <Icon
+              name="pencil"
+              className="text-subtle group-hover:text-subtle pointer-events-none absolute right-0 top-1/2 h-3 w-3 -translate-y-1/2"
+            />
           )}
         </label>
       </div>
