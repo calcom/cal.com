99834ce7ede1a4a3e78b705bbed1b5f8bac4b11b
Hariom Balhara <hariombalhara@gmail.com>
2025-12-02 14:24:52 +0530

    Remove Dynamic Posthog Provider from AppProvider.tsx because that is only used by the pages router, and the pages router is only in use by the /router endpoint, which is a booking page and we havent implemented corresponding GeoProvider support for it

--- apps/web/lib/hooks/useIsBookingPage.ts
+++ apps/web/lib/hooks/useIsBookingPage.ts
@@ -1,7 +1,9 @@
 import { usePathname } from "next/navigation";
 
 import { useCompatSearchParams } from "@calcom/lib/hooks/useCompatSearchParams";
-
+// TODO: This approach of checking booking page isn't correct.
+// app.cal.com/rick is a booking page but useIsBookingPage won't return true. This is because all unregistered router in Next.js could technically be a booking page throw catch all routes.
+// The only way to confirm it is by actually checking if we actually rendered a booking route.
 export default function useIsBookingPage(): boolean {
   const pathname = usePathname();
   const isBookingPage = [
