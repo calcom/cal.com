1547f2bb2aeb0d48dae268e1b8c8b185da52f31b
Hariom Balhara <hariombalhara@gmail.com>
2025-12-03 14:12:55 +0530

    fix module deps

--- apps/api/v2/src/ee/event-types-private-links/event-types-private-links.module.ts
+++ apps/api/v2/src/ee/event-types-private-links/event-types-private-links.module.ts
@@ -4,15 +4,18 @@ import { EventTypeOwnershipGuard } from "@/modules/event-types/guards/event-type
 import { PrismaModule } from "@/modules/prisma/prisma.module";
 import { TokensModule } from "@/modules/tokens/tokens.module";
 import { Module } from "@nestjs/common";
-
+import { OrganizationsRepository } from "@/modules/organizations/index/organizations.repository";
 import { EventTypesPrivateLinksController } from "./controllers/event-types-private-links.controller";
 import { PrivateLinksRepository } from "./private-links.repository";
 import { PrivateLinksInputService } from "./services/private-links-input.service";
 import { PrivateLinksOutputService } from "./services/private-links-output.service";
 import { PrivateLinksService } from "./services/private-links.service";
-
+import { UsersRepository } from "@/modules/users/users.repository";
+import { StripeModule } from "@/modules/stripe/stripe.module";
+import { IsNotPlatformOrgGuard } from "@/modules/auth/guards/organizations/is-not-platform-org.guard";
 @Module({
-  imports: [TokensModule, PrismaModule, EventTypesModule_2024_06_14],
+  imports: [TokensModule, PrismaModule, EventTypesModule_2024_06_14, StripeModule,
+  ],
   controllers: [EventTypesPrivateLinksController],
   providers: [
     PrivateLinksService,
@@ -21,7 +24,10 @@ import { PrivateLinksService } from "./services/private-links.service";
     PrivateLinksRepository,
     EventTypeOwnershipGuard,
     IsNotManagedUserGuard,
+    IsNotPlatformOrgGuard,
+    UsersRepository,
+    OrganizationsRepository,
   ],
   exports: [PrivateLinksService],
 })
-export class EventTypesPrivateLinksModule {}
+export class EventTypesPrivateLinksModule { }
