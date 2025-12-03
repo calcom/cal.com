d3bf3c6689b6f9dace85a9a510286c8861f89d3f
cal.com <morgan@cal.com>
2025-12-03 11:42:47 +0200

    fix: module imports

--- apps/api/v2/src/ee/event-types-private-links/event-types-private-links.module.ts
+++ apps/api/v2/src/ee/event-types-private-links/event-types-private-links.module.ts
@@ -3,19 +3,17 @@ import { IsNotManagedUserGuard } from "@/modules/auth/guards/users/is-not-manage
 import { EventTypeOwnershipGuard } from "@/modules/event-types/guards/event-type-ownership.guard";
 import { PrismaModule } from "@/modules/prisma/prisma.module";
 import { TokensModule } from "@/modules/tokens/tokens.module";
+import { UsersRepository } from "@/modules/users/users.repository";
 import { Module } from "@nestjs/common";
-import { OrganizationsRepository } from "@/modules/organizations/index/organizations.repository";
+
 import { EventTypesPrivateLinksController } from "./controllers/event-types-private-links.controller";
 import { PrivateLinksRepository } from "./private-links.repository";
 import { PrivateLinksInputService } from "./services/private-links-input.service";
 import { PrivateLinksOutputService } from "./services/private-links-output.service";
 import { PrivateLinksService } from "./services/private-links.service";
-import { UsersRepository } from "@/modules/users/users.repository";
-import { StripeModule } from "@/modules/stripe/stripe.module";
-import { IsNotPlatformOrgGuard } from "@/modules/auth/guards/organizations/is-not-platform-org.guard";
+
 @Module({
-  imports: [TokensModule, PrismaModule, EventTypesModule_2024_06_14, StripeModule,
-  ],
+  imports: [TokensModule, PrismaModule, EventTypesModule_2024_06_14],
   controllers: [EventTypesPrivateLinksController],
   providers: [
     PrivateLinksService,
@@ -24,10 +22,8 @@ import { IsNotPlatformOrgGuard } from "@/modules/auth/guards/organizations/is-no
     PrivateLinksRepository,
     EventTypeOwnershipGuard,
     IsNotManagedUserGuard,
-    IsNotPlatformOrgGuard,
     UsersRepository,
-    OrganizationsRepository,
   ],
   exports: [PrivateLinksService],
 })
-export class EventTypesPrivateLinksModule { }
+export class EventTypesPrivateLinksModule {}
