c3d6538352fa41c12c370d16611ba510a5509ff3
Emmanuel Isenah <emmanuelisenah@gmail.com>
2025-12-07 10:09:34 +0100

    fix: add `bookingLimits` to mock User object

--- packages/features/eventtypes/lib/defaultEvents.ts
+++ packages/features/eventtypes/lib/defaultEvents.ts
@@ -56,6 +56,7 @@ const user: User & { credentials: CredentialPayload[] } = {
   travelSchedules: [],
   locked: false,
   isPlatformManaged: false,
+  bookingLimits: null,
 };
 
 const customInputs: CustomInputSchema[] = [];
