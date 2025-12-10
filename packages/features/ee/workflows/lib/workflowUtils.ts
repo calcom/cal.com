2dd3d38efac5cc7ea2d1dd5449b9dbcc42d10847
hbjORbj <sldisek783@gmail.com>
2025-12-10 18:21:07 +0900

    remove workflowUtils

--- packages/features/ee/workflows/lib/workflowUtils.ts
+++
@@ -1,5 +0,0 @@
-// Re-export all workflow utilities from their individual files
-export { isAuthorized } from "./isAuthorized";
-export { getAllWorkflowsFromEventType } from "./getAllWorkflowsFromEventType";
-export { scheduleWorkflowNotifications } from "./scheduleWorkflowNotifications";
-export { scheduleBookingReminders } from "./scheduleBookingReminders";
