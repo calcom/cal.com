7857f09d5f8545260b890020f252a05c4942a309
Anik Dhabal Babu <81948346+anikdhabal@users.noreply.github.com>
2025-12-11 22:29:20 +0530

    Fix key format for huddle in input-event-types.service

--- apps/api/v2/src/ee/event-types/event-types_2024_06_14/services/input-event-types.service.ts
+++ apps/api/v2/src/ee/event-types/event-types_2024_06_14/services/input-event-types.service.ts
@@ -581,7 +581,7 @@ export class InputEventTypesService_2024_06_14 {
       "mirotalk-video": "mirotalk",
       "jelly-video": "jelly",
       "jelly-conferencing": "jelly",
-      huddle: "huddle01",
+      "huddle": "huddle01",
       "element-call-video": "element-call",
       "eightxeight-video": "eightxeight",
       "discord-video": "discord",
