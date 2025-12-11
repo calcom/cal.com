8d0d61f177e2471266ddc1ab0dedc8c0c5c7fd62
Joe Au-Yeung <j.auyeung419@gmail.com>
2025-12-11 14:12:07 -0500

    Add more services to `IntegrationAttributeSyncService` - getById - Init updateIncludeRulesAndMappings

--- packages/features/ee/integration-attribute-sync/services/IntegrationAttributeSyncService.ts
+++ packages/features/ee/integration-attribute-sync/services/IntegrationAttributeSyncService.ts
@@ -2,6 +2,7 @@ import type { CredentialRepository } from "@calcom/features/credentials/reposito
 
 import { enabledAppSlugs } from "../constants";
 import type { IIntegrationAttributeSyncRepository } from "../repositories/IIntegrationAttributeSyncRepository";
+import type { ISyncFormData } from "../schemas/zod";
 
 interface IIntegrationAttributeSyncServiceDeps {
   credentialRepository: CredentialRepository;
@@ -11,14 +12,37 @@ interface IIntegrationAttributeSyncServiceDeps {
 export class IntegrationAttributeSyncService {
   constructor(private readonly deps: IIntegrationAttributeSyncServiceDeps) {}
 
-  async getEnabledAppCredentials({ organizationId }: { organizationId: number }) {
+  async getEnabledAppCredentials(organizationId: number) {
     return this.deps.credentialRepository.findByTeamIdAndSlugs({
       teamId: organizationId,
       slugs: enabledAppSlugs,
     });
   }
 
-  async getAllAttributeSyncs(organizationId: number) {
-    return this.deps.integrationAttributeSyncRepository.getIntegrationAttributeSyncs(organizationId);
+  async getAllIntegrationAttributeSyncs(organizationId: number) {
+    return this.deps.integrationAttributeSyncRepository.getByOrganizationId(organizationId);
+  }
+
+  async getById(id: string) {
+    return this.deps.integrationAttributeSyncRepository.getById(id);
+  }
+
+  async updateIncludeRulesAndMappings(data: ISyncFormData) {
+    const { _syncFieldMappings, _rule, ..._integrationAttributeSync } = data;
+    // Get existing mappings to determine what to delete
+    const existingMappings = await this.deps.integrationAttributeSyncRepository.getSyncFieldMappings(data.id);
+
+    // Determine which mappings to delete
+    const existingMappingIds = new Set(existingMappings.map((m) => m.id));
+    const incomingMappingIds = new Set(data.syncFieldMappings.filter((m) => m.id).map((m) => m.id!));
+
+    const toDelete = Array.from(existingMappingIds).filter((id) => !incomingMappingIds.has(id));
+
+    // Mappings without IDs are new, mappings with IDs need to be updated
+    const toCreate = data.syncFieldMappings.filter((m) => !m.id);
+    const toUpdate = data.syncFieldMappings.filter((m) => m.id);
+
+    // TODO: Implement the actual update call to the repository
+    console.log({ toDelete, toCreate, toUpdate });
   }
 }
