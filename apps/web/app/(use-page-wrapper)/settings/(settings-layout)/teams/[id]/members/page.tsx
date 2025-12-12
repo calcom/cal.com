76e19b04cad882af955bc79ebbcced0d4d5b12bf
avnotaklu <sukhmander111101@gmail.com>
2025-12-12 15:35:54 +0530

    Fix for metadata in all parts of the app

--- apps/web/app/(use-page-wrapper)/settings/(settings-layout)/teams/[id]/members/page.tsx
+++ apps/web/app/(use-page-wrapper)/settings/(settings-layout)/teams/[id]/members/page.tsx
@@ -3,6 +3,21 @@ import TeamMembersView from "@calid/features/modules/teams/settings/TeamMembersV
 
 // import MembersView from "@calcom/features/ee/teams/pages/team-members-view";
 
+import { _generateMetadata } from "app/_utils";
+
+export const generateMetadata = async ({ params }: { params: Promise<{ pages: string[] }> }) => {
+  const teamId = (await params).id;
+
+  return await _generateMetadata(
+    (t) => `${t("team_members")}`,
+    (t) => `${t("manage_team_members")}`,
+    undefined,
+    undefined,
+    `/settings/teams/${teamId}/members`
+  );
+};
+
+
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
   const teamId = parseInt(id);
