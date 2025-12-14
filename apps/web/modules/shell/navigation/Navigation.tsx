16df9230b30e7304501dd14f23e89130fe958d73
hbjORbj <sldisek783@gmail.com>
2025-12-15 02:41:23 +0900

    wip

--- apps/web/modules/shell/navigation/Navigation.tsx
+++ apps/web/modules/shell/navigation/Navigation.tsx
@@ -2,7 +2,6 @@ import { useSession } from "next-auth/react";
 import { useMemo } from "react";
 
 import { useIsEmbed } from "@calcom/embed-core/embed-iframe";
-import UnconfirmedBookingBadge from "@calcom/features/bookings/UnconfirmedBookingBadge";
 import { useHasPaidPlan } from "@calcom/features/billing/hooks/useHasPaidPlan";
 import {
   useOrgBranding,
@@ -10,6 +9,8 @@ import {
 } from "@calcom/features/ee/organizations/context/provider";
 import { KBarTrigger } from "@calcom/features/kbar/Kbar";
 import classNames from "@calcom/ui/classNames";
+
+import UnconfirmedBookingBadge from "~/bookings/components/UnconfirmedBookingBadge";
 
 import { TeamInviteBadge } from "../TeamInviteBadge";
 import type { NavigationItemType } from "./NavigationItem";
@@ -29,7 +30,6 @@ const getNavigationItems = (
   orgBranding: OrganizationBranding,
   hasInsightsAccess: boolean
 ): NavigationItemType[] => [
- 
   {
     name: "event_types_page_title",
     href: "/event-types",
