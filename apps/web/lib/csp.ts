0735519d4969e2a19c51d141a06304f651b6f64c
nandavarampritham <nandavarampritham@gmail.com>
2025-12-14 21:40:46 +0530

    style: fix csp.ts formatting

--- apps/web/lib/csp.ts
+++ apps/web/lib/csp.ts
@@ -16,16 +16,18 @@ function getCspPolicy(nonce: string) {
   // FIXME: Write a layer to extract out EventType Analytics tracking endpoints and add them to img-src or connect-src as needed. e.g. fathom, Google Analytics and others
   return `
 	  default-src 'self' ${IS_PRODUCTION ? "" : "data:"};
-	  script-src ${IS_PRODUCTION
-      ? // 'self' 'unsafe-inline' https: added for Browsers not supporting strict-dynamic
-      `'nonce-${nonce}' 'strict-dynamic' 'self' 'unsafe-inline' https:`
-      : // Note: We could use 'strict-dynamic' with 'nonce-..' instead of unsafe-inline but there are some streaming related scripts that get blocked(because they don't have nonce on them). It causes a really frustrating full page error model by Next.js to show up sometimes
-      "'unsafe-inline' 'unsafe-eval' https: http:"
+	  script-src ${
+      IS_PRODUCTION
+        ? // 'self' 'unsafe-inline' https: added for Browsers not supporting strict-dynamic
+          `'nonce-${nonce}' 'strict-dynamic' 'self' 'unsafe-inline' https:`
+        : // Note: We could use 'strict-dynamic' with 'nonce-..' instead of unsafe-inline but there are some streaming related scripts that get blocked(because they don't have nonce on them). It causes a really frustrating full page error model by Next.js to show up sometimes
+          "'unsafe-inline' 'unsafe-eval' https: http:"
     };
     object-src 'none';
     base-uri 'none';
 	  child-src app.cal.com;
-	  style-src 'self' ${IS_PRODUCTION ? (useNonStrictPolicy ? "'unsafe-inline'" : "") : "'unsafe-inline'"
+	  style-src 'self' ${
+      IS_PRODUCTION ? (useNonStrictPolicy ? "'unsafe-inline'" : "") : "'unsafe-inline'"
     } app.cal.com;
 	  font-src 'self';
 	  img-src 'self' ${WEBAPP_URL} https://img.youtube.com data:;
