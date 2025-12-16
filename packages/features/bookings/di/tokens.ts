c44fcff9ded1eb1180cbb6a34588138cc54b2166
Hariom Balhara <hariombalhara@gmail.com>
2025-12-16 15:41:51 +0530

    fix formatting issue

--- packages/features/bookings/di/tokens.ts
+++ packages/features/bookings/di/tokens.ts
@@ -7,7 +7,7 @@ export const BOOKING_DI_TOKENS = {
   INSTANT_BOOKING_CREATE_SERVICE_MODULE: Symbol("InstantBookingCreateServiceModule"),
   BOOKING_CANCEL_SERVICE: Symbol("BookingCancelService"),
   BOOKING_CANCEL_SERVICE_MODULE: Symbol("BookingCancelServiceModule"),
-BOOKING_REFERENCE_REPOSITORY: Symbol("BookingReferenceRepository"),
+  BOOKING_REFERENCE_REPOSITORY: Symbol("BookingReferenceRepository"),
   BOOKING_REFERENCE_REPOSITORY_MODULE: Symbol("BookingReferenceRepositoryModule"),
   BOOKING_ATTENDEE_REPOSITORY: Symbol("BookingAttendeeRepository"),
   BOOKING_ATTENDEE_REPOSITORY_MODULE: Symbol("BookingAttendeeRepositoryModule"),
