4343c117f4b4c1c0c69a9730f872e2fbdbd0fbb2
Anik Dhabal Babu <adhabal2002@gmail.com>
2025-12-18 13:48:44 +0530

    using service

--- packages/trpc/server/routers/viewer/organizations/adminUpdate.handler.ts
+++ packages/trpc/server/routers/viewer/organizations/adminUpdate.handler.ts
@@ -1,11 +1,6 @@
-import { getOrgFullOrigin } from "@calcom/ee/organizations/lib/orgDomains";
 import { getOrganizationRepository } from "@calcom/features/ee/organizations/di/OrganizationRepository.container";
-import { renameDomain } from "@calcom/lib/domainManager/organization";
-import { getMetadataHelpers } from "@calcom/lib/getMetadataHelpers";
-import { HttpError } from "@calcom/lib/http-error";
+import { AdminOrganizationUpdateService } from "@calcom/features/ee/organizations/lib/service/AdminOrganizationUpdateService";
 import { prisma } from "@calcom/prisma";
-import type { Prisma } from "@calcom/prisma/client";
-import { teamMetadataStrictSchema } from "@calcom/prisma/zod-utils";
 
 import type { TrpcSessionUser } from "../../../types";
 import type { TAdminUpdate } from "./adminUpdate.schema";
@@ -18,129 +13,13 @@ type AdminUpdateOptions = {
 };
 
 export const adminUpdateHandler = async ({ input }: AdminUpdateOptions) => {
-  const { id, organizationSettings, ...restInput } = input;
   const organizationRepository = getOrganizationRepository();
-  const existingOrg = await organizationRepository.findByIdIncludeOrganizationSettings({ id });
-
-  if (!existingOrg) {
-    throw new HttpError({
-      message: "Organization not found",
-      statusCode: 404,
-    });
-  }
-
-  const { mergeMetadata } = getMetadataHelpers(teamMetadataStrictSchema.unwrap(), existingOrg.metadata || {});
-
-  const data: Prisma.TeamUpdateArgs["data"] = restInput;
-  const oldSlug = existingOrg.slug;
-  const newSlug = restInput.slug;
-
-  if (newSlug) {
-    await throwIfSlugConflicts({ id, slug: newSlug });
-    const isSlugChanged = newSlug !== oldSlug;
-    if (isSlugChanged) {
-      // If slug is changed, we need to rename the domain first
-      // If renaming fails, we don't want to update the new slug in DB
-      await renameDomain(oldSlug, newSlug);
-    }
-    data.slug = newSlug;
-    data.metadata = mergeMetadata({
-      // If we save slug, we don't need the requestedSlug anymore
-      requestedSlug: undefined,
-    });
-  }
-
-  const updatedOrganization = await prisma.$transaction(async (tx) => {
-    const updatedOrganization = await tx.team.update({
-      where: { id },
-      data,
-    });
-
-    // Update all TempOrgRedirect records that point to the old org URL to use the new org URL
-    if (newSlug && oldSlug && newSlug !== oldSlug) {
-      const oldOrgUrlPrefix = getOrgFullOrigin(oldSlug);
-      const newOrgUrlPrefix = getOrgFullOrigin(newSlug);
-
-      const redirectsToUpdate = await tx.tempOrgRedirect.findMany({
-        where: {
-          toUrl: {
-            startsWith: oldOrgUrlPrefix,
-          },
-        },
-        select: {
-          id: true,
-          toUrl: true,
-        },
-      });
-
-      for (const redirect of redirectsToUpdate) {
-        const newToUrl = redirect.toUrl.replace(oldOrgUrlPrefix, newOrgUrlPrefix);
-        await tx.tempOrgRedirect.update({
-          where: {
-            id: redirect.id,
-          },
-          data: {
-            toUrl: newToUrl,
-          },
-        });
-      }
-    }
-
-    if (organizationSettings || existingOrg.organizationSettings) {
-      await tx.organizationSettings.update({
-        where: {
-          organizationId: updatedOrganization.id,
-        },
-        data: {
-          isOrganizationConfigured:
-            organizationSettings?.isOrganizationConfigured ||
-            existingOrg.organizationSettings?.isOrganizationConfigured,
-          isOrganizationVerified:
-            organizationSettings?.isOrganizationVerified ||
-            existingOrg.organizationSettings?.isOrganizationVerified,
-          isAdminReviewed: organizationSettings?.isAdminReviewed,
-          orgAutoAcceptEmail:
-            organizationSettings?.orgAutoAcceptEmail || existingOrg.organizationSettings?.orgAutoAcceptEmail,
-          isAdminAPIEnabled: !!(
-            organizationSettings?.isAdminAPIEnabled ?? existingOrg.organizationSettings?.isAdminAPIEnabled
-          ),
-        },
-      });
-    }
-    return updatedOrganization;
+  const adminOrganizationUpdateService = new AdminOrganizationUpdateService({
+    prismaClient: prisma,
+    organizationRepository,
   });
 
-  return updatedOrganization;
+  return adminOrganizationUpdateService.updateOrganization(input);
 };
 
 export default adminUpdateHandler;
-
-async function throwIfSlugConflicts({ id, slug }: { id: number; slug: string }) {
-  const organizationsWithSameSlug = await prisma.team.findMany({
-    where: {
-      slug: slug,
-      parentId: null,
-    },
-  });
-
-  if (organizationsWithSameSlug.length > 1) {
-    throw new HttpError({
-      message: "There can only be one organization with a given slug",
-      statusCode: 400,
-    });
-  }
-
-  const foundOrg = organizationsWithSameSlug[0];
-  if (!foundOrg) {
-    // No org with same slug found - So, no conflict.
-    return;
-  }
-
-  // If foundOrg isn't same as the org being edited
-  if (foundOrg.id !== id) {
-    throw new HttpError({
-      message: "Organization or a Team with same slug already exists",
-      statusCode: 400,
-    });
-  }
-}
