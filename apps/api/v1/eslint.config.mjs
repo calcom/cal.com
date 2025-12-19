946378935f16d34ead1aa3853c393c98c6e4cbaa
niel98 <mosesdaniel201798@gmail.com>
2025-12-19 22:40:36 +0100

    fix: add @calcom/eslint-plugin-eslint to api v1 ESLint config

--- apps/api/v1/eslint.config.mjs
+++ apps/api/v1/eslint.config.mjs
@@ -1,3 +1,14 @@
 import { nextJsConfig } from "@calcom/eslint-config/next-js";
+import calcomEslintPlugin from "@calcom/eslint-plugin-eslint";
 
-export default nextJsConfig;
+export default [
+  ...nextJsConfig,
+  {
+    plugins: {
+      "@calcom/eslint": calcomEslintPlugin,
+    },
+    rules: {
+      ...calcomEslintPlugin.configs.recommended.rules,
+    },
+  },
+];
