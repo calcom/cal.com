505b5116f3a122026868e21d2f104ca640dfc4da
cal.com <morgan@cal.com>
2025-12-20 20:53:05 +0200

    fix: no need to handle errors there is a middleware

--- packages/trpc/server/routers/viewer/oAuth/generateAuthCode.handler.ts
+++ packages/trpc/server/routers/viewer/oAuth/generateAuthCode.handler.ts
@@ -1,11 +1,6 @@
 import { getOAuthService } from "@calcom/features/oauth/di/OAuthService.container";
-import { ErrorWithCode } from "@calcom/lib/errors";
-import { getHttpStatusCode } from "@calcom/lib/server/getServerErrorFromUnknown";
 import type { AccessScope } from "@calcom/prisma/enums";
-import { httpStatusToTrpcCode } from "@calcom/trpc/server/lib/toTRPCError";
 import type { TrpcSessionUser } from "@calcom/trpc/server/types";
-
-import { TRPCError } from "@trpc/server";
 
 import type { TGenerateAuthCodeInputSchema } from "./generateAuthCode.schema";
 
@@ -20,30 +15,16 @@ export const generateAuthCodeHandler = async ({ ctx, input }: AddClientOptions) 
   const { clientId, scopes, teamSlug, codeChallenge, codeChallengeMethod, state, redirectUri } = input;
   const oAuthService = getOAuthService();
 
-  try {
-    const oAuthClientRedirectUri = redirectUri;
-    const { authorizationCode, client } = await oAuthService.generateAuthorizationCode(
-      clientId,
-      ctx.user.id,
-      oAuthClientRedirectUri,
-      scopes as AccessScope[],
-      state,
-      teamSlug,
-      codeChallenge,
-      codeChallengeMethod
-    );
-    return { client, authorizationCode: authorizationCode };
-  } catch (err) {
-    if (err instanceof ErrorWithCode) {
-      throw new TRPCError({
-        code: httpStatusToTrpcCode(getHttpStatusCode(err)),
-        message: err.message,
-      });
-    }
-
-    throw new TRPCError({
-      code: "INTERNAL_SERVER_ERROR",
-      message: "server_error",
-    });
-  }
+  const oAuthClientRedirectUri = redirectUri;
+  const { authorizationCode, client } = await oAuthService.generateAuthorizationCode(
+    clientId,
+    ctx.user.id,
+    oAuthClientRedirectUri,
+    scopes as AccessScope[],
+    state,
+    teamSlug,
+    codeChallenge,
+    codeChallengeMethod
+  );
+  return { client, authorizationCode: authorizationCode };
 };
--- packages/trpc/server/routers/viewer/oAuth/getClient.handler.ts
+++ packages/trpc/server/routers/viewer/oAuth/getClient.handler.ts
@@ -1,8 +1,4 @@
 import { getOAuthService } from "@calcom/features/oauth/di/OAuthService.container";
-import { HttpError } from "@calcom/lib/http-error";
-import { httpStatusToTrpcCode } from "@calcom/trpc/server/lib/toTRPCError";
-
-import { TRPCError } from "@trpc/server";
 
 import type { TGetClientInputSchema } from "./getClient.schema";
 
@@ -15,24 +11,13 @@ export const getClientHandler = async ({ input }: GetClientOptions) => {
 
   const oAuthService = getOAuthService();
 
-  try {
-    const oAuthClient = await oAuthService.getClient(clientId);
+  const oAuthClient = await oAuthService.getClient(clientId);
 
-    return {
-      clientId: oAuthClient.clientId,
-      redirectUri: oAuthClient.redirectUri,
-      name: oAuthClient.name,
-      logo: oAuthClient.logo,
-      isTrusted: oAuthClient.isTrusted,
-    };
-  } catch (err) {
-    if (err instanceof HttpError) {
-      throw new TRPCError({ code: httpStatusToTrpcCode(err.statusCode), message: err.message });
-    }
-
-    throw new TRPCError({
-      code: "INTERNAL_SERVER_ERROR",
-      message: "server_error",
-    });
-  }
+  return {
+    clientId: oAuthClient.clientId,
+    redirectUri: oAuthClient.redirectUri,
+    name: oAuthClient.name,
+    logo: oAuthClient.logo,
+    isTrusted: oAuthClient.isTrusted,
+  };
 };
