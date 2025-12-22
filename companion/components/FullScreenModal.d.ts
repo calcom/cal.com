ad2b5929329d5e513290b4d92d60b9af2bfe2922
Dhairyashil <dhairyashil10101010@gmail.com>
2025-12-23 01:49:37 +0530

    declare FullScreenModal  FullScreenModalProps

--- companion/components/FullScreenModal.d.ts
+++ companion/components/FullScreenModal.d.ts
@@ -1,2 +1,10 @@
-export { FullScreenModal } from "./FullScreenModal.native";
-export type { FullScreenModalProps } from "./FullScreenModal.native";
+import type React from "react";
+import type { ModalProps } from "react-native";
+
+export interface FullScreenModalProps extends ModalProps {
+  visible: boolean;
+  onRequestClose: () => void;
+  children: React.ReactNode;
+}
+
+export declare function FullScreenModal(props: FullScreenModalProps): JSX.Element;
