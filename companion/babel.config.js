5b3778968677bd4e5939dacaa0d7ed19862d00f9
Dhairyashil <dhairyashil10101010@gmail.com>
2025-12-27 21:58:56 +0530

    remove compilation mode 'all', it will use infer by default

--- companion/babel.config.js
+++ companion/babel.config.js
@@ -7,14 +7,7 @@ module.exports = (api) => {
         {
           jsxImportSource: "nativewind",
           "react-compiler": {
-            compilationMode: "all",
             panicThreshold: "all_errors",
-            sources: (filename) => {
-              // Exclude non-React files from compilation
-              if (filename.includes("/services/")) return false;
-              if (filename.includes("/utils/")) return false;
-              return true;
-            },
           },
         },
       ],
