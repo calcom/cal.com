a2776b5d5843d6e3b9bec10f539d8a7239c49ae4
Dhairyashil <dhairyashil10101010@gmail.com>
2026-01-02 02:27:11 +0530

    fix lint issue

--- companion/components/screens/AvailabilityDetailScreen.tsx
+++ companion/components/screens/AvailabilityDetailScreen.tsx
@@ -220,7 +220,7 @@ export const AvailabilityDetailScreen = forwardRef<
         setOverrides([]);
       }
     },
-    [dayNameToNumber]
+    []
   );
 
   const fetchSchedule = useCallback(async () => {
