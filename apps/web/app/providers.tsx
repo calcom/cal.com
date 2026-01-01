1ca478eb4c79b1e7e182bae2fd489600b985fc72
hbjORbj <sldisek783@gmail.com>
2026-01-01 21:26:35 +0900

    fix

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -1,12 +1,15 @@
 "use client";
 
-import { GeoProvider } from "@calcom/features/ee/event-tracking/context/GeoContext";
-import { WebPushProvider } from "@calcom/features/notifications/WebPushContext";
-import { NotificationSoundHandler } from "@calcom/web/components/notification-sound-handler";
-import useIsBookingPage from "@lib/hooks/useIsBookingPage";
 import { TrpcProvider } from "app/_trpc/trpc-provider";
 import { SessionProvider } from "next-auth/react";
 import CacheProvider from "react-inlinesvg/provider";
+
+import { WebPushProvider } from "@calcom/features/notifications/WebPushContext";
+import { NotificationSoundHandler } from "@calcom/web/components/notification-sound-handler";
+
+import useIsBookingPage from "@lib/hooks/useIsBookingPage";
+
+import { GeoProvider } from "./GeoContext";
 
 type ProvidersProps = {
   isEmbed: boolean;
