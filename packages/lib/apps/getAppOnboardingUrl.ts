58f191e3e61c511f6fb7701da88a3898cd6758ad
Volnei Munhoz <volnei.munhoz@gmail.com>
2026-01-02 08:55:43 -0300

    fix small type-check issue

--- packages/lib/apps/getAppOnboardingUrl.ts
+++ packages/lib/apps/getAppOnboardingUrl.ts
@@ -1,9 +1,7 @@
-
 import type { AppOnboardingSteps } from "@calcom/lib/apps/appOnboardingSteps";
 
-const stringify = (obj: Record<string, string>): string =>
+const stringify = (obj: Record<string, string | number | number[]>): string =>
   new URLSearchParams(obj).toString();
-
 
 export const getAppOnboardingUrl = ({
   slug,
