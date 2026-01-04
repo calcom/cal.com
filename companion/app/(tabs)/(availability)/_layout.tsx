c1e5e0ae664bb4f736abb6339ed312f69fa748e7
Dhairyashil Shinde <93669429+dhairyashiil@users.noreply.github.com>
2026-01-04 21:18:37 +0530

    feat(companion): new availability detail and actions pages for ios (#26424)
    * version 1
    
    * version 1.1
    
    * better code
    
    * covered all edge cases
    
    * address cubics comments
    
    * address cubics comments

--- companion/app/(tabs)/(availability)/_layout.tsx
+++ companion/app/(tabs)/(availability)/_layout.tsx
@@ -1,9 +1,11 @@
 import { Stack } from "expo-router";
+import { Platform } from "react-native";
 
 export default function AvailabilityLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="availability-detail" options={{ headerShown: Platform.OS === "ios" }} />
     </Stack>
   );
 }
