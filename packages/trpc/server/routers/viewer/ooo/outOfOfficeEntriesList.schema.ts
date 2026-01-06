9d969ade12dc4b20eba0990a4eadcf8c7d0b7cf6
Adarsh Tiwari <adarshtiwari797023@gmail.com>
2026-01-06 15:57:25 +0530

    fix

--- packages/trpc/server/routers/viewer/ooo/outOfOfficeEntriesList.schema.ts
+++ packages/trpc/server/routers/viewer/ooo/outOfOfficeEntriesList.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const ZOutOfOfficeEntriesListSchema = z.object({
   limit: z.number().min(1).max(100),
-  offset: z.number(),
+  offset: z.number().min(0),
   fetchTeamMembersEntries: z.boolean().optional().default(false),
   searchTerm: z.string().optional(),
   endDateFilterStartRange: z.string().optional(),
