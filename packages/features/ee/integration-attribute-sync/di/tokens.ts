d126a57a3e3fda3b94711b181ed96923c0a94e7e
Joe Au-Yeung <j.auyeung419@gmail.com>
2026-01-07 15:43:58 -0500

    Create DI container for attributeSyncService

--- packages/features/ee/integration-attribute-sync/di/tokens.ts
+++ packages/features/ee/integration-attribute-sync/di/tokens.ts
@@ -3,4 +3,8 @@ export const INTEGRATION_ATTRIBUTE_SYNC_DI_TOKENS = {
   INTEGRATION_ATTRIBUTE_SYNC_SERVICE_MODULE: Symbol("IntegrationAttributeSyncServiceModule"),
   INTEGRATION_ATTRIBUTE_SYNC_REPOSITORY: Symbol("IntegrationAttributeSyncRepository"),
   INTEGRATION_ATTRIBUTE_SYNC_REPOSITORY_MODULE: Symbol("IntegrationAttributeSyncRepositoryModule"),
+  ATTRIBUTE_SYNC_RULE_SERVICE: Symbol("AttributeSyncRuleService"),
+  ATTRIBUTE_SYNC_RULE_SERVICE_MODULE: Symbol("AttributeSyncRuleServiceModule"),
+  ATTRIBUTE_TO_USER_REPOSITORY: Symbol("AttributeToUserRepository"),
+  ATTRIBUTE_TO_USER_REPOSITORY_MODULE: Symbol("AttributeToUserRepositoryModule"),
 };
