ed175b3534c9f039be832c88ca4bd713550b9068
hbjORbj <sldisek783@gmail.com>
2026-01-08 16:58:10 +0900

    wip

--- packages/features/schedules/lib/use-schedule/types.ts
+++
@@ -1,7 +0,0 @@
-import type { RouterOutputs } from "@calcom/trpc/react";
-
-export type Slots = RouterOutputs["viewer"]["slots"]["getSchedule"]["slots"];
-
-export type Slot = Slots[string][number] & { showConfirmButton?: boolean };
-
-export type GetSchedule = RouterOutputs["viewer"]["slots"]["getSchedule"];
