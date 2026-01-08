c8874a7187aaad33d09dd9fbd3173a9f1bebd9a4
Anik Dhabal Babu <81948346+anikdhabal@users.noreply.github.com>
2026-01-09 00:50:41 +0530

    Change conferencing app from 'Whereby' to 'Around'
    Updated the conferencing app from 'Whereby' to 'Around' and adjusted related test cases to skip until more apps are added.

--- apps/web/playwright/apps/conferencing/conferencingApps.e2e.ts
+++ apps/web/playwright/apps/conferencing/conferencingApps.e2e.ts
@@ -1,17 +1,19 @@
 import { test } from "../../lib/fixtures";
 import type { TApp } from "./types";
 
+// Around is not available in the app store anymore
 const APP: TApp = {
-  slug: "whereby",
-  type: "integrations:whereby_video",
-  organizerInputPlaceholder: "https://www.whereby.com/cal",
-  label: "Whereby Video",
+  slug: "around",
+  type: "integrations:around_video",
+  organizerInputPlaceholder: "https://www.around.co/rick",
+  label: "Around Video",
 };
 
 test.describe.configure({ mode: "parallel" });
 test.afterEach(({ users }) => users.deleteAll());
 
-test("check non-oAuth link-based conferencing apps", () => {
+// TODO: remove skip once more apps are added
+test.skip("check non-oAuth link-based conferencing apps", () => {
   test(`check conferencing app: ${APP.slug} by skipping the configure step`, async ({
     appsPage,
     page,
@@ -24,18 +26,12 @@ test("check non-oAuth link-based conferencing apps", () => {
   });
 });
 
-test("check non-oAuth link-based conferencing apps using the new flow", () => {
-  test(`can add ${APP.slug} app and book with it`, async ({
-    appsPage,
-    page,
-    users,
-  }) => {
+test.skip("check non-oAuth link-based conferencing apps using the new flow", () => {
+  test(`can add ${APP.slug} app and book with it`, async ({ appsPage, page, users }) => {
     const user = await users.create();
     await user.apiLogin();
     const eventTypes = await user.getUserEventsAsOwner();
-    const eventTypeIds = eventTypes
-      .map((item) => item.id)
-      .filter((item, index) => index < 1);
+    const eventTypeIds = eventTypes.map((item) => item.id).filter((item, index) => index < 1);
     await appsPage.installConferencingAppNewFlow(APP, eventTypeIds);
     await appsPage.verifyConferencingAppNew(APP, eventTypeIds);
   });
