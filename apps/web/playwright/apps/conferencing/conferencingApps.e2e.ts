33149a1eb3ee573c6dba84135c37a7dd0cf94664
Anik Dhabal Babu <adhabal2002@gmail.com>
2026-01-09 00:25:42 +0530

    test: cleanup & revived  old skipped e2e tests

--- apps/web/playwright/apps/conferencing/conferencingApps.e2e.ts
+++ apps/web/playwright/apps/conferencing/conferencingApps.e2e.ts
@@ -1,19 +1,17 @@
 import { test } from "../../lib/fixtures";
 import type { TApp } from "./types";
 
-// Around is not available in the app store anymore
 const APP: TApp = {
-  slug: "around",
-  type: "integrations:around_video",
-  organizerInputPlaceholder: "https://www.around.co/rick",
-  label: "Around Video",
+  slug: "whereby",
+  type: "integrations:whereby_video",
+  organizerInputPlaceholder: "https://www.whereby.com/cal",
+  label: "Whereby Video",
 };
 
 test.describe.configure({ mode: "parallel" });
 test.afterEach(({ users }) => users.deleteAll());
 
-// TODO: remove skip once more apps are added
-test.skip("check non-oAuth link-based conferencing apps", () => {
+test("check non-oAuth link-based conferencing apps", () => {
   test(`check conferencing app: ${APP.slug} by skipping the configure step`, async ({
     appsPage,
     page,
@@ -26,12 +24,18 @@ test.skip("check non-oAuth link-based conferencing apps", () => {
   });
 });
 
-test.skip("check non-oAuth link-based conferencing apps using the new flow", () => {
-  test(`can add ${APP.slug} app and book with it`, async ({ appsPage, page, users }) => {
+test("check non-oAuth link-based conferencing apps using the new flow", () => {
+  test(`can add ${APP.slug} app and book with it`, async ({
+    appsPage,
+    page,
+    users,
+  }) => {
     const user = await users.create();
     await user.apiLogin();
     const eventTypes = await user.getUserEventsAsOwner();
-    const eventTypeIds = eventTypes.map((item) => item.id).filter((item, index) => index < 1);
+    const eventTypeIds = eventTypes
+      .map((item) => item.id)
+      .filter((item, index) => index < 1);
     await appsPage.installConferencingAppNewFlow(APP, eventTypeIds);
     await appsPage.verifyConferencingAppNew(APP, eventTypeIds);
   });
