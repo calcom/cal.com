2ac36eda9c4bb57d2644f8f5021481ef63576cec
hbjORbj <sldisek783@gmail.com>
2026-01-11 20:31:32 +0900

    fix

--- packages/platform/atoms/booker/types.ts
+++ packages/platform/atoms/booker/types.ts
@@ -111,3 +111,27 @@ export type BookerPlatformWrapperAtomPropsForTeam = BookerPlatformWrapperAtomPro
   routingFormSearchParams?: RoutingFormSearchParams;
   rrHostSubsetIds?: number[];
 };
+
+type SlotInfo = {
+  time: string;
+  attendees?: number;
+  bookingUid?: string;
+  away?: boolean;
+  fromUser?: {
+    id: number;
+    name: string | null;
+    username: string | null;
+  };
+  toUser?: {
+    id: number;
+    name: string | null;
+    username: string | null;
+  };
+  reason?: string;
+  emoji?: string;
+  showNotePublicly?: boolean;
+};
+
+export type GetAvailableSlotsResponse = {
+  slots: Record<string, SlotInfo[]>;
+};
