daf04c45b36a5de2bd8bb6ca2464d371a98a55a9
supalarry <laurisskraucis@gmail.com>
2026-01-12 11:37:11 +0100

    refactor: delete all prisma migrations

--- packages/prisma/migrations/20251222143926_add_website_url_to_oauth_client/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."OAuthClient" ADD COLUMN     "websiteUrl" TEXT;
--- packages/prisma/migrations/20260108133500_oauth_client_default_status_as_accepted/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."OAuthClient" ALTER COLUMN "approvalStatus" SET DEFAULT 'pending';
