734451dd27b739c4f6d52901bc4f0767ead76b34
Sean Brydon <sean@cal.com>
2026-01-13 09:29:00 +0000

    add error log in billing portal

--- packages/trpc/server/routers/viewer/admin/billingPortalLink.handler.ts
+++ packages/trpc/server/routers/viewer/admin/billingPortalLink.handler.ts
@@ -7,6 +7,7 @@ import {
 } from "@calcom/features/ee/deployment/lib/getDeploymentKey";
 import { DeploymentRepository } from "@calcom/features/ee/deployment/repositories/DeploymentRepository";
 import { CALCOM_PRIVATE_API_ROUTE } from "@calcom/lib/constants";
+import logger from "@calcom/lib/logger";
 import { prisma } from "@calcom/prisma";
 
 import type { TrpcSessionUser } from "../../../types";
@@ -56,6 +57,10 @@ const billingPortalLinkHandler = async ({ input }: GetOptions) => {
   const data = await response.json();
 
   if (!response.ok) {
+    logger.warn("Failed to fetch billing portal link", {
+      message: data?.message,
+      status: response.status,
+    });
     throw new Error(data?.message || "Failed to fetch billing portal link");
   }
 
