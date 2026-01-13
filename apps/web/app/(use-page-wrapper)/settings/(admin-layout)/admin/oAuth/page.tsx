6691d50925154bdff60891f5cc90c3a1a2845eec
supalarry <laurisskraucis@gmail.com>
2026-01-13 18:03:51 +0100

    refactor: admin/oauth not allowed if not logged in

--- apps/web/app/(use-page-wrapper)/settings/(admin-layout)/admin/oAuth/page.tsx
+++ apps/web/app/(use-page-wrapper)/settings/(admin-layout)/admin/oAuth/page.tsx
@@ -1,6 +1,23 @@
 import { _generateMetadata, getTranslate } from "app/_utils";
+import { cookies, headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { getServerSession } from "@calcom/features/auth/lib/getServerSession";
+
+import { buildLegacyRequest } from "@lib/buildLegacyCtx";
 
 import OAuthClientsAdminView from "~/settings/admin/oauth-clients-admin-view";
+
+const Page = async () => {
+  const session = await getServerSession({ req: buildLegacyRequest(await headers(), await cookies()) });
+  await getTranslate();
+
+  if (!session) {
+    redirect("/auth/login?callbackUrl=/settings/admin/oauth");
+  }
+
+  return <OAuthClientsAdminView />;
+};
 
 export const generateMetadata = async () =>
   await _generateMetadata(
@@ -10,10 +27,5 @@ export const generateMetadata = async () =>
     undefined,
     "/settings/admin/oauth"
   );
-
-const Page = async () => {
-  const t = await getTranslate();
-  return <OAuthClientsAdminView />;
-};
 
 export default Page;
