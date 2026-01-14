c846e4af285dfa63d2aa11c6d6ca0ae45d531b2c
Anik Dhabal Babu <adhabal2002@gmail.com>
2026-01-14 21:25:21 +0530

    update

--- apps/web/modules/bookings/components/BookingDetailsSheet.tsx
+++ apps/web/modules/bookings/components/BookingDetailsSheet.tsx
@@ -466,7 +466,9 @@ function WhoSection({ booking }: { booking: BookingOutput }) {
             <div className="min-w-0 flex-1">
               <div className="flex items-center gap-2">
                 <p className="text-emphasis truncate text-sm leading-[1.2]">
-                  {booking.user.name || booking.user.email}
+                  {booking.eventType?.hideOrganizerEmail
+                    ? booking.user.name || t("organizer")
+                    : booking.user.name || booking.user.email}
                 </p>
                 <Badge variant="purple" size="sm" className="capitalize">
                   {t("host")}
