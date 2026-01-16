718389b01968519b6020e799a110396e3e579e4d
Udit Takkar <udit222001@gmail.com>
2026-01-16 16:31:43 +0530

    fix: seated event bug

--- packages/features/ee/round-robin/roundRobinManualReassignment.ts
+++ packages/features/ee/round-robin/roundRobinManualReassignment.ts
@@ -446,6 +446,7 @@ export const roundRobinManualReassignment = async ({
       await sendReassignedUpdatedEmailsAndSMS({
         calEvent: evtWithoutCancellationReason,
         eventTypeMetadata: eventType?.metadata as EventTypeMetadata,
+        showAttendees: !!eventType.seatsShowAttendees,
       });
     }
 
