75f697a173f3a32569926488aa35d2b728c75912
cal.com <morgan@cal.com>
2026-01-19 18:11:55 +0200

    fixup! fix: type issues

--- apps/web/test/lib/handleChildrenEventTypes.test.ts
+++ apps/web/test/lib/handleChildrenEventTypes.test.ts
@@ -130,8 +130,6 @@ describe("handleChildrenEventTypes", () => {
 
   describe("Happy paths", () => {
     it("Adds new users", async () => {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
       const {
         schedulingType,
         id,
