c505aef59713abf9d72a1968f3a1e271dfeaf95d
Hariom Balhara <hariombalhara@gmail.com>
2026-01-19 12:30:04 +0530

    feat: allow Segment component to filter matching team members by event type hosts.

--- apps/web/modules/event-types/components/AddMembersWithSwitch.tsx
+++ apps/web/modules/event-types/components/AddMembersWithSwitch.tsx
@@ -1,26 +1,23 @@
-import { useMemo, type ComponentProps, type Dispatch, type SetStateAction } from "react";
-import { useFormContext } from "react-hook-form";
-import { Controller } from "react-hook-form";
-import type { Options } from "react-select";
-
 import { AddMembersWithSwitchPlatformWrapper } from "@calcom/atoms/add-members-switch/AddMembersWithSwitchPlatformWrapper";
-import { AddMembersWithSwitchWebWrapper } from "./AddMembersWithSwitchWebWrapper";
 import { useIsPlatform } from "@calcom/atoms/hooks/useIsPlatform";
-import { Segment } from "@calcom/features/Segment";
 import type {
   FormValues,
   Host,
   SettingsToggleClassNames,
   TeamMember,
 } from "@calcom/features/eventtypes/lib/types";
+import { Segment } from "@calcom/features/Segment";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import type { AttributesQueryValue } from "@calcom/lib/raqb/types";
-import { Label } from "@calcom/ui/components/form";
-import { SettingsToggle } from "@calcom/ui/components/form";
+import { Label, SettingsToggle } from "@calcom/ui/components/form";
+import { type ComponentProps, type Dispatch, type SetStateAction, useMemo } from "react";
+import { Controller, useFormContext } from "react-hook-form";
+import type { Options } from "react-select";
+import { AddMembersWithSwitchWebWrapper } from "./AddMembersWithSwitchWebWrapper";
 
 import AssignAllTeamMembers from "./AssignAllTeamMembers";
-import CheckedTeamSelect from "./CheckedTeamSelect";
 import type { CheckedSelectOption, CheckedTeamSelectCustomClassNames } from "./CheckedTeamSelect";
+import CheckedTeamSelect from "./CheckedTeamSelect";
 
 interface IUserToValue {
   id: number | null;
@@ -193,7 +190,7 @@ export type AddMembersWithSwitchProps = {
   customClassNames?: AddMembersWithSwitchCustomClassNames;
 };
 
-const enum AssignmentState {
+enum AssignmentState {
   TOGGLES_OFF_AND_ALL_TEAM_MEMBERS_NOT_APPLICABLE = "TOGGLES_OFF_AND_ALL_TEAM_MEMBERS_NOT_APPLICABLE",
   TOGGLES_OFF_AND_ALL_TEAM_MEMBERS_APPLICABLE = "TOGGLES_OFF_AND_ALL_TEAM_MEMBERS_APPLICABLE",
   ALL_TEAM_MEMBERS_ENABLED_AND_SEGMENT_APPLICABLE = "ALL_TEAM_MEMBERS_ENABLED_AND_SEGMENT_APPLICABLE",
