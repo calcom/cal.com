199b53c18fdd315da570b0b985a9a257dd8b2422
Anik Dhabal Babu <81948346+anikdhabal@users.noreply.github.com>
2026-01-20 19:56:02 +0530

    Update ShellMainAppDir.tsx

--- apps/web/app/(use-page-wrapper)/(main-nav)/ShellMainAppDir.tsx
+++ apps/web/app/(use-page-wrapper)/(main-nav)/ShellMainAppDir.tsx
@@ -1,5 +1,6 @@
 import { ShellMainAppDirBackButton } from "app/(use-page-wrapper)/(main-nav)/ShellMainAppDirBackButton";
 import classNames from "classnames";
+
 import type { LayoutProps } from "~/shell/Shell";
 
 // Copied from `ShellMain` but with a different `ShellMainAppDirBackButton` import
@@ -10,7 +11,7 @@ export function ShellMainAppDir(props: LayoutProps) {
       {(props.heading || !!props.backPath) && (
         <div
           className={classNames(
-            "flex items-center md:mt-0 md:mb-6",
+            "flex items-center md:mb-6 md:mt-0",
             props.smallHeading ? "lg:mb-7" : "lg:mb-8"
           )}>
           {!!props.backPath && <ShellMainAppDirBackButton backPath={props.backPath} />}
@@ -18,21 +19,25 @@ export function ShellMainAppDir(props: LayoutProps) {
             <header
               className={classNames(
                 props.large && "py-8",
-                "flex w-full max-w-full flex-wrap items-center gap-4"
+                "flex w-full max-w-full items-center flex-wrap md:flex-nowrap gap-2 md:gap-0"
               )}>
               {props.HeadingLeftIcon && <div className="ltr:mr-4">{props.HeadingLeftIcon}</div>}
-              <div className={classNames("hidden min-w-0 flex-1 truncate md:block", props.headerClassName)}>
+              <div
+                className={classNames(
+                  "hidden min-w-0 flex-1 ltr:mr-4 rtl:ml-4 md:block",
+                  props.headerClassName
+                )}>
                 {props.heading && (
                   <h3
                     className={classNames(
-                      "inline max-w-28 truncate font-cal font-semibold text-emphasis text-lg tracking-wide sm:max-w-72 sm:text-xl md:block md:max-w-80 xl:max-w-full",
+                      "font-cal text-emphasis max-w-28 sm:max-w-72 md:max-w-80 inline truncate text-lg font-semibold tracking-wide sm:text-xl md:block xl:max-w-full",
                       props.smallHeading ? "text-base" : "text-xl"
                     )}>
                     {props.heading}
                   </h3>
                 )}
                 {props.subtitle && (
-                  <p className="hidden text-default text-sm lg:block" data-testid="subtitle">
+                  <p className="text-default hidden text-sm md:block" data-testid="subtitle">
                     {props.subtitle}
                   </p>
                 )}
@@ -43,8 +48,8 @@ export function ShellMainAppDir(props: LayoutProps) {
                   className={classNames(
                     props.backPath
                       ? "relative"
-                      : "fixed bottom-20 pwa:bottom-[max(7rem,_calc(5rem_+_env(safe-area-inset-bottom)))] z-40 md:z-auto ltr:right-4 md:ltr:right-0 rtl:left-4 md:rtl:left-0",
-                    "shrink-0 [-webkit-app-region:no-drag] md:relative md:right-auto md:bottom-auto"
+                      : "pwa:bottom-[max(7rem,_calc(5rem_+_env(safe-area-inset-bottom)))] fixed bottom-20 z-40 ltr:right-4 rtl:left-4 md:z-auto md:ltr:right-0 md:rtl:left-0",
+                    "shrink-0 [-webkit-app-region:no-drag] md:relative md:bottom-auto md:right-auto"
                   )}>
                   {props.CTA}
                 </div>
