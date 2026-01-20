cbdf3f0d5e7533101d486f5c54000aba8fff457d
Ryukemeister <sahalrajiv6900@gmail.com>
2026-01-20 17:33:11 +0530

    chore: disable dashboard for creating new platform accounts

--- apps/web/app/(use-page-wrapper)/settings/platform/new/page.tsx
+++ apps/web/app/(use-page-wrapper)/settings/platform/new/page.tsx
@@ -1,39 +1,32 @@
-import { _generateMetadata } from "app/_utils";
+import { getServerSession } from "@calcom/features/auth/lib/getServerSession";
+import prisma from "@calcom/prisma";
+import { buildLegacyRequest } from "@lib/buildLegacyCtx";
 import { cookies, headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-import { getServerSession } from "@calcom/features/auth/lib/getServerSession";
-import LicenseRequired from "~/ee/common/components/LicenseRequired";
-import { WEBAPP_URL } from "@calcom/lib/constants";
-
-import { buildLegacyRequest } from "@lib/buildLegacyCtx";
-
-import LegacyPage, { LayoutWrapper } from "~/settings/platform/new/create-new-view";
-
-export const generateMetadata = async () =>
-  await _generateMetadata(
-    (t) => t("set_up_your_platform_organization"),
-    (t) => t("platform_organization_description"),
-    undefined,
-    undefined,
-    "/settings/platform/new"
-  );
-
 const ServerPage = async () => {
   const session = await getServerSession({ req: buildLegacyRequest(await headers(), await cookies()) });
-  const callbackUrl = `${WEBAPP_URL}/settings/platform/new`;
 
-  if (!session?.user) {
-    redirect(`/login?callbackUrl=${callbackUrl}`);
+  if (!session?.user?.id) {
+    redirect("/login");
   }
 
-  return (
-    <LayoutWrapper>
-      <LicenseRequired>
-        <LegacyPage />
-      </LicenseRequired>
-    </LayoutWrapper>
-  );
+  const user = await prisma.user.findUnique({
+    where: { id: session.user.id },
+    select: {
+      organization: {
+        select: { isPlatform: true },
+      },
+    },
+  });
+
+  const isPlatformUser = user?.organization?.isPlatform ?? false;
+
+  if (isPlatformUser) {
+    redirect("/settings/platform");
+  }
+
+  redirect("/event-types");
 };
 
 export default ServerPage;
