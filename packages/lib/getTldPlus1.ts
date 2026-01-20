6f8b7e683d231c9a42155c21f10940c61209bf23
Dhairyashil <dhairyashil10101010@gmail.com>
2026-01-20 17:44:32 +0530

    use same comment

--- packages/lib/getTldPlus1.ts
+++ packages/lib/getTldPlus1.ts
@@ -1,6 +1,6 @@
 /**
- * Extracts TLD+1 from hostname (e.g., "cal.com" from "app.cal.com")
- * Note: Doesn't support multi-part TLDs like .co.uk
+ * Note: It doesn't support multipart tlds like .co.uk and thus makes only one part tld's safe like .com(and thus cal.com)
+ * If we want to use it elsewhere as well(apart from embed/preview.ts) we must consider Public Suffix List
  */
 export function getTldPlus1(hostname: string): string {
   return hostname.split(".").slice(-2).join(".");
