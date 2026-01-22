d66875ade23921aa1acc07da7b8bbf7538efafad
Lachlan Harris <121010139+lachlanharrisdev@users.noreply.github.com>
2026-01-22 09:14:02 +0000

    fix(umami): add default SCRIPT_URL in zod.ts

--- packages/app-store/umami/zod.ts
+++ packages/app-store/umami/zod.ts
@@ -5,7 +5,7 @@ import { eventTypeAppCardZod } from "@calcom/app-store/eventTypeAppCardZod";
 export const appDataSchema = eventTypeAppCardZod.merge(
   z.object({
     SITE_ID: z.string().optional(),
-    SCRIPT_URL: z.string().optional(),
+    SCRIPT_URL: z.string().optional().default("https://us.umami.is/script.js").or(z.undefined()),
   })
 );
 
