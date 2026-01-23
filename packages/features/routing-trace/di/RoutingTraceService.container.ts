760cfac89d362e6363c1a339f01ada3b2d1b948a
Joe Au-Yeung <j.auyeung419@gmail.com>
2026-01-23 15:52:12 -0500

    Update DI containers

--- packages/features/routing-trace/di/RoutingTraceService.container.ts
+++ packages/features/routing-trace/di/RoutingTraceService.container.ts
@@ -1,8 +1,10 @@
 import { createContainer } from "@calcom/features/di/di";
 
 import type { IPendingRoutingTraceRepository } from "../repositories/PendingRoutingTraceRepository.interface";
+import type { IRoutingTraceRepository } from "../repositories/RoutingTraceRepository.interface";
 import { RoutingTraceService } from "../services/RoutingTraceService";
 import { moduleLoader as pendingRoutingTraceRepositoryModuleLoader } from "./PendingRoutingTraceRepository.module";
+import { moduleLoader as routingTraceRepositoryModuleLoader } from "./RoutingTraceRepository.module";
 
 const routingTraceContainer = createContainer();
 
@@ -11,6 +13,11 @@ function getPendingRoutingTraceRepository(): IPendingRoutingTraceRepository {
   return routingTraceContainer.get<IPendingRoutingTraceRepository>(
     pendingRoutingTraceRepositoryModuleLoader.token
   );
+}
+
+function getRoutingTraceRepository(): IRoutingTraceRepository {
+  routingTraceRepositoryModuleLoader.loadModule(routingTraceContainer);
+  return routingTraceContainer.get<IRoutingTraceRepository>(routingTraceRepositoryModuleLoader.token);
 }
 
 /**
@@ -25,6 +32,17 @@ export function withRoutingTrace<T>(fn: () => T): T {
     },
     fn
   );
+}
+
+/**
+ * Creates a RoutingTraceService instance with both repositories.
+ * Use this for processing pending traces during booking creation.
+ */
+export function getRoutingTraceService(): RoutingTraceService {
+  return new RoutingTraceService({
+    pendingRoutingTraceRepository: getPendingRoutingTraceRepository(),
+    routingTraceRepository: getRoutingTraceRepository(),
+  });
 }
 
 export { RoutingTraceService };
