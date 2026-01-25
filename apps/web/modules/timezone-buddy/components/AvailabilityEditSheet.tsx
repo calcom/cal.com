6a8ee1b7a1a6f694a9d8f8fdde9713b9ac19605b
hbjORbj <sldisek783@gmail.com>
2026-01-26 02:59:55 +0900

    wip

--- apps/web/modules/timezone-buddy/components/AvailabilityEditSheet.tsx
+++ apps/web/modules/timezone-buddy/components/AvailabilityEditSheet.tsx
@@ -3,7 +3,7 @@ import { useFieldArray, useForm, useFormContext } from "react-hook-form";
 import dayjs from "@calcom/dayjs";
 import { TimezoneSelect } from "@calcom/features/components/timezone-select";
 import DateOverrideInputDialog from "@calcom/web/modules/schedules/components/DateOverrideInputDialog";
-import DateOverrideList from "@calcom/web/modules/schedules/components/DateOverrideList";
+import DateOverrideList from "@calcom/features/schedules/components/DateOverrideList";
 import Schedule from "@calcom/web/modules/schedules/components/Schedule";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { HttpError } from "@calcom/lib/http-error";
