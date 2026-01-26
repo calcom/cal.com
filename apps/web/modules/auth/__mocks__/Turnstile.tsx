7f6de1441b4d54b45d3e8576f8f4c6bb6192dfe5
hbjORbj <sldisek783@gmail.com>
2026-01-26 18:52:11 +0900

    fix

--- apps/web/modules/auth/__mocks__/Turnstile.tsx
+++
@@ -1,9 +0,0 @@
-import { vi } from "vitest";
-
-vi.mock("@calcom/web/modules/auth/components/Turnstile", () => ({
-  default: ({ onVerify }: { onVerify: (token: string) => void }) => (
-    <div data-testid="turnstile-captcha" onClick={() => onVerify("test-token")}>
-      Mock Captcha
-    </div>
-  ),
-}));
