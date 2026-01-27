7ed3cc3ca0b7923186132923ea40fd9e64dd2493
hbjORbj <sldisek783@gmail.com>
2026-01-27 14:47:10 +0900

    update barrel file

--- packages/features/data-table/index.ts
+++ packages/features/data-table/index.ts
@@ -3,5 +3,3 @@ export * from "./lib/utils";
 export * from "./lib/serializers";
 export * from "./lib/separator";
 export * from "./DataTableProvider";
-export * from "./hooks/useColumnResizing";
-export * from "./hooks";
