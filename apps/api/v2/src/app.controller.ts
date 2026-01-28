88f397b9c12e0b5d754992981e391eb1179fa33c
cal.com <morgan@cal.com>
2026-01-28 12:57:26 +0200

    chore: trigger.version.ts instead of .env.production

--- apps/api/v2/src/app.controller.ts
+++ apps/api/v2/src/app.controller.ts
@@ -1,12 +1,14 @@
-import { getEnv } from "@/env";
-import { Controller, Get, Version, VERSION_NEUTRAL } from "@nestjs/common";
-import { ApiTags as DocsTags, ApiExcludeController as DocsExcludeController } from "@nestjs/swagger";
-
+import { Controller, Get, VERSION_NEUTRAL, Version } from "@nestjs/common";
 @Controller()
 export class AppController {
   @Get("health")
   @Version(VERSION_NEUTRAL)
   getHealth(): "OK" {
+    console.log("NODE_ENV", process.env.NODE_ENV);
+    console.log("VERCEL_ENV", process.env.VERCEL_ENV);
+    console.log("VERCEL", process.env.VERCEL);
+    console.log("TRIGGER_VERSION", process.env.TRIGGER_VERSION);
+    console.log("CALCOM_ENV", process.env.CALCOM_ENV);
     return "OK";
   }
 }
