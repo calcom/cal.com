d870a77cff942ed670d57d054b06cab4b4e5aeb0
Onehash1 <onehash1@192.168.1.7>
2026-01-30 11:25:06 +0530

    add:so-busy-toggle

--- packages/prisma/zod-utils.ts
+++ packages/prisma/zod-utils.ts
@@ -133,6 +133,7 @@ const _eventTypeMetaDataSchemaWithoutApps = z.object({
     .optional()
     .nullable(),
   billingAddressRequired: z.boolean().optional(),
+  soBusy: z.boolean().optional(),
 });
 
 export const eventTypeMetaDataSchemaWithUntypedApps = _eventTypeMetaDataSchemaWithoutApps.merge(
