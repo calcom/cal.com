7f82bfd60bde52097bf78e635de511823321e797
Romit <romitgabani@icloud.com>
2026-02-05 02:19:52 +0530

    chore

--- apps/web/components/booking/BookingListItem.tsx
+++ apps/web/components/booking/BookingListItem.tsx
@@ -237,7 +237,7 @@ function BookingListItem(booking: BookingItemProps) {
     attendeeList,
     getSeatReferenceUid,
     t,
-  };
+  } as BookingActionContext;
 
   const RequestSentMessage = () => {
     return (
