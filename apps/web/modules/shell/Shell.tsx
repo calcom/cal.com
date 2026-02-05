112ff5302875a0e3d527fc76ea929a0ce7718d77
Deepanshu Verma <deepanshuverma186@email.com>
2026-02-05 20:45:12 +0530

    fix respect disableSticky and add spacing to navbar elements

--- apps/web/modules/shell/Shell.tsx
+++ apps/web/modules/shell/Shell.tsx
@@ -137,13 +137,15 @@ export function ShellMain(props: LayoutProps) {
   return (
     <>
       {(props.heading || !!props.backPath) && (
-        <div
-          style={headerStyle}
-          className={classNames(
-  "bg-default mb-0 flex items-center md:mb-6 md:mt-0 md:w-[calc(100%-3.5rem)] md:pt-3 md:pb-2 md:pl-5 md:left-14 lg:left-56 lg:w-[calc(100%-14rem)]",
-  props.smallHeading ? "lg:mb-7" : "lg:mb-8",
-  !props.disableSticky && "sticky top-0 z-10 md:fixed"
-)} >
+<div
+  className={classNames(
+    "bg-default mb-0 flex items-center md:mb-6 md:mt-0 md:pt-3 md:pb-2 md:pl-5",
+    props.smallHeading ? "lg:mb-7" : "lg:mb-8",
+    !props.disableSticky 
+      ? "sticky top-0 z-10 md:fixed md:w-[calc(100%-3.5rem)] lg:left-56 lg:w-[calc(100%-14rem)] pr-5 "
+      : "md:w-[100%] pr-5"
+  )}
+>
           {!!props.backPath && (
             <Button
               variant="icon"
@@ -196,9 +198,8 @@ export function ShellMain(props: LayoutProps) {
           )}
         </div>
       )}
-      {(props.heading || !!props.backPath) && !props.disableSticky && (
-        <div className="hidden md:block md:h-14" aria-hidden="true" />
-      )}
+
+       {!props.disableSticky && <div className="hidden md:block md:h-20" aria-hidden="true" />}
       {props.afterHeading && <>{props.afterHeading}</>}
       <div className={classNames(props.flexChildrenContainer && "flex flex-1 flex-col")}>
         {props.children}
