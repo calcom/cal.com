d548f3dad52fe5584e60a3459bb05ecdd222d1ca
Ryukemeister <sahalrajiv6900@gmail.com>
2026-02-06 11:07:41 +0530

    fixup

--- apps/web/modules/troubleshooter/components/EventScheduleItem.tsx
+++ apps/web/modules/troubleshooter/components/EventScheduleItem.tsx
@@ -1,12 +1,13 @@
+import { EventScheduleItemComponent } from "@calcom/features/troubleshooter/components/EventScheduleItemComponent";
 import { useTroubleshooterStore } from "@calcom/features/troubleshooter/store";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 import { trpc } from "@calcom/trpc/react";
 import { Badge } from "@calcom/ui/components/badge";
-import { Label } from "@calcom/ui/components/form";
 import Link from "next/link";
-import { TroubleshooterListItemHeader } from "./TroubleshooterListItemContainer";
 
-export function EventScheduleItem() {
+export { EventScheduleItemComponent };
+
+export function EventScheduleItem(): JSX.Element {
   const { t } = useLocale();
   const selectedEventType = useTroubleshooterStore((state) => state.event);
 
@@ -21,26 +22,21 @@ export function EventScheduleItem() {
     );
 
   return (
-    <div>
-      <Label>{t("availability_schedule")}</Label>
-      <TroubleshooterListItemHeader
-        className="group rounded-md border-b"
-        prefixSlot={<div className="w-4 rounded-[4px] bg-black" />}
-        title={schedule?.name ?? "Loading"}
-        suffixSlot={
-          schedule && (
-            <Link href={`/availability/${schedule.id}`} className="inline-flex">
-              <Badge
-                color="orange"
-                size="sm"
-                className="invisible hover:cursor-pointer group-hover:visible"
-              >
-                {t("edit")}
-              </Badge>
-            </Link>
-          )
-        }
-      />
-    </div>
+    <EventScheduleItemComponent
+      schedule={schedule ?? null}
+      suffixSlot={
+        schedule && (
+          <Link href={`/availability/${schedule.id}`} className="inline-flex">
+            <Badge
+              color="orange"
+              size="sm"
+              className="invisible hover:cursor-pointer group-hover:visible"
+            >
+              {t("edit")}
+            </Badge>
+          </Link>
+        )
+      }
+    />
   );
 }
