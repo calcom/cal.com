0263f1079f391ecbf3942f35b6672eb92b57f5b4
arjun3492 <arjun.kohli.330@gmail.com>
2026-02-12 10:51:15 +0530

    calendar-sync package to consume calendar-sync jobs with prisma layer

---
+++ packages/calendar-sync/src/calendars/google/index.ts
@@ -0,0 +1,11 @@
+import { prisma } from "../../prisma.js";
+
+export const handleGoogleCalendarSync = async (userId: number) => {
+  const user = await prisma.user.findUnique({ where: { id: userId } });
+
+  if (!user) {
+    console.error(`User with id ${userId} not found.`);
+    return;
+  }
+  console.log(`Handling Google Calendar sync for user: ${user.email}`);
+};
