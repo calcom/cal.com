69dc084d20fab2d54ae2a55e7d05b728a8c7cf9e
Syed Ali Shahbaz <52925846+alishaz-polymath@users.noreply.github.com>
2026-02-17 15:27:19 +0400

    Revert

---
+++ packages/trpc/server/routers/viewer/eventTypes/heavy/duplicate.handler.test.ts
@@ -0,0 +1,56 @@
+import prismaMock from "@calcom/testing/lib/__mocks__/prismaMock";
+
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { describe, it, expect, vi, beforeEach } from "vitest";
+
+import { TRPCError } from "@trpc/server";
+
+import { duplicateHandler } from "./duplicate.handler";
+
+vi.mock("@calcom/prisma", () => ({
+  default: prismaMock,
+}));
+vi.mock("@calcom/features/eventtypes/repositories/eventTypeRepository");
+
+describe("duplicateHandler", () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const ctx = { user: { id: 1, profile: { id: 1 } } } as any;
+  const input = {
+    id: 123,
+    slug: "test-event",
+    title: "Test",
+    description: "Test",
+    length: 30,
+    teamId: null,
+  };
+  const eventType = { id: 123, userId: 1, teamId: null, users: [{ id: 1 }] };
+
+  beforeEach(() => {
+    vi.resetAllMocks();
+    prismaMock.eventType.findUnique.mockResolvedValue(eventType);
+  });
+
+  it("should throw INTERNAL_SERVER_ERROR in case of unique constraint violation", async () => {
+    const { EventTypeRepository } = await import(
+      "@calcom/features/eventtypes/repositories/eventTypeRepository"
+    );
+    vi.mocked(EventTypeRepository).mockImplementation(function () {
+      return {
+        create: vi.fn().mockRejectedValue(
+          new PrismaClientKnownRequestError("Unique constraint failed", {
+            code: "P2002",
+            clientVersion: "mockedVersion",
+          })
+        ),
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } as any;
+    });
+
+    await expect(duplicateHandler({ ctx, input })).rejects.toThrow(
+      new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Error duplicating event type PrismaClientKnownRequestError: Unique constraint failed",
+      })
+    );
+  });
+});
