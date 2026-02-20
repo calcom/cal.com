c0030bd1d7ef22b423ca3d787b879b8229981687
arjun3492 <arjun.kohli.330@gmail.com>
2026-02-20 15:03:03 +0530

    fix:build issue

--- apps/web/instrumentation.ts
+++ apps/web/instrumentation.ts
@@ -1,12 +1,12 @@
 import * as Sentry from "@sentry/nextjs";
 
-async function registerBullMqCronJobs() {
-  const { setupWhatsappTemplateSync } = await import(
-    "@calcom/whatsapp-business/lib/setupWhatsappTemplateSync"
-  );
+// async function registerBullMqCronJobs() {
+//   const { setupWhatsappTemplateSync } = await import(
+//     "@calcom/whatsapp-business/lib/setupWhatsappTemplateSync"
+//   );
 
-  await setupWhatsappTemplateSync();
-}
+//   await setupWhatsappTemplateSync();
+// }
 
 export async function register() {
   if (process.env.NEXT_PUBLIC_SENTRY_DSN && process.env.NEXT_RUNTIME === "nodejs") {
