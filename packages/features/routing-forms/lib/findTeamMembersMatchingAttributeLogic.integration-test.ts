28d6a0df175cdf89faa66e24f0eb496bb4d32561
Romit <romitgabani1.work@gmail.com>
2026-03-07 20:57:41 +0530

    revert: make test timeout back to 1 min

--- packages/features/routing-forms/lib/findTeamMembersMatchingAttributeLogic.integration-test.ts
+++ packages/features/routing-forms/lib/findTeamMembersMatchingAttributeLogic.integration-test.ts
@@ -1313,7 +1313,7 @@ describe("findTeamMembersMatchingAttributeLogic", () => {
           timeTaken,
         });
         expect(totalTimeTaken).toBeLessThan(5000);
-      }, 120000);
+      }, 60000);
     });
   });
 
