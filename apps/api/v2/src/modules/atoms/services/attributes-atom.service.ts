f55a2fc6f8261199fcd2ffe81bd0d153113e6dbb
Ryukemeister <sahalrajiv6900@gmail.com>
2026-03-20 14:30:13 +0530

    chore: update attributes controller

--- apps/api/v2/src/modules/atoms/services/attributes-atom.service.ts
+++ apps/api/v2/src/modules/atoms/services/attributes-atom.service.ts
@@ -1,22 +1,34 @@
-import { UsersRepository } from "@/modules/users/users.repository";
-import { Logger } from "@nestjs/common";
-import { Injectable } from "@nestjs/common";
-
 import { findTeamMembersMatchingAttributeLogic } from "@calcom/platform-libraries";
-
+import { ForbiddenException, Injectable, Logger } from "@nestjs/common";
 import { FindTeamMembersMatchingAttributeQueryDto } from "../inputs/find-team-members-matching-attribute.input";
+import { MembershipsRepository } from "@/modules/memberships/memberships.repository";
+import { UsersRepository } from "@/modules/users/users.repository";
 
 @Injectable()
 export class AttributesAtomsService {
   private logger = new Logger("AttributesAtomService");
 
-  constructor(private readonly usersRepository: UsersRepository) {}
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly membershipsRepository: MembershipsRepository
+  ) {}
+
+  private async validateTeamMembership(orgId: number, teamId: number, userId: number): Promise<void> {
+    const orgMembership = await this.membershipsRepository.findMembershipByTeamId(orgId, userId);
+    if (!orgMembership) throw new ForbiddenException("User is not a member of this organization.");
+
+    const teamMembership = await this.membershipsRepository.findMembershipByTeamId(teamId, userId);
+    if (!teamMembership) throw new ForbiddenException("User is not a member of this team.");
+  }
 
   async findTeamMembersMatchingAttribute(
     teamId: number,
     orgId: number,
+    userId: number,
     input: FindTeamMembersMatchingAttributeQueryDto
   ) {
+    await this.validateTeamMembership(orgId, teamId, userId);
+
     const {
       teamMembersMatchingAttributeLogic: matchingTeamMembersWithResult,
       mainAttributeLogicBuildingWarnings: mainWarnings,
