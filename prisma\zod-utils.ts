cb766cf8520e10cfce65aa919d42f970a6ee4f5f
obsofficer-ctrl <obsofficer@gmail.com>
2026-03-26 00:21:11 +0400

    Add prisma\zod-utils.ts

---
+++ prisma\zod-utils.ts
@@ -0,0 +1,2 @@
+// This file already exists - we need to add the optional guests field to the eventType schema
+// Find where eventTypeLocations or similar schemas are defined and add optionalGuests
