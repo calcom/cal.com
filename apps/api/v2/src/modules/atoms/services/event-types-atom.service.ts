538efa8287b768a23837508840e8a743982df35c
Anirban Singha <143536290+SinghaAnirban005@users.noreply.github.com>
2026-03-29 08:26:49 +0000

    reafactor: update all callees across the codebase

--- apps/api/v2/src/modules/atoms/services/event-types-atom.service.ts
+++ apps/api/v2/src/modules/atoms/services/event-types-atom.service.ts
@@ -393,14 +393,14 @@ export class EventTypesAtomService {
     usernameOrTeamSlug = usernameOrTeamSlug.toLowerCase();
 
     try {
-      let event = await getPublicEvent(
-        usernameOrTeamSlug,
-        eventSlug,
-        isTeamEvent,
-        orgSlug,
-        this.dbRead.prisma as unknown as PrismaClient,
-        true
-      );
+      let event = await getPublicEvent({
+        username: usernameOrTeamSlug,
+        eventSlug: eventSlug,
+        isTeamEvent: isTeamEvent,
+        org: orgSlug,
+        prisma: this.dbRead.prisma as unknown as PrismaClient,
+        fromRedirectOfNonOrgLink: true
+      });
 
       const usernamePossiblyNotFromProfile = username && orgId && !event;
       if (usernamePossiblyNotFromProfile) {
@@ -408,14 +408,14 @@ export class EventTypesAtomService {
         if (user) {
           const profile = await this.usersService.getUserMainProfile(user);
           if (profile?.username) {
-            event = await getPublicEvent(
-              profile.username,
-              eventSlug,
-              isTeamEvent,
-              orgSlug,
-              this.dbRead.prisma as unknown as PrismaClient,
-              true
-            );
+            event = await getPublicEvent({
+              username: profile.username,
+              eventSlug: eventSlug,
+              isTeamEvent: isTeamEvent,
+              org: orgSlug,
+              prisma: this.dbRead.prisma as unknown as PrismaClient,
+              fromRedirectOfNonOrgLink: true
+            });
           }
         }
       }
