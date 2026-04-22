567d729b685652f0ca0cef63e7c8327211030cb6
Shaizuu <mohamedaymen.elhadhri@medtech.tn>
2026-04-22 21:06:44 +0100

    fix: pre-commit fails when path contains space

--- packages/app-store/payment.services.generated.ts
+++ packages/app-store/payment.services.generated.ts
@@ -3,10 +3,10 @@
     Don't modify this file manually.
 **/
 export const PaymentServiceMap = {
-  alby: import("./alby/lib/PaymentService"),
-  btcpayserver: import("./btcpayserver/lib/PaymentService"),
-  hitpay: import("./hitpay/lib/PaymentService"),
-  "mock-payment-app": import("./mock-payment-app/lib/PaymentService"),
-  paypal: import("./paypal/lib/PaymentService"),
-  stripepayment: import("./stripepayment/lib/PaymentService"),
+"alby": import("./alby/lib/PaymentService"),
+"btcpayserver": import("./btcpayserver/lib/PaymentService"),
+"hitpay": import("./hitpay/lib/PaymentService"),
+"mock-payment-app": import("./mock-payment-app/lib/PaymentService"),
+"paypal": import("./paypal/lib/PaymentService"),
+"stripepayment": import("./stripepayment/lib/PaymentService"),
 };
