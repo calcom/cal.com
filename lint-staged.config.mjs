60f85fab4b8c34c942c6e125c9d40d15b721b1ca
Shaizuu <mohamedaymen.elhadhri@medtech.tn>
2026-04-22 22:07:11 +0100

    fix: pre-commit fail when path contains space

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,5 @@
 export default {
   "(apps|packages|companion)/**/*.{js,ts,jsx,tsx}": (files) =>
-    `biome lint --reporter summary --config-path=biome-staged.json ${files.join(" ")}`,
+    `biome lint --reporter summary --config-path=biome-staged.json ${files.map(f => `"${f}"`).join(" ")}`,
   "packages/prisma/schema.prisma": ["prisma format"],
 };
