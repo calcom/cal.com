60ad9e9d114872de1d5c1ae74e9b328de0c1c0f4
evan <evan@cuwengr.local>
2026-04-27 11:18:30 +0000

    fix(closecom): replace console.log/error with structured logger in CrmService and OAuth callback

--- packages/app-store/closecom/lib/CrmService.ts
+++ packages/app-store/closecom/lib/CrmService.ts
@@ -202,11 +202,11 @@ class CloseComCRMService implements CRM {
   }
 
   getAppOptions() {
-    console.log("No options implemented");
+    this.log.debug("getAppOptions: no options implemented");
   }
 
   async handleAttendeeNoShow() {
-    console.log("Not implemented");
+    this.log.debug("handleAttendeeNoShow: not implemented");
   }
 }
 
