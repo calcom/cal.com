6cb4ac5cde25a4854e4aef9c0c45c89996774e8e
evan <evan@cuwengr.local>
2026-04-27 12:44:40 +0000

    fix(users): replace console.warn with structured logger in deleteUser

--- packages/features/users/lib/deleteUser.ts
+++ packages/features/users/lib/deleteUser.ts
@@ -1,10 +1,13 @@
 import { deleteStripeCustomer } from "@calcom/app-store/stripepayment/lib/customer";
+import logger from "@calcom/lib/logger";
 import prisma from "@calcom/prisma";
 import type { User } from "@calcom/prisma/client";
 
+const log = logger.getSubLogger({ prefix: ["deleteUser"] });
+
 export async function deleteUser(user: Pick<User, "id" | "email" | "metadata">) {
   // If 2FA is disabled or totpCode is valid then delete the user from stripe and database
-  await deleteStripeCustomer(user).catch(console.warn);
+  await deleteStripeCustomer(user).catch((err) => log.warn("Failed to delete Stripe customer", { userId: user.id, err }));
   // Remove my account
   // TODO: Move this to Repository pattern.
   await prisma.user.delete({
