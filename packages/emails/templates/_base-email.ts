404b97a6f1ae5e7780dfe8c2b2e780d70c224b91
evan <evan@cuwengr.local>
2026-04-27 13:09:29 +0000

    fix(emails): replace console.warn/log/error with structured logger in BaseEmail

--- packages/emails/templates/_base-email.ts
+++ packages/emails/templates/_base-email.ts
@@ -4,10 +4,13 @@ import { z } from "zod";
 import dayjs from "@calcom/dayjs";
 import { FeaturesRepository } from "@calcom/features/flags/features.repository";
 import isSmsCalEmail from "@calcom/lib/isSmsCalEmail";
+import logger from "@calcom/lib/logger";
 import { serverConfig } from "@calcom/lib/serverConfig";
 import { getServerErrorFromUnknown } from "@calcom/lib/server/getServerErrorFromUnknown";
 import { setTestEmail } from "@calcom/lib/testEmails";
 import { prisma } from "@calcom/prisma";
+
+const log = logger.getSubLogger({ prefix: ["BaseEmail"] });
 
 import { sanitizeDisplayName } from "../lib/sanitizeDisplayName";
 
@@ -34,7 +37,7 @@ export default class BaseEmail {
     const emailsDisabled = await featuresRepository.checkIfFeatureIsEnabledGlobally("emails");
     /** If email kill switch exists and is active, we prevent emails being sent. */
     if (emailsDisabled) {
-      console.warn("Skipped Sending Email due to active Kill Switch");
+      log.warn("Skipped Sending Email due to active Kill Switch");
       return new Promise((r) => r("Skipped Sending Email due to active Kill Switch"));
     }
 
@@ -42,7 +45,7 @@ export default class BaseEmail {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       //@ts-expect-error
       setTestEmail(await this.getNodeMailerPayload());
-      console.log(
+      log.info(
         "Skipped Sending Email as process.env.NEXT_PUBLIC_UNIT_TESTS is set. Emails are available in globalThis.testEmails"
       );
       return new Promise((r) => r("Skipped sendEmail for Unit Tests"));
@@ -54,7 +57,7 @@ export default class BaseEmail {
     const to = "to" in payload ? (payload.to as string) : "";
 
     if (isSmsCalEmail(to)) {
-      console.log(`Skipped Sending Email to faux email: ${to}`);
+      log.info(`Skipped Sending Email to faux email: ${to}`);
       return new Promise((r) => r(`Skipped Sending Email to faux email: ${to}`));
     }
 
@@ -86,7 +89,7 @@ export default class BaseEmail {
         }
       )
     ).catch((e) =>
-      console.error(
+      log.error(
         "sendEmail",
         `from: ${"from" in payloadWithUnEscapedSubject ? payloadWithUnEscapedSubject.from : ""}`,
         `subject: ${"subject" in payloadWithUnEscapedSubject ? payloadWithUnEscapedSubject.subject : ""}`,
@@ -105,6 +108,6 @@ export default class BaseEmail {
   protected printNodeMailerError(error: Error): void {
     /** Don't clog the logs with unsent emails in E2E */
     if (process.env.NEXT_PUBLIC_IS_E2E) return;
-    console.error(`${this.name}_ERROR`, error);
+    log.error(`${this.name}_ERROR`, error);
   }
 }
