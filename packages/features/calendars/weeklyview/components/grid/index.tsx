cfe1e8bf748d1a67b41ebec85733bfaa281b32cf
akhil-ecc <akhil.ecc@gmail.com>
2026-05-06 09:52:44 +0530

    fix: use nullish coalescing for offsetHeight fallback

--- packages/features/calendars/weeklyview/components/grid/index.tsx
+++ packages/features/calendars/weeklyview/components/grid/index.tsx
@@ -15,7 +15,7 @@ export const SchedulerColumns = React.forwardRef<HTMLOListElement, Props>(functi
     <ol
       ref={ref}
       className="scheduler-grid-row-template scheduler-wrapper col-start-1 col-end-2 row-start-1 grid auto-cols-auto text-[0px]"
-      style={{ marginTop: offsetHeight || "var(--calendar-offset-top)", zIndex }}
+      style={{ marginTop: offsetHeight ?? "var(--calendar-offset-top)", zIndex }}
       data-gridstopsperday={gridStopsPerDay}>
       {children}
     </ol>
