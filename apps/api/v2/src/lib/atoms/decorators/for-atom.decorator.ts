1b514a54c89e09c6f3566bec900081f86985592e
Nguyễn Phương <55752553+akamabu@users.noreply.github.com>
2026-05-13 08:08:15 +0700

    Docs: Document for-atom.decorator.ts

--- apps/api/v2/src/lib/atoms/decorators/for-atom.decorator.ts
+++ apps/api/v2/src/lib/atoms/decorators/for-atom.decorator.ts
@@ -1,3 +1,4 @@
+/** Documents apps/api/v2/src/lib/atoms/decorators/for-atom.decorator.ts module purpose and public usage context */
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 
 export const ForAtom = createParamDecorator((_data: unknown, ctx: ExecutionContext) => {
