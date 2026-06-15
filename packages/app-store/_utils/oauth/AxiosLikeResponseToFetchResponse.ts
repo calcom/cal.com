855293e187273ac91345c53ab7c2e83284006e9b
gmrnlg1971 <gmrnlg1971@gmail.com>
2026-06-15 07:33:08 +0530

    fix: use proper ReadableStream type for body override to fix TS error

--- packages/app-store/_utils/oauth/AxiosLikeResponseToFetchResponse.ts
+++ packages/app-store/_utils/oauth/AxiosLikeResponseToFetchResponse.ts
@@ -8,14 +8,14 @@ export class AxiosLikeResponseToFetchResponse<
     data: unknown;
   }
 > extends Response {
-  body: unknown;
+  body: ReadableStream<Uint8Array> | null;
 
   constructor(axiomResponse: T) {
     super(JSON.stringify(axiomResponse.data), {
       status: axiomResponse.status,
       statusText: axiomResponse.statusText,
     });
-    this.body = axiomResponse.data;
+    this.body = axiomResponse.data as unknown as ReadableStream<Uint8Array> | null;
   }
   async json() {
     return super.json() as unknown as T["data"];
