b943a4f074b73f86e7719b3845a96a7dee50b280
GAURAV07C <gaurav07c@gmail.com>
2026-06-26 01:33:46 +0530

    fix(ui): render markdown headings properly in event descriptions

--- packages/lib/markdownToSafeHTMLClient.ts
+++ packages/lib/markdownToSafeHTMLClient.ts
@@ -1,6 +1,5 @@
-import DOMPurify from "dompurify";
-
 import { md } from "@calcom/lib/markdownIt";
+import DOMPurify from "dompurify";
 
 if (typeof window == "undefined") {
   console.warn(
@@ -24,7 +23,13 @@ export function markdownToSafeHTMLClient(markdown: string | null) {
       /<ol>/g,
       "<ol style='list-style-type: decimal; list-style-position: inside; margin-left: 12px; margin-bottom: 4px'>"
     )
-    .replace(/<a\s+href=/g, "<a target='_blank' class='text-blue-500 hover:text-blue-600' href=");
+    .replace(/<a\s+href=/g, "<a target='_blank' class='text-blue-500 hover:text-blue-600' href=")
+    .replace(/<h1/g, "<h1 class='mt-6 mb-4 text-2xl font-semibold'")
+    .replace(/<h2/g, "<h2 class='mt-5 mb-3 text-xl font-semibold'")
+    .replace(/<h3/g, "<h3 class='mt-4 mb-2 text-lg font-semibold'")
+    .replace(/<h4/g, "<h4 class='mt-4 mb-2 text-base font-semibold'")
+    .replace(/<h5/g, "<h5 class='mt-4 mb-2 text-sm font-semibold'")
+    .replace(/<h6/g, "<h6 class='mt-4 mb-2 text-xs font-semibold'");
 
   // Match: <li>Some text </li><li><ul>...</ul></li> or
   // Convert to: <li>Some text <ul>...</ul></li>
