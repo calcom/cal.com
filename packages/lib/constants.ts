d481ffe038a8b73f48d6c7d0f2db2028f04c9911
oliverkierepka <mrmoree2@gmail.com>
2026-06-27 19:46:46 +0000

    fix(config): change ports back to 3000

--- packages/lib/constants.ts
+++ packages/lib/constants.ts
@@ -28,11 +28,11 @@ export const WEBAPP_URL =
   RAILWAY_STATIC_URL ||
   HEROKU_URL ||
   RENDER_URL ||
-  "http://localhost:4000";
+  "http://localhost:3000";
 
 // OAuth needs to have HTTPS(which is not generally setup locally) and a valid tld(*.local isn't a valid tld)
 // So for development purpose, we would stick to localhost only
-export const WEBAPP_URL_FOR_OAUTH = IS_PRODUCTION || IS_DEV ? WEBAPP_URL : "http://localhost:4000";
+export const WEBAPP_URL_FOR_OAUTH = IS_PRODUCTION || IS_DEV ? WEBAPP_URL : "http://localhost:3000";
 
 /** @deprecated use `WEBAPP_URL` */
 export const BASE_URL = WEBAPP_URL;
