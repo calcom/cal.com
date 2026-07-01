3b0bb1acbce2ea53cf03a4d2ab66849209e5fb1a
GAURAV07C <gaurav07c@gmail.com>
2026-07-02 01:08:52 +0530

    fix(ui): revert external link changes in markdownToSafeHTML

--- packages/lib/markdownToSafeHTMLClient.ts
+++ packages/lib/markdownToSafeHTMLClient.ts
@@ -25,11 +25,8 @@ export function markdownToSafeHTMLClient(markdown: string | null) {
     )
     .replace(/<a\s+href=/g, "<a target='_blank' class='text-blue-500 hover:text-blue-600' href=")
     .replace(/<h1/g, "<h1 class='mt-6 mb-4 text-2xl font-semibold'")
-    .replace(/<h2/g, "<h2 class='mt-5 mb-3 text-xl font-semibold'")
-    .replace(/<h3/g, "<h3 class='mt-4 mb-2 text-lg font-semibold'")
-    .replace(/<h4/g, "<h4 class='mt-4 mb-2 text-base font-semibold'")
-    .replace(/<h5/g, "<h5 class='mt-4 mb-2 text-sm font-semibold'")
-    .replace(/<h6/g, "<h6 class='mt-4 mb-2 text-xs font-semibold'");
+    .replace(/<h2/g, "<h2 class='mt-5 mb-3 text-xl font-semibold'");
+    
 
   // Match: <li>Some text </li><li><ul>...</ul></li> or
   // Convert to: <li>Some text <ul>...</ul></li>
