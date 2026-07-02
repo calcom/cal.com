ba0b3c9c3a65cc25ab9b926fb47c24c0a0adca5c
Goran <Gdinov@gmail.com>
2026-07-02 23:07:30 +0200

    Refactor EmailBodyLogo to use Html component

--- packages/emails/src/components/EmailBodyLogo.tsx
+++ packages/emails/src/components/EmailBodyLogo.tsx
@@ -1,50 +1,171 @@
-import { WEBAPP_URL } from "@calcom/lib/constants";
+/* eslint-disable @next/next/no-head-element */
+import BaseTable from "./BaseTable";
+import EmailBodyLogo from "./EmailBodyLogo";
+import EmailHead from "./EmailHead";
+import EmailScheduledBodyHeaderContent from "./EmailScheduledBodyHeaderContent";
+import EmailSchedulingBodyDivider from "./EmailSchedulingBodyDivider";
+import type { BodyHeadType } from "./EmailSchedulingBodyHeader";
+import RawHtml from "./RawHtml";
+import Row from "./Row";
 
-const EmailBodyLogo = () => {
-  const image = `${WEBAPP_URL}/sunset-logo.png`;
+const Html = (props: { children: React.ReactNode }) => (
+  <>
+    <RawHtml html="<!doctype html>" />
+    <html>{props.children}</html>
+  </>
+);
 
+export const BaseEmailHtml = (props: {
+  children: React.ReactNode;
+  callToAction?: React.ReactNode;
+  subject: string;
+  title?: string;
+  subtitle?: React.ReactNode | string;
+  headerType?: BodyHeadType;
+  hideLogo?: boolean;
+}) => {
   return (
-    <table
-      role="presentation"
-      cellPadding="0"
-      cellSpacing="0"
-      style={{ width: "100%", borderCollapse: "collapse" }}>
-      <tbody>
-        <tr>
-          <td align="center" style={{ padding: "28px 25px 6px 25px" }}>
-            <img
-              src={image}
-              alt="Sunset Services U.S."
-              width="200"
-              style={{
-                display: "block",
-                margin: "0 auto",
-                width: "200px",
-                maxWidth: "70%",
-                height: "auto",
-                border: "0",
-                outline: "none",
-                textDecoration: "none",
-              }}
-            />
-          </td>
-        </tr>
-        <tr>
-          <td
-            align="center"
+    <Html>
+      <EmailHead title={props.subject} />
+      <body style={{ wordSpacing: "normal", backgroundColor: "#F3F4F6" }}>
+        <div style={{ backgroundColor: "#F3F4F6" }}>
+          <RawHtml
+            html={`<!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->`}
+          />
+          {!Boolean(props.hideLogo) && <EmailBodyLogo />}
+          <div
             style={{
-              padding: "0 25px 14px 25px",
-              fontFamily: "Arial, Helvetica, sans-serif",
-              fontSize: "13px",
-              letterSpacing: "2px",
-              color: "#8C8C8C",
+              margin: "0px auto",
+              maxWidth: 600,
+              borderRadius: "8px",
+              border: "1px solid #E5E7EB",
+              padding: "2px",
+              backgroundColor: "#FFFFFF",
             }}>
-            Craft Your Outdoor Legacy
-          </td>
-        </tr>
-      </tbody>
-    </table>
+            {props.title && (
+              <EmailScheduledBodyHeaderContent
+                headStyles={{ border: 0 }}
+                title={props.title}
+                subtitle={props.subtitle}
+              />
+            )}
+            {(props.title || props.subtitle) && (
+              <EmailSchedulingBodyDivider headStyles={{ border: 0 }} />
+            )}
+
+            <RawHtml
+              html={`<!--[if mso | IE]></td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" className="" style="width:600px;" width="600" bgcolor="#FFFFFF" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->`}
+            />
+            <div
+              style={{
+                background: "#FFFFFF",
+                backgroundColor: "#FFFFFF",
+                margin: "0px auto",
+                maxWidth: 600,
+              }}>
+              <Row
+                align="center"
+                border="0"
+                style={{ background: "#FFFFFF", backgroundColor: "#FFFFFF", width: "100%" }}>
+                <td
+                  style={{
+                    direction: "ltr",
+                    fontSize: 0,
+                    padding: 0,
+                    textAlign: "center",
+                  }}>
+                  <RawHtml
+                    html={`<!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td className="" style="vertical-align:top;width:598px;" ><![endif]-->`}
+                  />
+                  <div
+                    className="mj-column-per-100 mj-outlook-group-fix"
+                    style={{
+                      fontSize: 0,
+                      textAlign: "left",
+                      direction: "ltr",
+                      display: "inline-block",
+                      verticalAlign: "top",
+                      width: "100%",
+                    }}>
+                    <Row border="0" style={{ verticalAlign: "top" }} width="100%">
+                      <td align="left" style={{ fontSize: 0, padding: "10px 25px", wordBreak: "break-word" }}>
+                        <div
+                          style={{
+                            fontFamily: "Roboto, Helvetica, sans-serif",
+                            fontSize: 16,
+                            fontWeight: 500,
+                            lineHeight: 1,
+                            textAlign: "left",
+                            color: "#101010",
+                          }}>
+                          {props.children}
+                        </div>
+                      </td>
+                    </Row>
+                  </div>
+                  <RawHtml html="<!--[if mso | IE]></td></tr></table><![endif]-->" />
+                </td>
+              </Row>
+            </div>
+            {props.callToAction && <EmailSchedulingBodyDivider headStyles={{ border: 0 }} />}
+            <RawHtml
+              html={`<!--[if mso | IE]></td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" className="" style="width:600px;" width="600" bgcolor="#FFFFFF" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->`}
+            />
+
+            <div
+              style={{
+                background: "#FFFFFF",
+                backgroundColor: "#FFFFFF",
+                margin: "0px auto",
+                maxWidth: 600,
+              }}>
+              <Row
+                align="center"
+                border="0"
+                style={{ background: "#FFFFFF", backgroundColor: "#FFFFFF", width: "100%" }}>
+                <td
+                  style={{
+                    direction: "ltr",
+                    fontSize: 0,
+                    padding: 0,
+                    textAlign: "center",
+                  }}>
+                  <RawHtml
+                    html={`<!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td className="" style="vertical-align:top;width:598px;" ><![endif]-->`}
+                  />
+                  {props.callToAction && (
+                    <div
+                      className="mj-column-per-100 mj-outlook-group-fix"
+                      style={{
+                        fontSize: 0,
+                        textAlign: "left",
+                        direction: "ltr",
+                        display: "inline-block",
+                        verticalAlign: "top",
+                        width: "100%",
+                      }}>
+                      <BaseTable border="0" style={{ verticalAlign: "top" }} width="100%">
+                        <tbody>
+                          <tr>
+                            <td
+                              align="center"
+                              vertical-align="middle"
+                              style={{ fontSize: 0, padding: "10px 25px", wordBreak: "break-word" }}>
+                              {props.callToAction}
+                            </td>
+                          </tr>
+                        </tbody>
+                      </BaseTable>
+                    </div>
+                  )}
+                  <RawHtml html="<!--[if mso | IE]></td></tr></table><![endif]-->" />
+                </td>
+              </Row>
+            </div>
+          </div>
+          <RawHtml html="<!--[if mso | IE]></td></tr></table><![endif]-->" />
+        </div>
+      </body>
+    </Html>
   );
 };
-
-export default EmailBodyLogo;
