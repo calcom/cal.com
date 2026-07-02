b2174c350fcf85fb3388352989d42c261ecf555f
Goran <Gdinov@gmail.com>
2026-07-02 20:06:40 +0200

    Update email logo to sunset logo

--- packages/emails/src/components/EmailBodyLogo.tsx
+++ packages/emails/src/components/EmailBodyLogo.tsx
@@ -1,78 +1,49 @@
 import { WEBAPP_URL } from "@calcom/lib/constants";
 
-import RawHtml from "./RawHtml";
-import Row from "./Row";
-
-const CommentIE = ({ html = "" }) => <RawHtml html={`<!--[if mso | IE]>${html}<![endif]-->`} />;
-
 const EmailBodyLogo = () => {
-  const image = `${WEBAPP_URL}/emails/logo.png`;
+  const image = `${WEBAPP_URL}/sunset-logo.png`;
 
   return (
-    <>
-      <CommentIE
-        html={`</td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"></td>`}
-      />
-      <div style={{ margin: "0px auto", maxWidth: 600 }}>
-        <Row align="center" border="0" style={{ width: "100%" }}>
-          <td
-            style={{
-              direction: "ltr",
-              fontSize: "0px",
-              padding: "0px",
-              textAlign: "center",
-            }}>
-            <CommentIE
-              html={`<table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td style="vertical-align:top;width:600px;" >`}
-            />
-            <div
-              className="mj-column-per-100 mj-outlook-group-fix"
+    <table
+      role="presentation"
+      cellPadding="0"
+      cellSpacing="0"
+      style={{ width: "100%", borderCollapse: "collapse" }}>
+      <tbody>
+        <tr>
+          <td align="center" style={{ padding: "28px 25px 6px 25px" }}>
+            <img
+              src={image}
+              alt="Sunset Services U.S."
+              width="200"
               style={{
-                fontSize: "0px",
-                textAlign: "left",
-                direction: "ltr",
-                display: "inline-block",
-                verticalAlign: "top",
-                width: "100%",
-              }}>
-              <Row border="0" style={{ verticalAlign: "top" }} width="100%">
-                <td
-                  align="center"
-                  style={{
-                    fontSize: "0px",
-                    padding: "10px 25px",
-                    paddingTop: "32px",
-                    wordBreak: "break-word",
-                  }}>
-                  <Row border="0" style={{ borderCollapse: "collapse", borderSpacing: "0px" }}>
-                    <td style={{ width: "89px" }}>
-                      <a href={WEBAPP_URL} target="_blank" rel="noreferrer">
-                        <img
-                          height="19"
-                          src={image}
-                          style={{
-                            border: "0",
-                            display: "block",
-                            outline: "none",
-                            textDecoration: "none",
-                            height: "19px",
-                            width: "100%",
-                            fontSize: "13px",
-                          }}
-                          width="89"
-                          alt=""
-                        />
-                      </a>
-                    </td>
-                  </Row>
-                </td>
-              </Row>
-            </div>
-            <CommentIE html="</td></tr></table>" />
+                display: "block",
+                margin: "0 auto",
+                width: "200px",
+                maxWidth: "70%",
+                height: "auto",
+                border: "0",
+                outline: "none",
+                textDecoration: "none",
+              }}
+            />
           </td>
-        </Row>
-      </div>
-    </>
+        </tr>
+        <tr>
+          <td
+            align="center"
+            style={{
+              padding: "0 25px 14px 25px",
+              fontFamily: "Arial, Helvetica, sans-serif",
+              fontSize: "13px",
+              letterSpacing: "2px",
+              color: "#8C8C8C",
+            }}>
+            Craft Your Outdoor Legacy
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
