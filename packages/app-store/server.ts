35bee2f3878bb4114aa28b1fcab4968daa0e181b
Anirban Singha <143536290+SinghaAnirban005@users.noreply.github.com>
2026-07-05 13:24:22 +0000

    chore: removed unused imports

--- packages/app-store/server.ts
+++ packages/app-store/server.ts
@@ -2,11 +2,9 @@ import type { TFunction } from "i18next";
 
 import { enrichUserWithDelegationConferencingCredentialsWithoutOrgId } from "@calcom/app-store/delegationCredential";
 import { defaultVideoAppCategories } from "@calcom/app-store/utils";
-import { buildNonDelegationCredentials } from "@calcom/lib/delegationCredential";
 import { prisma } from "@calcom/prisma";
 import type { Prisma } from "@calcom/prisma/client";
 import { AppCategories } from "@calcom/prisma/enums";
-import { credentialForCalendarServiceSelect } from "@calcom/prisma/selects/credential";
 import { PrismaCredentialRepository } from "./repositories/PrismaCredentialRepository";
 
 import getEnabledAppsFromCredentials from "./_utils/getEnabledAppsFromCredentials";
