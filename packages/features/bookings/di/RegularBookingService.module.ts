854c96d3fdc0abc5e2d87e048df27affcfad4712
SinghaAnirban005 <anirbansingha20@gmail.com>
2026-08-28 23:02:41 +0530

    refactor: remove direct prismaClient dependency from RegularBookingService DI module

--- packages/features/bookings/di/RegularBookingService.module.ts
+++ packages/features/bookings/di/RegularBookingService.module.ts
@@ -1,14 +1,16 @@
 import { moduleLoader as bookingEventHandlerModuleLoader } from "@calcom/features/bookings/di/BookingEventHandlerService.module";
+import { moduleLoader as bookingSeatRepositoryModuleLoader } from "@calcom/features/bookings/di/BookingSeatRepository.module";
 import { RegularBookingService } from "@calcom/features/bookings/lib/service/RegularBookingService";
+import { moduleLoader as credentialRepositoryModuleLoader } from "@calcom/features/credentials/di/CredentialRepository.module";
 import { bindModuleToClassOnToken, createModule, type ModuleLoader } from "@calcom/features/di/di";
 import { moduleLoader as bookingRepositoryModuleLoader } from "@calcom/features/di/modules/Booking";
 import { moduleLoader as checkBookingAndDurationLimitsModuleLoader } from "@calcom/features/di/modules/CheckBookingAndDurationLimits";
 import { moduleLoader as luckyUserServiceModuleLoader } from "@calcom/features/di/modules/LuckyUser";
-import { moduleLoader as prismaModuleLoader } from "@calcom/features/di/modules/Prisma";
 import { moduleLoader as userRepositoryModuleLoader } from "@calcom/features/di/modules/User";
 import { DI_TOKENS } from "@calcom/features/di/tokens";
 import { moduleLoader as webhookProducerModuleLoader } from "@calcom/features/di/webhooks/modules/WebhookProducerService.module";
 import { moduleLoader as hashedLinkServiceModuleLoader } from "@calcom/features/hashedLink/di/HashedLinkService.module";
+import { moduleLoader as profileRepositoryModuleLoader } from "@calcom/features/users/di/Profile.module";
 import { moduleLoader as bookingEmailAndSmsTaskerModuleLoader } from "./tasker/BookingEmailAndSmsTasker.module";
 
 const thisModule = createModule();
@@ -20,10 +22,11 @@ const loadModule = bindModuleToClassOnToken({
   token,
   classs: RegularBookingService,
   depsMap: {
-    // TODO: In a followup PR, we aim to remove prisma dependency and instead inject the repositories as dependencies.
-    prismaClient: prismaModuleLoader,
     checkBookingAndDurationLimitsService: checkBookingAndDurationLimitsModuleLoader,
     bookingRepository: bookingRepositoryModuleLoader,
+    bookingSeatRepository: bookingSeatRepositoryModuleLoader,
+    profileRepository: profileRepositoryModuleLoader,
+    credentialRepository: credentialRepositoryModuleLoader,
     luckyUserService: luckyUserServiceModuleLoader,
     userRepository: userRepositoryModuleLoader,
     hashedLinkService: hashedLinkServiceModuleLoader,
